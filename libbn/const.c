13eaa975fc1e2408e8e7cfe6608d6541908bb1f2
Michael John Muuss <mike@brlcad.org>
1990-02-10 00:29:45 +0000

    Added pi/4 and 1/(pi*4)
    svn:revision:3946
    cvs:account:mike
    cvs:branch:trunk

--- libbn/const.c
+++ libbn/const.c
@@ -17,15 +17,16 @@ static char RCSmat[] = "@(#)$Header$ (BRL)";
 #endif
 
 #include <stdio.h>
-#include <math.h>
 #include "machine.h"
 #include "vmath.h"
 #include "raytrace.h"
 
-CONST double rt_pi     = 3.14159265358979323846;	/* pi */
-CONST double rt_twopi  = 6.28318530717958647692;	/* pi*2 */
-CONST double rt_halfpi = 1.57079632679489661923;	/* pi/2 */
-CONST double rt_invpi  = 0.318309886183790671538;	/* 1/pi */
-CONST double rt_inv2pi = 0.159154943091895335769;	/* 1/(pi*2) */
+CONST double rt_pi	= 3.14159265358979323846;	/* pi */
+CONST double rt_twopi	= 6.28318530717958647692;	/* pi*2 */
+CONST double rt_halfpi	= 1.57079632679489661923;	/* pi/2 */
+CONST double rt_quarterpi=0.78539816339744830961;	/* pi/4 */
+CONST double rt_invpi	= 0.318309886183790671538;	/* 1/pi */
+CONST double rt_inv2pi	= 0.159154943091895335769;	/* 1/(pi*2) */
+CONST double rt_inv4pi	= 0.07957747154594766788;	/* 1/(pi*4) */
 
 CONST double rt_inv255 = 1.0/255.0;
