41e987447bf3d0944ca5efe366c0a0fd414e4da2
Christopher Johnson <cjohnson@brlcad.org>
1990-04-10 01:02:29 +0000

    Fix order indices.  Works now.
    svn:revision:4024
    cvs:account:cjohnson
    cvs:branch:trunk

--- halftone/tone_classic.c
+++ halftone/tone_classic.c
@@ -3,8 +3,16 @@ static char rcsid[] = "$Header$";
 #endif
 #include <stdio.h>
 extern int Debug;
-#define THRESHOLD	127
-/*	tone_simple	thresh hold method
+
+static unsigned char	ordered[6][6] = {
+	{5,4,3,14,15,16},
+	{6,1,2,13,18,17},
+	{9,7,8,10,12,11},
+	{14,15,16,5,4,3},
+	{13,18,17,6,1,2},
+	{10,12,11,9,7,8}};
+
+/*	tone_classic	classic diaginal clustered halftones.
  *
  * Entry:
  *	Pix	Pixel value	0-255
@@ -30,17 +38,14 @@ extern int Debug;
  *	Christopher T. Johnson	- 90/03/21
  *
  * $Log$
- * Revision 1.2  90/04/09  17:05:37  cjohnson
- * define Pix to be of type integer.
- * 
  * Revision 1.1  90/04/09  16:16:37  cjohnson
  * Initial revision
  * 
  */
-tone_simple(Pix,X,Y,NX,NY,New)
+tone_classic(Pix,X,Y,NX,NY,New)
 int	Pix;
 int	X, Y, NX, NY;
 int	New;
 {
-	return(Pix>THRESHOLD);
+	return (Pix >= 14*ordered[( X + 3 ) % 6 ][ Y % 6] );
 }
