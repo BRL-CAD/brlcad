0267c57ead025fedcc3ac83720d5e3e43328c2e7
Christopher Johnson <cjohnson@brlcad.org>
1990-04-10 03:34:21 +0000

    Add Intensity Levels Add tonescale mapping
    svn:revision:4031
    cvs:account:cjohnson
    cvs:branch:trunk

--- halftone/main.c
+++ halftone/main.c
@@ -37,6 +37,9 @@ static char rcsid[] = "$Header$";
  *	Christopher T. Johnson	- 90/03/21
  *
  * $Log$
+ * Revision 1.3  90/04/10  01:04:08  cjohnson
+ * Add Classic halftone method
+ * 
  * Revision 1.2  90/04/09  17:12:26  cjohnson
  * Finished parameter cracking and processing threshold halftoning.
  * 
@@ -53,7 +56,7 @@ double Beta;	/* Beta for sharpening */
 #define	M_FOLLY 3
 int Method=M_FLOYD;	/* Method of halftoning */
 int Surpent=0;		/* use serpentine scan lines */
-int Levels=2;		/* Number of levels */
+int Levels=1;		/* Number of levels-1 */
 int Debug=0;
 
 
@@ -89,9 +92,11 @@ char **argv;
 			Surpent = 1;
 		break;
 		case 'I':
-			Levels = atoi(optarg);
+			Levels = atoi(optarg)-1;
+			if (Levels < 1) Levels = 1;
 		break;
-		case 't':
+		case 'T':
+			--optind;
 			for(i=optind; i < argc && (isdigit(*argv[i]) || 
 			    (*argv[i] == '-' && isdigit(*(argv[i]+1)))) ; i++);
 			if ((c=i-optind) % 2) {
@@ -101,14 +106,14 @@ char **argv;
 			Xlist = (int *) malloc((c+2)*sizeof(int));
 			Ylist = (int *) malloc((c+2)*sizeof(int));
 
-			for (j=0;optind <= i; optind++) {
+			for (j=0;optind < i; ) {
 				Xlist[j] = atoi(argv[optind++]);
 				Ylist[j] = atoi(argv[optind++]);
 				j++;
 			}
 			Xlist[j] = 1024;
 			Ylist[j] = 1024;
-			(void) cubic_init(c,Xlist,Ylist);
+			(void) cubic_init(c-1,Xlist,Ylist);
 			free(Xlist);
 			free(Ylist);
 		break;
@@ -126,12 +131,23 @@ main(argc,argv)
 int argc;
 char **argv;
 {
-	int pixel,x,y;
+	int pixel,x,y,i;
 	unsigned char *Line;
 	int NewFlag = 1;
+	int scale;
+	unsigned char Map[256];
 
 	setup(argc,argv);
-	
+	(void) tonescale(Map,1.0,0.0,0);
+	scale = 255/Levels;
+
+	if (Debug) {
+		fprintf(stderr,"Debug = %d, scale = %d\n",Debug, scale);
+	}
+	if (Debug>2) {
+		for(i=0;i<256;i++) fprintf(stderr,"%d ",Map[i]);
+		fprintf(stderr,"\n");
+	}
 	Line = (unsigned char *) malloc(width);
 
 	for (y=0; y<height; y++) {
@@ -140,16 +156,19 @@ char **argv;
 (void)		fread(Line,1,width,stdin);
 		for (x=0; x<width; x++) {
 			NextX = (x == width-1) ? 0 : x+1;
-			pixel = Line[x];
+			pixel = Map[Line[x]];
 			switch (Method) {
-			case M_FLOYD:
 			case M_FOLLY:
+				putchar(scale*tone_folly(pixel, x, y, NextX,
+				    y+1, NewFlag));
+			break;
+			case M_FLOYD:
 			case M_THRESH:
-				putchar(255*tone_simple(pixel, x, y, NextX,
+				putchar(scale*tone_simple(pixel, x, y, NextX,
 				    y+1, NewFlag));
 			break;
 			case M_CLASSIC:
-				putchar(255*tone_classic(pixel, x, y, NextX,
+				putchar(scale*tone_classic(pixel, x, y, NextX,
 				    y+1, NewFlag));
 			break;
 			}
