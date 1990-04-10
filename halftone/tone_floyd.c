dd4c6246d34342fd8ece802b5133ef42b50e8283
Christopher Johnson <cjohnson@brlcad.org>
1990-04-10 05:23:24 +0000

    Simple Floyd-Steinburg dither.
    svn:revision:4033
    cvs:account:cjohnson
    cvs:branch:trunk

---
+++ halftone/tone_floyd.c
@@ -0,0 +1,80 @@
+#ifndef lint
+static char rcsid[] = "$Header$";
+#endif
+#include <stdio.h>
+extern int Debug;
+extern int Levels;
+extern int width;
+
+/*	tone_floyd	floyd-steinberg dispersed error method.
+ *
+ * Entry:
+ *	Pix	Pixel value	0-255
+ *	X	Current column
+ *	Y	Current row
+ *	NX	Next column
+ *	NY	Next row
+ *	New	New row flag.
+ *
+ * Exit:
+ *	returns	0 or 1
+ *
+ * Uses:
+ *	None.
+ *
+ * Calls:
+ *	None.
+ *
+ * Method:
+ *	straight-forward.
+ *
+ * Author:
+ *	Christopher T. Johnson	- 90/03/21
+ *
+ * $Log$
+ */
+tone_floyd(Pix,X,Y,NX,NY,New)
+int	Pix;
+int	X, Y, NX, NY;
+int	New;
+{
+	static int *error = 0;
+	static int *thisline;
+	int diff,value;
+	int Dir = NX-X;
+
+	if (!error) {
+		register int i;
+		error = (int *) malloc(width*sizeof(int));
+		thisline = (int *) malloc(width*sizeof(int));
+		for (i=0; i<width; i++) {
+			error[i] = 0;
+			thisline[i] = 0;
+		}
+	}
+	if (New) {
+		int *p;
+		p = error;
+		error = thisline;
+		thisline = p;
+	}
+
+	Pix += thisline[X];
+	thisline[X] = 0;
+	if (Pix > 127) {
+		value=Levels;
+		diff = Pix - 255;
+	} else {
+		value = 0;
+		diff = Pix;
+	}
+	if (X+Dir < width && X+Dir >= 0) {
+		thisline[X+Dir] += diff*7/16;
+		error[X+Dir] += diff/16;
+	}
+	error[X] += diff*5/16;
+	if (X-Dir < width && X-Dir >= 0) {
+		error[X-Dir] += diff*3/16;
+	}
+	return(value);
+}
