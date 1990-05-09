38778dbc9323c8f3bb8a3f8eb0df434f541abd6e
Michael John Muuss <mike@brlcad.org>
1990-05-09 01:47:34 +0000

    New interface for booleans, called nmg_evaluate_boolean().
    svn:revision:4161
    cvs:account:mike
    cvs:branch:trunk

--- librt/nmg_eval.c
+++ librt/nmg_eval.c
@@ -28,28 +28,6 @@ static char RCSnmg_eval[] = "@(#)$Header$ (BRL)";
 #include "raytrace.h"
 
 /*
- *			N M G _ T O S S _ L O O P S
- *
- *	throw away all loops in the loopuse list "lu"
- */
-static void nmg_toss_loops(lu, n)
-struct loopuse **lu;
-int n;
-{
-	int				i;
-	struct faceuse			*fu;	
-
-	for (i=0 ; i < n ; ++i) {
-		fu = lu[i]->up.fu_p;
-		nmg_klu(lu[i]);
-		lu[i] = (struct loopuse *)NULL;	/* sanity */
-		if (!fu->lu_p) {
-			nmg_kfu(fu);
-		}
-	}
-}
-
-/*
  *			N M G _ R E V E R S E _ F A C E
  *
  *  Reverse the orientation of a face.
@@ -156,8 +134,6 @@ register struct faceuse	*fu;
 #define BACTION_RETAIN			2
 #define BACTION_RETAIN_AND_FLIP		3
 
-static struct nmg_ptbl	*classified_shell_loops[6];
-
 /* Actions are listed: onAinB, onAonB, onAoutB, inAonB, onAonB, outAonB */
 static int		subtraction_actions[6] = {
 	BACTION_KILL,
@@ -188,6 +164,8 @@ static int		intersect_actions[6] = {
 
 /*
  *			N M G _ A C T _ O N _ L O O P
+ *
+ *  Perform the given action on all the loops listed in the loopuse array.
  */
 void
 nmg_act_on_loop( ltbl, action, dest, src )
@@ -196,10 +174,10 @@ int		action;
 struct shell	*dest;
 struct shell	*src;
 {
-	struct loopuse	**lup;
-	struct loopuse	*lu;
-	struct faceuse	*fu;
-	int		i;
+	register struct loopuse	**lup;
+	register struct loopuse	*lu;
+	register struct faceuse	*fu;
+	register int		i;
 
 	NMG_CK_SHELL( dest );
 	NMG_CK_SHELL( src );
@@ -210,8 +188,20 @@ struct shell	*src;
 		/* NOTREACHED */
 
 	case BACTION_KILL:
-		nmg_toss_loops( (struct loopuse **)(ltbl->buffer), ltbl->end );
-		return;
+		i = ltbl->end-1;
+		lup = &((struct loopuse **)(ltbl->buffer))[i];
+		for( ; i >= 0; i--, lup-- ) {
+			lu = *lup;
+			*lup = 0;			/* sanity */
+			NMG_CK_LOOPUSE(lu);
+			fu = lu->up.fu_p;
+			NMG_CK_FACEUSE(fu);
+			/* Kill this loop use */
+			nmg_klu( lu );
+			/* If no loops remain in this face use, kill face use */
+			if( !fu->lu_p )  nmg_kfu( fu );
+		}
+		break;
 
 	case BACTION_RETAIN:
 		i = ltbl->end-1;
@@ -225,7 +215,7 @@ struct shell	*src;
 			if( fu->s_p == dest )  continue;
 			nmg_mv_fu_between_shells( dest, src, fu );
 		}
-		return;
+		break;
 
 	case BACTION_RETAIN_AND_FLIP:
 		i = ltbl->end-1;
@@ -240,31 +230,54 @@ struct shell	*src;
 			if( fu->s_p == dest )  continue;
 			nmg_mv_fu_between_shells( dest, src, fu );
 		}
-		return;
+		break;
 	}
 }
 
-/*	S U B T R A C T I O N
+/*
+ *			N M G _ E V A L U A T E _ B O O L E A N
  *
- *	reshuffle the faces of two shells to perform a subtraction of
- *	one shell from the other.  Shell "sB" disappears and shell "sA"
- *	contains:
- *		the faces that were in sA outside of sB
- *		the faces that were in sB inside of sA (normals flipped)
+ *  Evaluate a boolean operation on the two shells "A" and "B",
+ *  of the form "answer = A op B".
+ *  As input, each loop in both A and B has been classified as being
+ *  "in", "on", or "out" of the other shell.
+ *  Using these classifications, operate on the faces.
+ *  At the end, shell A contains the resultant object, and
+ *  shell B is destroyed.
+ *
+ *  XXX need to ensure each face is treated only once, maybe.
+ *  XXX really need onAonBsame and onAonBopposite lists, not AonB & BonA.
  */
-void subtraction(sA, AinB, AonB, AoutB, sB, BinA, BonA, BoutA)
-struct shell *sA, *sB;
+void
+nmg_evaluate_boolean( sA, sB, op, AinB, AonB, AoutB, BinA, BonA, BoutA)
+struct shell	*sA;
+struct shell	*sB;
+int		op;
 struct nmg_ptbl *AinB, *AonB, *AoutB, *BinA, *BonA, *BoutA;
 {
+	struct nmg_ptbl	*classified_shell_loops[6];
 	struct nmg_ptbl faces;
-	struct loopuse *lu;
-	struct faceuse *fu;
-	union {
-		struct loopuse **lu;
-		long **l;
-	} p;
-	int i;
+	struct loopuse	*lu;
+	struct faceuse	*fu;
+	int		*actions;
+	int		i;
 
+	switch( op )  {
+	case NMG_BOOL_SUB:
+		actions = subtraction_actions;
+		break;
+	case NMG_BOOL_ADD:
+		actions = union_actions;
+		break;
+	case NMG_BOOL_ISECT:
+		actions = intersect_actions;
+		break;
+	default:
+		rt_log("ERROR nmg_evaluate_boolean() op=%d.\n", op);
+		rt_bomb("bad boolean\n");
+	}
+
+	/* XXX for the future, ensure each face is treated only once */
 	(void)nmg_tbl(&faces, TBL_INIT, (long *)NULL);
 
 	classified_shell_loops[0] = AinB;
@@ -276,118 +289,31 @@ struct nmg_ptbl *AinB, *AonB, *AoutB, *BinA, *BonA, *BoutA;
 
 	for( i=0; i<6; i++ )  {
 		nmg_act_on_loop( classified_shell_loops[i],
-			subtraction_actions[i],
-			sA, sB );
+			actions[i], sA, sB );
 	}
 
 	/* Plot the result */
 	if (rt_g.NMG_debug & DEBUG_SUBTRACT) {
 		FILE *fp, *fopen();
 
-		if ((fp=fopen("sub.pl", "w")) == (FILE *)NULL) {
-			(void)perror("sub.pl");
+		if ((fp=fopen("bool_ans.pl", "w")) == (FILE *)NULL) {
+			(void)perror("bool_ans.pl");
 			exit(-1);
 		}
-    		rt_log("plotting sub.pl\n");
+    		rt_log("plotting bool_ans.pl\n");
 		nmg_pl_s( fp, sA );
 		(void)fclose(fp);
 	}
 
 
 	if (sB->fu_p) {
-		rt_log("Why does shell B still have faces?\n");
-	} else if (!sB->lu_p && !sB->eu_p && !sB->vu_p) {
-		struct nmgregion	*r;
-
-		r = sB->r_p;
-		nmg_ks(sB);
-		if (!r->s_p && r->next != r) nmg_kr(r);
+		rt_log("WARNING:  nmg_evaluate_boolean():  shell B still has faces!\n");
 	}
-
-
-	(void)nmg_tbl(&faces, TBL_FREE, (long *)NULL);
-}
-/*	A D D I T I O N
- *
- *	add (union) two shells together
- *
- */
-void addition(sA, AinB, AonB, AoutB, sB, BinA, BonA, BoutA)
-struct shell *sA, *sB;
-struct nmg_ptbl *AinB, *AonB, *AoutB, *BinA, *BonA, *BoutA;
-{
-	struct nmg_ptbl faces;
-	struct loopuse *lu;
-	struct faceuse *fu;
-	union {
-		struct loopuse **lu;
-		long **l;
-	} p;
-	int i;
-
-
-	(void)nmg_tbl(&faces, TBL_INIT, (long *)NULL);
-
-	classified_shell_loops[0] = AinB;
-	classified_shell_loops[1] = AonB;
-	classified_shell_loops[2] = AoutB;
-	classified_shell_loops[3] = BinA;
-	classified_shell_loops[4] = BonA;
-	classified_shell_loops[5] = BoutA;
-
-	for( i=0; i<6; i++ )  {
-		nmg_act_on_loop( classified_shell_loops[i],
-			union_actions[i],
-			sA, sB );
+	if (sB->lu_p || sB->eu_p || sB->vu_p) {
+		rt_log("WARNING:  nmg_evaluate_boolean():  shell B still has loops/edges/verts!\n");
 	}
-
-	if (sB->fu_p) {
-		rt_log("Why does shell B still have faces?\n");
-	} else if (!sB->lu_p && !sB->eu_p && !sB->vu_p) {
-		nmg_ks(sB);
-	}
-
-	(void)nmg_tbl(&faces, TBL_FREE, (long *)NULL);
-}
-
-/*	I N T E R S E C T I O N
- *
- *	resulting shell is all faces of sA in sB and all faces of sB in sA
- */
-void intersection(sA, AinB, AonB, AoutB, sB, BinA, BonA, BoutA)
-struct shell *sA, *sB;
-struct nmg_ptbl *AinB, *AonB, *AoutB, *BinA, *BonA, *BoutA;
-{
-	struct nmg_ptbl faces;
-	struct loopuse *lu;
-	struct faceuse *fu;
-	union {
-		struct loopuse **lu;
-		long **l;
-	} p;
-	int i;
-
-
-	(void)nmg_tbl(&faces, TBL_INIT, (long *)NULL);
-
-	classified_shell_loops[0] = AinB;
-	classified_shell_loops[1] = AonB;
-	classified_shell_loops[2] = AoutB;
-	classified_shell_loops[3] = BinA;
-	classified_shell_loops[4] = BonA;
-	classified_shell_loops[5] = BoutA;
-
-	for( i=0; i<6; i++ )  {
-		nmg_act_on_loop( classified_shell_loops[i],
-			intersect_actions[i],
-			sA, sB );
-	}
-
-	if (sB->fu_p) {
-		rt_log("Why does shell B still have faces?\n");
-	} else if (!sB->lu_p && !sB->eu_p && !sB->vu_p) {
-		nmg_ks(sB);
-	}
+	/* Regardless of what is in it, kill shell B */
+	nmg_ks( sB );
 
 	(void)nmg_tbl(&faces, TBL_FREE, (long *)NULL);
 }
