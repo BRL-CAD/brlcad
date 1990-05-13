cb1af1556e544fafd0a7771f464ab19b21c84776
Michael John Muuss <mike@brlcad.org>
1990-05-13 12:35:56 +0000

    Made more generic.
    svn:revision:4189
    cvs:account:mike
    cvs:branch:trunk

--- sh/newvers.sh
+++ sh/newvers.sh
@@ -3,7 +3,26 @@
 #
 # Update the "version" file, and create a new "vers.c" from it.
 #
+#  Optional args:
+#	variable name to put version string in (default="version")
+#	title
+#
 #	@(#)$Header$ (BRL)
+
+if test $# -gt 0
+then
+	VARIABLE="$1"
+	shift
+else
+	VARIABLE="version"
+fi
+
+if test $# -gt 0
+then
+	TITLE="$*"
+else
+	TITLE="Graphics Editor (MGED)"
+fi
 
 eval `machinetype.sh -b`	# sets MACHINE, UNIXTYPE, etc
 
@@ -16,18 +35,29 @@ else
 fi
 
 DIR=`pwd`
-if test x$DIR = x; then DIR=".mged.$MACHINE"; fi
+if test x$DIR = x; then DIR="/unknown"; fi
 
-if test ! -r version ; then echo 0 > version; fi
-touch version
-awk '	{	version = $1 + 1; }\
-END	{	printf "char version[] = \"@(#) BRL-CAD Release '$RELEASE' Graphics Editor (MGED) Compilation %d", version > "vers.c";\
-		printf "%d\n", version > "version"; }' < version
-
-# The ECHO command operates differently between BSD and SYSV
-if test x$UNIXTYPE = xBSD
+if test ! -w version
 then
-	/bin/echo '\n    '`date`'\n    '$USER'@'`hostname`':'$DIR'\n";' >> vers.c
-else
-	echo '\\n    '`date`'\\n    '$USER'@'`uname -n`':'$DIR'\\n";' >> vers.c
+	rm -f version; echo 0 > version; chmod 664 version
 fi
+
+awk '	{	version = $1 + 1; }\
+END	{	printf "%d\n", version > "version"; }' < version
+
+VERSION=`cat version`
+DATE=`date`
+if test x$UNIXTYPE = xBSD -o x$MACHINE = x4d
+then
+	HOST=`hostname`
+else
+	HOST=`uname -n`
+fi
+
+rm -f vers.c
+cat << EOF > vers.c
+char ${VARIABLE}[] = "\\
+@(#) BRL-CAD Release ${RELEASE}   ${TITLE}\n\\
+	${DATE}, Compilation ${VERSION}\n\\
+	${USER}@${HOST}:${DIR}\n";
+EOF
