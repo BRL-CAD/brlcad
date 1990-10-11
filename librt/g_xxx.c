c28b2ab33737e822a2c3d5f2e2787af313067dac
Michael John Muuss <mike@brlcad.org>
1990-10-11 02:37:41 +0000

    Enhanced with more arg declarations, neatened up some comments, added vector stubs and error returns for a better "first-cut".
    svn:revision:4431
    cvs:account:mike
    cvs:branch:trunk

--- librt/g_xxx.c
+++ librt/g_xxx.c
@@ -1,5 +1,5 @@
 /*
- *			X X X . C
+ *			G _ X X X . C
  *
  *  Purpose -
  *	Intersect a ray with a 
@@ -9,14 +9,14 @@
  *  Source -
  *	SECAD/VLD Computing Consortium, Bldg 394
  *	The U. S. Army Ballistic Research Laboratory
- *	Aberdeen Proving Ground, Maryland  21005
+ *	Aberdeen Proving Ground, Maryland  21005-5066
  *  
  *  Copyright Notice -
- *	This software is Copyright (C) 1989 by the United States Army.
+ *	This software is Copyright (C) 1990 by the United States Army.
  *	All rights reserved.
  */
 #ifndef lint
-static char RCSsph[] = "@(#)$Header$ (BRL)";
+static char RCSxxx[] = "@(#)$Header$ (BRL)";
 #endif
 
 #include <stdio.h>
@@ -36,7 +36,7 @@ struct xxx_specific {
 };
 
 /*
- *  			X X X _ P R E P
+ *  			R T _ X X X _ P R E P
  *  
  *  Given a pointer to a GED database record, and a transformation matrix,
  *  determine if this is a valid XXX, and if so, precompute various
@@ -51,16 +51,19 @@ struct xxx_specific {
  *  	stp->st_specific for use by xxx_shot().
  */
 int
-xxx_prep( stp, rec, rtip )
+rt_xxx_prep( stp, rec, rtip )
 struct soltab		*stp;
-struct rt_i		*rtip;
 union record		*rec;
+struct rt_i		*rtip;
 {
 	register struct xxx_specific *xxx;
 }
 
+/*
+ *			R T _ X X X _ P R I N T
+ */
 void
-xxx_print( stp )
+rt_xxx_print( stp )
 register struct soltab *stp;
 {
 	register struct xxx_specific *xxx =
@@ -68,7 +71,7 @@ register struct soltab *stp;
 }
 
 /*
- *  			X X X _ S H O T
+ *  			R T _ X X X _ S H O T
  *  
  *  Intersect a ray with a xxx.
  *  If an intersection occurs, a struct seg will be acquired
@@ -79,7 +82,7 @@ register struct soltab *stp;
  *	>0	HIT
  */
 int
-xxx_shot( stp, rp, ap, seghead )
+rt_xxx_shot( stp, rp, ap, seghead )
 struct soltab		*stp;
 register struct xray	*rp;
 struct application	*ap;
@@ -95,31 +98,31 @@ struct seg		*seghead;
 #define SEG_MISS(SEG)		(SEG).seg_stp=(struct soltab *) 0;	
 
 /*
- *			S P H _ V S H O T
+ *			R T _ X X X _ V S H O T
  *
  *  Vectorized version.
  */
 void
-xxx_vshot( stp, rp, segp, n, resp)
+rt_xxx_vshot( stp, rp, segp, n, resp)
 struct soltab	       *stp[]; /* An array of solid pointers */
 struct xray		*rp[]; /* An array of ray pointers */
 struct  seg            segp[]; /* array of segs (results returned) */
 int		  	    n; /* Number of ray/object pairs */
 struct resource         *resp; /* pointer to a list of free segs */
 {
-	register struct xxx_specific *xxx;
+	rt_vstub( stp, rp, segp, n, resp );
 }
 
 /*
- *  			X X X _ N O R M
+ *  			R T _ X X X _ N O R M
  *  
  *  Given ONE ray distance, return the normal and entry/exit point.
  */
 void
-xxx_norm( hitp, stp, rp )
-register struct hit *hitp;
-struct soltab *stp;
-register struct xray *rp;
+rt_xxx_norm( hitp, stp, rp )
+register struct hit	*hitp;
+struct soltab		*stp;
+register struct xray	*rp;
 {
 	register struct xxx_specific *xxx =
 		(struct xxx_specific *)stp->st_specific;
@@ -128,15 +131,15 @@ register struct xray *rp;
 }
 
 /*
- *			X X X _ C U R V E
+ *			R T _ X X X _ C U R V E
  *
  *  Return the curvature of the xxx.
  */
 void
-xxx_curve( cvp, hitp, stp )
+rt_xxx_curve( cvp, hitp, stp )
 register struct curvature *cvp;
-register struct hit *hitp;
-struct soltab *stp;
+register struct hit	*hitp;
+struct soltab		*stp;
 {
 	register struct xxx_specific *xxx =
 		(struct xxx_specific *)stp->st_specific;
@@ -148,7 +151,7 @@ struct soltab *stp;
 }
 
 /*
- *  			X X X _ U V
+ *  			R T _ X X X _ U V
  *  
  *  For a hit on the surface of an xxx, return the (u,v) coordinates
  *  of the hit point, 0 <= u,v <= 1.
@@ -156,21 +159,21 @@ struct soltab *stp;
  *  v = elevation
  */
 void
-xxx_uv( ap, stp, hitp, uvp )
-struct application *ap;
-struct soltab *stp;
-register struct hit *hitp;
-register struct uvcoord *uvp;
+rt_xxx_uv( ap, stp, hitp, uvp )
+struct application	*ap;
+struct soltab		*stp;
+register struct hit	*hitp;
+register struct uvcoord	*uvp;
 {
 	register struct xxx_specific *xxx =
 		(struct xxx_specific *)stp->st_specific;
 }
 
 /*
- *		X X X _ F R E E
+ *		R T _ X X X _ F R E E
  */
 void
-xxx_free( stp )
+rt_xxx_free( stp )
 register struct soltab *stp;
 {
 	register struct xxx_specific *xxx =
@@ -179,27 +182,56 @@ register struct soltab *stp;
 	rt_free( (char *)xxx, "xxx_specific" );
 }
 
+/*
+ *			R T _ X X X _ C L A S S
+ */
 int
-xxx_class()
+rt_xxx_class()
 {
 	return(0);
 }
 
+/*
+ *			R T _ X X X _ P L O T
+ */
 int
-xxx_plot()
+rt_xxx_plot( rp, mat, vhead, dp, abs_tol, rel_tol, norm_tol )
+union record	*rp;
+mat_t		mat;
+struct vlhead	*vhead;
+struct directory *dp;
+double		abs_tol;
+double		rel_tol;
+double		norm_tol;
 {
+	return(-1);
 }
 
+/*
+ *			R T _ X X X _ T E S S
+ */
 int
-xxx_tess()
+rt_xxx_tess( r, m, rp, mat, dp, abs_tol, rel_tol, norm_tol )
+struct nmgregion	**r;
+struct model		*m;
+union record		*rp;
+mat_t			mat;
+struct directory	*dp;
+double			abs_tol;
+double			rel_tol;
+double			norm_tol;
 {
+	return(-1);
 }
 
+/*
+ *			R T _ X X X _ I M P O R T
+ */
 int
-xxx_import( xxx, rp, matp )
+rt_xxx_import( xxx, rp, mat )
 struct xxx_internal	*xxx;
 union record		*rp;
-register matp_t		matp;
+register mat_t		mat;
 {
 	return(0);			/* OK */
 }
