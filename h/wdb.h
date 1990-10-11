ff31dc58fa852aee07822e98d2b18335946a1d9e
Michael John Muuss <mike@brlcad.org>
1990-10-11 02:19:45 +0000

    Added pipe support. Added ANSI C subroutine prototypes
    svn:revision:4427
    cvs:account:mike
    cvs:branch:trunk

--- h/wdb.h
+++ h/wdb.h
@@ -3,6 +3,10 @@
  *
  *  Interface structures and routines for libwdb
  *
+ *  Note -
+ *	Any source file that includes this header file must also include
+ *	<stdio.h> to obtain the typedef for FILE.
+ *
  *  Author -
  *	Michael John Muuss
  *  
@@ -20,7 +24,23 @@
 
 #ifndef WDB_H
 #define WDB_H seen
+/*
+ *  Macros for providing function prototypes, regardless of whether
+ *  the compiler understands them or not.
+ *  It is vital that the argument list given for "args" be enclosed
+ *  in parens.
+ */
+#if __STDC__
+#	define	WDB_EXTERN(type_and_name,args)	extern type_and_name args
+#	define	WDB_ARGS(args)			args
+#else
+#	define	WDB_EXTERN(type_and_name,args)	extern type_and_name()
+#	define	WDB_ARGS(args)			()
+#endif
 
+/*
+ *  In-memory form of database combinations
+ */
 struct wmember  {
 	long	wm_magic;
 	char	wm_name[16+3];	/* NAMESIZE */
@@ -33,40 +53,78 @@ struct wmember  {
 
 #define WMEMBER_MAGIC	0x43128912
 
+#define WMEMBER_HEAD_INIT(hp)	{ (hp)->wm_forw = (hp)->wm_back = (hp); }
+
+/*
+ *  Definitions for pipe (wire) segments
+ */
+struct wdb_pipeseg  {
+	point_t		ps_start;	/* start point of centerline */
+	point_t		ps_bendcenter;	/* BEND only: center of bend circle */
+	fastf_t		ps_id;		/* inner diam, <=0 if solid (wire) */
+	fastf_t		ps_od;		/* pipe outer diam */
+	int		ps_type;	/* WDB_PIPESEG_TYPE_{END, LINEAR, BEND} */
+	struct wdb_pipeseg *ps_next;
+};
+#define WDB_PIPESEG_NULL		((struct wdb_pipeseg *)0)
+#define WDB_PIPESEG_TYPE_END	1	/* End of pipe.  Required, last */
+#define WDB_PIPESEG_TYPE_LINEAR	2	/* Linear pipe segment */
+#define WDB_PIPESEG_TYPE_BEND	3	/* Bending pipe segment */
+
 /*
  *  Solid conversion routines
  */
-extern int	mk_id();
-extern int	mk_half();
-extern int	mk_rpp();
-extern int	mk_arb4();
-extern int	mk_arb8();
-extern int	mk_sph();
-extern int	mk_ell();
-extern int	mk_tor();
-extern int	mk_rcc();
-extern int	mk_tgc();
-extern int	mk_trc();
-extern int	mk_trc_top();
-extern int	mk_polysolid();
-extern int	mk_poly();
-extern int	mk_fpoly();
+WDB_EXTERN(int mk_id, (FILE *fp, char *title) );
+WDB_EXTERN(int mk_half, (FILE *fp, char *name, vect_t norm, double d) );
+WDB_EXTERN(int mk_rpp, (FILE *fp, char *name, point_t min, point_t max) );
+WDB_EXTERN(int mk_arb4, (FILE *fp, char *name, point_t pts[]) );
+WDB_EXTERN(int mk_arb8, (FILE *fp, char *name, point_t pts[]) );
+WDB_EXTERN(int mk_sph, (FILE *fp, char *name, point_t center, fastf_t radius) );
+WDB_EXTERN(int mk_ell, (FILE *fp, char *name, point_t center,
+			vect_t a, vect_t b, vect_t c) );
+WDB_EXTERN(int mk_tor, (FILE *fp, char *name, point_t center, vect_t inorm,
+			double r1, double r2) );
+WDB_EXTERN(int mk_rcc, (FILE *fp, char *name, point_t base, vect_t height,
+			fastf_t radius) );
+WDB_EXTERN(int mk_tgc, (FILE *fp, char *name, point_t base, vect_t height,
+			vect_t a, vect_t b, vect_t c, vect_t d) );
+WDB_EXTERN(int mk_trc, (FILE *fp, char *name, point_t ibase, vect_t iheight,
+			fastf_t radbase, fastf_t radtop) );
+WDB_EXTERN(int mk_trc_top, (FILE *fp, char *name, point_t ibase, point_t itop,
+			fastf_t radbase, fastf_t radtop) );
+WDB_EXTERN(int mk_polysolid, (FILE *fp, char *name) );
+WDB_EXTERN(int mk_poly, (FILE *fp, int npts,
+			fastf_t verts[][3], fastf_t norms[][3]) );
+WDB_EXTERN(int mk_fpoly, (FILE *fp, int npts, fastf_t verts[][3]) );
 
-extern int	mk_arbn();
-extern int	mk_ars();
-extern int	mk_bsolid();
-extern int	mk_bsurf();
+WDB_EXTERN(int mk_arbn, (FILE *fp, char *name, int neqn, plane_t eqn[]) );
+WDB_EXTERN(int mk_ars, (FILE *fp, char *name, int ncurves, int pts_per_curve,
+			fastf_t	*curves[]) );
+WDB_EXTERN(int mk_bsolid, (FILE *fp, char *name, int nsurf, double res) );
+WDB_EXTERN(int mk_bsurf, (FILE *fp, struct b_spline *bp) );
 
 /*
  *  Combination conversion routines
  */
-extern int	mk_comb();
-extern int	mk_rcomb();
-extern int	mk_fcomb();
-extern int	mk_memb();
-extern struct wmember	*mk_addmember();
-extern int	mk_lcomb();
-extern int	mk_lrcomb();
+WDB_EXTERN(int mk_comb, (FILE *fp, char *name, int len, int region_flag,
+			char *matname, char *matparm, char *rgb,
+			int inherit_flag) );
+WDB_EXTERN(int mk_rcomb, (FILE *fp, char *name, int len, int region_flag,
+			char *matname, char *matparm, char *rgb,
+			int id, int air, int material, int los,
+			int inherit_flag) );
+WDB_EXTERN(int mk_fcomb, (FILE *fp, char *name, int len, int region_flag) );
+WDB_EXTERN(int mk_memb, (FILE *fp, char *name, mat_t mat, int bool_op) );
+WDB_EXTERN(struct wmember *mk_addmember, (char *name, struct wmember *headp) );
+WDB_EXTERN(int mk_lcomb, (FILE *fp, char *name, struct wmember *headp,
+			int region_flag,
+			char *matname, char *matparm, char *rgb,
+			int inherit_flag) );
+WDB_EXTERN(int mk_lrcomb, (FILE *fp, char *name, struct wmember *headp,
+			int region_flag,
+			char *matname, char *matparm, char *rgb,
+			int id, int air, int material, int los,
+			int inherit_flag) );
 
 /* Values for wm_op.  These must track db.h */
 #define WMOP_INTERSECT	'+'
@@ -80,14 +138,19 @@ extern int	mk_lrcomb();
 /*
  *  Routines to establish conversion factors
  */
-extern int		mk_conversion();
-extern double		mk_cvt_factor();
-extern int		mk_set_conversion();
+WDB_EXTERN(int mk_conversion, (char *units_string) );
+WDB_EXTERN(int mk_set_conversion, (double val) );
+WDB_EXTERN(double mk_cvt_factor, (char *units_string) );
 
 /*
  * This internal variable should not be directly modified;
  * call mk_conversion() or mk_set_conversion() instead.
  */
 extern double	mk_conv2mm;		/* Conversion factor to mm */
+
+/*
+ *  Internal routines
+ */
+WDB_EXTERN(int mk_freemembers, (struct wmember *headp) );
 
 #endif /* WDB_H */
