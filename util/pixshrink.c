2a3545b33b817d09118d7f3111e46981436c3ebf
Lee Butler <iraytrace@users.noreply.github.com>
1990-11-03 14:48:18 +0000

    Added undersampling support
    svn:revision:4523
    cvs:account:butler
    cvs:branch:trunk

--- util/pixshrink.c
+++ util/pixshrink.c
@@ -13,18 +13,143 @@
 #include <string.h>
 #endif
 
+#define UCHAR unsigned char
+
 /* declarations to support use of getopt() system call */
-char *options = "hs:w:n:f:";
+char *options = "uhs:w:n:f:";
 extern char *optarg;
 extern int optind, opterr, getopt();
 
 char *progname = "(noname)";
 char *filename = "(stdin)";
 
+/*	R E A D _ I M A G E
+ *
+ *	read image into memory
+ */
+UCHAR *read_image(scanlen, Width, Height, buffer)
+int scanlen, Width, Height;
+UCHAR *buffer;
+{
+	int total_bytes, in_bytes, count;
+	UCHAR *malloc();
+
+	if (!buffer && (buffer=malloc(scanlen * Height)) == (UCHAR *)NULL) {
+		(void)fprintf(stderr, "%s: cannot allocate input buffer\n", 
+			progname);
+		exit(-1);
+	}
+
+	total_bytes = Width * Height * 3;
+	in_bytes = 0;
+	while (in_bytes < total_bytes && 
+	    (count=read(0, (char *)&buffer[in_bytes], (unsigned)(total_bytes-in_bytes))) >= 0)
+		in_bytes += count;
+
+	if (count < 0) {
+		perror(filename);
+		exit(-1);
+	}
+
+	return(buffer);
+}
+
+/*	W R I T E _ I M A G E
+ *
+ *
+ */
+void write_image(Width, Height, buffer)
+int Width, Height;
+UCHAR *buffer;
+{
+	int count, out_bytes, total_bytes;
+	
+	total_bytes = Width * Height * 3;
+	out_bytes = 0;
+
+	while (out_bytes < total_bytes &&
+	    (count=write(1, (char *)&buffer[out_bytes], total_bytes-out_bytes)) >= 0)
+		out_bytes += count;
+
+	if (count < 0) {
+		perror("stdout");
+		exit(-1);
+	}
+}
+
+/*	S H R I N K _ I M A G E
+ *
+ *	
+ */
+shrink_image(scanlen, Width, Height, buffer, Factor)
+UCHAR *buffer;
+int scanlen, Factor, Width, Height;
+{
+	UCHAR *pixelp, *finalpixel;
+	unsigned int p[3];
+	int facsq, x, y, px, py;
+
+	facsq = Factor * Factor;
+	finalpixel = buffer;
+
+	for (y=0 ; y < Height ; y += Factor)
+		for (x=0 ; x < Width ; x += Factor) {
+
+			/* average factor by factor pixels */
+
+			for (py = 0 ; py < 3 ; ++py)
+				p[py] = 0;
+
+			for (py = 0 ; py < Factor ; py++) {
+
+				/* get first pixel in scanline */
+				pixelp = &buffer[y*scanlen+x*3];
+
+				/* add pixels from scanline to average */
+				for (px = 0 ; px < Factor ; px++) {
+					p[0] += *pixelp++;
+					p[1] += *pixelp++;
+					p[2] += *pixelp++;
+				}
+			}
+			
+			/* store resultant pixel back in buffer */
+			for (py = 0 ; py < 3 ; ++py)
+				*finalpixel++ = p[py] / facsq;
+		}
+}
+
+/*
+ *	Undersample image pixels
+ */
+usample_image(scanlen, Width, Height, buffer, Factor)
+UCHAR *buffer;
+int scanlen, Factor, Width, Height;
+{
+	register int t, x, y;
+
+	UCHAR *p;
+
+	p = buffer;
+
+	for (y=0 ; y < Height ; y += Factor)
+		for (x=0 ; x < Width ; x += Factor, p += 3) {
+			t = x*3 + y*scanlen;
+			p[0] = buffer[t];
+			p[1] = buffer[t+1];
+			p[2] = buffer[t+2];
+		}
+}
+
+
 int width = 512;
 int height = 512;
 int scanlen;
 int factor = 2;
+
+#define METH_BOXCAR 1
+#define METH_UNDERSAMPLE 2
+int method = METH_BOXCAR;
 
 /*
  *	U S A G E --- tell user how to invoke this program, then exit
@@ -32,7 +157,7 @@ int factor = 2;
 void usage()
 {
 	(void) fprintf(stderr,
-"Usage: %s [-h] [-w width] [-n scanlines] [-s squaresize]\n\
+"Usage: %s [-u] [-h] [-w width] [-n scanlines] [-s squaresize]\n\
 [-f shrink_factor] [pixfile] > pixfile\n", progname);
 	exit(1);
 }
@@ -71,7 +196,8 @@ char *av[];
 		case 's'	: if ((c=atoi(optarg)) > 0)
 					height = width = c;
 				  break;
-		case '?'	: 
+		case 'u'	: method = METH_UNDERSAMPLE; break; 
+		case '?'	:
 		default		: usage(); break;
 		}
 
@@ -93,102 +219,6 @@ char *av[];
 
 
 
-/*	R E A D _ I M A G E
- *
- *	read image into memory
- */
-unsigned char *read_image(Width, Height, buffer)
-int Width, Height;
-unsigned char *buffer;
-{
-	int total_bytes, in_bytes, count;
-	unsigned char *malloc();
-
-	scanlen = Width * 3;
-	if (!buffer && (buffer=malloc(scanlen * Height)) == (unsigned char *)NULL) {
-		(void)fprintf(stderr, "%s: cannot allocate input buffer\n", 
-			progname);
-		exit(-1);
-	}
-
-	total_bytes = Width * Height * 3;
-	in_bytes = 0;
-	while (in_bytes < total_bytes && 
-	    (count=read(0, (char *)&buffer[in_bytes], (unsigned)(total_bytes-in_bytes))) >= 0)
-		in_bytes += count;
-
-	if (count < 0) {
-		perror(filename);
-		exit(-1);
-	}
-
-	return(buffer);
-}
-
-/*	W R I T E _ I M A G E
- *
- *
- */
-void write_image(Width, Height, buffer)
-int Width, Height;
-unsigned char *buffer;
-{
-	int count, out_bytes, total_bytes;
-	
-	total_bytes = Width * Height * 3;
-	out_bytes = 0;
-
-	while (out_bytes < total_bytes &&
-	    (count=write(1, (char *)&buffer[out_bytes], total_bytes-out_bytes)) >= 0)
-		out_bytes += count;
-
-	if (count < 0) {
-		perror("stdout");
-		exit(-1);
-	}
-}
-
-/*	S H R I N K _ I M A G E
- *
- *	
- */
-shrink_image(Width, Height, buffer, Factor)
-unsigned char *buffer;
-int Factor, Width, Height;
-{
-	unsigned char *pixelp, *finalpixel;
-	unsigned int p[3];
-	int facsq, x, y, px, py;
-
-	facsq = Factor * Factor;
-	finalpixel = buffer;
-
-	for (y=0 ; y < Height ; y += factor)
-		for (x=0 ; x < Width ; x += factor) {
-
-			/* average factor by factor pixels */
-
-			for (py = 0 ; py < 3 ; ++py)
-				p[py] = 0;
-
-			for (py = 0 ; py < factor ; py++) {
-
-				/* get first pixel in scanline */
-				pixelp = &buffer[y*scanlen+x*3];
-
-				/* add pixels from scanline to average */
-				for (px = 0 ; px < factor ; px++) {
-					p[0] += *pixelp++;
-					p[1] += *pixelp++;
-					p[2] += *pixelp++;
-				}
-			}
-			
-			/* store resultant pixel back in buffer */
-			for (py = 0 ; py < 3 ; ++py)
-				*finalpixel++ = p[py] / facsq;
-		}
-}
 
 /*
  *	M A I N
@@ -200,15 +230,21 @@ int main(ac,av)
 int ac;
 char *av[];
 {
-	unsigned char *buffer = (char *)NULL;
+	UCHAR *buffer = (UCHAR *)NULL;
 
 	(void)parse_args(ac, av);
 	if (isatty(fileno(stdin))) usage();
 
 	/* process stdin */
-	buffer = read_image(width, height, buffer);
+	scanlen = width * 3;
+	buffer = read_image(scanlen, width, height, buffer);
 
-	shrink_image(width, height, buffer, factor);
+	switch (method) {
+	case METH_BOXCAR : shrink_image(scanlen, width, height, buffer, factor); break;
+	case METH_UNDERSAMPLE : usample_image(scanlen, width, height, buffer, factor);
+				break;
+	default: return(-1);
+	}
 
 	write_image(width/factor, height/factor, buffer);
 	return(0);
