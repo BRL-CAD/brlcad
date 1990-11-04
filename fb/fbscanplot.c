8e3b1b70423adf094bedb4bfdd32b5f965756b0d
Michael John Muuss <mike@brlcad.org>
1990-11-04 02:17:35 +0000

    Modified to support scanlines of arbitrary width.
    svn:revision:4543
    cvs:account:mike
    cvs:branch:trunk

--- fb/fbscanplot.c
+++ fb/fbscanplot.c
@@ -22,20 +22,22 @@ static char RCSid[] = "@(#)$Header$ (BRL)";
 #include <stdio.h>
 #include "fb.h"
 
-RGBpixel scan[1024+2];
-RGBpixel outline[1024], backgnd[1024];
-
 extern int	getopt();
 extern char	*optarg;
 extern int	optind;
 
-int yline;		/* line to plot */
-int width = 0;		/* framebuffer width */
-int verbose = 0;	/* output scanline values to stdout */
-int fb_overlay = 0;	/* plot on background, else black with grid */
-int reverse = 0;	/* highlight chosen line by inverting it */
-char *outframebuffer = NULL;
-FBIO *fbp, *fboutp;
+RGBpixel	*scan;		/* Scanline to be examined */
+RGBpixel	*outline;	/* output line buffer */
+RGBpixel	*backgnd;	/* copy of line to be overlaid */
+
+int	yline;			/* line to plot */
+int	scr_width = 0;		/* framebuffer width */
+int	scr_height = 0;		/* framebuffer height */
+int	verbose = 0;		/* output scanline values to stdout */
+int	fb_overlay = 0;		/* plot on background, else black with grid */
+int	reverse = 0;		/* highlight chosen line by inverting it */
+char	*outframebuffer = NULL;
+FBIO	*fbp, *fboutp;
 
 char usage[] = "\
 Usage: fbscanplot [-h] [-v] [-o] [-r]\n\
@@ -52,7 +54,7 @@ register char **argv;
 			verbose++;
 			break;
 		case 'h':
-			width = 1024;
+			scr_width = scr_height = 1024;
 			break;
 		case 'o':
 			fb_overlay++;
@@ -61,7 +63,7 @@ register char **argv;
 			reverse++;
 			break;
 		case 'W':
-			width = atoi(optarg);
+			scr_width = atoi(optarg);
 			break;
 		case 'F':
 			outframebuffer = optarg;
@@ -95,29 +97,37 @@ int argc; char **argv;
 		exit( 1 );
 	}
 
-	if( (fbp = fb_open( NULL, width, width )) == NULL )
+	if( (fbp = fb_open( NULL, scr_width, scr_height )) == NULL )
 		exit( 2 );
+	scr_width = fb_getwidth(fbp);
+	scr_height = fb_getheight(fbp);
 
 	if( outframebuffer != NULL ) {
-		if( (fboutp = fb_open( outframebuffer, width, width )) == NULL )
+		if( (fboutp = fb_open( outframebuffer, scr_width, scr_width )) == NULL )
 			exit( 3 );
 	} else
 		fboutp = fbp;
 
-	width = fb_getwidth(fbp);
-	fb_read( fbp, 0, yline, &scan[1][RED], width );
+	/* Allocate the buffers */
+	scan = (RGBpixel *)malloc( (scr_width+2) * sizeof(RGBpixel) );
+	outline = (RGBpixel *)malloc( (scr_width+2) * sizeof(RGBpixel) );
+	backgnd = (RGBpixel *)malloc( (scr_width+2) * sizeof(RGBpixel) );
 
-	/* extend the edges */
+	/* Read the scanline to be examined */
+	if( fb_read( fbp, 0, yline, &scan[1][RED], scr_width ) != scr_width )
+		exit(4);
+
+	/* extend the edges with one duplicate pixel each way */
 	scan[0][RED] = scan[1][RED];
 	scan[0][GRN] = scan[1][GRN];
 	scan[0][BLU] = scan[1][BLU];
-	scan[width+1][RED] = scan[width][RED];
-	scan[width+1][GRN] = scan[width][GRN];
-	scan[width+1][BLU] = scan[width][BLU];
+	scan[scr_width+1][RED] = scan[scr_width][RED];
+	scan[scr_width+1][GRN] = scan[scr_width][GRN];
+	scan[scr_width+1][BLU] = scan[scr_width][BLU];
 
 	/* figure out where to put it on the screen */
-	if( fb_overlay == 0 && fboutp == fbp && yline < fb_getheight(fbp)/2 ) {
-		yoffset = fb_getheight(fbp) - 256;
+	if( fb_overlay == 0 && fboutp == fbp && yline < scr_height/2 ) {
+		yoffset = scr_height - 256;
 		if( yoffset <= yline )
 			yoffset = 0;
 	} else
@@ -125,21 +135,22 @@ int argc; char **argv;
 
 	if( reverse ) {
 		/* Output the negative of the chosen line */
-		for( x = 0; x < width; x++ ) {
+		for( x = 0; x < scr_width; x++ ) {
 			outline[x][RED] = 255 - scan[x+1][RED];
 			outline[x][GRN] = 255 - scan[x+1][GRN];
 			outline[x][BLU] = 255 - scan[x+1][BLU];
 		}
-		fb_write( fbp, 0, yline, outline, width );
+		fb_write( fbp, 0, yline, outline, scr_width );
 	}
 
+	/* The scanplot takes 256 lines, one for each intensity value */
 	for( y = 0; y < 256; y++ ) {
 		if( fb_overlay )
-			fb_read( fboutp, 0, y+yoffset, backgnd, width );
+			fb_read( fboutp, 0, y+yoffset, backgnd, scr_width );
 
 		ip = &scan[1][RED];
 		op = &outline[0][RED];
-		for( x = 0; x < width; x++, op += 3, ip += 3 ) {
+		for( x = 0; x < scr_width; x++, op += 3, ip += 3 ) {
 			if( y > ip[RED] ) {
 				op[RED] = 0;
 			} else {
@@ -190,11 +201,11 @@ int argc; char **argv;
 			}
 		}
 
-		fb_write( fboutp, 0, y+yoffset, outline, width );
+		fb_write( fboutp, 0, y+yoffset, outline, scr_width );
 	}
 
 	if( verbose ) {
-		for( x = 0; x < width; x++ )
+		for( x = 0; x < scr_width; x++ )
 			printf( "%3d: %3d %3d %3d\n", x,
 			   scan[x+1][RED], scan[x+1][GRN], scan[x+1][BLU] );
 	}
