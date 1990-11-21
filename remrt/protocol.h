c06af87e4a526df393a59dc884982d9d5ace5af7
Michael John Muuss <mike@brlcad.org>
1990-11-21 01:22:48 +0000

    Two additions
    svn:revision:4584
    cvs:account:mike
    cvs:branch:trunk

--- remrt/protocol.h
+++ remrt/protocol.h
@@ -31,6 +31,8 @@
 #define	MSG_CD		10	/* change directory */
 #define MSG_CMD		11	/* server sends command to dispatcher */
 #define MSG_VERSION	12	/* server sends version to dispatcher */
+#define MSG_DB		13	/* request/send database lines 16k max */
+#define MSG_CHECK	14	/* check database files for match */
 
 /*
  *  This structure is used for MSG_PIXELS messages
