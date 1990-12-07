98de168f3a23401e5662ba13a6c43cb9d1be3a48
Michael John Muuss <mike@brlcad.org>
1990-12-07 05:41:35 +0000

    Converted to using db_scan for concat operation
    svn:revision:4720
    cvs:account:mike
    cvs:branch:trunk

--- mged/concat.c
+++ mged/concat.c
@@ -6,6 +6,7 @@
  *	f_concat()	routine to cat another GED file onto end of current file
  *
  *  Authors -
+ *	Michael John Muuss
  *	Keith A. Applin
  *  
  *  Source -
@@ -44,39 +45,21 @@ extern int	args;		/* total number of args available */
 extern int	argcnt;		/* holder for number of args added later */
 extern char	*cmd_args[];	/* array of pointers to args */
 
-void		prename();
-
 int			num_dups;
 struct directory	**dup_dirp;
 
 char	new_name[NAMESIZE];
-char	prestr[15];
+char	prestr[NAMESIZE];
 int	ncharadd;
 
-/*    P R E N A M E ( ): 	actually adds prefix to a name
- *				new_name[] = prestr[] + old_name[]
- */
-void
-prename( old_name )
-char old_name[NAMESIZE];
-{
-
-	(void)strcpy(new_name, prestr);
-	(void)strncat(new_name, old_name, NAMESIZE-1-ncharadd);
-	if( ncharadd + strlen( old_name ) > NAMESIZE-1 ) {
-		(void)printf("name truncated : %s + %s = %s\n",
-			prestr,old_name,new_name);
-	}
-}
-
 /*
- *			M G E D _ D I R A D D
+ *			M G E D _ D I R _ C H E C K
  *
- * Add an entry to the directory
+ * Check a name against the global directory.
  */
 int
-mged_diradd( loc_dbip, name, laddr, len, flags )
-register struct db_i	*loc_dbip;
+mged_dir_check( input_dbip, name, laddr, len, flags )
+register struct db_i	*input_dbip;
 register char		*name;
 long			laddr;
 int			len;
@@ -87,7 +70,7 @@ int			flags;
 	struct directory	*dupdp;
 	char			local[NAMESIZE+2];
 
-	if( loc_dbip->dbi_magic != DBI_MAGIC )  rt_bomb("mged_diradd:  bad dbip\n");
+	if( input_dbip->dbi_magic != DBI_MAGIC )  rt_bomb("mged_dir_check:  bad dbip\n");
 
 	/* Add the prefix, if any */
 	if( ncharadd > 0 )  {
@@ -99,7 +82,7 @@ int			flags;
 	local[NAMESIZE] = '\0';
 		
 	/* Look up this new name in the existing (main) database */
-	if( (dupdp = db_lookup( dbip, name, 0 )) != DIR_NULL )  {
+	if( (dupdp = db_lookup( dbip, local, LOOKUP_QUIET )) != DIR_NULL )  {
 		/* Duplicate found, add it to the list */
 		num_dups++;
 		*dup_dirp++ = dupdp;
@@ -137,7 +120,7 @@ char	**argv;
 		prestr[12] = '\0';
 	}
 
-	/* open the target file */
+	/* open the input file */
 	if( (newdbp = db_open( argv[1], "r" )) == DBI_NULL )  {
 		perror( argv[1] );
 		(void)fprintf(stderr, "dup: Can't open %s\n", argv[1]);
@@ -158,8 +141,8 @@ char	**argv;
 	}
 	dirp0 = dup_dirp;
 
-	/* Scan new directory for overlaps */
-	if( db_scan( newdbp, mged_diradd ) < 0 )  {
+	/* Scan new database for overlaps */
+	if( db_scan( newdbp, mged_dir_check, 0 ) < 0 )  {
 		(void)fprintf(stderr, "dup: db_scan failure\n");
 		return;
 	}
@@ -172,210 +155,147 @@ char	**argv;
 }
 
 
+/*
+ *			M G E D _ D I R _ A D D
+ *
+ *  Add a solid or conbination from an auxillary database
+ *  into the primary database.
+ */
+int
+mged_dir_add( input_dbip, name, laddr, len, flags )
+register struct db_i	*input_dbip;
+register char		*name;
+long			laddr;
+int			len;
+int			flags;
+{
+	register struct directory *input_dp;
+	register struct directory *dp;
+	union record		*rec;
+	char			local[NAMESIZE+2];
+
+	if( input_dbip->dbi_magic != DBI_MAGIC )  rt_bomb("mged_dir_add:  bad dbip\n");
+
+	/* Add the prefix, if any */
+	if( ncharadd > 0 )  {
+		(void)strncpy( local, prestr, ncharadd );
+		(void)strncpy( local+ncharadd, name, NAMESIZE-ncharadd );
+	} else {
+		(void)strncpy( local, name, NAMESIZE );
+	}
+	local[NAMESIZE] = '\0';
+		
+	/* Look up this new name in the existing (main) database */
+	if( (dp = db_lookup( dbip, local, LOOKUP_QUIET )) != DIR_NULL )  {
+		register int	c;
+
+		/* This object already exists under the (prefixed) name */
+		/* Shift name right two characters, and further prefix */
+		strncpy( local+2, name, NAMESIZE-2 );
+		local[1] = '@';			/* distinctive separater */
+		local[NAMESIZE] = '\0';		/* ensure null termination */
+
+		for( c = 'A'; c <= 'Z'; c++ )  {
+			local[0] = c;
+			if( (dp = db_lookup( dbip, local, LOOKUP_QUIET )) == DIR_NULL )
+				break;
+		}
+		if( c > 'Z' )  {
+			rt_log("mged_dir_add: Duplicate of name '%s', ignored\n",
+				local );
+			return 0;
+		}
+		rt_log("mged_dir_add: Duplicate of '%s' given new name '%s'\n",
+			name, local );
+	}
+
+	/* First, register this object in input database */
+	if( (input_dp = db_diradd( input_dbip, local, laddr, len, flags)) == DIR_NULL )
+		return(-1);
+
+	/* Then, register a new object in the main database */
+	if( (dp = db_diradd( dbip, local, -1L, len, flags)) == DIR_NULL )
+		return(-1);
+	if( db_alloc( dbip, dp, len ) < 0 )
+		return(-1);
+
+	/* Read in all the records for this object */
+	if( (rec = db_getmrec( input_dbip, input_dp )) == (union record *)0 )
+		return(-1);
+
+	/* Update the name, and any references */
+	if( flags & DIR_SOLID )  {
+		printf("adding solid '%s'\n", local );
+		/* Depends on all kinds of solids having name in same place */
+		NAMEMOVE( local, rec->s.s_name );
+	} else {
+		register int i;
+		char	mref[NAMESIZE+2];
+
+		printf("adding comb '%s'\n", local );
+		NAMEMOVE( local, rec->c.c_name );
+
+		/* Update all the member records */
+		for( i=1; i < dp->d_len; i++ )  {
+			if( ncharadd ) {
+				(void)strncpy( mref, prestr, ncharadd );
+				(void)strncpy( mref+ncharadd,
+					rec[i].M.m_instname,
+					NAMESIZE-1-ncharadd );
+				NAMEMOVE( mref, rec[i].M.m_instname );
+			}
+		}
+	}
+
+	/* Write the new record into the main database */
+	if( db_put( dbip, dp, rec, 0, len ) < 0 )
+		return(-1);
+
+	return 0;
+}
+
 
 /*
+ *			F _ C O N C A T
  *
- *		F _ C O N C A T
+ *  Concatenate another GED file into the current file.
+ *  Interrupts are not permitted during this function.
  *
- *	concatenates another GED file onto end of current file
+ *  Usage:  dup file.g [prefix]
  */
 void
 f_concat( argc, argv )
 int	argc;
 char	**argv;
 {
-	union record	record;
-	FILE *catfp;
-	int nskipped, i, length;
-	register struct directory *dp;
-
-	(void)signal( SIGINT, sig2 );		/* interrupts */
-
-	/* save number of args entered initially */
-	args = argc;
-	argcnt = 0;
-
-	/* get target file name */
-	while( args < 2 ) {
-		(void)printf("Enter the target file name: ");
-		argcnt = getcmd(args);
-		/* add any new args entered */
-		args += argcnt;
-	}
-
-	/* open the target file */
-	if( (catfp=fopen(cmd_args[1], "r")) == NULL ) {
-		(void)fprintf(stderr,"Can't open %s\n",cmd_args[1]);
-		return;
-	}
+	struct db_i		*newdbp;
 
 	/* get any prefix */
-	if( args < 3 ) {
-		(void)printf("Enter prefix string or CR: ");
-		argcnt = getcmd(args);
-		/* add any new args entered */
-		args += argcnt;
-		/* no prefix is acceptable */
-		if(args == 2)
-			cmd_args[2][0] = '\0';
+	if( argc < 3 ) {
+		prestr[0] = '\0';
+	} else {
+		(void)strcpy(prestr, argv[2]);
+	}
+	if( (ncharadd = strlen( prestr )) > 12 )  {
+		ncharadd = 12;
+		prestr[12] = '\0';
 	}
 
-	ncharadd = nskipped = 0;
-	(void)strcpy(prestr, cmd_args[2]);
-	ncharadd = strlen( prestr );
-
-	fread( (char *)&record, sizeof record, 1, catfp );
-	if(record.u_id != ID_IDENT) {
-		(void)printf("%s: Not a correct GED data base - STOP\n",
-				cmd_args[1]);
-		(void)fclose( catfp );
+	/* open the input file */
+	if( (newdbp = db_open( argv[1], "r" )) == DBI_NULL )  {
+		perror( argv[1] );
+		(void)fprintf(stderr, "concat: Can't open %s\n", argv[1]);
 		return;
 	}
 
-	(void)signal( SIGINT, SIG_IGN );		/* NO interrupts */
-
-	while( fread( (char *)&record, sizeof record, 1, catfp ) == 1 && ! feof(catfp) ) {
-
-		switch( record.u_id ) {
-
-			case ID_IDENT:
-			case ID_FREE:
-			case ID_ARS_B:
-			case ID_MEMB:
-			case ID_P_DATA:
-				break;
-
-			case ID_MATERIAL:
-				rt_color_addrec( &record, -1 );
-				break;
-
-			case ID_SOLID:
-				if(record.s.s_name[0] == 0)
-					break;
-				if( ncharadd ) {
-					prename(record.s.s_name);
-					NAMEMOVE(new_name, record.s.s_name);
-				}
-				if( db_lookup( dbip, record.s.s_name, LOOKUP_QUIET) != DIR_NULL ) {
-					nskipped++;
-					(void)printf("SOLID (%s) already exists in %s....will skip\n",
-						record.s.s_name, dbip->dbi_filename);
-					break;
-				}
-				/* add to the directory */
-printf("adding solid \"%s\"\n", record.s.s_name);
-				if( (dp = db_diradd( dbip, record.s.s_name, -1, 1, DIR_SOLID)) == DIR_NULL )
-					return;
-				/* add the record to the data base file */
-				db_alloc( dbip, dp, 1 );
-				db_put( dbip, dp, &record, 0, 1 );
-				break;
-
-			case ID_ARS_A:
-				if( ncharadd ) {
-					prename(record.a.a_name);
-					NAMEMOVE(new_name, record.a.a_name);
-				}
-				if( db_lookup( dbip, record.a.a_name, LOOKUP_QUIET) != DIR_NULL ) {
-					nskipped++;
-					(void)printf("ARS (%s) already exists in %s....will skip\n",
-							record.a.a_name, dbip->dbi_filename);
-					break;
-				}
-				/* add to the directory */
-				length = record.a.a_totlen;
-				if( (dp = db_diradd( dbip, record.a.a_name, -1, length+1, DIR_SOLID)) == DIR_NULL )
-					return;
-				/* add the record to the data base file */
-				db_alloc( dbip, dp, length+1 );
-				db_put( dbip, dp, &record, 0, 1 );
-				/* get the b_records */
-				for(i=1; i<=length; i++) {
-					(void)fread( (char *)&record, sizeof record, 1, catfp );
-					db_put( dbip, dp, &record, i, 1 );
-				}
-				break;
-
-			case ID_COMB:
-				if( ncharadd ) {
-					prename(record.c.c_name);
-					NAMEMOVE(new_name, record.c.c_name);
-				}
-				if( db_lookup( dbip, record.c.c_name, LOOKUP_QUIET) != DIR_NULL ) {
-					nskipped++;
-					(void)printf("COMB (%s) already exists in %s....will skip\n",
-							record.c.c_name, dbip->dbi_filename);
-					break;
-				}
-				/* add to the directory */
-#if 0
-				length = record.c.c_length;
-#else
-	/* XXXXX XXXXX XXXXX */
-				length = 0;
-#endif
-printf("adding comb \"%s\"\n", record.c.c_name);
-				if( (dp = db_diradd( dbip,  record.c.c_name, -1,
-						length+1,
-						record.c.c_flags == 'R' ?
-						DIR_COMB|DIR_REGION : DIR_COMB
-						) ) == DIR_NULL )
-					return;
-				/* add the record to the data base file */
-				db_alloc( dbip, dp, length+1 );
-				db_put( dbip, dp, &record, 0, 1 );
-				/* get the member records */
-				for(i=1; i<=length; i++) {
-					(void)fread( (char *)&record, sizeof record, 1, catfp );
-					if( record.M.m_brname[0] && ncharadd ) {
-						prename(record.M.m_brname);
-						NAMEMOVE(new_name, record.M.m_brname);
-					}
-					if( record.M.m_instname[0] && ncharadd ) {
-						prename(record.M.m_instname);
-						NAMEMOVE(new_name, record.M.m_instname);
-					}
-					db_put( dbip, dp, &record, i, 1 );
-				}
-				break;
-
-			case ID_BSOLID:
-				if( ncharadd ) {
-					prename(record.B.B_name);
-					NAMEMOVE(new_name, record.B.B_name);
-				}
-				if( db_lookup( dbip, record.B.B_name, LOOKUP_QUIET) != DIR_NULL ) {
-					nskipped++;
-					(void)printf("SPLINE (%s) already exists in %s....will skip\n",
-							record.B.B_name, dbip->dbi_filename);
-					break;
-				}
-	printf("SPLINE not implemented yet, aborting!\n");
-				/* Need to miss the knots and mesh */
-				return;
-
-			case ID_P_HEAD:
-				if(record.p.p_name[0] == 0)
-					break;
-				if( ncharadd ) {
-					prename(record.p.p_name);
-					NAMEMOVE(new_name, record.p.p_name);
-				}
-				if( db_lookup( dbip, record.p.p_name, LOOKUP_QUIET) != DIR_NULL ) {
-					nskipped++;
-					(void)printf("POLYGON (%s) already exists in %s....will skip\n",
-							record.p.p_name, dbip->dbi_filename);
-					break;
-				}
-printf("POLYGONS not implemented yet.....SKIP %s\n",record.p.p_name);
-				break;
-
-			default:
-				(void)printf("BAD record type (%c) in %s\n",
-						record.u_id,cmd_args[1]);
-				break;
-
-		}
-
+	/* Scan new database, adding everything encountered. */
+	if( db_scan( newdbp, mged_dir_add, 1 ) < 0 )  {
+		(void)fprintf(stderr, "concat: db_scan failure\n");
+		return;
 	}
+
+	/* Free all the directory entries, and close the input database */
+	db_close( newdbp );
+
+	sync();		/* just in case... */
 }
