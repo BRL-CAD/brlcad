31153d87c6ca96c9b583d9e08ee143b5955d5399
Michael John Muuss <mike@brlcad.org>
1991-01-11 06:04:32 +0000

    Added 2 new routines
    svn:revision:4971
    cvs:account:mike
    cvs:branch:trunk

--- h/raytrace.h
+++ h/raytrace.h
@@ -1470,6 +1470,11 @@ RT_EXTERN(double rt_dist_pt_lseg, (point_t pca, point_t a, point_t b, point_t p)
 /* CxDiv, CxSqrt */
 extern void rt_pr_roots();		/* print complex roots */
 
+/* pr.c */
+RT_EXTERN(void rt_pr_fallback_angle, (struct rt_vls *str, char *prefix
+	double angles[5]));
+RT_EXTERN(void rt_find_fallback_angle, (double angles[5], vect_t vec));
+
 /*
  *  Constants provided and used by the RT library.
  */
