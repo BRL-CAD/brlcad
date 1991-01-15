6692d738d122be331d29544853253b07246576bc
Lee Butler <iraytrace@users.noreply.github.com>
1991-01-15 14:27:58 +0000

    changed command line parsing to use getopt, added -i flag for calibration image display, added -F flage for selecting framebuffer
    svn:revision:5051
    cvs:account:butler
    cvs:branch:trunk

--- fb/fbgamma.c
+++ fb/fbgamma.c
@@ -24,10 +24,86 @@ static char RCSid[] = "@(#)$Header$ (BRL)";
 #include "externs.h"
 #include "fb.h"
 
+char *options = "ihoF:";
+extern char *optarg;
+extern int optind, opterr, getopt();
+
 void	checkgamma();
 
+unsigned char rampval[10] = { 255, 128, 64, 32, 16, 8, 4, 2, 1, 0 };
+int x, y, scr_width, scr_height, patch_width, patch_height;
+char *altline, *line;
+char *framebuffer = (char *)NULL;
+int image = 0;
+
 static char usage[] = "\
-Usage: fbgamma [-h -o] val [gval bval]\n";
+Usage: fbgamma [-h -o -i] [-F framebuffer] val [gval bval]\n";
+
+void mk_ramp(fb, r, g, b, n)
+FBIO *fb;
+int r, g, b, n;
+{
+
+	/* grey ramp */
+	for (x=0 ; x < scr_width ; ++x) {
+		if (r) line[x*3] = rampval[x / patch_width + 1];
+		else line[x*3] = 0;
+		if (g) line[x*3+1] = rampval[x / patch_width + 1];
+		else line[x*3+1] = 0;
+		if (b) line[x*3+2] = rampval[x / patch_width + 1];
+		else line[x*3+2] = 0;
+	}
+	for (y=patch_height*n ; y < patch_height*(n+1) && y < scr_height ; ++y) {
+		fb_write(fb, 0, y, line, scr_width);
+	}
+
+	for (x=0 ; x < scr_width ; ++x) {
+		if (r) line[x*3] = rampval[x / patch_width];
+		else line[x*3] = 0;
+		if (g) line[x*3+1] = rampval[x / patch_width];
+		else line[x*3+1] = 0;
+		if (b) line[x*3+2] = rampval[x / patch_width];
+		else line[x*3+2] = 0;
+	}
+	for (y=patch_height*(n+1) ; y < patch_height*(n+2) && y < scr_height ; y += 2) {
+		fb_write(fb, 0, y, altline, scr_width);
+		fb_write(fb, 0, y+1, line, scr_width);
+	}
+}
+
+void disp_image(fb)
+FBIO *fb;
+{
+
+	scr_width = fb_getwidth(fb);
+	scr_height = fb_getheight(fb);
+
+	patch_width = scr_width / 8;
+	patch_height = scr_height / 14;
+
+	if ((line = malloc(scr_width*3)) == (char *)NULL) {
+		exit(-1);
+	}
+
+	if ((altline = malloc(scr_width*3)) == (char *)NULL) {
+		exit(-1);
+	} else {
+		bzero(altline, scr_width*3);
+	}
+
+	mk_ramp(fb, 1, 1, 1, 0);
+	mk_ramp(fb, 1, 0, 0, 2);
+	mk_ramp(fb, 1, 1, 0, 4);
+	mk_ramp(fb, 0, 1, 0, 6);
+	mk_ramp(fb, 0, 1, 1, 8);
+	mk_ramp(fb, 0, 0, 1, 10);
+	mk_ramp(fb, 1, 0, 1, 12);
+
+	(void)free(line);
+	(void)free(altline);
+}
+
+
 
 main( argc, argv )
 int argc; char **argv;
@@ -44,33 +120,32 @@ int argc; char **argv;
 	onegamma = 0;
 
 	/* check for flags */
-	while( argc > 1 ) {
-		if( strcmp(argv[1],"-h") == 0 ) {
-			fbsize = 1024;
-		} else if( strcmp(argv[1],"-o") == 0 ) {
-			overlay++;
-		} else
-			break;
-		argc--;
-		argv++;
+	opterr = 0;
+	while ((i=getopt(argc, argv, options)) != EOF) {
+		switch(i) {
+		case 'h'	: fbsize = 1024; break;
+		case 'o'	: overlay++; break;
+		case 'i'	: image = !image; break;
+		case 'F'	: framebuffer = optarg; break;
+		default		: break;
+		}
 	}
 
-	/* now get the gammas */
-	if( argc == 2 ) {
+	if (optind == argc - 1) {
 		/* single value for all channels */
-		f = atof( argv[1] );
+		f = atof( argv[optind] );
 		checkgamma( f );
 		gamr = gamg = gamb = 1.0 / f;
 		onegamma++;
-	} else if( argc == 4 ) {
+	} else if (optind == argc - 4 ) {
 		/* different RGB values */
-		f = atof( argv[1] );
+		f = atof( argv[optind] );
 		checkgamma( f );
 		gamr = 1.0 / f;
-		f = atof( argv[2] );
+		f = atof( argv[optind+1] );
 		checkgamma( f );
 		gamg = 1.0 / f;
-		f = atof( argv[3] );
+		f = atof( argv[optind+2] );
 		checkgamma( f );
 		gamb = 1.0 / f;
 	} else {
@@ -78,9 +153,12 @@ int argc; char **argv;
 		exit( 1 );
 	}
 
-	if( (fbp = fb_open( NULL, fbsize, fbsize )) == FBIO_NULL ) {
+	if( (fbp = fb_open( framebuffer, fbsize, fbsize )) == FBIO_NULL ) {
 		exit( 2 );
 	}
+
+	/* draw the gamma image if requested */
+	if (image) disp_image(fbp);
 
 	/* get the starting map */
 	if( overlay ) {
