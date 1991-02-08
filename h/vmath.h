6fcbad8d3fb6d829ca3b33e14886d894239cfec0
Michael John Muuss <mike@brlcad.org>
1991-02-08 22:41:02 +0000

    VMINMAX needed extra parens
    svn:revision:5186
    cvs:account:mike
    cvs:branch:trunk

--- h/vmath.h
+++ h/vmath.h
@@ -627,9 +627,9 @@ typedef fastf_t	plane_t[ELEMENTS_PER_PLANE];
 /* Macros to update min and max X,Y,Z values to contain a point */
 #define V_MIN(r,s)	if( (r) > (s) ) r = (s)
 #define V_MAX(r,s)	if( (r) < (s) ) r = (s)
-#define VMIN(r,s)	{ V_MIN(r[X],s[X]); V_MIN(r[Y],s[Y]); V_MIN(r[Z],s[Z]); }
-#define VMAX(r,s)	{ V_MAX(r[X],s[X]); V_MAX(r[Y],s[Y]); V_MAX(r[Z],s[Z]); }
-#define VMINMAX( min, max, pt )	{ VMIN( min, pt ); VMAX( max, pt ); }
+#define VMIN(r,s)	{ V_MIN((r)[X],(s)[X]); V_MIN((r)[Y],(s)[Y]); V_MIN((r)[Z],(s)[Z]); }
+#define VMAX(r,s)	{ V_MAX((r)[X],(s)[X]); V_MAX((r)[Y],(s)[Y]); V_MAX((r)[Z],(s)[Z]); }
+#define VMINMAX( min, max, pt )	{ VMIN( (min), (pt) ); VMAX( (max), (pt) ); }
 
 /* Divide out homogeneous parameter from hvect_t, creating vect_t */
 #ifdef SHORT_VECTORS
