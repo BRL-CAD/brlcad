e3aeaf8c07eaaf1b0622510e03b7af42124eed36
Michael John Muuss <mike@brlcad.org>
1991-02-08 21:15:04 +0000

    Arg order change
    svn:revision:5185
    cvs:account:mike
    cvs:branch:trunk

--- libwdb/export.c
+++ libwdb/export.c
@@ -38,11 +38,11 @@ static char RCSid[] = "@(#)$Header$ (BRL)";
  *	<0	error
  */
 int
-mk_export_fwrite( fp, gp, id, name )
+mk_export_fwrite( fp, name, gp, id )
 FILE		*fp;
+char		*name;
 genptr_t	gp;
 int		id;
-char		*name;
 {
 	struct rt_db_internal	intern;
 	struct rt_external	ext;
