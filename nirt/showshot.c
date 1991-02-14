99a67d924b67c527f9424486eca0282c571e8ad3
Paul Tanenbaum <pjt@brlcad.org>
1991-02-14 14:01:15 +0000

    1. Added check to ensure that the names of the objects created    won't exced NAMESIZE and added the '-n name' command-line    option. 2. Eliminated the '-d' command-line option.
    svn:revision:5213
    cvs:account:pjt
    cvs:branch:trunk

--- nirt/showshot.c
+++ nirt/showshot.c
@@ -13,20 +13,27 @@
  *	    15 Jan 91	Eliminated the mandatory argument model.g
  *			from the command line.  The database name
  *			wasn't used anyway!
+ *	    14 Feb 91	Added check to ensure that the names of
+ *			the objects created won't exceed NAMESIZE,
+ *			and added the '-n name' option.  Eliminated
+ *			the '-d' debug option.
  */
+
 #ifndef lint
 static char RCSid[] = "$Header$";
 #endif
 
 #include <stdio.h>
 #include <strings.h>
+#include <ctype.h>
 #include <machine.h>
 #include <vmath.h>
+#include <db.h>
 
 #define		BUF_LEN		128
-#define		OPT_STRING	"dr:?"
+#define		OPT_STRING	"n:r:?"
 #define		RAY_COLOR	"255 255 0"
-#define		print_usage()	fputs("Usage: 'show-shot [-d] [-r rad]'\n",\
+#define		print_usage()	(void) fputs("Usage: 'show-shot [-n name] [-r radius]'\n",\
 					stderr)
 
 main (argc, argv)
@@ -37,14 +44,13 @@ char	**argv;
 {
     char	buf[BUF_LEN];	/* Contents of current input line */
     char	*bp;		/* Pointer into buf */
-    char	gname[BUF_LEN];	/* Name of the group */
+    char	*nlp;		/* Location of newline in buf */
     char	rname[BUF_LEN];	/* Name of current region */
-    char	rayname[BUF_LEN];/* Name of ray */
-    fastf_t	ray_radius=1.0;	/* Thickness of the RCC */
+    char	rayname[BUF_LEN];	/* Name of ray */
+    fastf_t	ray_radius = 1.0;	/* Thickness of the RCC */
     point_t	entryp;		/* Ray's entry into current region */
     point_t	exitp;		/* Ray's exit from current region */
     point_t	first_entryp;	/* Ray's entry into the entire geometry */
-    int		debug = 0;	/* Show actions instead of performing them? */
     int		i;		/* Index into rname */
     int		line_nm = 0;	/* Number of current line of input */
     int		opt;		/* Command-line option returned by getopt() */
@@ -55,17 +61,18 @@ char	**argv;
 
     int         getopt();
 
+    *rayname = '\0';
     /* Handle command-line options */
     while ((opt = getopt(argc, argv, OPT_STRING)) != -1)
 	switch (opt)
 	{
-	    case 'd':
-		debug = 1;
+	    case 'n':
+		(void) strcpy(rayname, optarg);
 		break;
 	    case 'r':
 		if (sscanf(optarg, "%F", &ray_radius) != 1)
 		{
-		    fprintf(stderr, "Illegal radius: '%s'\n", optarg);
+		    (void) fprintf(stderr, "Illegal radius: '%s'\n", optarg);
 		    exit (1);
 		}
 		break;
@@ -75,25 +82,32 @@ char	**argv;
 	}
     
     /* Ensure proper command-line syntax */
-    if (! debug && (argc != optind))
+    if (optind != argc)
     {
 	print_usage();
 	exit (1);
     }
 
-    /* Construct the name of the group in which the results will be put */
-    sprintf(rayname, "nirt.ray.%d", pid);
-    printf("in %s.s sph 0 0 0 1\n", rayname);
-    printf("r %s.r u %s.s\n", rayname, rayname);
-    printf("mater %s.r\n\n\n%s\n\n", rayname, RAY_COLOR);
-    sprintf(gname, "nirt.%d", pid);
-    printf("g %s", gname);
+    /* Construct the names of the objects to add to the database */
+    if (*rayname == '\0')	/* Was one given on command line? */
+	(void) sprintf(rayname, "ray.%d", pid);
+    if (strlen(rayname) > NAMESIZE - 3)
+    {
+	(void) fprintf(stderr,
+	    "Name '%s.s' for ray solid may not exceed %d characters\n",
+	    rayname, NAMESIZE - 1);
+	(void) fputs("Use the '-n name' option to specify a different name\n",
+	    stderr);
+	exit (1);
+    }
+    (void) printf("in %s.s sph 0 0 0 1\n", rayname);
+    (void) printf("r %s.r u %s.s\n", rayname, rayname);
+    (void) printf("mater %s.r\n\n\n%s\n\n", rayname, RAY_COLOR);
+    (void) printf("g %s", rayname);
 
     /* Read the input */
     while (fgets(buf, BUF_LEN, stdin) != NULL)
     {
-	char	*nlp;	/* Location of newline in buf */
-
 	++line_nm;
 	bp = buf;
 	if ((nlp = index(bp, '\n')) != 0)
@@ -114,16 +128,13 @@ char	**argv;
 		&exitp[X], &exitp[Y], &exitp[Z])
 	    != 6)
 	{
-	    fprintf(stderr, "Illegal data on line %d: '%s'\n", line_nm, bp);
+	    (void) fprintf(stderr,
+		"Illegal data on line %d: '%s'\n", line_nm, bp);
 	    exit (1);
 	}
 
-	printf(" %s", rname);
-#if 0
-	printf("entered %s at (%f, %f, %f) and exited at (%f, %f, %f)\n",
-	    rname, entryp[X], entryp[Y], entryp[Z], exitp[X], exitp[Y], exitp[Z]);
-#endif
-	
+	(void) printf(" %s", rname);
+
 	if (line_nm == 1)
 	{
 	    VMOVE(first_entryp, entryp);
@@ -131,17 +142,17 @@ char	**argv;
     }
     if (! feof(stdin))
     {
-	fputs("Error from fgets().  This shouldn't happen", stderr);
+	(void) fputs("Error from fgets().  This shouldn't happen", stderr);
 	exit (1);
     }
 
-    printf("\nkill %s.s\nin %s.s rcc\n\t%f %f %f\n\t%f %f %f\n\t%f\n",
+    (void) printf("\nkill %s.s\nin %s.s rcc\n\t%f %f %f\n\t%f %f %f\n\t%f\n",
 	rayname, rayname,
 	first_entryp[X], first_entryp[Y], first_entryp[Z],
 	exitp[X] - first_entryp[X],
 	exitp[Y] - first_entryp[Y],
 	exitp[Z] - first_entryp[Z],
 	ray_radius);
-    printf("g %s %s.r\n", gname, rayname);
-    fprintf(stderr, "Group is '%s'\n", gname);
+    (void) printf("g %s %s.r\n", rayname, rayname);
+    (void) fprintf(stderr, "Group is '%s'\n", rayname);
 }
