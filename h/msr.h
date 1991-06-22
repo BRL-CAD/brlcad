ee0369a2232ea8917fe3e0b0cfb2f81664d4d69a
Christopher Johnson <cjohnson@brlcad.org>
1991-06-22 02:22:23 +0000

    Fix some types and otherwise clean up the include file.
    svn:revision:5704
    cvs:account:cjohnson
    cvs:branch:trunk

--- h/msr.h
+++ h/msr.h
@@ -5,6 +5,9 @@
  * and to limit the area/volume that a set of random numbers inhabit.
  *
  *	$Log$
+ * Revision 1.1  90/04/20  02:05:15  cjohnson
+ * Initial revision
+ * 
  * 
  */
 /*
@@ -26,32 +29,38 @@ struct msr_unif {
  */
 struct msr_gauss {
 	long	msr_gauss_seed;
+	int	msr_gauss_dbl_ptr;
+	double	*msr_gauss_doubles;
 	int	msr_gauss_ptr;
 	double	*msr_gausses;
 };
+double msr_unif_double_fill();
+double msr_gauss_fill();
+struct msr_unif *msr_unif_init();
+struct msr_gauss *msr_gauss_init();
 
 #define	MSR_UNIF_LONG(_p)	(((_p)->msr_long_ptr ) ? \
 	(_p)->msr_longs[--(_p)->msr_long_ptr] : \
-	msr_unif_fill_long(_p))
+	msr_unif_long_fill(_p))
 #define MSR_UNIF_DOUBLE(_p)	(((_p)->msr_double_ptr) ? \
-	(_p)->msr_longs[--(_p)->msr_double_ptr] : \
+	(_p)->msr_doubles[--(_p)->msr_double_ptr] : \
 	msr_unif_double_fill(_p))
 
 #define MSR_UNIF_CIRCLE(_p,_x,_y,_r) { \
 	do { \
-		(_x) = 2.0*MSR_UNIF_DOUBLE((_p)) - 1.0; \
-		(_y) = 2.0*MSR_UNIF_DOUBLE((_p)) - 1.0; \
+		(_x) = 2.0*MSR_UNIF_DOUBLE((_p)); \
+		(_y) = 2.0*MSR_UNIF_DOUBLE((_p)); \
 		(_r) = (_x)*(_x)+(_y)*(_y); \
 	} while ((_r) >= 1.0);  }
 
 #define	MSR_UNIF_SPHERE(_p,_x,_y,_z,_r) { \
 	do { \
-		(_x) = 2.0*MSR_UNIF_DOUBLE(_p) - 1.0; \
-		(_y) = 2.0*MSR_UNIF_DOUBLE(_p) - 1.0; \
-		(_z) = 2.0*MSR_UNIF_DOUBLE(_p) - 1.0; \
+		(_x) = 2.0*MSR_UNIF_DOUBLE(_p); \
+		(_y) = 2.0*MSR_UNIF_DOUBLE(_p); \
+		(_z) = 2.0*MSR_UNIF_DOUBLE(_p); \
 		(_r) = (_x)*(_x)+(_y)*(_y)+(_z)*(_z);\
 	} while ((_r) >= 1.0) }
 
 #define	MSR_GAUSS_DOUBLE(_p)	(((_p)->msr_gauss_ptr) ? \
-	(_p)->msr_gausses[--(_p)->msr_gauss_ptr)] : \
-	msr_gause_fill(_p))
+	(_p)->msr_gausses[--(_p)->msr_gauss_ptr] : \
+	msr_gauss_fill(_p))
