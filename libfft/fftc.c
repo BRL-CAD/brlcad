60d9e118309df535d6f91038c0181dad12be82b4
Michael John Muuss <mike@brlcad.org>
1991-07-02 01:26:04 +0000

    libfft module
    svn:revision:5905
    cvs:branch:trunk
    cvs:account:mike

---
+++ libfft/fftc.c
@@ -0,0 +1,29 @@
+/*
+ *	Split Radix Decimation in Time FFT C code generator.
+ */
+extern int adds, mults;
+#include <stdio.h>
+#include <math.h>
+
+static char usage[] = "\
+Usage: fftc length > fftlength.c\n";
+
+main( argc, argv )
+int	argc;
+char	**argv;
+{
+	double	x[4097];
+	int	i, n, m;
+
+	if( argc != 2 ) {
+		fprintf( stderr, usage );
+		exit( 1 );
+	}
+
+	n = atoi(argv[1]);
+	m = log((double)n)/log(2.0) + 0.5;	/* careful truncation */
+
+	splitdit( x, n, m );
+(void)fprintf( stderr, "adds = %d, mults = %d\n", adds, mults );
+	return(0);
+}
---
+++ libfft/ifftc.c
@@ -0,0 +1,29 @@
+/*
+ *  Split Radix Decimation in Freq Inverse FFT C code generator.
+ */
+extern int adds, mults;
+#include <stdio.h>
+#include <math.h>
+
+static char usage[] = "\
+Usage: ifftc length > fftlength.c\n";
+
+main( argc, argv )
+int	argc;
+char	**argv;
+{
+	double	x[4097];
+	int	i, n, m;
+
+	if( argc != 2 ) {
+		fprintf( stderr, usage );
+		exit( 1 );
+	}
+
+	n = atoi(argv[1]);
+	m = log((double)n)/log(2.0) + 0.5;	/* careful truncation */
+
+	ditsplit( x, n, m );
+fprintf( stderr, "adds = %d, mults = %d\n", adds, mults );
+	return(0);
+}
