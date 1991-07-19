4f891641313742c68abc9aa7ca7a7c3b0578d3f1
Michael John Muuss <mike@brlcad.org>
1991-07-19 01:52:33 +0000

    abs() is defined already on IBM.
    svn:revision:6169
    cvs:account:mike
    cvs:branch:trunk

--- nirt/nirt.h
+++ nirt/nirt.h
@@ -40,7 +40,9 @@
 /*	MACROS WITH ARGUMENTS	*/
 #define    max(a,b)             (((a)>(b))?(a):(b))
 #define    min(a,b)             (((a)<(b))?(a):(b))
-#define    abs(a)               ((a)>=0 ? (a):(-a))
+#if !defined(abs)
+# define    abs(a)               ((a)>=0 ? (a):(-a))
+#endif
 #define    getmem(S)            (S*)malloc(sizeof(S))
 #define    com_usage(c)         fprintf (stderr, "Usage:  %s %s\n", \
 				    c -> com_name, c -> com_args);
