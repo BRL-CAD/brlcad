988a3d5629652b4006aae0c401d720e27e8179c8
Michael John Muuss <mike@brlcad.org>
1991-07-23 02:31:01 +0000

    Added NeXT
    svn:revision:6212
    cvs:account:mike
    cvs:branch:trunk

--- libbu/htond.c
+++ libbu/htond.c
@@ -75,7 +75,7 @@ static char RCSid[] = "@(#)$Header$ (BRL)";
 	(defined(sgi) && defined(mips)) || \
 	(defined(__sgi) && defined(__mips)) || \
 	defined(pyr) || defined(apollo) || defined(aux) || \
-	defined(_AIX)
+	defined(_AIX) || defined(NeXT)
 
 	/*  These systems already operate in
 	 *  IEEE format internally, using big-endian order.
