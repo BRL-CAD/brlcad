deca0923c8e1952576cd743f79c582d287892392
Paul Tanenbaum <pjt@brlcad.org>
1991-07-25 12:54:04 +0000

    Corrected default fmt h, by swapping "LOS" and "Obliq_in" to agree with what the fmt p was actually producing
    svn:revision:6254
    cvs:account:pjt
    cvs:branch:trunk

--- nirt/parse_fmt.c
+++ nirt/parse_fmt.c
@@ -71,8 +71,8 @@ FILE		*outf = stdout;
 char		*def_fmt[] =
 		{
 		    "\"Origin (x y z) = (%.2f %.2f %.2f)  (h v d) = (%.2f %.2f %.2f)\nDirection (x y z) = (%.4f %.4f %.4f)  (az el) = (%.2f %.2f)\n\" x_orig y_orig z_orig h v d_orig x_dir y_dir z_dir a e",
-		    "\"    Region Name               Entry (x y z)           Obliq_in  LOS\n\"",
-		    "\"%-20s (%9.3f %9.3f %9.3f) %8.2f %7.3f\n\" reg_name x_in y_in z_in los obliq_in",
+		    "\"    Region Name               Entry (x y z)              LOS  Obliq_in\n\"",
+		    "\"%-20s (%9.3f %9.3f %9.3f) %8.2f %8.3f\n\" reg_name x_in y_in z_in los obliq_in",
 		    "\"\"",
 		    "\"You missed the target\n\"",
 		    "\"OVERLAP: '%s' and '%s' xyz_in=(%g %g %g) los=%g\n\" ov_reg1_name ov_reg2_name ov_x_in ov_y_in ov_z_in ov_los"
