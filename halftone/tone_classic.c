97b5942a89912fff5597d3c058a92b0731b3d501
Michael John Muuss <mike@brlcad.org>
1991-07-26 20:56:26 +0000

    Formal parameters to a subroutine should be all lower case. The upper case guys conflicted with pre-defined macros.
    svn:revision:6268
    cvs:account:mike
    cvs:branch:trunk

--- halftone/tone_classic.c
+++ halftone/tone_classic.c
@@ -1,8 +1,24 @@
+/*
+ *			T O N E _ C L A S S I C
+ *
+ *  Author -
+ *	Christopher T. Johnson	- 90/03/21
+ *  
+ *  Source -
+ *	SECAD/VLD Computing Consortium, Bldg 394
+ *	The U. S. Army Ballistic Research Laboratory
+ *	Aberdeen Proving Ground, Maryland  21005-5066
+ *  
+ *  Distribution Status -
+ *	Public Domain, Distribution Unlimitied.
+ */
 #ifndef lint
-static char rcsid[] = "$Header$";
+static char RCSid[] = "@(#)$Header$ (BRL)";
 #endif
+
 #include <stdio.h>
 #include "msr.h"
+
 extern int Debug;
 extern int Levels;
 extern struct msr_unif *RandomFlag;
@@ -38,49 +54,18 @@ static unsigned char	ordered[6][6] = {
  *	Levels	- Number of Intensity Levels
  *	RandomFlag - Show we toss random numbers?
  *
- * Calls:
- *	Random(0) - To get a random number.
- *
- * Method:
- *	straight-forward.
- *
  * Author:
  *	Christopher T. Johnson	- 90/03/21
- *
- * $Log$
- * Revision 2.2  90/04/13  01:46:14  cjohnson
- * Change include "*.h" to "./*.h"
- * 
- * Revision 2.1  90/04/13  01:23:15  cjohnson
- * First Relese.
- * 
- * Revision 1.6  90/04/13  00:51:47  cjohnson
- * Comment clean up.
- * 
- * Revision 1.5  90/04/12  17:36:46  cjohnson
- * Add Random number processing.
- * 
- * Revision 1.4  90/04/10  16:45:49  cjohnson
- * Fix Intensity methods 
- * 
- * Revision 1.3  90/04/10  03:31:25  cjohnson
- * Add intensity levels.
- * 
- * Revision 1.2  90/04/10  01:02:29  cjohnson
- * Fix order indices.  Works now.
- * 
- * Revision 1.1  90/04/09  16:16:37  cjohnson
- * Initial revision
- * 
  */
-tone_classic(Pix,X,Y,NX,NY,New)
-int	Pix;
-int	X, Y, NX, NY;
-int	New;
+int
+tone_classic(pix,x,y,nx,ny,new)
+int	pix;
+int	x, y, nx, ny;
+int	new;
 {
-	register int threshold = 14*ordered[( X + 3) % 6][ Y % 6];
+	register int threshold = 14*ordered[( x + 3) % 6][ y % 6];
 	if (RandomFlag) {
 		threshold += MSR_UNIF_DOUBLE(RandomFlag)*63;
 	}
-	return ((Pix*Levels + threshold)/255);
+	return ((pix*Levels + threshold)/255);
 }
