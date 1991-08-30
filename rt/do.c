dd2a3987894c05acf7004430ca80cbd1e620d270
Michael John Muuss <mike@brlcad.org>
1991-08-30 23:11:20 +0000

    Need externs.h
    svn:revision:6421
    cvs:account:mike
    cvs:branch:trunk

--- rt/do.c
+++ rt/do.c
@@ -40,6 +40,7 @@ static char RCSrt[] = "@(#)$Header$ (BRL)";
 #include "vmath.h"
 #include "raytrace.h"
 #include "fb.h"
+#include "externs.h"
 #include "./material.h"
 #include "./rdebug.h"
 #include "../librt/debug.h"
