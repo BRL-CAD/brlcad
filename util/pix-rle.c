44defe2a2ede6ca630fe213f2f5440a6096b5d60
Michael John Muuss <mike@brlcad.org>
1991-08-30 00:06:04 +0000

    Stardent ANSI c
    svn:revision:6358
    cvs:account:mike
    cvs:branch:trunk

--- util/pix-rle.c
+++ util/pix-rle.c
@@ -157,7 +157,7 @@ char	*argv[];
 	outrle.xmin = outrle.ymin = 0;
 	outrle.xmax = file_width-1;
 	outrle.ymax = file_height-1;
-	outrle.comments = (char **)0;
+	outrle.comments = (CONST_DECL char **)0;
 
 	/* Add comments to the header file, since we have one */
 	sprintf( comment, "converted_from=%s", infile );
