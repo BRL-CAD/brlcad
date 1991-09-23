540a80b7687db29299d20884533b2e9cb3f84444
Michael John Muuss <mike@brlcad.org>
1991-09-23 02:55:36 +0000

    Added protection
    svn:revision:6509
    cvs:account:mike
    cvs:branch:trunk

--- jove/mkversion.sh
+++ jove/mkversion.sh
@@ -1,5 +1,12 @@
+#!/bin/sh
+# @(#)$Header$ (BRL)
+
 p=`cat version.number`
 c=`echo 0 1 $p +p | dc`
+
+rm -f version.c version.number
+
 echo "char	*version = \"1.$c\";" > version.c
-cc -c version.c
 echo $c > version.number
+
+cc -c version.c
