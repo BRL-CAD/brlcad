b660fb714c18acf25c43cd8755d8a9a367edf249
Paul Stay <stay@brlcad.org>
1992-04-13 10:21:55 +0000

    added nurb_curvature routines for ansiposix definition
    svn:revision:6835
    cvs:account:stay
    cvs:branch:trunk

--- h/nurb.h
+++ h/nurb.h
@@ -188,6 +188,9 @@ RT_EXTERN(struct knot_vector *rt_nurb_gen_knot_vector, (int order,
 /* nurb_norm.c */
 RT_EXTERN(fastf_t *rt_nurb_s_norm, (struct snurb *srf, fastf_t u, fastf_t v));
 
+/* nurb_c2.c */
+RT_EXTERN(fastf_t *rt_nurb_curvature, (struct snurb *srf, fastf_t u, fastf_t v));
+
 /* nurb_plot.c */
 
 /* nurb_poly.c */
