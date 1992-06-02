c0f72538e8d2d6b30867eb9362ffc25067b0dfbc
Michael John Muuss <mike@brlcad.org>
1992-06-02 23:26:01 +0000

    Changed to using rt_nurb_curvature, rather than using inline code.
    svn:revision:6926
    cvs:account:mike
    cvs:branch:trunk

--- librt/g_nurb.c
+++ librt/g_nurb.c
@@ -275,7 +275,7 @@ struct seg		*seghead;
 				rt_free( (char *)o,
 					"rt_nurb_shot:rt_nurb_uv_hit structure");
 
-				rt_nurb_add_hit( &hit_list, hit);
+				rt_nurb_add_hit( &hit_list, hit, tol );
 			}
 			n = n->next;  
 		}
@@ -400,150 +400,21 @@ struct soltab		*stp;
 /*	register struct nurb_specific *nurb =
 		(struct nurb_specific *)stp->st_specific; */
 	struct snurb * srf = (struct snurb *) hitp->hit_private;
-	struct snurb * us, *vs, * uus, * vvs, *uvs;
-	fastf_t *ue, *ve, *uue, *vve, *uve, *se;
         fastf_t         u, v;
-        fastf_t         E, F, G;                /* First Fundamental Form */
-        fastf_t         L, M, N;                /* Second Fundamental form */
-        fastf_t         denom;
-        fastf_t         wein[4];                /*Weingarten matrix */
-        fastf_t         evec[3];
-        fastf_t         mean, gauss, discrim;
-        vect_t          norm;
-	int 		i;
 
 	if( srf->order[0] <= 2 && srf->order[1] <= 2)
 	{
-
 	 	cvp->crv_c1 = cvp->crv_c2 = 0;
 
 		/* any tangent direction */
-	 	vec_ortho( cvp->crv_pdir, hitp->hit_normal );
+	 	mat_vec_ortho( cvp->crv_pdir, hitp->hit_normal );
 		return;
 	}
 	
 	u = hitp->hit_vpriv[0];
 	v = hitp->hit_vpriv[1];
 	
-	us = (struct snurb *) rt_nurb_s_diff(srf, RT_NURB_SPLIT_ROW);
-	vs = (struct snurb *) rt_nurb_s_diff(srf, RT_NURB_SPLIT_COL);
-	uus = (struct snurb *) rt_nurb_s_diff(us, RT_NURB_SPLIT_ROW);
-	vvs = (struct snurb *) rt_nurb_s_diff(vs, RT_NURB_SPLIT_COL);
-	uvs = (struct snurb *) rt_nurb_s_diff(vs, RT_NURB_SPLIT_ROW);
-	
-	se = (fastf_t *) rt_nurb_s_eval(srf, u, v);
-	ue = (fastf_t *) rt_nurb_s_eval(us, u,v);
-	ve = (fastf_t *) rt_nurb_s_eval(vs, u,v);
-	uue = (fastf_t *) rt_nurb_s_eval(uus, u,v);
-	vve = (fastf_t *) rt_nurb_s_eval(vvs, u,v);
-	uve = (fastf_t *) rt_nurb_s_eval(uvs, u,v);
-
-	rt_nurb_free_snurb( us);
-	rt_nurb_free_snurb( vs);
-	rt_nurb_free_snurb( uus);
-	rt_nurb_free_snurb( vvs);
-	rt_nurb_free_snurb( uvs);
-
-	if( RT_NURB_IS_PT_RATIONAL( srf->pt_type ))
-	{
-		for( i = 0; i < 3; i++)
-		{
-			ue[i] = (1.0 / se[3] * ue[i]) -
-				(ue[3]/se[3]) * se[0]/se[3];
-			ve[i] = (1.0 / se[3] * ve[i]) -
-				(ve[3]/se[3]) * se[0]/se[3];
-		}
-		VCROSS(norm, ue, ve);
-		VUNITIZE(norm);
-		E = VDOT( ue, ue);
-		F = VDOT( ue, ve);
-		G = VDOT( ve, ve);
-		
-		for( i = 0; i < 3; i++)
-		{
-			uue[i] = (1.0 / se[3] * uue[i]) -
-				2 * (uue[3]/se[3]) * uue[i] -
-				uue[3]/se[3] * (se[i]/se[3]);
-
-			vve[i] = (1.0 / se[3] * vve[i]) -
-				2 * (vve[3]/se[3]) * vve[i] -
-				vve[3]/se[3] * (se[i]/se[3]);
-
-			 uve[i] = 1.0 / se[3] * uve[i] +
-	                        (-1.0 / (se[3] * se[3])) *
-        	                (ve[3] * ue[i] + ue[3] * ve[i] +
-                	         uve[3] * se[i]) + 
-				(-2.0 / (se[3] * se[3] * se[3])) *
-	                        (ve[3] * ue[3] * se[i]);
-		}
-
-		L = VDOT( norm, uue);
-		M = VDOT( norm, uve);
-		N = VDOT( norm, vve);
-		
-	} else
-	{
-		VCROSS( norm, ue, ve);
-		VUNITIZE( norm );
-		E = VDOT( ue, ue);
-		F = VDOT( ue, ve);
-		G = VDOT( ve, ve);
-		
-		L = VDOT( norm, uue);
-		M = VDOT( norm, uve);
-		N = VDOT( norm, vve);
-	}
-
-	denom = ( (E*G) - (F*F) );
-	gauss = (L * N - M *M)/denom;
-	mean = (G * L + E * N - 2 * F * M) / (2 * denom);
-	discrim = sqrt( mean * mean - gauss);
-	
-	cvp->crv_c1 = mean - discrim;
-	cvp->crv_c2 = mean + discrim;
-
-	if( APX_EQ( ( E*G - F*F), 0.0 ))
-	{
-		rt_log("first fundamental form is singular E = %g F= %g G = %g\n",
-			E,F,G);
-		goto cleanup;
-	}
-
-
-        wein[0] = ( (G * L) - (F * M))/ (denom);
-        wein[1] = ( (G * M) - (F * N))/ (denom);
-        wein[2] = ( (E * M) - (F * L))/ (denom);
-        wein[3] = ( (E * N) - (F * M))/ (denom);
-
-
-        if ( APX_EQ( wein[1] , 0.0 ) && APX_EQ( wein[3] - cvp->crv_c1, 0.0) )
-        {
-                evec[0] = 0.0; evec[1] = 1.0;
-        } else
-        {
-                evec[0] = 1.0;
-                if( fabs( wein[1] ) > fabs( wein[3] - cvp->crv_c1) )
-                {
-                        evec[1] = (cvp->crv_c1 - wein[0]) / wein[1];
-                } else
-                {
-                        evec[1] = wein[2] / ( cvp->crv_c1 - wein[3] );
-                }
-        }
-
-        VSET( cvp->crv_pdir, 0.0, 0.0, 0.0 );
-	cvp->crv_pdir[0] = evec[0] * ue[0] + evec[1] * ve[0];
-        cvp->crv_pdir[1] = evec[0] * ue[1] + evec[1] * ve[1];
-        cvp->crv_pdir[2] = evec[0] * ue[2] + evec[1] * ve[2];
-	VUNITIZE( cvp->crv_pdir);
-
-cleanup:
-	rt_free( (char *) se, "rt_nurb_curv:se");
-	rt_free( (char *) ue, "rt_nurb_curv:ue");
-	rt_free( (char *) ve, "rt_nurb_curv:ve");
-	rt_free( (char *) uue, "rt_nurb_curv:uue");
-	rt_free( (char *) vve, "rt_nurb_curv:vve");
-	rt_free( (char *) uve, "rt_nurb_curv:uve");
+	rt_nurb_curvature( cvp, srf, u, v );
 }
 
 /*
@@ -838,11 +709,10 @@ struct rt_nurb_uv_hit * h;
 	return (struct nurb_hit *) hit;
 }
 
-#define EQ_HIT(a,b) ( fabs((a) - (b)) < EPSILON)
-
-rt_nurb_add_hit( head, hit )
+rt_nurb_add_hit( head, hit, tol )
 struct nurb_hit * head;
 struct nurb_hit * hit;
+struct rt_tol	*tol;
 {
 	register struct nurb_hit * h_ptr;
 
@@ -855,6 +725,7 @@ struct nurb_hit * hit;
 	}
 #endif
 	
+	/* If this is the only one, nothing to check against */
 	if( head->next == (struct nurb_hit *) 0)
 	{
 		head->next = hit;
@@ -865,19 +736,27 @@ struct nurb_hit * hit;
 	/* Check for duplicates */
 	for( h_ptr = head->next; h_ptr != (struct nurb_hit *)0; h_ptr = h_ptr->next)
 	{
-		if( EQ_HIT(hit->hit_dist,h_ptr->hit_dist) &&
-			EQ_HIT(hit->hit_uv[0], h_ptr->hit_uv[0]) &&
-			EQ_HIT(hit->hit_uv[1], h_ptr->hit_uv[1]) )
-		{
-			rt_free( (char *) hit, "add hit: hit");
-			return;
-		}
+		register fastf_t	f;
+
+		/* This test a distance in model units (mm) */
+		f = hit->hit_dist - h_ptr->hit_dist;
+		if( NEAR_ZERO( f, tol->dist ) )  goto duplicate;
+
+		/* These tests are in parameter space, 0..1 */
+		f = hit->hit_uv[0] - h_ptr->hit_uv[0];
+		if( NEAR_ZERO( f, 0.0001 ) )  goto duplicate;
+		f = hit->hit_uv[1] - h_ptr->hit_uv[1];
+		if( NEAR_ZERO( f, 0.0001 ) )  goto duplicate;
 	}
 
 	hit->prev = head;
 	hit->next = head->next;
 	hit->next->prev = hit;
 	head->next = hit;
+	return;
+duplicate:
+	rt_free( (char *) hit, "add hit: hit");
+	return;
 }
 
 struct nurb_hit *
