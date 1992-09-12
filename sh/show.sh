dd7af551356a927527ff43c2895a7e531f992d7a
Michael John Muuss <mike@brlcad.org>
1992-09-12 06:35:47 +0000

    show.sh Regardless of file type, show it on the framebuffer
    svn:revision:7052
    cvs:account:mike
    cvs:branch:trunk

---
+++ sh/show.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# Regardless of file type, show it on the framebuffer.
+
+if test "$1" = ""
+then
+	echo "Usage: show.sh file.pix [flags]"
+	exit 1
+fi
+
+FILE=$1
+shift
+FLAGS="$*"
+
+if test ! -f $FILE
+then
+	echo "$0: $FILE does not exist"
+	exit 1
+fi
+
+eval `pixinfo.sh $FILE`	# sets BASE, SUFFIX, WIDTH, HEIGHT
+
+set -x
+case $SUFFIX in
+pix)
+	pix-fb -w$WIDTH -n$HEIGHT $FLAGS $FILE;;
+bw)
+	bw-fb -w$WIDTH -n$HEIGHT $FLAGS $FILE;;
+jpg|jpeg)
+	jpeg-fb $FLAGS $FILE;;
+gif)
+	gif-fb $FLAGS $FILE;;
+*)
+	echo "$SUFFIX is not supported by this script"
+	exit 1
+esac
+
