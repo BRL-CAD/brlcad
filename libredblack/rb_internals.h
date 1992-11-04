3962a90d53ffee47cce5f326b8849a0f9a59662e
Paul Tanenbaum <pjt@brlcad.org>
1992-11-04 10:28:34 +0000

    Header file for use within LIBREDBLACK(3)
    svn:revision:7132
    cvs:branch:trunk
    cvs:account:pjt

---
+++ libredblack/rb_internals.h
@@ -0,0 +1,80 @@
+/*			R B _ I N T E R N A L S . H
+ *
+ *	Written by:	Paul Tanenbaum
+ *
+ *  $Header$
+ */
+
+#ifndef REDBLACK_H
+#include "redblack.h"
+#endif
+
+#ifndef RB_INTERNALS_H
+#define RB_INTERNALS_H seen
+
+/*
+ *	Macros without parameters
+ */
+#define		RB_RED		0
+#define		RB_BLACK	1
+#define		RB_TREE_MAGIC	0x72627472
+#define		RB_NODE_MAGIC	0x72626e6f
+
+/*			R B _ C K M A G ( )
+ *	    Check and validate a structure pointer
+ *
+ *	This macro has three parameters: a pointer, the magic number
+ *	expected at that location, and a string describing the expected
+ *	structure type.
+ */
+#define	RB_CKMAG(p, m, _s)						\
+    if ((p) == 0)							\
+    {									\
+	fprintf(stderr, "Error: Null %s pointer, file %s, line %d\n",	\
+	    (_s), __FILE__, __LINE__);					\
+	exit (0);							\
+    }									\
+    else if (*((long *)(p)) != (m))					\
+    {									\
+	fprintf(stderr,							\
+	    "Error Bad %s pointer x%x s/b x%x was x%x, file %s, line %d\n", \
+	    (_s), (p), (m), *((long *)(p)), __FILE__, __LINE__);	\
+	exit (0);							\
+    }									\
+
+/*
+ *	Access functions for fields of rb_tree and (struct rb_node)
+ */
+#define	rb_order_func(t, o)	(((t) -> rbt_order)[o])
+#define	rb_parent(n, o)		(((n) -> rbn_parent)[o])
+#define	rb_left_child(n, o)	(((n) -> rbn_left)[o])
+#define	rb_right_child(n, o)	(((n) -> rbn_right)[o])
+#define	rb_get_color(n, o)						\
+(									\
+    (((n) -> rbn_color)[(o)/8] & (0x1 << ((o) % 8))) ? 1 : 0		\
+)
+
+#define	rb_set_color(n, o, c)						\
+{									\
+    int	_b = (o) / 8;							\
+    int _p = (o) - _b * 8;						\
+									\
+    RB_CKMAG((n), RB_NODE_MAGIC, "red-black node");			\
+    ((n) -> rbn_color)[_b] &= 0x1 << _p;				\
+    ((n) -> rbn_color)[_b] |= (c) << _p;				\
+}
+
+/*
+ *	Global variables within LIBREDBLACK
+ */
+#ifdef RB_CREATE
+    struct rb_node		*current_node = RB_NODE_NULL;
+#else
+    extern struct rb_node	*current_node;
+#endif
+
+/*
+ *	Functions internal to LIBREDBLACK
+ */
+
+#endif /* RB_INTERNALS_H */
