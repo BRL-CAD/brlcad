6af27636d8240ced7c54591b3b38cbcad0ab2cd9
Paul Tanenbaum <pjt@brlcad.org>
1993-01-14 09:27:11 +0000

    1. Now use rb_null() and rb_data() 2. _rb_search() now returns (struct rb_node *)
    svn:revision:7231
    cvs:account:pjt
    cvs:branch:trunk

--- libbu/rb_search.c
+++ libbu/rb_search.c
@@ -18,20 +18,20 @@
  *	This function has four parameters: the root of the tree
  *	in which to search, the order number and the comparison function,
  *	and a data block containing the desired value of the key.  On
- *	success, _rb_search() returns a pointer to the data block
- *	in the discovered node.  Otherwise, it returns NULL.
+ *	success, _rb_search() returns a pointer to the discovered node.
+ *	Otherwise, it returns (tree -> rbt_empty_node).
  */
-static void *_rb_search
+struct rb_node *_rb_search
 	(struct rb_node *root, int order_nm, int (*order)(), void *data)
 {
     int	result;
 
     while (1)
     {
-	if (root == RB_NODE_NULL)
-	    return (NULL);
 	RB_CKMAG(root, RB_NODE_MAGIC, "red-black node");
-	if ((result = (*order)(data, root -> rbn_data)) == 0)
+	if (root == rb_null(root -> rbn_tree))
+	    return (root);
+	if ((result = (*order)(data, rb_data(root, order_nm))) == 0)
 	    break;
 	else if (result < 0)
 	    root = rb_left_child(root, order_nm);
@@ -39,7 +39,7 @@ static void *_rb_search
 	    root = rb_right_child(root, order_nm);
     }
     current_node = root;
-    return (root -> rbn_data);
+    return (root);
 }
 
 /*		        R B _ S E A R C H ( )
@@ -56,9 +56,14 @@ void *rb_search (rb_tree *tree, int order, void *data)
 {
 
     int			(*compare)();
+    struct rb_node	*node;
 
     RB_CKMAG(tree, RB_TREE_MAGIC, "red-black tree");
     RB_CKORDER(tree, order);
     compare = rb_order_func(tree, order);
-    return(_rb_search(rb_root(tree, order), order, compare, data));
+    node = _rb_search(rb_root(tree, order), order, compare, data);
+    if (node == rb_null(tree))
+	return (NULL);
+    else
+	return (rb_data(node, order));
 }
