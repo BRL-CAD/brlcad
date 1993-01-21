de450a5f2f2557ee1d06bc3686541d072a7bd931
Paul Tanenbaum <pjt@brlcad.org>
1993-01-21 14:33:47 +0000

    1. The sort functions now try harder to break ties 2. Now look at all the presidents
    svn:revision:7281
    cvs:account:pjt
    cvs:branch:trunk

--- libredblack/application.c
+++ libredblack/application.c
@@ -28,18 +28,30 @@ typedef struct
  */
 last_name_sort (void *v1, void *v2)
 {
+    int		result;
     record	*r1 = v1;
     record	*r2 = v2;
 
-    return(strcmp(r1 -> last, r2 -> last));
+    if (((result = strcmp(r1 -> last, r2 -> last)) == 0)
+    &&	((result = strcmp(r1 -> first, r2 -> first)) == 0))
+	result = (r1 -> party < r2 -> party) ? -1 :
+		 (r1 -> party > r2 -> party) ?  1 : 0;
+
+    return(result);
 }
 
 first_name_sort (void *v1, void *v2)
 {
+    int		result;
     record	*r1 = v1;
     record	*r2 = v2;
 
-    return(strcmp(r1 -> first, r2 -> first));
+    if (((result = strcmp(r1 -> first, r2 -> first)) == 0)
+    &&	((result = strcmp(r1 -> last, r2 -> last)) == 0))
+	result = (r1 -> party < r2 -> party) ? -1 :
+		 (r1 -> party > r2 -> party) ?  1 : 0;
+
+    return(result);
 }
 
 party_sort (void *v1, void *v2)
@@ -89,14 +101,42 @@ main ()
     record		p;
     static record	pres[] = 
 			{
-#if 1
+			    {RECORD_MAGIC, "Washington", "George", DEMOCRAT},
+			    {RECORD_MAGIC, "Adams", "John", DEMOCRAT},
+			    {RECORD_MAGIC, "Jefferson", "Thomas", DEMOCRAT},
+			    {RECORD_MAGIC, "Madison", "James", DEMOCRAT},
+			    {RECORD_MAGIC, "Monroe", "James", DEMOCRAT},
+			    {RECORD_MAGIC, "Adams", "John Quincy", DEMOCRAT},
+			    {RECORD_MAGIC, "Jackson", "Andrew", DEMOCRAT},
+			    {RECORD_MAGIC, "Van Buren", "Martin", DEMOCRAT},
+			    {RECORD_MAGIC, "Harrison", "William", DEMOCRAT},
+			    {RECORD_MAGIC, "Tyler", "James", DEMOCRAT},
+			    {RECORD_MAGIC, "Polk", "James Knox", DEMOCRAT},
+			    {RECORD_MAGIC, "Taylor", "Zachary", DEMOCRAT},
+			    {RECORD_MAGIC, "Fillmore", "Millard", DEMOCRAT},
+			    {RECORD_MAGIC, "Pierce", "Franklin", DEMOCRAT},
+			    {RECORD_MAGIC, "Buchannan", "James", DEMOCRAT},
+			    {RECORD_MAGIC, "Lincoln", "Abraham", DEMOCRAT},
+			    {RECORD_MAGIC, "Johnson", "Andrew", DEMOCRAT},
+			    {RECORD_MAGIC, "Grant", "Ulysses", DEMOCRAT},
+			    {RECORD_MAGIC, "Hayes", "Rutherford", DEMOCRAT},
+			    {RECORD_MAGIC, "Garfield", "James", DEMOCRAT},
+			    {RECORD_MAGIC, "Arthur", "Chester", DEMOCRAT},
+			    {RECORD_MAGIC, "Cleveland", "Grover", DEMOCRAT},
+			    {RECORD_MAGIC, "Harrison", "Benjamin", DEMOCRAT},
+			    {RECORD_MAGIC, "McKinley", "William", DEMOCRAT},
+			    {RECORD_MAGIC, "Roosevelt", "Theodore", DEMOCRAT},
+			    {RECORD_MAGIC, "Taft", "William", DEMOCRAT},
+			    {RECORD_MAGIC, "Wilson", "Woodrow", DEMOCRAT},
+			    {RECORD_MAGIC, "Harding", "Warren", DEMOCRAT},
+			    {RECORD_MAGIC, "Coolidge", "Calvin", DEMOCRAT},
+			    {RECORD_MAGIC, "Hoover", "Herbert", DEMOCRAT},
 			    {RECORD_MAGIC, "Roosevelt", "Franklin", DEMOCRAT},
 			    {RECORD_MAGIC, "Truman", "Harry", DEMOCRAT},
 			    {RECORD_MAGIC, "Eisenhower", "Dwight", REPUBLICAN},
 			    {RECORD_MAGIC, "Kennedy", "John", DEMOCRAT},
 			    {RECORD_MAGIC, "Johnson", "Lyndon", DEMOCRAT},
 			    {RECORD_MAGIC, "Nixon", "Richard", REPUBLICAN},
-#endif
 			    {RECORD_MAGIC, "Ford", "Gerald", REPUBLICAN},
 			    {RECORD_MAGIC, "Carter", "Jimmy", DEMOCRAT},
 			    {RECORD_MAGIC, "Reagan", "Ronald", REPUBLICAN},
@@ -121,15 +161,16 @@ main ()
 	fputs("rb_create() bombed\n", stderr);
 	exit (1);
     }
-    tree -> rbt_print = print_last_name;
+    tree -> rbt_print = describe_president;
 
     for (i = 0; i < nm_presidents - 1; ++i)
+    {
 	rb_insert(tree, (void *) &(pres[i]));
+    }
     
     rt_log("Before we begin...\n");
-    rb_diagnose_tree(tree, ORDER_LASTNAME, PREORDER);
-    rb_diagnose_tree(tree, ORDER_FIRSTNAME, PREORDER);
-    rb_diagnose_tree(tree, ORDER_PARTY, PREORDER);
+    rb_diagnose_tree(tree, ORDER_FIRSTNAME, INORDER);
+    exit (0);
 
     /*
      *	Delete Reagan and Eisenhower
@@ -139,14 +180,12 @@ main ()
 	rb_delete(tree, ORDER_LASTNAME);
     rt_log("After deleting Reagan...\n");
     rb_diagnose_tree(tree, ORDER_LASTNAME, PREORDER);
-    rb_diagnose_tree(tree, ORDER_FIRSTNAME, PREORDER);
 
     r = (record *) rb_search(tree, ORDER_LASTNAME, (void *) (pres + 2));
     if (r != NULL)
 	rb_delete(tree, ORDER_LASTNAME);
     rt_log("After deleting Eisenhower...\n");
     rb_diagnose_tree(tree, ORDER_LASTNAME, PREORDER);
-    rb_diagnose_tree(tree, ORDER_FIRSTNAME, PREORDER);
     
     /*
      *	Insert Clinton
@@ -154,7 +193,6 @@ main ()
     rb_insert(tree, (void *) &(pres[nm_presidents - 1]));
     rt_log("After inserting Clinton...\n");
     rb_diagnose_tree(tree, ORDER_LASTNAME, PREORDER);
-    rb_diagnose_tree(tree, ORDER_FIRSTNAME, PREORDER);
 
     /*
      *	Delete Roosevelt
@@ -164,5 +202,4 @@ main ()
 	rb_delete(tree, ORDER_LASTNAME);
     rt_log("After deleting Roosevelt...\n");
     rb_diagnose_tree(tree, ORDER_LASTNAME, PREORDER);
-    rb_diagnose_tree(tree, ORDER_FIRSTNAME, PREORDER);
 }
