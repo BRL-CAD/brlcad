0b4ef593758503eb7915541022c5f068a372d8b9
Michael John Muuss <mike@brlcad.org>
1993-07-20 15:20:01 +0000

    Eliminated nested comment warning
    svn:revision:7642
    cvs:account:mike
    cvs:branch:trunk

--- h/nmg.h
+++ h/nmg.h
@@ -19,13 +19,13 @@
  *  Include Sequencing -
  *	#include <stdio.h>
  *	#include <math.h>
- *	#include "machine.h"	/* For fastf_t definition on this machine *_/
- *	#include "vmath.h"	/* For vect_t definition *_/
- *	#include "rtlist.h"	/* OPTIONAL, auto-included by raytrace.h *_/
- *	#include "rtstring.h"	/* OPTIONAL, auto-included by raytrace.h *_/
+ *	#include "machine.h"	/_* For fastf_t definition on this machine *_/
+ *	#include "vmath.h"	/_* For vect_t definition *_/
+ *	#include "rtlist.h"	/_* OPTIONAL, auto-included by raytrace.h *_/
+ *	#include "rtstring.h"	/_* OPTIONAL, auto-included by raytrace.h *_/
  *	#include "nmg.h"
  *	#include "raytrace.h"
- *	#include "nurb.h"	/* OPTIONAL, follows raytrace.h when used *_/
+ *	#include "nurb.h"	/_* OPTIONAL, follows raytrace.h when used *_/
  *
  *  Libraries Used -
  *	LIBRT LIBRT_LIBES -lm -lc
