355c43a18526352611dfc028a6a1689fa2d6131d
Michael John Muuss <mike@brlcad.org>
1993-08-11 20:29:48 +0000

    Changed to rev 2
    svn:revision:7703
    cvs:account:mike
    cvs:branch:trunk

--- jove/mkversion.sh
+++ jove/mkversion.sh
@@ -6,7 +6,7 @@ c=`echo 0 1 $p +p | dc`
 
 rm -f version.c version.number
 
-echo "char	*version = \"1.$c\";" > version.c
+echo "char	*version = \"2.$c\";" > version.c
 echo $c > version.number
 
 cc -c version.c
