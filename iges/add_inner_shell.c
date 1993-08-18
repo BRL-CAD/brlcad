82ad1a4634b73bc0d8b37344952279b2054f45cc
John Anderson <daytonajra@gmail.com>
1993-08-18 09:57:14 +0000

    corrected faceuse orientation
    svn:revision:7722
    cvs:account:jra
    cvs:branch:trunk

--- iges/add_inner_shell.c
+++ iges/add_inner_shell.c
@@ -27,7 +27,7 @@
 #include "raytrace.h"
 #include "wdb.h"
 
-RT_EXTERN( struct faceuse *Add_face_to_shell , ( struct shell *s , int entityno , int loop_orient) );
+RT_EXTERN( struct faceuse *Add_face_to_shell , ( struct shell *s , int entityno , int face_orient) );
 
 Add_inner_shell( r , entityno , shell_orient )
 struct nmgregion *r;
@@ -39,6 +39,7 @@ int shell_orient;
 	int		no_of_faces;		/* Number of faces in shell */
 	int		*face_de;		/* Directory seqence numbers for faces */
 	int		*face_orient;		/* Orientation of faces */
+	int		face_use_orient;	/* Face orientation for shell */
 	int		face;
 	struct shell	*s;			/* NMG shell */
 	struct faceuse	**fu;			/* list of faceuses */
@@ -65,7 +66,13 @@ int shell_orient;
 
 	s = RT_LIST_FIRST( shell , &r->s_hd );
 	for( face=0 ; face<no_of_faces ; face++ )
-		 fu[face] = Add_face_to_shell( s , (face_de[face]-1)/2 , face_orient[face] );
+	{
+		if( face_orient[face] == shell_orient )
+			face_use_orient = 1;
+		else
+			face_use_orient = 0;
+		 fu[face] = Add_face_to_shell( s , (face_de[face]-1)/2 , face_use_orient );
+	}
 
 	rt_free( (char *)face_de , "Add_inner_shell: face DE's" );
 	rt_free( (char *)face_orient , "Add_inner_shell: face orients" );
