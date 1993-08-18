317460fce22e6ba835a08bef126e2b75a123ee78
John Anderson <daytonajra@gmail.com>
1993-08-18 08:50:56 +0000

    Header file for the brlcad-to-IGES converter
    svn:revision:7715
    cvs:account:jra
    cvs:branch:trunk

---
+++ iges/iges.h
@@ -0,0 +1,15 @@
+struct iges_properties
+{
+	char			name[NAMESIZE+1];
+	char			material_name[32];
+	char			material_params[60];
+	char			region_flag;
+	short			ident;
+	short			air_code;
+	short			material_code;
+	short			los_density;
+	short			inherit;
+	short			color_defined;
+	unsigned char		color[3];
+
+};
