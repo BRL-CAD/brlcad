8e37107ccf7600a93335c82bb5cf8f505b33afbb
John Anderson <daytonajra@gmail.com>
1993-08-18 09:40:21 +0000

    corrected use of face orientation
    svn:revision:7719
    cvs:account:jra
    cvs:branch:trunk

--- iges/get_outer_shell.c
+++ iges/get_outer_shell.c
@@ -27,7 +27,7 @@
 #include "raytrace.h"
 #include "wdb.h"
 
-RT_EXTERN( struct faceuse *Add_face_to_shell , ( struct shell *s , int entityno , int loop_orient) );
+RT_EXTERN( struct faceuse *Add_face_to_shell , ( struct shell *s , int entityno , int face_orient) );
 
 int
 Get_outer_shell( r , entityno , shell_orient )
@@ -40,6 +40,7 @@ int shell_orient;
 	int		no_of_faces;		/* Number of faces in shell */
 	int		*face_de;		/* Directory seqence numbers for faces */
 	int		*face_orient;		/* Orientation of faces */
+	int		face_use_orient;	/* Face orientation for shell */
 	int		face;
 	struct shell	*s;			/* NMG shell */
 	struct faceuse	**fu;			/* list of faceuses */
@@ -68,7 +69,13 @@ int shell_orient;
 	s = nmg_msv( r );
 
 	for( face=0 ; face<no_of_faces ; face++ )
-		 fu[face] = Add_face_to_shell( s , (face_de[face]-1)/2 , face_orient[face] );
+	{
+		if( face_orient[face] == shell_orient )
+			face_use_orient = 1;
+		else
+			face_use_orient = 0;
+		fu[face] = Add_face_to_shell( s , (face_de[face]-1)/2 , face_use_orient  );
+	}
 
 	rt_free( (char *)face_de , "Get_outer_shell: face DE's" );
 	rt_free( (char *)face_orient , "Get_outer_shell: face orients" );
