003775002d50d3d27d2f7eccd937fe4d7c349c2b
Michael John Muuss <mike@brlcad.org>
1993-12-09 03:02:38 +0000

    Added ability to specify file names.
    svn:revision:8056
    cvs:account:mike
    cvs:branch:trunk

--- util/pixfieldsep.c
+++ util/pixfieldsep.c
@@ -1,38 +1,110 @@
+/*
+ *			P I X F I E L D S E P . C
+ *
+ *  Separate an interlaced video image into two separate .pix files.
+ *
+ *  Author -
+ *	Michael John Muuss
+ *  
+ *  Source -
+ *	The U. S. Army Research Laboratory
+ *	Aberdeen Proving Ground, Maryland  21005-5068  USA
+ *  
+ *  Distribution Status -
+ *	Public Domain, Distribution Unlimitied.
+ */
+#ifndef lint
+static char RCSid[] = "@(#)$Header$ (ARL)";
+#endif
+
 #include <stdio.h>
+#include "machine.h"
+#include "externs.h"
 
 FILE	*out1;
 FILE	*out2;
 
 char	*buf;
-int	width = 720;
+int	file_width = 720;
+int	bytes_per_sample = 3;
+
+char	*even_file = "even.pix";
+char	*odd_file = "odd.pix";
+
+static char usage[] = "\
+Usage: pixfieldsep [-w file_width] [-s square_size] [-# nbytes/pixel] \
+	[even.pix odd.pix]\n";
+
+get_args( argc, argv )
+int	argc;
+register char **argv;
+{
+	register int c;
+
+	while ( (c = getopt( argc, argv, "s:w:#:" )) != EOF )  {
+		switch( c )  {
+		case '#':
+			bytes_per_sample = atoi(optarg);
+			break;
+		case 's':
+			/* square file size */
+			file_width = atoi(optarg);
+			break;
+		case 'w':
+			file_width = atoi(optarg);
+			break;
+
+		default:		/* '?' */
+			return(0);
+		}
+	}
+
+	if( optind < argc )  {
+		even_file = argv[optind++];
+	}
+	if( optind < argc )  {
+		odd_file = argv[optind++];
+	}
+
+	if( ++optind <= argc )
+		(void)fprintf( stderr, "pixfieldsep: excess argument(s) ignored\n" );
+
+	return(1);		/* OK */
+}
 
 main(argc, argv)
 int	argc;
 char	*argv[];
 {
-	if( (out1 = fopen("even.pix", "w")) == NULL )  {
-		perror("even.pix");
+
+	if ( !get_args( argc, argv ) )  {
+		(void)fputs(usage, stderr);
+		exit( 1 );
+	}
+
+	if( (out1 = fopen(even_file, "w")) == NULL )  {
+		perror(even_file);
 		exit(1);
 	}
-	if( (out2 = fopen("odd.pix", "w")) == NULL )  {
-		perror("odd.pix");
+	if( (out2 = fopen(odd_file, "w")) == NULL )  {
+		perror(odd_file);
 		exit(2);
 	}
 
-	buf = (char *)malloc( (width+1)*3 );
+	buf = (char *)malloc( (file_width+1)*bytes_per_sample );
 
 	for(;;)  {
 		/* Even line */
-		if( fread( buf, 3, width, stdin ) != width )
+		if( fread( buf, bytes_per_sample, file_width, stdin ) != file_width )
 			break;
-		if( fwrite( buf, 3, width, out1 ) != width )  {
+		if( fwrite( buf, bytes_per_sample, file_width, out1 ) != file_width )  {
 			perror("fwrite even");
 			exit(1);
 		}
 		/* Odd line */
-		if( fread( buf, 3, width, stdin ) != width )
+		if( fread( buf, bytes_per_sample, file_width, stdin ) != file_width )
 			break;
-		if( fwrite( buf, 3, width, out2 ) != width )  {
+		if( fwrite( buf, bytes_per_sample, file_width, out2 ) != file_width )  {
 			perror("fwrite odd");
 			exit(1);
 		}
