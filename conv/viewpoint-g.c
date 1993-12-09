47727a6ac7389e1b129173e457bd67594c808a0c
John Anderson <daytonajra@gmail.com>
1993-12-09 12:47:30 +0000

    modification to capture vertex normals if they are in the input file
    svn:revision:8063
    cvs:account:jra
    cvs:branch:trunk

--- conv/viewpoint-g.c
+++ conv/viewpoint-g.c
@@ -1,3 +1,29 @@
+/*
+ *	V I E W P O I N T - G
+ *
+ *  Converter from Viewpoint Datalabs coor/elem format
+ *  to BRLCAD format.  Will assign vertex normals if they
+ *  are present in the input files.  Two files are expected
+ *  one containing vertex coordinates (and optional normals)
+ *  and the second which lists the vertex numbers for each polygonal face.
+ *
+ *  Author:
+ *	John R. Anderson
+ *
+ *  Source -
+ *	The U. S. Army Research Laboratory
+ *	Aberdeen Proving Ground, Maryland  21005-5068  USA
+ *  
+ *  Distribution Notice -
+ *	Re-distribution of this software is restricted, as described in
+ *	your "Statement of Terms and Conditions for the Release of
+ *	The BRL-CAD Pacakge" agreement.
+ *
+ *  Copyright Notice -
+ *	This software is Copyright (C) 1993 by the United States Army
+ *	in all countries except the USA.  All rights reserved.
+ */
+
 #include <stdio.h>
 #include <math.h>
 #include <string.h>
@@ -20,39 +46,42 @@ extern int errno;
 struct viewpoint_verts
 {
 	point_t coord;
+	vect_t norm;
 	struct vertex *vp;
+	short has_norm;
 };
 
 #define	LINELEN	256 /* max input line length from elements file */
 
-static char *tok_sep=" ";
-static char *coords_suffix=".coor.txt";
-static char *elems_suffix=".elem.txt";
-static char *usage="viewpoint-g input_file_base output_file_name";
+static char *tok_sep=" ";		/* seperator used in input files */
+static char *usage="viewpoint-g [-t tol] -c coord_file_name -e elements_file_name -o output_file_name";
 
 main( int argc , char *argv[] )
 {
 	register int c;
-	FILE *coords,*elems;
-	FILE *out_fp;
-	char *base_name,*coords_name,*elems_name;
+	FILE *coords,*elems;		/* input file pointers */
+	FILE *out_fp;			/* output file pointers */
+	char *base_name;		/* title and top level group name */
+	char *coords_name;		/* input coordinates file name */
+	char *elems_name;		/* input elements file name */
+	float x,y,z,nx,ny,nz;		/* vertex and normal coords */
+	char *ptr1,*ptr2;
+	int name_len;
 	struct rt_tol tol;
-	float x,y,z;
 	int done=0;
 	int i;
 	int no_of_verts;
 	int no_of_faces=0;
 	char line[LINELEN];
-	struct nmg_ptbl vertices;
-	struct nmg_ptbl faces;
-	struct nmg_ptbl names;
+	struct nmg_ptbl vertices;	/* table of vertices for one face */
+	struct nmg_ptbl faces;		/* table of faces for one element */
+	struct nmg_ptbl names;		/* table of element names */
 	struct model *m;
 	struct nmgregion *r;
 	struct shell *s;
 	struct faceuse *fu;
-	struct viewpoint_verts *verts;
+	struct viewpoint_verts *verts;	/* array of structures holding coordinates and normals */
 	struct wmember reg_head,*wmem;
-	float tmp;
 
         /* XXX These need to be improved */
         tol.magic = RT_TOL_MAGIC;
@@ -69,7 +98,7 @@ main( int argc , char *argv[] )
 		rt_bomb( usage );
 
 	/* get command line arguments */
-	while ((c = getopt(argc, argv, "t:i:o:")) != EOF)
+	while ((c = getopt(argc, argv, "t:c:e:o:")) != EOF)
 	{
 		switch( c )
 		{
@@ -77,21 +106,19 @@ main( int argc , char *argv[] )
 				tol.dist = atof( optarg );
 				tol.dist_sq = tol.dist * tol.dist;
 				break;
-			case 'i': /* input file name */
-				base_name = (char *)rt_malloc( strlen( optarg ) + 1 , "Viewpoint-g: base name" );
-				strcpy( base_name , optarg );
-				coords_name = (char *)rt_malloc( strlen( optarg ) + strlen( coords_suffix ) + 1 , "viewpoint-g: coord file name" );
+			case 'c': /* input coordinates file name */
+				coords_name = (char *)rt_malloc( strlen( optarg ) + 1 , "Viewpoint-g: base name" );
 				strcpy( coords_name , optarg );
-				strcat( coords_name , coords_suffix );
 				if( (coords = fopen( coords_name , "r" )) == NULL )
 				{
 					rt_log( "Cannot open %s\n" , coords_name );
 					perror( "viewpoint-g" );
 					rt_bomb( "Cannot open input file" );
 				}
-				elems_name = (char *)rt_malloc( strlen( optarg ) + strlen( elems_suffix ) + 1 , "viewpoint-g: coord file name" );
+				break;
+			case 'e': /* input elements file name */
+				elems_name = (char *)rt_malloc( strlen( optarg ) + 1 , "Viewpoint-g: base name" );
 				strcpy( elems_name , optarg );
-				strcat( elems_name , elems_suffix );
 				if( (elems = fopen( elems_name , "r" )) == NULL )
 				{
 					rt_log( "Cannot open %s\n" , elems_name );
@@ -113,27 +140,61 @@ main( int argc , char *argv[] )
 		}
 	}
 
+	/* Must have some input */
 	if( coords == NULL || elems == NULL )
 		rt_bomb( usage );
 
+	/* build a title for the BRLCAD database */
+	ptr1 = strrchr( coords_name , '/' );
+	if( ptr1 == NULL )
+		ptr1 = coords_name;
+	else
+		ptr1++;
+	ptr2 = strchr( ptr1 , '.' );
+
+	if( ptr2 == NULL )
+		name_len = strlen( ptr1 );
+	else
+		name_len = ptr2 - ptr1;
+
+	base_name = (char *)rt_malloc( name_len + 1 , "base_name" );
+	strncpy( base_name , ptr1 , name_len );
+
+	/* make title record */	
 	mk_id( out_fp , base_name );
 
 	/* count vertices */
 	no_of_verts = 1;
-	while( fscanf( coords , "%d,%f,%f,%f" , &i , &tmp , &tmp , &tmp ) != EOF )
+	while( fgets( line , LINELEN , coords ) != NULL )
 		no_of_verts++;
 
+	/* allocate memory to store vertex coordinates and normal coordinates and a pointer to
+	 * an NMG vertex structure */
 	verts = (struct viewpoint_verts *)rt_calloc( no_of_verts , sizeof( struct viewpoint_verts ) , "viewpoint-g: vertex list" );
+
+	/* Now read the vertices again and store them */
 	rewind( coords );
-	while( fscanf( coords , "%d,%f,%f,%f" , &i , &x , &z , &y ) != EOF )
+	while( fgets( line , LINELEN ,  coords ) != NULL )
 	{
+		int number_scanned;
+		number_scanned = sscanf( line , "%d,%f,%f,%f,%f,%f,%f" , &i , &x , &z , &y , &nx , &ny , &nz );
+		if( number_scanned < 4 )
+			break;
 		if( i >= no_of_verts )
 			rt_log( "vertex number too high (%d) only allowed for %d\n" , i , no_of_verts );
-		VSET( verts[i].coord , (-x) , y , z );
+		VSET( verts[i].coord , x , y , z );
+
+		if( number_scanned == 7 ) /* we get normals too!!! */
+		{
+			VSET( verts[i].norm , nx , ny , nz );
+			verts[i].has_norm = 1;
+		}
 	}
 
+	/* Let the user know that something is happening */
 	fprintf( stderr , "%d vertices\n" , no_of_verts-1 );
 
+	/* initialize tables */
 	nmg_tbl( &vertices , TBL_INIT , NULL );
 	nmg_tbl( &faces , TBL_INIT , NULL );
 	nmg_tbl( &names , TBL_INIT , NULL );
@@ -155,6 +216,8 @@ main( int argc , char *argv[] )
 				/* this is the first element processed */
 				curr_name = rt_malloc( sizeof( name ) + 1 , "viewpoint-g: component name" );
 				strcpy( curr_name , name );
+
+				/* add this name to the table */
 				nmg_tbl( &names , TBL_INS , (long *)curr_name );
 				done = 0;
 				break;
@@ -178,6 +241,8 @@ main( int argc , char *argv[] )
 					/* didn't find name, so this becomes the current name */
 					curr_name = rt_malloc( sizeof( name ) + 1 , "viewpoint-g: component name" );
 					strcpy( curr_name , name );
+
+					/* add it to the table */
 					nmg_tbl( &names , TBL_INS , (long *)curr_name );
 					done = 0;
 					break;
@@ -189,6 +254,7 @@ main( int argc , char *argv[] )
 		if( curr_name == NULL )
 			break;
 
+		/* Hopefully, the user is still around */
 		fprintf( stderr , "\tMaking %s\n" , curr_name );
 
 		/* make basic nmg structures */
@@ -203,7 +269,7 @@ main( int argc , char *argv[] )
 		/* read elements file and make faces */
 		while( !eof )
 		{
-			/* loop through vertx numbers */
+			/* loop through vertex numbers */
 			while( (ptr = strtok( (char *)NULL , tok_sep ) ) != NULL )
 			{
 				i = atoi( ptr );
@@ -214,15 +280,18 @@ main( int argc , char *argv[] )
 				nmg_tbl( &vertices , TBL_INS , (long *)(&verts[i].vp) );
 			}
 
-			/* make face */
-			fu = nmg_cmface( s , (struct vertex ***)NMG_TBL_BASEADDR( &vertices ) , NMG_TBL_END( &vertices ) );
-			no_of_faces++;
+			if( NMG_TBL_END( &vertices ) )
+			{
+				/* make face */
+				fu = nmg_cmface( s , (struct vertex ***)NMG_TBL_BASEADDR( &vertices ) , NMG_TBL_END( &vertices ) );
+				no_of_faces++;
 
-			/* put faceuse in list for the current named object */
-			nmg_tbl( &faces , TBL_INS , (long *)fu );
+				/* put faceuse in list for the current named object */
+				nmg_tbl( &faces , TBL_INS , (long *)fu );
 
-			/* restart the vertex list for the next face */
-			nmg_tbl( &vertices , TBL_RST , NULL );
+				/* restart the vertex list for the next face */
+				nmg_tbl( &vertices , TBL_RST , NULL );
+			}
 
 			/* skip elements with the wrong name */
 			name = NULL;
@@ -246,7 +315,23 @@ main( int argc , char *argv[] )
 		for( i=0 ; i<no_of_verts ; i++ )
 		{
 			if( verts[i].vp )
+			{
+				NMG_CK_VERTEX( verts[i].vp );
 				nmg_vertex_gv( verts[i].vp , verts[i].coord );
+
+				/* check if a vertex normal exists */
+				if( verts[i].has_norm )
+				{
+					struct vertexuse *vu;
+
+					/* assign this normal to all uses of this vertex */
+					for( RT_LIST_FOR( vu , vertexuse , &verts[i].vp->vu_hd ) )
+					{
+						NMG_CK_VERTEXUSE( vu );
+						nmg_vertexuse_nv( vu , verts[i].norm );
+					}
+				}
+			}
 		}
 
 		/* calculate plane equations for faces */
@@ -283,11 +368,13 @@ main( int argc , char *argv[] )
 	/* make a top level group with all the objects as members */
 	RT_LIST_INIT( &reg_head.l );
 	for( i=0 ; i<NMG_TBL_END( &names ) ; i++ )
+	{
 		if( (wmem = mk_addmember( (char *)NMG_TBL_GET( &names , i ) , &reg_head , WMOP_UNION ) ) == WMEMBER_NULL )
 		{
 			rt_log( "Cannot make top level group\n" );
 			exit( 1 );
 		}
+	}
 
 	fprintf( stderr , "Making top level group (%s)\n" , base_name );
 	if( mk_lcomb( out_fp , base_name , &reg_head , 0, (char *)0, (char *)0, (char *)0, 0 ) )
