981650ff23ad515175dcac3f5656215e80438369
Carl Nuzman <carl.nuzman@nokia-bell-labs.com>
1994-06-24 18:30:00 +0000

    changed azimuth,elevation,twist to yaw,pitch,roll
    svn:revision:8714
    cvs:account:cnuzman
    cvs:branch:trunk

--- libbn/anim.c
+++ libbn/anim.c
@@ -157,26 +157,26 @@ fastf_t *angle;
 	return(return_value);
 }
 
-/* MAT2AET - Convert the rotational part of a 4x4 transformation matrix
- * to az-el-tw form, that is to say, +twist degrees about the x-axis, 
- * -elevation degrees about the y-axis, and +azimuth degrees about the
- * z-axis. The angles are stored in radians as a vector in the order a,e,t.
+/* MAT2YPR - Convert the rotational part of a 4x4 transformation matrix
+ * to yaw-pitch-roll form, that is to say, +roll degrees about the x-axis, 
+ * -pitch degrees about the y-axis, and +yaw degrees about the
+ * z-axis. The angles are stored in radians as a vector in the order y,p,r.
  * A return of ERROR1 means that arbitrary assumptions were necessary.
  * ERROR2 means that the conversion failed.
  */
-int mat2aet(angle,viewrot)
+int mat2ypr(angle,viewrot)
 fastf_t *viewrot;
 fastf_t angle[3];
 {
         int i, return_value;
-        fastf_t sina, sint, cosa, cost;
+        fastf_t sin_y, sin_r, cos_y, cos_r;
         static fastf_t prev_angle[3];
 
         if ((viewrot[9]==0.0) && (viewrot[10]==0.0)){
                 return_value = ERROR1;
                 angle[2] = 0.0;
                 angle[0] = atan2(-viewrot[1],viewrot[5]);
-                fprintf(stderr,"Warning: twist arbitrarily set to 0.0; azimuth set to %f radians.\n",angle[0]);
+                fprintf(stderr,"Warning: roll arbitrarily set to 0.0; yaw set to %f radians.\n",angle[0]);
         }
         else {
                 return_value = NORMAL;
@@ -184,21 +184,21 @@ fastf_t angle[3];
                 angle[2] = atan2(viewrot[9],viewrot[10]);
         }
 
-        sina = sin(angle[0]);
-        sint = sin(angle[2]);
-        cosa = cos(angle[0]);
-        cost = cos(angle[2]);
+        sin_y = sin(angle[0]);
+        sin_r = sin(angle[2]);
+        cos_y = cos(angle[0]);
+        cos_r = cos(angle[2]);
 
-        if ( (cosa*sint) != 0.0 )
-                angle[1] = atan2( -(viewrot[1]+sina*cost)/(cosa*sint),viewrot[9]/sint);
-        else if ( (sina*cost) != 0.0)
-                angle[1] = atan2( -(viewrot[6]+cosa*sint)/(sina*cost),viewrot[10]/cost);
-        else if ( (sina*sint) != 0.0)
-                angle[1] = atan2( -(viewrot[5]-cosa*cost)/(sina*sint),viewrot[4]/sina);
-        else if ( (cosa*cost) != 0.0)
-                angle[1] = atan2( -(viewrot[2]-sina*sint)/(cosa*cost),viewrot[0]/cosa);
+        if ( (cos_y*sin_r) != 0.0 )
+                angle[1] = atan2( -(viewrot[1]+sin_y*cos_r)/(cos_y*sin_r),viewrot[9]/sin_r);
+        else if ( (sin_y*cos_r) != 0.0)
+                angle[1] = atan2( -(viewrot[6]+cos_y*sin_r)/(sin_y*cos_r),viewrot[10]/cos_r);
+        else if ( (sin_y*sin_r) != 0.0)
+                angle[1] = atan2( -(viewrot[5]-cos_y*cos_r)/(sin_y*sin_r),viewrot[4]/sin_y);
+        else if ( (cos_y*cos_r) != 0.0)
+                angle[1] = atan2( -(viewrot[2]-sin_y*sin_r)/(cos_y*cos_r),viewrot[0]/cos_y);
         else {
-                /* unable to calculate elevation*/
+                /* unable to calculate pitch*/
                 return(ERROR2);
         }
 
@@ -277,34 +277,34 @@ fastf_t *quat, *viewrot;
 }
 
 
-/* AET2MAT - Create a premultiplication rotation matrix to turn the front
- * of an object (its x-axis) to the given azimuth, elevation, and twist, 
+/* YPR2MAT - Create a premultiplication rotation matrix to turn the front
+ * of an object (its x-axis) to the given yaw, pitch, and roll, 
  * which is stored in radians in the vector a.
  */
-void aet2mat(m,a)
+void ypr2mat(m,a)
 fastf_t m[11];
 fastf_t a[3];
 {
-	fastf_t cosa,cose,cost,sina,sine,sint;
+	fastf_t cos_y,cos_p,cos_r,sin_y,sin_p,sin_r;
 
-	cosa = cos(a[0]);	
-	cose = cos(a[1]);	
-	cost = cos(a[2]);	
-	sina = sin(a[0]);	
-	sine = sin(a[1]);	
-	sint = sin(a[2]);	
+	cos_y = cos(a[0]);	
+	cos_p = cos(a[1]);	
+	cos_r = cos(a[2]);	
+	sin_y = sin(a[0]);	
+	sin_p = sin(a[1]);	
+	sin_r = sin(a[2]);	
 
-        m[0] =	 cosa*cose;
-	m[1] =	 -cosa*sine*sint-sina*cost;
-        m[2] =	 -cosa*sine*cost+sina*sint;
+        m[0] =	 cos_y*cos_p;
+	m[1] =	 -cos_y*sin_p*sin_r-sin_y*cos_r;
+        m[2] =	 -cos_y*sin_p*cos_r+sin_y*sin_r;
 	m[3] =	0;
-        m[4] = 	sina*cose;
-        m[5] =	-sina*sine*sint+cosa*cost;
-        m[6] =	-sina*sine*cost-cosa*sint;
+        m[4] = 	sin_y*cos_p;
+        m[5] =	-sin_y*sin_p*sin_r+cos_y*cos_r;
+        m[6] =	-sin_y*sin_p*cos_r-cos_y*sin_r;
 	m[7] =	0;
-        m[8]= 	sine;
-        m[9] = 	cose*sint;
-        m[10] = cose*cost;
+        m[8]= 	sin_p;
+        m[9] = 	cos_p*sin_r;
+        m[10] = cos_p*cos_r;
 	m[11] =	0.0;
 	m[12] =	0.0;
 	m[13] =	0.0;
@@ -312,40 +312,40 @@ fastf_t a[3];
 	m[15] =	1.0;
 }
 
-/* AET2VMAT - Create a post-multiplication rotation matrix ,which could
- * be used to move the virtual camera to the given azimuth, elevation, 
- * and twist,  which are stored in radians in the given vector a. The 
+/* YPR2VMAT - Create a post-multiplication rotation matrix ,which could
+ * be used to move the virtual camera to the given yaw, pitch, 
+ * and roll,  which are stored in radians in the given vector a. The 
  * following are equivalent sets of commands:
- * 	aet2vmat(matrix,a);
+ * 	ypr2vmat(matrix,a);
  *		or
- *	aet2mat(matrix,a);
+ *	ypr2mat(matrix,a);
  * 	v_permute(matrix);
  *	transpose(matrix;
  */
-void aet2vmat(m,a)
+void ypr2vmat(m,a)
 fastf_t m[11];
 fastf_t a[3];
 {
-	fastf_t cosa,cose,cost,sina,sine,sint;
+	fastf_t cos_y,cos_p,cos_r,sin_y,sin_p,sin_r;
 
-	cosa = cos(a[0]);	
-	cose = cos(a[1]);	
-	cost = cos(a[2]);	
-	sina = sin(a[0]);	
-	sine = sin(a[1]);	
-	sint = sin(a[2]);	
+	cos_y = cos(a[0]);	
+	cos_p = cos(a[1]);	
+	cos_r = cos(a[2]);	
+	sin_y = sin(a[0]);	
+	sin_p = sin(a[1]);	
+	sin_r = sin(a[2]);	
 
-	m[0] =    -cosa*sine*sint-sina*cost;
-	m[1] =    -sina*sine*sint+cosa*cost;
-	m[2] =     cose*sint;
+	m[0] =    -cos_y*sin_p*sin_r-sin_y*cos_r;
+	m[1] =    -sin_y*sin_p*sin_r+cos_y*cos_r;
+	m[2] =     cos_p*sin_r;
 	m[3] =     0;
-	m[4] =    -cosa*sine*cost+sina*sint;
-	m[5] =    -sina*sine*cost-cosa*sint;
-	m[6] =     cose*cost;
+	m[4] =    -cos_y*sin_p*cos_r+sin_y*sin_r;
+	m[5] =    -sin_y*sin_p*cos_r-cos_y*sin_r;
+	m[6] =     cos_p*cos_r;
 	m[7] =     0;
-	m[8] =     cosa*cose;
-	m[9] =     sina*cose;
-	m[10] =    sine;
+	m[8] =     cos_y*cos_p;
+	m[9] =     sin_y*cos_p;
+	m[10] =    sin_p;
 	m[11] =	   0.0;
 	m[12] =	   0.0;
 	m[13] =	   0.0;
@@ -353,28 +353,28 @@ fastf_t a[3];
 	m[15] =	   1.0;
 }
 
-/* A_E_T2MAT - Make matrix to rotate an object to the given azimuth,
- * elevation, and twist. (Specified in radians.)
+/* Y_P_R2MAT - Make matrix to rotate an object to the given yaw,
+ * pitch, and roll. (Specified in radians.)
  */
-void a_e_t2mat(m,a,e,t) /*make object rotation matrix from radian aet*/
+void y_p_r2mat(m,a,e,t) /*make object rotation matrix from radian ypr*/
 double m[], a, e, t;
 {
-        double cosa = cos(a);
-        double sina = sin(a);
-        double cose = cos(e);
-        double sine = sin(e);
-        double cost = cos(t);
-        double sint = sin(t);
+        double cos_y = cos(a);
+        double sin_y = sin(a);
+        double cos_p = cos(e);
+        double sin_p = sin(e);
+        double cos_r = cos(t);
+        double sin_r = sin(t);
 
-        m[0] = cosa*cose;
-        m[1] = -cosa*sine*sint-sina*cost;
-        m[2] = -cosa*sine*cost+sina*sint;
-        m[4] = sina*cose;
-        m[5] = -sina*sine*sint+cosa*cost;
-        m[6] = -sina*sine*cost-cosa*sint;
-        m[8]= sine;
-        m[9] = cose*sint;
-        m[10] = cose*cost;
+        m[0] = cos_y*cos_p;
+        m[1] = -cos_y*sin_p*sin_r-sin_y*cos_r;
+        m[2] = -cos_y*sin_p*cos_r+sin_y*sin_r;
+        m[4] = sin_y*cos_p;
+        m[5] = -sin_y*sin_p*sin_r+cos_y*cos_r;
+        m[6] = -sin_y*sin_p*cos_r-cos_y*sin_r;
+        m[8]= sin_p;
+        m[9] = cos_p*sin_r;
+        m[10] = cos_p*cos_r;
         m[3]=m[7]=m[11]=m[12]=m[13]=m[14]=0;
         m[15]=1;
 }
@@ -382,42 +382,42 @@ double m[], a, e, t;
 
 
 
-/* DA_E_T2MAT - Make matrix to rotate an object to the given azimuth,
- * elevation, and twist. (Specified in degrees.)
+/* DY_P_R2MAT - Make matrix to rotate an object to the given yaw,
+ * pitch, and roll. (Specified in degrees.)
  */
-void da_e_t2mat(m,a,e,t) /*make object rotation matrix from aet*/
+void dy_p_r2mat(m,a,e,t) /*make object rotation matrix from ypr*/
 double m[], a, e, t;
 {
-        double radian_azimuth = a*(M_PI*0.0055555555556);
-        double radian_elevation = e*(M_PI*0.0055555555556);
-        double radian_twist = t*(M_PI*0.0055555555556);
+        double radian_yaw = a*(M_PI*0.0055555555556);
+        double radian_pitch = e*(M_PI*0.0055555555556);
+        double radian_roll = t*(M_PI*0.0055555555556);
 
-        double cosa = cos(radian_azimuth);
-        double sina = sin(radian_azimuth);
-        double cose = cos(radian_elevation);
-        double sine = sin(radian_elevation);
-        double cost = cos(radian_twist);
-        double sint = sin(radian_twist);
+        double cos_y = cos(radian_yaw);
+        double sin_y = sin(radian_yaw);
+        double cos_p = cos(radian_pitch);
+        double sin_p = sin(radian_pitch);
+        double cos_r = cos(radian_roll);
+        double sin_r = sin(radian_roll);
 
-        m[0] = cosa*cose;
-        m[1] = -cosa*sine*sint-sina*cost;
-        m[2] = -cosa*sine*cost+sina*sint;
-        m[4] = sina*cose;
-        m[5] = -sina*sine*sint+cosa*cost;
-        m[6] = -sina*sine*cost-cosa*sint;
-        m[8]= sine;
-        m[9] = cose*sint;
-        m[10] = cose*cost;
+        m[0] = cos_y*cos_p;
+        m[1] = -cos_y*sin_p*sin_r-sin_y*cos_r;
+        m[2] = -cos_y*sin_p*cos_r+sin_y*sin_r;
+        m[4] = sin_y*cos_p;
+        m[5] = -sin_y*sin_p*sin_r+cos_y*cos_r;
+        m[6] = -sin_y*sin_p*cos_r-cos_y*sin_r;
+        m[8]= sin_p;
+        m[9] = cos_p*sin_r;
+        m[10] = cos_p*cos_r;
         m[3]=m[7]=m[11]=m[12]=m[13]=m[14]=0;
         m[15]=1;
 }
 
 
-/* DA_E_T2VMAT - Make a view rotation matrix, given desired azimuth, elevation
- * and twist. (Note that the matrix is a permutation of the object rotation
+/* DY_P_R2VMAT - Make a view rotation matrix, given desired yaw, pitch
+ * and roll. (Note that the matrix is a permutation of the object rotation
  * matrix).
  */
-void da_e_t2vmat(m,az,el,tw) /*make view rotation matrix from aet*/
+void dy_p_r2vmat(m,az,el,tw) /*make view rotation matrix from ypr*/
 float m[], az, el, tw;
 {
 
@@ -425,22 +425,22 @@ float m[], az, el, tw;
 	float rel = el*(M_PI*0.0055555555556);
 	float rtw = tw*(M_PI*0.0055555555556);
 	
-	float cosa = cos(raz);
-	float sina = sin(raz);
-	float cose = cos(rel);
-	float sine = sin(rel);
-	float cost = cos(rtw);
-	float sint = sin(rtw);
+	float cos_y = cos(raz);
+	float sin_y = sin(raz);
+	float cos_p = cos(rel);
+	float sin_p = sin(rel);
+	float cos_r = cos(rtw);
+	float sin_r = sin(rtw);
 	
-	m[0] = -cosa*sine*sint-sina*cost;
-	m[1] = -sina*sine*sint+cosa*cost;
-	m[2] = cose*sint;
-	m[4] = -cosa*sine*cost+sina*sint;
-	m[5] = -sina*sine*cost-cosa*sint;
-	m[6] = cose*cost;
-	m[8] = cosa*cose;
-	m[9] = sina*cose;
-	m[10]= sine;
+	m[0] = -cos_y*sin_p*sin_r-sin_y*cos_r;
+	m[1] = -sin_y*sin_p*sin_r+cos_y*cos_r;
+	m[2] = cos_p*sin_r;
+	m[4] = -cos_y*sin_p*cos_r+sin_y*sin_r;
+	m[5] = -sin_y*sin_p*cos_r-cos_y*sin_r;
+	m[6] = cos_p*cos_r;
+	m[8] = cos_y*cos_p;
+	m[9] = sin_y*cos_p;
+	m[10]= sin_p;
 	m[3]=m[7]=m[11]=0;
 	m[12]=m[13]=m[14]=0;
 	m[15]=1;
@@ -651,7 +651,7 @@ double m[], d[], d2[];
         sign = 1.0;
         hypotenuse = sqrt(d[0]*d[0]+d[1]*d[1]);
         if (hypotenuse < VDIVIDE_TOL){ /* vertical direction - use d2 to
-                                        * determine twist */
+                                        * determine roll */
                 hypotenuse = sqrt(d2[0]*d2[0]+d2[1]*d2[1]);
                 if (hypotenuse < VDIVIDE_TOL){ /* use x-axis as default*/
                         VSET(d2,1,0,0);
@@ -666,7 +666,7 @@ double m[], d[], d2[];
                 m[8] = sign;
                 m[0]=m[4]=m[9]=m[10]=0.0;
         }
-        else { /* normal - no twist*/
+        else { /* normal - no roll*/
                 m[0] = d[0];
                 m[1] = -d[1]/hypotenuse;
                 m[2] = -d[0]*d[2]/hypotenuse;
@@ -699,10 +699,10 @@ void rotatez(a,d)
 double a, d[];
 {
         double temp[3];
-        double cosa = cos(a);
-        double sina = sin(a);
-        temp[0] = d[0]*cosa - d[1]*sina;
-        temp[1] = d[0]*sina + d[1]*cosa;
+        double cos_y = cos(a);
+        double sin_y = sin(a);
+        temp[0] = d[0]*cos_y - d[1]*sin_y;
+        temp[1] = d[0]*sin_y + d[1]*cos_y;
         d[0]=temp[0];
         d[1]=temp[1];
 }
