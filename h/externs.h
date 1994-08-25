ec5a66e2822c9da30c3586b529bf6b7a21f4c3f5
Glenn Durfee <gdurfee@gmail.com>
1994-08-25 15:03:03 +0000

    Cleaned up some machine-specific #ifdefs
    svn:revision:9198
    cvs:account:gdurf
    cvs:branch:trunk

--- h/externs.h
+++ h/externs.h
@@ -20,40 +20,65 @@
  *
  *  $Header$
  */
-#ifndef EXTERNS_H_SEENYET
-#define EXTERNS_H_SEENYET
+#ifndef EXTERNS_H
+#define EXTERNS_H
 
-/*
- *  First, define all the routines found in BRL-CAD's libsysv
- */
-#if USE_PROTOTYPES
-extern int	bsdselect( long readfds, int sec, int us );
-#if !defined(__stardent) && !defined(__bsdi__)
-extern int	getopt( int argc, char **argv, char *optstr );
-#endif
-extern char	*re_comp( char *s );
-extern int	re_exec( char *s );
+/* We need sys/select.h under Irix 5 to get fd_set. */
 
-#else
-extern int	bsdselect();
-extern int	getopt();
-extern char	*re_comp();
-extern int	re_exec();
+#ifdef HAVE_SYS_SELECT_H
+#include <sys/select.h>
 #endif
 
-extern char	*optarg;
-extern int	optind;
-extern int	opterr;
+/* Some additional fd_set manipulation routines */
 
-#if (__STDC__ && !apollo) || (sgi && mips)
-	/* SGI has this, even though it isn't ANSI */
+#ifndef FD_OR
+#define FD_OR(x, a, b) { register int _i; for (_i = 0; _i < FD_SETSIZE; _i++) \
+	if (FD_ISSET(_i, a) || FD_ISSET(_i, b)) FD_SET(_i, x); \
+	else FD_CLR(_i, x); }
+#endif
+
+#ifndef FD_AND
+#define FD_AND(x, a, b) { register int _i; for (_i = 0; _i < FD_SETSIZE; _i++)\
+	if (FD_ISSET(_i, a) && FD_ISSET(_i, b)) FD_SET(_i, x); \
+	else FD_CLR(_i, x); }
+#endif
+
+#ifndef FD_MOVE
+#define FD_MOVE(a, b) { register int _i; for (_i = 0; _i < FD_SETSIZE; _i++) \
+	if (FD_ISSET(_i, b)) FD_SET(_i, a); else FD_CLR(_i, a); }
+#endif
+
+#ifndef FD_OREQ
+#define FD_OREQ(a, b) { register int _i; for (_i = 0; _i < FD_SETSIZE; _i++) \
+	if (FD_ISSET(_i, b)) FD_SET(_i, a); }
+#endif
+
+#ifndef FD_ANDEQ
+#define FD_ANDEQ(a, b) { register int _i; for (_i = 0; _i < FD_SETSIZE; _i++) \
+	if (!FD_ISSET(_i, a) || !FD_ISSET(_i, b)) FD_CLR(_i, a) }
+#endif
+
+
+/* Here, we want to include unistd.h if we have it to get the definitions
+   of things such as off_t.  If we don't have it, make some good guesses. */
+
+#ifdef HAVE_UNISTD_H
+#	include <unistd.h>		/* For many important definitions */
+#endif
+
+#ifdef HAVE_STDLIB_H
 #	include <stdlib.h>
-
 #	if defined(__stardent)
 		extern FILE	*popen( const char *, const char * );
 		extern FILE	*fdopen( int, const char * );
 #	endif
 #else
+
+#if !defined(OFF_T) && !defined(HAVE_OFF_T)
+#	define	off_t	long
+#endif
+
+#ifndef HAVE_UNISTD_H    /* We will have already included many of these in unistd.h */
 
 /*
  *	System calls
@@ -64,30 +89,21 @@ extern int	dup();
 extern void	exit();
 #endif
 
-#if !defined(__convexc__)
 extern int	execl();
 extern int	fork();
 extern int	getuid();
 
-#ifdef BSD
-extern int	lseek();
-#else
-extern long	lseek();
-#endif
-
 extern int	open();
-
-#endif /* !defined(__convexc__) */
-
+extern off_t	lseek();
 extern int	nice();
 extern int	pipe();
 extern int	read();
-extern char	*sbrk();
 extern unsigned	sleep();
 extern void	sync();
 extern int	unlink();
 extern int	wait();
 extern int	write();
+
 
 /*
  *	C Library Routines
@@ -111,6 +127,8 @@ extern int	strcmp();
 
 extern long	time();
 
+#endif
+
 /*
  *	STDIO Library Routine supplements
  */
@@ -124,7 +142,53 @@ extern long	time();
 /*
  *	Math Library Routines
  */
-extern double	atof();
+extern double	atof();			/* Should be in math.h or stdlib.h */
 
 #endif /* __STDC__ */
-#endif /* EXTERNS_H_SEENYET */
+
+#if defined(alliant) && !defined(__STDC__)
+extern double   modf();
+#endif
+
+
+/*
+ *  Now, define all the routines found in BRL-CAD's libsysv
+ */
+#if USE_PROTOTYPES
+extern void	port_setlinebuf( FILE *fp );
+#if !defined(__stardent) && !defined(__bsdi__)
+extern int	getopt( int argc, char **argv, char *optstr );
+#endif
+extern char	*re_comp( char *s );
+extern int	re_exec( char *s );
+
+#else
+extern void	port_setlinebuf();
+extern int	getopt();
+extern char	*re_comp();
+extern int	re_exec();
+#endif
+
+/* getopt munchies */
+extern char	*optarg;
+extern int	optind;
+extern int	opterr;
+
+/* sys_errlist and errno */
+#ifndef HAVE_SYS_ERRLIST_DECL
+extern int	errno;
+extern int	sys_nerr;
+extern char *	sys_errlist[];
+#endif
+
+#ifndef HAVE_SBRK_DECL
+extern char *	sbrk();
+extern int	brk();
+#endif
+
+   /* IRIX 5 string.h is unwilling to define strdup */
+#if (IRIX >= 5)
+extern char *	strdup(const char *s);
+#endif
+
+#endif /* EXTERNS_H */
