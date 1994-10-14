7831e26025377dc1cd0ac080cb27701bdc0da52c
John Anderson <daytonajra@gmail.com>
1994-10-14 10:52:34 +0000

    Added "Suggestions" to give the user a hint when supplied options don't match the contents of the IGES file.
    svn:revision:9616
    cvs:account:jra
    cvs:branch:trunk

--- iges/main.c
+++ iges/main.c
@@ -53,15 +53,84 @@ mat_t *identity;
 
 extern char	version[];
 
+static int do_splines=0;
+static int do_drawings=0;
+static int trimmed_surf=0;
+
+static char *iges_file;
+
+static char *msg1=
+"\nThis IGES file contains solid model entities, but your options do not permit\n\
+converting them to BRL-CAD. You may want to try 'iges-g -o file.g %s' to\n\
+convert the solid model elements\n";
+
+static char *msg2=
+"\nThis IGES file contains drawing entities, but no solid model entities. You may\n\
+convert the drawing to BRL-CAD by 'iges-g -d -o file.g %s'. Note that the resulting\n\
+BRL-CAD object will be a 2D drawing, not a solid object\n";
+
+static char *msg3=
+"\nThis IGES file contains spline surfaces, but no solid model entities. All the spline\n\
+surfaces in the IGES file may be combined into a single BRL-CAD spline solid by\n\
+'iges-g -n -o file.g %s'\n";
+
+static char *msg4=
+"\nThis IGES file contains trimmed surfaces, but no solid model entities.\n\
+A '-t' option for 'iges-g' is under construction that will eventually allow you\n\
+to convert all the trimmed surfaces in an IGES file into one BRL-CAD solid.\n\
+Sorry this isn't complete yet.\n";
+
+void
+Suggestions()
+{
+	int i;
+	int csg=0;
+	int brep=0;
+	int splines=0;
+	int tsurfs=0;
+	int drawing=0;
+
+	/* categorize the elements in the IGES file as to whether they are
+	 * CSG, BREP, Trimmed surfaces, Spline surfaces, or drawing elements
+	 */
+	for( i=0 ; i<NTYPES ; i++ )
+	{
+		if( typecount[i].type >= 150 && typecount[i].type <= 184 ||
+		    typecount[i].type == 430 )
+			csg += typecount[i].count;
+		else if( typecount[i].type == 186 ||
+			 typecount[i].type >= 502 && typecount[i].type <=514 )
+			brep += typecount[i].count;
+		else if( typecount[i].type == 128 )
+			splines += typecount[i].count;
+		else if( typecount[i].type == 144 )
+			tsurfs += typecount[i].count;
+		else if( typecount[i].type >= 100 && typecount[i].type <= 112 ||
+			 typecount[i].type == 126 ||
+			 typecount[i].type >= 202 && typecount[i].type <= 230 ||
+			 typecount[i].type == 404 || typecount[i].type == 410 )
+			drawing += typecount[i].count;
+	}
+
+	if( (csg || brep) && (do_splines || do_drawings || trimmed_surf ) )
+		printf( msg1 , iges_file );
+
+	if( drawing && csg == 0 && brep == 0 && !do_drawings )
+		printf( msg2 , iges_file );
+
+	if( splines && csg == 0 && brep == 0 && !do_splines )
+		printf( msg3 , iges_file );
+
+	if( tsurfs && csg == 0 && brep == 0 && !trimmed_surf )
+		printf( msg4 , iges_file );
+}
+
 main( argc , argv )
 int argc;
 char *argv[];
 {
 	int i;
 	int c;
-	int do_splines=0;
-	int do_drawings=0;
-	int trimmed_surf=0;
 	char *output_file=(char *)NULL;
 
 	while( (c=getopt( argc , argv , "dnto:" )) != EOF )
@@ -104,6 +173,7 @@ char *argv[];
 
 	argc -= optind;
 	argv += optind;
+	iges_file = argv[0];
 	fd = fopen( argv[0] , "r" );	/* open IGES file */
 	if( fd == NULL )
 	{
@@ -161,19 +231,19 @@ char *argv[];
 	if( do_drawings )
 		Conv_drawings();	/* convert drawings to wire edges */
 	else if( trimmed_surf )
-		Convtrimsurfs();
+		Convtrimsurfs();	/* try to convert trimmed surfaces to a single solid */
+	else if( do_splines )
+		Convsurfs();		/* Convert NURBS to a single solid */
 	else
 	{
 		Convinst();	/* Handle Instances */
 
 		Convsolids();	/* Convert solid entities */
 
-		if( do_splines )
-			Convsurfs();	/* Convert NURBS */
-
 		Convtree();	/* Convert Boolean Trees */
 
 		Convassem();	/* Convert solid assemblies */
 	}
 
+	Suggestions();
 }
