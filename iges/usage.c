8d748149b8f75e49cbdf3af8cc0a6a6556751d8b
John Anderson <daytonajra@gmail.com>
1994-11-03 18:29:17 +0000

    updated usage.
    svn:revision:9847
    cvs:account:jra
    cvs:branch:trunk

--- iges/usage.c
+++ iges/usage.c
@@ -20,13 +20,19 @@
 #include "machine.h"
 #include "vmath.h"
 
+char *message="Usage:  iges-g [-n|d|t] -o file.g file.iges\n\
+	-n - Convert all rational B-spline surfaces to a single spline solid\n\
+	-d - Convert IGES drawings to NMG objects (and ignore solid objects)\n\
+	-t - Convert all trimmed surfaces to NMG trimmed NURBS\n\
+	-o - Specify BRLCAD output file\n\
+The n, d, and t options are mutually exclusive.\n\
+With none of the n, d, or t options specified, the default action\n\
+is to convert only IGES solid model entities (CSG and planar face BREP)\n";
+
+	
+
 usage()
 {
-	fprintf( stderr , "Usage:  iges-g [-n|d|t] -o file.g file.iges\n" );
-	fprintf( stderr , "\t-n - Convert all rational B-spline surfaces to a single spline solid\n" );
-	fprintf( stderr , "\t-d - Convert IGES drawings to NMG objects (and ignore solid objects)\n" );
-	fprintf( stderr , "\t-t - Convert all trimmed surfaces to NMG trimmed NURBS\n" );
-	fprintf( stderr , "\t\tthe n, d, and t options are mutually exclusive\n" );
-	fprintf( stderr , "\t-o - Specify BRLCAD output file\n" );
+	fprintf( stderr , message );
 	exit( 1 );
 }
