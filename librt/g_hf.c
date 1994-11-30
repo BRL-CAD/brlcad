8c27d4a9fbbf5dda74e2e6973cb756b5d64dc6a9
Christopher Johnson <cjohnson@brlcad.org>
1994-11-30 01:49:49 +0000

    We need to optimize the cookies before comparing.
    svn:revision:10074
    cvs:account:cjohnson
    cvs:branch:trunk

--- librt/g_hf.c
+++ librt/g_hf.c
@@ -583,7 +583,7 @@ err1:
 		out_cookie = cv_cookie("hf");
 	}
 
-	if( in_cookie == out_cookie )  {
+	if( cv_optimize(in_cookie) == cv_optimize(out_cookie) )  {
 		/* Don't replicate the data, just re-use the pointer */
 		mp->apbuf = mp->buf;
 		return 0;		/* OK */
