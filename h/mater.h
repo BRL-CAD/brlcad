982dc905e0bff05ebef250a9f8ef45a0583e9514
Michael John Muuss <mike@brlcad.org>
1994-12-27 17:18:43 +0000

    Bug 247, changed MaterHead to rt_material_head
    svn:revision:10386
    cvs:account:mike
    cvs:branch:trunk

--- h/mater.h
+++ h/mater.h
@@ -31,4 +31,4 @@ struct mater {
 #define MATER_NULL	((struct mater *)0)
 #define MATER_NO_ADDR	(-1L)		/* invalid mt_daddr */
 
-extern struct mater *MaterHead;		/* defined in mater.c */
+extern struct mater *rt_material_head;		/* defined in mater.c */
