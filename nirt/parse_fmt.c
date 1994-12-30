df78d7e61a2cf851fe03357b7db6c07c11395eb3
Glenn Durfee <gdurfee@gmail.com>
1994-12-30 18:36:45 +0000

    Fixed #ifdef around popen declaration
    svn:revision:10494
    cvs:account:gdurf
    cvs:branch:trunk

--- nirt/parse_fmt.c
+++ nirt/parse_fmt.c
@@ -610,7 +610,7 @@ com_table	*ctp;
     int 	i = 0;      /* current position on the *buffer        */
     FILE	*newf;
     static char	*new_dest;
-#ifndef CRAY2
+#if !defined(HAVE_POPEN_DECL) && !defined(CRAY2)
     RT_EXTERN(FILE *popen, (CONST char *command, CONST char *type) );
 #endif
     static FILE	*(*openfunc)() = 0;
