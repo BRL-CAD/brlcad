0f863e943f07f35e5b5c6bd8eb3349ea26a5ae3e
Paul Tanenbaum <pjt@brlcad.org>
1995-04-19 19:04:11 +0000

    1. Allocate storage for and initialize rbt_unique member. 2. Initialize rbt_debug
    svn:revision:10896
    cvs:account:pjt
    cvs:branch:trunk

--- libredblack/rb_create.c
+++ libredblack/rb_create.c
@@ -46,6 +46,9 @@ int	(**order_funcs)();
     tree -> rbt_root = (struct rb_node **)
 		    rt_malloc(nm_orders * sizeof(struct rb_node),
 			"red-black roots");
+    tree -> rbt_unique = (char *)
+		rt_malloc((size_t) ceil((double) (nm_orders / 8.0)),
+			    "red-black uniqueness flags");
     tree -> rbt_empty_node = (struct rb_node *)
 		    rt_malloc(sizeof(struct rb_node), "red-black empty node");
     rb_null(tree) -> rbn_parent = (struct rb_node **)
@@ -62,6 +65,8 @@ int	(**order_funcs)();
     tree -> rbt_nm_orders = nm_orders;
     tree -> rbt_order = order_funcs;
     tree -> rbt_print = 0;
+    rb_uniq_all_off(tree);
+    tree -> rbt_debug = 0x0;
     tree -> rbt_current = tree -> rbt_empty_node;
     for (order = 0; order < nm_orders; ++order)
 	rb_root(tree, order) = rb_null(tree);
