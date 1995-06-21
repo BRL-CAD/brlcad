768aee272d7fc8a6c56034d7f22eabf8e0e8eecb
Douglas Gwyn <dagwyn@aol.com>
1995-06-21 03:45:48 +0000

    Eliminated blank line at EOF. String literals are now arrays rather than pointers. ProcTmp now points into /usr/tmp; was /tmp. TempFilePrefix was unused. Configure $(LIBDIR) paths during make, to produce tune.c which is compiled.
    svn:revision:11041
    cvs:account:gwyn
    cvs:branch:trunk

--- jove/jove_tune.c
+++ jove/jove_tune.c
@@ -2,42 +2,46 @@
  *		J O V E _ T U N E . C
  */
 
-char	*TempFilePrefix	= "jovet";	/* for tempnam */
-char	*TempFile	= "/usr/tmp/jovetXXXXXX";
+#include	"./jove.h"
+
+#if ! HAS_TEMPNAM
+char	TempFile[]	= "/usr/tmp/jovetXXXXXX";
 			/* Where mktemp should put the temp file */
 
-char	*ProcTmp	= "/tmp/jovepXXXXXX";
+char	ProcTmp[]	= "/usr/tmp/jovepXXXXXX";
 			/* Where jove should put process temp files */
+#endif
 
-char	*BinShell	= "/bin/sh";	/* /bin/sh or /bin/csh */
+char	BinShell[]	= "/bin/sh";	/* /bin/sh (preferably) or /bin/csh */
 					/* getenv("SHELL") will overide */
-char	*Describe	= "/usr/brl/lib/jove/describe.com";
-char	*Findcom	= "/usr/brl/lib/jove/findcom";
-char	*Joverc		= "/usr/brl/lib/jove/joverc";
+/* Following paths are edited during the "make" process to create tune.c from jove_tune.c: */
+char	Describe[]	= "LibDir/describe.com";
+char	Findcom[]	= "LibDir/findcom";
+char	Joverc[]	= "LibDir/joverc";
 			/* Where to search for the describe command */
 int	Dfltmode	= 0644;
 
 #ifdef pdp11
-char	*cerrfmt = "\\([^:]*\\):\\([0-9][0-9]*\\):";
+char	cerrfmt[] = "\\([^:]*\\):\\([0-9][0-9]*\\):";
 	/* Very old pdp11 C error scanf format string for errors of the form
 	 * filename:linenum: error message
 	 */
 #else
 # ifdef CRAY
 	/* Cray has their own stupid error format */
-char	*cerrfmt = " \\* [FW] \\* :\\([^:]*\\): \\([0-9][0-9]*\\):";
+char	cerrfmt[] = " \\* [FW] \\* :\\([^:]*\\): \\([0-9][0-9]*\\):";
 	/* C error scanf format string for errors of the form
 	 * sp * sp [FW] sp * sp :filename: sp linenum:sp error message.
 	 */
 # else
 #    if	(defined(__sgi) && defined(__mips))
-char	*cerrfmt = "[^:]*: [^:]*: \\([^:]*\\), line \\([0-9][0-9]*\\):";
+char	cerrfmt[] = "[^:]*: [^:]*: \\([^:]*\\), line \\([0-9][0-9]*\\):";
 	/* SGI has a new format, too.
 	 * accom: Error: filename, line linenum: error message.
 	 *         ^___ or "Warning ###" here
 	 */
 #    else
-char	*cerrfmt = "\"\\([^:]*\\)\", line \\([0-9][0-9]*\\):";
+char	cerrfmt[] = "\"\\([^:]*\\)\", line \\([0-9][0-9]*\\):";
 	/* C error scanf format string for errors of the form
 	 * "filename", line linenum: error message.
 	 * used on all SYSV and all modern BSD systems.
@@ -46,8 +50,7 @@ char	*cerrfmt = "\"\\([^:]*\\)\", line \\([0-9][0-9]*\\):";
 # endif
 #endif
 
-char	*lerrfmt = "\"\\([^:]*\\)\", line \\([0-9][0-9]*\\):";
+char	lerrfmt[] = "\"\\([^:]*\\)\", line \\([0-9][0-9]*\\):";
 	/* Lint error for errors of the form
 	 * "filename", line linenum: error message.
 	 */
-
