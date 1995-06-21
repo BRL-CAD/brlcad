72fd2b8db9dce44fbe5ca520ac71708dc905b49e
Douglas Gwyn <dagwyn@aol.com>
1995-06-21 03:42:30 +0000

    Eliminated trailing blanks. BinShell, version, Joverc are now arrays rather than string literal pointers. Changed memcpy calls back to bcopy.  (Fixed confusing use of SYSV vs. SYS5.) Fixed varargs functions, using VA_* macros. Fixed usage of FNDELAY, which is obsolete. Improved use of conditionals dealing with system variations. Made reset of SIGINT handler independent of SIGTSTP.
    svn:revision:11035
    cvs:account:gwyn
    cvs:branch:trunk

--- jove/jove_main.c
+++ jove/jove_main.c
@@ -1,9 +1,12 @@
 /*
- *			J O V E _ M A I N . C 
+ *			J O V E _ M A I N . C
  *
  * $Revision$
  *
  * $Log$
+ * Revision 11.3  95/01/04  23:26:50  mike
+ * Irix 5.3 fix
+ *
  * Revision 11.2  1995/01/04  20:09:00  stay
  * Fixed for Irix 5.3 which no longer declares
  * FNDELAY for POSIX_SOURCE
@@ -13,16 +16,16 @@
  *
  * Revision 10.13  94/12/29  23:02:59  mike
  * Bug #158.  Eliminated ^V doubling on DEC Alpha, and Solaris.
- * 
+ *
  * Revision 10.12  94/12/14  15:33:54  jra
  * Cray also needs file.h included.
- * 
+ *
  * Revision 10.11  94/10/14  17:29:25  mike
  * SunOS 5.2
- * 
+ *
  * Revision 10.10  94/09/22  18:53:54  butler
  * Added ifdef needed for Solaris
- * 
+ *
  * Revision 10.9  1994/09/17  04:57:35  butler
  * changed all calls to bcopy to be memcpy instead.  Useful for Solaris 5.2
  *
@@ -31,91 +34,91 @@
  *
  * Revision 10.7  93/12/10  05:35:40  mike
  * Additional POSIX support
- * 
+ *
  * Revision 10.6  93/10/26  06:32:59  mike
  * Changed printf() to jprintf() so that all modules could safely
  * use stdio.h
- * 
+ *
  * Revision 10.5  93/10/26  06:01:51  mike
  * Changed getchar() to jgetchar() to prevent stdio.h conflict
- * 
+ *
  * Revision 10.4  93/10/26  05:42:22  mike
  * POSIX
- * 
+ *
  * Revision 10.3  93/10/26  03:38:55  mike
  * termios stuff
- * 
+ *
  * Revision 10.2  93/04/01  04:25:24  mike
  * Arg to FIONREAD is an "int", not a "long" these days.
- * 
+ *
  * Revision 10.1  91/10/12  06:54:00  mike
  * Release_4.0
- * 
+ *
  * Revision 2.16  91/08/30  20:24:48  mike
  * Added !defined(SYS5) protection around some BSD ioctls
- * 
+ *
  * Revision 2.15  91/08/30  19:28:59  mike
  * Added BRL-specific identification
- * 
+ *
  * Revision 2.14  91/08/30  18:11:05  mike
  * Made explicit that termcap.h to be used is the local version
- * 
+ *
  * Revision 2.13  91/08/30  17:54:35  mike
  * Changed #include directives to distinguish between local and system header
  * files.
- * 
+ *
  * Revision 2.12  91/08/30  17:49:09  mike
  * Paul Stay mods for ANSI C
- * 
+ *
  * Revision 2.11  87/09/16  21:28:40  mike
  * Hopefully, the Final Word on the SysV "poll" for input
  * code.
- * 
+ *
  * Revision 2.10  87/09/10  00:23:01  mike
  * Hopefully, the fix for the "scramble the input" bug on the Crays
  * and other SysV machines.
- * 
+ *
  * Revision 2.9  87/05/05  21:04:12  dpk
  * Simplified getchar code. Now all systems use the read loop
- * 
+ *
  * Revision 2.8  87/04/14  20:14:21  dpk
  * Added SIGHUP fix.
- * 
+ *
  * Revision 2.7  86/09/23  22:28:52  mike
  * Externs now declared properly.
  * I/O fixes for SysV
- * 
+ *
  * Revision 2.7  86/09/23  22:26:46  mike
  * Externs now declared properly.
  * I/O fixes for SysV
- * 
+ *
  * Revision 2.6  86/04/11  09:09:51  gwyn
  * last mod was incomplete
- * 
+ *
  * Revision 2.5  86/04/06  06:28:48  gwyn
  * Fixed endless loop on joverc under System V.
- * 
+ *
  * Revision 2.4  86/01/17  13:23:37  gwyn
  * fixed SYS5 terminal mode setup
- * 
+ *
  * Revision 2.3  86/01/11  03:01:51  gwyn
  * fixed window size code
- * 
+ *
  * Revision 2.2  85/05/14  01:44:41  dpk
  * Added changes to support System V (conditional on SYS5)
- * 
+ *
  * Revision 2.1  85/01/17  23:58:24  dpk
- * 
- * 
+ *
+ *
  * Revision 2.0  84/12/26  16:46:45  dpk
  * System as distributed to Berkeley 26 Dec 84
- * 
+ *
  * Revision 1.3  84/03/20  22:27:17  dpk
  * Added better termcap handling
- * 
+ *
  * Revision 1.2  83/12/16  00:08:44  dpk
  * Added distinctive RCS header
- * 
+ *
  */
 #ifndef lint
 static char RCSid[] = "@(#)$Header$";
@@ -138,9 +141,6 @@ static char RCSid[] = "@(#)$Header$";
 #  endif
 # include <termios.h>
 # include <fcntl.h>
-# if defined(sun) || defined(sparc) || defined(__sparc) || defined(cray)
-#   include <sys/file.h>  /* Solaris and UNICOS need this for FNDELAY */
-# endif
 #else
 # ifndef SYS5
 #  include <sgtty.h>
@@ -151,29 +151,20 @@ static char RCSid[] = "@(#)$Header$";
 #endif
 #include <errno.h>
 
-				/* IRIX 5.x needs FNDELAY defined */
-#if IRIX >= 5
-#include <sys/file.h>
-#endif
-
-extern char	*version;
-extern char	*BinShell;
-extern char	*Joverc;
+extern char	version[];
+extern char	BinShell[];
+extern char	Joverc[];
+#if !defined(__STDC__) && !defined(_POSIX_SOURCE)	/* i.e. fairly old UNIX */
 extern int	errno;
-
-extern char	*sprint();
+#endif
 
 void		DoKeys();
 
-#ifndef	BRLUNIX			/* this is not found on a BRL pdp-11.	*/
-/***** #include <sys/ioctl.h> *****/
-#endif
-
 #if defined(TIOCSLTC) && !defined(SYS5)
 struct ltchars	ls1, ls2;
-#endif 
+#endif
 
-#if !defined(SYS5) && !defined(HAS_TERMIOS)
+#if defined(TIOCSETC) && !defined(SYS5)
 struct tchars	tc1, tc2;	/* only on old-style BSD */
 #endif
 
@@ -227,9 +218,7 @@ finish(code)
 	if (code == SIGINT) {
 		char	c;
 
-#ifndef SIGTSTP			/* Job stopping in other words */
 		ignorf(signal(code, finish));
-#endif
 		message("Quit? ");
 		UpdateMesg();
 		ignore(read(0, &c, 1));
@@ -289,7 +278,7 @@ jgetchar()
 		do {
 			nchars = read(Input, smbuf, sizeof smbuf);
 #ifdef SYS5
-		} while ((nchars == 0 && Input == 0)	/* DAG -- added Input test */
+		} while ((nchars == 0 && Input == 0)
 			 || (nchars < 0 && errno == EINTR));
 #else
 		} while (nchars < 0 && errno == EINTR);
@@ -321,14 +310,14 @@ charp()
 			return( 1 );
 		else
 			return( 0 );
-#else 
+#else
 #ifdef FIONREAD
 		int c = 0;
 
 		if (ioctl(0, FIONREAD, (char *) &c) == -1)
 			c = 0;
 #else
-#if defined(SYS5) || defined(HAS_TERMIOS)
+#if defined(SYS5) || HAS_TERMIOS
 		int c, flags;
 
 		/* Since VMIN=1, we need to be able to poll for input
@@ -338,8 +327,8 @@ charp()
 		 * characters here, to see if there are any.
 		 */
 		flags = fcntl( Input, F_GETFL, 0);
-#  if defined(HAS_TERMIOS)
-		(void)fcntl( Input, F_SETFL, flags|FNDELAY );
+#  if HAS_TERMIOS
+		(void)fcntl( Input, F_SETFL, flags|O_NONBLOCK );
 #  else
 		(void)fcntl( Input, F_SETFL, flags|O_NDELAY );
 #  endif
@@ -450,7 +439,7 @@ register int	prompt;
 int	OKXonXoff = 0;		/* ^S and ^Q initially DON'T work */
 
 #ifndef	BRLUNIX
-# if defined(HAS_TERMIOS)
+# if HAS_TERMIOS
 struct termios	oldtty, newtty;
 # else
 #  if defined(SYS5)
@@ -465,7 +454,7 @@ struct sg_brl	oldtty, newtty;
 
 ttsetup() {
 #ifndef BRLUNIX
-#  if defined(HAS_TERMIOS)
+#  if HAS_TERMIOS
 	if (tcgetattr( 0, &oldtty ) < 0) {
 #  else
 #    if defined(SYS5)
@@ -484,14 +473,13 @@ ttsetup() {
 	/* One time setup of "raw mode" stty struct */
 	newtty = oldtty;
 #ifndef BRLUNIX
-#if !defined(SYS5) && !defined(HAS_TERMIOS)
+#if !defined(SYS5) && !HAS_TERMIOS
 	newtty.sg_flags &= ~(ECHO | CRMOD);
 	newtty.sg_flags |= CBREAK;
 #else
 	newtty.c_iflag &= ~(INLCR|ICRNL);
 	newtty.c_lflag &= ~(ISIG|ICANON|ECHO);
 
-	/* DAG -- bug fix (was missing) */
 	/* Not all exist in POSIX.  If not there, no need to clear them. */
 #	if defined(OLCUC)
 		newtty.c_oflag &= ~(OLCUC);
@@ -578,8 +566,8 @@ ttinit()
 	ls2.t_lnextc = (char) -1;
 #endif
 
-	/* Change interupt and quit. */
-#if defined(TIOCGETC) && !defined(SYS5)
+	/* Change interrupt and quit. */
+#if defined(TIOCSETC) && !defined(SYS5)
 	ioctl(0, TIOCGETC, (char *) &tc1);
 	tc2 = tc1;
 	tc2.t_intrc = (char) -1;
@@ -608,7 +596,7 @@ ttinit()
 #ifndef BRLUNIX
 ttyset(n)
 {
-#if defined(HAS_TERMIOS)
+#if HAS_TERMIOS
 	struct termios	tty;
 #else
 # if defined(SYS5)
@@ -623,10 +611,10 @@ ttyset(n)
 	else
 		tty = oldtty;
 
-#if defined(HAS_TERMIOS)
+#if HAS_TERMIOS
 	if (tcsetattr( 0, TCSANOW, &tty) == -1)
 #else
-#  if !defined(SYS5) && !defined(HAS_TERMIOS)
+#  if !defined(SYS5)
 	if (ioctl(0, TIOCSETN, (char *) &tty) == -1)
 #  else
 	if (ioctl (0, TCSETAW, (char *) &tty) == -1)
@@ -643,7 +631,7 @@ ttyset(n)
 	ioctl(0, TIOCSLTC, n == 0 ? (char *) &ls1 : (char *) &ls2);
 #endif
 }
-#else
+#else	/* BRLUNIX */
 ttyset(n)
 {
 	struct sg_brl	tty;
@@ -782,43 +770,60 @@ Ignorf(a)
 }
 #endif
 
-/* VARARGS1 */
+/* VARARGS */
 
-error(fmt, a, b, c, d)
-char	*fmt;
-char	*a, *b, *c, *d;
+void
+error(VA_T(const char *fmt) VA_ALIST)
+	VA_DCL
 {
+	VA_D(char *fmt)
+	VA_LIST(ap)
+
+	VA_START(ap, fmt)
+	VA_I(ap, char *, fmt)
 	if (fmt) {
-		sprintf(mesgbuf, fmt, a, b, c, d);
+		vsprintf(mesgbuf, fmt, ap);
 		UpdMesg++;
 	}
+	VA_END(ap)
 	rbell();
 	longjmp(mainjmp, ERROR);
 }
 
-/* VARARGS1 */
+/* VARARGS */
 
-complain(fmt, a, b, c, d)
-char	*fmt;
-char	*a, *b, *c, *d;
+void
+complain(VA_T(const char *fmt) VA_ALIST)
+	VA_DCL
 {
+	VA_D(char *fmt)
+	VA_LIST(ap)
+
+	VA_START(ap, fmt)
+	VA_I(ap, char *, fmt)
 	if (fmt) {
-		sprintf(mesgbuf, fmt, a, b, c, d);
+		vsprintf(mesgbuf, fmt, ap);
 		UpdMesg++;
 	}
+	VA_END(ap)
 	rbell();	/* Always ring the bell now */
 	longjmp(mainjmp, COMPLAIN);
 }
 
-/* VARARGS1 */
+/* VARARGS */
 
-confirm(fmt, a, b, c, d)
-char	*fmt;
-char	*a, *b, *c, *d;
+void
+confirm(VA_T(const char *fmt) VA_ALIST)
+	VA_DCL
 {
+	VA_D(char *fmt)
+	VA_LIST(ap)
 	char *yorn;
 
-	sprintf(mesgbuf, fmt, a, b, c, d);
+	VA_START(ap, fmt)
+	VA_I(ap, char *, fmt)
+	vsprintf(mesgbuf, fmt, ap);
+	VA_END(ap)
 	yorn = ask((char *)0, mesgbuf);
 	if (*yorn != 'Y' && *yorn != 'y')
 		longjmp(mainjmp, COMPLAIN);
@@ -854,7 +859,7 @@ DoKeys(first)
 	register int	c;
 	jmp_buf	savejmp;
 
-	memcpy((char *) savejmp, (char *) mainjmp, sizeof savejmp);
+	bcopy((char *) mainjmp, (char *) savejmp, sizeof savejmp);
 
 	switch( setjmp(mainjmp) ) {
 	case 0:
@@ -863,7 +868,7 @@ DoKeys(first)
 		break;
 
 	case QUIT:
-		memcpy((char *) mainjmp, (char *) savejmp, sizeof mainjmp);
+		bcopy((char *) savejmp, (char *) mainjmp, sizeof mainjmp);
 		return;
 
 	case ERROR:
@@ -961,7 +966,7 @@ char	*argv[];
 		putpad(KS, 1);
 	putpad(CL, 1);
 
-	memcpy(origflags, curbuf->b_flags, NFLAGS*sizeof(int));
+	bcopy(curbuf->b_flags, origflags, NFLAGS*sizeof(int));
 	/* All new buffers will have these flags when created. */
 	RedrawDisplay();	/* Start the redisplay process */
 	DoKeys(1);
