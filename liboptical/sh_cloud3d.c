d6f4bd9fe2d7b7bed587cc3a0c6b0ab3a722a680
Lee Butler <iraytrace@users.noreply.github.com>
1995-08-19 05:06:34 +0000

    renamed to scloud for "solid cloud" to avoid conflict with old "cloud" shader
    svn:revision:11295
    cvs:account:butler
    cvs:branch:trunk

--- liboptical/sh_cloud3d.c
+++ liboptical/sh_cloud3d.c
@@ -1,7 +1,7 @@
 /*
- *	S H _ C L O U D 3 D . C
+ *	S H _ S C L O U D . C
  *
- *	A 3D cloud shader
+ *	A 3D "solid" cloud shader
  */
 #include "conf.h"
 
@@ -19,7 +19,7 @@
 #define FLOOR(x)	(  (int)(x) - (  (x) < 0 && (x) != (int)(x)  )  )
 #define CEIL(x)		(  (int)(x) + (  (x) > 0 && (x) != (int)(x)  )  )
 
-struct cloud3d_specific {
+struct scloud_specific {
 	double	lacunarity;
 	double	h_val;
 	double	octaves;
@@ -29,7 +29,7 @@ struct cloud3d_specific {
 	mat_t	xform;
 };
 
-static struct cloud3d_specific cloud3d_defaults = {
+static struct scloud_specific scloud_defaults = {
 	2.1753974,	/* lacunarity */
 	1.0,		/* h_val */
 	4.0,		/* octaves */
@@ -38,40 +38,40 @@ static struct cloud3d_specific cloud3d_defaults = {
 	{ 1.0, 1.0, 1.0 },	/* scale */
 	};
 
-#define CLOUD3D_NULL	((struct cloud3d_specific *)0)
-#define CLOUD3D_O(m)	offsetof(struct cloud3d_specific, m)
-#define CLOUD3D_AO(m)	offsetofarray(struct cloud3d_specific, m)
+#define SHDR_NULL	((struct scloud_specific *)0)
+#define SHDR_O(m)	offsetof(struct scloud_specific, m)
+#define SHDR_AO(m)	offsetofarray(struct scloud_specific, m)
 
-struct structparse cloud3d_pr[] = {
-	{"%f",	1, "lacunarity",	CLOUD3D_O(lacunarity),	FUNC_NULL },
-	{"%f",	1, "H", 		CLOUD3D_O(h_val),	FUNC_NULL },
-	{"%f",	1, "octaves", 		CLOUD3D_O(octaves),	FUNC_NULL },
-	{"%f",	1, "thresh",		CLOUD3D_O(thresh),	FUNC_NULL },
-	{"%f",  3, "scale",		CLOUD3D_AO(scale),	FUNC_NULL },
-	{"%f",  3, "delta",		CLOUD3D_AO(delta),	FUNC_NULL },
+struct structparse scloud_pr[] = {
+	{"%f",	1, "lacunarity",	SHDR_O(lacunarity),	FUNC_NULL },
+	{"%f",	1, "H", 		SHDR_O(h_val),	FUNC_NULL },
+	{"%f",	1, "octaves", 		SHDR_O(octaves),	FUNC_NULL },
+	{"%f",	1, "thresh",		SHDR_O(thresh),	FUNC_NULL },
+	{"%f",  3, "scale",		SHDR_AO(scale),	FUNC_NULL },
+	{"%f",  3, "delta",		SHDR_AO(delta),	FUNC_NULL },
 	{"",	0, (char *)0,		0,			FUNC_NULL }
 };
-struct structparse cloud3d_parse[] = {
-	{"%f",	1, "lacunarity",	CLOUD3D_O(lacunarity),	FUNC_NULL },
-	{"%f",	1, "H", 		CLOUD3D_O(h_val),	FUNC_NULL },
-	{"%f",	1, "octaves", 		CLOUD3D_O(octaves),	FUNC_NULL },
-	{"%f",	1, "thresh",		CLOUD3D_O(thresh),	FUNC_NULL },
-	{"%f",  3, "scale",		CLOUD3D_AO(scale),	FUNC_NULL },
-	{"%f",  3, "delta",		CLOUD3D_AO(delta),	FUNC_NULL },
-	{"%f",	1, "l",			CLOUD3D_O(lacunarity),	FUNC_NULL },
-	{"%f",	1, "o", 		CLOUD3D_O(octaves),	FUNC_NULL },
-	{"%f",	1, "t",			CLOUD3D_O(thresh),	FUNC_NULL },
-	{"%f",  3, "s",			CLOUD3D_AO(scale),	FUNC_NULL },
-	{"%f",  3, "d",			CLOUD3D_AO(delta),	FUNC_NULL },
+struct structparse scloud_parse[] = {
+	{"%f",	1, "lacunarity",	SHDR_O(lacunarity),	FUNC_NULL },
+	{"%f",	1, "H", 		SHDR_O(h_val),	FUNC_NULL },
+	{"%f",	1, "octaves", 		SHDR_O(octaves),	FUNC_NULL },
+	{"%f",	1, "thresh",		SHDR_O(thresh),	FUNC_NULL },
+	{"%f",  3, "scale",		SHDR_AO(scale),	FUNC_NULL },
+	{"%f",  3, "delta",		SHDR_AO(delta),	FUNC_NULL },
+	{"%f",	1, "l",			SHDR_O(lacunarity),	FUNC_NULL },
+	{"%f",	1, "o", 		SHDR_O(octaves),	FUNC_NULL },
+	{"%f",	1, "t",			SHDR_O(thresh),	FUNC_NULL },
+	{"%f",  3, "s",			SHDR_AO(scale),	FUNC_NULL },
+	{"%f",  3, "d",			SHDR_AO(delta),	FUNC_NULL },
 	{"",	0, (char *)0,		0,			FUNC_NULL }
 };
 
-HIDDEN int	cloud3d_setup(), cloud3d_render();
-HIDDEN void	cloud3d_print(), cloud3d_free();
+HIDDEN int	scloud_setup(), scloud_render();
+HIDDEN void	scloud_print(), scloud_free();
 
-struct mfuncs cloud3d_mfuncs[] = {
-	{"cloud3d",	0,		0,		MFI_NORMAL|MFI_HIT|MFI_UV,
-	cloud3d_setup,	cloud3d_render,	cloud3d_print,	cloud3d_free },
+struct mfuncs scloud_mfuncs[] = {
+	{"scloud",	0,		0,		MFI_NORMAL|MFI_HIT|MFI_UV,
+	scloud_setup,	scloud_render,	scloud_print,	scloud_free },
 
 	{(char *)0,	0,		0,		0,
 	0,		0,		0,		0 }
@@ -80,35 +80,35 @@ struct mfuncs cloud3d_mfuncs[] = {
 
 
 /*
- *	C L O U D 3 D _ S E T U P
+ *	S C L O U D _ S E T U P
  */
 HIDDEN int
-cloud3d_setup( rp, matparm, dpp, mfp, rtip )
+scloud_setup( rp, matparm, dpp, mfp, rtip )
 register struct region *rp;
 struct rt_vls	*matparm;
 char	**dpp;	/* pointer to reg_udata in *rp */
 struct mfuncs		*mfp;
 struct rt_i		*rtip;
 {
-	register struct cloud3d_specific *cloud3d;
+	register struct scloud_specific *scloud;
 	struct db_full_path full_path;
 	mat_t	region_to_model;
 	mat_t	model_to_region;
 	mat_t	tmp;
 
 	RT_VLS_CHECK( matparm );
-	GETSTRUCT( cloud3d, cloud3d_specific );
-	*dpp = (char *)cloud3d;
+	GETSTRUCT( scloud, scloud_specific );
+	*dpp = (char *)scloud;
 
-	memcpy(cloud3d, &cloud3d_defaults, sizeof(struct cloud3d_specific) );
+	memcpy(scloud, &scloud_defaults, sizeof(struct scloud_specific) );
 	if( rdebug&RDEBUG_SHADE)
-		rt_log("cloud3d_setup\n");
+		rt_log("scloud_setup\n");
 
-	if( rt_structparse( matparm, cloud3d_parse, (char *)cloud3d ) < 0 )
+	if( rt_structparse( matparm, scloud_parse, (char *)scloud ) < 0 )
 		return(-1);
 
 	if( rdebug&RDEBUG_SHADE)
-		rt_structprint( rp->reg_name, cloud3d_parse, (char *)cloud3d );
+		rt_structprint( rp->reg_name, scloud_parse, (char *)scloud );
 
 	/* get transformation between world and "region" coordinates */
 	if (db_string_to_path( &full_path, rtip->rti_dbip, rp->reg_name) ) {
@@ -128,55 +128,55 @@ struct rt_i		*rtip;
 
 	/* add the noise-space scaling */
 	mat_idn(tmp);
-	tmp[0] = 1. / cloud3d->scale[0];
-	tmp[5] = 1. / cloud3d->scale[1];
-	tmp[10] =  1. / cloud3d->scale[2];
+	tmp[0] = 1. / scloud->scale[0];
+	tmp[5] = 1. / scloud->scale[1];
+	tmp[10] =  1. / scloud->scale[2];
 
-	mat_mul(cloud3d->xform, tmp, model_to_region);
+	mat_mul(scloud->xform, tmp, model_to_region);
 
 	/* add the translation within noise space */
 	mat_idn(tmp);
-	tmp[MDX] = cloud3d->delta[0];
-	tmp[MDY] = cloud3d->delta[1];
-	tmp[MDZ] = cloud3d->delta[2];
-	mat_mul2(tmp, cloud3d->xform);
+	tmp[MDX] = scloud->delta[0];
+	tmp[MDY] = scloud->delta[1];
+	tmp[MDZ] = scloud->delta[2];
+	mat_mul2(tmp, scloud->xform);
 
 	return(1);
 }
 
 /*
- *	C L O U D 3 D _ P R I N T
+ *	S C L O U D _ P R I N T
  */
 HIDDEN void
-cloud3d_print( rp, dp )
+scloud_print( rp, dp )
 register struct region *rp;
 char	*dp;
 {
-	rt_structprint( rp->reg_name, cloud3d_pr, (char *)dp );
+	rt_structprint( rp->reg_name, scloud_pr, (char *)dp );
 }
 
 /*
- *	C L O U D 3 D _ F R E E
+ *	S C L O U D _ F R E E
  */
 HIDDEN void
-cloud3d_free( cp )
+scloud_free( cp )
 char *cp;
 {
-	rt_free( cp, "cloud3d_specific" );
+	rt_free( cp, "scloud_specific" );
 }
 
 /*
- *	C L O U D 3 D _ R E N D E R
+ *	S C L O U D _ R E N D E R
  */
 int
-cloud3d_render( ap, pp, swp, dp )
+scloud_render( ap, pp, swp, dp )
 struct application	*ap;
 struct partition	*pp;
 struct shadework	*swp;
 char	*dp;
 {
-	register struct cloud3d_specific *cloud3d_sp =
-		(struct cloud3d_specific *)dp;
+	register struct scloud_specific *scloud_sp =
+		(struct scloud_specific *)dp;
 	point_t in_pt, out_pt, pt;
 	vect_t	v_cloud;
 	double	thickness;
@@ -191,9 +191,9 @@ char	*dp;
 	RT_CK_REGION(pp->pt_regionp);
 
 #if 0
-	MAT4X3PNT(in_pt, cloud3d_sp->xform, swp->sw_hit.hit_point);
-		val = noise_fbm(in_pt, cloud3d_sp->h_val, 
-			cloud3d_sp->lacunarity, cloud3d_sp->octaves );
+	MAT4X3PNT(in_pt, scloud_sp->xform, swp->sw_hit.hit_point);
+		val = noise_fbm(in_pt, scloud_sp->h_val, 
+			scloud_sp->lacunarity, scloud_sp->octaves );
  	VSET(swp->sw_color, val, val, val);
 	return 1;
 #endif
@@ -201,8 +201,8 @@ char	*dp;
 	VJOIN1(out_pt, ap->a_ray.r_pt, pp->pt_outhit->hit_dist, ap->a_ray.r_dir);
 
 	/* transform point into "noise-space coordinates" */
-	MAT4X3PNT(in_pt, cloud3d_sp->xform, in_pt);
-	MAT4X3PNT(out_pt, cloud3d_sp->xform, out_pt);
+	MAT4X3PNT(in_pt, scloud_sp->xform, in_pt);
+	MAT4X3PNT(out_pt, scloud_sp->xform, out_pt);
 
 	VSUB2(v_cloud, out_pt, in_pt);
 	thickness = MAGNITUDE(v_cloud);
@@ -218,15 +218,18 @@ char	*dp;
 		/* compute the next point in the cloud space */
 		VJOIN1(pt, in_pt, i*step_delta, v_cloud);
 
-		val = noise_fbm(pt, cloud3d_sp->h_val, 
-			cloud3d_sp->lacunarity, cloud3d_sp->octaves );
+		val = noise_fbm(pt, scloud_sp->h_val, 
+			scloud_sp->lacunarity, scloud_sp->octaves );
 
-		val = 1.0 - fabs(val*cloud3d_sp->thresh*.5 + .5);
+		val = (val+1.) * .5;
 
-		transmission *= val;
-		if (transmission < 0.000001) {
-			transmission = 0.;
-			break;
+		if (val > scloud_sp->thresh) {
+
+			transmission -= 1-val;
+			if (transmission < 0.000001) {
+				transmission = 0.;
+				break;
+			}
 		}
 	}
 
