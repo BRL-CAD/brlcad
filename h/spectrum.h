a5dbd54f267ba4ebdbaa321be74494e7f76f7d7a
Michael John Muuss <mike@brlcad.org>
1996-04-06 00:58:18 +0000

    Moved rt_table stuff out into h/tabdata.h
    svn:revision:11862
    cvs:account:mike
    cvs:branch:trunk

--- h/spectrum.h
+++ h/spectrum.h
@@ -1,8 +1,5 @@
 /*
- *			T A B L E . H
- * and
  *			S P E C T R U M . H
-    ...soon to be separated...
  *
  *  A header file containing data structures to assist with
  *  recording spectral data.
@@ -10,16 +7,11 @@
  *  as compact as possible (typically just an array of power levels),
  *  with all the context stored in one place.
  *
- *  NOTE that this is really a much more general mechanism than originally
- *  envisioned -- these structures and support routines apply to
- *  any measured "curve" or "function" or "table" with one independent
- *  variable and scalar dependent variable(s).
- *  It is unclear how to properly generalize the names....  Ahh, packaging.
- *  Perhaps the "table" package?
- *
  *  The context is kept in an 'rt_table' structure, and
  *  the data for one particular sample are kept in an 'rt_tabdata'
  *  structure.
+ *  tabdata.h provides the data structures, 
+ *  librt/tabdata.c provides the routines.
  *
  *  The contents of the spectral sample in val[j] are interpreted
  *  in the interval (wavel[j]..wavel[j+1]).
@@ -86,87 +78,8 @@
  *  $Header$
  */
 
-struct rt_table {
-	long		magic;
-	int		nx;
-	fastf_t		x[1];	/* array of nx+1 wavelengths, dynamically sized */
-};
-#define RT_TABLE_MAGIC	0x53706374
-#define RT_CK_TABLE(_p)	RT_CKMAG(_p, RT_TABLE_MAGIC, "rt_table")
-
-/* Gets an rt_table, with x[] having size _nx+1 */
-#define RT_GET_TABLE(_table, _nx)  { \
-	if( (_nx) < 1 )  rt_bomb("RT_GET_TABLE() _nx < 1\n"); \
-	_table = (struct rt_table *)rt_calloc( 1, \
-		sizeof(struct rt_table) + sizeof(fastf_t)*(_nx), \
-		"struct rt_table" ); \
-	_table->magic = RT_TABLE_MAGIC; \
-	_table->nx = (_nx);  }
-
-
-struct rt_tabdata {
-	long		magic;
-	int		ny;
-	CONST struct rt_table *table;	/* Up pointer to definition of X axis */
-	fastf_t		y[1];		/* array of ny samples, dynamically sized */
-};
-#define RT_TABDATA_MAGIC	0x53736d70
-#define RT_CK_TABDATA(_p)	RT_CKMAG(_p, RT_TABDATA_MAGIC, "rt_tabdata")
-
-#define RT_SIZEOF_TABDATA(_table)	( sizeof(struct rt_tabdata) + \
-			sizeof(fastf_t)*((_table)->nx-1) )
-
-/* Gets an rt_tabdata, with y[] having size _ny */
-#define RT_GET_TABDATA(_data, _table)  { \
-	RT_CK_TABLE(_table);\
-	_data = (struct rt_tabdata *)rt_calloc( 1, \
-		RT_SIZEOF_TABDATA(_table), "struct rt_tabdata" ); \
-	_data->magic = RT_TABDATA_MAGIC; \
-	_data->ny = (_table)->nx; \
-	_data->table = (_table); }
-
 /*
- * *** Table-generic routines
- */
-
-RT_EXTERN( void			rt_ck_table, (CONST struct rt_table *tabp));
-RT_EXTERN( struct rt_table	*rt_table_make_uniform, (int num, double first,
-					double last));
-RT_EXTERN( void			rt_tabdata_add, (struct rt_tabdata *out,
-					CONST struct rt_tabdata *in1,
-					CONST struct rt_tabdata *in2));
-RT_EXTERN( void			rt_tabdata_mul, (struct rt_tabdata *out,
-					CONST struct rt_tabdata *in1,
-					CONST struct rt_tabdata *in2));
-RT_EXTERN( void			rt_tabdata_scale, (struct rt_tabdata *out,
-					CONST struct rt_tabdata *in1,
-					double scale));
-RT_EXTERN( double		rt_tabdata_area1, (CONST struct rt_tabdata *in));
-RT_EXTERN( double		rt_tabdata_area2, (CONST struct rt_tabdata *in));
-RT_EXTERN( fastf_t		rt_table_lin_interp, (CONST struct rt_tabdata *samp,
-					double wl));
-RT_EXTERN( struct rt_tabdata	*rt_tabdata_resample, (
-					CONST struct rt_table *newtable,
-					CONST struct rt_tabdata *olddata));
-RT_EXTERN( int			rt_table_write, (CONST char *filename,
-					CONST struct rt_table *tabp));
-RT_EXTERN( struct rt_table	*rt_table_read, (CONST char *filename));
-RT_EXTERN( int			rt_pr_table_and_tabdata, (CONST char *filename,
-					CONST struct rt_tabdata *data));
-RT_EXTERN( struct rt_tabdata	*rt_read_table_and_tabdata, (
-					CONST char *filename));
-RT_EXTERN( struct rt_tabdata	*rt_tabdata_binary_read, (CONST char *filename,
-					int num,
-					CONST struct rt_table *tabp));
-RT_EXTERN( struct rt_tabdata	*rt_tabdata_malloc_array, (
-					CONST struct rt_table *tabp,
-					int num));
-RT_EXTERN( void			rt_tabdata_copy, (struct rt_tabdata *out,
-					CONST struct rt_tabdata *in));
-
-
-/*
- * *** Spectrum-specific routines
+ * Routines
  */
 RT_EXTERN( void			rt_spect_make_CIE_XYZ, (
 					struct rt_tabdata **x,
