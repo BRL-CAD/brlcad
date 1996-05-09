3cb2dc55cbf3d4dc955202569da1153829b558ea
Lee Butler <iraytrace@users.noreply.github.com>
1996-05-09 03:24:58 +0000

    moved light visibility calculations in from shade.c
    svn:revision:11932
    cvs:account:butler
    cvs:branch:trunk

--- rt/sh_light.c
+++ rt/sh_light.c
@@ -28,6 +28,7 @@ static char RCSlight[] = "@(#)$Header$ (BRL)";
 #include "rtlist.h"
 #include "raytrace.h"
 #include "./material.h"
+#include "./mathtab.h"
 #include "./light.h"
 #include "./rdebug.h"
 
@@ -405,6 +406,37 @@ light_init()
 	return(nlights);
 }
 
+
+/*
+ *			L I G H T _ C L E A N U P
+ *
+ *  Called from view_end().
+ *  Take care of releasing storage for any lights which will not
+ *  be cleaned up by mlib_free():
+ *	implicitly created lights, because they have no associated region, and
+ *	invisible lights, because their region was destroyed.
+ */
+void
+light_cleanup()
+{
+	register struct light_specific *lp, *zaplp;
+
+	if( RT_LIST_UNINITIALIZED( &(LightHead.l ) ) )  {
+		RT_LIST_INIT( &(LightHead.l) );
+		return;
+	}
+	for( RT_LIST_FOR( lp, light_specific, &(LightHead.l) ) )  {
+		RT_CK_LIGHT(lp);
+		if( lp->lt_rp != REGION_NULL && lp->lt_invisible == 0 )  {
+			/* Will be cleaned up by mlib_free() */
+			continue;
+		}
+		zaplp = lp;
+		lp = RT_LIST_PREV( light_specific, &(lp->l) );
+		light_free( (genptr_t)zaplp );
+	}
+}
+/**********************************************************************/
 /* 
  *			L I G H T _ H I T
  *
@@ -427,6 +459,8 @@ light_init()
  *  Would also be nice to return an actual energy level, rather than
  *  a boolean, which could account for distance, etc.
  */
+
+int
 light_hit(ap, PartHeadp)
 struct application *ap;
 struct partition *PartHeadp;
@@ -440,7 +474,10 @@ struct partition *PartHeadp;
 	vect_t	filter_color;
 	int	light_visible;
 
-	/*
+
+	/*XXX Bogus with Air.  We should check to see if it is the same 
+	 * surface.
+	 *
 	 *  Since the light visibility ray started at the surface of a solid,
 	 *  it is likely that the solid will be the first partition on
 	 *  the list, with pt_outhit->hit_dist being roughly zero.
@@ -538,6 +575,7 @@ struct partition *PartHeadp;
 		goto out;
 	}
 
+
 	/*
 	 *  Determine transparency parameters of this object.
 	 *  All we really need here is the opacity information;
@@ -587,6 +625,7 @@ out:
  *  the light, so return light_visible = TRUE.
  */
 /* ARGSUSED */
+int
 light_miss(ap, PartHeadp)
 register struct application *ap;
 struct partition *PartHeadp;
@@ -604,31 +643,123 @@ struct partition *PartHeadp;
 }
 
 /*
- *			L I G H T _ C L E A N U P
- *
- *  Called from view_end().
- *  Take care of releasing storage for any lights which will not
- *  be cleaned up by mlib_free():
- *	implicitly created lights, because they have no associated region, and
- *	invisible lights, because their region was destroyed.
+ *	Determine the visibility of each light source in the scen from a
+ *	particular location.
  */
 void
-light_cleanup()
+light_visibility(ap, swp, have)
+struct application *ap;
+struct shadework *swp;
+int have;
 {
-	register struct light_specific *lp, *zaplp;
+		register struct light_specific *lp;
+		register int	i;
+		register fastf_t *intensity, *tolight;
+		register fastf_t f;
+		struct application sub_ap;
 
-	if( RT_LIST_UNINITIALIZED( &(LightHead.l ) ) )  {
-		RT_LIST_INIT( &(LightHead.l) );
-		return;
-	}
-	for( RT_LIST_FOR( lp, light_specific, &(LightHead.l) ) )  {
-		RT_CK_LIGHT(lp);
-		if( lp->lt_rp != REGION_NULL && lp->lt_invisible == 0 )  {
-			/* Will be cleaned up by mlib_free() */
-			continue;
+		/*
+		 *  Determine light visibility
+		 */
+		i = 0;
+		intensity = swp->sw_intensity;
+		tolight = swp->sw_tolight;
+		for( RT_LIST_FOR( lp, light_specific, &(LightHead.l) ) )  {
+			RT_CK_LIGHT(lp);
+			/* compute the light direction */
+			if( lp->lt_infinite ) {
+				/* Infinite lights are point sources, no fuzzy penumbra */
+				VMOVE( tolight, lp->lt_vec );
+			} else {
+				VSUB2(tolight, lp->lt_pos,
+					swp->sw_hit.hit_point);
+#if 0
+		/*
+		 *  Dither light pos for penumbra by +/- 0.5 light radius;
+		 *  this presently makes a cubical light source distribution.
+		 */
+		f = lp->lt_radius * 0.9;
+		tolight[X] = lp->lt_pos[X] +
+			rand_half(ap->a_resource->re_randptr)*f -
+			swp->sw_hit.hit_point[X];
+		tolight[Y] = lp->lt_pos[Y] +
+			rand_half(ap->a_resource->re_randptr)*f -
+			swp->sw_hit.hit_point[Y];
+		tolight[Z] = lp->lt_pos[Z] +
+			rand_half(ap->a_resource->re_randptr)*f -
+			swp->sw_hit.hit_point[Z];
+#endif
+
+			}
+
+			/*
+			 *  If we have a normal, test against light direction
+			 */
+			if( (have & MFI_NORMAL) && (swp->sw_transmit <= 0) )  {
+				if( VDOT(swp->sw_hit.hit_normal,tolight) < 0 ) {
+					/* backfacing, opaque */
+					swp->sw_visible[i] = (char *)0;
+					goto next;
+				}
+			}
+			VUNITIZE( tolight );
+
+			/*
+			 * See if ray from hit point to light lies within light beam
+			 * Note: this is should always be true for infinite lights!
+			 */
+			if( -VDOT(tolight, lp->lt_aim) < lp->lt_cosangle )  {
+				/* dark (outside of light beam) */
+				swp->sw_visible[i] = (char *)0;
+				goto next;
+			}
+			if( !(lp->lt_shadows) )  {
+				/* "fill light" in beam, don't care about shadows */
+				swp->sw_visible[i] = (char *)lp;
+				VSETALL( intensity, 1 );
+				goto next;
+			}
+
+			/*
+			 *  Fire ray at light source to check for shadowing.
+			 *  (This SHOULD actually return an energy spectrum).
+			 *  Advance start point slightly off surface.
+			 */
+			sub_ap = *ap;			/* struct copy */
+			VMOVE( sub_ap.a_ray.r_dir, tolight );
+			{
+				register fastf_t f;
+				f = ap->a_rt_i->rti_tol.dist;
+				VJOIN1( sub_ap.a_ray.r_pt,
+					swp->sw_hit.hit_point,
+					f, tolight );
+			}
+			sub_ap.a_hit = light_hit;
+			sub_ap.a_miss = light_miss;
+			sub_ap.a_user = -1;		/* sanity */
+			sub_ap.a_uptr = (genptr_t)lp;	/* so we can tell.. */
+			sub_ap.a_level = 0;
+			/* Will need entry & exit pts, for filter glass */
+			sub_ap.a_onehit = 2;
+
+			VSETALL( sub_ap.a_color, 1 );	/* vis intens so far */
+			sub_ap.a_purpose = lp->lt_name;	/* name of light shot at */
+
+			RT_CK_LIGHT((struct light_specific *)(sub_ap.a_uptr));			
+
+
+			if( rt_shootray( &sub_ap ) )  {
+				/* light visible */
+				swp->sw_visible[i] = (char *)lp;
+				VMOVE( intensity, sub_ap.a_color );
+			} else {
+				/* dark (light obscured) */
+				swp->sw_visible[i] = (char *)0;
+			}
+next:
+			/* Advance to next light */
+			i++;
+			intensity += 3;
+			tolight += 3;
 		}
-		zaplp = lp;
-		lp = RT_LIST_PREV( light_specific, &(lp->l) );
-		light_free( (genptr_t)zaplp );
-	}
 }
