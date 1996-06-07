9ae6abd6727e021e195416bf3c1be5b175a4076a
John Anderson <daytonajra@gmail.com>
1996-06-07 16:11:14 +0000

    Added some assembly cut capability.
    svn:revision:11985
    cvs:account:jra
    cvs:branch:trunk

--- pro-engineer/proe-brl.c
+++ pro-engineer/proe-brl.c
@@ -1,19 +1,35 @@
 #include <unistd.h>
+#include <stdio.h>
 #include "prodevelop.h"
+#include "pro_hardware.h"
+#include "pro_wchar_t.h"
+#include "pd_prototype.h"
+#include "prodev_const.h"
+#include "prodev_size.h"
 #include "profiles.h"
 #include "proincludes.h"
+#include "pro_unit.h"
+#include "pro_feattype.h"
+#include "prodev_feat.h"
+
+extern char *tempnam();
 
 extern int errno;
 
 static int quality;
 static int curr_win_id;
-static int win_id=(-1);
+static int win_id;
+
+void Output_object();	
 
 static	wchar_t		  msgfil[PRODEV_NAME_SIZE];
 #define MSGFIL		  pro_str_to_wstr(msgfil,"usermsg.txt")
 
 #define	FILE_COPY_SIZE	2048
+#define	MAX_LINE_LEN	512
 #define DEFAULT_QUALITY 3
+
+#define	V3ARGS(x)	(x)[0], (x)[1], (x)[2]
 
 struct object_list
 {
@@ -71,7 +87,8 @@ char *name;
 	char tmp_file_name[256];
 	wchar_t wtmp_file_name[256];
 	FILE *fd_tmp;
-	char *copy_buf[FILE_COPY_SIZE];
+	char copy_buf[FILE_COPY_SIZE];
+	char line[MAX_LINE_LEN+1];
 	int copy_len;
 	int i,name_len;
 	int c;
@@ -79,6 +96,13 @@ char *name;
 	Pro_object_info obj_info;
 	int status;
 	char *attrib;
+	wchar_t unit_name[PRODEV_NAME_SIZE];
+	double conv_factor;
+	int unit_subtype;
+	int no_of_features;
+	int *feature_ids;
+	int assem_cuts;
+	int feature_mask=0xffffffff;
 
 	/* Create a temporary file to store the render format for this part */
 	tmpnam_return = tempnam( NULL , "proe_brl" );
@@ -135,7 +159,127 @@ char *name;
 	fprintf( fd_out , "solid %s %x\n" , name , obj );
 
 	/* skip the first line in the temporary file (similar to above line, but less info */
-	while( (c=fgetc( fd_tmp )) != '\n' && c != EOF );
+	if( fgets( line, MAX_LINE_LEN, fd_tmp ) == NULL )
+	{
+		fprintf( stderr, "Failed to read tmp file (%s) containing exported part %s\n",
+			tmp_file_name, name );
+		promsg_print( MSGFIL , "USER_EXPORT_FAILED" , tmp_file_name , name );
+		fprintf( fd_out, "endsolid %s\n", name );
+		fclose( fd_tmp );
+		return;
+	}
+
+	while( fgets( line, MAX_LINE_LEN, fd_tmp ) )
+	{
+		if( !strncmp( "endsolid", line, 8 ) )
+		{
+			int output_start=1;
+			int output_count=0;
+
+			/* Add any Assembly Cut info before end of solid */
+
+			no_of_features = prodb_get_feature_ids( obj, feature_mask, &feature_ids );
+			assem_cuts = 0;
+			for( i=0 ; i<no_of_features ; i++ )
+			{
+				int feat_type;
+				Pro_feature feat_info;
+
+				if( (feat_type=prodb_get_feat_type( obj, feature_ids[i] )) == FT_ASSEM_CUT )
+				{
+					char **surfaces;
+					int nsurfs;
+					int j;
+
+					nsurfs = prodb_get_feature_surfaces( obj, feature_ids[i], &surfaces );
+					if( nsurfs < 0 )
+					{
+						fprintf( stderr, "Failed to get Assembly Cut feature surfaces for part %s\n", name );
+						continue;
+					}
+					else if( nsurfs == 0 )
+					{
+						fprintf( stderr, "Assembly Cut in part %s has no surfaces\n", name );
+						continue;
+					}
+					else if( nsurfs != 1 )
+					{
+						fprintf( stderr, "Assembly Cut in part %s is too complex to convert\n", name );
+						fprintf( stderr, "\tYou will probably need to edit %s\n", name );
+						
+					}
+					assem_cuts += nsurfs;
+
+					if( output_start )
+					{
+						output_start = 0;
+						fprintf( fd_out, "  modifiers\n" );
+					}
+
+					for( j=0 ; j<nsurfs ; j++ )
+					{
+						Ptc_surf *surf;
+						Ptc_srfshape *shape;
+
+						if( (surf = prodb_get_surface( surfaces[j] ) ) == NULL )
+						{
+							fprintf( stderr, "Cannot get surface #%d for Assembly Cut in part %s\n", j+1, name );
+							continue;
+						}
+						shape = &surf->srf_shape;
+
+						switch( surf->type )
+						{
+
+							case 34:	/* plane */
+								fprintf( fd_out, "    plane\n" );
+								fprintf( fd_out, "      %12e %12e %12e\n", V3ARGS( shape->plane.origin ) );
+								fprintf( fd_out, "      %12e %12e %12e\n", V3ARGS( shape->plane.e1 ) );
+								fprintf( fd_out, "      %12e %12e %12e\n", V3ARGS( shape->plane.e2 ) );
+								fprintf( fd_out, "      %12e %12e %12e\n", V3ARGS( shape->plane.e3 ) );
+								fprintf( fd_out, "      %12e %12e\n", surf->uv_min[0], surf->uv_min[1] );
+								fprintf( fd_out, "      %12e %12e\n", surf->uv_max[0], surf->uv_max[1] );
+								fprintf( fd_out, "      %d\n", surf->orient );
+								fprintf( fd_out, "    endplane\n" );
+								output_count++;
+								break;
+#if 0
+							case 36:	/* cylinder */
+								fprintf( fd_out, "    cylinder\n" );
+								fprintf( fd_out, "      %12e %12e %12e\n", V3ARGS( shape->cylinder.origin ) );
+								fprintf( fd_out, "      %12e %12e %12e\n", V3ARGS( shape->cylinder.e1 ) );
+								fprintf( fd_out, "      %12e %12e %12e\n", V3ARGS( shape->cylinder.e2 ) );
+								fprintf( fd_out, "      %12e %12e %12e\n", V3ARGS( shape->cylinder.e3 ) );
+								fprintf( fd_out, "      %12e\n", shape->cylinder.radius );
+								fprintf( fd_out, "      %12e %12e\n", surf->uv_min[0], surf->uv_min[1] );
+								fprintf( fd_out, "      %12e %12e\n", surf->uv_max[0], surf->uv_max[1] );
+								fprintf( fd_out, "      %d\n", surf->orient );
+								fprintf( fd_out, "    endcylinder\n" );
+								break;
+#endif
+							default:
+								break;
+						}
+						prodb_rls_surface( surf );
+					}
+				}
+			}
+			if( !output_start )
+				fprintf( fd_out, "  endmodifiers\n" );
+			if( assem_cuts != output_count )
+			{
+				int cuts_not_done;
+
+				cuts_not_done = assem_cuts - output_count;
+				fprintf( stderr, "Part (%s) has %d assembly cuts that will not be reflected in the converted part\n", name, cuts_not_done );
+				fprintf( stderr, "\tYou will probably need to edit this part\n" );
+				promsg_clear();
+				promsg_print( MSGFIL, "ASSEM_CUTS", name, &cuts_not_done );
+			}
+
+		}
+		fprintf( fd_out, "%s", line );
+	}
 
 	copy_len = FILE_COPY_SIZE;
 	copy_len = fread( copy_buf , sizeof( char ) , copy_len , fd_tmp );
@@ -155,6 +299,81 @@ char *name;
 }
 
 void
+Output_assembly( fd_out, obj, name )
+FILE *fd_out;
+char *obj;
+char *name;
+{
+	int i;
+	int memb_id;
+	wchar_t unit_name[PRODEV_NAME_SIZE];
+	double conv_factor;
+	int unit_subtype;
+	int no_of_features;
+	int *feature_ids;
+	int assem_cuts;
+	int feature_mask=0xffffffff;
+
+	no_of_features = prodb_get_feature_ids( obj, feature_mask, &feature_ids );
+	assem_cuts = 0;
+	for( i=0 ; i<no_of_features ; i++ )
+	{
+		int feat_type;
+
+		if( (feat_type=prodb_get_feat_type( obj, feature_ids[i] )) == FT_ASSEM_CUT )
+			assem_cuts++;
+	}
+
+	if( assem_cuts )
+	{
+		fprintf( stderr, "Assembly (%s) has %d assembly cuts that will not be reflected in the converted part\n", name, assem_cuts );
+		fprintf( stderr, "\tYou will probably need to edit this assembly\n" );
+		promsg_clear();
+		promsg_print( MSGFIL, "ASSEM_CUTS", name, &assem_cuts );
+	}
+
+
+	fprintf( fd_out , "assembly %s %x\n" , name , obj );
+
+	memb_id = prodb_first_member( obj );
+	while( memb_id != (-1) )
+	{
+		char memb_name[80];
+		char *memb_obj;
+		Pro_object_info memb_info;
+		double x[3],y[3],z[3],origin[3];
+
+		memb_obj = prodb_member_to_object( obj , memb_id );
+		if( prodb_get_object_info( memb_obj , &memb_info ) )
+		{
+			pro_wstr_to_str( memb_name , memb_info.name );
+			fprintf( fd_out , " member %s %x\n" , memb_name , memb_obj );
+
+			if( prodb_member_transform( obj , memb_id , x , y , z , origin ) )
+			{
+				fprintf( fd_out , " matrix\n" );
+				fprintf( fd_out , " %f %f %f 0.0\n" , x[0] , x[1] , x[2] );
+				fprintf( fd_out , " %f %f %f 0.0\n" , y[0] , y[1] , y[2] );
+				fprintf( fd_out , " %f %f %f 0.0\n" , z[0] , z[1] , z[2] );
+				fprintf( fd_out , " %f %f %f 1.0\n" , origin[0] , origin[1] , origin[2] );
+			}
+		}
+		memb_id = prodb_next_member( obj , memb_id );
+	}
+	fprintf( fd_out , "endassembly %s\n" , name );
+
+	memb_id = prodb_first_member( obj );
+	while( memb_id != (-1) )
+	{
+		char *memb_obj;
+
+		memb_obj = prodb_member_to_object( obj , memb_id );
+		Output_object( fd_out , memb_obj );
+		memb_id = prodb_next_member( obj , memb_id );
+	}
+}
+
+void
 Output_object( fd_out , obj )
 FILE *fd_out;
 char *obj;
@@ -162,6 +381,10 @@ char *obj;
 	Pro_object_info obj_info;
 	char name[80];
 	char type[10];
+	char tmp_str[80];
+	int nlayers;
+	wchar_t **layer_array;
+	int i;
 
 	if( !prodb_get_object_info( obj , &obj_info ) )
 	{
@@ -175,50 +398,33 @@ char *obj;
 	pro_wstr_to_str( name , obj_info.name );
 	pro_wstr_to_str( type , obj_info.type );
 
+#if 0
+	nlayers = prolayer_get_names( obj, &layer_array );
+	for( i=0 ; i<nlayers; i++ )
+	{
+		pro_wstr_to_str( tmp_str, layer_array[i] );
+		printf( "%s is in layer %s\n" , name, tmp_str );
+		free( layer_array[i] );
+	}
+#endif
+
 	if( !strncmp( type , "ASM" , 3 ) )
 	{
-		int memb_id;
-
-		fprintf( fd_out , "assembly %s %x\n" , name , obj );
-		memb_id = prodb_first_member( obj );
-		while( memb_id != (-1) )
-		{
-			char memb_name[80];
-			char *memb_obj;
-			Pro_object_info memb_info;
-			double x[3],y[3],z[3],origin[3];
-
-			memb_obj = prodb_member_to_object( obj , memb_id );
-			if( prodb_get_object_info( memb_obj , &memb_info ) )
-			{
-				pro_wstr_to_str( memb_name , memb_info.name );
-				fprintf( fd_out , " member %s %x\n" , memb_name , memb_obj );
-
-				if( prodb_member_transform( obj , memb_id , x , y , z , origin ) )
-				{
-					fprintf( fd_out , " matrix\n" );
-					fprintf( fd_out , " %f %f %f 0.0\n" , x[0] , x[1] , x[2] );
-					fprintf( fd_out , " %f %f %f 0.0\n" , y[0] , y[1] , y[2] );
-					fprintf( fd_out , " %f %f %f 0.0\n" , z[0] , z[1] , z[2] );
-					fprintf( fd_out , " %f %f %f 1.0\n" , origin[0] , origin[1] , origin[2] );
-				}
-			}
-			memb_id = prodb_next_member( obj , memb_id );
-		}
-		fprintf( fd_out , "endassembly %s\n" , name );
-
-		memb_id = prodb_first_member( obj );
-		while( memb_id != (-1) )
-		{
-			char *memb_obj;
-
-			memb_obj = prodb_member_to_object( obj , memb_id );
-			Output_object( fd_out , memb_obj );
-			memb_id = prodb_next_member( obj , memb_id );
-		}
+		Output_assembly( fd_out, obj, name );
 	}
 	else if( !strncmp( type , "PRT" , 3 ) )
 	{
+#if 0
+		wchar_t material_name[PRODEV_NAME_SIZE];
+
+		if( prodb_get_material_name( obj, material_name ) != PRODEV_NO_ERROR )
+			printf( "Could not get material name for %s\n" , name );
+		else
+		{
+			pro_wstr_to_str( tmp_str, material_name );
+			printf( "\t%s is made of %s\n" , name, tmp_str );
+		}
+#endif
 		Output_part( fd_out , obj , name );
 	}
 	else
@@ -236,9 +442,14 @@ proe_brl()
 	char output_file[PRODEV_NAME_SIZE+4];
 	wchar_t buff[256];
 	int range[2];
+	int unit_subtype;
+	wchar_t unit_name[PRODEV_NAME_SIZE];
+	double conv_factor;
 
 	range[0] = 1;
 	range[1] = 10;
+
+	win_id = (-1);
 
 	curr_win_id = pro_get_current_window();
 	if( curr_win_id == (-1) )
@@ -262,6 +473,14 @@ proe_brl()
 		return( 1 );
 	}
 
+	if( !prodb_get_model_units( obj, LENGTH_UNIT, &unit_subtype, unit_name, &conv_factor ) )
+	{
+		promsg_print( MSGFIL, "NO_UNITS_FOR_MODEL" );
+		conv_factor = 1.0;
+	}
+	else
+		conv_factor *= 25.4;
+
 	pro_wstr_to_str( output_file , obj_info.name );
 	strcat( output_file , ".brl" );
 	promsg_print( MSGFIL , "USER_GET_OUTPUT_FILE_NAME" , output_file );
@@ -281,11 +500,15 @@ proe_brl()
 		return( 1 );
 	}
 
+	fprintf( fd_out, "%g\n", conv_factor );
+
 	Output_object( fd_out , obj );
 
 	pro_set_current_window( curr_win_id );
 	if( win_id != (-1) )
 		pro_close_object_window( win_id );
+
+	pro_refresh_window( -1 );
 
 	fclose( fd_out );
 
