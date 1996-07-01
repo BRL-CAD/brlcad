5920aff69831fb0ac6dd1adaeb80cf5d811529bf
Paul Tanenbaum <pjt@brlcad.org>
1996-07-01 19:22:05 +0000

    1. Now handles multiple idents on command line, including ranges 2. Improved test of whether the application is MGED
    svn:revision:12062
    cvs:account:pjt
    cvs:branch:trunk

--- tclscripts/mged/e_id.tcl
+++ tclscripts/mged/e_id.tcl
@@ -1,9 +1,9 @@
 #
 #				E - I D
 #
-#	A TCL macro for MGED(1) to edit all objects with a specified ident number
+#	A TCL macro for MGED(1) to edit all objects with specified ident numbers
 #
-#	Usage -  'e-id ident'
+#	Usage -  'e-id ident[-ident] ...'
 #	Author - Paul Tanenbaum
 #
 #	Grab the output of MGED's 'whichid' command, and use that to construct
@@ -15,7 +15,7 @@
 #    If any application besides MGED tries to run this script,
 #    print a warning message
 #
-if {[winfo name .] != "MGED"} {
+if {[info exists mged_display] == 0} {
     set f_name [info script]
     puts "Warning: script '$f_name' contains MGED(1)-specific macros"
 }
@@ -23,32 +23,63 @@ if {[winfo name .] != "MGED"} {
 #
 #    The actual macro
 #
-proc e-id {{ident ""}} {
+proc e-id {args} {
     #
-    #	Handle command-line syntax
+    #	Ensure that at least one argument was given
     #
-    if {$ident == ""} {
-	puts "Usage: e-id <ident>\n       \
-		(edit object(s) with the specified ident)"
+    if {$args == ""} {
+	puts "Usage: e-id <idents and/or ident_ranges>\n       \
+		(edit object(s) with the specified idents)"
 	return
+    }
+    #
+    #	Construct a list of all the specified identifiers
+    #
+    set ident_list {}
+    foreach arg $args {
+	if {[regexp {([0-9]+)-([0-9]+)} $arg range lo hi] == 1} {
+	    if {$lo > $hi} {
+		puts "Illegal ident range: '$arg'"
+	    }
+	    for {set i $lo} {$i <= $hi} {incr i} {
+		lappend ident_list $i
+	    }
+	} elseif {[regexp {([0-9]+)} $arg lo] == 1} {
+	    lappend ident_list $lo
+	} else {
+	    puts "Invalid ident specification: '$arg'"
+	}
+    }
+    #
+    #	Sort the specified identifiers, throwing out any duplicates
+    #
+    set ident_list [lsort $ident_list]
+    set uniq_ident_list {}
+    set prev_ident ""
+    foreach ident $ident_list {
+	if {$ident != $prev_ident} {
+	    lappend uniq_ident_list $ident
+	}
+	set prev_ident $ident
     }
     #
     #	Construct a list of the names of the appropriate regions
     #
-    set raw_result [whichid $ident]
-    set index [string first "\n" $raw_result]
-    set reg_list [split [string trim [string range $raw_result $index end]]]
+    set cmd "whichid $uniq_ident_list"
+    set raw_list [split [eval $cmd] \n]
     set result {}
-    foreach element $reg_list {
-	if {$element != {}} {
-	    set result [lappend result $element]
+    foreach element $raw_list {
+	set non_null [expr {$element != {}}]
+	set header [regexp {Region\[s\] with ident [0-9]+:} $element]
+	if {[expr ($non_null && !$header)]} {
+	    lappend result [string trim $element]
 	}
     }
     #
     #	If any regions are appropriate, go ahead and 'e' them
     #
-    if {$reg_list == {}} {
-	puts "There are no objects with ident '$ident'"
+    if {$result == {}} {
+	puts "There are no objects with idents '$args'"
     } else {
 	puts "objects found: $result"
 	eval [concat [list e] $result]
