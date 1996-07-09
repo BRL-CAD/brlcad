615904484e0ad8884f7e5b9c30c20cc8b860244b
Paul Tanenbaum <pjt@brlcad.org>
1996-07-09 14:50:10 +0000

    Added tclscripts to list of directories to make within $BASEDIR
    svn:revision:12086
    cvs:account:pjt
    cvs:branch:trunk

--- setup.sh
+++ setup.sh
@@ -28,7 +28,7 @@ echo "  BINDIR = ${BINDIR},  BASEDIR = ${BASEDIR}"
 
 for LAST in \
 	bin include include/brlcad lib vfont \
-	man man/man1 man/man3 man/man5 etc tcl tk
+	man man/man1 man/man3 man/man5 etc tcl tk tclscripts
 do
 	if test ! -d $BASEDIR/$LAST
 	then
