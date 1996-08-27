240d3e8b3280453a1eba03333e2a6e4bde5dffdf
Michael John Muuss <mike@brlcad.org>
1996-08-27 06:16:40 +0000

    Added rt_vls support
    svn:revision:12356
    cvs:account:mike
    cvs:branch:trunk

--- h/compat4.h
+++ h/compat4.h
@@ -24,10 +24,14 @@
 extern "C" {
 #endif
 
+/* raytrace.h macro replacements */
+#define GETSTRUCT	BU_GETSTRUCT
+#define GETUNION	BU_GETUNION
+#define RT_CKMAG	BU_CKMAG
+
 /* The ordering of these sections should track the order of externs in bu.h */
 
 /* badmagic.c */
-#define RT_CKMAG	BU_CKMAG
 #define rt_badmagic	bu_badmagic
 
 /* bomb.c */
@@ -38,6 +42,7 @@ extern "C" {
 #define RT_UNSETJUMP	BU_UNSETJUMP
 
 /* log.c */
+/* Change variable rt_g.rtg_logindent to a call to bu_log_indent_delta() */
 #define rt_log		bu_log
 #define rt_add_hook	bu_add_hook
 #define rt_delete_hook	bu_delete_hook
@@ -101,6 +106,30 @@ extern "C" {
 #define RT_VLS_CHECK(_vp)	BU_CK_VLS(_vp)
 #define RT_VLS_ADDR		bu_vls_addr
 #define RT_VLS_INIT		bu_vls_init
+#define rt_vls_init		bu_vls_init
+#define rt_vls_vlsinit		bu_vls_vlsinit
+#define rt_vls_addr		bu_vls_addr
+#define rt_vls_strdup		bu_vls_strdup
+#define rt_vls_strgrab		bu_vls_strgrab
+#define rt_vls_extend		bu_vls_extend
+#define rt_vls_setlen		bu_vls_setlen
+#define rt_vls_strlen		bu_vls_strlen
+#define rt_vls_trunc		bu_vls_trunc
+#define rt_vls_trunc2		bu_vls_trunc2
+#define rt_vls_nibble		bu_vls_nibble
+#define rt_vls_free		bu_vls_free
+#define rt_vls_vlsfree		bu_vls_vlsfree
+#define rt_vls_strcpy		bu_vls_strcpy
+#define rt_vls_strncpy		bu_vls_strncpy
+#define rt_vls_strcat		bu_vls_strcat
+#define rt_vls_strncat		bu_vls_strncat
+#define rt_vls_vlscat		bu_vls_vlscat
+#define rt_vls_vlscatzap	bu_vls_vlscatzap
+#define rt_vls_from_argv	bu_vls_from_argv
+#define rt_vls_fwrite		bu_vls_fwrite
+#define rt_vls_gets		bu_vls_gets
+#define rt_vls_putc		bu_vls_putc
+#define rt_vls_printf		bu_vls_printf
 
 #ifdef __cplusplus
 }
