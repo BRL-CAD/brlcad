f2f0936f1821eb7ae5c20955abd3c082ab8d4041
Michael John Muuss <mike@brlcad.org>
1996-10-19 05:38:42 +0000

    Moved combination import routines to db_comb.c
    svn:revision:12728
    cvs:account:mike
    cvs:branch:trunk

--- librt/db_tree.c
+++ librt/db_tree.c
@@ -33,307 +33,13 @@ static char RCSid[] = "@(#)$Header$ (BRL)";
 #endif
 
 #include "machine.h"
+#include "bu.h"
 #include "vmath.h"
-#include "db.h"
+#include "bn.h"
 #include "nmg.h"
 #include "raytrace.h"
 
 #include "./debug.h"
-
-struct tree_list {
-	union tree *tl_tree;
-	int	tl_op;
-};
-#define TREE_LIST_NULL	((struct tree_list *)0)
-
-/* -------------------------------------------------- */
-
-
-RT_EXTERN( union tree *db_mkbool_tree , (struct tree_list *tree_list , int howfar ) );
-RT_EXTERN( union tree *db_mkgift_tree , (struct tree_list *tree_list , int howfar , struct db_tree_state *tsp ) );
-
-/*
- *			R T _ C O M B _ V 4 _ I M P O R T
- *
- *  Import a combination record from a V4 database into internal form.
- */
-int
-rt_comb_v4_import( ip, ep, matrix )
-struct rt_db_internal		*ip;
-CONST struct bu_external	*ep;
-CONST matp_t			matrix;		/* NULL if identity */
-{
-	union record		*rp;
-	struct tree_list	*tree_list;
-	union tree		*tree;
-	struct rt_comb_internal	*comb;
-	int			i,j;
-	int			node_count;
-
-	BU_CK_EXTERNAL( ep );
-	rp = (union record *)ep->ext_buf;
-
-	if( rp[0].u_id != ID_COMB )
-	{
-		bu_log( "rt_comb_v4_import: Attempt to import a non-combination\n" );
-		return( -1 );
-	}
-
-	/* Compute how many granules of MEMBER records follow */
-	node_count = ep->ext_nbytes/sizeof( union record ) - 1;
-
-	if( node_count )
-		tree_list = (struct tree_list *)rt_calloc( node_count , sizeof( struct tree_list ) , "tree_list" );
-	else
-		tree_list = (struct tree_list *)NULL;
-
-	for( j=0 ; j<node_count ; j++ )
-	{
-		if( rp[j+1].u_id != ID_MEMB )
-		{
-			rt_free( (char *)tree_list , "rt_comb_v4_import: tree_list" );
-			bu_log( "rt_comb_v4_import(): granule in external buffer is not ID_MEMB, id=%d\n", rp[j+1].u_id );
-			return( -1 );
-		}
-
-		switch( rp[j+1].M.m_relation )
-		{
-			case '+':
-				tree_list[j].tl_op = OP_INTERSECT;
-				break;
-			case '-':
-				tree_list[j].tl_op = OP_SUBTRACT;
-				break;
-			default:
-				bu_log("rt_comb_v4_import() unknown op=x%x, assuming UNION\n", rp[j+1].M.m_relation );
-				/* Fall through */
-			case 'u':
-				tree_list[j].tl_op = OP_UNION;
-				break;
-		}
-		/* Build leaf node for in-memory tree */
-		{
-			union tree		*tp;
-			struct tree_db_leaf	*mi;
-			mat_t			diskmat;
-			char			namebuf[NAMESIZE+2];
-
-			BU_GETUNION( tp, tree );
-			tree_list[j].tl_tree = tp;
-			tp->tr_l.magic = RT_TREE_MAGIC;
-			tp->tr_l.tl_op = OP_DB_LEAF;
-			strncpy( namebuf, rp[j+1].M.m_instname, NAMESIZE );
-			namebuf[NAMESIZE] = '\0';	/* ensure null term */
-			tp->tr_l.tl_name = bu_strdup( namebuf );
-
-			/* See if disk record is identity matrix */
-			rt_mat_dbmat( diskmat, rp[j+1].M.m_mat );
-			if( mat_is_identity( diskmat ) )  {
-				if( matrix == NULL )  {
-					tp->tr_l.tl_mat = NULL;	/* identity */
-				} else {
-					tp->tr_l.tl_mat = mat_dup( matrix );
-				}
-			} else {
-				if( matrix == NULL )  {
-					tp->tr_l.tl_mat = mat_dup( diskmat );
-				} else {
-					mat_t	prod;
-					mat_mul( prod, matrix, diskmat );
-					tp->tr_l.tl_mat = mat_dup( prod );
-				}
-			}
-/* bu_log("M_name=%s, matp=x%x\n", tp->tr_l.tl_name, tp->tr_l.tl_mat ); */
-		}
-	}
-	if( node_count )
-		tree = db_mkgift_tree( tree_list, node_count, (struct db_tree_state *)NULL );
-	else
-		tree = (union tree *)NULL;
-
-	RT_INIT_DB_INTERNAL( ip );
-	ip->idb_type = ID_COMBINATION;
-	comb = (struct rt_comb_internal *)rt_malloc( sizeof( struct rt_comb_internal ) , "rt_comb_v4_import: rt_comb_internal" );
-	ip->idb_ptr = (genptr_t)comb;
-	comb->magic = RT_COMB_MAGIC;
-	bu_vls_init( &comb->shader_name );
-	bu_vls_init( &comb->shader_param );
-	bu_vls_init( &comb->material );
-	comb->tree = tree;
-	comb->region_flag = (rp[0].c.c_flags == 'R');
-
-	comb->region_id = rp[0].c.c_regionid;
-	comb->aircode = rp[0].c.c_aircode;
-	comb->GIFTmater = rp[0].c.c_material;
-	comb->los = rp[0].c.c_los;
-
-	comb->rgb_valid = rp[0].c.c_override;
-	comb->rgb[0] = rp[0].c.c_rgb[0];
-	comb->rgb[1] = rp[0].c.c_rgb[1];
-	comb->rgb[2] = rp[0].c.c_rgb[2];
-	bu_vls_strncpy( &comb->shader_name , rp[0].c.c_matname, 32 );
-	bu_vls_strncpy( &comb->shader_param , rp[0].c.c_matparm, 60 );
-	/* XXX Separate flags for color inherit, shader inherit, (new) material inherit? */
-	/* XXX cf: ma_cinherit, ma_minherit */
-	comb->inherit = rp[0].c.c_inherit;
-	/* Automatic material table lookup here? */
-	bu_vls_printf( &comb->material, "gift%d", comb->GIFTmater );
-
-	return( 0 );
-}
-
-/*
- *			R T _ C O M B _ I F R E E
- *
- *  Free the storage associated with the rt_db_internal version of this combination.
- */
-void
-rt_comb_ifree( ip )
-struct rt_db_internal	*ip;
-{
-	register struct rt_comb_internal	*comb;
-
-	RT_CK_DB_INTERNAL(ip);
-	comb = (struct rt_comb_internal *)ip->idb_ptr;
-	RT_CK_COMB(comb);
-
-	/* If tree hasn't been stolen, release it */
-	if(comb->tree) db_free_tree( comb->tree );
-	comb->tree = NULL;
-
-	bu_vls_free( &comb->shader_name );
-	bu_vls_free( &comb->shader_param );
-	bu_vls_free( &comb->material );
-
-	comb->magic = 0;			/* sanity */
-	rt_free( (char *)comb, "comb ifree" );
-	ip->idb_ptr = GENPTR_NULL;	/* sanity */
-}
-
-/*
- *			R T _ G E T _ C O M B
- *
- *  A convenience wrapper to retrive a combination from the database
- *  into internal form.
- *
- *  Returns -
- *	-1	FAIL
- *	 0	OK
- */
-int
-rt_get_comb( ip, dp, matp, dbip )
-struct rt_db_internal	*ip;
-CONST struct directory	*dp;
-CONST mat_t		*matp;
-CONST struct db_i	*dbip;
-{
-	struct bu_external	ext;
-
-	RT_CK_DIR(dp);
-	RT_CHECK_DBI( dbip );
-
-	/* Load the entire combination into contiguous memory */
-	BU_INIT_EXTERNAL( &ext );
-	if( db_get_external( &ext, dp, dbip ) < 0 )
-		return -1;
-
-	/* Switch out to right routine, based on database version */
-	if( rt_comb_v4_import( ip, &ext, matp ) < 0 )
-		return -1;
-
-	db_free_external( &ext );
-	return 0;
-}
-
-
-/* -------------------- */
-/* Some export support routines */
-
-/*
- *			D B _ C K _ L E F T _ H E A V Y _ T R E E
- *
- *  Support routine for db_ck_v4gift_tree().
- *  Ensure that the tree below 'tp' is left-heavy, i.e. that there are
- *  nothing but solids on the right side of any binary operations.
- *
- *  Returns -
- *	-1	ERROR
- *	 0	OK
- */
-int
-db_ck_left_heavy_tree( tp, no_unions )
-CONST union tree	*tp;
-int			no_unions;
-{
-	RT_CK_TREE(tp);
-	switch( tp->tr_op )  {
-
-	case OP_DB_LEAF:
-		break;
-
-	case OP_UNION:
-		if( no_unions )  return -1;
-		/* else fall through */
-	case OP_INTERSECT:
-	case OP_SUBTRACT:
-	case OP_XOR:
-		if( tp->tr_b.tb_right->tr_op != OP_DB_LEAF )
-			return -1;
-		return db_ck_left_heavy_tree( tp->tr_b.tb_left, no_unions );
-
-	default:
-		bu_log("db_ck_left_heavy_tree: bad op %d\n", tp->tr_op);
-		rt_bomb("db_ck_left_heavy_tree\n");
-	}
-	return 0;
-}
-
-
-/*
- *			D B _ C K _ V 4 G I F T _ T R E E
- *
- *  Look a gift-tree in the mouth.
- *  Ensure that this boolean tree conforms to the GIFT convention that
- *  union operations must bind the loosest.
- *  There are two stages to this check:
- *  1)  Ensure that if unions are present they are all at the root of tree,
- *  2)  Ensure non-union children of union nodes are all left-heavy
- *      (nothing but solid nodes permitted on rhs of binary operators).
- *
- *  Returns -
- *	-1	ERROR
- *	 0	OK
- */
-int
-db_ck_v4gift_tree( tp )
-CONST union tree	*tp;
-{
-	RT_CK_TREE(tp);
-	switch( tp->tr_op )  {
-
-	case OP_DB_LEAF:
-		break;
-
-	case OP_UNION:
-		if( db_ck_v4gift_tree( tp->tr_b.tb_left ) < 0 )
-			return -1;
-		return db_ck_v4gift_tree( tp->tr_b.tb_right );
-
-	case OP_INTERSECT:
-	case OP_SUBTRACT:
-	case OP_XOR:
-		return db_ck_left_heavy_tree( tp, 1 );
-
-	default:
-		bu_log("db_ck_v4gift_tree: bad op %d\n", tp->tr_op);
-		rt_bomb("db_ck_v4gift_tree\n");
-	}
-	return 0;
-}
-
-
-
-/* -------------------------------------------------- */
 
 /*
  *			D B _ N E W _ C O M B I N E D _ T R E E _ S T A T E
@@ -458,7 +164,8 @@ register CONST struct rt_comb_internal	*comb;
 				rt_free(sofar, "path string");
 			}
 			/* Just quietly ignore it -- it's being subtracted off */
-		} else if( tsp->ts_mater.ma_cinherit == DB_INH_LOWER )  {
+		} else if( tsp->ts_mater.ma_cinherit == 0 )  {
+			/* DB_INH_LOWER -- lower nodes in tree override */
 			tsp->ts_mater.ma_override = 1;
 			tsp->ts_mater.ma_color[0] =
 				(((double)(comb->rgb[0]))*bn_inv255);
@@ -480,7 +187,8 @@ register CONST struct rt_comb_internal	*comb;
 				rt_free(sofar, "path string");
 			}
 			/* Just quietly ignore it -- it's being subtracted off */
-		} else if( tsp->ts_mater.ma_minherit == DB_INH_LOWER )  {
+		} else if( tsp->ts_mater.ma_minherit == 0 )  {
+			/* DB_INH_LOWER -- lower nodes in tree override */
 			/* XXX ts_mater should become a bu_vls */
 			strncpy( tsp->ts_mater.ma_matname, bu_vls_addr(&comb->shader_name), sizeof(tsp->ts_mater.ma_matname) );
 			strncpy( tsp->ts_mater.ma_matparm, bu_vls_addr(&comb->shader_param), sizeof(tsp->ts_mater.ma_matparm) );
@@ -509,99 +217,6 @@ register CONST struct rt_comb_internal	*comb;
 		}
 	}
 	return(0);	/* Success */
-}
-
-/*
- *			D B _ A P P L Y _ S T A T E _ F R O M _ M E M B
- *
- *  Updates state via *tsp, pushes member's directory entry on *pathp.
- *  (Caller is responsible for popping it).
- *
- *  Returns -
- *	-1	failure
- *	 0	success, member pushed on path
- */
-int
-db_apply_state_from_memb( tsp, pathp, mp )
-struct db_tree_state	*tsp;
-struct db_full_path	*pathp;
-CONST struct member	*mp;
-{
-	register struct directory *mdp;
-	mat_t			xmat;
-	mat_t			old_xlate;
-	register struct animate *anp;
-	char			namebuf[NAMESIZE+2];
-
-	if( mp->m_id != ID_MEMB )  {
-		char	*sofar = db_path_to_string(pathp);
-		bu_log("db_apply_state_from_memb:  defective member rec in '%s'\n", sofar);
-		rt_free(sofar, "path string");
-		return(-1);
-	}
-
-	/* Trim m_instname, and add to the path */
-	strncpy( namebuf, mp->m_instname, NAMESIZE );
-	namebuf[NAMESIZE] = '\0';
-	if( (mdp = db_lookup( tsp->ts_dbip, namebuf, LOOKUP_NOISY )) == DIR_NULL )
-		return(-1);
-
-	db_add_node_to_full_path( pathp, mdp );
-
-	mat_copy( old_xlate, tsp->ts_mat );
-
-	/* convert matrix to fastf_t from disk format */
-	rt_mat_dbmat( xmat, mp->m_mat );
-
-	/* Check here for animation to apply */
-	if ((mdp->d_animate != ANIM_NULL) && (rt_g.debug & DEBUG_ANIM)) {
-		char	*sofar = db_path_to_string(pathp);
-		bu_log("Animate %s with...\n", sofar);
-		rt_free(sofar, "path string");
-	}
-	/*
-	 *  For each of the animations attached to the mentioned object,
-	 *  see if the current accumulated path matches the path
-	 *  specified in the animation.
-	 *  Comparison is performed right-to-left (from leafward to rootward).
-	 */
-	for( anp = mdp->d_animate; anp != ANIM_NULL; anp = anp->an_forw ) {
-		register int i;
-		register int j = pathp->fp_len-1;
-		register int anim_flag;
-		
-		RT_CK_ANIMATE(anp);
-		i = anp->an_path.fp_len-1;
-		anim_flag = 1;
-
-		if (rt_g.debug & DEBUG_ANIM) {
-			char	*str;
-
-			str = db_path_to_string( &(anp->an_path) );
-			bu_log( "\t%s\t", str );
-			rt_free( str, "path string" );
-			bu_log("an_path.fp_len-1:%d  pathp->fp_len-1:%d\n",
-				i, j);
-
-		}
-		for( ; i>=0 && j>=0; i--, j-- )  {
-			if( anp->an_path.fp_names[i] != pathp->fp_names[j] ) {
-				if (rt_g.debug & DEBUG_ANIM) {
-					bu_log("%s != %s\n",
-					     anp->an_path.fp_names[i]->d_namep,
-					     pathp->fp_names[j]->d_namep);
-				}
-				anim_flag = 0;
-				break;
-			}
-		}
-		/* Perhaps tsp->ts_mater should be just tsp someday? */
-		if (anim_flag)
-			db_do_anim( anp, old_xlate, xmat, &(tsp->ts_mater) );
-	}
-
-	mat_mul(tsp->ts_mat, old_xlate, xmat);
-	return(0);		/* Success */
 }
 
 /*
@@ -970,131 +585,6 @@ fail:
 
 
 /*
- *			D B _ M K B O O L _ T R E E
- *
- *  Given a tree_list array, build a tree of "union tree" nodes
- *  appropriately connected together.  Every element of the
- *  tree_list array used is replaced with a TREE_NULL.
- *  Elements which are already TREE_NULL are ignored.
- *  Returns a pointer to the top of the tree.
- */
-HIDDEN union tree *
-db_mkbool_tree( tree_list, howfar )
-struct tree_list *tree_list;
-int		howfar;
-{
-	register struct tree_list *tlp;
-	register int		i;
-	register struct tree_list *first_tlp = (struct tree_list *)0;
-	register union tree	*xtp;
-	register union tree	*curtree;
-	register int		inuse;
-
-	if( howfar <= 0 )
-		return(TREE_NULL);
-
-	/* Count number of non-null sub-trees to do */
-	for( i=howfar, inuse=0, tlp=tree_list; i>0; i--, tlp++ )  {
-		if( tlp->tl_tree == TREE_NULL )
-			continue;
-		if( inuse++ == 0 )
-			first_tlp = tlp;
-	}
-
-	/* Handle trivial cases */
-	if( inuse <= 0 )
-		return(TREE_NULL);
-	if( inuse == 1 )  {
-		curtree = first_tlp->tl_tree;
-		first_tlp->tl_tree = TREE_NULL;
-		return( curtree );
-	}
-
-	if( first_tlp->tl_op != OP_UNION )  {
-		first_tlp->tl_op = OP_UNION;	/* Fix it */
-		if( rt_g.debug & DEBUG_TREEWALK )  {
-			bu_log("db_mkbool_tree() WARNING: non-union (%c) first operation ignored\n",
-				first_tlp->tl_op );
-		}
-	}
-
-	curtree = first_tlp->tl_tree;
-	first_tlp->tl_tree = TREE_NULL;
-	tlp=first_tlp+1;
-	for( i=howfar-(tlp-tree_list); i>0; i--, tlp++ )  {
-		if( tlp->tl_tree == TREE_NULL )
-			continue;
-
-		BU_GETUNION( xtp, tree );
-		xtp->magic = RT_TREE_MAGIC;
-		xtp->tr_b.tb_left = curtree;
-		xtp->tr_b.tb_right = tlp->tl_tree;
-		xtp->tr_b.tb_regionp = (struct region *)0;
-		xtp->tr_op = tlp->tl_op;
-		curtree = xtp;
-		tlp->tl_tree = TREE_NULL;	/* empty the input slot */
-	}
-	return(curtree);
-}
-
-/*
- *			D B _ M K G I F T _ T R E E
- */
-HIDDEN union tree *
-db_mkgift_tree( trees, subtreecount, tsp )
-struct tree_list	*trees;
-int			subtreecount;
-struct db_tree_state	*tsp;
-{
-	register struct tree_list *tstart;
-	register struct tree_list *tnext;
-	union tree		*curtree;
-	int	i;
-	int	j;
-
-	/*
-	 * This is how GIFT interpreted equations, so it is duplicated here.
-	 * Any expressions between UNIONs are evaluated first.  For example:
-	 *		A - B - C u D - E - F
-	 * becomes	(A - B - C) u (D - E - F)
-	 * so first do the parenthesised parts, and then go
-	 * back and glue the unions together.
-	 * As always, unions are the downfall of free enterprise!
-	 */
-	tstart = trees;
-	tnext = trees+1;
-	for( i=subtreecount-1; i>=0; i--, tnext++ )  {
-		/* If we went off end, or hit a union, do it */
-		if( i>0 && tnext->tl_op != OP_UNION )
-			continue;
-		if( (j = tnext-tstart) <= 0 )
-			continue;
-		curtree = db_mkbool_tree( tstart, j );
-		/* db_mkbool_tree() has side effect of zapping tree array,
-		 * so build new first node in array.
-		 */
-		tstart->tl_op = OP_UNION;
-		tstart->tl_tree = curtree;
-
-		if(rt_g.debug&DEBUG_TREEWALK)  {
-			bu_log("db_mkgift_tree() intermediate term:\n");
-			rt_pr_tree(tstart->tl_tree, 0);
-		}
-
-		/* tstart here at union */
-		tstart = tnext;
-	}
-
-final:
-	curtree = db_mkbool_tree( trees, subtreecount );
-	if(rt_g.debug&DEBUG_TREEWALK)  {
-		bu_log("db_mkgift_tree() returns:\n");
-		rt_pr_tree(curtree, 0);
-	}
-	return( curtree );
-}
-
-/*
  *			D B _ R E C U R S E _ S U B T R E E
  *
  *  Helper routine for db_recurse()
@@ -1176,8 +666,6 @@ struct combined_tree_state	**region_start_statepp;
 	struct bu_external	ext;
 	struct rt_db_internal	intern;
 	int			i;
-	struct tree_list	*tlp;		/* cur elem of trees[] */
-	struct tree_list	*trees = TREE_LIST_NULL;	/* array */
 	union tree		*curtree = TREE_NULL;
 
 	RT_CHECK_DBI( tsp->ts_dbip );
@@ -1368,7 +856,6 @@ region_end:
 out:
 	if( intern.idb_ptr )  rt_comb_ifree( &intern );
 	db_free_external( &ext );
-	if( trees )  rt_free( (char *)trees, "tree_list array" );
 	if(rt_g.debug&DEBUG_TREEWALK)  {
 		char	*sofar = db_path_to_string(pathp);
 		bu_log("db_recurse() return curtree=x%x, pathp='%s', *statepp=x%x\n",
