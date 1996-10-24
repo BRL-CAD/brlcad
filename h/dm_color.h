6d9568251dd6c1f60ea4469d4ea28adacad22bb6
Robert Parker <unacknowledged@users.noreply.github.com>
1996-10-24 22:14:01 +0000

    LIBDM's colormap utilities include file
    svn:revision:12750
    cvs:account:bparker
    cvs:branch:trunk

---
+++ h/dm_color.h
@@ -0,0 +1,7 @@
+#ifndef SEEN_DM_COLOR
+#define SEEN_DM_COLOR
+
+extern unsigned long dm_get_pixel();
+extern void dm_copy_default_colors();
+extern void dm_allocate_color_cube();
+#endif /* SEEN_DM_COLOR */
