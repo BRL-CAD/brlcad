de17066974adb9d738d9d1a3873ce3f6dbb776c9
Lee Butler <iraytrace@users.noreply.github.com>
1997-01-14 05:42:08 +0000

    started working on mist-above-terrain function tmist_render()
    svn:revision:13143
    cvs:account:butler
    cvs:branch:trunk

--- rt/sh_air.c
+++ rt/sh_air.c
@@ -17,30 +17,28 @@
 #define air_MAGIC 0x41697200	/* "Air" */
 struct air_specific {
 	long	magic;
-	double	d_p_m;	/* density per unit meter */
+	double	d_p_mm;	/* density per unit millimeter (specified in m)*/
 	double	scale;
 	double	delta;
-	double	color[3];
-	int	noise_type;
-	double	octaves;
-	double	lacunarity;
+	char	*name;	/* name of "ground" object for emist_terrain_render */
 };
 #define CK_air_SP(_p) RT_CKMAG(_p, air_MAGIC, "air_specific")
 
 static struct air_specific air_defaults = {
 	air_MAGIC,
-	.1,		/* d_p_m */	
+	.1,		/* d_p_mm */	
 	.01,		/* scale */
 	0.0,		/* delta */
-	{ .5, .5, .625 }/* color */
 	};
 
 #define SHDR_NULL	((struct air_specific *)0)
 #define SHDR_O(m)	offsetof(struct air_specific, m)
 #define SHDR_AO(m)	offsetofarray(struct air_specific, m)
 
+static void dpm_hook();
+
 struct bu_structparse air_parse[] = {
-	{"%f",  1, "dpm",		SHDR_O(d_p_m),		FUNC_NULL },
+	{"%f",  1, "dpm",		SHDR_O(d_p_mm),		dpm_hook },
 	{"%f",  1, "scale",		SHDR_O(scale),		FUNC_NULL },
 	{"%f",  1, "s",			SHDR_O(scale),		FUNC_NULL },
 	{"%f",  1, "delta",		SHDR_O(delta),		FUNC_NULL },
@@ -48,7 +46,7 @@ struct bu_structparse air_parse[] = {
 	{"",	0, (char *)0,		0,			FUNC_NULL }
 };
 
-HIDDEN int	air_setup(), airtest_render(), air_render(), emist_render();
+HIDDEN int	air_setup(), airtest_render(), air_render(), emist_render(), tmist_render();
 HIDDEN void	air_print(), air_free();
 
 struct mfuncs air_mfuncs[] = {
@@ -64,11 +62,24 @@ struct mfuncs air_mfuncs[] = {
 	{"emist",	0,		0,		MFI_HIT, MFF_PROC,
 	air_setup,	emist_render,	air_print,	air_free },
 
+	{"tmist",	0,		0,		MFI_HIT, MFF_PROC,
+	air_setup,	tmist_render,	air_print,	air_free },
+
 	{(char *)0,	0,		0,		0,	0,
 	0,		0,		0,		0 }
 };
-
-
+static void 
+dpm_hook(sdp, name, base, value)
+register CONST struct bu_structparse	*sdp;	/* structure description */
+register CONST char			*name;	/* struct member name */
+char					*base;	/* begining of structure */
+CONST char				*value;	/* string containing value */
+{
+#define meters_to_millimeters 0.001
+	struct air_specific *air_sp = (struct air_specific *)base;
+	
+	air_sp->d_p_mm *= meters_to_millimeters;
+}
 /*	A I R _ S E T U P
  *
  *	This routine is called (at prep time)
@@ -202,12 +213,6 @@ char	*dp;
 			V3ARGS(ap->a_ray.r_pt));
 	}
 
-	/* XXX
-	 * We can't trust the reflect/refract support in rt to do the right
-	 * thing so we have to take over here.
-	 */
-
-
 	/* Beer's Law Homogeneous Fog */
 
 	/* get the path length right */
@@ -216,8 +221,8 @@ char	*dp;
 	else
 		dist = (pp->pt_outhit->hit_dist - pp->pt_inhit->hit_dist);
 
-	/* tau = optical path depth = density per m * distance */
-	tau = (air_sp->d_p_m*0.001 ) * dist;
+	/* tau = optical path depth = density per mm * distance in mm */
+	tau = air_sp->d_p_mm * dist;
 
 	/* transmission = e^(-tau) */
 	swp->sw_transmit = exp(-tau);
@@ -229,23 +234,119 @@ char	*dp;
 	 * the "color of the air".
 	 */
 
-#if 0
-	VMOVE(swp->sw_color, air_sp->color);
-	VMOVE(swp->sw_basecolor, air_sp->color);
-#endif
-
 	if( swp->sw_reflect > 0 || swp->sw_transmit > 0 )
 		(void)rr_render( ap, pp, swp );
 
 	if( rdebug&RDEBUG_SHADE)
-		rt_log("air o dist:%gmm tau:%g transmit:%g color(%g %g %g)\n",
-			dist, tau, swp->sw_transmit, V3ARGS(swp->sw_color) );
+		rt_log("air o dist:%gmm tau:%g transmit:%g\n",
+			dist, tau, swp->sw_transmit);
 
 	return(1);
 }
 
+int
+tmist_hit(ap, PartHeadp)
+register struct application *ap;
+struct partition *PartHeadp;
+{
+	/* go looking for the object named in
+	 * ((struct air_specific *)ap->a_uptr)->name
+	 * in the partition list,
+	 * set ap->a_dist to distance to that object
+	 */
+}
+int
+tmist_miss( ap )
+register struct application *ap;
+{
+	/* we missed?!  This is bogus!
+	 * but set ap->a_dist to something big
+	 */
+}
+
+/*
+ *	T M I S T _ R E N D E R
+ *
+ * Use height above named terrain object
+ *
+ *
+ *	This is called (from viewshade() in shade.c)
+ *	once for each hit point to be shaded.
+ */
+int
+tmist_render( ap, pp, swp, dp )
+struct application	*ap;
+struct partition	*pp;
+struct shadework	*swp;
+char	*dp;
+{
+	register struct air_specific *air_sp =
+		(struct air_specific *)dp;
+	point_t in_pt, out_pt;
+	vect_t vapor_path;
+	fastf_t in_altitude;
+	fastf_t out_altitude;
+	fastf_t dist;
+	fastf_t step_dist;
+	fastf_t tau;
+	fastf_t dt;
+	struct application my_ap;
+	long meters;
+
+	RT_AP_CHECK(ap);
+	RT_CHECK_PT(pp);
+	CK_air_SP(air_sp);
+
+	/* Get entry point */
+	if (pp->pt_inhit->hit_dist < 0.0) {
+		VMOVE(in_pt, ap->a_ray.r_pt);
+	} else {
+		VJOIN1(in_pt, ap->a_ray.r_pt,
+			pp->pt_inhit->hit_dist, ap->a_ray.r_dir);
+	}
+
+	/* Get exit point */
+	VJOIN1(out_pt,
+		ap->a_ray.r_pt, pp->pt_outhit->hit_dist, ap->a_ray.r_dir);
+
+	VSUB2(vapor_path, out_pt, in_pt);
+
+	dist = MAGNITUDE(vapor_path);
+	meters = (long)dist;
+	meters = meters >> 3;
+
+	if (meters < 1) step_dist = dist;
+	else step_dist = dist / (fastf_t)meters;
+
+	for (dt=0.0 ; dt <= dist ; dt += step_dist) {
+		memcpy(my_ap, ap, sizeof(struct application));
+		VJOIN1(my_ap.a_ray.r_pt, in_pt, dt, my_ap.a_ray.r_dir);
+		VSET(my_ap.a_ray.r_dir, 0.0, 0.0, -1.0);
+		my_ap.a_hit = tmist_hit;
+		my_ap.a_miss = tmist_miss;
+		my_ap.a_onehit = 0;
+		my_ap.a_uptr = air_sp;
+		rt_shootray( &my_ap );
+
+		/* XXX check my_ap.a_dist for distance to ground */
+
+		/* XXX compute optical density at this altitude */
+
+		/* XXX integrate in the effects of this meter of air */
+
+	}
 
 
+	swp->sw_transmit = exp(-tau);
+
+	if (swp->sw_transmit > 1.0) swp->sw_transmit = 1.0;
+	else if (swp->sw_transmit < 0.0) swp->sw_transmit = 0.0;
+
+	if( rdebug&RDEBUG_SHADE)
+		rt_log("tmist transmit = %g\n", swp->sw_transmit);
+
+	return(1);
+}
 /*
  *	E M I S T _ R E N D E R
  *
@@ -256,13 +357,13 @@ char	*dp;
  * Zo = elevation at ray start
  * Ze = elevation at ray end
  * Zd = Z component of normalized ray vector 
- * d_p_m = overall fog density
+ * d_p_mm = overall fog density
  * B = density falloff with altitude
  *
  *
  *	delta = height at which fog starts
  *	scale = stretches exponential decay zone
- *	d_p_m = maximum density @ ground
+ *	d_p_mm = maximum density @ ground
  *
  *	This is called (from viewshade() in shade.c)
  *	once for each hit point to be shaded.
@@ -303,9 +404,9 @@ char	*dp;
 	Zd = ap->a_ray.r_dir[Z];
 
 	if ( NEAR_ZERO( Zd, SQRT_SMALL_FASTF ) )
-		tau = (air_sp->d_p_m*0.001) * te * exp( -Zo);
+		tau = air_sp->d_p_mm * te * exp( -Zo);
 	else
-		tau = (( (air_sp->d_p_m*0.001) * te) /  Zd) * ( exp(-Zo) - exp(-Ze) );
+		tau = (( air_sp->d_p_mm * te) /  Zd) * ( exp(-Zo) - exp(-Ze) );
 
 /*	XXX future
 	tau *= noise_fbm(pt);
@@ -331,13 +432,13 @@ char	*dp;
  * Zo = elevation at ray start
  * Ze = elevation at ray end
  * Zd = Z component of normalized ray vector 
- * d_p_m = overall fog density
+ * d_p_mm = overall fog density
  * B = density falloff with altitude
  *
  *
  *	delta = height at which fog starts
  *	scale = stretches exponential decay zone
- *	d_p_m = maximum density @ ground
+ *	d_p_mm = maximum density @ ground
  *
  *	This is called (from viewshade() in shade.c)
  *	once for each hit point to be shaded.
@@ -356,6 +457,7 @@ char	*dp;
 	double dist, delta;
 	double tau;
 	double Zo, Ze, Zd, te;
+	struct application my_ap;
 
 	RT_AP_CHECK(ap);
 	RT_CHECK_PT(pp);
