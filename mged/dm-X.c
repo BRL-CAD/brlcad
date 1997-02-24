575de1ec15e71267a6b5104a070e870539bf4b71
Robert Parker <unacknowledged@users.noreply.github.com>
1997-02-24 22:08:43 +0000

    mods to X_doevent
    .,
    
    svn:revision:13353
    cvs:account:bparker
    cvs:branch:trunk

--- mged/dm-X.c
+++ mged/dm-X.c
@@ -119,7 +119,7 @@ char *argv[];
     return TCL_ERROR;
 
   dmp->dm_eventHandler = X_doevent;
-  curr_dm_list->s_info->opp = &pathName;
+  curr_dm_list->s_info->opp = &tkName;
   Tk_CreateGenericHandler(X_doevent, (ClientData)DM_TYPE_X);
   X_configure_window_shape(dmp);
 
@@ -195,7 +195,9 @@ XEvent *eventPtr;
       else if(XdoMotion)
 	/* trackball not active so do the regular thing */
 	/* Constant tracking (e.g. illuminate mode) bound to M mouse */
-	bu_vls_printf( &cmd, "M 0 %d %d\n", Xx_TO_GED(dmp, mx), Xy_TO_GED(dmp, my));
+	bu_vls_printf( &cmd, "M 0 %d %d\n",
+		       (int)(Xx_TO_GED(dmp, mx) / dmp->dm_aspect),
+		       Xy_TO_GED(dmp, my));
       else
 	goto end;
 
@@ -208,11 +210,15 @@ XEvent *eventPtr;
     case ALT_MOUSE_MODE_TRANSLATE:
       if(EDIT_TRAN && mged_variables.edit){
 	vect_t view_pos;
-
-	view_pos[X] = (mx/(fastf_t)((struct x_vars *)dmp->dm_vars)->width
+#if 0
+	view_pos[X] = (mx/(fastf_t)dmp->dm_width
 		       - 0.5) * 2.0;
+#else
+	view_pos[X] = (mx/(fastf_t)dmp->dm_width - 0.5) /
+	              dmp->dm_aspect * 2.0;
+#endif
 	view_pos[Y] = (0.5 - my/
-		       (fastf_t)((struct x_vars *)dmp->dm_vars)->height) * 2.0;
+		       (fastf_t)dmp->dm_height) * 2.0;
 	view_pos[Z] = 0.0;
 
 	if(state == ST_S_EDIT)
@@ -224,10 +230,16 @@ XEvent *eventPtr;
       }else{
 	fastf_t fx, fy;
 
+#if 0
 	fx = (mx - ((struct x_vars *)dmp->dm_vars)->omx) /
-	  (fastf_t)((struct x_vars *)dmp->dm_vars)->width * 2.0;
+	  (fastf_t)dmp->dm_width * 2.0;
+#else
+	fx = (mx - ((struct x_vars *)dmp->dm_vars)->omx) /
+	  (fastf_t)dmp->dm_width /
+	  dmp->dm_aspect * 2.0;
+#endif
 	fy = (((struct x_vars *)dmp->dm_vars)->omy - my) /
-	  (fastf_t)((struct x_vars *)dmp->dm_vars)->height * 2.0;
+	  (fastf_t)dmp->dm_height * 2.0;
 	bu_vls_printf( &cmd, "knob -i aX %f aY %f\n", fx, fy );
       }
 
@@ -235,17 +247,33 @@ XEvent *eventPtr;
     case ALT_MOUSE_MODE_ZOOM:
       bu_vls_printf( &cmd, "knob -i aS %f\n",
 		     (((struct x_vars *)dmp->dm_vars)->omy - my)/
-		     (fastf_t)((struct x_vars *)dmp->dm_vars)->height);
+		     (fastf_t)dmp->dm_height);
       break;
     }
 
     ((struct x_vars *)dmp->dm_vars)->omx = mx;
     ((struct x_vars *)dmp->dm_vars)->omy = my;
   } else {
+    /*XXX Hack to prevent Tk from choking on Ctrl-c */
+    if(eventPtr->type == KeyPress && eventPtr->xkey.state & ControlMask){
+      char buffer[1];
+      KeySym keysym;
+
+      XLookupString(&(eventPtr->xkey), buffer, 1,
+		    &keysym, (XComposeStatus *)NULL);
+
+      if(*buffer = 'c'){
+	bu_vls_free(&cmd);
+	curr_dm_list = save_dm_list;
+
+	return TCL_RETURN;
+      }
+    }
+
     XGetWindowAttributes( ((struct x_vars *)dmp->dm_vars)->dpy,
 			  ((struct x_vars *)dmp->dm_vars)->win, &xwa);
-    ((struct x_vars *)dmp->dm_vars)->height = xwa.height;
-    ((struct x_vars *)dmp->dm_vars)->width = xwa.width;
+    dmp->dm_height = xwa.height;
+    dmp->dm_width = xwa.width;
 
     goto end;
   }
@@ -360,15 +388,40 @@ char *argv[];
       return TCL_ERROR;
     }
 
-    bu_vls_init(&vls);
-    bu_vls_printf(&vls, "M %s %d %d", argv[2],
-		  Xx_TO_GED(dmp, atoi(argv[3])),
-		  Xy_TO_GED(dmp, atoi(argv[4])));
-    status = Tcl_Eval(interp, bu_vls_addr(&vls));
-#if 0
-    mged_print_result(status);
-#endif
-    return status;
+    {
+      int x;
+      int y;
+      int old_show_menu;
+
+      old_show_menu = mged_variables.show_menu;
+      x = Xx_TO_GED(dmp, atoi(argv[3]));
+      y = Xy_TO_GED(dmp, atoi(argv[4]));
+
+      if(mged_variables.faceplate &&
+	 mged_variables.show_menu &&
+	 *argv[2] == '1'){
+#define        MENUXLIM        (-1250)
+	if(scroll_active)
+	  goto end;
+
+	if(x >= MENUXLIM && scroll_select( x, y, 0 ))
+	  goto end;
+
+	if(x < MENUXLIM && mmenu_select( y, 0))
+	  goto end;
+      }
+
+      x = (int)(x / dmp->dm_aspect);
+      mged_variables.show_menu = 0;
+end:
+      bu_vls_init(&vls);
+      bu_vls_printf(&vls, "M %s %d %d", argv[2], x, y);
+      status = Tcl_Eval(interp, bu_vls_addr(&vls));
+      mged_variables.show_menu = old_show_menu;
+      bu_vls_free(&vls);
+
+      return status;
+    }
   }
 
   status = TCL_OK;
@@ -399,11 +452,17 @@ char *argv[];
 	if(EDIT_TRAN && mged_variables.edit){
 	  vect_t view_pos;
 
+#if 0
 	  view_pos[X] = (((struct x_vars *)dmp->dm_vars)->omx /
-			 (fastf_t)((struct x_vars *)dmp->dm_vars)->width -
+			 (fastf_t)dmp->dm_width -
 			 0.5) * 2.0;
+#else
+	  view_pos[X] = (((struct x_vars *)dmp->dm_vars)->omx /
+			(fastf_t)dmp->dm_width - 0.5) /
+	                dmp->dm_aspect * 2.0; 
+#endif
 	  view_pos[Y] = (0.5 - ((struct x_vars *)dmp->dm_vars)->omy /
-			 (fastf_t)((struct x_vars *)dmp->dm_vars)->height) * 2.0;
+			 (fastf_t)dmp->dm_height) * 2.0;
 	  view_pos[Z] = 0.0;
 
 	  if(state == ST_S_EDIT)
