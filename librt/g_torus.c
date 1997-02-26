dc1d0bf6429ca0cb98edef0f876bfa7ec77c8218
Paul Tanenbaum <pjt@brlcad.org>
1997-02-26 21:26:33 +0000

    Began adding a real rt_tor_uv()
    svn:revision:13363
    cvs:account:pjt
    cvs:branch:trunk

--- librt/g_torus.c
+++ librt/g_torus.c
@@ -879,10 +879,30 @@ struct soltab		*stp;
 register struct hit	*hitp;
 register struct uvcoord	*uvp;
 {
+#if 0
 	/* Do nothing.  Really, should do something like what REC does,
 	 * ie, angle around center & angle around rim */
 	uvp->uv_u = uvp->uv_v = 0;
 	uvp->uv_du = uvp->uv_dv = 0;
+#else
+	register struct tor_specific	*tor =
+			(struct tor_specific *) stp -> st_specific;
+	LOCAL vect_t			work;
+	LOCAL vect_t			pprime;
+	LOCAL vect_t			pprime2;
+	LOCAL fastf_t			costheta;
+
+	VSUB2(work, hitp -> hit_point, tor -> tor_V);
+	MAT4X3VEC(pprime, tor -> tor_SoR, work);
+	uvp -> uv_u = atan2(pprime[Y], pprime[X]) * rt_inv2pi;
+
+	VSET(work, pprime[X], pprime[Y], 0.0);
+	VUNITIZE(work);
+	VSUB2(pprime2, pprime, work);
+	VUNITIZE(pprime2);
+	costheta = VDOT(pprime2, work);
+	uvp -> uv_v = atan2(costheta, pprime2[Z]) * rt_inv2pi;
+#endif
 }
 
 /*
