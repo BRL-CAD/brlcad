4267d0443ca60f4538544cb51a2d42d98803ae53
Robert Parker <unacknowledged@users.noreply.github.com>
1997-03-03 18:32:28 +0000

    add a few more options
    svn:revision:13397
    cvs:account:bparker
    cvs:branch:trunk

--- libdm/options.c
+++ libdm/options.c
@@ -7,21 +7,41 @@
 #include "dm.h"
 
 int
-dm_process_options(dmp, argc, argv)
+dm_process_options(dmp, width, height, argc, argv)
 struct dm *dmp;
+int *width;
+int *height;
 int argc;
 char *argv[];
 {
   register int c;
 
-  bu_optind = 1;	 /* re-init bu_getopt */
-  while((c = bu_getopt(argc, argv, "n:i:")) != EOF){
+  bu_optind = 0;	 /* re-init bu_getopt */
+  while((c = bu_getopt(argc, argv, "N:S:W:d:i:n:t:")) != EOF){
     switch(c){
-    case 'n':
-      bu_vls_strcpy(&dmp->dm_pathName, bu_optarg);
+    case 'N':
+      *height = atoi(bu_optarg);
+      break;
+    case 'S':
+      *width = *height = atoi(bu_optarg);
+      break;
+    case 'W':
+      *width = atoi(bu_optarg);
+      break;
+    case 'd':
+      bu_vls_strcpy(&dmp->dm_dName, bu_optarg);
       break;
     case 'i':
       bu_vls_strcpy(&dmp->dm_initWinProc, bu_optarg);
+      break;
+    case 'n':
+      if(*bu_optarg != '.')
+	bu_vls_printf(&dmp->dm_pathName, ".%s", bu_optarg);
+      else
+	bu_vls_strcpy(&dmp->dm_pathName, bu_optarg);
+      break;
+    case 't':
+      dmp->dm_top = atoi(bu_optarg);
       break;
     default:
       break;
