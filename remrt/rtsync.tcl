5256508e131eed5c6fe2a442b41b7502962ebc9f
Michael John Muuss <mike@brlcad.org>
1997-03-06 08:56:07 +0000

    improvements
    svn:revision:13441
    cvs:account:mike
    cvs:branch:trunk

--- remrt/rtsync.tcl
+++ remrt/rtsync.tcl
@@ -1,6 +1,16 @@
 # rtsync.tcl
 # A prototype GUI for rtsync
+# Depends on rtsync defining $dbip and $rtip.
+# Uses various RTSYNC built-in commands, as well as LIBRT's Tcl commands.
 #  -Mike Muuss, ARL, March 97.
+
+global red grn blu
+set red 0
+set grn 0
+set blu 0
+
+global sun_region_name
+set sun_region_name "light.r"
 
 puts "running rtsync.tcl"
 option add *background #ffffff
@@ -10,8 +20,8 @@ option add *background #ffffff
 frame .mbar -relief raised -bd 2 ; pack .mbar -side top
 frame .logo_fr ; pack .logo_fr -side top
 frame .title_fr ; pack .title_fr -side top
-frame .sunangle_fr ; pack .sunangle_fr -side top
-frame .suncolor_fr ; pack .suncolor_fr -side top
+frame .sunangle_fr -relief ridge ; pack .sunangle_fr -side top
+frame .suncolor_fr -relief ridge ; pack .suncolor_fr -side top
 frame .button_fr ; pack .button_fr -side top
 
 # Menu bar, acros very top
@@ -38,6 +48,19 @@ pack .sunangle_title .sunangle -side top -in .sunangle_fr -fill x
 proc new_sunangle {degrees} {
 	puts "Sun angle ought to be $degrees"
 }
+button .apply_angle -text "Apply" -command apply_angle
+pack .apply_angle -side top -in .sunangle_fr
+
+proc apply_angle {} {
+	global red grn blu
+	global sun_region_name
+	# send new stuff to servers
+	node_send rt_wdb_inmem_rgb {$dbip} $sun_region_name $red $grn $blu
+	# indicate re-prep required.
+	reprep
+	# Send this to MGED to get pov message sent back.
+	vrmgr_send refresh
+}
 
 # The sun color
 label .suncolor_title -text "Sun Color"
@@ -46,7 +69,7 @@ scale .suncolor -label "Yellow to Blue" -from 0 -to 100 -orient horizontal \
 frame .suncolor_swatch -height 1c -width 6c
 pack .suncolor_title .suncolor .suncolor_swatch -in .suncolor_fr
 proc new_suncolor {percent} {
-	global red,grn,blu
+	global red grn blu
 	# alpha is how much yellow, beta is how much blue.
 	set beta [expr $percent / 100.0 ]
 	set alpha [expr 1.0 - $beta ]
@@ -59,13 +82,19 @@ proc new_suncolor {percent} {
 	puts "Sun color $percent percent of yellow = $red $grn $blu"
 }
 
-# The buttons
-button .apply -text "Apply" -command apply_settings
-pack .apply -side top -in .button_fr
+button .apply_color -text "Apply" -command apply_color
+pack .apply_color -side top -in .suncolor_fr
 
-proc apply_settings {} {
-	# send new stuff to servers
-	# indicate re-prep required.
+entry .suncolor_region -width 16 -relief sunken -bd 2 -textvariable sun_region_name
+pack .suncolor_region -side left -in .suncolor_fr
+
+proc apply_color {} {
+	global red grn blu
+	global sun_region_name
+	# send new stuff to servers.  No reprep needed for this.
+	node_send sh_directchange_rgb {$rtip} $sun_region_name $red $grn $blu
+	# Send this to MGED to get pov message sent back.
+	vrmgr_send refresh
 }
 
 puts "done rtsync.tcl"
