42271ba8034a0ffe7dcb0b8111575c9c3f738686
Robert Parker <unacknowledged@users.noreply.github.com>
1997-04-04 19:53:18 +0000

    add prompt for display manager
    svn:revision:13559
    cvs:account:bparker
    cvs:branch:trunk

--- mged/ged.c
+++ mged/ged.c
@@ -1678,6 +1678,7 @@ char	**argv;
 
 	if(first){
 	  first = 0;
+	  get_attached();
 #if 0
 	  Tcl_AppendResult(interp, "Note: the attach command can be used\n",
 			   "      to open a display window.\n\n", (char *)NULL);
