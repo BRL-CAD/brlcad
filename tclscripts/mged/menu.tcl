dca5a6b027c2af758a903e104ab590b08be41ceb
Robert Parker <unacknowledged@users.noreply.github.com>
1997-04-18 13:22:09 +0000

    more mods to enhance use with multiple instances
    svn:revision:13614
    cvs:account:bparker
    cvs:branch:trunk

--- tclscripts/mged/menu.tcl
+++ tclscripts/mged/menu.tcl
@@ -5,79 +5,78 @@
 #             user interface.
 #
 
-proc mmenu_set { i menu } {
+proc mmenu_set { w id i menu } {
     global mmenu
-    global player_count
-    global player_ids
 
-    set mmenu($i) $menu
-    for { set j 0} { $j < $player_count } { incr j } {
-	set w .mmenu$player_ids($j)
+    set mmenu($id,$i) $menu
+    if {![winfo exists $w]} {
+	return
+    }
 
-	if {![winfo exists $w]} {
-	    return
-	}
+    if { [llength $menu]<=0 } {
+	pack forget $w.f$i
+	return
+    }
 
-	if { [llength $menu]<=0 } then {
-	    pack forget $w.f$i
-	    continue
-	}
+    $w.f$i.l delete 0 end
+    foreach item $menu {
+	$w.f$i.l insert end $item
+    }
+    $w.f$i.l configure -height [llength $menu]
 
-	$w.f$i.l delete 0 end
-	foreach item $menu {
-	    $w.f$i.l insert end $item
-	}
-	$w.f$i.l configure -height [llength $menu]
-
-	if { [winfo ismapped $w.f$i]==0 } then {
-	    set packcmd "pack $w.f$i -side top -fill both -expand yes"
-	    for { set scan [expr $i-1] } { $scan >= 0 } { incr scan -1 } {
-		if { [llength $mmenu($scan)]>0 } then {
-		    lappend packcmd -after $w.f$scan
-		    break
-		}
+    if { [winfo ismapped $w.f$i]==0 } {
+	set packcmd "pack $w.f$i -side top -fill both -expand yes"
+	for { set scan [expr $i-1] } { $scan >= 0 } { incr scan -1 } {
+	    if { [llength $mmenu($id,$scan)]>0 } {
+		lappend packcmd -after $w.f$scan
+		break
 	    }
-	    for { set scan [expr $i+1] } { $scan < $mmenu(num) } { incr scan } {
-		if { [llength $mmenu($scan)]>0 } then {
-		    lappend packcmd -before $w.f$scan
-		    break
-		}
-	    }
-	    eval $packcmd
 	}
+	for { set scan [expr $i+1] } { $scan < $mmenu($id,num) } { incr scan } {
+	    if { [llength $mmenu($id,$scan)]>0 } then {
+		lappend packcmd -before $w.f$scan
+		break
+	    }
+	}
+	eval $packcmd
     }
 }
 
+
 proc mmenu_init { id } {
     global mmenu
-    global player_ids
-    global player_count
     global player_screen
+    global mged_display
 
+    cmd_set $id
     set w .mmenu$id
     catch { destroy $w }
     toplevel $w -screen $player_screen($id)
-    wm title $w "MGED Button Menu"
 
     label $w.state -textvariable mged_display(state)
     pack $w.state -side top
     
     set i 0
-    set mmenu(num) 0
+    set mmenu($id,num) 0
 
     foreach menu [mmenu_get] {
-	incr mmenu(num)
+	incr mmenu($id,num)
 	
 	frame $w.f$i -relief raised -bd 1
 	listbox $w.f$i.l -bd 2 -exportselection false
         pack $w.f$i.l -side left -fill both -expand yes
 
-	bind $w.f$i.l <Double-Button-1> "doit_press $id %W"
+	bind $w.f$i.l <Button-1> "handle_select %W %y; doit_press $id %W"
 	bind $w.f$i.l <Button-2> "handle_select %W %y; doit_press $id %W"
 
-	mmenu_set $i $menu
+	mmenu_set $w $id $i $menu
+
 	incr i
     }
+
+    wm title $w "$id\'s MGED Button Menu"
+    wm protocol $w WM_DELETE_WINDOW "toggle_button_menu $id"
+    wm resizable $w 0 0
 
     return
 }
@@ -85,4 +84,34 @@ proc mmenu_init { id } {
 proc doit_press { id w } {
     cmd_set $id
     press [$w get [$w curselection]]
+}
+
+proc reconfig_mmenu { id } {
+    global mmenu
+
+    if {![winfo exists .mmenu$id]} {
+	return
+    }
+
+    set w .mmenu$id
+    for {set i 0} {$i < 3} {incr i} {
+	catch { destroy $w.f$i } 
+    }
+
+    set i 0
+    set mmenu($id,num) 0
+    foreach menu [mmenu_get] {
+	incr mmenu($id,num)
+	
+	frame $w.f$i -relief raised -bd 1
+	listbox $w.f$i.l -bd 2 -exportselection false
+        pack $w.f$i.l -side left -fill both -expand yes
+
+	bind $w.f$i.l <Button-1> "handle_select %W %y; doit_press $id %W"
+	bind $w.f$i.l <Button-2> "handle_select %W %y; doit_press $id %W"
+
+	mmenu_set $w $id $i $menu
+
+	incr i
+    }
 }
