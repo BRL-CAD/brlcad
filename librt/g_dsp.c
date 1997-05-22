8abe19820ca7da13d554d84700d1cc55230eeaf8
Lee Butler <iraytrace@users.noreply.github.com>
1997-05-22 09:28:06 +0000

    checkpoint
    svn:revision:13762
    cvs:account:butler
    cvs:branch:trunk

--- librt/g_dsp.c
+++ librt/g_dsp.c
@@ -58,16 +58,28 @@ struct dsp_specific {
 	struct rt_dsp_internal dsp_i;
 };
 
+/* access to the array */
+#define DSP(_dsp,_x,_y) (((short *)(_dsp->dsp_mp->buf))[(_y)*_dsp->dsp_xcnt+(_x)])
+
 #define DSP_O(m) offsetof(struct rt_dsp_internal, m)
 #define DSP_AO(a) offsetofarray(struct rt_dsp_internal, a)
 
-static struct bu_structparse rt_dsp_parse[] = {
+struct bu_structparse rt_dsp_parse[] = {
 	{"%s",	DSP_NAME_LEN, "file", DSP_AO(dsp_file), FUNC_NULL },
 	{"%d",	1, "xc", DSP_O(dsp_xcnt), FUNC_NULL },
 	{"%d",	1, "yc", DSP_O(dsp_ycnt), FUNC_NULL },
-	{"%d",	1, "xs", DSP_O(dsp_xs), FUNC_NULL },
-	{"%d",	1, "ys", DSP_O(dsp_ys), FUNC_NULL },
-	{"%d",	1, "ys", DSP_O(dsp_zs), FUNC_NULL },
+	{"%f",	1, "xs", DSP_O(dsp_xs), FUNC_NULL },
+	{"%f",	1, "ys", DSP_O(dsp_ys), FUNC_NULL },
+	{"%f",	1, "zs", DSP_O(dsp_zs), FUNC_NULL },
+	{"%f", 16, "stom", DSP_AO(dsp_stom), FUNC_NULL },
+	{"",	0, (char *)0, 0,			FUNC_NULL }
+};
+
+struct bu_structparse rt_dsp_ptab[] = {
+	{"%s",	DSP_NAME_LEN, "file", DSP_AO(dsp_file), FUNC_NULL },
+	{"%d",	1, "xc", DSP_O(dsp_xcnt), FUNC_NULL },
+	{"%d",	1, "yc", DSP_O(dsp_ycnt), FUNC_NULL },
+	{"%f", 16, "stom", DSP_AO(dsp_stom), FUNC_NULL },
 	{"",	0, (char *)0, 0,			FUNC_NULL }
 };
 
@@ -97,6 +109,9 @@ struct rt_i		*rtip;
 	register struct dsp_specific	*dsp;
 	CONST struct rt_tol		*tol = &rtip->rti_tol;
 
+	if (rt_g.debug & DEBUG_SOLIDS)
+		bu_log("rt_dsp_prep()\n");
+
 	RT_CK_DB_INTERNAL(ip);
 	dsp_ip = (struct rt_dsp_internal *)ip->idb_ptr;
 	RT_DSP_CK_MAGIC(dsp_ip);
@@ -111,6 +126,10 @@ register CONST struct soltab *stp;
 {
 	register CONST struct dsp_specific *dsp =
 		(struct dsp_specific *)stp->st_specific;
+
+	if (rt_g.debug & DEBUG_SOLIDS)
+		bu_log("rt_dsp_print()\n");
+
 }
 
 /*
@@ -136,6 +155,9 @@ struct seg		*seghead;
 	register struct seg *segp;
 	CONST struct rt_tol	*tol = &ap->a_rt_i->rti_tol;
 
+	if (rt_g.debug & DEBUG_SOLIDS)
+		bu_log("rt_dsp_shot()\n");
+
 	return(0);			/* MISS */
 }
 
@@ -154,6 +176,9 @@ struct  seg            segp[]; /* array of segs (results returned) */
 int		  	    n; /* Number of ray/object pairs */
 struct application	*ap;
 {
+	if (rt_g.debug & DEBUG_SOLIDS)
+		bu_log("rt_dsp_vshot()\n");
+
 	rt_vstub( stp, rp, segp, n, ap );
 }
 
@@ -171,6 +196,9 @@ register struct xray	*rp;
 	register struct dsp_specific *dsp =
 		(struct dsp_specific *)stp->st_specific;
 
+	if (rt_g.debug & DEBUG_SOLIDS)
+		bu_log("rt_dsp_norm()\n");
+
 	VJOIN1( hitp->hit_point, rp->r_pt, hitp->hit_dist, rp->r_dir );
 }
 
@@ -187,6 +215,9 @@ struct soltab		*stp;
 {
 	register struct dsp_specific *dsp =
 		(struct dsp_specific *)stp->st_specific;
+
+	if (rt_g.debug & DEBUG_SOLIDS)
+		bu_log("rt_dsp_curve()\n");
 
  	cvp->crv_c1 = cvp->crv_c2 = 0;
 
@@ -211,6 +242,9 @@ register struct uvcoord	*uvp;
 {
 	register struct dsp_specific *dsp =
 		(struct dsp_specific *)stp->st_specific;
+
+	if (rt_g.debug & DEBUG_SOLIDS)
+		bu_log("rt_dsp_uv()\n");
 }
 
 /*
@@ -223,6 +257,12 @@ register struct soltab *stp;
 	register struct dsp_specific *dsp =
 		(struct dsp_specific *)stp->st_specific;
 
+	if (rt_g.debug & DEBUG_SOLIDS)
+		bu_log("rt_dsp_free()\n");
+
+	RT_CK_MAPPED_FILE(dsp->dsp_i.dsp_mp);
+	rt_close_mapped_file(dsp->dsp_i.dsp_mp);
+
 	rt_free( (char *)dsp, "dsp_specific" );
 }
 
@@ -232,6 +272,9 @@ register struct soltab *stp;
 int
 rt_dsp_class()
 {
+	if (rt_g.debug & DEBUG_SOLIDS)
+		bu_log("rt_dsp_class()\n");
+
 	return(0);
 }
 
@@ -246,12 +289,156 @@ CONST struct rt_tess_tol *ttol;
 struct rt_tol		*tol;
 {
 	LOCAL struct rt_dsp_internal	*dsp_ip;
+	point_t m_pt;
+	point_t s_pt;
+	int x, y;
+	int step;
+	int half_step;
+	int goal;
+	int xlim;
+	int ylim;
+
+	if (rt_g.debug & DEBUG_SOLIDS)
+		bu_log("rt_dsp_plot()\n");
 
 	RT_CK_DB_INTERNAL(ip);
 	dsp_ip = (struct rt_dsp_internal *)ip->idb_ptr;
 	RT_DSP_CK_MAGIC(dsp_ip);
 
-	return(-1);
+#define MOVE() \
+	MAT4X3PNT(m_pt, dsp_ip->dsp_stom, s_pt); \
+	RT_ADD_VLIST( vhead, m_pt, RT_VLIST_LINE_MOVE )
+
+#define DRAW() \
+	MAT4X3PNT(m_pt, dsp_ip->dsp_stom, s_pt); \
+	RT_ADD_VLIST( vhead, m_pt, RT_VLIST_LINE_DRAW )
+
+
+	/* Draw the Bottom */
+	VSETALL(s_pt, 0.0);
+	MOVE();
+
+	s_pt[X] = dsp_ip->dsp_xcnt-1;
+	DRAW();
+
+	s_pt[Y] = dsp_ip->dsp_ycnt-1;
+	DRAW();
+
+	s_pt[X] = 0.0;
+	DRAW();
+
+	s_pt[Y] = 0.0;
+	DRAW();
+
+
+	/* Draw the corners */
+	s_pt[Z] = DSP(dsp_ip,0,0);
+	DRAW();
+
+	VSET(s_pt, dsp_ip->dsp_xcnt-1, 0.0, 0.0);
+	MOVE();
+	s_pt[Z] = DSP(dsp_ip, dsp_ip->dsp_xcnt-1, 0);
+	DRAW();
+
+	VSET(s_pt, dsp_ip->dsp_xcnt-1, dsp_ip->dsp_ycnt-1, 0.0);
+	MOVE();
+	s_pt[Z] = DSP(dsp_ip, dsp_ip->dsp_xcnt-1, dsp_ip->dsp_ycnt-1);
+	DRAW();
+
+	VSET(s_pt, 0.0, dsp_ip->dsp_ycnt-1, 0.0);
+	MOVE();
+	s_pt[Z] = DSP(dsp_ip, 0, dsp_ip->dsp_ycnt-1);
+	DRAW();
+
+
+	/* Draw the outside line of the top 
+	 * We draw the four sides of the top at full resolution.
+	 * This helps edge matching.
+	 * The inside of the top, we draw somewhat coarser
+	 */
+	for (y=0 ; y < dsp_ip->dsp_ycnt ; y += dsp_ip->dsp_ycnt-1 ) {
+		VSET(s_pt, 0.0, y, DSP(dsp_ip, 0, y));
+		MOVE();
+
+		for (x=0 ; x < dsp_ip->dsp_xcnt ; x++) {
+			s_pt[X] = x;
+			s_pt[Z] = DSP(dsp_ip, x, y);
+			DRAW();
+		}
+	}
+
+
+	for (x=0 ; x < dsp_ip->dsp_xcnt ; x += dsp_ip->dsp_xcnt-1 ) {
+		VSET(s_pt, x, 0.0, DSP(dsp_ip, x, 0));
+		MOVE();
+
+		for (y=0 ; y < dsp_ip->dsp_ycnt ; y++) {
+			s_pt[Y] = y;
+			s_pt[Z] = DSP(dsp_ip, x, y);
+			DRAW();
+		}
+	}
+
+	/* now draw the body of the top */
+	if( ttol->rel )  {
+		int	rstep;
+		rstep = dsp_ip->dsp_xcnt;
+		V_MAX( rstep, dsp_ip->dsp_ycnt );
+		step = (int)(ttol->rel * rstep);
+	} else { 
+		goal = 10000;
+		goal -= 5;
+		goal -= 4 + 2 * (dsp_ip->dsp_xcnt+dsp_ip->dsp_ycnt);
+
+		if (goal <= 0) return 0;
+
+		/* Compute data stride based upon producing no more than 'goal' vectors */
+		step = ceil(
+			sqrt( 2*(dsp_ip->dsp_xcnt-1)*(dsp_ip->dsp_ycnt-1) /
+				(double)goal )
+			);
+	}
+	if( step < 1 )  step = 1;
+	if( (half_step = step/2) < 1 )  half_step = 1;
+	xlim = dsp_ip->dsp_xcnt - half_step;
+	ylim = dsp_ip->dsp_ycnt - half_step;
+
+	for (y=step ; y < ylim ; y += step ) {
+		VSET(s_pt, 0.0, y, DSP(dsp_ip, 0, y));
+		MOVE();
+
+		for (x=step ; x < xlim ; x+=step ) {
+			s_pt[X] = x;
+			s_pt[Z] = DSP(dsp_ip, x, y);
+			DRAW();
+		}
+		
+		x = dsp_ip->dsp_xcnt-1;
+		s_pt[X] = x;
+		s_pt[Z] = DSP(dsp_ip, x, y);
+		DRAW();
+
+	}
+
+	for (x=step ; x < xlim ; x += step ) {
+		VSET(s_pt, x, 0.0, DSP(dsp_ip, x, 0));
+		MOVE();
+		
+		for (y=step ; y < ylim ; y+=step) {
+			s_pt[Y] = y;
+			s_pt[Z] = DSP(dsp_ip, x, y);
+			DRAW();
+		}
+		
+		y = dsp_ip->dsp_ycnt-1;
+		s_pt[Y] = y;
+		s_pt[Z] = DSP(dsp_ip, x, y);
+		DRAW();
+	}
+
+#undef MOVE
+#undef DRAW
+	return(0);
 }
 
 /*
@@ -270,6 +457,9 @@ CONST struct rt_tess_tol *ttol;
 struct rt_tol		*tol;
 {
 	LOCAL struct rt_dsp_internal	*dsp_ip;
+
+	if (rt_g.debug & DEBUG_SOLIDS)
+		bu_log("rt_dsp_tess()\n");
 
 	RT_CK_DB_INTERNAL(ip);
 	dsp_ip = (struct rt_dsp_internal *)ip->idb_ptr;
@@ -296,15 +486,21 @@ register CONST mat_t		mat;
 	mat_t tmp;
 
 #define IMPORT_FAIL(s) \
-	bu_log("rt_ebm_import() '%s' %s\n", dsp_ip->dsp_file, s); \
+	bu_log("rt_ebm_import(%d) '%s' %s\n", __LINE__, dsp_ip->dsp_file, s); \
 	bu_free( (char *)dsp_ip , "rt_dsp_import: dsp_ip" ); \
 	ip->idb_type = ID_NULL; \
 	ip->idb_ptr = (genptr_t)NULL; \
 	return -2
 
-
 	RT_CK_EXTERNAL( ep );
 	rp = (union record *)ep->ext_buf;
+
+	if (rt_g.debug & DEBUG_SOLIDS)
+		bu_log("rt_dsp_import(%s)\n", rp->ss.ss_args);
+
+
+
+
 	/* Check record type */
 	if( rp->u_id != DBID_STRSOL )  {
 		rt_log("rt_dsp_import: defective record\n");
@@ -317,18 +513,21 @@ register CONST mat_t		mat;
 	dsp_ip = (struct rt_dsp_internal *)ip->idb_ptr;
 	dsp_ip->magic = RT_DSP_INTERNAL_MAGIC;
 
-	dsp_ip->dsp_file[0] = '\0';
+	/* set defaults */
+	/* XXX bu_struct_parse does not set the null? */
+	memset(&dsp_ip->dsp_file[0], 0, DSP_NAME_LEN); 
 	dsp_ip->dsp_xcnt = dsp_ip->dsp_ycnt = 0;
 	dsp_ip->dsp_xs = dsp_ip->dsp_ys = dsp_ip->dsp_zs = 0.0;
-	mat_idn(&dsp_ip->dsp_mtos);
 	mat_idn(&dsp_ip->dsp_stom);
+	mat_idn(&dsp_ip->dsp_mtos);
+
 	bu_vls_init( &str );
 	bu_vls_strcpy( &str, rp->ss.ss_args );
 	if (bu_struct_parse( &str, rt_dsp_parse, (char *)dsp_ip ) < 0) {
 		bu_vls_free( &str );
 		IMPORT_FAIL("parse error");
 	}
-	bu_vls_free( &str );
+
 	
 	/* Validate parameters */
 	if (dsp_ip->dsp_xcnt == 0 || dsp_ip->dsp_ycnt == 0) {
@@ -336,9 +535,11 @@ register CONST mat_t		mat;
 	}
 	
 	/* Apply Modeling transoform */
-	mat_copy(tmp, dsp_ip->dsp_mtos);
-	mat_mul(dsp_ip->dsp_mtos, mat, dsp_ip->dsp_mtos);
+	mat_copy(tmp, dsp_ip->dsp_stom);
+	mat_mul(dsp_ip->dsp_stom, mat, tmp);
 	
+	mat_inv(dsp_ip->dsp_mtos, dsp_ip->dsp_stom);
+
 	/* get file */
 	if( !(dsp_ip->dsp_mp = rt_open_mapped_file( dsp_ip->dsp_file, "dsp" )) )  {
 		IMPORT_FAIL("unable to open");
@@ -347,7 +548,13 @@ register CONST mat_t		mat;
 		IMPORT_FAIL("buffer wrong size");
 	}
 
+	if (rt_g.debug & DEBUG_SOLIDS) {
+		bu_vls_trunc(&str, 0);
+		bu_vls_struct_print( &str, rt_dsp_ptab, (char *)dsp_ip);
+		bu_log("  imported as(%s)\n", bu_vls_addr(&str));
 
+	}
+	bu_vls_free( &str );
 	return(0);			/* OK */
 }
 
@@ -367,13 +574,14 @@ double				local2mm;
 	union record		*rec;
 	struct bu_vls		str;
 
+
 	RT_CK_DB_INTERNAL(ip);
 	if( ip->idb_type != ID_DSP )  return(-1);
 	dsp_ip = (struct rt_dsp_internal *)ip->idb_ptr;
 	RT_DSP_CK_MAGIC(dsp_ip);
 
 	RT_INIT_EXTERNAL(ep);
-	ep->ext_nbytes = sizeof(union record);
+	ep->ext_nbytes = sizeof(union record)*DB_SS_NGRAN;
 	ep->ext_buf = (genptr_t)rt_calloc( 1, ep->ext_nbytes, "dsp external");
 	rec = (union record *)ep->ext_buf;
 
@@ -385,12 +593,16 @@ double				local2mm;
 	 */
 	dsp.dsp_mtos[15] /= local2mm;
 
+	bu_vls_init( &str );
+	bu_vls_struct_print( &str, rt_dsp_ptab, (char *)&dsp);
+	if (rt_g.debug & DEBUG_SOLIDS)	
+		bu_log("rt_dsp_export(%s)\n", bu_vls_addr(&str) );
+
 	rec->ss.ss_id = DBID_STRSOL;
 	strncpy( rec->ss.ss_keyword, "dsp", NAMESIZE-1 );
-
-	bu_vls_init( &str );
-	bu_vls_struct_print( &str, rt_dsp_parse, (char *)&dsp);
 	strncpy( rec->ss.ss_args, bu_vls_addr(&str), DB_SS_LEN-1 );
+
+
 	bu_vls_free( &str );
 
 
@@ -416,17 +628,35 @@ double			mm2local;
 	struct bu_vls vls;
 
 
+	if (rt_g.debug & DEBUG_SOLIDS)
+		bu_log("rt_dsp_describe()\n");
 
 	RT_DSP_CK_MAGIC(dsp_ip);
 
 	bu_vls_init( &vls );
 
-	rt_vls_strcat( str, "Displacement Map\n");
-
-	bu_vls_printf( &vls, "  file='%s' xc=%d yc=%d\n",
+	bu_vls_printf( &vls, "Displacement Map\n  file='%s' xc=%d yc=%d\n",
 		dsp_ip->dsp_file, dsp_ip->dsp_xcnt, dsp_ip->dsp_ycnt);
 
+	bu_vls_printf( &vls, "  stom=\n");
+	bu_vls_printf( &vls, "  %8.3f %8.3f %8.3f %8.3f\n",
+		V4ARGS(dsp_ip->dsp_stom) );
+
+	bu_vls_printf( &vls, "  %8.3f %8.3f %8.3f %8.3f\n",
+		V4ARGS( &dsp_ip->dsp_stom[4]) );
+
+	bu_vls_printf( &vls, "  %8.3f %8.3f %8.3f %8.3f\n",
+		V4ARGS( &dsp_ip->dsp_stom[8]) );
+
+	bu_vls_printf( &vls, "  %8.3f %8.3f %8.3f %8.3f\n",
+		V4ARGS( &dsp_ip->dsp_stom[12]) );
+
+
 	bu_vls_vlscat( str, &vls );
+
+
+
+
 	bu_vls_free( &vls );
 
 #if 0
@@ -451,10 +681,20 @@ struct rt_db_internal	*ip;
 {
 	register struct rt_dsp_internal	*dsp_ip;
 
+	if (rt_g.debug & DEBUG_SOLIDS)
+		bu_log("rt_dsp_ifree()\n");
+
 	RT_CK_DB_INTERNAL(ip);
 	dsp_ip = (struct rt_dsp_internal *)ip->idb_ptr;
 	RT_DSP_CK_MAGIC(dsp_ip);
+
+	if (dsp_ip->dsp_mp) {
+		RT_CK_MAPPED_FILE(dsp_ip->dsp_mp);
+		rt_close_mapped_file(dsp_ip->dsp_mp);
+	}
+
 	dsp_ip->magic = 0;			/* sanity */
+	dsp_ip->dsp_mp = (struct rt_mapped_file *)0;
 
 	rt_free( (char *)dsp_ip, "dsp ifree" );
 	ip->idb_ptr = GENPTR_NULL;	/* sanity */
