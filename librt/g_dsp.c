38f682444d28ffa7b1398ed5f3ca4970ea2c982f
Lee Butler <iraytrace@users.noreply.github.com>
1997-05-30 05:11:46 +0000

    checkpoint.  Now raytraces as an RPP.
    svn:revision:13806
    cvs:account:butler
    cvs:branch:trunk

--- librt/g_dsp.c
+++ librt/g_dsp.c
@@ -25,7 +25,15 @@
  *	go to /cad/libwdb and create mk_dsp() routine
  *	go to /cad/mged and create the edit support
  *
+ *
+ *
+ *	The top surfaces of the dsp are numbered 0 .. wid*len*2
+ *	(2 triangles per ``cell'')
+ *	The bounding planes on the sides and bottom are numbered -5 .. -1
+ *	The pseudo-surface on the top of the bounding box is -6
+ *
  *  Authors -
+ *  	Lee A. Butler
  *  
  *  Source -
  *	SECAD/VLD Computing Consortium, Bldg 394
@@ -53,21 +61,44 @@ static char RCSdsp[] = "@(#)$Header$ (BRL)";
 #include "./debug.h"
 
 
+#define BBOX_PLANES	6
+
 /* ray tracing form of solid, including precomputed terms */
 struct dsp_specific {
 	struct rt_dsp_internal dsp_i;
+	unsigned short dsp_min; 
+	unsigned short dsp_max; 
+	plane_t		dsp_planes[BBOX_PLANES];
+
+	mat_t		dsp_mtos;
 };
 
+/* plane equations (minus offset distances) for bounding RPP */
+static CONST plane_t	dsp_pl[BBOX_PLANES] = {
+	{-1.0, 0.0, 0.0, 0.0},
+	{ 1.0, 0.0, 0.0, 0.0},
+
+	{0.0, -1.0, 0.0, 0.0},
+	{0.0,  1.0, 0.0, 0.0},
+
+	{0.0, 0.0, -1.0, 0.0},
+	{0.0, 0.0,  1.0, 0.0},
+};
+
+
 /* access to the array */
-#define DSP(_dsp,_x,_y) (((short *)(_dsp->dsp_mp->buf))[(_y)*_dsp->dsp_xcnt+(_x)])
+#define DSP(_dsp,_x,_y) \
+	(((unsigned short *)(_dsp->dsp_mp->buf))[ \
+				(_y) * _dsp->dsp_xcnt + (_x) \
+				])
 
 #define DSP_O(m) offsetof(struct rt_dsp_internal, m)
 #define DSP_AO(a) offsetofarray(struct rt_dsp_internal, a)
 
 struct bu_structparse rt_dsp_parse[] = {
 	{"%s",	DSP_NAME_LEN, "file", DSP_AO(dsp_file), FUNC_NULL },
-	{"%d",	1, "xc", DSP_O(dsp_xcnt), FUNC_NULL },
-	{"%d",	1, "yc", DSP_O(dsp_ycnt), FUNC_NULL },
+	{"%d",	1, "w", DSP_O(dsp_xcnt), FUNC_NULL },
+	{"%d",	1, "n", DSP_O(dsp_ycnt), FUNC_NULL },
 	{"%f",	1, "xs", DSP_O(dsp_xs), FUNC_NULL },
 	{"%f",	1, "ys", DSP_O(dsp_ys), FUNC_NULL },
 	{"%f",	1, "zs", DSP_O(dsp_zs), FUNC_NULL },
@@ -77,8 +108,8 @@ struct bu_structparse rt_dsp_parse[] = {
 
 struct bu_structparse rt_dsp_ptab[] = {
 	{"%s",	DSP_NAME_LEN, "file", DSP_AO(dsp_file), FUNC_NULL },
-	{"%d",	1, "xc", DSP_O(dsp_xcnt), FUNC_NULL },
-	{"%d",	1, "yc", DSP_O(dsp_ycnt), FUNC_NULL },
+	{"%d",	1, "w", DSP_O(dsp_xcnt), FUNC_NULL },
+	{"%d",	1, "n", DSP_O(dsp_ycnt), FUNC_NULL },
 	{"%f", 16, "stom", DSP_AO(dsp_stom), FUNC_NULL },
 	{"",	0, (char *)0, 0,			FUNC_NULL }
 };
@@ -108,13 +139,112 @@ struct rt_i		*rtip;
 	struct rt_dsp_internal		*dsp_ip;
 	register struct dsp_specific	*dsp;
 	CONST struct rt_tol		*tol = &rtip->rti_tol;
+	int x, y;
+	register unsigned short t, dsp_min, dsp_max;
+	point_t pt, bbpt;
+	vect_t work;
+	fastf_t f;
 
-	if (rt_g.debug & DEBUG_SOLIDS)
+	if (rt_g.debug & DEBUG_HF)
 		bu_log("rt_dsp_prep()\n");
 
 	RT_CK_DB_INTERNAL(ip);
 	dsp_ip = (struct rt_dsp_internal *)ip->idb_ptr;
 	RT_DSP_CK_MAGIC(dsp_ip);
+
+	GETSTRUCT( dsp, dsp_specific );
+	dsp->dsp_i = *dsp_ip; /* struct copy */
+	stp->st_specific = (genptr_t) dsp;
+
+	/* find min/max values in displacement map */
+	dsp_min = 0xffff;
+	dsp_max = 0;
+	for (y=0 ; y < dsp_ip->dsp_ycnt ; y++) {
+		for (x=0 ; x < dsp_ip->dsp_xcnt ; x++) {
+			t = DSP(dsp_ip, x, y);
+			if ( t > dsp_max ) dsp_max = t;
+			if ( t < dsp_min ) dsp_min = t;
+		}
+	}
+	dsp->dsp_min = dsp_min;
+	dsp->dsp_max = dsp_max;
+
+	if (rt_g.debug & DEBUG_HF)
+		bu_log("min %d max %d\n", dsp_min, dsp_max);
+
+	/* set the plane equations (in solid coord space) */
+	memcpy(dsp->dsp_planes, dsp_pl, sizeof(dsp_pl));
+	dsp->dsp_planes[1][H] = (fastf_t)dsp_ip->dsp_xcnt;
+	dsp->dsp_planes[3][H] = (fastf_t)dsp_ip->dsp_ycnt;
+	dsp->dsp_planes[5][H] = (fastf_t)dsp_max;
+
+	
+	/* compute bounting box and spere */
+
+#define BBOX_PT(_x, _y, _z) \
+	VSET(pt, (fastf_t)_x, (fastf_t)_y, (fastf_t)_z); \
+	MAT4X3PNT(bbpt, dsp_ip->dsp_stom, pt); \
+	VMINMAX( stp->st_min, stp->st_max, bbpt)
+
+	BBOX_PT(0,		  0,		    0);
+	BBOX_PT(dsp_ip->dsp_xcnt, 0,		    0);
+	BBOX_PT(dsp_ip->dsp_xcnt, dsp_ip->dsp_ycnt, 0);
+	BBOX_PT(0,		  dsp_ip->dsp_ycnt, 0);
+	BBOX_PT(0,		  0,		    dsp_max);
+	BBOX_PT(dsp_ip->dsp_xcnt, 0,		    dsp_max);
+	BBOX_PT(dsp_ip->dsp_xcnt, dsp_ip->dsp_ycnt, dsp_max);
+	BBOX_PT(0,		  dsp_ip->dsp_ycnt, dsp_max);
+
+#undef BBOX_PT
+
+
+	VADD2SCALE( stp->st_center, stp->st_min, stp->st_max, 0.5 );
+	VSUB2SCALE( work, stp->st_max, stp->st_min, 0.5 );
+
+	f = work[X];
+	if( work[Y] > f )  f = work[Y];
+	if( work[Z] > f )  f = work[Z];
+	stp->st_aradius = f;
+	stp->st_bradius = MAGNITUDE(work);
+
+	if (rt_g.debug & DEBUG_HF) {
+		bu_log("model space bbox (%g %g %g) (%g %g %g)\n",
+			V3ARGS(stp->st_min),
+			V3ARGS(stp->st_max));
+	}
+
+	return 0;
+}
+
+static void
+dsp_print(vls, dsp_ip)
+struct bu_vls *vls;
+struct rt_dsp_internal *dsp_ip;
+{
+	point_t pt, v;
+	RT_DSP_CK_MAGIC(dsp_ip);
+
+	bu_vls_init( vls );
+	bu_vls_printf( vls, "Displacement Map\n  file='%s' xc=%d yc=%d\n",
+		dsp_ip->dsp_file, dsp_ip->dsp_xcnt, dsp_ip->dsp_ycnt);
+
+	VSETALL(pt, 0.0);
+	MAT4X3PNT(v, dsp_ip->dsp_stom, pt);
+	bu_vls_printf( vls, "  V=(%g %g %g)\n", V3ARGS(pt));
+
+	bu_vls_printf( vls, "  stom=\n");
+	bu_vls_printf( vls, "  %8.3f %8.3f %8.3f %8.3f\n",
+		V4ARGS(dsp_ip->dsp_stom) );
+
+	bu_vls_printf( vls, "  %8.3f %8.3f %8.3f %8.3f\n",
+		V4ARGS( &dsp_ip->dsp_stom[4]) );
+
+	bu_vls_printf( vls, "  %8.3f %8.3f %8.3f %8.3f\n",
+		V4ARGS( &dsp_ip->dsp_stom[8]) );
+
+	bu_vls_printf( vls, "  %8.3f %8.3f %8.3f %8.3f\n",
+		V4ARGS( &dsp_ip->dsp_stom[12]) );
+
 }
 
 /*
@@ -126,13 +256,95 @@ register CONST struct soltab *stp;
 {
 	register CONST struct dsp_specific *dsp =
 		(struct dsp_specific *)stp->st_specific;
+	struct bu_vls vls;
 
-	if (rt_g.debug & DEBUG_SOLIDS)
-		bu_log("rt_dsp_print()\n");
+	RT_DSP_CK_MAGIC(dsp);
+
+	dsp_print(&vls, &dsp->dsp_i);
+
+	bu_log("%s", bu_vls_addr( &vls));
+	bu_vls_free( &vls );
 
 }
+void
+isect_ray_dsp(dist, surf, ap, stp, seglist)
+double dist[2];
+int surf[2];
+struct application	*ap;
+struct soltab		*stp;
+struct bu_list		*seglist;
+{
+	register struct seg *segp;
+	RT_GET_SEG( segp, ap->a_resource );
+	segp->seg_stp = stp;
+	segp->seg_in.hit_dist = dist[0];
+	segp->seg_in.hit_surfno = surf[0];
 
-/*
+	segp->seg_out.hit_dist = dist[1];
+	segp->seg_out.hit_surfno = surf[1];
+	BU_LIST_INSERT( seglist, &(segp->l) );
+}
+
+
+int
+isect_ray_bbox(dt, surf, r, planes, tol)
+fastf_t 	dt[2];
+int		surf[2];
+struct xray 	*r;
+plane_t		planes[6];
+struct bn_tol	*tol;
+{
+	register double NdotD;
+	double dist;
+	int i;
+	
+	surf[0] = surf[1] = -1;
+	dt[0] = - (dt[1] = MAX_FASTF);
+	
+	/* intersect the ray with the bounding box */
+	for (i=0 ; i < BBOX_PLANES ; i++) {
+
+		NdotD = VDOT(planes[i], r->r_dir);
+
+		/* if N/ray vectors are perp, ray misses plane */
+		if (BN_VECT_ARE_PERP(NdotD, tol)) continue;
+
+		/* ray hits plane, compute distance along ray to plane */
+		dist = - ( (VDOT(planes[i], r->r_pt) - planes[i][H]) / NdotD);
+
+		if (rt_g.debug & DEBUG_HF)
+			bu_log("surf[%d](%g %g %g %g) dot:%g dist:%g\n",
+				i, V4ARGS(planes[i]), NdotD, dist);
+
+		if (NdotD < 0.0) {
+			/* entering */
+			if (dist > dt[0]) {
+				dt[0] = dist;
+				surf[0] = i;
+			}
+		} else { /*  (NdotD > 0.0) */
+			/* leaving */
+			if (dist < dt[1]) {
+				dt[1] = dist;
+				surf[1] = i;
+			}
+		}
+	}
+
+	if (rt_g.debug & DEBUG_HF) {
+		bu_log("solid rpp in  surf[%d] dist:%g\n", surf[0], dt[0]);
+		bu_log("solid rpp out surf[%d] dist:%g\n", surf[1], dt[1]);
+	}
+	/* validate */
+	if (dt[0] >= dt[1] || surf[0] == -1 || surf[1] == -1 ||
+	    dt[1] >= INFINITY)
+		return 0; /* MISS */
+
+	
+
+
+	return 1;
+}/*
  *  			R T _ D S P _ S H O T
  *  
  *  Intersect a ray with a dsp.
@@ -154,11 +366,90 @@ struct seg		*seghead;
 		(struct dsp_specific *)stp->st_specific;
 	register struct seg *segp;
 	CONST struct rt_tol	*tol = &ap->a_rt_i->rti_tol;
+	struct xray r;
+	int	i;
+	double	dist[2];
+	int	surf[2];
+	vect_t	dir, v;
+	struct bu_list	seglist;
 
-	if (rt_g.debug & DEBUG_SOLIDS)
-		bu_log("rt_dsp_shot()\n");
+	if (rt_g.debug & DEBUG_HF)
+		bu_log("rt_dsp_shot(pt:(%g %g %g)\n\tdir[%g]:(%g %g %g))\n",
+			V3ARGS(rp->r_pt),
+			MAGNITUDE(rp->r_dir),
+			V3ARGS(rp->r_dir));
 
-	return(0);			/* MISS */
+	RT_DSP_CK_MAGIC(dsp);
+
+
+	/* map the ray into the coordinate system of the dsp */
+	MAT4X3PNT(r.r_pt, dsp->dsp_i.dsp_mtos, rp->r_pt);
+	MAT4X3VEC(dir, dsp->dsp_i.dsp_mtos, rp->r_dir);
+	VMOVE(r.r_dir, dir);
+	VUNITIZE(r.r_dir);
+
+	if (rt_g.debug & DEBUG_HF) {
+		bn_mat_print("mtos", dsp->dsp_i.dsp_mtos);
+		bu_log("Solid space ray pt:(%g %g %g)\n\tdir[%g]:[%g %g %g]\n\tu_dir(%g %g %g)\n",
+			V3ARGS(r.r_pt),
+			MAGNITUDE(dir),
+			V3ARGS(dir),
+			V3ARGS(r.r_dir));
+
+	}
+
+	/* intersect ray with bounding cube */
+	if ( isect_ray_bbox(dist, surf, &r, dsp->dsp_planes, tol) == 0)
+		return 0;
+
+
+	/* intersect ray with dsp data */
+	BU_LIST_INIT(&seglist);
+
+	isect_ray_dsp(dist, surf, ap, stp, &seglist);
+
+	if (BU_LIST_IS_EMPTY(&seglist))
+		return 0;
+
+	/* map hit distances back to model space */
+	i = 0;
+	for (BU_LIST_FOR(segp, seg, &seglist)) {
+		i += 2;
+		if (rt_g.debug & DEBUG_HF) {
+			bu_log("solid in:%6g out:%6g\n",
+				segp->seg_in.hit_dist,
+				segp->seg_out.hit_dist);
+		}
+
+		VSCALE(dir, r.r_dir, segp->seg_in.hit_dist);
+		MAT4X3VEC(v, dsp->dsp_i.dsp_stom, dir)
+		segp->seg_in.hit_dist = MAGNITUDE(v);
+
+		VSCALE(dir, r.r_dir, segp->seg_out.hit_dist);
+		MAT4X3VEC(v, dsp->dsp_i.dsp_stom, dir)
+		segp->seg_out.hit_dist = MAGNITUDE(v);
+
+		if (rt_g.debug & DEBUG_HF) {
+			bu_log("model in:%6g out:%6g\n",
+			segp->seg_in.hit_dist,
+			segp->seg_out.hit_dist);
+		}
+	}
+
+	if (rt_g.debug & DEBUG_HF) {
+		double NdotD;
+		double d;
+		CONST static plane_t plane = {0.0, 0.0, -1.0, 0.0};
+
+		NdotD = VDOT(plane, rp->r_dir);
+		d = - ( (VDOT(plane, rp->r_pt) - plane[H]) / NdotD);	
+		bu_log("rp -> Z=0 dist: %g\n", d);
+	}
+
+	/* transfer list of hitpoints */
+	BU_LIST_APPEND_LIST( &(seghead->l), &seglist);
+
+	return i;
 }
 
 #define RT_DSP_SEG_MISS(SEG)	(SEG).seg_stp=RT_SOLTAB_NULL
@@ -176,7 +467,7 @@ struct  seg            segp[]; /* array of segs (results returned) */
 int		  	    n; /* Number of ray/object pairs */
 struct application	*ap;
 {
-	if (rt_g.debug & DEBUG_SOLIDS)
+	if (rt_g.debug & DEBUG_HF)
 		bu_log("rt_dsp_vshot()\n");
 
 	rt_vstub( stp, rp, segp, n, ap );
@@ -195,11 +486,25 @@ register struct xray	*rp;
 {
 	register struct dsp_specific *dsp =
 		(struct dsp_specific *)stp->st_specific;
+	vect_t N;
 
-	if (rt_g.debug & DEBUG_SOLIDS)
+	if (rt_g.debug & DEBUG_HF)
 		bu_log("rt_dsp_norm()\n");
 
 	VJOIN1( hitp->hit_point, rp->r_pt, hitp->hit_dist, rp->r_dir );
+	MAT4X3VEC( N, dsp->dsp_i.dsp_stom, 
+		dsp->dsp_planes[ hitp->hit_surfno ] );
+
+	VUNITIZE(N);
+	VMOVE(hitp->hit_normal, N);
+
+	
+	if (rt_g.debug & DEBUG_HF)
+		bu_log("surf[%d] pt(%g %g %g) Norm[%g %g %g](%g %g %g)\n",
+			hitp->hit_surfno,
+			V3ARGS(hitp->hit_point),
+			V3ARGS(dsp->dsp_planes[ hitp->hit_surfno ]),
+			V3ARGS(N));
 }
 
 /*
@@ -216,7 +521,7 @@ struct soltab		*stp;
 	register struct dsp_specific *dsp =
 		(struct dsp_specific *)stp->st_specific;
 
-	if (rt_g.debug & DEBUG_SOLIDS)
+	if (rt_g.debug & DEBUG_HF)
 		bu_log("rt_dsp_curve()\n");
 
  	cvp->crv_c1 = cvp->crv_c2 = 0;
@@ -243,8 +548,13 @@ register struct uvcoord	*uvp;
 	register struct dsp_specific *dsp =
 		(struct dsp_specific *)stp->st_specific;
 
-	if (rt_g.debug & DEBUG_SOLIDS)
+	if (rt_g.debug & DEBUG_HF)
 		bu_log("rt_dsp_uv()\n");
+
+	uvp->uv_u = 0.0;
+	uvp->uv_v = 0.0;
+	uvp->uv_du = 0.0;
+	uvp->uv_dv = 0.0;
 }
 
 /*
@@ -257,7 +567,7 @@ register struct soltab *stp;
 	register struct dsp_specific *dsp =
 		(struct dsp_specific *)stp->st_specific;
 
-	if (rt_g.debug & DEBUG_SOLIDS)
+	if (rt_g.debug & DEBUG_HF)
 		bu_log("rt_dsp_free()\n");
 
 	RT_CK_MAPPED_FILE(dsp->dsp_i.dsp_mp);
@@ -272,7 +582,7 @@ register struct soltab *stp;
 int
 rt_dsp_class()
 {
-	if (rt_g.debug & DEBUG_SOLIDS)
+	if (rt_g.debug & DEBUG_HF)
 		bu_log("rt_dsp_class()\n");
 
 	return(0);
@@ -288,22 +598,23 @@ struct rt_db_internal	*ip;
 CONST struct rt_tess_tol *ttol;
 struct rt_tol		*tol;
 {
-	LOCAL struct rt_dsp_internal	*dsp_ip;
+	struct rt_dsp_internal	*dsp_ip =
+		(struct rt_dsp_internal *)ip->idb_ptr;
 	point_t m_pt;
 	point_t s_pt;
 	int x, y;
 	int step;
 	int half_step;
-	int goal;
-	int xlim;
-	int ylim;
+	int xlim = dsp_ip->dsp_xcnt - 1;
+	int ylim = dsp_ip->dsp_ycnt - 1;
+	int xfudge, yfudge;
 
-	if (rt_g.debug & DEBUG_SOLIDS)
+	if (rt_g.debug & DEBUG_HF)
 		bu_log("rt_dsp_plot()\n");
 
 	RT_CK_DB_INTERNAL(ip);
-	dsp_ip = (struct rt_dsp_internal *)ip->idb_ptr;
 	RT_DSP_CK_MAGIC(dsp_ip);
+
 
 #define MOVE() \
 	MAT4X3PNT(m_pt, dsp_ip->dsp_stom, s_pt); \
@@ -318,10 +629,10 @@ struct rt_tol		*tol;
 	VSETALL(s_pt, 0.0);
 	MOVE();
 
-	s_pt[X] = dsp_ip->dsp_xcnt-1;
+	s_pt[X] = xlim;
 	DRAW();
 
-	s_pt[Y] = dsp_ip->dsp_ycnt-1;
+	s_pt[Y] = ylim;
 	DRAW();
 
 	s_pt[X] = 0.0;
@@ -332,22 +643,23 @@ struct rt_tol		*tol;
 
 
 	/* Draw the corners */
-	s_pt[Z] = DSP(dsp_ip,0,0);
+	s_pt[Z] = DSP(dsp_ip, 0, 0);
 	DRAW();
 
-	VSET(s_pt, dsp_ip->dsp_xcnt-1, 0.0, 0.0);
+	VSET(s_pt, xlim, 0.0, 0.0);
 	MOVE();
-	s_pt[Z] = DSP(dsp_ip, dsp_ip->dsp_xcnt-1, 0);
+	s_pt[Z] = DSP(dsp_ip, xlim, 0);
 	DRAW();
 
-	VSET(s_pt, dsp_ip->dsp_xcnt-1, dsp_ip->dsp_ycnt-1, 0.0);
+
+	VSET(s_pt, xlim, ylim, 0.0);
 	MOVE();
-	s_pt[Z] = DSP(dsp_ip, dsp_ip->dsp_xcnt-1, dsp_ip->dsp_ycnt-1);
+	s_pt[Z] = DSP(dsp_ip, xlim, ylim);
 	DRAW();
 
-	VSET(s_pt, 0.0, dsp_ip->dsp_ycnt-1, 0.0);
+	VSET(s_pt, 0.0, ylim, 0.0);
 	MOVE();
-	s_pt[Z] = DSP(dsp_ip, 0, dsp_ip->dsp_ycnt-1);
+	s_pt[Z] = DSP(dsp_ip, 0, ylim);
 	DRAW();
 
 
@@ -356,7 +668,7 @@ struct rt_tol		*tol;
 	 * This helps edge matching.
 	 * The inside of the top, we draw somewhat coarser
 	 */
-	for (y=0 ; y < dsp_ip->dsp_ycnt ; y += dsp_ip->dsp_ycnt-1 ) {
+	for (y=0 ; y < dsp_ip->dsp_ycnt ; y += ylim ) {
 		VSET(s_pt, 0.0, y, DSP(dsp_ip, 0, y));
 		MOVE();
 
@@ -368,7 +680,7 @@ struct rt_tol		*tol;
 	}
 
 
-	for (x=0 ; x < dsp_ip->dsp_xcnt ; x += dsp_ip->dsp_xcnt-1 ) {
+	for (x=0 ; x < dsp_ip->dsp_xcnt ; x += xlim ) {
 		VSET(s_pt, x, 0.0, DSP(dsp_ip, x, 0));
 		MOVE();
 
@@ -386,53 +698,55 @@ struct rt_tol		*tol;
 		V_MAX( rstep, dsp_ip->dsp_ycnt );
 		step = (int)(ttol->rel * rstep);
 	} else { 
-		goal = 10000;
+		int goal = 10000;
 		goal -= 5;
-		goal -= 4 + 2 * (dsp_ip->dsp_xcnt+dsp_ip->dsp_ycnt);
+		goal -= 8 + 2 * (dsp_ip->dsp_xcnt+dsp_ip->dsp_ycnt);
 
 		if (goal <= 0) return 0;
 
 		/* Compute data stride based upon producing no more than 'goal' vectors */
 		step = ceil(
-			sqrt( 2*(dsp_ip->dsp_xcnt-1)*(dsp_ip->dsp_ycnt-1) /
+			sqrt( 2*(xlim)*(ylim) /
 				(double)goal )
 			);
 	}
 	if( step < 1 )  step = 1;
 	if( (half_step = step/2) < 1 )  half_step = 1;
-	xlim = dsp_ip->dsp_xcnt - half_step;
-	ylim = dsp_ip->dsp_ycnt - half_step;
 
-	for (y=step ; y < ylim ; y += step ) {
+	xfudge = (dsp_ip->dsp_xcnt % step + step) / 2 ;
+	yfudge = (dsp_ip->dsp_ycnt % step + step) / 2 ;
+
+	if (xfudge < 1) xfudge = 1;
+	if (yfudge < 1) yfudge = 1;
+
+	for (y=yfudge ; y < ylim ; y += step ) {
 		VSET(s_pt, 0.0, y, DSP(dsp_ip, 0, y));
 		MOVE();
 
-		for (x=step ; x < xlim ; x+=step ) {
+		for (x=xfudge ; x < xlim ; x+=step ) {
 			s_pt[X] = x;
 			s_pt[Z] = DSP(dsp_ip, x, y);
 			DRAW();
 		}
 		
-		x = dsp_ip->dsp_xcnt-1;
-		s_pt[X] = x;
-		s_pt[Z] = DSP(dsp_ip, x, y);
+		s_pt[X] = xlim;
+		s_pt[Z] = DSP(dsp_ip, xlim, y);
 		DRAW();
 
 	}
 
-	for (x=step ; x < xlim ; x += step ) {
+	for (x=xfudge ; x < xlim ; x += step ) {
 		VSET(s_pt, x, 0.0, DSP(dsp_ip, x, 0));
 		MOVE();
 		
-		for (y=step ; y < ylim ; y+=step) {
+		for (y=yfudge ; y < ylim ; y+=step) {
 			s_pt[Y] = y;
 			s_pt[Z] = DSP(dsp_ip, x, y);
 			DRAW();
 		}
 		
-		y = dsp_ip->dsp_ycnt-1;
-		s_pt[Y] = y;
-		s_pt[Z] = DSP(dsp_ip, x, y);
+		s_pt[Y] = ylim;
+		s_pt[Z] = DSP(dsp_ip, x, ylim);
 		DRAW();
 	}
 
@@ -458,7 +772,7 @@ struct rt_tol		*tol;
 {
 	LOCAL struct rt_dsp_internal	*dsp_ip;
 
-	if (rt_g.debug & DEBUG_SOLIDS)
+	if (rt_g.debug & DEBUG_HF)
 		bu_log("rt_dsp_tess()\n");
 
 	RT_CK_DB_INTERNAL(ip);
@@ -495,7 +809,7 @@ register CONST mat_t		mat;
 	RT_CK_EXTERNAL( ep );
 	rp = (union record *)ep->ext_buf;
 
-	if (rt_g.debug & DEBUG_SOLIDS)
+	if (rt_g.debug & DEBUG_HF)
 		bu_log("rt_dsp_import(%s)\n", rp->ss.ss_args);
 
 
@@ -548,7 +862,7 @@ register CONST mat_t		mat;
 		IMPORT_FAIL("buffer wrong size");
 	}
 
-	if (rt_g.debug & DEBUG_SOLIDS) {
+	if (rt_g.debug & DEBUG_HF) {
 		bu_vls_trunc(&str, 0);
 		bu_vls_struct_print( &str, rt_dsp_ptab, (char *)dsp_ip);
 		bu_log("  imported as(%s)\n", bu_vls_addr(&str));
@@ -595,7 +909,7 @@ double				local2mm;
 
 	bu_vls_init( &str );
 	bu_vls_struct_print( &str, rt_dsp_ptab, (char *)&dsp);
-	if (rt_g.debug & DEBUG_SOLIDS)	
+	if (rt_g.debug & DEBUG_HF)	
 		bu_log("rt_dsp_export(%s)\n", bu_vls_addr(&str) );
 
 	rec->ss.ss_id = DBID_STRSOL;
@@ -628,35 +942,13 @@ double			mm2local;
 	struct bu_vls vls;
 
 
-	if (rt_g.debug & DEBUG_SOLIDS)
+	if (rt_g.debug & DEBUG_HF)
 		bu_log("rt_dsp_describe()\n");
 
 	RT_DSP_CK_MAGIC(dsp_ip);
 
-	bu_vls_init( &vls );
-
-	bu_vls_printf( &vls, "Displacement Map\n  file='%s' xc=%d yc=%d\n",
-		dsp_ip->dsp_file, dsp_ip->dsp_xcnt, dsp_ip->dsp_ycnt);
-
-	bu_vls_printf( &vls, "  stom=\n");
-	bu_vls_printf( &vls, "  %8.3f %8.3f %8.3f %8.3f\n",
-		V4ARGS(dsp_ip->dsp_stom) );
-
-	bu_vls_printf( &vls, "  %8.3f %8.3f %8.3f %8.3f\n",
-		V4ARGS( &dsp_ip->dsp_stom[4]) );
-
-	bu_vls_printf( &vls, "  %8.3f %8.3f %8.3f %8.3f\n",
-		V4ARGS( &dsp_ip->dsp_stom[8]) );
-
-	bu_vls_printf( &vls, "  %8.3f %8.3f %8.3f %8.3f\n",
-		V4ARGS( &dsp_ip->dsp_stom[12]) );
-
-
+	dsp_print(&vls, dsp_ip);
 	bu_vls_vlscat( str, &vls );
-
-
-
-
 	bu_vls_free( &vls );
 
 #if 0
@@ -681,7 +973,7 @@ struct rt_db_internal	*ip;
 {
 	register struct rt_dsp_internal	*dsp_ip;
 
-	if (rt_g.debug & DEBUG_SOLIDS)
+	if (rt_g.debug & DEBUG_HF)
 		bu_log("rt_dsp_ifree()\n");
 
 	RT_CK_DB_INTERNAL(ip);
