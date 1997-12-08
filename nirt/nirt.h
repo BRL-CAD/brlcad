b83846a4b2048665be6327ed63e56ed2d71caafc
Paul Tanenbaum <pjt@brlcad.org>
1997-12-08 18:55:55 +0000

    1. Got rid of some sytactic sugar, like AND and OR macros, and    some unused #defines 2. Implemented the -e option
    svn:revision:14566
    cvs:account:pjt
    cvs:branch:trunk

--- nirt/nirt.h
+++ nirt/nirt.h
@@ -1,57 +1,53 @@
 /*      NIRT.H          */
 
 /*	CONSTANTS	*/
-#define    VAR_NULL     ((struct VarTable *) 0) 
-#define    CT_NULL	((com_table *) 0)
-#define    SILENT_UNSET	0
-#define    SILENT_YES	1
-#define    SILENT_NO	-1
-#define    NIRT_PROMPT   "nirt>  "
-#define    EVER         ;;              /* syntactic sugar       */
-#define    TITLE_LEN    80 
-#define    MAX_RAND     2147483647      /* maximum random number */ 
+#define	VAR_NULL	((struct VarTable *) 0) 
+#define	CT_NULL		((com_table *) 0)
+#define	SILENT_UNSET	0
+#define	SILENT_YES	1
+#define	SILENT_NO	-1
+#define	NIRT_PROMPT	"nirt>  "
+#define	TITLE_LEN	80 
 #if !defined(PI)
-#define    PI           3.141592654
+#define	PI		3.141592654
 #endif
-#define    BACKOUT_DIST	1000.0
-#define    OFF          0
-#define    ON		1
-#define    YES          1
-#define    NO           0
-#define    HIT          1     /* HIT the target  */
-#define    MISS         0     /* MISS the target */
-#define    END          2
-#define    HORZ         0
-#define    VERT         1
-#define    DIST         2
-#define    AND          &&
-#define    OR           ||
-#define    POS		1   
-#define    NEG		0
-#define		AIR	1
-#define		NO_AIR	0
-#define    deg2rad      0.01745329
-#if	(defined(sgi) && defined(mips) && !defined(SGI4D_Rel2))
-#define    RAND_NUM     (double)rand()/MAX_RAND  /* 0 < number < 1 */
-#else
-#define    RAND_NUM     (double)random()/MAX_RAND  /* 0 < number < 1 */
-#endif
+#define	BACKOUT_DIST	1000.0
+#define	OFF		0
+#define	ON		1
+#define	YES		1
+#define	NO		0
+#define	HIT		1     /* HIT the target  */
+#define	MISS		0     /* MISS the target */
+#define	END		2
+#define	HORZ		0
+#define	VERT		1
+#define	DIST		2
+#define	POS		1   
+#define	NEG		0
+#define	AIR		1
+#define	NO_AIR		0
+#define	READING_FILE	1
+#define	READING_STRING	2
+#define	deg2rad		0.01745329
 
 /*	FLAG VALUES FOR nirt_debug	*/
-#define	DEBUG_MAT	0x001
-#define	DEBUG_BACKOUT	0x002
+#define	DEBUG_INTERACT	0x001
+#define	DEBUG_MAT	0x002
+#define	DEBUG_BACKOUT	0x004
 
 /*	STRING FOR USE WITH GETOPT(3)	*/
-#define         OPT_STRING      "bf:Msu:vx:X:?"
+#define	OPT_STRING      "be:f:Msu:vx:X:?"
+
+#define	made_it()	bu_log("Made it to %s:%d\n", __FILE__, __LINE__)
 
 /*	MACROS WITH ARGUMENTS	*/
-#define    max(a,b)             (((a)>(b))?(a):(b))
-#define    min(a,b)             (((a)<(b))?(a):(b))
+#define	max(a,b)	(((a)>(b))?(a):(b))
+#define	min(a,b)	(((a)<(b))?(a):(b))
 #if !defined(abs)
-# define    abs(a)               ((a)>=0 ? (a):(-a))
+# define	abs(a)	((a)>=0 ? (a):(-a))
 #endif
-#define    com_usage(c)         fprintf (stderr, "Usage:  %s %s\n", \
-				    c -> com_name, c -> com_args);
+#define	com_usage(c)	fprintf (stderr, "Usage:  %s %s\n", \
+				c -> com_name, c -> com_args);
 
 /*	DATA STRUCTURES		*/
 typedef struct {
