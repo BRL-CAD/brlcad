c1fa4e2427b712e6e19bf902781bc96f01fa8586
Robert Parker <unacknowledged@users.noreply.github.com>
1998-02-20 23:06:40 +0000

    *- added update_grids(), snap_view_to_grid() and snap_grid_to_view_center() *- mods to draw_grid()
    svn:revision:14840
    cvs:account:bparker
    cvs:branch:trunk

--- mged/grid.c
+++ mged/grid.c
@@ -4,8 +4,10 @@
  * Routines to implement MGED's snap to grid capability.
  *
  * Functions -
- *	draw_grid	draw the grid according to user specification
- *	snap_to_grid	snap values to the nearest grid point
+ *	draw_grid			Draw the grid according to user specification
+ *	snap_to_grid			Snap values to the nearest grid point
+ *	snap_grid_to_view_center	Make the grid point nearest the view center
+ *					the new view center.
  *
  * Source -
  *	SLAD CAD Team
@@ -43,8 +45,8 @@ void
 draw_grid()
 {
   register int i, j;
-  register nh, nv;
-  register nh_lines, nv_lines;
+  register int nh, nv;
+  register int nh_lines, nv_lines;
   register fastf_t fx, fy;
   register fastf_t sf;
   register fastf_t inv_sf;
@@ -60,11 +62,15 @@ draw_grid()
   fastf_t inv_grid_res_h;
   fastf_t inv_grid_res_v;
 
-  sf = Viewscale*base2local;
-  inv_sf = 1 / sf;
+  if(NEAR_ZERO(mged_variables->grid_res_h, (fastf_t)SMALL_FASTF) ||
+     NEAR_ZERO(mged_variables->grid_res_v, (fastf_t)SMALL_FASTF))
+    return;
 
   inv_grid_res_h= 1.0 / mged_variables->grid_res_h;
   inv_grid_res_v= 1.0 / mged_variables->grid_res_v;
+
+  sf = Viewscale*base2local;
+  inv_sf = 1 / sf;
 
   VSCALE(model_grid_anchor, mged_variables->grid_anchor, local2base);
   MAT4X3PNT(view_grid_anchor, model2view, model_grid_anchor);
@@ -74,18 +80,26 @@ draw_grid()
   VSCALE(view_lleft_corner_local, view_lleft_corner, sf);
   nh = (view_grid_anchor_local[X] - view_lleft_corner_local[X]) * inv_grid_res_h;
   nv = (view_grid_anchor_local[Y] - view_lleft_corner_local[Y]) * inv_grid_res_v;
-  VSET(view_grid_start_pt_local,
-       view_grid_anchor_local[X] - (nh * mged_variables->grid_res_h),
-       view_grid_anchor_local[Y] - (nv * mged_variables->grid_res_v),
-       0.0);
 
-  nh_lines = 2.0 * sf * inv_grid_res_v + 2;
-  nv_lines = 2.0 * sf * inv_grid_res_h + 2;
+  {
+    int nmh, nmv;
+
+    nmh = nh / mged_variables->grid_res_major_h + 1;
+    nmv = nv / mged_variables->grid_res_major_v + 1;
+    VSET(view_grid_start_pt_local,
+	 view_grid_anchor_local[X] - (nmh * mged_variables->grid_res_h * mged_variables->grid_res_major_h),
+	 view_grid_anchor_local[Y] - (nmv * mged_variables->grid_res_v * mged_variables->grid_res_major_v),
+	 0.0);
+  }
+
+  nh_lines = 2.0 * sf * inv_grid_res_v + (2 * mged_variables->grid_res_major_v);
+  nv_lines = 2.0 * sf * inv_grid_res_h + (2 * mged_variables->grid_res_major_h);
 
   DM_SET_COLOR(dmp,
 	       mged_variables->grid_color[0], 
 	       mged_variables->grid_color[1],
 	       mged_variables->grid_color[2], 1);
+  DM_SET_LINE_ATTR(dmp, 1, 0);		/* solid lines */
 
   /* draw horizontal dots */
   for(i = 0; i < nh_lines; i += mged_variables->grid_res_major_v){
@@ -115,14 +129,18 @@ snap_to_grid(mx, my)
 fastf_t *mx;		/* input and return values */
 fastf_t *my;		/* input and return values */
 {
-  register int nh, nv;
+  register int nh, nv;		/* whole grid units */
   point_t view_pt;
   point_t view_grid_anchor;
   point_t model_grid_anchor;
-  fastf_t grid_units_h;
-  fastf_t grid_units_v;
+  fastf_t grid_units_h;		/* eventually holds only fractional horizontal grid units */
+  fastf_t grid_units_v;		/* eventually holds only fractional vertical grid units */
   register fastf_t sf;
   register fastf_t inv_sf;
+
+  if(NEAR_ZERO(mged_variables->grid_res_h, (fastf_t)SMALL_FASTF) ||
+     NEAR_ZERO(mged_variables->grid_res_v, (fastf_t)SMALL_FASTF))
+    return;
 
   sf = Viewscale*base2local;
   inv_sf = 1 / sf;
@@ -158,5 +176,109 @@ fastf_t *my;		/* input and return values */
 
   *mx *= inv_sf;
   *my *= inv_sf;
+}
+
+void
+snap_grid_to_view_center()
+{
+  point_t view_pt, model_pt;
+  struct bu_vls vls;
+
+  MAT_DELTAS_GET_NEG(model_pt, toViewcenter);
+  MAT4X3PNT(view_pt, model2view, model_pt);
+  snap_to_grid(&view_pt[X], &view_pt[Y]);
+  MAT4X3PNT(model_pt, view2model, view_pt);
+
+  MAT_DELTAS_VEC_NEG(toViewcenter, model_pt);
+  new_mats();
+
+  VSCALE(model_pt, model_pt, base2local);
+
+  /* save new center in local units */
+  VMOVE(dml_work_pt, model_pt);
+  dml_mouse_dx = dml_mouse_dy = 0;
+}
+
+void
+snap_view_to_grid(view_dx, view_dy)
+fastf_t view_dx, view_dy;
+{
+  struct bu_vls vls;
+  point_t model_pt, view_pt;
+  point_t vcenter, diff;
+  fastf_t grid_units_h, grid_units_v;
+  fastf_t sf, inv_sf;
+  fastf_t dx, dy;
+  int nh, nv;
+
+  if(NEAR_ZERO(mged_variables->grid_res_h, (fastf_t)SMALL_FASTF) ||
+     NEAR_ZERO(mged_variables->grid_res_v, (fastf_t)SMALL_FASTF))
+    return;
+
+  sf = Viewscale*base2local;
+  inv_sf = 1 / sf;
+
+  /* convert mouse distance to grid units */
+  grid_units_h = view_dx * sf / mged_variables->grid_res_h;
+  grid_units_v = view_dy * sf /  mged_variables->grid_res_v;
+  nh = grid_units_h;
+  nv = grid_units_v;
+  grid_units_h -= nh;
+  grid_units_v -= nv;
+
+  if(grid_units_h <= -0.5)
+    dx = (nh - 1) * mged_variables->grid_res_h;
+  else if(0.5 <= grid_units_h)
+    dx = (nh + 1) * mged_variables->grid_res_h;
+  else
+    dx = nh * mged_variables->grid_res_h;
+
+  if(grid_units_v <= -0.5)
+    dy = (nv - 1) * mged_variables->grid_res_v;
+  else if(0.5 <= grid_units_v)
+    dy = (nv + 1) * mged_variables->grid_res_v;
+  else
+    dy = nv * mged_variables->grid_res_v;
+
+  VSET(view_pt, dx, dy, 0.0);
+  VSCALE(view_pt, view_pt, inv_sf);
+  MAT4X3PNT(model_pt, view2model, view_pt);
+
+  MAT_DELTAS_GET_NEG(vcenter, toViewcenter);
+  VSUB2(diff, model_pt, vcenter);
+  VSCALE(diff, diff, base2local);
+  VSUB2(model_pt, dml_work_pt, diff);
+
+  VSCALE(model_pt, model_pt, local2base);
+  MAT_DELTAS_VEC_NEG(toViewcenter, model_pt);
+  new_mats();
+}
+
+void
+update_grids(sf)
+fastf_t sf;
+{
+  register struct dm_list *dlp;
+  struct bu_vls save_result;
+  struct bu_vls cmd;
+
+  FOR_ALL_DISPLAYS(dlp, &head_dm_list.l){
+    dlp->_mged_variables->grid_res_h *= sf;
+    dlp->_mged_variables->grid_res_v *= sf;
+    VSCALE(dlp->_mged_variables->grid_anchor, dlp->_mged_variables->grid_anchor, sf);
+  }
+
+  bu_vls_init(&save_result);
+  bu_vls_init(&cmd);
+
+  bu_vls_strcpy(&save_result, interp->result);
+
+  bu_vls_printf(&cmd, "grid_control_update %lf\n", sf);
+  (void)Tcl_Eval(interp, bu_vls_addr(&cmd));
+
+  Tcl_SetResult(interp, bu_vls_addr(&save_result), TCL_VOLATILE);
+
+  bu_vls_free(&save_result);
+  bu_vls_free(&cmd);
 }
 #endif
