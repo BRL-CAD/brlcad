88c801332b158abd3f027e7a0760788dd9ed9f39
Robert Parker <unacknowledged@users.noreply.github.com>
1998-02-26 15:26:37 +0000

    *- removed everything -- starting over *- added dm_validXType() and dm_bestXType() which actually 	queries the X server to make a determination
    svn:revision:14874
    cvs:account:bparker
    cvs:branch:trunk

--- libdm/query.c
+++ libdm/query.c
@@ -5,163 +5,62 @@
 #include "bu.h"
 #include "dm.h"
 
-int dm_best_type();
-char *dm_best_name();
-int dm_name2type();
-char **dm_names();
-char *dm_type2name();
-int *dm_types();
+int dm_validXType();
+char *dm_bestXType();
 
 int
-dm_best_type()
-{
-	int t;
-
-	t = DM_TYPE_NULL;
-
-#ifdef DM_X
-	t = DM_TYPE_X;
-#endif
-
-#ifdef DM_GLX
-	t = DM_TYPE_GLX;
-#endif
-
-#ifdef DM_OGL
-	t = DM_TYPE_OGL;
-#endif  
-
-	return t;
-}
-
-char *
-dm_best_name()
-{
-#ifdef DM_OGL
-  return dm_type2name(DM_TYPE_OGL);
-#endif  
-
-#ifdef DM_GLX
-  return dm_type2name(DM_TYPE_GLX);
-#endif
-
-#ifdef DM_X
-  return dm_type2name(DM_TYPE_X);
-#endif
-
-  return dm_type2name(DM_TYPE_BAD);
-}
-
-int
-dm_name2type(name)
+dm_validXType(dpy_string, name)
+char *dpy_string;
 char *name;
 {
-  if(!strcmp("nu", name))
-    return DM_TYPE_NULL;
+  Display *dpy;
+  int return_val;
+  int val = 0;
 
-  if(!strcmp("plot", name))
-    return DM_TYPE_PLOT;
-
-  if(!strcmp("ps", name))
-    return DM_TYPE_PS;
+  if((dpy = XOpenDisplay(dpy_string)) == NULL){
+    bu_log("dm_validXType: failed to open display - %s\n", dpy_string);
+    return val;
+  }
 
 #ifdef DM_OGL
-  if(!strcmp("ogl", name))
-    return DM_TYPE_OGL;
-#endif
-
-#ifdef DM_GLX
-  if(!strcmp("glx", name))
-    return DM_TYPE_GLX;
-#endif
-
+  if(!strcmp(name, "ogl") &&
+     XQueryExtension(dpy, "GLX", &return_val, &return_val, &return_val))
+      val = 1;
+  else
+#endif  
 #ifdef DM_X
-  if(!strcmp("X", name))
-    return DM_TYPE_X;
+    if(!strcmp(name, "X"))
+      val = 1;
 #endif
 
-  return DM_TYPE_BAD;
-}
+  XCloseDisplay(dpy);
 
-char **
-dm_names()
-{
-  int *types;
-  int i;
-  char **names = (char **)NULL;
-
-  types = dm_types();
-  for(i = 0; types[i] != DM_TYPE_BAD; ++i){
-    names = (char **)bu_realloc((genptr_t)names, sizeof(char **)*(i+1), "dm_names: names");
-    names[i] = dm_type2name(types[i]);
-  }
-  names = (char **)bu_realloc((genptr_t)names, sizeof(char **)*(i+1), "dm_names: names");
-  names[i] = (char *)NULL;
-
-  bu_free((genptr_t)types, "dm_names: types");
-  return names;
+  return val;
 }
 
 char *
-dm_type2name(type)
-int type;
+dm_bestXType(dpy_string)
+char *dpy_string;
 {
-  switch(type){
-  case DM_TYPE_NULL:
-    return "nu";
-  case DM_TYPE_PLOT:
-    return "plot";
-  case DM_TYPE_PS:
-    return "ps";
-#ifdef DM_OGL
-  case DM_TYPE_OGL:
-    return "ogl";
-#endif
-#ifdef DM_GLX
-  case DM_TYPE_GLX:
-    return "glx";
-#endif
-#ifdef DM_X
-  case DM_TYPE_X:
-    return "X";
-#endif
-  default:
-    return (char *)NULL;
+  Display *dpy;
+  int return_val;
+  char *name = (char *)NULL;
+
+  if((dpy = XOpenDisplay(dpy_string)) == NULL){
+    bu_log("dm_bestXType: failed to open display - %s\n", dpy_string);
+    return name;
   }
-}
-
-int *
-dm_types()
-{
-  int *types = (int *)NULL;
-  int i = 4;
-
-  types = (int *)bu_realloc((genptr_t)types, sizeof(int)*i, "dm_types: types");
-  types[0] = DM_TYPE_NULL;
-  types[1] = DM_TYPE_PLOT;
-  types[2] = DM_TYPE_PS;
-  types[3] = DM_TYPE_BAD;
 
 #ifdef DM_OGL
-  ++i;
-  types = (int *)bu_realloc((genptr_t)types, sizeof(int)*i, "dm_types: types");
-  types[i - 2] = DM_TYPE_OGL;
-  types[i - 1] = DM_TYPE_BAD;
-#endif
-
-#ifdef DM_GLX
-  ++i;
-  types = (int *)bu_realloc((genptr_t)types, sizeof(int)*i, "dm_types: types");
-  types[i - 2] = DM_TYPE_GLX;
-  types[i - 1] = DM_TYPE_BAD;
-#endif
-
+  if(XQueryExtension(dpy, "GLX", &return_val, &return_val, &return_val))
+    name = "ogl";
+  else
+#endif  
 #ifdef DM_X
-  ++i;
-  types = (int *)bu_realloc((genptr_t)types, sizeof(int)*i, "dm_types: types");
-  types[i - 2] = DM_TYPE_X;
-  types[i - 1] = DM_TYPE_BAD;
+    name = "X";
 #endif
 
-  return types;
+  XCloseDisplay(dpy);
+
+  return name;
 }
