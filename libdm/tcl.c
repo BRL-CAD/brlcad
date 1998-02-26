0455db86af2c88c52fb2e6491c03563e494d4223
Robert Parker <unacknowledged@users.noreply.github.com>
1998-02-26 15:33:53 +0000

    *- cleanup *- add Tcl support for dm_validXType() and dm_bestXType()
    svn:revision:14875
    cvs:account:bparker
    cvs:branch:trunk

--- libdm/tcl.c
+++ libdm/tcl.c
@@ -5,25 +5,11 @@
 #include "bu.h"
 #include "dm.h"
 
-extern int dm_best_type();
-extern char *dm_best_name();
-extern int dm_name2type();
-extern char **dm_names();
-extern char *dm_type2name();
-extern int *dm_types();
-#if 0
-extern int dm_send_xevent;
-#endif
+extern int dm_validXType();
+extern char *dm_bestXType();
 
-static int dm_best_type_tcl();
-static int dm_best_name_tcl();
-static int dm_name2type_tcl();
-static int dm_names_tcl();
-static int dm_type2name_tcl();
-static int dm_types_tcl();
-#if 0
-static int dm_send_xevent_tcl();
-#endif
+static int dm_validXType_tcl();
+static int dm_bestXType_tcl();
 
 struct cmdtab {
   char *ct_name;
@@ -31,15 +17,8 @@ struct cmdtab {
 };
 
 static struct cmdtab cmdtab[] = {
-  "dm_best_type", dm_best_type_tcl,
-  "dm_best_name", dm_best_name_tcl,
-  "dm_name2type", dm_name2type_tcl,
-  "dm_names", dm_names_tcl,
-  "dm_type2name", dm_type2name_tcl,
-  "dm_types", dm_types_tcl,
-#if 0
-  "dm_send_xevent", dm_send_xevent_tcl,
-#endif
+  "dm_validXType", dm_validXType_tcl,
+  "dm_bestXType", dm_bestXType_tcl,
   0, 0
 };
 
@@ -58,7 +37,7 @@ Tcl_Interp *interp;
 }
 
 static int
-dm_best_type_tcl(clientData, interp, argc, argv)
+dm_validXType_tcl(clientData, interp, argc, argv)
 ClientData clientData;
 Tcl_Interp *interp;
 int     argc;
@@ -68,14 +47,14 @@ char    **argv;
 
   bu_vls_init(&vls);
 
-  if(argc != 1){
-    bu_vls_printf(&vls, "helplib dm_best_type");
+  if(argc != 3){
+    bu_vls_printf(&vls, "helplib dm_validXType");
     Tcl_Eval(interp, bu_vls_addr(&vls));
     bu_vls_free(&vls);
     return TCL_ERROR;
   }
 
-  bu_vls_printf(&vls, "%d", dm_best_type());
+  bu_vls_printf(&vls, "%d", dm_validXType(argv[1], argv[2]));
   Tcl_AppendResult(interp, bu_vls_addr(&vls), (char *)NULL);
   bu_vls_free(&vls);
 
@@ -83,184 +62,22 @@ char    **argv;
 }
 
 static int
-dm_best_name_tcl(clientData, interp, argc, argv)
+dm_bestXType_tcl(clientData, interp, argc, argv)
 ClientData clientData;
 Tcl_Interp *interp;
 int     argc;
 char    **argv;
 {
-  if(argc != 1){
-    struct bu_vls vls;
-
-    bu_vls_init(&vls);
-    bu_vls_printf(&vls, "helplib dm_best_type");
-    Tcl_Eval(interp, bu_vls_addr(&vls));
-    bu_vls_free(&vls);
-    return TCL_ERROR;
-  }
-
-  Tcl_AppendResult(interp, dm_best_name(), (char *)NULL);
-
-  return TCL_OK;
-}
-
-static int
-dm_name2type_tcl(clientData, interp, argc, argv)
-ClientData clientData;
-Tcl_Interp *interp;
-int     argc;
-char    **argv;
-{
-  int type;
-  struct bu_vls vls;
-
-  bu_vls_init(&vls);
-
-  if(argc != 2){
-    bu_vls_printf(&vls, "helplib dm_name2type");
-    Tcl_Eval(interp, bu_vls_addr(&vls));
-    bu_vls_free(&vls);
-    return TCL_ERROR;
-  }
-
-  type = dm_name2type(argv[1]);
-  bu_vls_printf(&vls, "%d", type);
-  Tcl_AppendResult(interp, bu_vls_addr(&vls), (char *)NULL);
-  bu_vls_free(&vls);
-
-  return TCL_OK;
-}
-
-static int
-dm_names_tcl(clientData, interp, argc, argv)
-ClientData clientData;
-Tcl_Interp *interp;
-int     argc;
-char    **argv;
-{
-  struct bu_vls vls;
-  char **names;
-  char **cpp;
-
-  bu_vls_init(&vls);
-
-  if(argc != 1){
-    bu_vls_printf(&vls, "helplib dm_names");
-    Tcl_Eval(interp, bu_vls_addr(&vls));
-    bu_vls_free(&vls);
-    return TCL_ERROR;
-  }
-
-  names = dm_names();
-  bu_vls_printf(&vls, "{");
-  for(cpp = names; *cpp != (char *)NULL; ++cpp)
-    bu_vls_printf(&vls, " %s", *cpp);
-
-  bu_vls_printf(&vls, " }");
-  Tcl_AppendResult(interp, bu_vls_addr(&vls), (char *)NULL);
-  bu_vls_free(&vls);
-  bu_free((genptr_t)names, "dm_names_tcl: names");
-
-  return TCL_OK;
-}
-
-static int
-dm_type2name_tcl(clientData, interp, argc, argv)
-ClientData clientData;
-Tcl_Interp *interp;
-int     argc;
-char    **argv;
-{
-  char *name;
-
   if(argc != 2){
     struct bu_vls vls;
 
     bu_vls_init(&vls);
-    bu_vls_printf(&vls, "helplib dm_type2name");
+    bu_vls_printf(&vls, "helplib dm_bestXType");
     Tcl_Eval(interp, bu_vls_addr(&vls));
     bu_vls_free(&vls);
     return TCL_ERROR;
   }
 
-  name = dm_type2name(atoi(argv[1]));
-  Tcl_AppendResult(interp, name, (char *)NULL);
-
+  Tcl_AppendResult(interp, dm_bestXType(argv[1]), (char *)NULL);
   return TCL_OK;
 }
-
-static int
-dm_types_tcl(clientData, interp, argc, argv)
-ClientData clientData;
-Tcl_Interp *interp;
-int     argc;
-char    **argv;
-{
-  struct bu_vls vls;
-  int *types;
-  int *ip;
-
-  bu_vls_init(&vls);
-
-  if(argc != 1){
-    bu_vls_printf(&vls, "helplib dm_types");
-    Tcl_Eval(interp, bu_vls_addr(&vls));
-    bu_vls_free(&vls);
-    return TCL_ERROR;
-  }
-
-  types = dm_types();
-  bu_vls_printf(&vls, "{");
-  for(ip = types; *ip != DM_TYPE_BAD; ++ip)
-    bu_vls_printf(&vls, " %d", *ip);
-
-  bu_vls_printf(&vls, " }");
-  Tcl_AppendResult(interp, bu_vls_addr(&vls), (char *)NULL);
-  bu_vls_free(&vls);
-  bu_free((genptr_t)types, "dm_types_tcl: types");
-
-  return TCL_OK;
-}
-
-#if 0
-/*XXX experimenting */
-static int
-dm_send_xevent_tcl(clientData, interp, argc, argv)
-ClientData	clientData;
-Tcl_Interp     *interp;
-int		argc;
-char	      **argv;
-{
-  Tk_Window tk_w;
-  Display *dpy;
-  Window w;
-  int type;
-  int propagate;
-
-  if(argc != 4){
-    struct bu_vls vls;
-
-    bu_vls_init(&vls)
-    bu_vls_printf(&vls, "helplib dm_send_xevent");
-    Tcl_Eval(interp, bu_vls_addr(&vls));
-    bu_vls_free(&vls);
-    return TCL_ERROR;
-  }
-
-  tk_w = Tk_NameToWindow(interp, argv[1], tkwin);
-  dpy = Tk_Display(tk_w);
-  w = Tk_WindowId(tk_w);
-
-  if(sscanf(argv[2], "%d", &type) != 1){
-    Tcl_AppendResult(interp, "dm_send_xevent_tcl: failed to get event type", (char *)NULL);
-    return TCL_ERROR;
-  }
-
-  if(sscanf(argv[2], "%d", &propagate) != 1){
-    Tcl_AppendResult(interp, "dm_send_xevent_tcl: failed to get propagation", (char *)NULL);
-    return TCL_ERROR;
-  }
-
-  return dm_send_xevent(dpy, w, type, propagate, argv[3]);
-}
-#endif
