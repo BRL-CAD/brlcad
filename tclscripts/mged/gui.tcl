bd0c4fa86f80a24bf615d8396eb588e27851af9f
Robert Parker <unacknowledged@users.noreply.github.com>
1998-04-07 14:52:57 +0000

    initialize mged_gui_create and mged_gui_destroy
    svn:revision:15098
    cvs:account:bparker
    cvs:branch:trunk

--- tclscripts/mged/gui.tcl
+++ tclscripts/mged/gui.tcl
@@ -1,3 +1,32 @@
+# Author -
+#	Bob Parker
+#
+# Source -
+#	The U. S. Army Research Laboratory
+#	Aberdeen Proving Ground, Maryland  21005
+#
+# Distribution Notice -
+#	Re-distribution of this software is restricted, as described in
+#       your "Statement of Terms and Conditions for the Release of
+#       The BRL-CAD Package" agreement.
+#
+# Copyright Notice -
+#       This software is Copyright (C) 1998 by the United States Army
+#       in all countries except the USA.  All rights reserved.
+#
+# Description -
+#	Commands to create and destroy an MGED Tcl/Tk interface.
+#
+# $Revision
+#
+if ![info exists mged_gui_create] {
+    set mged_gui_create gui_create_default
+}
+
+if ![info exists mged_default_gui_destroy] {
+    set mged_gui_destroy gui_destroy_default
+}
+
 proc gui {args} {
     eval gui_create $args
 }
