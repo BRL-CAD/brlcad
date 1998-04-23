ad788533ba51df7dc95077d9a42f75a237e8bc70
Robert Parker <unacknowledged@users.noreply.github.com>
1998-04-23 13:41:09 +0000

    added proc bind_troubled_ones
    svn:revision:15146
    cvs:account:bparker
    cvs:branch:trunk

--- tclscripts/mged/bindings.tcl
+++ tclscripts/mged/bindings.tcl
@@ -71,7 +71,91 @@ proc mged_bind_dm { w } {
     bind $w <Control-p> "winset $w; prev_view"
     bind $w <Control-t> "winset $w; toggle_view"
 
-# troublesome ones
+    bind_troubled_ones $w
+}
+
+proc bind_troubled_ones { w } {
+    bind $w <Alt-A> "break"
+    bind $w <Alt-a> "break"
+    bind $w <Alt-B> "break"
+    bind $w <Alt-b> "break"
+    bind $w <Alt-C> "break"
+    bind $w <Alt-c> "break"
+    bind $w <Alt-D> "break"
+    bind $w <Alt-d> "break"
+    bind $w <Alt-E> "break"
+    bind $w <Alt-e> "break"
+    bind $w <Alt-F> "break"
+    bind $w <Alt-f> "break"
+    bind $w <Alt-G> "break"
+    bind $w <Alt-g> "break"
+    bind $w <Alt-H> "break"
+    bind $w <Alt-h> "break"
+    bind $w <Alt-I> "break"
+    bind $w <Alt-i> "break"
+    bind $w <Alt-J> "break"
+    bind $w <Alt-j> "break"
+    bind $w <Alt-K> "break"
+    bind $w <Alt-k> "break"
+    bind $w <Alt-L> "break"
+    bind $w <Alt-l> "break"
+    bind $w <Alt-M> "break"
+    bind $w <Alt-m> "break"
+    bind $w <Alt-N> "break"
+    bind $w <Alt-n> "break"
+    bind $w <Alt-O> "break"
+    bind $w <Alt-o> "break"
+    bind $w <Alt-P> "break"
+    bind $w <Alt-p> "break"
+    bind $w <Alt-Q> "break"
+    bind $w <Alt-q> "break"
+    bind $w <Alt-R> "break"
+    bind $w <Alt-r> "break"
+    bind $w <Alt-S> "break"
+    bind $w <Alt-s> "break"
+    bind $w <Alt-T> "break"
+    bind $w <Alt-t> "break"
+    bind $w <Alt-U> "break"
+    bind $w <Alt-u> "break"
+    bind $w <Alt-V> "break"
+    bind $w <Alt-v> "break"
+    bind $w <Alt-W> "break"
+    bind $w <Alt-w> "break"
+    bind $w <Alt-X> "break"
+    bind $w <Alt-x> "break"
+    bind $w <Alt-Y> "break"
+    bind $w <Alt-y> "break"
+    bind $w <Alt-Z> "break"
+    bind $w <Alt-z> "break"
+
+#    bind $w <Alt-`> "break"
+    bind $w <Alt-KeyPress-1> "break"
+    bind $w <Alt-KeyPress-2> "break"
+    bind $w <Alt-KeyPress-3> "break"
+    bind $w <Alt-KeyPress-4> "break"
+    bind $w <Alt-KeyPress-5> "break"
+    bind $w <Alt-KeyPress-6> "break"
+    bind $w <Alt-KeyPress-7> "break"
+    bind $w <Alt-KeyPress-8> "break"
+    bind $w <Alt-KeyPress-9> "break"
+    bind $w <Alt-KeyPress-0> "break"
+#    bind $w <Alt--> "break"
+#    bind $w <Alt-=> "break"
+#    bind $w <Alt-~> "break"
+#    bind $w <Alt-!> "break"
+#    bind $w <Alt-@> "break"
+#    bind $w <Alt-#> "break"
+#    bind $w <Alt-$> "break"
+#    bind $w <Alt-%> "break"
+#    bind $w <Alt-^> "break"
+#    bind $w <Alt-&> "break"
+#    bind $w <Alt-*> "break"
+#    bind $w <Alt-(> "break"
+#    bind $w <Alt-)> "break"
+#    bind $w <Alt-_> "break"
+#    bind $w <Alt-+> "break"
+    bind $w <Alt-KeyPress> "break"
+
     bind $w <Tab> "break"
 }
 
@@ -82,7 +166,6 @@ proc print_return_val str {
     }
 }
 
-
 proc do_mouse_bindings { w } {
     global transform
 
@@ -92,12 +175,12 @@ proc do_mouse_bindings { w } {
     bind $w <3> "winset $w; zoom 2.0"
 
     bind $w <ButtonRelease> "winset $w; dm idle"
-    bind $w <KeyRelease-Control_L> "winset $w; dm idle"
-    bind $w <KeyRelease-Control_R> "winset $w; dm idle"
-    bind $w <KeyRelease-Shift_L> "winset $w; dm idle"
-    bind $w <KeyRelease-Shift_R> "winset $w; dm idle"
-    bind $w <KeyRelease-Alt_L> "winset $w; dm idle"
-    bind $w <KeyRelease-Alt_R> "winset $w; dm idle"
+    bind $w <KeyRelease-Control_L> "winset $w; dm idle; break"
+    bind $w <KeyRelease-Control_R> "winset $w; dm idle; break"
+    bind $w <KeyRelease-Shift_L> "winset $w; dm idle; break"
+    bind $w <KeyRelease-Shift_R> "winset $w; dm idle; break"
+    bind $w <KeyRelease-Alt_L> "winset $w; dm idle; break"
+    bind $w <KeyRelease-Alt_R> "winset $w; dm idle; break"
 
     set adcflag [adc draw]
 
