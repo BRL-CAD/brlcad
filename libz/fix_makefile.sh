df3d8ebb105a91eead9e630a9d7755fb74a64c08
John Anderson <daytonajra@gmail.com>
1998-06-25 17:16:46 +0000

    Mods to get it to compile when NFS is 1.
    svn:revision:15383
    cvs:account:jra
    cvs:branch:trunk

--- libz/fix_makefile.sh
+++ libz/fix_makefile.sh
@@ -2,3 +2,11 @@
 
 sed -f ../libz/sfile Makefile
 
+for FILE in  ../libz/*.c
+do
+	SRC=${FILE##.*/}
+	OBJ=${SRC%\.c}.o
+	echo "${OBJ}: ../libz/$SRC"
+	echo "	$CC $CFLAGS -c ../libz/$SRC"
+	echo
+done
