7cdeda5abab1c36e5201544021330a6d82bcf763
John Anderson <daytonajra@gmail.com>
1998-07-06 15:32:25 +0000

    Removed non-standard variable substitution commands.
    svn:revision:15447
    cvs:account:jra
    cvs:branch:trunk

--- libz/fix_makefile.sh
+++ libz/fix_makefile.sh
@@ -4,8 +4,8 @@ sed -f ../libz/sfile Makefile
 
 for FILE in  ../libz/*.c
 do
-	SRC=${FILE##.*/}
-	OBJ=${SRC%\.c}.o
+	SRC=`echo $FILE | sed -e 's;.*/;;'`
+	OBJ=`echo $SRC | sed -e 's/\.c\$/.o/'`
 	echo "${OBJ}: ../libz/$SRC"
 	echo "	$CC $CFLAGS -c ../libz/$SRC"
 	echo
