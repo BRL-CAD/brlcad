d1298259666e939fd41e3d7797babc5fba304d46
John Anderson <daytonajra@gmail.com>
1998-07-13 20:55:29 +0000

    Fixed bug in line for include files.
    svn:revision:15472
    cvs:account:jra
    cvs:branch:trunk

--- libtk/fix_makefile.sh
+++ libtk/fix_makefile.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sed -e s@lib/tk\$\(VERSION\)@tclscripts@ Makefile |\
-	 sed -e s@/\(prefix\)/include@/\(prefix\)/include/brlcad@ |\
+	 sed -e s@\(prefix\)/include@\(prefix\)/include/brlcad@ |\
 	 sed -e "s@\$(\C\C)  \$(WISH_OBJS)@\$(\C\C) $LDFLAGS \$(WISH_OBJS)@" |\
 	 sed -e "s@\${\C\C}  \$(TKTEST_OBJS)@\${\C\C} $LDFLAGS \$(TKTEST_OBJS)@" |\
 	 sed -e s@tk8.0@tk@ | sed -e s@tcl8.0@tcl@ |\
