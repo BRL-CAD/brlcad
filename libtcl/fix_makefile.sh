91543ae220d3db97e99e877c1bb101dfbcb6512b
John Anderson <daytonajra@gmail.com>
1998-07-13 20:48:25 +0000

    Fixed error in line for include directory.
    svn:revision:15469
    cvs:account:jra
    cvs:branch:trunk

--- libtcl/fix_makefile.sh
+++ libtcl/fix_makefile.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sed -e s@lib/tcl\$\(VERSION\)@tclscripts@ Makefile |\
-	 sed -e s@/\(prefix\)/include@/\(prefix\)/include/brlcad@  |\
+	 sed -e s@\(prefix\)/include@\(prefix\)/include/brlcad@  |\
 	 sed -e "s@\${\C\C}  \${TCLSH_OBJS}@\${\C\C} $LDFLAGS \${TCLSH_OBJS}@" |\
 	 sed -e "s@\${\C\C}  \${TCLTEST_OBJS}@\${\C\C} $LDFLAGS \${TCLTEST_OBJS}@" |\
 	 sed -e s@tcl8.0@tcl@ |\
