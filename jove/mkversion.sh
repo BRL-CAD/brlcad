b628441ea65cfbdbcd5d84c15b88788dd98acfeb
Michael John Muuss <mike@brlcad.org>
1998-08-23 01:25:17 +0000

    Back to using mkversion.sh script again; simplifies the Cakefile.
    svn:revision:15547
    cvs:account:mike
    cvs:branch:trunk

--- jove/mkversion.sh
+++ jove/mkversion.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 # @(#)$Header$ (BRL)
 
-echo XXX mkversion.sh should not be invoked XXX
-exit 1
-
 if test ! -f version.number
 then
 	echo 0 > version.number
@@ -15,5 +12,3 @@ echo $c | awk '{version = $1 + 1; };END{printf "%d\n", version > "version.number
 c=`cat version.number`
 
 echo "char	*version = \"2.$c\";" > version.c
-
-cc -c version.c
