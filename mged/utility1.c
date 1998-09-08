e05f4e8c5ef12084f27dc93755627228a4d0f284
Robert Parker <unacknowledged@users.noreply.github.com>
1998-09-08 13:55:56 +0000

    *- rewrote whichid to loop through the database at most one time.
    svn:revision:15568
    cvs:account:bparker
    cvs:branch:trunk

--- mged/utility1.c
+++ mged/utility1.c
@@ -406,6 +406,17 @@ register char *a, *b;
 
 }
 
+struct id_names {
+	struct bu_list l;
+	struct bu_vls name;		/* name associated with region id */
+};
+
+struct id_to_names {
+	struct bu_list l;
+	int rid;			/* starting region id */
+	struct id_names headName;	/* head of list of names */
+};
+
 /*
  *	F _ W H I C H _ I D ( ) :	finds all regions with given idents
  */
@@ -422,11 +433,14 @@ char	**argv;
 	struct rt_comb_internal *comb;
 	int		item;
 	int sflag = 0;
+	struct id_to_names headIdName;
+	struct id_to_names *itnp;
+	struct id_names *inp;
 
-	if(dbip == DBI_NULL)
+	if (dbip == DBI_NULL)
 	  return TCL_OK;
 
-	if(argc < 2 || MAXARGS < argc){
+	if (argc < 2 || MAXARGS < argc){
 	  struct bu_vls vls;
 
 	  bu_vls_init(&vls);
@@ -436,7 +450,7 @@ char	**argv;
 	  return TCL_ERROR;
 	}
 
-	if(strcmp(argv[1], "-s") == 0){
+	if (strcmp(argv[1], "-s") == 0){
 	  --argc;
 	  ++argv;
 
@@ -453,46 +467,125 @@ char	**argv;
 	  sflag = 1;
 	}
 
-	if( setjmp( jmp_env ) == 0 )
+	if ( setjmp( jmp_env ) == 0 )
 	  (void)signal( SIGINT, sig3);  /* allow interupts */
         else
 	  return TCL_OK;
 
-	for( j=1; j<argc; j++) {
-		item = atoi( argv[j] );
+	BU_LIST_INIT(&headIdName.l);
 
-		if(!sflag)
-		  Tcl_AppendResult(interp, "Region[s] with ident ", argv[j],
-				   ":\n", (char *)NULL);
+	/* Build list of id_to_names */
+	for ( j=1; j<argc; j++ ) {
+		int n;
+		int start, end;
+		int range;
+		int k;
 
-		/* Examine all COMB nodes */
-		for( i = 0; i < RT_DBNHASH; i++ )  {
-			for( dp = dbip->dbi_Head[i]; dp != DIR_NULL; dp = dp->d_forw )  {
-				if( !(dp->d_flags & DIR_REGION) )
-					continue;
-				if( rt_db_get_internal( &intern, dp, dbip, (mat_t *)NULL ) < 0 )
-				{
-					(void)signal( SIGINT, SIG_IGN );
-					TCL_READ_ERR_return;
-				}
-				comb = (struct rt_comb_internal *)intern.idb_ptr;
-				if( comb->region_id != item )
-					continue;
+		n = sscanf(argv[j], "%d%*[:-]%d", &start, &end);
+		switch(n) {
+		case 1:
+			for ( BU_LIST_FOR(itnp,id_to_names,&headIdName.l) )
+				if (itnp->rid == start)
+					break;
 
-				if(sflag)
-				  Tcl_AppendElement(interp, dp->d_namep);
-				else
-				  Tcl_AppendResult(interp, "   ", dp->d_namep,
-						   "\n", (char *)NULL);
-
-				rt_comb_ifree( &intern );
+			/* rid not found */
+			if (BU_LIST_IS_HEAD(itnp,&headIdName.l)) {
+				BU_GETSTRUCT(itnp,id_to_names);
+				itnp->rid = start;
+				BU_LIST_INSERT(&headIdName.l,&itnp->l);
+				BU_LIST_INIT(&itnp->headName.l);
 			}
+
+			break;
+		case 2:
+			if (start < end)
+				range = end - start + 1;
+			else if (end < start) {
+				range = start - end + 1;
+				start = end;
+			} else
+				range = 1;
+
+			for ( k = 0; k < range; ++k ) {
+				int rid = start + k;
+
+				for ( BU_LIST_FOR(itnp,id_to_names,&headIdName.l) )
+					if (itnp->rid == rid)
+						break;
+
+				/* rid not found */
+				if (BU_LIST_IS_HEAD(itnp,&headIdName.l)) {
+					BU_GETSTRUCT(itnp,id_to_names);
+					itnp->rid = rid;
+					BU_LIST_INSERT(&headIdName.l,&itnp->l);
+					BU_LIST_INIT(&itnp->headName.l);
+				}
+			}
+
+			break;
 		}
+	}
+
+	/* Examine all COMB nodes */
+	for ( i = 0; i < RT_DBNHASH; i++ )  {
+		for ( dp = dbip->dbi_Head[i]; dp != DIR_NULL; dp = dp->d_forw )  {
+			if ( !(dp->d_flags & DIR_REGION) )
+				continue;
+
+			if ( rt_db_get_internal( &intern, dp, dbip, (mat_t *)NULL ) < 0 )
+			{
+				(void)signal( SIGINT, SIG_IGN );
+				TCL_READ_ERR_return;
+			}
+			comb = (struct rt_comb_internal *)intern.idb_ptr;
+
+			/* check to see if region id matches one in our list */
+			for ( BU_LIST_FOR(itnp,id_to_names,&headIdName.l) ) {
+				if ( comb->region_id == itnp->rid ) {
+					/* add region name to our name list for this region */
+					BU_GETSTRUCT(inp,id_names);
+					bu_vls_init(&inp->name);
+					bu_vls_strcpy(&inp->name, dp->d_namep);
+					BU_LIST_INSERT(&itnp->headName.l,&inp->l);
+					break;
+				}
+			}
+
+			rt_comb_ifree( &intern );
+		}
+	}
+
+	/* place data in interp and free memory */
+	 while ( BU_LIST_WHILE(itnp,id_to_names,&headIdName.l) ) {
+		if (!sflag) {
+			struct bu_vls vls;
+
+			bu_vls_init(&vls);
+			bu_vls_printf(&vls, "Region[s] with ident %d:\n", itnp->rid);
+			Tcl_AppendResult(interp, bu_vls_addr(&vls), (char *)NULL);
+			bu_vls_free(&vls);
+		}
+
+		while ( BU_LIST_WHILE(inp,id_names,&itnp->headName.l) ) {
+			if (sflag)
+				Tcl_AppendElement(interp, bu_vls_addr(&inp->name));
+			else
+				Tcl_AppendResult(interp, "   ", bu_vls_addr(&inp->name),
+							"\n", (char *)NULL);
+
+			BU_LIST_DEQUEUE(&inp->l);
+			bu_vls_free(&inp->name);
+			bu_free((genptr_t)inp, "f_which_id: inp");
+		}
+
+		BU_LIST_DEQUEUE(&itnp->l);
+		bu_free((genptr_t)itnp, "f_which_id: itnp");
 	}
 
 	(void)signal( SIGINT, SIG_IGN );
 	return TCL_OK;
 }
+
 /*		F _ W H I C H _ S H A D E R
  *
  *	Finds all combinations using the given shaders
