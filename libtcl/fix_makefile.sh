b34a18d81a8250f8989c227bf015ead1085b023a
John Anderson <daytonajra@gmail.com>
1998-10-30 03:30:01 +0000

    A slight mod for FreeBSD.
    svn:revision:15730
    cvs:account:jra
    cvs:branch:trunk

--- libtcl/fix_makefile.sh
+++ libtcl/fix_makefile.sh
@@ -5,4 +5,5 @@ sed -e s@lib/tcl\$\(VERSION\)@tclscripts@ Makefile |\
 	 sed -e "s@\${\C\C}  \${TCLSH_OBJS}@\${\C\C} $LDFLAGS \${TCLSH_OBJS}@" |\
 	 sed -e "s@\${\C\C}  \${TCLTEST_OBJS}@\${\C\C} $LDFLAGS \${TCLTEST_OBJS}@" |\
 	 sed -e s@tcl8.0@tcl@ |\
+	 sed -e s@tcl80.so.1.0@tcl.so@ |\
 	 sed -e s@tcl80@tcl@
