58ecb8e555bbbb779f0cc5d74bf8c102b0b0b6dc
Robert Parker <unacknowledged@users.noreply.github.com>
1998-11-06 22:02:11 +0000

    *- use new structures *- remove f_ax_set *- set up to use rset
    svn:revision:15756
    cvs:account:bparker
    cvs:branch:trunk

--- mged/axes.c
+++ mged/axes.c
@@ -37,7 +37,55 @@
 extern point_t es_keypoint;
 extern point_t e_axes_pos;
 extern point_t curr_e_axes_pos;
+
+static void ax_set_dirty_flag();
 static void draw_axes();
+
+struct _axes_state default_axes_state = {
+/* ax_rc */			1,
+/* ax_model_draw */    	        0,
+/* ax_model_size */		500,
+/* ax_model_linewidth */	1,
+/* ax_model_pos */		{ 0.0, 0.0, 0.0 },
+/* ax_view_draw */    	        0,
+/* ax_view_size */		500,
+/* ax_view_linewidth */		1,
+/* ax_view_pos */		{ 0, 0 },
+/* ax_edit_draw */		0,
+/* ax_edit_size1 */		500,
+/* ax_edit_size2 */		500,
+/* ax_edit_linewidth1 */	1,
+/* ax_edit_linewidth2 */	1
+};
+
+#define AX_O(_m)	offsetof(struct _axes_state, _m)
+#define AX_OA(_m)	offsetofarray(struct _axes_state, _m)
+struct bu_structparse axes_vparse[] = {
+	{"%d",  1, "model_draw",	AX_O(ax_model_draw),		ax_set_dirty_flag },
+	{"%d",  1, "model_size",	AX_O(ax_model_size),		ax_set_dirty_flag },
+	{"%d",  1, "model_linewidth",	AX_O(ax_model_linewidth),	ax_set_dirty_flag },
+	{"%f",	3, "model_pos",		AX_OA(ax_model_pos),		ax_set_dirty_flag },
+	{"%d",  1, "view_draw",		AX_O(ax_view_draw),		ax_set_dirty_flag },
+	{"%d",  1, "view_size",		AX_O(ax_view_size),		ax_set_dirty_flag },
+	{"%d",  1, "view_linewidth",	AX_O(ax_view_linewidth),	ax_set_dirty_flag },
+	{"%d",  2, "view_pos",		AX_OA(ax_view_pos),		ax_set_dirty_flag },
+	{"%d",  1, "edit_draw",		AX_O(ax_edit_draw),		ax_set_dirty_flag },
+	{"%d",  1, "edit_size1",	AX_O(ax_edit_size1),		ax_set_dirty_flag },
+	{"%d",  1, "edit_size2",	AX_O(ax_edit_size2),		ax_set_dirty_flag },
+	{"%d",  1, "edit_linewidth1",	AX_O(ax_edit_linewidth1),	ax_set_dirty_flag },
+	{"%d",  1, "edit_linewidth2",	AX_O(ax_edit_linewidth2),	ax_set_dirty_flag },
+	{"",	0, (char *)0,		0,				BU_STRUCTPARSE_FUNC_NULL }
+};
+
+static void
+ax_set_dirty_flag()
+{
+  struct dm_list *dmlp;
+
+  FOR_ALL_DISPLAYS(dmlp, &head_dm_list.l)
+    if(dmlp->dml_axes_state == axes_state)
+      dmlp->dml_dirty = 1;
+}
 
 static void
 draw_axes(vpos, rot_mat, size, axes_color, label_color, linewidth)
@@ -136,31 +184,31 @@ draw_e_axes()
   mat_t rot_mat;
 
   if(state == ST_S_EDIT){
-    MAT4X3PNT(v_ap1, model2view, e_axes_pos);
-    MAT4X3PNT(v_ap2, model2view, curr_e_axes_pos);
+    MAT4X3PNT(v_ap1, view_state->vs_model2view, e_axes_pos);
+    MAT4X3PNT(v_ap2, view_state->vs_model2view, curr_e_axes_pos);
   }else if(state == ST_O_EDIT){
     point_t m_ap2;
 
-    MAT4X3PNT(v_ap1, model2view, es_keypoint);
+    MAT4X3PNT(v_ap1, view_state->vs_model2view, es_keypoint);
     MAT4X3PNT(m_ap2, modelchanges, es_keypoint);
-    MAT4X3PNT(v_ap2, model2view, m_ap2);
+    MAT4X3PNT(v_ap2, view_state->vs_model2view, m_ap2);
   }else
     return;
 
   draw_axes(v_ap1,
-	    Viewrot,
-	    mged_variables->e_axes_size1 * INV_GED,
-	    color_scheme->edit_axes1,
-	    color_scheme->edit_axes_label1,
-	    mged_variables->e_axes_linewidth1);
+	    view_state->vs_Viewrot,
+	    axes_state->ax_edit_size1 * INV_GED,
+	    color_scheme->cs_edit_axes1,
+	    color_scheme->cs_edit_axes_label1,
+	    axes_state->ax_edit_linewidth1);
 
-  bn_mat_mul(rot_mat, Viewrot, acc_rot_sol);
+  bn_mat_mul(rot_mat, view_state->vs_Viewrot, acc_rot_sol);
   draw_axes(v_ap2,
 	    rot_mat,
-	    mged_variables->e_axes_size2 * INV_GED,
-	    color_scheme->edit_axes2,
-	    color_scheme->edit_axes_label2,
-	    mged_variables->e_axes_linewidth2);
+	    axes_state->ax_edit_size2 * INV_GED,
+	    color_scheme->cs_edit_axes2,
+	    color_scheme->cs_edit_axes_label2,
+	    axes_state->ax_edit_linewidth2);
 }
 
 void
@@ -169,14 +217,14 @@ draw_m_axes()
   point_t m_ap;			/* axes position in model coordinates, mm */
   point_t v_ap;			/* axes position in view coordinates */
 
-  VSCALE(m_ap, mged_variables->m_axes_pos, local2base);
-  MAT4X3PNT(v_ap, model2view, m_ap);
+  VSCALE(m_ap, axes_state->ax_model_pos, local2base);
+  MAT4X3PNT(v_ap, view_state->vs_model2view, m_ap);
   draw_axes(v_ap,
-	    Viewrot,
-	    mged_variables->m_axes_size * INV_GED,
-	    color_scheme->model_axes,
-	    color_scheme->model_axes_label,
-	    mged_variables->m_axes_linewidth);
+	    view_state->vs_Viewrot,
+	    axes_state->ax_model_size * INV_GED,
+	    color_scheme->cs_model_axes,
+	    color_scheme->cs_model_axes_label,
+	    axes_state->ax_model_linewidth);
 }
 
 void
@@ -185,14 +233,14 @@ draw_v_axes()
   point_t v_ap;			/* axes position in view coordinates */
 
   VSET(v_ap,
-       mged_variables->v_axes_pos[X] * INV_GED,
-       mged_variables->v_axes_pos[Y] * INV_GED / dmp->dm_aspect,
+       axes_state->ax_view_pos[X] * INV_GED,
+       axes_state->ax_view_pos[Y] * INV_GED / dmp->dm_aspect,
        0.0);
 
   draw_axes(v_ap,
-	    Viewrot,
-	    mged_variables->v_axes_size * INV_GED,
-	    color_scheme->view_axes,
-	    color_scheme->view_axes_label,
-	    mged_variables->v_axes_linewidth);
+	    view_state->vs_Viewrot,
+	    axes_state->ax_view_size * INV_GED,
+	    color_scheme->cs_view_axes,
+	    color_scheme->cs_view_axes_label,
+	    axes_state->ax_view_linewidth);
 }
