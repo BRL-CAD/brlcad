124ed5dc8d51a859816cb7698a10be1fd19c0037
Michael John Muuss <mike@brlcad.org>
1998-11-19 02:16:46 +0000

    Initial support for multispectral stuff
    svn:revision:15793
    cvs:account:mike
    cvs:branch:trunk

--- h/shadework.h
+++ h/shadework.h
@@ -15,15 +15,25 @@
  *			S H A D E W O R K
  */
 struct shadework {
+/* XXX At least the first three of these need to be spectral curves */
 	fastf_t		sw_transmit;	/* 0.0 -> 1.0 */
 	fastf_t		sw_reflect;	/* 0.0 -> 1.0 */
-	fastf_t		sw_refrac_index;
 	fastf_t		sw_extinction;	/* extinction coeff, mm^-1 */
+	fastf_t		sw_refrac_index;
+#if RT_MULTISPECTRAL
+	struct rt_tabdata *msw_color;
+	struct rt_tabdata *msw_basecolor;
+#else
 	fastf_t		sw_color[3];	/* shaded color */
 	fastf_t		sw_basecolor[3]; /* base color */
+#endif
 	struct hit	sw_hit;		/* ray hit (dist,point,normal) */
 	struct uvcoord	sw_uv;
+#if RT_MULTISPECTRAL
+	struct rt_tabdata *msw_intensity[SW_NLIGHTS];
+#else
 	fastf_t		sw_intensity[3*SW_NLIGHTS]; /* light intensities */
+#endif
 	fastf_t		sw_tolight[3*SW_NLIGHTS];   /* light directions */
 	char		*sw_visible[SW_NLIGHTS]; /* visibility flags/ptrs */
 	int		sw_xmitonly;	/* flag: need sw_transmit only */
