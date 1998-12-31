4f9c04733e7ace31f1bf42d6a0f3f1b5e31e6904
Michael John Muuss <mike@brlcad.org>
1998-12-31 02:33:06 +0000

    Improved error checking, better messages. Depends on machinetype.sh being installed, which is important.
    svn:revision:15982
    cvs:account:mike
    cvs:branch:trunk

--- sh/brlcad-check.sh
+++ sh/brlcad-check.sh
@@ -13,9 +13,65 @@
 SHELL=/bin/sh
 export SHELL
 
-eval `sh sh/machinetype.sh -v`
+# At this point, we expect machinetype.sh to have been installed.
+eval `machinetype.sh -v`
 BRLCAD_ROOT=$BASEDIR
 export BRLCAD_ROOT
+
+BINDIR=$BASEDIR/bin
+
+############################################################################
+#
+# Path & Directory sanity check
+# Make sure that BINDIR is in the current user's search path
+# For this purpose, specifically exclude "dot" from the check.
+#
+# Unlike the code in setup.sh, this version only checks, it
+# does not create/modify anything.
+#
+############################################################################
+echo verify.sh: Verifying that ${BINDIR} is in your search path.
+PATH_ELEMENTS=`echo $PATH | sed 's/^://
+				s/:://g
+				s/:$//
+				s/:\\.:/:/g
+				s/:/ /g'`
+
+not_found=1		# Assume cmd not found
+for PREFIX in ${PATH_ELEMENTS}
+do
+	if test ${PREFIX} = ${BINDIR}
+	then
+		# This was -x, but older BSD systems don't do -x.
+		if test -d ${PREFIX}
+		then
+			# all is well
+			not_found=0
+			break
+		fi
+		echo "$0 WARNING:  ${PREFIX} is in the search path, but is not a directory."
+		exit 1
+	fi
+
+	# Make sure that there are no conflicting files earlier in path.
+	if test -f ${PREFIX}/machinetype.sh -o -f ${PREFIX}/cake
+	then
+		echo " "
+		echo "$0 ERROR: Different version of BRL-CAD detected in ${PREFIX},"
+		echo " which is earlier in your search path than ${BINDIR}."
+		echo " Please place ${BINDIR} earlier in your PATH."
+		exit 2
+	fi
+done
+if test ${not_found} -ne 0
+then
+	echo "$0 ERROR:  ${BINDIR} (BINDIR) is not in your Shell search path!"
+	echo "$0 ERROR:  BRL-CAD setup can not proceed until this has been fixed."
+	echo "$0 ERROR:  Consult installation directions for more information,"
+	echo "$0 ERROR:  file: install.doc, section INSTALLATION DIRECTORIES."
+	exit 3		# Die
+fi
+
 
 ############################################################################
 #
@@ -27,7 +83,7 @@ export BRLCAD_ROOT
 #  the results.
 #
 ############################################################################
-echo Comparing settings between machinetype.sh and Cakefile.defs
+echo verify.sh: Comparing machinetype.sh and Cakefile.defs
 
 IN_FILE=/tmp/verify_in$$
 OUT_FILE=/tmp/verify_out$$
@@ -61,14 +117,14 @@ if test $? -ne 0
 then
 	/bin/rm -f ${IN_FILE} ${OUT_FILE}
 	echo "$0: cake returned error code"
-	exit 1
+	exit 4
 fi
 
 if test ! -f ${OUT_FILE}
 then
 	/bin/rm -f ${IN_FILE} ${OUT_FILE}
 	echo "$0: cake run failed to create expected output file"
-	exit 1
+	exit 5
 fi
 
 # Source the output file as a shell script, to set C_* variables here.
@@ -84,9 +140,9 @@ then
 	echo "$0 ERROR:  machinetype.sh claims ${MACHINE} ${UNIXTYPE} ${HAS_TCP}"
 	echo "$0 ERROR:  Cakefile.defs claims ${C_MACHINE} ${C_UNIXTYPE} ${C_HAS_TCP}"
 	echo "It may be useful to examine ${IN_FILE} and ${OUT_FILE}"
-	exit 1		# Die
+	exit 6		# Die
 fi
 
-echo "OK: PATH, cake, machinetype.sh, and Cakefile.defs all match."
+echo " OK: PATH, cake, machinetype.sh, and Cakefile.defs are set properly."
 /bin/rm -f ${IN_FILE} ${OUT_FILE}
 exit 0
