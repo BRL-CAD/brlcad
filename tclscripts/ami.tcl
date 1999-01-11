47500a4e9c68513cad2422b868dc6faa97b578d0
John Anderson <daytonajra@gmail.com>
1999-01-11 20:30:06 +0000

    Added code to look for tclsh or tclsh8.0.
    svn:revision:16029
    cvs:account:jra
    cvs:branch:trunk

--- tclscripts/ami.tcl
+++ tclscripts/ami.tcl
@@ -1,6 +1,26 @@
 #!/bin/sh
+# this is a comment \
+eval `machinetype.sh -b`
+# this is a comment \
+if test -x ${BASEDIR}/bin/tclsh
+# this is a comment \
+then
+# this is a comment \
+	TCLSH_NAME=tclsh
+# this is a comment \
+elif test -x /usr/local/bin/tclsh8.0 
+# this is a comment \
+then
+# this is a comment \
+	TCLSH_NAME=tclsh8.0
+# this is a comment \
+else
+# this is a comment \
+	TCLSH_NAME=tclsh
+# this is a comment \
+fi
 # the next line restarts using tclsh \
-exec tclsh "$0" "$@"
+exec $TCLSH_NAME "$0" "$@"
 
 foreach arg $argv {
     catch { auto_mkindex $arg *.tcl }
