877d5b40ff1ec69d57dc1fe891e6cb67ac0c5119
Christopher Sean Morrison <brlcad@gmail.com>
1999-01-12 05:53:16 +0000

    modified wavelet.c throughout (minor changes) changes include extra notes at beginning, variables renamed, and changed a divide by 2.0 to a multiply by 0.5
    svn:revision:16037
    cvs:account:morrison
    cvs:branch:trunk

--- libbn/wavelet.c
+++ libbn/wavelet.c
@@ -2,19 +2,33 @@
  *
  *  Wavelet decompose/reconstruct operations
  *
- *	bn_wlt_1d_double_decompose(tbuf, buf, dimen, depth, limit)
- *	bn_wlt_1d_float_decompose (tbuf, buf, dimen, depth, limit)
- *	bn_wlt_1d_char_decompose  (tbuf, buf, dimen, depth, limit)
- *	bn_wlt_1d_short_decompose (tbuf, buf, dimen, depth, limit)
- *	bn_wlt_1d_int_decompose   (tbuf, buf, dimen, depth, limit)
- *	bn_wlt_1d_long_decompose  (tbuf, buf, dimen, depth, limit)
+ *	bn_wlt_1d_double_decompose(tbuffer, buffer, dimen, channels, limit)
+ *	bn_wlt_1d_float_decompose (tbuffer, buffer, dimen, channels, limit)
+ *	bn_wlt_1d_char_decompose  (tbuffer, buffer, dimen, channels, limit)
+ *	bn_wlt_1d_short_decompose (tbuffer, buffer, dimen, channels, limit)
+ *	bn_wlt_1d_int_decompose   (tbuffer, buffer, dimen, channels, limit)
+ *	bn_wlt_1d_long_decompose  (tbuffer, buffer, dimen, channels, limit)
  *
- *	bn_wlt_1d_double_reconstruct(tbuf, buf, dimen, depth, sub_sz, limit)
- *	bn_wlt_1d_float_reconstruct (tbuf, buf, dimen, depth, sub_sz, limit)
- *	bn_wlt_1d_char_reconstruct  (tbuf, buf, dimen, depth, sub_sz, limit)
- *	bn_wlt_1d_short_reconstruct (tbuf, buf, dimen, depth, sub_sz, limit)
- *	bn_wlt_1d_int_reconstruct   (tbuf, buf, dimen, depth, sub_sz, limit)
- *	bn_wlt_1d_long_reconstruct  (tbuf, buf, dimen, depth, sub_sz, limit)
+ *	bn_wlt_1d_double_reconstruct(tbuffer, buffer, dimen, channels, sub_sz, limit)
+ *	bn_wlt_1d_float_reconstruct (tbuffer, buffer, dimen, channels, sub_sz, limit)
+ *	bn_wlt_1d_char_reconstruct  (tbuffer, buffer, dimen, channels, sub_sz, limit)
+ *	bn_wlt_1d_short_reconstruct (tbuffer, buffer, dimen, channels, sub_sz, limit)
+ *	bn_wlt_1d_int_reconstruct   (tbuffer, buffer, dimen, channels, sub_sz, limit)
+ *	bn_wlt_1d_long_reconstruct  (tbuffer, buffer, dimen, channels, sub_sz, limit)
+ *
+ *	bn_wlt_2d_double_decompose(tbuffer, buffer, dimen, channels, limit)
+ *	bn_wlt_2d_float_decompose (tbuffer, buffer, dimen, channels, limit)
+ *	bn_wlt_2d_char_decompose  (tbuffer, buffer, dimen, channels, limit)
+ *	bn_wlt_2d_short_decompose (tbuffer, buffer, dimen, channels, limit)
+ *	bn_wlt_2d_int_decompose   (tbuffer, buffer, dimen, channels, limit)
+ *	bn_wlt_2d_long_decompose  (tbuffer, buffer, dimen, channels, limit)
+ *
+ *	bn_wlt_2d_double_reconstruct(tbuffer, buffer, dimen, channels, sub_sz, limit)
+ *	bn_wlt_2d_float_reconstruct (tbuffer, buffer, dimen, channels, sub_sz, limit)
+ *	bn_wlt_2d_char_reconstruct  (tbuffer, buffer, dimen, channels, sub_sz, limit)
+ *	bn_wlt_2d_short_reconstruct (tbuffer, buffer, dimen, channels, sub_sz, limit)
+ *	bn_wlt_2d_int_reconstruct   (tbuffer, buffer, dimen, channels, sub_sz, limit)
+ *	bn_wlt_2d_long_reconstruct  (tbuffer, buffer, dimen, channels, sub_sz, limit)
  *
  *	
  *  
@@ -33,55 +47,58 @@
  *
  *
  *
- *  bn_wlt_1d_*_decompose(tbuf, buf, dimen, depth, limit)
+ *  bn_wlt_1d_*_decompose(tbuffer, buffer, dimen, channels, limit)
  *  Parameters:
- *	tbuf	a temporary data buffer 1/2 as large as "buf". See (1) below.
- *	buf	pointer to the data to be decomposed
- *	dimen	The number of samples in the data buffer 
- *	depth	The number of values per sample
- *	limit	The extent of the decomposition
+ *	tbuffer     a temporary data buffer 1/2 as large as "buffer". See (1) below.
+ *	buffer      pointer to the data to be decomposed
+ *	dimen    the number of samples in the data buffer 
+ *	channels the number of values per sample
+ *	limit    the extent of the decomposition
  *
- *  Perform a Haar wavelet decomposition on the data in buffer "buf".  The
- *  decomposition is done "in place" on the data, hence the values in "buf"
+ *  Perform a Haar wavelet decomposition on the data in buffer "buffer".  The
+ *  decomposition is done "in place" on the data, hence the values in "buffer"
  *  are not preserved, but rather replaced by their decomposition.
  *  The number of original samples in the buffer (parameter "dimen") and the
  *  decomposition limit ("limit") must both be a power of 2 (e.g. 512, 1024).
  *  The buffer is decomposed into "average" and "detail" halves until the
  *  size of the "average" portion reaches "limit".  Simultaneous 
  *  decomposition of multi-plane (e.g. pixel) data, can be performed by
- *  indicating the number of planes in the "depth" parameter.
+ *  indicating the number of planes in the "channels" parameter.
  *  
  *  (1) The process requires a temporary buffer which is 1/2 the size of the
- *  longest span to be decomposed.  If the "tbuf" argument is non-null then
+ *  longest span to be decomposed.  If the "tbuffer" argument is non-null then
  *  it is a pointer to a temporary buffer.  If the pointer is NULL, then a
  *  local temporary buffer will be allocated (and freed).
  *
  *  Examples:
- *	double dbuf[512], cbuf[256];
+ *	double dbuffer[512], cbuffer[256];
  *	...
- *	bn_wlt_1d_double_decompose(cbuf, dbuf, 512, 1, 1);
+ *	bn_wlt_1d_double_decompose(cbuffer, dbuffer, 512, 1, 1);
  *
- *    performs complete decomposition on the data in array "dbuf".
+ *    performs complete decomposition on the data in array "dbuffer".
  *
- *	double buf[3][512];	 /_* 512 samples, 3 values/sample (e.g. RGB?)*_/
- *	double tbuf[3][256];	 /_* the temporary buffer *_/
+ *	double buffer[3][512];	 /_* 512 samples, 3 values/sample (e.g. RGB?)*_/
+ *	double tbuffer[3][256];	 /_* the temporary buffer *_/
  *	...
- *	bn_wlt_1d_double_decompose(tbuf, buf, 512, 3, 1);
+ *	bn_wlt_1d_double_decompose(tbuffer, buffer, 512, 3, 1);
  *
- *    This will completely decompose the data in buf.  The first sample will
+ *    This will completely decompose the data in buffer.  The first sample will
  *    be the average of all the samples.  Alternatively:
  *
- *	bn_wlt_1d_double_decompose(tbuf, buf, 512, 3, 64);
+ *	bn_wlt_1d_double_decompose(tbuffer, buffer, 512, 3, 64);
  *
- *    decomposes buf into a 64-sample "average image" and 3 "detail" sets.
+ *    decomposes buffer into a 64-sample "average image" and 3 "detail" sets.
  *
  *
  *
- *  bn_wlt_1d_*_reconstruct(tbuf, buf, dimen, depth, sub_sz, limit)
+ *  bn_wlt_1d_*_reconstruct(tbuffer, buffer, dimen, channels, sub_sz, limit)
  *
  *
  *  Author -
  *	Lee A. Butler
+ *
+ *  Modifications -
+ *      Christopher Sean Morrison
  *  
  *  Source -
  *	The U. S. Army Research Laboratory
@@ -90,26 +107,12 @@
  *  Distribution Status -
  *	Public Domain, Distribution Unlimited.
  */
-#ifndef lint
-static char RCSid[] = "@(#)$Header$ (ARL)";
-#endif
+
 #include <stdio.h>
 #include "machine.h"
 #include "bu.h"
 #include "vmath.h"
 #include "bn.h"
-
-#define CK_POW_2(dimen) { register unsigned long j; register int ok;\
-	for (ok=0, j=0 ; j < sizeof(unsigned long) * 8 ; j++) { \
-		if ( (1<<j) == dimen) { ok = 1;  break; } \
-	} \
-	if ( ! ok ) { \
-		bu_log("%s:%d Dimension %d should be power of 2 (%d)\n", \
-			__FILE__, __LINE__, dimen, j); \
-		bu_bomb("CK_POW_2"); \
-	} \
-}
-
 
 
 #ifdef __STDC__
@@ -123,11 +126,11 @@ static char RCSid[] = "@(#)$Header$ (ARL)";
 #define make_wlt_1d_decompose(DATATYPE)  \
 void \
 decompose_1d(DATATYPE) \
-( tbuf, buf, dimen, depth, limit ) \
-DATATYPE *tbuf;		/* temporary buffer */ \
-DATATYPE *buf;		/* data buffer */ \
+( tbuffer, buffer, dimen, channels, limit ) \
+DATATYPE *tbuffer;		/* temporary buffer */ \
+DATATYPE *buffer;		/* data buffer */ \
 unsigned long dimen;	/* # of samples in data buffer */ \
-unsigned long depth;	/* # of data values per sample */ \
+unsigned long channels;	/* # of data values per sample */ \
 unsigned long limit;	/* extent of decomposition */ \
 { \
 	register DATATYPE *detail; \
@@ -136,12 +139,13 @@ unsigned long limit;	/* extent of decomposition */ \
 	unsigned long half_size; \
 	int do_free = 0; \
 	unsigned long x, x_tmp, d, i, j; \
+	register fastf_t onehalf = (fastf_t)0.5; \
 \
 	CK_POW_2( dimen ); \
 \
-	if ( ! tbuf ) { \
-		tbuf = (DATATYPE *)bu_malloc( \
-				(dimen/2) * depth * sizeof( *buf ), \
+	if ( ! tbuffer ) { \
+		tbuffer = (DATATYPE *)bu_malloc( \
+				(dimen/2) * channels * sizeof( *buffer ), \
 				"1d wavelet buffer"); \
 		do_free = 1; \
 	} \
@@ -153,31 +157,31 @@ unsigned long limit;	/* extent of decomposition */ \
 \
 		half_size = img_size/2; \
 		 \
-		detail = tbuf; \
-		avg = buf; \
+		detail = tbuffer; \
+		avg = buffer; \
 \
 		for ( x=0 ; x < img_size ; x += 2 ) { \
-			x_tmp = x*depth; \
+			x_tmp = x*channels; \
 \
-			for (d=0 ; d < depth ; d++, avg++, detail++) { \
+			for (d=0 ; d < channels ; d++, avg++, detail++) { \
 				i = x_tmp + d; \
-				j = i + depth; \
-				*detail = (buf[i] - buf[j]) / 2.0; \
-				*avg    = (buf[i] + buf[j]) / 2.0; \
+				j = i + channels; \
+				*detail = (buffer[i] - buffer[j]) * onehalf; \
+				*avg    = (buffer[i] + buffer[j]) * onehalf; \
 			} \
 		} \
 \
 		/* "avg" now points to the first element AFTER the "average \
 		 * image" section, and hence is the START of the "image  \
 		 * detail" portion.  Convenient, since we now want to copy \
-		 * the contents of "tbuf" (which holds the image detail) into \
+		 * the contents of "tbuffer" (which holds the image detail) into \
 		 * place. \
 		 */ \
-		memcpy(avg, tbuf, sizeof(*buf) * depth * half_size); \
+		memcpy(avg, tbuffer, sizeof(*buffer) * channels * half_size); \
 	} \
 	 \
 	if (do_free) \
-		bu_free( (genptr_t)tbuf, "1d wavelet buffer"); \
+		bu_free( (genptr_t)tbuffer, "1d wavelet buffer"); \
 }
 
 
@@ -190,11 +194,11 @@ unsigned long limit;	/* extent of decomposition */ \
 #define make_wlt_1d_reconstruct( DATATYPE ) \
 void \
 reconstruct(DATATYPE) \
-( tbuf, buf, dimen, depth, subimage_size, limit )\
-DATATYPE *tbuf; \
-DATATYPE *buf; \
+( tbuffer, buffer, dimen, channels, subimage_size, limit )\
+DATATYPE *tbuffer; \
+DATATYPE *buffer; \
 unsigned long dimen; \
-unsigned long depth; \
+unsigned long channels; \
 unsigned long subimage_size; \
 unsigned long limit; \
 { \
@@ -209,13 +213,27 @@ unsigned long limit; \
 	CK_POW_2( dimen ); \
 	CK_POW_2( limit ); \
 \
-	/* XXX check for: \
-	 * subimage_size < dimen && subimage_size < limit \
-	 * limit <= dimen \
-	 */ \
+        if ( ! (subimage_size < dimen) ) { \
+		bu_log("%s:%d Dimension %d should be greater than subimage size (%d)\n", \
+			__FILE__, __LINE__, dimen, subimage_size); \
+		bu_bomb("reconstruct"); \
+	} \
 \
-	if ( ! tbuf ) { \
-		tbuf = ( DATATYPE *)bu_malloc((dimen/2) * depth * sizeof( *buf ), \
+        if ( ! (subimage_size < limit) ) { \
+		bu_log("%s:%d Channels limit %d should be greater than subimage size (%d)\n", \
+			__FILE__, __LINE__, limit, subimage_size); \
+		bu_bomb("reconstruct"); \
+	} \
+\
+        if ( ! (limit <= dimen) ) { \
+		bu_log("%s:%d Dimension %d should be greater than or equal to the channels limit (%d)\n", \
+			__FILE__, __LINE__, dimen, limit); \
+		bu_bomb("reconstruct"); \
+	} \
+\
+\
+	if ( ! tbuffer ) { \
+		tbuffer = ( DATATYPE *)bu_malloc((dimen/2) * channels * sizeof( *buffer ), \
 				"1d wavelet reconstruct tmp buffer"); \
 		do_free = 1; \
 	} \
@@ -226,27 +244,27 @@ unsigned long limit; \
 	for (img_size=subimage_size ; img_size < limit ; img_size=dbl_size) { \
 		dbl_size = img_size * 2; \
 \
-		d = img_size * depth; \
-		detail = &buf[ d ]; \
+		d = img_size * channels; \
+		detail = &buffer[ d ]; \
 \
 		/* copy the original or "average" data to temporary buffer */ \
-		avg = tbuf; \
-		memcpy(avg, buf, sizeof(*buf) * d ); \
+		avg = tbuffer; \
+		memcpy(avg, buffer, sizeof(*buffer) * d ); \
 \
 \
 		for (x=0 ; x < dbl_size ; x += 2 ) { \
-			x_tmp = x * depth; \
-			for (d=0 ; d < depth ; d++, avg++, detail++ ) { \
+			x_tmp = x * channels; \
+			for (d=0 ; d < channels ; d++, avg++, detail++ ) { \
 				i = x_tmp + d; \
-				j = i + depth; \
-				buf[i] = *avg + *detail; \
-				buf[j] = *avg - *detail; \
+				j = i + channels; \
+				buffer[i] = *avg + *detail; \
+				buffer[j] = *avg - *detail; \
 			} \
 		} \
 	} \
 \
 	if (do_free) \
-		bu_free( (genptr_t)tbuf, \
+		bu_free( (genptr_t)tbuffer, \
 			"1d wavelet reconstruct tmp buffer"); \
 }
 
@@ -280,11 +298,11 @@ make_wlt_1d_reconstruct(long)
 #define make_wlt_2d_decompose(DATATYPE) \
 void \
 decompose_2d(DATATYPE) \
-(tbuf, buf, dimen, depth, limit) \
-DATATYPE *tbuf; \
-DATATYPE *buf; \
+(tbuffer, buffer, dimen, channels, limit) \
+DATATYPE *tbuffer; \
+DATATYPE *buffer; \
 unsigned long dimen; \
-unsigned long depth; \
+unsigned long channels; \
 unsigned long limit; \
 { \
 	register DATATYPE *detail; \
@@ -293,17 +311,16 @@ unsigned long limit; \
 	unsigned long img_size; \
 	unsigned long half_size; \
 	unsigned long x, y, x_tmp, y_tmp, d, i, j; \
-	int do_free; \
+	int do_free = 0; \
+	register fastf_t onehalf = (fastf_t)0.5; \
 \
 	CK_POW_2( dimen ); \
 \
-	if ( ! tbuf ) { \
-		tbuf = (DATATYPE *)bu_malloc( \
-				(dimen/2) * depth * sizeof( *buf ), \
+	if ( ! tbuffer ) { \
+		tbuffer = (DATATYPE *)bu_malloc( \
+				(dimen/2) * channels * sizeof( *buffer ), \
 				"1d wavelet buffer"); \
 		do_free = 1; \
-	} else { \
-		do_free = 0; \
 	} \
 \
 	/* each iteration of this loop decomposes the data into 4 quarters: \
@@ -315,47 +332,47 @@ unsigned long limit; \
 \
 		/* do a horizontal detail decomposition first */ \
 		for (y=0 ; y < img_size ; y++ ) { \
-			y_tmp = y * dimen * depth; \
+			y_tmp = y * dimen * channels; \
 \
-			detail = tbuf; \
-			avg = &buf[y_tmp]; \
+			detail = tbuffer; \
+			avg = &buffer[y_tmp]; \
 \
 			for (x=0 ; x < img_size ; x += 2 ) { \
-				x_tmp = x*depth + y_tmp; \
+				x_tmp = x*channels + y_tmp; \
 \
-				for (d=0 ; d < depth ; d++, avg++, detail++){ \
+				for (d=0 ; d < channels ; d++, avg++, detail++){ \
 					i = x_tmp + d; \
-					j = i + depth; \
-					*detail = (buf[i] - buf[j]) / 2.0; \
-					*avg    = (buf[i] + buf[j]) / 2.0; \
+					j = i + channels; \
+					*detail = (buffer[i] - buffer[j]) * onehalf; \
+					*avg    = (buffer[i] + buffer[j]) * onehalf; \
 				} \
 			} \
 			/* "avg" now points to the first element AFTER the \
 			 * "average image" section, and hence is the START \
 			 * of the "image detail" portion.  Convenient, since \
-			 * we now want to copy the contents of "tbuf" (which \
+			 * we now want to copy the contents of "tbuffer" (which \
 			 * holds the image detail) into place. \
 			 */ \
-			memcpy(avg, tbuf, sizeof(*buf) * depth * half_size); \
+			memcpy(avg, tbuffer, sizeof(*buffer) * channels * half_size); \
 		} \
 \
 		/* Now do the vertical decomposition */ \
 		for (x=0 ; x < img_size ; x ++ ) { \
-			x_tmp = x*depth; \
+			x_tmp = x*channels; \
 \
-			detail = tbuf; \
-			avg = &buf[x_tmp]; \
+			detail = tbuffer; \
+			avg = &buffer[x_tmp]; \
 \
 			for (y=0 ; y < img_size ; y += 2) { \
-				y_tmp =y*dimen*depth + x_tmp; \
+				y_tmp =y*dimen*channels + x_tmp; \
 \
-				for (d=0 ; d < depth ; d++, avg++, detail++) { \
+				for (d=0 ; d < channels ; d++, avg++, detail++) { \
 					i = y_tmp + d; \
-					j = i + dimen*depth; \
-					*detail = (buf[i] - buf[j]) / 2.0; \
-					*avg    = (buf[i] + buf[j]) / 2.0; \
+					j = i + dimen*channels; \
+					*detail = (buffer[i] - buffer[j]) * onehalf; \
+					*avg    = (buffer[i] + buffer[j]) * onehalf; \
 				} \
-				avg += (dimen-1)*depth; \
+				avg += (dimen-1)*channels; \
 			} \
 \
 			/* "avg" now points to the element ABOVE the \
@@ -365,12 +382,12 @@ unsigned long limit; \
 			 * There is no memcpy for the columns, so we have to \
 			 * copy the data back to the user buffer ourselves. \
 			 */ \
-			detail = tbuf; \
+			detail = tbuffer; \
 			for (y=half_size ; y < img_size ; y++) { \
-				for (d=0; d < depth ; d++) { \
+				for (d=0; d < channels ; d++) { \
 					*avg++ = *detail++; \
 				} \
-				avg += (dimen-1)*depth; \
+				avg += (dimen-1)*channels; \
 			} \
 		} \
 	} \
