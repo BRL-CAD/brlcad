c27dc07a385312462f3dde26c6e19eecb2c98f01
Michael John Muuss <mike@brlcad.org>
1999-06-03 01:52:40 +0000

    Shaders don't live here any more.
    svn:revision:16694
    cvs:account:mike
    cvs:branch:trunk

--- rt/fast.sh
+++ rt/fast.sh
@@ -10,27 +10,7 @@ cake \
  material.o \
  mathtab.o \
  opt.o \
- refract.o \
- sh_air.o \
- sh_fire.o &
-
-cake \
- sh_cloud.o \
- sh_cook.o \
- sh_light.o &
-
-cake \
- sh_marble.o \
- sh_plastic.o \
- sh_points.o \
- sh_spm.o &
-
-cake \
- sh_stack.o \
- sh_stxt.o \
- sh_gauss.o \
- sh_noise.o \
- sh_text.o &
+ refract.o &
 
 cake \
  view.o \
