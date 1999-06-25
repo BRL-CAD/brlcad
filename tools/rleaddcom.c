9952076505a9c2d504643cde3a49143824df9f4e
Michael John Muuss <mike@brlcad.org>
1999-06-25 18:42:52 +0000

    Eliminated warning on FreeBSD
    svn:revision:16741
    cvs:account:mike
    cvs:branch:trunk

--- tools/rleaddcom.c
+++ tools/rleaddcom.c
@@ -39,9 +39,7 @@
 #include "rle.h"
 
 #include <sys/types.h>
-#if defined(vax) && defined(BSD) && BSD < 44
 #undef BSD	/* /usr/include/sys/param.h redefines this */
-#endif
 #include <sys/param.h>			/* for MAXPATHLEN */
 #include <sys/stat.h>
 
