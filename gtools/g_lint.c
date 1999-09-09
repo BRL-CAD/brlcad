cf4fcbcc058972c66bc0f89f74d69248b7f6a388
Paul Tanenbaum <pjt@brlcad.org>
1999-09-09 15:50:31 +0000

    char *r1name, *r2name are now CONST char *
    svn:revision:16884
    cvs:account:pjt
    cvs:branch:trunk

--- gtools/g_lint.c
+++ gtools/g_lint.c
@@ -283,7 +283,7 @@ void	*v;
 int	show_origin;
 
 {
-    char		*r1name, *r2name;
+    CONST char		*r1name, *r2name;
     struct g_lint_ovlp	*op = (struct g_lint_ovlp *) v;
     struct g_lint_seg	*sp;
 
