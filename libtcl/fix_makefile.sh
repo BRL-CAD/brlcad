3413ffd610168fd92b807893651a828b79970137
John Anderson <daytonajra@gmail.com>
1999-10-21 19:50:52 +0000

    Mods to use the new TCLTK_VERS macro
    svn:revision:16956
    cvs:account:jra
    cvs:branch:trunk

--- libtcl/fix_makefile.sh
+++ libtcl/fix_makefile.sh
@@ -3,7 +3,4 @@
 sed -e s@lib/tcl\$\(VERSION\)@tcl\$\(VERSION\)@ Makefile |\
 	 sed -e s@\(prefix\)/include@\(prefix\)/include/brlcad@ |\
 	 sed -e "s@\${\C\C}  \${TCLSH_OBJS}@\${\C\C} $LDFLAGS \${TCLSH_OBJS}@" |\
-	 sed -e "s@\${\C\C}  \${TCLTEST_OBJS}@\${\C\C} $LDFLAGS \${TCLTEST_OBJS}@" |\
-	 sed -e s@tcl8.2@tcl@ |\
-	 sed -e s@tcl80.so.1.0@tcl.so@ |\
-	 sed -e s@tcl80@tcl@
+	 sed -e "s@\${\C\C}  \${TCLTEST_OBJS}@\${\C\C} $LDFLAGS \${TCLTEST_OBJS}@"
