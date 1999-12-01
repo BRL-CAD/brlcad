19878b9d659889ddcb1551b100bfdf022a2f1393
Robert Parker <unacknowledged@users.noreply.github.com>
1999-12-01 16:03:37 +0000

    *- add proc init_dm_obj
    svn:revision:17175
    cvs:account:bparker
    cvs:branch:trunk

--- tclscripts/libdm.tcl
+++ tclscripts/libdm.tcl
@@ -1,8 +1,19 @@
-proc _init_dm { func win } {
-    $func $win
-    catch { init_dm $win }
+proc _init_dm { func w } {
+    $func $w
+    catch { init_dm $w }
 }
 
-proc bind_dm { name } {
+proc bind_dm { w } {
 }
 
+## - init_dm_obj
+#
+# Called to initialize display manager objects.
+#
+proc init_dm_obj { w } {
+    if ![winfo exists $w] {
+	return
+    }
+
+    bind $w <Configure> {catch {%W configure}}
+}
