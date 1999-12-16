aeaf0d671a957afb43f53d46c3a241039be26701
John Anderson <daytonajra@gmail.com>
1999-12-16 17:37:48 +0000

    Added a case with repeated vertices
    svn:revision:17243
    cvs:account:jra
    cvs:branch:trunk

--- proc-db/bottest.c
+++ proc-db/bottest.c
@@ -38,7 +38,7 @@ main( argc, argv )
 int argc;
 char *argv[];
 {
-	fastf_t vertices[12];
+	fastf_t vertices[36];
 	int faces[12];
 	struct bu_bitv *face_mode;
 	fastf_t thickness[4];
@@ -260,6 +260,43 @@ char *argv[];
 	faces[11] = 1;
 
 	mk_bot( stdout, "bot_cw_plate", RT_BOT_PLATE, RT_BOT_CW, 0, 4, 4, vertices, faces, thickness, face_mode );
+
+	/* Make a bot with duplicate vertices to test the "fuse" and "condense" code */
+	VSET( vertices, 0.0, 0.0, 0.0 )
+	VSET( &vertices[3], 0.0, 100.0, 0.0 )
+	VSET( &vertices[6], 0.0, 100.0, 50.0 )
+	VMOVE( &vertices[9], &vertices[0] )
+	VMOVE( &vertices[12], &vertices[6] )
+	VSET( &vertices[15], 200.0, 0.0, 0.0 )
+	VMOVE( &vertices[18], &vertices[0] )
+	VMOVE( &vertices[21], &vertices[3] )
+	VMOVE( &vertices[24], &vertices[15] )
+	VMOVE( &vertices[27], &vertices[3] )
+	VMOVE( &vertices[30], &vertices[6] )
+	VMOVE( &vertices[33], &vertices[15] )
+
+	/* face #1 */
+	faces[0] = 0;
+	faces[1] = 1;
+	faces[2] = 2;
+
+	/* face #2 */
+	faces[3] = 3;
+	faces[4] = 4;
+	faces[5] = 5;
+
+	/* face #3 */
+	faces[6] = 6;
+	faces[7] = 7;
+	faces[8] = 8;
+
+	/* face #4 */
+
+	faces[9] = 9;
+	faces[10] = 10;
+	faces[11] = 11;
+
+	mk_bot( stdout, "bot_solid_dup_vs", RT_BOT_SOLID, RT_BOT_UNORIENTED, 0, 12, 4, vertices, faces, (fastf_t *)NULL, (struct bu_bitv *)NULL );
 
 	bu_free( (char *)face_mode, "bottest: face_mode" );
 }
