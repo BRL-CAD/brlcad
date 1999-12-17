40beeb44a7f49828538b915338f062173237c76b
Robert Parker <unacknowledged@users.noreply.github.com>
1999-12-17 16:10:16 +0000

    *- add struct cmdhist to support command history
    svn:revision:17256
    cvs:account:bparker
    cvs:branch:trunk

--- h/cmd.h
+++ h/cmd.h
@@ -1,13 +1,25 @@
 #ifndef SEEN_CMD_H
 #define SEEN_CMD_H
 
+#include "bu.h"
+
 #define MAXARGS 9000
+#define CMD_NULL (int (*)())NULL
 
 struct cmdtab {
   char *ct_name;
   int (*ct_func)();
 };
 
+struct cmdhist {
+  struct bu_list l;
+  struct bu_vls h_command;
+  struct timeval h_start;
+  struct timeval h_finish;
+  int h_status;
+};
+
 extern int do_cmd();
+extern void register_cmds();
 
 #endif /* SEEN_CMD_H */
