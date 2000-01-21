0f4b5e659e01b0c8553352f5bbac305d9b7f1c68
Michael John Muuss <mike@brlcad.org>
2000-01-21 22:52:45 +0000

    fixed error preventing -s50 from rendering a full framebuffer-sized image rather than a zoomed-up 50x50 pixel image.
    svn:revision:17630
    cvs:account:mike
    cvs:branch:trunk

--- rt/main.c
+++ rt/main.c
@@ -265,8 +265,8 @@ char **argv;
 
 		bu_semaphore_acquire( BU_SEM_SYSCALL );
 		/* If fb came out smaller than requested, do less work */
-		if( fb_getwidth(fbp) < xx )  width = fb_getwidth(fbp);
-		if( fb_getheight(fbp) < yy )  height = fb_getheight(fbp);
+		if( fb_getwidth(fbp) < width )  width = fb_getwidth(fbp);
+		if( fb_getheight(fbp) < height )  height = fb_getheight(fbp);
 
 		/* If the fb is lots bigger (>= 2X), zoom up & center */
 		if( width > 0 && height > 0 )  {
