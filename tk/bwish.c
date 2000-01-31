77da605647f10a4028c2807a73fe46f36da08042
John Anderson <daytonajra@gmail.com>
2000-01-31 16:33:35 +0000

    Eliminated an unused variable
    svn:revision:17682
    cvs:account:jra
    cvs:branch:trunk

--- tk/bwish.c
+++ tk/bwish.c
@@ -107,8 +107,6 @@ int
 Tcl_AppInit(interp)
     Tcl_Interp *interp;		/* Interpreter for application. */
 {
-    Tk_Window main;
-
     if (Tcl_Init(interp) == TCL_ERROR) {
 	return TCL_ERROR;
     }
