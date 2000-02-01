411f0714c6abba91bbe0d5edda338f5ef538cd58
Robert Parker <unacknowledged@users.noreply.github.com>
2000-02-01 15:36:38 +0000

    *- mods to use itclsh3.0
    svn:revision:17694
    cvs:account:bparker
    cvs:branch:trunk

--- tclscripts/ami.tcl
+++ tclscripts/ami.tcl
@@ -2,31 +2,33 @@
 # this is a comment \
 eval `machinetype.sh -b`
 # this is a comment \
-if test -x ${BASEDIR}/bin/tclsh
+if test -x ${BASEDIR}/bin/itclsh3.0
 # this is a comment \
 then
 # this is a comment \
-	TCLSH_NAME=${BASEDIR}/bin/tclsh
+	ITCLSH_NAME=${BASEDIR}/bin/itclsh3.0
 # this is a comment \
-elif test -x /usr/local/bin/tclsh8.0 
-# this is a comment \
-then
-# this is a comment \
-	TCLSH_NAME=/usr/local/bin/tclsh8.0
-# this is a comment \
-elif test -x /usr/bin/tclsh8.0 
+elif test -x /usr/local/bin/itclsh3.0 
 # this is a comment \
 then
 # this is a comment \
-	TCLSH_NAME=/usr/bin/tclsh8.0
+	ITCLSH_NAME=/usr/local/bin/itclsh3.0
+# this is a comment \
+elif test -x /usr/bin/itclsh3.0 
+# this is a comment \
+then
+# this is a comment \
+	ITCLSH_NAME=/usr/bin/itclsh3.0
 # this is a comment \
 else
 # this is a comment \
-	TCLSH_NAME=tclsh
+	ITCLSH_NAME=itclsh
 # this is a comment \
 fi
-# the next line restarts using tclsh \
-exec $TCLSH_NAME "$0" "$@"
+# this is a comment \
+echo "ITCLSH_NAME=$ITCLSH_NAME"
+# the next line restarts using itclsh \
+exec $ITCLSH_NAME "$0" "$@"
 
 foreach arg $argv {
     catch { auto_mkindex $arg *.tcl }
