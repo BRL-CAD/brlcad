82bd658b6333b4dd7f0923110b754b04405f0f7c
Michael John Muuss <mike@brlcad.org>
2000-02-11 21:41:55 +0000

    Added -v option. Fixed bug with loosing the -F option
    svn:revision:17774
    cvs:account:mike
    cvs:branch:trunk

--- fb/fb-fb.c
+++ fb/fb-fb.c
@@ -30,6 +30,7 @@ static unsigned char *scanline;		/* 1 scanline pixel buffer */
 static int	scanbytes;		/* # of bytes of scanline */
 static int	scanpix;		/* # of pixels of scanline */
 static int	streamline;		/* # scanlines to do at once */
+static int	verbose;
 
 static char	*in_fb_name;
 static char	*out_fb_name;
@@ -38,7 +39,7 @@ static int	scr_width = 0;		/* screen tracks file if not given */
 static int	scr_height = 0;
 
 static char usage[] = "\
-Usage: fb-fb [-F output_framebuffer]\n\
+Usage: fb-fb [-v] [-F output_framebuffer]\n\
 	input_framebuffer [output_framebuffer]\n";
 
 get_args( argc, argv )
@@ -46,10 +47,13 @@ register char **argv;
 {
 	register int c;
 
-	while ( (c = getopt( argc, argv, "ahiczF:s:w:n:x:y:X:Y:S:W:N:" )) != EOF )  {
+	while ( (c = getopt( argc, argv, "vF:" )) != EOF )  {
 		switch( c )  {
 		case 'F':
 			out_fb_name = optarg;
+			break;
+		case 'v':
+			verbose++;
 			break;
 
 		default:		/* '?' */
@@ -63,7 +67,6 @@ register char **argv;
 	in_fb_name = argv[optind++];
 
 	if( optind >= argc )  {
-		out_fb_name = NULL;
 		return(1);	/* OK */
 	}
 	out_fb_name = argv[optind++];
@@ -88,6 +91,8 @@ char **argv;
 		exit( 1 );
 	}
 
+	if( verbose )
+		fprintf(stderr, "fb-fb: infb=%s, outfb=%s\n", in_fb_name, out_fb_name );
 
 	if( (in_fbp = fb_open( in_fb_name, 0, 0 )) == NULL )  {
 		if( in_fb_name )
@@ -99,6 +104,9 @@ char **argv;
 	scr_width = fb_getwidth(in_fbp);
 	scr_height = fb_getheight(in_fbp);
 
+	if( verbose )
+		fprintf(stderr, "fb-fb: width=%d height=%d\n", scr_width, scr_height);
+
 	if( (out_fbp = fb_open( out_fb_name, scr_width, scr_height )) == FBIO_NULL )  {
 		if( out_fb_name )
 			fprintf(stderr,"fb-fb: unable to open output '%s'\n", out_fb_name );
@@ -106,7 +114,7 @@ char **argv;
 	}
 
 	scanpix = scr_width;			/* # pixels on scanline */
-	streamline = 8;
+	streamline = 64;			/* # scanlines per block */
 	scanbytes = scanpix * streamline * sizeof(RGBpixel);
 	if( (scanline = (unsigned char *)malloc(scanbytes)) == RGBPIXEL_NULL )  {
 		fprintf(stderr,
@@ -117,6 +125,10 @@ char **argv;
 
 	/* Bottom to top with multi-line reads & writes */
 	for( y = 0; y < scr_height; y += streamline )  {
+		if( y+streamline > scr_height )
+			streamline = scr_height-y;
+		if( verbose )
+			fprintf(stderr, "fb-fb: y=%d, nlines=%d\n", y, streamline);
 		n = fb_readrect( in_fbp, 0, y, scr_width, streamline,
 			scanline );
 		if( n <= 0 ) break;
