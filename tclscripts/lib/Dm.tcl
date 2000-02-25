4457bc6a4aae806730079a9915ae0cd8c1562a95
Robert Parker <unacknowledged@users.noreply.github.com>
2000-02-25 21:39:35 +0000

    *- mods to inherit from itk::Widget
    svn:revision:17841
    cvs:account:bparker
    cvs:branch:trunk

--- tclscripts/lib/Dm.tcl
+++ tclscripts/lib/Dm.tcl
@@ -20,79 +20,24 @@
 #	The Dm class wraps LIBDM's display manager object.
 #
 class Dm {
-    protected variable dm ""
-    protected variable width 512
-    protected variable height 512
-    protected variable invWidth ""
-    protected variable aspect ""
-    private variable initializing 1
+    inherit itk::Widget
 
-    public variable name ""
-    public variable istoplevel 1
-    public variable type X
-    public variable size 512
-    public variable title ""
-    public variable listen -1
-    public variable fb_active 0
-    public variable fb_update 1
-    public variable bg "0 0 0"
-    public variable light 0
-    public variable zclip 0
-    public variable zbuffer 0
-    public variable perspective 0
-    public variable debug 0
-    public variable linewidth 0
+    constructor {args} {}
+    destructor {}
 
-    constructor {args} {
-	# process options
-	eval configure $args
+    itk_option define -dmsize dmsize Dmsize {512 512}
+    itk_option define -listen listen Listen -1
+    itk_option define -fb_active fb_active Fb_active 0
+    itk_option define -fb_update fb_update Fb_update 1
+    itk_option define -bg bg Bg {0 0 0}
+    itk_option define -light light Light 0
+    itk_option define -zclip zclip Zclip 0
+    itk_option define -zbuffer zbuffer Zbuffer 0
+    itk_option define -perspective perspective Perspective 0
+    itk_option define -debug debug Debug 0
+    itk_option define -linewidth linewidth Linewidth 0
 
-	# create default name
-	if {$name == ""} {
-	    set name .[string map {:: ""} $this]
-	}
-
-	# create default title
-	if {$title == ""} {
-	    set title $this
-	}
-
-	# create display manager object
-	set dm [dm_open $name $type -t $istoplevel -W $width -N $height]
-
-	# initialize display manager object
-	$dm clear
-	eval bg $bg
-	light $light
-	zclip $zclip
-	zbuffer $zbuffer
-	debug $debug
-	listen $listen
-	linewidth $linewidth
-
-	# event bindings
-	doBindings
-
-	if {$istoplevel} {
-	    wm title $dm $title
-	}
-
-	set initializing 0
-    }
-
-    destructor {
-	$dm listen -1
-	$dm close
-    }
-
-    # methods for handling window events
-    protected method toggle_zclip {}
-    protected method toggle_zbuffer {}
-    protected method toggle_light {}
-    protected method toggle_perspective {}
-    protected method doBindings {}
-
-    # methods that wrap LIBDM display manager object commands
+    # methods that wrap LIBDM-display-manager-object commands
     public method observer {args}
     public method drawBegin {}
     public method drawEnd {}
@@ -118,156 +63,162 @@ class Dm {
     public method refreshfb {}
     public method flush {}
     public method sync {}
-    public method size {args}
+    public method dmsize {args}
     public method get_aspect {}
 
     # new methods
     public method get_name {}
+    public method get_type {}
     public method fb_active {args}
     public method fb_update {args}
+
+    # methods for handling window events
+    protected method toggle_zclip {}
+    protected method toggle_zbuffer {}
+    protected method toggle_light {}
+    protected method toggle_perspective {}
+    protected method doBindings {}
+
+    protected variable dm ""
+    protected variable width 512
+    protected variable height 512
+    protected variable invWidth ""
+    protected variable aspect ""
+
+    private variable type X
 }
 
-configbody Dm::name {
-    if {!$initializing} {
-	return -code error "name is read-only"
-    }
-}
-
-configbody Dm::size {
-    # save size
-    set s $size
-
-    # For now, put back the old value.
-    # If the size really does change, size will
-    # be set in the handle_configure method.
-    set size "$width $height"
-
-    # request a size change
-    eval Dm::size $s
-}
-
-configbody Dm::title {
-    if {$istoplevel && [winfo exists $dm]} {
-	wm title $dm $title
-    }
-}
-
-configbody Dm::istoplevel {
-    if {!$initializing} {
-	return -code error "istoplevel is read-only"
-    }
-}
-
-configbody Dm::type {
-    if {!$initializing} {
-	return -code error "type is read-only"
-    }
-    
-    switch $type {
+body Dm::constructor {_type args} {
+    switch $_type {
 	X -
 	ogl {
+	    set type $_type
 	}
 	default {
 	    set type X
 	}
     }
+
+    itk_component add dm {
+	dm_open $itk_interior.dm $type -t 0 -W $width -N $height
+    } {
+    }
+
+    pack $itk_component(dm) -fill both -expand yes
+
+    # prcess options
+    eval itk_initialize $args
+
+    # initialize display manager object
+    eval Dm::bg $itk_option(-bg)
+    Dm::light $itk_option(-light)
+    Dm::zbuffer $itk_option(-zbuffer)
+    Dm::zclip $itk_option(-zclip)
+    Dm::debug $itk_option(-debug)
+    Dm::listen $itk_option(-listen)
+    Dm::linewidth $itk_option(-linewidth)
+
+    # event bindings
+    doBindings
+}
+
+body Dm::destructor {} {
+    $itk_component(dm) listen -1
+    $itk_component(dm) close
+}
+
+configbody Dm::dmsize {
+    # save size
+    set s $itk_option(-dmsize)
+
+    # For now, put back the old value.
+    # If the size really does change, size will
+    # be set in the handle_configure method.
+    set itk_option(-dmsize) "$width $height"
+
+    # request a size change
+    eval Dm::dmsize $s
 }
 
 configbody Dm::listen {
-    if {!$initializing} {
-	Dm::listen $listen
-    }
+    Dm::listen $itk_option(-listen)
 }
 
 configbody Dm::fb_active {
-    Dm::fb_active $fb_active
+    Dm::fb_active $itk_option(-fb_active)
 }
 
 configbody Dm::fb_update {
-    Dm::fb_update $fb_update
+    Dm::fb_update $itk_option(-fb_update)
 }
 
 configbody Dm::bg {
-    if {!$initializing} {
-	Dm::bg $bg
-    }
+    eval Dm::bg $itk_option(-bg)
 }
 
 configbody Dm::light {
-    if {!$initializing} {
-	Dm::light $light
-    }
+    Dm::light $itk_option(-light)
 }
 
 configbody Dm::zclip {
-    if {!$initializing} {
-	Dm::zclip $zclip
-    }
+    Dm::zclip $itk_option(-zclip)
 }
 
 configbody Dm::zbuffer {
-    if {!$initializing} {
-	Dm::zbuffer $zbuffer
-    }
+    Dm::zbuffer $itk_option(-zbuffer)
 }
 
 configbody Dm::perspective {
-    if {!$initializing} {
-	Dm::perspective $perspective
-    }
+    Dm::perspective $itk_option(-perspective)
 }
 
 configbody Dm::debug {
-    if {!$initializing} {
-	Dm::debug $debug
-    }
+    Dm::debug $itk_option(-debug)
 }
 
 configbody Dm::linewidth {
-    if {!$initializing} {
-	Dm::linewidth $linewidth
-    }
+    Dm::linewidth $itk_option(-linewidth)
 }
 
 body Dm::observer {args} {
-    eval $dm observer $args
+    eval $itk_component(dm) observer $args
 }
 
 body Dm::drawBegin {} {
-    $dm drawBegin
+    $itk_component(dm) drawBegin
 }
 
 body Dm::drawEnd {} {
-    $dm drawEnd
+    $itk_component(dm) drawEnd
 }
 
 # Clear the display manager window
 body Dm::clear {} {
-    $dm clear
+    $itk_component(dm) clear
 }
 
 body Dm::normal {} {
-    $dm normal
+    $itk_component(dm) normal
 }
 
 body Dm::loadmat {mat eye} {
-    $dm loadmat $mat $eye
+    $itk_component(dm) loadmat $mat $eye
 }
 
 body Dm::drawString {str x y size use_aspect} {
-    $dm drawString $str $x $y $size $use_aspect
+    $itk_component(dm) drawString $str $x $y $size $use_aspect
 }
 
 body Dm::drawPoint {x y} {
-    $dm drawPoint $x $y
+    $itk_component(dm) drawPoint $x $y
 }
 
 body Dm::drawLine {x1 y1 x2 y2} {
-    $dm drawLine $x1 $y1 $x2 $y2
+    $itk_component(dm) drawLine $x1 $y1 $x2 $y2
 }
 
 body Dm::drawGeom {args} {
-    eval $dm drawGeom $args
+    eval $itk_component(dm) drawGeom $args
 }
 
 # Get/set the background color
@@ -276,16 +227,16 @@ body Dm::bg {args} {
 	return $bg
     }
 
-    $dm bg $args
+    $itk_component(dm) bg $args
     set bg $args
 }
 
 # Get/set the foreground color
 body Dm::fg {args} {
     if {$args == ""} {
-	$dm fg
+	$itk_component(dm) fg
     } else {
-	$dm fg $args
+	$itk_component(dm) fg $args
     }
 }
 
@@ -295,106 +246,106 @@ body Dm::linewidth {args} {
 	return $linewidth
     }
 
-    $dm linewidth $args
+    $itk_component(dm) linewidth $args
     set linewidth $args
 }
 
 # Get/set the line style
 body Dm::linestyle {args} {
     if {$args == ""} {
-	$dm linestyle
+	$itk_component(dm) linestyle
     } else {
-	$dm linestyle $args
+	$itk_component(dm) linestyle $args
     }
 }
 
 body Dm::handle_configure {} {
-    $dm configure
-    set size [$dm size]
-    set width [lindex $size 0]
-    set height [lindex $size 1]
+    $itk_component(dm) configure
+    set $itk_option(-dmsize) [$itk_component(dm) size]
+    set width [lindex $itk_option(-dmsize) 0]
+    set height [lindex $itk_option(-dmsize) 1]
     set invWidth [expr 1.0 / $width]
     set aspect [expr (1.0 * $width) / $height]
 }
 
 body Dm::zclip {args} {
     if {$args == ""} {
-	return $zclip
+	return $itk_option(-zclip)
     }
 
-    $dm zclip $args
-    set zclip $args
+    $itk_component(dm) zclip $args
+    set itk_option(-zclip) $args
 }
 
 body Dm::zbuffer {args} {
     if {$args == ""} {
-	return $zbuffer
+	return $itk_optoin(-zbuffer)
     }
 
-    $dm zbuffer $args
-    set zbuffer $args
+    $itk_component(dm) zbuffer $args
+    set itk_option(-zbuffer) $args
 }
 
 # Get/set light
 body Dm::light {args} {
     if {$args == ""} {
-	return $light
+	return $itk_option(-light)
     }
 
-    $dm light $args
-    set light $args
+    $itk_component(dm) light $args
+    set itk_option(-light) $args
 }
 
 body Dm::perspective {args} {
     if {$args == ""} {
-	return $perspective
+	return $itk_option(-perspective)
     }
 
-    $dm perspective $args
-    set perspective $args
+    $itk_component(dm) perspective $args
+    set itk_option(-perspective) $args
 }
 
 body Dm::bounds {args} {
-    eval $dm bounds $args
+    eval $itk_component(dm) bounds $args
 }
 
 body Dm::debug {args} {
     if {$args == ""} {
-	return $debug
+	return $itk_option(-debug)
     }
 
-    $dm debug $args
-    set debug $args
+    $itk_component(dm) debug $args
+    set itk_option(-debug) $args
 }
 
 body Dm::listen {args} {
     if {$args == ""} {
-	return $listen
+	return $itk_option(-listen)
     }
 
-    $dm listen $args
-    set listen $args
+    $itk_component(dm) listen $args
+    set itk_option(-listen) $args
 }
 
 
 body Dm::refreshfb {} {
-    $dm refreshfb
+    $itk_component(dm) refreshfb
 }
 
 body Dm::flush {} {
-    $dm flush
+    $itk_component(dm) flush
 }
 
 body Dm::sync {} {
-    $dm sync
+    $itk_component(dm) sync
 }
 
-body Dm::size {args} {
+body Dm::dmsize {args} {
     set nargs [llength $args]
 
     # get display manager window size
     if {$nargs == 0} {
-	return $size
+	return $itk_option(-size)
     }
 
     if {$nargs == 1} {
@@ -407,26 +358,20 @@ body Dm::size {args} {
 	return -code error "size: bad size - $args"
     }
 
-    if {$initializing} {
-	set width $w
-	set height $h
-	set size $args
-    } else {
-	# make request to set display manager window size
-	if {$istoplevel} {
-	    wm geometry $dm $w\x$h
-	} else {
-	    $dm size $w $h
-	}
-    }
+    $itk_component(dm) size $w $h
+    set itk_option(-dmsize) "$w $h"
 }
 
 body Dm::get_aspect {} {
-    $dm get_aspect
+    $itk_component(dm) get_aspect
 }
 
 body Dm::get_name {} {
-    return $dm
+    return $itk_component(dm)
+}
+
+body Dm::get_type {} {
+    return $type
 }
 
 body Dm::fb_active {args} {
@@ -443,10 +388,8 @@ body Dm::fb_active {args} {
 	}
 
 	# update saved value
-	set fb_active $fba
+	set itk_option(-fb_active) $fba
     }
-
-    return $fb_active
 }
 
 body Dm::fb_update {args} {
@@ -463,59 +406,57 @@ body Dm::fb_update {args} {
 	}
 
 	# update saved value
-	set fb_update $fbu
+	set itk_option(-fb_update) $fbu
     }
-
-    return $fb_update
 }
 
 body Dm::toggle_zclip {} {
-    if {$zclip} {
-	$dm zclip 0
-	set zclip 0
+    if {$itk_option(-zclip)} {
+	$itk_component(dm) zclip 0
+	set itk_option(-zclip) 0
     } else {
-	$dm zclip 1
-	set zclip 1
+	$itk_component(dm) zclip 1
+	set itk_option(-zclip) 1
     }
 }
 
 body Dm::toggle_zbuffer {} {
-    if {$zbuffer} {
-	$dm zbuffer 0
-	set zbuffer 0
+    if {$itk_option(-zbuffer)} {
+	$itk_component(dm) zbuffer 0
+	set itk_option(-zbuffer) 0
     } else {
-	$dm zbuffer 1
-	set zbuffer 1
+	$itk_component(dm) zbuffer 1
+	set itk_option(-zbuffer) 1
     }
 }
 
 body Dm::toggle_light {} {
-    if {$light} {
-	$dm light 0
-	set light 0
+    if {$itk_option(-light)} {
+	$itk_component(dm) light 0
+	set itk_option(-light) 0
     } else {
-	$dm light 1
-	set light 1
+	$itk_component(dm) light 1
+	set itk_opton(-light) 1
     }
 }
 
 body Dm::toggle_perspective {} {
-    if {$perspective} {
-	$dm perspective 0
-	set perspective 0
+    if {$itk_option(-perspective)} {
+	$itk_component(dm) perspective 0
+	set itk_option(-perspective) 0
     } else {
-	$dm perspective 1
-	set perspective 1
+	$itk_component(dm) perspective 1
+	set itk_option(-perspective) 1
     }
 }
 
 body Dm::doBindings {} {
-    bind $dm <Enter> "focus $dm;"
-    bind $dm <Configure> "[code $this Dm::handle_configure]; break"
+    bind $itk_component(dm) <Enter> "focus $itk_component(dm);"
+    bind $itk_component(dm) <Configure> "[code $this Dm::handle_configure]; break"
 
     # Key Bindings
-    bind $dm <F2> "$this Dm::toggle_zclip; break"
-    bind $dm <F3> "$this Dm::toggle_perspective; break"
-    bind $dm <F4> "$this Dm::toggle_zbuffer; break"
-    bind $dm <F5> "$this Dm::toggle_light; break"
+    bind $itk_component(dm) <F2> "$this Dm::toggle_zclip; break"
+    bind $itk_component(dm) <F3> "$this Dm::toggle_perspective; break"
+    bind $itk_component(dm) <F4> "$this Dm::toggle_zbuffer; break"
+    bind $itk_component(dm) <F5> "$this Dm::toggle_light; break"
 }
