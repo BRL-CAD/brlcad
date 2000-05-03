1ea6d0f90396007ea0d34c83a0e63cb6a08e4750
Robert Parker <unacknowledged@users.noreply.github.com>
2000-05-03 18:47:49 +0000

    *- remove extra comments
    svn:revision:18144
    cvs:account:bparker
    cvs:branch:trunk

--- libdm/dm_obj.c
+++ libdm/dm_obj.c
@@ -12,10 +12,6 @@
  * Author -
  *	Robert G. Parker
  *
- *  Source -
- *	The U. S. Army Research Laboratory
- *	Aberdeen Proving Ground, Maryland  21005-5068  USA
- *  
  *  Distribution Notice -
  *	Re-distribution of this software is restricted, as described in
  *	your "Statement of Terms and Conditions for the Release of
