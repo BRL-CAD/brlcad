bd4fdbb9d5a8d95f4889a2ece780ce7da8322c61
Michael John Muuss <mike@brlcad.org>
2000-07-11 03:01:16 +0000

    Eliminated use of v4 "db.h" file. Moved old v4-only routines out into reg4.c
    svn:revision:18745
    cvs:account:mike
    cvs:branch:trunk

--- libwdb/reg.c
+++ libwdb/reg.c
@@ -37,7 +37,6 @@ static const char RCSid[] = "@(#)$Header$ (BRL)";
 #include "machine.h"
 #include "externs.h"
 #include "bu.h"
-#include "db.h"
 #include "vmath.h"
 #include "bn.h"
 #include "raytrace.h"
@@ -103,210 +102,6 @@ mk_comb_internal( struct wmember *headp )
 	return comb;
 }
 
-/* so we don't have to include mat.o */
-static fastf_t ident_mat[16] = {
-	1.0, 0.0, 0.0, 0.0,
-	0.0, 1.0, 0.0, 0.0,
-	0.0, 0.0, 1.0, 0.0,
-	0.0, 0.0, 0.0, 1.0
-};
-
-/* -------------------- Begin old code, compat only -------------------- */
-
-/*
- *			M K _ C O M B
- *
- *  Make a combination with material properties info.
- *  Must be followed by 'len' mk_memb() calls before any other mk_* routines.
- *
- *  XXX WARNING:  This routine should not be called by new code.
- *  XXX use mk_lcomb() instead.
- */
-int
-mk_comb( fp, name, len, region, matname, matparm, rgb, inherit )
-FILE			*fp;
-CONST char		*name;
-int			len;
-int			region;
-CONST char		*matname;
-CONST char		*matparm;
-CONST unsigned char	*rgb;
-int			inherit;
-{
-	union record rec;
-
-	BU_ASSERT_LONG( mk_version, <=, 4 );
-
-	bzero( (char *)&rec, sizeof(rec) );
-	rec.c.c_id = ID_COMB;
-	/* XXX What values to pass for FASTGEN plate and volume regions? */
-	if( region )
-		rec.c.c_flags = DBV4_REGION;
-	else
-		rec.c.c_flags = DBV4_NON_REGION;
-	NAMEMOVE( name, rec.c.c_name );
-	rec.c.c_pad1 = len;		/* backwards compat, was c_length */
-	if( matname ) {
-		strncpy( rec.c.c_matname, matname, sizeof(rec.c.c_matname) );
-		if( matparm )
-			strncpy( rec.c.c_matparm, matparm,
-				sizeof(rec.c.c_matparm) );
-	}
-	if( rgb )  {
-		rec.c.c_override = 1;
-		rec.c.c_rgb[0] = rgb[0];
-		rec.c.c_rgb[1] = rgb[1];
-		rec.c.c_rgb[2] = rgb[2];
-	}
-	rec.c.c_inherit = inherit;
-	if( fwrite( (char *)&rec, sizeof(rec), 1, fp ) != 1 )
-		return(-1);
-	return(0);
-}
-
-/*
- *			M K _ R C O M B
- *
- *  Make a combination with material properties info.
- *  Must be followed by 'len' mk_memb() calls before any other mk_* routines.
- *  Like mk_comb except for additional region parameters.
- *
- *  XXX WARNING:  This routine should not be called by new code.
- *  XXX use mk_addmember() instead.
- */
-int
-mk_rcomb( fp, name, len, region, matname, matparm, rgb, id, air, material, los, inherit )
-FILE		*fp;
-CONST char	*name;
-int		len;
-int		region;
-CONST char	*matname;
-CONST char	*matparm;
-CONST unsigned char	*rgb;
-int		id;
-int		air;
-int		material;
-int		los;
-int		inherit;
-{
-	union record rec;
-
-	BU_ASSERT_LONG( mk_version, <=, 4 );
-
-	bzero( (char *)&rec, sizeof(rec) );
-	rec.c.c_id = ID_COMB;
-	if( region ){
-		switch( region )  {
-		case DBV4_NON_REGION:	/* sanity, fixes a non-bool arg */
-		case DBV4_REGION:
-		case DBV4_REGION_FASTGEN_PLATE:
-		case DBV4_REGION_FASTGEN_VOLUME:
-			rec.c.c_flags = region;
-			break;
-		default:
-			rec.c.c_flags = DBV4_REGION;
-		}
-		rec.c.c_inherit = inherit;
-		rec.c.c_regionid = id;
-		rec.c.c_aircode = air;
-		rec.c.c_material = material;
-		rec.c.c_los = los;
-	}
-	else
-		rec.c.c_flags = DBV4_NON_REGION;
-	NAMEMOVE( name, rec.c.c_name );
-	rec.c.c_pad1 = len;		/* backwards compat, was c_length */
-	if( matname ) {
-		strncpy( rec.c.c_matname, matname, sizeof(rec.c.c_matname) );
-		if( matparm )
-			strncpy( rec.c.c_matparm, matparm,
-				sizeof(rec.c.c_matparm) );
-	}
-	if( rgb )  {
-		rec.c.c_override = 1;
-		rec.c.c_rgb[0] = rgb[0];
-		rec.c.c_rgb[1] = rgb[1];
-		rec.c.c_rgb[2] = rgb[2];
-	}
-
-	if( fwrite( (char *)&rec, sizeof(rec), 1, fp ) != 1 )
-		return(-1);
-	return(0);
-}
-
-
-/*
- *			M K _ F C O M B
- *
- *  Make a simple combination header ("fast" version).
- *  Must be followed by 'len' mk_memb() calls before any other mk_* routines.
- *
- *  XXX WARNING:  This routine should not be called by new code.
- */
-int
-mk_fcomb( fp, name, len, region )
-FILE		*fp;
-CONST char	*name;
-int		len;
-int		region;
-{
-	union record rec;
-
-	BU_ASSERT_LONG( mk_version, <=, 4 );
-
-	bzero( (char *)&rec, sizeof(rec) );
-	rec.c.c_id = ID_COMB;
-	if( region )
-		rec.c.c_flags = DBV4_REGION;
-	else
-		rec.c.c_flags = DBV4_NON_REGION;
-	NAMEMOVE( name, rec.c.c_name );
-	rec.c.c_pad1 = len;		/* backwards compat, was c_length */
-	if( fwrite( (char *)&rec, sizeof(rec), 1, fp ) != 1 )
-		return(-1);
-	return(0);
-}
-
-/*
- *			M K _ M E M B
- *
- *  Must be part of combination/member clump of records.
- *
- *  XXX WARNING:  This routine should not be called by new code.
- */
-int
-mk_memb( fp, name, mat, bool_op )
-FILE		*fp;
-CONST char	*name;
-CONST mat_t	mat;
-int		bool_op;
-{
-	union record rec;
-	register int i;
-
-	BU_ASSERT_LONG( mk_version, <=, 4 );
-
-	bzero( (char *)&rec, sizeof(rec) );
-	rec.M.m_id = ID_MEMB;
-	NAMEMOVE( name, rec.M.m_instname );
-	if( bool_op )
-		rec.M.m_relation = bool_op;
-	else
-		rec.M.m_relation = UNION;
-	if( mat ) {
-		for( i=0; i<16; i++ )
-			rec.M.m_mat[i] = mat[i];  /* double -> float */
-	} else {
-		for( i=0; i<16; i++ )
-			rec.M.m_mat[i] = ident_mat[i];
-	}
-	if( fwrite( (char *)&rec, sizeof(rec), 1, fp ) != 1 )
-		return(-1);
-	return(0);
-}
-
-/* -------------------- End old code, compat only -------------------- */
-
 /*
  *			M K _ A D D M E M B E R
  *
@@ -332,19 +127,16 @@ int	op;
 	strncpy( wp->wm_name, name, sizeof(wp->wm_name) );
 	switch( op )  {
 	case WMOP_UNION:
-		wp->wm_op = UNION;
-		break;
 	case WMOP_INTERSECT:
-		wp->wm_op = INTERSECT;
-		break;
 	case WMOP_SUBTRACT:
-		wp->wm_op = SUBTRACT;
+		wp->wm_op = op;
 		break;
 	default:
 		fprintf(stderr, "mk_addmember() op=x%x is bad\n", op);
 		return(WMEMBER_NULL);
 	}
-	bcopy( ident_mat, wp->wm_mat, sizeof(mat_t) );
+	bn_mat_idn( wp->wm_mat );
+
 	/* Append to end of doubly linked list */
 	BU_LIST_INSERT( &headp->l, &wp->l );
 	return(wp);
@@ -394,7 +186,7 @@ int		inherit;
 	/* Release the member structure dynamic storage */
 	mk_freemembers( headp );
 
-	return mk_export_fwrite( fp, name, comb, ID_COMB );
+	return mk_export_fwrite( fp, name, comb, ID_COMBINATION );
 }
 
 /*
@@ -475,7 +267,7 @@ int	inherit;
 	/* Release the member structure dynamic storage */
 	mk_freemembers( headp );
 
-	return mk_export_fwrite( fp, name, comb, ID_COMB );
+	return mk_export_fwrite( fp, name, comb, ID_COMBINATION );
 }
 
 /*
@@ -558,6 +350,6 @@ int	inherit;
 	/* Release the member structure dynamic storage */
 	mk_freemembers( headp );
 
-	return mk_export_fwrite( fp, name, comb, ID_COMB );
+	return mk_export_fwrite( fp, name, comb, ID_COMBINATION );
 }
 
