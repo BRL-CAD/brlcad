ce48bbc83a2699dcd6322f9007bd6df1875f1599
John Anderson <daytonajra@gmail.com>
2000-07-31 20:45:56 +0000

    Added definition for DBV4_NON_REGION_NULL to handle some old models that had '\0' as the c_flag for non-regions
    svn:revision:18918
    cvs:account:jra
    cvs:branch:trunk

--- h/db.h
+++ h/db.h
@@ -205,6 +205,7 @@ union record  {
 		char	c_id;			/* ID_COMB */
 		char	c_flags;		/* `R' if region, else ` ' */
 #define DBV4_NON_REGION			' '
+#define DBV4_NON_REGION_NULL		'\0'
 #define DBV4_REGION			'R'
 #define DBV4_REGION_FASTGEN_PLATE	'P'
 #define DBV4_REGION_FASTGEN_VOLUME	'V'
