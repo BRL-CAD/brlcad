c62a5dc774465078a538f1073ad48856f4493e38
John Anderson <daytonajra@gmail.com>
2000-08-17 17:03:23 +0000

    Needed a declaration of nmg_region_end()
    svn:revision:19023
    cvs:account:jra
    cvs:branch:trunk

--- conv/g-vrml.c
+++ conv/g-vrml.c
@@ -89,6 +89,7 @@ struct bu_structparse vrml_mat_parse[]={
 };
 
 BU_EXTERN(union tree *do_region_end, (struct db_tree_state *tsp, struct db_full_path *pathp, union tree *curtree, genptr_t client_data));
+BU_EXTERN(union tree *nmg_region_end, (struct db_tree_state *tsp, struct db_full_path *pathp, union tree *curtree, genptr_t client_data));
 
 static char	usage[] = "Usage: %s [-v] [-xX lvl] [-d tolerance_distance (mm) ] [-a abs_tol (mm)] [-r rel_tol] [-n norm_tol] [-o out_file] brlcad_db.g object(s)\n";
 
