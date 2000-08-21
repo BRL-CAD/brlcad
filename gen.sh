ac06cd3e17334e8faa1bd87b73216a3c5fc54b8b
John Anderson <daytonajra@gmail.com>
2000-08-21 14:26:32 +0000

    Set release to 5.2
    svn:revision:19065
    cvs:account:jra
    cvs:branch:rel-5-1-branch

--- gen.sh
+++ gen.sh
@@ -37,7 +37,7 @@ NFS=1
 # Label number for this CAD Release,
 # RCS main Revision number, and date.
 #RELEASE=M.N;	RCS_REVISION=X;		REL=DATE=dd-mmm-yy
-RELEASE=5.1;	RCS_REVISION=13;	REL_DATE=Today		# beyond 5.1
+RELEASE=5.2;	RCS_REVISION=13;	REL_DATE=21-Aug-00	# 5.2
 #RELEASE=5.0;	RCS_REVISION=12;	REL_DATE=15-Sept-99	# 5.0 production
 #RELEASE=4.6;	RCS_REVISION=11;	REL_DATE=7-Jul-99	# 5.0beta
 #RELEASE=4.5;	RCS_REVISION=11;	REL_DATE=14-Feb-98	# 4.5 production
