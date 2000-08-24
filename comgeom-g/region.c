b5be699df090ed9b5ffc5bf118d3954941b84de2
Michael John Muuss <mike@brlcad.org>
2000-08-24 04:56:48 +0000

    mk_addmember
    svn:revision:19138
    cvs:account:mike
    cvs:branch:trunk

--- comgeom-g/region.c
+++ comgeom-g/region.c
@@ -183,7 +183,7 @@ top:
 				namecvt( inst_num, inst_name, 's' );
 			reg_reg_flag = 0;
 
-			(void)mk_addmember( inst_name, &wmp[reg_num], op );
+			(void)mk_addmember( inst_name, &wmp[reg_num].l, op );
 		}
 	}
 
@@ -346,7 +346,7 @@ char		*name;
 	i = 0;
 
 add:
-	(void)mk_addmember( name, &groups[i].grp_wm, WMOP_UNION );
+	(void)mk_addmember( name, &groups[i].grp_wm.l, WMOP_UNION );
 }
 
 void
@@ -367,7 +367,7 @@ group_write()
 		mk_lfcomb( outfp, wp->wm_name, wp, 0 );
 
 		/* Add it to "all.g" */
-		(void)mk_addmember( wp->wm_name, &allhead, WMOP_UNION );
+		(void)mk_addmember( wp->wm_name, &allhead.l, WMOP_UNION );
 
 		if(verbose) col_pr( wp->wm_name );
 	}
