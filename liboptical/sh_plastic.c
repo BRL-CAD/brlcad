b7d4683e75da312a7a2e9d3c73222e7a597a6002
Christopher Johnson <cjohnson@brlcad.org>
2000-09-11 05:56:12 +0000

    Added support for experamental message passing between shaders. Phong shader catches many parameters.
    svn:revision:19369
    cvs:account:cjohnson
    cvs:branch:trunk

--- liboptical/sh_plastic.c
+++ liboptical/sh_plastic.c
@@ -355,6 +355,12 @@ char	*dp;
 	swp->sw_reflect = ps->reflect;
 	swp->sw_refrac_index = ps->refrac_index;
 	swp->sw_extinction = ps->extinction;
+#if SW_SET_TRANSMIT
+	if (swp->sw_phong_set_vector & SW_SET_TRANSMIT) swp->sw_transmit = swp->sw_phong_transmit;
+	if (swp->sw_phong_set_vector & SW_SET_REFLECT) swp->sw_reflect = swp->sw_phong_reflect;
+	if (swp->sw_phong_set_vector & SW_SET_REFRAC_INDEX) swp->sw_refrac_index = swp->sw_phong_ri;
+	if (swp->sw_phong_set_vector & SW_SET_EXTINCTION) swp->sw_extinction = swp->sw_phong_extinction;
+#endif /* SW_SET_TRANSMIT */
 	if (swp->sw_xmitonly ) {
 		if (swp->sw_xmitonly > 1 )
 			return(1);	/* done -- wanted parameters only */
@@ -384,7 +390,15 @@ char	*dp;
 			VPRINT(" r_dir ", ap->a_ray.r_dir);
 			cosine = 1;
 		}
+#if SW_SET_TRANSMIT
+		if (swp->sw_phong_set_vector & SW_SET_AMBIENT) {
+			cosine *= swp->sw_phong_ambient;
+		} else {
+			cosine *= AmbientIntensity;
+		}
+#else
 		cosine *= AmbientIntensity;
+#endif
 #if RT_MULTISPECTRAL
 		bn_tabdata_scale( swp->msw_color, ms_matcolor, cosine );
 #else
@@ -401,15 +415,31 @@ char	*dp;
 	/* Emission.  0..1 is normal range, -1..0 sucks light out, like OpenGL */
 #if RT_MULTISPECTRAL
 	{
+		fastf_t emission = ps->emission;
 		struct bn_tabdata	*ms_emission = BN_TABDATA_NULL;
+#if SW_SET_TRANSMIT
+		if (swp->sw_phong_set_vector & SW_SET_EMISSION) {
+			emission = swp->sw_phong_emission;
+		}
+#endif
 		/* XXX Really should get a curve at prep, not expand RGB samples */
 		BN_GET_TABDATA( ms_emission, spectrum );
-		rt_spect_reflectance_rgb( ms_emission, ps->emission );
+		rt_spect_reflectance_rgb( ms_emission, emission );
 		bn_tabdata_add( swp->msw_color, swp->msw_color, ms_emission );
 		bn_tabdata_free( ms_emission );
 	}
 #else
+#if SW_SET_TRANSMIT
+	if (swp->sw_phong_set_vector & SW_SET_EMISSION) {
+		vect_t tmp;
+		VSETALL(tmp,swp->sw_phong_emission);
+		VADD2( swp->sw_color, swp->sw_color, tmp);
+	} else {
+		VADD2( swp->sw_color, swp->sw_color, ps->emission );
+	}
+#else
 	VADD2( swp->sw_color, swp->sw_color, ps->emission );
+#endif /* SW_SET_TRANSMIT */
 #endif
 
 	/* With the advent of procedural shaders, the caller can no longer
