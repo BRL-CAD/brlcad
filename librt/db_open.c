fc856a96d8c9df66e7397eb4c9239ee5b3dd0c28
Robert Parker <unacknowledged@users.noreply.github.com>
2000-10-11 16:06:14 +0000

    Copy guts of db_create from developmental version. This fixes a bug that shows up in MGED (i.e. Mged was modified to use the new db_create, but was still getting the old one).
    svn:revision:19410
    cvs:account:bparker
    cvs:branch:rel-5-1-branch

--- librt/db_open.c
+++ librt/db_open.c
@@ -182,36 +182,39 @@ struct db_i *
 db_create( name )
 CONST char *name;
 {
-	union record new;
+	FILE	*fp;
+	struct db_i	*dbip;
 
 	if(rt_g.debug&DEBUG_DB) bu_log("db_create(%s, %s)\n", name );
 
-	/* Prepare the IDENT record */
-	bzero( (char *)&new, sizeof(new) );
-	new.i.i_id = ID_IDENT;
-	new.i.i_units = ID_MM_UNIT;
-	strncpy( new.i.i_version, ID_VERSION, sizeof(new.i.i_version) );
-	strcpy( new.i.i_title, "Untitled MGED Database" );
-
-#ifdef HAVE_UNIX_IO
-	{
-		int	fd;
-		if( (fd = creat(name, 0644)) < 0 ||
-		    write( fd, (char *)&new, sizeof(new) ) != sizeof(new) )
-			return(DBI_NULL);
-		(void)close(fd);
+	if( (fp = fopen( name, "w" )) == NULL )  {
+		perror(name);
+		return(DBI_NULL);
 	}
-#else /* HAVE_UNIX_IO */
-	{
-		FILE	*fp;
-		if( (fp = fopen( name, "w" )) == NULL )
-			return(DBI_NULL);
-		(void)fwrite( (char *)&new, 1, sizeof(new), fp );
+
+#if 0
+	/* Create a v5 database */
+	if( db5_fwrite_ident( fp, "Untitled v5 BRL-CAD Database", 1.0 ) < 0 )  {
 		(void)fclose(fp);
+		return DBI_NULL;
+	}
+#else
+	/* Create a v4 database */
+	if( db_fwrite_ident( fp, "Untitled v4 BRL-CAD Database", 1.0 ) < 0 )  {
+		(void)fclose(fp);
+		return DBI_NULL;
 	}
 #endif
 
-	return( db_open( name, "r+w" ) );
+	(void)fclose(fp);
+
+	if( (dbip = db_open( name, "r+w" ) ) == DBI_NULL )
+		return DBI_NULL;
+
+	/* Do a quick scan to determine version, find _GLOBAL, etc. */
+	if( db_dirbuild( dbip ) < 0 )
+		return DBI_NULL;
+	return dbip;
 }
 
 /*
