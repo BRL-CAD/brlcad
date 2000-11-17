27c38963fcb62836e0285d5029f6cb86667d94ed
Robert Parker <unacknowledged@users.noreply.github.com>
2000-11-17 22:46:19 +0000

    Rewrote place_near_mouse to be more accurate about window placement.
    svn:revision:19709
    cvs:branch:rel-5-1-branch
    cvs:account:bparker

--- tclscripts/mouse.tcl
+++ tclscripts/mouse.tcl
@@ -19,13 +19,43 @@
 # Description -
 #       Mouse routines.
 
-proc place_near_mouse { top } {
+## - place_near_mouse
+#
+# Places window near the mouse while keeping the 
+# window under the mouse and completely (mostly)
+# on the screen.
+#
+proc place_near_mouse {top} {
+    if {[winfo toplevel $top] != $top} {
+	error "place_near_mouse: $top is not a toplevel window"
+    }
+
     set pxy [winfo pointerxy $top]
-    set x [lindex $pxy 0]
-    set y [lindex $pxy 1]
+    set px [lindex $pxy 0]
+    set py [lindex $pxy 1]
 
-    set x [expr {int($x - [winfo reqwidth $top] * 0.5)}]
-    set y [expr {int($y - [winfo reqheight $top] * 0.5)}]
+    wm withdraw $top
+    update
+    set width [winfo reqwidth $top]
+    set height [winfo reqheight $top]
+    set screenwidth [winfo screenwidth $top]
+    set screenheight [winfo screenheight $top]
 
-    catch { wm geometry $top +$x+$y }
+    set x [expr {int($px - $width * 0.5)}]
+    set y [expr {int($py - $height * 0.5)}]
+
+    if {$x < 0} {
+	set x 0
+    } elseif {[expr {$x + $width}] > $screenwidth} {
+	set x [expr {$screenwidth - $width}]
+    }
+
+    if {$y < 0} {
+	set y 0
+    } elseif {[expr {$y + $height}] > $screenheight} {
+	set y [expr {$screenheight - $height}]
+    }
+
+    wm geometry $top +$x+$y
+    wm deiconify $top
 }
