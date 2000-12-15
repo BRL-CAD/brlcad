31d033a28e986e4fe092b50537db5784b8fa1d1b
Robert Parker <unacknowledged@users.noreply.github.com>
2000-12-15 21:26:09 +0000

    Change find to dbfind.
    svn:revision:19793
    cvs:branch:rel-5-1-branch
    cvs:account:bparker

--- mged/dir.c
+++ mged/dir.c
@@ -652,7 +652,7 @@ char	**argv;
     struct bu_vls vls;
 
     bu_vls_init(&vls);
-    bu_vls_printf(&vls, "help find");
+    bu_vls_printf(&vls, "help %s", argv[0]);
     Tcl_Eval(interp, bu_vls_addr(&vls));
     bu_vls_free(&vls);
     return TCL_ERROR;
