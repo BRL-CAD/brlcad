48bc3fee8c07f717738b229f518350fc6856f9e8
Robert Parker <unacknowledged@users.noreply.github.com>
2001-03-09 15:34:39 +0000

    *- add the following methods:    units, vrot, vtra, coord,    keypoint, rotate_about, init *- add the following public variables:    coord, rotate_about, keypoint units
    svn:revision:20007
    cvs:account:bparker
    cvs:branch:trunk

--- tclscripts/lib/View.tcl
+++ tclscripts/lib/View.tcl
@@ -24,27 +24,39 @@ class View {
     public variable center {0 0 0}
     public variable aet "0 90 0"
     public variable perspective_angle 90
+    public variable coord "v"
+    public variable rotate_about "v"
+    public variable keypoint "0 0 0"
+    public variable units "mm"
 
     constructor {args} {}
     destructor {}
 
     public method aet {args}
     public method center {args}
+    public method coord {args}
     public method get_viewname {}
+    public method keypoint {args}
     public method model2view {}
     public method observer {args}
     public method perspective {args}
     public method pmodel2view {}
     public method rot {args}
+    public method rotate_about {args}
     public method size {args}
     public method slew {x y}
     public method tra {args}
+    public method units {args}
+    public method vrot {args}
+    public method vtra {args}
     public method zoom {sf}
 
     public method ? {}
     public method apropos {key}
     public method help {args}
     public method getUserCmds {}
+
+    private method init {}
     private method help_init {}
 
     private variable view
@@ -67,6 +79,22 @@ configbody View::perspective_angle {
     View::perspective $perspective_angle
 }
 
+configbody View::coord {
+    View::coord $coord
+}
+
+configbody View::rotate_about {
+    View::rotate_about $rotate_about
+}
+
+configbody View::keypoint {
+    eval View::keypoint $keypoint
+}
+
+configbody View::units {
+    View::units $units
+}
+
 body View::constructor {args} {
     # first create view object
     set view [v_open $this\_view]
@@ -74,11 +102,7 @@ body View::constructor {args} {
     # process options
     eval configure $args
 
-    View::size $size
-    eval View::center $center
-    eval View::aet $aet
-    View::perspective $perspective_angle
-    View::help_init
+    View::init
 }
 
 body View::destructor {} {
@@ -121,6 +145,7 @@ body View::rot {args} {
     $view rot $args
 
     set aet [$view aet]
+    set center [$view center]
 }
 
 body View::slew {x y} {
@@ -145,8 +170,33 @@ body View::size {args} {
 
     # set size
     $view size $args
-
     set size $args
+}
+
+body View::units {args} {
+    # get units
+    if {$args == ""} {
+	return $units
+    }
+
+    # set units
+    $view units $args
+    set units $args
+}
+
+body View::vrot {args} {
+    # rotate view
+    $view rot -v $args
+
+    set aet [$view aet]
+    set center [$view center]
+}
+
+body View::vtra {args} {
+    # translate view
+    $view tra -v $args
+
+    set center [$view center]
 }
 
 body View::zoom {sf} {
@@ -173,6 +223,36 @@ body View::perspective {args} {
     set perspective_angle $args
 }
 
+body View::coord {args} {
+    # get coord
+    if {$args == ""} {
+	return $coord
+    }
+
+    $view coord $args
+    set coord $args
+}
+
+body View::keypoint {args} {
+    # get keypoint
+    if {$args == ""} {
+	return $keypoint
+    }
+
+    $view keypoint $args
+    set keypoint $args
+}
+
+body View::rotate_about {args} {
+    # get rotate_about
+    if {$args == ""} {
+	return $rotate_about
+    }
+
+    $view rotate_about $args
+    set rotate_about $args
+}
+
 body View::? {} {
     return [$help ? 20 4]
 }
@@ -189,13 +269,28 @@ body View::help {args} {
     return [eval $help get $args]
 }
 
+body View::init {} {
+    View::size $size
+    eval View::center $center
+    eval View::aet $aet
+    View::perspective $perspective_angle
+    View::coord $coord
+    View::rotate_about $rotate_about
+    View::keypoint $keypoint
+    View::units $units
+    View::help_init
+}
+
 body View::help_init {} {
     set help [cadwidgets::Help #auto]
 
     $help add aet		{{["az el tw"]} {set/get the azimuth, elevation and twist}}
     $help add center		{{["x y z"]} {set/get the view center}}
+    $help add coord		{{[m|v]} {set/get the coodinate system}}
+    $help add keypoint		{{[point]} {set/get the keypoint}}
     $help add perspective	{{[angle]} {set/get the perspective angle}}
     $help add rot		{{"x y z"} {rotate the view}}
+    $help add rotate_about	{{[e|k|m|v]} {set/get the rotate about point}}
     $help add size		{{vsize} {set/get the view size}}
     $help add slew		{{"x y"} {slew the view}}
     $help add tra		{{"x y z"} {translate the view}}
