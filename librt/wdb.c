67821428e1b0d08b39effb1f56c14eeee25177c3
Paul Tanenbaum <pjt@brlcad.org>
2001-03-20 21:54:09 +0000

    Added DB5_MAJORTYPE_BRLCAD as last arg. to rt_db_cvt_to_external5()
    svn:revision:20071
    cvs:account:pjt
    cvs:branch:trunk

--- librt/wdb.c
+++ librt/wdb.c
@@ -298,7 +298,7 @@ wdb_put_internal(
 		}
 		db_wrap_v4_external( &ext, name );
 	} else {
-		if( rt_db_cvt_to_external5( &ext, name, ip, local2mm, wdbp->dbip, &rt_uniresource ) < 0 )  {
+		if( rt_db_cvt_to_external5( &ext, name, ip, local2mm, wdbp->dbip, &rt_uniresource, DB5_MAJORTYPE_BRLCAD ) < 0 )  {
 			bu_log("wdb_export(%s): solid export failure\n",
 				name );
 			ret = -2;
