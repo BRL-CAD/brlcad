b019a52cecdcd36e01b3254c4e906f7c8f9d21f8
Christopher Sean Morrison <brlcad@gmail.com>
2001-03-20 20:03:17 +0000

    added script to test d-a
    svn:revision:20057
    cvs:account:morrison
    cvs:branch:trunk

---
+++ notes/regression/d-a.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+if [ -f d-a.log ] ; then
+  rm d-a.log
+fi
+d-a
+if [ -f d-a.log ] ; then
+  echo "WARNING: d-a output when none expected"
+  rm d-a.log
+fi
+d-a <badtestascii.txt >d-a.log
+if [ -f d-a.log ] ; then
+  echo "WARNING: d-a output when none expected"
+  rm d-a.log
+fi
+d-a <testascii.txt >d-a.log
+if [ -f d-a.log ] ; then 
+  echo "WARNING: d-a output when none expected"
+  rm d-a.log
+fi
+d-a <testdouble.bin >d-a.log
+if [ ! -f d-a.log ] ; then
+  echo "ERROR: d-a output not found"
+fi
