fd9e3eef5398c3f872c53b19050283f4978abe1b
Christopher Sean Morrison <brlcad@gmail.com>
2001-03-20 19:51:19 +0000

    added the regression tool set including testing files.  very raw initial state. primary function is to test functionality for v6 tools.
    svn:revision:20056
    cvs:account:morrison
    cvs:branch:trunk

---
+++ notes/regression/a-d.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+if [ -f a-d.log ] ; then
+  rm a-d.log
+fi
+a-d
+if [ -f a-d.log ] ; then
+  echo "WARNING: a-d output when none expected"
+  rm a-d.log
+fi
+a-d <badtestascii.txt >a-d.log
+if [ -f a-d.log ] ; then
+  echo "WARNING: a-d output when none expected"
+  rm a-d.log
+fi
+a-d <testascii.txt >a-d.log
+if [ -f a-d.log ] ; then 
+  echo "WARNING: a-d output when none expected"
+  rm a-d.log
+fi
+a-d <testasciiint.txt >a-d.log
+if [ ! -f a-d.log ] ; then
+  echo "ERROR: a-d output not found"
+fi
+a-d <testasciifloat.txt >a-d.log
+if [ ! -f a-d.log ] ; then
+  echo "ERROR: a-d output not found"
+fi
+a-d <testasciidouble.txt >a-d.log
+if [ ! -f a-d.log ] ; then
+  echo "ERROR: a-d output not found"
+fi
