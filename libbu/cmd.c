f4e3075b80bda662cf7f785a23fc573b5ee9d06f
Robert Parker <unacknowledged@users.noreply.github.com>
2001-03-21 21:55:39 +0000

    *- removed bu_build_cmd_vls
    svn:revision:20117
    cvs:account:bparker
    cvs:branch:trunk

--- libbu/cmd.c
+++ libbu/cmd.c
@@ -78,20 +78,3 @@ bu_register_cmds(interp, cmds)
     (void)Tcl_CreateCommand(interp, ctp->ct_name, ctp->ct_func,
 			    (ClientData)ctp, (Tcl_CmdDeleteProc *)NULL);
 }
-
-/*
- * Builds a command string in the provided vls.
- */
-void
-bu_build_cmd_vls(vls, cmd, argc, argv)
-     struct bu_vls	*vls;
-     CONST char		*cmd;
-     int		argc;
-     char		**argv;
-{
-	register int i;
-
-	bu_vls_strcpy(vls, cmd);
-	for (i = 0; i < argc; ++i)
-		bu_vls_printf(vls, " %s", argv[i]);
-}
