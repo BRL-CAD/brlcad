a4738ee99cb118a197fbe66e812812b5df7ffb2d
Ronald Bowers <rbowers@brlcad.org>
2001-03-23 21:26:54 +0000

    Version 0.99!
    Added these capabilities:
        Set foreground color
        Set background color
        Toggle id test, region test, distance test, normal test.
    
    svn:revision:20149
    cvs:account:rbowers
    cvs:branch:trunk

--- rt/viewedge.c
+++ rt/viewedge.c
@@ -45,19 +45,19 @@ int	using_mlib = 0;			/* Material routines NOT used */
 extern 	FBIO	*fbp;			/* Framebuffer handle */
 extern	fastf_t	viewsize;
 extern	int	lightmodel;
-extern	double	AmbientIntensity;	/* XXX - temp hack for contrast! */
 extern	int	width, height;
 extern int	per_processor_chunk;
 
 static	int pixsize = 0;		/* bytes per pixel in scanline */
 
 struct cell {
-        int	c_ishit;
-	fastf_t	c_dist;			/* distance from emanation plane to in_hit */
-	int	c_id;			/* region_id of component hit */
-	point_t	c_hit;			/* 3-space hit point of ray */
-	vect_t	c_normal;		/* surface normal at the hit point */
-	vect_t	c_rdir;			/* ray direction, permits perspective */
+        int		c_ishit;
+	struct region *	c_region;
+	fastf_t		c_dist;		/* distance from emanation plane to in_hit */
+	int		c_id;		/* region_id of component hit */
+	point_t		c_hit;		/* 3-space hit point of ray */
+	vect_t		c_normal;	/* surface normal at the hit point */
+	vect_t		c_rdir;		/* ray direction, permits perspective */
 };
 
 #define MISS_DIST	-1
@@ -66,22 +66,77 @@ struct cell {
 static unsigned char *scanline[MAX_PSW];
 int   		nEdges = 0;
 int   		nPixels = 0;
-fastf_t		pit_depth;		/* min. distance for drawing pits/mountains */
-fastf_t		maxangle;		/* value of the cosine of the angle bet. surface normals that triggers shading */
+fastf_t		max_dist;	/* min. distance for drawing pits/mountains */
+fastf_t		maxangle;	/* value of the cosine of the angle bet. surface normals that triggers shading */
 
+typedef int color[3];
+color	foreground = { 255, 255, 255};
+color	background = { 0, 0, 0};
+
+/*
+ * Flags that set which edges are detected.
+ * detect_ids means detect boundaries region id codes.
+ * detect_regions -> detect region boundaries.
+ * detect_distance -> detect noticable differences in hit distance.
+ * detect_normals -> detect rapid change in surface normals
+ */
+int	detect_ids = 1;
+int	detect_regions = 0;
+int	detect_distance = 1;
+int	detect_normals = 1;
+
+/*
+ * Prototypes for the viewedge edge detection functions
+ */
+static int is_edge(struct application *, struct cell *, struct cell *,
+	struct cell *below);
 static int rayhit (struct application *, struct partition *, struct seg *);
 static int rayhit2 (struct application *, struct partition *, struct seg *);
 static int raymiss (struct application *);
 static int raymiss2 (struct application *);
 static int handle_main_ray(struct application *, struct partition *, struct seg *);
 
+#define COSTOL          0.91    /* normals differ if dot product < COSTOL */
+#define OBLTOL          0.1     /* high obliquity if cosine of angle < OBLTOL ! */
+#define is_Odd(_a)      ((_a)&01)
+#define ARCTAN_87       19.08
 
 #ifndef Abs
 #define Abs( x )        ((x) < 0 ? -(x) : (x))                  /* UNSAFE */
 #endif
 
+/*
+ * From do.c
+ */
+#if CRAY
+#	define byteoffset(_i)	(((int)&(_i)))	/* actually a word offset */
+#else
+#  if IRIX > 5 && _MIPS_SIM != _MIPS_SIM_ABI32
+#	define byteoffset(_i)	((size_t)__INTADDR__(&(_i)))
+#  else
+#    if sgi || __convexc__ || ultrix || _HPUX_SOURCE
+	/* "Lazy" way.  Works on reasonable machines with byte addressing */
+#	define byteoffset(_i)	((int)((char *)&(_i)))
+#    else
+	/* "Conservative" way of finding # bytes as diff of 2 char ptrs */
+#	define byteoffset(_i)	((int)(((char *)&(_i))-((char *)0)))
+#    endif
+#  endif
+#endif
 /* Viewing module specific "set" variables */
 struct bu_structparse view_parse[] = {
+	{"%d", 1, "detect_regions", byteoffset(detect_regions), BU_STRUCTPARSE_FUNC_NULL},
+	{"%d", 1, "dr", byteoffset(detect_regions), BU_STRUCTPARSE_FUNC_NULL},
+	{"%d", 1, "detect_distance", byteoffset(detect_distance), BU_STRUCTPARSE_FUNC_NULL},
+	{"%d", 1, "dd", byteoffset(detect_distance), BU_STRUCTPARSE_FUNC_NULL},
+	{"%d", 1, "detect_normals", byteoffset(detect_normals), BU_STRUCTPARSE_FUNC_NULL},
+	{"%d", 1, "dn", byteoffset(detect_normals), BU_STRUCTPARSE_FUNC_NULL},
+	{"%d", 1, "detect_ids", byteoffset(detect_ids), BU_STRUCTPARSE_FUNC_NULL},
+	{"%d", 1, "di", byteoffset(detect_ids), BU_STRUCTPARSE_FUNC_NULL},
+	{"%d", 3, "foreground", byteoffset(foreground), BU_STRUCTPARSE_FUNC_NULL},
+	{"%d", 3, "fg", byteoffset(foreground), BU_STRUCTPARSE_FUNC_NULL},
+	{"%d", 3, "background", byteoffset(background),	BU_STRUCTPARSE_FUNC_NULL},
+	{"%d", 3, "bg", byteoffset(background),	BU_STRUCTPARSE_FUNC_NULL},	
 	{"",	0, (char *)0,	0,	BU_STRUCTPARSE_FUNC_NULL }
 };
 
@@ -90,11 +145,10 @@ Usage:  rtedge [options] model.g objects... >file.pix\n\
 Options:\n\
  -s #               Grid size in pixels, (default 512)\n\
  -w # -n #          Grid size width and height in pixels\n\
- -A angle           Angle between surface normals (default=5degrees)\n\
  -V #               View (pixel) aspect ratio (width/height)\n\
  -a #               Azimuth in deg\n\
  -e #               Elevation in deg\n\
-h -M                 Read matrix+cmds on stdin\n\
+ -M                 Read matrix+cmds on stdin\n\
  -N #               NMG debug flags\n\
  -o model.pix       Output file, .pix format (default=fb)\n\
  -x #               librt debug flags\n\
@@ -119,8 +173,6 @@ view_init( struct application *ap, char *file, char *obj, int minus_o )
     ap->a_miss = raymiss;
     ap->a_onehit = 1;
 
-    per_processor_chunk = width;
-
     if( minus_o ) {
 	/*
 	 * Output is to a file stream.
@@ -140,20 +192,36 @@ void
 view_2init( struct application *ap )
 {
     int i;
-    register struct region *rp;
-    register struct rt_i *rtip = ap->a_rt_i;
 
-    pixsize = 3;		/* Frame buffer */
+    /*
+     * Per_processor_chuck specifies the number of pixels rendered
+     * per each pass of a worker. By making this value equal to the
+     * width of the image, each worker will render one scanline at
+     * a time.
+     */
+    per_processor_chunk = width;
+
+    /*
+     * Use three bytes per pixel.
+     */
+    pixsize = 3;
+
+    /*
+     * Create a buffer for each scanline.
+     */
     for ( i = 0; i < npsw; ++i ) {
 	if (scanline[i] == NULL) {
 	    scanline[i] = (unsigned char *)
 		bu_malloc( per_processor_chunk*pixsize, "scanline buffer" );
 	}	
     }
-	
-    for( BU_LIST_FOR( rp, region, &(rtip->HeadRegion) ) )  {
-        rp->reg_udata = (genptr_t) NULL;
-    }
+
+    /*
+     * Set the hit distance difference necessary to trigger an edge.
+     * This algorythm stolen from lgt, I may make it settable later.
+     */
+    max_dist = (cell_width*ARCTAN_87)+2;
+
 }
 
 /* end of each pixel */
@@ -223,8 +291,9 @@ int rayhit2 (struct application *ap, register struct partition *pt,
     struct hit		*hitp = pt->pt_forw->pt_inhit;
     struct cell 	*c = (struct cell *)ap->a_uptr;
 
-    c->c_ishit = 1;
-    c->c_id = pp->pt_regionp->reg_regionid;
+    c->c_ishit 		= 1;
+    c->c_region 	= pp->pt_regionp;
+    c->c_id 		= pp->pt_regionp->reg_regionid;
     VMOVE(c->c_rdir, ap->a_ray.r_dir);
     VJOIN1(c->c_hit, ap->a_ray.r_pt, hitp->hit_dist, ap->a_ray.r_dir );
     RT_HIT_NORMAL(c->c_normal, hitp,
@@ -241,9 +310,10 @@ int raymiss2( register struct application *ap )
 {
     struct cell *c = (struct cell *)ap->a_uptr;
 
-    c->c_ishit    = 0;
-    c->c_dist   = MISS_DIST;
-    c->c_id	    = MISS_ID;	
+    c->c_ishit    	= 0;
+    c->c_region   	= 0;
+    c->c_dist     	= MISS_DIST;
+    c->c_id	    	= MISS_ID;	
     VSETALL(c->c_hit, MISS_DIST);
     VSETALL(c->c_normal, 0);
     VMOVE(c->c_rdir, ap->a_ray.r_dir);
@@ -251,16 +321,9 @@ int raymiss2( register struct application *ap )
     return(0);
 }
 
-
-#define COSTOL          0.91    /* normals differ if dot product < COSTOL */
-#define OBLTOL          0.1     /* high obliquity if cosine of angle < OBLTOL ! */
-#define is_Odd(_a)      ((_a)&01)
-#define ARCTAN_87       19.08
-
 int is_edge(struct application *ap, struct cell *here,
     struct cell *left, struct cell *below)
 {
-    fastf_t  max_dist = (cell_width*ARCTAN_87)+2;
 
     if( here->c_id == -1 && left->c_id == -1 && below->c_id == -1) {
 	/*
@@ -269,19 +332,33 @@ int is_edge(struct application *ap, struct cell *here,
 	return 0;
     }
 
-    if (here->c_id != -1 &&
-        (here->c_id != left->c_id || here->c_id != below->c_id)) {
-	return 1;
+    if (detect_ids) {
+        if (here->c_id != -1 &&
+            (here->c_id != left->c_id || here->c_id != below->c_id)) {
+	    return 1;
+        }
     }
 
-    if ( Abs(here->c_dist - left->c_dist) > max_dist ||
-	 Abs(here->c_dist - below->c_dist) > max_dist) {
-	return 1;
+    if (detect_regions) {
+        if (here->c_region != 0 &&
+            (here->c_region != left->c_region
+            || here->c_region != below->c_region)) {
+	    return 1;
+        }
     }
 
-    if ((VDOT(here->c_normal, left->c_normal) < COSTOL) ||
-	(VDOT(here->c_normal, below->c_normal)< COSTOL)) {
-	return 1;	
+    if (detect_distance) {
+        if (Abs(here->c_dist - left->c_dist) > max_dist ||
+	    Abs(here->c_dist - below->c_dist) > max_dist) {
+	    return 1;
+        }
+    }
+
+    if (detect_normals) {
+    	if ((VDOT(here->c_normal, left->c_normal) < COSTOL) ||
+	    (VDOT(here->c_normal, below->c_normal)< COSTOL)) {
+	     return 1;	
+        }
     }
 
     return 0;
@@ -365,16 +442,16 @@ handle_main_ray( struct application *ap, register struct partition *PartHeadp,
 	 */
 	if (is_edge (ap, &me, &left, &below)) {
 	    bu_semaphore_acquire (RT_SEM_RESULTS);	
-	    	scanline[cpu][ap->a_x*3+RED] = 255;
-		scanline[cpu][ap->a_x*3+GRN] = 255;
-		scanline[cpu][ap->a_x*3+BLU] = 255;
+	    	scanline[cpu][ap->a_x*3+RED] = foreground[RED];
+		scanline[cpu][ap->a_x*3+GRN] = foreground[GRN];
+		scanline[cpu][ap->a_x*3+BLU] = foreground[BLU];
 	    bu_semaphore_release (RT_SEM_RESULTS);
 	    edge = 1;
 	} else {
 	    bu_semaphore_acquire (RT_SEM_RESULTS);
-	       	scanline[cpu][ap->a_x*3+RED] = 0;
-		scanline[cpu][ap->a_x*3+GRN] = 0;
-		scanline[cpu][ap->a_x*3+BLU] = 0;
+	       	scanline[cpu][ap->a_x*3+RED] = background[RED];
+		scanline[cpu][ap->a_x*3+GRN] = background[GRN];
+		scanline[cpu][ap->a_x*3+BLU] = background[BLU];
 	    bu_semaphore_release (RT_SEM_RESULTS);
 	    edge = 0;
 	}
