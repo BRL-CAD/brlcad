48464db1c770a3a3442133777a6ec481b4d79a5c
John Anderson <daytonajra@gmail.com>
2001-04-25 13:45:44 +0000

    Now creates a sketch with a Bezier curve segment
    svn:revision:20256
    cvs:account:jra
    cvs:branch:trunk

--- proc-db/sketch.c
+++ proc-db/sketch.c
@@ -46,16 +46,23 @@ int argc;
 char *argv[];
 {
 	struct rt_sketch_internal *skt;
+	struct bezier_seg *bsg;
+	struct line_seg *lsg;
+	struct carc_seg *csg;
 	point_t V;
 	vect_t u_vec, v_vec;
 	struct curve *crv;
-	struct carc_seg *csg;
 	point2d_t verts[] = {
-		{ 20, 10 },
-		{ 10, 10 },
-		{ 0, 0 },
-		{ 50, 0 },
-		{ 0, 50 }
+		{ 250, 0 },	/* 0 */
+		{ 500, 0 },	/* 1 */
+		{ 500, 500 },	/* 2 */
+		{ 0, 500 },	/* 3 */
+		{ 0, 250 },	/* 4 */
+		{ 250, 250 },	/* 5 */
+		{ 125, 125 },	/* 6 */
+		{ 0, 125 },	/* 7 */
+		{ 125, 0 },	/* 8 */
+		{ 200, 200 }	/* 9 */
 	};
 	int i;
 
@@ -64,26 +71,66 @@ char *argv[];
 	VSET( v_vec, 0, 1, 0 );
 
 	skt = (struct rt_sketch_internal *)bu_calloc( 1, sizeof( struct rt_sketch_internal ), "sketch" );
+	skt->magic = RT_SKETCH_INTERNAL_MAGIC;
 	VMOVE( skt->V, V );
 	VMOVE( skt->u_vec, u_vec );
 	VMOVE( skt->v_vec, v_vec );
-	skt->vert_count = 5;
+	skt->vert_count = 10;
 	skt->verts = (point2d_t *)bu_calloc( skt->vert_count, sizeof( point2d_t ), "verts" );
-	for( i=0 ; i<skt->vert_count ; i++ )
-		V2MOVE( skt->verts[i], verts[i] )
+	for( i=0 ; i<skt->vert_count ; i++ ) {
+		V2MOVE( skt->verts[i], verts[i] );
+	}
 
-	/* curve is a full cirle */
-	crv = &skt->skt_curve;
-	crv->seg_count = 1;
-	crv->segments = (genptr_t *)bu_calloc( 3, sizeof( genptr_t ), "segments" );
-	crv->reverse = (int *)bu_calloc( 3, sizeof( int ), "reverse" );
-	csg = (struct carc_seg *)bu_calloc( 1, sizeof( struct carc_seg ), "carc" );
-	crv->segments[0] = (genptr_t)csg;
+	skt->skt_curve.seg_count = 6;
+	skt->skt_curve.reverse = (int *)bu_calloc( skt->skt_curve.seg_count, sizeof( int ), "sketch: reverse" );
+
+	skt->skt_curve.segments = (genptr_t *)bu_calloc( skt->skt_curve.seg_count, sizeof( genptr_t ), "segs" );
+	bsg = (struct bezier_seg *)bu_malloc( sizeof( struct bezier_seg ), "sketch: bsg" );
+	bsg->magic = CURVE_BEZIER_MAGIC;
+	bsg->degree = 4;
+	bsg->ctl_points = (int *)bu_calloc( bsg->degree+1, sizeof( int ), "sketch: bsg->ctl_points" );
+	bsg->ctl_points[0] = 4;
+	bsg->ctl_points[1] = 7;
+	bsg->ctl_points[2] = 9;
+	bsg->ctl_points[3] = 8;
+	bsg->ctl_points[4] = 0;
+	skt->skt_curve.segments[0] = (genptr_t)bsg;
+
+	lsg = (struct line_seg *)bu_malloc( sizeof( struct line_seg ), "sketch: lsg" );
+	lsg->magic = CURVE_LSEG_MAGIC;
+	lsg->start = 0;
+	lsg->end = 1;
+
+	skt->skt_curve.segments[1] = (genptr_t)lsg;
+
+	lsg = (struct line_seg *)bu_malloc( sizeof( struct line_seg ), "sketch: lsg" );
+	lsg->magic = CURVE_LSEG_MAGIC;
+	lsg->start = 1;
+	lsg->end = 2;
+
+	skt->skt_curve.segments[2] = (genptr_t)lsg;
+
+	lsg = (struct line_seg *)bu_malloc( sizeof( struct line_seg ), "sketch: lsg" );
+	lsg->magic = CURVE_LSEG_MAGIC;
+	lsg->start = 2;
+	lsg->end = 3;
+
+	skt->skt_curve.segments[3] = (genptr_t)lsg;
+
+	lsg = (struct line_seg *)bu_malloc( sizeof( struct line_seg ), "sketch: lsg" );
+	lsg->magic = CURVE_LSEG_MAGIC;
+	lsg->start = 3;
+	lsg->end = 4;
+
+	skt->skt_curve.segments[4] = (genptr_t)lsg;
+
+	csg = (struct carc_seg *)bu_malloc( sizeof( struct carc_seg ), "sketch: csg" );
 	csg->magic = CURVE_CARC_MAGIC;
-	csg->start = 0;
-	csg->end = 1;
 	csg->radius = -1.0;
-	csg->orientation = 0;
+	csg->start = 6;
+	csg->end = 5;
+
+	skt->skt_curve.segments[5] = (genptr_t)csg;
 
 	outfp = wdb_fopen( "sketch.g" );
 	mk_id( outfp, "sketch test" );
