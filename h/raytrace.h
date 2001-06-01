847065d569cc3d527f407c7fd8c1ac3cbf4706b5
Robert Parker <unacknowledged@users.noreply.github.com>
2001-06-01 16:55:35 +0000

    *- add member (wdb_interp) to "struct rt_wdb" *- add members (vo_callback and vo_clientData) to "struct view_obj" *- declare functions for the view object and    the drawable geometry object
    svn:revision:20334
    cvs:account:bparker
    cvs:branch:trunk

--- h/raytrace.h
+++ h/raytrace.h
@@ -1130,6 +1130,7 @@ struct rt_wdb  {
 	int		wdb_mat_default;/* GIFT material code */
 	int		wdb_los_default;/* Line-of-sight estimate */
 	struct bu_observer	wdb_observers;
+	Tcl_Interp	*wdb_interp;
 };
 
 #define	RT_WDB_MAGIC			0x5f576462
@@ -1221,9 +1222,13 @@ struct view_obj {
 	mat_t			vo_view2model;
 	mat_t			vo_pmat;		/* perspective matrix */
 	struct bu_observer	vo_observers;
+	void 			(*vo_callback)();	/* called in vo_update with vo_clientData and vop */
+	genptr_t		vo_clientData;		/* passed to vo_callback */
 };
 extern struct view_obj HeadViewObj;		/* head of view object list */
 #define RT_VIEW_OBJ_NULL		((struct view_obj *)NULL)
+#define RT_MINVIEWSIZE 0.0001
+#define RT_MINVIEWSCALE 0.00005
 
 /*
  *			A N I M A T E
@@ -4659,6 +4664,7 @@ BU_EXTERN(size_t		db5_type_sizeof_n_binu,
 #endif
 
 /* defined in wdb_obj.c */
+BU_EXTERN(int	wdb_get_tcl,		(ClientData clientData, Tcl_Interp *interp, int argc, char **argv));
 BU_EXTERN(int	wdb_init_obj,		(Tcl_Interp *interp, struct rt_wdb *wdbp, char *oname));
 BU_EXTERN(int	wdb_close_cmd,		(struct rt_wdb *wdbp, Tcl_Interp *interp, int argc, char **argv));
 BU_EXTERN(int	wdb_reopen_cmd,		(struct rt_wdb *wdbp, Tcl_Interp *interp, int argc, char **argv));
@@ -4714,6 +4720,85 @@ BU_EXTERN(int	wdb_shells_cmd,		(struct rt_wdb *wdbp, Tcl_Interp *interp, int arg
 BU_EXTERN(int	wdb_xpush_cmd,		(struct rt_wdb *wdbp, Tcl_Interp *interp, int argc, char **argv));
 BU_EXTERN(int	wdb_showmats_cmd,	(struct rt_wdb *wdbp, Tcl_Interp *interp, int argc, char **argv));
 BU_EXTERN(int	wdb_copyeval_cmd,	(struct rt_wdb *wdbp, Tcl_Interp *interp, int argc, char **argv));
+
+/* defined in dg_obj.c */
+BU_EXTERN(void	dgo_autoview,		(struct dg_obj *dgop, struct view_obj *vop, Tcl_Interp *interp));
+BU_EXTERN(int	dgo_autoview_cmd,	(struct dg_obj *dgop, struct view_obj *vop, Tcl_Interp *interp, int argc, char **argv));
+BU_EXTERN(int	dgo_blast_cmd,		(struct dg_obj *dgop, Tcl_Interp *interp, int argc, char **argv));
+BU_EXTERN(int	dgo_draw_cmd,		(struct dg_obj *dgop, Tcl_Interp *interp, int argc, char **argv, int kind));
+BU_EXTERN(int	dgo_E_cmd,		(struct dg_obj *dgop, Tcl_Interp *interp, int argc, char **argv));
+BU_EXTERN(int	dgo_erase_cmd,		(struct dg_obj *dgop, Tcl_Interp *interp, int argc, char **argv));
+BU_EXTERN(int	dgo_erase_all_cmd,	(struct dg_obj *dgop, Tcl_Interp *interp, int argc, char **argv));
+BU_EXTERN(int	dgo_illum_cmd,		(struct dg_obj *dgop, Tcl_Interp *interp, int argc, char **argv));
+BU_EXTERN(int	dgo_label_cmd,		(struct dg_obj *dgop, Tcl_Interp *interp, int argc, char **argv));
+BU_EXTERN(struct dg_obj	*dgo_open_cmd,	(char *oname, struct rt_wdb *wdbp));
+BU_EXTERN(int	dgo_overlay_cmd,	(struct dg_obj *dgop, Tcl_Interp *interp, int argc, char **argv));
+BU_EXTERN(int	dgo_report_cmd,		(struct dg_obj *dgop, Tcl_Interp *interp, int argc, char **argv));
+BU_EXTERN(int	dgo_rt_cmd,		(struct dg_obj *dgop, struct view_obj *vop, Tcl_Interp *interp, int argc, char **argv));
+BU_EXTERN(int	dgo_rtabort_cmd,	(struct dg_obj *dgop, Tcl_Interp *interp, int argc, char **argv));
+BU_EXTERN(int	dgo_rtcheck_cmd,	(struct dg_obj *dgop, struct view_obj *vop, Tcl_Interp *interp, int argc, char **argv));
+BU_EXTERN(int	dgo_vdraw_cmd,		(struct dg_obj *dgop, Tcl_Interp *interp, int argc, char **argv));
+BU_EXTERN(int	dgo_who_cmd,		(struct dg_obj *dgop, Tcl_Interp *interp, int argc, char **argv));
+BU_EXTERN(void	dgo_zap_cmd,		(struct dg_obj *dgop, Tcl_Interp *interp));
+
+extern void dgo_color_soltab();
+extern void dgo_drawH_part2();
+extern void dgo_eraseobjall_callback();
+extern void dgo_eraseobjpath();
+extern void dgo_impending_wdb_close();
+extern int dgo_invent_solid();
+extern void dgo_notify();
+extern void dgo_zapall();
+
+/* defined in bigE.c */
+BU_EXTERN(int	dg_E_cmd,		(struct dg_obj *dgop, Tcl_Interp *interp, int argc, char **argv));
+
+/* defined in view_obj.c */
+BU_EXTERN(struct view_obj	*vo_open_cmd,		(char *oname));
+BU_EXTERN(void	vo_center,		(struct view_obj *vop, Tcl_Interp *interp, point_t center));
+BU_EXTERN(int	vo_center_cmd,		(struct view_obj *vop, Tcl_Interp *interp, int argc, char **argv));
+BU_EXTERN(void	vo_size,		(struct view_obj *vop, Tcl_Interp *interp, fastf_t size));
+BU_EXTERN(int	vo_size_cmd,		(struct view_obj *vop, Tcl_Interp *interp, int argc, char **argv));
+BU_EXTERN(int	vo_invSize_cmd,		(struct view_obj *vop, Tcl_Interp *interp, int argc, char **argv));
+BU_EXTERN(void	vo_mat_aet,		(struct view_obj *vop));
+BU_EXTERN(int	vo_zoom,		(struct view_obj *vop, Tcl_Interp *interp, fastf_t sf));
+BU_EXTERN(int	vo_zoom_cmd,		(struct view_obj *vop, Tcl_Interp *interp, int argc, char **argv));
+BU_EXTERN(int	vo_orientation_cmd,	(struct view_obj *vop, Tcl_Interp *interp, int argc, char **argv));
+BU_EXTERN(int	vo_lookat_cmd,		(struct view_obj *vop, Tcl_Interp *interp, int argc, char **argv));
+BU_EXTERN(void	vo_setview,		(struct view_obj *vop, Tcl_Interp *interp, double a1, double a2, double a3));
+BU_EXTERN(int	vo_setview_cmd,		(struct view_obj *vop, Tcl_Interp *interp, int argc, char **argv));
+BU_EXTERN(int	vo_eye_cmd,		(struct view_obj *vop, Tcl_Interp *interp, int argc, char **argv));
+BU_EXTERN(int	vo_eye_pos_cmd,		(struct view_obj *vop, Tcl_Interp *interp, int argc, char **argv));
+BU_EXTERN(int	vo_pmat_cmd,		(struct view_obj *vop, Tcl_Interp *interp, int argc, char **argv));
+BU_EXTERN(int	vo_perspective_cmd,	(struct view_obj *vop, Tcl_Interp *interp, int argc, char **argv));
+BU_EXTERN(void	vo_update,		(struct view_obj *vop, Tcl_Interp *interp, int oflag));
+BU_EXTERN(int	vo_aet_cmd,		(struct view_obj *vop, Tcl_Interp *interp, int argc, char **argv));
+BU_EXTERN(int	vo_rmat_cmd,		(struct view_obj *vop, Tcl_Interp *interp, int argc, char **argv));
+BU_EXTERN(int	vo_model2view_cmd,	(struct view_obj *vop, Tcl_Interp *interp, int argc, char **argv));
+BU_EXTERN(int	vo_pmodel2view_cmd,	(struct view_obj *vop, Tcl_Interp *interp, int argc, char **argv));
+BU_EXTERN(int	vo_view2model_cmd,	(struct view_obj *vop, Tcl_Interp *interp, int argc, char **argv));
+BU_EXTERN(int	vo_pov_cmd,		(struct view_obj *vop, Tcl_Interp *interp, int argc, char **argv));
+BU_EXTERN(int	vo_units_cmd,		(struct view_obj *vop, Tcl_Interp *interp, int argc, char **argv));
+BU_EXTERN(int	vo_base2local_cmd,	(struct view_obj *vop, Tcl_Interp *interp, int argc, char **argv));
+BU_EXTERN(int	vo_local2base_cmd,	(struct view_obj *vop, Tcl_Interp *interp, int argc, char **argv));
+BU_EXTERN(int	vo_rot,			(struct view_obj *vop, Tcl_Interp *interp, char coord, char origin,
+					 mat_t rmat, int (*func)()));
+BU_EXTERN(int	vo_rot_cmd,		(struct view_obj *vop, Tcl_Interp *interp, int argc, char **argv, int (*func)()));
+BU_EXTERN(int	vo_arot_cmd,		(struct view_obj *vop, Tcl_Interp *interp, int argc, char **argv, int (*func)()));
+BU_EXTERN(int	vo_mrot_cmd,		(struct view_obj *vop, Tcl_Interp *interp, int argc, char **argv, int (*func)()));
+BU_EXTERN(int	vo_tra,			(struct view_obj *vop, Tcl_Interp *interp, char coord, vect_t tvec,
+					 int (*func)()));
+BU_EXTERN(int	vo_tra_cmd,		(struct view_obj *vop, Tcl_Interp *interp, int argc, char **argv,
+					 int (*func)()));
+BU_EXTERN(int	vo_slew,		(struct view_obj *vop, Tcl_Interp *interp, vect_t svec));
+BU_EXTERN(int	vo_slew_cmd,		(struct view_obj *vop, Tcl_Interp *interp, int argc, char **argv));
+BU_EXTERN(int	vo_observer_cmd,	(struct view_obj *vop, Tcl_Interp *interp, int argc, char **argv));
+BU_EXTERN(int	vo_coord_cmd,		(struct view_obj *vop, Tcl_Interp *interp, int argc, char **argv));
+BU_EXTERN(int	vo_rotate_about_cmd,	(struct view_obj *vop, Tcl_Interp *interp, int argc, char **argv));
+BU_EXTERN(int	vo_keypoint_cmd,	(struct view_obj *vop, Tcl_Interp *interp, int argc, char **argv));
+BU_EXTERN(int	vo_vrot_cmd,		(struct view_obj *vop, Tcl_Interp *interp, int argc, char **argv));
+BU_EXTERN(int	vo_sca,			(struct view_obj *vop, Tcl_Interp *interp, fastf_t sf, int (*func)()));
+BU_EXTERN(int	vo_sca_cmd,		(struct view_obj *vop, Tcl_Interp *interp, int argc, char **argv, int (*func)()));
 
 /*
  *  Constants provided and used by the RT library.
