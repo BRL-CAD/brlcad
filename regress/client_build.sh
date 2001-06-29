af004d83f64fedb63df91be40d9e40ef131e4ad1
Lee Butler <iraytrace@users.noreply.github.com>
2001-06-29 18:21:19 +0000

    misc
    svn:revision:20410
    cvs:account:butler
    cvs:branch:trunk

--- regress/client_build.sh
+++ regress/client_build.sh
@@ -3,6 +3,50 @@
 #  This script is intended to be run only by the client_build.sh script.
 #  That is where the necessary environment variables are set.
 
+args=`getopt b:d:e:qr:u: $*`
+
+if [ $? != 0 ] ; then
+	echo "Usage: $0 [-r brlcad_root] [-u user] -d regress_dir "
+	exit 2
+fi
+
+set -- $args
+
+MAILUSER=acst
+QUIET=0
+BRLCAD_ROOT=/usr/brlcad
+
+for i in $* ; do
+	case "$i" in
+		-d)
+			echo "-d $2";
+			REGRESS_DIR=$2;
+			export REGRESS_DIR
+			shift 2;;
+		-q)
+			echo "-q";
+			QUIET=1;
+			shift;;
+		-r)
+			echo "-r $2";
+			BRLCAD_ROOT=$2;
+			shift 2;;
+		-u)
+			echo "-d $2";
+			MAILUSER=$2;
+			export MAILUSER
+			shift 2;;
+		--)
+			shift; break;;
+	esac
+done
+
+
+export QUIET
+export BRLCAD_ROOT
+
+
+
 PATH=$PATH:$BRLCAD_ROOT/bin
 export PATH
 
