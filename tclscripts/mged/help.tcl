c7b71f3e54ef5edb89e8483c570530c5a6bbb0cb
John Anderson <daytonajra@gmail.com>
2001-08-21 13:26:07 +0000

    Added help for "attr" and "attr_rm" commands
    svn:revision:20574
    cvs:account:jra
    cvs:branch:trunk

--- tclscripts/mged/help.tcl
+++ tclscripts/mged/help.tcl
@@ -26,6 +26,10 @@ set mged_help_data(arot)            {{x y z angle} {rotate about axis x,y,z by a
 set mged_help_data(attach)	{{[-d display_string] [-i init_script] [-n name]
 	      [-t is_toplevel] [-W width] [-N height]
 	      [-S square_size] win_type}	{attach to a display manager}}
+set mged_help_data(attr)        {{object attr_name attr_value [attr_name attr_value ...]}
+	      {assign or adjust attribute values for the specified object}}
+set mged_help_data(attr_rm)     {{object attr_name [attr_name attr_name ...]}
+	      {delete attributes for the specified object}}
 set mged_help_data(autoview)	{{}	{set view size and center so that all displayed solids are in view}}
 set mged_help_data(B)		{{-C#/#/# <objects>}	{clear screen, edit objects}}
 set mged_help_data(bev)		{{[-t] [-P#] new_obj obj1 op obj2 op obj3 op ...}	{boolean evaluation of objects via NMG's}}
