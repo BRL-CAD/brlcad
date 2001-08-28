ead01e722bdfeb60521ddb8771ea8a9f128b9db5
Christopher Sean Morrison <brlcad@gmail.com>
2001-08-28 15:59:23 +0000

    added crontab entries and comments about this file's utility
    svn:revision:20631
    cvs:account:morrison
    cvs:branch:trunk

--- regress/nightly.sh
+++ regress/nightly.sh
@@ -1,4 +1,23 @@
 #!/bin/sh
+#
+###
+#
+# This script intended for internal use.  It verifies that there is a current
+# checkout of the regression suite available in the current working directory
+# and then will either invoke the master script or the status script.
+#
+###
+#
+# crontab entries for running the master scripts
+#
+#   0 5 * * * ~/nightly.sh master
+#   0 8 * * * ~/nightly.sh status
+#
+# crontab entries for running the client scripts with nfs or local disk access
+#
+#   30 5 * * * ~/brlcad/regress/client_wait.sh -d /c/regress
+#
+###############################################################################
 
 case $1 in
 master)
