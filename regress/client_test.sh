3fa54e061b8748d93afdf209b6ace01610e0fe9e
Christopher Sean Morrison <brlcad@gmail.com>
2001-09-06 02:52:45 +0000

    *** empty log message ***
    svn:revision:20676
    cvs:account:morrison
    cvs:branch:trunk

--- regress/client_test.sh
+++ regress/client_test.sh
@@ -17,25 +17,25 @@ initializeVariable TESTS_DIR "$PWD/tests.d"
 # if [ -f ${REGRESS_DIR}/.regress.${ARCH}/MAKE_LOG ] ; then mv ${REGRESS_DIR}/.regress.${ARCH}/MAKE_LOG ${REGRESS_DIR}/.regress.${ARCH}/MAKE_LOG.bak ; fi
 
 if [ -d $TESTS_DIR ] ; then
-    echo "Running tests found in $TESTS_DIR"
+    log "Running tests found in $TESTS_DIR"
 
     SCRIPTS=`ls -B $TESTS_DIR`
     for SCRIPT in $SCRIPTS ; do
 	if [ -f ${TESTS_DIR}/$SCRIPT ] && [ -x ${TESTS_DIR}/$SCRIPT ] ; then
-		echo "Running [${TESTS_DIR}/$SCRIPT] test" 
-		echo "RUNNING TEST:" >> $REGRESS_DIR/.regress.${ARCH}/MAKE_LOG
-		echo ${TESTS_DIR}/$SCRIPT >> $REGRESS_DIR/.regress.${ARCH}/MAKE_LOG
+		log "Running [${TESTS_DIR}/$SCRIPT] test" 
+		log "RUNNING TEST:" >> $REGRESS_DIR/.regress.${ARCH}/MAKE_LOG
+		log ${TESTS_DIR}/$SCRIPT >> $REGRESS_DIR/.regress.${ARCH}/MAKE_LOG
 		${TESTS_DIR}/$SCRIPT >> $REGRESS_DIR/.regress.${ARCH}/MAKE_LOG 2>&1
 	fi
     done
 else
-    echo "WARNING: could not find any tests to run in $TESTS_DIR"
+    log "WARNING: could not find any tests to run in $TESTS_DIR"
 fi
 
 ###cd $REGRESS_DIR/.regress.${ARCH}
 
 
-echo "$HOSTNAME regression complete at `date`" >> $REGRESS_DIR/.regress.${ARCH}/MAKE_LOG
+log "$HOSTNAME regression complete at `date`" >> $REGRESS_DIR/.regress.${ARCH}/MAKE_LOG
 
 #
 # Now we check to see how we compare to our "reference" run
@@ -49,19 +49,19 @@ else
 # only master (non-client) scripts should be setting reference
 #	cp $REGRESS_DIR/.regress.${ARCH}/MAKE_LOG ./ref_${ARCH}
 
-	echo "No reference file was available" >> $REGRESS_DIR/.regress.${ARCH}/DIFFS
+	log "No reference file was available" >> $REGRESS_DIR/.regress.${ARCH}/DIFFS
 	cat $REGRESS_DIR/.regress.${ARCH}/MAKE_LOG  >> $REGRESS_DIR/.regress.${ARCH}/DIFFS
 fi
 
-echo "--- Execution Test Status ---" >> $REGRESS_DIR/.regress.${ARCH}/DIFFS
+log "--- Execution Test Status ---" >> $REGRESS_DIR/.regress.${ARCH}/DIFFS
 tail -10 $REGRESS_DIR/.regress.${ARCH}/MAKE_LOG >> $REGRESS_DIR/.regress.${ARCH}/DIFFS
 
 #export MAILUSER=morrison@arl.army.mil
 #if [ -s $REGRESS_DIR/.regress.${ARCH}/DIFFS -a X$MAILUSER != X ] ; then
-#    echo "MAILING OUT DIFF FILE"
+#    log "MAILING OUT DIFF FILE"
 #    mail -s "Regression Errors .regress.${ARCH}" $MAILUSER < $REGRESS_DIR/.regress.${ARCH}/DIFFS
 # 	$MAIL -s "Regression Errors .regress.${ARCH}"  $REPORT_TO_USER \
 # 		< $REGRESS_DIR/.regress.${ARCH}/DIFFS
 #fi
 
-echo "Done client_test.sh"
+log "Done $0"
