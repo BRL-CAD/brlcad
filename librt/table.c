9ceb7e888fa4fdcf6fe0ea2af91e98ee27c71a39
Robert Parker <unacknowledged@users.noreply.github.com>
2001-10-15 15:24:27 +0000

    *- fixed syntax error
    svn:revision:20937
    cvs:account:bparker
    cvs:branch:trunk

--- librt/table.c
+++ librt/table.c
@@ -165,7 +165,6 @@ const struct bu_structparse rt_nul_parse[] = {
 			const struct rt_db_internal *ip, int verbose, \
 			double mm2local, struct resource *resp)); \
 	BU_EXTERN(int rt_/**/name/**/_xform, (struct rt_db_internal *op, \
-
 			const mat_t mat, struct rt_db_internal *ip, \
 			int free, struct db_i *dbip, \
 			struct resource *resp)); \
