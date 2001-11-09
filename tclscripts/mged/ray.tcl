a0949261bc4eecd5607e59ddb5f284e68a81b2c8
Robert Parker <unacknowledged@users.noreply.github.com>
2001-11-09 20:26:48 +0000

    *- mods to mouse_shoot_ray: 	Take into account whether or not we're zclipping. 	If we are not, then back out of the geometry 	before shooting.
    svn:revision:21049
    cvs:account:bparker
    cvs:branch:trunk

--- tclscripts/mged/ray.tcl
+++ tclscripts/mged/ray.tcl
@@ -12,14 +12,30 @@
 proc mouse_shoot_ray { x y } {
     global perspective_mode
 
+    # get zclip setting
+    set zclip [dm set zclip]
+
     eval .inmem rt_gettrees ray -i -u [_mged_who]
     ray prep 1
     ray no_bool 1
     set xx [expr $x / 2048.0]
     set yy [expr $y / 2048.0]
     set target [view2model $xx $yy 0]
-    if { $perspective_mode } {
+    if {$perspective_mode} {
 	set start [view2model 0 0 1]
+    } elseif {!$zclip} {
+	# calculate a view Z that's in front of all geometry
+
+	# get center and size of displayed geometry
+	set center_size [get_autoview]
+
+	# extract view Z from center of geometry, then back the rest of the way out of the geometry
+	set Z [expr [lindex [eval model2view [lindex $center_size 1]] 2] + [expr [lindex $center_size 3] / [size]]]
+	if {$Z < 1} {
+	    set Z 1
+	}
+
+	set start [view2model $xx $yy $Z]
     } else {
 	set start [view2model $xx $yy 1]
     }
