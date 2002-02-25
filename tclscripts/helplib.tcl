6495a5d6c05ad9b5d14889818e107b863ec404eb
John Anderson <daytonajra@gmail.com>
2002-02-25 21:53:34 +0000

    Added "-A" and "-o" options to "B", "e", and "draw" commands
    svn:revision:21257
    cvs:account:jra
    cvs:branch:trunk

--- tclscripts/helplib.tcl
+++ tclscripts/helplib.tcl
@@ -151,9 +151,9 @@ set helplib_data(vo_zoom)		{{sf} {zoom view by specified scale factor}}
 
 set helplib_data(dgo_assoc)		{{[wdb_obj]} {set/get the associated database object}}
 set helplib_data(dgo_autoview)		{{view_obj} {calculate an appropriate view size and center for view_obj}}
-set helplib_data(dgo_blast)		{{[-C#/#/#] object(s)} {erase all currently displayed geometry and draw the specified object(s)}}
+set helplib_data(dgo_blast)		{{[-A -o -C#/#/#] <object(s) | attribute name/value pairs>} {erase all currently displayed geometry and draw the specified object(s)}}
 set helplib_data(dgo_clear)		{{} {erase all objects from the display}}
-set helplib_data(dgo_draw)		{{[-C#/#/#] <objects>} {prepare object(s) for display}}
+set helplib_data(dgo_draw)		{{[-A -o -C#/#/#] <objects | attribute name/value pairs>} {prepare object(s) for display}}
 set helplib_data(dgo_E)			{{[-s] <objects>} {evaluated display of objects}}
 set helplib_data(dgo_erase)		{{<objects>} {erase objects from the display}}
 set helplib_data(dgo_erase_all)		{{<objects>} {erase all occurrences of objects from the display}}
