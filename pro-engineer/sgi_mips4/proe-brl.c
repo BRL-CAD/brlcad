da7ad001b69143c03615a2c5c8dfd910ea6e0418
John Anderson <daytonajra@gmail.com>
2002-09-20 15:13:43 +0000

    Removing empty parts now is inside a "catch" command
    svn:revision:21559
    cvs:account:jra
    cvs:branch:rel-6-0-1-branch

--- pro-engineer/sgi_mips4/proe-brl.c
+++ pro-engineer/sgi_mips4/proe-brl.c
@@ -263,7 +263,7 @@ kill_empty_parts()
 	ptr = empty_parts_root;
 	while( ptr ) {
 		fprintf( outfp, "set combs [dbfind %s]\n", ptr->name );
-		fprintf( outfp, "foreach comb $combs {\n\trm $comb %s\n}\n", ptr->name );
+		fprintf( outfp, "foreach comb $combs {\n\tcatch {rm $comb %s}\n}\n", ptr->name );
 		ptr = ptr->next;
 	}
 }
