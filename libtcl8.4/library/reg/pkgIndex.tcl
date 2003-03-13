ad8d0f54596bdf586435d79fe72954126d5b25d5
John Anderson <daytonajra@gmail.com>
2003-03-13 19:18:53 +0000

    *** empty log message ***
    svn:revision:21834
    cvs:account:jra
    cvs:branch:trunk

---
+++ libtcl8.4/library/reg/pkgIndex.tcl
@@ -0,0 +1,8 @@
+if {![package vsatisfies [package provide Tcl] 8]} {return}
+if {[info exists tcl_platform(debug)]} {
+    package ifneeded registry 1.1.1 \
+            [list load [file join $dir tclreg11g.dll] registry]
+} else {
+    package ifneeded registry 1.1.1 \
+            [list load [file join $dir tclreg11.dll] registry]
+}
