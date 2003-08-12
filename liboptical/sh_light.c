7430b6f9acd3dcf557b2de9874a8e8e9663d3677
Justin Shumaker <twingy@brlcad.org>
2003-08-12 22:53:40 +0000

    light update with new default lumens
    svn:revision:21990
    cvs:account:justin
    cvs:branch:photonmap-branch

--- liboptical/sh_light.c
+++ liboptical/sh_light.c
@@ -832,7 +832,7 @@ light_maker(int num, mat_t v2m)
 
 		/* XXX Is it bogus to set lt_aim? */
 		VSET( lsp->lt_aim, 0, 0, -1 );	/* any direction: spherical */
-		lsp->lt_intensity = 1000.0;
+		lsp->lt_intensity = 1.0;
 		lsp->lt_radius = 0.1;		/* mm, "point" source */
 		lsp->lt_visible = 0;		/* NOT explicitly modeled */
 		lsp->lt_invisible = 1;		/* NOT explicitly modeled */
