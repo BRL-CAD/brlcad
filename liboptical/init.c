a917d0e8e143e7bf477423953aa990486a340fda
Christopher Sean Morrison <brlcad@gmail.com>
2003-09-27 01:03:20 +0000

    rdebug, AmbientIntensity, and background globals were moved to here from the raytrace front-end
    svn:revision:22125
    cvs:account:morrison
    cvs:branch:trunk

--- liboptical/init.c
+++ liboptical/init.c
@@ -39,8 +39,17 @@ static const char RCSid[] = "@(#)$Header$ (ARL)";
 #include "shadework.h"
 #include "rtprivate.h"
 
+int		rdebug;			/* RT program debugging */
+double		AmbientIntensity = 0.4;	/* Ambient light intensity */
+
 #define MFUNCS(_name)	\
 	{ extern struct mfuncs _name[]; mlib_add_shader( headp, _name ); }
+
+#if 0
+vect_t	background = { 0.25, 0, 0.5 };	/* Dark Blue Background */
+#else
+extern vect_t	background = { 0, 0, 1.0/255 };	/* Nearly Black */
+#endif
 
 /*
  *			O P T I C A L _ S H A D E R _ I N I T
