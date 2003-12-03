f301acbcbe2deb910de370fe5bf2d7d5451f66fa
Erik Greenwald <erik@brlcad.org>
2003-12-03 16:25:43 +0000

    updates from HEAD, part 1
    svn:revision:22237
    cvs:branch:AUTOCONF
    cvs:account:erikg

---
+++ liboptical/vers.c
@@ -0,0 +1,4 @@
+char liboptical_version[] = "\
+@(#) BRL-CAD Release 6.1   liboptical\n\
+    Wed Dec  3 10:01:24 EST 2003, Compilation 2\n\
+    erikg@xon.arl.army.mil:/vld/erikg/src/brlcad/liboptical\n";
