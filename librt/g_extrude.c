159e28a997eb18b8ba5a44171d292320406ff9b9
John Anderson <daytonajra@gmail.com>
2004-02-03 21:45:26 +0000

    Corrected signature for rt_extrude_import5()
    svn:revision:22375
    cvs:account:jra
    cvs:branch:trunk

--- librt/g_extrude.c
+++ librt/g_extrude.c
@@ -2344,7 +2344,8 @@ rt_extrude_import5(
 	const struct bu_external	*ep,
 	register const mat_t		mat,
 	const struct db_i		*dbip,
-	struct resource			*resp)
+	struct resource			*resp,
+	const int			minor_type )
 {
 	LOCAL struct rt_extrude_internal	*extrude_ip;
 	struct rt_db_internal			tmp_ip;
