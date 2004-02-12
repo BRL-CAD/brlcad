76f4a36ee661fbc23f9f53c9a958d1e07679330b
Erik Greenwald <erik@brlcad.org>
2004-02-12 18:39:33 +0000

    merge from HEAD
    svn:revision:22387
    cvs:branch:AUTOCONF
    cvs:account:erikg

--- liboptical/vers.c
+++ liboptical/vers.c
@@ -1,4 +1,4 @@
 char liboptical_version[] = "\
-@(#) BRL-CAD Release 6.1   liboptical\n\
-    Wed Dec  3 12:37:39 EST 2003, Compilation 4\n\
-    erikg@xon.arl.army.mil:/vld/erikg/src/brlcad/liboptical\n";
+@(#) BRL-CAD Release 6.1DP   The BRL-CAD Optical Shader Library\n\
+    Thu Jan  2 20:43:20 EST 2003, Compilation 1\n\
+    morrison@hole.arl.army.mil:/Users/morrison/brlcad/liboptical\n";
