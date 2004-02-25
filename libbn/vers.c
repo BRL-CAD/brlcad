78870aea83a1514b193cbef3f5f6d76bb4206916
Erik Greenwald <erik@brlcad.org>
2004-02-25 14:55:26 +0000

    vers.c files are generated
    svn:revision:22406
    cvs:account:erikg
    cvs:branch:AUTOCONF

--- libbn/vers.c
+++
@@ -1,4 +0,0 @@
-char bn_version[] = "\
-@(#) BRL-CAD Release 6.1DP   The BRL-CAD Numerical Computation Library\n\
-    Thu Jan  2 20:19:35 EST 2003, Compilation 1\n\
-    morrison@hole.arl.army.mil:/Users/morrison/brlcad/libbn\n";
--- libfb/vers.c
+++
@@ -1,4 +0,0 @@
-char fb_version[] = "\
-@(#) BRL-CAD Release 6.1DP   The BRL-CAD FrameBuffer Library\n\
-    Thu Jan  2 20:22:41 EST 2003, Compilation 1\n\
-    morrison@hole.arl.army.mil:/Users/morrison/brlcad/libfb\n";
