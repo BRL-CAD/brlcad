56fcaff311a8e08fdc92424b62751995e7a7cafe
Christopher Sean Morrison <brlcad@gmail.com>
2004-03-03 19:53:34 +0000

    stop messing with the makefiles since it really just messes things up.  cakefile should do the changes if the changes are needed.  this means that tcl8.4 and tk8.4 dirs are now in /usr/brlcad/lib/. as would normally be expected from a manual install.
    svn:revision:22428
    cvs:account:morrison
    cvs:branch:trunk

--- libtcl8.4/fix_makefile.sh
+++ libtcl8.4/fix_makefile.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-sed -e s@lib/tcl\$\(VERSION\)@tcl\$\(VERSION\)@ Makefile |\
-	 sed -e s@\(prefix\)/include@\(prefix\)/include/brlcad@ |\
-	 sed -e "s@\${\C\C}  \${TCLSH_OBJS}@\${\C\C} $LDFLAGS \${TCLSH_OBJS}@" |\
-	 sed -e "s@\${\C\C}  \${TCLTEST_OBJS}@\${\C\C} $LDFLAGS \${TCLTEST_OBJS}@" |\
-         sed -e "s@\$(BIN_INSTALL_DIR)/tclsh\$(VERSION)@\$(BIN_INSTALL_DIR)/tclsh@"
+cat Makefile
+
+#sed -e s@lib/tcl\$\(VERSION\)@tcl\$\(VERSION\)@ Makefile |\
+#	 sed -e s@\(prefix\)/include@\(prefix\)/include/brlcad@ |\
+#	 sed -e "s@\${\C\C}  \${TCLSH_OBJS}@\${\C\C} $LDFLAGS \${TCLSH_OBJS}@" |\
+#	 sed -e "s@\${\C\C}  \${TCLTEST_OBJS}@\${\C\C} $LDFLAGS \${TCLTEST_OBJS}@" |\
+#         sed -e "s@\$(BIN_INSTALL_DIR)/tclsh\$(VERSION)@\$(BIN_INSTALL_DIR)/tclsh@"
