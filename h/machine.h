72f97f8a2df0bc4f6ce26b5bf0666cff114005bc
Christopher Sean Morrison <brlcad@gmail.com>
2004-03-12 14:13:59 +0000

    unsnafu machine defines that were obliterated
    svn:revision:22474
    cvs:account:morrison
    cvs:branch:windows-branch

--- h/machine.h
+++ h/machine.h
@@ -205,15 +205,258 @@
  *				  *
  **********************************/
 
+
 #ifdef WIN32
+/********************************
+ *				*
+ *  Windows Windows		*
+ *				*
+ ********************************/
 typedef double fastf_t;	
 #define LOCAL auto	
 #define FAST register	
 typedef long bitv_t;	
-#define CONST const	 
 #define BITV_SHIFT	5
 // assume only one processor for now
 #define MAX_PSW	4
+#define DEFAULT_PSW	1
+#define PARALLEL	1
+#define MALLOC_NOT_MP_SAFE 1
+#endif
+
+
+#ifdef HEP
+/********************************
+ *				*
+ *  Denelcor HEP H-1000		*
+ *				*
+ ********************************/
+#define IBM_FLOAT 1		/* Uses IBM style floating point */
+typedef double	fastf_t;	/* double|float, "Fastest" float type */
+#define LOCAL	auto		/* static|auto, for serial|parallel cpu */
+#define FAST	register	/* LOCAL|register, for fastest floats */
+typedef long	bitv_t;		/* largest integer type */
+#define BITV_SHIFT	6	/* log2( bits_wide(bitv_t) ) */
+
+#define MAX_PSW		128	/* Max number of process streams */
+#define DEFAULT_PSW	MAX_PSW
+#define PARALLEL	1
+#endif
+
+
+#if defined(__alpha)
+/********************************
+ *				*
+ *	  DEC Alpha (AXP)	*
+ *				*
+ ********************************/
+#define IEEE_FLOAT 1		/* Uses IEEE style floating point */
+#if !defined(LITTLE_ENDIAN)
+	/* Often defined in <alpha/endian.h> */
+#	define LITTLE_ENDIAN	1	/* Under the influence of Intel Corp */
+#endif
+typedef double	fastf_t;	/* double|float, "Fastest" float type */
+#define LOCAL	auto		/* static|auto, for serial|parallel cpu */
+#define FAST	register	/* LOCAL|register, for fastest floats */
+typedef long	bitv_t;		/* largest integer type */
+#define BITV_SHIFT	6	/* log2( bits_wide(bitv_t) ) */
+
+#define MAX_PSW		1	/* only one processor, max */
+#define DEFAULT_PSW	1
+#endif
+
+
+#if defined(alliant) && !defined(i860)
+/********************************
+ *				*
+ *	Alliant FX/8		*
+ *				*
+ ********************************/
+#define IEEE_FLOAT 1		/* Uses IEEE style floating point */
+typedef double	fastf_t;	/* double|float, "Fastest" float type */
+#define LOCAL	auto		/* static|auto, for serial|parallel cpu */
+#define FAST	register	/* LOCAL|register, for fastest floats */
+typedef long	bitv_t;		/* largest integer type */
+#define BITV_SHIFT	5	/* log2( bits_wide(bitv_t) ) */
+
+#define MAX_PSW		8	/* Max number of processors */
+#define DEFAULT_PSW	MAX_PSW
+#define PARALLEL	1
+
+#endif
+
+
+#if defined(alliant) && defined(i860)
+/********************************
+ *				*
+ *	Alliant FX/2800		*
+ *				*
+ ********************************/
+#define IEEE_FLOAT 1		/* Uses IEEE style floating point */
+#define LITTLE_ENDIAN	1	/* Under the influence of Intel Corp */
+typedef double	fastf_t;	/* double|float, "Fastest" float type */
+#define LOCAL	auto		/* static|auto, for serial|parallel cpu */
+#define FAST	register	/* LOCAL|register, for fastest floats */
+typedef long	bitv_t;		/* largest integer type */
+#define BITV_SHIFT	5	/* log2( bits_wide(bitv_t) ) */
+
+#define MAX_PSW		28	/* Max number of processors */
+#define DEFAULT_PSW	MAX_PSW
+#define PARALLEL	1
+
+#endif
+
+
+#ifdef CRAY
+/********************************
+ *				*
+ *  Cray-X/MP, COS or UNICOS	*
+ *  Cray-2 under "UNICOS"	*
+ *				*
+ ********************************/
+typedef double	fastf_t;	/* double|float, "Fastest" float type */
+#define LOCAL	auto		/* static|auto, for serial|parallel cpu */
+#define FAST	register	/* LOCAL|register, for fastest floats */
+typedef long	bitv_t;		/* largest integer type */
+#define BITV_SHIFT	6	/* log2( bits_wide(bitv_t) ) */
+
+#define MAX_PSW		4	/* Max number of processors */
+#define DEFAULT_PSW	1
+#define PARALLEL	1
+
+#  if 0
+#	define CRAY_COS	1	/* Running on Cray under COS w/bugs */
+#  endif
+#endif
+
+#if defined(convex) || defined(__convex__)
+/********************************
+ *				*
+ *  Convex C1 & C2		*
+ *				*
+ ********************************/
+typedef double		fastf_t;/* double|float, "Fastest" float type */
+#define LOCAL		auto	/* static|auto, for serial|parallel cpu */
+#define FAST		register /* LOCAL|register, for fastest floats */
+#if 1
+typedef long long	bitv_t;	/* largest integer type */
+#define BITV_SHIFT	6	/* log2( bits_wide(bitv_t) ) */
+#else
+typedef long		bitv_t;
+#define BITV_SHIFT	5
+#endif
+
+#define MAX_PSW		4	/* Max number of processors */
+#define DEFAULT_PSW	1	/* for now */
+#define PARALLEL	1
+#endif
+
+#ifdef ardent
+/********************************
+ *				*
+ *  Stardent (formerly Ardent) 	*
+ *  "Titan" Workstation		*
+ *				*
+ ********************************/
+typedef double	fastf_t;	/* double|float, "Fastest" float type */
+#define LOCAL	auto		/* for parallel cpus */
+#define FAST	register	/* LOCAL|register, for fastest floats */
+typedef long	bitv_t;		/* largest integer type */
+#define BITV_SHIFT	5	/* log2( bits_wide(bitv_t) ) */
+
+#define MAX_PSW		4	/* # processors, max */
+#define DEFAULT_PSW	1
+#define PARALLEL	1
+#endif
+
+#ifdef __stardent
+/********************************
+ *				*
+ *  Stardent VISTRA Workstation	*
+ *  based on Intel i860 chip	*
+ *				*
+ ********************************/
+#define __unix	1		/* It really is unix */
+#define LITTLE_ENDIAN	1	/* Under the influence of Intel Corp */
+typedef double	fastf_t;	/* double|float, "Fastest" float type */
+#define LOCAL	auto		/* for parallel cpus */
+#define FAST	register	/* LOCAL|register, for fastest floats */
+typedef long	bitv_t;		/* largest integer type */
+#define BITV_SHIFT	5	/* log2( bits_wide(bitv_t) ) */
+
+#define MAX_PSW		1	/* only one processor, max */
+#define DEFAULT_PSW	1
+#endif
+
+#if	(defined(__sgi) && defined(__mips))
+/* Strict ANSI C does not define CPP symbols that don't start with __ */
+#	define sgi	1
+#	define mips	1
+#endif
+#if	(defined(sgi) && defined(mips))
+/********************************
+ *				*
+ *  SGI 4D, multi-processor	*
+ *				*
+ ********************************/
+#define IEEE_FLOAT 1		/* Uses IEEE style floating point */
+typedef double	fastf_t;	/* double|float, "Fastest" float type */
+#define LOCAL	auto		/* static|auto, for serial|parallel cpu */
+#define FAST	register	/* LOCAL|register, for fastest floats */
+typedef long	bitv_t;		/* largest integer type */
+#if defined( _MIPS_SZLONG ) && _MIPS_SZLONG == 64
+#define BITV_SHIFT	6	/* log2( bits_wide(bitv_t) ) */
+#else
+#define BITV_SHIFT	5	/* log2( bits_wide(bitv_t) ) */
+#endif
+#define const	const
+
+#define MAX_PSW		1024
+#define DEFAULT_PSW	MAX_PSW
+#define PARALLEL	1
+
+#endif
+
+#ifdef apollo
+/********************************
+ *				*
+ *  Apollo			*
+ *  with SR 10			*
+ *				*
+ ********************************/
+#if __STDC__
+#define const	/**/		/* Does not support const keyword */
+#define const	/**/		/* Does not support const keyword */
+#endif
+
+typedef double	fastf_t;	/* double|float, "Fastest" float type */
+#define LOCAL	static		/* static|auto, for serial|parallel cpu */
+#define FAST	LOCAL		/* LOCAL|register, for fastest floats */
+typedef long	bitv_t;		/* largest integer type */
+#define BITV_SHIFT	5	/* log2( bits_wide(bitv_t) ) */
+
+#define MAX_PSW		1	/* only one processor, max */
+#define DEFAULT_PSW	1
+#define MALLOC_NOT_MP_SAFE 1
+
+#endif
+
+
+#ifdef n16
+/********************************
+ *				*
+ *     Encore Multi-Max		*
+ *				*
+ ********************************/
+#define IEEE_FLOAT	1	/* Uses IEEE style floating point */
+#define LITTLE_ENDIAN	1	/* Under the influence of National Semiconductor */
+typedef double	fastf_t;	/* double|float, "Fastest" float type */
+#define LOCAL	auto		/* static|auto, for serial|parallel cpu */
+#define FAST	register	/* LOCAL|register, for fastest floats */
+typedef long	bitv_t;		/* largest integer type */
+#define BITV_SHIFT	5	/* log2( bits_wide(bitv_t) ) */
+
+#define MAX_PSW		32	/* This number is uncertain */
 #define DEFAULT_PSW	1
 #define PARALLEL	1
 #define MALLOC_NOT_MP_SAFE 1
@@ -378,9 +621,41 @@ typedef long	bitv_t;		/* largest integer type */
  *  Definitions for big-endian -vs- little-endian.
  *	BIG_ENDIAN:	Byte [0] is on left side of word (msb).
  *	LITTLE_ENDIAN:	Byte [0] is on right side of word (lsb).
+ */
+#ifdef vax
+# define LITTLE_ENDIAN	1
+#endif
 
- *  Bit vector mask */
+#if !defined(BIG_ENDIAN) && !defined(LITTLE_ENDIAN)
+# define BIG_ENDIAN	1	/* The common case */
+#endif
+
+/*  Bit vector mask */
 #define BITV_MASK	((1<<BITV_SHIFT)-1)
+
+/*
+ * Definitions about limits of floating point representation
+ * Eventually, should be tied to type of hardware (IEEE, IBM, Cray)
+ * used to implement the fastf_t type.
+ */
+#if defined(vax) || (defined(sgi) && !defined(mips))
+	/* DEC VAX "D" format, the most restrictive */
+#define MAX_FASTF		1.0e37	/* Very close to the largest number */
+#define SQRT_MAX_FASTF		1.0e18	/* This squared just avoids overflow */
+#define SMALL_FASTF		1.0e-37	/* Anything smaller is zero */
+#define SQRT_SMALL_FASTF	1.0e-18	/* This squared gives zero */
+#else
+	/* IBM format, being the next most restrictive format */
+#define MAX_FASTF		1.0e73	/* Very close to the largest number */
+#define SQRT_MAX_FASTF		1.0e36	/* This squared just avoids overflow */
+#define SMALL_FASTF		1.0e-77	/* Anything smaller is zero */
+#if defined(aux)
+#  define SQRT_SMALL_FASTF	1.0e-40 /* _doprnt error in libc */
+#else
+#  define SQRT_SMALL_FASTF	1.0e-39	/* This squared gives zero */
+#endif
+#endif
+#define SMALL			SQRT_SMALL_FASTF
 
 /*
  *  Definition of a "generic" pointer that can hold a pointer to anything.
@@ -397,27 +672,35 @@ typedef long	bitv_t;		/* largest integer type */
 #  define GENPTR_NULL	((genptr_t)0)
 #endif
 
-/* A portable way of handling the ANSI C const keyword: use CONST */
-#if !defined(CONST)
-# if __STDC__
-#	define	CONST	const
-# else
-#	define	CONST	/**/
-# endif
+/* A portable way of handling pre-ANSI C: remove const keyword */
+#if !defined(__STDC__)
+#  define	const	/**/
 #endif
+#if defined(CONST)
+#  undef  CONST
+#endif
+#define CONST deprecated
 
 /* Even in C++ not all compilers know the "bool" keyword yet */
 #if !defined(BOOL_T)
 # define BOOL_T	int
 #endif
 
-/* A portable way of dealing with pre-ANSI C.  Assume signed variables */
-#if !defined(SIGNED)
-# if __STDC__
-#	define SIGNED	signed
-# else
-#	define SIGNED	/**/
-# endif
+/* A portable way of handling pre-ANSI C: remove signed keyword */
+#if !defined(__STDC__)
+#  define	signed	/**/
+#endif
+#if defined(SIGNED)
+#  undef SIGNED
+#endif
+#define SIGNED deprecated
+
+/*
+ *  Some very common BSD --> SYSV conversion aids
+ */
+#if defined(SYSV) && !defined(bzero) && !defined(HAVE_BZERO)
+#	define bzero(str,n)		memset( str, 0, n )
+#	define bcopy(from,to,count)	memcpy( to, from, count )
 #endif
 
 /* Functions local to one file should be declared HIDDEN:  (nil)|static */
@@ -430,8 +713,17 @@ typedef long	bitv_t;		/* largest integer type */
 # endif
 #endif
 
-#if defined(WIN32)
-#	define hypot _hypot
+/*
+ *  ANSI and POSIX do not seem to have prototypes for the hypot() routine,
+ *  but several vendors include it in their -lm math library.
+ */
+#if defined(_POSIX_SOURCE) && !defined(__USE_MISC)
+	/* But the sgi -lm does have a hypot routine so lets use it */
+#if defined(__sgi) || defined(__convexc__)
+        extern double hypot(double, double);
+#else
+#	define hypot(x,y)      sqrt( (x)*(x)+(y)*(y) )
+#endif
 #endif
 
 #if defined(SUNOS) && SUNOS >= 52
