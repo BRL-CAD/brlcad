85649daf5bd304c7e84538846600f9be615ca07a
Erik Greenwald <erik@brlcad.org>
2004-03-16 13:00:08 +0000

    fbsd/irix fixes
    svn:revision:22511
    cvs:account:erikg
    cvs:branch:autoconf-branch

--- h/bu.h
+++ h/bu.h
@@ -50,6 +50,14 @@ extern "C" {
 
 #define BU_H_VERSION	"@(#)$Header$ (BRL)"
 
+#ifdef HAVE_TIME_H
+# include <time.h>
+#endif
+
+#ifdef HAVE_SYS_TIME_H
+# include <sys/time.h>
+#endif
+
 /*----------------------------------------------------------------------*/
 /*
  *  System library routines used by LIBBS.
