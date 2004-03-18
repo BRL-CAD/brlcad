b9ef4a9fe06707ffbb380985fe9428ae1d0e62d2
Christopher Sean Morrison <brlcad@gmail.com>
2004-03-18 15:43:57 +0000

    merge from AUTOCONF into HEAD
    svn:revision:22547
    cvs:account:morrison
    cvs:branch:trunk

--- libwdb/vers.c
+++
@@ -1,4 +0,0 @@
-char libwdb_version[] = "\
-@(#) BRL-CAD Release 6.1DP   The BRL-CAD Library for Writing Database\n\
-    Thu Jan  2 20:20:16 EST 2003, Compilation 1\n\
-    morrison@hole.arl.army.mil:/Users/morrison/brlcad/libwdb\n";
