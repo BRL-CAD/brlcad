e21f65b4ea7e1f4cbe69625e6e678f2bdfe8fc64
Erik Greenwald <erik@brlcad.org>
2004-03-31 20:22:31 +0000

    automakeification
    svn:revision:22566
    cvs:account:erikg
    cvs:branch:trunk

--- libtclcad/vers.c
+++
@@ -1,4 +0,0 @@
-char tclcad_version[] = "\
-@(#) BRL-CAD Release 6.0   BRL-CAD specific routines for Tcl/Tk\n\
-    Mon Apr  1 13:31:16 CST 2002, Compilation 1\n\
-    root@Mustang:/disk1/brlcad6.0/libtclcad\n";
