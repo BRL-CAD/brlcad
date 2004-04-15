c1ff3860fa8eec01359eccc3a6ddc4ae843b600a
Christopher Sean Morrison <brlcad@gmail.com>
2004-04-15 11:24:25 +0000

    use autoreconf if it is available and rephrase progress
    svn:revision:22687
    cvs:account:morrison
    cvs:branch:trunk

--- autogen.sh
+++ autogen.sh
@@ -2,13 +2,22 @@
 # script to prepare the sources
 ###
 
-libtoolize --automake --copy --force || glibtoolize --automake --copy --force
-[ ! $? = 0 ] && echo "libtoolize failed" && echo 1
-aclocal && [ ! $? = 0 ] && echo "aclocal failed" && exit 2
-autoheader && [ ! $? = 0 ] && echo "autoheader failed" && exit 3
-automake --add-missing --copy && [ ! $? = 0 ] && echo "automake failed" && exit 4
-autoconf && [ ! $? = 0 ] && echo "autoconf failed" && exit 5
-
-echo "BRL-CAD sources are now prepared. To build here, run:"
+which autoreconf > /dev/null 2>&1
+if [ $? = 0 ] ; then
+    echo -n "Autoconfiguring..."
+    autoreconf --force --install
+    [ ! $? = 0 ] && echo "autoreconf failed" && exit 1
+else
+    echo -n "Configuring..."
+    libtoolize --automake --copy --force || glibtoolize --automake --copy --force
+    [ ! $? = 0 ] && echo "libtoolize failed" && exit 1
+    aclocal && [ ! $? = 0 ] && echo "aclocal failed" && exit 2
+    autoheader && [ ! $? = 0 ] && echo "autoheader failed" && exit 3
+    automake --add-missing --copy && [ ! $? = 0 ] && echo "automake failed" && exit 4
+    autoconf && [ ! $? = 0 ] && echo "autoconf failed" && exit 5
+fi
+echo "done"
+echo
+echo "The BRL-CAD build system is now prepared.  To build here, run:"
 echo "  ./configure"
 echo "  make"
