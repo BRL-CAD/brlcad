eac36658ffd711760d8e9f029673ad9842ee0b70
Christopher Sean Morrison <brlcad@gmail.com>
2004-04-20 14:29:43 +0000

    add support for running autogen.sh from other directories. add support for providing build arguments for one-step configure-builds. check for help.
    svn:revision:22693
    cvs:account:morrison
    cvs:branch:trunk

--- autogen.sh
+++ autogen.sh
@@ -2,13 +2,20 @@
 # script to prepare the sources
 ###
 
+ARGS="$*"
+PATH_TO_AUTOGEN="`dirname $0`"
+
+# prepare build via autoreconf or manually
+###
+_prev_path="`pwd`"
+cd "$PATH_TO_AUTOGEN"
 which autoreconf > /dev/null 2>&1
 if [ $? = 0 ] ; then
-    echo -n "Autoconfiguring..."
+    echo -n "Automatically preparing build ... "
     autoreconf --force --install
     [ ! $? = 0 ] && echo "autoreconf failed" && exit 1
 else
-    echo -n "Configuring..."
+    echo -n "Preparing build ... "
     libtoolize --automake --copy --force || glibtoolize --automake --copy --force
     [ ! $? = 0 ] && echo "libtoolize failed" && exit 1
     aclocal && [ ! $? = 0 ] && echo "aclocal failed" && exit 2
@@ -16,8 +23,34 @@ else
     automake --add-missing --copy && [ ! $? = 0 ] && echo "automake failed" && exit 4
     autoconf && [ ! $? = 0 ] && echo "autoconf failed" && exit 5
 fi
+cd "$_prev_path"
 echo "done"
 echo
-echo "The BRL-CAD build system is now prepared.  To build here, run:"
-echo "  ./configure"
-echo "  make"
+
+# check for help arg, and bypass running make
+###
+which sed > /dev/null 2>&1
+if [ $? = 0 ] ; then
+    if [ "x`echo $ARGS | sed 's/.*help.*/help/'`" = "xhelp" ] ; then
+	echo "$PATH_TO_AUTOGEN/configure $ARGS"
+	$PATH_TO_AUTOGEN/configure $ARGS
+	exit
+    fi
+elif [ "x$ARGS" = "x--help" ] ; then
+    echo "$PATH_TO_AUTOGEN/configure $ARGS"
+    $PATH_TO_AUTOGEN/configure $ARGS
+    exit
+fi
+
+# summarize.  actually build if arg was provided.
+###
+if [ ! "x$ARGS" = "x" ] ; then
+    echo "The BRL-CAD build system is now prepared.  Building here with:"
+    echo "$PATH_TO_AUTOGEN/configure $ARGS"
+    $PATH_TO_AUTOGEN/configure $ARGS
+    make
+else
+    echo "The BRL-CAD build system is now prepared.  To build here, run:"
+    echo "  $PATH_TO_AUTOGEN/configure"
+    echo "  make"
+fi
