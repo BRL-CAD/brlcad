7bbc89326fa0c49afc2b5f36e4a3ae4bfa2706c0
Christopher Sean Morrison <brlcad@gmail.com>
2004-05-21 18:06:15 +0000

    moved from top-level to src/other/.
    svn:revision:22835
    cvs:account:morrison
    cvs:branch:trunk

--- libtcl8.4/fix_makefile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cat Makefile
-
-#sed -e s@lib/tcl\$\(VERSION\)@tcl\$\(VERSION\)@ Makefile |\
-#	 sed -e s@\(prefix\)/include@\(prefix\)/include/brlcad@ |\
-#	 sed -e "s@\${\C\C}  \${TCLSH_OBJS}@\${\C\C} $LDFLAGS \${TCLSH_OBJS}@" |\
-#	 sed -e "s@\${\C\C}  \${TCLTEST_OBJS}@\${\C\C} $LDFLAGS \${TCLTEST_OBJS}@" |\
-#         sed -e "s@\$(BIN_INSTALL_DIR)/tclsh\$(VERSION)@\$(BIN_INSTALL_DIR)/tclsh@"
