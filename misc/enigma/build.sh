de95215cfc2a2f0bdec14d7e96bb5d7984550f89
Christopher Sean Morrison <brlcad@gmail.com>
2004-05-25 13:59:18 +0000

    quell the newer automake by adding the missing files
    svn:revision:22886
    cvs:account:morrison
    cvs:branch:trunk

--- misc/enigma/build.sh
+++ misc/enigma/build.sh
@@ -2,6 +2,18 @@
 # configures and builds enigma
 ###
 
+if ! test -f NEWS ; then
+    touch NEWS
+fi
+if ! test -f AUTHORS ; then
+    touch AUTHORS
+fi
+if ! test -f ChangeLog ; then
+    touch ChangeLog
+fi
+if ! test -f INSTALL ; then
+    touch INSTALL
+fi
 aclocal && \
 autoheader && \
 automake --add-missing --copy && \
