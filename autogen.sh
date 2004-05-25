efae2cf86950c97ee5bf0f016a5d8a5cdd3007e2
Christopher Sean Morrison <brlcad@gmail.com>
2004-05-25 14:23:58 +0000

    add local var section and apply ws style change
    svn:revision:22890
    cvs:account:morrison
    cvs:branch:trunk

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 #                        a u t o g e n . s h
 #
-# script to prepare the sources
+# Script for automatically preparing the sources for compilation by
+# performing the myrid of necessary steps.  The script attempts to
+# detect proper version support, and outputs warnings about particular
+# systems that have autotool peculiarities.
+#
+# Author: Christopher Sean Morrison
+# This script is in the public domain
 #
 ######################################################################
 
@@ -23,7 +29,7 @@ AUTOMAKE_PATCH_VERSION=0
 _have_sed="`echo no | sed 's/no/yes/'`"
 HAVE_SED=no
 if [ $? = 0 ] ; then
-    [ "x$_have_sed" = "xyes" ] && HAVE_SED=yes
+  [ "x$_have_sed" = "xyes" ] && HAVE_SED=yes
 fi
 
 
@@ -34,35 +40,35 @@ autoconf -V > /dev/null 2>&1
 [ $? = 0 ] && _autofound=yes || _autofound=no
 _report_error=no
 if [ ! "x$_autofound" = "xyes" ] ; then
-    echo "ERROR:  Unable to locate GNU Autoconf."
-    _report_error=yes
+  echo "ERROR:  Unable to locate GNU Autoconf."
+  _report_error=yes
 else
-    _version_line="`autoconf -V | head -1`"
-    if [ "x$HAVE_SED" = "xyes" ] ; then
-	_maj_version="`echo $_version_line | sed 's/.*\([0-9]\)\..*/\1/'`"
-	_min_version="`echo $_version_line | sed 's/.*\.\([0-9][0-9]\).*/\1/'`"
-	if [ $? = 0 ] ; then
-	    if [ $_maj_version -lt $AUTOCONF_MAJOR_VERSION ] ; then
-		_report_error=yes
-	    elif [ $_min_version -lt $AUTOCONF_MINOR_VERSION ] ; then
-		_report_error=yes
-	    fi
-	fi
-	echo "Found GNU Autoconf version $_maj_version.$_min_version"
-    else
-	echo "Warning:  sed is not available to properly detect version of GNU Autoconf"
+  _version_line="`autoconf -V | head -1`"
+  if [ "x$HAVE_SED" = "xyes" ] ; then
+    _maj_version="`echo $_version_line | sed 's/.*\([0-9]\)\..*/\1/'`"
+    _min_version="`echo $_version_line | sed 's/.*\.\([0-9][0-9]\).*/\1/'`"
+    if [ $? = 0 ] ; then
+      if [ $_maj_version -lt $AUTOCONF_MAJOR_VERSION ] ; then
+	_report_error=yes
+      elif [ $_min_version -lt $AUTOCONF_MINOR_VERSION ] ; then
+	_report_error=yes
+      fi
     fi
-    echo
+    echo "Found GNU Autoconf version $_maj_version.$_min_version"
+  else
+    echo "Warning:  sed is not available to properly detect version of GNU Autoconf"
+  fi
+  echo
 fi
 if [ "x$_report_error" = "xyes" ] ; then
-    echo "ERROR:  To prepare the BRL-CAD build system from scratch,"
-    echo "        At least version $AUTOCONF_MAJOR_VERSION.$AUTOCONF_MINOR_VERSION of GNU Autoconf must be installed."
-    echo 
-    echo "$PATH_TO_AUTOGEN/autogen.sh does not need to be run on the same machine that will"
-    echo "run configure or make.  Either the GNU Autotools will need to be installed"
-    echo "or upgraded on this system, or $PATH_TO_AUTOGEN/autogen.sh must be run on the source"
-    echo "code on another system and transferred to here. -- Cheers!"
-    exit 1
+  echo "ERROR:  To prepare the BRL-CAD build system from scratch,"
+  echo "        At least version $AUTOCONF_MAJOR_VERSION.$AUTOCONF_MINOR_VERSION of GNU Autoconf must be installed."
+  echo 
+  echo "$PATH_TO_AUTOGEN/autogen.sh does not need to be run on the same machine that will"
+  echo "run configure or make.  Either the GNU Autotools will need to be installed"
+  echo "or upgraded on this system, or $PATH_TO_AUTOGEN/autogen.sh must be run on the source"
+  echo "code on another system and transferred to here. -- Cheers!"
+  exit 1
 fi
 
 
@@ -72,7 +78,7 @@ fi
 HAVE_AUTORECONF=yes
 autoreconf -V > /dev/null 2>&1
 if [ ! $? = 0 ] ; then
-    HAVE_AUTORECONF=no
+  HAVE_AUTORECONF=no
 fi
 
 
@@ -83,35 +89,35 @@ HAVE_LIBTOOLIZE=yes
 HAVE_GLIBTOOLIZE=no
 libtoolize --version > /dev/null 2>&1
 if [ ! $? = 0 ] ; then
-    HAVE_LIBTOOLIZE=no
-    if [ "x$HAVE_AUTORECONF" = "xno" ] ; then
-	echo "Warning:  libtoolize does not appear to be available."
-    else
-	echo "Warning:  libtoolize does not appear to be available.  This means that"
-	echo "autoreconf cannot be used."
-    fi
+  HAVE_LIBTOOLIZE=no
+  if [ "x$HAVE_AUTORECONF" = "xno" ] ; then
+    echo "Warning:  libtoolize does not appear to be available."
+  else
+    echo "Warning:  libtoolize does not appear to be available.  This means that"
+    echo "autoreconf cannot be used."
+  fi
 
-    _glibtoolize="`glibtoolize --version 2>/dev/null`"
+  _glibtoolize="`glibtoolize --version 2>/dev/null`"
+  if [ $? = 0 ] ; then
+    HAVE_GLIBTOOLIZE=yes
+    echo 
+    echo "Fortunately, glibtoolize was found which means that your system may simply"
+    echo "have a non-standard or incomplete GNU Autotools install.  If you have"
+    echo "sufficient system access, it may be possible to quell this warning by"
+    echo "running:"
+    echo
+    sudo -V > /dev/null 2>&1
     if [ $? = 0 ] ; then
-	HAVE_GLIBTOOLIZE=yes
-	echo 
-	echo "Fortunately, glibtoolize was found which means that your system may simply"
-	echo "have a non-standard or incomplete GNU Autotools install.  If you have"
-	echo "sufficient system access, it may be possible to quell this warning by"
-	echo "running:"
-	echo
-	sudo -V > /dev/null 2>&1
-	if [ $? = 0 ] ; then
-	    _glti="`which glibtoolize`"
-	    _gltidir="`dirname $_glti`"
-	    echo "   sudo ln -s $_glti $_gltidir/libtoolize"
-	else
-	    echo "   ln -s $glti $_gltidir/libtoolize"
-	    echo 
-	    echo "Run that as root or with proper permissions to the $_gltidir directory"
-	fi
-	echo
+      _glti="`which glibtoolize`"
+      _gltidir="`dirname $_glti`"
+      echo "   sudo ln -s $_glti $_gltidir/libtoolize"
+    else
+      echo "   ln -s $glti $_gltidir/libtoolize"
+      echo 
+      echo "Run that as root or with proper permissions to the $_gltidir directory"
     fi
+    echo
+  fi
 fi
 
 
@@ -127,16 +133,16 @@ cd "$PATH_TO_AUTOGEN"
 ##################################
 
 if ! test -f AUTHORS ; then
-    touch AUTHORS
+  touch AUTHORS
 fi
 if ! test -f ChangeLog ; then
-    touch ChangeLog
+  touch ChangeLog
 fi
 if ! test -f INSTALL ; then
-    touch INSTALL
+  touch INSTALL
 fi
 if ! test -f NEWS ; then
-    touch NEWS
+  touch NEWS
 fi
 
 
@@ -144,26 +150,26 @@ fi
 # prepare build via autoreconf or manually #
 ############################################
 if [ "x$HAVE_AUTORECONF" = "xyes" ] && [ "x$HAVE_LIBTOOLIZE" = "xyes" ] ; then
-    echo -n "Automatically preparing build ..."
-    autoreconf --force --i
-    [ ! $? = 0 ] && echo "ERROR: autoreconf failed" && exit 2
+  echo -n "Automatically preparing build ..."
+  autoreconf --force --i
+  [ ! $? = 0 ] && echo "ERROR: autoreconf failed" && exit 2
 else
-    echo -n "Preparing build ..."
-    if [ "x$HAVE_LIBTOOLIZE" = "xyes" ] ; then 
-	libtoolize --automake -c -f
-	[ ! $? = 0 ] && echo "ERROR: libtoolize failed" && exit 2
-    else
-	[ "x$HAVE_GLIBTOOLIZE" = "xyes" ] && glibtoolize --automake --copy --force
-	[ ! $? = 0 ] && echo "ERROR: glibtoolize failed" && exit 2
-    fi
-    aclocal
-    [ ! $? = 0 ] && echo "ERROR: aclocal failed" && exit 2
-    autoheader 
-    [ ! $? = 0 ] && echo "ERROR: autoheader failed" && exit 2
-    automake -f -a -c 
-    [ ! $? = 0 ] && echo "ERROR: automake failed" && exit 2
-    autoconf -f
-    [ ! $? = 0 ] && echo "ERROR: autoconf failed" && exit 2
+  echo -n "Preparing build ..."
+  if [ "x$HAVE_LIBTOOLIZE" = "xyes" ] ; then 
+    libtoolize --automake -c -f
+    [ ! $? = 0 ] && echo "ERROR: libtoolize failed" && exit 2
+  else
+    [ "x$HAVE_GLIBTOOLIZE" = "xyes" ] && glibtoolize --automake --copy --force
+    [ ! $? = 0 ] && echo "ERROR: glibtoolize failed" && exit 2
+  fi
+  aclocal
+  [ ! $? = 0 ] && echo "ERROR: aclocal failed" && exit 2
+  autoheader 
+  [ ! $? = 0 ] && echo "ERROR: autoheader failed" && exit 2
+  automake -f -a -c 
+  [ ! $? = 0 ] && echo "ERROR: automake failed" && exit 2
+  autoconf -f
+  [ ! $? = 0 ] && echo "ERROR: autoconf failed" && exit 2
 fi
 
 
@@ -182,10 +188,10 @@ _help=no
 [ "x$HAVE_SED" = "xyes" ] && [ "x`echo $ARGS | sed 's/.*help.*/help/'`" = "xhelp" ] && _help=yes
 [ "x$ARGS" = "x--help" ] && _help=yes
 if [ "x$_help" = "xyes" ] ; then
-    echo "Help was requested.  No configuration and compilation will be done."
-    echo "Running: $PATH_TO_AUTOGEN/configure $ARGS"
-    $PATH_TO_AUTOGEN/configure $ARGS
-    exit 0
+  echo "Help was requested.  No configuration and compilation will be done."
+  echo "Running: $PATH_TO_AUTOGEN/configure $ARGS"
+  $PATH_TO_AUTOGEN/configure $ARGS
+  exit 0
 fi
 
 
@@ -193,15 +199,24 @@ fi
 # summarize.  actually build if arg was provided #
 ##################################################
 if [ ! "x$ARGS" = "x" ] ; then
-    echo "The BRL-CAD build system is now prepared.  Building here with:"
-    echo "$PATH_TO_AUTOGEN/configure $ARGS"
-    $PATH_TO_AUTOGEN/configure $ARGS
-    [ ! $? = 0 ] && echo "ERROR: configure failed" && exit $?
-    make
-    [ ! $? = 0 ] && echo "ERROR: make failed" && exit $?
-    exit 0
+  echo "The BRL-CAD build system is now prepared.  Building here with:"
+  echo "$PATH_TO_AUTOGEN/configure $ARGS"
+  $PATH_TO_AUTOGEN/configure $ARGS
+  [ ! $? = 0 ] && echo "ERROR: configure failed" && exit $?
+  make
+  [ ! $? = 0 ] && echo "ERROR: make failed" && exit $?
+  exit 0
 fi
 
 echo "The BRL-CAD build system is now prepared.  To build here, run:"
 echo "  $PATH_TO_AUTOGEN/configure"
 echo "  make"
+
+# Local Variables: ***
+# mode: sh ***
+# tab-width: 8 ***
+# sh-basic-offset: 2 ***
+# sh-indentation: 2 ***
+# indent-tabs-mode: t ***
+# End: ***
+# ex: shiftwidth=2 tabstop=8
