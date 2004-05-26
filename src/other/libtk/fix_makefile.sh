b0dc4ddfdf677bc63d4b6a5b3587ee94944366a4
Christopher Sean Morrison <brlcad@gmail.com>
2004-05-26 14:36:39 +0000

    should no longer need to fix the makefile
    svn:revision:22901
    cvs:account:morrison
    cvs:branch:trunk

--- src/other/libtk/fix_makefile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cat Makefile
-
-#sed -e s@lib/tk\$\(VERSION\)@tk\$\(VERSION\)@ Makefile |\
-#	 sed -e s@\(prefix\)/include@\(prefix\)/include/brlcad@ |\
-#	 sed -e "s@\$(\C\C)  \$(WISH_OBJS)@\$(\C\C) $LDFLAGS \$(WISH_OBJS)@" |\
-#	 sed -e "s@\${\C\C}  \$(TKTEST_OBJS)@\${\C\C} $LDFLAGS \$(TKTEST_OBJS)@" |\
-#         sed -e "s@wish\$(VERSION)@wish@"
