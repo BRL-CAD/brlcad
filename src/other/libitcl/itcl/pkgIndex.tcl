25dbf5db2647a9ff1bc77d3719ca50606a8b4613
Christopher Sean Morrison <brlcad@gmail.com>
2004-06-10 16:51:58 +0000

    moved libitcl to incrTcl, which matches their cvs module
    svn:revision:23005
    cvs:account:morrison
    cvs:branch:trunk

--- src/other/libitcl/itcl/pkgIndex.tcl
+++
@@ -1,3 +0,0 @@
-# Tcl package index file, version 1.0
-
-package ifneeded Itcl 3.3 [list load [file join $dir "libitcl3.3.so"] Itcl]
--- src/other/libitcl/itk/pkgIndex.tcl
+++
@@ -1,3 +0,0 @@
-# Tcl package index file, version 1.0
-
-package ifneeded Itk 3.3 [list load [file join $dir "libitk3.3.so"] Itk]
