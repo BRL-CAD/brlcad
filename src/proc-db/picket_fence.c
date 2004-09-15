ac352d21eda277c522666e36fb03cb86b48c6474
Christopher Sean Morrison <brlcad@gmail.com>
2004-09-15 07:16:14 +0000

    fix slew of warnings regarding need for struct rt_wdb * instead of FILE * and using the list to the mk_*() instead of the whole struct
    svn:revision:23335
    cvs:account:morrison
    cvs:branch:trunk

--- src/proc-db/picket_fence.c
+++ src/proc-db/picket_fence.c
@@ -9,7 +9,7 @@
 
 int main(int argc, char *argv[])
 {
-  FILE *f;
+  struct rt_wdb *fp_db;
   long i, j, k, l;
   char name[256];
   char pname[256];
@@ -78,8 +78,12 @@ int main(int argc, char *argv[])
   strcpy(prefix, argv[2]);
   ps = atof(argv[4]);
 
-  f = fopen(argv[1], "w");
-  mk_id(f, "Picket Fence");
+  fp_db = wdb_fopen(argv[1]);
+  if (fp_db == NULL) {
+    perror(argv[1]);
+    exit(2);
+  }
+  mk_id(fp_db, "Picket Fence");
   
   BU_LIST_INIT(&wm.l);
   BU_LIST_INIT(&wm2.l);
@@ -118,45 +122,45 @@ int main(int argc, char *argv[])
       c0h[2] = height;
 
       sprintf(w1name, "%swedge1-%ld.s", prefix, j);
-      mk_wedge(f, w1name, w0, w0x, w0z, xlen, ylen, zlen, x_top_len);
+      mk_wedge(fp_db, w1name, w0, w0x, w0z, xlen, ylen, zlen, x_top_len);
       
       sprintf(w2name, "%swedge2-%ld.s", prefix, j);
-      mk_wedge(f, w2name, w1, w1x, w1z, xlen, ylen, zlen, x_top_len);
+      mk_wedge(fp_db, w2name, w1, w1x, w1z, xlen, ylen, zlen, x_top_len);
       
       sprintf(name, "%spost-%ld.s", prefix, j);
-      mk_arb8(f, name, s0);
-      mk_addmember(name, &wm, NULL, WMOP_UNION);
-      mk_addmember(w1name, &wm, NULL, WMOP_SUBTRACT);
-      mk_addmember(w2name, &wm, NULL, WMOP_SUBTRACT);
+      mk_arb8(fp_db, name, s0);
+      mk_addmember(name, &wm.l, NULL, WMOP_UNION);
+      mk_addmember(w1name, &wm.l, NULL, WMOP_SUBTRACT);
+      mk_addmember(w2name, &wm.l, NULL, WMOP_SUBTRACT);
       
       if (round)
 	{
 	  sprintf(name, "%spost_c.s", prefix);
-	  mk_tgc(f, name, c0, c0h, c0a, c0b, c0c, c0d);
-	  mk_addmember(name, &wm, NULL, WMOP_UNION);
-	  mk_addmember(w1name, &wm, NULL, WMOP_SUBTRACT);
-	  mk_addmember(w2name, &wm, NULL, WMOP_SUBTRACT);
+	  mk_tgc(fp_db, name, c0, c0h, c0a, c0b, c0c, c0d);
+	  mk_addmember(name, &wm.l, NULL, WMOP_UNION);
+	  mk_addmember(w1name, &wm.l, NULL, WMOP_SUBTRACT);
+	  mk_addmember(w2name, &wm.l, NULL, WMOP_SUBTRACT);
 	}
       sprintf(name, "%sls%ld.s", prefix, j);
-      mk_arb8(f, name, s1);
-      mk_addmember(name, &swm, NULL, WMOP_UNION);
+      mk_arb8(fp_db, name, s1);
+      mk_addmember(name, &swm.l, NULL, WMOP_UNION);
       
       for (k = 0; k < 8; k++)
 	s1[(3 * k) + 2] += (height / 3);
       
       sprintf(name, "%shs%ld.s", prefix, j);
-      mk_arb8(f, name, s1);
-      mk_addmember(name, &swm, NULL, WMOP_UNION);
+      mk_arb8(fp_db, name, s1);
+      mk_addmember(name, &swm.l, NULL, WMOP_UNION);
       
       sprintf(pname, "%sp-%ld.c", prefix, j);
-      mk_lcomb(f, pname, &wm, 0, "plastic", "", "50 30 10", 0);
+      mk_lcomb(fp_db, pname, &wm, 0, "plastic", "", "50 30 10", 0);
       
       for (i = 0; i < numposts; i++)
 	{
 	  sprintf(name, "%sp%ld-%ld.r", prefix, j, i);
-	  mk_addmember(pname, &wm2, NULL, WMOP_UNION);
-	  mk_lcomb(f, name, &wm2, 0, "plastic", "", "50 50 20", 0);
-	  nwm = mk_addmember(name, &swm, NULL, WMOP_UNION);
+	  mk_addmember(pname, &wm2.l, NULL, WMOP_UNION);
+	  mk_lcomb(fp_db, name, &wm2, 0, "plastic", "", "50 50 20", 0);
+	  nwm = mk_addmember(name, &swm.l, NULL, WMOP_UNION);
 	  for (k = 0; k < 16; k++)
 	    nwm->wm_mat[k] = 0;
 	  nwm->wm_mat[0] = 1;
@@ -168,8 +172,8 @@ int main(int argc, char *argv[])
 	}
       
       sprintf(name, "%ssec%ld.c", prefix, j);
-      mk_lcomb(f, name, &swm, 0, "plastic", "", "50 50 20", 0);
-      nwm = mk_addmember(name, &fwm, NULL, WMOP_SUBTRACT);
+      mk_lcomb(fp_db, name, &swm, 0, "plastic", "", "50 50 20", 0);
+      nwm = mk_addmember(name, &fwm.l, NULL, WMOP_SUBTRACT);
       xt = x1 - x0;
       yt = y1 - y0;
       xt /= sqrt((xt * xt) + (yt * yt));
@@ -183,7 +187,7 @@ int main(int argc, char *argv[])
       nwm->wm_mat[10] = 1;
       nwm->wm_mat[11] = z0;
       nwm->wm_mat[15] = 1;
-      nwm = mk_addmember(name, &fwm, NULL, WMOP_UNION);
+      nwm = mk_addmember(name, &fwm.l, NULL, WMOP_UNION);
       xt = x1 - x0;
       yt = y1 - y0;
       xt /= sqrt((xt * xt) + (yt * yt));
@@ -204,9 +208,9 @@ int main(int argc, char *argv[])
 	    first_mat[l] = nwm->wm_mat[l];
 	}
     }
-  nwm = mk_addmember(firstname, &fwm, NULL, WMOP_SUBTRACT);
+  nwm = mk_addmember(firstname, &fwm.l, NULL, WMOP_SUBTRACT);
   for (l = 0; l < 16; l++)
     nwm->wm_mat[l] = first_mat[l];
   sprintf(name, "%sfence.c", prefix);
-  mk_lcomb(f, name, &fwm, 0, "plastic", "", "50 50 20", 0);
+  mk_lcomb(fp_db, name, &fwm, 0, "plastic", "", "50 50 20", 0);
 }
