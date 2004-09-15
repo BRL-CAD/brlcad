67719b4bdceebdf9a9b03f883cfe799798336f64
Christopher Sean Morrison <brlcad@gmail.com>
2004-09-15 05:14:37 +0000

    don't discard the qualifier from the target pointer type on rt_plot_cell extern declaration (ia64 quell)
    svn:revision:23322
    cvs:account:morrison
    cvs:branch:trunk

--- src/librt/bundle.c
+++ src/librt/bundle.c
@@ -31,7 +31,7 @@ static const char RCSbundle[] = "@(#)$Header$ (BRL)";
 #include "raytrace.h"
 #include "./debug.h"
 
-extern void	rt_plot_cell(union cutter *cutp, struct rt_shootray_status *ssp, struct bu_list *waiting_segs_hd, struct rt_i *rtip);		/* at end of file */
+extern void	rt_plot_cell(const union cutter *cutp, struct rt_shootray_status *ssp, struct bu_list *waiting_segs_hd, struct rt_i *rtip);
 
 /*
  *			R T _ F I N D _ N U G R I D
