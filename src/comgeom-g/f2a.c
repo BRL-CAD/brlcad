6cdac213085ef7361c43a51efaf1f07f03902dda
Christopher Sean Morrison <brlcad@gmail.com>
2004-12-18 02:21:22 +0000

    acquire copyright and GPL license
    svn:revision:23622
    cvs:account:morrison
    cvs:branch:trunk

--- src/comgeom-g/f2a.c
+++ src/comgeom-g/f2a.c
@@ -1,4 +1,29 @@
-/* F 2 A () ====  convert float to ascii  w.df format.	No leading blanks */
+/*                           F 2 A . C
+ * BRL-CAD
+ *
+ * Copyright (c) 2004 United States Government as represented by the
+ * U.S. Army Research Laboratory.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this file; see the file named COPYING for more
+ * information.
+ *
+ **//** @file f2a.c
+ *
+ * convert float to ascii.
+ */
+
+ /** F 2 A () ====  convert float to ascii  w.df format.	No leading blanks */
 f2a(float f, char *s, int w, int d)
      	  		/* INPUT	===*/
     	       		/* OUTPUT	===*/
