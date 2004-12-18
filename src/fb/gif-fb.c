3b2dd4b5a29b9445098a87f92ac2ffd4680315b4
Christopher Sean Morrison <brlcad@gmail.com>
2004-12-18 06:09:12 +0000

    assign copyright and GPL license
    svn:revision:23627
    cvs:account:morrison
    cvs:branch:trunk

--- src/fb/gif-fb.c
+++ src/fb/gif-fb.c
@@ -1,4 +1,25 @@
-/*
+/*                        G I F - F B . C
+ * BRL-CAD
+ *
+ * Copyright (c) 2004 United States Government as represented by the
+ * U.S. Army Research Laboratory.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this file; see the file named COPYING for more
+ * information.
+ *
+ */
+/** @file gif-fb.c
 	gif-fb -- convert a GIF file to (overlaid) frame buffer images
 
 	created:	89/04/29	D A Gwyn
--- src/fb/rle-fb.c
+++ src/fb/rle-fb.c
@@ -1,5 +1,25 @@
-/*
- *			R L E - F B . C
+/*                        R L E - F B . C
+ * BRL-CAD
+ *
+ * Copyright (c) 2004 United States Government as represented by the
+ * U.S. Army Research Laboratory.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this file; see the file named COPYING for more
+ * information.
+ *
+ */
+/** @file rle-fb.c
  *
  *  Decode a Utah Raster Toolkit RLE image, and display on a
  *  BRL libfb(3) framebuffer.
