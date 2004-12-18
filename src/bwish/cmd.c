bff129e28bbe1c3b95763a0e482ff051d7bc8e47
Christopher Sean Morrison <brlcad@gmail.com>
2004-12-18 01:54:48 +0000

    assign copyright and GPL license
    svn:revision:23619
    cvs:account:morrison
    cvs:branch:trunk

--- src/bwish/cmd.c
+++ src/bwish/cmd.c
@@ -1,5 +1,24 @@
-/*
- *				C M D . C
+/*                           C M D . C
+ * BRL-CAD
+ *
+ * Copyright (c) 1998-2004 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this file; see the file named COPYING for more
+ * information.
+ *
+ **//** @file cmd.c
  *
  * This is the place where BWISH/BTCLSH's commands live.
  * The history routines were borrowed from mged/history.c
--- src/bwish/input.c
+++ src/bwish/input.c
@@ -1,5 +1,24 @@
-/*
- *				I N P U T . C
+/*                         I N P U T . C
+ * BRL-CAD
+ *
+ * Copyright (c) 1998-2004 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this file; see the file named COPYING for more
+ * information.
+ *
+ **//** @file input.c
  *
  * Input handling routines. Some of this code was borrowed from mged/ged.c
  * and modified for use in this application.
