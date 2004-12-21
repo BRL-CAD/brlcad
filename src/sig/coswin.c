053c03d276a4980379b40cf939560e623702938c
Christopher Sean Morrison <brlcad@gmail.com>
2004-12-21 03:11:57 +0000

    assign copyright and GPL
    svn:revision:23649
    cvs:account:morrison
    cvs:branch:trunk

--- src/sig/coswin.c
+++ src/sig/coswin.c
@@ -1,5 +1,24 @@
-/*
- *			C O S W I N
+/*                        C O S W I N . C
+ * BRL-CAD
+ *
+ * Copyright (c) 2004 United States Government as represented by the
+ * U.S. Army Research Laboratory.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file coswin.c
  *
  *  Do Cosine windowing effecting p percent of the samples in
  *  the buffer.  Precomputes the window function.
