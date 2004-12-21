714bc354f8a50b0c5f719a1e41534e2673fb01b0
Christopher Sean Morrison <brlcad@gmail.com>
2004-12-21 02:37:22 +0000

    assign copyright and GPL
    svn:revision:23644
    cvs:branch:trunk
    cvs:account:morrison

--- src/mk/mk_bolt.c
+++ src/mk/mk_bolt.c
@@ -1,3 +1,26 @@
+/*                       M K _ B O L T . C
+ * BRL-CAD
+ *
+ * Copyright (c) 2004 United States Government as represented by the
+ * U.S. Army Research Laboratory.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file mk_bolt.c
+ *
+ */
 
 /*  File:  mk_bolt.c  */
 /*  S.Coates - 2 September 1992  */
