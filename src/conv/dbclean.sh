2a4431095b35c67cf568c98210cf22d649aa46ec
Christopher Sean Morrison <brlcad@gmail.com>
2004-12-21 01:31:47 +0000

    should remove the bsd license then, and fix the copyright date
    svn:revision:23641
    cvs:account:morrison
    cvs:branch:trunk

--- src/conv/dbclean.sh
+++ src/conv/dbclean.sh
@@ -5,41 +5,6 @@
 # Copyright (c) 1991-2004 United States Government as represented by
 # the U.S. Army Research Laboratory.
 #
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#
-# 1. Redistributions of source code must retain the above copyright
-# notice, this list of conditions and the following disclaimer.
-#
-# 2. Redistributions in binary form must reproduce the above 
-# copyright notice, this list of conditions and the following
-# disclaimer in the documentation and/or other materials provided
-# with the distribution.
-#
-# 3. The name of the author may not be used to endorse or promote
-# products derived from this software without specific prior written
-# permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS
-# OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
-# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
-# GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
-###
-
-# BRL-CAD		D B C L E A N . S H
-#
-# Copyright (c) 2004 United States Government as represented by the
-# U.S. Army Research Laboratory.
-#
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
 # published by the Free Software Foundation; either version 2 of the
@@ -53,8 +18,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this file; see the file named COPYING for more
 # information.
-#
-
+###
 
 # This shell program compacts mged (.g) databases to remove spaces left
 # by the kill/killall commands.  This is achieved by converting the
@@ -64,8 +28,6 @@
 # solids --- results.  Caveat Emptor.
 
 # Author: Susanne Muuss, J.D.
-# Copyright (C), 1991, U.S. Army
-
 
 
 # Test to see that two command line arguments are seen: the original
