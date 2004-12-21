92dcabb0136192f755b5992e8ce38132c9a0ae53
Christopher Sean Morrison <brlcad@gmail.com>
2004-12-21 02:24:40 +0000

    assign copyright and LGPL
    svn:revision:23643
    cvs:branch:trunk
    cvs:account:morrison

--- src/libdm/dm-generic.c
+++ src/libdm/dm-generic.c
@@ -1,5 +1,24 @@
-/*
- *				D M - G E N E R I C . C
+/*                    D M - G E N E R I C . C
+ * BRL-CAD
+ *
+ * Copyright (c) 1999-2004 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public License
+ * as published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file dm-generic.c
  *
  * Generic display manager routines.
  * 
