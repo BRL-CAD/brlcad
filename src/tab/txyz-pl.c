a09bcb354ad3cbedeeeca9dd3e300ac85253c236
Lee Butler <iraytrace@users.noreply.github.com>
2004-12-21 03:53:50 +0000

    GPL license changes
    svn:revision:23654
    cvs:account:butler
    cvs:branch:trunk

--- src/tab/txyz-pl.c
+++ src/tab/txyz-pl.c
@@ -1,4 +1,24 @@
-/*
+/*                       T X Y Z - P L . C
+ * BRL-CAD
+ *
+ * Copyright (c) 1988-2004 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file txyz-pl.c
  *			X Y Z - P L . C
  *
  *  Program to take (up to) 4-column input, expressed as
@@ -13,10 +33,6 @@
  *	SECAD/VLD Computing Consortium, Bldg 394
  *	The U. S. Army Ballistic Research Laboratory
  *	Aberdeen Proving Ground, Maryland  21005-5066
- *  
- *  Copyright Notice -
- *	This software is Copyright (C) 1988-2004 by the United States Army.
- *	All rights reserved.
  */
 #ifndef lint
 static const char RCSid[] = "@(#)$Header$ (BRL)";
