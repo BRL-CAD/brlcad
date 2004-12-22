badea1fd995809297af0d2e034d6a5e00afbbb6a
Lee Butler <iraytrace@users.noreply.github.com>
2004-12-22 03:39:01 +0000

    missed getting the LGPL on these two
    svn:revision:23685
    cvs:account:lbutler
    cvs:branch:trunk

--- include/shadefuncs.h
+++ include/shadefuncs.h
@@ -1,7 +1,24 @@
-#ifndef SHADEFUNCS 
-#define SHADEFUNCS
-/*
- *			S H A D E F U N C S . H
+/*			S H A D E F U N C S . H
+ * BRL-CAD
+ *
+ * Copyright (c) 1993-2004 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file shadefuncs.h
  *  
  *  Source -
  *	The U. S. Army Research Laboratory
@@ -9,6 +26,8 @@
  *
  *  $Header$
  */
+#ifndef SHADEFUNCS 
+#define SHADEFUNCS
 
 /*
  *			M F U N C S
--- include/shadework.h
+++ include/shadework.h
@@ -1,7 +1,24 @@
-#ifndef SHADEWORK_H
-#define SHADEWORK_H
-/*
- *			S H A D E W O R K . H
+/*                          S H A D E W O R K . H
+ * BRL-CAD
+ *
+ * Copyright (c) 1993-2004 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file shadework.h
  *  
  *  Source -
  *	SECAD/VLD Computing Consortium, Bldg 394
@@ -10,6 +27,8 @@
  *
  *  $Header$
  */
+#ifndef SHADEWORK_H
+#define SHADEWORK_H
 
 #define SW_NLIGHTS	16		/* Max # of light sources */
 
