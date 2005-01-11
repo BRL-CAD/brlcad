e7c337336048a028b913c1c66e1958f3d35cf553
Christopher Sean Morrison <brlcad@gmail.com>
2005-01-11 03:48:31 +0000

    oop, the var setting needs to come before the command to not interfere with $?
    svn:revision:23711
    cvs:account:brlcad
    cvs:branch:trunk

--- autogen.sh
+++ autogen.sh
@@ -40,8 +40,8 @@ case `echo "testing\c"; echo 1,2,3`,`echo -n testing; echo 1,2,3` in
   *c*,*  ) ECHO_N=-n ECHO_C= ECHO_T= ;;
   *)       ECHO_N= ECHO_C='\c' ECHO_T= ;;
 esac
-echo "test" | tail -n 1 2>/dev/null 1>&2
 TAIL_N=""
+echo "test" | tail -n 1 2>/dev/null 1>&2
 if [ $? = 0 ] ; then
     TAIL_N="-n "
 fi
