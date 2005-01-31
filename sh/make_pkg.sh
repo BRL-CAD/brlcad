0f66c0d26577cb268be902711a7e804974e7403e
Christopher Sean Morrison <brlcad@gmail.com>
2005-01-31 06:28:11 +0000

    initial untested implementation that should generate an installer pkg that is compatible with both the old and new Installer
    svn:revision:23816
    cvs:account:brlcad
    cvs:branch:trunk

--- sh/make_pkg.sh
+++ sh/make_pkg.sh
@@ -36,12 +36,250 @@
 ###
 #
 # Script for generating a Mac OS X Installer package (.pkg) from a
-# clean BRL-CAD installation.
+# clean package installation.  The package should be compatible with
+# old and new versions of Installer.
 #
 # Author: Christopher Sean Morrison
 #
 ######################################################################
 
+NAME="$1"
+MAJOR_VERSION="$2"
+MINOR_VERSION="$3"
+PATCH_VERSION="$4"
+ARCHIVE="$5"
+if [ "x$NAME" = "x" ] ; then
+    echo "Usage: $0 title major_version minor_version patch_version archive_dir"
+    echo "ERROR: must specify a package name"
+    exit 1
+fi
+if [ "x$MINOR_VERSION" = "x" ] ; then
+    echo "Usage: $0 title major_version minor_version patch_version archive_dir"
+    echo "ERROR: must specify a major package version"
+    exit 1
+fi
+if [ "x$MINOR_VERSION" = "x" ] ; then
+    echo "ERROR: must specify a minor package version"
+    echo "Usage: $0 title major_version minor_version patch_version archive_dir"
+    exit 1
+fi
+if [ "x$PATCH_VERSION" = "x" ] ; then
+    echo "Usage: $0 title major_version minor_version patch_version archive_dir"
+    echo "ERROR: must specify a patch package version"
+    exit 1
+fi
+if [ "x$ARCHIVE" = "x" ] ; then
+    echo "Usage: $0 title major_version minor_version patch_version archive_dir"
+    echo "ERROR: must specify an archive directory"
+    exit 1
+fi
+if [ ! -d "$ARCHIVE" ] ; then
+    echo "ERROR: specified archive path (${ARCHIVE}) is not a directory"
+    exit 1
+fi
+
+VERSION="${MAJOR_VERSION}.${MINOR_VERSION}.${PATCH_VERSION}"
+PKG_NAME="${NAME}-${VERSION}"
+mkdir "${PKG_NAME}.pkg"
+if [ ! -d "${PKG_NAME}.pkg" ] ; then
+    echo "ERROR: unable to create the package directory"
+    exit 1
+fi
+
+mkdir "${PKG_NAME}.pkg/Contents"
+if [ ! -d "${PKG_NAME}.pkg/Contents" ] ; then
+    echo "ERROR: unable to create the package contents directory"
+    exit 1
+fi
+
+mkdir "${PKG_NAME}.pkg/Contents/Resources"
+if [ ! -d "${PKG_NAME}.pkg/Contents/Resources" ] ; then
+    echo "ERROR: unable to create the package resources directory"
+    exit 1
+fi
+
+cat > "${PKG_NAME}.pkg/Contents/PkgInfo" <<EOF
+pmkrpkg1
+EOF
+if [ ! -f "${PKG_NAME}.pkg/Contents/PkgInfo" ] ; then
+    echo "ERROR: unable to create PkgInfo file"
+    exit 1
+fi
+
+cat > "${PKG_NAME}.pkg/Contents/Info.plist" <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+        <key>CFBundleGetInfoString</key>
+        <string>${NAME} ${VERSION}</string>
+        <key>CFBundleIdentifier</key>
+        <string>org.brlcad.${NAME}</string>
+        <key>CFBundleName</key>
+        <string>${NAME}</string>
+        <key>CFBundleShortVersionString</key>
+        <string>${MAJOR_VERSION}.${MINOR_VERSION}</string>
+        <key>IFMajorVersion</key>
+        <integer>${MAJOR_VERSION}</integer>
+        <key>IFMinorVersion</key>
+        <integer>${MINOR_VERSION}</integer>
+        <key>IFPkgFlagAllowBackRev</key>
+        <true/>
+        <key>IFPkgFlagAuthorizationAction</key>
+        <string>AdminAuthorization</string>
+        <key>IFPkgFlagDefaultLocation</key>
+        <string>/</string>
+        <key>IFPkgFlagInstallFat</key>
+        <false/>
+        <key>IFPkgFlagIsRequired</key>
+        <true/>
+        <key>IFPkgFlagRelocatable</key>
+        <false/>
+        <key>IFPkgFlagRestartAction</key>
+        <string>NoRestart</string>
+        <key>IFPkgFlagRootVolumeOnly</key>
+        <false/>
+        <key>IFPkgFlagUpdateInstalledLanguages</key>
+        <false/>
+        <key>IFPkgFormatVersion</key>
+        <real>0.10000000149011612</real>
+</dict>
+</plist>
+EOF
+if [ ! -f "${PKG_NAME}.pkg/Contents/Info.plist" ] ; then
+    echo "ERROR: unable to create Info.plist file"
+    exit 1
+fi
+
+pax -w -f "${PKG_NAME}.pkg/Contents/Archive.pax" "$ARCHIVE"
+if [ $? != 0 ] ; then
+    echo "ERROR: unable to successfully create a pax archive of $ARCHIVE"
+    exit 1
+fi
+if [ ! -f "${PKG_NAME}.pkg/Contents/Archive.pax" ] ; then
+    echo "ERROR: pax archive does not exist"
+    exit 1
+fi
+
+gzip -c "${PKG_NAME}.pkg/Contents/Archive.pax" > "${PKG_NAME}.pkg/Contents/Archive.pax.gz"
+if [ $? != 0 ] ; then
+    echo "ERROR: unable to successfully compress the pax archive"
+    exit 1
+fi
+if [ ! -f "${PKG_NAME}.pkg/Contents/Archive.pax.gz" ] ; then
+    echo "ERROR: compressed pax archive does not exist"
+    exit 1
+fi
+
+rm -f "${PKG_NAME}.pkg/Contents/Archive.pax"
+if [ -f "${PKG_NAME}.pkg/Contents/Archive.pax" ] ; then
+    echo "ERROR: unable to remove uncompressed pax archive"
+    exit 1
+fi
+
+mkbom "$ARCHIVE" "${PKG_NAME}.pkg/Contents/Archive.bom"
+if [ $? != 0 ] ; then
+    echo "ERROR: unable to successfully generate a bill of materials"
+    exit 1
+fi
+if [ ! -f "${PKG_NAME}.pkg/Contents/Archive.bom" ] ; then
+    echo "ERROR: bill of materials file does not exist"
+    exit 1
+fi
+
+NUM_FILES=`find "${ARCHIVE} -type f | wc | awk '{print $1}'`
+if [ "x$NUM_FILES" = "x" ] ; then
+    echo "ERROR: unable to get a file count from $ARCHIVE"
+    exit 1
+fi
+
+INST_SIZE=`du -k -s "${ARCHIVE}" | awk '{print $1}'`
+if [ "x$INST_SIZE" = "x" ] ; then
+    echo "ERROR: unable to get a usage size from $ARCHIVE"
+    exit 1
+fi
+
+COMP_SIZE=`ls -l "${PKG_NAME}.pkg/Contents/Archive.pax.gz" | awk '{print $5}'`
+if [ "x$COMP_SIZE" = "x" ] ; then
+    echo "ERROR: unable to get the compressed archive size"
+    exit 1
+fi
+
+cat > "${PKG_NAME}.pkg/Contents/Resources/${PKG_NAME}.sizes" <<EOF
+NumFiles $NUM_FILES
+InstalledSize $INST_SIZE
+CompressedSize $COMP_SIZE
+EOF
+if [ ! -f "${PKG_NAME}.pkg/Contents/Resources/${PKG_NAME}.sizes" ] ; then
+    echo "ERROR: unable to create the ${PKG_NAME}.size file"
+    exit 1
+fi
+
+cat > "${PKG_NAME}.pkg/Contents/Resources/${PKG_NAME}.info" <<EOF
+Title $NAME
+Version $VERSION
+Description $NAME $VERSION
+DefaultLocation /
+DeleteWarning Don't do it... untested.
+
+### Package Flags
+
+NeedsAuthorization NO
+Required YES
+Relocatable NO
+RequiresReboot NO
+UseUserMask YES
+OverwritePermissions NO
+InstallFat NO
+RootVolumeOnly NO
+EOF
+if [ ! -f "${PKG_NAME}.pkg/Contents/Resources/${PKG_NAME}.info" ] ; then
+    echo "ERROR: unable to create the ${PKG_NAME}.info file"
+    exit 1
+fi
+
+cat > "${PKG_NAME}.pkg/Contents/Resources/Description.plist" <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+        <key>IFPkgDescriptionDeleteWarning</key>
+        <string>Don't do it... untested.</string>
+        <key>IFPkgDescriptionDescription</key>
+        <string>${NAME} ${VERSION}</string>
+        <key>IFPkgDescriptionTitle</key>
+        <string>${NAME}</string>
+        <key>IFPkgDescriptionVersion</key>
+        <string>${VERSION}</string>
+</dict>
+</plist>
+EOF
+if [ ! -f "${PKG_NAME}.pkg/Contents/Resources/Description.plist" ] ; then
+    echo "ERROR: unable to create the Description.plist file"
+    exit 1
+fi
+
+ln -s ../Archive.bom "${PKG_NAME}.pkg/Contents/Resources/${PKG_NAME}.bom"
+if [ $? != 0 ] ; then
+    echo "ERROR: unable to successfully create a symbolic link to the Archive.bom"
+    exit 1
+fi
+if [ ! -h "${PKG_NAME}.pkg/Contents/Resources/${PKG_NAME}.bom" ] ; then
+    echo "ERROR: symbolic link ${PKG_NAME}.bom does not exist"
+    exit 1
+fi
+
+ln -s ../Archive.pax.gz "${PKG_NAME}.pkg/Contents/Resources/${PKG_NAME}.pax.gz"
+if [ $? != 0 ] ; then
+    echo "ERROR: unable to successfully create a symbolic link to the Archive.pax.gz"
+    exit 1
+fi
+if [ ! -h "${PKG_NAME}.pkg/Contents/Resources/${PKG_NAME}.pax.gz" ] ; then
+    echo "ERROR: symbolic link ${PKG_NAME}.pax.gz does not exist"
+    exit 1
+fi
+
+# woo hoo .. done
 
 # Local Variables:
 # mode: sh
