ffdd770c14dd3b3aba71deedad716ff04f62a435
Lee Butler <iraytrace@users.noreply.github.com>
2005-02-10 00:05:07 +0000

    Fixed bad boundary condition where bot has EXACTLY rt_bot_minpieces triangles.  This caused the bot to have a bogus st_aradius, which caused the primitive to be deleted from the model.
    svn:revision:23821
    cvs:account:lbutler
    cvs:branch:trunk

--- src/librt/g_bot_include.c
+++ src/librt/g_bot_include.c
@@ -280,7 +280,7 @@ struct rt_i		*rtip;
 		VMOVE( p2, &bot_ip->vertices[bot_ip->faces[tri_index*3 + 1]*3] );
 		VMOVE( p3, &bot_ip->vertices[bot_ip->faces[tri_index*3 + 2]*3] );
 
-		if( rt_bot_minpieces <= 0 || bot_ip->num_faces < rt_bot_minpieces ) {
+		if( rt_bot_minpieces <= 0 || bot_ip->num_faces <= rt_bot_minpieces ) {
 			VMINMAX( stp->st_min, stp->st_max, p1 );
 			VMINMAX( stp->st_min, stp->st_max, p2 );
 			VMINMAX( stp->st_min, stp->st_max, p3 );
