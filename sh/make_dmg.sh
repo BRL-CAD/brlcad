918f7762434c390f27f68b7d009fc7255d5dc6f8
Christopher Sean Morrison <brlcad@gmail.com>
2005-04-07 04:41:19 +0000

    SWEET! It now has the basic components and will create a DMG on demand.  It still does not do anything with the background image yes thought it does detect it.  The script also does not run misc/macosx/openUp yet either.  Still, sweet progress.
    svn:revision:24152
    cvs:account:brlcad
    cvs:branch:trunk

--- sh/make_dmg.sh
+++ sh/make_dmg.sh
@@ -47,34 +47,29 @@ NAME="$1"
 MAJOR_VERSION="$2"
 MINOR_VERSION="$3"
 PATCH_VERSION="$4"
-BACKGROUND="$5"
+DMG_CAPACITY=100
 
 if [ "x$NAME" = "x" ] ; then
-    echo "Usage: $0 title major_version minor_version patch_version [background]"
+    echo "Usage: $0 title major_version minor_version patch_version [background] [contents ...]"
     echo "ERROR: must specify a title for the package name"
     exit 1
 fi
 if [ "x$MINOR_VERSION" = "x" ] ; then
-    echo "Usage: $0 title major_version minor_version patch_version [background]"
+    echo "Usage: $0 title major_version minor_version patch_version [background] [contents ...]"
     echo "ERROR: must specify a major package version"
     exit 1
 fi
 if [ "x$MINOR_VERSION" = "x" ] ; then
     echo "ERROR: must specify a minor package version"
-    echo "Usage: $0 title major_version minor_version patch_version [background]"
+    echo "Usage: $0 title major_version minor_version patch_version [background] [contents ...]"
     exit 1
 fi
 if [ "x$PATCH_VERSION" = "x" ] ; then
-    echo "Usage: $0 title major_version minor_version patch_version [background]"
+    echo "Usage: $0 title major_version minor_version patch_version [background] [contents ...]"
     echo "ERROR: must specify a patch package version"
     exit 1
 fi
-if [ ! "x$BACKGROUND" = "x" ] ; then
-    if [ ! -r "$BACKGROUND" ] ; then
-	echo "ERROR: specified content (${BACKGROUND}) is not readable"
-	exit 1
-    fi
-fi
+shift 4
 
 #if [ "x`id -u`" != "x0" ] ; then
 #    echo "This script requires superuser privileges, restarting via sudo"
@@ -83,14 +78,197 @@ fi
 #fi
 
 VERSION="${MAJOR_VERSION}.${MINOR_VERSION}.${PATCH_VERSION}"
-PKG_NAME="${NAME}-${VERSION}.pkg"
-if [ ! -d "${PKG_NAME}" ] ; then
-    echo "ERROR: unable to locate the installer package"
+DMG="${NAME}-${VERSION}.dmg"
+if [ -d "$DMG" ] ; then
+    echo "ERROR: there is a directory of same name in the way of creating $DMG"
+    exit 1
+fi
+if [ -f "$DMG" ] ; then
+    remove=""
+    while [ "x$remove" = "x" ] ; do
+	echo "WARNING: the disk mounting image ($DMG) already exists, remove it?"
+	echo -n "yes/no? "
+	read remove
+	case x$remove in
+	    x[yY][eE][sS])
+	        remove=yes
+		;;
+	    x[yY])
+	        remove=yes
+		;;
+	    x[nN][oO])
+	        remove=no
+		;;
+	    x[nN])
+	        remove=no
+		;;
+	    *)
+		remove=""
+		;;
+	esac
+    done
+
+    if [ "x$remove" = "xyes" ] ; then
+	rm -f "$DMG"
+    fi
+    if [ -f "$DMG" ] ; then
+	echo "ERROR: cannot continue with $DMG in the way"
+	exit 1
+    fi
+fi
+
+hdiutil create "$DMG" -megabytes $DMG_CAPACITY -layout NONE
+if [ ! "x$?" = "x0" ] ; then
+    echo "ERROR: hdiutil failed to complete successfully"
+    exit 1
+fi
+if [ ! -f "$DMG" ] ; then
+    echo "ERROR: hdiutil failed to create $DMG"
     exit 1
 fi
 
-DMG_NAME="${NAME}-${VERSION}.dmg"
-echo "creating $DMG_NAME"
+hdidDisk=`hdid -nomount "$DMG" | head -1 | grep '/dev/disk[0-9]*' | awk '{print $1}'`
+if [ ! "x$?" = "x0" ] ; then
+    echo "ERROR: unable to successfully get the hdid device name"
+    exit 1
+fi
+if [ "x$hdidDisk" = "x" ] ; then
+    echo "ERROR: unable to get the hdid device name"
+    exit 1
+fi
+
+/sbin/newfs_hfs -w -v ${NAME}-${VERSION} -b 4096 $hdidDisk
+if [ ! "x$?" = "x0" ] ; then
+    echo "ERROR: unable to successfully create a new hfs filesystem on $hdidDisk"
+    exit 1
+fi
+
+hdiutil eject $hdidDisk
+if [ ! "x$?" = "x0" ] ; then
+    echo "ERROR: unable to successfully eject $hdidDisk"
+    exit 1
+fi
+
+hdidMountedDisk=`hdid $DMG | head -1 | grep '/dev/disk[0-9]*' | awk '{print $1}'`
+if [ ! "x$?" = "x0" ] ; then
+    echo "ERROR: unable to successfully get the mounted hdid device name"
+    exit 1
+fi
+if [ "x$hdidMountedDisk" = "x" ] ; then
+    echo "ERROR: unable to get the mounted hdid device name"
+    exit 1
+fi
+
+timeout=20
+VOL_DIR="/Volumes/${NAME}-${VERSION}"
+while [ $timeout -gt 0 ] ; do
+    if [ -d "$VOL_DIR" ] ; then
+	timeout=1
+    fi
+    timeout="`expr $timeout - 1`"
+    sleep 1
+done
+if [ ! -d "$VOL_DIR" ] ; then
+    echo "ERROR: timed out waiting for $DMG to mount"
+    exit 1
+fi
+
+PKG_NAME="${NAME}-${VERSION}.pkg"
+if [ -d "${PKG_NAME}" ] ; then
+    if [ ! -r "${PKG_NAME}" ] ; then
+	echo "ERROR: unable to read the installer package"
+	hdiutil eject $hdidMountedDisk
+	exit 1
+    fi
+
+    cp -pR "$PKG_NAME" "${VOL_DIR}/."
+    if [ ! "x$?" = "x0" ] ; then
+	echo "ERROR: unable to successfully copy $PKG_NAME to $VOL_DIR"
+	hdiutil eject $hdidMountedDisk
+	exit 1
+    fi
+fi
+
+while [ ! "x$*" = "x" ] ; do
+    ARG="$1"
+    shift
+
+    if [ ! -r "$ARG" ] ; then
+	if [ ! -f "$ARG" ] ; then
+	    echo "ERROR: specified content ($ARG) does not exist"
+	    hdiutil eject $hdidMountedDisk
+	    exit 1
+	fi
+	echo "ERROR: specified content ($ARG) is not readable"
+	hdiutil eject $hdidMountedDisk
+	exit 1
+    fi
+
+    if [ -d "$ARG" ] ; then
+	cp -pR "$ARG" "${VOL_DIR}/."
+	if [ ! "x$?" = "x0" ] ; then
+	    echo "ERROR: unable to successfully copy $ARG to $VOL_DIR"
+	    hdiutil eject $hdidMountedDisk
+	    exit 1
+	fi
+    elif [ -f "$ARG" ] ; then
+
+	cp -p "$ARG" "${VOL_DIR}/."
+	if [ ! "x$?" = "x0" ] ; then
+	    echo "ERROR: unable to successfully copy $ARG to $VOL_DIR"
+	    hdiutil eject $hdidMountedDisk
+	    exit 1
+	fi
+
+	filename="`basename $ARG`"
+	if [ ! "x$filename" = "x" ] ; then
+	    background="`echo $filename | sed 's/^[bB][aA][cC][kK][gG][rR][oO][uU][nN][dD]\.[a-zA-Z][a-zA-Z][a-zA-Z]*$/__bg__/'`"
+	    if [ "x$background" = "x__bg__" ] ; then
+		echo "$ARG is a background"
+	    fi
+	fi
+    else
+	echo "ERROR: $ARG does not exist as a file or directory (sanity check)"
+	hdiutil eject $hdidMountedDisk
+	exit 1
+    fi
+done
+
+hdiutil eject $hdidMountedDisk
+if [ ! "x$?" = "x0" ] ; then
+    echo "ERROR: unable to successfully eject $hdidMountedDisk"
+    exit 1
+fi
+
+mv "$DMG" "${DMG}.prep.dmg"
+if [ ! "x$?" = "x0" ] ; then
+    echo "ERROR: unable to successfully rename $DMG to ${DMG}.prep.dmg"
+    exit 1
+fi
+if [ ! -f "${DMG}.prep.dmg" ] ; then
+    echo "ERROR: failed to rename $DMG to ${DMG}.prep.dmg"
+    exit 1
+fi
+
+hdiutil convert "${DMG}.prep.dmg" -format UDCO -o "$DMG"
+if [ ! "x$?" = "x0" ] ; then
+    echo "ERROR: unable to successfully compress $DMG via hdiutil convert"
+    exit 1
+fi
+if [ ! -f "$DMG" ] ; then
+    echo "ERROR: hdiutil failed to compress ${DMG}.prep.dmg to $DMG"
+    exit 1
+fi
+
+rm "${DMG}.prep.dmg"
+if [ ! "x$?" = "x0" ] ; then
+    echo "ERROR: unable to successfully remove the ${DMG}.prep.dmg temporary"
+    exit 1
+fi
+if [ -f "${DMG}.prep.dmg" ] ; then
+    echo "ERROR: the ${DMG}.prep.dmg temporary still exists"
+    exit 1
+fi
 
 
 # Local Variables:
