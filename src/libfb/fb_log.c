88098e8f9ce6edc24ba84d582ed2ed34c94f84f0
Christopher Sean Morrison <brlcad@gmail.com>
2005-05-09 16:14:00 +0000

    indent-region, error if we can't log at compile-time
    svn:revision:24347
    cvs:account:brlcad
    cvs:branch:trunk

--- src/libfb/fb_log.c
+++ src/libfb/fb_log.c
@@ -19,32 +19,30 @@
  * information.
  */
 /** @file fb_log.c
-  Author -
-	Gary S. Moss
+    Author -
+    Gary S. Moss
 
-  Source -
-	SECAD/VLD Computing Consortium, Bldg 394
-	The U. S. Army Ballistic Research Laboratory
-	Aberdeen Proving Ground, Maryland  21005-5066
+    Source -
+    SECAD/VLD Computing Consortium, Bldg 394
+    The U. S. Army Ballistic Research Laboratory
+    Aberdeen Proving Ground, Maryland  21005-5066
   
 
-	$Header$ (BRL)
- */
+    $Header$ (BRL)
+*/
 #ifndef lint
 static const char RCSid[] = "@(#)$Header$ (BRL)";
 #endif
 
 #include "common.h"
 
-
-
 #include <stdio.h>
 #ifdef HAVE_STDARG_H
-#	include <stdarg.h>
+#  include <stdarg.h>
 #else
-# if HAVE_VARARGS_H
-#	include <varargs.h>
-# endif
+#  if HAVE_VARARGS_H
+#    include <varargs.h>
+#  endif
 #endif
 
 #include "machine.h"
@@ -59,42 +57,43 @@ static const char RCSid[] = "@(#)$Header$ (BRL)";
 void
 fb_log( char *fmt, ... )
 {
-	va_list ap;
+    va_list ap;
 
-	va_start( ap, fmt );
-	(void)vfprintf( stderr, fmt, ap );
-	va_end(ap);
+    va_start( ap, fmt );
+    (void)vfprintf( stderr, fmt, ap );
+    va_end(ap);
 }
 
 #else  /* defined(HAVE_STDARG_H) */
-# if defined(HAVE_VARARGS_H)
+#  if defined(HAVE_VARARGS_H)
 
 /* VARARGS */
 void
-fb_log( fmt, va_alist )
-char	*fmt;
-va_dcl
-	{	va_list		ap;
-	va_start( ap );
+fb_log( char *fmt, va_dcl va_alist )
+{
+    va_list		ap;
+    va_start( ap );
 # ifdef HAVE_VPRINTF
-	(void) vfprintf( stderr, fmt, ap);
+    (void) vfprintf( stderr, fmt, ap);
 # else
-	(void) _doprnt( fmt, ap, stderr );
+    (void) _doprnt( fmt, ap, stderr );
 # endif
-	va_end( ap );
-	return;
-	}
+    va_end( ap );
+    return;
+}
 
 # else /* defined(HAVE_VARARGS_H) */
 
 void
 fb_log( fmt, a,b,c,d,e,f,g,h,i )
-char	*fmt;
+     char	*fmt;
 {
-	fprintf( stderr, fmt, a,b,c,d,e,f,g,h,i );
+    fprintf( stderr, fmt, a,b,c,d,e,f,g,h,i );
 }
 
-# endif	/* defined(HAVE_VARARGS_H) */
+#  else
+#    error "No means to fb_log"
+#  endif	/* defined(HAVE_VARARGS_H) */
 #endif  /* defined(HAVE_STDARG_H) */
 
 /*
