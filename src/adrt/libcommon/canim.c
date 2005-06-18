75f1e80461f3706e30c060c707cf9e53d9d565c8
Justin Shumaker <twingy@brlcad.org>
2005-06-18 01:56:30 +0000

    Added licensing info to libcommon files.
    svn:revision:24550
    cvs:account:twingy
    cvs:branch:trunk

--- src/adrt/libcommon/canim.c
+++ src/adrt/libcommon/canim.c
@@ -1,4 +1,35 @@
-/*
+/*                     C A N I M . C
+ * BRL-CAD
+ *
+ * Copyright (C) 2002-2005 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file canim.c
+ *                     C A N I M . C
+ *
+ *  Common Library - Animation Parsing
+ *
+ *  Author -
+ *      Justin L. Shumaker
+ *
+ *  Source -
+ *      The U. S. Army Research Laboratory
+ *      Aberdeen Proving Ground, Maryland  21005-5068  USA
+ *
  * $Id$
  */
 
--- src/adrt/libcommon/canim.h
+++ src/adrt/libcommon/canim.h
@@ -1,3 +1,38 @@
+/*                     C A N I M . H
+ * BRL-CAD
+ *
+ * Copyright (C) 2002-2005 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file canim.h
+ *                     C A N I M . H
+ *
+ *  Common Library - Animation Header
+ *
+ *  Author -
+ *      Justin L. Shumaker
+ *
+ *  Source -
+ *      The U. S. Army Research Laboratory
+ *      Aberdeen Proving Ground, Maryland  21005-5068  USA
+ *
+ * $Id$
+ */
+
 #ifndef _COMMON_ANIM_H
 #define _COMMON_ANIM_H
 
