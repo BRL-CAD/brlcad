3b558df7b150902e5d82505a6369d9d95196e28b
Justin Shumaker <twingy@brlcad.org>
2005-06-18 00:49:06 +0000

    Added licensing info to libtie files.
    svn:revision:24547
    cvs:account:twingy
    cvs:branch:trunk

--- src/adrt/libtie/tie.c
+++ src/adrt/libtie/tie.c
@@ -1,19 +1,34 @@
-/** @addtogroup libtie
+/*                     T I E . C
+ * BRL-CAD
  *
- * The calling sequence is as follows:
- *	@li tie_init()	initialize the data structure
- *	@li tie_push()	add triangles to the universe to be raytraced
- *	@li tie_prep()	build the BSP for the triangles
- *	@li tie_work()	shoot some ray
- *	@li tie_work()	shoot some ray
- *	@li tie_work()	shoot some ray
- *	@li tie_work()	shoot some ray
- *	@li tie_work()	shoot some ray
- *	@li tie_free()	Free up all the memory
- * @{
- * $Id$
+ * Copyright (C) 2002-2005 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
  */
 /** @file tie.c
+ *                     T I E . C
+ *
+ *  Triangle Intersection Engine
+ *
+ *  Author -
+ *      Justin L. Shumaker
+ *
+ *  Source -
+ *      The U. S. Army Research Laboratory
+ *      Aberdeen Proving Ground, Maryland  21005-5068  USA
  *
  * @brief support routines for shooting at triangles
  *
@@ -27,8 +42,23 @@
  *	- tie_work()	shoot some ray
  *	- tie_work()	shoot some ray
  *	- tie_free()	Free up all the memory
+ *
+ * $Id$
  */
-
+/** @addtogroup libtie
+ *
+ * The calling sequence is as follows:
+ *	@li tie_init()	initialize the data structure
+ *	@li tie_push()	add triangles to the universe to be raytraced
+ *	@li tie_prep()	build the BSP for the triangles
+ *	@li tie_work()	shoot some ray
+ *	@li tie_work()	shoot some ray
+ *	@li tie_work()	shoot some ray
+ *	@li tie_work()	shoot some ray
+ *	@li tie_work()	shoot some ray
+ *	@li tie_free()	Free up all the memory
+ * @{
+ */
 #ifdef HAVE_CONFIG_H
 # include "brlcad_config.h"
 #endif
