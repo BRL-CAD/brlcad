0bdb245573b201f1323e66e972bc2006fd38e1a9
Justin Shumaker <twingy@brlcad.org>
2005-07-10 03:58:19 +0000

    IGVT->ISST 90%
    svn:revision:24746
    cvs:account:twingy
    cvs:branch:trunk

--- src/adrt/igvt/master/igvt_python.h
+++
@@ -1,7 +0,0 @@
-#ifndef _IVAT_PYTHON_H
-#define _IVAT_PYTHON_H
-
-extern void ivat_python_init();
-extern void ivat_python_code(char *command);
-
-#endif
--- src/adrt/igvt/slave/slave.h
+++
@@ -1,8 +0,0 @@
-#ifndef _IVAT_SLAVE_H
-#define _IVAT_SLAVE_H
-
-
-extern	void	ivat_slave(int port, char *host, int threads);
-
-
-#endif
