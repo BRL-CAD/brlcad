9a8d8d7e499ffec9df9258348f1201a1d54f6429
Justin Shumaker <twingy@brlcad.org>
2005-08-10 05:32:53 +0000

    depth reflects kdtree var
    svn:revision:24939
    cvs:account:twingy
    cvs:branch:trunk

--- src/adrt/librender/depth.c
+++ src/adrt/librender/depth.c
@@ -20,5 +20,5 @@ void render_depth_work(render_t *render, tie_t *tie, tie_ray_t *ray, TIE_3 *pixe
 
   /* Visualize ray depth, must put ray->depth++ hack into bsp for this to be of any use */
   if((mesh = (common_mesh_t *)tie_work(tie, ray, &id, render_hit, NULL)))
-    pixel->v[0] = 0.0075 * ray->bsp_depth;
+    pixel->v[0] = 0.0075 * ray->kdtree_depth;
 }
