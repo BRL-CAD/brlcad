596bcdf55651a8a0b814bc5e9a6f32ac5871a711
Justin Shumaker <twingy@brlcad.org>
2005-08-13 03:45:37 +0000

    un-used source for more than a year.
    svn:revision:24944
    cvs:account:twingy
    cvs:branch:trunk

--- src/adrt/libtie/sim_brlcad.h
+++
@@ -1,3 +0,0 @@
-extern int ADrt_gettree(struct rt_i *rtip, const char *name);
-extern void ADrt_prep_parallel(struct rt_i *rtip, const int ncpu);
-extern int ADrt_shootray( struct application *ap );
