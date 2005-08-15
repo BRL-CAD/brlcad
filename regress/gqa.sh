4a9cb9dfda53721251f10abe74d5ae9e7426acf3
Lee Butler <iraytrace@users.noreply.github.com>
2005-08-15 22:31:00 +0000

    Regression test refinements for g_qa
    svn:revision:24969
    cvs:account:lbutler
    cvs:branch:trunk

--- regress/gqa.sh
+++ regress/gqa.sh
@@ -1,19 +1,8 @@
 #!/bin/sh
 rm -f gqa.g density_table.txt gqa.log
-echo "5 7.68 stuff" > density_table.txt
+
+echo "5 1 stuff" > density_table.txt
 echo "2 1 gas" >> density_table.txt
-
-
-# box1.s = 10x10x10	= 1000 m^3
-# box2.s = 8x8x8	=  512 m^3
-# box3.s = 8x8x9	=  576 m^3
-# adj_air1.r = 512-256	= 256 m^3
-# adj_air2.r = 512-256	= 256 m^3
-# closed_box.r =1000-512= 488 m^3
-# exposed_air.r 	= 576 m^3
-# open_box.r = 1000-576 = 424 m^3
-
-
 
 mged -c gqa.g <<EOF > /dev/null 2>&1
 
@@ -24,6 +13,10 @@ in box1.s rpp 0 10 0 10 0 10
 in box2.s rpp 1  9 1  9 1  9
 in box3.s rpp 1 10 1  9 1  9
 in box4.s rpp 0.5  9.5 0.5  5 0.5  9.5
+
+r solid_box.r u box1.s
+adjust solid_box.r GIFTmater 5
+mater solid_box.r "plastic tr=0.5 di=0.5 sp=0.5" 128 128 128 0
 
 r closed_box.r u box1.s - box2.s
 adjust closed_box.r GIFTmater 5
@@ -61,33 +54,72 @@ g overlaps closed_box.r overlap_obj.r
 q
 EOF
 
-GQA="../src/gtools/.libs/g_qa -u m,m^3,kg"
+#
+# now that the inputs have been built, run the tests
+#
+# box1.s = 10x10x10	= 1000 m^3
+# box2.s = 8x8x8	=  512 m^3
+# box3.s = 8x8x9	=  576 m^3
+# adj_air1.r = 512-256	= 256 m^3
+# adj_air2.r = 512-256	= 256 m^3
+# closed_box.r =1000-512= 488 m^3
+# exposed_air.r 	= 576 m^3
+# open_box.r = 1000-576 = 424 m^3
+
+
+GQA="../src/gtools/.libs/g_qa -u m,m^3,kg -g 0.25m,0.5mm -p"
 export GQA
 
-rm -f gqa.log
-
-echo $GQA -Ao gqa.g overlaps
-$GQA -Ao gqa.g overlaps >> gqa.log 2>&1
 
 
-echo >> gqa.log
-echo $GQA -Ae gqa.g exposed_air.g
-$GQA -Ae gqa.g exposed_air.g >> gqa.log 2>&1
-
-echo >> gqa.log
-echo $GQA -Ag -r gqa.g gap.g
-$GQA -Ag gqa.g gap.g >> gqa.log 2>&1
-
-echo >> gqa.log
-echo $GQA -Av -r gqa.g closed_box.r
-$GQA -Av gqa.g closed_box.r >> gqa.log 2>&1
-
-echo >> gqa.log
-echo $GQA -Aw -r gqa.g closed_box.r
-$GQA -Aw gqa.g closed_box.r >> gqa.log 2>&1
+CMD="$GQA -Ao gqa.g overlaps"
+echo $CMD
+echo $CMD >> gqa.log
+$CMD >> gqa.log 2>&1
 
 
 echo >> gqa.log
-echo $GQA -Avw gqa.g adj_air.g
-$GQA -Avw gqa.g adj_air.g >> gqa.log 2>&1
+CMD="$GQA -Ae gqa.g exposed_air.g"
+echo $CMD
+echo $CMD >> gqa.log 2>&1
+$CMD >> gqa.log 2>&1
+
+echo >> gqa.log
+CMD="$GQA -Aa gqa.g adj_air.g"
+echo $CMD
+echo $CMD >> gqa.log 2>&1
+$CMD >> gqa.log 2>&1
+
+echo >> gqa.log
+CMD="$GQA -Ag gqa.g gap.g"
+echo $CMD
+echo $CMD >> gqa.log 2>&1
+$CMD >> gqa.log 2>&1
+
+echo >> gqa.log
+CMD="$GQA -Av -v gqa.g closed_box.r"
+echo $CMD
+echo $CMD >> gqa.log 2>&1
+$CMD >> gqa.log 2>&1
+
+echo >> gqa.log
+CMD="$GQA -r -Aw -v gqa.g closed_box.r"
+echo $CMD
+echo $CMD >> gqa.log 2>&1
+$CMD >> gqa.log 2>&1
+
+
+echo >> gqa.log
+CMD="$GQA -r -Avw gqa.g solid_box.r"
+echo $CMD
+echo $CMD >> gqa.log 2>&1
+$CMD >> gqa.log 2>&1
+
+
+echo >> gqa.log
+CMD="$GQA -r -Avw gqa.g adj_air.g"
+echo $CMD
+echo $CMD >> gqa.log 2>&1
+$CMD >> gqa.log 2>&1
+
 
