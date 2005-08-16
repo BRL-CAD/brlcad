5536fa50830a843746aea0e57ae721eb6e7cb742
Christopher Sean Morrison <brlcad@gmail.com>
2005-08-16 20:33:48 +0000

    remove the SNPRINTF define since it should be safe to use snprintf() now that it's properly accounted for in config_win.h as a compat function
    svn:revision:24978
    cvs:account:brlcad
    cvs:branch:trunk

--- include/bu.h
+++ include/bu.h
@@ -78,12 +78,6 @@ __BEGIN_DECLS
 #  endif
 #endif
 
-#if defined(_WIN32) && !defined(__CYGWIN__)
-#   define SNPRINTF _snprintf
-#else
-#   define SNPRINTF snprintf
-#endif
-
 #include <setjmp.h>
 
 #define BU_H_VERSION	"@(#)$Header$ (BRL)"
