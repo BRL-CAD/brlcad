407fda3eb2a3fe62810438c851e6427b3046c0b7
Lee Butler <iraytrace@users.noreply.github.com>
2005-08-22 21:24:07 +0000

    Trying to get nightly smoke to run again
    svn:revision:25019
    cvs:account:lbutler
    cvs:branch:trunk

--- regress/nightly.sh
+++ regress/nightly.sh
@@ -4,7 +4,7 @@ HOSTS="wopr liu amdws2 vast"
 MASTERHOST="wopr"
 
 export MYNAME=`hostname | awk -F '.' '{print $1}'`
-export START_TIME=`date "+%y%m%H%M"`
+export START_TIME=`date "+%y%m%d%H%M"`
 export LOG_FILE=`pwd`/${MYNAME}_${START_TIME}.log
 
 if [ X$MYNAME == X$MASTERHOST ] ; then
@@ -13,9 +13,37 @@ if [ X$MYNAME == X$MASTERHOST ] ; then
     export CVS_RSH=ssh
     cvs  -z3 -d:pserver:anonymous@cvs.sf.net:/cvsroot/brlcad co -P brlcad > $LOG_FILE 2>&1
 
+    if [ ! -d brlcad ] ; then
+	/bin/echo "unable to extract source from CVS repository"
+	exit -1
+    fi
     cd brlcad
+
+    # extract existing release numbers, old an new
+    eval `awk '/Release/ {print $2}' < README | \
+	awk -F. '{print "export MAJOR=" $1  "\nexport MINOR=" $2 "\nexport PATCH=" $3}'`
+
+    OLD_MAJOR=`awk -F\= '/^MAJOR_VERSION/ {print $2}' < configure.ac`
+    OLD_MINOR=`awk -F\= '/^MINOR_VERSION/ {print $2}' < configure.ac`
+    OLD_PATCH=`awk -F\= '/^PATCH_VERSION/ {print $2}' < configure.ac`
+
+
+    # Update configure.ac
+    /bin/echo update configure
+    if [ ! -f configure.ac.orig ] ; then
+	mv configure.ac configure.ac.orig
+    fi
+
+    sed -e "s/$OLD_MAJOR\.$OLD_MINOR\.$OLD_PATCH/$MAJOR\.$MINOR\.$PATCH/" \
+	-e "s/MAJOR_VERSION=$OLD_MAJOR/MAJOR_VERSION=$MAJOR/" \
+	-e "s/MINOR_VERSION=$OLD_MINOR/MINOR_VERSION=$MINOR/" \
+	-e "s/^PATCH_VERSION=$OLD_PATCH/PATCH_VERSION=$PATCH/" \
+	< configure.ac.orig > configure.ac
+
+    /bin/echo autogen
     /bin/sh ./autogen.sh >> $LOG_FILE 2>&1
 
+    /bin/echo semaphores
     cd ..
     for i in $HOSTS ; do
 	touch $i
@@ -26,7 +54,7 @@ fi
 
 while [ ! -f $MYNAME ] ; do
 
-    NOW=`date "+%y%m%H%M"`
+    NOW=`date "+%y%m%d%H%M"`
     DELTA=`expr $START_TIME - $NOW`
 
     if [ $DELTA -gt 300 ] ; then 
@@ -39,5 +67,5 @@ while [ ! -f $MYNAME ] ; do
     sleep 10
 done
 
-
+/bin/echo "Starting build"
 /bin/sh brlcad/regress/main.sh
