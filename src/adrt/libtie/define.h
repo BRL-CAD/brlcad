d8141034c2bf7d644cca2903691f17f055fe5272
Justin Shumaker <twingy@brlcad.org>
2005-08-29 07:31:04 +0000

    defaults at biggest bang for the buck right now.
    svn:revision:25093
    cvs:account:twingy
    cvs:branch:trunk

--- src/adrt/libtie/define.h
+++ src/adrt/libtie/define.h
@@ -46,8 +46,8 @@
 #define TIE_SINGLE_PREC		1		/* Use Single Precision Math */
 #define	TIE_TAB1		"\1\0\0\2\2\1"	/* Triangle Index Table */
 #define	TIE_KDTREE_NODE_MAX	4		/* Maximum number of triangles that can reside in a given node until it should be split */
-#define	TIE_KDTREE_DEPTH_K1	1.5		/* K1 Depth Constant Coefficient */
-#define	TIE_KDTREE_DEPTH_K2	2		/* K2 Contant */
+#define	TIE_KDTREE_DEPTH_K1	1.4		/* K1 Depth Constant Coefficient */
+#define	TIE_KDTREE_DEPTH_K2	1		/* K2 Contant */
 
 #define MAX_SLICES 100
 #define MIN_SLICES 25
