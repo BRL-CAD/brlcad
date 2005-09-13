315ac2169c1d3f24cce193780f6352b91cabb18f
Robert Parker <unacknowledged@users.noreply.github.com>
2005-09-13 17:42:04 +0000

    Add a few defines for the st_mode field of the stat structure
    svn:revision:25203
    cvs:account:bob1961
    cvs:branch:bobWinPort

--- include/config_win.h
+++ include/config_win.h
@@ -101,10 +101,20 @@
 #define R_OK 4
 #define W_OK 2
 #define MAXPATHLEN _MAX_PATH
+#define O_APPEND _O_APPEND
 #define O_CREAT _O_CREAT
 #define O_EXCL _O_EXCL
 #define O_RDONLY _O_RDONLY
 #define O_RDWR _O_RDWR
+#define O_TRUNC _O_TRUNC
+#define O_WRONLY _O_WRONLY
+#define S_IFMT _S_IFMT
+#define S_IFDIR _S_IFDIR
+#define S_IFCHR _S_IFCHR
+#define S_IFREG _S_IFREG
+#define S_IREAD _S_IREAD
+#define S_IWRITE _S_IWRITE
+#define S_IEXEC _S_IEXEC
 #define access _access
 #define chmod _chmod
 #define close _close
@@ -112,7 +122,9 @@
 #define creat _creat
 #define dup _dup
 #define dup2 _dup2
+#if 0
 #define eof _eof
+#endif
 #define fdopen _fdopen
 #define fileno _fileno
 #define fstat _fstat
