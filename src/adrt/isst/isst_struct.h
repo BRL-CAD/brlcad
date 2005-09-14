f56c2a962c7218c52d8e89d78001b80795efe748
Justin Shumaker <twingy@brlcad.org>
2005-09-14 02:03:59 +0000

    isst_event_t handles 32/64-bit compatible event stucture, SDL_Event is not.
    svn:revision:25217
    cvs:account:twingy
    cvs:branch:trunk

--- src/adrt/isst/isst_struct.h
+++ src/adrt/isst/isst_struct.h
@@ -2,6 +2,9 @@
 #define _ISST_STRUCT_H
 
 
+#include <inttypes.h>
+
+
 typedef struct isst_overlay_data_s {
   TIE_3 camera_position;
   tfloat camera_azimuth;
@@ -14,5 +17,19 @@ typedef struct isst_overlay_data_s {
   tfloat scale;
 } isst_overlay_data_t;
 
+
+/*
+* isst_event_t exists because the SDL_Event structure is not
+* the same size on 32-bit and 64-bit machines due to a pointer
+* in the syswm struct in the SDL_Event struct.
+*/
+typedef struct isst_event_s {
+  uint8_t type;
+  uint16_t keysym;
+  uint8_t button;
+  uint8_t motion_state;
+  int16_t motion_xrel;
+  int16_t motion_yrel;
+} isst_event_t;
 
 #endif
