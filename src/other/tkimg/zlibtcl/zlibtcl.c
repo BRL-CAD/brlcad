35963d7b559f48936c08d0ff49d16651c174b906
Robert Parker <unacknowledged@users.noreply.github.com>
2005-09-26 12:36:28 +0000

    Not exporting Z_Init
    svn:revision:25408
    cvs:account:bob1961
    cvs:branch:bobWinPort

--- src/other/tkimg/zlibtcl/zlibtcl.c
+++ src/other/tkimg/zlibtcl/zlibtcl.c
@@ -24,34 +24,6 @@
     (TCL_MAJOR_VERSION > 8 || TCL_MAJOR_VERSION == 8 && (TCL_MINOR_VERSION > 1 || \
     (TCL_MINOR_VERSION == 1 && TCL_RELEASE_LEVEL == TCL_FINAL_RELEASE)))
 
-#if 1
-/*
- * Declarations for externally visible functions.
- */
-
-#undef TCL_STORAGE_CLASS
-#ifdef BUILD_zlibtcl
-# define TCL_STORAGE_CLASS DLLEXPORT
-#else
-# ifdef USE_ZLIBTCL_STUBS
-#  define TCL_STORAGE_CLASS
-# else
-#  define TCL_STORAGE_CLASS DLLIMPORT
-# endif
-#endif
-
-#ifdef _DEBUG
-EXTERN int Z_d_Init     _ANSI_ARGS_((Tcl_Interp *interp));
-EXTERN int Z_d_SafeInit _ANSI_ARGS_((Tcl_Interp *interp));
-#else
-EXTERN int Z_Init     _ANSI_ARGS_((Tcl_Interp *interp));
-EXTERN int Z_SafeInit _ANSI_ARGS_((Tcl_Interp *interp));
-#endif
-
-#undef  TCL_STORAGE_CLASS
-#define TCL_STORAGE_CLASS DLLIMPORT
-#endif
-
 /*
  * Prototypes for procedures defined later in this file:
  */
