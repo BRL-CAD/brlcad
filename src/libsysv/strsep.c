33e5a53774bfa4f79bf9616c657c2b406291ca8a
Christopher Sean Morrison <brlcad@gmail.com>
2005-10-27 04:02:40 +0000

    header cleanup
    svn:revision:25525
    cvs:account:brlcad
    cvs:branch:trunk

--- src/libsysv/strsep.c
+++ src/libsysv/strsep.c
@@ -2,7 +2,7 @@
  *			S T R S E P . C
  *
  * Copyright (c) 1990, 1993
- *The Regents of the University of California.  All rights reserved.
+ * The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
