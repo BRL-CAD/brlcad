9da5a1ee0c0ab5eac7cc1c25693c35e5fda2810d
Christopher Sean Morrison <brlcad@gmail.com>
2005-12-09 03:44:31 +0000

    no sense in bombing if we're asked to free an internal with an unset meth function pointer.  if it's set, make sure it's valid, then call the callback.
    svn:revision:25826
    cvs:account:brlcad
    cvs:branch:trunk

--- src/librt/dir.c
+++ src/librt/dir.c
@@ -251,15 +251,24 @@ void
 rt_db_free_internal( struct rt_db_internal *ip, struct resource *resp )
 {
 	RT_CK_DB_INTERNAL( ip );
-	RT_CK_FUNCTAB( ip->idb_meth );
+
+	/* meth is not required since may be asked to free something
+	 * that was never set.
+	 */
+	if (ip->idb_meth) {
+	    RT_CK_FUNCTAB(ip->idb_meth);
+	    ip->idb_meth->ft_ifree(ip, resp);
+	}
+
 	/* resp is not checked, since most ifree's don't take/need it
 	 * (only combinations use it) -- leave it up to ft_ifree to check it
 	 */
 	if( ip->idb_ptr )  {
-		ip->idb_meth->ft_ifree(ip, resp);
-		ip->idb_ptr = NULL;		/* sanity.  Should be handled by INIT, below */
+	    ip->idb_ptr = NULL;		/* sanity.  Should be handled by INIT, below */
 	}
-	if( ip->idb_avs.magic == BU_AVS_MAGIC )  bu_avs_free(&ip->idb_avs);
+	if( ip->idb_avs.magic == BU_AVS_MAGIC ) {
+	    bu_avs_free(&ip->idb_avs);
+	}
 	RT_INIT_DB_INTERNAL(ip);
 }
 
