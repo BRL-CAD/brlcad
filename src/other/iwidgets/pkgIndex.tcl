d239de58114b3c1e4ab64cac2ad216f674ee95d9
Christopher Sean Morrison <brlcad@gmail.com>
2006-01-04 03:55:10 +0000

    meh, shouldn't be in cvs, but update to 7.7.0 anyways
    svn:revision:25965
    cvs:account:brlcad
    cvs:branch:trunk

--- src/other/iwidgets/pkgIndex.tcl
+++ src/other/iwidgets/pkgIndex.tcl
@@ -1,3 +1,3 @@
 # Tcl package index file, version 1.0
 
-package ifneeded Iwidgets 7.6.6 [list source [file join $dir iwidgets.tcl]]
+package ifneeded Iwidgets 7.7.0 [list source [file join $dir iwidgets.tcl]]
