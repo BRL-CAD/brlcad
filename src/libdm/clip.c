289522065af9d7cdc1f6789a7f182c9801b4569a
Christopher Sean Morrison <brlcad@gmail.com>
2006-01-23 04:28:44 +0000

    don't use INFINITY as it's a publicly defined macro usually, regardless, it's used as a clipping plane distance so call it that.  increase it one order to a billion while we're at it.
    svn:revision:26155
    cvs:account:brlcad
    cvs:branch:trunk

--- src/libdm/clip.c
+++ src/libdm/clip.c
@@ -43,9 +43,8 @@ static const char RCSid[] = "@(#)$Header$ (BRL)";
 
 #include "common.h"
 
-
-
 #include <stdio.h>
+
 #include "machine.h"
 #include "vmath.h"
 #include "dm.h"
@@ -124,8 +123,8 @@ code (fastf_t x, fastf_t y)
 	return (cval);
 }
 
-#define EPSILON		0.0001
-#define INFINITY	100000000.0
+#define ANGLE_EPSILON 0.0001
+#define CLIP_DISTANCE 1000000000.0
 
 
 /*
@@ -153,19 +152,19 @@ vclip(fastf_t *a, fastf_t *b, register fastf_t *min, register fastf_t *max)
 	register fastf_t *dir = &diff[0];
 	register int i;
 
-	mindist = -INFINITY;
-	maxdist = INFINITY;
+	mindist = -CLIP_DISTANCE;
+	maxdist = CLIP_DISTANCE;
 	VSUB2( diff, b, a );
 
 	for( i=0; i < 3; i++, pt++, dir++, max++, min++ )  {
-		if( *dir < -EPSILON )  {
+		if( *dir < -ANGLE_EPSILON )  {
 			if( (sv = (*min - *pt) / *dir) < 0.0 )
 				return(0);	/* MISS */
 			if(maxdist > sv)
 				maxdist = sv;
 			if( mindist < (st = (*max - *pt) / *dir) )
 				mindist = st;
-		}  else if( *dir > EPSILON )  {
+		}  else if( *dir > ANGLE_EPSILON )  {
 			if( (st = (*max - *pt) / *dir) < 0.0 )
 				return(0);	/* MISS */
 			if(maxdist > st)
