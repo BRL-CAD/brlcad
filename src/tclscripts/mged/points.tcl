cd58a2cb8cfddefa32250d758dab36c6b7be879e
Christopher Sean Morrison <brlcad@gmail.com>
2006-01-31 02:04:38 +0000

    footer
    svn:revision:26244
    cvs:account:brlcad
    cvs:branch:trunk

--- src/tclscripts/mged/points.tcl
+++ src/tclscripts/mged/points.tcl
@@ -694,3 +694,12 @@ if { 1 == 0 } {
 
 }
 
+
+# Local Variables:
+# mode: Tcl
+# tab-width: 8
+# c-basic-offset: 4
+# tcl-indent-level: 4
+# indent-tabs-mode: t
+# End:
+# ex: shiftwidth=4 tabstop=8
