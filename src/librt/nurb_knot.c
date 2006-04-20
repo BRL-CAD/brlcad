472841972fac1ef78e3e4301b71b056ffc692e23
Jason Owens <github.jlowenz@xoxy.net>
2006-04-20 13:11:43 +0000

    logging updates
    svn:revision:26471
    cvs:account:jlowenz
    cvs:branch:trimnurbs-branch

--- src/librt/nurb_knot.c
+++ src/librt/nurb_knot.c
@@ -95,10 +95,10 @@ rt_nurb_kvmult(struct knot_vector *new_kv, const struct knot_vector *kv, int num
 
 	check.k_size = num - n;
 	if( check.k_size <= 0 )  {
-		bu_log("rt_nurb_kvmult(new_kv=x%x, kv=x%x, num=%d, val=%g)\n",
-			new_kv, kv, num, val);
+		bu_log("rt_nurb_kvmult(new_kv=x%x, kv=x%x, num=%d, val=%g, n=%d)\n",
+		       new_kv, kv, num, val, n);
 		rt_nurb_pr_kv(kv);
-		rt_bomb("rt_nurb_kvmult\n");
+		bu_bomb("rt_nurb_kvmult failed: check.k_size <= 0\n");
 	}
 
 	check.knots = (fastf_t * ) bu_malloc( sizeof(fastf_t) * check.k_size,
