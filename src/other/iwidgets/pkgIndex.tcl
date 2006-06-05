bac928c9534a916db0d0819e61649368b6644ada
Christopher Sean Morrison <brlcad@gmail.com>
2006-06-05 01:10:20 +0000

    commit the right version for iwidgets, 4.0.1
    svn:revision:26579
    cvs:account:brlcad
    cvs:branch:trunk

--- src/other/iwidgets/pkgIndex.tcl
+++ src/other/iwidgets/pkgIndex.tcl
@@ -1,3 +1,3 @@
 # Tcl package index file, version 1.0
 
-package ifneeded Iwidgets 7.7.0 [list source [file join $dir iwidgets.tcl]]
+package ifneeded Iwidgets 4.0.1 [list source [file join $dir iwidgets.tcl]]
