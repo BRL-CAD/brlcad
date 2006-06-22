c41aa9c135bc7036c5d9d391458be2d548df3e36
Christopher Sean Morrison <brlcad@gmail.com>
2006-06-22 13:58:35 +0000

    ws
    svn:revision:26607
    cvs:account:brlcad
    cvs:branch:trunk

--- src/libbu/bitv.c
+++ src/libbu/bitv.c
@@ -68,18 +68,18 @@ static const char libbu_bitv_RCSid[] = "@(#)$Header$ (ARL)";
 struct bu_bitv *
 bu_bitv_new(unsigned int nbits)
 {
-	struct bu_bitv	*bv;
-	int		bv_bytes;
-	int		total_bytes;
+    struct bu_bitv	*bv;
+    int		bv_bytes;
+    int		total_bytes;
 
-	bv_bytes = BU_BITS2BYTES(nbits);
-	total_bytes = sizeof(struct bu_bitv) - 2*sizeof(bitv_t) + bv_bytes;
+    bv_bytes = BU_BITS2BYTES(nbits);
+    total_bytes = sizeof(struct bu_bitv) - 2*sizeof(bitv_t) + bv_bytes;
 
-	bv = (struct bu_bitv *)bu_malloc( (size_t)total_bytes, "struct bu_bitv" );
-	BU_LIST_INIT( &bv->l );
-	bv->l.magic = BU_BITV_MAGIC;
-	bv->nbits = bv_bytes * 8;
-	return bv;
+    bv = (struct bu_bitv *)bu_malloc( (size_t)total_bytes, "struct bu_bitv" );
+    BU_LIST_INIT( &bv->l );
+    bv->l.magic = BU_BITV_MAGIC;
+    bv->nbits = bv_bytes * 8;
+    return bv;
 }
 
 /*
@@ -92,10 +92,10 @@ bu_bitv_new(unsigned int nbits)
 void
 bu_bitv_free(struct bu_bitv *bv)
 {
-	BU_CK_BITV(bv);
+    BU_CK_BITV(bv);
 
-	bv->l.forw = bv->l.back = BU_LIST_NULL;	/* sanity */
-	bu_free( (char *)bv, "struct bu_bitv" );
+    bv->l.forw = bv->l.back = BU_LIST_NULL;	/* sanity */
+    bu_free( (char *)bv, "struct bu_bitv" );
 }
 
 /*
@@ -108,9 +108,9 @@ bu_bitv_free(struct bu_bitv *bv)
 void
 bu_bitv_clear(struct bu_bitv *bv)
 {
-	BU_CK_BITV(bv);
+    BU_CK_BITV(bv);
 
-	BU_BITV_ZEROALL(bv);
+    BU_BITV_ZEROALL(bv);
 }
 
 /*
@@ -119,21 +119,21 @@ bu_bitv_clear(struct bu_bitv *bv)
 void
 bu_bitv_or(struct bu_bitv *ov, const struct bu_bitv *iv)
 {
-	register bitv_t		*out;
-	register const bitv_t	*in;
-	register int		words;
+    register bitv_t		*out;
+    register const bitv_t	*in;
+    register int		words;
 
-	if( ov->nbits != iv->nbits )  bu_bomb("bu_bitv_or: length mis-match");
-	out = ov->bits;
-	in = iv->bits;
-	words = BU_BITS2WORDS(iv->nbits);
+    if( ov->nbits != iv->nbits )  bu_bomb("bu_bitv_or: length mis-match");
+    out = ov->bits;
+    in = iv->bits;
+    words = BU_BITS2WORDS(iv->nbits);
 #ifdef VECTORIZE
 #	include "noalias.h"
-	for( --words; words >= 0; words-- )
-		out[words] |= in[words];
+    for( --words; words >= 0; words-- )
+	out[words] |= in[words];
 #else
-	while( words-- > 0 )
-		*out++ |= *in++;
+    while( words-- > 0 )
+	*out++ |= *in++;
 #endif
 }
 
@@ -143,21 +143,21 @@ bu_bitv_or(struct bu_bitv *ov, const struct bu_bitv *iv)
 void
 bu_bitv_and(struct bu_bitv *ov, const struct bu_bitv *iv)
 {
-	register bitv_t		*out;
-	register const bitv_t	*in;
-	register int		words;
+    register bitv_t		*out;
+    register const bitv_t	*in;
+    register int		words;
 
-	if( ov->nbits != iv->nbits )  bu_bomb("bu_bitv_and: length mis-match");
-	out = ov->bits;
-	in = iv->bits;
-	words = BU_BITS2WORDS(iv->nbits);
+    if( ov->nbits != iv->nbits )  bu_bomb("bu_bitv_and: length mis-match");
+    out = ov->bits;
+    in = iv->bits;
+    words = BU_BITS2WORDS(iv->nbits);
 #ifdef VECTORIZE
 #	include "noalias.h"
-	for( --words; words >= 0; words-- )
-		out[words] &= in[words];
+    for( --words; words >= 0; words-- )
+	out[words] &= in[words];
 #else
-	while( words-- > 0 )
-		*out++ &= *in++;
+    while( words-- > 0 )
+	*out++ &= *in++;
 #endif
 }
 
@@ -169,25 +169,25 @@ bu_bitv_and(struct bu_bitv *ov, const struct bu_bitv *iv)
 void
 bu_bitv_vls(struct bu_vls *v, register const struct bu_bitv *bv)
 {
-	int		seen = 0;
-	register int	i;
-	int		len;
+    int		seen = 0;
+    register int	i;
+    int		len;
 
-	BU_CK_VLS( v );
-	BU_CK_BITV( bv );
+    BU_CK_VLS( v );
+    BU_CK_BITV( bv );
 
-	len = bv->nbits;
+    len = bv->nbits;
 
-	bu_vls_strcat( v, "(" );
+    bu_vls_strcat( v, "(" );
 
-	/* Visit all the bits in ascending order */
-	for( i=0; i<len; i++ )  {
-		if( BU_BITTEST(bv, i) == 0 )  continue;
-		if( seen )  bu_vls_strcat( v, ", " );
-		bu_vls_printf( v, "%d", i );
-		seen = 1;
-	}
-	bu_vls_strcat( v, ") " );
+    /* Visit all the bits in ascending order */
+    for( i=0; i<len; i++ )  {
+	if( BU_BITTEST(bv, i) == 0 )  continue;
+	if( seen )  bu_vls_strcat( v, ", " );
+	bu_vls_printf( v, "%d", i );
+	seen = 1;
+    }
+    bu_vls_strcat( v, ") " );
 }
 
 /*
@@ -199,15 +199,15 @@ bu_bitv_vls(struct bu_vls *v, register const struct bu_bitv *bv)
 void
 bu_pr_bitv(const char *str, register const struct bu_bitv *bv)
 {
-	struct bu_vls	v;
+    struct bu_vls	v;
 
-	BU_CK_BITV(bv)
+    BU_CK_BITV(bv)
 	bu_vls_init( &v );
-	bu_vls_strcat( &v, str );
-	bu_vls_strcat( &v, ": " );
-	bu_bitv_vls( &v, bv );
-	bu_log("%s", bu_vls_addr( &v ) );
-	bu_vls_free( &v );
+    bu_vls_strcat( &v, str );
+    bu_vls_strcat( &v, ": " );
+    bu_bitv_vls( &v, bv );
+    bu_log("%s", bu_vls_addr( &v ) );
+    bu_vls_free( &v );
 }
 
 /*
@@ -219,23 +219,23 @@ bu_pr_bitv(const char *str, register const struct bu_bitv *bv)
 void
 bu_bitv_to_hex(struct bu_vls *v, register const struct bu_bitv *bv)
 {
-	unsigned int word_count, byte_no;
+    unsigned int word_count, byte_no;
 
-	BU_CK_VLS( v );
-	BU_CK_BITV( bv );
+    BU_CK_VLS( v );
+    BU_CK_BITV( bv );
 
-	word_count = bv->nbits/8/sizeof( bitv_t );
-	byte_no = sizeof( bitv_t );
+    word_count = bv->nbits/8/sizeof( bitv_t );
+    byte_no = sizeof( bitv_t );
 
-	bu_vls_extend( v, word_count * sizeof( bitv_t ) * 2 + 1 );
-	while( word_count-- )
+    bu_vls_extend( v, word_count * sizeof( bitv_t ) * 2 + 1 );
+    while( word_count-- )
 	{
-		while( byte_no-- )
+	    while( byte_no-- )
 		{
-			bu_vls_printf( v, "%02lx",
-				       ((bv->bits[word_count] & (((bitv_t)0xff)<<(byte_no*8))) >> (byte_no*8)) & (bitv_t)0xff );
+		    bu_vls_printf( v, "%02lx",
+				   ((bv->bits[word_count] & (((bitv_t)0xff)<<(byte_no*8))) >> (byte_no*8)) & (bitv_t)0xff );
 		}
-		byte_no = sizeof( bitv_t );
+	    byte_no = sizeof( bitv_t );
 	}
 }
 
@@ -247,63 +247,63 @@ bu_bitv_to_hex(struct bu_vls *v, register const struct bu_bitv *bv)
 struct bu_bitv *
 bu_hex_to_bitv(const char *str)
 {
-	char abyte[3];
-	const char *str_start;
-	unsigned int len=0;
-	int bytes;
-	struct bu_bitv *bv;
-	unsigned long c;
-	int word_count, byte_no;
+    char abyte[3];
+    const char *str_start;
+    unsigned int len=0;
+    int bytes;
+    struct bu_bitv *bv;
+    unsigned long c;
+    int word_count, byte_no;
 
-	abyte[2] = '\0';
+    abyte[2] = '\0';
 
-	/* skip over any initial white space */
-	while( isspace( *str ) )
-		str++;
+    /* skip over any initial white space */
+    while( isspace( *str ) )
+	str++;
 
-	str_start = str;
-	/* count hex digits */
-	while( isxdigit( *str ) )
-		str++;
+    str_start = str;
+    /* count hex digits */
+    while( isxdigit( *str ) )
+	str++;
 
-	len = str - str_start;
+    len = str - str_start;
 
-	if( len < 2 || len%2 )
+    if( len < 2 || len%2 )
 	{
-		/* Must be two digits per byte */
-		bu_log( "bu_hex_to_bitv: illegal hex bitv (%s)\n", str_start );
-		return( (struct bu_bitv *)NULL );
+	    /* Must be two digits per byte */
+	    bu_log( "bu_hex_to_bitv: illegal hex bitv (%s)\n", str_start );
+	    return( (struct bu_bitv *)NULL );
 	}
 
-	bytes = len / 2; /* two hex digits per byte */
-	bv = bu_bitv_new( len * 4 ); /* 4 bits per hex digit */
-	bu_bitv_clear( bv );
-	word_count = bytes/sizeof( bitv_t );
-	byte_no = bytes % sizeof( bitv_t );
-	if( !byte_no )
-		byte_no = sizeof( bitv_t );
-	else
-		word_count++;
+    bytes = len / 2; /* two hex digits per byte */
+    bv = bu_bitv_new( len * 4 ); /* 4 bits per hex digit */
+    bu_bitv_clear( bv );
+    word_count = bytes/sizeof( bitv_t );
+    byte_no = bytes % sizeof( bitv_t );
+    if( !byte_no )
+	byte_no = sizeof( bitv_t );
+    else
+	word_count++;
 
-	str = str_start;
-	while( word_count-- )
+    str = str_start;
+    while( word_count-- )
 	{
-		while( byte_no-- )
+	    while( byte_no-- )
 		{
-			/* get next two hex digits from string */
-			abyte[0] = *str++;
-			abyte[1] = *str++;
+		    /* get next two hex digits from string */
+		    abyte[0] = *str++;
+		    abyte[1] = *str++;
 
-			/* convert into an unsigned long */
-			c = strtoul( abyte, (char **)NULL, 16 );
+		    /* convert into an unsigned long */
+		    c = strtoul( abyte, (char **)NULL, 16 );
 
-			/* set the appropriate bits in the bit vector */
-			bv->bits[word_count] |= (bitv_t)c<<(byte_no*8);
+		    /* set the appropriate bits in the bit vector */
+		    bv->bits[word_count] |= (bitv_t)c<<(byte_no*8);
 		}
-		byte_no = sizeof( bitv_t );
+	    byte_no = sizeof( bitv_t );
 	}
 
-	return( bv );
+    return( bv );
 }
 
 /*
@@ -314,13 +314,13 @@ bu_hex_to_bitv(const char *str)
 struct bu_bitv *
 bu_bitv_dup(register const struct bu_bitv *bv)
 {
-	struct bu_bitv *bv2;
+    struct bu_bitv *bv2;
 
-	bv2 = bu_bitv_new( bv->nbits );
-	bu_bitv_clear( bv2 );
-	bu_bitv_or( bv2, bv );
+    bv2 = bu_bitv_new( bv->nbits );
+    bu_bitv_clear( bv2 );
+    bu_bitv_or( bv2, bv );
 
-	return( bv2 );
+    return( bv2 );
 }
 
 /*
