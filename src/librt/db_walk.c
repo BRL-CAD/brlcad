d5a8390713db6ed39efcf5998bbe5ced1c6c87e5
Christopher Sean Morrison <brlcad@gmail.com>
2006-06-26 15:22:34 +0000

    ws
    svn:revision:26642
    cvs:account:brlcad
    cvs:branch:trunk

--- src/librt/db_walk.c
+++ src/librt/db_walk.c
@@ -76,34 +76,34 @@ db_functree_subtree(struct db_i *dbip,
 		    struct resource *resp,
 		    genptr_t client_data)
 {
-	struct directory *dp;
+    struct directory *dp;
 
-	if( !tp )
+    if( !tp )
+	return;
+
+    RT_CHECK_DBI( dbip );
+    RT_CK_TREE( tp );
+    RT_CK_RESOURCE( resp );
+
+    switch( tp->tr_op )  {
+
+	case OP_DB_LEAF:
+	    if( (dp=db_lookup( dbip, tp->tr_l.tl_name, LOOKUP_NOISY )) == DIR_NULL )
 		return;
+	    db_functree( dbip, dp, comb_func, leaf_func, resp, client_data);
+	    break;
 
-	RT_CHECK_DBI( dbip );
-	RT_CK_TREE( tp );
-	RT_CK_RESOURCE( resp );
-
-	switch( tp->tr_op )  {
-
-		case OP_DB_LEAF:
-			if( (dp=db_lookup( dbip, tp->tr_l.tl_name, LOOKUP_NOISY )) == DIR_NULL )
-				return;
-			db_functree( dbip, dp, comb_func, leaf_func, resp, client_data);
-			break;
-
-		case OP_UNION:
-		case OP_INTERSECT:
-		case OP_SUBTRACT:
-		case OP_XOR:
-			db_functree_subtree( dbip, tp->tr_b.tb_left, comb_func, leaf_func, resp, client_data );
-			db_functree_subtree( dbip, tp->tr_b.tb_right, comb_func, leaf_func, resp, client_data );
-			break;
-		default:
-			bu_log( "db_functree_subtree: unrecognized operator %d\n", tp->tr_op );
-			bu_bomb( "db_functree_subtree: unrecognized operator\n" );
-	}
+	case OP_UNION:
+	case OP_INTERSECT:
+	case OP_SUBTRACT:
+	case OP_XOR:
+	    db_functree_subtree( dbip, tp->tr_b.tb_left, comb_func, leaf_func, resp, client_data );
+	    db_functree_subtree( dbip, tp->tr_b.tb_right, comb_func, leaf_func, resp, client_data );
+	    break;
+	default:
+	    bu_log( "db_functree_subtree: unrecognized operator %d\n", tp->tr_op );
+	    bu_bomb( "db_functree_subtree: unrecognized operator\n" );
+    }
 }
 
 /**
@@ -124,54 +124,53 @@ db_functree(struct db_i *dbip,
 	    struct resource *resp,
 	    genptr_t client_data)
 {
-	register int		i;
+    register int		i;
 
-	RT_CK_DBI(dbip);
-	if(RT_G_DEBUG&DEBUG_DB) bu_log("db_functree(%s) x%x, x%x, comb=x%x, leaf=x%x, client_data=x%x\n",
-		dp->d_namep, dbip, dp, comb_func, leaf_func, client_data );
+    RT_CK_DBI(dbip);
+    if(RT_G_DEBUG&DEBUG_DB) bu_log("db_functree(%s) x%x, x%x, comb=x%x, leaf=x%x, client_data=x%x\n",
+				   dp->d_namep, dbip, dp, comb_func, leaf_func, client_data );
 
-	if( dp->d_flags & DIR_COMB )  {
-		if( dbip->dbi_version < 5 ) {
-			register union record	*rp;
-			register struct directory *mdp;
-			/*
-			 * Load the combination into local record buffer
-			 * This is in external v4 format.
-			 */
-			if( (rp = db_getmrec( dbip, dp )) == (union record *)0 )
-				return;
+    if( dp->d_flags & DIR_COMB )  {
+	if( dbip->dbi_version < 5 ) {
+	    register union record	*rp;
+	    register struct directory *mdp;
+	    /*
+	     * Load the combination into local record buffer
+	     * This is in external v4 format.
+	     */
+	    if( (rp = db_getmrec( dbip, dp )) == (union record *)0 )
+		return;
 
-			/* recurse */
-			for( i=1; i < dp->d_len; i++ )  {
-				if( (mdp = db_lookup( dbip, rp[i].M.m_instname,
-				    LOOKUP_NOISY )) == DIR_NULL )
-					continue;
-				db_functree( dbip, mdp, comb_func, leaf_func, resp, client_data );
-			}
-			bu_free( (char *)rp, "db_functree record[]" );
-		} else {
-			struct rt_db_internal in;
-			struct rt_comb_internal *comb;
-
-			if( rt_db_get_internal5( &in, dp, dbip, NULL, resp ) < 0 )
-				return;
-
-			comb = (struct rt_comb_internal *)in.idb_ptr;
-			db_functree_subtree( dbip, comb->tree, comb_func, leaf_func, resp, client_data );
-			rt_db_free_internal( &in, resp );
-		}
-
-		/* Finally, the combination itself */
-		if( comb_func )
-			comb_func( dbip, dp, client_data );
-
-	} else if( dp->d_flags & DIR_SOLID || dp->d_major_type & DB5_MAJORTYPE_BINARY_MASK )  {
-		if( leaf_func )
-			leaf_func( dbip, dp, client_data );
+	    /* recurse */
+	    for( i=1; i < dp->d_len; i++ )  {
+		if( (mdp = db_lookup( dbip, rp[i].M.m_instname, LOOKUP_NOISY )) == DIR_NULL )
+		    continue;
+		db_functree( dbip, mdp, comb_func, leaf_func, resp, client_data );
+	    }
+	    bu_free( (char *)rp, "db_functree record[]" );
 	} else {
-		bu_log("db_functree:  %s is neither COMB nor SOLID?\n",
-			dp->d_namep );
+	    struct rt_db_internal in;
+	    struct rt_comb_internal *comb;
+
+	    if( rt_db_get_internal5( &in, dp, dbip, NULL, resp ) < 0 )
+		return;
+
+	    comb = (struct rt_comb_internal *)in.idb_ptr;
+	    db_functree_subtree( dbip, comb->tree, comb_func, leaf_func, resp, client_data );
+	    rt_db_free_internal( &in, resp );
 	}
+
+	/* Finally, the combination itself */
+	if( comb_func )
+	    comb_func( dbip, dp, client_data );
+
+    } else if( dp->d_flags & DIR_SOLID || dp->d_major_type & DB5_MAJORTYPE_BINARY_MASK )  {
+	if( leaf_func )
+	    leaf_func( dbip, dp, client_data );
+    } else {
+	bu_log("db_functree:  %s is neither COMB nor SOLID?\n",
+	       dp->d_namep );
+    }
 }
 
 /*
