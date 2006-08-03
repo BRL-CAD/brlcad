21ce257b533c641b75d8d483b228f47e016a904d
Christopher Sean Morrison <brlcad@gmail.com>
2006-08-03 17:28:25 +0000

    compute how many lines of the source code are actually blank and don't include those numbers in the totals for a slightly better/invariant approximation
    svn:revision:26892
    cvs:account:brlcad
    cvs:branch:trunk

--- sh/lines.sh
+++ sh/lines.sh
@@ -79,7 +79,6 @@ dc_lc="`echo \"$dc\" | sort | xargs wc -l`"
 dc_lc_lines="`echo \"$dc_lc\" | grep -v 'total$' | awk '{print $1}'`"
 dc_lc_total="`sum $dc_lc_lines`"
 
-
 # compute build infrastructure counts
 bic1="`find \"$BASE\" -type f \( -name \*.am -or -name configure.ac -or -name autogen.sh \)`"
 bic2="`find \"$BASE/sh\" -type f \( -name \*.sh \)`"
@@ -88,30 +87,40 @@ $bic2"
 bic_lc="`echo \"$bic\" | sort | xargs wc -l`"
 bic_lc_lines="`echo \"$bic_lc\" | grep -v 'total$' | awk '{print $1}'`"
 bic_lc_total="`sum $bic_lc_lines`"
+bic_lc_blank="`echo \"$bic\" | xargs awk ' /^[ 	]*$/ { ++x } END { print x } '`"
+bic_lc_total="`expr $bic_lc_total - $bic_lc_blank`"
 
 # compute header code counts
 header="`find \"$BASE\" -type f \( -name \*.h \) | grep -v '/other/' | grep -v '/sh/' | grep -v misc`"
 header_lc="`echo \"$header\" | sort | xargs wc -l`"
 header_lc_lines="`echo \"$header_lc\" | grep -v 'total$' | awk '{print $1}'`"
 header_lc_total="`sum $header_lc_lines`"
+header_lc_blank="`echo \"$header\" | xargs awk ' /^[ 	]*$/ { ++x } END { print x } '`"
+header_lc_total="`expr $header_lc_total - $header_lc_blank`"
 
 # compute non-header library code counts
 sourcelib="`find \"$BASE\" -type f \( -name \*.c \) | grep -v '/other/' | grep -v '/sh/' | grep -v misc | grep lib`"
 sourcelib_lc="`echo \"$sourcelib\" | sort | xargs wc -l`"
 sourcelib_lc_lines="`echo \"$sourcelib_lc\" | grep -v 'total$' | awk '{print $1}'`"
 sourcelib_lc_total="`sum $sourcelib_lc_lines`"
+sourcelib_lc_blank="`echo \"$sourcelib\" | xargs awk ' /^[ 	]*$/ { ++x } END { print x } '`"
+sourcelib_lc_total="`expr $sourcelib_lc_total - $sourcelib_lc_blank`"
 
 # compute non-header application code counts
 sourcebin="`find \"$BASE\" -type f \( -name \*.c \) | grep -v '/other/' | grep -v '/sh/' | grep -v misc | grep -v lib`"
 sourcebin_lc="`echo \"$sourcebin\" | sort | xargs wc -l`"
 sourcebin_lc_lines="`echo \"$sourcebin_lc\" | grep -v 'total$' | awk '{print $1}'`"
 sourcebin_lc_total="`sum $sourcebin_lc_lines`"
+sourcebin_lc_blank="`echo \"$sourcebin\" | xargs awk ' /^[ 	]*$/ { ++x } END { print x } '`"
+sourcebin_lc_total="`expr $sourcebin_lc_total - $sourcebin_lc_blank`"
 
 # compute script code counts
 scripts="`find \"$BASE\" -type f \( -name \*.sh -or -name \*.tcl -or -name \*.tk -or -name \*.itcl -or -name \*.itk \) | grep -v '/other/' | grep -v '/sh/' | grep -v misc`"
 scripts_lc="`echo \"$scripts\" | sort | xargs wc -l`"
 scripts_lc_lines="`echo \"$scripts_lc\" | grep -v 'total$' | awk '{print $1}'`"
 scripts_lc_total="`sum $scripts_lc_lines`"
+scripts_lc_blank="`echo \"$scripts\" | xargs awk ' /^[ 	]*$/ { ++x } END { print x } '`"
+scripts_lc_total="`expr $scripts_lc_total - $scripts_lc_blank`"
 
 # compute 3rd party code counts
 other="`find \"$BASE\" -type f \( -name \*.c -or -name \*.h -or -name \*.tcl -or -name \*.tk -or -name \*.itcl -or -name \*.itk -or -name \*.sh \) | grep '/other/'`"
@@ -122,6 +131,7 @@ other_lc_total="`sum $other_lc_lines`"
 # compute totals
 sc_lc_total="`echo \"$sourcelib_lc_total $sourcebin_lc_total $scripts_lc_total + + p\" | dc`"
 sch_lc_total="`echo \"$sc_lc_total $header_lc_total + p\" | dc`"
+blank_lc_total="`echo \"$bic_lc_blank $header_lc_blank $sourcelib_lc_blank $sourcebin_lc_blank $scripts_lc_blank ++++ p\" | dc`"
 total_code="`echo \"$bic_lc_total $sch_lc_total + p\" | dc`"
 total_noncode="`echo \"$dc_lc_total p\" | dc`"
 total="`echo \"$total_code $total_noncode + p\" | dc`"
@@ -131,6 +141,7 @@ printf -- "********************************\n"
 printf "** BRL-CAD LINE COUNT SUMMARY **\n"
 printf -- "********************************\n"
 printf "%7d\t%s\n" "$other_lc_total" "3rd Party Code (not counted)"
+printf "%7d\t%s\n" "$blank_lc_total" "Blank Lines of Source Code (not counted)"
 printf -- "--------------------------------\n"
 printf "%7d\t%s\n" "$dc_lc_total" "Documentation"
 printf "%7d\t%s\n" "$bic_lc_total" "Build Infrastructure"
