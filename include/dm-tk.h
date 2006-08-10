964dd99addc17c6fb85e4a07ec844ba602be7daa
Christopher Sean Morrison <brlcad@gmail.com>
2006-08-10 00:46:18 +0000

    add initial DM_TK interface header (from Tim Myers)
    svn:revision:26963
    cvs:account:brlcad
    cvs:branch:trunk

---
+++ include/dm-tk.h
@@ -0,0 +1,51 @@
+/*                          D M - T K . H
+ * BRL-CAD
+ *
+ * Copyright (C) 1993-2005 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file dm-tk.h
+ *
+ */
+#ifndef SEEN_DM_TK
+#define SEEN_DM_TK
+
+#include "dm_color.h"
+#define CMAP_BASE 40
+#define CUBE_DIMENSION 6
+#define NUM_PIXELS 216    /* CUBE_DIMENSION * CUBE_DIMENSION * CUBE_DIMENSION */
+#define ColormapNull (Colormap *)NULL
+
+struct Tk_vars {
+  GC gc;
+  Pixmap pix;
+  mat_t xmat;
+  int is_trueColor;
+  unsigned long bd, bg, fg;   /* color of border, background, foreground */
+  unsigned long pixels[NUM_PIXELS];
+};
+#endif /* SEEN_DM_TK */
+
+/*
+ * Local Variables:
+ * mode: C
+ * tab-width: 8
+ * c-basic-offset: 4
+ * indent-tabs-mode: t
+ * End:
+ * ex: shiftwidth=4 tabstop=8
+ */
