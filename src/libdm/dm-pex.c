19261de0114a6a1654c07fec2afc0ad89b2d1fdb
Christopher Sean Morrison <brlcad@gmail.com>
2006-08-23 14:53:11 +0000

    missed pex interface, use xtkwin insted of dpy with Tk_IsMapped()
    svn:revision:27049
    cvs:account:brlcad
    cvs:branch:trunk

--- src/libdm/dm-pex.c
+++ src/libdm/dm-pex.c
@@ -221,7 +221,7 @@ struct dm *dmp;
   ((struct pex_vars *)dmp->dmr_vars)->dpy = Tk_Display(((struct pex_vars *)dmp->dmr_vars)->xtkwin);
 
   /* make sure there really is a display before proceeding. */
-  if (!((struct pex_vars *)dmp->dm_vars)->dpy || !Tk_IsMapped(((struct pex_vars *)dmp->dm_vars)->dpy)) {
+  if (!((struct pex_vars *)dmp->dm_vars)->dpy || !Tk_IsMapped(((struct pex_vars *)dmp->dm_vars)->xtkwin)) {
       bu_vls_free(&str);
       (void)pex_close(dmp);
       return TCL_ERROR;
