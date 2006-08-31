1fe84a42b7dac6d04f3650b88ed4fc1e6862a097
Lee Butler <iraytrace@users.noreply.github.com>
2006-08-31 05:55:04 +0000

    doxygen patches
    svn:revision:27115
    cvs:account:lbutler
    cvs:branch:trunk

--- src/libbn/anim.c
+++ src/libbn/anim.c
@@ -18,7 +18,7 @@
  * License along with this file; see the file named COPYING for more
  * information.
  */
-/** \defgroup libbn libbn */
+/** \addtogroup libbn */
 /*@{*/
 /** @file anim.c
  *	Routines useful in animation programs.
