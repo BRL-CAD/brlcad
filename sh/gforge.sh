353581c4049890de10e3a2a95202f9f52eeabd00
Lee Butler <iraytrace@users.noreply.github.com>
2006-09-28 19:20:35 +0000

    script for querying a gforge site
    svn:revision:27185
    cvs:account:lbutler
    cvs:branch:trunk

---
+++ sh/gforge.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#
+# Retrieve stats from gforge
+#
+psql -U forge -h forge forge << EOF
+select project_name,summary,priority,percent_complete from 
+project_group_list l,project_task t where l.group_id=10 and 
+l.group_project_id=t.group_project_id
+order by project_name,priority asc,percent_complete desc;
+\q
+EOF
