d7fb2995938c9824a6e55a1e2f3b11a1557c6616
Christopher Sean Morrison <brlcad@gmail.com>
2006-10-05 23:06:57 +0000

    update the script to include the names of the person(s) that the task item is assigned to.  genericize the script to any gforge project while not requiring HOST/PROJECT to be set for some defaults.  modify the table output a little to change the assigned to field to be blank when nobody is assigned.
    svn:revision:27187
    cvs:account:brlcad
    cvs:branch:trunk

--- sh/gforge.sh
+++ sh/gforge.sh
@@ -2,10 +2,22 @@
 #
 # Retrieve stats from gforge
 #
-psql -U forge -h $HOST forge << EOF
-select project_name,summary,priority,percent_complete from 
-project_group_list l,project_task t where l.group_id=10 and 
-l.group_project_id=t.group_project_id
-order by project_name,priority asc,percent_complete desc;
-\q
+
+if test "x$PROJECT" = "x" ; then
+    PROJECT="BRL-CAD"
+fi
+if test "x$HOST" = "x" ; then
+    HOST=localhost
+fi
+
+psql -U forge -h $HOST forge << EOF | sed 's/None//g'
+SELECT project_name AS project, summary, priority, percent_complete, user_name AS assigned_to
+FROM groups g, project_group_list pgl, project_task pt, project_assigned_to pat, users u
+WHERE g.group_name = '$PROJECT'
+AND pgl.group_id = g.group_id
+AND pgl.group_project_id = pt.group_project_id
+AND pt.project_task_id = pat.project_task_id
+AND pat.assigned_to_id = u.user_id
+ORDER BY project_name, priority ASC, percent_complete DESC, summary
 EOF
+
