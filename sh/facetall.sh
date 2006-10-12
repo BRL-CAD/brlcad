e5d19b060372af628048dd3e2e8d7b7f3ae7b26e
Lee Butler <iraytrace@users.noreply.github.com>
2006-10-12 04:03:39 +0000

    tolerance and EOF ignore
    svn:revision:27195
    cvs:account:lbutler
    cvs:branch:trunk

--- sh/facetall.sh
+++ sh/facetall.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
-
-if [ $# -ne 1 ] ; then
-    echo "Usage: $0 database.g"
+export IGNOREEOF=10
+if [ $# -lt 1 -o $# -gt 2 ] ; then
+    echo "Usage: $0 database.g [tol]"
     exit -1
+fi
+
+if [ $# -eq 2 ] ; then
+    export TOL=$2
+else
+TOL="rel 0.01"
 fi
 
 STATUS=1
@@ -11,10 +17,10 @@ STATUS=1
 while [ X$STATUS != X0 ] ; do
 
     mged -c $1 <<EOF
-source facetall.mged
-facetize_all_regions
-q
+tol $TOL
+facetize_all_regions $1.fail
 EOF
     STATUS=$?
 
 done
+echo status $STATUS
