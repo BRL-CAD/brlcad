c42da314609fba3b10154a4770fdda8a6582f7c7
Lee Butler <iraytrace@users.noreply.github.com>
2006-10-14 02:34:07 +0000

    updates to support expanding combination record trees.  Serious improvements. changes to facetization of all regions.
    svn:revision:27202
    cvs:account:lbutler
    cvs:branch:trunk

--- src/tclscripts/mged/facetize_all_regions.tcl
+++ src/tclscripts/mged/facetize_all_regions.tcl
@@ -1,116 +1,7 @@
 #
-#  Expand a single boolean node in a tree
-#  to create new object called $name
-#
-proc expand_bool_node {name tree s} {
-
-    puts "$s expand_bool_node $name"
-
-    if { [catch {get $name} dbrec] } {
-	
-    } else {
-	# exists
-	puts "$name already exists... skipping\n$dbrec"
-	return
-    }
-
-    set left [lindex $tree 1]
-    set right [lindex $tree 2]
-
-    if {[lindex $left 0] != "l"} {
-	# need to expand left
-	set newname ${name}l
-	expand_bool_node $newname $left "$s  "
-	set left "l $newname"
-    }
-
-    if {[lindex $right 0] != "l"} {
-	# need to expand right
-	set newname ${name}r
-	expand_bool_node $newname $right "$s  "
-	set right "l $newname"
-    }
-
-
-    set tree [list [lindex $tree 0] $left $right]
-    set cmd "db put $name comb tree [list $tree]"
-    eval $cmd
-}
-
-#
-#  Create database objects for all boolean nodes in a combination tree
-#
-#  Options: -c  don't modify the given combination, copy it and expand that one.
-#               This allows the original combination record to remain intact.
-#
-# return 0 expanded all
-#	1 one or more did not expand (already single boolean)
-#
-#
-proc expand_comb_tree {args} {
-    set return_val 0
-
-    if {[llength $args] == 0} {
-	puts "usage: expand_comb_tree \[-c\] regname ..."
-    }
-
-    if {[string compare [lindex $args 0] "-c"] == 0} {
-	set duplicate 1
-    } else {
-	set duplicate 0
-    }
-
-    foreach r [lrange $args $duplicate end] {
-
-	puts "expand $r"
-
-	if {[catch {get $r tree} tree]} {
-	    puts "Error: could not get tree for $r.  Skipping"
-	    continue
-	}
-
-	if { [lindex $tree 0] == "l" } {
-	    # a combination of a single leaf?
-	    puts "single leaf [lindex $tree 1]"
-	    continue
-	}
-
-	set left [lindex $tree 1]
-	set right [lindex $tree 2]
-
-	# check to see if we are already a single boolean op
-	if {[lindex $left 0] == "l" && [lindex $right 0] == "l"} {
-	    puts "just 2 leaf nodes [lindex $left 1] [lindex $right 1]"} {
-	    set return_val 1
-	}
-	
-	# at lease one branch is deaper than a leaf
-
-	if {[lindex $left 0] != "l"} {
-	    expand_bool_node ${r}_xpand_l $left "  "
-	    set left "l ${r}_xpand_l"
-	}
-	if {[lindex $right 0] != "l"} {
-	    expand_bool_node  ${r}_xpand_r $right "  "
-	    set right "l ${r}_xpand_r"
-	}
-
-	set tree "[lindex $tree 0] [list $left] [list $right]"
-	if {$duplicate} {
-	    # create new region/combnation
-	    cp $r ${r}_xpand
-	    db adjust ${r}_xpand tree $tree
-	} else {
-	    db adjust $r tree $tree
-	}
-    }
-
-    return $return_val
-}
-
-#
 #
 # return 
+# -1 bot already exists
 # 0 success
 # 1 failure
 # 2 previously failed
@@ -119,10 +10,10 @@ proc attempt_facetize {reg reg_fail fail} {
 
     # check to see if the bot already exists
     # or previously failed to facetize
-    if { ! [catch {db get $reg.bot} regval]} {
+    if { ! [catch {db get_type $reg.bot} dbtype]} {
 	# already exists
-	puts " ---- $reg.bot exists $regval"
-	return 0
+	puts " ---- $reg.bot exists $dbtype"
+	return 3
     }
 
     if {[lsearch $reg_fail $reg] >= 0} {
@@ -131,9 +22,7 @@ proc attempt_facetize {reg reg_fail fail} {
 	return 2
     }
 
-
     # not in the list of previous failures.  Facetize it
-
     puts "---- Attempting to facetize $reg"
 
     # record the region we are about to attempt as failed
@@ -147,19 +36,76 @@ proc attempt_facetize {reg reg_fail fail} {
 	# facetize failed
 	puts "---- $reg failed to facetize"
 	return 1
-    } else {
-	puts "---- $reg now done"
+    } 
 
-	# remove $reg from list of failed
-	seek $fail $pos_fail
-	set erase [format "%*s" [string length $buf] " "]
-	puts -nonewline $fail $erase
-	flush $fail
-	seek $fail $pos_fail
-    }
+    puts "---- $reg now done"
+
+    # remove $reg from list of failed
+    seek $fail $pos_fail
+    set erase [format "%*s" [string length $buf] " "]
+    puts -nonewline $fail $erase
+    flush $fail
+    seek $fail $pos_fail
     
     return 0
 }
+
+
+#
+#  Attempt to facetize each region in a database
+#  This proc is usually called from facetall.sh
+#  which manages re-starting mged when we get a 
+#  core dump
+#
+proc facetize_all_regions {filename} {
+    global glob_compat_mode
+
+    set glob_compat_mode 0
+    set reg_list [ls -r]
+
+    puts "\nStarting to facetize regions\n\n"
+
+    # open list of files that previously failed to convert
+    # get list
+    if {[file exists $filename]} {
+	set fail [open $filename r]
+	set reg_fail [eval list [read $fail]]
+	close $fail
+    } else {
+	set reg_fail ""
+    }
+
+    set fail [open $filename a]
+
+    foreach reg $reg_list {
+	puts "------ <$reg> ------"
+	switch -exact [attempt_facetize $reg $reg_fail $fail] {
+	    0 { 
+		#succeeded
+		puts "$reg ok"
+	    }
+	    1 {
+		# failed
+
+		puts "expanding $reg"
+		expand_comb ${reg}_ $reg 0
+		puts "sub-facetizing $reg"
+#		facetize_failed_comb ${reg}_ $reg_fail $fail
+	    }
+	    2 { # previous failure
+		puts "$reg failed before"
+	    }
+	    3 { # previously succeeded 
+		puts "$reg previously succeeded"
+	    }
+	    default { puts "what?" }
+	}
+    }
+    set glob_compat_mode 1
+    return
+}
+
+
 
 #
 #  Facetize a sub-portion of a combination that failed to fully facetize
@@ -218,59 +164,4 @@ proc facetize_failed_comb {reg reg_fail fail} {
 	    }
 	}
     }
-}
-
-#
-#  Attempt to facetize each region in a database
-#  This proc is usually called from facetall.sh
-#  which manages re-starting mged when we get a 
-#  core dump
-#
-proc facetize_all_regions {filename} {
-    global glob_compat_mode
-
-    set glob_compat_mode 0
-    set reg_list [ls -r]
-
-    puts "\nStarting to facetize regions\n\n"
-
-    # open list of files that previously failed to convert
-    # get list
-    if {[file exists $filename]} {
-	set fail [open $filename r]
-	set reg_fail [eval list [read $fail]]
-	close $fail
-    } else {
-	set reg_fail ""
-    }
-
-    set fail [open $filename a]
-
-    foreach reg $reg_list {
-	puts "------ <$reg> ------"
-	switch [attempt_facetize $reg $reg_fail $fail] {
-	    0 { 
-		#succeeded
-		puts "$reg ok"
-	    }
-	    1 {
-		# failed
-
-		puts "expanding $reg"
-		if { [expand_comb_tree -c $reg] } {
-		    puts "sub-facetizing $reg"
-#		    facetize_failed_comb ${reg} $reg_fail $fail
-		} else {
-		    puts "sub-facetizing ${reg}_xpand"
-#		    facetize_failed_comb ${reg}_xpand $reg_fail $fail
-		}
-	    }
-	    2 { # previous failure
-		puts "$reg failed before"
-	    }
-	    default { puts "what?" }
-	}
-    }
-    set glob_compat_mode 1
-    return
 }
