611f72b7fac6bc5add57dd2252df032e75dae527
Christopher Sean Morrison <brlcad@gmail.com>
2007-01-20 23:17:27 +0000

    add header and footer; embed the psql sql statement directly
    svn:revision:27359
    cvs:account:brlcad
    cvs:branch:trunk

--- sh/prior.sh
+++ sh/prior.sh
@@ -1,5 +1,56 @@
+#                        P R I O R . S H
+# BRL-CAD
+#
+# Copyright (c) 2006-2007 United States Government as represented by
+# the U.S. Army Research Laboratory.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above
+# copyright notice, this list of conditions and the following
+# disclaimer in the documentation and/or other materials provided
+# with the distribution.
+#
+# 3. The name of the author may not be used to endorse or promote
+# products derived from this software without specific prior written
+# permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS
+# OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+# GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+###
 
-psql -f prior.psql -U forge -h forge forge | \
+psql -U forge -h forge forge <<EOF | \
 sed -E -e 's/[  ]{2,}//g' -e 's/[ ]*\|[ ]*/\|/g' -e 's/\|/,/g' | \
 awk -F , '{ if (match($4, "[12]+")) { sub(1, "Open", $4) ;  printf "%s,Forge,%s,%s,%s\n", $1, $2, $3 + 10, $4 	 } }'
-exit
+SELECT project_task.project_task_id,summary,priority,status_id,realname
+FROM project_task,users,project_assigned_to
+WHERE project_assigned_to.project_task_id=project_task.project_task_id
+AND project_assigned_to.assigned_to_id=users.user_id
+AND group_project_id=21
+AND status_id=1
+ORDER BY -priority,project_task.project_task_id;
+EOF
+
+# Local Variables:
+# mode: sh
+# tab-width: 8
+# sh-indentation: 4
+# sh-basic-offset: 4
+# indent-tabs-mode: t
+# End:
+# ex: shiftwidth=4 tabstop=8
