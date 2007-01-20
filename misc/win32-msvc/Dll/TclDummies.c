1c21e424b9436865d57d8c4fd6aa75afa073817a
Christopher Sean Morrison <brlcad@gmail.com>
2007-01-20 21:35:21 +0000

    header cleanup
    svn:revision:27350
    cvs:account:brlcad
    cvs:branch:trunk

--- misc/win32-msvc/Dll/TclDummies.c
+++ misc/win32-msvc/Dll/TclDummies.c
@@ -1,25 +1,23 @@
-/*                   T C L D U M M I E S . C
- *
- * @file TclDummies.c
- *
+/*                    T C L D U M M I E S . C
  * BRL-CAD
  *
- * Copyright (C) 2002-2007 United States Government as represented by
+ * Copyright (c) 2002-2007 United States Government as represented by
  * the U.S. Army Research Laboratory.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public License
- * as published by the Free Software Foundation; either version 2 of
- * the License, or (at your option) any later version.
+ * as published by the Free Software Foundation.
  *
  * This library is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Library General Public License for more details.
+ * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this file; see the file named COPYING for more
  * information.
+ */
+/** @file TclDummies.c
  *
  *  Comments -
  *      TCL dummy functions to make BRL-CAD work without TCL/TK
