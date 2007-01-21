5aa974750ca6f5bd0660e5ddf3a2907e76d69eff
Christopher Sean Morrison <brlcad@gmail.com>
2007-01-21 00:12:17 +0000

    standard header and footer
    svn:revision:27364
    cvs:account:brlcad
    cvs:branch:trunk

--- src/adrt/bench/bench.c
+++ src/adrt/bench/bench.c
@@ -1,3 +1,28 @@
+/*                         B E N C H . C
+ * BRL-CAD
+ *
+ * Copyright (c) 2007 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file bench.c
+ *
+ * Author -
+ *   Justin Shumaker
+ */
+
 #include "bench.h"
 #include <math.h>
 #include <pthread.h>
@@ -205,3 +230,13 @@ void bench(char *proj, int cache, int image) {
     free(kdcache);
   }
 }
+
+/*
+ * Local Variables:
+ * mode: C
+ * tab-width: 8
+ * c-basic-offset: 4
+ * indent-tabs-mode: t
+ * End:
+ * ex: shiftwidth=4 tabstop=8
+ */
--- src/adrt/bench/bench.h
+++ src/adrt/bench/bench.h
@@ -1,3 +1,29 @@
+/*                         B E N C H . H
+ * BRL-CAD
+ *
+ * Copyright (c) 2007 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file bench.h
+ *
+ * Author -
+ *   Justin Shumaker
+ *
+ */
+
 #ifndef _BENCH_H
 #define _BENCH_H
 
@@ -6,3 +32,13 @@ extern	void bench(char *proj, int cache, int image);
 
 
 #endif
+
+/*
+ * Local Variables:
+ * mode: C
+ * tab-width: 8
+ * c-basic-offset: 4
+ * indent-tabs-mode: t
+ * End:
+ * ex: shiftwidth=4 tabstop=8
+ */
