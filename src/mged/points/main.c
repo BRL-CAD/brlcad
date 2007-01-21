d14fd9ae67214c4e26591c5ba5caa14adcda23d6
Christopher Sean Morrison <brlcad@gmail.com>
2007-01-21 07:40:57 +0000

    missing header
    svn:revision:27375
    cvs:account:brlcad
    cvs:branch:trunk

--- src/mged/points/main.c
+++ src/mged/points/main.c
@@ -1,3 +1,28 @@
+/*                          M A I N . C
+ * BRL-CAD
+ *
+ * Copyright (c) 2007 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file main.c
+ *
+ * Author -
+ *   Christopher Sean Morrison
+ */
+
 #include <stdio.h>
 
 #include "./count.h"
