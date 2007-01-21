d4137d603d5728a18ada1a663fe9ba56eaff122e
Christopher Sean Morrison <brlcad@gmail.com>
2007-01-21 07:30:35 +0000

    Questionable whether they actually hold copyright or are simply being given credit.  declare the more flexible bsd terms for now.
    svn:revision:27373
    cvs:account:brlcad
    cvs:branch:trunk

--- src/libtclcad/tkImgFmtPIX.c
+++ src/libtclcad/tkImgFmtPIX.c
@@ -1,5 +1,38 @@
-/*
- * tkImgFmtPIX.c --
+/*                   T K I M G F M T P I X . C
+ * BRL-CAD
+ *
+ * Copyright (c) 1994-2007 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided
+ * with the distribution.
+ *
+ * 3. The name of the author may not be used to endorse or promote
+ * products derived from this software without specific prior written
+ * permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+/** @file tkImgFmtPIX.c
  *
  *      A photo image file handler for BRL-CAD(tm) ".pix" format files.
  *
@@ -10,18 +43,16 @@
  *	The U. S. Army Research Laboratory
  *	Aberdeen Proving Ground, Maryland  21005-5068  USA
  *
- *
- *
  * Based on:
  *	tkImgFmtPPM.c --
  *
  *	A photo image file handler for PPM (Portable PixMap) files.
  *
+ * Questionable whether they actually hold copyright or are simply
+ * being given credit:
+ *
  * Copyright (c) 1994 The Australian National University.
  * Copyright (c) 1994-1995 Sun Microsystems, Inc.
- *
- * See the file "license.terms" for information on usage and redistribution
- * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
  * Author: Paul Mackerras (paulus@cs.anu.edu.au),
  *	   Department of Computer Science,
