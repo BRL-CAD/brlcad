20313e670f1d694afd5efaa6a56add2b8affd336
Christopher Sean Morrison <brlcad@gmail.com>
2007-01-21 07:23:20 +0000

    bsd header
    svn:revision:27372
    cvs:account:brlcad
    cvs:branch:trunk

--- src/librt/raydebug.tcl
+++ src/librt/raydebug.tcl
@@ -1,11 +1,48 @@
 #!/bin/sh
-# The next line restarts the shell script using best WISH in $PATH
-# exec bwish "$0" "$@"
+#                    R A Y D E B U G . T C L
+# BRL-CAD
+#
+# Copyright (c) 2007 United States Government as represented by
+# the U.S. Army Research Laboratory.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above
+# copyright notice, this list of conditions and the following
+# disclaimer in the documentation and/or other materials provided
+# with the distribution.
+#
+# 3. The name of the author may not be used to endorse or promote
+# products derived from this software without specific prior written
+# permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS
+# OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+# GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+###
 # demo.tcl
 #	setenv LD_LIBRARY_PATH /usr/lib/X11:/usr/X11/lib
 #
 # A GUI to drive MGED through a series of plot files.
 # -Mike Muuss, ARL, July 97.
+####
+
+# The next line restarts the shell script using BRL-CAD's WISH \
+exec bwish "$0" "$@"
 
 puts "running raydebug.tcl"
 
