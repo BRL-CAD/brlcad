b3480ace4ee9caa6b9d8710d57fbd371faf9515b
Christopher Sean Morrison <brlcad@gmail.com>
2007-02-01 16:15:48 +0000

    define BLT_DBL_EPSILON and BLT_FLT_EPSILON so that configless compilation will succeed.
    svn:revision:27458
    cvs:account:brlcad
    cvs:branch:trunk

--- src/other/blt/src/bltMath.h
+++ src/other/blt/src/bltMath.h
@@ -82,9 +82,6 @@
  *
  * ----------------------------------------------------------------------
  */
-#ifndef BLT_DBL_EPSILON
-#  define BLT_DBL_EPSILON 2.2204460492503131e-16
-#endif
 /*
  * Don't want to include __infinity (definition of HUGE_VAL (SC1.x))
  */
@@ -93,9 +90,6 @@
 #define DBL_MIN		2.2250738585072014E-308
 #define DBL_EPSILON	2.2204460492503131e-16
 #else
-#ifndef DBL_EPSILON
-#define DBL_EPSILON	BLT_DBL_EPSILON
-#endif
 #ifdef HUGE_VAL
 #define DBL_MAX		HUGE_VAL
 #define DBL_MIN		(1/HUGE_VAL)
@@ -112,6 +106,21 @@
 #endif /*HUGE*/
 #endif /*HUGE_VAL*/
 #endif /*sun*/
+
+#ifndef BLT_DBL_EPSILON
+#  define BLT_DBL_EPSILON 2.2204460492503131e-16
+#endif
+#ifndef BLT_FLT_EPSILON
+#  define BLT_FLT_EPSILON 1.19209290e-07F
+#endif
+
+#ifndef DBL_EPSILON
+#  define DBL_EPSILON	BLT_DBL_EPSILON
+#endif
+#ifndef FLT_EPSILON
+#  define FLT_EPSILON	BLT_FLT_EPSILON
+#endif
+
 #endif /*!HAVE_FLOAT_H*/
 
 /*
