386bb732dbd3ded850b55bd538c3bc1774ba2199
Christopher Sean Morrison <brlcad@gmail.com>
2007-02-14 09:37:55 +0000

    major rewrite, add support for -s, -l, and -i options for silent, verbose output (ala cmp's -l option), and support for skipping input.  make the return/error codes more uniform as well as more detail on the usage.  still need to actually add the print statement for -l, but everything else should be done.  basic operation and previous use remains unaffected.
    svn:revision:27563
    cvs:account:brlcad
    cvs:branch:trunk

--- bench/pixcmp.c
+++ bench/pixcmp.c
@@ -31,10 +31,7 @@
  *  Author -
  *	Charles M. Kennedy
  *	Michael John Muuss
- *
- *  Source -
- *	SECAD/VLD Computing Consortium, Bldg 394
- *	The U. S. Army Ballistic Research Laboratory
+ *	Christopher Sean Morrison
  */
 #ifndef lint
 static const char RCSid[] = "@(#)$Header$ (BRL)";
@@ -44,102 +41,229 @@ static const char RCSid[] = "@(#)$Header$ (BRL)";
 #include <stdlib.h>
 #include <string.h>
 
-long	matching;
-long	off1;
-long	offmany;
+#ifndef _WIN32
+#  include <unistd.h>
+#endif
 
-char usage[] = "Usage: pixcmp f1.pix f2.pix\n";
+/* exit codes for argument processing errors */
+#define OPTS_ERROR 127
+#define FILE_ERROR 126
 
-int main(int argc, char **argv);
+/* exit codes for normal operation */
+#define OFF_BY_NONE 0
+#define OFF_BY_ONE  1
+#define OFF_BY_MANY 2
 
+
+void
+usage(const char *name)
+{
+    fprintf(stderr, "Usage: %s [OPTIONS] FILE1 [FILE2 [SKIP1 [SKIP2]]]\n%s", name,
+	    "Compare two PIX image files pixel by pixel.\n\n"
+	    "  -l   List pixel numbers and values for all pixels that differ.\n"
+	    "  -i SKIP\n"
+	    "       Skip the first SKIP pixels of input (for FILE1 and FILE2)\n"
+	    "  -i SKIP1:SKIP2\n"
+	    "       Skip the first SKIP1 pixels in FILE1 and SKIP2 pixels in FILE2.\n"
+	    "  -s   Silent output.  Only return an exit status.\n\n"
+	    "If FILE is `-' or is missing, then input is read from standard input.\n"
+	    "Pixel numbers are indexed linearly from zero.\n");
+    return;
+}
+
+
+void
+handle_i_opt(const char *arg, long *skip1, long *skip2)
+{
+    const char *endptr = arg;
+    if ((arg == NULL) ||
+	((skip1 == NULL) && (skip2 == NULL))){
+	/* nothing to do */
+	return;
+    }
+    while ((endptr != NULL) && (endptr[0] != ':') && (endptr[0] != '\0')) {
+	endptr++;
+    }
+    if (endptr == arg) {
+	/* probably empty string */
+	if (skip2) {
+	    endptr++;
+	    *skip2 = strtol(endptr, NULL, 10);
+	}
+    } else if ((endptr == NULL) || (endptr[0] == '\0')) {
+	/* no : found */
+	if (skip2) {
+	    *skip2 = strtol(arg, NULL, 10);
+	}
+	if (skip1) {
+	    *skip1 = *skip2;
+	}
+    } else if (endptr[0] == ':') {
+	/* found : */
+	if (skip1) {
+	    *skip1 = strtol(arg, NULL, 10);
+	}
+	if (skip2) {
+	    endptr++; /* skip over : */
+	    *skip2 = strtol(endptr, NULL, 10);
+	}
+    } else {
+	fprintf(stderr, "Unexpected input processing [%s]\n", arg);
+	exit(OPTS_ERROR);
+    }
+}
 
 int
-main(int argc, char **argv)
+main(int argc, char *argv[])
 {
-	register FILE *f1, *f2;
+    FILE *f1, *f2;
 
-	if( argc != 3 )  {
-		fprintf(stderr, "%s", usage);
-		exit(0);
+    long matching;
+    long off1;
+    long offmany;
+
+    int c;
+    int list_pixel_values = 0;
+    int silent = 0;
+    long f1_skip = 0;
+    long f2_skip = 0;
+
+    /* process opts */
+    while ((c = getopt(argc, argv, "li:s")) != EOF) {
+	switch (c) {
+	    case 'l':
+		list_pixel_values = 1;
+		break;
+	    case 'i':
+		handle_i_opt(optarg, &f1_skip, &f2_skip);
+		argc--; argv++;
+		break;
+	    case 's':
+		silent = 1;
+		break;
+	    default:
+		fprintf(stderr, "\n");
+		usage(argv[0]);
+		exit(OPTS_ERROR);
 	}
+	argc--;	argv++;
+    }
 
-	if( strcmp( argv[1], "-" ) == 0 )
-		f1 = stdin;
-	else if( (f1 = fopen( argv[1], "r" ) ) == NULL )  {
-		perror( argv[1] );
-		exit(1);
-	}
-	if( strcmp( argv[2], "-" ) == 0 )
-		f2 = stdin;
-	else if( (f2 = fopen( argv[2], "r" ) ) == NULL )  {
-		perror( argv[2] );
-		exit(1);
-	}
-	while(1)  {
-		register int r1, g1, b1;
-		int r2, g2, b2;
+    /* validate what is left over */
+    if( argc < 2 || argc > 5)  {
+	fprintf(stderr, "ERROR: incorrect number of arguments provided\n\n");
+	usage(argv[0]);
+	exit(OPTS_ERROR);
+    }
 
-		r1 = fgetc( f1 );
-		g1 = fgetc( f1 );
-		b1 = fgetc( f1 );
-		r2 = fgetc( f2 );
-		g2 = fgetc( f2 );
-		b2 = fgetc( f2 );
-		if( feof(f1) || feof(f2) )  break;
+    /* handle optional SKIP1 and SKIP2 following filenames */
+    if (argc > 3) {
+	char range[64] = {0};
+	snprintf(range, 64, "%s%s%s", argc>3?argv[3]:"", argc>4?":":"", argc>4?argv[4]:"");
+	handle_i_opt(range, &f1_skip, &f2_skip);
+    }
 
-		if( r1 != r2 || g1 != g2 || b1 != b2 )  {
-			register int i;
+    /* printf("Skip from FILE1: %ld and from FILE2: %ld\n", f1_skip, f2_skip); */
 
-			/* Highlight differing channels */
-			if( r1 != r2 )  {
-				if( (i = r1 - r2) < 0 )  i = -i;
-				if( i > 1 )  {
-					offmany++;
-				} else {
-					off1++;
-				}
-			} else {
-				matching++;
-			}
-			if( g1 != g2 )  {
-				if( (i = g1 - g2) < 0 )  i = -i;
-				if( i > 1 )  {
-					offmany++;
-				} else {
-					off1++;
-				}
-			} else {
-				matching++;
-			}
-			if( b1 != b2 )  {
-				if( (i = b1 - b2) < 0 )  i = -i;
-				if( i > 1 )  {
-					offmany++;
-				} else {
-					off1++;
-				}
-			} else {
-				matching++;
-			}
-		}  else  {
-			/* Common case:  equal.  Give B&W NTSC average */
-			matching += 3;
+    if (strcmp( argv[1], "-" ) == 0 ) {
+	f1 = stdin;
+    } else if ((f1 = fopen( argv[1], "r" )) == NULL)  {
+	perror(argv[1]);
+	exit(FILE_ERROR);
+    }
+    if ((argc < 3) || (strcmp(argv[2], "-") == 0)) {
+	f2 = stdin;
+    } else if ((f2 = fopen( argv[2], "r" )) == NULL) {
+	perror(argv[2]);
+	exit(FILE_ERROR);
+    }
+
+    /* skip requested pixels in FILE1 */
+    if (fseek(f1, f1_skip * 3, SEEK_SET)) {
+	fprintf(stderr, "ERROR: Unable to seek %ld pixels (%ld bytes) in FILE1\n", f1_skip, f1_skip*3);
+	perror("FILE1 fseek failure");
+	exit(FILE_ERROR);
+    }
+    
+    /* skip requested pixels in FILE2 */
+    if (fseek(f2, f2_skip * 3, SEEK_SET)) {
+	fprintf(stderr, "ERROR: Unable to seek %ld pixels (%ld bytes) in FILE2\n", f2_skip, f2_skip*3);
+	perror("FILE2 fseek failure");
+	exit(FILE_ERROR);
+    }
+
+    while(1)  {
+	register int r1, g1, b1;
+	int r2, g2, b2;
+
+	r1 = fgetc( f1 );
+	g1 = fgetc( f1 );
+	b1 = fgetc( f1 );
+	r2 = fgetc( f2 );
+	g2 = fgetc( f2 );
+	b2 = fgetc( f2 );
+	if( feof(f1) || feof(f2) )  break;
+
+	if( r1 != r2 || g1 != g2 || b1 != b2 )  {
+	    register int i;
+
+	    /* Highlight differing channels */
+	    if( r1 != r2 )  {
+		if( (i = r1 - r2) < 0 )  i = -i;
+		if( i > 1 )  {
+		    offmany++;
+		} else {
+		    off1++;
 		}
+	    } else {
+		matching++;
+	    }
+	    if( g1 != g2 )  {
+		if( (i = g1 - g2) < 0 )  i = -i;
+		if( i > 1 )  {
+		    offmany++;
+		} else {
+		    off1++;
+		}
+	    } else {
+		matching++;
+	    }
+	    if( b1 != b2 )  {
+		if( (i = b1 - b2) < 0 )  i = -i;
+		if( i > 1 )  {
+		    offmany++;
+		} else {
+		    off1++;
+		}
+	    } else {
+		matching++;
+	    }
+	}  else  {
+	    /* Common case:  equal.  Give B&W NTSC average */
+	    matching += 3;
 	}
+    }
+    if (!silent) {
 	fprintf(stderr,
-		"pixcmp bytes: %8ld matching, %8ld off by 1, %8ld off by many\n",
+		"pixcmp pixels: %8ld matching, %8ld off by 1, %8ld off by many\n",
 		matching, off1, offmany );
+    }
 
-	/* If files were of different lengths, that is an error */
-	if( feof(f1) != feof(f2) )
-		exit(2);
+    /* If files were of different lengths, that is an error */
+    if (feof(f1) != feof(f2)) {
+	return FILE_ERROR;
+    }
 
-	/* If any pixels were off by more than +/- 1, that is an error */
-	if( offmany)
-		exit(1);
+    /* indicate how many differences there were overall */
+    if (offmany) {
+	return OFF_BY_MANY;
+    }
+    if (off1) {
+	return OFF_BY_ONE;
+    }
 
-	/* Success! */
-	exit(0);
+    /* Success! */
+    return OFF_BY_NONE;
 }
 
 /*
