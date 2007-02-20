4281e55d21919e7d8ae3b42c0edaeb0d17eeb47a
Christopher Sean Morrison <brlcad@gmail.com>
2007-02-20 03:42:32 +0000

    use a filesystem group, these routines have nothing to do with logging
    svn:revision:27619
    cvs:account:brlcad
    cvs:branch:trunk

--- src/libbu/brlcad_path.c
+++ src/libbu/brlcad_path.c
@@ -17,7 +17,7 @@
  * License along with this file; see the file named COPYING for more
  * information.
  */
-/** @addtogroup bu_log */
+/** @addtogroup filesystem */
 /** @{ */
 /** @file brlcad_path.c
  *
