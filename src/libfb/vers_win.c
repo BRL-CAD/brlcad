24a3d20351966682708c77898c3c6a502cdbf4b1
Christopher Sean Morrison <brlcad@gmail.com>
2007-04-16 21:28:12 +0000

    remove the vers_win.c files as the globals they declare are no longer used (for bn, bu, fb, rt)
    svn:revision:28044
    cvs:account:brlcad
    cvs:branch:trunk

--- src/libfb/vers_win.c
+++
@@ -1,4 +0,0 @@
-const char fb_version[] = "\
-@(#) BRL-CAD Release 7.4.1   The BRL-CAD Framebuffer Library\n\
-    Mon Sep 12 16:00:00 EDT 2005, Compilation 1\n\
-    bob@mako:/home/bob/src/brlcad/src/libfb\n";
