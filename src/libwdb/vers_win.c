9c9dc0d89efca1504f12b83ea4d6aeca43f202f4
Christopher Sean Morrison <brlcad@gmail.com>
2007-04-17 00:50:09 +0000

    remove the unused version global
    svn:revision:28055
    cvs:account:brlcad
    cvs:branch:trunk

--- src/libwdb/vers_win.c
+++
@@ -1,4 +0,0 @@
-const char wdb_version[] = "\
-@(#) BRL-CAD Release 7.4.1   Graphics Editor (MGED)\n\
-    Mon Sep 12 16:00:00 EDT 2005, Compilation 1\n\
-    bob@mako:/home/bob/src/brlcad/src/libwdb\n";
