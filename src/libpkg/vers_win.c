16da0669e45df5f8ddc6b2d2792e292b73b4b199
Christopher Sean Morrison <brlcad@gmail.com>
2007-04-17 04:39:54 +0000

    windows version files no longer needed/used
    svn:revision:28066
    cvs:account:brlcad
    cvs:branch:trunk

--- src/libpkg/vers_win.c
+++
@@ -1,4 +0,0 @@
-char pkg_version[] = "\
-@(#) BRL-CAD Release 7.6.7   The BRL-CAD Network Package (PKG) Library\n\
-    Mon Sep 12 16:00:00 EDT 2005, Compilation 1\n\
-    bob@mako:/home/bob/src/brlcad/src/libpkg\n";
