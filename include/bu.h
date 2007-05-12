e8d521139282065e52d225a4a8d090a6a8abb09d
Christopher Sean Morrison <brlcad@gmail.com>
2007-05-12 00:09:54 +0000

    add BU_DEBUG_BACKTRACE, cleanup and slot the unused
    svn:revision:28189
    cvs:account:brlcad
    cvs:branch:trunk

--- include/bu.h
+++ include/bu.h
@@ -1254,26 +1254,36 @@ struct bu_mro {
  * Section for BU_DEBUG values
  *
  * These can be set from the command-line of RT-compatible programs
- * using the "-! ###" option.
+ * using the "-!" option.
  */
 BU_EXPORT extern int	bu_debug;
 /* These definitions are each for one bit */
 #define BU_DEBUG_OFF		0	/* No debugging */
 
-#define BU_DEBUG_COREDUMP	0x00000001	/* 001 If set, bu_bomb() will dump core */
+#define BU_DEBUG_COREDUMP	0x00000001	/* If set, bu_bomb() will dump core */
+#define BU_DEBUG_MEM_CHECK	0x00000002	/* Mem barrier & leak checking */
+#define BU_DEBUG_MEM_LOG	0x00000004	/* Print all dynamic memory operations */
+#define BU_DEBUG_DB		0x00000008	/* Database debugging */
 
-#define BU_DEBUG_MEM_CHECK	0x00000002	/* 002 Mem barrier & leak checking */
-#define BU_DEBUG_MEM_LOG	0x00000004	/* 003 Print all dynamic memory operations */
-#define BU_DEBUG_DB		0x00000008	/* 004 Database debugging */
+#define BU_DEBUG_PARALLEL	0x00000010	/* parallel support */
+#define BU_DEBUG_MEM_QCHECK	0x00000020	/* Fast mem leak checking (won't work with corruption) */
+#define BU_DEBUG_BACKTRACE	0x00000040	/* If set, backtrace details will be logged */
+#define BU_DEBUG_PATHS		0x00000080	/* debug path searching */
 
-#define BU_DEBUG_PARALLEL	0x00000010	/* 005 parallel support */
-#define BU_DEBUG_MEM_QCHECK	0x00000020	/* 006 Fast mem leak checking (won't work with corruption) */
-#define BU_DEBUG_MATH		0x00000100	/* 011 Fundamental math routines (plane.c, mat.c) */
-#define BU_DEBUG_PTBL		0x00000200	/* 012 bu_ptbl_*() logging */
-#define BU_DEBUG_AVS		0x00000400	/* 013 bu_avs_*() logging */
-#define BU_DEBUG_MAPPED_FILE	0x00000800	/* 014 bu_mapped_file logging */
+#define BU_DEBUG_MATH		0x00000100	/* Fundamental math routines (plane.c, mat.c) */
+#define BU_DEBUG_PTBL		0x00000200	/* bu_ptbl_*() logging */
+#define BU_DEBUG_AVS		0x00000400	/* bu_avs_*() logging */
+#define BU_DEBUG_MAPPED_FILE	0x00000800	/* bu_mapped_file logging */
 
-#define BU_DEBUG_TABDATA	0x00010000	/* 025 LIBBN: tabdata */
+#define BU_DEBUG_UNUSED_1	0x00001000	/* unused */
+#define BU_DEBUG_UNUSED_2	0x00002000	/* unused */
+#define BU_DEBUG_UNUSED_3	0x00004000	/* unused */
+#define BU_DEBUG_UNUSED_4	0x00008000	/* unused */
+
+#define BU_DEBUG_TABDATA	0x00010000	/* LIBBN: tabdata */
+#define BU_DEBUG_UNUSED_5	0x00020000	/* unused */
+#define BU_DEBUG_UNUSED_6	0x00040000	/* unused */
+#define BU_DEBUG_UNUSED_7	0x00080000	/* unused */
 
 /* Format string for bu_printb() */
 #define BU_DEBUG_FORMAT	\
