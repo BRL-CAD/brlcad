84ac3860b62d863e28b89d3a4fe8a434c3a8cb48
Christopher Sean Morrison <brlcad@gmail.com>
2007-05-25 05:46:59 +0000

    quotes, and update comment
    svn:revision:28390
    cvs:account:brlcad
    cvs:branch:trunk

--- src/tclscripts/mged/mged.tcl
+++ src/tclscripts/mged/mged.tcl
@@ -61,16 +61,14 @@ if { [info exists tk_strictMotif] == 0 } {
 # html_library.tcl  :  Stephen Uhler's routines for processing HTML
 #==============================================================================
 
-# XXX what MGED html manual directory search order precedence should be:
+# MGED html manual directory search order precedence should be:
 #   MGED_HTML_DIR
 #   bu_brlcad_data/html/manuals/mged
-#   bu_brlcad_root/html/manuals/mged
-#
 
 if ![info exists mged_default(html_dir)] {
     set mged_default(html_dir) [bu_brlcad_data "html/manuals/mged"]
     if {![file exists $mged_default(html_dir)]} {
-	set mged_default(html_dir) [bu_brlcad_data doc/html/manuals/mged]
+	set mged_default(html_dir) [bu_brlcad_data "doc/html/manuals/mged"]
     }
 }
 
