4a4fc7e0defd621608683a08d66de9007b4e883c
Jason Owens <github.jlowenz@xoxy.net>
2007-07-13 20:22:58 +0000

    Muck with tolerances a bit (they may not be the problem right now - i'm currently thinking its a trimming issue)
    svn:revision:28695
    cvs:account:jlowenz
    cvs:branch:trunk

--- include/brep.h
+++ include/brep.h
@@ -71,7 +71,8 @@ typedef struct _on_brep_placeholder {
     /* Root finding threshold */
 #define BREP_INTERSECTION_ROOT_EPSILON 1e-6
     /* Jungle Gym epsilon */
-#define BREP_EDGE_MISS_TOLERANCE 1e-6
+#define BREP_EDGE_MISS_TOLERANCE 5e-4
+#define BREP_SAME_POINT_TOLERANCE 1e-3
     /* Use vector operations? For debugging */
 #define DO_VECTOR 1
     
