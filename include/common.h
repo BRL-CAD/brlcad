290cb3a4af0706ffb14ac3bbe351fac0b83ef2a0
Christopher Sean Morrison <brlcad@gmail.com>
2007-09-15 03:27:59 +0000

    oops, but the genie back in the bottle.  these don't need to go here.
    svn:revision:28929
    cvs:account:brlcad
    cvs:branch:trunk

--- include/common.h
+++ include/common.h
@@ -64,23 +64,6 @@
 #  define __END_DECLS
 #endif
 
-/* c89 headers */
-#define HAVE_ASSERT_H 1
-#define HAVE_CTYPE_H 1
-#define HAVE_ERRNO_H 1
-#define HAVE_FLOAT_H 1
-#define HAVE_LIMITS_H 1
-#define HAVE_LOCALE_H 1
-#define HAVE_MATH_H 1
-#define HAVE_SETJMP_H 1
-#define HAVE_SIGNAL_H 1
-#define HAVE_STDARG_H 1
-#define HAVE_STDDEF_H 1
-#define HAVE_STDIO_H 1
-#define HAVE_STDLIB_H 1
-#define HAVE_STRING_H 1
-#define HAVE_TIME_H 1
-
 #endif  /* __COMMON_H__ */
 /** @} */
 /*
