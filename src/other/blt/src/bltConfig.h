105f77b87c16f3b4994eec3258a807989b1c89a7
Christopher Sean Morrison <brlcad@gmail.com>
2007-09-15 03:23:14 +0000

    since we don't call blt's configure, make sure it knows that the c89 headers are presumed.  this is intentionally here and not in the top-level configure to prevent repropagation of their use in the rest of the code.  blt should be a subconfigure.
    svn:revision:28927
    cvs:account:brlcad
    cvs:branch:trunk

--- src/other/blt/src/bltConfig.h
+++ src/other/blt/src/bltConfig.h
@@ -1,2 +1,20 @@
 /* utilize BRL-CAD's primary configure script which already has tests for everything BLT needs */
 #include "common.h"
+
+/* c89 headers */
+#define HAVE_ASSERT_H 1
+#define HAVE_CTYPE_H 1
+#define HAVE_ERRNO_H 1
+#define HAVE_FLOAT_H 1
+#define HAVE_LIMITS_H 1
+#define HAVE_LOCALE_H 1
+#define HAVE_MATH_H 1
+#define HAVE_SETJMP_H 1
+#define HAVE_SIGNAL_H 1
+#define HAVE_STDARG_H 1
+#define HAVE_STDDEF_H 1
+#define HAVE_STDIO_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRING_H 1
+#define HAVE_TIME_H 1
+
