abd8a920a46fe87d7508fc700945f9cde93d248b
Christopher Sean Morrison <brlcad@gmail.com>
2007-10-17 01:46:51 +0000

    update the usage to reflect the change
    svn:revision:29105
    cvs:account:brlcad
    cvs:branch:trunk

--- src/tclscripts/helplib.tcl
+++ src/tclscripts/helplib.tcl
@@ -142,7 +142,7 @@ set helplib_data(wdb_title)		{{description} {set/get database title}}
 set helplib_data(wdb_tol)		{{[abs|rel|norm|dist|perp [#]} {Set/get tessellation and calculation tolerances}}
 set helplib_data(wdb_tops)		{{[-n] [-u] [-g]}	{find all top level objects}}
 set helplib_data(wdb_track)		{{args} {create a track}}
-set helplib_data(wdb_tree)		{{[-c] [-o outfile] [-i indentSize] object(s)} {print out a tree of all members of an object}}
+set helplib_data(wdb_tree)		{{[-c] [-o outfile] [-i indentSize] [object(s)]} {print out the object hierarchy for all object(s) specified or for all currently displayed}}
 set helplib_data(wdb_unhide)            {{[objects]} {unset the "hidden" flag for the specified objects so they will appear in a "t" or "ls" command output}}
 set helplib_data(wdb_units)		{{[mm|cm|m|in|ft|...]}	{get/set current working units for distance measurements}}
 set helplib_data(wdb_version)		{{} {returns the database version}}
