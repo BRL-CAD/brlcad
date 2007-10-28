525cf44186ef8eaa3c82eb6f2a655fa239237104
Christopher Sean Morrison <brlcad@gmail.com>
2007-10-28 05:10:15 +0000

    ws
    svn:revision:29189
    cvs:account:brlcad
    cvs:branch:trunk

--- src/burst/error.c
+++ src/burst/error.c
@@ -19,13 +19,6 @@
  *
  */
 /** @file error.c
- *	Author:		Gary S. Moss
- */
-#ifndef lint
-static const char RCSid[] = "@(#)$Header$ (BRL)";
-#endif
-/*
- *			E R R O R
  *
  *  Ray Tracing library and Framebuffer library, error handling routines.
  *
@@ -33,85 +26,69 @@ static const char RCSid[] = "@(#)$Header$ (BRL)";
  *	brst_log		Called to log RT library events.
  *	fb_log		Called to log FB library events.
  *
+ *	Author:		Gary S. Moss
  *	Idea originated by Mike John Muuss
  */
+#include "common.h"
 
 #include <stdio.h>
 #include <signal.h>
+
 #include "./Sc.h"
 #include "./extern.h"
-
 
 /*
  *  		B R S T _  L O G
  *
  *  Log an RT library event
  */
-/* VARARGS */
 void
-#if defined(HAVE_STDARG_H)
 brst_log( char *fmt, ... )
-	{
-	va_list	ap;
-	va_start( ap, fmt );
-#else
-brst_log( va_alist )
-va_dcl
-	{
-	register char *fmt; /* picked up by va_arg() */
-	va_list	ap;
-	va_start( ap );
-	fmt  = va_arg( ap, char * );
-#endif
-	if( tty && (errfile[0] == '\0' || ! strcmp( errfile, "/dev/tty" )) )
-		{
-		clr_Tabs( HmTtyFd );
-		if( ScDL != NULL )
-			{
-			(void) ScMvCursor( 1, SCROLL_TOP );
-			(void) ScDeleteLn();
-			(void) ScMvCursor( 1, SCROLL_BTM );
-			(void) ScClrEOL();
-			(void) vprintf( fmt, ap );
-			}
-		else
-		if( ScSetScrlReg( SCROLL_TOP, SCROLL_BTM+1 ) )
-			{	char buf[LNBUFSZ];
-				char *p;
-			(void) ScMvCursor( 1, SCROLL_BTM+1 );
-			(void) ScClrEOL();
-			/* Work around for problem with vprintf(): it doesn't
-				cause the screen to scroll, don't know why. */
-			(void) vsprintf( buf, fmt, ap );
-			/* Newline will cause double scroll. */
-			p = buf+strlen(buf)-1;
-			if( *p == '\n' )
-				*p = '\0'; /* clobber newline */
-			(void) puts( buf );
-			/*(void) vprintf( fmt, ap );*/
-			(void) ScMvCursor( 1, SCROLL_BTM+1 );
-			(void) ScClrScrlReg();
-			}
-		else
-			{
-			(void) fprintf( stderr,
-					"%s %s %s %s!\n",
-					"This terminal has no delete line",
-					"or scrolling region capability,",
-					"please dump it somewhere and get",
-					"a real terminal"
-					);
-			exit( 1 );
-			}
-		(void) fflush( stdout );
-		}
-	else
-		{
-		(void) vfprintf( stderr, fmt, ap );
-		}
-	va_end( ap );
+{
+    va_list	ap;
+    va_start( ap, fmt );
+    if (tty && (errfile[0] == '\0' || ! strcmp( errfile, "/dev/tty" ))) {
+	clr_Tabs( HmTtyFd );
+	if( ScDL != NULL ) {
+	    (void) ScMvCursor( 1, SCROLL_TOP );
+	    (void) ScDeleteLn();
+	    (void) ScMvCursor( 1, SCROLL_BTM );
+	    (void) ScClrEOL();
+	    (void) vprintf( fmt, ap );
+	} else {
+	    if (ScSetScrlReg( SCROLL_TOP, SCROLL_BTM+1 )) {
+		char buf[LNBUFSZ];
+		char *p;
+		(void) ScMvCursor( 1, SCROLL_BTM+1 );
+		(void) ScClrEOL();
+		/* Work around for problem with vprintf(): it doesn't
+		   cause the screen to scroll, don't know why. */
+		(void) vsprintf( buf, fmt, ap );
+		/* Newline will cause double scroll. */
+		p = buf+strlen(buf)-1;
+		if( *p == '\n' )
+		    *p = '\0'; /* clobber newline */
+		(void) puts( buf );
+		/*(void) vprintf( fmt, ap );*/
+		(void) ScMvCursor( 1, SCROLL_BTM+1 );
+		(void) ScClrScrlReg();
+	    } else {
+		(void) fprintf( stderr,
+				"%s %s %s %s!\n",
+				"This terminal has no delete line",
+				"or scrolling region capability,",
+				"please dump it somewhere and get",
+				"a real terminal"
+		    );
+		exit( 1 );
+	    }
 	}
-
+	(void) fflush( stdout );
+    } else {
+	(void) vfprintf( stderr, fmt, ap );
+    }
+    va_end( ap );
+}
 
 /*
  * Local Variables:
