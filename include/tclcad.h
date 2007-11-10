1664846fa3aa5daed969142edc40ff45531e9dd4
Christopher Sean Morrison <brlcad@gmail.com>
2007-11-10 19:31:29 +0000

    make tclcad_tcl_library take an interp
    svn:revision:29311
    cvs:account:brlcad
    cvs:branch:trunk

--- include/tclcad.h
+++ include/tclcad.h
@@ -71,7 +71,7 @@ __BEGIN_DECLS
 
 TCLCAD_EXPORT TCLCAD_EXTERN(int tclcad_tk_setup, (Tcl_Interp *interp));
 TCLCAD_EXPORT TCLCAD_EXTERN(void tclcad_auto_path, (Tcl_Interp *interp));
-TCLCAD_EXPORT TCLCAD_EXTERN(void tclcad_tcl_library, (void));
+TCLCAD_EXPORT TCLCAD_EXTERN(void tclcad_tcl_library, (Tcl_Interp *interp));
 
 __END_DECLS
 
