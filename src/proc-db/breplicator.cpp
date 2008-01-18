3079fcd8e47aa1fc76634b658e49fed861145da8
Christopher Sean Morrison <brlcad@gmail.com>
2008-01-18 23:27:23 +0000

    create vertices, curves, and edges for the test cube
    svn:revision:29998
    svn:branch:trunk
    svn:account:brlcad

--- src/proc-db/breplicator.cpp
+++ src/proc-db/breplicator.cpp
@@ -45,12 +45,165 @@ generate_brep(int count, ON_3dPoint *points)
     
     /* make an arb8 */
 
+    // VERTICES
+
     for (int i=0; i<count; i++) {
-	bu_log("Adding vertex %d (%.2f, %.2f, %.2f)\n", i, points[i][0], points[i][1], points[i][2]);
-	ON_BrepVertex& v = brep->NewVertex(points[i]);
-	v.m_tolerance = VDIVIDE_TOL;
+	brep->NewVertex(points[i], VDIVIDE_TOL);
     }
+
+    // LEFT SEGMENTS
+
+    // 0
+    ON_Curve* segment01 = new ON_LineCurve(points[0], points[1]);
+    segment01->SetDomain(0.0, 1.0);
+    brep->m_C3.Append(segment01);
+
+    // 1
+    ON_Curve* segment12 = new ON_LineCurve(points[1], points[2]);
+    segment12->SetDomain(0.0, 1.0);
+    brep->m_C3.Append(segment12);
+
+    // 2
+    ON_Curve* segment23 = new ON_LineCurve(points[2], points[3]);
+    segment23->SetDomain(0.0, 1.0);
+    brep->m_C3.Append(segment23);
+
+    // 3
+    ON_Curve* segment30 = new ON_LineCurve(points[3], points[0]);
+    segment30->SetDomain(0.0, 1.0);
+    brep->m_C3.Append(segment30);
+
+    // RIGHT SEGMENTS (these are flipped)
+
+    // 4
+    ON_Curve* segment45 = new ON_LineCurve(points[4], points[5]);
+    segment45->SetDomain(0.0, 1.0);
+    brep->m_C3.Append(segment45);
+
+    // 5
+    ON_Curve* segment56 = new ON_LineCurve(points[5], points[6]);
+    segment56->SetDomain(0.0, 1.0);
+    brep->m_C3.Append(segment56);
+
+    // 6
+    ON_Curve* segment67 = new ON_LineCurve(points[6], points[7]);
+    segment67->SetDomain(0.0, 1.0);
+    brep->m_C3.Append(segment67);
+
+    // 7
+    ON_Curve* segment74 = new ON_LineCurve(points[7], points[4]);
+    segment74->SetDomain(0.0, 1.0);
+    brep->m_C3.Append(segment74);
+
+    // HORIZONTAL SEGMENTS
+
+    // 8
+    ON_Curve* segment04 = new ON_LineCurve(points[0], points[4]);
+    segment04->SetDomain(0.0, 1.0);
+    brep->m_C3.Append(segment04);
+
+    // 9
+    ON_Curve* segment51 = new ON_LineCurve(points[5], points[1]);
+    segment51->SetDomain(0.0, 1.0);
+    brep->m_C3.Append(segment51);
+
+    // 10
+    ON_Curve* segment26 = new ON_LineCurve(points[2], points[6]);
+    segment26->SetDomain(0.0, 1.0);
+    brep->m_C3.Append(segment26);
+
+    // 11
+    ON_Curve* segment73 = new ON_LineCurve(points[7], points[3]);
+    segment73->SetDomain(0.0, 1.0);
+    brep->m_C3.Append(segment73);
+
+    // SURFACES
+
+    ON_NurbsSurface* surf0123 = new ON_NurbsSurface(3 /*dimension*/, FALSE /*nonrational*/, 2 /*u*/, 2 /*v*/, 2 /*#u*/, 2 /*#v*/);
+    surf0123->SetKnot(0, 0, 0.0); surf0123->SetKnot(0, 1, 1.0); surf0123->SetKnot(1, 0, 0.0); surf0123->SetKnot(1, 1, 1.0);
+    surf0123->SetCV(0, 0, points[0]);
+    surf0123->SetCV(1, 0, points[1]);
+    surf0123->SetCV(1, 1, points[2]);
+    surf0123->SetCV(0, 1, points[3]);
+    brep->m_S.Append(surf0123);
     
+    ON_NurbsSurface* surf4765 = new ON_NurbsSurface(3 /*dimension*/, FALSE /*nonrational*/, 2 /*u*/, 2 /*v*/, 2 /*#u*/, 2 /*#v*/);
+    surf4765->SetKnot(0, 0, 0.0); surf4765->SetKnot(0, 1, 1.0); surf4765->SetKnot(1, 0, 0.0); surf4765->SetKnot(1, 1, 1.0);
+    surf4765->SetCV(0, 0, points[4]);
+    surf4765->SetCV(1, 0, points[7]);
+    surf4765->SetCV(1, 1, points[6]);
+    surf4765->SetCV(0, 1, points[5]);
+    brep->m_S.Append(surf4765);
+    
+    ON_NurbsSurface* surf0451 = new ON_NurbsSurface(3 /*dimension*/, FALSE /*nonrational*/, 2 /*u*/, 2 /*v*/, 2 /*#u*/, 2 /*#v*/);
+    surf0451->SetKnot(0, 0, 0.0); surf0451->SetKnot(0, 1, 1.0); surf0451->SetKnot(1, 0, 0.0); surf0451->SetKnot(1, 1, 1.0);
+    surf0451->SetCV(0, 0, points[0]);
+    surf0451->SetCV(1, 0, points[4]);
+    surf0451->SetCV(1, 1, points[5]);
+    surf0451->SetCV(0, 1, points[1]);
+    brep->m_S.Append(surf0451);
+    
+    ON_NurbsSurface* surf2673 = new ON_NurbsSurface(3 /*dimension*/, FALSE /*nonrational*/, 2 /*u*/, 2 /*v*/, 2 /*#u*/, 2 /*#v*/);
+    surf2673->SetKnot(0, 0, 0.0); surf2673->SetKnot(0, 1, 1.0); surf2673->SetKnot(1, 0, 0.0); surf2673->SetKnot(1, 1, 1.0);
+    surf2673->SetCV(0, 0, points[2]);
+    surf2673->SetCV(1, 0, points[6]);
+    surf2673->SetCV(1, 1, points[7]);
+    surf2673->SetCV(0, 1, points[3]);
+    brep->m_S.Append(surf2673);
+    
+    ON_NurbsSurface* surf1562 = new ON_NurbsSurface(3 /*dimension*/, FALSE /*nonrational*/, 2 /*u*/, 2 /*v*/, 2 /*#u*/, 2 /*#v*/);
+    surf1562->SetKnot(0, 0, 0.0); surf1562->SetKnot(0, 1, 1.0); surf1562->SetKnot(1, 0, 0.0); surf1562->SetKnot(1, 1, 1.0);
+    surf1562->SetCV(0, 0, points[1]);
+    surf1562->SetCV(1, 0, points[5]);
+    surf1562->SetCV(1, 1, points[6]);
+    surf1562->SetCV(0, 1, points[2]);
+    brep->m_S.Append(surf1562);
+    
+    ON_NurbsSurface* surf0374 = new ON_NurbsSurface(3 /*dimension*/, FALSE /*nonrational*/, 2 /*u*/, 2 /*v*/, 2 /*#u*/, 2 /*#v*/);
+    surf0374->SetKnot(0, 0, 0.0); surf0374->SetKnot(0, 1, 1.0); surf0374->SetKnot(1, 0, 0.0); surf0374->SetKnot(1, 1, 1.0);
+    surf0374->SetCV(0, 0, points[0]);
+    surf0374->SetCV(1, 0, points[3]);
+    surf0374->SetCV(1, 1, points[7]);
+    surf0374->SetCV(0, 1, points[4]);
+    brep->m_S.Append(surf0374);
+
+    // TRIM CURVES
+
+    ON_Curve* trim01 = new ON_LineCurve(ON_2dPoint(0,0), ON_2dPoint(1,0));
+    trim01->SetDomain(0.0, 1.0);
+    brep->m_C2.Append(trim01);
+
+    ON_Curve* trim12 = new ON_LineCurve(ON_2dPoint(1,0), ON_2dPoint(1,1));
+    trim12->SetDomain(0.0, 1.0);
+    brep->m_C2.Append(trim12);
+
+    ON_Curve* trim23 = new ON_LineCurve(ON_2dPoint(1,1), ON_2dPoint(0,1));
+    trim23->SetDomain(0.0, 1.0);
+    brep->m_C2.Append(trim23);
+
+    ON_Curve* trim30 = new ON_LineCurve(ON_2dPoint(0,1), ON_2dPoint(0,0));
+    trim30->SetDomain(0.0, 1.0);
+    brep->m_C2.Append(trim30);
+
+    // EDGES
+
+    brep->NewEdge(brep->m_V[0], brep->m_V[1], 0, NULL, VDIVIDE_TOL);
+    brep->NewEdge(brep->m_V[1], brep->m_V[2], 1, NULL, VDIVIDE_TOL);
+    brep->NewEdge(brep->m_V[2], brep->m_V[3], 2, NULL, VDIVIDE_TOL);
+    brep->NewEdge(brep->m_V[3], brep->m_V[0], 3, NULL, VDIVIDE_TOL);
+
+    brep->NewEdge(brep->m_V[4], brep->m_V[7], 7, NULL, VDIVIDE_TOL);
+    brep->NewEdge(brep->m_V[7], brep->m_V[6], 6, NULL, VDIVIDE_TOL);
+    brep->NewEdge(brep->m_V[6], brep->m_V[5], 5, NULL, VDIVIDE_TOL);
+    brep->NewEdge(brep->m_V[5], brep->m_V[4], 4, NULL, VDIVIDE_TOL);
+
+    brep->NewEdge(brep->m_V[5], brep->m_V[1], 9, NULL, VDIVIDE_TOL);
+    brep->NewEdge(brep->m_V[0], brep->m_V[4], 8, NULL, VDIVIDE_TOL);
+    brep->NewEdge(brep->m_V[2], brep->m_V[6], 10, NULL, VDIVIDE_TOL);
+    brep->NewEdge(brep->m_V[7], brep->m_V[3], 11, NULL, VDIVIDE_TOL);
+
+
+
     return brep;
 }
 
@@ -69,13 +222,13 @@ main(int argc, char *argv[])
 	/* left */
 	ON_3dPoint(0.0, 0.0, 0.0), // 0
 	ON_3dPoint(1.0, 0.0, 0.0), // 1
-	ON_3dPoint(1.0, 0.0, 2.0), // 2
-	ON_3dPoint(0.0, 0.0, 2.0), // 3
+	ON_3dPoint(1.0, 0.0, 2.5), // 2
+	ON_3dPoint(0.0, 0.0, 2.5), // 3
 	/* right */
 	ON_3dPoint(0.0, 1.0, 0.0), // 4
 	ON_3dPoint(1.0, 1.0, 0.0), // 5
-	ON_3dPoint(1.0, 1.0, 2.0), // 6
-	ON_3dPoint(0.0, 1.0, 2.0), // 7
+	ON_3dPoint(1.0, 1.0, 2.5), // 6
+	ON_3dPoint(0.0, 1.0, 2.5), // 7
     };
 
     brep = generate_brep(8, points);
