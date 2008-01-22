37bca099152703d458cea5d19c074d768d1fd3b4
Christopher Sean Morrison <brlcad@gmail.com>
2008-01-22 08:27:07 +0000

    bah, second mistake almost in a row... almost completely forgot to update the callers to bu_fchmod to remove the filename parameter (!).  time to call it a night before I start making other stupid mistakes at this late hour.
    svn:revision:30043
    svn:branch:trunk
    svn:account:brlcad

--- src/fb/fb-png.c
+++ src/fb/fb-png.c
@@ -129,7 +129,7 @@ get_args(int argc, register char **argv)
 				file_name );
 			return(0);
 		}
-		(void)bu_fchmod(file_name, outfp, 0444);
+		(void)bu_fchmod(outfp, 0444);
 	}
 
 	if ( argc > ++bu_optind )
