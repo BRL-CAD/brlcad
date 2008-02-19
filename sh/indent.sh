ad7962346c60d29efc90f1840753d2009c980107
Christopher Sean Morrison <brlcad@gmail.com>
2008-02-19 16:37:09 +0000

    ignore the m4 files.  gets them pretty much totally wrong.
    svn:revision:30363
    svn:branch:trunk
    svn:account:brlcad

--- sh/indent.sh
+++ sh/indent.sh
@@ -70,40 +70,42 @@ fi
 # arguments were provided.
 if [ "x$files" = "x" ] ; then
     files="`find . -type f -and \( \
-	    -name '*.c' -or \
-	    -name '*.h' -or \
-	    -name '*.tcl' -or \
-	    -name '*.tk' -or \
-	    -name '*.itcl' -or \
-	    -name '*.itk' -or \
-	    -name '*.pl' -or \
-	    -name '*.java' -or \
-	    -name '*.el' -or \
-	    -name '*.f' -or \
-	    -name '*.m4' -or \
-	    -name '*.sh' -or \
-	    -name '*.cc' -or \
-	    -name '*.cp' -or \
-	    -name '*.cxx' -or \
-	    -name '*.cpp' -or \
-	    -name '*.CPP' -or \
-	    -name '*.c++' -or \
-	    -name '*.C' -or \
-	    -name '*.hh' -or \
-	    -name '*.H' -or \
-	    -name '*.m' -or \
-	    -name '*.mm' -or \
-	    -name '*.M' \
-	    \) | \
-	    grep -v '/other/' | \
-	    grep -v '/doc/' | \
-	    grep -v '/\.svn' | \
-	    grep -v '/CVS' | \
-	    grep -v '/misc/enigma/' | \
-	    grep -v '/misc/vfont/' | \
-	    grep -v 'autom4te.cache' | \
-	    grep -v 'aclocal.m4' \
-	  `"
+	-name '*.c' -or \
+	-name '*.h' -or \
+	-name '*.tcl' -or \
+	-name '*.tk' -or \
+	-name '*.itcl' -or \
+	-name '*.itk' -or \
+	-name '*.pl' -or \
+	-name '*.java' -or \
+	-name '*.el' -or \
+	-name '*.f' -or \
+	-name '*.sh' -or \
+	-name '*.cc' -or \
+	-name '*.cp' -or \
+	-name '*.cxx' -or \
+	-name '*.cpp' -or \
+	-name '*.CPP' -or \
+	-name '*.c++' -or \
+	-name '*.C' -or \
+	-name '*.hh' -or \
+	-name '*.H' -or \
+	-name '*.m' -or \
+	-name '*.mm' -or \
+	-name '*.M' \
+	\) \
+	-not -regex '.*cache.*' \
+	-not -regex '.*config.*' \
+	-not -regex '.*Makefile.*' \
+	| \
+	grep -v '/.svn/' | \
+	grep -v '/CVS/' | \
+	grep -v '/doc/' | \
+	grep -v '/m4/' | \
+	grep -v '/misc/' | \
+	grep -v '/other/' | \
+	grep -v 'aclocal.m4' \
+	`"
 fi
 
 for file in $files ; do
