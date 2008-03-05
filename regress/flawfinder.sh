b9c5b1b621e2732263c7b7b0a74be8060c6a0ee2
Christopher Sean Morrison <brlcad@gmail.com>
2008-03-05 09:36:54 +0000

    add another coding check for the common.h inclusion ordering.  this makes sure common.h always comes first if/when listed in a file.  alas the script isn't tweaked to work right if there are spaces in the path name, but then there are probably other scripts like that in regress too
    svn:revision:30461
    svn:branch:trunk
    svn:account:brlcad

--- regress/flawfinder.sh
+++ regress/flawfinder.sh
@@ -44,6 +44,35 @@ else
     echo "-> bio.h check FAILED"
 fi
 
+echo "running common.h inclusion order check..."
+
+if test ! -f "$TOPSRC/include/common.h" ; then
+    echo "Unable to find include/common.h, aborting"
+    exit 1
+fi
+
+# make sure common.h is always included first
+COMMONFILES="`find $TOPSRC -type f \( -name \*.c -o -name \*.cpp -o -name \*.cxx -o -name \*.h -o -name \*.y -o -name \*.l \) -not -regex '.*src/other.*' -not -regex '.*~' -not -regex '.*\.log' -not -regex '.*Makefile.*' -not -regex '.*cache.*' -not -regex '.*\.svn.*' -exec grep -n -I -e '#[[:space:]]*include' {} /dev/null \; | grep '\"common.h\"' | sed 's/:.*//g'`"
+
+FOUND=
+for file in $COMMONFILES ; do 
+    if test -f "`echo $file | sed 's/\.c$/\.l/g'`" ; then 
+	continue
+    fi
+    MATCH="`grep '#[[:space:]]*include' $file /dev/null | head -n 1 | grep -v '\"common.h\"' | sed 's/:.*//g'`"
+    if test ! "x$MATCH" = "x" ; then
+	echo "Header (common.h) out of order: $MATCH"
+	FOUND=1
+    fi
+done
+
+if test "x$FOUND" = "x" ; then
+    echo "-> common.h check succeeded"
+else
+    echo "-> common.h check FAILED"
+fi
+
+
 exit $NUMBER_WRONG
 
 # Local Variables:
