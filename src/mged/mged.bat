9672a5fe67a6e9c8c3c02f9d5184da4da1152e02
Robert Parker <unacknowledged@users.noreply.github.com>
2008-04-04 20:26:47 +0000

    Mods to get rid of complaints about nul.
    svn:revision:30629
    svn:branch:trunk
    svn:account:bob1961

--- src/mged/mged.bat
+++ src/mged/mged.bat
@@ -1,5 +1,25 @@
 @ECHO OFF
 
+REM                      M G E D . B A T
+REM  BRL-CAD
+REM 
+REM  Copyright (c) 2006-2008 United States Government as represented by
+REM  the U.S. Army Research Laboratory.
+REM 
+REM  This library is free software; you can redistribute it and/or
+REM  modify it under the terms of the GNU Lesser General Public License
+REM  version 2.1 as published by the Free Software Foundation.
+REM 
+REM  This library is distributed in the hope that it will be useful, but
+REM  WITHOUT ANY WARRANTY; without even the implied warranty of
+REM  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+REM  Lesser General Public License for more details.
+REM 
+REM  You should have received a copy of the GNU Lesser General Public
+REM  License along with this file; see the file named COPYING for more
+REM  information.
+REM 
+
 REM	Author:		Bob Parker
 REM	Company:	Survice Engineering
 
@@ -13,9 +33,9 @@ CD %SAVE_CD%
 SET WEB_BROWSER=C:\Program Files\Internet Explorer\IEXPLORE.EXE
 
 IF "%1"=="-g" (
-    START /B mged.exe %2
+    START /B mged %2
 ) ELSE (
-    START /B mged.exe 2>&1 nul
+    START /B mged %1
 )
 
 CLS
