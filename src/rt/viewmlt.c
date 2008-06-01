6198c1da0316cd9f457b40e93c37373ad6a217ec
André Castelo <andrecastelo@gmail.com>
2008-06-01 15:41:51 +0000

    Added viewmlt.c to src/rt/ and made changes to src/rt/Makefile.am to support that.
    svn:revision:31251
    svn:branch:trunk
    svn:account:andrecastelo

---
+++ src/rt/viewmlt.c
@@ -0,0 +1,38 @@
+/*                      M L T _ M A I N . C
+ * BRL-CAD
+ *
+ * Copyright (c) 2008 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file mlt_main.c
+ *
+ * Brief description
+ *
+ */
+
+#include "mlt_def.h"
+
+int main() { };
+
+/*
+ * Local Variables:
+ * tab-width: 8
+ * mode: C
+ * indent-tabs-mode: t
+ * c-file-style: "stroustrup"
+ * End:
+ * ex: shiftwidth=4 tabstop=8
+ */
