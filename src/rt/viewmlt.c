433e3880ff6cc1767e922ca2b0a0c4001c019508
André Castelo <andrecastelo@gmail.com>
2008-06-04 02:18:55 +0000

    Merged definitions from mlt_defs.h into viewmlt.c. Added functions to make viewmlt.c a rt compatible application. Added missing files (worker.c, do.c, main.c and opt.c) to win32-msvc9/rtmlt/rtmlt.vcproj. It should build correctly now.
    svn:revision:31288
    svn:branch:trunk
    svn:account:andrecastelo

--- src/rt/viewmlt.c
+++ src/rt/viewmlt.c
@@ -19,10 +19,170 @@
  */
 /** @file viewmlt.c
  *
- * Brief description
+ * Implementation of the Metropolis Light Transport algorithm. 
  *
  */
 
+#include "common.h"
+
+#include <stdio.h>
+
+#include "vmath.h"
+#include "raytrace.h"
+
+#include "rtprivate.h"
+
+int use_air = 0;
+
+const char title[] = "Metropolis Light Transport renderer";
+const char usage[] = "\
+Usage:  rtmlt [options] model.g objects... > file.pix\n\
+Options:\n\
+ -s #		Grid size in pixels, default 512\n\
+ -a Az		Azimuth in degrees\n\
+ -e Elev	Elevation in degrees\n\
+ -M		Read matrix, cmds on stdin\n\
+ -o file.pix	Output file name\n\
+ -x #		Set librt debug flags\n\
+";
+
+int	rayhit(register struct application *ap, struct partition *PartHeadp);
+int	raymiss(register struct application *ap);
+
+struct bu_structparse view_parse[] = {
+    "",	0, (char *)0,	0,	BU_STRUCTPARSE_FUNC_NULL
+};
+
+struct point_list {
+    struct bu_list l;
+    point_t pt_cell;
+};
+
+struct mlt_path {
+    struct bu_list l;
+    struct point_list * points;
+};
+
+struct mlt_app {
+    struct application * ap;
+    struct mlt_path * path_list;
+    point_t eye;
+    point_t camera;
+    /* ... */
+};
+
+int
+mlt_hit(register struct application *ap, struct partition *PartHeadp);
+
+int
+mlt_miss(register struct application * ap);
+
+/*
+ *  			V I E W _ I N I T
+ *
+ *  Called by main() at the start of a run.
+ *  Returns 1 if framebuffer should be opened, else 0.
+ */
+view_init(register struct application *ap, char *file, char *obj, int minus_o)
+{
+    return(0);		/* no framebuffer needed */
+}
+
+/*
+ *			V I E W _ 2 I N I T
+ *
+ *  The beginning of a frame.
+ *  Called by do_frame() just before raytracing starts.
+ */
+void
+view_2init(struct application *ap)
+{
+    ap->a_hit = mlt_hit;
+    ap->a_miss = mlt_miss;
+    ap->a_onehit = 1;
+}
+
+/*
+ *			V I E W _ P I X E L
+ *
+ *  Called by worker() after the end of proccessing for each pixel.
+ */
+void
+view_pixel(register struct application *ap)
+{
+}
+
+/*
+ *			V I E W _ E O L
+ *
+ *  Called by worker() at the end of each line.  Depricated.
+ *  Any end-of-line processing should be done in view_pixel().
+ */
+void
+view_eol(register struct application *ap)
+{
+}
+
+/*
+ *			V I E W _ E N D
+ *
+ *  Called in do_frame() at the end of a frame,
+ *  just after raytracing completes.
+ */
+void
+view_end(register struct application *ap)
+{
+}
+
+/*
+ *			V I E W _ S E T U P
+ *
+ *  Called by do_prep(), just before rt_prep() is called, in do.c
+ *  This allows the lighting model to get set up for this frame,
+ *  e.g., generate lights, associate materials routines, etc.
+ */
+void
+view_setup(struct rt_i *rtip)
+{
+}
+
+/*
+ *			V I E W _ C L E A N U P
+ *
+ *  Called by "clean" command, just before rt_clean() is called, in do.c
+ */
+void
+view_cleanup(struct rt_i *rtip)
+{
+}
+
+/*
+ *			R A Y H I T
+ *
+ *  Called via a_hit linkage from rt_shootray() when ray hits.
+ */
+int
+mlt_hit(register struct application *ap, struct partition *PartHeadp)
+{
+    bu_log("hit: 0x%x\n", ap->a_resource);
+
+    return(1);	/* report hit to main routine */
+}
+
+/*
+ *			R A Y M I S S
+ *
+ *  Called via a_miss linkage from rt_shootray() when ray misses.
+ */
+int
+mlt_miss(register struct application *ap)
+{
+    bu_log("miss: 0x%x\n", ap->a_resource);
+
+    return(0);
+}
+
+void application_init (void) {}
 
 /*
  * Local Variables:
