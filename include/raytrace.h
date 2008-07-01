09efddf64c7b59c967e43db39a6744cffa2998fd
Christopher Sean Morrison <brlcad@gmail.com>
2008-07-01 23:06:25 +0000

    make rt_check_curve const since it doesn't modify anything, make sure it considers empty sketches invalid too, and remove the g_ references to the former primitive file names.
    svn:revision:31701
    svn:branch:trunk
    svn:account:brlcad

--- include/raytrace.h
+++ include/raytrace.h
@@ -1277,7 +1277,7 @@ struct rt_piecelist  {
 };
 #define RT_CK_PIECELIST(_p)	BU_CKMAG(_p, RT_PIECELIST_MAGIC, "struct rt_piecelist")
 
-/* Used to set globals declared in g_bot.c */
+/* Used to set globals declared in bot.c */
 #define RT_DEFAULT_MINPIECES		32
 #define RT_DEFAULT_TRIS_PER_PIECE	4
 
@@ -2865,12 +2865,12 @@ RT_EXPORT BU_EXTERN(union tree *db_mkgift_tree,
 		     int			subtreecount,
 		     struct resource		*resp));
 
-/* g_tgc.c */
+/* tgc.c */
 RT_EXPORT BU_EXTERN(void rt_pt_sort,
 		    (register fastf_t t[],
 		     int npts));
 
-/* g_ell.c */
+/* ell.c */
 RT_EXPORT BU_EXTERN(void rt_ell_16pts,
 		    (register fastf_t *ov,
 		     register fastf_t *V,
@@ -2980,7 +2980,7 @@ RT_EXPORT BU_EXTERN(int rt_comb_import5,
 		     struct resource         *resp,
 		     const int		minor_type));
 
-/* g_extrude.c */
+/* extrude.c */
 RT_EXPORT BU_EXTERN(int rt_extrude_import5,
 		    (struct rt_db_internal	*ip,
 		     const struct bu_external	*ep,
@@ -3317,7 +3317,7 @@ RT_EXPORT BU_EXTERN(struct directory *rt_mirror,
   struct db_traverse *dtp));
 */
 
-/* g_arb.c */
+/* arb.c */
 RT_EXPORT BU_EXTERN(int rt_arb_get_cgtype,
 		    ());		/* needs rt_arb_internal for arg list */
 RT_EXPORT BU_EXTERN(int rt_arb_std_type,
@@ -3367,7 +3367,7 @@ RT_EXPORT extern short earb6[10][18];
 RT_EXPORT extern short earb5[9][18];
 RT_EXPORT extern short earb4[5][18];
 
-/* g_epa.c */
+/* epa.c */
 RT_EXPORT BU_EXTERN(void rt_ell,
 		    (fastf_t *ov,
 		     const fastf_t *V,
@@ -3375,7 +3375,7 @@ RT_EXPORT BU_EXTERN(void rt_ell,
 		     const fastf_t *B,
 		     int sides));
 
-/* g_pipe.c */
+/* pipe.c */
 RT_EXPORT BU_EXTERN(void rt_vls_pipept,
 		    (struct bu_vls *vp,
 		     int seg_no,
@@ -3386,7 +3386,7 @@ RT_EXPORT BU_EXTERN(void rt_pipept_print,
 RT_EXPORT BU_EXTERN(int rt_pipe_ck,
 		    (const struct bu_list *headp));
 
-/* g_metaball.c */
+/* metaball.c */
 struct rt_metaball_internal;
 RT_EXPORT BU_EXTERN(void rt_vls_metaballpt,
 		    (struct bu_vls *vp,
@@ -3447,12 +3447,12 @@ RT_EXPORT BU_EXTERN(struct bu_list *rt_vlblock_find,
 		     int g,
 		     int b));
 
-/* g_ars.c */
+/* ars.c */
 RT_EXPORT BU_EXTERN(void rt_hitsort,
 		    (struct hit h[],
 		     int nh));
 
-/* g_pg.c */
+/* pg.c */
 RT_EXPORT BU_EXTERN(int rt_pg_to_bot,
 		    (struct rt_db_internal *ip,
 		     const struct bn_tol *tol,
@@ -3468,12 +3468,12 @@ RT_EXPORT BU_EXTERN(int rt_pg_plot_poly,
 		     const struct rt_tess_tol	*ttol,
 		     const struct bn_tol	*tol));
 
-/* g_hf.c */
+/* hf.c */
 RT_EXPORT BU_EXTERN(int rt_hf_to_dsp,
 		    (struct rt_db_internal *db_intern,
 		     struct resource *resp));
 
-/* g_dsp.c */
+/* dsp.c */
 RT_EXPORT BU_EXTERN(int dsp_pos,
 		    (point_t out,
 		     struct soltab *stp,
@@ -3687,7 +3687,7 @@ RT_EXPORT BU_EXTERN(void rt_label_vlist_verts,
 		     double mm2local));
 
 #ifdef __RTGEOM_H__
-/* g_sketch.c */
+/* sketch.c */
 RT_EXPORT BU_EXTERN(void rt_sketch_ifree,
 		    (struct rt_db_internal	*ip));
 RT_EXPORT BU_EXTERN(int curve_to_vlist,
@@ -3700,8 +3700,8 @@ RT_EXPORT BU_EXTERN(int curve_to_vlist,
 		     struct curve		*crv));
 
 RT_EXPORT BU_EXTERN(int rt_check_curve,
-		    (struct curve *crv,
-		     struct rt_sketch_internal *skt,
+		    (const struct curve *crv,
+		     const struct rt_sketch_internal *skt,
 		     int noisy));
 
 RT_EXPORT BU_EXTERN(void rt_curve_reverse_segment,
@@ -3713,10 +3713,6 @@ RT_EXPORT BU_EXTERN(void rt_copy_curve,
 		    (struct curve *crv_out,
 		     const struct curve *crv_in));
 
-RT_EXPORT BU_EXTERN(int rt_check_curve,
-		    (struct curve *crv,
-		     struct rt_sketch_internal *skt,
-		     int noisy));
 RT_EXPORT BU_EXTERN(void rt_curve_free,
 		    (struct curve *crv));
 RT_EXPORT BU_EXTERN(void rt_copy_curve,
@@ -4654,7 +4650,7 @@ RT_EXPORT BU_EXTERN(int nmg_edge_collapse,
 		     const fastf_t tol_coll,
 		     const fastf_t min_angle));
 
-/* g_bot.c */
+/* bot.c */
 RT_EXPORT BU_EXTERN(int rt_bot_edge_in_list,
 		    (const int v1,
 		     const int v2,
@@ -4725,11 +4721,11 @@ RT_EXPORT BU_EXTERN(char *nmg_shell_manifolds,
 RT_EXPORT BU_EXTERN(char *nmg_manifolds,
 		    (struct model *m));
 
-/* g_nmg.c */
+/* nmg.c */
 RT_EXPORT BU_EXTERN(int nmg_ray_segs,
 		    (struct ray_data	*rd));
 
-/* g_torus.c */
+/* torus.c */
 RT_EXPORT BU_EXTERN(int rt_num_circular_segments,
 		    (double maxerr,
 		     double radius));
@@ -5882,15 +5878,15 @@ RT_EXPORT BU_EXTERN(int rt_mk_binunif,
 
 /* XXX do not rely on *_ifree() functions, why are these needed? */
 #ifdef _RT_DECL_IFREE
-/* defined in g_dsp.c */
+/* defined in dsp.c */
 RT_EXPORT BU_EXTERN(void rt_dsp_ifree,
 		    (struct rt_db_internal *ip));
 
-/* defined in g_ebm.c */
+/* defined in ebm.c */
 RT_EXPORT BU_EXTERN(void rt_ebm_ifree,
 		    (struct rt_db_internal *ip));
 
-/* defined in g_vol.c */
+/* defined in vol.c */
 RT_EXPORT BU_EXTERN(void rt_vol_ifree,
 		    (struct rt_db_internal *ip));
 #endif
@@ -5901,10 +5897,10 @@ RT_EXPORT BU_EXTERN(void rt_binunif_free,
 RT_EXPORT BU_EXTERN(void rt_binunif_dump,
 		    (struct rt_binunif_internal *bip));
 
-/* defined in g_cline.c */
+/* defined in cline.c */
 RT_EXPORT extern fastf_t rt_cline_radius;
 
-/* defined in g_bot.c */
+/* defined in bot.c */
 RT_EXPORT extern int rt_bot_minpieces;
 RT_EXPORT extern int rt_bot_tri_per_piece;
 #ifdef __RTGEOM_H__
