e06fd6cdf7bc67567929e20bdff470d636ea1319
Robert Parker <unacknowledged@users.noreply.github.com>
2008-07-03 18:52:01 +0000

    mods to f_ill (.i.e. nargv not being incremented)
    svn:revision:31737
    svn:branch:trunk
    svn:account:bob1961

--- src/mged/chgview.c
+++ src/mged/chgview.c
@@ -1640,6 +1640,7 @@ f_ill(ClientData clientData, Tcl_Interp *interp, int argc, const char *argv[])
     int early_out = 0;
 
     char **nargv;
+    char **orig_nargv;
     struct bu_vls vlsargv;
 
     CHECK_DBI_NULL;
@@ -1657,11 +1658,12 @@ f_ill(ClientData clientData, Tcl_Interp *interp, int argc, const char *argv[])
     bu_vls_init(&vlsargv);
     bu_vls_from_argv(&vlsargv, argc, argv);
     nargv = bu_calloc(argc+1, sizeof(char *), "calloc f_ill nargv");
+    orig_nargv = nargv;
     c = bu_argv_from_string(nargv, argc, bu_vls_addr(&vlsargv));
     if (c != argc) {
 	Tcl_AppendResult(interp, "ERROR: unable to processes command arguments for f_ill()\n", (char*)NULL);
 	
-	bu_free(nargv, "free f_ill nargv");
+	bu_free(orig_nargv, "free f_ill nargv");
 	bu_vls_free(&vlsargv);
 
 	return TCL_ERROR;
@@ -1706,7 +1708,7 @@ f_ill(ClientData clientData, Tcl_Interp *interp, int argc, const char *argv[])
     }
 
     argc -= (bu_optind - 1);
-    argv += (bu_optind - 1);
+    nargv += (bu_optind - 1);
 
     if (argc != 2) {
 	struct bu_vls vls;
@@ -1716,7 +1718,7 @@ f_ill(ClientData clientData, Tcl_Interp *interp, int argc, const char *argv[])
 	Tcl_Eval(interp, bu_vls_addr(&vls));
 	bu_vls_free(&vls);
 
-	bu_free(nargv, "free f_ill nargv");
+	bu_free(orig_nargv, "free f_ill nargv");
 	bu_vls_free(&vlsargv);
 
 	return TCL_ERROR;
@@ -1732,14 +1734,16 @@ f_ill(ClientData clientData, Tcl_Interp *interp, int argc, const char *argv[])
 	;
 
     if (nm_pieces == 0) {
-	Tcl_AppendResult(interp, "Bad solid path: '", argv[1], "'\n", (char *)NULL);
+	Tcl_AppendResult(interp, "Bad solid path: '", nargv[1], "'\n", (char *)NULL);
 	goto bail_out;
     }
 
     basename = path_piece[nm_pieces - 1];
 
-    if ( (dp = db_lookup( dbip,  basename, LOOKUP_NOISY )) == DIR_NULL )
+    if ( (dp = db_lookup( dbip,  basename, LOOKUP_NOISY )) == DIR_NULL ) {
+	Tcl_AppendResult(interp, "db_lookup failed for '", basename, "'\n", (char *)NULL);
 	goto bail_out;
+    }
 
     nmatch = 0;
     if (!(dp -> d_flags & DIR_SOLID)) {
@@ -1774,13 +1778,13 @@ f_ill(ClientData clientData, Tcl_Interp *interp, int argc, const char *argv[])
     }
 
     if (nmatch == 0) {
-	Tcl_AppendResult(interp, argv[1], " not being displayed\n", (char *)NULL);
+	Tcl_AppendResult(interp, nargv[1], " not being displayed\n", (char *)NULL);
 	goto bail_out;
     }
 
     /* preserve same old behavior */
     if (nmatch > 1 && ri == 0) {
-	Tcl_AppendResult(interp, argv[1], " multiply referenced\n", (char *)NULL);
+	Tcl_AppendResult(interp, nargv[1], " multiply referenced\n", (char *)NULL);
 	goto bail_out;
     } else if (ri != 0 && ri != nmatch) {
 	Tcl_AppendResult(interp,
@@ -1811,7 +1815,7 @@ f_ill(ClientData clientData, Tcl_Interp *interp, int argc, const char *argv[])
 	bu_free((genptr_t) path_piece, "f_ill: char **");
     }
 
-    bu_free(nargv, "free f_ill nargv");
+    bu_free(orig_nargv, "free f_ill nargv");
     bu_vls_free(&vlsargv);
 
     return TCL_OK;
@@ -1837,7 +1841,7 @@ f_ill(ClientData clientData, Tcl_Interp *interp, int argc, const char *argv[])
 	bu_free((genptr_t) path_piece, "f_ill: char **");
     }
 
-    bu_free(nargv, "free f_ill nargv");
+    bu_free(orig_nargv, "free f_ill nargv");
     bu_vls_free(&vlsargv);
 
     return TCL_ERROR;
