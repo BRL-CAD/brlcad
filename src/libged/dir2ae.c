7aac37f000b6637fb9c0c9928143f4c4a68995ed
Robert Parker <unacknowledged@users.noreply.github.com>
2008-07-10 20:04:12 +0000

    Added ae2dir and dir2ae commands to libtclcad's ged object.
    svn:revision:31785
    svn:branch:trunk
    svn:account:bob1961

---
+++ src/libged/dir2ae.c
@@ -0,0 +1,93 @@
+/*                         D I R 2 A E. C
+ * BRL-CAD
+ *
+ * Copyright (c) 2008 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file dir2ae.c
+ *
+ * The dir2ae command.
+ *
+ */
+
+#include "common.h"
+
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+#include "ged_private.h"
+
+int
+ged_dir2ae(struct ged *gedp, int argc, const char *argv[])
+{
+    fastf_t az, el;
+    vect_t dir;
+    int iflag;
+    static const char *usage = "[-i] x y z";
+
+    GED_CHECK_DATABASE_OPEN(gedp, BRLCAD_ERROR);
+
+    /* initialize result */
+    bu_vls_trunc(&gedp->ged_result_str, 0);
+    gedp->ged_result = GED_RESULT_NULL;
+    gedp->ged_result_flags = 0;
+
+    /* must be wanting help */
+    if (argc == 1) {
+	gedp->ged_result_flags |= GED_RESULT_FLAGS_HELP_BIT;
+	bu_vls_printf(&gedp->ged_result_str, "Usage: %s %s", argv[0], usage);
+	return BRLCAD_OK;
+    }
+
+    if (argv[1][0] == '-' && argv[1][1] == 'i' && argv[1][2] == '\0') {
+	iflag = 1;
+	--argc;
+	++argv;
+    } else
+	iflag = 0;
+
+    if (argc != 4) {
+	bu_vls_printf(&gedp->ged_result_str, "Usage: %s %s", argv[0], usage);
+	return BRLCAD_ERROR;
+    }
+
+    if (sscanf(argv[1], "%lf", &dir[X]) != 1 ||
+	sscanf(argv[2], "%lf", &dir[Y]) != 1 ||
+	sscanf(argv[3], "%lf", &dir[Z]) != 1) {
+	bu_vls_printf(&gedp->ged_result_str, "Usage: %s %s", argv[0], usage);
+	return BRLCAD_ERROR;
+    }
+
+    AZEL_FROM_V3DIR(az, el, dir);
+
+    if (iflag)
+	VSCALE(dir, dir, -1);
+
+    bu_vls_printf(&gedp->ged_result_str, "%lf %lf", az, el);
+
+    return BRLCAD_OK;
+}
+
+/*
+ * Local Variables:
+ * tab-width: 8
+ * mode: C
+ * indent-tabs-mode: t
+ * c-file-style: "stroustrup"
+ * End:
+ * ex: shiftwidth=4 tabstop=8
+ */
