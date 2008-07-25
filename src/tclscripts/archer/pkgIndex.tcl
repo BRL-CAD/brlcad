a9df71c7609fb7b6729d4d95d20d3a8e327eb7c1
Christopher Sean Morrison <brlcad@gmail.com>
2008-07-25 00:36:14 +0000

    revert the inadvertent index mods.  someone(tm) should make it so this doesn't keep happening.
    svn:revision:31939
    svn:branch:trunk
    svn:account:brlcad

--- src/tclscripts/archer/pkgIndex.tcl
+++ src/tclscripts/archer/pkgIndex.tcl
@@ -9,3 +9,4 @@
 # full path name of this file's directory.
 
 package ifneeded ArcherCore 1.0 [list source [file join $dir ArcherCore.tcl]]
+package ifneeded Archer 1.0 [list source [file join $dir Archer.tcl]]
