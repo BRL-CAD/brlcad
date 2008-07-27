357b1e9cc9a16a1224a41527cb2b9b79bc539701
John Anderson <daytonajra@gmail.com>
2008-07-27 03:33:42 +0000

    Improved memory cleanup on shutdown. limitations of rt_clean_resource() prevent complete cleanup.
    svn:revision:31971
    svn:branch:trunk
    svn:account:johnranderson

--- src/librtserver/rtserver.c
+++ src/librtserver/rtserver.c
@@ -91,10 +91,10 @@ static pthread_mutex_t apps_mutex = PTHREAD_MUTEX_INITIALIZER;
         } else { \
                 int app_no = app_count++; \
                 pthread_mutex_unlock( &apps_mutex ); \
-                _p = (struct application *)bu_malloc( sizeof(struct application), "apps"); \
+                _p = (struct application *)bu_malloc( sizeof(struct application), "struct application"); \
                 RT_APPLICATION_INIT(_p); \
                 _p->a_rt_i = myrtip; \
-                _p->a_uptr = (struct bu_vlb *)bu_calloc( sizeof(struct bu_vlb), 1, "bu_vlb"); \
+                _p->a_uptr = (struct bu_vlb *)bu_calloc( sizeof(struct bu_vlb), 1, "bu_vlb GET_APPLICATION"); \
                 bu_vlb_init(_p->a_uptr); \
                 _p->a_resource = (struct resource *)bu_calloc( sizeof(struct resource), 1, "resource"); \
                 rt_init_resource( _p->a_resource, app_no, _p->a_rt_i ); \
@@ -333,7 +333,7 @@ rts_clean( int sessionid)
 
 	    if ( rtsrtip->rtrti_rtip ) {
 		if ( isLastUseOfRti( rtsrtip->rtrti_rtip, i ) ) {
-		    rt_clean( rtsrtip->rtrti_rtip );
+		    rt_free_rti( rtsrtip->rtrti_rtip );
 		    rtsrtip->rtrti_rtip = NULL;
 		}
 	    }
@@ -341,7 +341,10 @@ rts_clean( int sessionid)
                 Tcl_DeleteHashTable( rtsrtip->rtrti_region_names );
                 bu_free(rtsrtip->rtrti_region_names, "region names hash table");
             }
+            
+            bu_free(rtsrtip, "rtserver_rti");
 	}
+        bu_free(rts_geometry[sessionid]->rts_rtis, "rtserver_rti *");
 	if ( rts_geometry[sessionid]->rts_comp_names ) {
 	    Tcl_DeleteHashTable( rts_geometry[sessionid]->rts_comp_names );
             bu_free(rts_geometry[sessionid]->rts_comp_names, "component names hash table");
@@ -416,7 +419,9 @@ rts_load_geometry( char *filename, int num_trees, char **objects )
 	rts_geometry = NULL;
     }
     
-    bu_ptbl_init( &apps, 8, "application structure list" );
+    if( !BU_PTBL_TEST(&apps) ) {
+        bu_ptbl_init( &apps, 8, "application structure list" );
+    }
 
     if ( hash_table_exists ) {
 	Tcl_DeleteHashTable( &name_tbl );
@@ -433,7 +438,7 @@ rts_load_geometry( char *filename, int num_trees, char **objects )
 	sessionid = 0;
 	rts_geometry = (struct rtserver_geometry **)bu_calloc( num_geometries,
 							       sizeof( struct rts_geometry *),
-							       "rts_geometry" );
+							       "rts_geometry *" );
     }
 
     /* open the BRL-CAD model */
@@ -463,7 +468,7 @@ rts_load_geometry( char *filename, int num_trees, char **objects )
     /* just one RT instance */
     rts_geometry[sessionid]->rts_number_of_rtis = 1;
     rts_geometry[sessionid]->rts_rtis = (struct rtserver_rti **)bu_malloc( sizeof( struct rtserver_rti *),
-            "rtserver_rti" );
+            "rtserver_rti *" );
     rts_geometry[sessionid]->rts_rtis[0] = (struct rtserver_rti *)bu_calloc( 1,
             sizeof( struct rtserver_rti ),
             "rtserver_rti" );
@@ -472,20 +477,6 @@ rts_load_geometry( char *filename, int num_trees, char **objects )
     if ( verbose ) {
         fprintf( stderr, "num_trees = %d\n", num_trees );
     }
-
-    /* malloc some memory for the rtserver geometry structure (bu_calloc zeros the memory) */
-    rts_geometry[sessionid] = (struct rtserver_geometry *)bu_calloc( 1,
-            sizeof( struct rtserver_geometry ),
-            "rtserver geometry" );
-
-    /* just one RT instance */
-    rts_geometry[sessionid]->rts_number_of_rtis = 1;
-    rts_geometry[sessionid]->rts_rtis = (struct rtserver_rti **)bu_malloc( sizeof( struct rtserver_rti *),
-            "rtserver_rti" );
-    rts_geometry[sessionid]->rts_rtis[0] = (struct rtserver_rti *)bu_calloc( 1,
-            sizeof( struct rtserver_rti ),
-            "rtserver_rti" );
-    rts_geometry[sessionid]->rts_rtis[0]->rtrti_rtip = rtip;
 
     /* initialize our overall bounding box */
     VSETALL( rts_geometry[sessionid]->rts_mdl_min, MAX_FASTF );
@@ -1961,15 +1952,13 @@ JNIEXPORT jobjectArray JNICALL Java_mil_army_arl_brlcadservice_impl_BrlcadJNIWra
     return jNameArray;
 }
 
-JNIEXPORT void JNICALL Java_mil_army_arl_brlcadservice_impl_BrlcadJNIWrapper_shutdownNative(JNIEnv *env, jobject obj)
+void rts_shutdown()
 {
     struct application *ap;
+    struct resource *resp;
+    int i;
     
     bu_log( "Shutting down...");
-    for(BU_PTBL_FOR(ap, (struct application *), &apps)) {
-        bu_free(ap, "struct application");
-    }
-    bu_ptbl_free(&apps);
     rts_clean(0);
     
     if ( hash_table_exists ) {
@@ -1989,4 +1978,43 @@ JNIEXPORT void JNICALL Java_mil_army_arl_brlcadservice_impl_BrlcadJNIWrapper_shu
         bu_free(title, "title");
         title = NULL;
     }
+    
+    for( i=0 ; i<BU_PTBL_LEN(&apps) ; i++ ) {
+        struct bu_vlb *vlb;
+        
+        ap = (struct application *)BU_PTBL_GET( &apps, i);
+        
+        vlb = (struct bu_vlb *)ap->a_uptr;
+        if(vlb != NULL) {
+            bu_vlb_free(vlb);
+            bu_free(vlb, "vlb");
+        }
+
+        if( ap->a_resource != NULL ) {
+            rt_clean_resource(NULL, ap->a_resource);
+            bu_free(ap->a_resource, "resource");
+        }
+
+        bu_free(ap, "struct application");
+    }
+    bu_ptbl_free(&apps);
+    memset(&apps, 0, sizeof( struct bu_ptbl));
+    
+    resp = &rt_uniresource;
+    rt_clean_resource(NULL, resp);
+    if ( BU_LIST_IS_INITIALIZED( &resp->re_directory_blocks.l ) )  {
+        struct directory **dpp;
+        BU_CK_PTBL( &resp->re_directory_blocks );
+        for ( BU_PTBL_FOR( dpp, (struct directory **), &resp->re_directory_blocks ) )  {
+            RT_CK_DIR(*dpp);	/* Head of block will be a valid seg */
+            bu_free( (genptr_t)(*dpp), "struct directory block" );
+        }
+        bu_ptbl_free( &resp->re_directory_blocks );
+    }
+
+}
+
+JNIEXPORT void JNICALL Java_mil_army_arl_brlcadservice_impl_BrlcadJNIWrapper_shutdownNative(JNIEnv *env, jobject obj)
+{
+    rts_shutdown();
 }
