450cb38d32be290c9d11305e2cf2f29d0742bb35
Christopher Sean Morrison <brlcad@gmail.com>
2008-07-27 01:15:28 +0000

    since it's happened twice now, add a basic sanity check for an infinite loop when trying to find a new object name during dbconcat
    svn:revision:31968
    svn:branch:trunk
    svn:account:brlcad

--- src/libged/wdb_obj.c
+++ src/libged/wdb_obj.c
@@ -3147,11 +3147,13 @@ get_new_name(const char *name,
 	     struct concat_data *cc_data)
 {
     struct bu_vls new_name;
+    struct bu_vls prev_name;
     Tcl_HashEntry *ptr = NULL;
     char *aname = NULL;
     char *ret_name = NULL;
-    int new=0;
-    long num=0;
+    int new_entry = 0;
+    long num = 0;
+    long repeat = 0;
 
     RT_CK_DBI(dbip);
     BU_ASSERT(name_tbl);
@@ -3163,13 +3165,14 @@ get_new_name(const char *name,
 	name = "UNKNOWN";
     }
 
-    ptr = Tcl_CreateHashEntry( name_tbl, name, &new );
+    ptr = Tcl_CreateHashEntry( name_tbl, name, &new_entry );
 
-    if ( !new ) {
+    if ( !new_entry ) {
 	return( (char *)Tcl_GetHashValue( ptr ) );
     }
 
-    bu_vls_init( &new_name );
+    bu_vls_init(&new_name);
+    bu_vls_init(&prev_name);
 
     do {
 	/* iterate until we find an object name that is not in
@@ -3212,7 +3215,7 @@ get_new_name(const char *name,
 	    bu_vls_printf( &new_name, "%ld_", num );
 	    bu_vls_strcat( &new_name, name );
 	} else {
-	    /* no custom suffix/prefix specified, use prefix */
+	    /* no custom suffix/prefix specified, use suffix */
 	    if (num > 0) {
 		bu_vls_printf( &new_name, "_%ld", num );
 	    }
@@ -3229,6 +3232,18 @@ get_new_name(const char *name,
 	aname = bu_vls_addr( &new_name );
 
 	num++;
+
+	/* basic infinite loop sanity check */
+	if (bu_vls_strcmp(&new_name, &prev_name) == 0) {
+	    if (repeat > 100) {
+		bu_log("ERROR: we seem to be stuck in an infinite loop generating a new name (%s)\n", bu_vls_addr(&new_name));
+		return NULL;
+	    }
+	    repeat++;
+	} else {
+	    bu_vls_trunc(&prev_name, 0);
+	    bu_vls_vlscat(&prev_name, &new_name);
+	}
 
     } while (db_lookup( dbip, aname, LOOKUP_QUIET ) != DIR_NULL ||
 	     Tcl_FindHashEntry( used_names_tbl, aname ) != NULL);
@@ -3247,7 +3262,7 @@ get_new_name(const char *name,
     /* we should now have a unique name.  store it in the hash */
     ret_name = bu_vls_strgrab( &new_name );
     Tcl_SetHashValue( ptr, (ClientData)ret_name );
-    (void)Tcl_CreateHashEntry( used_names_tbl, ret_name, &new );
+    (void)Tcl_CreateHashEntry( used_names_tbl, ret_name, &new_entry );
     bu_vls_free( &new_name );
 
     return( ret_name );
@@ -3415,6 +3430,7 @@ wdb_concat_cmd(struct rt_wdb	*wdbp,
 	return TCL_ERROR;
     }
 
+    memset(&cc_data, 0, sizeof(struct concat_data));
     bu_vls_init( &cc_data.affix );
     cc_data.copy_mode = 0;
 
