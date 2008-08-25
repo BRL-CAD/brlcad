7d4506d70c2be810186f67d273c1c4da2952f067
Robert Parker <unacknowledged@users.noreply.github.com>
2008-08-25 14:18:59 +0000

    Added call to GED_CHECK_ARGC_GT_0.
    svn:revision:32530
    svn:branch:trunk
    svn:account:bob1961

--- src/libged/putmat.c
+++ src/libged/putmat.c
@@ -63,6 +63,7 @@ ged_putmat(struct ged *gedp, int argc, const char *argv[])
 
     GED_CHECK_DATABASE_OPEN(gedp, BRLCAD_ERROR);
     GED_CHECK_READ_ONLY(gedp, BRLCAD_ERROR);
+    GED_CHECK_ARGC_GT_0(gedp, argc, BRLCAD_ERROR);
 
     /* initialize result */
     bu_vls_trunc(&gedp->ged_result_str, 0);
