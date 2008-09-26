777e923f7bdb4441982ef77693a9bb5528ac630e
Christopher Sean Morrison <brlcad@gmail.com>
2008-09-26 00:50:48 +0000

    these files are missing the standard copyright header for all contributions.  per the original brl-cad license agreement, all rights of use were granted to the gov't when the code was provided so the sources are covered like everything else in the package.  (copyright was claimed and subsequently granted/assigned to the gov't after a massive change to the collective work).  this inconsistency tweak is in response to the sf.net bug posting 2128053 (Copyright issue) by Giuseppe Iuculano - derevko who is working on Debian integration.
    svn:revision:32773
    svn:branch:trunk
    svn:account:brlcad

--- src/util/pixcut.c
+++ src/util/pixcut.c
@@ -1,5 +1,23 @@
-/*
- *		P I X C U T . C
+/*                        P I X C U T . C
+ * BRL-CAD
+ *
+ * Copyright (c) 1992-2008 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file pixcut.c
  *
  * Extract a piece of a pix file.  If the parameters of the file to be
  * extracted do not fit within the original pix file then the extra area is
--- src/util/pixpaste.c
+++ src/util/pixpaste.c
@@ -1,5 +1,23 @@
-/*
- *		P I X P A S T E . C
+/*                      P I X P A S T E . C
+ * BRL-CAD
+ *
+ * Copyright (c) 1992-2008 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file pixpaste.c
  *
  * pixpaste will insert an arbitrary pix file into another pixfile.
  * If the image being pasted does not fit within the destination file
