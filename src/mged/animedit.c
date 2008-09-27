fca24bc3ab456c753fffde9eb4386aa9d7d2c6c9
Christopher Sean Morrison <brlcad@gmail.com>
2008-09-27 16:06:48 +0000

    apply standard header with copyright claim on the collective work through 2004, addresses sf tracker item 2132144 from derevko
    svn:revision:32780
    svn:branch:trunk
    svn:account:brlcad

--- src/mged/animedit.c
+++ src/mged/animedit.c
@@ -1,8 +1,21 @@
 /*                      A N I M E D I T . C
  * BRL-CAD
  *
- * Copyright (c) 1994-2007 Geometric Solutions, Inc.
- * All rights reserved.
+ * Copyright (c) 2004-2008 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
  */
 /** @file animedit.c
  *
@@ -13,6 +26,9 @@
  *
  *  Author -
  *	Christopher T. Johnson
+ *
+ * Copyright (c) 1994-2007 Geometric Solutions, Inc.
+ * All rights reserved.
  */
 
 #include "common.h"
