a9bab91bdfb000def7645161a130bb9dbbeb2d14
Robert Parker <unacknowledged@users.noreply.github.com>
2008-10-01 12:45:14 +0000

    Added code to dump sat files.
    svn:revision:32797
    svn:branch:trunk
    svn:account:bob1961

--- src/conv/bot_dump.c
+++ src/conv/bot_dump.c
@@ -38,14 +38,15 @@
 #include "raytrace.h"
 #include "wdb.h"
 
-#define V3ARGSIN(_a)       (_a)[X]/25.4, (_a)[Y]/25.4, (_a)[Z]/25.4
+#define V3ARGSIN(_a)       (_a)[X]*mm2inches, (_a)[Y]*mm2inches, (_a)[Z]*mm2inches
 
 static char usage[] = "\
-Usage: bot-stl [-b] [-i] [-m directory] [-o file] [-t dxf|obj|stl] [-v] geom.g\n";
+Usage: bot-stl [-b] [-i] [-m directory] [-o file] [-t dxf|obj|sat|stl] [-v] geom.g\n";
 
 enum otype {
     OTYPE_DXF = 1,
     OTYPE_OBJ,
+    OTYPE_SAT,
     OTYPE_STL
 };
 
@@ -58,6 +59,17 @@ static int v_offset = 1;
 static char *output_file = NULL;	/* output filename */
 static char *output_directory = NULL;	/* directory name to hold output files */
 
+static int curr_line_num = 0;
+static int curr_body_id;
+static int curr_lump_id;
+static int curr_shell_id;
+static int curr_face_id;
+static int curr_loop_id;
+static int curr_coedge_id;
+static int curr_edge_id;
+static int curr_vertex_id;
+static int curr_point_id;
+
 /* Byte swaps a four byte value */
 static void
 lswap(unsigned int *v)
@@ -67,6 +79,210 @@ lswap(unsigned int *v)
     r =*v;
     *v = ((r & 0xff) << 24) | ((r & 0xff00) << 8) | ((r & 0xff0000) >> 8)
 	| ((r & 0xff000000) >> 24);
+}
+
+void
+write_bot_sat(struct rt_bot_internal *bot, FILE *fp, char *name)
+{
+    register int i, j;
+    fastf_t *vertices;
+    int *faces;
+    int first_vertex;
+    int last_vertex;
+    int first_pt;
+    int last_pt;
+    int first_coedge;
+    int last_coedge;
+    int first_edge;
+    int last_edge;
+    int first_face;
+    int last_face;
+    int first_loop;
+    int last_loop;
+    int edge_id;
+    int num_vertices = bot->num_vertices;
+    int num_faces = bot->num_faces;
+    int num_edges = num_faces*3;
+
+    vertices = bot->vertices;
+    faces = bot->faces;
+
+    curr_body_id = curr_line_num;
+    curr_lump_id = curr_body_id + 1;
+    curr_shell_id = curr_lump_id + 1;
+    curr_face_id = curr_shell_id + 1 + num_vertices*2 + num_faces*6;
+
+    fprintf( fp, "-%d body $-1 $%d $-1 $-1 #\n", curr_body_id, curr_lump_id);
+    fprintf( fp, "-%d lump $-1 $-1 $%d $%d #\n", curr_lump_id, curr_shell_id, curr_body_id);
+    fprintf( fp, "-%d shell $-1 $-1 $-1 $%d $-1 $%d #\n", curr_shell_id, curr_face_id, curr_lump_id);
+
+    curr_line_num += 3;
+
+    /* Dump out vertices */
+    first_vertex = curr_line_num;
+    for (i = 0; i < num_vertices; i++) {
+	curr_edge_id = -1;
+	for (j = 0; j < num_faces; j++) {
+	    if (faces[3*j]+first_vertex == curr_line_num) {
+		curr_edge_id = first_vertex + num_vertices*2 + num_faces*3 + j*3;
+		break;
+	    } else if (faces[3*j+1]+first_vertex == curr_line_num) {
+		curr_edge_id = first_vertex + num_vertices*2 + num_faces*3 + j*3 + 1;
+		break;
+	    } else if (faces[3*j+2]+first_vertex == curr_line_num) {
+		curr_edge_id = first_vertex + num_vertices*2 + num_faces*3 + j*3 + 2;
+		break;
+	    }
+	}
+
+	fprintf( fp, "-%d vertex $-1 $%d $%d #\n", curr_line_num, curr_edge_id, curr_line_num+num_vertices);
+	++curr_line_num;
+    }
+    last_vertex = curr_line_num-1;
+
+    /* Dump out points */
+    first_pt = curr_line_num;
+    for (i = 0; i < num_vertices; i++) {
+	if (inches)
+	    fprintf( fp, "-%d point $-1 %f %f %f #\n", curr_line_num, V3ARGSIN(&vertices[3*i]));
+	else
+	    fprintf( fp, "-%d point $-1 %f %f %f #\n", curr_line_num, V3ARGS(&vertices[3*i]));
+
+	++curr_line_num;
+    }
+    last_pt = curr_line_num-1;
+
+    /* Dump out coedges */
+    first_coedge = curr_line_num;
+    curr_loop_id = first_coedge+num_faces*7;
+    for (i = 0; i < num_faces; i++) {
+	fprintf( fp, "-%d coedge $-1 $%d $%d $-1 $%d forward $%d $-1 #\n",
+		 curr_line_num, curr_line_num+1, curr_line_num+2,
+		 curr_line_num+num_faces*3, curr_loop_id);
+	++curr_line_num;
+	fprintf( fp, "-%d coedge $-1 $%d $%d $-1 $%d forward $%d $-1 #\n",
+		 curr_line_num, curr_line_num+1, curr_line_num-2,
+		 curr_line_num+num_faces*3, curr_loop_id);
+	++curr_line_num;
+	fprintf( fp, "-%d coedge $-1 $%d $%d $-1 $%d forward $%d $-1 #\n",
+		 curr_line_num, curr_line_num-2, curr_line_num-1,
+		 curr_line_num+num_faces*3,  curr_loop_id);
+	++curr_line_num;
+	++curr_loop_id;
+    }
+    last_coedge = curr_line_num-1;
+
+    /* Dump out edges */
+    first_edge = curr_line_num;
+    for (i = 0; i < num_faces; i++) {
+	fprintf( fp, "-%d edge $-1 $%d $%d $%d $%d forward #\n", curr_line_num,
+		 faces[3*i]+first_vertex, faces[3*i+1]+first_vertex,
+		 first_coedge + i*3, curr_line_num + num_faces*5);
+	++curr_line_num;
+	fprintf( fp, "-%d edge $-1 $%d $%d $%d $%d forward #\n", curr_line_num,
+		 faces[3*i+1]+first_vertex, faces[3*i+2]+first_vertex,
+		 first_coedge + i*3 + 1, curr_line_num + num_faces*5);
+	++curr_line_num;
+	fprintf( fp, "-%d edge $-1 $%d $%d $%d $%d forward #\n", curr_line_num,
+		 faces[3*i+2]+first_vertex, faces[3*i]+first_vertex,
+		 first_coedge + i*3 + 2, curr_line_num + num_faces*5);
+	++curr_line_num;
+    }
+    last_edge = curr_line_num-1;
+
+    /* Dump out faces */
+    first_face = curr_line_num;
+    for (i = 0; i < num_faces-1; i++) {
+	fprintf( fp, "-%d face $-1 $%d $%d $%d $-1 $%d forward single #\n",
+		 curr_line_num, curr_line_num+1, curr_line_num+num_faces,
+		 curr_shell_id, curr_line_num + num_faces*5);
+	++curr_line_num;
+    }
+    fprintf( fp, "-%d face $-1 $-1 $%d $%d $-1 $%d forward single #\n",
+	     curr_line_num, curr_line_num+num_faces, curr_shell_id,
+             curr_line_num + num_faces*5);
+    ++curr_line_num;
+    last_face = curr_line_num-1;
+
+    /* Dump out loops */
+    first_loop = curr_line_num;
+    for (i = 0; i < num_faces; i++) {
+	fprintf( fp, "-%d loop $-1 $-1 $%d $%d #\n",
+		 curr_line_num, first_coedge+i*3, first_face+i);
+	++curr_line_num;
+    }
+    last_loop = curr_line_num-1;
+
+    /* Dump out straight-curves for each edge */
+    for (i = 0; i < num_faces; i++) {
+	point_t A;
+	point_t B;
+	point_t C;
+	point_t center;
+	vect_t BmA;
+	vect_t CmB;
+	vect_t AmC;
+	int vi;
+
+	vi = 3*faces[3*i];
+	VSET(A, vertices[vi], vertices[vi+1], vertices[vi+2]);
+	vi = 3*faces[3*i+1];
+	VSET(B, vertices[vi], vertices[vi+1], vertices[vi+2]);
+	vi = 3*faces[3*i+2];
+	VSET(C, vertices[vi], vertices[vi+1], vertices[vi+2]);
+	VSUB2(BmA, B, A);
+	VSUB2(CmB, C, B);
+	VSUB2(AmC, A, C);
+	VUNITIZE(BmA);
+	VUNITIZE(CmB);
+	VUNITIZE(AmC);
+
+	fprintf(fp, "-%d straight-curve $-1 %f %f %f %f %f %f I I #\n", curr_line_num, V3ARGS(A), V3ARGS(BmA));
+	++curr_line_num;
+	fprintf(fp, "-%d straight-curve $-1 %f %f %f %f %f %f I I #\n", curr_line_num, V3ARGS(B), V3ARGS(CmB));
+	++curr_line_num;
+	fprintf(fp, "-%d straight-curve $-1 %f %f %f %f %f %f I I #\n", curr_line_num, V3ARGS(C), V3ARGS(AmC));
+	++curr_line_num;
+    }
+
+    /* Dump out plane-surfaces for each face */
+    for (i = 0; i < num_faces; i++) {
+	point_t A;
+	point_t B;
+	point_t C;
+	point_t center;
+	vect_t BmA;
+	vect_t CmA;
+	vect_t norm;
+	int vi;
+	fastf_t sf = 1.0/3.0;
+
+	vi = 3*faces[3*i];
+	VSET(A, vertices[vi], vertices[vi+1], vertices[vi+2]);
+	vi = 3*faces[3*i+1];
+	VSET(B, vertices[vi], vertices[vi+1], vertices[vi+2]);
+	vi = 3*faces[3*i+2];
+	VSET(C, vertices[vi], vertices[vi+1], vertices[vi+2]);
+
+	VADD3(center, A, B, C);
+	VSCALE(center, center, sf);
+
+	VSUB2(BmA, B, A);
+	VSUB2(CmA, C, A);
+	if (bot->orientation != RT_BOT_CW) {
+	    VCROSS(norm, BmA, CmA);
+	} else {
+	    VCROSS(norm, CmA, BmA);
+	}
+	VUNITIZE(norm);
+
+	VUNITIZE(BmA);
+
+	fprintf(fp, "-%d plane-surface $-1 %f %f %f %f %f %f %f %f %f forward_v I I I I #\n",
+		curr_line_num, V3ARGS(A), V3ARGS(norm), V3ARGS(BmA));
+
+	++curr_line_num;
+    }
 }
 
 void
@@ -320,6 +536,8 @@ main(int argc, char *argv[])
 		output_type = OTYPE_DXF;
 	    else if (!strcmp("obj", bu_optarg))
 		output_type = OTYPE_OBJ;
+	    else if (!strcmp("sat", bu_optarg))
+		output_type = OTYPE_SAT;
 	    else if (!strcmp("stl", bu_optarg))
 		output_type = OTYPE_STL;
 	    else
@@ -351,14 +569,9 @@ main(int argc, char *argv[])
 
 	/* Set this to something non-null in order to possibly write eof */
 	output_file = "stdout";
+    }
 
-	/* output DXF header and start of TABLES section */
-	if (output_type == OTYPE_DXF) {
-	    fprintf(fp,
-		    "0\nSECTION\n2\nHEADER\n999\n%s (All Bots)\n0\nENDSEC\n0\nSECTION\n2\nENTITIES\n",
-		    argv[argc-1]);
-	}
-    } else if ( output_file ) {
+    if ( output_file ) {
 	if ( binary && output_type == OTYPE_STL ) {
 	    char buf[81];	/* need exactly 80 chars for header */
 
@@ -389,11 +602,26 @@ main(int argc, char *argv[])
 		bu_exit(1, "Cannot open ASCII output file (%s) for writing\n", output_file );
 	    }
 
-	    /* output DXF header and start of TABLES section */
-	    if (output_type == OTYPE_DXF) {
+	    switch (output_type) {
+	    case OTYPE_DXF:
+		/* output DXF header and start of TABLES section */
 		fprintf(fp,
 			"0\nSECTION\n2\nHEADER\n999\n%s (All Bots)\n0\nENDSEC\n0\nSECTION\n2\nENTITIES\n",
 			argv[argc-1]);
+		break;
+	    case OTYPE_SAT:
+		fprintf( fp, "400 0 1 0\n");
+                /*XXX Temporarily hardwired */
+#if 1
+		fprintf( fp, "37 SolidWorks(2008000)-Sat-Convertor-2.0 11 ACIS 8.0 NT 24 Wed Dec 03 09:26:53 2003\n");
+#else
+		fprintf( fp, "08 BRL-CAD-bot_dump-4.0 11 ACIS 4.0 NT 24 Thur Sep 25 15:00:00 2008\n");
+#endif
+		fprintf( fp, "1 9.9999999999999995e-007 1e-010\n");
+		curr_line_num = 0;
+		break;
+	    default:
+		break;
 	    }
 	}
     }
@@ -417,6 +645,9 @@ main(int argc, char *argv[])
 	    break;
 	case OTYPE_OBJ:
 	    file_ext = ".obj";
+	    break;
+	case OTYPE_SAT:
+	    file_ext = ".sat";
 	    break;
 	case OTYPE_STL:
 	default:
@@ -517,6 +748,21 @@ main(int argc, char *argv[])
 		v_offset = 1;
 		write_bot_obj(bot, fp, dp->d_namep);
 		break;
+	    case OTYPE_SAT:
+		curr_line_num = 0;
+
+		fprintf( fp, "400 0 1 0\n");
+                /*XXX Temporarily hardwired */
+#if 1
+		fprintf( fp, "37 SolidWorks(2008000)-Sat-Convertor-2.0 11 ACIS 8.0 NT 24 Wed Dec 03 09:26:53 2003\n");
+#else
+		fprintf( fp, "08 BRL-CAD-bot_dump-4.0 11 ACIS 4.0 NT 24 Thur Sep 25 15:00:00 2008\n");
+#endif
+		fprintf( fp, "1 9.9999999999999995e-007 1e-010\n");
+
+		write_bot_sat(bot, fp, dp->d_namep);
+		fprintf( fp, "End-of-ACIS-data\n");
+		break;
 	    case OTYPE_STL:
 	    default:
 		write_bot_stl(bot, fp, dp->d_namep);
@@ -536,6 +782,9 @@ main(int argc, char *argv[])
 		break;
 	    case OTYPE_OBJ:
 		write_bot_obj(bot, fp, dp->d_namep);
+		break;
+	    case OTYPE_SAT:
+		write_bot_sat(bot, fp, dp->d_namep);
 		break;
 	    case OTYPE_STL:
 	    default:
@@ -562,8 +811,16 @@ main(int argc, char *argv[])
 	    close(fd);
 	} else {
 	    /* end of layers section, start of ENTITIES SECTION */
-	    if (output_type == OTYPE_DXF)
+	    switch (output_type) {
+	    case OTYPE_DXF:
 		fprintf( fp, "0\nENDSEC\n0\nEOF\n" );
+		break;
+	    case OTYPE_SAT:
+		fprintf( fp, "End-of-ACIS-data\n");
+		break;
+	    default:
+		break;
+	    }
 
 	    fclose(fp);
 	}
