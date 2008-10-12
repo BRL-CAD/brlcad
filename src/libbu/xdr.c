ca1ebbc35f5f6c7c9ef69a499e7f1a5989ac164d
Christopher Sean Morrison <brlcad@gmail.com>
2008-10-12 20:37:19 +0000

    get rid of dead code, bu_gtimeval bu_ptimeval
    svn:revision:32914
    svn:branch:trunk
    svn:account:brlcad

--- src/libbu/xdr.c
+++ src/libbu/xdr.c
@@ -49,18 +49,12 @@ unsigned short
 bu_gshort(const unsigned char *msgp)
 {
     register const unsigned char *p = msgp;
-#ifdef vax
-    /*
-     * vax compiler doesn't put shorts in registers
-     */
-    register unsigned long u;
-#else
     register unsigned short u;
-#endif
 
     u = *p++ << 8;
     return ((unsigned short)(u | *p));
 }
+
 
 /**
  * B U _ G L O N G
@@ -77,6 +71,7 @@ bu_glong(const unsigned char *msgp)
     return (u | *p);
 }
 
+
 /**
  * B U _ P S H O R T
  */
@@ -88,6 +83,7 @@ bu_pshort(register unsigned char *msgp, register int s)
     msgp[0] = s >> 8;
     return (msgp+2);
 }
+
 
 /**
  * B U _ P L O N G
@@ -102,40 +98,6 @@ bu_plong(register unsigned char *msgp, register long unsigned int l)
     msgp[0] = l >> 8;
     return (msgp+4);
 }
-
-#if 0
-/* XXX How do we get "struct timeval" declared for all of bu.h? */
-/**
- * B U _ G T I M E V A L
- *
- * Get a timeval structure from an external representation
- * which "on the wire" is a 64-bit seconds, followed by a 32-bit usec.
- */
-typedef unsigned char ext_timeval_t[8+4];	/* storage for on-wire format */
-
-void
-bu_gtimeval(tvp, msgp)
-    struct timeval *tvp;
-    const unsigned char *msgp;
-{
-    tvp->tv_sec = (((time_t)BU_GLONG(msgp+0)) << 32) |
-	BU_GLONG(msgp+4);
-    tvp->tv_usec = BU_GLONG(msgp+8);
-}
-
-unsigned char *
-bu_ptimeval(msgp, tvp)
-    const struct timeval *tvp;
-    unsigned char *msgp;
-{
-    long upper = (long)(tvp->tv_sec >> 32);
-    long lower = (long)(tvp->tv_sec & 0xFFFFFFFFL);
-
-    (void)bu_plong(msgp+0, upper);
-    (void)bu_plong(msgp+4, lower);
-    return bu_plong(msgp+8, tvp->tv_usec);
-}
-#endif
 
 /** @} */
 /*
