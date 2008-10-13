1e80825fd151b884a639c766154b0d8a50513658
Christopher Sean Morrison <brlcad@gmail.com>
2008-10-13 20:11:00 +0000

    begin LIBGCV, a geometry conversion library.  the intent of this library is to allow conversion to/from a variety of geometry file formats, migrating and generalizing logic from src/conv into a useful library.  start with a simple region callback that is logic shared by many of the mesh-format exporters.
    svn:revision:32927
    svn:branch:trunk
    svn:account:brlcad

--- src/conv/dxf/g-dxf.c
+++ src/conv/dxf/g-dxf.c
@@ -40,9 +40,10 @@
 #include "nmg.h"
 #include "rtgeom.h"
 #include "raytrace.h"
-#include "brlcad_version.h"
+#include "gcv.h"
 
 /* private headers */
+#include "brlcad_version.h"
 #include "./dxf.h"
 
 
@@ -343,200 +344,6 @@ nmg_to_dxf( struct nmgregion *r, struct db_full_path *pathp, int region_id, int 
 }
 
 
-/**
- * D O _ R E G I O N _ E N D
- *
- * Called from db_walk_tree().
- *
- * This routine must be prepared to run in parallel.
- */
-union tree *do_region_end(tsp, pathp, curtree, client_data)
-    register struct db_tree_state	*tsp;
-    struct db_full_path	*pathp;
-    union tree		*curtree;
-    genptr_t		client_data;
-{
-    union tree		*ret_tree;
-    struct bu_list		vhead;
-    struct nmgregion	*r;
-
-    void (*write_region)(struct nmgregion *r, struct db_full_path *pathp, int region_id, int material_id, float color[3]) = client_data;
-
-    if (!write_region) {
-	bu_log("do_region_end missing conversion callback\n");
-	return TREE_NULL;
-    }
-
-    RT_CK_FULL_PATH(pathp);
-    RT_CK_TREE(curtree);
-    RT_CK_TESS_TOL(tsp->ts_ttol);
-    BN_CK_TOL(tsp->ts_tol);
-    NMG_CK_MODEL(*tsp->ts_m);
-
-    BU_LIST_INIT(&vhead);
-
-    if (RT_G_DEBUG&DEBUG_TREEWALK || verbose) {
-	char	*sofar = db_path_to_string(pathp);
-	bu_log("\ndo_region_end(%d %d%%) %s\n",
-	       regions_tried,
-	       regions_tried>0 ? (regions_converted * 100) / regions_tried : 0,
-	       sofar);
-	bu_free(sofar, "path string");
-    }
-
-    if (curtree->tr_op == OP_NOP)
-	return  curtree;
-
-    regions_tried++;
-
-    /* Begin bomb protection */
-    if ( ncpu == 1 ) {
-	if ( BU_SETJUMP )  {
-	    /* Error, bail out */
-	    char *sofar;
-	    BU_UNSETJUMP;		/* Relinquish the protection */
-
-	    sofar = db_path_to_string(pathp);
-	    bu_log( "FAILED in Boolean evaluation: %s\n", sofar );
-	    bu_free( (char *)sofar, "sofar" );
-
-	    /* Sometimes the NMG library adds debugging bits when
-	     * it detects an internal error, before bombing out.
-	     */
-	    rt_g.NMG_debug = NMG_debug;	/* restore mode */
-
-	    /* Release any intersector 2d tables */
-	    nmg_isect2d_final_cleanup();
-
-	    /* Release the tree memory & input regions */
-	    /*XXX*/			/* db_free_tree(curtree);*/		/* Does an nmg_kr() */
-
-	    /* Get rid of (m)any other intermediate structures */
-	    if ( (*tsp->ts_m)->magic == NMG_MODEL_MAGIC )  {
-		nmg_km(*tsp->ts_m);
-	    } else {
-		bu_log("WARNING: tsp->ts_m pointer corrupted, ignoring it.\n");
-	    }
-
-	    /* Now, make a new, clean model structure for next pass. */
-	    *tsp->ts_m = nmg_mm();
-	    goto out;
-	}
-    }
-    if ( verbose )
-	bu_log("Attempting to process region %s\n", db_path_to_string( pathp ));
-
-    ret_tree = nmg_booltree_evaluate( curtree, tsp->ts_tol, &rt_uniresource );	/* librt/nmg_bool.c */
-    BU_UNSETJUMP;		/* Relinquish the protection */
-
-    if ( ret_tree ) {
-	r = ret_tree->tr_d.td_r;
-    } else {
-	if ( verbose ) {
-	    bu_log( "\tNothing left of this region after Boolean evaluation\n" );
-	}
-	regions_written++; /* don't count as a failure */
-	r = (struct nmgregion *)NULL;
-    }
-    /*	regions_done++;  XXX */
-
-    regions_converted++;
-
-    if (r != (struct nmgregion *)NULL) {
-	struct shell *s;
-	int empty_region=0;
-	int empty_model=0;
-
-	/* Kill cracks */
-	s = BU_LIST_FIRST( shell, &r->s_hd );
-	while ( BU_LIST_NOT_HEAD( &s->l, &r->s_hd ) ) {
-	    struct shell *next_s;
-
-	    next_s = BU_LIST_PNEXT( shell, &s->l );
-	    if ( nmg_kill_cracks( s ) ) {
-		if ( nmg_ks( s ) ) {
-		    empty_region = 1;
-		    break;
-		}
-	    }
-	    s = next_s;
-	}
-
-	/* kill zero length edgeuses */
-	if ( !empty_region ) {
-	    empty_model = nmg_kill_zero_length_edgeuses( *tsp->ts_m );
-	}
-
-	if ( !empty_region && !empty_model ) {
-	    if ( BU_SETJUMP ) {
-		char *sofar;
-
-		BU_UNSETJUMP;
-
-		sofar = db_path_to_string(pathp);
-		bu_log( "FAILED in triangulator: %s\n", sofar );
-		bu_free( (char *)sofar, "sofar" );
-
-		/* Sometimes the NMG library adds debugging bits when
-		 * it detects an internal error, before bombing out.
-		 */
-		rt_g.NMG_debug = NMG_debug;	/* restore mode */
-
-		/* Release any intersector 2d tables */
-		nmg_isect2d_final_cleanup();
-
-		/* Get rid of (m)any other intermediate structures */
-		if ( (*tsp->ts_m)->magic == NMG_MODEL_MAGIC ) {
-		    nmg_km(*tsp->ts_m);
-		} else {
-		    bu_log("WARNING: tsp->ts_m pointer corrupted, ignoring it.\n");
-		}
-
-		/* Now, make a new, clean model structure for next pass. */
-		*tsp->ts_m = nmg_mm();
-		goto out;
-	    }
-	    /* Write the region out */
-	    write_region( r, pathp, tsp->ts_regionid, tsp->ts_gmater, tsp->ts_mater.ma_color );
-
-	    regions_written++;
-
-	    BU_UNSETJUMP;
-	}
-
-	if ( !empty_model )
-	    nmg_kr( r );
-    }
-
- out:
-    /*
-     *  Dispose of original tree, so that all associated dynamic
-     *  memory is released now, not at the end of all regions.
-     *  A return of TREE_NULL from this routine signals an error,
-     *  and there is no point to adding _another_ message to our output,
-     *  so we need to cons up an OP_NOP node to return.
-     */
-
-
-    if (regions_tried>0) {
-	float npercent, tpercent;
-
-	npercent = (float)(regions_converted * 100) / regions_tried;
-	tpercent = (float)(regions_written * 100) / regions_tried;
-	if ( verbose )
-	    bu_log("Tried %d regions, %d conv. to NMG's %d conv. to tri. nmgper = %.2f%% triper = %.2f%% \n",
-		   regions_tried, regions_converted, regions_written, npercent, tpercent);
-    }
-
-    db_free_tree(curtree, &rt_uniresource);		/* Does an nmg_kr() */
-
-    BU_GETUNION(curtree, tree);
-    curtree->magic = RT_TREE_MAGIC;
-    curtree->tr_op = OP_NOP;
-    return(curtree);
-}
-
-
 union tree *get_layer(tsp, pathp, curtree, client_data)
     register struct db_tree_state	*tsp;
     struct db_full_path	*pathp;
@@ -568,7 +375,7 @@ union tree *get_layer(tsp, pathp, curtree, client_data)
  * 4. db_walk_tree (get_layer) for layer names only
  * 5. Initialize tree_state
  * 6. Initialize model (nmg)\
- * 7. db_walk_tree (do_region_end)
+ * 7. db_walk_tree (gcv_region_end)
  * 8. Cleanup
  */
 int
@@ -729,9 +536,9 @@ main(argc, argv)
 			1,			/* ncpu */
 			&tree_state,
 			0,			/* take all regions */
-			do_region_end,
+			gcv_region_end,
 			nmg_booltree_leaf_tess,
-			(genptr_t)nmg_to_dxf);	/* callback for do_region_end */
+			(genptr_t)nmg_to_dxf);	/* callback for gcv_region_end */
 
     percent = 0;
     if (regions_tried>0) {
