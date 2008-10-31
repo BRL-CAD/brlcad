00b85024d775c798502faec107bfb2188c9b5d65
Erik Greenwald <erik@brlcad.org>
2008-10-31 19:10:26 +0000

    added wdb_fflush(), just cuz
    svn:revision:33093
    svn:branch:trunk
    svn:account:erikgreenwald

--- src/librt/wdb.c
+++ src/librt/wdb.c
@@ -413,6 +413,19 @@ wdb_init(struct rt_wdb *wdbp, struct db_i *dbip, int mode)
 }
 
 /**
+ *			W D B _ F S Y N C
+ *
+ *  Flush pending data to the storage device.
+ */
+int
+wdb_fflush(struct rt_wdb *wdbp)
+{
+    RT_CK_WDB(wdbp);
+    RT_CK_DBI(wdbp->dbip);
+    return fflush(wdbp->dbip->dbi_fp);
+}
+
+/**
  *			W D B _ C L O S E
  *
  *  Release from associated database "file", destroy dynamic data structure.
