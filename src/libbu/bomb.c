d50be7145bc1485f22b5f2352778c8785f3431f6
Christopher Sean Morrison <brlcad@gmail.com>
2008-12-19 22:00:18 +0000

    why do we bother to init the vls in bu_exit() if we might not use it
    svn:revision:33405
    svn:branch:trunk
    svn:account:brlcad

--- src/libbu/bomb.c
+++ src/libbu/bomb.c
@@ -201,19 +201,20 @@ bu_exit(int status, const char *fmt, ...)
     va_list ap;
     struct bu_vls message;
 
-    bu_vls_init(&message);
-
     if (fmt && strlen(fmt) > 0) {
 	va_start(ap, fmt);
+
+	bu_vls_init(&message);
+
 	bu_vls_vprintf(&message, fmt, ap);
 
 	if (!BU_SETJUMP) {
 	    bu_bomb(bu_vls_addr(&message));
 	}
 	BU_UNSETJUMP;
-    }
 
-    bu_vls_free(&message);
+	bu_vls_free(&message);
+    }
 
     exit(status);
 }
