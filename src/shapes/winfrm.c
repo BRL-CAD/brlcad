5443dcfe5d79c676b34582cc39bedce47cfad2aa
Christopher Sean Morrison <brlcad@gmail.com>
2009-01-09 23:50:45 +0000

    rename winfrm to window_frame
    svn:revision:33514
    svn:branch:trunk
    svn:account:brlcad

--- src/shapes/winfrm.c
+++ src/shapes/winfrm.c
@@ -1,14 +1,14 @@
-/*                        W I N F R M . C
+/*                  W I N D O W _ F R A M E . C
  * BRL-CAD
  *
- * Copyright (c) 2004-2008 United States Government as represented by
+ * Copyright (c) 2009 United States Government as represented by
  * the U.S. Army Research Laboratory.
  *
- * This program is free software; you can redistribute it and/or
+ * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public License
  * version 2.1 as published by the Free Software Foundation.
  *
- * This program is distributed in the hope that it will be useful, but
+ * This library is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
@@ -17,7 +17,7 @@
  * License along with this file; see the file named COPYING for more
  * information.
  */
-/** @file winfrm.c
+/** @file window_frame.c
  *
  * Program to make a window frame using libwdb.  The objects will be
  * in millimeters.  The window frames are composed of four arb8s and *
