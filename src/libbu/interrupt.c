ce381b0fb6bb195be735e211dc9cb1f8af63eecb
Christopher Sean Morrison <brlcad@gmail.com>
2009-01-22 21:00:11 +0000

    rename signal.c to interrupt.c
    svn:revision:33585
    svn:branch:trunk
    svn:account:brlcad

--- src/libbu/interrupt.c
+++ src/libbu/interrupt.c
@@ -1,4 +1,24 @@
-/*
+/*                     I N T E R R U P T . C
+ * BRL-CAD
+ *
+ * Copyright (c) 2009 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file interrupt.c
+ *
  * Routines for managing signals.  In particular, provide a common
  * means to temporarily buffer processing a signal during critical
  * write operations.
