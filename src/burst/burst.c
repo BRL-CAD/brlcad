bcebd6462ed5b066fcda58fb1f42e0843073f653
Christopher Sean Morrison <brlcad@gmail.com>
2009-02-19 19:42:43 +0000

    cleanup
    svn:revision:33832
    svn:branch:trunk
    svn:account:brlcad

--- src/burst/burst.c
+++ src/burst/burst.c
@@ -43,7 +43,7 @@
 #define DEBUG_BURST	0	/* 1 enables debugging for this module */
 
 /*
-  int getCommand( char *name, char *buf, int len, FILE *fp )
+  int getCommand(char *name, char *buf, int len, FILE *fp)
 
   Read next command line into buf and stuff the command name into name
   from input stream fp.  buf must be at least len bytes long.
@@ -53,27 +53,22 @@
   0 for end of file
 */
 static int
-getCommand( char *name, char *buf, int len, FILE *fp )
+getCommand(char *name, char *buf, int len, FILE *fp)
 {
-    assert( name != NULL );
-    assert( buf != NULL );
-    assert( fp != NULL );
-    while ( bu_fgets( buf, len, fp ) != NULL )
-    {
-	if ( buf[0] != CHAR_COMMENT )
-	{
-	    if ( sscanf( buf, "%1330s", name ) == 1 ) /* LNBUFSZ */
-	    {
+    assert(name != NULL);
+    assert(buf != NULL);
+    assert(fp != NULL);
+    while (bu_fgets(buf, len, fp) != NULL) {
+	if (buf[0] != CHAR_COMMENT) {
+	    if (sscanf(buf, "%1330s", name) == 1) {
+		/* LNBUFSZ */
 		buf[strlen(buf)-1] = NUL; /* clobber newline */
 		return	1;
-	    }
-	    else /* Skip over blank lines. */
+	    } else /* Skip over blank lines. */
 		continue;
-	}
-	else
-	{
+	} else {
 	    /* Generate comment command. */
-	    bu_strlcpy( name, CMD_COMMENT, LNBUFSZ );
+	    bu_strlcpy(name, CMD_COMMENT, LNBUFSZ);
 	    return	1;
 	}
     }
@@ -81,32 +76,30 @@ getCommand( char *name, char *buf, int len, FILE *fp )
 }
 
 /*
-  void setupSigs( void )
+  void setupSigs(void)
 
   Initialize all signal handlers.
 */
 
 static void
-setupSigs( void )
+setupSigs(void)
 {
     register int i;
-    for ( i = 0; i < NSIG; i++ )
-	switch ( i )
-	{
+    for (i = 0; i < NSIG; i++)
+	switch (i) {
 	    case SIGINT :
-		if ( (norml_sig = signal( i, SIG_IGN )) == SIG_IGN )
+		if ((norml_sig = signal(i, SIG_IGN)) == SIG_IGN)
 		    abort_sig = SIG_IGN;
-		else
-		{
+		else {
 		    norml_sig = intr_sig;
 		    abort_sig = abort_RT;
-		    (void) signal( i,  norml_sig );
+		    (void) signal(i,  norml_sig);
 		}
 		break;
 	    case SIGCHLD :
 		break; /* leave SIGCLD alone */
 	    case SIGPIPE :
-		(void) signal( i, SIG_IGN );
+		(void) signal(i, SIG_IGN);
 		break;
 	    case SIGQUIT :
 		break;
@@ -117,19 +110,17 @@ setupSigs( void )
 }
 
 /*
-  int parsArgv( int argc, char **argv )
+  int parsArgv(int argc, char **argv)
 
   Parse program command line.
 */
 static int
-parsArgv( int argc, char **argv )
+parsArgv(int argc, char **argv)
 {
     register int c;
 /* Parse options.						*/
-    while ( (c = bu_getopt( argc, argv, "b" )) != EOF )
-    {
-	switch ( c )
-	{
+    while ((c = bu_getopt(argc, argv, "b")) != EOF) {
+	switch (c) {
 	    case 'b' :
 		tty = 0;
 		break;
@@ -137,46 +128,40 @@ parsArgv( int argc, char **argv )
 		return	0;
 	}
     }
-    return	1;
+    return 1;
 }
 
 /*
-  void readBatchInput( FILE *fp )
+  void readBatchInput(FILE *fp)
 
   Read and execute commands from input stream fp.
 */
 void
-readBatchInput( FILE *fp )
+readBatchInput(FILE *fp)
 {
-    assert( fp != (FILE *) NULL );
+    assert(fp != (FILE *) NULL);
     batchmode = 1;
-    while ( getCommand( cmdname, cmdbuf, LNBUFSZ, fp ) )
-    {
+    while (getCommand(cmdname, cmdbuf, LNBUFSZ, fp)) {
 	Func	*cmdfunc;
-	if ( (cmdfunc = getTrie( cmdname, cmdtrie )) == NULL )
-	{
-	    register int i, len = strlen( cmdname );
-	    brst_log( "ERROR -- command syntax:\n" );
-	    brst_log( "\t%s\n", cmdbuf );
-	    brst_log( "\t" );
-	    for ( i = 0; i < len; i++ )
-		brst_log( " " );
-	    brst_log( "^\n" );
-	}
-	else
-	    if ( strcmp( cmdname, CMD_COMMENT ) == 0 )
-	    {
+	if ((cmdfunc = getTrie(cmdname, cmdtrie)) == NULL) {
+	    register int i, len = strlen(cmdname);
+	    brst_log("ERROR -- command syntax:\n");
+	    brst_log("\t%s\n", cmdbuf);
+	    brst_log("\t");
+	    for (i = 0; i < len; i++)
+		brst_log(" ");
+	    brst_log("^\n");
+	} else
+	    if (strcmp(cmdname, CMD_COMMENT) == 0) {
 		/* special handling for comments */
 		cmdptr = cmdbuf;
 		cmdbuf[strlen(cmdbuf)-1] = '\0'; /* clobber newline */
-		(*cmdfunc)( (HmItem *) 0 );
-	    }
-	    else
-	    {
+		(*cmdfunc)((HmItem *) 0);
+	    } else {
 		/* Advance pointer past nul at end of
 		   command name. */
-		cmdptr = cmdbuf + strlen( cmdname ) + 1;
-		(*cmdfunc)( (HmItem *) 0 );
+		cmdptr = cmdbuf + strlen(cmdname) + 1;
+		(*cmdfunc)((HmItem *) 0);
 	    }
     }
     batchmode = 0;
@@ -184,10 +169,10 @@ readBatchInput( FILE *fp )
 }
 
 /*
-  int main( int argc, char *argv[] )
+  int main(int argc, char *argv[])
 */
 int
-main( int argc, char *argv[] )
+main(int argc, char *argv[])
 {
     bu_setlinebuf(stderr);
 
@@ -195,46 +180,46 @@ main( int argc, char *argv[] )
     if (!tmpfp) {
 	bu_exit(EXIT_FAILURE, "ERROR: Unable to create temporary file.\n");
     }
-    if ( ! parsArgv( argc, argv ) ) {
+    if (! parsArgv(argc, argv)) {
 	prntUsage();
 	return EXIT_FAILURE;
     }
 
     setupSigs();
-    if ( ! initUi() ) /* must be called before any output is produced */
+    if (! initUi()) /* must be called before any output is produced */
 	return EXIT_FAILURE;
 
 #if DEBUG_BURST
-    prntTrie( cmdtrie, 0 );
+    prntTrie(cmdtrie, 0);
 #endif
-    assert( airids.i_next == NULL );
-    assert( armorids.i_next == NULL );
-    assert( critids.i_next == NULL );
+    assert(airids.i_next == NULL);
+    assert(armorids.i_next == NULL);
+    assert(critids.i_next == NULL);
 
-    if ( ! isatty( 0 ) || ! tty )
-	readBatchInput( stdin );
-    if ( tty )
-	(void) HmHit( mainhmenu );
-    exitCleanly( EXIT_SUCCESS );
+    if (! isatty(0) || ! tty)
+	readBatchInput(stdin);
+    if (tty)
+	(void) HmHit(mainhmenu);
+    exitCleanly(EXIT_SUCCESS);
 
     /* not reached */
     return EXIT_SUCCESS;
 }
 
 /*
-  void exitCleanly( int code )
+  void exitCleanly(int code)
 
   Should be only exit from program after success of initUi().
 */
 void
-exitCleanly( int code )
+exitCleanly(int code)
 {
-    if ( tty )
+    if (tty)
 	closeUi(); /* keep screen straight */
-    (void) fclose( tmpfp );
-    if ( unlink( tmpfname ) == -1 )
-	locPerror( tmpfname );
-    exit( code );
+    (void) fclose(tmpfp);
+    if (unlink(tmpfname) == -1)
+	locPerror(tmpfname);
+    exit(code);
 }
 
 /*
