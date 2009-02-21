903a7bc7751893483a789322663b6dbe9c0b8803
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2009-02-21 17:34:27 +0000

    Merge in conditionally undef INCL_WINSOCK_API_TYPEDEFS from r29126
    svn:revision:33862
    svn:branch:trunk
    svn:account:starseeker

--- src/other/tcl/win/tclWinPort.h
+++ src/other/tcl/win/tclWinPort.h
@@ -75,6 +75,9 @@
 /*
  * Ask for the winsock function typedefs, also.
  */
+#ifdef INCL_WINSOCK_API_TYPEDEFS
+#undef INCL_WINSOCK_API_TYPEDEFS
+#endif
 #define INCL_WINSOCK_API_TYPEDEFS   1
 #include <winsock2.h>
 
