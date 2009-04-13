e7c45b257b05bbc2bab826c61cae87b58b15b800
Christopher Sean Morrison <brlcad@gmail.com>
2009-04-13 15:49:57 +0000

    do the same to memset, use void and size_t to match standard
    svn:revision:34205
    svn:branch:trunk
    svn:account:brlcad

--- include/sysv.h
+++ include/sysv.h
@@ -59,7 +59,7 @@ SYSV_EXPORT extern int bcmp(const void *b1, const void *b2, size_t len);
 #endif
 
 #ifndef HAVE_MEMSET
-SYSV_EXPORT extern char *memset(char *s, char c, int n);
+SYSV_EXPORT extern void *memset(void *s, int c, size_t n);
 #endif
 
 #ifndef HAVE_STRCHR
