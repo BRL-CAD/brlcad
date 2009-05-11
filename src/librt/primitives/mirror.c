784187d0a1ef08e16d966c2bcdfb0856e99cc6fd
Christopher Sean Morrison <brlcad@gmail.com>
2009-05-11 20:58:08 +0000

    odd ws snuckage
    svn:revision:34494
    svn:branch:trunk
    svn:account:brlcad

--- src/librt/primitives/mirror.c
+++ src/librt/primitives/mirror.c
@@ -1,22 +1,22 @@
 /*                        M I R R O R . C
-  * BRL-CAD
-  *
-  * Copyright (c) 2007-2009 United States Government as represented by
-  * the U.S. Army Research Laboratory.
-  *
-  * This library is free software; you can redistribute it and/or
-  * modify it under the terms of the GNU Lesser General Public License
-  * version 2.1 as published by the Free Software Foundation.
-  *
-  * This library is distributed in the hope that it will be useful, but
-  * WITHOUT ANY WARRANTY; without even the implied warranty of
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  * Lesser General Public License for more details.
-  *
-  * You should have received a copy of the GNU Lesser General Public
-  * License along with this file; see the file named COPYING for more
-  * information.
-  */
+ * BRL-CAD
+ *
+ * Copyright (c) 2007-2009 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
 /** @file mirror.c
  *
  * Routine(s) to mirror objects.
