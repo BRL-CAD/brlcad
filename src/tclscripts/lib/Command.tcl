0785cfd9e46b3844c63b79400c9ef12ecfcf7952
Christopher Sean Morrison <brlcad@gmail.com>
2009-06-03 03:04:35 +0000

    protect the gets rename the same way mged/text.tcl does it by making sure the proc has a body/exists first.  the s2 folks reported that they're getting an error about gets not existing which would be consistent with the Command.tcl constructor getting read multiple times.
    svn:revision:34641
    svn:branch:trunk
    svn:account:brlcad

--- src/tclscripts/lib/Command.tcl
+++ src/tclscripts/lib/Command.tcl
@@ -210,7 +210,9 @@
     $itk_component(text) tag configure oldcmd -foreground $itk_option(-oldcmd_color)
     $itk_component(text) tag configure result -foreground $itk_option(-result_color)
 
-    rename ::gets ::tcl_gets
+    if {[catch {info body ::gets}] == 1} {
+        rename ::gets ::tcl_gets
+    }
 }
 
 ::itcl::body Command::destructor {} {
