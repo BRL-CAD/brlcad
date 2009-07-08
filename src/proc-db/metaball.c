711a81045a193c6c48a6f7ce5cc68d95e1f125d7
Christopher Sean Morrison <brlcad@gmail.com>
2009-07-08 19:42:15 +0000

    hijack the stubbed metaballs example with something entirely different.  this one creates a couple metaballs using the (now working) mk_metaball() routine as a means to show creation, and next manipulation.  to be added is how to read them back in from the .g and combine them into one megametaball.
    svn:revision:35010
    svn:branch:trunk
    svn:account:brlcad

--- src/proc-db/metaball.c
+++ src/proc-db/metaball.c
@@ -17,10 +17,10 @@
  * License along with this file; see the file named COPYING for more
  * information.
  */
-
 /** @file metaball.c
  *
- * He slimed me!
+ * Some fun at dinner.
+ *
  */
 
 #include "common.h"
@@ -35,117 +35,94 @@
 #include "raytrace.h"
 #include "wdb.h"
 
-#define FMTSIZ 64
 
-static const char usage[] = "\
+static void
+make_meatballs(struct rt_wdb *fp, const char *name, int count)
+{
+    static float *ctx; /* random context */
+    static const int method = 1; /* 1==ISO */
+    static const fastf_t threshold = 1.0;
+    static const fastf_t SZ = 1000.0;
+
+    int i;
+    fastf_t **pts;
+
+    bn_rand_init(ctx, rand());
+    bu_log("Creating [%s] with %d random points\n", name, count);
+
+    pts = (fastf_t **)bu_calloc(count, sizeof(fastf_t*), "alloc metaball pts array");
+    for (i=0; i < count; i++) {
+	pts[i] = (fastf_t *)bu_malloc(sizeof(fastf_t)*5, "alloc metaball point");
+    }
+
+    /* create random metaball points */
+    for (i=0; i < count; i++) {
+	/* just for explicit clarity */
+	fastf_t x = bn_rand_half(ctx) * SZ;
+	fastf_t y = bn_rand_half(ctx) * SZ;
+	fastf_t z = bn_rand_half(ctx) * SZ;
+	fastf_t field_strength = bn_rand0to1(ctx) * SZ / sqrt(count);
+
+	VSET(pts[i], x, y, z);
+	pts[i][3] = field_strength; /* something blobbly random */
+	pts[i][4] = 0.0; /* not sweaty */
+    }
+
+    /* pack the meat */
+    mk_metaball(fp, name, count, method, threshold, pts);
+
+    /* free up our junk */
+    for (i=0; i < count; i++) {
+	bu_free(pts[i], "dealloc metaball point");
+    }
+    bu_free(pts, "dealloc metaball pts array");
+}
+
+
+static void
+mix_balls(struct rt_wdb *fp, const char *name, int ac, const char *av[])
+{
+    int i;
+    bu_log("Combining together the following metaballs:\n");
+    for (i = 0; i < ac; i++) {
+	bu_log("\t%s\n", av[i]);
+    }
+    bu_log("Joining balls together and creating [%s]\n", name);
+
+    /* TODO: show how to load and join metaballs together */
+}
+
+
+static void
+make_spaghetti(struct rt_wdb *fp, const char *name)
+{
+    const char *balls[3] = {"someballs.s", "moreballs.s", NULL};
+    make_meatballs(fp, balls[0], 100);
+    make_meatballs(fp, balls[1], 100);
+
+    mix_balls(fp, name, 2, balls);
+}
+
+
+int
+main(int argc, char *argv[])
+{
+    static const char usage[] = "\
 Usage:\n\
-\t%s [-h] [-n number of frames] [-m method] [-o outfile]\n\
+\t%s [-h] [-o outfile]\n\
 \n\
 \t-h\tShow help\n\
-\t-n #\tNumber of frames to (objects) to create\n\
-\t-m #\tMetaball render method (1 for iso, 2 for Blinn)\n\
-\t-t title\tTitle of geometry\n\
-\t-o file\tFile to write to\n\
-\t-g\tGravotronic animation\n\
-\t-c #\tNumber of control points (require Gravotronic mode)\n\
+\t-o file\tFile to write out\n\
 \n";
 
-/******************************************************************/
-
-/* hideous hack to generate a hideous dynamic format string for snprintf. *sob*/
-int
-mkfmt(char *fmt, char *prefix, char *postfix, int nframes)
-{
-    return snprintf(fmt, FMTSIZ, "%s%%0%dd%s", prefix, (int)ceil(log10((double)nframes)), postfix);
-}
-
-int
-bitronic(struct rt_wdb *outfp, int method, fastf_t threshold, int nframes)
-{
-    /*
-      char buf[BUFSIZ];
-    */
-    char fmt[FMTSIZ];
-    fastf_t step;
-    struct rt_metaball_internal *mb;
-
-    BU_GETSTRUCT(mb, rt_metaball_internal);
-    mb->magic = RT_METABALL_INTERNAL_MAGIC;
-    mb->threshold = threshold > 0 ? threshold : 1.0;
-    mb->method = method >= 0 ? method : 0;	/* default to Blinn blob */
-
-    step = 2.0 / (double) nframes;
-
-    mkfmt(fmt, "mball", ".s", nframes);
-
-#if 0
-    bleh[0].l->next = &bleh[1];
-    bleh[1].l->next = NULL;
-
-    while (nframes--) {
-	struct wdb_metaballpt *mbpt;
-	snprintf(buf, BUFSIZ, fmt, nframes);
-#define PT(B, X, Y, Z, FLDSTR, GOO)	B.type = WDB_METABALLPT_TYPE_POINT; B.fldstr = FLDSTR; B.sweat = GOO; VSET(B.coord, X, Y, Z);
-	BU_GETSTRUCT(mbpt, wdb_metaballpt);
-	PT(bleh[0], - ((double)nframes) * step, 0, 0, 1, 1);
-	PT(bleh[1], ((double)nframes) * step, 0, 0, 1, 1);
-#undef PT
-	BU_LIST_INSERT(&mb->metaball_ctrl_head, &mbpt->l);
-	/*
-	  mk_metaball(outfp, buf, 2, method, threshold, bleh);
-	*/
-	printf("%s\t%f %f\n", buf, - ((double)nframes) * step, ((double)nframes) * step);
-    }
-#endif
-    return EXIT_FAILURE;
-}
-
-int
-gravotronic(struct rt_wdb *outfp, int method, fastf_t threshold, int nframes, int count)
-{
-    char buf[BUFSIZ], fmt[FMTSIZ];
-
-    mkfmt(fmt, "mball", ".s", nframes);
-    snprintf(buf, BUFSIZ, fmt, nframes);
-    /* probably need a list or something
-       BU_LIST_INIT(&head.l);
-    */
-    
-    /* do something
-       mk_lfcomb(outfp, "mball.g", &head, 0);
-    */
-
-    return EXIT_FAILURE;
-}
-
-/******************************************************************/
-
-int
-main(int argc, char **argv)
-{
     char title[BUFSIZ] = "metaball", outfile[MAXPATHLEN] = "metaball.g";
-    int nframes = 1, method = 1, optc, gravotron = 0, count = 0, retval = EXIT_SUCCESS;
+    int optc = 0, retval = EXIT_SUCCESS;
     struct rt_wdb *outfp;
 
-    while ((optc = bu_getopt(argc, argv, "Hhm:n:o:t:")) != -1)
+    while ((optc = bu_getopt(argc, argv, "Hho:")) != -1)
 	switch (optc) {
-	    case 'n' :
-		nframes = atoi(bu_optarg);
-		break;
-	    case 'm' :
-		method = atoi(bu_optarg);
-		break;
-	    case 't':
-		snprintf(title, BUFSIZ, "%s", bu_optarg);;
-		break;
 	    case 'o':
 		snprintf(outfile, MAXPATHLEN, "%s", bu_optarg);;
-		break;
-	    case 'g':
-		++gravotron;
-		break;
-	    case 'c':
-		count = atoi(bu_optarg);
 		break;
 	    case 'h' :
 	    case 'H' :
@@ -154,38 +131,23 @@ main(int argc, char **argv)
 		return optc == '?' ? EXIT_FAILURE : EXIT_SUCCESS;
 	}
 
-    /* sanity checking on the various parameters */
-
-    if (count && !gravotron) {
-	fprintf(stderr, "Count only works in gravotronic mode. Turning on the gravitrons.\n");
-	++gravotron;
+    if (bu_file_exists(outfile)) {
+	bu_exit(EXIT_FAILURE, "ERROR: %s already exists.  Remove file and try again.", outfile);
     }
-	
-    /* prepare for DB generation */
 
+    /* prepare for guests */
     outfp = wdb_fopen(outfile);
     if (outfp == RT_WDB_NULL) {
-	perror("Failed to open file for writing. Aborting.\n");
+	perror("Failed to open file for writing.  Aborting.\n");
 	return EXIT_FAILURE;
     }
     mk_id(outfp, title);
 
-    /* here we go! */
-
-    if (gravotron)
-	retval = gravotronic(outfp, method, 1.0, nframes, count);
-    else
-	retval = bitronic(outfp, method, 1.0, nframes);
+    /* make dinner */
+    make_spaghetti(outfp, "meatballs.s");
 
     /* and clean up */
-
     wdb_close(outfp);
-
-    if (retval != EXIT_SUCCESS) {
-	fprintf(stderr, "Removing \"%s\".\n", outfile);
-	if (unlink(outfile) == -1)
-	    perror("Failure removing: ");
-    }
 
     return retval;
 }
