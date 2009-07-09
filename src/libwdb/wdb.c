971ca775c618e8c7ffb63f7939837d325f3816ad
Christopher Sean Morrison <brlcad@gmail.com>
2009-07-09 00:41:54 +0000

    missed a commit.. have to initialize the ctrl head else bad things happen
    svn:revision:35021
    svn:branch:trunk
    svn:account:brlcad

--- src/libwdb/wdb.c
+++ src/libwdb/wdb.c
@@ -726,6 +726,7 @@ mk_metaball(
     mb->magic = RT_METABALL_INTERNAL_MAGIC;
     mb->threshold = threshold > 0 ? threshold : 1.0;
     mb->method = method >= 0 ? method : 2;	/* default to Blinn blob */
+    BU_LIST_INIT(&mb->metaball_ctrl_head);
 
     for (i = 0; i < nctlpt; i++) {
 	if( rt_metaball_add_point (mb, (const point_t *)verts[i], verts[i][3], verts[i][4]) != 0 ) {
