aa9e3d28f60c3f61af2f524967d0909824bfe9ac
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2009-07-23 00:38:06 +0000

    Add some output options to bbsize, allow user to specify multiple objects and build the composite bounding box for all of them, add a man page
    svn:revision:35254
    svn:branch:trunk
    svn:account:starseeker

--- src/libged/bbsize.c
+++ src/libged/bbsize.c
@@ -39,7 +39,14 @@ int
 ged_bbsize(struct ged *gedp, int argc, const char *argv[])
 {
     point_t rpp_min, rpp_max;
-    int	use_air = 0;
+    point_t obj_min, obj_max;
+    int c;
+    int	use_air = 1;
+    int print_header = 1;
+    int print_rpp = 0;
+    int print_dim = 0;
+    int print_vol = 0;
+    int i;
     static const char *usage = "object";
     const char *str;
     double xlen;
@@ -65,24 +72,83 @@ ged_bbsize(struct ged *gedp, int argc, const char *argv[])
 	return GED_ERROR;
     }
 
-    /* look for a USEAIR option */
-    if ( ! strcmp(argv[1], "-u") )
-	use_air = 1;
+    bu_optind = 1;      /* re-init bu_getopt() */
+    while ((c = bu_getopt(argc, (char * const *)argv, "dequv")) != EOF) {
+	switch (c) {
+	    case 'd':
+		print_dim = 1;
+		break;
+	    case 'e':
+		print_rpp = 1;
+		break;
+	    case 'q':
+		print_header = 0;
+		break;
+	    case 'u':
+		use_air = 0;
+		break;
+	    case 'v':
+		print_vol = 1;
+		break;
+	    default:
+		bu_vls_printf(&gedp->ged_result_str, "Unrecognized option - %c", c);
+		return GED_ERROR;
+	}
+    }
 
-    if (ged_get_obj_bounds(gedp, argc-1, (const char **)argv+1, use_air, rpp_min, rpp_max) == GED_ERROR)
-	return GED_ERROR;
+    /* Don't want to print NO info, so default to dim and vol printout if nothing specified. */
+    if (print_rpp == 0 && print_vol == 0 && print_dim == 0) {
+	print_dim = 1;
+       	print_vol = 1;
+    }
+
+    /* skip options processed plus command name, should just leave object names */
+    argc -= bu_optind;
+    argv += bu_optind;
+
+    VSETALL(rpp_min, MAX_FASTF);
+    VSETALL(rpp_max, -MAX_FASTF);
+    for (i = 0; i < argc; i++) {
+    	if (ged_get_obj_bounds(gedp, argc - i, (const char **)argv+i, use_air, obj_min, obj_max) == GED_ERROR)
+    	    return GED_ERROR;
+	VMINMAX(rpp_min, rpp_max, (double *)obj_min);
+	VMINMAX(rpp_min, rpp_max, (double *)obj_max);
+    }
 
     /* Report Bounding Box Information */
     str = bu_units_string(gedp->ged_wdbp->dbip->dbi_local2base);
     if (!str) str = "Unknown_unit";
-    bu_vls_printf(&gedp->ged_result_str, "Object %s Bounding Box Dimensions:\n", argv[1]);
-    xlen = fabs(rpp_max[X] - rpp_min[X])*gedp->ged_wdbp->dbip->dbi_base2local;
-    ylen = fabs(rpp_max[Y] - rpp_min[Y])*gedp->ged_wdbp->dbip->dbi_base2local;
-    zlen = fabs(rpp_max[Z] - rpp_min[Z])*gedp->ged_wdbp->dbip->dbi_base2local;
-    bu_vls_printf(&gedp->ged_result_str, "X Length: %.1f %s\nY Length: %.1f %s\nZ Length: %.1f %s\n", xlen, str, ylen, str, zlen, str); 
-    vol = xlen * ylen * zlen;
-    bu_vls_printf(&gedp->ged_result_str, "Bounding Box Volume: %.1f %s^3\n", vol, str);
-   
+
+    // Print Header
+    if (print_header == 1) {
+    	bu_vls_printf(&gedp->ged_result_str, "Bounding Box Dimensions, Object(s)");
+	for (i = 0; i < argc - 1 ; i++) {
+	    bu_vls_printf(&gedp->ged_result_str, " %s, ", argv[i]);
+	}
+	bu_vls_printf(&gedp->ged_result_str, " %s:\n", argv[argc-1]);
+    }
+
+    // Print rpp
+    if (print_rpp == 1) {
+	bu_vls_printf(&gedp->ged_result_str, "min {%f %f %f} max {%f %f %f}\n", rpp_min[0], rpp_min[1], rpp_min[2], rpp_max[0], rpp_max[1], rpp_max[2]);
+    }
+
+    // Print dim info
+    if (print_dim == 1) {
+    	xlen = fabs(rpp_max[X] - rpp_min[X])*gedp->ged_wdbp->dbip->dbi_base2local;
+    	ylen = fabs(rpp_max[Y] - rpp_min[Y])*gedp->ged_wdbp->dbip->dbi_base2local;
+    	zlen = fabs(rpp_max[Z] - rpp_min[Z])*gedp->ged_wdbp->dbip->dbi_base2local;
+    	bu_vls_printf(&gedp->ged_result_str, "X Length: %.1f %s\nY Length: %.1f %s\nZ Length: %.1f %s\n", xlen, str, ylen, str, zlen, str); 
+    }
+
+    if (print_vol == 1) {
+	xlen = fabs(rpp_max[X] - rpp_min[X])*gedp->ged_wdbp->dbip->dbi_base2local;
+    	ylen = fabs(rpp_max[Y] - rpp_min[Y])*gedp->ged_wdbp->dbip->dbi_base2local;
+    	zlen = fabs(rpp_max[Z] - rpp_min[Z])*gedp->ged_wdbp->dbip->dbi_base2local;
+    	vol = xlen * ylen * zlen;
+    	bu_vls_printf(&gedp->ged_result_str, "Bounding Box Volume: %.1f %s^3\n", vol, str);
+    }
+
     return GED_OK;
 }
 
