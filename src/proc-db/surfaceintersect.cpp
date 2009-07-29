0349e7e5fab4279429bc375453f900a931bdafee
Joe Doliner <jdoliner@gmail.com>
2009-07-29 16:47:11 +0000

    Implemented surfacesurfaceintersect which facilitates finding starting points of intersection, walking intersections and removing starting points once they've been crossed.
    svn:revision:35379
    svn:branch:trunk
    svn:account:jdoliner

--- src/proc-db/surfaceintersect.cpp
+++ src/proc-db/surfaceintersect.cpp
@@ -108,8 +108,7 @@ double Jiggle(
 	double *s1,
 	double *s2,
 	double *t1,
-	double *t2,
-	double stepsize
+	double *t2
 	)
 {
     ON_3dPoint p1 = surf1->PointAt(*s1, *t1);
@@ -133,35 +132,61 @@ void WalkIntersection(
 	double t1,
 	double t2,
 	double stepsize,
+	double tol,
 	ON_BezierCurve *out
 	)
 {
-    ON_Polyline intersectionPoints;
+    ON_3dPointArray intersectionPoints;
     double olds1, olds2, oldt1, oldt2;
-    while (surf1->Domain(0).Includes(s1, true) && surf1->Domain(1).Includes(t1, true) && surf2->Domain(0).Includes(s2, true) && surf2->Domain(1).Includes(t2, true) && !intersectionPoints.IsClosed(stepsize)) {
-	intersectionPoints.Append(surf1->PointAt(s1, t1));
-	olds1 = s1;
-	olds2 = s2;
-	oldt1 = t1;
-	oldt2 = t2;
-	Step(surf1, surf2, &s1, &s2, &t1, &t2, stepsize);
+    double inits1 = s1, inits2 = s2, initt1 = t1, initt2 = t2;
+    double distance;
+    int passes;
+    /* this function is meant to be called with an arbitrary point on the curve
+       and return the entire curve, regardless of which point was passed in,
+       this means that if the intersection doesn't happen to be a loop,
+       we need to walk both directions from the initial point. */
+    for (passes = 0; passes < 2; passes++) {
+	while (surf1->Domain(0).Includes(s1, true) && surf1->Domain(1).Includes(t1, true) && surf2->Domain(0).Includes(s2, true) && surf2->Domain(1).Includes(t2, true) && !ON_Polyline(intersectionPoints).IsClosed(stepsize)) {
+	    do {
+		distance = Jiggle(surf1, surf2, &s1, &s2, &t1, &t2);
+	    } while (distance > tol);
+	    intersectionPoints.Append(surf1->PointAt(s1, t1));
+	    olds1 = s1;
+	    olds2 = s2;
+	    oldt1 = t1;
+	    oldt2 = t2;
+	    if (passes == 0) {
+		Step(surf1, surf2, &s1, &s2, &t1, &t2, stepsize);
+	    } else {
+		Step(surf1, surf2, &s1, &s2, &t1, &t2, -stepsize);
+	    }
+	}
+	if (!ON_Polyline(intersectionPoints).IsClosed(stepsize)) {
+	    /* we stepped off the edge of our domain, we need to get a point right on the edge */
+	    double news1, news2, newt1, newt2;
+	    news1 = ClosestValue(s1, surf1->Domain(0));
+	    news2 = ClosestValue(s2, surf2->Domain(1));
+	    newt1 = ClosestValue(t1, surf1->Domain(0));
+	    newt2 = ClosestValue(t2, surf2->Domain(1));
+	    news1 = olds1 + MIN((news1 - olds1) / (s1 - olds1), (newt1 - oldt1) / (t1 - oldt1)) * (s1 - olds1);
+	    news2 = olds2 + MIN((news2 - olds2) / (s2 - olds2), (newt2 - oldt2) / (t2 - oldt2)) * (s2 - olds2);
+	    newt1 = oldt1 + MIN((news1 - olds1) / (s1 - olds1), (newt1 - oldt1) / (t1 - oldt1)) * (t1 - oldt1);
+	    newt2 = oldt2 + MIN((news2 - olds2) / (s2 - olds2), (newt2 - oldt2) / (t2 - oldt2)) * (t2 - oldt2);
+	    double newstep = MIN(surf1->PointAt(olds1, oldt1).DistanceTo(surf1->PointAt(news1, newt1)), surf2->PointAt(olds2, oldt2).DistanceTo(surf2->PointAt(news2, newt2)));
+	    Step(surf1, surf2, &s1, &s2, &t1, &t2, newstep);
+	    intersectionPoints.Append(surf1->PointAt(s1, t1));
+	}
+	if (ON_Polyline(intersectionPoints).IsClosed(stepsize)) {
+	    break; /* we're done, no second pass required */
+	} else {
+	    intersectionPoints.Reverse(); /* this is a bit cute, we are assured to hit this part on the second pass, so we either reverse 0 or 2 times */
+	    s1 = inits1;
+	    s2 = inits2;
+	    t1 = initt1;
+	    t2 = initt2;
+	}
     }
-    if (!intersectionPoints.IsClosed(stepsize)) {
-	/* we stepped off the edge of our domain, we need to get a point right on the edge */
-	double news1, news2, newt1, newt2;
-	news1 = ClosestValue(s1, surf1->Domain(0));
-	news2 = ClosestValue(s2, surf2->Domain(1));
-	newt1 = ClosestValue(t1, surf1->Domain(0));
-	newt2 = ClosestValue(t2, surf2->Domain(1));
-	news1 = olds1 + MIN((news1 - olds1) / (s1 - olds1), (newt1 - oldt1) / (t1 - oldt1)) * (s1 - olds1);
-	news2 = olds2 + MIN((news2 - olds2) / (s2 - olds2), (newt2 - oldt2) / (t2 - oldt2)) * (s2 - olds2);
-	newt1 = oldt1 + MIN((news1 - olds1) / (s1 - olds1), (newt1 - oldt1) / (t1 - oldt1)) * (t1 - oldt1);
-	newt2 = oldt2 + MIN((news2 - olds2) / (s2 - olds2), (newt2 - oldt2) / (t2 - oldt2)) * (t2 - oldt2);
-	double newstep = MIN(surf1->PointAt(olds1, oldt1).DistanceTo(surf1->PointAt(news1, newt1)), surf2->PointAt(olds2, oldt2).DistanceTo(surf2->PointAt(news2, newt2)));
-	Step(surf1, surf2, &s1, &s2, &t1, &t2, newstep);
-	intersectionPoints.Append(surf1->PointAt(s1, t1));
-    }
-    *out = ON_BezierCurve(intersectionPoints);
+    out = new ON_BezierCurve((ON_3dPointArray) intersectionPoints);
 }
 
 bool GetStartPoints(
@@ -177,8 +202,12 @@ bool GetStartPoints(
 	return_value = false;
     } else if (surf1->IsPlanar(NULL, tol) && surf2->IsPlanar(NULL, tol)) {
 	if (!surf1->BoundingBox().IsDisjoint(surf2->BoundingBox())) {
-	    start_points1.Append(ON_2dPoint(surf1->Domain(0).Mid(), surf1->Domain(1).Mid()));
-	    start_points2.Append(ON_2dPoint(surf2->Domain(0).Mid(), surf2->Domain(1).Mid()));
+	    double distance, s1 = surf1->Domain(0).Mid(), s2 = surf2->Domain(0).Mid(), t1 = surf1->Domain(1).Mid(), t2 = surf2->Domain(1).Mid();
+	    do {
+		distance = Jiggle(surf1, surf2, &s1, &s2, &t1, &t2);
+	    } while (distance > tol);
+	    start_points1.Append(ON_2dPoint(s1, t1));
+	    start_points2.Append(ON_2dPoint(s2, t2));
 	} else {
 	    return_value = false;
 	}
@@ -198,6 +227,42 @@ bool GetStartPoints(
 	    }
 	}
     }
+}
+
+bool SurfaceSurfaceIntersect(
+	ON_Surface *surf1,
+	ON_Surface *surf2,
+	double stepsize,
+	double tol,
+	ON_SimpleArray<ON_BezierCurve>& curves
+	)
+{
+    ON_2dPointArray start_points1, start_points2;
+    if (!GetStartPoints(surf1, surf2, start_points1, start_points2, tol)) {
+	return false;
+    }
+    int i, j;
+    ON_BezierCurve *out;
+    ON_2dPoint start1, start2;
+    for (i = 0; i < start_points1.Count(); i++) {
+	start1 = *start_points1.First();
+	start2 = *start_points2.First();
+	WalkIntersection(surf1, surf2, start1[0], start2[0], start1[1], start2[1], stepsize, tol, out);
+	start_points1.Remove(0);
+	start_points2.Remove(0);
+	for (j = 1; j < start_points1.Count(); j++) {
+	    ON_3dPoint p1 = surf1->PointAt(start_points1[j][0], start_points1[j][1]);
+	    ON_3dPoint p2 = surf2->PointAt(start_points2[j][0], start_points2[j][1]);
+	    double dummy;
+	    if (out->GetClosestPoint(p1, &dummy, tol, NULL) || out->GetClosestPoint(p2, &dummy, tol, NULL)) {
+		start_points1.Remove(j);
+		start_points2.Remove(j);
+		j--;
+	    }
+	}
+	curves.Append(*out);
+    }
+    return true;
 }
 
 int main()
