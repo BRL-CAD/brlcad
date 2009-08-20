2523bdf058b4348d3fc6cb0c1fb046a0bc8bdee1
Robert Parker <unacknowledged@users.noreply.github.com>
2009-08-20 15:11:13 +0000

    Added the Utility class. Not much here yet. At the moment it simply provides a class for Utility plugins to inherit from.
    svn:revision:35657
    svn:branch:trunk
    svn:account:bob1961

---
+++ src/tclscripts/archer/Utility.tcl
@@ -0,0 +1,35 @@
+#                      U T I L I T Y . T C L
+# BRL-CAD
+#
+# Copyright (c) 2002-2009 United States Government as represented by
+# the U.S. Army Research Laboratory.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public License
+# version 2.1 as published by the Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this file; see the file named COPYING for more
+# information.
+#
+###
+
+::itcl::class Utility {
+    inherit itk::Widget
+
+    constructor {args} {}
+    destructor {}
+
+    public {
+	common utilityMajorType ""
+	common utilityMinorType ""
+	common utilityName ""
+	common utilityVersion ""
+	common utilityClass "Utility"
+    }
+}
