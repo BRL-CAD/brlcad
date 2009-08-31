dd989e521a638878916389ee9973853c5a65fe76
Christopher Sean Morrison <brlcad@gmail.com>
2009-08-31 19:23:42 +0000

    add standard header/footers.
    svn:revision:35787
    svn:branch:trunk
    svn:account:brlcad

--- src/conv/iges/brlcad.hpp
+++ src/conv/iges/brlcad.hpp
@@ -1,3 +1,28 @@
+/*                      B R L C A D . H P P
+ * BRL-CAD
+ *
+ * Copyright (c) 2008-2009 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file brlcad.hpp
+ *
+ * Brief description
+ *
+ */
+
 #ifndef __BRLCAD_HPP
 #define __BRLCAD_HPP
 
@@ -106,3 +131,12 @@ namespace brlcad {
 }
 
 #endif
+
+// Local Variables:
+// tab-width: 8
+// mode: C++
+// c-basic-offset: 4
+// indent-tabs-mode: t
+// c-file-style: "stroustrup"
+// End:
+// ex: shiftwidth=4 tabstop=8
--- src/conv/iges/brlcad_brep.cpp
+++ src/conv/iges/brlcad_brep.cpp
@@ -1,3 +1,28 @@
+/*                 B R L C A D _ B R E P . C P P
+ * BRL-CAD
+ *
+ * Copyright (c) 2008-2009 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file brlcad_brep.cpp
+ *
+ * Brief description
+ *
+ */
+
 #include <assert.h>
 
 #include "brlcad.hpp"
@@ -423,3 +448,12 @@ namespace brlcad {
   BRLCADBrepHandler::handleOffsetCurve() { return 0; }
 
 }
+
+// Local Variables:
+// tab-width: 8
+// mode: C++
+// c-basic-offset: 4
+// indent-tabs-mode: t
+// c-file-style: "stroustrup"
+// End:
+// ex: shiftwidth=4 tabstop=8
