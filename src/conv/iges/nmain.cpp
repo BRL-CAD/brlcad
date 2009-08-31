6275c8bc310a3eb603fdd8864a36b534a351d085
Christopher Sean Morrison <brlcad@gmail.com>
2009-08-31 19:26:12 +0000

    add standard header/footer.
    svn:revision:35788
    svn:branch:trunk
    svn:account:brlcad

--- src/conv/iges/nmain.cpp
+++ src/conv/iges/nmain.cpp
@@ -1,23 +1,62 @@
+/*                       N M A I N . C P P
+ * BRL-CAD
+ *
+ * Copyright (c) 2008-2009 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file nmain.cpp
+ *
+ * Brief description
+ *
+ */
+
+#include "common.h"
+
 #include <iostream>
+
 #include "n_iges.hpp"
 #include "brlcad.hpp"
+
 
 using namespace brlcad;
 
 int
 main(int argc, char** argv) {
-  cout << argc << endl;
-  if (argc != 3) {
-    bu_exit(0, "iges <iges_filename> <output_filename>\n");
-  }
+    cout << argc << endl;
+    if (argc != 3) {
+	bu_exit(0, "iges <iges_filename> <output_filename>\n");
+    }
 
-  string file(argv[1]);
-  IGES iges(file);
+    string file(argv[1]);
+    IGES iges(file);
 
-  BRLCADBrepHandler bh;
-  iges.readBreps(&bh);
-  string out(argv[2]);
-  bh.write(out);
+    BRLCADBrepHandler bh;
+    iges.readBreps(&bh);
+    string out(argv[2]);
+    bh.write(out);
 
-  return 0;
+    return 0;
 }
+
+
+// Local Variables:
+// tab-width: 8
+// mode: C++
+// c-basic-offset: 4
+// indent-tabs-mode: t
+// c-file-style: "stroustrup"
+// End:
+// ex: shiftwidth=4 tabstop=8
