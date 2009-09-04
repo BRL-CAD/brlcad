539410350611e5472641379f45a671561118b3ae
Robert Parker <unacknowledged@users.noreply.github.com>
2009-09-04 15:47:58 +0000

    No longer enabling two sided lighting.
    svn:revision:35849
    svn:branch:trunk
    svn:account:bob1961

--- src/libdm/dm-ogl.c
+++ src/libdm/dm-ogl.c
@@ -1637,7 +1637,9 @@ ogl_setLight(struct dm *dmp, int lighting_on)
 	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, amb_three);
 	glLightModeli(GL_LIGHT_MODEL_LOCAL_VIEWER, GL_FALSE);
 
+#if 0
 	glLightModeli(GL_LIGHT_MODEL_TWO_SIDE, GL_TRUE);
+#endif
 	glLightfv(GL_LIGHT0, GL_DIFFUSE, light0_diffuse);
 	glLightfv(GL_LIGHT0, GL_SPECULAR, light0_diffuse);
 
