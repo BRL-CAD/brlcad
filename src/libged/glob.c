ede78be2d9cce36c7952ff96626e0b8bd4a1ddd3
Christopher Sean Morrison <brlcad@gmail.com>
2009-09-17 11:35:15 +0000

    first example use of GED_INITIALIZED() to make the database open check return status conditional on the database being initialized.
    svn:revision:35935
    svn:branch:trunk
    svn:account:brlcad

--- src/libged/glob.c
+++ src/libged/glob.c
@@ -119,8 +119,7 @@ ged_glob(struct ged *gedp, int argc, const char *argv[])
 	   it to the database. */
 
 	if (regexp) {
-	    if (!(gedp->ged_wdbp)) return GED_OK;
-	    GED_CHECK_DATABASE_OPEN(gedp, GED_ERROR);
+	    GED_CHECK_DATABASE_OPEN(gedp, GED_INITIALIZED(gedp) ? GED_ERROR : GED_OK);
 	    bu_vls_trunc( &temp, 0 );
 	    if ( db_regexp_match_all( &temp, gedp->ged_wdbp->dbip,
 				      bu_vls_addr(&word) ) == 0 ) {
