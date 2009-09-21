283265401c7f8407501fd6e7ca25889bec380794
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2009-09-21 14:56:11 +0000

    Erm, this should be pulling the magnitude of the vector, not the x coordinate...
    svn:revision:35965
    svn:branch:trunk
    svn:account:starseeker

--- src/librt/primitives/sph/sph_brep.cpp
+++ src/librt/primitives/sph/sph_brep.cpp
@@ -42,7 +42,7 @@ rt_sph_brep(ON_Brep **b, const struct rt_db_internal *ip, const struct bn_tol *)
     tip = (struct rt_ell_internal *)ip->idb_ptr;
     RT_ELL_CK_MAGIC(tip);
     
-    ON_Sphere sph(tip->v, tip->a[0]);
+    ON_Sphere sph(tip->v, MAGNITUDE(tip->a));
     *b = ON_BrepSphere(sph);
 }
 
