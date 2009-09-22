64329b90e81b6ee06d4ae13319c9812bbbf9d7ad
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2009-09-22 13:48:08 +0000

    Start to stub out what is needed for testing a sketch brep conversion.
    svn:revision:35972
    svn:branch:trunk
    svn:account:starseeker

---
+++ src/librt/primitives/sketch/sketch_brep.cpp
@@ -0,0 +1,317 @@
+/*                    S K E T C H _ B R E P . C P P
+ * BRL-CAD
+ *
+ * Copyright (c) 2008-2009 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file sketch_brep.cpp
+ *
+ * Convert a sketch to b-rep form (does not creat a solid brep)
+ *
+ */
+
+#include "common.h"
+
+#include "raytrace.h"
+#include "rtgeom.h"
+#include "brep.h"
+
+/*
+void FindLoops(struct rt_sketch_internal *sk, ON_SimpleArray<ON_SimpleArray<genptr_t *> *> *loops) {
+    ON_SimpleArray<genptr_t *> allsegments;
+    long                *lng;
+    struct line_seg     *lsg;
+    struct carc_seg     *csg;
+    struct bezier_seg   *bsg;
+    int currentstart, currentend;
+    int foundfirst = 0;
+    for (int i = 0; i < sk->skt_curve.seg_count; i++) {
+	allsegments.Append(sk->skt_curve.segments[i]);
+    }
+    int crvcnt = 0;
+    while ((foundfirst != 1) && (crvcnt < allsegments.Count())) {
+	    lng = (long *)allsegments[crvcnt];
+	    crvcnt++;
+	    switch (*lng) {
+    	    	case CURVE_LSEG_MAGIC:
+		    lsg = (struct line_seg *)lng;
+		    int currentstart = lsg->start;
+		    int currentend = lsg->end;
+		    foundfirst == 1;
+		    break;
+		case CURVE_CARC_MAGIC:
+     		    csg = (struct carc_seg *)lng;
+		    int currentstart = csg->start;
+		    int currentend = csg->end;
+		    foundfirst == 1;
+		    break;
+		case CURVE_BEZIER_MAGIC:
+     		    bsg = (struct bezier_seg *)lng;
+		    int currentstart = bsg->ctl_points[0];
+		    int currentend = bsg->ctl_points[bsg->degree];    
+		    foundfirst == 1;
+		    break;
+	    	default:
+    		    bu_log("Unhandled sketch type - FindLoops\n");
+		    foundfirst == 0;
+    		break;
+	    }
+    }
+    loops->AppendNew();
+    loops[0]->Append(allsegments[0]);
+    allsegments.Remove(0);
+    while (allsegments.Count() > 0) {
+	int j = 0;
+	int current_loop = loops->Count() - 1;
+	while (allsegments.Count() > 0 && j < allsegments.Count()) {
+	    lng = (long *)allsegments[j];
+	    switch (*lng) {
+		case CURVE_LSEG_MAGIC:
+		    lsg = (struct line_seg *)lng;
+		    if ((currentstart == lsg->end) || (currentend == lsg->start)) {
+			if (currentstart == lsg->end) currentstart = lsg->start;
+			if (currentend == lsg->end) currentend = lsg->end;
+			loops[current_loop]->Append(allsegments[j]);
+			allsegments.Remove(j);
+			j == -1;
+		    }
+		    j++;
+		    break;
+		case CURVE_CARC_MAGIC:
+		    csg = (struct carc_seg *)lng;
+		    if ((csg->orientation == 0) &&  ((currentstart == csg->end) || (currentend == csg->start))) {
+			if (currentstart == csg->end) currentstart = csg->start;
+			if (currentend == csg->end) currentend = csg->end;
+			loops[current_loop]->Append(allsegments[j]);
+			allsegments.Remove(j);
+			j == -1;
+		    }
+		    if ((csg->orientation != 0) &&  ((currentstart == csg->start) || (currentend == csg->end))) {
+			if (currentstart == csg->start) currentstart = csg->start;
+			if (currentend == csg->start) currentend = csg->end;
+			loops[current_loop]->Append(allsegments[j]);
+			allsegments.Remove(j);
+			j == -1;
+		    }
+		    j++;
+		    break;
+		case CURVE_BEZIER_MAGIC:
+		    bsg = (struct bezier_seg *)lng;
+		    if ((currentstart == bsg->end) || (currentend == bsg->start)) {
+			if (currentstart == bsg->end) currentstart = bsg->start;
+			if (currentend == bsg->end) currentend = bsg->end;
+			loops[current_loop]->Append(allsegments[j]);
+			allsegments.Remove(j);
+			j == -1;
+		    }
+		    j++;
+		    break;
+		default:
+		    bu_log("Unhandled sketch type - FindLoops\n");
+		    j++;
+		    break;
+	    }
+	}
+	if (allsegments.Count != 0) loops->AppendNew();
+	loops[0]->Append(allsegments[0]);
+	allsegments.Remove(0);
+    }	
+ */
+
+
+/**
+ *			R T _ S K E T C H _ B R E P
+ */
+extern "C" void
+rt_sketch_brep(ON_Brep **b, const struct rt_db_internal *ip, const struct bn_tol *tol)
+{
+   /*
+    struct rt_sketch_internal	*eip;
+
+    *b = NULL; 
+
+    RT_CK_DB_INTERNAL(ip);
+    eip = (struct rt_sketch_internal *)ip->idb_ptr;
+    RT_SKETCH_CK_MAGIC(eip);
+
+    *b = new ON_Brep();
+
+
+    ON_TextLog dump_to_stdout;
+    ON_TextLog* dump = &dump_to_stdout;
+    
+    ON_3dPoint plane_origin;
+    ON_3dVector plane_x_dir, plane_y_dir;
+    
+    //  First, find plane in 3 space corresponding to the sketch.
+    
+    plane1_origin = ON_3dPoint(eip->V);
+    plane_x_dir = ON_3dVector(eip->u_vec);
+    plane_y_dir = ON_3dVector(eip->v_vec);
+    const ON_Plane* sketch_plane = new ON_Plane(plane1_origin, plane_x_dir, plane_y_dir); 
+    ON_PlaneSurface sketch_surf = ON_Plane::New(sketch_plane);
+  
+    //  For the brep, need the list of 3D vertex points.  In sketch, they
+    //  are stored as 2D points. Also, save the min and max for the purposes
+    //  of setting the extents.
+    point_t pt, pmin, pmax;
+    VMOVE(pmin, eip->V);
+    VSETALL(pmax, -INT_MAX);
+    for (int i = 0; i < eip->vert_count; i++) {
+	VJOIN2(pt, eip->V, eip->verts[i][0], eip->u_vec, eip->verts[i][1], eip->v_vec);
+	VMINMAX(pmin, pmax, pt);
+	(*b)->NewVertex(pt, SMALL_FASTF);
+    }
+
+    // Once the vertices are added, set the extents of the surface to ensure
+    // it will hold all the vertices.
+    vect_t vect_min, vect_max;
+    double umin, umax, vmin, vmax;
+    VSUB2(vect_min, pmin, eip->V);
+    VSUB2(vect_max, pmax, eip->V);
+    VPROJECT(vect_min, plane_x_dir, umin, vmin);
+    VPROJECT(vect_max, plane_x_dir, umax, vmax);
+    sketch_surf->SetDomain(0, umin, umax);
+    sketch_surf->SetDomain(1, vmin, vmax);
+    sketch_surf->SetExtents(0, sketch_surf->Domain(0));
+    sketch_surf->SetExtents(1, sketch_surf->Domain(1));
+
+    // Now, append the surface to the brep surface list
+    (*b)->m_S.Append(sketch_surf);
+
+    // For the purposes of BREP creation, it is necessary to identify
+    // loops created by sketch segments.  This information is not stored
+    // in the sketch data structures themselves, and thus must be deduced
+    ON_SimpleArray<ON_SimpleArray<struct curve*> *> loops;
+    FindLoops(eip, loops);
+
+    // Create the brep elements corresponding to the sketch lines, curves
+    // and bezier segments
+    struct line_seg *lsg;
+    struct carc_seg *csg;
+    struct bezier_seg *bsg;
+    ON_SimpleArray<ON_Curve*> boundary;
+    struct curve *crv = &eip->skt_curve;
+    long *lng;
+    for (int i = 0; i < crv->seg_count; i++) {
+	lng = (long *)crv->segments[i];
+	switch (*lng) {
+	    case CURVE_LSEG_MAGIC:
+		lsg = (struct line_seg *)lng;
+		ON_Curve* c3d = new ON_LineCurve((*b)->m_V[lsg->start].Point(), (*b)->m_V[lsg->end].Point());
+		boundary.Append(c3d);
+		break;
+	    case CURVE_CARC_MAGIC:
+		csg = (struct carc_seg *)lng;
+		if (csg->radius < 0) {
+		    ON_Circle* c3dcirc = new ON_Circle();
+		    ON_Curve* c3d = new ON_ArcCurve();
+		} else {
+		    ON_Arc* c3darc = new ON_Arc();
+    		    ON_Curve* c3d = new ON_ArcCurve();
+		}
+		boundary.Append(c3d);
+		break;
+	    case CURVE_BEZIER_MAGIC:
+		bsg = (struct bezier_seg *)lng;
+		ON_Curve* c3d = new ON_BezierCurve();
+		boundary.Append(c3d);
+		break;
+	    default:
+		bu_log("Unhandled sketch object\n");
+		break;
+	}
+	c3d->SetDomain(0.0, 1.0);
+    	(*b)->m_C3.Append(c3d);
+    }
+
+    // Create a single brep face and loops in that face
+    const int bsi = (*b)->m_S.Count() - 1;
+    ON_BrepFace& bface = (*b)->NewFace(bsi);
+    (*b)->NewPlanarFaceLoop(bface.m_face_index, ON_BrepLoop::outer, boundary, true);
+    (*b)->SetTrimIsoFlags(bface);
+	     
+    
+    
+    //  Next, create a parabolic NURBS curve corresponding to the shape of
+    //  the parabola in the two planes.
+    point_t x_rev_dir, ep1, ep2, ep3, tmppt;
+    VREVERSE(x_rev_dir, x_dir);
+
+    VADD2(ep1, p1_origin, x_rev_dir);
+    VSCALE(tmppt, eip->rpc_B, 2);
+    VADD2(ep2, p1_origin, tmppt);
+    VADD2(ep3, p1_origin, x_dir);
+    ON_3dPoint onp1 = ON_3dPoint(ep1);
+    ON_3dPoint onp2 = ON_3dPoint(ep2);
+    ON_3dPoint onp3 = ON_3dPoint(ep3);
+
+
+    ON_NurbsCurve* parabnurbscurve = ON_NurbsCurve::New(3,false,3,3);
+    parabnurbscurve->SetKnot(0, 0);
+    parabnurbscurve->SetKnot(1, 0);
+    parabnurbscurve->SetKnot(2, 1);
+    parabnurbscurve->SetKnot(3, 1);
+    parabnurbscurve->SetCV(0,ON_3dPoint(ep1));
+    parabnurbscurve->SetCV(1,ON_3dPoint(ep2));
+    parabnurbscurve->SetCV(2,ON_3dPoint(ep3));
+    bu_log("Valid nurbs curve: %d\n", parabnurbscurve->IsValid(dump));
+    parabnurbscurve->Dump(*dump);
+
+    // Also need a staight line from the beginning to the end to
+    // complete the loop
+
+    ON_LineCurve* straightedge = new ON_LineCurve(onp3,onp1);   
+    bu_log("Valid curve: %d\n", straightedge->IsValid(dump));
+    straightedge->Dump(*dump);
+   
+    // Generate the bottom cap 
+   
+    ON_PlaneSurface* bp = new ON_PlaneSurface();
+    bp->m_plane = (*rpc_bottom_plane);
+    bp->SetDomain(0, -100.0, 100.0 );
+    bp->SetDomain(1, -100.0, 100.0 );
+    bp->SetExtents(0, bp->Domain(0) );
+    bp->SetExtents(1, bp->Domain(1) );
+    (*b)->m_S.Append(bp);
+    const int bsi = (*b)->m_S.Count() - 1;
+    ON_BrepFace& bface = (*b)->NewFace(bsi);
+    (*b)->NewPlanarFaceLoop(bface.m_face_index, ON_BrepLoop::outer, boundary, true); 
+    const ON_BrepLoop* bloop = (*b)->m_L.Last();
+    bp->SetDomain(0, bloop->m_pbox.m_min.x, bloop->m_pbox.m_max.x );
+    bp->SetDomain(1, bloop->m_pbox.m_min.y, bloop->m_pbox.m_max.y );
+    bp->SetExtents(0,bp->Domain(0));
+    bp->SetExtents(1,bp->Domain(1));
+    (*b)->SetTrimIsoFlags(bface);
+    
+    // Now the side face and top cap - extrude the bottom face and set the cap flag to true
+    vect_t vp2;
+    VADD2(vp2,eip->rpc_V, eip->rpc_H);
+    const ON_Curve* extrudepath = new ON_LineCurve(ON_3dPoint(eip->rpc_V), ON_3dPoint(vp2));
+    ON_Brep& brep = *(*b);
+    ON_BrepExtrudeFace(brep,0, *extrudepath, true);
+ */ 
+}
+
+// Local Variables:
+// tab-width: 8
+// mode: C++
+// c-basic-offset: 4
+// indent-tabs-mode: t
+// c-file-style: "stroustrup"
+// End:
+// ex: shiftwidth=4 tabstop=8
+
--- src/proc-db/csgbrep.cpp
+++ src/proc-db/csgbrep.cpp
@@ -26,6 +26,10 @@
 #include "common.h"
 #include "bu.h"
 #include "opennurbs.h"
+#include "rtgeom.h"
+#include "wdb.h"
+
+extern struct rt_sketch_internal *sketch_start();
 
 #define OBJ_BREP
 /* without OBJ_BREP, this entire procedural example is disabled */
@@ -52,6 +56,8 @@ extern "C" {
     extern void rt_ehy_brep(ON_Brep **bi, struct rt_db_internal *ip, const struct bn_tol *tol);
     extern void rt_tgc_brep(ON_Brep **bi, struct rt_db_internal *ip, const struct bn_tol *tol);
     extern void rt_tor_brep(ON_Brep **bi, struct rt_db_internal *ip, const struct bn_tol *tol);
+    extern void rt_sketch_brep(ON_Brep **bi, struct rt_db_internal *ip, const struct bn_tol *tol);
+    extern void rt_sketch_ifree(struct rt_db_internal *ip, struct resource *resp);
 
 #ifdef __cplusplus
 }
@@ -81,7 +87,7 @@ main(int argc, char** argv)
     outfp = wdb_fopen("csgbrep.g");
     const char* id_name = "CSG B-Rep Examples";
     mk_id(outfp, id_name);
-
+/*
     bu_log("Writing an ARB4 (via NMG) brep...\n");
     ON_Brep* arb4brep = ON_Brep::New();
     struct rt_arb_internal *arb4;
@@ -305,7 +311,7 @@ main(int argc, char** argv)
     const char* ell_name = "ell_nurb.s";
     mk_brep(outfp, ell_name, ellbrep);
     delete ellbrep;
-/*    
+    
     bu_log("Writing a RHC b-rep...\n");
     ON_Brep* rhcbrep = ON_Brep::New();
     struct rt_rhc_internal *rhc;
@@ -321,7 +327,7 @@ main(int argc, char** argv)
     const char* rhc_name = "rhc_nurb.s";
     mk_brep(outfp, rhc_name, rhcbrep);
     delete rhcbrep;
- 
+
     bu_log("Writing a RPC b-rep...\n");
     ON_Brep* rpcbrep = ON_Brep::New();
     struct rt_rpc_internal *rpc;
@@ -354,7 +360,7 @@ main(int argc, char** argv)
     const char* epa_name = "epa_nurb.s";
     mk_brep(outfp, epa_name, epabrep);
     delete epabrep;
- */
+ 
     bu_log("Writing an EHY b-rep...\n");
     ON_Brep* ehybrep = ON_Brep::New();
     struct rt_ehy_internal *ehy;
@@ -371,7 +377,7 @@ main(int argc, char** argv)
     const char* ehy_name = "ehy_nurb.s";
     mk_brep(outfp, ehy_name, ehybrep);
     delete ehybrep;
-   
+  
     bu_log("Writing a TGC b-rep...\n");
     ON_Brep* tgcbrep = ON_Brep::New();
     struct rt_tgc_internal *tgc;
@@ -424,7 +430,100 @@ main(int argc, char** argv)
     const char* eto_name = "eto_nurb.s";
     mk_brep(outfp, eto_name, etobrep);
     delete etobrep;
-
+*/
+    bu_log("Writing a Sketch (non-solid) b-rep...\n");
+    int cnti;
+    ON_Brep* sketchbrep = ON_Brep::New();
+    struct rt_sketch_internal *skt;
+    struct bezier_seg *bsg;
+    struct line_seg *lsg;
+    struct carc_seg *csg;
+    point_t V;
+    vect_t u_vec, v_vec;
+    point2d_t verts[] = {
+        { 250, 0 },     /* 0 */
+	{ 500, 0 },     /* 1 */
+	{ 500, 500 },   /* 2 */
+	{ 0, 500 },     /* 3 */
+	{ 0, 250 },     /* 4 */
+	{ 250, 250 },   /* 5 */
+	{ 125, 125 },   /* 6 */
+	{ 0, 125 },     /* 7 */
+	{ 125, 0 },     /* 8 */
+	{ 200, 200 }    /* 9 */
+    };
+    VSET(V, 10, 20, 30);
+    VSET(u_vec, 1, 0, 0);
+    VSET(v_vec, 0, 1, 0);
+    skt = (struct rt_sketch_internal *)bu_calloc(1, sizeof(struct rt_sketch_internal), "sketch");
+    skt->magic = RT_SKETCH_INTERNAL_MAGIC;
+    VMOVE(skt->V, V);
+    VMOVE(skt->u_vec, u_vec);
+    VMOVE(skt->v_vec, v_vec);
+    skt->vert_count = 10;
+    skt->verts = (point2d_t *)bu_calloc(skt->vert_count, sizeof(point2d_t), "verts");
+    for (cnti=0; cnti < skt->vert_count; cnti++) {
+	V2MOVE(skt->verts[cnti], verts[cnti]);
+    }
+    skt->skt_curve.seg_count = 6;
+    skt->skt_curve.reverse = (int *)bu_calloc(skt->skt_curve.seg_count, sizeof(int), "sketch: reverse");
+    skt->skt_curve.segments = (genptr_t *)bu_calloc(skt->skt_curve.seg_count, sizeof(genptr_t), "segs");
+    bsg = (struct bezier_seg *)bu_malloc(sizeof(struct bezier_seg), "sketch: bsg");
+    bsg->magic = CURVE_BEZIER_MAGIC;
+    bsg->degree = 4;
+    bsg->ctl_points = (int *)bu_calloc(bsg->degree+1, sizeof(int), "sketch: bsg->ctl_points");
+    bsg->ctl_points[0] = 4;
+    bsg->ctl_points[1] = 7;
+    bsg->ctl_points[2] = 9;
+    bsg->ctl_points[3] = 8;
+    bsg->ctl_points[4] = 0;
+    skt->skt_curve.segments[0] = (genptr_t)bsg;
+    
+    lsg = (struct line_seg *)bu_malloc(sizeof(struct line_seg), "sketch: lsg");
+    lsg->magic = CURVE_LSEG_MAGIC;
+    lsg->start = 0;
+    lsg->end = 1;
+    
+    skt->skt_curve.segments[1] = (genptr_t)lsg;
+    
+    lsg = (struct line_seg *)bu_malloc(sizeof(struct line_seg), "sketch: lsg");
+    lsg->magic = CURVE_LSEG_MAGIC;
+    lsg->start = 1;
+    lsg->end = 2;
+    
+    skt->skt_curve.segments[2] = (genptr_t)lsg;
+    
+    lsg = (struct line_seg *)bu_malloc(sizeof(struct line_seg), "sketch: lsg");
+    lsg->magic = CURVE_LSEG_MAGIC;
+    lsg->start = 2;
+    lsg->end = 3;
+    
+    skt->skt_curve.segments[3] = (genptr_t)lsg;
+    
+    lsg = (struct line_seg *)bu_malloc(sizeof(struct line_seg), "sketch: lsg");
+    lsg->magic = CURVE_LSEG_MAGIC;
+    lsg->start = 3;
+    lsg->end = 4;
+    
+    skt->skt_curve.segments[4] = (genptr_t)lsg;
+    
+    csg = (struct carc_seg *)bu_malloc(sizeof(struct carc_seg), "sketch: csg");
+    
+    csg->magic = CURVE_CARC_MAGIC;
+    csg->radius = -1.0;
+    csg->start = 6;
+    csg->end = 5;
+    
+    skt->skt_curve.segments[5] = (genptr_t)csg;
+    
+    const char* sketch_name_csg = "sketch.s";
+    mk_sketch(outfp, sketch_name_csg, skt);
+        
+    tmp_internal->idb_ptr = (genptr_t)skt;
+    rt_sketch_brep(&sketchbrep, tmp_internal, tol);
+    const char* sketch_name = "sketch_nurb.s";
+    mk_brep(outfp, sketch_name, sketchbrep);
+    delete sketchbrep;
 
     bu_free(tmp_internal, "free tmp_internal");
     wdb_close(outfp);
