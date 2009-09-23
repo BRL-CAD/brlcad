5d46e1f0c0ecdba1130c74fc8a2112fc12b4ce80
Erik Greenwald <erik@brlcad.org>
2009-09-23 13:54:38 +0000

    wrap declarations in braces to avoid case label cross initialization error
    svn:revision:35997
    svn:branch:trunk
    svn:account:erikgreenwald

--- src/librt/primitives/sketch/sketch_brep.cpp
+++ src/librt/primitives/sketch/sketch_brep.cpp
@@ -138,7 +138,7 @@ void FindLoops(struct rt_sketch_internal *sk, ON_SimpleArray<ON_SimpleArray<genp
 extern "C" void
 rt_sketch_brep(ON_Brep **b, const struct rt_db_internal *ip, const struct bn_tol *tol)
 {
-   
+
     struct rt_sketch_internal	*eip;
 
     *b = NULL; 
@@ -152,13 +152,13 @@ rt_sketch_brep(ON_Brep **b, const struct rt_db_internal *ip, const struct bn_tol
 
     ON_TextLog dump_to_stdout;
     ON_TextLog* dump = &dump_to_stdout;
-    
+
     ON_3dPoint plane_origin;
     ON_3dVector plane_x_dir, plane_y_dir;
-    
- 
+
+
     //  Find plane in 3 space corresponding to the sketch.
-    
+
     plane_origin = ON_3dPoint(eip->V);
     plane_x_dir = ON_3dVector(eip->u_vec);
     plane_y_dir = ON_3dVector(eip->v_vec);
@@ -169,7 +169,7 @@ rt_sketch_brep(ON_Brep **b, const struct rt_db_internal *ip, const struct bn_tol
     (*b)->m_S.Append(sketch_surf);
     int surfindex = (*b)->m_S.Count();
     ON_BrepFace& face = (*b)->NewFace(surfindex - 1);
- 
+
     //  For the brep, need the list of 3D vertex points.  In sketch, they
     //  are stored as 2D coordinates, so use the sketch_plane to define 3 space
     //  points for the vertices.
@@ -189,45 +189,51 @@ rt_sketch_brep(ON_Brep **b, const struct rt_db_internal *ip, const struct bn_tol
 	lng = (long *)(&eip->skt_curve)->segments[i];
 	switch (*lng) {
 	    case CURVE_LSEG_MAGIC:
-		lsg = (struct line_seg *)lng;
-		ON_Curve* lsg3d = new ON_LineCurve((*b)->m_V[lsg->start].Point(), (*b)->m_V[lsg->end].Point());
-		lsg3d->SetDomain(0.0, 1.0);
-		(*b)->m_C3.Append(lsg3d);
-		(*b)->NewEdge((*b)->m_V[lsg->start], (*b)->m_V[lsg->end] , (*b)->m_C3.Count() - 1);
+		{
+		    lsg = (struct line_seg *)lng;
+		    ON_Curve* lsg3d = new ON_LineCurve((*b)->m_V[lsg->start].Point(), (*b)->m_V[lsg->end].Point());
+		    lsg3d->SetDomain(0.0, 1.0);
+		    (*b)->m_C3.Append(lsg3d);
+		    (*b)->NewEdge((*b)->m_V[lsg->start], (*b)->m_V[lsg->end] , (*b)->m_C3.Count() - 1);
+		}
 		edgcnt = (*b)->m_E.Count() - 1;
 		(*b)->m_E[edgcnt].m_tolerance = 0.0;
 		break;
 	    case CURVE_CARC_MAGIC:
 		csg = (struct carc_seg *)lng;
 		if (csg->radius < 0) {
-		    ON_3dPoint cntrpt = (*b)->m_V[csg->end].Point();
-		    ON_3dPoint edgept = (*b)->m_V[csg->start].Point();
-		    ON_Circle* c3dcirc = new ON_Circle(cntrpt,cntrpt.DistanceTo(edgept));
-		    ON_Curve* c3d = new ON_ArcCurve((const ON_Circle)*c3dcirc);
-		    c3d->SetDomain(0.0,1.0);
-		    (*b)->m_C3.Append(c3d);
-		    (*b)->NewEdge((*b)->m_V[csg->start], (*b)->m_V[csg->end] , (*b)->m_C3.Count() - 1);
+		    {
+			ON_3dPoint cntrpt = (*b)->m_V[csg->end].Point();
+			ON_3dPoint edgept = (*b)->m_V[csg->start].Point();
+			ON_Circle* c3dcirc = new ON_Circle(cntrpt,cntrpt.DistanceTo(edgept));
+			ON_Curve* c3d = new ON_ArcCurve((const ON_Circle)*c3dcirc);
+			c3d->SetDomain(0.0,1.0);
+			(*b)->m_C3.Append(c3d);
+			(*b)->NewEdge((*b)->m_V[csg->start], (*b)->m_V[csg->end] , (*b)->m_C3.Count() - 1);
+		    }
 		    edgcnt = (*b)->m_E.Count() - 1;
 		    (*b)->m_E[edgcnt].m_tolerance = 0.0;
 		} else {
 		    // need to calculated 3rd point on arc - look to sketch.c around line 581 for
 		    // logic
 		    ON_Arc* c3darc = new ON_Arc();
-    		    ON_Curve* c3d = new ON_ArcCurve();
+		    ON_Curve* c3d = new ON_ArcCurve();
 		}
 		break;
 	    case CURVE_BEZIER_MAGIC:
 		bsg = (struct bezier_seg *)lng;
-		ON_3dPointArray *bezpoints = new ON_3dPointArray(bsg->degree + 1);
-		for (int i = 0; i < bsg->degree + 1; i++) {
-		    bezpoints->Append((*b)->m_V[bsg->ctl_points[i]].Point());
+		{
+		    ON_3dPointArray *bezpoints = new ON_3dPointArray(bsg->degree + 1);
+		    for (int i = 0; i < bsg->degree + 1; i++) {
+			bezpoints->Append((*b)->m_V[bsg->ctl_points[i]].Point());
+		    }
+		    ON_BezierCurve* bez3d = new ON_BezierCurve((const ON_3dPointArray)*bezpoints);
+		    ON_NurbsCurve* beznurb3d = new ON_NurbsCurve();
+		    bez3d->GetNurbForm(*beznurb3d);
+		    beznurb3d->SetDomain(0.0,1.0);
+		    (*b)->m_C3.Append(beznurb3d);
+		    (*b)->NewEdge((*b)->m_V[bsg->ctl_points[0]], (*b)->m_V[bsg->ctl_points[bsg->degree]] , (*b)->m_C3.Count() - 1);
 		}
-		ON_BezierCurve* bez3d = new ON_BezierCurve((const ON_3dPointArray)*bezpoints);
-		ON_NurbsCurve* beznurb3d = new ON_NurbsCurve();
-		bez3d->GetNurbForm(*beznurb3d);
-		beznurb3d->SetDomain(0.0,1.0);
-		(*b)->m_C3.Append(beznurb3d);
-		(*b)->NewEdge((*b)->m_V[bsg->ctl_points[0]], (*b)->m_V[bsg->ctl_points[bsg->degree]] , (*b)->m_C3.Count() - 1);
 		edgcnt = (*b)->m_E.Count() - 1;
 		(*b)->m_E[edgcnt].m_tolerance = 0.0;
 		break;
@@ -241,14 +247,14 @@ rt_sketch_brep(ON_Brep **b, const struct rt_db_internal *ip, const struct bn_tol
 
     // Once the vertices are added, set the extents of the surface to ensure
     // it will hold all the vertices.
-/*    vect_t vect_diag, vect_max, parallel, orthogonal;
-    VSUB2(vect_diag, pmax, pmin);
-    VPROJECT(vect_min, eip->u_vec, parallel, orthogonal);
-    sketch_surf.SetDomain(0, MAGNITUDE(parallel), MAGNITUDE(orthogonal));
-    VPROJECT(vect_max, eip->u_vec, parallel, orthogonal);
-    sketch_surf.SetDomain(1, MAGNITUDE(parallel), MAGNITUDE(orthogonal));
-    sketch_surf.SetExtents(0, sketch_surf.Domain(0));
-    sketch_surf.SetExtents(1, sketch_surf.Domain(1));
+    /*    vect_t vect_diag, vect_max, parallel, orthogonal;
+	  VSUB2(vect_diag, pmax, pmin);
+	  VPROJECT(vect_min, eip->u_vec, parallel, orthogonal);
+	  sketch_surf.SetDomain(0, MAGNITUDE(parallel), MAGNITUDE(orthogonal));
+	  VPROJECT(vect_max, eip->u_vec, parallel, orthogonal);
+	  sketch_surf.SetDomain(1, MAGNITUDE(parallel), MAGNITUDE(orthogonal));
+	  sketch_surf.SetExtents(0, sketch_surf.Domain(0));
+	  sketch_surf.SetExtents(1, sketch_surf.Domain(1));
 
     // Now, append the surface to the brep surface list
     (*b)->m_S.Append(ON_Surface::Cast(&sketch_surf));
@@ -264,9 +270,9 @@ rt_sketch_brep(ON_Brep **b, const struct rt_db_internal *ip, const struct bn_tol
     ON_BrepFace& bface = (*b)->NewFace(bsi);
     (*b)->NewPlanarFaceLoop(bface.m_face_index, ON_BrepLoop::outer, boundary, true);
     (*b)->SetTrimIsoFlags(bface);
-	     
-    
-    
+
+
+
     //  Next, create a parabolic NURBS curve corresponding to the shape of
     //  the parabola in the two planes.
     point_t x_rev_dir, ep1, ep2, ep3, tmppt;
@@ -298,9 +304,9 @@ rt_sketch_brep(ON_Brep **b, const struct rt_db_internal *ip, const struct bn_tol
     ON_LineCurve* straightedge = new ON_LineCurve(onp3,onp1);   
     bu_log("Valid curve: %d\n", straightedge->IsValid(dump));
     straightedge->Dump(*dump);
-   
+
     // Generate the bottom cap 
-   
+
     ON_PlaneSurface* bp = new ON_PlaneSurface();
     bp->m_plane = (*rpc_bottom_plane);
     bp->SetDomain(0, -100.0, 100.0 );
@@ -317,14 +323,14 @@ rt_sketch_brep(ON_Brep **b, const struct rt_db_internal *ip, const struct bn_tol
     bp->SetExtents(0,bp->Domain(0));
     bp->SetExtents(1,bp->Domain(1));
     (*b)->SetTrimIsoFlags(bface);
-    
+
     // Now the side face and top cap - extrude the bottom face and set the cap flag to true
     vect_t vp2;
     VADD2(vp2,eip->rpc_V, eip->rpc_H);
     const ON_Curve* extrudepath = new ON_LineCurve(ON_3dPoint(eip->rpc_V), ON_3dPoint(vp2));
     ON_Brep& brep = *(*b);
     ON_BrepExtrudeFace(brep,0, *extrudepath, true);
- */ 
+    */ 
 }
 
 // Local Variables:
