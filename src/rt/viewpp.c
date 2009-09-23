02b82276d15fbba7f15d8a6ba68c57ebda81200c
Christopher Sean Morrison <brlcad@gmail.com>
2009-09-23 21:44:44 +0000

    more format tweakage.
    svn:revision:36009
    svn:branch:trunk
    svn:account:brlcad

--- src/rt/viewpp.c
+++ src/rt/viewpp.c
@@ -19,14 +19,14 @@
  */
 /** @file viewpp.c
  *
- *  Ray Tracing program RTPP bottom half.
+ * Ray Tracing program RTPP bottom half.
  *
- *  This module takes the first hit from rt_shootray(), and produces
- *  GIFT format .PP (pretty picture) files
+ * This module takes the first hit from rt_shootray(), and produces
+ * GIFT format .PP (pretty picture) files
  *
- *  To make a picture out of a .PP file, use:
- *	pp-ik or
- *	pp-fb
+ * To make a picture out of a .PP file, use:
+ * pp-ik or
+ * pp-fb
  *
  */
 
@@ -49,8 +49,8 @@ Options:\n\
  -x #		Set librt debug flags\n\
 ";
 
-extern int	width;
-extern int	height;
+extern int width;
+extern int height;
 
 /* Viewing module specific "set" variables */
 struct bu_structparse view_parse[] = {
@@ -64,8 +64,25 @@ static int last_ihigh;			/* last inten_high */
 static int ntomiss;			/* number of pixels to miss */
 static int col;				/* column; for PP 75 char/line crap */
 
-void	view_pixel(void) {}
-void	pknum(int arg);
+void view_pixel(void) {}
+
+/*
+ * P K N U M
+ *
+ * Oddball 5-bits in a char ('@', 'A', ... on up) number packing.
+ * Number is written 5 bits at a time, right to left (low to high)
+ * until there are no more non-zero bits remaining.
+ */
+void
+pknum(int arg)
+{
+    register long i = arg;
+
+    do {
+	pchar((int)('@'+(i & 037)));
+	i >>= 5;
+    } while (i > 0);
+}
 
 /* Support for pretty-picture files */
 int
@@ -75,50 +92,50 @@ pphit(register struct application *ap, struct partition *PartHeadp, struct seg *
     register struct hit *hitp;
     double cosI0;
     register int i, j;
-    vect_t		normal;
+    vect_t normal;
 
-    for ( pp=PartHeadp->pt_forw; pp != PartHeadp; pp = pp->pt_forw )
-	if ( pp->pt_outhit->hit_dist >= 0.0 )  break;
-    if ( pp == PartHeadp )  {
+    for (pp=PartHeadp->pt_forw; pp != PartHeadp; pp = pp->pt_forw)
+	if (pp->pt_outhit->hit_dist >= 0.0) break;
+    if (pp == PartHeadp) {
 	bu_log("pphit:  no hit out front?\n");
 	return(0);
     }
     hitp = pp->pt_inhit;
-    RT_HIT_NORMAL( normal, hitp, pp->pt_inseg->seg_stp, &(ap->a_ray), pp->pt_inflip );
+    RT_HIT_NORMAL(normal, hitp, pp->pt_inseg->seg_stp, &(ap->a_ray), pp->pt_inflip);
 
-#define pchar(c) {putc(c, stdout);if(col++==74) {putc('\n', stdout);col=0;}}
+#define pchar(c) {putc(c, stdout);if (col++==74) {putc('\n', stdout);col=0;}}
 
     cosI0 = -VDOT(normal, ap->a_ray.r_dir);
-    if ( pp->pt_inflip )
+    if (pp->pt_inflip)
 	cosI0 = -cosI0;
-    if ( cosI0 <= 0.0 )  {
+    if (cosI0 <= 0.0) {
 	ntomiss++;
 	return(0);
     }
-    if ( ntomiss > 0 )  {
-	pchar(' ');	/* miss target cmd */
-	pknum( ntomiss );
+    if (ntomiss > 0) {
+	pchar(' ');		/* miss target cmd */
+	pknum(ntomiss);
 	ntomiss = 0;
 	last_solidp = SOLTAB_NULL;
     }
-    if ( last_item != pp->pt_regionp->reg_regionid )  {
+    if (last_item != pp->pt_regionp->reg_regionid) {
 	last_item = pp->pt_regionp->reg_regionid;
-	pchar( '#' );	/* new item cmd */
-	pknum( last_item );
+	pchar('#');		/* new item cmd */
+	pknum(last_item);
 	last_solidp = SOLTAB_NULL;
     }
-    if ( last_solidp != pp->pt_inseg->seg_stp )  {
+    if (last_solidp != pp->pt_inseg->seg_stp) {
 	last_solidp = pp->pt_inseg->seg_stp;
-	pchar( '!' );		/* new solid cmd */
+	pchar('!');		/* new solid cmd */
     }
     i = cosI0 * 255.0;		/* integer angle */
     j = (i>>5) & 07;
-    if ( j != last_ihigh )  {
+    if (j != last_ihigh) {
 	last_ihigh = j;
-	pchar( '0'+j );		/* new inten high */
+	pchar('0'+j);		/* new inten high */
     }
     j = i & 037;
-    pchar( '@'+j );			/* low bits of pixel */
+    pchar('@'+j);		/* low bits of pixel */
     return(1);
 }
 
@@ -133,41 +150,23 @@ ppmiss(struct application *ap)
 void
 view_eol(void)
 {
-    pchar( '.' );		/* End of scanline */
+    pchar('.');		/* End of scanline */
     last_solidp = SOLTAB_NULL;
     ntomiss = 0;
 }
 
 /*
- *  Called when the picture is finally done.
+ * Called when the picture is finally done.
  */
 void
 view_end(void)
 {
-    fprintf( stdout, "/\n" );	/* end of view */
-    fflush( stdout );
+    fprintf(stdout, "/\n");	/* end of view */
+    fflush(stdout);
 }
 
 /*
- *  			P K N U M
- *
- *  Oddball 5-bits in a char ('@', 'A', ... on up) number packing.
- *  Number is written 5 bits at a time, right to left (low to high)
- *  until there are no more non-zero bits remaining.
- */
-void
-pknum(int arg)
-{
-    register long i = arg;
-
-    do {
-	pchar( (int)('@'+(i & 037)) );
-	i >>= 5;
-    } while ( i > 0 );
-}
-
-/*
- *  			V I E W _ I N I T
+ * V I E W _ I N I T
  */
 int
 view_init(register struct application *ap, char *file, char *obj, int minus_o)
@@ -178,19 +177,19 @@ view_init(register struct application *ap, char *file, char *obj, int minus_o)
     ap->a_miss = ppmiss;
     ap->a_onehit = 1;
 
-    if ( !minus_o )
+    if (!minus_o)
 	fprintf(stderr, "Warning:  -o ignored, .PP goes to stdout\n");
 
-    fprintf(stdout, "%s: %s (RT)\n", file, obj );
-    fprintf(stdout, "%10d%10d", (int)azimuth, (int)elevation );
-    fprintf(stdout, "%10d%10d\n", width, height );
+    fprintf(stdout, "%s: %s (RT)\n", file, obj);
+    fprintf(stdout, "%10d%10d", (int)azimuth, (int)elevation);
+    fprintf(stdout, "%10d%10d\n", width, height);
     return(0);		/* no framebuffer needed */
 }
 
-void	view_2init(void) {;}
+void view_2init(void) {;}
 
-void	view_setup(void) {}
-void	view_cleanup(void) {}
+void view_setup(void) {}
+void view_cleanup(void) {}
 
 void application_init (void) {}
 
