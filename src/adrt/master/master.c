1051ea9cad47753cd0b963e454c81cfdfb3d0856
Keith Boman <indianlarry@verizon.net>
2009-09-28 10:35:50 +0000

    Added signal header to adrt slave/master.
    svn:revision:36062
    svn:branch:trunk
    svn:account:indianlarry

--- src/adrt/master/master.c
+++ src/adrt/master/master.c
@@ -25,6 +25,7 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
 #include <string.h>
 #include <sys/time.h>
 
--- src/adrt/slave/slave.c
+++ src/adrt/slave/slave.c
@@ -29,6 +29,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
 #include <string.h>
 #include <sys/time.h>
 #include <pthread.h>
