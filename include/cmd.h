00c95ad31f81cb9ef75c9ad27965f42d86b0d095
Christopher Sean Morrison <brlcad@gmail.com>
2009-10-04 15:47:56 +0000

    migrate command func docs into the headers
    svn:revision:36150
    svn:branch:trunk
    svn:account:brlcad

--- include/cmd.h
+++ include/cmd.h
@@ -22,7 +22,7 @@
 /** @file cmd.h
  *
  * @brief
- *   Command history data structures
+ * Command history data structures
  *
  */
 #ifndef __CMD_H__
@@ -57,13 +57,82 @@ struct bu_cmdhist_obj {
     struct bu_cmdhist *cho_curr;
 };
 
+
+/**
+ * b u _ c m d
+ *
+ * This function is intended to be used for parsing subcommands.  If
+ * the command is found in the array of commands, the associated
+ * function is called. Otherwise, an error message is created and
+ * added to interp.
+ *
+ * @param clientData	- data/state associated with the command
+ * @param interp	- tcl interpreter wherein this command is registered
+ * (Note - the result of the command is also stored here)
+ * @param argc		- number of arguments in argv
+ * @param argv		- command to execute and its arguments
+ * @param cmds		- commands and related function pointers
+ * @param cmd_index	- indicates which argv element holds the subcommand
+ *
+ * @return TCL_OK if successful, otherwise, TCL_ERROR.
+ */
 BU_EXPORT BU_EXTERN(int bu_cmd, (ClientData clientData, Tcl_Interp *interp, int argc, const char *argv[], struct bu_cmdtab *cmds, int cmd_index));
+
+/**
+ * b u _ r e g i s t e r _ c m d s
+ *
+ * This is a convenience routine for registering an array of commands
+ * with a Tcl interpreter. Note - this is not intended for use by
+ * commands with associated state (i.e. ClientData).
+ *
+ * @param interp - Tcl interpreter wherein to register the commands
+ * @param cmds	 - commands and related function pointers
+ *
+ * @return
+ * void
+ */
 BU_EXPORT BU_EXTERN(void bu_register_cmds, (Tcl_Interp *interp, struct bu_cmdtab *cmds));
 
+/**
+ * @brief
+ * Prints out the command history.
+ *
+ * USAGE:
+ * procname history [-delays] [-outfile filename]
+ */
 BU_EXPORT BU_EXTERN(int bu_cmdhist_history, (ClientData clientData, Tcl_Interp *interp, int argc, const char **argv));
+
+/**
+ * @brief
+ * Add a command to the history list.
+ *
+ * USAGE:
+ * procname add cmd
+ */
 BU_EXPORT BU_EXTERN(int bu_cmdhist_add, (ClientData clientData, Tcl_Interp *interp, int argc, const char **argv));
+
+/**
+ * Return the current command.
+ *
+ * USAGE:
+ * procname curr
+ */
 BU_EXPORT BU_EXTERN(int bu_cmdhist_curr, (ClientData clientData, Tcl_Interp *interp, int argc, const char **argv));
+
+/**
+ * Set the current command to the next command.
+ *
+ * USAGE:
+ * procname next
+ */
 BU_EXPORT BU_EXTERN(int bu_cmdhist_next, (ClientData clientData, Tcl_Interp *interp, int argc, const char **argv));
+
+/**
+ * Set the current command to the previous command.
+ *
+ * USAGE:
+ * procname prev
+ */
 BU_EXPORT BU_EXTERN(int bu_cmdhist_prev, (ClientData clientData, Tcl_Interp *interp, int argc, const char **argv));
 
 BU_EXPORT BU_EXTERN(int cho_open_tcl, (ClientData clientData, Tcl_Interp *interp, int argc, char **argv));
