5c5cf8ae3c60771d07c17fcf001bbbdfe5b5e889
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2009-10-05 16:38:05 +0000

    stub out files for a command to change display manager types.
    svn:revision:36152
    svn:branch:trunk
    svn:account:starseeker

---
+++ src/tclscripts/mged/dmtype.tcl
@@ -0,0 +1,73 @@
+#
+#                        D M T Y P E . T C L
+# BRL-CAD
+#
+# Copyright (c) 2009 United States Government as represented by
+# the U.S. Army Research Laboratory.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public License
+# version 2.1 as published by the Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this file; see the file named COPYING for more
+# information.
+#
+###
+#
+# Description -
+#       Without arguments, reports current display manager type.  With the
+#   "set" argument, if display manager type supplied is different from
+#   current type, destroy old display manager and start up new ones of the
+#   new type.
+#
+# Usage:
+#       dmtype
+#       dmtype set {X, ogl, rtgl, ...}
+#
+proc dmtype {args} {
+  global mged_gui
+  global mged_default
+  global mged_players
+  set id $mged_players
+  
+  set argc [llength $args]
+  if {$argc == 0} {
+    return $mged_gui($id,dtype)
+  }
+  if {$argc == 1} {
+    return [help dmtype]
+  }
+  if {$argc > 2} {
+    return [help dmtype]
+  }
+  set arg [lindex $args 0]
+  if {$arg != "set"} {
+    return [help dmtype]
+  }
+  set arg [lindex $args 1]
+  
+  # New dm type is requested
+  destroy $mged_gui($id,top).ur
+  destroy $mged_gui($id,top).ul
+  destroy $mged_gui($id,top).lr
+  destroy $mged_gui($id,top).ll
+
+  
+
+}
+
+
+# Local Variables:
+# mode: Tcl
+# tab-width: 8
+# c-basic-offset: 4
+# tcl-indent-level: 4
+# indent-tabs-mode: t
+# End:
+# ex: shiftwidth=4 tabstop=8
