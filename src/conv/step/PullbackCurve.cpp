9c60c233c8e17bdd380e5a2b17588f35a123bfc1
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2009-10-30 18:41:48 +0000

    Put headers on PullBackCurve files
    svn:revision:36359
    svn:branch:trunk
    svn:account:starseeker

--- src/conv/step/PullbackCurve.cpp
+++ src/conv/step/PullbackCurve.cpp
@@ -1,3 +1,27 @@
+/*               P U L L B A C K C U R V E . C P P
+ * BRL-CAD
+ *
+ * Copyright (c) 2009 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file PullbackCurve.cpp
+ *
+ * Pull curve back into UV space from 3D space
+ *
+ */
 
 #include "common.h"
 
--- src/conv/step/PullbackCurve.h
+++ src/conv/step/PullbackCurve.h
@@ -1,3 +1,50 @@
+/*                 P U L L B A C K C U R V E . H
+ * BRL-CAD
+ *
+ * Copyright (c) 2009 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file PullbackCurve.h
+ *
+ * Pull an arbitrary model-space *curve* onto the given *surface* as a
+ * curve within the surface's domain when, for each point c = C(t) on
+ * the curve and the closest point s = S(u, v) on the surface, we have:
+ * distance(c, s) <= *tolerance*.
+ * 
+ * The resulting 2-dimensional curve will be approximated using the
+ * following process:
+ * 
+ * 1. Adaptively sample the 3d curve in the domain of the surface
+ * (ensure tolerance constraint). Sampling terminates when the
+ * following flatness criterion is met:
+ * 
+ * given two parameters on the curve t1 and t2 (which map to points p1 and p2 on the curve)
+ * let m be a parameter randomly chosen near the middle of the interval [t1, t2]
+ * ____
+ * 
+ * then the curve between t1 and t2 is flat if distance(C(m), p1p2) < flatness
+ *                                                                             
+ * 2. Use the sampled points to perform a global interpolation using
+ * universal knot generation to build a B-Spline curve.
+ * 
+ * 3. If the curve is a line or an arc (determined with openNURBS routines),
+ * return the appropriate ON_Curve subclass (otherwise, return an ON_NurbsCurve).
+ *                                                                                           
+ */
+
 #ifndef PULLBACK_CURVE
 #define PULLBACK_CURVE
 
@@ -6,28 +53,7 @@
 
 /*                  p u l l b a c k _ c u r v e
 *
-* Pull an arbitrary model-space *curve* onto the given *surface* as a
-* curve within the surface's domain when, for each point c = C(t) on
-* the curve and the closest point s = S(u, v) on the surface, we have:
-* distance(c, s) <= *tolerance*.
-*
-* The resulting 2-dimensional curve will be approximated using the
-* following process:
-*
-* 1. Adaptively sample the 3d curve in the domain of the surface
-* (ensure tolerance constraint). Sampling terminates when the
-* following flatness criterion is met:
-*    given two parameters on the curve t1 and t2 (which map to points p1 and p2 on the curve)
-*    let m be a parameter randomly chosen near the middle of the interval [t1, t2]
-*                                                              ____
-*    then the curve between t1 and t2 is flat if distance(C(m), p1p2) < flatness
-*
-* 2. Use the sampled points to perform a global interpolation using
-*    universal knot generation to build a B-Spline curve.
-*
-* 3. If the curve is a line or an arc (determined with openNURBS routines),
-*    return the appropriate ON_Curve subclass (otherwise, return an ON_NurbsCurve).
-*
+
 *
 */
 
