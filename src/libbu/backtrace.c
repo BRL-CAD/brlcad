8b1d7a16143c25430430915e321de92b15c7e736
Christopher Sean Morrison <brlcad@gmail.com>
2009-11-20 20:26:12 +0000

    add sanity check on the return value from dup to make sure it succeeded.  quellage.
    svn:revision:36578
    svn:branch:trunk
    svn:account:brlcad

--- src/libbu/backtrace.c
+++ src/libbu/backtrace.c
@@ -128,9 +128,22 @@ backtrace(char **args, int fd)
 
     pid = fork();
     if (pid == 0) {
-	close(0); dup(input[0]); /* set the stdin to the in pipe */
-	close(1); dup(output[1]); /* set the stdout to the out pipe */
-	close(2); dup(output[1]); /* set the stderr to the out pipe */
+	int ret;
+
+	close(0);
+	ret = dup(input[0]); /* set the stdin to the in pipe */
+	if (ret == -1)
+	    perror("dup");
+
+	close(1);
+	ret = dup(output[1]); /* set the stdout to the out pipe */
+	if (ret == -1)
+	    perror("dup");
+
+	close(2);
+	ret = dup(output[1]); /* set the stderr to the out pipe */
+	if (ret == -1)
+	    perror("dup");
 
 	execvp(args[0], args); /* invoke debugger */
 	perror("exec failed");
