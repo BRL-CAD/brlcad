7358a41060eb12460212af939f650393151faf44
Erik Greenwald <erik@brlcad.org>
2009-12-16 21:19:08 +0000

    add prototype for genmap
    svn:revision:36928
    svn:branch:trunk
    svn:account:erikgreenwald

--- src/libfb/if_X.c
+++ src/libfb/if_X.c
@@ -70,9 +70,11 @@
 #ifdef USE_PROTOTYPES
 HIDDEN void	Monochrome( unsigned char *bitbuf, unsigned char *bytebuf, int width, int height, int method);
 HIDDEN int	X_do_event( FBIO	*ifp );
+HIDDEN void	genmap(unsigned char *rmap, unsigned char *gmap, unsigned char *bmap);
 #else
 HIDDEN void	Monochrome();
 HIDDEN int	X_do_event();
+HIDDEN void	genmap();
 #endif
 /*
  * Per window state information.
