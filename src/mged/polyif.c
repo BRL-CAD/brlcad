e0364039bec6d0a2eca36dff332fbd8d45491828
Christopher Sean Morrison <brlcad@gmail.com>
2010-02-24 01:28:47 +0000

    ws consistency indent style cleanup
    svn:revision:37735
    svn:branch:trunk
    svn:account:brlcad

--- src/mged/polyif.c
+++ src/mged/polyif.c
@@ -17,13 +17,6 @@
  * License along with this file; see the file named COPYING for more
  * information.
  */
-/** @file polyif.c
- *
- *  Author -
- *	Michael John Muuss
- *	Christopher T. Johnson
- *
- */
 
 #include "common.h"
 
@@ -39,16 +32,18 @@
 #include "./mged.h"
 #include "./mged_dm.h"
 
-/* XXX When finalized, this stuff belongs in a header file of it's own */
-struct polygon_header  {
-    int	magic;			/* magic number */
-    int	ident;			/* identification number */
-    int	interior;		/* >0 => interior loop, gives ident # of exterior loop */
-    vect_t	normal;			/* surface normal */
-    unsigned char	color[3];	/* Color from containing region */
-    int	npts;			/* number of points */
+
+/* When finalized, this stuff belongs in a header file of it's own */
+struct polygon_header {
+    int magic;			/* magic number */
+    int ident;			/* identification number */
+    int interior;		/* >0 => interior loop, gives ident # of exterior loop */
+    vect_t normal;			/* surface normal */
+    unsigned char color[3];	/* Color from containing region */
+    int npts;			/* number of points */
 };
-#define POLYGON_HEADER_MAGIC	0x8623bad2
+#define POLYGON_HEADER_MAGIC 0x8623bad2
+
 
 struct bu_structparse polygon_desc[] = {
     {"%d", 1, "magic", bu_offsetof(struct polygon_header, magic), BU_STRUCTPARSE_FUNC_NULL },
@@ -59,48 +54,36 @@ struct bu_structparse polygon_desc[] = {
     {"%d", 1, "npts", bu_offsetof(struct polygon_header, npts), BU_STRUCTPARSE_FUNC_NULL },
     {"",   0, (char *)0, 0, BU_STRUCTPARSE_FUNC_NULL }
 };
+
+
 struct bu_structparse vertex_desc[] = {
     {"%f", 3, "vertex", 0, BU_STRUCTPARSE_FUNC_NULL },
     {"",   0, (char *)0, 0, BU_STRUCTPARSE_FUNC_NULL }
 };
 
-#if 0
-struct rt_imexport  polygon_desc[] = {
-    {"%d",	bu_offsetof(struct polygon_header, magic),		1 },
-    {"%d",	bu_offsetof(struct polygon_header, ident),		1 },
-    {"%d",	bu_offsetof(struct polygon_header, interior),	1 },
-    {"%f",	bu_offsetofarray(struct polygon_header, normal),	3 },
-    {"%c",	bu_offsetofarray(struct polygon_header, color),	3 },
-    {"%d",	bu_offsetof(struct polygon_header, npts),		1 },
-    {"",	0,						0 }
-};
-struct rt_imexport vertex_desc[] = {
-    {"%f",	0,	99 },	/* im_count will be filled in at runtime */
-    {"",	0,	0 }
-};
-#endif
+
 /*
- *			F _ P O L Y B I N O U T
+ * F _ P O L Y B I N O U T
  *
- *  Experimental interface for writing binary polygons
- *  that represent the current (evaluated) view.
+ * Experimental interface for writing binary polygons that represent
+ * the current (evaluated) view.
  *
- *  Usage:  polybinout file
+ * Usage:  polybinout file
  */
 int
 f_polybinout(ClientData clientData, Tcl_Interp *interp, int argc, char **argv)
 {
     struct ged_display_list *gdlp;
     struct ged_display_list *next_gdlp;
-    struct solid		*sp;
-    struct bn_vlist	*vp;
-    FILE	*fp;
-    int	pno = 1;
+    struct solid *sp;
+    struct bn_vlist *vp;
+    FILE *fp;
+    int pno = 1;
     struct polygon_header ph;
-#define MAX_VERTS	10000
-    vect_t	verts[MAX_VERTS];
-    int	need_normal = 0;
-    struct	bu_external	obuf;
+#define MAX_VERTS 10000
+    vect_t verts[MAX_VERTS];
+    int need_normal = 0;
+    struct bu_external obuf;
 
     if (argc < 2 || 2 < argc) {
 	struct bu_vls vls;
@@ -112,7 +95,7 @@ f_polybinout(ClientData clientData, Tcl_Interp *interp, int argc, char **argv)
 	return TCL_ERROR;
     }
 
-    if ( (fp = fopen( argv[1], "w" )) == NULL )  {
+    if ((fp = fopen(argv[1], "w")) == NULL) {
 	perror(argv[1]);
 	return TCL_ERROR;
     }
@@ -122,92 +105,92 @@ f_polybinout(ClientData clientData, Tcl_Interp *interp, int argc, char **argv)
 	next_gdlp = BU_LIST_PNEXT(ged_display_list, gdlp);
 
 	FOR_ALL_SOLIDS(sp, &gdlp->gdl_headSolid) {
-	    for ( BU_LIST_FOR( vp, bn_vlist, &(sp->s_vlist) ) )  {
-		int	i;
-		int	nused = vp->nused;
-		int	*cmd = vp->cmd;
+	    for (BU_LIST_FOR(vp, bn_vlist, &(sp->s_vlist))) {
+		int i;
+		int nused = vp->nused;
+		int *cmd = vp->cmd;
 		point_t *pt = vp->pt;
-		for ( i = 0; i < nused; i++, cmd++, pt++ )  {
+		for (i = 0; i < nused; i++, cmd++, pt++) {
 		    /* For each polygon, spit it out.  Ignore vectors */
-		    switch ( *cmd )  {
-		    case BN_VLIST_LINE_MOVE:
-			/* Move, start line */
-			break;
-		    case BN_VLIST_LINE_DRAW:
-			/* Draw line */
-			break;
-		    case BN_VLIST_POLY_VERTNORM:
-			/* Ignore per-vertex normal */
-			break;
-		    case BN_VLIST_POLY_START:
-			/* Start poly marker & normal, followed by POLY_MOVE */
-			ph.magic = POLYGON_HEADER_MAGIC;
-			ph.ident = pno++;
-			ph.interior = 0;
-			memcpy(ph.color, sp->s_basecolor, 3);
-			ph.npts = 0;
-			/* Set surface normal (vl_pnt points outward) */
-			VMOVE( ph.normal, *pt );
-			need_normal = 0;
-			break;
-		    case BN_VLIST_POLY_MOVE:
-			/* Start of polygon, has first point */
-			/* fall through to... */
-		    case BN_VLIST_POLY_DRAW:
-			/* Polygon Draw */
-			if ( ph.npts >= MAX_VERTS )  {
-			    Tcl_AppendResult(interp, "excess vertex skipped\n",
-					     (char *)NULL);
+		    switch (*cmd) {
+			case BN_VLIST_LINE_MOVE:
+			    /* Move, start line */
 			    break;
-			}
-			VMOVE( verts[ph.npts], *pt );
-			ph.npts++;
-			break;
-		    case BN_VLIST_POLY_END:
-			/*
-			 *  End Polygon.  Point given is repeat of
-			 *  first one, ignore it.
-			 * XXX note:  if poly_markers was not set,
-			 * XXX poly will end with next POLY_MOVE.
-			 */
-			if ( ph.npts < 3 )  {
-			    struct bu_vls tmp_vls;
+			case BN_VLIST_LINE_DRAW:
+			    /* Draw line */
+			    break;
+			case BN_VLIST_POLY_VERTNORM:
+			    /* Ignore per-vertex normal */
+			    break;
+			case BN_VLIST_POLY_START:
+			    /* Start poly marker & normal, followed by POLY_MOVE */
+			    ph.magic = POLYGON_HEADER_MAGIC;
+			    ph.ident = pno++;
+			    ph.interior = 0;
+			    memcpy(ph.color, sp->s_basecolor, 3);
+			    ph.npts = 0;
+			    /* Set surface normal (vl_pnt points outward) */
+			    VMOVE(ph.normal, *pt);
+			    need_normal = 0;
+			    break;
+			case BN_VLIST_POLY_MOVE:
+			    /* Start of polygon, has first point */
+			    /* fall through to... */
+			case BN_VLIST_POLY_DRAW:
+			    /* Polygon Draw */
+			    if (ph.npts >= MAX_VERTS) {
+				Tcl_AppendResult(interp, "excess vertex skipped\n",
+						 (char *)NULL);
+				break;
+			    }
+			    VMOVE(verts[ph.npts], *pt);
+			    ph.npts++;
+			    break;
+			case BN_VLIST_POLY_END:
+			    /*
+			     * End Polygon.  Point given is repeat of
+			     * first one, ignore it.
+			     * XXX note:  if poly_markers was not set,
+			     * XXX poly will end with next POLY_MOVE.
+			     */
+			    if (ph.npts < 3) {
+				struct bu_vls tmp_vls;
 
-			    bu_vls_init(&tmp_vls);
-			    bu_vls_printf(&tmp_vls, "polygon with %d points discarded\n",
-					  ph.npts);
-			    Tcl_AppendResult(interp, bu_vls_addr(&tmp_vls), (char *)NULL);
-			    bu_vls_free(&tmp_vls);
+				bu_vls_init(&tmp_vls);
+				bu_vls_printf(&tmp_vls, "polygon with %d points discarded\n",
+					      ph.npts);
+				Tcl_AppendResult(interp, bu_vls_addr(&tmp_vls), (char *)NULL);
+				bu_vls_free(&tmp_vls);
+				break;
+			    }
+			    if (need_normal) {
+				vect_t e1, e2;
+				VSUB2(e1, verts[0], verts[1]);
+				VSUB2(e2, verts[0], verts[2]);
+				VCROSS(ph.normal, e1, e2);
+			    }
+			    if (bu_struct_export(&obuf, (genptr_t)&ph, polygon_desc) < 0) {
+				Tcl_AppendResult(interp, "header export error\n", (char *)NULL);
+				break;
+			    }
+			    if (bu_struct_put(fp, &obuf) != obuf.ext_nbytes) {
+				perror("bu_struct_put");
+				break;
+			    }
+			    db_free_external(&obuf);
+			    /* Now export the vertices */
+			    vertex_desc[0].sp_count = ph.npts * 3;
+			    if (bu_struct_export(&obuf, (genptr_t)verts, vertex_desc) < 0) {
+				Tcl_AppendResult(interp, "vertex export error\n", (char *)NULL);
+				break;
+			    }
+			    if (bu_struct_put(fp, &obuf) != obuf.ext_nbytes) {
+				perror("bu_struct_wrap_buf");
+				break;
+			    }
+			    db_free_external(&obuf);
+			    ph.npts = 0;		/* sanity */
 			    break;
-			}
-			if ( need_normal )  {
-			    vect_t	e1, e2;
-			    VSUB2( e1, verts[0], verts[1] );
-			    VSUB2( e2, verts[0], verts[2] );
-			    VCROSS( ph.normal, e1, e2 );
-			}
-			if ( bu_struct_export( &obuf, (genptr_t)&ph, polygon_desc ) < 0 )  {
-			    Tcl_AppendResult(interp, "header export error\n", (char *)NULL);
-			    break;
-			}
-			if (bu_struct_put(fp, &obuf) != obuf.ext_nbytes) {
-			    perror("bu_struct_put");
-			    break;
-			}
-			db_free_external( &obuf );
-			/* Now export the vertices */
-			vertex_desc[0].sp_count = ph.npts * 3;
-			if ( bu_struct_export( &obuf, (genptr_t)verts, vertex_desc ) < 0 )  {
-			    Tcl_AppendResult(interp, "vertex export error\n", (char *)NULL);
-			    break;
-			}
-			if ( bu_struct_put( fp, &obuf ) != obuf.ext_nbytes )  {
-			    perror("bu_struct_wrap_buf");
-			    break;
-			}
-			db_free_external( &obuf );
-			ph.npts = 0;		/* sanity */
-			break;
 		    }
 		}
 	    }
@@ -216,9 +199,10 @@ f_polybinout(ClientData clientData, Tcl_Interp *interp, int argc, char **argv)
 	gdlp = next_gdlp;
     }
 
-    fclose( fp );
+    fclose(fp);
     return TCL_OK;
 }
+
 
 /*
  * Local Variables:
