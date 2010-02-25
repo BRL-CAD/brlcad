d4faba4d11211268ff203d4547cc5b6684196f2b
Christopher Sean Morrison <brlcad@gmail.com>
2010-02-25 23:29:56 +0000

    expand all of the stdint types using msvc's __int# types.
    svn:revision:37789
    svn:branch:trunk
    svn:account:brlcad

--- include/config_win.h
+++ include/config_win.h
@@ -175,11 +175,13 @@ typedef int socklen_t;
 typedef unsigned int gid_t;
 typedef unsigned int uid_t;
 
-typedef unsigned char uint8_t;
-typedef unsigned short uint16_t;
-typedef unsigned int uint32_t;
+typedef unsigned __int8 uint8_t;
+typedef unsigned __int16  uint16_t;
+typedef unsigned __int32 uint32_t;
 typedef unsigned __int64 uint64_t;
 
+typedef __int8 int8_t;
+typedef __int16 int16_t;
 typedef __int32 int32_t;
 typedef __int64 int64_t;
 typedef __int64 intmax_t;
@@ -189,6 +191,8 @@ typedef __int64 intmax_t;
 #define HAVE_UINT32_T		1
 #define HAVE_UINT64_T		1
 
+#define HAVE_INT8_T		1
+#define HAVE_INT16_T		1
 #define HAVE_INT32_T		1
 #define HAVE_INT64_T		1
 
