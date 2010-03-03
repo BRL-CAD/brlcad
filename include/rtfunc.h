1d61f30ceab7e8ae7c1a0b777d605d8612c891c8
Christopher Sean Morrison <brlcad@gmail.com>
2010-03-03 04:52:00 +0000

    expand comments
    svn:revision:37872
    svn:branch:trunk
    svn:account:brlcad

--- include/rtfunc.h
+++ include/rtfunc.h
@@ -31,132 +31,132 @@
  */
 
 /**
- *
+ * prep an object for ray tracing
  */
 RT_EXPORT extern int rt_prep(struct soltab *stp, struct rt_db_internal *ip, struct rt_i *rtip);
 
 /**
- *
+ * shoot a ray at an object that has been prepped for ray tracing
  */
 RT_EXPORT extern int rt_shot(struct soltab *stp, register struct xray *rp, struct application *ap, struct seg *seghead);
 
 /**
- *
+ * TBD.
  */
 RT_EXPORT extern int rt_piece_shot(struct rt_piecestate *psp, struct rt_piecelist *plp, double dist_corr, struct xray *rp, struct application *ap, struct seg *seghead);
 
 /**
- *
+ * TBD.
  */
 RT_EXPORT extern void rt_piece_hitsegs(struct rt_piecestate *psp, struct seg *seghead, struct application *ap);
 
 /**
- *
+ * print an objects parameters in debug/diagnostic form
  */
 RT_EXPORT extern void rt_print(const struct soltab *stp);
 
 /**
- *
+ * calculate a normal on an object that has been hit via rt_shot()
  */
 RT_EXPORT extern void rt_norm(struct hit *hitp, struct soltab *stp, struct xray *rp);
 
 /**
- *
+ * calculate object uv parameterization for a given hit point
  */
 RT_EXPORT extern void rt_uv(struct application *ap, struct soltab *stp, struct hit *hitp, struct uvcoord *uvp);
 
 /**
- *
+ * calculate object curvature for a given hit point
  */
 RT_EXPORT extern void rt_curve(struct curvature *cvp, struct hit *hitp, struct soltab *stp);
 
 /**
- *
+ * DEPRECATED: Unimplemented.
  */
 RT_EXPORT extern int rt_class();
 
 /**
- *
+ * release the memory used by a solid
  */
 RT_EXPORT extern void rt_free(struct soltab *stp);
 
 /**
- *
+ * obtain a vlist wireframe representation of an object for plotting purposes
  */
 RT_EXPORT extern int rt_plot(struct bu_list *vhead, struct rt_db_internal *ip, const struct rt_tess_tol *ttol, const struct bn_tol *tol);
 
 /**
- *
+ * shoot an array of rays at a set of homogenous objects.
  */
 RT_EXPORT extern void rt_vshot(struct soltab *stp[], struct xray *rp[], struct seg segp[], int n, struct application *ap);
 
 /**
- *
+ * tessellate an object (into NMG form)
  */
 RT_EXPORT extern int rt_tess(struct nmgregion **r, struct model *m, struct rt_db_internal *ip, const struct rt_tess_tol *ttol, const struct bn_tol *tol);
 
 /**
- *
+ * tessellate an object (into NURBS NMG form)
  */
 RT_EXPORT extern int rt_tnurb(struct nmgregion **r, struct model *m, struct rt_db_internal *ip, const struct bn_tol *tol);
 
 /**
- *
+ * v5 object import from disk
  */
 RT_EXPORT extern int rt_import5(struct rt_db_internal *ip, const struct bu_external *ep, const mat_t mat, const struct db_i *dbip, struct resource *resp);
 
 /**
- *
+ * v5 object export to disk
  */
 RT_EXPORT extern int rt_export5(struct bu_external *ep, const struct rt_db_internal *ip, double local2mm, const struct db_i *dbip, struct resource *resp);
 
 /**
- *
+ * v4 object import from disk
  */
 RT_EXPORT extern int rt_import4(struct rt_db_internal *ip, const struct bu_external *ep, const mat_t mat, const struct db_i *dbip, struct resource *resp);
 
 /**
- *
+ * v4 object export to disk
  */
 RT_EXPORT extern int rt_export4(struct bu_external *ep, const struct rt_db_internal *ip, double local2mm, const struct db_i *dbip, struct resource *resp);
 
 /**
- *
+ * free the internal representation of an object
  */
 RT_EXPORT extern void rt_ifree(struct rt_db_internal *ip);
 
 /**
- *
+ * db object 'get' support, obtain a tcl list representation
  */
 RT_EXPORT extern int rt_get(struct bu_vls *logstr, const struct rt_db_internal *intern, const char *attr);
 
 /**
- *
+ * db object 'adjust' support, modify a tcl list representation
  */
 RT_EXPORT extern int rt_adjust(struct bu_vls *logstr, struct rt_db_internal *intern, int argc, char **argv);
 
 /**
- *
+ * describe an object in text form (used by the 'l' command)
  */
 RT_EXPORT extern int rt_describe(struct bu_vls *str, const struct rt_db_internal *ip, int verbose, double mm2local, struct resource *resp, struct db_i *db_i);
 
 /**
- *
+ * create a 'default' object
  */
 RT_EXPORT extern void rt_make(const struct rt_functab *ftp, struct rt_db_internal *intern);
 
 /**
- *
+ * apply a matrix transformation to an object (translation, rotation, scale)
  */
 RT_EXPORT extern int rt_xform(struct rt_db_internal *op, const mat_t mat, struct rt_db_internal *ip, int release, struct db_i *dbip, struct resource *resp);
 
 /**
- *
+ * obtain parameters for an object in libpc form
  */
 RT_EXPORT extern int rt_params(struct pc_pc_set *ps, const struct rt_db_internal *ip);
 
 /**
- *
+ * mirror an object about a plane
  */
 RT_EXPORT extern int rt_mirror(struct rt_db_internal *ip, const plane_t *plane);
 
