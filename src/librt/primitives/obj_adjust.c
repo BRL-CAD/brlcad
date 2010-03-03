ddea0f762b4c883a728315572ed00367c26d9af4
Christopher Sean Morrison <brlcad@gmail.com>
2010-03-03 07:26:35 +0000

    add rt_obj_get and rt_obj_adjust, for rt_functab refactoring
    svn:revision:37894
    svn:branch:trunk
    svn:account:brlcad

---
+++ src/librt/primitives/obj_adjust.c
@@ -0,0 +1,60 @@
+/*                  O B J _ A D J U S T . C
+ * BRL-CAD
+ *
+ * Copyright (c) 2010 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+
+#include "common.h"
+
+#include "raytrace.h"
+
+
+int
+rt_obj_adjust(struct bu_vls *logstr, struct rt_db_internal *ip, int argc, char **argv)
+{
+    int id;
+    const struct rt_functab *ft;
+
+    if (!logstr || !ip)
+	return -1;
+
+    BU_CK_VLS(logstr);
+    RT_CK_DB_INTERNAL(ip);
+
+    id = ip->idb_minor_type;
+    if (id < 0)
+	return -2;
+
+    ft = &rt_functab[id];
+    if (!ft)
+	return -3;
+    if (!ft->ft_adjust)
+	return -4;
+
+    return ft->ft_adjust(logstr, ip, argc, argv);
+}
+
+
+/*
+ * Local Variables:
+ * mode: C
+ * tab-width: 8
+ * indent-tabs-mode: t
+ * c-file-style: "stroustrup"
+ * End:
+ * ex: shiftwidth=4 tabstop=8
+ */
