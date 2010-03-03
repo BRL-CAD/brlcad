b073874ec09d05755906dff716652535e85d407c
Christopher Sean Morrison <brlcad@gmail.com>
2010-03-03 05:56:15 +0000

    add the first of functab refactorings, obj_prep.c containing rt_obj_prep().
    svn:revision:37874
    svn:branch:trunk
    svn:account:brlcad

---
+++ src/librt/primitives/obj_prep.c
@@ -0,0 +1,59 @@
+/*                    O B J _ P R E P . C
+ * BRL-CAD
+ *
+ * Copyright (c) 2010 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+
+#include "common.h"
+
+#include "raytrace.h"
+
+
+int
+rt_obj_prep(struct soltab *stp, struct rt_db_internal *ip, struct rt_i *rtip)
+{
+    int id;
+    const struct rt_functab *ft;
+
+    if (!stp || !ip)
+	return -1;
+
+    RT_CK_SOLTAB(stp);
+    RT_CK_DB_INTERNAL(ip);
+    if (rtip) RT_CK_RTI(rtip);
+
+    id = stp->st_id;
+    if (id < 0)
+	return -2;
+
+    ft = &rt_functab[id];
+    if (!ft)
+	return -3;
+
+    return ft->ft_prep(stp, ip, rtip);
+}
+
+
+/*
+ * Local Variables:
+ * mode: C
+ * tab-width: 8
+ * indent-tabs-mode: t
+ * c-file-style: "stroustrup"
+ * End:
+ * ex: shiftwidth=4 tabstop=8
+ */
