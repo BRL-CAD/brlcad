68e1d3a2002b59ec5779b4dead1f406797247a1d
Christopher Sean Morrison <brlcad@gmail.com>
2010-03-09 18:31:16 +0000

    simplify.  use the new pstdint.h header instead of rolling our own tests.  thusfar, only tested on mac 10.4 so consider it tentative and preliminary.
    svn:revision:37962
    svn:branch:trunk
    svn:account:brlcad

--- include/common.h
+++ include/common.h
@@ -108,62 +108,13 @@ typedef ptrdiff_t ssize_t;
 #  endif
 #endif
 
-/* C99 says uintptr_t is an optional type, so make sure something is
- * provided.  make sure all of the C99 stdint types are provided.
- *
- * make sure we get the limit macros for C++ as well even though
- * they're not in the standard via __STDC_LIMIT_MACROS.
+/* make sure most of the C99 stdint types are provided including the
+ * optional uintptr_t type.
  */
-#if !defined(HAVE_UINTPTR_T) && !defined(uintptr_t)
-#  ifndef __STDC_LIMIT_MACROS
-#    define __STDC_LIMIT_MACROS 1
-#  endif
-#  ifdef HAVE_STDINT_H
-#    include <stdint.h>
-#  endif
-#  ifdef HAVE_INTTYPES_H
-#    include <inttypes.h>
-#  endif
-#  if !defined(UINTPTR_MAX)
-#    if defined(_I64_MIN)
-typedef __int8 int8_t;
-typedef __int16 int16_t;
-typedef __int32 int32_t;
-typedef __int64 int64_t;
-typedef __int64 intmax_t;
-typedef unsigned __int8 uint8_t;
-typedef unsigned __int16 uint16_t;
-typedef unsigned __int32 uint32_t;
-typedef unsigned __int64 uint64_t;
-#      if !defined(_UINTPTR_T_DEFINED)
-typedef uint64_t uintptr_t;
-#      endif
-#    else
-typedef signed char int8_t;
-typedef short int16_t;
-typedef int int32_t;
-typedef long long int64_t;
-typedef int64_t intmax_t;
-typedef unsigned char uint8_t;
-typedef unsigned short  uint16_t;
-typedef unsigned int uint32_t;
-typedef unsigned long long uint64_t;
-#      if !defined(_UINTPTR_T_DEFINED)
-typedef unsigned long long uintptr_t;
-#      endif
-#    endif
-#    define HAVE_INT8_T 1
-#    define HAVE_INT16_T 1
-#    define HAVE_INT32_T 1
-#    define HAVE_INT64_T 1
-#    define HAVE_UINT8_T 1
-#    define HAVE_UINT16_T 1
-#    define HAVE_UINT32_T 1
-#    define HAVE_UINT64_T 1
-#    define HAVE_INTMAX_T 1
-#    define HAVE_UINTPTR_T 1
-#  endif
+#ifdef HAVE_STDINT_H
+#  include <stdint.h>
 #endif
+#include "pstdint.h"
 
 #endif  /* __COMMON_H__ */
 /** @} */
