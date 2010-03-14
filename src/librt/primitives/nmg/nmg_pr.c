463eea435730d7e017b6cceae4fc79ac4a67d191
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2010-03-14 17:16:34 +0000

    Fix some debug bu_log statements in nmg_pr
    svn:revision:38045
    svn:branch:trunk
    svn:account:starseeker

--- src/librt/primitives/nmg/nmg_pr.c
+++ src/librt/primitives/nmg/nmg_pr.c
@@ -231,7 +231,7 @@ nmg_pr_fg(const unsigned long *magic, char *h)
 	    for (i=0; i<fgs->s_size[0]; i++) {
 		bu_log("%s  ", h);
 		for (j=0; j<fgs->s_size[1]; j++) {
-		    bu_log(" (", h);
+		    bu_log(" (%s ", h);
 		    for (k=0; k<ncoords; k++)
 			bu_log("%f ", fgs->ctl_points[l+k]);
 		    bu_log(")");
@@ -804,7 +804,7 @@ nmg_pr_vua(const unsigned long *magic_p, char *h)
     switch (*magic_p) {
 	case NMG_VERTEXUSE_A_PLANE_MAGIC:
 	    bu_log("%s N=(%g, %g, %g, %g)\n", h,
-		   V3ARGS(((struct vertexuse_a_plane *)magic_p)->N));
+		   V4ARGS(((struct vertexuse_a_plane *)magic_p)->N));
 	    break;
 	case NMG_VERTEXUSE_A_CNURB_MAGIC:
 	    bu_log("%s param=(%g, %g, %g)\n", h,
