cba50d6347036ad8f7ae43f3581909f0c7d511fe
Erik Greenwald <erik@brlcad.org>
2010-03-16 23:50:19 +0000

    this func changed signature.
    svn:revision:38083
    svn:branch:trunk
    svn:account:erikgreenwald

--- include/raytrace.h
+++ include/raytrace.h
@@ -4691,7 +4691,6 @@ RT_EXPORT BU_EXTERN(void nmg_triangulate_model_mc,
 RT_EXPORT BU_EXTERN(int rt_nmg_mc_realize_cube,
 		    (struct shell *s,
 		     int pv,
-		     point_t *p,
 		     point_t *edges,
 		     const struct bn_tol *tol));
 
