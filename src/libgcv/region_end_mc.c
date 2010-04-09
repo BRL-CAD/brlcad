37f37d56486646c5cbb836a2548da647974f0314
Erik Greenwald <erik@brlcad.org>
2010-04-09 20:09:23 +0000

    fuse the vertices and edges before the regions/shells are killed.
    svn:revision:38421
    svn:branch:trunk
    svn:account:erikgreenwald

--- src/libgcv/region_end_mc.c
+++ src/libgcv/region_end_mc.c
@@ -50,7 +50,7 @@ gcv_region_end_mc(struct db_tree_state *tsp, const struct db_full_path *pathp, u
     int empty_region = 0;
     int empty_model = 0;
     int NMG_debug_state = 0;
-    int count = 0, removed = 0;
+    int count = 0, removed_verts = 0, removed_faces = 0, removed_edges = 0;
 
     void (*write_region)(struct nmgregion *, const struct db_full_path *, int, int, float [3]);
 
@@ -110,8 +110,31 @@ gcv_region_end_mc(struct db_tree_state *tsp, const struct db_full_path *pathp, u
 	return TREE_NULL;
     }
 
+    bu_log("Target is shot, %d triangles seen.\n", count);
+    removed_faces = 0;
+    removed_verts = 0;
+    removed_edges = 0;
+    removed_verts = nmg_model_vertex_fuse(m, tsp->ts_tol);
+    bu_log("Fusing vertices: %d eliminated\n", removed_verts);
     nmg_mark_edges_real(&s->l.magic);
+    nmg_kill_zero_length_edgeuses( m );
     nmg_region_a(r, tsp->ts_tol);
+    nmg_kill_zero_length_edgeuses( m );
+    removed_edges = nmg_model_edge_fuse( m, tsp->ts_tol );
+    bu_log("Fusing edges: %d eliminated\n", removed_edges);
+#if 0
+    bu_log("Fusing faces: "); fflush(stdout);
+    rt_g.NMG_debug = DEBUG_BASIC;
+    removed_faces = nmg_model_face_fuse(m, tsp->ts_tol);
+    rt_g.NMG_debug = 0;
+    bu_log("%d eliminated (%d/%d left)\n", removed_faces, count-removed_faces, count);
+    count = count - removed_faces;
+#endif
+    /*
+    bu_log("Fusing\n"); fflush(stdout);
+    nmg_model_fuse(m, tsp->ts_tol);
+    bu_log("Done\n"); fflush(stdout);
+    */
 
     /* Kill cracks */
     while (BU_LIST_NOT_HEAD(&s->l, &r->s_hd)) {
@@ -160,17 +183,12 @@ gcv_region_end_mc(struct db_tree_state *tsp, const struct db_full_path *pathp, u
 
 	return _gcv_cleanup(NMG_debug_state, tp);
     } else {
-
 	/* Write the region out */
 	write_region(r, pathp, tsp->ts_regionid, tsp->ts_gmater, tsp->ts_mater.ma_color);
 
     } BU_UNSETJUMP; /* Relinquish bomb protection */
 
     nmg_kr(r);
-
-    bu_log("Fusing vertices: %d eliminated\n", nmg_model_vertex_fuse(m, tsp->ts_tol));
-    removed = nmg_edge_collapse(m, tsp->ts_tol, tsp->ts_ttol->abs, tsp->ts_ttol->norm);
-    bu_log("decimate reduced %d faces to %d in %s\n", count, count - removed, db_path_to_string(pathp));
 
     return _gcv_cleanup(NMG_debug_state, tp);
 }
