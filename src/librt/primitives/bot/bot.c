37532550b36a2da3b6947317dff848131d68a10e
Robert Parker <unacknowledged@users.noreply.github.com>
2010-04-28 17:30:27 +0000

    If the new bot is plate mode, set the thickness and face_mode for each face.
    svn:revision:38854
    svn:branch:trunk
    svn:account:bob1961

--- src/librt/primitives/bot/bot.c
+++ src/librt/primitives/bot/bot.c
@@ -4155,6 +4155,12 @@ rt_bot_create(struct rt_bot_internal *bot, struct tri_pts *newTpp)
 	    REMAP_BOT_VERTS(bot,newbot,vmap,vcount,tpp->b,i*3+1);
 	    REMAP_BOT_VERTS(bot,newbot,vmap,vcount,tpp->c,i*3+2);
 
+	    newbot->thickness[i] = bot->thickness[tpp->tri];
+	    if (BU_BITTEST(bot->face_mode, tpp->tri))
+		BU_BITSET(newbot->face_mode, i);
+	    else
+		BU_BITCLR(newbot->face_mode, i);
+
 	    ++i;
 	}
 
