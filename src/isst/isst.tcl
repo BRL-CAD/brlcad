6e55c8503d32517f00fa7d436b615ea939238c71
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2010-05-20 02:16:53 +0000

    Rather than fight it, leave the key bindings with the parent window and never force the focus all the way to the togl widget.
    svn:revision:39413
    svn:branch:trunk
    svn:account:starseeker

--- src/isst/isst.tcl
+++ src/isst/isst.tcl
@@ -41,27 +41,9 @@ proc ::isst::setup {} {
 proc ::isst::drawview {win {tick 100} } {
     global az el resolution oglwin
     togl $win -width 800 -height 600 -rgba true -double true -depth true -privatecmap false -time $tick -create isst_init -destroy isst_zap -display refresh_ogl -reshape reshape -timer idle
-     set oglwin $win
-     focus $win
-     bind $win <Key-1> {focus %W; render_mode %W phong}
-     bind $win <Key-2> {focus %W; render_mode %W normal}
-     bind $win <Key-3> {focus %W; render_mode %W depth}
-     bind $win <Key-4> {focus %W; render_mode %W component}
-     bind $win <Key-0> {focus %W; reset %W}
-     
-    bind $win <Key-w> {focus %W; ::isst::MoveForward %W}
-    bind $win <Key-s> {focus %W; ::isst::MoveBackward %W}
-    bind $win <Key-a> {focus %W; ::isst::MoveLeft %W}
-    bind $win <Key-d> {focus %W; ::isst::MoveRight %W}
-
-    bind $win <Key-minus> {focus %W; ::isst::Resolution %W 1}
-    bind $win <Key-equal> {focus %W; ::isst::Resolution %W -1}
-     
-    bind $win <ButtonPress-1> {::isst::RotStart %x %y %W}
-    bind $win <ButtonPress-3> {::isst::RotStart %x %y %W}
-    bind $win <B1-Motion> {::isst::RotMove %x %y %W}
-    bind $win <B3-Motion> {::isst::RotMove2 %x %y %W}
+    set oglwin $win
     load_g $win /usr/brlcad/rel-7.16.2/share/brlcad/7.16.2/db/ktank.g tank
+#    load_g $win /usr/brlcad/share/brlcad/7.16.9/db/ktank.g tank
     pack $win -expand true -fill both
 }
 
@@ -120,7 +102,6 @@ proc ::isst::RotMove2 {x y W} {
 }
 
 if { [info script] == $argv0 } {
-    global az el
-	::isst::setup
-        puts "$az $el"
+    global az el oglwin
+    ::isst::setup
 }
