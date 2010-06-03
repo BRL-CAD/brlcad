a3f8f65d6869816a4d7c5bd01a6d87e1eb093bfd
Christopher Sean Morrison <brlcad@gmail.com>
2010-06-03 21:28:40 +0000

    allow ogl windows to receive signals while lingering!  ignoring HUP and INT are particularly annoying as they screw with the process and prevent tools like pix-fb from being quit with ctrl-c
    svn:revision:39540
    svn:branch:trunk
    svn:account:brlcad

--- src/libfb/if_ogl.c
+++ src/libfb/if_ogl.c
@@ -1599,20 +1599,12 @@ fb_ogl_close(FBIO *ifp)
      *
      * The simple for i=0..20 loop will not work, because that smashes
      * some window-manager files.  Therefore, we content ourselves
-     * with eliminating stdin and stdout (fd 0, 1), in the hopes that
-     * this will successfully terminate any pipes or network
-     * connections.  Standard error/out may be used to print
-     * framebuffer debug messages, so they're kept around.
+     * with eliminating stdin, in the hopes that this will
+     * successfully terminate any pipes or network connections.
+     * Standard error/out may be used to print framebuffer debug
+     * messages, so they're kept around.
      */
     fclose(stdin);
-
-    /* Ignore likely signals, perhaps in the background, from other
-     * typing at the keyboard
-     */
-    (void)signal(SIGHUP, SIG_IGN);
-    (void)signal(SIGINT, SIG_IGN);
-    (void)signal(SIGQUIT, SIG_IGN);
-    (void)signal(SIGALRM, SIG_IGN);
 
     while (0 < OGL(ifp)->alive) {
 	ogl_do_event(ifp);
