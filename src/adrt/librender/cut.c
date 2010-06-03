9cebed8fd84f8dea98975082f48c08937f46cc9a
Erik Greenwald <erik@brlcad.org>
2010-06-03 19:35:16 +0000

    set cut line width based on model size and length to begin at viewpoint and end just after model
    svn:revision:39535
    svn:branch:trunk
    svn:account:erikgreenwald

--- src/adrt/librender/cut.c
+++ src/adrt/librender/cut.c
@@ -202,6 +202,15 @@ render_cut_init(render_t *render, char *buf)
     sscanf(buf, "#(%f %f %f) #(%f %f %f)",
 	    ray_pos.v, ray_pos.v+1, ray_pos.v+2,
 	    ray_dir.v, ray_dir.v+1, ray_dir.v+2);
+    VUNITIZE(ray_dir.v);
+
+    shot_width = 0.01 * render->tie->radius;
+    {
+	vect_t v;
+
+	VSUB2(v, ray_pos.v, render->tie->mid);
+	shot_len = 2.0 * render->tie->radius + MAGNITUDE(v) - render->tie->radius;;
+    }
 
 #if 0
     /*
@@ -243,28 +252,15 @@ render_cut_init(render_t *render, char *buf)
     tie_init(&d->tie, 2, TIE_KDTREE_FAST);
 
     /* Triangle 1 */
-    VMOVE(list[0].v, ray_pos.v);
-    list[0].v[2] -= shot_width;
-
-    list[0].v[0] = ray_pos.v[0];
-    list[0].v[1] = ray_pos.v[1];
-    list[0].v[2] = ray_pos.v[2] - shot_width;
-
-    list[1].v[0] = ray_pos.v[0] + shot_len*ray_dir.v[0];
-    list[1].v[1] = ray_pos.v[1] + shot_len*ray_dir.v[1];
-    list[1].v[2] = ray_pos.v[2] + shot_len*ray_dir.v[2] - shot_width;
-
-    list[2].v[0] = ray_pos.v[0] + shot_len*ray_dir.v[0];
-    list[2].v[1] = ray_pos.v[1] + shot_len*ray_dir.v[1];
-    list[2].v[2] = ray_pos.v[2] + shot_len*ray_dir.v[2] + shot_width;
+    VSET(list[0].v, ray_pos.v[0], ray_pos.v[1], ray_pos.v[2] - shot_width);
+    VSET(list[1].v, ray_pos.v[0] + shot_len*ray_dir.v[0], ray_pos.v[1] + shot_len*ray_dir.v[1], ray_pos.v[2] + shot_len*ray_dir.v[2] - shot_width);
+    VSET(list[2].v, ray_pos.v[0] + shot_len*ray_dir.v[0], ray_pos.v[1] + shot_len*ray_dir.v[1], ray_pos.v[2] + shot_len*ray_dir.v[2] + shot_width);
 
     /* Triangle 2 */
     VMOVE(list[3].v, ray_pos.v);
     list[3].v[2] -= shot_width;
 
-    list[4].v[0] = ray_pos.v[0] + shot_len*ray_dir.v[0];
-    list[4].v[1] = ray_pos.v[1] + shot_len*ray_dir.v[1];
-    list[4].v[2] = ray_pos.v[2] + shot_len*ray_dir.v[2] + shot_width;
+    VSET(list[4].v, ray_pos.v[0] + shot_len*ray_dir.v[0], ray_pos.v[1] + shot_len*ray_dir.v[1], ray_pos.v[2] + shot_len*ray_dir.v[2] + shot_width);
 
     VMOVE(list[5].v, ray_pos.v);
     list[5].v[2] += shot_width;
