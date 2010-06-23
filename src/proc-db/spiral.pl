02326c5f339747836fe08426db768d026cebbe7a
Christopher Sean Morrison <brlcad@gmail.com>
2010-06-23 00:31:41 +0000

    add a fudge factor of 6 degrees to compensate for the fact that we're translating the segments based on their lower corner point instead of their natural center.  the x/y needs to change and/or the rotation angle needs to take the size and keypoint of the box into consideration.  fudge is 'good enough' for this scripting example, though.
    we also batch together mged invocations in sets of 100 to reduce overhead.  improves runtime by two orders of magnitude.  names are simplified as well.
    
    svn:revision:39670
    svn:branch:trunk
    svn:account:brlcad

--- src/proc-db/spiral.pl
+++ src/proc-db/spiral.pl
@@ -11,7 +11,7 @@
 ######################################################################
 
 use warnings;
-use constant PI => 4 * atan2 1, 1;
+use constant PI => 4.0 * atan2(1, 1);
 
 if (-e "spiral.g") {
   print "ERROR: spiral.g already exists.  Delete or move it.\n";
@@ -20,7 +20,7 @@ if (-e "spiral.g") {
 
 $t_min = 0;
 $t_max = 10 * PI;
-$granularity = 1000; # Set the granularity of the model.
+$granularity = 1500; # Set the granularity of the model.
 
 # in 1.s rpp 0 .5 0 1 0 1
 # in 2.s rpp -2 2 0.1 0.9 0.1 0.9
@@ -42,25 +42,42 @@ $ymax2 = $ymax - $thickness;
 $zmin2 = $zmin + $thickness; # -1;
 $zmax2 = $zmax - $thickness; #  1; # You want this going through so that you can cut the object.
 
+# default keypoint is bottom corner, so calculate the box base center
+$xkey = ($xmax - $xmin) / 2.0;
+$ykey = ($ymax - $ymin) / 2.0;
+
 # Future improvement possibility: make it spiral downward.
 
 # generate the spiral
 $i = 0; # $i is the counter.
+$command = "";
 for ($j = $t_min; $j <= $t_max; $j += $t_max / $granularity, $i++) {
   $x = $j * cos($j); $y = $j * sin($j);
-  $ang = 90.0 - atan2($y, $x) * 180 / PI;
-  $command = "front ; in mflb2_$i.s rpp $xmin $xmax $ymin $ymax $zmin $zmax ; in caboff1_$i.s rpp $xmin2 $xmax2 $ymin2 $ymax2 $zmin2 $zmax2 ; comb cross_section_region_$i.c u mflb2_$i.s - caboff1_$i.s ; draw cross_section_region_$i.c ; oed / /cross_section_region_$i.c/mflb2_$i.s ; rot 0 0 $ang ; translate $x $y 0 ; accept ;";
+  $ang = 96.0 - atan2($y, $x) * 180.0 / PI; # 6 degrees fudge factor since we're not calculating the proper x/y
+  $command .= "in box_$i.s rpp $xmin $xmax $ymin $ymax $zmin $zmax ; in hole_$i.s rpp $xmin2 $xmax2 $ymin2 $ymax2 $zmin2 $zmax2 ; comb seg_$i.c u box_$i.s - hole_$i.s ; draw seg_$i.c ; oed / /seg_$i.c/box_$i.s ; keypoint $xkey $ykey 0 ; rot 0 0 $ang ; translate $x $y 0 ; accept ;";
+  # create 100 segments at a time to reduce process initialization overhead
+  if ($i % 100 == 0) {
+    print "#$i at\tx = $x\ty=$y\n\t$command\n\n";
+    `mged -c spiral.g \'$command\'`;
+    $command = "";
+  }
+}
+# pick up any remaining segments if $granularity isn't a multiple of 100
+if ($command ne "") {
   print "#$i at\tx = $x\ty=$y\n\t$command\n\n";
   `mged -c spiral.g \'$command\'`;
+  $command = "";
 }
 
-# make the spiral into one grouped shape
+print "Grouping the spiral segments together into one shape...(please wait)...";
 `mged -c spiral.g 'g spiral.c *.c'`;
+print "done.\n";
 
-# make that shape into a solid region (so it now has mass and occupies space)
+print "Making that shape into a solid region (so it now has mass and occupies space)...\n";
 `mged -c spiral.g 'r spiral.r u spiral.c'`;
 
-# ` g-stl -o spiral.g.stl spiral.g all.g ;`; 
-# ` viewstl spiral.g.stl `;
+# `rt -a 35 -e 25 -s1024 spiral.g spiral.r`;
+# `g-stl -o spiral.g.stl spiral.g all.g`; 
+# `viewstl spiral.g.stl`;
 
 print "All done.\n=n";
