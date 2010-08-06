1ad481eebe280bbce9a834c7b3ffd01a7ea67520
Nicholas Reed <nreed1@umbc.edu>
2010-08-06 21:06:46 +0000

    working button commands in bot editor plus minor interface changes
    svn:revision:40027
    svn:branch:trunk
    svn:account:n_reed

--- src/tclscripts/boteditor/botEditor.tcl
+++ src/tclscripts/boteditor/botEditor.tcl
@@ -19,10 +19,9 @@ if {[catch {
 ::itcl::class BotEditor {
     inherit ::itk::Toplevel
 
-    constructor {args} {}
+    constructor {bot args} {}
 
     public {
-	method load {bot}
 	method onChange {}
 	method apply {}
 	method revert {}
@@ -30,34 +29,39 @@ if {[catch {
 	method reject {}
 
 	proc localizeDialog {d}
+	proc focusOnEnter {d}
     }    
 
     private {
 	variable original ""
+	variable copy ""
 	method showUnsaved {show}
     }
-}
 
-# basic initialization - nothing can really be done until load
-#
-::itcl::body BotEditor::constructor {args} {
-    eval itk_initialize $args
-    
-    $this configure -title "BoT Editor"
+    itk_option define -prefix prefix Prefix "" {}
 }
 
 # open editor for bot named 'bot'
 # this bot is assumed to exist in the current database
 #
-::itcl::body BotEditor::load {bot} {
+::itcl::body BotEditor::constructor {bot args} {
 
-    set original $bot
+    eval itk_initialize $args
+
+    # use proper command context
+    set _ $itk_option(-prefix)
+
+    # remove path prefix from bot name
+    while {[regsub {.*/} $bot {} bot]} {}
 
     # show name of bot being edited
     $this configure -title "[eval $this cget -title] - $bot"
+    set statusLine "editing \"$bot\""
 
-    # handle window close
-#    wm protocol $itk_interior WM_DELETE_WINDOW "$this reject"
+    # make working copy
+    set original $bot
+    set copy $original.edit
+    $_ cp $original $copy
 
     # create layout frames
     itk_component add histFrame {
@@ -65,11 +69,15 @@ if {[catch {
 	    -padding 7
     } {}
     itk_component add eframe {
-	EditPane $itk_interior.editPane
+	EditPane $itk_interior.editPane $copy \
+	    -prefix $itk_option(-prefix)
     } {}
     itk_component add closeframe {
 	ttk::frame $itk_interior.closeFrame \
 	    -padding 7
+    } {}
+    itk_component add statusframe {
+	ttk::frame $itk_interior.statusFrame
     } {}
     
     # add widgets to history frame
@@ -96,10 +104,21 @@ if {[catch {
 	    -command "$this reject"
     } {}
 
+    # add widgets to status frame
+    itk_component add rule {
+	ttk::separator $itk_component(statusframe).separator \
+	    -orient horizontal
+    } {}
+    itk_component add status {
+	ttk::label $itk_component(statusframe).statusLabel \
+	    -text $statusLine
+    } {}
+
     # display main frames
-    grid $itk_component(histFrame) -row 0 -column 0 -sticky news
+#    grid $itk_component(histFrame) -row 0 -column 0 -sticky news
     grid $itk_component(eframe) -row 1 -column 0 -sticky news
     grid $itk_component(closeframe) -row 2 -column 0 -sticky news
+    grid $itk_component(statusframe) -row 3 -column 0 -sticky news
     grid rowconfigure $itk_interior 1 -weight 1
     grid columnconfigure $itk_interior 0 -weight 1
 
@@ -110,6 +129,10 @@ if {[catch {
     # display close frame components
     pack $itk_component(reject) -side right
     pack $itk_component(accept) -side right -padx 5
+
+    # display status frame components
+    pack $itk_component(rule) -expand yes -fill x
+    pack $itk_component(status) -expand yes -fill both
 }
 
 # give feedback on unsaved change
@@ -150,50 +173,64 @@ if {[catch {
 # apply changes to original
 ::itcl::body BotEditor::accept {} {
 
+    set _ $itk_option(-prefix)
+    set cmd "$_ kill $original; $_ mv $copy $original; delete object $this"
+
     # get confirmation
     itk_component add confirm {
 	ConfirmationDialog $itk_interior.confirmDialog \
 	    -title {Confirm Accept} \
-	    -text "This operation will overwrite $original in the current database.\n\
+	    -text "This operation will overwrite $original in the current database.\n\n\
 		Are you sure you want to save your changes and exit?" \
-	    -yescommand "delete object $this"
+	    -yescommand $cmd
     } {}
-
-    # place dialog
-    localizeDialog $itk_component(confirm)
 }
 
 # discard all changes
 ::itcl::body BotEditor::reject {} {
+
+    set _ $itk_option(-prefix)
+    set cmd "$_ kill $copy; delete object $this"
 
     # get confirmation
     itk_component add confirm {
 	ConfirmationDialog $itk_interior.confirmDialog \
 	    -title {Confirm Reject} \
 	    -text {Are you sure you want to discard all changes and exit?} \
-	    -yescommand "delete object $this"
+	    -yescommand $cmd
     } {}
-
-    # place dialog
-    localizeDialog $itk_component(confirm)
 }
 
-# place dialog near its parent
+# center dialog with respect to its parent
 ::itcl::body BotEditor::localizeDialog {d} {
 
-    update
+    # get parent geometry
     set p [winfo parent $d]
+    set pwidth [winfo width $p]
+    set pheight [winfo height $p]
+    set px [winfo x $p]
+    set py [winfo y $p]
 
-    # calculate offset
-    set x [winfo x $p]
-    set y [winfo y $p]
-    set x [expr "$x + 50"]
-    set y [expr "$y + 50"]
+    # first draw tiny at center
+    # (this minimizes the visual distraction when we redraw)
+    wm geometry $d 0x0+[expr "$pwidth / 2"]+[expr "$pheight / 2"]
+    update
 
-    # place dialog
+    # now that the window is drawn, we can get accurate
+    # values for the requested dimensions
     set width [winfo reqwidth $d]
     set height [winfo reqheight $d]
-    wm geometry $d ${width}x${height}+${x}+$y
+
+    # draw again, properly centered 
+    set x [expr "$px + ($pwidth - $width) / 2"]
+    set y [expr "$py + ($pheight - $height) / 2"]
+    wm geometry $d ${width}x${height}+${x}+${y}
+}
+
+# automatically focus window when mouse enters it
+::itcl::body BotEditor::focusOnEnter {d} {
+
+    bind $d <Enter> "focus $d"
 }
 
 # Confirmation Dialog helper class
@@ -203,9 +240,6 @@ if {[catch {
 
     constructor {args} {
 	eval itk_initialize $args
-
-	# disable resize
-	wm resizable $itk_interior 0 0
 
 	# make container frame
 	itk_component add main {
@@ -226,17 +260,18 @@ if {[catch {
 	# add components
 	itk_component add msg {
 	    ttk::label $itk_component(info).message \
-		-text $itk_option(-text)
+		-text $itk_option(-text) \
+		-wraplength 300 
 	} {}
 	itk_component add yes {
 	    ttk::button $itk_component(act).confirm \
 		-text Yes \
-		-command "$itk_option(-yescommand)"
+		-command "grab release $itk_interior; $itk_option(-yescommand)"
 	} {}
 	itk_component add no {
 	    ttk::button $itk_component(act).deny \
 		-text No \
-		-command "delete object $this"
+		-command "grab release $itk_interior; delete object $this"
 	} {}
 
 	# display frames
@@ -248,6 +283,17 @@ if {[catch {
 	pack $itk_component(msg) -expand yes
 	pack $itk_component(no) -side right
 	pack $itk_component(yes) -side right -padx 5
+
+
+	# make modal
+	grab $itk_interior
+
+	# disable resize
+	wm resizable $itk_interior 0 0
+
+	# center and auto focus dialog
+	BotEditor::localizeDialog $itk_interior
+	BotEditor::focusOnEnter $itk_interior
     }
 
     itk_option define -text text Text "" {}
@@ -259,7 +305,7 @@ if {[catch {
 ::itcl::class EditPane {
     inherit itk::Widget
 
-    constructor {args} {
+    constructor {bot args} {
 	eval itk_initialize $args
 
 	# make container frame
@@ -288,7 +334,8 @@ if {[catch {
 
 	# add components
 	itk_component add tools {
-	    BotTools $itk_component(lframe).tools
+	    BotTools $itk_component(lframe).tools $bot \
+		-prefix $itk_option(-prefix)
 	} {}
 	itk_component add props {
 	    BotPropertyBox $itk_component(rframe).properties
@@ -313,4 +360,6 @@ if {[catch {
 	pack $itk_component(tools) -expand yes -fill both
 	pack $itk_component(props) -expand yes -fill both
     }
+
+    itk_option define -prefix prefix Prefix "" {}
 }
--- src/tclscripts/boteditor/botTools.tcl
+++ src/tclscripts/boteditor/botTools.tcl
@@ -1,16 +1,40 @@
+# BotTools class for wrapping BoT commands
+#     
+# Usage: BotTools <instance name> <bot name> [-prefix <Archer instance name>]
+# 
+# The -prefix option is used when this class is instanced in Archer,
+# where the ged commands are methods of the Archer mega-widget instance.
+#
+package require Tk
+package require Itcl
+package require Itk
+
 ::itcl::class BotTools {
     inherit itk::Widget
 
-    constructor {args} {}
+    constructor {bot args} {}
+
+    public {
+	method apply {cmd args}
+    }
 
     private {
 	variable panes 0
+	variable bot ""
 	method makePane {}
 	method makeQualityPane {}
     }
+
+    itk_option define -prefix prefix Prefix "" {}
 }
 
-::itcl::body BotTools::constructor {args} {
+# initialize
+::itcl::body BotTools::constructor {b args} {
+
+    eval itk_initialize $args
+
+    set bot $b
+
     # container frame
     itk_component add tframe {
 	ttk::frame $itk_interior.toolFrame
@@ -22,14 +46,27 @@
     } {}
 
     # add quality tab
-    $itk_component(nb) add [makeQualityPane] -text {Mesh Quality}
+    $itk_component(nb) add [makeQualityPane] \
+	-text {Mesh Quality} \
+	-sticky w
 
     pack $itk_component(tframe) -expand yes -fill both
     pack $itk_component(nb) -expand yes -fill both
-
-    eval itk_initialize $args
 }
 
+# convenience method for uniformly applying bot_* commands
+::itcl::body BotTools::apply {cmd args} {
+
+    # use proper context
+    set _ $itk_option(-prefix)
+
+    # run command; overwrite original
+    $_ mv $bot $bot.old
+    eval $_ $cmd $args $bot $bot.old
+    $_ kill $bot.old
+}
+
+# convenience method for creating a new tab pane
 ::itcl::body BotTools::makePane {} {
     set name "pane$panes"
     incr panes
@@ -42,46 +79,57 @@
     return $itk_component($name)
 }
 
+# makes 'Mesh Quality' pane
 ::itcl::body BotTools::makeQualityPane {} {
+
+    # get new container pane
     set pane [makePane]
 
+    # add pane components
     itk_component add condense {
 	ttk::button $pane.condense \
-	    -text Condense
+	    -text Condense \
+	    -command "$this apply bot_condense"
     } {}
-
     itk_component add ffaces {
 	ttk::button $pane.fuse_faces \
-	    -text {Fuse Faces}
+	    -text {Fuse Faces} \
+	    -command "$this apply bot_face_fuse"
     } {}
-
     itk_component add fvertices {
 	ttk::button $pane.fuse_vertices \
-	    -text {Fuse Vertices}
+	    -text {Fuse Vertices} \
+	    -command "$this apply bot_vertex_fuse"
     } {}
-
     itk_component add sort {
 	ttk::button $pane.sort_faces \
-	    -text {Sort Faces}
+	    -text {Sort Faces} \
+	    -command "$this apply bot_face_fuse"
     } {}
-
     itk_component add decimate {
 	ttk::button $pane.decimate \
-	    -text {Decimate}
+	    -text {Decimate} \
+	    -command "$this apply bot_decimate -n 5"
     } {}
-
     itk_component add auto {
 	ttk::button $pane.auto_simplify \
-	    -text {Automatically Simplify}
+	    -text {Automatically Simplify} \
     } {}
 
+    # draw components
     set padx 3; set pady 2
-    grid $itk_component(condense) -row 0 -column 0 -padx $padx -pady $pady
-    grid $itk_component(ffaces) -row 1 -column 0 -padx $padx -pady $pady
-    grid $itk_component(fvertices) -row 2 -column 0 -padx $padx -pady $pady
-    grid $itk_component(sort) -row 0 -column 1 -padx $padx -pady $pady
-    grid $itk_component(decimate) -row 1 -column 1 -padx $padx -pady $pady
-    grid $itk_component(auto) -row 3 -column 0 -columnspan 2 -padx $padx -pady $pady
+    grid $itk_component(condense) -row 0 -column 0 \
+        -padx $padx -pady $pady
+    grid $itk_component(ffaces) -row 1 -column 0 \
+        -padx $padx -pady $pady
+    grid $itk_component(fvertices) -row 2 -column 0 \
+        -padx $padx -pady $pady
+    grid $itk_component(sort) -row 0 -column 1 \
+        -padx $padx -pady $pady
+    grid $itk_component(decimate) -row 1 -column 1 \
+        -padx $padx -pady $pady
+    grid $itk_component(auto) -row 3 -column 0 \
+        -columnspan 2 -padx $padx -pady $pady
 
     return $pane
 }
