f131b14ba6e721dd4b6469b33a230f97e5bd7800
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2010-08-09 18:55:30 +0000

    Will need these files for step build too, in principle.
    svn:revision:40059
    svn:branch:cmake
    svn:account:starseeker

---
+++ src/other/step/misc/CMake/FindLEX.cmake
@@ -0,0 +1,86 @@
+#
+# $Id$
+#
+# - Find lex executable
+#
+# Based off of FindLEX.cmake from Wireshark - their license file in
+# the Cmake modules directory states:
+#
+# "All files in this directory are dual licensed by the licenses
+#  included below, unless the individual license of a file says
+#  otherwise."
+#
+# These license are the GPL and BSD licenses - BRL-CAD will opt
+# to use and modify the file under BSD and reproduce it here
+# as seen in the LICENSE.txt file from Wireshark
+#
+#  Copyright (c) ...
+#  All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# * Redistributions of source code must retain the above copyright
+#   notice, this list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright
+#   notice, this list of conditions and the following disclaimer in
+#   the documentation and/or other materials provided with the
+#   distribution.
+#
+# * Neither the name of the <ORGANIZATION> nor the names of its
+#   contributors may be used to endorse or promote products derived
+#   from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+FIND_PROGRAM(LEX_EXECUTABLE
+  NAMES
+    flex
+    lex
+  PATHS
+    /bin
+    /usr/bin
+    /usr/local/bin
+    /sbin
+)
+
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(LEX DEFAULT_MSG LEX_EXECUTABLE)
+
+MARK_AS_ADVANCED(LEX_EXECUTABLE)
+
+# flex a .l file
+
+MACRO(ADD_LEX_FILES _sources )
+    FOREACH (_current_FILE ${ARGN})
+      GET_FILENAME_COMPONENT(_in ${_current_FILE} ABSOLUTE)
+      GET_FILENAME_COMPONENT(_basename ${_current_FILE} NAME_WE)
+
+      SET(_outc ${CMAKE_CURRENT_BINARY_DIR}/${_basename}.c)
+      SET(_outh ${CMAKE_CURRENT_BINARY_DIR}/${_basename}_lex.h)
+
+      ADD_CUSTOM_COMMAND(
+        OUTPUT ${_outc}
+        COMMAND ${LEX_EXECUTABLE}
+          -o${_outc}
+          --header-file=${_outh}
+          ${_in}
+        DEPENDS ${_in}
+      )
+    SET(${_sources} ${${_sources}} ${_outc} )
+  ENDFOREACH (_current_FILE)
+ENDMACRO(ADD_LEX_FILES)
+
