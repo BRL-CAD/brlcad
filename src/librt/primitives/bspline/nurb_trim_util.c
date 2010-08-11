eda510ab25fc5a93ff786e698b3befcd588bc27c
Christopher Sean Morrison <brlcad@gmail.com>
2010-08-11 23:54:03 +0000

    ws indent style cleanup along with doxygen comment conversion.
    svn:revision:40121
    svn:branch:trunk
    svn:account:brlcad

--- src/librt/primitives/bspline/nurb_trim_util.c
+++ src/librt/primitives/bspline/nurb_trim_util.c
@@ -36,48 +36,46 @@
 #include "raytrace.h"
 #include "nurb.h"
 
-/* Check to see if the curve conmtrol polygon
- * wonders outside the parametric range given.
- * This is usefull if a trimming curve control polygon is outside
- * but the evaluated curve is not. We will want to refine the
- * curve so that is lies within the range;
+/**
+ * Check to see if the curve conmtrol polygon wonders outside the
+ * parametric range given.  This is usefull if a trimming curve
+ * control polygon is outside but the evaluated curve is not. We will
+ * want to refine the curve so that is lies within the range;
  * otherwise it breaks the surface evaluation
  */
-
 int
 rt_nurb_crv_in_range(struct edge_g_cnurb *crv, fastf_t u_min, fastf_t u_max, fastf_t v_min, fastf_t v_max)
 {
     point_t eval;
     fastf_t *pts;
-    int	coords = RT_NURB_EXTRACT_COORDS( crv->pt_type );
-    int 	rat = RT_NURB_IS_PT_RATIONAL(crv->pt_type);
-    int	i;
+    int coords = RT_NURB_EXTRACT_COORDS(crv->pt_type);
+    int rat = RT_NURB_IS_PT_RATIONAL(crv->pt_type);
+    int i;
 
     pts = &crv->ctl_points[0];
 
-    for ( i = 0; i < crv->c_size; i++)
-    {
-	if (rat)
-	{
+    for (i = 0; i < crv->c_size; i++) {
+	if (rat) {
 	    eval[0] = pts[0] / pts[2];
 	    eval[1] = pts[1] / pts[2];
 	    eval[2] = 1;
-	} else	{
+	} else {
 	    eval[0] = pts[0];
 	    eval[1] = pts[1];
 	    eval[2] = 1;
 	}
 
-	if ( eval[0] < u_min || eval[0] > u_max )
+	if (eval[0] < u_min || eval[0] > u_max)
 	    return 0;
 
-	if ( eval[1] < v_min || eval[1] > v_max )
+	if (eval[1] < v_min || eval[1] > v_max)
 	    return 0;
 
 	pts += coords;
     }
     return 1;
 }
+
 
 /*
  * Local Variables:
