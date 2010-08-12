e90a6441a038b7f7f33266c5b6af3bb96b6593ae
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2010-08-12 01:27:29 +0000

    Do the same license inclusion for FindYACC
    svn:revision:40124
    svn:branch:cmake
    svn:account:starseeker

--- misc/CMake/FindLEX.cmake
+++ misc/CMake/FindLEX.cmake
@@ -42,7 +42,7 @@
 #      ${LEX_MyScanner_OUTPUTS}
 #   )
 #  ====================================================================
-
+#
 #=============================================================================
 # Copyright 2010 United States Government as represented by
 #                the U.S. Army Research Laboratory.
@@ -76,6 +76,7 @@
 # THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#=============================================================================
 
 FIND_PROGRAM(LEX_EXECUTABLE flex DOC "path to the lex executable")
 IF(NOT LEX_EXECUTABLE)
