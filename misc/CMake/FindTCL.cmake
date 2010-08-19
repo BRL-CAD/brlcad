b31bcb5777ec85f36e56ebd0fd96b3c5177d4ed3
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2010-08-19 16:00:07 +0000

    start working on a macro for the looping search through versions
    svn:revision:40244
    svn:branch:cmake
    svn:account:starseeker

--- misc/CMake/FindTCL.cmake
+++ misc/CMake/FindTCL.cmake
@@ -134,7 +134,48 @@ MACRO(TKGRAPHICSSYSTEM wishcmd resultvar)
    MESSAGE("TK graphics: ${${resultvar}}")
 ENDMACRO()
 
-TKGRAPHICSSYSTEM(wish TK_GRAPHICSTYPE)
+#TKGRAPHICSSYSTEM(wish TK_GRAPHICSTYPE)
+
+# For ActiveState's Tcl/Tk install on Windows, there are some specific
+# paths that may be needed.  This is a macro-ized version of the paths
+# found in CMake's FindTCL.cmake
+MACRO(WIN32TCLTKPATHS vararray extension)
+  IF(WIN32)
+    GET_FILENAME_COMPONENT(
+      ActiveTcl_CurrentVersion 
+      "[HKEY_LOCAL_MACHINE\\SOFTWARE\\ActiveState\\ActiveTcl;CurrentVersion]" 
+      NAME)
+      SET(${vararray} ${${vararray}}
+        "[HKEY_LOCAL_MACHINE\\SOFTWARE\\ActiveState\\ActiveTcl\\${ActiveTcl_CurrentVersion}]/${extension}")
+      FOREACH(MAJORNUM ${TCLTK_POSSIBLE_MAJOR_VERSIONS})
+        FOREACH(MINORNUM ${TCLTK_POSSIBLE_MINOR_VERSIONS})
+         SET(${vararray} ${${vararray}} "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Scriptics\\Tcl\\${MAJORNUM}.${MINORNUM};Root]/${extension}")
+        ENDFOREACH()
+      ENDFOREACH()
+   ENDIF(WIN32)
+ENDMACRO(WIN32TCLTKPATHS)
+
+MACRO(FINDLIBRARYVERSIONS targetname pathnames options)
+   FOREACH(MAJORNUM ${TCLTK_POSSIBLE_MAJOR_VERSIONS})
+      FOREACH(MINORNUM ${TCLTK_POSSIBLE_MINOR_VERSIONS})
+         SET(TCLTK_${targetname}${MAJORNUM}${MINORNUM} TCLTK_${targetname}${MAJORNUM}${MINORNUM}-NOTFOUND)
+         FOREACH(SPATH ${pathnames})
+            FIND_LIBRARY(TCLTK_${targetname}${MAJORNUM}${MINORNUM} NAMES ${targetname}${MAJORNUM}.${MINORNUM} ${targetname}${MAJORNUM}${MINORNUM} PATHS ${SPATH} ${options})
+            IF(NOT TCLTK_${targetname}${MAJORNUM}${MINORNUM} MATCHES "NOTFOUND$")
+               # On repeat configures, CMAKE_INSTALL_PREFIX apparently gets added to the FIND_* paths.  This is a problem
+               # if re-installing over a previous install so strip these out - don't use output from the previous build to 
+	       # do the current build!
+               IF(NOT TCLTK_${targetname}${MAJORNUM}${MINORNUM} MATCHES "^${CMAKE_INSTALL_PREFIX}")
+                  SET(TCLTK_${targetname}${MAJORNUM}${MINORNUM}_LIST ${TCLTK_${targetname}${MAJORNUM}${MINORNUM}_LIST} ${TCLTK_${targetname}${MAJORNUM}${MINORNUM}})
+               endif()
+            endif()
+            SET(TCLTK_${targetname}${MAJORNUM}${MINORNUM} TCLTK_${targetname}${MAJORNUM}${MINORNUM}-NOTFOUND)
+         ENDFOREACH()
+	 MESSAGE("TCLTK_${targetname}${MAJORNUM}${MINORNUM}_LIST: ${TCLTK_${targetname}${MAJORNUM}${MINORNUM}_LIST}")
+	 SET(TCLTK_${targetname}${MAJORNUM}${MINORNUM}_LIST "")
+      ENDFOREACH()
+   ENDFOREACH()
+ENDMACRO()
 
 # There are four variables that will be used as "feeders"
 # for locating Tcl/Tk installations - TCL_PREFIX, TCL_INCLUDE_DIR,
@@ -147,66 +188,11 @@ IF(TCL_PREFIX)
    SET(TCL_PREFIX_INC "${TCL_PREFIX}/include")
 ENDIF(TCL_PREFIX)
 
-MACRO(WIN32TCLTKPATHS vararray extension)
-  IF(WIN32)
-    GET_FILENAME_COMPONENT(
-      ActiveTcl_CurrentVersion 
-      "[HKEY_LOCAL_MACHINE\\SOFTWARE\\ActiveState\\ActiveTcl;CurrentVersion]" 
-      NAME)
-    SET(${vararray} ${${vararray}}
-      "[HKEY_LOCAL_MACHINE\\SOFTWARE\\ActiveState\\ActiveTcl\\${ActiveTcl_CurrentVersion}]/${extension}")
-      FOREACH(MAJORNUM ${TCLTK_POSSIBLE_MAJOR_VERSIONS})
-        FOREACH(MINORNUM ${TCLTK_POSSIBLE_MINOR_VERSIONS})
-         SET(${vararray} ${${vararray}} "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Scriptics\\Tcl\\${MAJORNUM}.${MINORNUM};Root]/${extension}")
-        ENDFOREACH()
-      ENDFOREACH()
-    ENDIF(WIN32)
-ENDMACRO(WIN32TCLTKPATHS)
+FINDLIBRARYVERSIONS(tcl ${TCL_PREFIX_LIB} NO_SYSTEM_PATH)
+FINDLIBRARYVERSIONS(tcl "/usr" "")
 
-IF(WIN32)
-   WIN32TCLTKPATHS(SYSTEM_LOCALS bin)
-endif(WIN32)
-
-SET(SEARCHPATHS ${TCL_BIN_DIR} ${TCL_PREFIX_BIN} ${SYSTEM_LOCALS})
-
-FOREACH(MAJORNUM ${TCLTK_POSSIBLE_MAJOR_VERSIONS})
-   FOREACH(MINORNUM ${TCLTK_POSSIBLE_MINOR_VERSIONS})
-      SET(TCL_TCLSH${MAJORNUM}${MINORNUM} TCL_TCLSH${MAJORNUM}${MINORNUM}-NOTFOUND)
-      FOREACH(SPATH ${SEARCHPATHS})
-         FIND_PROGRAM(TCL_TCLSH${MAJORNUM}${MINORNUM} NAMES tclsh${MAJORNUM}.${MINORNUM} tclsh${MAJORNUM}${MINORNUM} PATHS ${SPATH} NO_SYSTEM_PATH)
-         IF(NOT TCL_TCLSH${MAJORNUM}${MINORNUM} MATCHES "NOTFOUND$")
-            SET(TCL_TCLSH${MAJORNUM}${MINORNUM}_LIST ${TCL_TCLSH${MAJORNUM}${MINORNUM}_LIST} ${TCL_TCLSH${MAJORNUM}${MINORNUM}})
-         endif()
-         SET(TCL_TCLSH${MAJORNUM}${MINORNUM} TCL_TCLSH${MAJORNUM}${MINORNUM}-NOTFOUND)
-      ENDFOREACH()
-      # Tried all the specific paths - now see what the system PATH has
-      FIND_PROGRAM(TCL_TCLSH${MAJORNUM}${MINORNUM} NAMES tclsh${MAJORNUM}.${MINORNUM} tclsh${MAJORNUM}${MINORNUM})
-      IF(NOT TCL_TCLSH${MAJORNUM}${MINORNUM} MATCHES "NOTFOUND$")
-         # Make sure we don't have it already
-         IF (NOT TCL_TCLSH${MAJORNUM}${MINORNUM}_LIST MATCHES "${TCL_TCLSH${MAJORNUM}${MINORNUM}}")
-            # On repeat configures, CMAKE_INSTALL_PREFIX apparently gets added to the FIND_PROGRAM search path.  This is a problem
-            # if re-installing over a previous compile that includes a tclsh of its own, so strip these out - never want
-            # to use a tclsh from a previous build to do the current build!
-            IF(NOT TCL_TCLSH${MAJORNUM}${MINORNUM} MATCHES "^${CMAKE_INSTALL_PREFIX}")
-               SET(TCL_TCLSH${MAJORNUM}${MINORNUM}_LIST ${TCL_TCLSH${MAJORNUM}${MINORNUM}_LIST} ${TCL_TCLSH${MAJORNUM}${MINORNUM}})
-            endif()
-         endif()
-      endif()
-      SET(TCL_FOUND_MAJOR_VERSIONS ${TCL_FOUND_MAJOR_VERSIONS} ${MAJORNUM})
-      SET(TCL_FOUND_MINOR_VERSIONS ${TCL_FOUND_MINOR_VERSIONS} ${MINORNUM})
-      LIST(GET TCL_TCLSH${MAJORNUM}${MINORNUM}_LIST 0 FIRST_ITEM)
-      IF(FIRST_ITEM)
-         SET(TCL_TCLSH${MAJORNUM}${MINORNUM} ${FIRST_ITEM} CACHE STRING "tclsh var" FORCE)
-         IF(NOT TCL_MAX_FOUND_MINOR_VERSION)
-	   SET(TCL_MAX_FOUND_MINOR_VERSION ${MINORNUM})
-         ENDIF()
-      endif()
-   MESSAGE("list${MAJORNUM}${MINORNUM}: ${TCL_TCLSH${MAJORNUM}${MINORNUM}_LIST}")
-   ENDFOREACH()
-ENDFOREACH()
-
-LIST(REMOVE_DUPLICATES TCL_FOUND_MAJOR_VERSIONS)
-LIST(REMOVE_DUPLICATES TCL_FOUND_MINOR_VERSIONS)
+#LIST(REMOVE_DUPLICATES TCL_FOUND_MAJOR_VERSIONS)
+#LIST(REMOVE_DUPLICATES TCL_FOUND_MINOR_VERSIONS)
 
 GET_FILENAME_COMPONENT(TCL_TCLSH_PATH_PARENT "${TCL_BIN_DIR}" PATH)
 SET(TCLTK_POSSIBLE_PATHS ${TCLTK_POSSIBLE_PATHS} ${TCL_TCLSH_PATH_PARENT})
