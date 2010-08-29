a4fb3632155c5a236ec6539e7005cb5a715d2428
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2010-08-29 03:03:57 +0000

    OK, this gets as far as successfully detecting tkpng, but also highlights a limitation of FindTCL - Ubuntu doesn't put its config files in the same place as the libraries, so gonna have to expand the directories to search in in several places.
    svn:revision:40363
    svn:branch:cmake
    svn:account:starseeker

--- misc/CMake/FindTclPackage.cmake
+++ misc/CMake/FindTclPackage.cmake
@@ -1,7 +1,7 @@
 MACRO(FIND_TCL_PACKAGE wishcmd packagename)
-   MESSAGE("wishcmd: ${wishcmd} packagename: ${packagename}")
    STRING(TOUPPER ${packagename} PKGNAME_UPPER)
    SET(packagefind_script "
+package require ${packagename}
 set packageversion NOTFOUND
 set packageversion [package versions ${packagename}]
 set filename \"${CMAKE_BINARY_DIR}/CMakeTmp/${PKGNAME_UPPER}_PKG_VERSION\"
