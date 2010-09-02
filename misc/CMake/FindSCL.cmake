ac6fba300042e839934178efea8951a580ecb193
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2010-09-02 13:57:18 +0000

    Whoops, make the FindSCL file slightly closer to sane
    svn:revision:40428
    svn:branch:cmake
    svn:account:starseeker

--- misc/CMake/FindSCL.cmake
+++ misc/CMake/FindSCL.cmake
@@ -16,17 +16,17 @@
 # SCL_EDITOR_LIB
 
 FIND_PROGRAM(SCL_EXPRESS_EXECUTABLE express  DOC "path to the SCL express executable")
-FIND_PROGRAM(SCL_SYMLINNK_EXECUTABLE express  DOC "path to the SCL symlink executable")
-FIND_PROGRAM(SCL_EXPPP_EXECUTABLE express  DOC "path to the SCL exppp executable")
-FIND_PROGRAM(SCL_FEDEX_OS_EXECUTABLE express  DOC "path to the SCL fedex_os executable")
-FIND_PROGRAM(SCL_FEDEX_IDL_EXECUTABLE express  DOC "path to the SCL fedex_idl executable")
-FIND_PROGRAM(SCL_FEDEX_PLUS_EXECUTABLE express  DOC "path to the SCL fedex_plus executable")
+FIND_PROGRAM(SCL_SYMLINK_EXECUTABLE symlink DOC "path to the SCL symlink executable")
+FIND_PROGRAM(SCL_EXPPP_EXECUTABLE exppp DOC "path to the SCL exppp executable")
+FIND_PROGRAM(SCL_FEDEX_OS_EXECUTABLE fedex_os DOC "path to the SCL fedex_os executable")
+FIND_PROGRAM(SCL_FEDEX_IDL_EXECUTABLE fedex_idl DOC "path to the SCL fedex_idl executable")
+FIND_PROGRAM(SCL_FEDEX_PLUS_EXECUTABLE fedex_plus  DOC "path to the SCL fedex_plus executable")
 
-FIND_LIBRARY(SCL_EXPPP_LIB exppp)
-FIND_LIBRARY(SCL_CORE_LIB exppp)
-FIND_LIBRARY(SCL_DAI_LIB exppp)
-FIND_LIBRARY(SCL_UTILS_LIB exppp)
-FIND_LIBRARY(SCL_EDITOR_LIB exppp)
+FIND_LIBRARY(SCL_EXPPP_LIB NAMES exppp)
+FIND_LIBRARY(SCL_CORE_LIB NAMES stepcore)
+FIND_LIBRARY(SCL_DAI_LIB NAMES stepdai)
+FIND_LIBRARY(SCL_UTILS_LIB NAMES steputils)
+FIND_LIBRARY(SCL_EDITOR_LIB NAMES stepeditor)
 
 INCLUDE(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(SCL DEFAULT_MSG SCL_CORE_LIB SCL_DAI_LIB SCL_UTILS_LIB SCL_FEDEX_PLUS_EXECUTABLE SCL_EXPRESS_EXECUTABLE)
