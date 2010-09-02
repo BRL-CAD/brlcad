3f32ee7540804bd54121dc5427d7e0a40df377e1
Erik Greenwald <erik@brlcad.org>
2010-09-02 21:04:08 +0000

    start wiring together the hit shtuff
    svn:revision:40443
    svn:branch:bottie
    svn:account:erikgreenwald

--- src/librt/primitives/bot/btg.c
+++ src/librt/primitives/bot/btg.c
@@ -27,14 +27,16 @@
 
 #include "raytrace.h"
 #include "rtgeom.h"
+#include "bot.h"
 #include "btg.h"
 
 #define TIE_PRECISION 1
+#include "tie.h"
 #include "tie.c"
 #include "tie_kdtree.c"
 
-void
-*bottie_allocn_double(unsigned long long ntri)
+void *
+bottie_allocn_double(unsigned long long ntri)
 {
     struct tie_s *tie;
     tie = bu_malloc(sizeof(struct tie_s), "TIE");
@@ -48,6 +50,8 @@ void
 bottie_push_double(void *vtie, double **tri, unsigned int ntri, void *u, unsigned int pstride)
 {
     struct tie_s *tie = (struct tie_s *)vtie;
+
+    tie_push(tie, tri, ntri, u, pstride);
 }
 
 int
@@ -57,16 +61,65 @@ bottie_prep_double(void *vtie)
     return 0;	/* always returning OK seems... bad. */
 }
 
-static int
-hitfunc()
+#define MAXHITS 128
+
+struct hitdata_s {
+    int nhits;
+    struct hit hits[MAXHITS];
+    struct xray *rp;
+};
+
+static void *
+hitfunc(tie_ray_t *ray, tie_id_t *id, tie_tri_t *tri, void *ptr)
 {
+    struct hitdata_s *h = (struct hitdata_s *)ptr;
+    struct hit *hp;
+
+    hp = &h->hits[h->nhits];
+    h->nhits++;
+
+    if(h->nhits > MAXHITS) {
+	bu_log("Too many hits!\n");
+	return 1;
+    }
+
+    hp->hit_magic = RT_HIT_MAGIC;
+    hp->hit_dist = id->dist;
+    hp->hit_private = tri->ptr;
+
+    /* add hitdist into array and add one to nhits */
+    return NULL;	/* continue firing */
 }
 
 int
 bottie_shot_double(struct soltab *stp, register struct xray *rp, struct application *ap, struct seg *seghead)
 {
+    struct bot_specific *bot = (struct bot_specific *)stp->st_specific;
+    struct tie_s *tie = (struct tie_s *)bot->tie;
+    struct hitdata_s hitdata;
+    tie_id_t id;
+    tie_ray_t ray;
+
+    hitdata.nhits = 0;
+    hitdata.rp = &ap->a_ray;
+
+    VMOVE(ray.pos.v, rp->r_pt);
+    VMOVE(ray.dir.v, rp->r_dir);
+    ray.depth = ray.kdtree_depth = 0;
+
+    bu_log("Pew\n");
+    tie_work(tie, &ray, &id, hitfunc, &hitdata);
+
     /* use hitfunc to build the hit list */
-    return -1;
+    if(hitdata.nhits == 0)
+	return 0;
+
+    bu_log("Bang!\n");
+
+    /* this func is in ars, for some reason. All it needs is the dist. */
+    rt_hitsort(hitdata.hits, hitdata.nhits);
+
+    return rt_bot_makesegs(hitdata.hits, hitdata.nhits, stp, rp, ap, seghead, NULL);
 }
 
 void
