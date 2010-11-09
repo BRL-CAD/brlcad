bbf279af6c997dbd27290a93fb52372eb4d590b4
Robert Parker <unacknowledged@users.noreply.github.com>
2010-11-09 16:10:50 +0000

    Modified the default_mouse_bindings proc by replacing the previous hack, related to bad mouse behavior on Mac OS 10.5, with a similar hack that let's the user determine whether or not to use the provided hack by setting a no_focus_hack variable in the .mgedrc file.
    svn:revision:41305
    svn:branch:trunk
    svn:account:bob1961

--- src/tclscripts/mged/bindings.tcl
+++ src/tclscripts/mged/bindings.tcl
@@ -273,6 +273,7 @@ proc forward_key_bindings { w } {
 }
 
 proc default_mouse_bindings { w } {
+    global no_focus_hack
     global transform
     global tcl_platform
 
@@ -281,7 +282,7 @@ proc default_mouse_bindings { w } {
 	set osMajorVersion 0
     }
 
-    if {$::tcl_platform(os) == "Darwin" && $osMajorVersion == 9} {
+    if {[info exists no_focus_hack] && $no_focus_hack} {
 	bind $w <1> "winset $w; zoom 0.5; break"
 	bind $w <2> "winset $w; set tmpstr \[dm m %x %y\]; print_return_val \$tmpstr; break"
 	bind $w <3> "winset $w; zoom 2.0; break"
