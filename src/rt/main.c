9c6a14fa7a2292bf46360f94a8d43ec70fb69e14
Christopher Sean Morrison <brlcad@gmail.com>
2010-11-17 20:30:13 +0000

    don't refer to them as 'MGED' databases.  they are BRL-CAD geometry databases.
    svn:revision:41394
    svn:branch:trunk
    svn:account:brlcad

--- src/rt/main.c
+++ src/rt/main.c
@@ -216,7 +216,7 @@ int main(int argc, char **argv)
     }
 
     if ( bu_optind >= argc )  {
-	fprintf(stderr, "%s:  MGED database not specified\n", argv[0]);
+	fprintf(stderr, "%s:  BRL-CAD geometry database not specified\n", argv[0]);
 	(void)fputs(usage, stderr);
 	return 1;
     }
