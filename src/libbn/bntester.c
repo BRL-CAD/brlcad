d20544d7789104154337b59e2c1a55ce0be8e7a3
Richard Weiss <r_weiss@brlcad.org>
2010-12-14 16:07:41 +0000

    Added and made changes within 'src/libbn' to place the initial stubs into BRLCAD for a test program (i.e. 'bntester') to test the 'libbn' library. Updated Makefile.am, and added files 'bntester.c' and 'bntester.dat'.
    svn:revision:41598
    svn:branch:trunk
    svn:account:r_weiss

---
+++ src/libbn/bntester.c
@@ -0,0 +1,50 @@
+/*                       B N T E S T E R . C
+ * BRL-CAD
+ *
+ * Copyright (c) 2004-2010 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+
+#include "common.h"
+
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+#include "bu.h"
+#include "vmath.h"
+#include "bn.h"
+
+
+int
+main(int argc, char **argv)
+{
+
+    bu_log("bntester\n");
+
+    exit(0);
+}
+
+/** @} */
+/*
+ * Local Variables:
+ * mode: C
+ * tab-width: 8
+ * indent-tabs-mode: t
+ * c-file-style: "stroustrup"
+ * End:
+ * ex: shiftwidth=4 tabstop=8
+ */
