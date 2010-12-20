c4bff9b04ff16728e18a94b341892d130fa8c4d3
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2010-12-20 16:54:37 +0000

    Whoops, get byacc too
    svn:revision:41721
    svn:branch:cmake
    svn:account:starseeker

--- misc/CMake/FindX11.cmake
+++ misc/CMake/FindX11.cmake
@@ -46,7 +46,7 @@
 # implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 # See the License for more information.
 #=============================================================================
-# (To distribute this file outside of CMake, substitute the full
+# (To distributed this file outside of CMake, substitute the full
 #  License text for the above reference.)
 
 IF (UNIX)
