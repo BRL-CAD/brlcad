94d08f61b752c20f217db87367e6be42d37d2116
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2011-01-04 01:17:13 +0000

    Sync library.sh with trunk.
    svn:revision:41907
    svn:branch:cmake
    svn:account:starseeker

--- regress/library.sh
+++ regress/library.sh
@@ -50,7 +50,7 @@ export LD_LIBRARY_PATH DYLD_LIBRARY_PATH
 
 ensearch ( ) {
     ensearch_file="$1"
-    ensearch_dirs="$ARG1/../cmake-build/bin ../src $ARG1/src $PATH_TO_THIS/../src ../src/$1 $ARG1/src/$1 $PATH_TO_THIS/../src/$1"
+    ensearch_dirs="$ARG1/src $PATH_TO_THIS/../src ../src/$1 $ARG1/src/$1 $PATH_TO_THIS/../src/$1 ../bin ../src ../src/util ../src/conv ../src/conv/iges ../src/gtools ../src/rt ../bench"
 
     if test "x$ensearch_file" = "x" ; then
 	# nothing to do
