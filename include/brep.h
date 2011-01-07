ca6de5fe7e805418ad1658023d9bcbba6a9fbc54
Christopher Sean Morrison <brlcad@gmail.com>
2011-01-07 09:00:34 +0000

    remove the inclusion of opennurbs_ext.h since we really want to consider it a private implementation detail header.  instead, just provide the minimal forward declarations for BBNode (even though they probably shouldn't be required by brep.h either).
    svn:revision:42006
    svn:branch:trunk
    svn:account:brlcad

--- include/brep.h
+++ include/brep.h
@@ -34,10 +34,17 @@
 #ifdef __cplusplus
 extern "C++" {
 #include "opennurbs.h"
-#include "opennurbs_ext.h"
 #include <iostream>
 #include <fstream>
+
+    namespace brlcad {
+	template <class T>
+	class BVNode;
+
+	typedef class BVNode<ON_BoundingBox> BBNode;
+    }
 }
+
 
 __BEGIN_DECLS
 
@@ -89,6 +96,7 @@ struct brep_specific {
     ON_Brep* brep;
     BrepBoundingVolume* bvh;
 };
+
 
 __END_DECLS
 
