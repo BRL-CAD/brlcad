e1c7e59e3ef14c6be4ea752dabc36c5c30eb8fe2
Christopher Sean Morrison <brlcad@gmail.com>
2011-01-12 19:55:08 +0000

    init to zero, another bu_exit failure to communicate.
    svn:revision:42171
    svn:branch:trunk
    svn:account:brlcad

--- src/util/imgdims.c
+++ src/util/imgdims.c
@@ -101,7 +101,7 @@ main (int argc, char **argv)
     int ch;
     int how = BELIEVE_NAME;
     int nm_bytes = -1;
-    int nm_pixels;
+    int nm_pixels = 0;
     unsigned long int width;
     unsigned long int height;
     struct stat stat_buf;
