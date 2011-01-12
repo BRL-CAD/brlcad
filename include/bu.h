79310a3169eb919a4e1f59b7889dfad29ee32822
Christopher Sean Morrison <brlcad@gmail.com>
2011-01-12 16:56:37 +0000

    follow suit and provide another BU_STR_EQUAL() macro for comparing two strings for equality.  this is similar to the STREQ recommendation by the ibm developerworks best practices article (http://www.ibm.com/developerworks/aix/library/au-hook_duttaC.html) so that developers can use equality as a truthfulness return value consistently.  there are approximately 1400 present uses of strcmp() in BRL-CAD that can use this macro.
    svn:revision:42150
    svn:branch:trunk
    svn:account:brlcad

--- include/bu.h
+++ include/bu.h
@@ -5075,12 +5075,23 @@ BU_EXPORT BU_EXTERN(char *bu_strdupm, (const char *cp, const char *label));
  * bu_strcmp() is a macro that includes the current file name and line
  * number that can be used when bu debugging is enabled.
  *
- * BU_STR_EMPTY() is a macro that tests a string for emptiness, i.e. "" or
- * NULL.
  */
 BU_EXPORT BU_EXTERN(int bu_strcmpm, (const char *string1, const char *string2, const char *label));
 #define bu_strcmp(s)    bu_strcmpm(s, BU_FLSTR)
+
+/**
+ * BU_STR_EMPTY() is a convenience macro that tests a string for
+ * emptiness, i.e. "" or NULL.
+*/
 #define BU_STR_EMPTY(s) (bu_strcmpm(s, "", BU_FLSTR) == 0)
+
+/**
+ * BU_STR_EQUAL() is a convenience macro for testing two
+ * null-terminaed strings for equality, i.e. A == B, and is equivalent
+ * to (bu_strcmp(s1, s2) == 0) returning true if the strings match and
+ * false if they do not.
+ */
+#define BU_STR_EQUAL(s1, s2) (bu_strcmp((s1), (s2)) == 0)
 
 /** @} */
 
