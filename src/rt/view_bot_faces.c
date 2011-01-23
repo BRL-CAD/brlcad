e7988103f1d81f5e4dd2127a6f6fd68b1f0e42e7
Christopher Sean Morrison <brlcad@gmail.com>
2011-01-23 05:28:37 +0000

    initialize all structparse fields
    svn:revision:42554
    svn:branch:trunk
    svn:account:brlcad

--- src/rt/view_bot_faces.c
+++ src/rt/view_bot_faces.c
@@ -60,7 +60,7 @@ static Tcl_HashTable bots;		/* hash table with a bot_face_list entry for each BO
 
 /* Viewing module specific "set" variables */
 struct bu_structparse view_parse[] = {
-    {"",	0, (char *)0,	0,		BU_STRUCTPARSE_FUNC_NULL }
+    {"",	0, (char *)0,	0,		BU_STRUCTPARSE_FUNC_NULL, NULL, NULL }
 };
 
 
