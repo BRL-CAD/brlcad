d1ce40831ba71ea0bf661bb2eb63077f49b4e9b7
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2011-01-28 20:28:52 +0000

    Let's not do the assignment in the if statement - quell MSVC warning
    svn:revision:42716
    svn:branch:trunk
    svn:account:starseeker

--- src/libbu/backtrace.c
+++ src/libbu/backtrace.c
@@ -296,15 +296,19 @@ bu_backtrace(FILE *fp)
     }
 
     /* make sure the debugger exists */
-    if ((locate_gdb = bu_which("gdb"))) {
+    locate_gdb == bu_which("gdb");
+    if (locate_gdb) {
 	debugger_args[0] = bu_strdup(locate_gdb);
 	if (UNLIKELY(bu_debug & BU_DEBUG_BACKTRACE)) {
 	    bu_log("Found gdb in USER path: %s\n", locate_gdb);
 	}
-    } else if ((locate_gdb = bu_whereis("gdb"))) {
-	debugger_args[0] = bu_strdup(locate_gdb);
-	if (UNLIKELY(bu_debug & BU_DEBUG_BACKTRACE)) {
-	    bu_log("Found gdb in SYSTEM path: %s\n", locate_gdb);
+    } else {
+        locate_gdb = bu_whereis("gdb");
+	if (locate_gdb) {
+	    debugger_args[0] = bu_strdup(locate_gdb);
+	    if (UNLIKELY(bu_debug & BU_DEBUG_BACKTRACE)) {
+	        bu_log("Found gdb in SYSTEM path: %s\n", locate_gdb);
+	    }
 	}
     } else {
 	if (UNLIKELY(bu_debug & BU_DEBUG_BACKTRACE)) {
