ae5635ad418b0cbda073134af463ac5fe4db0431
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2011-01-31 07:44:01 +0000

    A variety of fixes and cleanups - had broken Tcl/Tk search, got it working - more advanced options cleanup, got things pretty much where they should be now.  Move termlib logic into FindTERMLIB.cmake so it can work in the third party logic.  Added new ability to the ThirdParty.cmake code - can now FORCE enabling and disabling of any given library, over and above the ENABLE_ALL style logic.  Hopefully that will allow mimicking the autotools ./configure --enable-all --disable-*** style option setting.
    svn:revision:42790
    svn:branch:cmake
    svn:account:starseeker

--- misc/CMake/FindTERMLIB.cmake
+++ misc/CMake/FindTERMLIB.cmake
@@ -1,32 +1,46 @@
 # - Find termlib 
 # Find a curses or other terminal library
 #
-#  TERMLIB_INCLUDE_DIR  - where to find curses or terminal header.
-#  TERMLIB_LIBRARY       - List of libraries when using curses or terminal library.
-#  TERMLIB_FOUND         - True if an appropriate library found.
+#  TERMLIB_LIBRARY       - terminal library.
+#  TERMLIB_INCLUDE_DIR   - terminal header directory.
+#  TERMLIB_FOUND         - library found.
 #
 #=============================================================================
 
-INCLUDE(CheckLibraryExists)
+INCLUDE(ResolveCompilerPaths)
+MACRO(TERMLIB_CHECK_LIBRARY targetname lname func)
+	IF(NOT ${targetname}_LIBRARY)
+		CHECK_LIBRARY_EXISTS(${lname} ${func} "" HAVE_${targetname}_${lname})
+		IF(HAVE_${targetname}_${lname})
+			FIND_PATH(TERMLIB_INCLUDE_DIR ${lname}.h)
+			RESOLVE_LIBRARIES (${targetname}_LIBRARY "-l${lname}")
+			SET(${targetname}_LINKOPT "-l${lname}" CACHE STRING "${targetname} link option")
+			MARK_AS_ADVANCED(${targetname}_LINKOPT)
+		ENDIF(HAVE_${targetname}_${lname})
+	ENDIF(NOT ${targetname}_LIBRARY)
+ENDMACRO(TERMLIB_CHECK_LIBRARY lname func)
 
-SET(TERMHEADER_SEARCH_LIST "termlib;ncurses;curses;termcap;terminfo;tinfo")
-FOREACH(tname ${TERMHEADER_SEARCH_LIST})
-	IF(NOT TERMLIB_INCLUDE_DIR OR NOT TERMLIB_LIBRARY)
-		FIND_PATH(TERMLIB_INCLUDE_DIR ${tname}.h)
-		FIND_LIBRARY(TERMLIB_LIBRARY ${tname})
-		IF(TERMLIB_INCLUDE_DIR AND TERMLIB_LIBRARY)
-			CHECK_LIBRARY_EXISTS(${tname} tputs "" TERMLIB_LIBRARY_EXISTS)
-			IF(NOT TERMLIB_LIBRARY_EXISTS)
-				SET(TERMLIB_LIBRARY "NOTFOUND")
-			ENDIF(NOT TERMLIB_LIBRARY_EXISTS)
-		ENDIF(TERMLIB_INCLUDE_DIR AND TERMLIB_LIBRARY)
-	ENDIF(NOT TERMLIB_INCLUDE_DIR OR NOT TERMLIB_LIBRARY)
-ENDFOREACH(tname ${TERMHEADER_SEARCH_LIST})
 
-MARK_AS_ADVANCED(TERMLIB_LIBRARY TERMLIB_INCLUDE_DIR)
+TERMLIB_CHECK_LIBRARY(TERMLIB termlib tputs)
+TERMLIB_CHECK_LIBRARY(TERMLIB ncurses tputs)
+TERMLIB_CHECK_LIBRARY(TERMLIB curses tputs)
+TERMLIB_CHECK_LIBRARY(TERMLIB termcap tputs)
+TERMLIB_CHECK_LIBRARY(TERMLIB terminfo tputs)
+TERMLIB_CHECK_LIBRARY(TERMLIB tinfo tputs)
+TERMLIB_CHECK_LIBRARY(TERMLIB ccurses tputs)
+include(CheckCFileRuns)
+configure_file(${BRLCAD_CMAKE_DIR}/test_srcs/termlib.c.in ${CMAKE_BINARY_DIR}/CMakeTmp/termlib.c)
+SET(CMAKE_REQUIRED_LIBRARIES ${TERMLIB_LIBRARY})
+CHECK_C_FILE_RUNS(${CMAKE_BINARY_DIR}/CMakeTmp/termlib.c LIBTERM_RESULT)
+IF(NOT LIBTERM_RESULT)
+	SET(TERMLIB_LIBRARY "NOTFOUND" CACHE STRING "TERMLIB" FORCE)
+ELSE(NOT LIBTERM_RESULT)
+	SET(TERMLIB_LIBRARY ${TERMLIB_LIBRARY} CACHE STRING "TERMLIB" FORCE)
+ENDIF(NOT LIBTERM_RESULT)
+MARK_AS_ADVANCED(TERMLIB_LIBRARY)
 
 # handle the QUIETLY and REQUIRED arguments and set TERMLIB_FOUND to TRUE if 
 # all listed variables are TRUE
 INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(TERMLIB DEFAULT_MSG TERMLIB_INCLUDE_DIR TERMLIB_LIBRARY)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(TERMLIB DEFAULT_MSG TERMLIB_LIBRARY TERMLIB_INCLUDE_DIR)
 
