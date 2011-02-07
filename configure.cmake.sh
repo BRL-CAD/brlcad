05dc6f78ca7603631c1b497498e008d972c413fe
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2011-02-07 13:53:55 +0000

    Add beginnings of simple wrapper to allow '../configure --stuff' invocation of CMake configure process.
    svn:revision:43067
    svn:branch:cmake
    svn:account:starseeker

---
+++ configure.cmake.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#
+# This is a simple script to translate autotools style
+# configure options into CMake variables.  The resulting
+# translation is then run as a CMake configure.
+
+srcpath=$(dirname $0)
+options="$srcpath"
+while [ "$1" != "" ]
+do
+   case $1
+   in
+     --enable-all) options="$options -DBRLCAD-ENABLE_ALL_LOCAL_LIBS=ON";
+                   shift;;
+     --prefix=*)   inputstr=$1;
+     		   options="$options -DCMAKE_INSTALL_PREFIX=${inputstr#--prefix=}";
+     		   shift;;
+     *) 	   echo "Warning: unknown option $1";
+     		   shift;;
+   esac
+done 
+echo cmake $options
+cmake $options
