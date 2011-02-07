fb8779a7d1695ef546b2124be38be58a20bf7476
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2011-02-07 20:00:24 +0000

    Add a few more config options, remove commented out C99 stuff (belongs in CompilerFlags.cmake)
    svn:revision:43095
    svn:branch:cmake
    svn:account:starseeker

--- configure.cmake.sh
+++ configure.cmake.sh
@@ -10,15 +10,19 @@ while [ "$1" != "" ]
 do
    case $1
    in
-     --enable-all) 	options="$options -DBRLCAD-ENABLE_ALL_LOCAL_LIBS=ON";
-                   	shift;;
-     --enable-debug) 	options="$options -DCMAKE_BUILD_TYPE=Debug";
-                   	shift;;
-     --prefix=*)   	inputstr=$1;
-     		   	options="$options -DCMAKE_INSTALL_PREFIX=${inputstr#--prefix=}";
-     		   	shift;;
-     *) 	   	echo "Warning: unknown option $1";
-     		   	shift;;
+     --enable-all) 	          options="$options -DBRLCAD-ENABLE_ALL_LOCAL_LIBS=ON";
+                   	          shift;;
+     --enable-debug) 	          options="$options -DCMAKE_BUILD_TYPE=Debug";
+                   	          shift;;
+     --disable-strict)            options="$options -DBRLCAD-ENABLE_STRICT=OFF";
+     			          shift;;
+     --disable-warnings)          options="$options -DBRLCAD-ENABLE_WARNINGS=OFF";
+     			          shift;;
+     --prefix=*)   	          inputstr=$1;
+     		   	          options="$options -DCMAKE_INSTALL_PREFIX=${inputstr#--prefix=}";
+     		   	          shift;;
+     *) 	   	          echo "Warning: unknown option $1";
+     		   	          shift;;
    esac
 done 
 echo cmake $options
