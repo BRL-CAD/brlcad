8cb9d35bfb477eb11937246886a162d274bb46f2
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2011-03-04 22:02:22 +0000

    Variety of fixes to both libged and librt search logic.
    svn:revision:43706
    svn:branch:trunk
    svn:account:starseeker

--- src/libged/search.c
+++ src/libged/search.c
@@ -55,76 +55,90 @@ ged_search(struct ged *gedp, int argc, const char *argv_orig[])
 
     if (argc < 2) {
 	bu_vls_printf(&gedp->ged_result_str, " [path] [expressions...]\n");
-    } else {
-	/* initialize list of search paths */
-	BU_GETSTRUCT(path_list, db_full_path_list);
-	BU_LIST_INIT(&(path_list->l));
-
-	db_full_path_init(&dfp);
-	db_update_nref(gedp->ged_wdbp->dbip, &rt_uniresource);
-
-	/* initialize result */
-	bu_vls_trunc(&gedp->ged_result_str, 0);
-
-	/* Check if we're doing a unique object list */
-	if ((BU_STR_EQUAL(argv[plan_argv], "."))) {
-		build_uniq_list = 1;
-		plan_argv++;
-	}
-
-	while (!plan_found) {
-		if (!((argv[plan_argv][0] == '-') || (argv[plan_argv][0] == '!')  || (argv[plan_argv][0] == '(')) && (!BU_STR_EQUAL(argv[plan_argv], "/")) && (!BU_STR_EQUAL(argv[plan_argv], "."))) {
-			/* We seem to have a path - make sure it's valid */
-			if (db_string_to_path(&dfp, gedp->ged_wdbp->dbip, argv[plan_argv]) == -1) {
-				bu_vls_printf(&gedp->ged_result_str,  " Search path not found in database.\n");
-				db_free_full_path(&dfp);
-				bu_free_argv(argc, argv);	
-				return GED_ERROR;
-			} else {
-				BU_GETSTRUCT(new_entry, db_full_path_list);
-				new_entry->path = (struct db_full_path *) bu_malloc(sizeof(struct db_full_path), "new full path");
-				db_full_path_init(new_entry->path);
-				db_dup_full_path(new_entry->path, (const struct db_full_path *)&dfp);
-				BU_LIST_PUSH(&(path_list->l), &(new_entry->l));
-				plan_argv++;
-			}
-		} else {
-			plan_found = 1;
-		}
-	}
-
-	dbplan = db_search_formplan(&argv[plan_argv], gedp->ged_wdbp->dbip, gedp->ged_wdbp);
-	if (!dbplan) {
-		bu_vls_printf(&gedp->ged_result_str,  "Failed to build find plan.\n");
-		db_free_full_path(&dfp);
-		bu_free_argv(argc, argv);
-		db_free_full_path_list(path_list);
-		return GED_ERROR;
-	} else {
-		if (build_uniq_list) {
-			uniq_db_objs = db_search_unique_objects(dbplan, path_list, gedp->ged_wdbp->dbip, gedp->ged_wdbp);
-		} else {
-			search_results = db_search_full_paths(dbplan, path_list, gedp->ged_wdbp->dbip, gedp->ged_wdbp);
-		}
-	}
-
-	/* Assign results to string - if we're doing a list, process the results for unique objects - otherwise
-	 * just assemble the full path list and return it */
-	if (build_uniq_list) {
-		for (i=0; i < (int)BU_PTBL_LEN(uniq_db_objs); i++) {
-			dp = (struct directory *)BU_PTBL_GET(uniq_db_objs, i);
-			bu_vls_printf(&gedp->ged_result_str, "%s\n", dp->d_namep);
-		}
-		bu_ptbl_free(uniq_db_objs);
-	} else {
-		for(BU_LIST_FOR(entry, db_full_path_list, &(search_results->l))) {
-			bu_vls_printf(&gedp->ged_result_str, "%s\n", db_path_to_string(entry->path));
-		}
-		db_free_full_path_list(search_results);
-	}
+	return TCL_OK;
+    } 
+    /* Check if we're doing a unique object list */
+    if ((BU_STR_EQUAL(argv[plan_argv], "."))) {
+	    build_uniq_list = 1;
+	    plan_argv++;
+	    if (argc < 3) {
+		    bu_vls_printf(&gedp->ged_result_str, " [path] [expressions...]\n");
+		    return TCL_OK;
+	    }
     }
-    db_free_full_path_list(path_list);
+
+    /* initialize list of search paths */
+    BU_GETSTRUCT(path_list, db_full_path_list);
+    BU_LIST_INIT(&(path_list->l));
+
+    db_full_path_init(&dfp);
+    db_update_nref(gedp->ged_wdbp->dbip, &rt_uniresource);
+
+    /* initialize result */
+    bu_vls_trunc(&gedp->ged_result_str, 0);
+
+    while (!plan_found) {
+	    if (BU_STR_EQUAL(argv[plan_argv], "/") || BU_STR_EQUAL(argv[plan_argv], ".")) {
+		    plan_argv++;
+	    } else {
+		    if (!((argv[plan_argv][0] == '-') || (argv[plan_argv][0] == '!')  || (argv[plan_argv][0] == '(')) ) {
+			    /* We seem to have a path - make sure it's valid */
+			    if (db_string_to_path(&dfp, gedp->ged_wdbp->dbip, argv[plan_argv]) == -1) {
+				    bu_vls_printf(&gedp->ged_result_str,  " Search path not found in database.\n");
+				    db_free_full_path(&dfp);
+				    bu_free_argv(argc, argv);	
+				    return GED_ERROR;
+			    } else {
+				    BU_GETSTRUCT(new_entry, db_full_path_list);
+				    new_entry->path = (struct db_full_path *) bu_malloc(sizeof(struct db_full_path), "new full path");
+				    db_full_path_init(new_entry->path);
+				    db_dup_full_path(new_entry->path, (const struct db_full_path *)&dfp);
+				    BU_LIST_PUSH(&(path_list->l), &(new_entry->l));
+				    plan_argv++;
+				    if (plan_argv > argc) {
+					    bu_vls_printf(&gedp->ged_result_str, " [path] [expressions...]\n");
+					    db_free_full_path(&dfp);
+					    bu_free_argv(argc, argv);	
+					    return TCL_OK;
+				    }
+			    }
+		    } else {
+			    plan_found = 1;
+		    }
+	    }
+    }
+
+    dbplan = db_search_formplan(&argv[plan_argv], gedp->ged_wdbp->dbip, gedp->ged_wdbp);
+    if (!dbplan) {
+	    bu_vls_printf(&gedp->ged_result_str,  "Failed to build find plan.\n");
+	    db_free_full_path(&dfp);
+	    bu_free_argv(argc, argv);
+	    db_free_full_path_list(path_list);
+	    return GED_ERROR;
+    } else {
+	    if (build_uniq_list) {
+		    uniq_db_objs = db_search_unique_objects(dbplan, path_list, gedp->ged_wdbp->dbip, gedp->ged_wdbp);
+	    } else {
+		    search_results = db_search_full_paths(dbplan, path_list, gedp->ged_wdbp->dbip, gedp->ged_wdbp);
+	    }
+    }
+
+    /* Assign results to string - if we're doing a list, process the results for unique objects - otherwise
+     * just assemble the full path list and return it */
+    if (build_uniq_list) {
+	    for (i=0; i < (int)BU_PTBL_LEN(uniq_db_objs); i++) {
+		    dp = (struct directory *)BU_PTBL_GET(uniq_db_objs, i);
+		    bu_vls_printf(&gedp->ged_result_str, "%s\n", dp->d_namep);
+	    }
+	    bu_ptbl_free(uniq_db_objs);
+    } else {
+	    for(BU_LIST_FOR(entry, db_full_path_list, &(search_results->l))) {
+		    bu_vls_printf(&gedp->ged_result_str, "%s\n", db_path_to_string(entry->path));
+	    }
+	    db_free_full_path_list(search_results);
+    }
     db_free_full_path(&dfp);
+    db_free_full_path_list(path_list);
     bu_free_argv(argc, argv);	
     return TCL_OK;
 }
--- src/librt/search.c
+++ src/librt/search.c
@@ -110,13 +110,17 @@ int db_search_isoutput = 0;
 void
 db_free_full_path_list(struct db_full_path_list *path_list)
 {
-    struct db_full_path_list *currentpath;
-    while (BU_LIST_WHILE(currentpath, db_full_path_list, &(path_list->l))) {
-	db_free_full_path(currentpath->path);
-	BU_LIST_DEQUEUE((struct bu_list *)currentpath);
-	bu_free(currentpath, "free db_full_path_list entry");
-    }
-    bu_free(path_list, "free path_list");
+	struct db_full_path_list *currentpath;
+	if (path_list) {
+		if (!BU_LIST_IS_EMPTY(&(path_list->l))) {
+			while (BU_LIST_WHILE(currentpath, db_full_path_list, &(path_list->l))) {
+				db_free_full_path(currentpath->path);
+				BU_LIST_DEQUEUE((struct bu_list *)currentpath);
+				bu_free(currentpath, "free db_full_path_list entry");
+			}
+		}
+		bu_free(path_list, "free path_list");
+	}
 }
 
 
