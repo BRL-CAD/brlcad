8ad2218f1c80998388741d8191b31da826dc11a5
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2011-04-13 13:08:41 +0000

    Hmm - that only worked on the Mac.  Go the safe route and put basename into a tmp string.
    svn:revision:44342
    svn:branch:trunk
    svn:account:starseeker

--- src/libged/search.c
+++ src/libged/search.c
@@ -41,10 +41,11 @@
 #include "./ged_private.h"
 
 int _path_scrub(struct bu_vls *path) {
-	int islocal = 0;
+        struct bu_vls tmp;
+	bu_vls_init(&tmp);
+	int islocal = 1;
 	int newlen = 0;
 	int currlen = bu_vls_strlen(path);
-	if (bu_vls_addr(path)[0] == '.') islocal = 1;
 	if (bu_vls_addr(path)[0] == '/') islocal = 0;
 	bu_vls_trimchar(path, '.');
 	bu_vls_trimchar(path, '/');
@@ -55,7 +56,9 @@ int _path_scrub(struct bu_vls *path) {
 		bu_vls_trimchar(path, '/');
 		newlen = bu_vls_strlen(path);
 	}
-	bu_vls_sprintf(path, "%s", bu_basename(bu_vls_addr(path)));
+	bu_vls_sprintf(&tmp, "%s", bu_basename(bu_vls_addr(path)));
+	bu_vls_sprintf(path, "%s", bu_vls_addr(&tmp));
+	bu_vls_free(&tmp);
 	return islocal;
 }
 
