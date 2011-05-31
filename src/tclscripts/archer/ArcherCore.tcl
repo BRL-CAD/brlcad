6e716eefcee8dec47bcefddf70589aac9ff9f6bf
Robert Parker <unacknowledged@users.noreply.github.com>
2011-05-31 17:27:32 +0000

    Removed bot2pipe from mArcherCoreCommands.
    svn:revision:44717
    svn:branch:trunk
    svn:account:bob1961

--- src/tclscripts/archer/ArcherCore.tcl
+++ src/tclscripts/archer/ArcherCore.tcl
@@ -436,7 +436,7 @@ namespace eval ArcherCore {
 	# a few commands that are implemented here in ArcherCore.
 	variable mArcherCoreCommands { \
 					   3ptarb adjust arced attr bb bev blast bo \
-					   bot bot2pipe bot_condense bot_decimate bot_face_fuse \
+					   bot bot_condense bot_decimate bot_face_fuse \
 					   bot_face_sort bot_flip bot_merge bot_smooth bot_split bot_sync bot_vertex_fuse \
 					   c cd clear clone color comb comb_color combmem copy copyeval copymat \
 					   cp cpi dbconcat dbExpand decompose delete draw E edcodes edcolor edcomb \
