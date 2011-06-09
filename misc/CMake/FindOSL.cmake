6f719b375c6cb54dc610017e209a27aab6d25454
Guilherme Kunigami <kunigami@gmail.com>
2011-06-09 11:30:12 +0000

    Changed FindOSL so that it searches osl libraries from the OSLHOME environment variable (the path was hard-coded before)
    svn:revision:44860
    svn:branch:trunk
    svn:account:kunigami

--- misc/CMake/FindOSL.cmake
+++ misc/CMake/FindOSL.cmake
@@ -10,27 +10,35 @@ include(FindOIIO)
 # Find OSL
 # ================================================
 
+# If 'OSL' not set, use the env variable of that name if available
+if (NOT OSLHOME)
+    if (NOT $ENV{OSLHOME} STREQUAL "")
+        set (OSLHOME $ENV{OSLHOME})
+    endif ()
+endif ()
+
+message("OSL_HOME = ${OSLHOME}")
+
 # Find OSL library and its dependencies
 FIND_LIBRARY(OSLEXEC_LIBRARY 
 	NAMES liboslexec.dylib
-	PATHS /Users/kunigami/dev/osl/dist/macosx/lib)
+	PATHS ${OSLHOME}/lib)
 FIND_LIBRARY(OSLCOMP_LIBRARY 
 	NAMES liboslcomp.dylib
-	PATHS /Users/kunigami/dev/osl/dist/macosx/lib)
+	PATHS ${OSLHOME}/lib)
 FIND_LIBRARY(OSLQUERY_LIBRARY 
 	NAMES liboslquery.dylib
-	PATHS /Users/kunigami/dev/osl/dist/macosx/lib)
+	PATHS ${OSLHOME}/lib)
 
 FIND_PATH (OSL_INCLUDES
 	NAMES oslexec.h
-	PATHS /Users/kunigami/dev/osl/dist/macosx/include/OSL)
+	PATHS ${OSLHOME}/include/OSL)
 
-# == Dependences ==
-
-
-if (OSL_LIBRARY AND OSL_INCLUDES)
+if (OSLEXEC_LIBRARY AND OSLCOMP_LIBRARY AND OSLQUERY_LIBRARY AND OSL_INCLUDES)
    message("Found OSL")
-   message("OSL LIBRARY = ${OSL_LIBRARY}")
+   message("OSL EXEC = ${OSLEXEC_LIBRARY}")
+   message("OSL COMP = ${OSLCOMP_LIBRARY}")
+   message("OSL QUERY = ${OSLQUERY_LIBRARY}")
    message("OSL INCLUDES = ${OSL_INCLUDES}")
 endif()
 
