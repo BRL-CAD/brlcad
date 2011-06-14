115c6b29cad369919ed26aa47465bb199b98ede6
Christopher Sean Morrison <brlcad@gmail.com>
2011-06-14 20:08:59 +0000

    initialize the BU_BITV properly instead of setting the magic manually.
    svn:revision:44985
    svn:branch:trunk
    svn:account:brlcad

--- src/libbu/bitv.c
+++ src/libbu/bitv.c
@@ -91,8 +91,7 @@ bu_bitv_new(unsigned int nbits)
 
     /* get zero'd memory, otherwise need to call BU_BITV_ZEROALL */
     bv = (struct bu_bitv *)bu_calloc(1, (size_t)total_bytes, "struct bu_bitv");
-    BU_LIST_INIT(&bv->l);
-    bv->l.magic = BU_BITV_MAGIC;
+    BU_INIT_BITV(bv);
     bv->nbits = bv_bytes * 8;
 
     return bv;
