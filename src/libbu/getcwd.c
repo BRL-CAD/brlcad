2c64868e63b4fbdb0a7c31786c934131605212f7
Christopher Sean Morrison <brlcad@gmail.com>
2011-06-19 15:14:39 +0000

    be specific about which file's we're referring to for doxygen
    svn:revision:45108
    svn:branch:trunk
    svn:account:brlcad

--- src/libbu/getcwd.c
+++ src/libbu/getcwd.c
@@ -17,7 +17,7 @@
  * License along with this file; see the file named COPYING for more
  * information.
  */
-/** @file getcwd.c
+/** @file libbu/getcwd.c
  *
  * Routine(s) for getting the current working directory full pathname.
  *
