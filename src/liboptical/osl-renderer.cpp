f4c77f8e442857ca8e8eaddc86cc1a5791e0acb8
Guilherme Kunigami <kunigami@gmail.com>
2011-06-20 15:40:13 +0000

    Cleaning up formatting of osl-renderer.cpp
    svn:revision:45122
    svn:branch:trunk
    svn:account:kunigami

--- src/liboptical/osl-renderer.cpp
+++ src/liboptical/osl-renderer.cpp
@@ -1,3 +1,28 @@
+/*                O S L - R E N D E R E R . C P P
+ * BRL-CAD
+ *
+ * Copyright (c) 2011 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file osl-renderer.cpp
+ *
+ * This code represents an interface to OSL system. Through it, one can
+ * add OSL shaders and query colors, given some global parameters
+ */
+
 #include "osl-renderer.h"
 
 OSLRenderer::OSLRenderer(){
@@ -243,4 +268,11 @@ void oslrenderer_free(OSLRenderer **osl){
     *osl = NULL;
 }
 
-
+// Local Variables:
+// tab-width: 8
+// mode: C++
+// c-basic-offset: 4
+// indent-tabs-mode: t
+// c-file-style: "stroustrup"
+// End:
+// ex: shiftwidth=4 tabstop=8
