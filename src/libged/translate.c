e9b5f9e6bdd1b0a745f5a2d50504c34795914858
Brandon Hinesley <bhinesley@gmail.com>
2011-06-28 02:10:43 +0000

    Isolating core translate functionality into a seperate function, so that it could be more readily called internally. Laid out comments on how it will be finished (hopefully tomorrow). Also, ged_path_validate had a parameter byval when it should have been a const \*
    svn:revision:45252
    svn:branch:trunk
    svn:account:bhinesley

--- src/libged/translate.c
+++ src/libged/translate.c
@@ -33,6 +33,60 @@
 #include "raytrace.h"
 #include "ged.h"
 
+/* 
+ * it's concievable that this could be exposed, so keep a clean break from
+ * the ged cmd and don't make assumptions
+ */
+HIDDEN int
+translate(struct ged *gedp, point_t * const keypoint,
+	  struct db_full_path * const path, struct directory * const d_obj,
+	  int * const rel_flag) { 
+
+    struct db_full_path full_obj_path;	/* full path to object */
+
+    /* XXX quiet compiler for now */
+    (void)keypoint;
+    (void)rel_flag;
+
+    /* verify existence of path */
+    if (ged_path_validate(gedp, path) == GED_ERROR) {
+	char *s_path = db_path_to_string(path);
+	bu_vls_printf(&gedp->ged_result_str, "path \"%s\" doesn't exist\n",
+		      s_path);
+	bu_free((genptr_t)s_path, "path string");
+	return GED_ERROR;
+    }
+
+    /* verify that object exists under current directory in path */
+    db_full_path_init(&full_obj_path);
+    db_dup_path_tail(&full_obj_path, path, path->fp_len - 1);
+    db_add_node_to_full_path(&full_obj_path, d_obj);
+    if (ged_path_validate(gedp, &full_obj_path) == GED_ERROR) {
+	char *s_path = db_path_to_string(path);
+	bu_vls_printf(&gedp->ged_result_str, "object \"%s\" not found under"
+		      " path \"%s\"\n", d_obj->d_namep, s_path);
+	db_free_full_path(&full_obj_path);
+	bu_free((genptr_t)s_path, "path string");
+	return GED_ERROR;
+    }
+
+    /* build ged_combmem command arguments:
+     *   -everything in obj's tree is fetched and coords modified 
+     *    as necessary
+     *   -build argv
+     *   -call ged_combmem
+     */
+
+    /* no path; modify all instances of obj */
+    /* or */
+    /* path supplied; modify this instance of obj only */
+
+    // ged_combmem(gedp, argc, argv);
+
+    db_free_full_path(&full_obj_path);
+    return GED_OK;
+}
+
 int
 ged_translate(struct ged *gedp, int argc, const char *argv[])
 {
@@ -49,17 +103,13 @@ ged_translate(struct ged *gedp, int argc, const char *argv[])
     int rel_flag = 0;			/* use relative distance */
     char *kp_arg = NULL;        	/* keypoint argument */
     point_t keypoint;
+    vect_t new_vertex;			/* distance/position to translate to */
     const char *s_obj;
     const char *s_path;
     struct db_full_path obj;
     struct db_full_path path;
-    struct db_full_path full_obj_path;	/* full path to object */
     struct directory *d_obj;
     char *endchr = NULL;		/* for strtod's */
-
-    /* testing */
-    mat_t modelchanges, incr, old;
-    vect_t model_incr, ed_sol_pt, new_vertex;
 
     GED_CHECK_DATABASE_OPEN(gedp, GED_ERROR);
     GED_CHECK_READ_ONLY(gedp, GED_ERROR);
@@ -140,7 +190,7 @@ ged_translate(struct ged *gedp, int argc, const char *argv[])
 	;
     #endif
 
-    /* testing */
+    /* XXX testing */
     keypoint[0] = 0;
     keypoint[1] = 0;
     keypoint[2] = 0;
@@ -186,71 +236,51 @@ ged_translate(struct ged *gedp, int argc, const char *argv[])
 	db_free_full_path(&path);
 	return GED_ERROR;
     }
-
-    /* verify existence of path */
-    if (ged_path_validate(gedp, path) == GED_ERROR) {
-	bu_vls_printf(&gedp->ged_result_str, "path \"%s\" doesn't exist",
-		      s_path);
-	db_free_full_path(&path);
-	db_free_full_path(&obj);
-	return GED_ERROR;
-    }
-
-    /* verify that object exists under current directory in path */
-    db_full_path_init(&full_obj_path);
-    db_dup_path_tail(&full_obj_path, &path, path.fp_len - 1);
-    db_append_full_path(&full_obj_path, &obj);
-    if (ged_path_validate(gedp, full_obj_path) == GED_ERROR) {
-	bu_vls_printf(&gedp->ged_result_str, "object \"%s\" not found"
-		      " under path \"%s\"", s_obj, s_path);
-	db_free_full_path(&path);
-	db_free_full_path(&obj);
-	db_free_full_path(&full_obj_path);
-	return GED_ERROR;
+    if ((bu_optind + 1) <= argc) {
+	bu_vls_printf(&gedp->ged_result_str, "multiple objects not yet"
+					     " supported; ");
+	goto disabled;
     }
 
     /* 
-     * Perform transations
+     * Perform translations
      */
     d_obj = DB_FULL_PATH_ROOT_DIR(&obj);
     if (d_obj->d_flags & RT_DIR_SOLID) {
-       /* TODO: translation of solids (note:'p' cmd is not yet available) */
+	/* TODO: translation of solids (note:ged_p cmd is not yet available) */
 	bu_vls_printf(&gedp->ged_result_str, "translation of solids not yet"
 					     " supported; ");
 	goto disabled;
-    }
-    if (d_obj->d_flags & (RT_DIR_REGION | RT_DIR_COMB)) {
-       /* translation of combinations */
+    } else if (d_obj->d_flags & (RT_DIR_REGION | RT_DIR_COMB)) {
+	/* translation of combinations */
+	if (translate(gedp, &keypoint, &path, d_obj, &rel_flag) == GED_ERROR) {
+	    db_free_full_path(&path);
+	    db_free_full_path(&obj);
+	    bu_vls_printf(&gedp->ged_result_str, "translation failed");
+	    return GED_ERROR;
+	}
+
 	bu_vls_printf(&gedp->ged_result_str, "translation of combinations not"
 					     " yet supported; ");
 	goto disabled;
+    } else {
+	db_free_full_path(&path);
+	db_free_full_path(&obj);
+	bu_vls_printf(&gedp->ged_result_str, "unsupported object type");
+	return GED_ERROR;
     }
+	
 
     /* XXX disabled until functional */
-    if ((bu_optind + 1) <= argc)
-	bu_vls_printf(&gedp->ged_result_str, "multiple objects not yet"
-					     " supported; ");
     goto disabled;
-
-    /* do translation */
-    MAT_IDN(incr);
-    MAT_IDN(old);
-    MAT4X3PNT(ed_sol_pt, modelchanges, keypoint);
-    VSUB2(model_incr, new_vertex, ed_sol_pt);
-    MAT_DELTAS_VEC(incr, model_incr);
-    MAT_COPY(old, modelchanges);
-    bn_mat_mul(modelchanges, incr, old);
-    /* new_edit_mats(); */
-
+   
     db_free_full_path(&path);
     db_free_full_path(&obj);
-    db_free_full_path(&full_obj_path);
     return GED_OK;
 
     disabled:
     db_free_full_path(&path);
     db_free_full_path(&obj);
-    db_free_full_path(&full_obj_path);
     bu_vls_printf(&gedp->ged_result_str, "function not yet implemented");
     return GED_ERROR;
 }
