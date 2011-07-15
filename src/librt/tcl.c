3d1b5c0ddef0f3611bd9497563a8ded50cfdb3d7
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2011-07-15 21:28:35 +0000

    remove duplicate decls (XCode 4 clang  no likie)
    svn:revision:45507
    svn:branch:trunk
    svn:account:starseeker

--- src/librt/tcl.c
+++ src/librt/tcl.c
@@ -761,10 +761,6 @@ rt_tcl_import_from_path(Tcl_Interp *interp, struct rt_db_internal *ip, const cha
 void
 rt_tcl_setup(Tcl_Interp *interp)
 {
-    extern size_t rt_bot_minpieces;	/* from globals.c */
-    extern size_t rt_bot_tri_per_piece;	/* from globals.c */
-    extern size_t rt_bot_mintie;	/* from globals.c */
-
     Tcl_LinkVar(interp, "rt_bot_minpieces", (char *)&rt_bot_minpieces, TCL_LINK_WIDE_INT);
 
     Tcl_LinkVar(interp, "rt_bot_tri_per_piece",
