dc6e34b885e78ee52d5d5a4f82920480cdba31ce
Christopher Sean Morrison <brlcad@gmail.com>
2011-07-17 23:33:42 +0000

    FIXME, should not need to declare all liboptical shaders used by libmultispectral.  in theory, multispectral can use all of them and having to list shaders in three places really sucks.  could give multispectral a prefix or suffix so it's a different var, but they shouldn't even be globals.
    svn:revision:45523
    svn:branch:trunk
    svn:account:brlcad

--- include/optical.h
+++ include/optical.h
@@ -91,6 +91,13 @@ viewshade(struct application *app, const struct partition *pp, struct shadework 
 OPTICAL_EXPORT extern const char *optical_version(void);
 
 /* for libmultispectral */
+
+/* FIXME: these should not need to be declared for libmultispectral.
+ * breaks encapsulation/modularity of shaders if we have to list them
+ * in more than one place.  maybe give multispectral arrays their own
+ * suffix so as not to conflict.
+ */
+
 OPTICAL_EXPORT extern struct mfuncs camo_mfuncs[];
 OPTICAL_EXPORT extern struct mfuncs light_mfuncs[];
 OPTICAL_EXPORT extern struct mfuncs stk_mfuncs[];
