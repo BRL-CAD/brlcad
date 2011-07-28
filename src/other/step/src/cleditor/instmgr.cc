3197bfacccb961d9ea902a5e53e6a80f39f20b9f
Christopher Sean Morrison <brlcad@gmail.com>
2011-07-28 05:17:44 +0000

    plug a memory leak accounting for almost a half MB.  delete the InstMgr master and sorted master manager node arrays.
    svn:revision:45679
    svn:branch:trunk
    svn:account:brlcad

--- src/other/step/src/cleditor/instmgr.cc
+++ src/other/step/src/cleditor/instmgr.cc
@@ -63,6 +63,8 @@ InstMgr::~InstMgr()
     {
 	master->DeleteEntries();
     }
+    delete master;
+    delete sortedMaster;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
