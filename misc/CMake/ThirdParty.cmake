511e5e6d055b5125b697adac82febbab8e2fd4d0
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2011-08-12 20:59:15 +0000

    Begin a major re-work of the third-party-option-handling logic in CMake, taking advantage of CMake's ability to constrain variables to particular string values.  This should actually simplify the build logic somewhat in the end, but right now it's definitely alpha and untested in most configurations.
    svn:revision:45946
    svn:branch:trunk
    svn:account:starseeker

--- misc/CMake/ThirdParty.cmake
+++ misc/CMake/ThirdParty.cmake
@@ -1,40 +1,20 @@
 #-----------------------------------------------------------------------------
-MACRO(THIRD_PARTY_OPTION upper lower)
-	# Set up local forcing functions, if not already set up.
-	OPTION(${CMAKE_PROJECT_NAME}_BUILD_LOCAL_${upper}_FORCE_ON "Force the building of the local ${upper} library." OFF)
-	MARK_AS_ADVANCED(${CMAKE_PROJECT_NAME}_BUILD_LOCAL_${upper}_FORCE_ON)
-	OPTION(${CMAKE_PROJECT_NAME}_BUILD_LOCAL_${upper}_FORCE_OFF "Force disabling the local ${upper} library build." OFF)
-	MARK_AS_ADVANCED(${CMAKE_PROJECT_NAME}_BUILD_LOCAL_${upper}_FORCE_OFF)
-
-	# Deal with the case where both FORCE_ON and FORCE_OFF are enabled
-	IF(${CMAKE_PROJECT_NAME}_BUILD_LOCAL_${upper}_FORCE_ON AND ${CMAKE_PROJECT_NAME}_BUILD_LOCAL_${upper}_FORCE_OFF)
-		MESSAGE("Warning - both force options for ${upper} found - going with enable")
-		SET(${CMAKE_PROJECT_NAME}_BUILD_LOCAL_${upper}_FORCE_OFF OFF CACHE BOOL "Force disabling the local ${upper} library build." FORCE)
-	ENDIF(${CMAKE_PROJECT_NAME}_BUILD_LOCAL_${upper}_FORCE_ON AND ${CMAKE_PROJECT_NAME}_BUILD_LOCAL_${upper}_FORCE_OFF)
-
-	# Set BUILD_LOCAL variable based on FORCE options, if enabled
-	IF(${CMAKE_PROJECT_NAME}_BUILD_LOCAL_${upper}_FORCE_ON)
-		SET(${CMAKE_PROJECT_NAME}_BUILD_LOCAL_${upper} ON CACHE BOOL "Build the local ${upper} library." FORCE)
-	ENDIF(${CMAKE_PROJECT_NAME}_BUILD_LOCAL_${upper}_FORCE_ON)
-	IF(${CMAKE_PROJECT_NAME}_BUILD_LOCAL_${upper}_FORCE_OFF)
-		SET(${CMAKE_PROJECT_NAME}_BUILD_LOCAL_${upper} OFF CACHE BOOL "Build the local ${upper} library." FORCE)
-	ENDIF(${CMAKE_PROJECT_NAME}_BUILD_LOCAL_${upper}_FORCE_OFF)
-
-	# Handle ENABLE_ALL option
-	IF(${CMAKE_PROJECT_NAME}-ENABLE_ALL_LOCAL_LIBS)
-		OPTION(${CMAKE_PROJECT_NAME}_BUILD_LOCAL_${upper} "Build the local ${upper} library." ON)
-		IF(NOT ${CMAKE_PROJECT_NAME}_BUILD_LOCAL_${upper}_FORCE_OFF)
-			SET(${CMAKE_PROJECT_NAME}_BUILD_LOCAL_${upper} ON CACHE BOOL "Build the local ${upper} library." FORCE)
-		ENDIF(NOT ${CMAKE_PROJECT_NAME}_BUILD_LOCAL_${upper}_FORCE_OFF)
-	ELSE(${CMAKE_PROJECT_NAME}-ENABLE_ALL_LOCAL_LIBS)
-		OPTION(${CMAKE_PROJECT_NAME}_BUILD_LOCAL_${upper} "Build the local ${upper} library." OFF)
-		IF(NOT ${CMAKE_PROJECT_NAME}_BUILD_LOCAL_${upper}_FORCE_ON)
-			SET(${CMAKE_PROJECT_NAME}_BUILD_LOCAL_${upper} OFF CACHE BOOL "Build the local ${upper} library." FORCE)
-		ENDIF(NOT ${CMAKE_PROJECT_NAME}_BUILD_LOCAL_${upper}_FORCE_ON)
-	ENDIF(${CMAKE_PROJECT_NAME}-ENABLE_ALL_LOCAL_LIBS)
-
+MACRO(THIRD_PARTY lower dir)
+	STRING(TOUPPER ${lower} upper)
+	BUNDLE_OPTION(${CMAKE_PROJECT_NAME}_${upper} "")
+	FOREACH(extraarg ${ARGN})
+		IF(extraarg STREQUAL "NOSYS")
+			IF(${CMAKE_PROJECT_NAME}_${upper} STREQUAL "Auto (S)")
+				SET(${CMAKE_PROJECT_NAME}_${upper} "Auto" CACHE STRING "NOSYS passed - striped system flag from Auto" FORCE)
+			ENDIF(${CMAKE_PROJECT_NAME}_${upper} STREQUAL "Auto (S)")
+		ENDIF(extraarg STREQUAL "NOSYS")
+	ENDFOREACH(extraarg ${ARGN})
 	# Main search logic
-	IF(NOT ${CMAKE_PROJECT_NAME}_BUILD_LOCAL_${upper} OR ${CMAKE_PROJECT_NAME}-ENABLE_SYSTEM_LIBS_ONLY)
+	IF(${CMAKE_PROJECT_NAME}_${upper} STREQUAL "Bundled" OR ${CMAKE_PROJECT_NAME}_${upper} STREQUAL "Auto (B)")
+		SET(${CMAKE_PROJECT_NAME}_${upper}_BUILD ON)
+		SET(${upper}_LIBRARY "${lower}" CACHE STRING "set by THIRD_PARTY macro" FORCE)
+	ELSE(${CMAKE_PROJECT_NAME}_${upper} STREQUAL "Bundled" OR ${CMAKE_PROJECT_NAME}_${upper} STREQUAL "Auto (B)")
+		SET(${CMAKE_PROJECT_NAME}_${upper}_BUILD OFF)
 		# Stash the previous results (if any) so we don't repeatedly call out the tests - only report
 		# if something actually changes in subsequent runs.
 		SET(${upper}_FOUND_STATUS ${${upper}_FOUND})
@@ -54,12 +34,13 @@ MACRO(THIRD_PARTY_OPTION upper lower)
 			INCLUDE(${CMAKE_ROOT}/Modules/Find${upper}.cmake)
 		ENDIF(EXISTS ${${CMAKE_PROJECT_NAME}_CMAKE_DIR}/Find${upper}.cmake)
 		# If we didn't find it, and options allow it, enable the local copy
-		IF(NOT ${upper}_FOUND)
-			IF(NOT ${CMAKE_PROJECT_NAME}-ENABLE_SYSTEM_LIBS_ONLY) 
-				SET(${CMAKE_PROJECT_NAME}_BUILD_LOCAL_${upper} ON CACHE BOOL "Build the local ${upper} library." FORCE)
-				SET(${upper}_LIBRARY "${lower}" CACHE STRING "set by THIRD_PARTY macro" FORCE)
-			ENDIF(NOT ${CMAKE_PROJECT_NAME}-ENABLE_SYSTEM_LIBS_ONLY) 
-		ELSE(NOT ${upper}_FOUND)
+		IF(NOT ${upper}_FOUND AND NOT ${CMAKE_PROJECT_NAME}_${upper} STREQUAL "System" AND NOT ${CMAKE_PROJECT_NAME}_${upper} STREQUAL "Auto (S)")
+			SET(${CMAKE_PROJECT_NAME}_${upper}_BUILD ON)
+			SET(${upper}_LIBRARY "${lower}" CACHE STRING "set by THIRD_PARTY macro" FORCE)
+		ELSE(NOT ${upper}_FOUND AND NOT ${CMAKE_PROJECT_NAME}_${upper} STREQUAL "System" AND NOT ${CMAKE_PROJECT_NAME}_${upper} STREQUAL "Auto (S)")
+			SET(${upper}_FOUND "TRUE" CACHE STRING "${upper}_FOUND" FORCE)
+		ENDIF(NOT ${upper}_FOUND AND NOT ${CMAKE_PROJECT_NAME}_${upper} STREQUAL "System" AND NOT ${CMAKE_PROJECT_NAME}_${upper} STREQUAL "Auto (S)")
+		IF(${upper}_FOUND)
 			# We have to remove any previously built output from enabled local copies of the
 			# library in question, or the linker will get confused - a system lib was found and
 			# system libraries are to be preferred with current options.  This is unfortunate in
@@ -75,133 +56,24 @@ MACRO(THIRD_PARTY_OPTION upper lower)
 					)
 			ENDFOREACH(stale_file ${STALE_FILES})
 			IF("${${upper}_FOUND_STATUS}" MATCHES "${upper}-NOTFOUND")
-				MESSAGE("${upper} has been found - reconfiguring to use system version")
+				MESSAGE("Reconfiguring to use system ${upper}")
 			ENDIF("${${upper}_FOUND_STATUS}" MATCHES "${upper}-NOTFOUND")
-			SET(${upper}_FOUND "TRUE" CACHE STRING "${upper}_FOUND" FORCE)
-		ENDIF(NOT ${upper}_FOUND)
-	ELSE(NOT ${CMAKE_PROJECT_NAME}_BUILD_LOCAL_${upper} OR ${CMAKE_PROJECT_NAME}-ENABLE_SYSTEM_LIBS_ONLY)
-		SET(${upper}_LIBRARY "${lower}" CACHE STRING "set by THIRD_PARTY macro" FORCE)
-	ENDIF(NOT ${CMAKE_PROJECT_NAME}_BUILD_LOCAL_${upper} OR ${CMAKE_PROJECT_NAME}-ENABLE_SYSTEM_LIBS_ONLY)
-
-	MARK_AS_ADVANCED(${CMAKE_PROJECT_NAME}_BUILD_LOCAL_${upper})
+		ENDIF(${upper}_FOUND)
+	ENDIF(${CMAKE_PROJECT_NAME}_${upper} STREQUAL "Bundled" OR ${CMAKE_PROJECT_NAME}_${upper} STREQUAL "Auto (B)")
+	IF(${CMAKE_PROJECT_NAME}_${upper}_BUILD)
+		ADD_SUBDIRECTORY(${dir})
+		SET(${upper}_INCLUDE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/${dir};${CMAKE_CURRENT_BINARY_DIR}/${dir} CACHE STRING "set by THIRD_PARTY_SUBDIR macro" FORCE)
+		IF(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/${dir}.dist)
+			FILE(STRINGS ${CMAKE_CURRENT_SOURCE_DIR}/${dir}.dist ${dir}_ignore_files)
+			DISTCHECK_IGNORE(${dir} ${dir}_ignore_files)
+		ELSE(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/${dir}.dist)
+			MESSAGE("Local build, but file ${CMAKE_CURRENT_SOURCE_DIR}/${dir}.dist not found")
+		ENDIF(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/${dir}.dist)
+	ELSE(${CMAKE_PROJECT_NAME}_${upper}_BUILD)
+		DISTCHECK_IGNORE_ITEM(${dir})
+	ENDIF(${CMAKE_PROJECT_NAME}_${upper}_BUILD)
 	MARK_AS_ADVANCED(${upper}_LIBRARY)
 	MARK_AS_ADVANCED(${upper}_INCLUDE_DIR)
-ENDMACRO(THIRD_PARTY_OPTION)
-
-#-----------------------------------------------------------------------------
-MACRO(THIRD_PARTY_SUBDIR upper lower)
-	IF(${CMAKE_PROJECT_NAME}_BUILD_LOCAL_${upper} AND NOT ${CMAKE_PROJECT_NAME}-ENABLE_SYSTEM_LIBS_ONLY)
-		ADD_SUBDIRECTORY(${lower})
-		SET(${upper}_INCLUDE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/${lower};${CMAKE_CURRENT_BINARY_DIR}/${lower} CACHE STRING "set by THIRD_PARTY_SUBDIR macro" FORCE)
-		IF(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/${lower}.dist)
-			FILE(STRINGS ${CMAKE_CURRENT_SOURCE_DIR}/${lower}.dist ${lower}_ignore_files)
-			DISTCHECK_IGNORE(${lower} ${lower}_ignore_files)
-		ELSE(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/${lower}.dist)
-			MESSAGE("Local build, but file ${CMAKE_CURRENT_SOURCE_DIR}/${lower}.dist not found")
-		ENDIF(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/${lower}.dist)
-	ELSE(${CMAKE_PROJECT_NAME}_BUILD_LOCAL_${upper} AND NOT ${CMAKE_PROJECT_NAME}-ENABLE_SYSTEM_LIBS_ONLY)
-		DISTCHECK_IGNORE_ITEM(${lower})
-	ENDIF(${CMAKE_PROJECT_NAME}_BUILD_LOCAL_${upper} AND NOT ${CMAKE_PROJECT_NAME}-ENABLE_SYSTEM_LIBS_ONLY)
-ENDMACRO(THIRD_PARTY_SUBDIR)
-
-#-----------------------------------------------------------------------------
-
-include(ExternalProject)
-
-MACRO(THIRD_PARTY_NMAKE_EXTERNAL_PROJECT upper projname projpath srcpath extraopts)
-	IF(${CMAKE_PROJECT_NAME}_BUILD_LOCAL_${upper} AND NOT ${CMAKE_PROJECT_NAME}-ENABLE_SYSTEM_LIBS_ONLY)
-		ExternalProject_Add(
-			${projname}
-			DOWNLOAD_COMMAND ""
-			PREFIX ${${CMAKE_PROJECT_NAME}_SOURCE_DIR}/${projpath}/${srcpath}
-			SOURCE_DIR ${${CMAKE_PROJECT_NAME}_SOURCE_DIR}/${projpath}/${srcpath}
-			CONFIGURE_COMMAND ""
-			BUILD_COMMAND cd <SOURCE_DIR> && nmake -f makefile.vc INSTALLDIR=${CMAKE_INSTALL_PREFIX} ${extraopts}
-			INSTALL_COMMAND  cd <SOURCE_DIR> && nmake -f makefile.vc INSTALLDIR=${CMAKE_INSTALL_PREFIX} ${extraopts} install
-			)
-		SET(CMAKE_EXTERNAL_TARGET_LIST "${CMAKE_EXTERNAL_TARGET_LIST};${projname}" CACHE STRING "external target list" FORCE)
-	ENDIF(${CMAKE_PROJECT_NAME}_BUILD_LOCAL_${upper} AND NOT ${CMAKE_PROJECT_NAME}-ENABLE_SYSTEM_LIBS_ONLY)
-ENDMACRO(THIRD_PARTY_NMAKE_EXTERNAL_PROJECT)
+ENDMACRO(THIRD_PARTY)
 
 
-MACRO(THIRD_PARTY_CONFIGURE_EXTERNAL_PROJECT upper projname projpath srcpath extraopts)
-	IF(${CMAKE_PROJECT_NAME}_BUILD_LOCAL_${upper} AND NOT ${CMAKE_PROJECT_NAME}-ENABLE_SYSTEM_LIBS_ONLY)
-		ExternalProject_Add(
-			${projname}
-			DOWNLOAD_COMMAND ""
-			PREFIX ${${CMAKE_PROJECT_NAME}_BINARY_DIR}
-			SOURCE_DIR ${${CMAKE_PROJECT_NAME}_SOURCE_DIR}/${projpath}/${srcpath}
-			CONFIGURE_COMMAND mkdir -p
-			${${CMAKE_PROJECT_NAME}_BINARY_DIR}/${projpath}/ && cd
-			${${CMAKE_PROJECT_NAME}_BINARY_DIR}/${projpath}/ &&
-			<SOURCE_DIR>/configure --prefix=${${CMAKE_PROJECT_NAME}_PREFIX} --exec-prefix=${${CMAKE_PROJECT_NAME}_PREFIX} --mandir=${MAN_DIR} ${extraopts}
-			BUILD_COMMAND cd ${${CMAKE_PROJECT_NAME}_BINARY_DIR}/${projpath}/ && $(MAKE)
-			INSTALL_COMMAND  cd ${${CMAKE_PROJECT_NAME}_BINARY_DIR}/${projpath}/ && $(MAKE) install
-			)
-		SET(CMAKE_EXTERNAL_TARGET_LIST "${CMAKE_EXTERNAL_TARGET_LIST};${projname}" CACHE STRING "external target list" FORCE)
-	ENDIF(${CMAKE_PROJECT_NAME}_BUILD_LOCAL_${upper} AND NOT ${CMAKE_PROJECT_NAME}-ENABLE_SYSTEM_LIBS_ONLY)
-ENDMACRO(THIRD_PARTY_CONFIGURE_EXTERNAL_PROJECT)
-
-MACRO(THIRD_PARTY_AUTOCONF_EXTERNAL_PROJECT upper projname projpath srcpath extraopts)
-	IF(${CMAKE_PROJECT_NAME}_BUILD_LOCAL_${upper} AND NOT ${CMAKE_PROJECT_NAME}-ENABLE_SYSTEM_LIBS_ONLY)
-		ExternalProject_Add(
-			${projname}
-			DOWNLOAD_COMMAND ""
-			PREFIX ${${CMAKE_PROJECT_NAME}_BINARY_DIR}
-			SOURCE_DIR ${${CMAKE_PROJECT_NAME}_SOURCE_DIR}/${projpath}/${srcpath}
-			CONFIGURE_COMMAND autoconf -I <SOURCE_DIR> -o
-			<SOURCE_DIR>/configure <SOURCE_DIR>/configure.in && mkdir -p
-			${${CMAKE_PROJECT_NAME}_BINARY_DIR}/${projpath}/ && cd
-			${${CMAKE_PROJECT_NAME}_BINARY_DIR}/${projpath}/ &&
-			<SOURCE_DIR>/configure --prefix=${${CMAKE_PROJECT_NAME}_PREFIX} --exec-prefix=${${CMAKE_PROJECT_NAME}_PREFIX} --mandir=${MAN_DIR} ${extraopts}
-			BUILD_COMMAND cd ${${CMAKE_PROJECT_NAME}_BINARY_DIR}/${projpath}/ && $(MAKE)
-			INSTALL_COMMAND  cd ${${CMAKE_PROJECT_NAME}_BINARY_DIR}/${projpath}/ && $(MAKE) install
-			)
-		SET(CMAKE_EXTERNAL_TARGET_LIST "${CMAKE_EXTERNAL_TARGET_LIST};${projname}" CACHE STRING "external target list" FORCE)
-	ENDIF(${CMAKE_PROJECT_NAME}_BUILD_LOCAL_${upper} AND NOT ${CMAKE_PROJECT_NAME}-ENABLE_SYSTEM_LIBS_ONLY)
-ENDMACRO(THIRD_PARTY_AUTOCONF_EXTERNAL_PROJECT)
-
-
-MACRO(THIRD_PARTY_AUTORECONF_EXTERNAL_PROJECT upper projname projpath srcpath extraopts)
-	IF(${CMAKE_PROJECT_NAME}_BUILD_LOCAL_${upper} AND NOT ${CMAKE_PROJECT_NAME}-ENABLE_SYSTEM_LIBS_ONLY)
-		ExternalProject_Add(
-			${projname}
-			DOWNLOAD_COMMAND ""
-			PREFIX ${${CMAKE_PROJECT_NAME}_BINARY_DIR}
-			SOURCE_DIR ${${CMAKE_PROJECT_NAME}_SOURCE_DIR}/${projpath}/${srcpath}
-			CONFIGURE_COMMAND cd <SOURCE_DIR> && autoreconf -i -f && mkdir -p
-			${${CMAKE_PROJECT_NAME}_BINARY_DIR}/${projpath}/ && cd
-			${${CMAKE_PROJECT_NAME}_BINARY_DIR}/${projpath}/ &&
-			<SOURCE_DIR>/configure --prefix=${${CMAKE_PROJECT_NAME}_PREFIX} --exec-prefix=${${CMAKE_PROJECT_NAME}_PREFIX} --mandir=${MAN_DIR} ${extraopts}
-			BUILD_COMMAND cd ${${CMAKE_PROJECT_NAME}_BINARY_DIR}/${projpath}/ && $(MAKE)
-			INSTALL_COMMAND  cd ${${CMAKE_PROJECT_NAME}_BINARY_DIR}/${projpath}/ && $(MAKE) install
-			)
-		SET(CMAKE_EXTERNAL_TARGET_LIST "${CMAKE_EXTERNAL_TARGET_LIST};${projname}" CACHE STRING "external target list" FORCE)
-	ENDIF(${CMAKE_PROJECT_NAME}_BUILD_LOCAL_${upper} AND NOT ${CMAKE_PROJECT_NAME}-ENABLE_SYSTEM_LIBS_ONLY)
-ENDMACRO(THIRD_PARTY_AUTORECONF_EXTERNAL_PROJECT)
-
-
-#-----------------------------------------------------------------------------
-MACRO(THIRD_PARTY_WARNING_SUPPRESS upper lang)
-  IF(NOT ${upper}_WARNINGS_ALLOW)
-    # MSVC uses /w to suppress warnings.  It also complains if another
-    # warning level is given, so remove it.
-    IF(MSVC)
-      SET(${upper}_WARNINGS_BLOCKED 1)
-      STRING(REGEX REPLACE "(^| )([/-])W[0-9]( |$)" " "
-        CMAKE_${lang}_FLAGS "${CMAKE_${lang}_FLAGS}")
-      SET(CMAKE_${lang}_FLAGS "${CMAKE_${lang}_FLAGS} /w")
-    ENDIF(MSVC)
-
-    # Borland uses -w- to suppress warnings.
-    IF(BORLAND)
-      SET(${upper}_WARNINGS_BLOCKED 1)
-      SET(CMAKE_${lang}_FLAGS "${CMAKE_${lang}_FLAGS} -w-")
-    ENDIF(BORLAND)
-
-    # Most compilers use -w to suppress warnings.
-    IF(NOT ${upper}_WARNINGS_BLOCKED)
-      SET(CMAKE_${lang}_FLAGS "${CMAKE_${lang}_FLAGS} -w")
-    ENDIF(NOT ${upper}_WARNINGS_BLOCKED)
-  ENDIF(NOT ${upper}_WARNINGS_ALLOW)
-ENDMACRO(THIRD_PARTY_WARNING_SUPPRESS)
