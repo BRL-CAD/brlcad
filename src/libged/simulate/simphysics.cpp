edbf4dc7e4f9abdb8d131b53250a9c159d7569e0
Abhijit Nandy <abhijit.nandy@gmail.com>
2011-08-23 16:44:37 +0000

    Changes in the CMake build logic and source files to allow the simulate command to link to bullet
    svn:revision:46346
    svn:branch:trunk
    svn:account:abhi2011

--- src/libged/simulate/simphysics.cpp
+++ src/libged/simulate/simphysics.cpp
@@ -20,36 +20,150 @@
 /** @file libged/simphysics.cpp
  *
  *
- * Routines related to performing a single step of physics on passed objects
+ * Routines related to performing physics on the passed shape
  *
  * 
  * 
  */
 
+#include "common.h"
+
+#ifdef HAVE_BULLET
+
 #include <iostream>
 
-#include "common.h"
 #include "db.h"
+#include "vmath.h"
+
+#include <btBulletDynamicsCommon.h>
+
+/**
+ * Prints the 16 by 16 transform matrices for debugging
+ *
+ */
+void print_matrices(struct bu_vls *result_str, mat_t t, btScalar *m)
+{
+	int i, j;
+
+	bu_vls_printf(result_str,"------------Transformation matrices(Debug)--------------\n");
+
+	for (i=0 ; i<4 ; i++) {
+		for (j=0 ; j<4 ; j++) {
+			bu_vls_printf(result_str,"t[%d]: %f\t", (i*4 + j), t[i*4 + j] );
+		}
+		bu_vls_printf(result_str,"\n");
+	}
+
+	bu_vls_printf(result_str,"\n");
+
+	for (i=0 ; i<4 ; i++) {
+		for (j=0 ; j<4 ; j++) {
+			bu_vls_printf(result_str,"m[%d]: %f\t", (j*4 + i), m[j*4 + i] );
+		}
+		bu_vls_printf(result_str,"\n");
+	}
+
+	bu_vls_printf(result_str,"-------------------------------------------------------\n");
+
+}
 
 /**
  * C++ wrapper for doing physics using bullet : Doesn't have any significant physics code yet
  * 
  */
 extern "C" int
-single_step_sim(struct bu_vls *result_str, int argc, const char *argv[])
+run_simulation(struct bu_vls *result_str, mat_t t, int argc, const char *argv[])
 {
-        bu_vls_printf(result_str, "%s: In physics\n", argv[0]);
-        
-        
-        return 0;
+	int i = argc; // to eliminate 'unused' warning
+	btScalar m[16];
+	int num_steps = atoi(argv[1]);
+
+	bu_vls_printf(result_str, "Simulation will run for %d steps.\n",num_steps);
+	bu_vls_printf(result_str, "A 1 kg sphere will be dropped from a height of 50 m.\n");
+	bu_vls_printf(result_str, "Every time step is 1/60th of a second long(can be altered).\n");
+
+	btBroadphaseInterface* broadphase = new btDbvtBroadphase();
+
+	btDefaultCollisionConfiguration* collisionConfiguration = new btDefaultCollisionConfiguration();
+	btCollisionDispatcher* dispatcher = new btCollisionDispatcher(collisionConfiguration);
+
+	btSequentialImpulseConstraintSolver* solver = new btSequentialImpulseConstraintSolver;
+
+	btDiscreteDynamicsWorld* dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher,broadphase,solver,collisionConfiguration);
+
+	dynamicsWorld->setGravity(btVector3(0,-10,0));
+
+
+	btCollisionShape* groundShape = new btStaticPlaneShape(btVector3(0,1,0),1);
+
+	btCollisionShape* fallShape = new btSphereShape(1);
+
+
+	btDefaultMotionState* groundMotionState = new btDefaultMotionState(btTransform(btQuaternion(0,0,0,1),btVector3(0,-1,0)));
+	btRigidBody::btRigidBodyConstructionInfo
+			groundRigidBodyCI(0,groundMotionState,groundShape,btVector3(0,0,0));
+	btRigidBody* groundRigidBody = new btRigidBody(groundRigidBodyCI);
+	dynamicsWorld->addRigidBody(groundRigidBody);
+
+
+	btDefaultMotionState* fallMotionState =
+			new btDefaultMotionState(btTransform(btQuaternion(0,0,0,1),btVector3(0,50,0)));
+	btScalar mass = 1;
+	btVector3 fallInertia(0,0,0);
+	fallShape->calculateLocalInertia(mass,fallInertia);
+	btRigidBody::btRigidBodyConstructionInfo fallRigidBodyCI(mass,fallMotionState,fallShape,fallInertia);
+	btRigidBody* fallRigidBody = new btRigidBody(fallRigidBodyCI);
+	dynamicsWorld->addRigidBody(fallRigidBody);
+
+	bu_vls_printf(result_str, "----- Starting simulation -----\n");
+	btTransform trans;
+	for (i=0 ; i<num_steps ; i++) {
+		//time step of 1/60th of a second(same as internal fixedTimeStep, maxSubSteps=10 to cover 1/60th sec.)
+		dynamicsWorld->stepSimulation(1/60.f,10);
+		fallRigidBody->getMotionState()->getWorldTransform(trans);
+		bu_vls_printf(result_str, "Step %3d : Sphere height: %f\n", i, trans.getOrigin().getY());
+
+	}
+
+	bu_vls_printf(result_str, "----- Simulation Complete -----\n");
+	trans.getOpenGLMatrix(m);
+
+	//Copy the transform matrix : transpose to convert from column major to row major, y to z and vice versa
+	//Use transform matrix to convert openGL Y axis up to BRL-CAD Z axis up, both are right handed:TODO
+	t[0]  = m[0];  t[1]  = m[4];  t[2]  = m[8];  t[3]  = m[12];
+	t[4]  = m[1];  t[5]  = m[5];  t[6]  = m[9];  t[7]  = m[14];
+	t[8]  = m[2];  t[9]  = m[6];  t[10] = m[10]; t[11] = m[13];
+	t[12] = m[3]; t[13]  = m[7];  t[14] = m[11]; t[15] = m[15];
+
+	print_matrices(result_str, t, m);
+
+
+	//Cleanup in order of creation : better ways for cleanup is possible
+	dynamicsWorld->removeRigidBody(fallRigidBody);
+	delete fallRigidBody->getMotionState();
+	delete fallRigidBody;
+
+	dynamicsWorld->removeRigidBody(groundRigidBody);
+	delete groundRigidBody->getMotionState();
+	delete groundRigidBody;
+	delete fallShape;
+	delete groundShape;
+	delete dynamicsWorld;
+	delete solver;
+	delete collisionConfiguration;
+	delete dispatcher;
+	delete broadphase;
+
+	return 0;
 }
 
-/*
- * Local Variables:
- * tab-width: 8
- * mode: C++
- * indent-tabs-mode: t
- * c-file-style: "stroustrup"
- * End:
- * ex: shiftwidth=4 tabstop=8
- */
+#endif
+
+// Local Variables:
+// tab-width: 8
+// mode: C++
+// c-basic-offset: 4
+// indent-tabs-mode: t
+// c-file-style: "stroustrup"
+// End:
+// ex: shiftwidth=4 tabstop=8
--- src/libged/simulate/simulate.c
+++ src/libged/simulate/simulate.c
@@ -23,14 +23,12 @@
  *
  * Routines related to performing physics on passed objects only
  *
- * TODO:
- *   Insert Bullet code in the simphysics.cpp file and link to libs
- *   Add time option, frequency etc
- *
  * 
  */
 
 #include "common.h"
+
+#ifdef HAVE_BULLET
 
 #include <stdlib.h>
 #include <ctype.h>
@@ -41,11 +39,10 @@
 #include "vmath.h"
 #include "db.h"
 #include "raytrace.h"
-#include "./ged_private.h"
+#include "../ged_private.h"
 
 
-extern int single_step_sim(struct bu_vls *result_str, int argc, const char *argv[]);
-
+extern int run_simulation(struct bu_vls *result_str, mat_t t, int argc, const char *argv[]);
 
 /**
  * How to use simulate.Blissfully simple interface, more options will be added soon
@@ -53,11 +50,12 @@ extern int single_step_sim(struct bu_vls *result_str, int argc, const char *argv
 static void
 print_usage(struct bu_vls *str)
 {
-    bu_vls_printf(str, "Usage: simulate [-nft] <object>\n\n");
+    bu_vls_printf(str, "Usage: simulate <steps> <object>\n\n");
+    bu_vls_printf(str, "Currently this command aligns <object> to the final position of a falling sphere\n");
+    bu_vls_printf(str, "after <steps> number of simulation steps.\n");
     bu_vls_printf(str, "-n <n> <x> <y> <z>\t- Specifies number of steps.\n");
-    bu_vls_printf(str, "-f <n> <x> <y> <z>\t- Specifies frequency of update(eg 1/60 Hz)\n");
+    bu_vls_printf(str, "-f <n> <x> <y> <z>\t- Specifies frequency of update(eg 1/60 Hz).\n");
     bu_vls_printf(str, "-t <x> <y> <z>\t\t  - Specifies time for which to run(alternative to -n).\n");
-    bu_vls_printf(str, "-v\t\t\t- Prints version info.\n");
     return;
 }
 
@@ -69,11 +67,10 @@ int
 ged_simulate(struct ged *gedp, int argc, const char *argv[])
 {
     struct directory *ndp;
-    int i, rv;
+    int rv;
     struct rt_db_internal intern;
     static const char *transf_shape_name = "Transformed_Shape";
-    static const char *usage = "object(s)";
-    char *cmd_args[5], result_str[100];
+    char *cmd_args[5];
     mat_t t;
 
     GED_CHECK_DATABASE_OPEN(gedp, GED_ERROR);
@@ -84,87 +81,114 @@ ged_simulate(struct ged *gedp, int argc, const char *argv[])
 
     /* Must be wanting help */
     if (argc == 1) {
-	print_usage(gedp->ged_result_str);
-	return GED_HELP;
+        print_usage(gedp->ged_result_str);
+        return GED_HELP;
     }
 
-    if (argc < 2) {
-	bu_vls_printf(gedp->ged_result_str, "Usage: %s %s", argv[0], usage);
-	return GED_ERROR;
+    if (argc < 3) {
+        bu_vls_printf(gedp->ged_result_str, "Usage: %s <steps> <object>", argv[0]);
+        return GED_ERROR;
     }
     
     /* First duplicate the passed shape, so the original is untouched */
-    /* Check if the duplicate exists, and kill it if so */
+    /* Check if the duplicate already exists, and kill it if so(maybe of a different shape) */
     if (db_lookup(gedp->ged_wdbp->dbip, transf_shape_name, LOOKUP_QUIET) != RT_DIR_NULL) {
-	cmd_args[0] = "sim_kill";
+    	bu_vls_printf(gedp->ged_result_str, "%s: %s already exists, deleting it",  argv[0], transf_shape_name);
+    	cmd_args[0] = "kill";
         cmd_args[1] = (char *)transf_shape_name;
         cmd_args[2] = (char *)0;
+
         if(ged_kill(gedp, 2, (const char **)cmd_args) != GED_OK){
             bu_vls_printf(gedp->ged_result_str, "%s: could not delete existing %s", argv[0], transf_shape_name);
-	    return GED_ERROR;
-	}
-	bu_vls_printf(gedp->ged_result_str, "%s: %s already exists, deleting it",  argv[0], transf_shape_name);	
+            return GED_ERROR;
+        }
     }
    
     /* Copy the shape, clone is required for combinations(full tree copy) : TODO */
-    cmd_args[0] = "sim_copy";
-    cmd_args[1] = (char *)argv[1];             
+    cmd_args[0] = "copy";
+    cmd_args[1] = (char *)argv[2];
     cmd_args[2] = (char *)transf_shape_name;
     cmd_args[3] = (char *)0;
     rv = ged_copy(gedp, 3, (const char **)cmd_args);
-    if (rv != GED_OK)
-	bu_vls_printf(gedp->ged_result_str, "%s: WARNING: Failure cloning \"%s\" to \"%s\"\n", argv[0],
-	argv[1], transf_shape_name);    
-       
-    
-    /* Setup translation matrix of 1m in X for testing */
-    t[0]  = 1; t[1]  = 0; t[2]  = 0; t[3]  = 1000;
-    t[4]  = 0; t[5]  = 1; t[6]  = 0; t[7]  = 0;
-    t[8]  = 0; t[9]  = 0; t[10] = 1; t[11] = 0;
-    t[12] = 0; t[13] = 0; t[14] = 0; t[15] = 1;
-    
-    /* Loop not correct as of now, only 1 primitive can be transformed, but no error as argc=2 */
-    for (i = 1; i < argc; i++) {
-	if ((ndp = db_lookup(gedp->ged_wdbp->dbip,  transf_shape_name, LOOKUP_NOISY)) == RT_DIR_NULL){
-            bu_vls_printf(gedp->ged_result_str, "%s: Error db_lookup(%s) failed ! \n", argv[0], 
-            transf_shape_name);
-            return GED_ERROR;
-        }
-        
-        /* Get the internal representation of the object passed by the user */
-        GED_DB_GET_INTERNAL(gedp, &intern, ndp, bn_mat_identity, &rt_uniresource, GED_ERROR);
-       
-        bu_vls_printf(gedp->ged_result_str, "%s: Moving primitive : %s\n", argv[0], argv[i]);
-       
-        /* This is the cpp function with Bullet code, will be modified later to pass transformation 
-           matrices to and fro */  
-        rv = single_step_sim(gedp->ged_result_str, argc, argv);
-        bu_vls_printf(gedp->ged_result_str, "%s: Single step : \n%s\n", argv[0], result_str);
-          
-        /* Do the matrix transform which will be output from single_step_sim() */
-        if (rt_matrix_transform(&intern, t, &intern, 0, gedp->ged_wdbp->dbip, &rt_uniresource) < 0){ 
-           bu_vls_printf(gedp->ged_result_str, "%s: ERROR rt_matrix_transform failed !\n", argv[0]);
-           return GED_ERROR;
-        }
-        
-        /* Write the modified solid to the db so it can be redrawn at the new position & orientation by Mged */        
-        if (rt_db_put_internal(ndp, gedp->ged_wdbp->dbip, &intern, &rt_uniresource) < 0) {
-            bu_vls_printf(gedp->ged_result_str, "%s: Database write error, aborting\n", argv[0]);
-            return GED_ERROR;
-        }
-        
-        bu_vls_printf(gedp->ged_result_str, "%s: Simulation result is in  primitive : %s\n", 
-                                                                    argv[0], transf_shape_name); 
-                                                     
-        /* Draw the result : inserting it in argv[1] will cause it to be automatically drawn in cmd_wrapper */
- 	argv[1] = (char *)transf_shape_name;
-	argv[2] = (char *)0;
-	
+    if (rv != GED_OK){
+        bu_vls_printf(gedp->ged_result_str, "%s: could not copy \"%s\" to \"%s\"\n", argv[0], argv[1],
+        		transf_shape_name);
+        return GED_ERROR;
     }
-   
+    
+    /* Get the existing transform of the object : TODO */
+    
+    /* The shape which will reflect the new position is now ready. so the simulation can be run.
+     * run_simulation() is the C++ function with Bullet code. It will be modified later to pass
+     * transformation matrices to and fro . Error check to be added below : TODO
+     */
+    rv = run_simulation(gedp->ged_result_str, t, argc, argv);
+
+    /* Lookup the target shape to which the result of run_simulation() will be applied */
+    if ((ndp = db_lookup(gedp->ged_wdbp->dbip,  transf_shape_name, LOOKUP_NOISY)) == RT_DIR_NULL){
+        bu_vls_printf(gedp->ged_result_str, "%s: Error db_lookup(%s) failed ! \n", argv[0],
+        transf_shape_name);
+        return GED_ERROR;
+    }
+        
+	/* Get the internal representation of the same shape */
+	GED_DB_GET_INTERNAL(gedp, &intern, ndp, bn_mat_identity, &rt_uniresource, GED_ERROR);
+
+	/* Drop any existing transformations on it */
+	/* TODO */
+
+	/* Apply the transform output from run_simulation() on Transformed_Shape
+	 * This transform must be treated as an absolute world transform and
+	 * not an increment. Currently rt_matrix_transform() adds to the existing transform
+	 */
+	if (rt_matrix_transform(&intern, t, &intern, 0, gedp->ged_wdbp->dbip, &rt_uniresource) < 0){
+	   bu_vls_printf(gedp->ged_result_str, "%s: ERROR rt_matrix_transform failed !\n", argv[0]);
+	   return GED_ERROR;
+	}
+
+	/* Write the modified solid to the db so it can be redrawn at the new position & orientation by Mged */
+	if (rt_db_put_internal(ndp, gedp->ged_wdbp->dbip, &intern, &rt_uniresource) < 0) {
+		bu_vls_printf(gedp->ged_result_str, "%s: Database write error, aborting\n", argv[0]);
+		return GED_ERROR;
+	}
+
+	bu_vls_printf(gedp->ged_result_str, "%s: The simulation result is in primitive : %s\n",
+																argv[0], transf_shape_name);
+                                                     
+    /* Draw the result : inserting it in argv[1] will cause it to be automatically drawn in the cmd_wrapper */
+    argv[1] = (char *)transf_shape_name;
+    argv[2] = (char *)0;
 
     return GED_OK;
 }
+
+
+#else
+
+#include "../ged_private.h"
+
+/**
+ * Dummy simulation function in case no physics library found
+ */
+int
+ged_simulate(struct ged *gedp, int argc, const char *argv[])
+{
+    GED_CHECK_DATABASE_OPEN(gedp, GED_ERROR);
+    GED_CHECK_ARGC_GT_0(gedp, argc, GED_ERROR);
+
+    /* Initialize result */
+    bu_vls_trunc(gedp->ged_result_str, 0);
+
+    bu_vls_printf(gedp->ged_result_str, "%s : This command is disabled due to absence of a physics library",
+    		argv[0]);
+	return GED_ERROR;
+}
+
+#endif
+
+
+
+
 
 
 /*
