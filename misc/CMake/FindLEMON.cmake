7dfdf0f99b0e4f068691e5aa10e80388d391b942
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2011-08-25 14:51:14 +0000

    tweak so things are quieter on repeat runs of cmake
    svn:revision:46402
    svn:branch:trunk
    svn:account:starseeker

--- misc/CMake/FindLEMON.cmake
+++ misc/CMake/FindLEMON.cmake
@@ -64,17 +64,17 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #=============================================================================
 
-
 FIND_PROGRAM(LEMON_EXECUTABLE lemon DOC "path to the lemon executable")
 MARK_AS_ADVANCED(LEMON_EXECUTABLE)
 IF(LEMON_EXECUTABLE)
 	get_filename_component(lemon_path ${LEMON_EXECUTABLE} PATH)
 	IF(lemon_path)
+		SET(LEMON_TEMPLATE "")
 		IF(EXISTS ${lemon_path}/lempar.c)
 			SET(LEMON_TEMPLATE "${lemon_path}/lempar.c")
 		ENDIF(EXISTS ${lemon_path}/lempar.c)
-	ELSE(lemon_path)
-		SET(LEMON_TEMPLATE "")
+		INCLUDE(FindPackageHandleStandardArgs)
+		FIND_PACKAGE_HANDLE_STANDARD_ARGS(LEMON DEFAULT_MSG LEMON_EXECUTABLE LEMON_TEMPLATE)
 	ENDIF(lemon_path)
 ENDIF(LEMON_EXECUTABLE)
 MARK_AS_ADVANCED(LEMON_TEMPLATE)
@@ -126,8 +126,4 @@ MACRO(LEMON_TARGET Name LemonInput LemonOutput)
 ENDMACRO(LEMON_TARGET)
 #
 #============================================================
-
-INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(LEMON DEFAULT_MSG LEMON_EXECUTABLE LEMON_TEMPLATE)
-
 # FindLEMON.cmake ends here
