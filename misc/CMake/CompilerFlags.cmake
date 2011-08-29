e404b0132dc0218e3f9a99139271d3ed03976388
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2011-08-29 18:56:32 +0000

    Make _FORTIFY_SOURCE a config.h define instead of a compile flag check.
    svn:revision:46456
    svn:branch:trunk
    svn:account:starseeker

--- misc/CMake/CompilerFlags.cmake
+++ misc/CMake/CompilerFlags.cmake
@@ -213,15 +213,6 @@ IF(${BRLCAD_DEBUG_BUILD} STREQUAL "ON")
 		CHECK_C_FLAG_GATHER(ggdb3 DEBUG_FLAG)
 	ENDIF(APPLE)
 	CHECK_C_FLAG(debug DEBUG_FLAG)
-	# add -D_FORTIFY_SOURCE=2 to flags. provides compile-time
-	# best-practice error checking on certain libc functions
-	# (e.g., memcpy), and provides run-time checks on buffer
-	# lengths and memory regions.  Don't do this test for MSVC
-	# however - it "passes" and then doesn't know what to do
-	# with it.
-	IF(NOT MSVC)
-		CHECK_C_FLAG_GATHER("D_FORTIFY_SOURCE=2" DEBUG_FLAG)
-	ENDIF(NOT MSVC)
 	ADD_NEW_FLAG(C DEBUG_FLAG)
 	ADD_NEW_FLAG(CXX DEBUG_FLAG)
 	ADD_NEW_FLAG(SHARED_LINKER DEBUG_FLAG)
