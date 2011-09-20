7ab4a212a1bcb18cab399d870e9e6e65ffd33cda
Thomas Browder <tom.browder@gmail.com>
2011-09-20 23:03:12 +0000

    added path for oNVDL validation method
    svn:revision:46805
    svn:branch:trunk
    svn:account:tbrowder2

--- doc/docbook/BRLCAD_DB_VALIDATION.pm
+++ doc/docbook/BRLCAD_DB_VALIDATION.pm
@@ -1,10 +1,13 @@
 package BRLCAD_DB_VALIDATION;
 
-# path to xmllint
+# path to xmllint executable
 our $XMLLINT = '/usr/bin/xmllint';
 
-# path to Multi-Schema Validator (MSV) file
+# path to Multi-Schema Validator (MSV) jar file
 our $MSVJAR = '/opt/oracle/msv-20090415/msv.jar';
 
 # path to Java executable
-our $JAVA = "/opt/sun/jre1.6.0_21/bin/java";
+our $JAVA = '/opt/sun/jre1.6.0_21/bin/java';
+
+# path to oNVDL jar file
+our $ONVDLJAR = 'opt/oNVDL/bin/onvdl.jar';
