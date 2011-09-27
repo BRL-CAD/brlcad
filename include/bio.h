78799637231f7a6a67163387d81b37123e44b70d
Christopher Sean Morrison <brlcad@gmail.com>
2011-09-27 03:41:33 +0000

    don't repeat yourself.  stash the IGNORE macro definition and restore it later since windows api defines their own
    svn:revision:46922
    svn:branch:trunk
    svn:account:brlcad

--- include/bio.h
+++ include/bio.h
@@ -42,19 +42,21 @@
 
 #if defined(_WIN32) && !defined(__CYGWIN__)
 #  define NOMINMAX
+#  define _BIO_IGNORE(x) IGNORE(x)
 #  undef IGNORE
 #  include <windows.h>
 #  include <io.h>
 
-#   undef rad1 /* Win32 radio button 1 */
-#   undef rad2 /* Win32 radio button 2 */
-#   undef small /* defined as part of the Microsoft Interface Definition Language (MIDL) */
-#   undef IN
-#   undef OUT
+#  undef rad1 /* Win32 radio button 1 */
+#  undef rad2 /* Win32 radio button 2 */
+#  undef small /* defined as part of the Microsoft Interface Definition Language (MIDL) */
+#  undef IN
+#  undef OUT
 /* In case windows.h squashed our ignore, reinstate it - see common.h */
-#   undef IGNORE
-#   define IGNORE(parameter) (void)(parameter)
-
+#  ifdef IGNORE
+#    undef IGNORE
+#  endif
+#  define IGNORE(x) _BIO_IGNORE(x)
 #else
 #  include <unistd.h>
 #endif
