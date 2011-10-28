42678b1d96db1e0db2d29e0d5073e08e0abdac29
Christopher Sean Morrison <brlcad@gmail.com>
2011-10-28 08:49:23 +0000

    changes from trunk slipped through merging somehow.  sync.
    svn:revision:47344
    svn:branch:STABLE
    svn:account:brlcad

--- src/libged/erase.c
+++ src/libged/erase.c
@@ -193,10 +193,12 @@ ged_splitGDL(struct ged *gedp,
     struct ged_display_list *new_gdlp;
     char *pathname;
     int savelen;
+    int newlen = path->fp_len + 1;
 
-    if (path->fp_len < 3) {
+    if (newlen < 3) {
 	while (BU_LIST_WHILE(sp, solid, &gdlp->gdl_headSolid)) {
 	    savelen = sp->s_fullpath.fp_len;
+	    sp->s_fullpath.fp_len = newlen;
 	    pathname = db_path_to_string(&sp->s_fullpath);
 	    sp->s_fullpath.fp_len = savelen;
 
@@ -213,6 +215,7 @@ ged_splitGDL(struct ged *gedp,
 
 	    if (db_full_path_match_top(path, &sp->s_fullpath)) {
 		savelen = sp->s_fullpath.fp_len;
+		sp->s_fullpath.fp_len = newlen;
 		pathname = db_path_to_string(&sp->s_fullpath);
 		sp->s_fullpath.fp_len = savelen;
 
