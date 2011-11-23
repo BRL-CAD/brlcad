2368883a5c9070cfd56e5fb79885697fd7d4baee
Christopher Sean Morrison <brlcad@gmail.com>
2011-11-23 01:53:41 +0000

    starseeker right on both counts, need the empty BU_EXPORT so preprocessor makes the symbol go away and the error case needs to be tested first
    svn:revision:47607
    svn:branch:trunk
    svn:account:brlcad

--- include/bu.h
+++ include/bu.h
@@ -83,13 +83,14 @@ __BEGIN_DECLS
 #if 0
 
 #ifndef BU_EXPORT
-#  if defined(BU_DLL_EXPORTS)
+#  if defined(BU_DLL_EXPORTS) && defined(BU_DLL_IMPORTS)
+#    error "Only BU_DLL_EXPORTS or BU_DLL_IMPORTS can be defined, not both."
+#  elif defined(BU_DLL_EXPORTS)
 #    define BU_EXPORT __declspec(dllexport)
 #  elif defined(BU_DLL_IMPORTS)
 #    define BU_EXPORT __declspec(dllimport)
-#  elif defined(BU_DLL_EXPORTS) && defined(BU_DLL_IMPORTS)
-#    error "Only BU_DLL_EXPORTS or BU_DLL_IMPORTS can be defined, not both."
-#  endif
+#  else
+#    define BU_EXPORT
 #endif
 
 #endif
