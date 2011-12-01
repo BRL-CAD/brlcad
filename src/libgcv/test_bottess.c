5eef340622300d3af682e547e1a761da28c1a811
Erik Greenwald <erik@brlcad.org>
2011-12-01 14:00:33 +0000

    stub in unit testing for bottess
    svn:revision:47719
    svn:branch:trunk
    svn:account:erikgreenwald

---
+++ src/libgcv/test_bottess.c
@@ -0,0 +1,47 @@
+/*                B O T T E S S _ T E S T E R . C
+ * BRL-CAD
+ *
+ * Copyright (c) 2008-2011 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+
+/** @file libgcv/bottess_tester.c
+ *
+ * Test things in the bottess routines...
+ */
+
+#include "common.h"
+
+#include "bn.h"
+#include "raytrace.h"
+#include "nmg.h"
+#include "gcv.h"
+
+int
+main(void)
+{
+    return 0;
+}
+
+/*
+ * Local Variables:
+ * tab-width: 8
+ * mode: C
+ * indent-tabs-mode: t
+ * c-file-style: "stroustrup"
+ * End:
+ * ex: shiftwidth=4 tabstop=8
+ */
