a545669aa7723f9fbd3898be19e1140622164e1c
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2011-12-15 18:53:23 +0000

    Make a stab at macros for perplex targets
    svn:revision:47992
    svn:branch:trunk
    svn:account:starseeker

--- misc/CMake/FindPERPLEX.cmake
+++ misc/CMake/FindPERPLEX.cmake
@@ -4,6 +4,30 @@
 #
 #  PERPLEX_EXECUTABLE - path to the perplex program
 #
+# Also defines two macros - PERPLEX_TARGET, which takes perplex inputs and
+# runs both perplex and re2c to generate C source code/headers, and 
+# ADD_PERPLEX_LEMON_DEPENDENCY which is used to set up dependencies between
+# scanner and parser targets when necessary.
+#
+# #====================================================================
+#  Example:
+#
+#   find_package(LEMON)
+#   find_package(RE2C)
+#   find_package(PERPLEX)
+#
+#   LEMON_TARGET(MyParser parser.y ${CMAKE_CURRENT_BINARY_DIR}/parser.cpp
+#   PERPLEX_TARGET(MyScanner scanner.re  ${CMAKE_CURRENT_BIANRY_DIR}/scanner.cpp ${CMAKE_CURRENT_BINARY_DIR}/scanner_header.hpp)
+#   ADD_PERPLEX_LEMON_DEPENDENCY(MyScanner MyParser)
+#
+#   include_directories(${CMAKE_CURRENT_BINARY_DIR})
+#   add_executable(Foo
+#      Foo.cc
+#      ${LEMON_MyParser_OUTPUTS}
+#      ${PERPLEX_MyScanner_OUTPUTS}
+#   )
+#  ====================================================================
+# 
 #=============================================================================
 #               F I N D P E R P L E X . C M A K E
 #
@@ -57,6 +81,53 @@ IF(PERPLEX_EXECUTABLE)
 		FIND_PACKAGE_HANDLE_STANDARD_ARGS(PERPLEX DEFAULT_MSG PERPLEX_EXECUTABLE PERPLEX_TEMPLATE)
 	ENDIF(perplex_path)
 ENDIF(PERPLEX_EXECUTABLE)
+
+#============================================================
+# PERPLEX_TARGET (public macro)
+#============================================================
+MACRO(PERPLEX_TARGET Name Input OutputSrc OutputHeader)
+    SET(re2c_src "${CMAKE_CURRENT_BINARY_DIR}/${OutputSrc}_re2c")
+    IF(${ARGC} GREATER 4)
+	SET(Template ${ARGV4})
+    ELSE(${ARGC} GREATER 4)
+	SET(Template ${BRLCAD_SOURCE_DIR}/src/other/perplex/scanner_template.c)
+    ENDIF(${ARGC} GREATER 4)
+    ADD_CUSTOM_COMMAND(
+	OUTPUT ${re2c_src} ${OutputHeader}
+	COMMAND ${PERPLEX_EXECUTABLE} -o ${re2c_src} -h ${OutputHeader} -t ${Template}
+	DEPENDS ${Input} ${PERPLEX_EXECUTABLE_TARGET} ${RE2C_EXECUTABLE_TARGET}
+	COMMENT "[PERPLEX][${Name}] Generating re2c input with ${PERPLEX_EXECUTABLE}"
+	)
+    ADD_CUSTOM_COMMAND(
+	OUTPUT ${OutputSrc}
+	COMMAND ${RE2C_EXECUTABLE} -o${OutputSrc} ${Input}
+	DEPENDS ${Input} ${re2c_src} ${OutputHeader} ${PERPLEX_EXECUTABLE_TARGET} ${RE2C_EXECUTABLE_TARGET}
+	COMMENT "[RE2C][${Name}] Building scanner with ${RE2C_EXECUTABLE}"
+	WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+	)
+    SET(PERPLEX_${Name}_DEFINED TRUE)
+    SET(PERPLEX_${Name}_OUTPUTS ${OutputSrc})
+    SET(PERPLEX_${Name}_INPUT ${Input})
+ENDMACRO(PERPLEX_TARGET)
+
+#============================================================
+# ADD_PERPLEX_LEMON_DEPENDENCY (public macro)
+#============================================================
+MACRO(ADD_PERPLEX_LEMON_DEPENDENCY PERPLEXTarget LemonTarget)
+
+    IF(NOT PERPLEX_${PERPLEXTarget}_OUTPUTS)
+	MESSAGE(SEND_ERROR "PERPLEX target `${PERPLEXTarget}' does not exists.")
+    ENDIF()
+
+    IF(NOT LEMON_${LemonTarget}_OUTPUT_HEADER)
+	MESSAGE(SEND_ERROR "Lemon target `${LemonTarget}' does not exists.")
+    ENDIF()
+
+    SET_SOURCE_FILES_PROPERTIES(${PERPLEX_${PERPLEXTarget}_OUTPUTS}
+	PROPERTIES OBJECT_DEPENDS ${LEMON_${LemonTarget}_OUTPUT_HEADER})
+ENDMACRO(ADD_PERPLEX_LEMON_DEPENDENCY)
+
+
 MARK_AS_ADVANCED(PERPLEX_TEMPLATE)
 
 #============================================================
