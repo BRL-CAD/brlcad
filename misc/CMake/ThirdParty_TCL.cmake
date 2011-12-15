b6d7b6a442e9d033e2bd4e87487df124f94f79b0
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2011-12-15 04:58:28 +0000

    Have the THIRD_PARTY_TCL_PACKAGE macro handle the question of what to do when Tk is required and disabled - we're going to want the option defined regardless so we get the documentation, once we turn those features on.  Still need to rework the itcl/itk logic so it doesn't have to double-call the macro.
    svn:revision:47980
    svn:branch:trunk
    svn:account:starseeker

--- misc/CMake/ThirdParty_TCL.cmake
+++ misc/CMake/ThirdParty_TCL.cmake
@@ -36,33 +36,47 @@
 
 #-----------------------------------------------------------------------------
 MACRO(THIRD_PARTY_TCL_PACKAGE packagename dir wishcmd depends)
-	STRING(TOUPPER ${packagename} PKGNAME_UPPER)
+    STRING(TOUPPER ${packagename} PKGNAME_UPPER)
+    SET(ENABLE_PKG ${${CMAKE_PROJECT_NAME}_TCL_BUILD})
+    SET(NEEDS_TK 0) 
+    FOREACH(item ${depends})
+	IF(${item} STREQUAL "tk")
+	    SET(NEEDS_TK 1)
+	ENDIF(${item} STREQUAL "tk")
+    ENDFOREACH(item ${depends})
+    # If we need Tk and it's not available, this extension is a no-go
+    IF(NEEDS_TK AND NOT BRLCAD_ENABLE_TK)
+	SET(ENABLE_PKG OFF)
+	SET(DISABLE_TEST 1)
+	SET(${CMAKE_PROJECT_NAME}_${PKGNAME_UPPER}_BUILD OFF)
+    ENDIF(NEEDS_TK AND NOT BRLCAD_ENABLE_TK)
 
-	# If we are doing a local Tcl build, default to bundled.
-	BUNDLE_OPTION(${CMAKE_PROJECT_NAME}_TCL ${CMAKE_PROJECT_NAME}_TCL_BUILD ${CMAKE_PROJECT_NAME}_${PKGNAME_UPPER} "") 
+    # If we are doing a local Tcl build, default to bundled unless we need Tk and Tk isn't enabled.
+    BUNDLE_OPTION(${CMAKE_PROJECT_NAME}_TCL ENABLE_PKG ${CMAKE_PROJECT_NAME}_${PKGNAME_UPPER} "") 
 
+    IF(NOT DISABLE_TEST)
 	IF(${CMAKE_PROJECT_NAME}_${PKGNAME_UPPER} MATCHES "BUNDLED")
-		SET(${CMAKE_PROJECT_NAME}_${PKGNAME_UPPER}_BUILD ON)
+	    SET(${CMAKE_PROJECT_NAME}_${PKGNAME_UPPER}_BUILD ON)
 	ELSE(${CMAKE_PROJECT_NAME}_${PKGNAME_UPPER} MATCHES "BUNDLED")
-		SET(${CMAKE_PROJECT_NAME}_${PKGNAME_UPPER}_BUILD OFF)
+	    SET(${CMAKE_PROJECT_NAME}_${PKGNAME_UPPER}_BUILD OFF)
 
-		# Stash the previous results (if any) so we don't repeatedly call out the tests - only report
-		# if something actually changes in subsequent runs.
-		SET(${PKGNAME_UPPER}_FOUND_STATUS ${${PKGNAME_UPPER}_FOUND})
+	    # Stash the previous results (if any) so we don't repeatedly call out the tests - only report
+	    # if something actually changes in subsequent runs.
+	    SET(${PKGNAME_UPPER}_FOUND_STATUS ${${PKGNAME_UPPER}_FOUND})
 
-		IF(${wishcmd} STREQUAL "")
-			SET(${PKGNAME_UPPER}_FOUND "${PKGNAME_UPPER}-NOTFOUND" CACHE STRING "${PKGNAME_UPPER}_FOUND" FORCE)
+	    IF(${wishcmd} STREQUAL "")
+		SET(${PKGNAME_UPPER}_FOUND "${PKGNAME_UPPER}-NOTFOUND" CACHE STRING "${PKGNAME_UPPER}_FOUND" FORCE)
 
-			IF(${CMAKE_PROJECT_NAME}_${PKGNAME_UPPER} MATCHES "SYSTEM")
-				IF("${${PKGNAME_UPPER}_FOUND_STATUS}" MATCHES "NOTFOUND" AND NOT ${PKGNAME_UPPER}_FOUND)
-					MESSAGE(WARNING "No tclsh/wish command available for testing, but system version of ${packagename} is requested - assuming availability of package.")
-				ENDIF("${${PKGNAME_UPPER}_FOUND_STATUS}" MATCHES "NOTFOUND" AND NOT ${PKGNAME_UPPER}_FOUND)
-			ELSE(${CMAKE_PROJECT_NAME}_${PKGNAME_UPPER} MATCHES "SYSTEM")
-				# Can't test and we're not forced to system by either local settings or the toplevel - turn it on 
-				SET(${CMAKE_PROJECT_NAME}_${PKGNAME_UPPER}_BUILD ON)
-			ENDIF(${CMAKE_PROJECT_NAME}_${PKGNAME_UPPER} MATCHES "SYSTEM")
-		ELSE(${wishcmd} STREQUAL "")
-			SET(packagefind_script "
+		IF(${CMAKE_PROJECT_NAME}_${PKGNAME_UPPER} MATCHES "SYSTEM")
+		    IF("${${PKGNAME_UPPER}_FOUND_STATUS}" MATCHES "NOTFOUND" AND NOT ${PKGNAME_UPPER}_FOUND)
+			MESSAGE(WARNING "No tclsh/wish command available for testing, but system version of ${packagename} is requested - assuming availability of package.")
+		    ENDIF("${${PKGNAME_UPPER}_FOUND_STATUS}" MATCHES "NOTFOUND" AND NOT ${PKGNAME_UPPER}_FOUND)
+		ELSE(${CMAKE_PROJECT_NAME}_${PKGNAME_UPPER} MATCHES "SYSTEM")
+		    # Can't test and we're not forced to system by either local settings or the toplevel - turn it on 
+		    SET(${CMAKE_PROJECT_NAME}_${PKGNAME_UPPER}_BUILD ON)
+		ENDIF(${CMAKE_PROJECT_NAME}_${PKGNAME_UPPER} MATCHES "SYSTEM")
+	    ELSE(${wishcmd} STREQUAL "")
+		SET(packagefind_script "
 catch {package require ${packagename}}
 set packageversion NOTFOUND
 set packageversion [lindex [lsort -decreasing [package versions ${packagename}]] 0]
@@ -72,47 +86,48 @@ puts $fileId $packageversion
 close $fileId
 exit
 ")
-			SET(packagefind_scriptfile "${CMAKE_BINARY_DIR}/CMakeTmp/${packagename}_packageversion.tcl")
-			FILE(WRITE ${packagefind_scriptfile} ${packagefind_script})
-			EXEC_PROGRAM(${wishcmd} ARGS ${packagefind_scriptfile} OUTPUT_VARIABLE EXECOUTPUT)
-			FILE(READ ${CMAKE_BINARY_DIR}/CMakeTmp/${PKGNAME_UPPER}_PKG_VERSION pkgversion)
-			STRING(REGEX REPLACE "\n" "" ${PKGNAME_UPPER}_PACKAGE_VERSION ${pkgversion})
+		SET(packagefind_scriptfile "${CMAKE_BINARY_DIR}/CMakeTmp/${packagename}_packageversion.tcl")
+		FILE(WRITE ${packagefind_scriptfile} ${packagefind_script})
+		EXEC_PROGRAM(${wishcmd} ARGS ${packagefind_scriptfile} OUTPUT_VARIABLE EXECOUTPUT)
+		FILE(READ ${CMAKE_BINARY_DIR}/CMakeTmp/${PKGNAME_UPPER}_PKG_VERSION pkgversion)
+		STRING(REGEX REPLACE "\n" "" ${PKGNAME_UPPER}_PACKAGE_VERSION ${pkgversion})
 
-			IF(${PKGNAME_UPPER}_PACKAGE_VERSION)
-				SET(${CMAKE_PROJECT_NAME}_${PKGNAME_UPPER}_BUILD OFF)
-				SET(${PKGNAME_UPPER}_FOUND "TRUE" CACHE STRING "${PKGNAME_UPPER}_FOUND" FORCE)
-			ELSE(${PKGNAME_UPPER}_PACKAGE_VERSION)
-				SET(${CMAKE_PROJECT_NAME}_${PKGNAME_UPPER}_BUILD ON)
-				SET(${PKGNAME_UPPER}_FOUND "${PKGNAME_UPPER}-NOTFOUND" CACHE STRING "${PKGNAME_UPPER}_FOUND" FORCE)
-			ENDIF(${PKGNAME_UPPER}_PACKAGE_VERSION)
+		IF(${PKGNAME_UPPER}_PACKAGE_VERSION)
+		    SET(${CMAKE_PROJECT_NAME}_${PKGNAME_UPPER}_BUILD OFF)
+		    SET(${PKGNAME_UPPER}_FOUND "TRUE" CACHE STRING "${PKGNAME_UPPER}_FOUND" FORCE)
+		ELSE(${PKGNAME_UPPER}_PACKAGE_VERSION)
+		    SET(${CMAKE_PROJECT_NAME}_${PKGNAME_UPPER}_BUILD ON)
+		    SET(${PKGNAME_UPPER}_FOUND "${PKGNAME_UPPER}-NOTFOUND" CACHE STRING "${PKGNAME_UPPER}_FOUND" FORCE)
+		ENDIF(${PKGNAME_UPPER}_PACKAGE_VERSION)
 
-			# Be quiet if we're doing this over
-			IF("${${PKGNAME_UPPER}_FOUND_STATUS}" MATCHES "${PKGNAME_UPPER}-NOTFOUND" AND NOT ${PKGNAME_UPPER}_FOUND)
-				SET(${PKGNAME_UPPER}_FIND_QUIETLY TRUE)
-			ENDIF("${${PKGNAME_UPPER}_FOUND_STATUS}" MATCHES "${PKGNAME_UPPER}-NOTFOUND" AND NOT ${PKGNAME_UPPER}_FOUND)
+		# Be quiet if we're doing this over
+		IF("${${PKGNAME_UPPER}_FOUND_STATUS}" MATCHES "${PKGNAME_UPPER}-NOTFOUND" AND NOT ${PKGNAME_UPPER}_FOUND)
+		    SET(${PKGNAME_UPPER}_FIND_QUIETLY TRUE)
+		ENDIF("${${PKGNAME_UPPER}_FOUND_STATUS}" MATCHES "${PKGNAME_UPPER}-NOTFOUND" AND NOT ${PKGNAME_UPPER}_FOUND)
 
-			FIND_PACKAGE_HANDLE_STANDARD_ARGS(${PKGNAME_UPPER} DEFAULT_MSG ${PKGNAME_UPPER}_PACKAGE_VERSION)
-		ENDIF(${wishcmd} STREQUAL "")
+		FIND_PACKAGE_HANDLE_STANDARD_ARGS(${PKGNAME_UPPER} DEFAULT_MSG ${PKGNAME_UPPER}_PACKAGE_VERSION)
+	    ENDIF(${wishcmd} STREQUAL "")
 	ENDIF(${CMAKE_PROJECT_NAME}_${PKGNAME_UPPER} MATCHES "BUNDLED")
+    ENDIF(NOT DISABLE_TEST)
 
-	IF(${CMAKE_PROJECT_NAME}_${PKGNAME_UPPER}_BUILD)
-		STRING(TOLOWER ${packagename} PKGNAME_LOWER)
-		add_subdirectory(${dir})
+    IF(${CMAKE_PROJECT_NAME}_${PKGNAME_UPPER}_BUILD)
+	STRING(TOLOWER ${packagename} PKGNAME_LOWER)
+	add_subdirectory(${dir})
 
-		FOREACH(dep ${depends})
-			string(TOUPPER ${dep} DEP_UPPER)
-			if(BRLCAD_BUILD_${DEP_UPPER})
-				add_dependencies(${packagename} ${dep})
-			endif(BRLCAD_BUILD_${DEP_UPPER})
-		ENDFOREACH(dep ${depends})
+	FOREACH(dep ${depends})
+	    string(TOUPPER ${dep} DEP_UPPER)
+	    if(BRLCAD_BUILD_${DEP_UPPER})
+		add_dependencies(${packagename} ${dep})
+	    endif(BRLCAD_BUILD_${DEP_UPPER})
+	ENDFOREACH(dep ${depends})
 
-		INCLUDE(${CMAKE_CURRENT_SOURCE_DIR}/${PKGNAME_LOWER}.dist)
-		DISTCHECK_IGNORE(${dir} ${PKGNAME_LOWER}_ignore_files)
-	ELSE(${CMAKE_PROJECT_NAME}_${PKGNAME_UPPER}_BUILD)
-		DISTCHECK_IGNORE_ITEM(${dir})
-	ENDIF(${CMAKE_PROJECT_NAME}_${PKGNAME_UPPER}_BUILD)
+	INCLUDE(${CMAKE_CURRENT_SOURCE_DIR}/${PKGNAME_LOWER}.dist)
+	DISTCHECK_IGNORE(${dir} ${PKGNAME_LOWER}_ignore_files)
+    ELSE(${CMAKE_PROJECT_NAME}_${PKGNAME_UPPER}_BUILD)
+	DISTCHECK_IGNORE_ITEM(${dir})
+    ENDIF(${CMAKE_PROJECT_NAME}_${PKGNAME_UPPER}_BUILD)
 
-	MARK_AS_ADVANCED(${PKGNAME_UPPER}_FOUND)
+    MARK_AS_ADVANCED(${PKGNAME_UPPER}_FOUND)
 ENDMACRO(THIRD_PARTY_TCL_PACKAGE)
 
 # Local Variables:
