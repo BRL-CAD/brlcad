b2c32daa911d0b0b94c02410f0d709103e7d7376
Christopher Sean Morrison <brlcad@gmail.com>
2011-12-20 21:32:28 +0000

    mark a few more of the expressions that result in logging as UNLIKELY
    svn:revision:48083
    svn:branch:trunk
    svn:account:brlcad

--- src/libbu/str.c
+++ src/libbu/str.c
@@ -39,7 +39,7 @@ bu_strlcatm(char *dst, const char *src, size_t size, const char *label)
 	fprintf(stderr, "WARNING: NULL destination string, size %lu [%s]\n", (unsigned long)size, label);
 	bu_semaphore_release(BU_SEM_SYSCALL);
     }
-    if (!dst || !src || size <= 0) {
+    if (UNLIKELY(!dst || !src || size <= 0)) {
 	return 0;
     }
     if (!label) {
@@ -49,16 +49,16 @@ bu_strlcatm(char *dst, const char *src, size_t size, const char *label)
     dstsize = strlen(dst);
     srcsize = strlen(src);
 
-    if (dstsize == size - 1) {
+    if (UNLIKELY(dstsize == size - 1)) {
 	bu_semaphore_acquire(BU_SEM_SYSCALL);
 	fprintf(stderr, "WARNING: [%s] concatenation string is already full at %lu chars\n", label, (unsigned long)size-1);
 	bu_semaphore_release(BU_SEM_SYSCALL);
-    } else if (dstsize > size - 1) {
+    } else if (UNLIKELY(dstsize > size - 1)) {
 	/* probably missing null-termination or is not an initialized buffer */
 	bu_semaphore_acquire(BU_SEM_SYSCALL);
 	fprintf(stderr, "WARNING: [%s] concatenation string is already full, exceeds size (%lu > %lu)\n", label, (unsigned long)dstsize, (unsigned long)size-1);
 	bu_semaphore_release(BU_SEM_SYSCALL);
-    } else if (srcsize > size - dstsize - 1) {
+    } else if (UNLIKELY(srcsize > size - dstsize - 1)) {
 	if (UNLIKELY(bu_debug)) {
 	    bu_semaphore_acquire(BU_SEM_SYSCALL);
 	    fprintf(stderr, "WARNING: [%s] string truncation, exceeding %lu char max concatenating %lu chars (started with %lu)\n", label, (unsigned long)size-1, (unsigned long)srcsize, (unsigned long)dstsize);
@@ -90,12 +90,12 @@ bu_strlcpym(char *dst, const char *src, size_t size, const char *label)
     size_t srcsize;
 
 
-    if (!dst && label) {
+    if (UNLIKELY(!dst && label)) {
 	bu_semaphore_acquire(BU_SEM_SYSCALL);
 	fprintf(stderr, "WARNING: NULL destination string, size %lu [%s]\n", (unsigned long)size, label);
 	bu_semaphore_release(BU_SEM_SYSCALL);
     }
-    if (!dst || !src || size <= 0) {
+    if (UNLIKELY(!dst || !src || size <= 0)) {
 	return 0;
     }
     if (!label) {
@@ -136,7 +136,7 @@ bu_strdupm(register const char *cp, const char *label)
     char *base;
     size_t len;
 
-    if (!cp && label) {
+    if (UNLIKELY(!cp && label)) {
 	bu_semaphore_acquire(BU_SEM_SYSCALL);
 	fprintf(stderr, "WARNING: [%s] NULL copy buffer\n", label);
 	bu_semaphore_release(BU_SEM_SYSCALL);
@@ -165,7 +165,7 @@ bu_strcmpm(const char *string1, const char *string2, const char *label)
     const char *s1 = "";
     const char *s2 = "";
 
-    if ((!string1 || !string2) && label) {
+    if (UNLIKELY((!string1 || !string2) && label)) {
 	bu_semaphore_acquire(BU_SEM_SYSCALL);
 	fprintf(stderr, "WARNING: [%s] string comparison with NULL\n", label);
 	bu_semaphore_release(BU_SEM_SYSCALL);
