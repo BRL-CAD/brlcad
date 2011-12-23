ea84575ad001f35ebe884ab09a65488a4863ced9
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2011-12-23 04:07:44 +0000

    Docbook.cmake is logic (one of the reasons for breaking it out here) so put the header on it.
    svn:revision:48160
    svn:branch:trunk
    svn:account:starseeker

--- misc/CMake/Docbook.cmake
+++ misc/CMake/Docbook.cmake
@@ -1,3 +1,39 @@
+#                   D O C B O O K . C M A K E
+# BRL-CAD
+#
+# Copyright (c) 2011 United States Government as represented by
+# the U.S. Army Research Laboratory.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above
+# copyright notice, this list of conditions and the following
+# disclaimer in the documentation and/or other materials provided
+# with the distribution.
+#
+# 3. The name of the author may not be used to endorse or promote
+# products derived from this software without specific prior written
+# permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS
+# OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+# GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+###
+#
 # In principle, DocBook conversion and validation can be accomplished
 # with multiple programs.  BRL-CAD's CMake logic uses variables to
 # hold the "active" tools for each conversion operation, but will
@@ -14,6 +50,7 @@
 # cmake .. -DBRLCAD_EXTRADOCS_VALIDATE=ON -DVALIDATE_EXECUTABLE=rnv
 #
 # Note that rnv must be in the system path for this to work.
+
 
 # Handle default exec and sanity checking for XML validation
 IF(NOT DEFINED VALIDATE_EXECUTABLE)
