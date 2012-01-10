50043d0d1681475b9dbedbf588066ed3f808c79c
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2012-01-10 15:40:55 +0000

    Be a little better about freeing memory when returning TCL_ERROR
    svn:revision:48288
    svn:branch:trunk
    svn:account:starseeker

--- src/mged/dm-X.c
+++ src/mged/dm-X.c
@@ -109,14 +109,18 @@ X_dm(int argc, const char *argv[])
 	    bu_vls_struct_item_named(&vls, X_vparse, argv[1], (const char *)dmp, COMMA);
 	} else {
 	    struct bu_vls tmp_vls;
+	    int ret;
 
 	    bu_vls_init(&tmp_vls);
 	    bu_vls_printf(&tmp_vls, "%s=\"", argv[1]);
 	    bu_vls_from_argv(&tmp_vls, argc-2, (const char **)argv+2);
 	    bu_vls_putc(&tmp_vls, '\"');
-	    if (bu_struct_parse(&tmp_vls, X_vparse, (char *)dmp) < 0) 
-	      return TCL_ERROR;
+	    ret = bu_struct_parse(&tmp_vls, X_vparse, (char *)dmp);
 	    bu_vls_free(&tmp_vls);
+	    if (ret < 0) { 
+	      bu_vls_free(&vls);
+	      return TCL_ERROR;
+	    }
 	}
 
 	Tcl_AppendResult(INTERP, bu_vls_addr(&vls), (char *)NULL);
