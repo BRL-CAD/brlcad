36273d0825e599e86d1fe960917c47a383fffdb2
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2012-01-12 13:46:59 +0000

    Checking is pointless - eptr is always null in this case - CID 2063
    svn:revision:48483
    svn:branch:trunk
    svn:account:starseeker

--- src/libged/wdb_bigE.c
+++ src/libged/wdb_bigE.c
@@ -265,8 +265,6 @@ wdb_build_etree(union tree *tp,
 	    break;
 	case OP_DB_LEAF:
 	    if ((dp=db_lookup(dgcdp->dgop->dgo_wdbp->dbip, tp->tr_l.tl_name, LOOKUP_NOISY)) == RT_DIR_NULL) {
-                if (eptr)
-		  eptr->l.m = (struct model *)NULL;
 		break;
 	    }
 	    eptr = wdb_add_solid(dp, tp->tr_l.tl_mat, dgcdp);
