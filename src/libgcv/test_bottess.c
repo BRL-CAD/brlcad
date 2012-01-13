19165bdcb297d4a979b09cd20ac241964d599d0d
Thomas Browder <tom.browder@gmail.com>
2012-01-13 20:48:40 +0000

    unwrap macros so we can see what is happening
    svn:revision:48817
    svn:branch:trunk
    svn:account:tbrowder2

--- src/libgcv/test_bottess.c
+++ src/libgcv/test_bottess.c
@@ -110,14 +110,57 @@ test_face_split_single()
     t.dist = 0.005;
     t.dist_sq = t.dist * t.dist;
 
-	/* evil macros to do a lot of scaffolding, setup, execution, etc... */
+    /* evil macros to do a lot of scaffolding, setup, execution, etc... */
     /* prep take: the triangle to split (9 fastf_t), the intersection line (6
      * fastf_t), and the number of expected resulting triangles. */
-#define PREP(t00,t01,t02,t10,t11,t12,t20,t21,t22,ispt00,ispt01,ispt02,ispt10,ispt11,ispt12,_nsplt) {point_t isectpt[2];int urf[_nsplt+1];unsigned long int failure=0,numsplit;tcount++;numsplit=_nsplt;VSET(isectpt[0],ispt00,ispt01,ispt02);VSET(isectpt[1],ispt10,ispt11,ispt12);s.magic=SOUP_MAGIC;s.faces=NULL;s.maxfaces=0;s.nfaces=0;VSET(f.vert[0],t00,t01,t02);VSET(f.vert[1],t10,t11,t12);VSET(f.vert[2],t20,t21,t22);soup_add_face(&s,V3ARGS(f.vert),&t);VSET(f.plane,0,0,1);nsplt=split_face_single(&s,0,isectpt,&f,&t);if(nsplt!=s.nfaces){printf("Errr, nsplit %lu != s.nfaces %lu ?\n",numsplit,s.nfaces);}
+
+    /* NOTE THE "PREP" AND "POST" MACROS MUST BE USED IN PAIRS DUE TO THE SPLIT ENCLOSING CURLY BRACES */
+#define PREP(t00,t01,t02,t10,t11,t12,t20,t21,t22,                       \
+             ispt00,ispt01,ispt02,ispt10,ispt11,ispt12,_nsplt)          \
+          {                                                             \
+            point_t isectpt[2];                                         \
+            int urf[_nsplt+1];                                          \
+            unsigned long int failure = 0, numsplit;                    \
+            tcount++;                                                   \
+            numsplit = _nsplt;                                          \
+            VSET(isectpt[0],ispt00,ispt01,ispt02);                      \
+            VSET(isectpt[1],ispt10,ispt11,ispt12);                      \
+            s.magic = SOUP_MAGIC;                                         \
+            s.faces = NULL;                                               \
+            s.maxfaces = 0;                                             \
+            s.nfaces = 0;                                               \
+            VSET(f.vert[0],t00,t01,t02);                                \
+            VSET(f.vert[1],t10,t11,t12);                                \
+            VSET(f.vert[2],t20,t21,t22);                                \
+            soup_add_face(&s,V3ARGS(f.vert),&t);                        \
+            VSET(f.plane,0,0,1);                                        \
+            nsplt = split_face_single(&s,0,isectpt,&f,&t);              \
+            if (nsplt != s.nfaces) {                                    \
+              printf("Errr, nsplit %lu != s.nfaces %lu ?\n", numsplit, s.nfaces); \
+            }
+
     /* the _splits is an array of expected triangles, as 9 fastf_t tuples */
     /* fastf_t _splits[nsplt][9] = {{...},{...}} */
     /* post tests to see if the resulting triangles match and cleans up */
-#define POST(name) for(i=0;i<numsplit;i++){VSET(f.vert[0],_splits[i][0],_splits[i][1],_splits[i][2]);VSET(f.vert[1],_splits[i][3],_splits[i][4],_splits[i][5]);VSET(f.vert[2],_splits[i][6],_splits[i][7],_splits[i][8]);urf[i]=find_tri(&s,&f,&t);if(urf[i]==-1)failure++;}if(nsplt!=2&&urf[0]==-1&&urf[1]==-1){printf("\033[1;31mFAILURE "name"\033[m\n");printf("%lu faces now\n",s.nfaces);for(i=0;i<s.nfaces;i++)printf("%03lu: % 2g,% 2g,% 2g | % 2g,% 2g,% 2g | % 2g,% 2g,% 2g\n",i,V3ARGS(s.faces[i].vert[0]),V3ARGS(s.faces[i].vert[1]),V3ARGS(s.faces[i].vert[2]));count++;}free(s.faces);}
+#define POST(name)                                                      \
+    for (i = 0; i < numsplit; i++) {                                    \
+      VSET(f.vert[0],_splits[i][0],_splits[i][1],_splits[i][2]);        \
+      VSET(f.vert[1],_splits[i][3],_splits[i][4],_splits[i][5]);        \
+      VSET(f.vert[2],_splits[i][6],_splits[i][7],_splits[i][8]);        \
+      urf[i] = find_tri(&s,&f,&t);                                      \
+      if (urf[i] == -1) failure++;                                      \
+    }                                                                   \
+    if (nsplt != 2 && urf[0] == -1 && urf[1] == -1) {                   \
+      printf("\033[1;31mFAILURE "name"\033[m\n");                       \
+      printf("%lu faces now\n",s.nfaces);                               \
+      for (i = 0; i < s.nfaces; i++)                                    \
+        printf("%03lu: % 2g,% 2g,% 2g | % 2g,% 2g,% 2g | % 2g,% 2g,% 2g\n", \
+               i, V3ARGS(s.faces[i].vert[0]), V3ARGS(s.faces[i].vert[1]), V3ARGS(s.faces[i].vert[2])); \
+      count++;                                                          \
+    }                                                                   \
+    free(s.faces);                                                      \
+} /* NOTE THIS IS THE CLOSING BRACE FROM THE PREVIOUS MACRO!! */
+
 
     /* VERT/VERT */
     PREP(0,0,0, 0,1,0, 1,0,0, 0,0,0, 0,1,0, 1); fastf_t _splits[1][9] = {{0,0,0, 0,1,0, 1,0,0}}; POST("VERT/VERT");
