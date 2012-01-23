e9e96fe8657d88157d6434880bfe0291218adbe5
Thomas Browder <tom.browder@gmail.com>
2012-01-23 02:16:07 +0000

    Added and rearranged tests. Added separate handling of expected failures (none at the moment). Output number of expected failures to stderr and unexpected failures   to the exit code. Modified format of test_vls function to more readily see the input   being tested. Added some printf statements to help explain test output.
    svn:revision:48961
    svn:branch:trunk
    svn:account:tbrowder2

--- src/libbu/test_vls.c
+++ src/libbu/test_vls.c
@@ -23,9 +23,19 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <stdio.h>
+#include <signal.h>
 
 #include "bu.h"
 
+#if 0
+void
+termination_handler(const int signum)
+{
+  fprintf(stderr, "DEBUG: received signal %d (but exiting with status 0\n",
+          signum);
+  /* restore the signal */
+}
+#endif
 
 /* Test against sprintf */
 int
@@ -37,17 +47,20 @@ test_vls(const char *fmt, ...)
     va_list ap;
 
     va_start(ap, fmt);
+    /* use the libc version */
     vsprintf(buffer, fmt, ap);
     va_end(ap);
 
     va_start(ap, fmt);
+    /* use BRL-CAD bu_vls version for comparison */
     bu_vls_vprintf(&vls, fmt, ap);
+
     va_end(ap);
 
     if (BU_STR_EQUAL(buffer, bu_vls_addr(&vls))) {
-        printf("%24s -> %28s [PASS]\n", fmt, bu_vls_addr(&vls));
+        printf("%-24s -> '%s' [PASS]\n", fmt, bu_vls_addr(&vls));
     } else {
-        printf("%24s -> %28s [FAIL]  (should be: %s)\n", fmt, bu_vls_addr(&vls), buffer);
+        printf("%-24s -> '%s' [FAIL]  (should be: '%s')\n", fmt, bu_vls_addr(&vls), buffer);
         status = 1;
     }
 
@@ -56,16 +69,23 @@ test_vls(const char *fmt, ...)
     return status;
 }
 
-
 int
 main(int ac, char *av[])
 {
     int fails    = 0; /* track unexpected failures */
     int expfails = 0; /* track expected failures */
-    int n;
+    int f = 0;
+    int p = 0;
+    const char *word = "Lawyer";
 
-    printf("Testing vls\n");
+    printf("Testing vls...\n");
 
+    /* ======================================================== */
+    /* TESTS EXPECTED TO PASS
+     *
+     *   (see expected failures section below)
+     */
+    /* ======================================================== */
     /* various types */
     printf("An empty string (\"\"):\n");
     fails += test_vls("");
@@ -113,35 +133,96 @@ main(int ac, char *av[])
 
     /* combinations, e.g., bug ID 3475562, fixed at rev 48958 */
     /* left justify, right justify, in wider fields than the strings */
-    n = 2;
-    fails += test_vls("|%-*.*s|%*.*s|", n, n, "t", n, n, "t");
+    f = p = 2;
+    fails += test_vls("|%-*.*s|%*.*s|", f, p, "t", f, p, "t");
+    fails += test_vls("|%*s|%-*s|", f, "test", f, "test");
+    fails += test_vls("|%*s|%-*s|", f, word, f, word);
 
-    /* ======================================================== */
-    /* KNOWN FAILURES BELOW HERE. NOTE: increment expfail vs. fails */
-    /* FAILING TESTS (make vls-regress) ================ */
-    printf("\nExpected failures (don't use in production code):\n");
+    /* min field width; max string length ('precision'); string */
+    f = 2; p = 4;
+    printf("fw=%d, prec=%d, '%s': '%%%d.%ds'\n", f, p, word, f, p);
+    fails += test_vls("%*.*s", f, p, word);
+
+    f = 4; p = 2;
+    printf("fw=%d, prec=%d, '%s': '%%%d.%ds'\n", f, p, word, f, p);
+    fails += test_vls("%*.*s", f, p, word);
+
+    f = 4; p = 8;
+    printf("fw=%d, prec=%d, '%s': '%%%d.%ds'\n", f, p, word, f, p);
+    fails += test_vls("%*.*s", f, p, word);
+
+    f = 0; p = 8;
+    printf("fw=%d, prec=%d, '%s': '%%%d.%ds'\n", f, p, word, f, p);
+    fails += test_vls("%*.*s", f, p, word);
+
+    f = 8; p = 0;
+    printf("fw=%d, prec=%d, '%s': '%%%d.%ds'\n", f, p, word, f, p);
+    fails += test_vls("%*.*s", f, p, word);
+
+    /* same but left justify */
+
+    f = 2; p = 4;
+    printf("fw=%d, prec=%d, '%s': '%%-%d.%ds'\n", f, p, word, f, p);
+    fails += test_vls("%-*.*s", f, p, word);
+
+    f = 4; p = 2;
+    printf("fw=%d, prec=%d, '%s': '%%-%d.%ds'\n", f, p, word, f, p);
+    fails += test_vls("%-*.*s", f, p, word);
+
+    f = 4; p = 8;
+    printf("fw=%d, prec=%d, '%s': '%%-%d.%ds'\n", f, p, word, f, p);
+    fails += test_vls("%-*.*s", f, p, word);
+
+    f = 0; p = 8;
+    printf("fw=%d, prec=%d, '%s': '%%-%d.%ds'\n", f, p, word, f, p);
+    fails += test_vls("%-*.*s", f, p, word);
+
+    f = 8; p = 0;
+    printf("fw=%d, prec=%d, '%s': '%%-%d.%ds'\n", f, p, word, f, p);
+    fails += test_vls("%-*.*s", f, p, word);
 
     /* from "various types" */
-    expfails += test_vls("%f %F", 1.23, -3.21);
+    fails += test_vls("%f %F", 1.23, -3.21);
 
     /* from "two-character length modifiers" */
     /* the last field ('%Lu') should result in zero because the number
        is a negative float and it is converted to a an unsigned int
        (it shouldn't be used with an integer) */
-    expfails += test_vls("%ld %lld %Lu", 123, -123, -321.0);
+    fails += test_vls("%ld %lld %Lu", 123, -123, -321.0);
 
-    printf("%s: testing complete\n", av[0]);
+    /* ======================================================== */
+    /* EXPECTED FAILURES ONLY BELOW HERE                           */
+    /* ======================================================== */
+    /* EXPECTED FAILURES:
+     *
+     * Notes:
+     *
+     *   1. For these tests have the return value increment 'expfails'.
+     *   2. Test with both 'make vsl-regress' and 'make regress' because
+     *        some other tests use this function in unpredictable ways.
+     *   3. After a test is fixed, change the return value to increment
+     *        'fails', move it to the EXPECTED PASS group above, and add
+     *        some info about it as necessary to help those who may be
+     *        forced to revisit this.
+     *
+     */
+
+    printf("\nExpected failures (don't use in production code):\n");
+
+    printf("  NONE AT THIS TIME\n");
+
+    /* report results */
+    fprintf(stderr, "%d", expfails);
+
+    printf("\n%s: testing complete\n", av[0]);
 
     if (fails != 0) {
-      /* as long as fails is < 127 the STATUS will be the number of unexped failures */
+      /* as long as fails is < 127 the STATUS will be the number of unexpected failures */
       return fails;
     }
 
-    /* output expected failures to stderr to be used for the
-       regression test script */
-    fprintf(stderr, "%d", expfails);
-
     return 0;
+
 }
 
 
