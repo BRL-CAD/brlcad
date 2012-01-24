f310a7d647578e14c8ead0e15300c56ba419c2fc
Christopher Sean Morrison <brlcad@gmail.com>
2012-01-24 15:12:30 +0000

    stub in a new dsp primitive regression test
    svn:revision:49027
    svn:branch:trunk
    svn:account:brlcad

---
+++ regress/dsp.sh
@@ -0,0 +1,191 @@
+#!/bin/sh
+#                          D S P . S H
+# BRL-CAD
+#
+# Copyright (c) 2010-2012 United States Government as represented by
+# the U.S. Army Research Laboratory.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above
+# copyright notice, this list of conditions and the following
+# disclaimer in the documentation and/or other materials provided
+# with the distribution.
+#
+# 3. The name of the author may not be used to endorse or promote
+# products derived from this software without specific prior written
+# permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS
+# OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+# GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+###
+
+# Ensure /bin/sh
+export PATH || (echo "This isn't sh."; sh $0 $*; kill $$)
+
+# source common library functionality, setting ARGS, NAME_OF_THIS,
+# PATH_TO_THIS, and THIS.
+. $1/regress/library.sh
+
+MGED="`ensearch mged`"
+if test ! -f "$MGED" ; then
+    echo "Unable to find mged, aborting"
+    exit 1
+fi
+
+CV="`ensearch cv`"
+if test ! -f "$CV" ; then
+    echo "Unable to find cv, aborting"
+    exit 1
+fi
+
+rm -f dsp.log dsp.mged.log
+
+# create 0x0 dataset (null case)
+
+# create 1x1 datasets
+# ---
+# | |
+# ---
+# and
+# ---
+# |X|
+# ---
+
+# create 2x2 datasets
+# -----
+# |X|X|
+# -----
+# |X|X|
+# -----
+#  and
+# -----
+# |X|X|
+# -----
+# |X| |
+# -----
+#  and
+# -----
+# |X| |
+# -----
+# | |X|
+# -----
+#  and
+# -----
+# | | |
+# -----
+# |X|X|
+# -----
+#  and
+# -----
+# | | |
+# -----
+# | |X|
+# -----
+
+# create 3x3 datasets
+# -------
+# |X|X|X|
+# -------
+# |X|X|X|
+# -------
+# |X|X|X|
+# -------
+#   and
+# -------
+# |X|X|X|
+# -------
+# | |X| |
+# -------
+# |X|X|X|
+# -------
+#   and
+# -------
+# | |X| |
+# -------
+# |X|X|X|
+# -------
+# | |X| |
+# -------
+#   and
+# -------
+# |X|X|X|
+# -------
+# |X| |X|
+# -------
+# |X|X|X|
+# -------
+#   and
+# -------
+# |X|X|X|
+# -------
+# | | | |
+# -------
+# |X|X|X|
+# -------
+#   and
+# -------
+# |X| |X|
+# -------
+# | |X| |
+# -------
+# |X| |X|
+# -------
+#   and
+# -------
+# |X| |X|
+# -------
+# | | | |
+# -------
+# |X| |X|
+# -------
+#   and
+# -------
+# | | | |
+# -------
+# | |X| |
+# -------
+# | | | |
+# -------
+
+# create random 10x10 datasets
+
+
+#$MGED -c dsp.g <<EOF > dsp.mged.log 2>&1
+#
+#EOF
+
+
+STATUS=0
+
+if [ $STATUS = 0 ] ; then
+    echo "-> dsp.sh succeeded"
+else
+    echo "-> dsp.sh FAILED"
+fi
+
+exit $STATUS
+
+# Local Variables:
+# mode: sh
+# tab-width: 8
+# sh-indentation: 4
+# sh-basic-offset: 4
+# indent-tabs-mode: t
+# End:
+# ex: shiftwidth=4 tabstop=8
