dccea73a74227c05a442d97071d60943f7611ade
Christopher Sean Morrison <brlcad@gmail.com>
2012-01-27 03:10:34 +0000

    begs explanation, so detail what's going on here.  also need common.h for BRLCAD_ROOT.
    svn:revision:49122
    svn:branch:trunk
    svn:account:brlcad

--- misc/win32-msvc/bootstrap.cpp
+++ misc/win32-msvc/bootstrap.cpp
@@ -1,5 +1,14 @@
-#include <windows.h>
+#include "common.h"
+#include "bio.h"
 
+/*
+ * This bootstrap's application static initialization on Windows to
+ * add BRL-CAD's library installation path to the list of paths
+ * searched for DLLs.  This is useful in leu of requiring PATH being
+ * set apriori, avoids registry entries (and associated permissions
+ * required), and avoids putting libraries in the same dir as binaries
+ * (which would make our installation hierarchy inconsistent).
+ */
 static bool
 bootstrap()
 {
