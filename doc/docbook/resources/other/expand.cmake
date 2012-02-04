e352895277c4028962fd60a25af22dbffc6f13ba
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2012-02-04 17:42:09 +0000

    Move some docbook stuff around
    svn:revision:49237
    svn:branch:trunk
    svn:account:starseeker

--- doc/docbook/resources/other/expand.cmake
+++ doc/docbook/resources/other/expand.cmake
@@ -2,24 +2,27 @@
 # Define the commands to expand the third party components we will need to handle
 # DocBook processing from their archives into the build directory
 file(MAKE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/resources/other/docbook-schema)
+DISTCLEAN(${CMAKE_CURRENT_BINARY_DIR}/resources/other/fonts/docbook-schema)
 file(MAKE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/resources/other/fonts/dejavu-lgc)
 DISTCLEAN(${CMAKE_CURRENT_BINARY_DIR}/resources/other/fonts/dejavu-lgc)
 file(MAKE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/resources/other/fonts/stix)
 DISTCLEAN(${CMAKE_CURRENT_BINARY_DIR}/resources/other/fonts/stix)
 file(MAKE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/resources/other/offo)
+DISTCLEAN(${CMAKE_CURRENT_BINARY_DIR}/resources/other/offo/source)
+DISTCLEAN(${CMAKE_CURRENT_BINARY_DIR}/resources/other/offo/binary)
+DISTCLEAN(${CMAKE_CURRENT_BINARY_DIR}/resources/other/offo/README)
 file(MAKE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/resources/other/standard/svg)
+DISTCLEAN(${CMAKE_CURRENT_BINARY_DIR}/resources/other/standard/svg)
 file(MAKE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/resources/other/standard/xsl)
-if(NOT "${CMAKE_SOURCE_DIR}" STREQUAL "${CMAKE_BINARY_DIR}")
-	DISTCLEAN(${CMAKE_CURRENT_BINARY_DIR}/resources/other)
-endif(NOT "${CMAKE_SOURCE_DIR}" STREQUAL "${CMAKE_BINARY_DIR}")
+DISTCLEAN(${CMAKE_CURRENT_BINARY_DIR}/resources/other/standard/xsl)
 
 ADD_CUSTOM_COMMAND(
 	OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/resources/other/docbook-schema/schema.sentinel
-	COMMAND ${CMAKE_COMMAND} -E tar xjf	${CMAKE_CURRENT_SOURCE_DIR}/resources/other/docbook-schema/docbook-5.0.tar.bz2
-	COMMAND ${CMAKE_COMMAND} -E copy	${CMAKE_CURRENT_SOURCE_DIR}/resources/other/docbook-schema/docbookxi.nvdl ${CMAKE_CURRENT_BINARY_DIR}/resources/other/docbook-schema/docbookxi.nvdl
+	COMMAND ${CMAKE_COMMAND} -E tar xjf	${CMAKE_CURRENT_SOURCE_DIR}/resources/other/docbook-5.0.tar.bz2
+	COMMAND ${CMAKE_COMMAND} -E copy	${CMAKE_CURRENT_SOURCE_DIR}/resources/other/docbookxi.nvdl ${CMAKE_CURRENT_BINARY_DIR}/resources/other/docbook-schema/docbookxi.nvdl
 	COMMAND ${CMAKE_COMMAND} -E touch ${CMAKE_CURRENT_BINARY_DIR}/resources/other/docbook-schema/schema.sentinel
-	DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/resources/other/docbook-schema/docbookxi.nvdl ${CMAKE_CURRENT_SOURCE_DIR}/resources/other/docbook-schema/docbook-5.0.tar.bz2
-	WORKING_DIRECTORY	${CMAKE_CURRENT_BINARY_DIR}/resources/other/docbook-schema
+	DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/resources/other/docbookxi.nvdl ${CMAKE_CURRENT_SOURCE_DIR}/resources/other/docbook-5.0.tar.bz2
+	WORKING_DIRECTORY	${CMAKE_CURRENT_BINARY_DIR}/resources/other
 	)
 ADD_CUSTOM_TARGET(schema-expand DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/resources/other/docbook-schema/schema.sentinel)
 
@@ -52,20 +55,20 @@ ADD_CUSTOM_TARGET(offo-2-expand DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/resources/ot
 
 ADD_CUSTOM_COMMAND(
 	OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/resources/other/standard/svg/svg.sentinel
-	COMMAND ${CMAKE_COMMAND} -E tar xjf	${CMAKE_CURRENT_SOURCE_DIR}/resources/other/standard/svg/w3_svg_dtd.tar.bz2
-	COMMAND ${CMAKE_COMMAND} -E copy	${CMAKE_CURRENT_SOURCE_DIR}/resources/other/standard/svg/met-fonts.xsl	${CMAKE_CURRENT_BINARY_DIR}/resources/other/standard/svg/met-fonts.xsl
+	COMMAND ${CMAKE_COMMAND} -E tar xjf	${CMAKE_CURRENT_SOURCE_DIR}/resources/other/standard/w3_svg_dtd.tar.bz2
+	COMMAND ${CMAKE_COMMAND} -E copy	${CMAKE_CURRENT_SOURCE_DIR}/resources/other/standard/met-fonts.xsl	${CMAKE_CURRENT_BINARY_DIR}/resources/other/standard/svg/met-fonts.xsl
 	COMMAND ${CMAKE_COMMAND} -E touch ${CMAKE_CURRENT_BINARY_DIR}/resources/other/standard/svg/svg.sentinel
-	DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/resources/other/standard/svg/met-fonts.xsl ${CMAKE_CURRENT_SOURCE_DIR}/resources/other/standard/svg/w3_svg_dtd.tar.bz2
-	WORKING_DIRECTORY	${CMAKE_CURRENT_BINARY_DIR}/resources/other/standard/svg
+	DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/resources/other/standard/met-fonts.xsl ${CMAKE_CURRENT_SOURCE_DIR}/resources/other/standard/w3_svg_dtd.tar.bz2
+	WORKING_DIRECTORY	${CMAKE_CURRENT_BINARY_DIR}/resources/other/standard
 	)
 ADD_CUSTOM_TARGET(svg-dtd-expand DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/resources/other/standard/svg/svg.sentinel)
 
 ADD_CUSTOM_COMMAND(
 	OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/resources/other/standard/xsl/xsl.sentinel
-	COMMAND ${CMAKE_COMMAND} -E tar xjf	${CMAKE_CURRENT_SOURCE_DIR}/resources/other/standard/xsl/docbook-xsl-ns.tar.bz2
+	COMMAND ${CMAKE_COMMAND} -E tar xjf	${CMAKE_CURRENT_SOURCE_DIR}/resources/other/standard/docbook-xsl-ns.tar.bz2
 	COMMAND ${CMAKE_COMMAND} -E touch ${CMAKE_CURRENT_BINARY_DIR}/resources/other/standard/xsl/xsl.sentinel
-	DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/resources/other/standard/xsl/docbook-xsl-ns.tar.bz2
-	WORKING_DIRECTORY	${CMAKE_CURRENT_BINARY_DIR}/resources/other/standard/xsl
+	DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/resources/other/standard/docbook-xsl-ns.tar.bz2
+	WORKING_DIRECTORY	${CMAKE_CURRENT_BINARY_DIR}/resources/other/standard
 	)
 ADD_CUSTOM_TARGET(xsl-expand DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/resources/other/standard/xsl/xsl.sentinel)
 
