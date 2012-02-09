14cda0966089990c55d841a0701a1713f83cf873
Nicholas Reed <nreed1@umbc.edu>
2012-02-09 23:44:46 +0000

    Forgot to dereference char* before comparing to '\0'; try again. CID 389.
    svn:revision:49393
    svn:branch:trunk
    svn:account:n_reed

--- src/lgt/prnt.c
+++ src/lgt/prnt.c
@@ -28,6 +28,7 @@
 #include <assert.h>
 #include <stdarg.h>
 
+#include "bu.h"
 #include "vmath.h"
 #include "raytrace.h"
 #include "fb.h"
@@ -221,11 +222,14 @@ update_Screen(void)
 {
     int	tem_co, row, col;
     tem_co = FMIN(co, TEMPLATE_COLS);
-    for ( row = 0; template[row][0] != '\0'; row++ )
+    for ( row = 0; !BU_STR_EMPTY(template[row]); row++ )
     {
 	int	lastcol = -2;
-	if (template[row+1] == '\0')
+
+	if (BU_STR_EMPTY(template[row + 1])) {
 	    SetStandout();
+	}
+
 	for ( col = 0; col < tem_co; col++ )
 	    if ( screen[row][col] != template[row][col] )
 	    {
