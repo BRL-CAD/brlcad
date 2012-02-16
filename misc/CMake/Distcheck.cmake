741ae8a19af3ed36d01a256ff52b6305d2fb70bc
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2012-02-16 08:04:37 +0000

    'Anything worth doing is worth applying insane overengineering to, or something like that... This takes the CMake distcheck architecture to the next level.  It modularizes the distcheck process into repository verification, source generation, and build/regress testing components, with appropriate dependencies.  In addition to the advantage of being able to run 'only what you need' if, say, you want to check repo verification status this actually makes distribution testing of multiple build/configure settings independent.  So if distcheck needs to test both Debug and Release CMAKE_BUILD_TYPE settings (which looks like a good idea) a make -j9 distcheck will do the repo verifiction, build the source archives and launch multiple builds of BRL-CAD in parallel.  On massively parallel machines this means not only C source compilations in parallel but multiple CMake configures running in parallel as well.  The logging by default is much quieter now, althought there is a variable override if needed... to monitor the progress of individual configurations' builds you can now tail -f the distcheck_<type>.log files in the build directory - so even for parallel testing of multiple configurations the logging is organized automatically.  The standard distcheck target will do Debug and Release, but there is also a distcheck-full target that will run ALL defined configurations.  Right now there aren't any more, but there are some obvious candiates like a non-Tk build, and they can be added to distcheck-full with a single definition line in Distcheck.cmake
    svn:revision:49448
    svn:branch:trunk
    svn:account:starseeker

--- misc/CMake/Distcheck.cmake
+++ misc/CMake/Distcheck.cmake
@@ -1,3 +1,35 @@
+# Each config (Debug, Release, Tk disable, etc. etc.) is set up as an
+# individual distcheck target (distcheck-debug, distcheck-release, etc.) and then toplevel distcheck
+# and distcheck-full targets
+
+if("${CMAKE_VERBOSE_DISTCHECK}" STREQUAL "")
+	set(CMAKE_VERBOSE_DISTCHECK OFF)
+endif("${CMAKE_VERBOSE_DISTCHECK}" STREQUAL "")
+
+macro(CREATE_DISTCHECK TARGET_SUFFIX CMAKE_OPTS)
+	# Need to set these locally so configure_file will pick them up...
+	SET(TARGET_SUFFIX ${TARGET_SUFFIX})
+	SET(CMAKE_OPTS ${CMAKE_OPTS})
+
+    # Determine how to trigger the build in the distcheck target
+    if("${CMAKE_GENERATOR}" MATCHES "Make" AND ("${cmake_generator}" MATCHES "Make" OR NOT cmake_generator))
+		 if(NOT CMAKE_VERBOSE_DISTCHECK)
+		   set(TARGET_REDIRECT " >> distcheck_${TARGET_SUFFIX}.log ")
+		 endif(NOT CMAKE_VERBOSE_DISTCHECK)
+		 set(DISTCHECK_BUILD_CMD "${CMAKE_COMMAND} -E chdir  distcheck-${TARGET_SUFFIX}/build $(MAKE) ${TARGET_REDIRECT}")
+    else("${CMAKE_GENERATOR}" MATCHES "Make" AND ("${cmake_generator}" MATCHES "Make" OR NOT cmake_generator))
+		 set(DISTCHECK_BUILD_CMD "COMMAND ${CMAKE_COMMAND} -E build distcheck-${TARGET_SUFFIX}/build")
+		 set(TARGET_REDIRECT "")
+    endif("${CMAKE_GENERATOR}" MATCHES "Make" AND ("${cmake_generator}" MATCHES "Make" OR NOT cmake_generator))
+
+    # Based on the build command, generate a distcheck target definition from the template
+	 configure_file(${BRLCAD_CMAKE_DIR}/distcheck_target.cmake.in ${CMAKE_CURRENT_BINARY_DIR}/CMakeTmp/distcheck_target_${TARGET_SUFFIX}.cmake @ONLY)
+	 include(${CMAKE_CURRENT_BINARY_DIR}/CMakeTmp/distcheck_target_${TARGET_SUFFIX}.cmake)
+
+	 # Keep track of the distcheck targets
+	 set(distcheck_targets ${distcheck_targets} distcheck-${TARGET_SUFFIX})
+endmacro(CREATE_DISTCHECK)
+
 macro(DEFINE_DISTCHECK_TARGET)
   if(NOT IS_SUBBUILD)
     find_program(CPACK_EXEC cpack)
@@ -6,16 +38,28 @@ macro(DEFINE_DISTCHECK_TARGET)
     # Set up the script that will be used to verify the source archives
     configure_file(${BRLCAD_CMAKE_DIR}/distcheck_repo_verify.cmake.in ${CMAKE_CURRENT_BINARY_DIR}/CMakeTmp/distcheck_repo_verify.cmake @ONLY)
 
-    # Determine how to trigger the build in the distcheck target
-    if("${CMAKE_GENERATOR}" MATCHES "Make" AND ("${cmake_generator}" MATCHES "Make" OR NOT cmake_generator))
-      set(DISTCHECK_BUILD_CMD "${CMAKE_COMMAND} -E chdir _${CPACK_SOURCE_PACKAGE_FILE_NAME}-build $(MAKE) > distcheck_compilation_log")
-    else("${CMAKE_GENERATOR}" MATCHES "Make" AND ("${cmake_generator}" MATCHES "Make" OR NOT cmake_generator))
-      set(DISTCHECK_BUILD_CMD "COMMAND ${CMAKE_COMMAND} -E build _${CPACK_SOURCE_PACKAGE_FILE_NAME}-build")
-    endif("${CMAKE_GENERATOR}" MATCHES "Make" AND ("${cmake_generator}" MATCHES "Make" OR NOT cmake_generator))
+	 add_custom_target(distcheck-repo-verify
+		 COMMAND ${CMAKE_COMMAND} -E echo "*** Check files in Source Repository against files specified in Build Logic ***"
+		 COMMAND ${CMAKE_COMMAND} -P ${CMAKE_CURRENT_BINARY_DIR}/CMakeTmp/distcheck_repo_verify.cmake)
 
-    # Based on the build command, generate a distcheck target definition from the template
-    configure_file(${BRLCAD_CMAKE_DIR}/distcheck_target.cmake.in ${CMAKE_CURRENT_BINARY_DIR}/CMakeTmp/distcheck_target.cmake @ONLY)
-    include(${CMAKE_CURRENT_BINARY_DIR}/CMakeTmp/distcheck_target.cmake)
+	 add_custom_target(distcheck-source-archives
+		 COMMAND ${CMAKE_COMMAND} -E echo "*** Create source tgz, tbz2 and zip archives from toplevel archive ***"
+		 COMMAND ${CPACK_EXEC} --config ${CMAKE_CURRENT_BINARY_DIR}/CPackSourceConfig.cmake
+		 DEPENDS distcheck-repo-verify)
+
+
+	 CREATE_DISTCHECK(debug   "-DCMAKE_BUILD_TYPE=Debug")
+	 CREATE_DISTCHECK(release "-DCMAKE_BUILD_TYPE=Release")
+
+	 add_custom_target(distcheck 
+	    # The source repository verification script is responsible for generating this file
+       COMMAND ${CMAKE_COMMAND} -P ${CMAKE_CURRENT_BINARY_DIR}/CMakeTmp/distcheck_message
+		 DEPENDS distcheck-debug distcheck-release)
+
+	 add_custom_target(distcheck-full
+       # The source repository verification script is responsible for generating this file
+       COMMAND ${CMAKE_COMMAND} -P ${CMAKE_CURRENT_BINARY_DIR}/CMakeTmp/distcheck_message
+		 DEPENDS ${distcheck_targets})
 
   endif(NOT IS_SUBBUILD)
 endmacro(DEFINE_DISTCHECK_TARGET)
