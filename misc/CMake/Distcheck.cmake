88418553770ab0a9a64dbd4488497254c3055c9a
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2012-02-16 02:30:02 +0000

    Make a stab at cleaning up the distcheck logic a bit, or at least reducing the line count of the logic needed to define it...
    svn:revision:49440
    svn:branch:trunk
    svn:account:starseeker

---
+++ misc/CMake/Distcheck.cmake
@@ -0,0 +1,21 @@
+macro(DEFINE_DISTCHECK_TARGET)
+  if(NOT IS_SUBBUILD)
+    find_program(CPACK_EXEC cpack)
+    mark_as_advanced(CPACK_EXEC)
+
+    # Set up the script that will be used to verify the source archives
+    configure_file(${BRLCAD_CMAKE_DIR}/distcheck_buildsys.cmake.in ${CMAKE_CURRENT_BINARY_DIR}/CMakeTmp/distcheck_buildsys.cmake @ONLY)
+
+    # Determine how to trigger the build in the distcheck target
+    if("${CMAKE_GENERATOR}" MATCHES "Make" AND ("${cmake_generator}" MATCHES "Make" OR NOT cmake_generator))
+      set(DISTCHECK_BUILD_CMD "${CMAKE_COMMAND} -E chdir _${CPACK_SOURCE_PACKAGE_FILE_NAME}-build $(MAKE)")
+    else("${CMAKE_GENERATOR}" MATCHES "Make" AND ("${cmake_generator}" MATCHES "Make" OR NOT cmake_generator))
+      set(DISTCHECK_BUILD_CMD "COMMAND ${CMAKE_COMMAND} -E build _${CPACK_SOURCE_PACKAGE_FILE_NAME}-build")
+    endif("${CMAKE_GENERATOR}" MATCHES "Make" AND ("${cmake_generator}" MATCHES "Make" OR NOT cmake_generator))
+
+    # Based on the build command, generate a distcheck target definition from the template
+    configure_file(${BRLCAD_CMAKE_DIR}/distcheck_target.cmake.in ${CMAKE_CURRENT_BINARY_DIR}/CMakeTmp/distcheck_target.cmake @ONLY)
+    include(${CMAKE_CURRENT_BINARY_DIR}/CMakeTmp/distcheck_target.cmake)
+
+  endif(NOT IS_SUBBUILD)
+endmacro(DEFINE_DISTCHECK_TARGET)
