8209f21d90c597d8dba8a3a007e5ea4f5dc87357
Richard Weiss <r_weiss@brlcad.org>
2012-03-23 20:58:00 +0000

    Updated to TCL script 'grouper.tcl' commands 'gr' and 'dg'. To improve performance, changed logic to use libged function 'ged_select'. Also script now highlights in yellow objects in the current group.
    svn:revision:49807
    svn:branch:trunk
    svn:account:r_weiss

--- src/tclscripts/mged/grouper.tcl
+++ src/tclscripts/mged/grouper.tcl
@@ -11,6 +11,7 @@ proc childcount { args } {
 	return "[llength $collData]"	
 }
 
+
 proc remdup { args } {
 	if { [llength $args] != 1 } { return "Usage: remdup collection" }
 
@@ -42,143 +43,45 @@ proc remdup { args } {
 
 }
 
-proc get_Rectangle_coords {} {
+
+proc getObjInRectangle {} {
+
+        set size [dm size]
+        set sizeX [lindex $size 0].0
+        set sizeY [lindex $size 1].0
+
+	set adjX [expr {($sizeX / 2.0) - 1.0}]
+	set adjY [expr {($sizeY / 2.0) - 1.0}]
 
 	set dim [rset r dim]     
 	set dimX [lindex $dim 0].0     
 	set dimY [lindex $dim 1].0    
- 
+
 	if { $dimX == 0.0 || $dimY == 0.0 } {
 	    return "no_result"
 	}
 
-	set size [dm size]     
-	set sizeX [lindex $size 0].0     
-	set adj [expr ($sizeX / 2.0) - 1.0]   
-
 	set pos [rset r pos]     
 	set posX [lindex $pos 0].0     
 	set posY [lindex $pos 1].0  
-   
-	if { $dimX > 0 } { 	
-		set minX [expr $posX - $adj] 	
-		set maxX [expr ($posX + ($dimX - 1.0)) - $adj]     
-	} else { 	
-		set minX [expr ($posX + ($dimX + 1.0)) - $adj]     
-		set maxX [expr $posX - $adj] 	
-	}     
 
-	if { $dimY > 0 } { 	
-		set minY [expr $posY - $adj]	
-		set maxY [expr ($posY + ($dimY - 1.0)) - $adj]     
-	} else { 	
-		set minY [expr ($posY + ($dimY + 1.0)) - $adj]     
-		set maxY [expr $posY - $adj]	
-	}      
-	
-	#puts "Rectangle: $minX $maxX $minY $maxY"
-	return [list $minX $maxX $minY $maxY]
-}
+	set posX [expr ($posX - $adjX) / $adjX]
+	set posY [expr ($posY - $adjY) / $adjY]
+	set dimX [expr ($dimX / $adjX)]
+	set dimY [expr ($dimY / $adjY)]
 
+        set objs [select $posX $posY $dimX $dimY]
 
-proc getObjInRectangle {} {
-
-	set rect [get_Rectangle_coords]
-
-	if { $rect == "no_result" } {
-	    return $rect 
+	if { $objs == "" } {
+	    return "no_result"
 	}
 
-        set size [dm size]
-        set sizeX [lindex $size 0].0
-	set adj [expr {($sizeX / 2.0) - 1.0}]
+	foreach obj $objs {
+           set obj2 [file tail $obj]
+           lappend objs2 $obj2
+        }
 
-	set minX [lindex $rect 0]
-	set maxX [lindex $rect 1]
-	set minY [lindex $rect 2]
-	set maxY [lindex $rect 3]
-
-	# 'x 3' returns results in base units (mm)
-	set xList [x 3]
-
-	set indicies [lsearch -all $xList "/*"]
-	set objs ""     
-	set index -1
-	
-	#puts stdout "Starting run..."
-	#puts stdout "indicies\: $indicies"
-	#puts stdout "Length of xList\: [llength $xList]"
-
-	set indicies_length [llength $indicies]
-	
-	for {set a 0} { $a < $indicies_length } { incr a } {
-	
-		#set boundries
-		
-		set thisID [lindex $indicies $a]
-		if { [expr {$a + 1} ] == [llength $indicies]} {
-			#last element
-			set lastID [expr {[llength $xList] - 1} ]
-		} else {
-			set lastID [lindex $indicies [expr {$a + 1} ]]
-		}
-		
-		#get solid name
-		set line [lindex $xList $thisID]
-		set tempIndex [string last "/" $line ] ; incr tempIndex
-		set objName [string range $line $tempIndex end]
-		#puts stdout "$objName"
-		
-		incr thisID 14
-
-		#puts stdout "Searching range\: $thisID to $lastID"
-		
-		for {set i $thisID } {$i <= $lastID } { incr i } {
-			set line [lindex $xList $i]
-					
-			if {$line == "line"} {
-				#Extract the strings
-
-				#tk_messageBox -message "line0: '$line'"
-				
-				incr i 2 ; set line [lindex $xList $i]
-				set modelX [string range $line 1 end-1]
-				
-				incr i 1 ; set line [lindex $xList $i]
-				set modelY [string range $line 0 end-1]
-				
-				incr i 1 ; set line [lindex $xList $i]
-				set modelZ [string range $line 0 end-1]
-
-				#Convert
-				set viewCoord [model2view $modelX $modelY $modelZ]
-
-                                #Evaluate
-                                set ViewX [expr [lindex $viewCoord 0] * $adj]
-                                if { [expr $ViewX < $minX] } {
-                                    continue
-                                }
-                                if { [expr $ViewX > $maxX] } {
-                                    continue
-                                }
-                                set ViewY [expr [lindex $viewCoord 1] * $adj]
-                                if { [expr $ViewY < $minY] } {
-                                    continue
-                                }
-                                if { [expr $ViewY > $maxY] } {
-                                    continue
-                                }
-
-                                lappend objs $objName
-                                break
-			}
-		}
-	}
-
-	#puts stdout "Ending run..."
-
-	#puts stdout "Results\: $objs "
-	return $objs
+        return $objs2
 }
 
 
@@ -204,16 +107,24 @@ proc grouper {GroupName Boolean} {
 	rset r draw 1     
 	bind $mged_gui($id,active_dm) <ButtonRelease-2> " winset $mged_gui($id,active_dm); dm idle; do_grouper $GroupName $Boolean" 
 	bind $mged_gui($id,active_dm) <Control-ButtonRelease-2> " winset $mged_gui($id,active_dm); dm idle; done_grouper" 
+
+        # highlight in yellow current group
+	if {[search -name $GroupName] != ""} {
+            e -C255/255/0 $GroupName
+        }
 }
 
 proc do_grouper {GroupName Boolean } {
 
-	set objs [lsort -unique [getObjInRectangle]]
-	set tot_obj_in_rect [llength $objs]
+	set objs [getObjInRectangle]
 
 	if { $objs == "no_result" } {
+	    puts stdout "Rectangle is empty"
 	    return
 	}
+
+	set objs [lsort -unique $objs]
+	set tot_obj_in_rect [llength $objs]
 
 	if {[search -name $GroupName] == ""} {
 	    set grp_obj_list_before ""
@@ -256,6 +167,11 @@ proc do_grouper {GroupName Boolean } {
 	} else {
 	    puts stdout "Rectangle is empty"
 	}
+
+        # highlight in yellow current group
+	if {[search -name $GroupName] != ""} {
+            e -C255/255/0 $GroupName
+        }
 }
 
 proc dg {} { done_grouper }
