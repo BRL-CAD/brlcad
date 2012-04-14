b2b2d738df523a342a652443972a511bdfe48e07
Thomas Browder <tom.browder@gmail.com>
2012-04-14 00:52:41 +0000

    add start of regression tests for g-nff and g-dot
    svn:revision:50007
    svn:branch:trunk
    svn:account:tbrowder2

---
+++ regress/g-dot.sh
@@ -0,0 +1,98 @@
+#!/bin/sh
+#                         G - D O T . S H
+# BRL-CAD
+#
+# Copyright (c) 2010-2012 United States Government as represented by
+# the U.S. Army Research Laboratory.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above
+# copyright notice, this list of conditions and the following
+# disclaimer in the documentation and/or other materials provided
+# with the distribution.
+#
+# 3. The name of the author may not be used to endorse or promote
+# products derived from this software without specific prior written
+# permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS
+# OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+# GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+###
+
+# Ensure /bin/sh
+export PATH || (echo "This isn't sh."; sh $0 $*; kill $$)
+
+# source common library functionality, setting ARGS, NAME_OF_THIS,
+# PATH_TO_THIS, and THIS.
+. "$1/regress/library.sh"
+
+MGED="`ensearch mged`"
+if test ! -f "$MGED" ; then
+    echo "Unable to find mged, aborting"
+    exit 1
+fi
+
+GDOT="`ensearch g-dot`"
+if test ! -f "$GDOT" ; then
+    echo "Unable to find 'g-dot', aborting"
+    exit 1
+fi
+
+FILS='g-dot.log g-dot.g g-dot.dot'
+
+rm -f $FILS
+
+STATUS=0
+
+$MGED -c 2> g-dot.log <<EOF
+opendb g-dot.g y
+in rpp.s rpp 0 10 0 10 0 10
+r rpp.r u rpp.s
+g all rpp.r
+q
+EOF
+
+# .g to dot:
+$GDOT -o g-dot.dot g-dot.g all 2>> g-dot.log > /dev/null
+
+if [ $? != 0 ] ; then
+    echo g-dot FAILED
+    STATUS=1
+else
+    echo g-dot completed successfully
+fi
+
+
+if [ X$STATUS = X0 ] ; then
+    echo "-> g-dot.sh succeeded"
+    rm -f $FILS
+else
+    echo "-> g-dot.sh FAILED"
+fi
+
+exit $STATUS
+
+# Local Variables:
+# mode: sh
+# tab-width: 8
+# sh-indentation: 4
+# sh-basic-offset: 4
+# indent-tabs-mode: t
+# End:
+# ex: shiftwidth=4 tabstop=8
