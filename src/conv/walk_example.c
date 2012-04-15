8a3038252a63a543f059521d7955daaad43a1a44
Thomas Browder <tom.browder@gmail.com>
2012-04-15 11:04:57 +0000

    typo, ws
    svn:revision:50038
    svn:branch:trunk
    svn:account:tbrowder2

--- src/conv/walk_example.c
+++ src/conv/walk_example.c
@@ -264,7 +264,7 @@ int main(int ac, char *av[])
 
     if ((ac - arg_count) < 1) {
 	tmp_basename = bu_basename(av[0]);
-	usage(tmp_basename, "bad arugment count");
+	usage(tmp_basename, "bad argument count");
 	bu_free(tmp_basename, "tmp_basename free");
     }
 
@@ -274,7 +274,7 @@ int main(int ac, char *av[])
      *  the database.  It also gives you back the
      *  title string in the header (ID) record.
      */
-    rtip=rt_dirbuild(av[arg_count], idbuf, sizeof(idbuf));
+    rtip = rt_dirbuild(av[arg_count], idbuf, sizeof(idbuf));
     if (rtip == RTI_NULL) {
 	bu_exit(2, "%s: rt_dirbuild failure\n", av[0]);
     }
