5eb054f86ccab376c22a849f0f711d7492ec9c4b
Thomas Browder <tom.browder@gmail.com>
2012-04-16 11:55:00 +0000

    make test_quote work and report failures
    svn:revision:50071
    svn:branch:trunk
    svn:account:tbrowder2

--- src/libbu/test_quote.c
+++ src/libbu/test_quote.c
@@ -25,56 +25,105 @@
 #include "bu.h"
 
 
-/* Test for reversibility */
-void
+/* Test for reversibility:
+ *
+ *   1. encode the input string
+ *   2. decode the encoded string
+ *   3. decoded string should be the same as input
+ *
+ */
+int
 test_quote(const char *str)
 {
-    struct bu_vls quoted = BU_VLS_INIT_ZERO;
-    const char *dequoted = NULL;
+    int status = 0;
+    struct bu_vls encoded = BU_VLS_INIT_ZERO;
+    struct bu_vls decoded = BU_VLS_INIT_ZERO;
 
-    if (BU_STR_EQUAL(str, dequoted) && !BU_STR_EQUAL(str, bu_vls_addr(&quoted))) {
-	printf("%24s -> %28s [PASS]\n", str, bu_vls_addr(&quoted));
+    bu_vls_encode(&encoded, str);
+    bu_vls_encode(&decoded, bu_vls_addr(&encoded));
+
+
+    if (BU_STR_EQUAL(str, bu_vls_addr(&decoded))
+        && !BU_STR_EQUAL(str, bu_vls_addr(&encoded))) {
+	printf("%24s -> %28s [PASS]\n", str, bu_vls_addr(&encoded));
     } else {
-	printf("%24s -> %28s [FAIL]  (should be: %s)\n", str, bu_vls_addr(&quoted), str);
+	printf("%24s -> %28s [FAIL]  (should be: %s)\n", str, bu_vls_addr(&encoded), str);
+        status = 1;
     }
 
-    bu_vls_free(&quoted);
+    bu_vls_free(&encoded);
+    bu_vls_free(&decoded);
+
+    return status;
 }
 
 
 int
 main(int ac, char *av[])
 {
+    int fails    = 0; /* track unexpected failures */
+    int expfails = 0; /* track expected failures */
+
     printf("Testing quote\n");
 
     if (ac > 1)
 	printf("Usage: %s\n", av[0]);
 
-    test_quote(NULL);
-    test_quote("");
-    test_quote(" ");
-    test_quote("hello");
-    test_quote("\"");
-    test_quote("\'");
-    test_quote("\\");
-    test_quote("\\\"");
-    test_quote("\\\\");
-    test_quote("\"hello\"");
-    test_quote("\'hello\'");
-    test_quote("\\hello");
-    test_quote("\\hello\"");
-    test_quote("hello\\\\");
-    test_quote("\"hello\'\"");
-    test_quote("\"hello\'");
-    test_quote("\'hello\'");
-    test_quote("\'hello\"");
-    test_quote("\"\"hello\"");
-    test_quote("\'\'hello\'\'");
-    test_quote("\'\"hello\"\'");
-    test_quote("\"\"hello\"\"");
-    test_quote("\"\"\"hello\"\"\"");
+    fails += test_quote(NULL);
+    fails += test_quote("");
+    fails += test_quote(" ");
+    fails += test_quote("hello");
+    fails += test_quote("\"");
+    fails += test_quote("\'");
+    fails += test_quote("\\");
+    fails += test_quote("\\\"");
+    fails += test_quote("\\\\");
+    fails += test_quote("\"hello\"");
+    fails += test_quote("\'hello\'");
+    fails += test_quote("\\hello");
+    fails += test_quote("\\hello\"");
+    fails += test_quote("hello\\\\");
+    fails += test_quote("\"hello\'\"");
+    fails += test_quote("\"hello\'");
+    fails += test_quote("\'hello\'");
+    fails += test_quote("\'hello\"");
+    fails += test_quote("\"\"hello\"");
+    fails += test_quote("\'\'hello\'\'");
+    fails += test_quote("\'\"hello\"\'");
+    fails += test_quote("\"\"hello\"\"");
+    fails += test_quote("\"\"\"hello\"\"\"");
+
+    /* ======================================================== */
+    /* EXPECTED FAILURES ONLY BELOW HERE                           */
+    /* ======================================================== */
+    /* EXPECTED FAILURES:
+     *
+     * Notes:
+     *
+     *   1. For these tests have the return value increment 'expfails'.
+     *   2. Test with both 'make vsl-regress' and 'make regress' because
+     *        some other tests use this function in unpredictable ways.
+     *   3. After a test is fixed, change the return value to increment
+     *        'fails', move it to the EXPECTED PASS group above, and add
+     *        some info about it as necessary to help those who may be
+     *        forced to revisit this.
+     *
+     */
+
+    printf("\nExpected failures (don't use in production code):\n");
+
+    printf("  NONE AT THIS TIME\n");
+
+    /* report results */
+    fprintf(stderr, "%d", expfails);
 
     printf("%s: testing complete\n", av[0]);
+
+    if (fails != 0) {
+      /* as long as fails is < 127 the STATUS will be the number of unexpected failures */
+      return fails;
+    }
+
     return 0;
 }
 
