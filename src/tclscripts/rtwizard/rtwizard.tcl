cae43d05193db7161db28c2e2bfcc7f935e16cb8
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2012-04-20 02:23:16 +0000

    Start teaching rtwizard to take inputs from the command line (will provide a way to avoid the need for graphically picking components, and eventually will allow for complete non-graphical rtwizard image generation - useful for regression testing.)  At this stage, should just preserve old behavior.
    svn:revision:50189
    svn:branch:trunk
    svn:account:starseeker

--- src/tclscripts/rtwizard/rtwizard.tcl
+++ src/tclscripts/rtwizard/rtwizard.tcl
@@ -20,28 +20,30 @@
 #
 ###
 #
-# rtwizard wrapper script to make things seem normal
+# Start-up script for both the command line and graphical modes of
+# rtwizard
 #
+
 # The trailing backslash forces tcl to skip the next line \
 STARTUP_HOME=`dirname $0`/..
 #\
 export STARTUP_HOME
-# restart using bwish \
-WISH="bwish"
+# restart using btclsh \
+TCLSH="btclsh"
 #\
-for ish in bwish bwish_d ; do
+for clsh in btclsh btclsh_d ; do
 # see if we're installed \
-    if test -f ${STARTUP_HOME}/bin/$ish ; then
+    if test -f ${STARTUP_HOME}/bin/$clsh ; then
 #\
-	WISH="${STARTUP_HOME}/bin/$ish"
+	TCLSH="${STARTUP_HOME}/bin/$clsh"
 #\
 	break;
 #\
     fi
 # see if we're not installed yet \
-    if test -f ${STARTUP_HOME}/bwish/$ish ; then
+    if test -f ${STARTUP_HOME}/btclsh/$clsh ; then
 #\
-	WISH="${STARTUP_HOME}/bwish/$ish"
+	TCLSH="${STARTUP_HOME}/btclsh/$clsh"
 #\
 	break;
 #\
@@ -49,20 +51,154 @@ for ish in bwish bwish_d ; do
 #\
 done
 #\
-exec "$WISH" "$0" "$@"
+exec "$TCLSH" "$0" "$@"
 
 #
 # Begin Tcl here!
 #
-foreach i $auto_path {
-    set wizpath [ file join $i {..} tclscripts rtwizard RaytraceWizard.tcl ]
-    if { [file exists $wizpath] } {
-	source $wizpath
-	exit
-    }
+
+source [file join [bu_brlcad_data "tclscripts"] util getopt.tcl]
+getopt::init {
+        {verbose v {::verbose}}
+        {gui gui  {::use_gui}}
+        {no-gui no-gui  {::disable_gui}}
+        {g-file   i  {::have_gfile ::g_file}}
+        {output   o  {::output ::output_filename}}
+        {full-color-objects  c  {::have_full_color_objs ::color_objlist ...}}
+        {background-color background-color {::have_fc_bg_color ::fc_bg_color}}
+        {ghost-objects  g  {::have_ghost_objs ::ghost_objlist ...}}
+        {ghosting-intensity ghosting-intensity {::have_ghosting_intensity ::ghosting_intensity}}
+        {edge-objects  e  {::have_edge_objs ::edge_objlist ...}}
+        {line-color line-color {::have_line_color ::line_color}}
+        {non-line-color non-line-color {::have_non_line_color ::non_line_color}}
+        {occlusion occlusion {::have_occlusion_setting ::occlusion_setting}}
+        {type t {::have_picture_type ::picture_type}}
+        {matrix M {::have_matrix ::matrix}}
+        {azimuth a  {::have_azimuth ::azimuth}}
+        {elevation e {::have_elevation ::elevation}}
+        {cpus P  {::have_cpu_cnt ::cpus_use}}
+}
+set argv2 [getopt::getopt $argv]
+
+# During development, force default behavior (comment the following out to work on new code)
+set ::use_gui 1
+
+# If we have both gui and no-gui specified, use gui
+if {[info exists ::use_gui] && [info exists ::disable_gui]} {
+   puts "Warning - both -gui and -no-gui supplied - enabling gui"
+   unset ::disable_gui
 }
 
-puts "rtwizard not found in any of $auto_path"
+# If we have an explicit picture type, check whether we satisfy the minimum
+# data input for that type.
+if {[info exists ::have_picture_type] && ![info exists ::use_gui]} {
+  switch $::picture_type {
+    A   -
+    1	{
+          if {![info exists ::color_objlist]} {
+             if ([info exists ::disable_gui]) {
+               puts "Error - picture type $::picture_type specified, but no full color objects listed"
+               puts "Please specify full color objects using the -c option\n"
+               exit
+             } else {
+               set ::use_gui 1
+             }
+          }
+        }
+    B   -
+    2	{
+          if {![info exists ::edge_objlist]} {
+             if ([info exists ::disable_gui]) {
+               puts "Error - picture type $::picture_type specified, but no edge objects listed"
+               puts "Please specify edge objects using the -e option\n"
+               exit
+             } else {
+               set ::use_gui 1
+             }
+          }
+        }
+    C   -
+    D   -
+    3	-
+    4	{
+          if {![info exists ::color_objlist] || ![info exists ::edge_objlist]} {
+            if (![info exists ::disable_gui]) {
+               set ::use_gui 1
+            } else {
+              if {![info exists ::edge_objlist]} {
+                puts "Error - picture type $::picture_type specified, but no edge objects listed"
+                puts "Please specify edge objects using the -e option\n"
+              }
+              if {![info exists ::color_objlist]} {
+                puts "Error - picture type $::picture_type specified, but no color objects listed"
+                puts "Please specify full color objects using the -c option\n"
+              }
+             exit
+            }
+          }
+        }
+    E   -
+    5	{
+          if {![info exists ::color_objlist] || ![info exists ::ghost_objlist]} {
+            if (![info exists ::disable_gui]) {
+               set ::use_gui 1
+            } else {
+              if {![info exists ::ghost_objlist]} {
+                puts "Error - picture type $::picture_type specified, but no ghost objects listed"
+                puts "Please specify ghost objects using the -g option\n"
+              }
+              if {![info exists ::color_objlist]} {
+                puts "Error - picture type $::picture_type specified, but no color objects listed"
+                puts "Please specify full color objects using the -c option\n"
+              }
+             exit
+            }
+          }
+        }
+    F   -
+    6	{
+          if {![info exists ::color_objlist] || ![info exists ::edge_objlist] || ![info exists ::ghost_objlist]} {
+            if (![info exists ::disable_gui]) {
+               set ::use_gui 1
+            } else {
+              if {![info exists ::ghost_objlist]} {
+                puts "Error - picture type $::picture_type specified, but no ghost objects listed"
+                puts "Please specify ghost objects using the -g option\n"
+              }
+              if {![info exists ::color_objlist]} {
+                puts "Error - picture type $::picture_type specified, but no color objects listed"
+                puts "Please specify full color objects using the -c option\n"
+              }
+              if {![info exists ::edge_objlist]} {
+                puts "Error - picture type $::picture_type specified, but no edge objects listed"
+                puts "Please specify edge objects using the -e option\n"
+              }
+             exit
+            }
+          }
+        }
+    default {puts "Error - unknown picture type $::picture_type\n"; exit}
+  }
+}
+
+# If we're launching without enough arguments to fully specify an rtwizard 
+# run, a gui run has been specifically requested, or we've got arguments 
+# that aren't understood, go graphical
+if {$argc == 0 || "$argv2" != "" || [info exists ::use_gui]} {
+   # Have to do these loads until we get "package require tclcad" and "package require dm" 
+   # working - bwish loads them for us by default, but since rtwizard may be either
+   # graphical or command line we need to start with btclsh
+   load [file join [bu_brlcad_root "lib"] libtclcad[info sharedlibextension]]
+   load [file join [bu_brlcad_root "lib"] libdm[info sharedlibextension]]
+   # Now, load RaytraceWizard.tcl
+   source [file join [bu_brlcad_data "tclscripts"] rtwizard RaytraceWizard.tcl]
+   exit
+} else {
+
+puts "rtwizard arguments: $argv"
+if {[info exists ::verbose]} {puts "rtwizard verbose ON"}
+
+}
 
 # Local Variables:
 # mode: sh
---
+++ src/tclscripts/util/getopt.tcl
@@ -0,0 +1,167 @@
+# Copyright (c) 2012 United States Government as represented by
+# the U.S. Army Research Laboratory.
+# Copyright (c) 2008, Federico Ferri
+#
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of Federico Ferri nor the
+#       names of its contributors may be used to endorse or promote products
+#       derived from this software without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL FEDERICO FERRI BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# Apr 19 2012 - Enhancing original getopt functionality to support options
+# taking a variable number of arguments - CY
+
+# Example:
+#
+# getopt::init {
+#         {verbose  v  {::verbose}}
+#         {output   o  {::output ::output_filename}}
+#         {objects  l  {::have_objs ::objlist ...}}
+# }
+# set argv2 [getopt::getopt $argv]
+
+namespace eval getopt {
+        # list of option vars (keys are long option names)
+        variable optlist
+
+        # map short option names to long option names
+        variable stl_map
+}
+
+proc getopt::init {optdata} {
+        variable optlist
+        variable stl_map
+        array set optlist {}
+        array set stl_map {}
+        foreach item $optdata {
+                foreach {longname shortname varlist} $item {
+                        set optlist($longname) $varlist
+                        set stl_map($shortname) $longname
+                }
+        }
+}
+
+proc getopt::expandOptNames {argv} {
+        variable optlist
+        variable stl_map
+        set argv2 {}
+        set argc [llength $argv]
+        for {set i 0} {$i < $argc} {} {
+                set argv_i [lindex $argv $i]
+                incr i
+
+                if [isShortOpt $argv_i] {
+                        set argv_i_opts [split [regsub {^-} $argv_i {}] {}]
+                        foreach shortOpt $argv_i_opts {
+                                if [info exists stl_map($shortOpt)] {
+                                        set longOpt $stl_map($shortOpt)
+                                        lappend argv2 --$longOpt
+					set var2 [lindex $optlist($longOpt) 2]
+				        if {![regexp {^\.\.\.$} $var2]} {
+                                        set n_required_opt_args [expr {-1+[llength $optlist($longOpt)]}]
+                                        while {$n_required_opt_args > 0} {
+                                                incr n_required_opt_args -1
+                                                if {$i >= $argc} {
+                                                        puts "error: not enough arguments for option -$shortOpt"
+                                                        exit 3
+                                                }
+                                                lappend argv2 [lindex $argv $i]
+                                                incr i
+                                        }
+					} else {
+                                          set curr_argv [lindex $argv $i]
+				          while {![isShortOpt $curr_argv] && ![isLongOpt $curr_argv] && $i < $argc} {
+                                                lappend argv2 [lindex $argv $i]
+				                incr i
+                                                set curr_argv [lindex $argv $i]
+                                          }
+                                        }
+                                } else {
+                                        puts "error: unknown option: -$shortOpt"
+                                        exit 2
+                                }
+                        }
+                        continue
+                }
+
+                lappend argv2 $argv_i
+        }
+        return $argv2
+}
+
+proc getopt::isShortOpt {o} {
+        return [regexp {^-[a-zA-Z0-9]+} $o]
+}
+
+proc getopt::isLongOpt {o} {
+        return [regexp {^--[a-zA-Z0-9][a-zA-Z0-9]*} $o]
+}
+
+proc getopt::getopt {argv} {
+        variable optlist
+        set argv [expandOptNames $argv]
+        set argc [llength $argv]
+
+        set residualArgs {}
+
+        for {set i 0} {$i < $argc} {} {
+                set argv_i [lindex $argv $i]
+                incr i
+
+                if [isLongOpt $argv_i] {
+                        set optName [regsub {^--} $argv_i {}]
+                        if [info exists optlist($optName)] {
+                                set varlist $optlist($optName)
+                                uplevel [list set [lindex $optlist($optName) 0] 1]
+				set var2 [lindex $optlist($optName) 2]
+				if {![regexp {^\.\.\.$} $var2]} {
+                                set n_required_opt_args [expr {-1+[llength $varlist]}]
+                                set j 1
+                                while {$n_required_opt_args > 0} {
+                                        incr n_required_opt_args -1
+                                        if {$i >= $argc} {
+                                                puts "error: not enough arguments for option --$optName"
+                                                exit 5
+                                        }
+                                        uplevel [list set [lindex $varlist $j] [lindex $argv $i]]
+                                        incr j
+                                        incr i
+                                }
+				} else {
+			            set curr_argv [lindex $argv $i]
+				    set curr_container [lindex $optlist($optName) 1]
+				    while {![isShortOpt $curr_argv] && ![isLongOpt $curr_argv] && $i < $argc} {
+                                         lappend $curr_container [lindex $argv $i]
+				         incr i
+                                         set curr_argv [lindex $argv $i]
+                                    }
+				}
+                        } else {
+                                puts "error: unknown option: --$optName"
+                                exit 4
+                        }
+                        continue
+                }
+
+                lappend residualArgs $argv_i
+        }
+        return $residualArgs
+}
