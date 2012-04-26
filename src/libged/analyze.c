c5a65644a284e34fffb3820ca51c49d5dd26c272
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2012-04-26 18:07:17 +0000

    use C commenting
    svn:revision:50325
    svn:branch:STABLE
    svn:account:starseeker

--- src/libged/analyze.c
+++ src/libged/analyze.c
@@ -979,7 +979,7 @@ analyze_tor(struct ged *gedp, const struct rt_db_internal *ip)
                        );
 
 #if 0
-    OR Vol = %.8f (%.8f gal) Surface Area = %.8f\n",
+    /*OR Vol = %.8f (%.8f gal) Surface Area = %.8f\n",*/
 
     bu_vls_printf(gedp->ged_result_str, "TOR Vol = %.8f (%.8f gal) Surface Area = %.8f\n",
 		  vol*gedp->ged_wdbp->dbip->dbi_base2local*gedp->ged_wdbp->dbip->dbi_base2local*gedp->ged_wdbp->dbip->dbi_base2local,
