d911f5670dc2b12d9646e44a44fb5858dba296f8
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2012-05-03 20:00:42 +0000

    Switch to STDCXX_LINKLIB variable
    svn:revision:50430
    svn:branch:trunk
    svn:account:starseeker

---
+++ misc/CMake/FindSTL.cmake
@@ -0,0 +1,84 @@
+#                 F I N D S T L . C M A K E
+# BRL-CAD
+#
+# Copyright (c) 2011-2012 United States Government as represented by
+# the U.S. Army Research Laboratory.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above
+# copyright notice, this list of conditions and the following
+# disclaimer in the documentation and/or other materials provided
+# with the distribution.
+#
+# 3. The name of the author may not be used to endorse or promote
+# products derived from this software without specific prior written
+# permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS
+# OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+# GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+###
+# In some situations, it is necessary to explicitly link the C++ STL
+# library.  Traditional find_library searches won't work in all cases,
+# so an alternative approach is required.  By default stdc++ is searched
+# for, but this can be changed by setting the STDCXX_LINKLIB variable.
+# If already set, this routine will test the contents of STDCXX_LINKLIB.
+# STDCXX_LINKLIB will be set to the empty string if the test fails.
+#
+#  STDCXX_LINKLIB - Contains linker args for using STL library.
+#
+#=============================================================================
+
+if(NOT STDCXX_LINKLIB)
+  set(STDCXX_LINKLIB "-lstdc++")
+endif(NOT STDCXX_LINKLIB)
+
+set(CMAKE_REQUIRED_LIBRARIES_BAK "${CMAKE_REQUIRED_LIBRARIES}")
+set(CMAKE_REQUIRED_LIBRARIES "${STDCXX_LINKLIB}")
+
+set(stl_test_src "
+int
+main()
+{
+  int i;
+  i = 1;
+  return 0;
+}
+")
+CHECK_C_SOURCE_RUNS("${stl_test_src}" STL_LIB_TEST)
+
+SET(CMAKE_REQUIRED_LIBRARIES "${CMAKE_REQUIRED_LIBRARIES_BAK}")
+
+if("${STL_LIB_TEST}" EQUAL 1)
+  set(STDCXX_LINKLIB "${STDCXX_LINKLIB}" CACHE STRING "STL found" FORCE)
+else("${STL_LIB_TEST}" EQUAL 1)
+  set(STDCXX_LINKLIB "" CACHE STRING "STL not found" FORCE)
+endif("${STL_LIB_TEST}" EQUAL 1)
+mark_as_advanced(STDCXX_LINKLIB)
+
+# handle the QUIETLY and REQUIRED arguments 
+include(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(STL DEFAULT_MSG STDCXX_LINKLIB)
+
+
+# Local Variables:
+# tab-width: 8
+# mode: cmake
+# indent-tabs-mode: t
+# End:
+# ex: shiftwidth=2 tabstop=8
