d80690a8400ec5ac67d17202bfd415b95cfac0f3
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2012-05-03 17:41:23 +0000

    Try to make the stdc++ usage a matter of detection instead of hardcoding.  Untested on systems which would show the issue.
    svn:revision:50426
    svn:branch:trunk
    svn:account:starseeker

--- src/other/step/cmake/SCL_Utils.cmake
+++ src/other/step/cmake/SCL_Utils.cmake
@@ -12,7 +12,7 @@ ENDMACRO()
 
 MACRO(SCL_ADDEXEC execname srcslist libslist)
   add_executable(${execname} ${srcslist})
-  target_link_libraries(${execname} ${libslist} -lstdc++)
+  target_link_libraries(${execname} ${libslist} ${STDCXX_LINKLIB})
   INSTALL(TARGETS ${execname} DESTINATION bin)
   # Enable extra compiler flags if local executables and/or global options dictate
   SET(LOCAL_COMPILE_FLAGS "")
