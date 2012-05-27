618bd7324d284f1b232b046ee4b337d00d06663f
Thomas Browder <tom.browder@gmail.com>
2012-05-27 19:23:59 +0000

    split function bu_vls_vprintf into its own src file, put common vls data into a new internal header, modify build structure accordingly
    svn:revision:50715
    svn:branch:trunk
    svn:account:tbrowder2

---
+++ src/libbu/vls_internals.h
@@ -0,0 +1,39 @@
+/*                  V L S _ I N T E R N A L S . H
+ * BRL-CAD
+ *
+ * Copyright (c) 1998-2012 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+
+#ifndef BU_VLS_INTERNALS_H
+#define BU_VLS_INTERNALS_H seen
+
+/* private constants */
+
+/* minimum vls allocation increment size */
+static const size_t _VLS_ALLOC_STEP = 128;
+
+#endif /* BU_VLS_INTERNALS_H */
+
+/*
+ * Local Variables:
+ * mode: C
+ * tab-width: 8
+ * indent-tabs-mode: t
+ * c-file-style: "stroustrup"
+ * End:
+ * ex: shiftwidth=4 tabstop=8
+ */
