78c40e316a34fc465c4072d47201321a2468b798
Anurag Murty <anuragmurty@gmail.com>
2012-06-01 21:30:07 +0000

    This program prints out the voxels which are in(1) and which are not(0) for each ray
    svn:revision:50818
    svn:branch:trunk
    svn:account:anrgmrty

--- src/conv/g-voxel.c
+++ src/conv/g-voxel.c
@@ -41,104 +41,124 @@
  *
  * The 'segs' segment list is unused in this example.
  */
-int
-hit(struct application *ap, struct partition *PartHeadp, struct seg *UNUSED(segs))
+
+
+int 
+hit(struct application *ap, struct partition *PartHeadp, struct seg*UNUSED(segs))
 {
-    /* iterating over partitions, this will keep track of the current
-     * partition we're working on.
-     */
+    int xMin, low=-1, voxelNumIn, voxelNumOut, presentVoxel=-1, i, numVoxelX;
     struct partition *pp;
+    struct hit *hitOutp, *hitInp;
+    struct rt_i *rtip;
+    float hitDistIn, hitDistOut, sizeVoxelX, inDistance = 0.0, threshold = 0.5;
 
-    /* will serve as a pointer for the entry and exit hitpoints */
-    struct hit *hitp;
-
-    /* will serve as a pointer to the solid primitive we hit */
-    struct soltab *stp;
-
-    /* will contain surface curvature information at the entry */
-    struct curvature cur = RT_CURVATURE_INIT_ZERO;
-
-    /* will contain our hit point coordinate */
-    point_t pt;
-
-    /* will contain normal vector where ray enters geometry */
-     vect_t inormal;
-
-    /* will contain normal vector where ray exits geometry */
-    vect_t onormal;
-
-    /* iterate over each partition until we get back to the head.
-     * each partition corresponds to a specific homogeneous region of
-     * material.
+    xMin = ap->a_rt_i->mdl_max[0];
+    low = 0;
+    /**
+     * length of voxels in the X-direction is sizeVoxelX, rtip is structure 
+     * for raytracing
+     */      
+    sizeVoxelX = * (float*) ap->a_uptr;
+    rtip = ap->a_rt_i;
+    pp = PartHeadp->pt_forw;	
+    
+    /** 
+     * The following loop prints the voxels is present in path of ray (1-present)
      */
-    for (pp=PartHeadp->pt_forw; pp != PartHeadp; pp = pp->pt_forw) {
+    while(pp != PartHeadp) {
 
-	/* print the name of the region we hit as well as the name of
-	 * the primitives encountered on entry and exit.
+	/** 
+  	 * hitInp, hitOutp are hit structures to save distances where ray entered 
+	 * and exited the present partition.
+	 * hitDistIn, hitDistOut are the respective distances from the origin of ray.
+ 	 * voxelNumIn, voxelNumOut are the voxel numbers where ray enterd and exited
+	 * the present partition.
+	 * presentVoxel is the voxel index from which the ray came out of in the last
+	 * partition 	
 	 */
-/*	bu_log("\n--- Hit region %s (in %s, out %s)\n",
-	       pp->pt_regionp->reg_name,
-	       pp->pt_inseg->seg_stp->st_name,
-	       pp->pt_outseg->seg_stp->st_name );
-*/
-	/* entry hit point, so we type less */
-	hitp = pp->pt_inhit;
+	hitInp = pp->pt_inhit;
+	hitOutp = pp->pt_outhit;
+	
+        hitDistIn = hitInp->hit_dist;
+	hitDistOut = hitOutp->hit_dist;
 
-	/* construct the actual (entry) hit-point from the ray and the
-	 * distance to the intersection point (i.e., the 't' value).
-	 */
-	VJOIN1(pt, ap->a_ray.r_pt, hitp->hit_dist, ap->a_ray.r_dir);
+	voxelNumIn = (int) (hitDistIn - 1.0) / sizeVoxelX;
+	voxelNumOut = (int) (hitDistOut - 1.0) / sizeVoxelX;
+	
+	/**
+ 	* If ray does not enter next partition (ie the voxel index from which ray exited)
+ 	* in the same voxel index, this means that last voxel of previous partition can be
+ 	* evaluated. Also, the intermediate voxels are NOT filled. 
+ 	*/
+	if(presentVoxel != voxelNumIn) {
 
-	/* primitive we encountered on entry */
-	stp = pp->pt_inseg->seg_stp;
+	    if(inDistance / sizeVoxelX >= threshold) {
+		printf("1");
+	    } else {
+		if(presentVoxel!=-1) {
+		    printf("0");
+		}    
+	    }
 
-	/* compute the normal vector at the entry point, flipping the
-	 * normal if necessary.
-	 */
-	RT_HIT_NORMAL(inormal, hitp, stp, &(ap->a_ray), pp->pt_inflip);
+	    for(i = 0; i < voxelNumIn - presentVoxel - 1 ; i++) {
+		printf("0");
+	    }
+			
+	    presentVoxel = voxelNumIn ;
+	    inDistance = 0.0;
 
-	/* print the entry hit point info */
-	rt_pr_hit("  In", hitp);
-	VPRINT(   "  Ipoint", pt);
-	VPRINT(   "  Inormal", inormal);
+	}
 
-	/* This next macro fills in the curvature information which
-	 * consists on a principle direction vector, and the inverse
-	 * radii of curvature along that direction and perpendicular
-	 * to it.  Positive curvature bends toward the outward
-	 * pointing normal.
-	 */
-	RT_CURVATURE(&cur, hitp, pp->pt_inflip, stp);
+	/**
+ 	* If voxel entered and voxel exited are same then nothing can be evaluated till
+ 	* we see the next partition too. If not, evaluate entry voxel. Also, all the 
+ 	* intermediate voxels are in. 
+ 	* inDistance is given the distance covered before 
+ 	* exiting the last voxel of the present partition. 
+ 	*/
 
-	/* print the entry curvature information */
-	VPRINT("PDir", cur.crv_pdir);
-	bu_log(" c1=%g\n", cur.crv_c1);
-	bu_log(" c2=%g\n", cur.crv_c2);
+	if(voxelNumIn == voxelNumOut) {
+	    inDistance += hitDistOut - hitDistIn ;
+	} else {
+	    inDistance += (voxelNumIn + 1) * sizeVoxelX - hitDistOut ;
 
-	/* exit point, so we type less */
-	hitp = pp->pt_outhit;
+	    if(inDistance / sizeVoxelX >= threshold) {
+		printf("1");
+	    } else {
+		printf("0");
+	    }
+		
+	    for(i = 0; i < voxelNumOut - voxelNumIn -1; i++) {
+		printf("1");
+	    }
 
-	/* construct the actual (exit) hit-point from the ray and the
-	 * distance to the intersection point (i.e., the 't' value).
-	 */
-	VJOIN1(pt, ap->a_ray.r_pt, hitp->hit_dist, ap->a_ray.r_dir);
+	    presentVoxel = voxelNumOut;
+	    inDistance = hitDistOut - voxelNumOut * sizeVoxelX ;
+	}
+  		
 
-	/* primitive we exited from */
-	stp = pp->pt_outseg->seg_stp;
-
-	/* compute the normal vector at the exit point, flipping the
-	 * normal if necessary.
-	 */
-	RT_HIT_NORMAL(onormal, hitp, stp, &(ap->a_ray), pp->pt_outflip);
-
-	/* print the exit hit point info */
-	rt_pr_hit("  Out", hitp);
-	VPRINT(   "  Opoint", pt);
-	VPRINT(   "  Onormal", onormal);
+	pp = pp->pt_forw;
     }
 
-    return 1;
+    if(inDistance >= threshold) {
+	printf("1");
+    } else {
+	printf("0");
+    }
+    
+   /**
+    * voxels after the last partition are not in
+    */
+    numVoxelX = (int)(((rtip->mdl_max)[0] - (rtip->mdl_min)[0])/sizeVoxelX) + 1  ;
+    for( i = 0; i < numVoxelX - presentVoxel; i++) {
+	printf("0");
+    }
+
+    printf("\n");
+    return 0;		 	
+
 }
+
 
 
 /**
@@ -159,8 +179,10 @@ main(int argc, char **argv)
 {
     struct application	ap;
     static struct rt_i *rtip;
+
     char title[1024] = {0};
-    int i, j, numVoxelY, numVoxelZ , yMin, zMin;
+    int i, j, numVoxelX,  numVoxelY, numVoxelZ, xMin,  yMin, zMin;
+    float sizeVoxelX,  sizeVoxelY, sizeVoxelZ;
 
     /* Check for command-line arguments.  Make sure we have at least a
      * geometry file and one geometry object on the command line.
@@ -213,36 +235,46 @@ main(int argc, char **argv)
     VPRINT("\nmax of bounding rectangular parallelopiped --Pnt\n",rtip->mdl_max);
 
    
-    printf("\nx value of maximum on the highest corner of parallelopiped is: %f",(rtip->mdl_max)[0]);
+/*    printf("\nx value of maximum on the highest corner of parallelopiped is: %f",(rtip->mdl_max)[0]);
     printf("\ny value of maximum on the highest corner of parallelopiped is: %f",(rtip->mdl_max)[1]);
     printf("\nz value of maximum on the highest corner of parallelopiped is: %f",(rtip->mdl_max)[2]);
     printf("\nx value of minimum on the lowest corner of parallelopiped is: %f",(rtip->mdl_min)[0]);
     printf("\ny value of minimum on the lowest corner of parallelopiped is: %f",(rtip->mdl_min)[1]);
     printf("\nz value of minimum on the lowest corner of parallelopiped is: %f",(rtip->mdl_min)[2]);
+*/
 
-
-    /*assume voxels are unit size in each dimension*/
-    numVoxelY = (int)((rtip->mdl_max)[1] - (rtip->mdl_min)[1]) ;
-    numVoxelZ = (int)(rtip->mdl_max)[2] - (rtip->mdl_min)[2] ;
     
-    yMin = (int)((rtip->mdl_min)[1]);
-    zMin = (int)((rtip->mdl_min)[2]);
+    
+    sizeVoxelX = 1.0;
+    sizeVoxelY = 1.0;
+    sizeVoxelZ = 1.0;
+    
+
+    /*assume voxels are sizeVoxelX, sizeVoxelY, sizeVoxelZ size in each dimension*/
+    numVoxelX = (int)(((rtip->mdl_max)[0] - (rtip->mdl_min)[0])/sizeVoxelX) + 1  ;
+    numVoxelY = (int)(((rtip->mdl_max)[1] - (rtip->mdl_min)[1])/sizeVoxelY) + 1  ;
+    numVoxelZ = (int)(((rtip->mdl_max)[2] - (rtip->mdl_min)[2])/sizeVoxelZ) + 1  ;
+    
+    xMin = (int)((rtip->mdl_min)[0]) ;
+    yMin = (int)((rtip->mdl_min)[1]) ;
+    zMin = (int)((rtip->mdl_min)[2]) ;
 
     for(i = 0; i < numVoxelZ; i++) {
 	for( j = 0; j < numVoxelY; j++) { 
-    	    printf("Ray number is %d\n",numVoxelY*i + j);
+    	    printf("Ray number is %d\n", numVoxelY * i + j);
 	    RT_APPLICATION_INIT(&ap);
 	    ap.a_rt_i = rtip;
 	    ap.a_onehit = 0;
             
 	    /*ray is hit through midpoint of the unit sized voxels*/
-	    VSET(ap.a_ray.r_pt, (rtip->mdl_max)[0] + 1.0 , yMin + j + 0.5 , zMin + i + 0.5);
-	    VSET(ap.a_ray.r_dir, -1.0, 0.0, 0.0);
+	    VSET(ap.a_ray.r_pt, (rtip->mdl_min)[0] - 1.0, yMin + (j + 0.5) * sizeVoxelY, zMin + (i + 0.5) * sizeVoxelZ);
+	    VSET(ap.a_ray.r_dir, 1.0, 0.0, 0.0);
 	    
 	    ap.a_hit = hit;
 	    ap.a_miss = miss;
-	    
-	    (void)rt_shootray(&ap);
+	    ap.a_uptr = &sizeVoxelX;                
+    
+	    rt_shootray(&ap);    
 	}
     }
 
