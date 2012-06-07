a4a5a05da848dc0b2eb87042e2755d200fc83969
Richard Weiss <r_weiss@brlcad.org>
2012-06-07 17:16:01 +0000

    Update to file 'nmg_fuse.c' function 'v_ptr_comp' which is used by qsort within function 'nmg_model_edge_fuse'. This change was to correct a potential bug.
    svn:revision:50990
    svn:branch:trunk
    svn:account:r_weiss

--- src/librt/primitives/nmg/nmg_fuse.c
+++ src/librt/primitives/nmg/nmg_fuse.c
@@ -1032,9 +1032,10 @@ v_ptr_comp(const void *p1, const void *p2)
     i = ((size_t *)p1)[1];
     j = ((size_t *)p2)[1];
 
-	if (i >= j) {
+    if (i == j)
+	return 0;
+    else if (i > j)
 	return 1;
-    }
     return -1;
 }
 
