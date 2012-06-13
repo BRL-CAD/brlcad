66e121151d7c1183035093fb6cd949c7793e2287
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2012-06-13 19:33:33 +0000

    Only pull image from framebuffer if we have to - seems to speed up rtimage script in simple color-only cases.
    svn:revision:51100
    svn:branch:trunk
    svn:account:starseeker

--- src/tclscripts/lib/RtImage.tcl
+++ src/tclscripts/lib/RtImage.tcl
@@ -155,12 +155,13 @@ proc rtimage {_dbfile
 	catch {exec [file join $binpath fbclear] -F $_port [lindex $_bgcolor 0] [lindex $_bgcolor 1] [lindex $_bgcolor 2]}
     }
 
-    # Pull the image from the framebuffer
-    catch {exec [file join $binpath fb-pix] -w $_w -n $_n -F $_port $tfci}
-
     set occlude_objects [lsort -unique [concat $_color_objects $_ghost_objects]]
 
     if {[llength $_ghost_objects]} {
+
+        # Pull the image from the framebuffer
+        catch {exec [file join $binpath fb-pix] -w $_w -n $_n -F $_port $tfci}
+
 	set have_ghost_objects 1
 	set cmd [list [file join $binpath rt] -w $_w -n $_n \
 		     -o $tgi \
