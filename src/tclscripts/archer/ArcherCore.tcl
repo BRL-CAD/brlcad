26a90cdd2325974044478a7dd0b8691356e6d251
Robert Parker <unacknowledged@users.noreply.github.com>
2012-06-22 15:55:17 +0000

    Added ArcherCore::isdouble. This recognizes "." as a double.
    svn:revision:51284
    svn:branch:trunk
    svn:account:bob1961

--- src/tclscripts/archer/ArcherCore.tcl
+++ src/tclscripts/archer/ArcherCore.tcl
@@ -561,6 +561,7 @@ namespace eval ArcherCore {
 	variable mLastTags ""
 
 	method handleMoreArgs {args}
+	method isdouble {_str}
 
 	method gedWrapper {_cmd _eflag _hflag _sflag _tflag args}
 
@@ -1211,6 +1212,14 @@ namespace eval ArcherCore {
 ::itcl::body ArcherCore::handleMoreArgs {args} {
     eval $itk_component(cmd) print_more_args_prompt $args
     return [$itk_component(cmd) get_more_args]
+}
+
+::itcl::body ArcherCore::isdouble {_str} {
+    if {[string is double $_str] || $_str == "."} {
+	return 1
+    }
+
+    return 0
 }
 
 ::itcl::body ArcherCore::gedWrapper {cmd eflag hflag sflag tflag args} {
