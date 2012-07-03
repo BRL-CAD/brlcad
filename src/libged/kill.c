7e4743b9736d044af41b06cea73a129acd4af48c
Robert Parker <unacknowledged@users.noreply.github.com>
2012-07-03 17:58:36 +0000

    Putting back the code to initialize the result string.
    svn:revision:51411
    svn:branch:trunk
    svn:account:bob1961

--- src/libged/kill.c
+++ src/libged/kill.c
@@ -50,6 +50,9 @@ ged_kill(struct ged *gedp, int argc, const char *argv[])
     GED_CHECK_READ_ONLY(gedp, GED_ERROR);
     GED_CHECK_ARGC_GT_0(gedp, argc, GED_ERROR);
 
+    /* initialize result */
+    bu_vls_trunc(gedp->ged_result_str, 0);
+
     /* must be wanting help */
     if (argc == 1) {
 	bu_vls_printf(gedp->ged_result_str, "Usage: %s %s", argv[0], usage);
