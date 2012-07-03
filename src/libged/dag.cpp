ae62a260a3e0a788af633147231f453266e3b076
Cristina Precup <cprecup@cisco.com>
2012-07-03 00:48:45 +0000

    Added the function dag_comb that processes a comb node by linking it with the nodes of its subtree. The nodes are positioned linearly, by the order that they are read from the directory - this needs to be redone in a cleverer way
    svn:revision:51402
    svn:branch:trunk
    svn:account:cprecup

--- src/libged/dag.cpp
+++ src/libged/dag.cpp
@@ -32,6 +32,7 @@
 
 #define BRLCAD_PARALLEL PARALLEL
 #undef PARALLEL
+#define POSITION_COORDINATE 10.0
 
 /* Adaptagrams Header */
 #include "libavoid/libavoid.h"
@@ -39,8 +40,21 @@ using namespace Avoid;
 
 /* Public Header */
 #include "ged.h"
+#include "raytrace.h"
 
 
+struct output {
+    FILE *outfp;
+    struct bu_ptbl groups;
+    struct bu_ptbl regions;
+    struct bu_ptbl combinations;
+    struct bu_ptbl primitives;
+};
+
+
+/**
+ * This structure has fields related to the representation of a graph with the help of the Adaptagrams library.
+ */
 struct _ged_dag_data {
     Avoid::Router *router;
     Avoid::ConnRef *connRef;
@@ -69,17 +83,126 @@ conn_callback(void *ptr)
 /**
  * Add one object to the router of the 'dag' structure.
  */
-int
+static Avoid::ShapeRef*
 add_object(struct _ged_dag_data *dag)
 {
     dag->object_nr++;
-    Avoid::Point srcPt(1.2+dag->object_nr, 0.5);
-    Avoid::Point dstPt(1.5+dag->object_nr, 0.5);
-    Avoid::ConnRef *connRef = new Avoid::ConnRef(dag->router, srcPt, dstPt);
-    connRef->setCallback(conn_callback, connRef);
-    dag->router->processTransaction();
 
-    return GED_OK;
+    Avoid::Point srcPt(dag->object_nr * POSITION_COORDINATE + 1.0, dag->object_nr * POSITION_COORDINATE + 1.0);
+    Avoid::Point dstPt(POSITION_COORDINATE + dag->object_nr * POSITION_COORDINATE, POSITION_COORDINATE +
+        dag->object_nr * POSITION_COORDINATE);
+    Avoid::Rectangle shapeRect(srcPt, dstPt);
+    Avoid::ShapeRef *shapeRef = new Avoid::ShapeRef(dag->router, shapeRect);
+
+    return shapeRef;
+}
+
+/**
+ * Function to process a combination node.
+ */
+static void
+dag_comb(struct db_i *dbip, struct directory *dp, genptr_t out, struct _ged_dag_data *dag)
+{
+    size_t i;
+    struct rt_db_internal intern;
+    struct rt_comb_internal *comb;
+
+    struct output *o = (struct output *)out;
+
+    Avoid::ShapeRef *shapeRef1 = NULL;
+    Avoid::ShapeRef *shapeRef2 = NULL;
+    const unsigned int CENTRE = 1;
+
+    if (rt_db_get_internal(&intern, dp, dbip, (fastf_t *)NULL, &rt_uniresource) < 0) {
+    bu_log("ERROR: Database read error, skipping %s\n", dp->d_namep);
+    }
+    comb = (struct rt_comb_internal *)intern.idb_ptr;
+
+    if (comb->region_flag) {
+        if (bu_ptbl_ins_unique(&(o->regions), (long *)bu_hash((unsigned char *)dp->d_namep, strlen(dp->d_namep))) == -1) {
+            bu_log("\t\"%s\" [ color=blue shape=box3d ];\n", dp->d_namep);
+        }
+    } else {
+        if (bu_ptbl_ins_unique(&(o->groups), (long *)bu_hash((unsigned char *)dp->d_namep, strlen(dp->d_namep))) == -1) {
+            bu_log("\t\"%s\" [ color=green ];\n", dp->d_namep);
+        }
+    }
+
+    shapeRef1 = add_object(dag);
+
+    /* FIXME: this is yet-another copy of the commonly-used code that
+     * gets a list of comb members.  needs to return tabular data.
+     */
+    if (comb->tree) {
+    size_t node_count = 0;
+    size_t actual_count = 0;
+    struct bu_vls vls = BU_VLS_INIT_ZERO;
+    struct rt_tree_array *rt_tree_array = NULL;
+
+    if (db_ck_v4gift_tree(comb->tree) < 0) {
+        db_non_union_push(comb->tree, &rt_uniresource);
+        if (db_ck_v4gift_tree(comb->tree) < 0) {
+            bu_log("INTERNAL_ERROR: Cannot flatten tree of [%s] for listing", dp->d_namep);
+            return;
+        }
+    }
+
+    node_count = db_tree_nleaves(comb->tree);
+    if (node_count > 0) {
+        rt_tree_array = (struct rt_tree_array *)bu_calloc(node_count, sizeof(struct rt_tree_array), "tree list");
+        actual_count = (struct rt_tree_array *)db_flatten_tree(rt_tree_array, comb->tree, OP_UNION, 1, &rt_uniresource) - rt_tree_array;
+        BU_ASSERT_SIZE_T(actual_count, ==, node_count);
+        comb->tree = TREE_NULL;
+    } else {
+        actual_count = 0;
+        rt_tree_array = NULL;
+    }
+
+    bu_log("actual count: %d", actual_count);
+
+    for (i = 0; i < actual_count; i++) {
+        char op;
+
+        switch (rt_tree_array[i].tl_op) {
+            case OP_UNION:
+                op = '+';
+                break;
+            case OP_INTERSECT:
+                op = 'x';
+                break;
+            case OP_SUBTRACT:
+                op = '-';
+                break;
+            default:
+                op = '?';
+                break;
+        }
+
+        bu_log("\t\"%s\" -> \"%s\" [ label=\"%c\" ];\n", dp->d_namep, rt_tree_array[i].tl_tree->tr_l.tl_name, op);
+
+        /* Link this node with its subnode */
+        shapeRef2 = add_object(dag);
+
+        /* Create connection pins on shapes */
+        new Avoid::ShapeConnectionPin(shapeRef1, CENTRE, Avoid::ATTACH_POS_CENTRE, Avoid::ATTACH_POS_CENTRE);
+        new Avoid::ShapeConnectionPin(shapeRef2, CENTRE, Avoid::ATTACH_POS_CENTRE, Avoid::ATTACH_POS_CENTRE);
+
+        /* Create connector from each shape shapeRef2 to the input pin on shapeRef1 */
+        Avoid::ConnEnd dstEnd(shapeRef1, CENTRE);
+        Avoid::ConnEnd srcEnd(shapeRef2, CENTRE);
+        Avoid::ConnRef *connRef = new Avoid::ConnRef(dag->router, srcEnd, dstEnd);
+        connRef->setCallback(conn_callback, connRef);
+        dag->router->processTransaction();
+
+        db_free_tree(rt_tree_array[i].tl_tree, &rt_uniresource);
+    }
+    bu_vls_free(&vls);
+
+    if (rt_tree_array)
+        bu_free((char *)rt_tree_array, "printnode: rt_tree_array");
+    }
+
+    rt_db_free_internal(&intern);
 }
 
 
@@ -92,6 +215,10 @@ add_objects(struct ged *gedp, struct _ged_dag_data *dag)
     struct directory *dp = NULL, *ndp = NULL;
     struct bu_vls dp_name_vls = BU_VLS_INIT_ZERO;
     int i;
+    struct output o = {NULL, BU_PTBL_INIT_ZERO, BU_PTBL_INIT_ZERO, BU_PTBL_INIT_ZERO, BU_PTBL_INIT_ZERO};
+    o.outfp = NULL;
+    struct db_i *dbip = gedp->ged_wdbp->dbip;
+    Avoid::ShapeRef *shapeRef = NULL;
 
     /* Traverse the database 'gedp' */
     for (i = 0; i < RT_DBNHASH; i++) {
@@ -101,16 +228,19 @@ add_objects(struct ged *gedp, struct _ged_dag_data *dag)
             if (ndp) {
                 if(dp->d_flags & RT_DIR_SOLID) {
                     bu_log("Adding SOLID object [%s]\n", bu_vls_addr(&dp_name_vls));
-                    add_object(dag);
+                    dag->object_nr++;
+
+                    /* Create and add a shape (obstacle) to the router */
+                    Avoid::Rectangle rectangle(Avoid::Point(dag->object_nr * POSITION_COORDINATE + 1.0,
+                        dag->object_nr * POSITION_COORDINATE + 1.0), Avoid::Point(POSITION_COORDINATE +
+                        dag->object_nr * POSITION_COORDINATE, POSITION_COORDINATE + dag->object_nr *
+                        POSITION_COORDINATE));
+                    shapeRef = new Avoid::ShapeRef(dag->router, rectangle);
+                    dag->router->processTransaction();
                     bu_log("[SOLID]added %d objects.\n", dag->object_nr);
                 } else if (dp->d_flags & RT_DIR_COMB) {
                     bu_log("Adding COMB object [%s]\n", bu_vls_addr(&dp_name_vls));
-                    add_object(dag);
-                    bu_log("[COMB]added %d objects.\n", dag->object_nr);
-                } else if (dp->d_flags & RT_DIR_REGION) {
-                    bu_log("Adding REGION object [%s]\n", bu_vls_addr(&dp_name_vls));
-                    add_object(dag);
-                    bu_log("[REGION]added %d objects.\n", dag->object_nr);
+                    dag_comb(dbip, ndp, &o, dag);
                 } else {
                     bu_log("Something else: [%s]\n", bu_vls_addr(&dp_name_vls));
                 }
@@ -122,9 +252,11 @@ add_objects(struct ged *gedp, struct _ged_dag_data *dag)
 
     ged_close(gedp);
     bu_vls_free(&dp_name_vls);
+    dag->router->deleteShape(shapeRef);
 
     return GED_OK;
 }
+
 
 #define PARALLEL BRLCAD_PARALLEL
 #undef BRLCAD_PARALLEL
