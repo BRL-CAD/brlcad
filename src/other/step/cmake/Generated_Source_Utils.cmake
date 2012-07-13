2e42fcfe9c66acefb0218e78b2524f97517e123c
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2012-07-13 18:43:10 +0000

    Checkpoint some more work on generated src management.
    svn:revision:51498
    svn:branch:trunk
    svn:account:starseeker

--- src/other/step/cmake/Generated_Source_Utils.cmake
+++ src/other/step/cmake/Generated_Source_Utils.cmake
@@ -18,15 +18,38 @@ macro(FILEVAR filename var)
   string(REGEX REPLACE "[^a-zA-Z0-9]" "_" ${var} ${filename})
 endmacro(FILEVAR)
 
-macro(MD5_FILES)
-  foreach(filename ${ARGN})
+macro(VERIFY_FILES filelist warn resultvar)
+  set(${resultvar} 1)
+  foreach(fileitem ${filelist})
+    # Deal with absolute and relative paths a bit differently
+    get_filename_component(ITEM_ABS_PATH "${fileitem}" ABSOLUTE)
+    if("${fileitem}" STREQUAL "${ITEM_ABS_PATH}")
+      set(filefullname "${fileitem}")
+      get_filename_component(filename "${fileitem}" NAME)
+    else("${fileitem}" STREQUAL "${ITEM_ABS_PATH}")
+      set(filefullname "${CURRENT_SOURCE_DIR}/${fileitem}")
+      set(filename "${fileitem}")
+    endif("${fileitem}" STREQUAL "${ITEM_ABS_PATH}")
+    # Got filename components sorted - proceed
+    if(NOT EXISTS ${filefullname})
+      message(FATAL_ERROR "Attempted to verify non-existant file ${filefullname}")
+    endif(NOT EXISTS ${filefullname})
     FILEVAR(${filename} filevar)
-    MD5("${filename}" ${filevar}_md5)
-    message("${filename} md5: ${${filevar}_md5}")
-  endforeach(filename ${ARGN})
-endmacro(MD5_FILES)
+    if(NOT baseline_${filevar}_md5)
+      message(FATAL_ERROR "No baseline MD5 available for ${filename} - baseline_${filevar}_md5 is not defined.")
+    endif(NOT baseline_${filevar}_md5)
+    MD5(${filefullname} ${filevar}_md5)
+    if(NOT "${${filevar}_md5}" STREQUAL "${baseline_${filevar}_md5}")
+      if("${warn}" STREQUAL "1")
+	message("\n${filename} differs from baseline: baseline md5 hash is ${baseline_${filevar}_md5} and current hash is ${${filevar}_md5}.\n")
+      endif("${warn}" STREQUAL "1")
+      set(${resultvar} 0)
+    endif(NOT "${${filevar}_md5}" STREQUAL "${baseline_${filevar}_md5}")
+  endforeach(fileitem ${filelist})
+endmacro(VERIFY_FILES filelist resultvar)
 
-if(LEMON_EXECUTABLE AND PERPLEX_EXECUTABLE AND RE2C_EXECUTABLE)
+
+macro(TOOL_VERSIONS)
   # Read lemon version
   execute_process(COMMAND ${LEMON_EXECUTABLE} -x OUTPUT_VARIABLE lemon_version)
   string(REPLACE "Lemon version " "" lemon_version "${lemon_version}")
@@ -37,12 +60,11 @@ if(LEMON_EXECUTABLE AND PERPLEX_EXECUTABLE AND RE2C_EXECUTABLE)
   # Read perplex version
   execute_process(COMMAND ${PERPLEX_EXECUTABLE} -v OUTPUT_VARIABLE perplex_version)
   string(STRIP "${perplex_version}" perplex_version)
-
-  message("versions:  lemon: ${lemon_version}; re2c: ${re2c_version}; perplex: ${perplex_version}")
+endmacro(TOOL_VERSIONS)
 
   # Calculate the MD5 sums of the template files used for lemon and perplex
-  MD5_FILES("${LEMON_TEMPLATE}" "${PERPLEX_TEMPLATE}")
-
+  #MD5_FILES("${LEMON_TEMPLATE}" "${PERPLEX_TEMPLATE}")
+if(0)
   # Calculate the MD5 sums of the parser and scanner input files
   FILEVAR("${CMAKE_CURRENT_SOURCE_DIR}/expparse.y" expparse_y)
   MD5("${CMAKE_CURRENT_SOURCE_DIR}/expparse.y" ${expparse_y}_md5)
@@ -50,8 +72,7 @@ if(LEMON_EXECUTABLE AND PERPLEX_EXECUTABLE AND RE2C_EXECUTABLE)
   FILEVAR("${CMAKE_CURRENT_SOURCE_DIR}/expparse.l" expscan_l)
   MD5("${CMAKE_CURRENT_SOURCE_DIR}/expscan.l" ${expscan_l}_md5)
   message("expscan.l md5: ${${expscan_l}_md5}")
-
-endif(LEMON_EXECUTABLE AND PERPLEX_EXECUTABLE AND RE2C_EXECUTABLE)
+endif(0)
 
 # Local Variables:
 # tab-width: 8
