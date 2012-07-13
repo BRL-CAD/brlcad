47bdce23dd7f030b7f0009f9ec4f4859b0abb140
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2012-07-13 02:42:13 +0000

    fix filename in comments
    svn:revision:51492
    svn:branch:trunk
    svn:account:starseeker

--- src/other/perplex/perplex_template.c
+++ src/other/perplex/perplex_template.c
@@ -1,4 +1,4 @@
-/*              S C A N N E R _ T E M P L A T E . C
+/*              P E R P L E X _ T E M P L A T E . C
  * BRL-CAD
  *
  * Copyright (c) 1990-2011 United States Government as represented by
@@ -43,7 +43,7 @@
  *  to contract no. DE-AC03-76SF00098 between the United States
  *  Department of Energy and the University of California.
  */
-/** @file scanner_template.c
+/** @file perplex_template.c
  *
  * template for generated scanners
  *
