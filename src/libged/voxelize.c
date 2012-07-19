fd28bb6fa703a0185e390e154bbb0fabffda768b
Anurag Murty <anuragmurty@gmail.com>
2012-07-19 10:59:45 +0000

    static callback functions
    svn:revision:51588
    svn:branch:trunk
    svn:account:anrgmrty

--- src/libged/voxelize.c
+++ src/libged/voxelize.c
@@ -39,18 +39,18 @@
 /**
  * Function to print values to Screen
  */
-void
+/*HIDDEN void
 printToScreen(genptr_t callBackData, int x, int y, int z, const char* a, fastf_t fill) {
     fastf_t *threshold = (fastf_t *)callBackData;
 
     printf("%f\t(%4d,%4d,%4d)\t%s\t%f\n", *threshold, x, y, z, a, fill);
 }
-
+*/
 /**
  * Function to print values to File
  */
-void
-printToFile(genptr_t callBackData, int x, int y, int z, const char *a, fastf_t fill) {
+
+HIDDEN void printToFile(genptr_t callBackData, int x, int y, int z, const char *a, fastf_t fill) {
     fastf_t *threshold = (fastf_t *)callBackData;
     FILE *fp;
 
@@ -95,9 +95,6 @@ ged_voxelize(struct ged *gedp, int argc, const char *argv[])
 	argv++;
     }
 
-
-    bu_vls_printf(gedp->ged_result_str, "the minimum of bounding box is%d\n", (rtip->mdl_min)[0]);
-
     /* user parameters are being given values directly here*/
     sizeVoxel[0] = 1.0;
     sizeVoxel[1] = 1.0;
@@ -109,7 +106,7 @@ ged_voxelize(struct ged *gedp, int argc, const char *argv[])
     callBackData = (void *)(& threshold);
 
     /* voxelize function is called here with rtip(ray trace instance), userParameters and printToFile/printToScreen options */
-    voxelize(rtip, sizeVoxel, levelOfDetail, printToScreen, callBackData);
+    voxelize(rtip, sizeVoxel, levelOfDetail, printToFile, callBackData);
 
     rt_free_rti(rtip);
 
