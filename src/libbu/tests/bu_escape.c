c756a530eb8dee8ab4d306b6c4fb85e6be08d55b
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2012-07-23 14:45:10 +0000

    Add escape tests
    svn:revision:51638
    svn:branch:trunk
    svn:account:starseeker

--- src/libbu/tests/bu_escape.c
+++ src/libbu/tests/bu_escape.c
@@ -24,14 +24,15 @@
 
 #include "bu.h"
 
-
-static void
+static int 
 compare(const char *input, const char *output, const char *correct)
 {
     if (BU_STR_EQUAL(output, correct)) {
 	printf("%24s -> %28s [PASS]\n", input, output);
+        return 1;
     } else {
 	printf("%24s -> %28s [FAIL]  (should be '%s')\n", input, output, correct);
+        return 0;
     }
 }
 
@@ -39,94 +40,165 @@ compare(const char *input, const char *output, const char *correct)
 int
 main(int ac, char *av[])
 {
+    int function_num = 0;
+    int test_num = 0;
     char *bufp;
     char buffer[32];
 
-    if (ac > 1)
-	printf("Usage: %s\n", av[0]);
+    if (ac < 3)
+	printf("Usage: %s function_to_test test_number\n", av[0]);
 
-    printf("Testing unescape:\n");
+    sscanf(av[1], "%d", &function_num); 
+    sscanf(av[2], "%d", &test_num); 
 
+    if (function_num == 1) {
 #define CMP_UNESC(in, out) compare((in), bu_str_unescape((in), buffer, 32), (out))
-    CMP_UNESC(NULL, "");
-    CMP_UNESC("", "");
-    CMP_UNESC(" ", " ");
-    CMP_UNESC("hello", "hello");
-    CMP_UNESC("\"", "\"");
-    CMP_UNESC("\'", "\'");
-    CMP_UNESC("\\", "");
-    CMP_UNESC("\\\"", "\"");
-    CMP_UNESC("\\\\", "\\");
-    CMP_UNESC("\"hello\"", "\"hello\"");
-    CMP_UNESC("\'hello\'", "'hello'");
-    CMP_UNESC("\\hello", "hello");
-    CMP_UNESC("\\hello\"", "hello\"");
-    CMP_UNESC("hello\\\\", "hello\\");
-    CMP_UNESC("\"hello\'\"", "\"hello'\"");
-    CMP_UNESC("\"hello\'", "\"hello'");
-    CMP_UNESC("\'hello\'", "'hello'");
-    CMP_UNESC("\'hello\"", "'hello\"");
-    CMP_UNESC("\"\"hello\"", "\"\"hello\"");
-    CMP_UNESC("\'\'hello\'\'", "''hello''");
-    CMP_UNESC("\'\"hello\"\'", "'\"hello\"'");
-    CMP_UNESC("\"\"hello\"\"", "\"\"hello\"\"");
-    CMP_UNESC("\\\"\\\"\\\"hello\\", "\"\"\"hello");
+	switch (test_num) {
+	    case 1:
+		return !CMP_UNESC(NULL, "");
+	    case 2:
+		return !CMP_UNESC("", "");
+	    case 3:
+		return !CMP_UNESC(" ", " ");
+	    case 4:
+		return !CMP_UNESC("hello", "hello");
+	    case 5:
+		return !CMP_UNESC("\"", "\"");
+	    case 6:
+		return !CMP_UNESC("\'", "\'");
+	    case 7:
+		return !CMP_UNESC("\\", "");
+	    case 8:
+		return !CMP_UNESC("\\\"", "\"");
+	    case 9:
+		return !CMP_UNESC("\\\\", "\\");
+	    case 10:
+		return !CMP_UNESC("\"hello\"", "\"hello\"");
+	    case 11:
+		return !CMP_UNESC("\'hello\'", "'hello'");
+	    case 12:
+		return !CMP_UNESC("\\hello", "hello");
+	    case 13:
+		return !CMP_UNESC("\\hello\"", "hello\"");
+	    case 14:
+		return !CMP_UNESC("hello\\\\", "hello\\");
+	    case 15:
+		return !CMP_UNESC("\"hello\'\"", "\"hello'\"");
+	    case 16:
+		return !CMP_UNESC("\"hello\'", "\"hello'");
+	    case 17:
+		return !CMP_UNESC("\'hello\'", "'hello'");
+	    case 18:
+		return !CMP_UNESC("\'hello\"", "'hello\"");
+	    case 19:
+		return !CMP_UNESC("\"\"hello\"", "\"\"hello\"");
+	    case 20:
+		return !CMP_UNESC("\'\'hello\'\'", "''hello''");
+	    case 21:
+		return !CMP_UNESC("\'\"hello\"\'", "'\"hello\"'");
+	    case 22:
+		return !CMP_UNESC("\"\"hello\"\"", "\"\"hello\"\"");
+	    case 23:
+		return !CMP_UNESC("\\\"\\\"\\\"hello\\", "\"\"\"hello");
+	}
+	return 1;
+    }
 
-    printf("Testing escape:\n");
-
+    if (function_num == 2) {
 #define CMP_ESC(in, c, out) compare((in), bu_str_escape((in), (c), buffer, 32), (out))
-    CMP_ESC(NULL, NULL, "");
-    CMP_ESC(NULL, "", "");
-    CMP_ESC("", NULL, "");
-    CMP_ESC("", "", "");
-    CMP_ESC(" ", "", " ");
-    CMP_ESC("[ ]", " ", "[\\ ]");
-    CMP_ESC("[  ]", " ", "[\\ \\ ]");
-    CMP_ESC("h e l l o", " ", "h\\ e\\ l\\ l\\ o");
-    CMP_ESC("h\\ ello", " ", "h\\\\ ello");
-    CMP_ESC("[]", "\\", "[]");
-    CMP_ESC("\\", "\\", "\\\\");
-    CMP_ESC("\\\\", "\\", "\\\\\\\\");
-    CMP_ESC("\\a\\b", "\\", "\\\\a\\\\b");
-    CMP_ESC("abc", "a", "\\abc");
-    CMP_ESC("abc", "b", "a\\bc");
-    CMP_ESC("abc", "c", "ab\\c");
-    CMP_ESC("abc", "ab", "\\a\\bc");
-    CMP_ESC("abc", "bc", "a\\b\\c");
-    CMP_ESC("abc", "abc", "\\a\\b\\c");
-    CMP_ESC("aaa", "bc", "aaa");
-    CMP_ESC("aaa", "a", "\\a\\a\\a");
-    CMP_ESC("aaa", "aaa", "\\a\\a\\a");
-    CMP_ESC("abc", "^a", "a\\b\\c");
-    CMP_ESC("abc", "^b", "\\ab\\c");
-    CMP_ESC("abc", "^c", "\\a\\bc");
-    CMP_ESC("abc", "^ab", "ab\\c");
-    CMP_ESC("abc", "^bc", "\\abc");
-    CMP_ESC("abc", "^abc", "abc");
-    CMP_ESC("aaa", "^bc", "\\a\\a\\a");
-    CMP_ESC("aaa", "^a", "aaa");
-    CMP_ESC("aaa", "^aaa", "aaa");
+	switch (test_num) {
+	    case 1:
+		return !CMP_ESC(NULL, NULL, "");
+	    case 2:
+		return !CMP_ESC(NULL, "", "");
+	    case 3:
+		return !CMP_ESC("", NULL, "");
+	    case 4:
+		return !CMP_ESC("", "", "");
+	    case 5:
+		return !CMP_ESC(" ", "", " ");
+	    case 6:
+		return !CMP_ESC("[ ]", " ", "[\\ ]");
+	    case 7:
+		return !CMP_ESC("[  ]", " ", "[\\ \\ ]");
+	    case 8:
+		return !CMP_ESC("h e l l o", " ", "h\\ e\\ l\\ l\\ o");
+	    case 9:
+		return !CMP_ESC("h\\ ello", " ", "h\\\\ ello");
+	    case 10:
+		return !CMP_ESC("[]", "\\", "[]");
+	    case 11:
+		return !CMP_ESC("\\", "\\", "\\\\");
+	    case 12:
+		return !CMP_ESC("\\\\", "\\", "\\\\\\\\");
+	    case 13:
+		return !CMP_ESC("\\a\\b", "\\", "\\\\a\\\\b");
+	    case 14:
+		return !CMP_ESC("abc", "a", "\\abc");
+	    case 15:
+		return !CMP_ESC("abc", "b", "a\\bc");
+	    case 16:
+		return !CMP_ESC("abc", "c", "ab\\c");
+	    case 17:
+		return !CMP_ESC("abc", "ab", "\\a\\bc");
+	    case 18:
+		return !CMP_ESC("abc", "bc", "a\\b\\c");
+	    case 19:
+		return !CMP_ESC("abc", "abc", "\\a\\b\\c");
+	    case 20:
+		return !CMP_ESC("aaa", "bc", "aaa");
+	    case 21:
+		return !CMP_ESC("aaa", "a", "\\a\\a\\a");
+	    case 22:
+		return !CMP_ESC("aaa", "aaa", "\\a\\a\\a");
+	    case 23:
+		return !CMP_ESC("abc", "^a", "a\\b\\c");
+	    case 24:
+		return !CMP_ESC("abc", "^b", "\\ab\\c");
+	    case 25:
+		return !CMP_ESC("abc", "^c", "\\a\\bc");
+	    case 26:
+		return !CMP_ESC("abc", "^ab", "ab\\c");
+	    case 27:
+		return !CMP_ESC("abc", "^bc", "\\abc");
+	    case 28:
+		return !CMP_ESC("abc", "^abc", "abc");
+	    case 29:
+		return !CMP_ESC("aaa", "^bc", "\\a\\a\\a");
+	    case 30:
+		return !CMP_ESC("aaa", "^a", "aaa");
+	    case 31:
+		return !CMP_ESC("aaa", "^aaa", "aaa");
+	}
+	return 1;
+    }
 
-    printf("Testing escape+unescape:\n");
+    if (function_num == 3) {
+        int pass = 0;
+	switch (test_num) {
+	    case 1:
+		bufp = bu_str_unescape(bu_str_escape("abc", "b", buffer, 32), NULL, 0);
+		pass = compare("abc", bufp, "abc");
+		bu_free(bufp, NULL);
 
-    bufp = bu_str_unescape(bu_str_escape("abc", "b", buffer, 32), NULL, 0);
-    compare("abc", bufp, "abc");
-    bu_free(bufp, NULL);
+	    case 2:
+		bufp = bu_str_unescape(bu_str_escape("abc\\cba", "b", buffer, 32), NULL, 0);
+		pass = compare("abc\\cba", bufp, "abccba");
+		bu_free(bufp, NULL);
 
-    bufp = bu_str_unescape(bu_str_escape("abc\\cba", "b", buffer, 32), NULL, 0);
-    compare("abc\\cba", bufp, "abccba");
-    bu_free(bufp, NULL);
+	    case 3:
+		bufp = bu_str_unescape(bu_str_escape("abc\\\\cba", "b", buffer, 32), NULL, 0);
+		pass = compare("abc\\\\cba", bufp, "abc\\cba");
+		bu_free(bufp, NULL);
 
-    bufp = bu_str_unescape(bu_str_escape("abc\\\\cba", "b", buffer, 32), NULL, 0);
-    compare("abc\\\\cba", bufp, "abc\\cba");
-    bu_free(bufp, NULL);
-
-    bufp = bu_str_unescape(bu_str_escape("abc\\\\\\c\\ba\\", "b", buffer, 32), NULL, 0);
-    compare("abc\\\\\\c\\ba\\", bufp, "abc\\c\\ba");
-    bu_free(bufp, NULL);
-
-    printf("%s: testing complete\n", av[0]);
-    return 0;
+	    case 4:
+		bufp = bu_str_unescape(bu_str_escape("abc\\\\\\c\\ba\\", "b", buffer, 32), NULL, 0);
+		pass = compare("abc\\\\\\c\\ba\\", bufp, "abc\\c\\ba");
+		bu_free(bufp, NULL);
+	}
+	return !pass;
+    }
+    return 1;
 }
 
 
