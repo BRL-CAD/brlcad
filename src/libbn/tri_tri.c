ac794cef504036484c38f249beb1ec92c1ea8b6f
Christopher Sean Morrison <brlcad@gmail.com>
2012-07-26 02:55:22 +0000

    pd header, not lgpl
    svn:revision:51679
    svn:branch:trunk
    svn:account:brlcad

--- src/libbn/tri_tri.c
+++ src/libbn/tri_tri.c
@@ -1,21 +1,8 @@
 /*                       T R I _ T R I . C
  * BRL-CAD
  *
- * Copyright (c) 2012 United States Government as represented by
- * the U.S. Army Research Laboratory.
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public License
- * version 2.1 as published by the Free Software Foundation.
- *
- * This library is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this file; see the file named COPYING for more
- * information.
+ * Published in 2012 by the United States Government.
+ * This work is in the public domain.
  */
 /* Triangle/triangle intersection test routine,
  * by Tomas Moller, 1997.
