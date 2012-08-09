2e5579164cea1eca8bbf6a8db2a5a82db2995839
Carl Moore <cmoore@brlcad.org>
2012-08-09 21:02:34 +0000

    remove trailing blanks
    svn:revision:51875
    svn:branch:trunk
    svn:account:carlmoore

--- sh/news2tracker.sh
+++ sh/news2tracker.sh
@@ -342,7 +342,7 @@ if [ $VERBOSE -gt 2 ] ; then
     printf "\t$_stmp/${PROJECT}.log\n"
 fi
 echo "$_log" > "$_stmp/${PROJECT}.log"
-if [ $VERBOSE -gt 3 ] ; then 
+if [ $VERBOSE -gt 3 ] ; then
     echo "begin svn log NEWS output:"
     echo "$_log"
     echo "end of svn log NEWS output"
@@ -368,7 +368,7 @@ for _revision in $_revisions ; do
     if [ $(($_today - $_added)) -gt $CLOSED ] ; then
 	break;
     fi
-    
+
     [ $VERBOSE -gt 2 ] && echo "$_revision was added $(($_today - $_added)) days ago"
 
     # use the revision as the itemID
