c6f32392887efb4c7b86cac4b8f1b115d6cadcd9
Carl Moore <cmoore@brlcad.org>
2012-08-09 20:56:23 +0000

    removed trailing blanks
    svn:revision:51871
    svn:branch:trunk
    svn:account:carlmoore

--- regress/nirt.sh
+++ regress/nirt.sh
@@ -53,7 +53,7 @@ if test ! -f "$NIRT" ; then
     exit 1
 fi
 
-# Clear old files 
+# Clear old files
 rm -f nirt.mged nirt_g.log nirt.g nirt.ref nirt.out nirt.log
 
 cat > nirt.mged <<EOF
@@ -130,7 +130,7 @@ model_min = (-1, -1, -1)    model_max = (1, 1, 1)
 Origin (x y z) = (0.00000000 0.00000000 0.00000000)  (h v d) = (0.0000 0.0000 0.0000)
 Direction (x y z) = (-1.00000000 0.00000000 0.00000000)  (az el) = (0.00000000 0.00000000)
     Region Name               Entry (x y z)              LOS  Obliq_in Attrib
-center_cube.r        (   1.0000    0.0000    0.0000)   2.0000   0.0000 
+center_cube.r        (   1.0000    0.0000    0.0000)   2.0000   0.0000
 Quitting...
 *** Test 2 - xyz command ***
 Database file:  'nirt.g'
@@ -145,7 +145,7 @@ model_min = (-1, -1, -1)    model_max = (1, 1, 1)
 Origin (x y z) = (0.00000000 0.00000000 0.50000000)  (h v d) = (0.0000 0.5000 0.0000)
 Direction (x y z) = (-1.00000000 0.00000000 0.00000000)  (az el) = (0.00000000 0.00000000)
     Region Name               Entry (x y z)              LOS  Obliq_in Attrib
-center_cube.r        (   1.0000    0.0000    0.5000)   2.0000   0.0000 
+center_cube.r        (   1.0000    0.0000    0.5000)   2.0000   0.0000
 Quitting...
 *** Test 3 - backout command ***
 Database file:  'nirt.g'
@@ -159,12 +159,12 @@ model_min = (-3, -1, -1)    model_max = (3, 1, 1)
 Origin (x y z) = (0.00000000 0.00000000 0.00000000)  (h v d) = (0.0000 0.0000 0.0000)
 Direction (x y z) = (-1.00000000 0.00000000 0.00000000)  (az el) = (0.00000000 0.00000000)
     Region Name               Entry (x y z)              LOS  Obliq_in Attrib
-left_and_right_cubes.r (  -1.0000    0.0000    0.0000)   2.0000   0.0000 
+left_and_right_cubes.r (  -1.0000    0.0000    0.0000)   2.0000   0.0000
 Origin (x y z) = (6.63324958 0.00000000 0.00000000)  (h v d) = (0.0000 0.0000 0.0000)
 Direction (x y z) = (-1.00000000 0.00000000 0.00000000)  (az el) = (0.00000000 0.00000000)
     Region Name               Entry (x y z)              LOS  Obliq_in Attrib
-left_and_right_cubes.r (   3.0000    0.0000    0.0000)   2.0000   0.0000 
-left_and_right_cubes.r (  -1.0000    0.0000    0.0000)   2.0000   0.0000 
+left_and_right_cubes.r (   3.0000    0.0000    0.0000)   2.0000   0.0000
+left_and_right_cubes.r (  -1.0000    0.0000    0.0000)   2.0000   0.0000
 Quitting...
 *** Test 4 - backout/xyz interaction ***
 Database file:  'nirt.g'
@@ -179,18 +179,18 @@ model_min = (-3, -1, -1)    model_max = (3, 1, 1)
 Origin (x y z) = (0.00000000 0.00000000 0.50000000)  (h v d) = (0.0000 0.5000 0.0000)
 Direction (x y z) = (-1.00000000 0.00000000 0.00000000)  (az el) = (0.00000000 0.00000000)
     Region Name               Entry (x y z)              LOS  Obliq_in Attrib
-left_and_right_cubes.r (  -1.0000    0.0000    0.5000)   2.0000   0.0000 
+left_and_right_cubes.r (  -1.0000    0.0000    0.5000)   2.0000   0.0000
 (x, y, z) = (0.00, 0.00, 0.50)
 (x, y, z) = (0.00, 0.00, 0.50)
 Origin (x y z) = (6.63324958 0.00000000 0.80000000)  (h v d) = (0.0000 0.8000 0.0000)
 Direction (x y z) = (-1.00000000 0.00000000 0.00000000)  (az el) = (0.00000000 0.00000000)
     Region Name               Entry (x y z)              LOS  Obliq_in Attrib
-left_and_right_cubes.r (   3.0000    0.0000    0.8000)   2.0000   0.0000 
-left_and_right_cubes.r (  -1.0000    0.0000    0.8000)   2.0000   0.0000 
+left_and_right_cubes.r (   3.0000    0.0000    0.8000)   2.0000   0.0000
+left_and_right_cubes.r (  -1.0000    0.0000    0.8000)   2.0000   0.0000
 Origin (x y z) = (0.00000000 0.00000000 0.80000000)  (h v d) = (0.0000 0.8000 0.0000)
 Direction (x y z) = (-1.00000000 0.00000000 0.00000000)  (az el) = (0.00000000 0.00000000)
     Region Name               Entry (x y z)              LOS  Obliq_in Attrib
-left_and_right_cubes.r (  -1.0000    0.0000    0.8000)   2.0000   0.0000 
+left_and_right_cubes.r (  -1.0000    0.0000    0.8000)   2.0000   0.0000
 Quitting...
 *** Test 5 - dir command***
 Database file:  'nirt.g'
@@ -205,12 +205,12 @@ model_min = (-3, -1, -1)    model_max = (3, 1, 1)
 Origin (x y z) = (0.00000000 0.00000000 0.00000000)  (h v d) = (0.0000 0.0000 0.0000)
 Direction (x y z) = (-1.00000000 0.00000000 0.00000000)  (az el) = (0.00000000 0.00000000)
     Region Name               Entry (x y z)              LOS  Obliq_in Attrib
-left_and_right_cubes.r (  -1.0000    0.0000    0.0000)   2.0000   0.0000 
+left_and_right_cubes.r (  -1.0000    0.0000    0.0000)   2.0000   0.0000
 (x, y, z) = (-0.89, -0.45, 0.00)
 Origin (x y z) = (0.00000000 0.00000000 0.00000000)  (h v d) = (0.0000 0.0000 0.0000)
 Direction (x y z) = (-0.89442719 -0.44721360 0.00000000)  (az el) = (26.56505118 -0.00000000)
     Region Name               Entry (x y z)              LOS  Obliq_in Attrib
-left_and_right_cubes.r (  -1.0000   -0.5000    0.0000)   1.1180  26.5651 
+left_and_right_cubes.r (  -1.0000   -0.5000    0.0000)   1.1180  26.5651
 Origin (x y z) = (0.00000000 0.00000000 0.00000000)  (h v d) = (0.0000 0.0000 0.0000)
 Direction (x y z) = (0.00000000 0.00000000 1.00000000)  (az el) = (0.00000000 -90.00000000)
 You missed the target
@@ -227,14 +227,14 @@ model_min = (-3, -1, -1)    model_max = (3, 1, 1)
 Origin (x y z) = (6.63324958 0.00000000 0.00000000)  (h v d) = (0.0000 0.0000 0.0000)
 Direction (x y z) = (-1.00000000 0.00000000 0.00000000)  (az el) = (0.00000000 0.00000000)
     Region Name               Entry (x y z)              LOS  Obliq_in Attrib
-all_cubes.r          (   3.0000    0.0000    0.0000)   2.0000   0.0000 
+all_cubes.r          (   3.0000    0.0000    0.0000)   2.0000   0.0000
 OVERLAP: 'all_cubes.r' and 'center_overlap.r' xyz_in=(1 0 0) los=2
-center_overlap.r     (   1.0000    0.0000    0.0000)   2.0000   0.0000 
-all_cubes.r          (  -1.0000    0.0000    0.0000)   2.0000   0.0000 
+center_overlap.r     (   1.0000    0.0000    0.0000)   2.0000   0.0000
+all_cubes.r          (  -1.0000    0.0000    0.0000)   2.0000   0.0000
 Origin (x y z) = (0.00000000 0.00000000 6.63324958)  (h v d) = (0.0000 0.0000 0.0000)
 Direction (x y z) = (0.00000000 0.00000000 -1.00000000)  (az el) = (0.00000000 90.00000000)
     Region Name               Entry (x y z)              LOS  Obliq_in Attrib
-center_overlap.r     (   0.0000    0.0000    1.0000)   2.0000   0.0000 
+center_overlap.r     (   0.0000    0.0000    1.0000)   2.0000   0.0000
 OVERLAP: 'all_cubes.r' and 'center_overlap.r' xyz_in=(0 0 1) los=2
 Quitting...
 *** Test 7 - output formatting ***
@@ -248,7 +248,7 @@ Database units: 'mm'
 model_min = (-3, -1, -1)    model_max = (3, 1, 1)
 Ray:
 x_orig,y_orig,z_orig,d_orig,h,v,x_dir,y_dir,z_dir,az,el
-6.63324958,0.00000000,0.00000000,0.00000000,0.00000000,0.00000000,-1.00000000,0.00000000,0.00000000,0.00000000,0.00000000 
+6.63324958,0.00000000,0.00000000,0.00000000,0.00000000,0.00000000,-1.00000000,0.00000000,0.00000000,0.00000000,0.00000000
 
 Results:
 reg_name,path_name,reg_id,x_in,y_in,z_in,d_in,x_out,y_out,z_out,d_out,los,scaled_los,obliq_in,obliq_out,surf_num_in,surf_num_out
@@ -267,7 +267,7 @@ Database units: 'mm'
 model_min = (-3, -1, -1)    model_max = (3, 1, 1)
 Ray:
 x_orig,y_orig,z_orig,d_orig,h,v,x_dir,y_dir,z_dir,az,el
-6.63324958,0.00000000,0.00000000,0.00000000,0.00000000,0.00000000,-1.00000000,0.00000000,0.00000000,0.00000000,0.00000000 
+6.63324958,0.00000000,0.00000000,0.00000000,0.00000000,0.00000000,-1.00000000,0.00000000,0.00000000,0.00000000,0.00000000
 
 Results:
 reg_name,path_name,reg_id,x_in,y_in,z_in,d_in,x_out,y_out,z_out,d_out,los,scaled_los,obliq_in,obliq_out,surf_num_in,surf_num_out
@@ -287,9 +287,9 @@ model_min = (-3, -1, -1)    model_max = (3, 1, 1)
 Origin (x y z) = (6.63324958 0.00000000 0.00000000)  (h v d) = (0.0000 0.0000 0.0000)
 Direction (x y z) = (-1.00000000 0.00000000 0.00000000)  (az el) = (0.00000000 0.00000000)
     Region Name               Entry (x y z)              LOS  Obliq_in Attrib
-right_cube.r         (   3.0000    0.0000    0.0000)   2.0000   0.0000 
-center_cube.r        (   1.0000    0.0000    0.0000)   2.0000   0.0000 
-left_cube.r          (  -1.0000    0.0000    0.0000)   2.0000   0.0000 
+right_cube.r         (   3.0000    0.0000    0.0000)   2.0000   0.0000
+center_cube.r        (   1.0000    0.0000    0.0000)   2.0000   0.0000
+left_cube.r          (  -1.0000    0.0000    0.0000)   2.0000   0.0000
 Quitting...
 Database file:  'nirt.g'
 Building the directory...
@@ -302,9 +302,9 @@ model_min = (-3, -1, -1)    model_max = (3, 1, 1)
 Origin (x y z) = (6.63324958 0.00000000 0.00000000)  (h v d) = (0.0000 0.0000 0.0000)
 Direction (x y z) = (-1.00000000 0.00000000 0.00000000)  (az el) = (0.00000000 0.00000000)
     Region Name               Entry (x y z)                   Exit (x y z)             Obliq_in Attrib
-right_cube.r         (   3.0000    0.0000    0.0000) (   1.0000    0.0000    0.0000)   0.0000 
-center_cube.r        (   1.0000    0.0000    0.0000) (  -1.0000    0.0000    0.0000)   0.0000 
-left_cube.r          (  -1.0000    0.0000    0.0000) (  -3.0000    0.0000    0.0000)   0.0000 
+right_cube.r         (   3.0000    0.0000    0.0000) (   1.0000    0.0000    0.0000)   0.0000
+center_cube.r        (   1.0000    0.0000    0.0000) (  -1.0000    0.0000    0.0000)   0.0000
+left_cube.r          (  -1.0000    0.0000    0.0000) (  -3.0000    0.0000    0.0000)   0.0000
 Quitting...
 Database file:  'nirt.g'
 Building the directory...
@@ -317,9 +317,9 @@ model_min = (-3, -1, -1)    model_max = (3, 1, 1)
 Origin (x y z) = (6.63324958 0.00000000 0.00000000)  (h v d) = (0.0000 0.0000 0.0000)
 Direction (x y z) = (-1.00000000 0.00000000 0.00000000)  (az el) = (0.00000000 0.00000000)
     Region Name               Entry (x y z)              LOS  Obliq_in Attrib
-right_cube.r         (   3.0000    0.0000    0.0000)   2.0000   0.0000 
-center_cube.r        (   1.0000    0.0000    0.0000)   2.0000   0.0000 
-left_cube.r          (  -1.0000    0.0000    0.0000)   2.0000   0.0000 
+right_cube.r         (   3.0000    0.0000    0.0000)   2.0000   0.0000
+center_cube.r        (   1.0000    0.0000    0.0000)   2.0000   0.0000
+left_cube.r          (  -1.0000    0.0000    0.0000)   2.0000   0.0000
 Quitting...
 Database file:  'nirt.g'
 Building the directory...
@@ -332,9 +332,9 @@ model_min = (-3, -1, -1)    model_max = (3, 1, 1)
 Origin (x y z) = (6.63324958 0.00000000 0.00000000)  (h v d) = (0.0000 0.0000 0.0000)
 Direction (x y z) = (-1.00000000 0.00000000 0.00000000)  (az el) = (0.00000000 0.00000000)
     Region Name               Entry (x y z)              LOS  Obliq_in Attrib
-right_cube.r         (   3.0000    0.0000    0.0000)   2.0000   0.0000 
-center_cube.r        (   1.0000    0.0000    0.0000)   2.0000   0.0000 
-left_cube.r          (  -1.0000    0.0000    0.0000)   2.0000   0.0000 
+right_cube.r         (   3.0000    0.0000    0.0000)   2.0000   0.0000
+center_cube.r        (   1.0000    0.0000    0.0000)   2.0000   0.0000
+left_cube.r          (  -1.0000    0.0000    0.0000)   2.0000   0.0000
 Quitting...
 *** Test 8 - attribute reporting ***
 Database file:  'nirt.g'
@@ -353,9 +353,9 @@ Objects 'left_cube_color.r' 'center_cube_color.r' 'right_cube_color.r' processed
 Origin (x y z) = (6.63324958 0.00000000 0.00000000)  (h v d) = (0.0000 0.0000 0.0000)
 Direction (x y z) = (-1.00000000 0.00000000 0.00000000)  (az el) = (0.00000000 0.00000000)
     Region Name               Entry (x y z)              LOS  Obliq_in Attrib
-right_cube_color.r   (   3.0000    0.0000    0.0000)   2.0000   0.0000 color=0/0/255 
-center_cube_color.r  (   1.0000    0.0000    0.0000)   2.0000   0.0000 color=0/255/0 
-left_cube_color.r    (  -1.0000    0.0000    0.0000)   2.0000   0.0000 color=255/0/0 
+right_cube_color.r   (   3.0000    0.0000    0.0000)   2.0000   0.0000 color=0/0/255
+center_cube_color.r  (   1.0000    0.0000    0.0000)   2.0000   0.0000 color=0/255/0
+left_cube_color.r    (  -1.0000    0.0000    0.0000)   2.0000   0.0000 color=255/0/0
 "color"
 "region"
 
@@ -365,9 +365,9 @@ Objects 'left_cube_color.r' 'center_cube_color.r' 'right_cube_color.r' processed
 Origin (x y z) = (6.63324958 0.00000000 0.00000000)  (h v d) = (0.0000 0.0000 0.0000)
 Direction (x y z) = (-1.00000000 0.00000000 0.00000000)  (az el) = (0.00000000 0.00000000)
     Region Name               Entry (x y z)              LOS  Obliq_in Attrib
-right_cube_color.r   (   3.0000    0.0000    0.0000)   2.0000   0.0000 color=0/0/255 region=R 
-center_cube_color.r  (   1.0000    0.0000    0.0000)   2.0000   0.0000 color=0/255/0 region=R 
-left_cube_color.r    (  -1.0000    0.0000    0.0000)   2.0000   0.0000 color=255/0/0 region=R 
+right_cube_color.r   (   3.0000    0.0000    0.0000)   2.0000   0.0000 color=0/0/255 region=R
+center_cube_color.r  (   1.0000    0.0000    0.0000)   2.0000   0.0000 color=0/255/0 region=R
+left_cube_color.r    (  -1.0000    0.0000    0.0000)   2.0000   0.0000 color=255/0/0 region=R
 Quitting...
 Database file:  'nirt.g'
 Building the directory...
@@ -381,9 +381,9 @@ model_min = (-3, -1, -1)    model_max = (3, 1, 1)
 Origin (x y z) = (6.63324958 0.00000000 0.00000000)  (h v d) = (0.0000 0.0000 0.0000)
 Direction (x y z) = (-1.00000000 0.00000000 0.00000000)  (az el) = (0.00000000 0.00000000)
     Region Name               Entry (x y z)              LOS  Obliq_in Attrib
-right_cube_color.r   (   3.0000    0.0000    0.0000)   2.0000   0.0000 color=0/0/255 
-center_cube_color.r  (   1.0000    0.0000    0.0000)   2.0000   0.0000 color=0/255/0 
-left_cube_color.r    (  -1.0000    0.0000    0.0000)   2.0000   0.0000 color=255/0/0 
+right_cube_color.r   (   3.0000    0.0000    0.0000)   2.0000   0.0000 color=0/0/255
+center_cube_color.r  (   1.0000    0.0000    0.0000)   2.0000   0.0000 color=0/255/0
+left_cube_color.r    (  -1.0000    0.0000    0.0000)   2.0000   0.0000 color=255/0/0
 Quitting...
 *** Test 9 - units ***
 Database file:  'nirt.g'
@@ -398,19 +398,19 @@ units = 'mm'
 Origin (x y z) = (3.46410162 0.00000000 0.00000000)  (h v d) = (0.0000 0.0000 0.0000)
 Direction (x y z) = (-1.00000000 0.00000000 0.00000000)  (az el) = (0.00000000 0.00000000)
     Region Name               Entry (x y z)              LOS  Obliq_in Attrib
-center_cube.r        (   1.0000    0.0000    0.0000)   2.0000   0.0000 
+center_cube.r        (   1.0000    0.0000    0.0000)   2.0000   0.0000
 Origin (x y z) = (0.00346410 0.00000000 0.00000000)  (h v d) = (0.0000 0.0000 0.0000)
 Direction (x y z) = (-1.00000000 0.00000000 0.00000000)  (az el) = (0.00000000 0.00000000)
     Region Name               Entry (x y z)              LOS  Obliq_in Attrib
-center_cube.r        (   0.0010    0.0000    0.0000)   0.0020   0.0000 
+center_cube.r        (   0.0010    0.0000    0.0000)   0.0020   0.0000
 Origin (x y z) = (0.13638195 0.00000000 0.00000000)  (h v d) = (0.0000 0.0000 0.0000)
 Direction (x y z) = (-1.00000000 0.00000000 0.00000000)  (az el) = (0.00000000 0.00000000)
     Region Name               Entry (x y z)              LOS  Obliq_in Attrib
-center_cube.r        (   0.0394    0.0000    0.0000)   0.0787   0.0000 
+center_cube.r        (   0.0394    0.0000    0.0000)   0.0787   0.0000
 Origin (x y z) = (0.01136516 0.00000000 0.00000000)  (h v d) = (0.0000 0.0000 0.0000)
 Direction (x y z) = (-1.00000000 0.00000000 0.00000000)  (az el) = (0.00000000 0.00000000)
     Region Name               Entry (x y z)              LOS  Obliq_in Attrib
-center_cube.r        (   0.0033    0.0000    0.0000)   0.0066   0.0000 
+center_cube.r        (   0.0033    0.0000    0.0000)   0.0066   0.0000
 Quitting...
 *** Test 10 - air regions ***
 Database file:  'nirt.g'
@@ -424,7 +424,7 @@ model_min = (-3, -1, -1)    model_max = (-1, 1, 1)
 Origin (x y z) = (1.46410162 0.00000000 0.00000000)  (h v d) = (0.0000 0.0000 0.0000)
 Direction (x y z) = (-1.00000000 0.00000000 0.00000000)  (az el) = (0.00000000 0.00000000)
     Region Name               Entry (x y z)              LOS  Obliq_in Attrib
-left_cube.r          (  -1.0000    0.0000    0.0000)   2.0000   0.0000 
+left_cube.r          (  -1.0000    0.0000    0.0000)   2.0000   0.0000
 Quitting...
 Database file:  'nirt.g'
 Building the directory...
@@ -437,7 +437,7 @@ model_min = (-3, -1, -1)    model_max = (-1, 1, 1)
 Origin (x y z) = (1.46410162 0.00000000 0.00000000)  (h v d) = (0.0000 0.0000 0.0000)
 Direction (x y z) = (-1.00000000 0.00000000 0.00000000)  (az el) = (0.00000000 0.00000000)
     Region Name               Entry (x y z)              LOS  Obliq_in Attrib
-left_cube.r          (  -1.0000    0.0000    0.0000)   2.0000   0.0000 
+left_cube.r          (  -1.0000    0.0000    0.0000)   2.0000   0.0000
 Quitting...
 Database file:  'nirt.g'
 Building the directory...
@@ -450,8 +450,8 @@ model_min = (-3, -1, -1)    model_max = (1, 1, 1)
 Origin (x y z) = (3.89897949 0.00000000 0.00000000)  (h v d) = (0.0000 0.0000 0.0000)
 Direction (x y z) = (-1.00000000 0.00000000 0.00000000)  (az el) = (0.00000000 0.00000000)
     Region Name               Entry (x y z)              LOS  Obliq_in Attrib
-center_cube_air      (   1.0000    0.0000    0.0000)   2.0000   0.0000 
-left_cube.r          (  -1.0000    0.0000    0.0000)   2.0000   0.0000 
+center_cube_air      (   1.0000    0.0000    0.0000)   2.0000   0.0000
+left_cube.r          (  -1.0000    0.0000    0.0000)   2.0000   0.0000
 Quitting...
 EOF
 
