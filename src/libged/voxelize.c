1a8ee546ab25aca9368195275a2a0f3617acecb1
Anurag Murty <anuragmurty@gmail.com>
2012-08-14 18:23:33 +0000

    voxelize with command line options, regions defined
    svn:revision:52012
    svn:branch:trunk
    svn:account:anrgmrty

--- src/libged/voxelize.c
+++ src/libged/voxelize.c
@@ -43,6 +43,8 @@ struct voxelizeData
     fastf_t *bbMin;
     fastf_t sizeVoxel[3];
     struct rt_wdb *wdbp;
+    char *newname;
+    struct wmember content;
 };
 
 HIDDEN void 
@@ -61,7 +63,8 @@ create_boxes(genptr_t callBackData, int x, int y, int z, const char *UNUSED(a), 
     sprintf(bufz, "%d", z);
     if(dataValues->threshold <= fill) {
 	vp = bu_vls_vlsinit();
-	bu_vls_strcat(vp, "x");
+	bu_vls_strcat(vp, dataValues->newname);
+	bu_vls_strcat(vp, ".x");
 	bu_vls_strcat(vp, bufx);
 	bu_vls_strcat(vp, "y");
 	bu_vls_strcat(vp, bufy);
@@ -69,16 +72,16 @@ create_boxes(genptr_t callBackData, int x, int y, int z, const char *UNUSED(a), 
 	bu_vls_strcat(vp, bufz);
 	bu_vls_strcat(vp, ".s");
 
-        min[0] = (dataValues->bbMin)[0] + (x * (dataValues->sizeVoxel)[0]);
+	min[0] = (dataValues->bbMin)[0] + (x * (dataValues->sizeVoxel)[0]);
 	min[1] = (dataValues->bbMin)[1] + (y * (dataValues->sizeVoxel)[1]);
 	min[2] = (dataValues->bbMin)[2] + (z * (dataValues->sizeVoxel)[2]);
-        max[0] = (dataValues->bbMin)[0] + ( (x + 1.0) * (dataValues->sizeVoxel)[0]);
+	max[0] = (dataValues->bbMin)[0] + ( (x + 1.0) * (dataValues->sizeVoxel)[0]);
 	max[1] = (dataValues->bbMin)[1] + ( (y + 1.0) * (dataValues->sizeVoxel)[1]);
-        max[2] = (dataValues->bbMin)[2] + ( (z + 1.0) * (dataValues->sizeVoxel)[2]);
+	max[2] = (dataValues->bbMin)[2] + ( (z + 1.0) * (dataValues->sizeVoxel)[2]);
 
-        nameDestination = bu_vls_strgrab(vp);
-        mk_rpp(dataValues->wdbp,nameDestination, min, max);
-        
+	nameDestination = bu_vls_strgrab(vp);
+	mk_rpp(dataValues->wdbp,nameDestination, min, max);
+	mk_addmember(nameDestination, &dataValues->content.l, 0, WMOP_UNION);
     }
 }
 
@@ -86,11 +89,12 @@ int
 ged_voxelize(struct ged *gedp, int argc, const char *argv[])
 {
     struct rt_i *rtip;
-    static const char *usage = "object [object ...]";
+    static const char *usage = "[-s \"dx dy dz\"] [-d n] [-t f] new_obj old_obj [old_obj2 old_obj3 ...]";
     fastf_t sizeVoxel[3], threshold;
     int levelOfDetail;
     genptr_t callBackData;
     struct voxelizeData voxDat;
+    int c;
 
     GED_CHECK_DATABASE_OPEN(gedp, GED_ERROR);
     GED_CHECK_ARGC_GT_0(gedp, argc, GED_ERROR);
@@ -99,9 +103,65 @@ ged_voxelize(struct ged *gedp, int argc, const char *argv[])
     bu_vls_trunc(gedp->ged_result_str, 0);
 
     /* incorrect arguments */
-    if (argc == 1) {
+    if (argc < 3) {
 	bu_vls_printf(gedp->ged_result_str, "Usage: %s %s", argv[0], usage);
 	return GED_HELP;
+    }
+
+    sizeVoxel[0]  = 1.0;
+    sizeVoxel[1]  = 1.0;
+    sizeVoxel[2]  = 1.0;
+    levelOfDetail = 1;
+    threshold = 0.5;
+
+    bu_optind = 1;
+    while ((c = bu_getopt(argc, (char * const *)argv, (const char *)"s:d:t:")) != -1) {
+	switch (c) {
+	    case 's':
+		if (sscanf(bu_optarg, "%lf %lf %lf",
+			   &sizeVoxel[0],
+			   &sizeVoxel[1],
+			   &sizeVoxel[2]) != 3) {
+		    bu_vls_printf(gedp->ged_result_str, "Usage: %s %s", argv[0], usage);
+		    return GED_ERROR;
+		}
+		break;
+
+	    case 'd':
+		if (sscanf(bu_optarg, "%d", &levelOfDetail) != 1) {
+		    bu_vls_printf(gedp->ged_result_str, "Usage: %s %s", argv[0], usage);
+		    return GED_ERROR;
+		}
+		break;
+
+	    case 't':
+		if(sscanf(bu_optarg, "%lf", &threshold) != 1) {
+		    bu_vls_printf(gedp->ged_result_str, "Usage: %s %s", argv[0], usage);
+		    return GED_ERROR;
+		}
+		break;				
+
+	    default:
+		bu_vls_printf(gedp->ged_result_str, "Usage: %s %s", argv[0], usage);
+		return GED_ERROR;
+	}
+    }
+
+    argc -= bu_optind;
+    argv += bu_optind;
+
+    if (argc < 2) {
+	bu_vls_printf(gedp->ged_result_str, "error: missing argument(s)\n");
+	return GED_ERROR;
+    }
+
+    voxDat.newname = (char *)argv[0];
+    argc--;
+    argv++;
+
+    if (db_lookup(gedp->ged_wdbp->dbip, voxDat.newname, LOOKUP_QUIET) != RT_DIR_NULL) {
+	bu_vls_printf(gedp->ged_result_str, "error: solid '%s' already exists, aborting\n", voxDat.newname);
+	return GED_ERROR;
     }
 
     rtip = rt_new_rti(gedp->ged_wdbp->dbip);
@@ -110,25 +170,16 @@ ged_voxelize(struct ged *gedp, int argc, const char *argv[])
     /* Walk trees.  Here we identify any object trees in the database
      * that the user wants included in the ray trace.
      */
-    while(argc > 1) {
-	if(rt_gettree(rtip,argv[1]) < 0)
+    while(argc > 0) {
+	if(rt_gettree(rtip,argv[0]) < 0) {
+	    bu_vls_printf(gedp->ged_result_str, "error: object '%s' does not exists, aborting\n", argv[1]);
 	    return GED_ERROR;
+	}
+
 	argc--;
 	argv++;
     }
 
-    /* get bounding box values etc */
-    rt_prep_parallel(rtip, 1);
-
-
-
-    /* user parameters are being given values directly here*/
-    sizeVoxel[0] = 1.0;
-    sizeVoxel[1] = 1.0;
-    sizeVoxel[2] = 1.0;
-
-    threshold = 0.5;
-    levelOfDetail = 4;
 
     voxDat.sizeVoxel[0] = sizeVoxel[0];
     voxDat.sizeVoxel[1] = sizeVoxel[1];
@@ -136,11 +187,16 @@ ged_voxelize(struct ged *gedp, int argc, const char *argv[])
     voxDat.threshold = threshold;
     voxDat.wdbp = gedp->ged_wdbp;
     voxDat.bbMin = rtip->mdl_min;
+    BU_LIST_INIT(&voxDat.content.l);
     
     callBackData = (void*)(&voxDat);
 
    /* voxelize function is called here with rtip(ray trace instance), userParameter and create_boxes function */
     voxelize(rtip, sizeVoxel, levelOfDetail, create_boxes, callBackData);
+    
+    mk_comb(gedp->ged_wdbp, voxDat.newname, &voxDat.content.l, 1, "plastic", "sh=4 sp=0.5 di=0.5 re=0.1", 0, 1000, 0, 0, 100, 0, 0, 0);
+    
+    mk_freemembers(&voxDat.content.l);
     rt_free_rti(rtip);
 
     return GED_OK;
