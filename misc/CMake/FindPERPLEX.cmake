564bd0bdb644c2499dac203cbb834239e1bd33d6
Carl Moore <cmoore@brlcad.org>
2012-08-15 18:22:40 +0000

    remove trailing blanks/tabs
    svn:revision:52046
    svn:branch:trunk
    svn:account:carlmoore

--- misc/CMake/FindPERPLEX.cmake
+++ misc/CMake/FindPERPLEX.cmake
@@ -15,18 +15,18 @@
 # Copyright 2009 Kitware, Inc.
 # Copyright 2006 Tristan Carel
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
 # are met:
-#  
+#
 # * Redistributions of source code must retain the above copyright
 #   notice, this list of conditions and the following disclaimer.
-# 
+#
 # * Redistributions in binary form must reproduce the above copyright
 #   notice, this list of conditions and the following disclaimer in the
 #   documentation and/or other materials provided with the distribution.
-# 
+#
 # * The names of the authors may not be used to endorse or promote
 #   products derived from this software without specific prior written
 #   permission.
