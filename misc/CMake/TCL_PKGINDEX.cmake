7b2aa093207128b98cc562a30c310262b3b9474d
Carl Moore <cmoore@brlcad.org>
2012-08-16 21:43:12 +0000

    remove trailing blanks/tabs
    svn:revision:52078
    svn:branch:trunk
    svn:account:carlmoore

--- misc/CMake/TCL_PKGINDEX.cmake
+++ misc/CMake/TCL_PKGINDEX.cmake
@@ -3,18 +3,18 @@
 # Copyright (c) 2010-2012 United States Government as represented by
 #                the U.S. Army Research Laboratory.
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
 # are met:
-#  
+#
 # * Redistributions of source code must retain the above copyright
 #   notice, this list of conditions and the following disclaimer.
-# 
+#
 # * Redistributions in binary form must reproduce the above copyright
 #   notice, this list of conditions and the following disclaimer in the
 #   documentation and/or other materials provided with the distribution.
-# 
+#
 # * The names of the authors may not be used to endorse or promote
 #   products derived from this software without specific prior written
 #   permission.
