49c9db7f922e778f51b25e22933290afe781658b
Carl Moore <cmoore@brlcad.org>
2012-08-17 19:27:44 +0000

    remove trailing blanks
    svn:revision:52110
    svn:branch:trunk
    svn:account:carlmoore

--- src/adrt/isst.bat
+++ src/adrt/isst.bat
@@ -1,22 +1,22 @@
 REM                        I S S T . B A T
 REM  BRL-CAD
-REM 
+REM
 REM  Copyright (c) 2006-2012 United States Government as represented by
 REM  the U.S. Army Research Laboratory.
-REM 
+REM
 REM  This library is free software; you can redistribute it and/or
 REM  modify it under the terms of the GNU Lesser General Public License
 REM  version 2.1 as published by the Free Software Foundation.
-REM 
+REM
 REM  This library is distributed in the hope that it will be useful, but
 REM  WITHOUT ANY WARRANTY; without even the implied warranty of
 REM  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 REM  Lesser General Public License for more details.
-REM 
+REM
 REM  You should have received a copy of the GNU Lesser General Public
 REM  License along with this file; see the file named COPYING for more
 REM  information.
-REM 
+REM
 
 @ECHO OFF
 
