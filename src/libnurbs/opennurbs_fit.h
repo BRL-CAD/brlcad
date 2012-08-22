7c7ce751c2fb55239bf6c8c232ab27e756c3ebef
Carl Moore <cmoore@brlcad.org>
2012-08-22 20:51:19 +0000

    repair 'Moerwald'
    svn:revision:52161
    svn:branch:trunk
    svn:account:carlmoore

--- src/libnurbs/opennurbs_fit.h
+++ src/libnurbs/opennurbs_fit.h
@@ -5,7 +5,7 @@
  * surface fitting code in the Point Cloud Library (pcl). His code is
  * BSD licensed:
  *
- *  Copyright (c) 2011, Thomas Mörwald, Jonathan Balzer, Inc.
+ *  Copyright (c) 2011, Thomas Moerwald, Jonathan Balzer, Inc.
  *  All rights reserved.
  *
  *  Redistribution and use in source and binary forms, with or without
@@ -18,7 +18,7 @@
  *     copyright notice, this list of conditions and the following
  *     disclaimer in the documentation and/or other materials provided
  *     with the distribution.
- *   * Neither the name of Thomas Mörwald or Jonathan Balzer nor the names of its
+ *   * Neither the name of Thomas Moerwald or Jonathan Balzer nor the names of its
  *     contributors may be used to endorse or promote products derived
  *     from this software without specific prior written permission.
  *
@@ -343,7 +343,7 @@ private:
 
 /** \brief Fitting a B-Spline surface to 3D point-clouds using point-distance-minimization
  *  Based on paper: TODO
- * \author Thomas Mörwald
+ * \author Thomas Moerwald
  * \ingroup surface     */
 class FittingSurface
 {
