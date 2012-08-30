7f1b763cacfcb7b4809096e90b56cfaefd1f92c6
Carl Moore <cmoore@brlcad.org>
2012-08-30 20:53:14 +0000

    fix 1 spelling
    svn:revision:52299
    svn:branch:trunk
    svn:account:carlmoore

--- src/lgt/glob.c
+++ src/lgt/glob.c
@@ -101,7 +101,7 @@ fastf_t	degtorad = 0.0174532925;
 /* Translations of grid in plane of view.				*/
 fastf_t	x_grid_offset = 0.0, y_grid_offset = 0.0;
 
-/* Distance of grid from the model centroid measured in milimeters.	*/
+/* Distance of grid from the model centroid measured in millimeters.	*/
 fastf_t	grid_dist = 0.0;
 
 /* Rotation of grid around viewing axis (radians).			*/
