5f7975a7f7a819902820aba72e2c8eb10342e4d6
Christopher Sean Morrison <brlcad@gmail.com>
2012-10-04 19:50:57 +0000

    fixes sf bug 3574531 reporting compilation error due to type mismatch.  setWindowBounds takes an array of fastf_t, not ints.  surprising if it previously even worked.
    svn:revision:52787
    svn:branch:trunk
    svn:account:brlcad

--- src/util/pl-dm.c
+++ src/util/pl-dm.c
@@ -1056,7 +1056,7 @@ static struct cmdtab cmdtab[] = {
 static int
 X_dmInit()
 {
-    fastf_t windowbounds[6] = { 2047, -2048, 2047, -2048, 2047, -2048 };
+    fastf_t windowbounds[6] = { 2047.0, -2048.0, 2047.0, -2048.0, 2047.0, -2048.0 };
     const char *av[4];
 
     av[0] = "X_open";
@@ -1082,7 +1082,7 @@ X_dmInit()
 static int
 Ogl_dmInit()
 {
-    fastf_t windowbounds[6] = { 2047, -2048, 2047, -2048, 2047, -2048 };
+    fastf_t windowbounds[6] = { 2047.0, -2048.0, 2047.0, -2048.0, 2047.0, -2048.0 };
     char *av[4];
 
     av[0] = "Ogl_open";
