a03de45e636cac69a5371307409c9160f44339d2
Christopher Sean Morrison <brlcad@gmail.com>
2012-10-04 05:25:11 +0000

    flesh out declarations for all of the null interface callbacks so that they may be usable by the other manager interfaces
    svn:revision:52756
    svn:branch:trunk
    svn:account:brlcad

--- include/dm-Null.h
+++ include/dm-Null.h
@@ -25,7 +25,152 @@
 #ifndef __DM_NULL__
 #define __DM_NULL__
 
-/* yep, nothing to declare */
+DM_EXPORT extern struct dm dm_null;
+
+
+DM_EXPORT extern int
+null_close(struct dm *dmp);
+
+
+DM_EXPORT extern int
+null_drawBegin(struct dm *dmp);
+
+
+DM_EXPORT extern int
+null_drawEnd(struct dm *dmp);
+
+
+DM_EXPORT extern int
+null_normal(struct dm *dmp);
+
+
+DM_EXPORT extern int
+null_loadMatrix(struct dm *dmp, fastf_t *mat, int which_eye);
+
+
+DM_EXPORT extern int
+null_drawString2D(struct dm *dmp, const char *str, fastf_t x, fastf_t y, int size, int use_aspect);
+
+
+DM_EXPORT extern int
+null_drawLine2D(struct dm *dmp, fastf_t x_1, fastf_t y_1, fastf_t x_2, fastf_t y_2);
+
+
+DM_EXPORT extern int
+null_drawLine3D(struct dm *dmp, point_t pt1, point_t pt2);
+
+
+DM_EXPORT extern int
+null_drawLines3D(struct dm *dmp, int npoints, point_t *points, int sflag);
+
+
+DM_EXPORT extern int
+null_drawPoint2D(struct dm *dmp, fastf_t x, fastf_t y);
+
+
+DM_EXPORT extern int
+null_drawPoint3D(struct dm *dmp, point_t point);
+
+
+DM_EXPORT extern int
+null_drawPoints3D(struct dm *dmp, int npoints, point_t *points);
+
+
+DM_EXPORT extern int
+null_drawVList(struct dm *dmp, struct bn_vlist *vp);
+
+
+DM_EXPORT extern int
+null_drawVListHiddenLine(struct dm *dmp, struct bn_vlist *vp);
+
+
+DM_EXPORT extern int
+null_draw(struct dm *dmp, struct bn_vlist *(*callback_function)(void *), genptr_t *data);
+
+
+DM_EXPORT extern int
+null_setFGColor(struct dm *dmp, unsigned char r, unsigned char g, unsigned char b, int strict, fastf_t transparency);
+
+
+DM_EXPORT extern int
+null_setBGColor(struct dm *dmp, unsigned char r, unsigned char g, unsigned char b);
+
+
+DM_EXPORT extern int
+null_setLineAttr(struct dm *dmp, int width, int style);
+
+
+DM_EXPORT extern int
+null_configureWin(struct dm *dmp, int force);
+
+
+DM_EXPORT extern int
+null_setWinBounds(struct dm *dmp, fastf_t *w);
+
+
+DM_EXPORT extern int
+null_setLight(struct dm *dmp, int light_on);
+
+
+DM_EXPORT extern int
+null_setTransparency(struct dm *dmp, int transparency);
+
+
+DM_EXPORT extern int
+null_setDepthMask(struct dm *dmp, int mask);
+
+
+DM_EXPORT extern int
+null_setZBuffer(struct dm *dmp, int zbuffer_on);
+
+
+DM_EXPORT extern int
+null_debug(struct dm *dmp, int lvl);
+
+
+DM_EXPORT extern int
+null_beginDList(struct dm *dmp, unsigned int list);
+
+
+DM_EXPORT extern int
+null_endDList(struct dm *dmp);
+
+
+DM_EXPORT extern void
+null_drawDList(unsigned int list);
+
+
+DM_EXPORT extern int
+null_freeDLists(struct dm *dmp, unsigned int list, int range);
+
+
+DM_EXPORT extern int
+null_genDLists(struct dm *dmp, size_t range);
+
+
+DM_EXPORT extern int
+null_getDisplayImage(struct dm *dmp, unsigned char **image);
+
+
+DM_EXPORT extern int
+null_draw(struct dm *dmp, struct bn_vlist *(*callback_function)(void *), genptr_t *data);
+
+
+DM_EXPORT extern int
+null_fg(struct dm *dmp, unsigned char r, unsigned char g, unsigned char b, int strict, fastf_t transparency);
+
+
+DM_EXPORT extern int
+null_bg(struct dm *dmp, unsigned char r, unsigned char g, unsigned char b);
+
+
+DM_EXPORT extern int
+null_getDisplayImage(struct dm *dmp, unsigned char **image);
+
+
+DM_EXPORT extern void
+null_reshape(struct dm *dmp, int width, int height);
+
 
 #endif  /* __DM_NULL__ */
 
