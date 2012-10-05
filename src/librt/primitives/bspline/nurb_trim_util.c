cff5b066884e1dfd0dbe03ff793bb1b0abca1dc8
Carl Moore <cmoore@brlcad.org>
2012-10-05 18:23:08 +0000

    fix spelling (including 'wonders' to 'wanders') and change 'is' to 'it' and add missing period)
    svn:revision:52844
    svn:branch:trunk
    svn:account:carlmoore

--- src/librt/primitives/bspline/nurb_trim_util.c
+++ src/librt/primitives/bspline/nurb_trim_util.c
@@ -37,11 +37,11 @@
 #include "nurb.h"
 
 /**
- * Check to see if the curve conmtrol polygon wonders outside the
- * parametric range given.  This is usefull if a trimming curve
+ * Check to see if the curve control polygon wanders outside the
+ * parametric range given.  This is useful if a trimming curve
  * control polygon is outside but the evaluated curve is not. We will
- * want to refine the curve so that is lies within the range;
- * otherwise it breaks the surface evaluation
+ * want to refine the curve so that it lies within the range;
+ * otherwise it breaks the surface evaluation.
  */
 int
 rt_nurb_crv_in_range(struct edge_g_cnurb *crv, fastf_t u_min, fastf_t u_max, fastf_t v_min, fastf_t v_max)
