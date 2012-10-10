6d4a75e915c9363ddee5aaa2b5bf8f0bca59d42a
Carl Moore <cmoore@brlcad.org>
2012-10-10 19:42:24 +0000

    fix spelling
    svn:revision:52971
    svn:branch:trunk
    svn:account:carlmoore

--- src/librt/primitives/obj_vshot.c
+++ src/librt/primitives/obj_vshot.c
@@ -40,7 +40,7 @@ rt_obj_vshot(struct soltab *stp[], struct xray *rp[], struct seg *segp, int n, s
     if (segp) RT_CK_SEG(segp);
     if (ap) RT_CK_APPLICATION(ap);
 
-    /* should be improved, verify homogenous collection */
+    /* should be improved, verify homogeneous collection */
     id = stp[0]->st_id;
     if (id < 0)
 	return -2;
