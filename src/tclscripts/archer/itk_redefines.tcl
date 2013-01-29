af713150f4927772f6144f74a5b4a038fe8720fb
Christopher Sean Morrison <brlcad@gmail.com>
2013-01-29 06:23:29 +0000

    missing header/footer added
    svn:revision:54214
    svn:branch:trunk
    svn:account:brlcad

--- src/tclscripts/archer/itk_redefines.tcl
+++ src/tclscripts/archer/itk_redefines.tcl
@@ -1,3 +1,23 @@
+#               I T K _ R E D E F I N E S . T C L
+# BRL-CAD
+#
+# Copyright (c) 2013 United States Government as represented by
+# the U.S. Army Research Laboratory.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public License
+# version 2.1 as published by the Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this file; see the file named COPYING for more
+# information.
+#
+###
 catch {itcl::delete class itk::Toplevel}
 
 itcl::class itk::Toplevel {
@@ -51,3 +71,12 @@ itcl::class itk::Toplevel {
 
     private variable itk_hull ""
 }
+
+# Local Variables:
+# tab-width: 8
+# mode: Tcl
+# c-basic-offset: 4
+# tcl-indent-level: 4
+# indent-tabs-mode: t
+# End:
+# ex: shiftwidth=4 tabstop=8
