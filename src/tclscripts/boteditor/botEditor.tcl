ab89cf087905620429ed5803d585192c78f78507
Christopher Sean Morrison <brlcad@gmail.com>
2013-01-29 06:19:17 +0000

    add missing headers and footers
    svn:revision:54213
    svn:branch:trunk
    svn:account:brlcad

--- src/tclscripts/boteditor/botEditor.tcl
+++ src/tclscripts/boteditor/botEditor.tcl
@@ -1,3 +1,23 @@
+#                   B O T E D I T O R . T C L
+# BRL-CAD
+#
+# Copyright (c) 2013 United States Government as represented by
+# the U.S. Army Research Laboratory.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public License
+# version 2.1 as published by the Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this file; see the file named COPYING for more
+# information.
+#
+###
 # BotEditor class for editing a BoT primitive
 #
 # Usage: BotEditor <instance name> <bot name> [-prefix <Archer instance name>]
@@ -490,3 +510,12 @@ if {[catch {
 	method update {bot} {$itk_component(props) update $bot}
     }
 }
+
+# Local Variables:
+# tab-width: 8
+# mode: Tcl
+# c-basic-offset: 4
+# tcl-indent-level: 4
+# indent-tabs-mode: t
+# End:
+# ex: shiftwidth=4 tabstop=8
--- src/tclscripts/boteditor/botTools.tcl
+++ src/tclscripts/boteditor/botTools.tcl
@@ -1,3 +1,23 @@
+#                    B O T T O O L S . T C L
+# BRL-CAD
+#
+# Copyright (c) 2013 United States Government as represented by
+# the U.S. Army Research Laboratory.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public License
+# version 2.1 as published by the Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this file; see the file named COPYING for more
+# information.
+#
+###
 # BotTools class for wrapping BoT commands
 #
 # Usage: BotTools <instance name> <bot name> \
@@ -609,3 +629,12 @@ package require Itk
 
     loadDecimateSuggestions
 }
+
+# Local Variables:
+# tab-width: 8
+# mode: Tcl
+# c-basic-offset: 4
+# tcl-indent-level: 4
+# indent-tabs-mode: t
+# End:
+# ex: shiftwidth=4 tabstop=8
