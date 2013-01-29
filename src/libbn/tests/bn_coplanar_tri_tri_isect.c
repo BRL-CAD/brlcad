c2bc323e9a459053d620b1f864b346b975564ba6
Christopher Sean Morrison <brlcad@gmail.com>
2013-01-29 07:21:02 +0000

    update copyright for 2013 welcoming our files to the year of slithering slathering serpent subjects
    svn:revision:54217
    svn:branch:trunk
    svn:account:brlcad

--- src/libbn/tests/bn_coplanar_tri_tri_isect.c
+++ src/libbn/tests/bn_coplanar_tri_tri_isect.c
@@ -1,7 +1,7 @@
 /*         B N _ C O P L A N A R _ T R I _ T R I _ ISECT . C
  * BRL-CAD
  *
- * Copyright (c) 2011-2012 United States Government as represented by
+ * Copyright (c) 2011-2013 United States Government as represented by
  * the U.S. Army Research Laboratory.
  *
  * This library is free software; you can redistribute it and/or
--- src/libbn/tests/bn_tri_tri_isect.c
+++ src/libbn/tests/bn_tri_tri_isect.c
@@ -1,7 +1,7 @@
 /*                 B N _ T R I _ T R I _ ISECT . C
  * BRL-CAD
  *
- * Copyright (c) 2011-2012 United States Government as represented by
+ * Copyright (c) 2011-2013 United States Government as represented by
  * the U.S. Army Research Laboratory.
  *
  * This library is free software; you can redistribute it and/or
