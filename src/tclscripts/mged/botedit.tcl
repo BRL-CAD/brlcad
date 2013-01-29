07a706877f877d86baab93fedd5982f61c92fd1d
Christopher Sean Morrison <brlcad@gmail.com>
2013-01-29 06:12:15 +0000

    add missing header and footer
    svn:revision:54211
    svn:branch:trunk
    svn:account:brlcad

--- src/tclscripts/mged/botedit.tcl
+++ src/tclscripts/mged/botedit.tcl
@@ -1,3 +1,23 @@
+#                     B O T E D I T . T C L
+# BRL-CAD
+#
+# Copyright (c) 2013 United States Government as represented by
+# the U.S. Army Research Laboratory.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public License
+# version 2.1 as published by the Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this file; see the file named COPYING for more
+# information.
+#
+###
 # manually sourcing dependencies for now
 if {[catch {
     set script [file join [bu_brlcad_data "tclscripts"] boteditor botEditor.tcl]
@@ -15,3 +35,12 @@ proc bot_askforname {parent screen} {
     }
     BotEditor .botedit $botname
 }
+
+# Local Variables:
+# tab-width: 8
+# mode: Tcl
+# c-basic-offset: 4
+# tcl-indent-level: 4
+# indent-tabs-mode: t
+# End:
+# ex: shiftwidth=4 tabstop=8
