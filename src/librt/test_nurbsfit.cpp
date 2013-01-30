e6dc8e930dfda7866614d396c4a47007df42bd1a
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2013-01-30 23:00:57 +0000

    Add standard header for test_nurbsfit.cpp
    svn:revision:54239
    svn:branch:trunk
    svn:account:starseeker

--- src/librt/test_nurbsfit.cpp
+++ src/librt/test_nurbsfit.cpp
@@ -1,3 +1,30 @@
+/*               T E S T _ N U R B S F I T . C P P
+ * BRL-CAD
+ *
+ * Copyright (c) 2013 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file test_nurbsfit.cpp
+ *
+ * Test the fitting routines that map a NURBS surface to a BoT patch.
+ * *Very* simple - doesn't validate that the BoT projects to a plane
+ * without overlaps.
+ *
+ */
+
 #include "common.h"
 #include "vmath.h"
 #include "raytrace.h"
