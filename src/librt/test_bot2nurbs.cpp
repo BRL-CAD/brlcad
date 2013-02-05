e189e3bb3c38b4747f8ec3855f569e64da6ee8df
Carl Moore <cmoore@brlcad.org>
2013-02-05 15:21:38 +0000

    remove trailing blank or tab
    svn:revision:54303
    svn:branch:trunk
    svn:account:carlmoore

--- src/librt/test_bot2nurbs.cpp
+++ src/librt/test_bot2nurbs.cpp
@@ -1565,7 +1565,7 @@ int main(int argc, char *argv[])
 
     // For debugging, generate individual face BReps
     for (int fc = 0; fc < info.brep->m_F.Count(); fc++) {
-	ON_Brep *brep_face = info.brep->DuplicateFace(fc, false); 
+	ON_Brep *brep_face = info.brep->DuplicateFace(fc, false);
 	bu_vls_sprintf(&bname, "%s_face_%d", argv[2], fc);
 	if (mk_brep(wdbp, bu_vls_addr(&bname), brep_face) == 0) {
 	    bu_log("Generated brep object %s\n", bu_vls_addr(&bname));
