2dc545b26a2fb60d0e6885ea586a98ca106d9429
Christopher Sean Morrison <brlcad@gmail.com>
2013-03-07 02:56:02 +0000

    call malloc instead of get for now since the latter needs to be paired with BU_PUT calls
    svn:revision:54549
    svn:branch:trunk
    svn:account:brlcad

--- src/librt/comb/comb.c
+++ src/librt/comb/comb.c
@@ -480,7 +480,7 @@ rt_comb_import5(struct rt_db_internal *ip, const struct bu_external *ep,
     ip->idb_major_type = DB5_MAJORTYPE_BRLCAD;
     ip->idb_type = ID_COMBINATION;
     ip->idb_meth = &rt_functab[ID_COMBINATION];
-    BU_GET(comb, struct rt_comb_internal);
+    comb = bu_malloc(sizeof(struct rt_comb_internal), "comb");
     RT_COMB_INTERNAL_INIT(comb);
 
     ip->idb_ptr = (genptr_t)comb;
