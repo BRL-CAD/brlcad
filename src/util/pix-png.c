81e878a89a77d6a66f78669705a6abe6dbb4b450
Carl Moore <cmoore@brlcad.org>
2013-03-21 20:46:46 +0000

    reverted r54804 because we need to accept piped input too
    svn:revision:54813
    svn:branch:trunk
    svn:account:carlmoore

--- src/util/pix-png.c
+++ src/util/pix-png.c
@@ -202,9 +202,6 @@ main(int argc, char *argv[])
     infp = stdin;
     outfp = stdout;
 
-    if (argc == 1) {
-	bu_exit(1, "%s\n", usage);
-    }
     if (!get_args(argc, argv, &file_width, &file_height, &infp, &outfp)) {
 	bu_exit(1, "%s\n", usage);
     }
