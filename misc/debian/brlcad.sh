e188c0e771fb6471a5439e32e636590339df4899
Jordi Sayol <g.sayol@gmail.com>
2013-04-05 22:16:14 +0000

    Verify if $BRLCAD_PATH and $BRLCAD_MANPATH are already in $PATH and $MANPATH environment variables (deb and rpm packages).
    svn:revision:55071
    svn:branch:trunk
    svn:account:jordisayol

--- misc/debian/brlcad.sh
+++ misc/debian/brlcad.sh
@@ -4,13 +4,17 @@ BRLCAD_MANPATH=/usr/brlcad/share/man
 if [ -z "$PATH" ]
 then
 	export PATH=${BRLCAD_PATH}
-else
+# check if $BRLCAD_PATH is already in $PATH
+elif awk -F: -v j=$BRLCAD_PATH '{for(i=1;i<=NF;i++){if($i == j){exit 1}}}'<<<$PATH
+then
 	export PATH=${PATH}:${BRLCAD_PATH}
 fi
 
 if [ -z "$MANPATH" ]
 then
 	export MANPATH=${BRLCAD_MANPATH}
-else
+# check if $BRLCAD_MANPATH is already in $MANPATH
+elif awk -F: -v j=$BRLCAD_MANPATH '{for(i=1;i<=NF;i++){if($i == j){exit 1}}}'<<<$MANPATH
+then
 	export MANPATH=${MANPATH}:${BRLCAD_MANPATH}
 fi
