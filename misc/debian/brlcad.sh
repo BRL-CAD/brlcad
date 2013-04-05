9feafa823e430e50ae6799be4097cc8d219c2eb2
Jordi Sayol <g.sayol@gmail.com>
2013-04-05 12:59:25 +0000

    Properly add/modify the environment variables PATH and MANPATH on deb and rpm packages.
    svn:revision:55060
    svn:branch:trunk
    svn:account:jordisayol

--- misc/debian/brlcad.sh
+++ misc/debian/brlcad.sh
@@ -1,2 +1,16 @@
-export PATH=$PATH:/usr/brlcad/bin
-export MANPATH=$MANPATH:/usr/brlcad/share/man
+BRLCAD_PATH=/usr/brlcad/bin
+BRLCAD_MANPATH=/usr/brlcad/share/man
+
+if [ -z "$PATH" ]
+then
+	export PATH=${BRLCAD_PATH}
+else
+	export PATH=${PATH}:${BRLCAD_PATH}
+fi
+
+if [ -z "$MANPATH" ]
+then
+	export MANPATH=${BRLCAD_MANPATH}
+else
+	export MANPATH=${MANPATH}:${BRLCAD_MANPATH}
+fi
