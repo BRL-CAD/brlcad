f84051ff678cfeb46ed0fde6f71c82704ca5ad6b
Christopher Sean Morrison <brlcad@gmail.com>
2013-04-25 03:10:41 +0000

    if one section gets curlies, the other should too
    svn:revision:55237
    svn:branch:trunk
    svn:account:brlcad

--- src/util/cv.c
+++ src/util/cv.c
@@ -69,17 +69,17 @@ main(int argc, char **argv)
     }
 
     in_pat = argv[1];
-    if (BU_STR_EQUAL(in_pat, ""))
+    if (BU_STR_EQUAL(in_pat, "")) {
 	in_pat = "huc";
-    else if (strlen(in_pat) > 4 || strlen(in_pat) < 1) {
+    } else if (strlen(in_pat) > 4 || strlen(in_pat) < 1) {
 	fprintf(stderr, "cv: unrecognized input pattern\n");
 	return 1;
     }
 
     out_pat = argv[2];
-    if (BU_STR_EQUAL(out_pat, ""))
+    if (BU_STR_EQUAL(out_pat, "")) {
 	out_pat = "nuc";
-    else if (strlen(out_pat) > 4 || strlen(out_pat) < 1) {
+    } else if (strlen(out_pat) > 4 || strlen(out_pat) < 1) {
 	fprintf(stderr, "cv: unrecognized output pattern\n");
 	return 1;
     }
