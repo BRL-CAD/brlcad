0d3f3a7d7f79ba36e1e5f44778648b5486302fa6
Carl Moore <cmoore@brlcad.org>
2013-05-08 13:32:03 +0000

    fix type warnings
    svn:revision:55325
    svn:branch:trunk
    svn:account:carlmoore

--- src/liboptical/sh_text.c
+++ src/liboptical/sh_text.c
@@ -175,7 +175,7 @@ txt_load_datasource(struct txt_specific *texture, struct db_i *dbInstance, const
 
 	    /* check size of object */
 	    if (texture->tx_binunifp->count < size) {
-		bu_log("\nWARNING: %V needs %d bytes, binary object only has %d\n", texture->tx_name, size, texture->tx_binunifp->count);
+		bu_log("\nWARNING: %V needs %d bytes, binary object only has %lu\n", texture->tx_name, size, texture->tx_binunifp->count);
 	    } else if (texture->tx_binunifp->count > size) {
 		bu_log("\nWARNING: Binary object is larger than specified texture size\n\tBinary Object: %zu pixels\n\tSpecified Texture Size: %zu pixels\n...continuing to load using image subsection...", texture->tx_binunifp->count);
 	    }
@@ -193,9 +193,9 @@ txt_load_datasource(struct txt_specific *texture, struct db_i *dbInstance, const
 	    return -1;				/* FAIL */
 
 	if (texture->tx_mp->buflen < size) {
-	    bu_log("\nWARNING: %V needs %d bytes, file only has %d\n", &texture->tx_name, size, texture->tx_mp->buflen);
+	    bu_log("\nWARNING: %V needs %d bytes, file only has %lu\n", &texture->tx_name, size, texture->tx_mp->buflen);
 	} else if (texture->tx_mp->buflen > size) {
-	    bu_log("\nWARNING: Texture file size is larger than specified texture size\n\tInput File: %zu pixels\n\tSpecified Texture Size: %d pixels\n...continuing to load using image subsection...", texture->tx_mp->buflen, size);
+	    bu_log("\nWARNING: Texture file size is larger than specified texture size\n\tInput File: %zu pixels\n\tSpecified Texture Size: %lu pixels\n...continuing to load using image subsection...", texture->tx_mp->buflen, size);
 	}
 
     }
