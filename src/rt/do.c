3cefbda262ce207f6e3ad820aa41c05c3613fc2f
Christopher Sean Morrison <brlcad@gmail.com>
2013-07-15 05:00:05 +0000

    ordering for pre c99
    svn:revision:56052
    svn:branch:trunk
    svn:account:brlcad

--- src/rt/do.c
+++ src/rt/do.c
@@ -771,8 +771,8 @@ do_frame(int framenumber)
 
 		    /* Read existing pix data into the frame buffer */
 		    if (sb.st_size > 0) {
-			bu_log("Reading existing pix data from \"%s\".\n", framename);
 			size_t bytes_read = fread(pixmap, 1, (size_t)sb.st_size, outfp);
+			bu_log("Reading existing pix data from \"%s\".\n", framename);
 			if (bytes_read < (size_t)sb.st_size)
 			    return -1;
 		    }
