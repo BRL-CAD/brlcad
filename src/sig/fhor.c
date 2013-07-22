a6ec43f56a866416e424d7656915bdfef814790e
Christopher Sean Morrison <brlcad@gmail.com>
2013-07-22 23:08:55 +0000

    resort in usage order. eliminate dead code and need for forward declarations.
    svn:revision:56181
    svn:branch:trunk
    svn:account:brlcad

--- src/sig/fhor.c
+++ src/sig/fhor.c
@@ -50,16 +50,21 @@
 /* Max and Min horizon holders */
 static int upper[HSCREEN], lower[HSCREEN];
 
-FBIO *fbp;	/* XXX - debug */
+FBIO *fbp = NULL;	/* XXX - debug */
 
-void Efill(void);
-void Horizon(int sx1, int sy1, int sx2, int sy2);
-void Intersect(int sx1, int sy1, int x2, int y2, int *hor, int *xi, int *yi);
-void Draw(int sx1, int sy1, int x2, int y2);
-int fhvis(int x, int y);
-int sign(int i);
 
-void
+static int
+sign(int i)
+{
+    if (i > 0)
+	return 1;
+    if (i < 0)
+	return -1;
+    return 0;
+}
+
+
+static void
 fhinit(void)
 {
     int i;
@@ -73,10 +78,155 @@ fhinit(void)
 
 
 /*
+ * INTERNAL Visibility routine.
+ * Answers, Is Y visible at point X?
+ *
+ * Returns: 0 if invisible
+ * 1 if visible above upper horizon.
+ * -1 if visible below lower horizon.
+ */
+static int
+fhvis(int x, int y)
+{
+    /* See if hidden behind horizons */
+    if (y < upper[x] && y > lower[x])
+	return INVISIBLE;
+
+    if (y >= upper[x])
+	return ABOVE;
+
+    return BELOW;
+}
+
+
+/*
+ * INTERNAL Edge fill routine.
+ * NOT DONE YET.
+ */
+static void
+Efill(void)
+{
+}
+
+
+/*
+ * Fill the upper and lower horizon arrays from x_1 to x_2
+ * with a line spanning (x_1, y_1) to (x_2, y_2).
+ */
+static void
+Horizon(int x_1, int y_1, int x_2, int y_2)
+{
+    int xinc, x, y;
+    double slope;
+
+    xinc = sign(x_2 - x_1);
+    if (xinc == 0) {
+	/* Vertical line */
+	upper[x_2] = MAX(upper[x_2], y_2);
+	lower[x_2] = MIN(lower[x_2], y_2);
+    } else {
+	slope = (y_2 - y_1) / (x_2 - x_1);
+	for (x = x_1; x <= x_2; x += xinc) {
+	    y = slope * (x - x_1) + y_1;
+	    upper[x] = MAX(upper[x], y);
+	    lower[x] = MIN(lower[x], y);
+	}
+    }
+}
+
+
+/*
+ * DRAW - plot a line from (x_1, y_1) to (x_2, y_2)
+ * An integer Bresenham algorithm for any quadrant.
+ */
+static void
+Draw(int x_1, int y_1, int x_2, int y_2)
+{
+    int x, y, deltx, delty, error, i;
+    int temp, s1, s2, interchange;
+    static RGBpixel white = { 255, 255, 255 };	/* XXX - debug */
+
+    x = x_1;
+    y = y_1;
+    deltx = (x_2 > x_1 ? x_2 - x_1 : x_1 - x_2);
+    delty = (y_2 > y_1 ? y_2 - y_1 : y_1 - y_2);
+    s1 = sign(x_2 - x_1);
+    s2 = sign(y_2 - y_1);
+
+    /* check for swap of deltx and delty */
+    if (delty > deltx) {
+	temp = deltx;
+	deltx = delty;
+	delty = temp;
+	interchange = 1;
+    } else
+	interchange = 0;
+
+    /* init error term */
+    error = 2 * delty - deltx;
+
+    for (i = 0; i < deltx; i++) {
+	fb_write(fbp, x, y, white, 1);
+	while (error >= 0) {
+	    if (interchange == 1)
+		x += s1;
+	    else
+		y += s2;
+	    error -= 2 * deltx;
+	}
+	if (interchange == 1)
+	    y += s2;
+	else
+	    x += s1;
+	error += 2 * delty;
+    }
+}
+
+
+/*
+ * Find the intersection (xi, yi) between the line (x_1, y_1)->(x_2, y_2)
+ * and the horizon hor[].
+ */
+void
+Intersect(int x_1, int y_1, int x_2, int y_2, int *hor, int *xi, int *yi)
+{
+    int xinc, ysign;
+    int slope;
+
+    xinc = sign(x_2 - x_1);
+    if (xinc == 0) {
+	/* Vertical line */
+	*xi = x_2;
+	*yi = hor[x_2];
+    } else {
+	slope = (y_2 - y_1) / (x_2 - x_1);
+	ysign = sign(y_1 - hor[x_1 + xinc]);
+#ifdef MYMETHOD
+	for (*xi = x_1; *xi <= x_2; *xi += xinc) {
+	    *yi = y_1 + (*xi-x_1)*slope;	/* XXX */
+	    if (sign(*yi - hor[*xi + xinc]) != ysign)
+		break;
+	}
+	if (xinc == 1 && *xi > x_2) *xi = x_2;
+	if (xinc == -1 && *xi < x_2) *xi = x_2;
+#else
+	*yi = y_1;
+	*xi = x_1;
+	while (sign(*yi - hor[*xi + xinc]) == ysign) {
+	    for (*xi = x_1; *xi <= x_2; *xi += xinc)
+		*yi = *yi + slope;	/* XXX */
+	}
+	*xi = *xi + xinc;
+#endif /* MYMETHOD */
+    }
+}
+
+
+/*
  * Add another Z cut to the display.
  * This one goes "behind" the last one.
  */
-void
+static void
 fhnewz(int *f, int num)
 {
     int x, y, Xprev, Yprev, Xi, Yi;
@@ -170,218 +320,11 @@ fhnewz(int *f, int num)
 }
 
 
-/*
- * INTERNAL Visibility routine.
- * Answers, Is Y visible at point X?
- *
- * Returns: 0 if invisible
- * 1 if visible above upper horizon.
- * -1 if visible below lower horizon.
- */
 int
-fhvis(int x, int y)
+main(int argc, char **argv)
 {
-    /* See if hidden behind horizons */
-    if (y < upper[x] && y > lower[x])
-	return INVISIBLE;
+    static const char usage[] = "Usage: fhor [width] < doubles\n";
 
-    if (y >= upper[x])
-	return ABOVE;
-
-    return BELOW;
-}
-
-
-/*
- * INTERNAL Edge fill routine.
- * NOT DONE YET.
- */
-void
-Efill(void)
-{
-}
-
-
-/*
- * Fill the upper and lower horizon arrays from x_1 to x_2
- * with a line spanning (x_1, y_1) to (x_2, y_2).
- */
-void
-Horizon(int x_1, int y_1, int x_2, int y_2)
-{
-    int xinc, x, y;
-    double slope;
-
-    xinc = sign(x_2 - x_1);
-    if (xinc == 0) {
-	/* Vertical line */
-	upper[x_2] = MAX(upper[x_2], y_2);
-	lower[x_2] = MIN(lower[x_2], y_2);
-    } else {
-	slope = (y_2 - y_1) / (x_2 - x_1);
-	for (x = x_1; x <= x_2; x += xinc) {
-	    y = slope * (x - x_1) + y_1;
-	    upper[x] = MAX(upper[x], y);
-	    lower[x] = MIN(lower[x], y);
-	}
-    }
-}
-
-
-/*
- * Find the intersection (xi, yi) between the line (x_1, y_1)->(x_2, y_2)
- * and the horizon hor[].
- */
-void
-Intersect(int x_1, int y_1, int x_2, int y_2, int *hor, int *xi, int *yi)
-{
-    int xinc, ysign;
-    int slope;
-
-/*
-  printf("Intersect((%3d, %3d)->(%3d, %3d) & (%3d, %3d)->(%3d, %3d)) = ", x_1, y_1, x_2, y_2, x_1, hor[x_1], x_2, hor[x_2]);
-  fflush(stdout);
-*/
-    xinc = sign(x_2 - x_1);
-    if (xinc == 0) {
-	/* Vertical line */
-	*xi = x_2;
-	*yi = hor[x_2];
-/*printf("(vert x_2=%d) ", x_2);*/
-    } else {
-#ifdef FOOBARBAZ
-	denom = (hor[x_2]-hor[x_1])-(y_2-y_1);
-	if (denom == 0) {
-	    /* same line! */
-	    *xi = x_1;
-	} else
-	    *xi = x_1 + ((x_2-x_1)*(hor[x_1]-y_1))/denom;
-	*yi = y_1 + (*xi-x_1)*((y_2-y_1)/(x_2-x_1)) + 0.5;
-/*printf("(%3d, %3d)\n", *xi, *yi);*/
-	return;
-#endif /* FOOBARBAZ */
-
-	slope = (y_2 - y_1) / (x_2 - x_1);
-	ysign = sign(y_1 - hor[x_1 + xinc]);
-#ifdef MYMETHOD
-	for (*xi = x_1; *xi <= x_2; *xi += xinc) {
-	    *yi = y_1 + (*xi-x_1)*slope;	/* XXX */
-	    if (sign(*yi - hor[*xi + xinc]) != ysign)
-		break;
-	}
-	if (xinc == 1 && *xi > x_2) *xi = x_2;
-	if (xinc == -1 && *xi < x_2) *xi = x_2;
-#else
-	*yi = y_1;
-	*xi = x_1;
-	while (sign(*yi - hor[*xi + xinc]) == ysign) {
-	    for (*xi = x_1; *xi <= x_2; *xi += xinc)
-		*yi = *yi + slope;	/* XXX */
-/*printf("[%3d, %3d]", *xi, *yi);*/
-	}
-	*xi = *xi + xinc;
-#endif /* MYMETHOD */
-    }
-/*printf("(%3d, %3d)\n", *xi, *yi);*/
-}
-
-
-int
-sign(int i)
-{
-    if (i > 0)
-	return 1;
-    if (i < 0)
-	return -1;
-    return 0;
-}
-
-
-/*
- * DRAW - plot a line from (x_1, y_1) to (x_2, y_2)
- * An integer Bresenham algorithm for any quadrant.
- */
-void
-Draw(int x_1, int y_1, int x_2, int y_2)
-{
-    int x, y, deltx, delty, error, i;
-    int temp, s1, s2, interchange;
-    static RGBpixel white = { 255, 255, 255 };	/* XXX - debug */
-
-/*printf("Draw (%d %d) -> (%d %d)\n", x_1, y_1, x_2, y_2);*/
-    x = x_1;
-    y = y_1;
-    deltx = (x_2 > x_1 ? x_2 - x_1 : x_1 - x_2);
-    delty = (y_2 > y_1 ? y_2 - y_1 : y_1 - y_2);
-    s1 = sign(x_2 - x_1);
-    s2 = sign(y_2 - y_1);
-
-    /* check for swap of deltx and delty */
-    if (delty > deltx) {
-	temp = deltx;
-	deltx = delty;
-	delty = temp;
-	interchange = 1;
-    } else
-	interchange = 0;
-
-    /* init error term */
-    error = 2 * delty - deltx;
-
-    for (i = 0; i < deltx; i++) {
-/* plotxy(x, y);*/
-/* printf("(%3d, %3d)\n", x, y);*/
-	fb_write(fbp, x, y, white, 1);
-	while (error >= 0) {
-	    if (interchange == 1)
-		x += s1;
-	    else
-		y += s2;
-	    error -= 2 * deltx;
-	}
-	if (interchange == 1)
-	    y += s2;
-	else
-	    x += s1;
-	error += 2 * delty;
-    }
-}
-
-
-#ifdef SOMBRERO
-int main()
-{
-    int f[500];
-    int x, y, z;
-    double r;
-
-    fhinit();
-
-    fbp = fb_open(NULL, 512, 512);
-    fb_clear(fbp, PIXEL_NULL);
-
-    /* Nearest to Farthest */
-    for (z = 500; z > 0; z--) {
-	/* Left to Right */
-	for (x = 0; x < 500; x++) {
-	    r = (x - 250) * (x - 250) + (z - 250) * (z - 250);
-	    r = 0.10*sqrt(r) + 0.00001;
-	    y = 250.0 * sin(r) / r + 100.0 + (500-z)/3;
-	    f[x] = y;
-/* printf("f[%3d] = %d\n", x, y);*/
-	}
-	fhnewz(f, 500);
-    }
-
-    return 0;
-}
-#endif
-
-static const char usage[] = "\
-Usage: fhor [width] < doubles\n";
-
-int main(int argc, char **argv)
-{
     double inbuf[512];
     int f[512];
     int i, x, z;
@@ -414,7 +357,6 @@ int main(int argc, char **argv)
     z = 0;
     while (fread(inbuf, sizeof(*inbuf), size, stdin) != 0) {
 	/* Left to Right */
-	/*memset((char *)f, 0, 512*sizeof(*f));*/
 	for (i = 0; i < 512; i++) {
 	    f[i] = 4*z;	/* up 4 for every z back */
 	}
@@ -423,7 +365,6 @@ int main(int argc, char **argv)
 	    if (x >= 0 && x < 512) {
 		f[x] += 128 * inbuf[i];
 	    }
-	    /*printf("f[%3d] = %d\n", x, y);*/
 	}
 	fhnewz(f, 512);
 	z++;
