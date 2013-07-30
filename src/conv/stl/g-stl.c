65bd4483195075f7764d11162a764e1325192d77
Daniel Rossberg <33295041+drossberg@users.noreply.github.com>
2013-07-30 11:14:36 +0000

    apply Jonathan's patch from https://sourceforge.net/p/brlcad/patches/195/ - updated the usage strings and man pages, and made them a little more readable
    svn:revision:56321
    svn:branch:trunk
    svn:account:d_rossberg

--- src/conv/stl/g-stl.c
+++ src/conv/stl/g-stl.c
@@ -53,7 +53,8 @@
 }
 
 
-static char usage[] = "Usage: %s [-bviM8] [-xX lvl] [-a abs_tess_tol] [-r rel_tess_tol] [-n norm_tess_tol] [-D dist_calc_tol] [-o output_file_name.stl | -m directory_name] brlcad_db.g object(s)\n";
+static char usage[] = "Usage: %s [-bvi8] [-xX lvl] [-P num_cpus] [-a abs_tess_tol] [-r rel_tess_tol] [-n norm_tess_tol] [-D dist_calc_tol] [-o output_file_name.stl | -m directory_name] brlcad_db.g object(s)\n";
+
 
 static int verbose;
 static int NMG_debug;			/* saved arg of -X, for longjmp handling */
