7fd0dbdd8859b25970b4828d2bb119910719c4fe
Daniel Rossberg <33295041+drossberg@users.noreply.github.com>
2013-08-01 08:50:18 +0000

    added some flesh to the function stubs, they will comile with the strict flag now
    svn:revision:56406
    svn:branch:trunk
    svn:account:d_rossberg

--- misc/win32-msvc/Dll/TclDummies.c
+++ misc/win32-msvc/Dll/TclDummies.c
@@ -32,66 +32,68 @@
  * $Id$
  */
 
+#include "common.h"
+
 #define STATIC_BUILD
 #include "tcl.h"
 
 
-void                 Tcl_AppendResult() {}
-void                 Tcl_DStringEndSublist() {}
-void                 Tcl_DStringStartSublist() {}
-char                *Tcl_DStringAppendElement() {return 0;}
-int                  Tcl_SplitList() {return 0;}
-void                 Tcl_DStringFree() {}
-void                 Tcl_DStringResult() {}
-void                 Tcl_DStringInit() {}
-int                  Tcl_GetInt() {return 0;}
-int                  Tcl_GetDouble() {return 0;}
-int                  Tcl_GetBoolean() {return 0;}
-int                  Tcl_LinkVar() {return 0;}
-CONST84_RETURN char *Tcl_SetVar() {return 0;}
-int                  Tcl_PkgProvide() {return 0;}
-CONST84_RETURN char *Tcl_GetStringResult() {return 0;}
-int                  Tcl_Eval() {return 0;}
-char                *Tcl_GetStringFromObj() {return 0;}
-int                  Tcl_ListObjGetElements() {return 0;}
-void                 TclFreeObj() {}
-int                  Tcl_ListObjAppendList() {return 0;}
-Tcl_Obj             *Tcl_NewListObj() {return 0;}
-Tcl_Obj             *Tcl_NewStringObj() {return 0;}
-char                *Tcl_DStringAppend() {return 0;}
-void                 Tcl_SetResult() {}
-void                 Tcl_ResetResult() {}
-int                  Tcl_GetBooleanFromObj() {return 0;}
-int                  Tcl_GetDoubleFromObj() {return 0;}
-int                  Tcl_GetIntFromObj() {return 0;}
-char                *Tcl_GetString() {return 0;}
-int                  Tcl_ListObjIndex() {return 0;}
-int                  Tcl_ListObjLength() {return 0;}
-Tcl_Command          Tcl_CreateCommand() {return 0;}
-int                  Tcl_DeleteCommand() {return 0;}
-void                 Tcl_SetObjResult() {}
-void                 Tcl_Free() {}
-Tcl_Obj             *Tcl_GetObjResult() {return 0;}
-void                 Tcl_AppendElement() {}
-int                  Tcl_EvalFile() {return 0;}
-int                  Tcl_CreateAlias() {return 0;}
-Tcl_Interp          *Tcl_CreateSlave() {return 0;}
-Tcl_Interp          *Tcl_CreateInterp() {return 0;}
-void                 Tcl_DeleteHashTable() {}
-Tcl_HashEntry       *Tcl_NextHashEntry() {return 0;}
-Tcl_HashEntry       *Tcl_FirstHashEntry() {return 0;}
-void                 Tcl_InitHashTable() {}
-int                  Tcl_VarEval() {return 0;}
-Tcl_Obj             *Tcl_GetVar2Ex() {return 0;}
-int                  Tcl_DoOneEvent() {return 0;}
-int                  Tcl_GetCharLength() {return 0;}
-CONST84_RETURN char *Tcl_GetVar() {return 0;}
-int                  Tcl_Init() {return 0;}
-int                  Tcl_Close() {return 0;}
-void                 Tcl_DeleteChannelHandler() {}
-int                  Tcl_Eof() {return 0;}
-void                 Tcl_CreateChannelHandler() {}
-Tcl_Channel          Tcl_MakeFileChannel() {return 0;}
-void                 Tcl_SetErrno() {}
-Tcl_Obj             *Tcl_NewDoubleObj() {return 0;}
-Tcl_Obj             *Tcl_NewIntObj() {return 0;}
+void                 Tcl_AppendResult(Tcl_Interp *UNUSED(interp), ...) {}
+void                 Tcl_DStringEndSublist(Tcl_DString *UNUSED(dsPtr)) {}
+void                 Tcl_DStringStartSublist(Tcl_DString *UNUSED(dsPtr)) {}
+char                *Tcl_DStringAppendElement(Tcl_DString *UNUSED(dsPtr), CONST char *UNUSED(element)) {return 0;}
+int                  Tcl_SplitList(Tcl_Interp *UNUSED(interp), CONST char *UNUSED(listStr), int *UNUSED(argcPtr), CONST84 char ***UNUSED(argvPtr)) {return 0;}
+void                 Tcl_DStringFree(Tcl_DString *UNUSED(dsPtr)) {}
+void                 Tcl_DStringResult(Tcl_Interp *UNUSED(interp), Tcl_DString *UNUSED(dsPtr)) {}
+void                 Tcl_DStringInit(Tcl_DString *UNUSED(dsPtr)) {}
+int                  Tcl_GetInt(Tcl_Interp *UNUSED(interp), CONST char *UNUSED(src), int *UNUSED(intPtr)) {return 0;}
+int                  Tcl_GetDouble(Tcl_Interp *UNUSED(interp), CONST char *UNUSED(src), double *UNUSED(doublePtr)) {return 0;}
+int                  Tcl_GetBoolean(Tcl_Interp *UNUSED(interp), CONST char *UNUSED(src), int *UNUSED(boolPtr)) {return 0;}
+int                  Tcl_LinkVar(Tcl_Interp *UNUSED(interp), CONST char *UNUSED(varName), char *UNUSED(addr), int UNUSED(type)) {return 0;}
+CONST84_RETURN char *Tcl_SetVar(Tcl_Interp *UNUSED(interp), CONST char *UNUSED(varName), CONST char *UNUSED(newValue), int UNUSED(flags)) {return 0;}
+int                  Tcl_PkgProvide(Tcl_Interp *UNUSED(interp), CONST char *UNUSED(name), CONST char *UNUSED(version)) {return 0;}
+CONST84_RETURN char *Tcl_GetStringResult(Tcl_Interp *UNUSED(interp)) {return 0;}
+int                  Tcl_Eval(Tcl_Interp *UNUSED(interp), CONST char *UNUSED(script)) {return 0;}
+char                *Tcl_GetStringFromObj(Tcl_Obj *UNUSED(objPtr), int *UNUSED(lengthPtr)) {return 0;}
+int                  Tcl_ListObjGetElements(Tcl_Interp *UNUSED(interp), Tcl_Obj *UNUSED(listPtr), int *UNUSED(objcPtr), Tcl_Obj ***UNUSED(objvPtr)) {return 0;}
+void                 TclFreeObj(Tcl_Obj *UNUSED(objPtr)) {}
+int                  Tcl_ListObjAppendList(Tcl_Interp *UNUSED(interp), Tcl_Obj *UNUSED(listPtr), Tcl_Obj *UNUSED(elemListPtr)) {return 0;}
+Tcl_Obj             *Tcl_NewListObj(int UNUSED(objc), Tcl_Obj *CONST UNUSED(objv[])) {return 0;}
+Tcl_Obj             *Tcl_NewStringObj(CONST char *UNUSED(bytes), int UNUSED(length)) {return 0;}
+char                *Tcl_DStringAppend(Tcl_DString *UNUSED(dsPtr), CONST char *UNUSED(bytes), int UNUSED(length)) {return 0;}
+void                 Tcl_SetResult(Tcl_Interp *UNUSED(interp), char *UNUSED(result), Tcl_FreeProc *UNUSED(freeProc)) {}
+void                 Tcl_ResetResult(Tcl_Interp *UNUSED(interp)) {}
+int                  Tcl_GetBooleanFromObj(Tcl_Interp *UNUSED(interp), Tcl_Obj *UNUSED(objPtr), int *UNUSED(boolPtr)) {return 0;}
+int                  Tcl_GetDoubleFromObj(Tcl_Interp *UNUSED(interp), Tcl_Obj *UNUSED(objPtr), double *UNUSED(doublePtr)) {return 0;}
+int                  Tcl_GetIntFromObj(Tcl_Interp *UNUSED(interp), Tcl_Obj *UNUSED(objPtr), int *UNUSED(intPtr)) {return 0;}
+char                *Tcl_GetString(Tcl_Obj *UNUSED(objPtr)) {return 0;}
+int                  Tcl_ListObjIndex(Tcl_Interp *UNUSED(interp), Tcl_Obj *UNUSED(listPtr), int UNUSED(index), Tcl_Obj **UNUSED(objPtrPtr)) {return 0;}
+int                  Tcl_ListObjLength(Tcl_Interp *UNUSED(interp), Tcl_Obj *UNUSED(listPtr), int *UNUSED(lengthPtr)) {return 0;}
+Tcl_Command          Tcl_CreateCommand(Tcl_Interp *UNUSED(interp), CONST char *UNUSED(cmdName), Tcl_CmdProc *UNUSED(proc), ClientData UNUSED(clientData), Tcl_CmdDeleteProc *UNUSED(deleteProc)) {return 0;}
+int                  Tcl_DeleteCommand(Tcl_Interp *UNUSED(interp), CONST char *UNUSED(cmdName)) {return 0;}
+void                 Tcl_SetObjResult(Tcl_Interp *UNUSED(interp), Tcl_Obj *UNUSED(resultObjPtr)) {}
+void                 Tcl_Free(char *UNUSED(ptr)) {}
+Tcl_Obj             *Tcl_GetObjResult(Tcl_Interp *UNUSED(interp)) {return 0;}
+void                 Tcl_AppendElement(Tcl_Interp *UNUSED(interp), CONST char *UNUSED(element)) {}
+int                  Tcl_EvalFile(Tcl_Interp *UNUSED(interp), CONST char *UNUSED(fileName)) {return 0;}
+int                  Tcl_CreateAlias(Tcl_Interp *UNUSED(slave), CONST char *UNUSED(slaveCmd), Tcl_Interp *UNUSED(target), CONST char *UNUSED(targetCmd), int UNUSED(argc), CONST84 char *CONST *UNUSED(argv)) {return 0;}
+Tcl_Interp          *Tcl_CreateSlave(Tcl_Interp *UNUSED(interp), CONST char *UNUSED(slaveName), int UNUSED(isSafe)) {return 0;}
+Tcl_Interp          *Tcl_CreateInterp(void) {return 0;}
+void                 Tcl_DeleteHashTable(Tcl_HashTable *UNUSED(tablePtr)) {}
+Tcl_HashEntry       *Tcl_NextHashEntry(Tcl_HashSearch *UNUSED(searchPtr)) {return 0;}
+Tcl_HashEntry       *Tcl_FirstHashEntry(Tcl_HashTable *UNUSED(tablePtr), Tcl_HashSearch *UNUSED(searchPtr)) {return 0;}
+void                 Tcl_InitHashTable(Tcl_HashTable *UNUSED(tablePtr), int UNUSED(keyType)) {}
+int                  Tcl_VarEval(Tcl_Interp *UNUSED(interp), ...) {return 0;}
+Tcl_Obj             *Tcl_GetVar2Ex(Tcl_Interp *UNUSED(interp), CONST char *UNUSED(part1), CONST char *UNUSED(part2), int UNUSED(flags)) {return 0;}
+int                  Tcl_DoOneEvent(int UNUSED(flags)) {return 0;}
+int                  Tcl_GetCharLength(Tcl_Obj *UNUSED(objPtr)) {return 0;}
+CONST84_RETURN char *Tcl_GetVar(Tcl_Interp *UNUSED(interp), CONST char *UNUSED(varName), int UNUSED(flags)) {return 0;}
+int                  Tcl_Init(Tcl_Interp *UNUSED(interp)) {return 0;}
+int                  Tcl_Close(Tcl_Interp *UNUSED(interp), Tcl_Channel UNUSED(chan)) {return 0;}
+void                 Tcl_DeleteChannelHandler(Tcl_Channel UNUSED(chan), Tcl_ChannelProc *UNUSED(proc), ClientData UNUSED(clientData)) {}
+int                  Tcl_Eof(Tcl_Channel UNUSED(chan)) {return 0;}
+void                 Tcl_CreateChannelHandler(Tcl_Channel UNUSED(chan), int UNUSED(mask), Tcl_ChannelProc *UNUSED(proc), ClientData UNUSED(clientData)) {}
+Tcl_Channel          Tcl_MakeFileChannel(ClientData UNUSED(handle), int UNUSED(mode)) {return 0;}
+void                 Tcl_SetErrno(int UNUSED(err)) {}
+Tcl_Obj             *Tcl_NewDoubleObj(double UNUSED(doubleValue)) {return 0;}
+Tcl_Obj             *Tcl_NewIntObj(int UNUSED(intValue)) {return 0;}
