3f438d5f12adbc530e775f7705564c62a1872820
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2013-08-05 22:03:28 +0000

    Just use the std min and max...
    svn:revision:56606
    svn:branch:trunk
    svn:account:starseeker

--- src/other/libgdiam/gdiam.hpp
+++ src/other/libgdiam/gdiam.hpp
@@ -45,29 +45,11 @@ typedef  const gdiam_real  * gdiam_point_cnt;
 
 #ifndef __MINMAX_DEFINED
 #define __MINMAX_DEFINED
-/*
-template <class T> const inline   T& min( const T& t1, const T& t2 )
-{
-         return t1>t2 ? t2 : t1;
-}
 
-template <class T> inline  T& max( const T& t1, const T& t2 )
-{
-         return t1>t2 ? t1 : t2;
-}
-*/
+#include<algorithm>
+#define min std::min
+#define max std::max
 
-template <class T>
-inline T  min( T t1, T t2 )
-{
-         return t1>t2 ? t2 : t1;
-}
-
-template <class T>
-inline T  max( T t1, T t2 )
-{
-         return t1>t2 ? t1 : t2;
-}
 #endif /* MIN_MAX */
 
 template <class T>
