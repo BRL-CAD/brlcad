b0de3550be1e85724aea564697de506efca12129
Isacc Kamga <u2isaac@gmail.com>
2013-08-19 08:22:01 +0000

    Corrected spelling of polynomial in bn_poly_synthetic_div.c
    svn:revision:56944
    svn:branch:trunk
    svn:account:iiizzzaaakkk

--- src/libbn/tests/bn_poly_synthetic_div.c
+++ src/libbn/tests/bn_poly_synthetic_div.c
@@ -38,7 +38,7 @@ bn_poly_t quo[1] = { BN_POLY_INIT_ZERO };
 bn_poly_t rem[1] = { BN_POLY_INIT_ZERO };
 
 
-/* Initializes polnomial storing a negative, positive and zero
+/* Initializes polynomial storing a negative, positive and zero
  * coefficients.  Test polynomials will be of 4th, 3rd, and 2nd
  * degrees.
  */
