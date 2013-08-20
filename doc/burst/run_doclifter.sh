d460443ec0906b7cf0f68fc012cb672d9f9981fe
Thomas Browder <tom.browder@gmail.com>
2013-08-20 13:29:46 +0000

    pref and option changes
    svn:revision:56991
    svn:branch:trunk
    svn:account:tbrowder2

--- doc/burst/run_doclifter.sh
+++ doc/burst/run_doclifter.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
-doclifter -h hints.txt -vvvv paper.mm 2>t
+#MASTER=burst
+MASTER=debug
 
+doclifter -h hints.txt -x -vvvv $MASTER.mm #2>t
+
+STATUS=$?
+
+echo "doclifter return value for '$MASTER' was '$STATUS'"
