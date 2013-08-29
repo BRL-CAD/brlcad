822b3800183e71310fd83f6ec421e779e235442b
Mohit Daga <mohitdaga.lnmiit@gmail.com>
2013-08-29 22:51:07 +0000

    Correcting app synopsis.
    svn:revision:57289
    svn:branch:trunk
    svn:account:mohitdaga

--- src/util/bw-pix.c
+++ src/util/bw-pix.c
@@ -31,7 +31,7 @@
 #include "bu.h"
 
 char usage[] = "\
-Usage: bw-pix [-h] [out_file.pix] [file.pix] > [out_file.pix]\n";
+Usage: bw-pix [-h] [-o out_file.pix] [file.pix] > [out_file.pix]\n";
 
 char *out_file = NULL;
 char *in_file = NULL;
