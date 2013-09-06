e84cbc6e5bb649120c72bc783b7d36691f23bdc2
Christopher Sean Morrison <brlcad@gmail.com>
2013-09-06 03:51:41 +0000

    if we're building in place, we also need to ignore the lexer output source files that inject system headers before our common.h header
    svn:revision:57459
    svn:branch:trunk
    svn:account:brlcad

--- regress/repository.sh
+++ regress/repository.sh
@@ -88,7 +88,8 @@ echo "running common.h inclusion order check..."
 # small performance savings.
 FILES="`grep -I -e '#[[:space:]]*include' $SRCFILES $INCFILES | grep -E 'common.h|<' | sed 's/:.*//g' | sort | uniq`"
 
-EXEMPT="bin.h bio.h config_win.h pstdint.h uce-dirent.h ttcp.c"
+LEXERS="schema.h obj_grammar.c obj_grammar.cpp obj_scanner.h points_scan.c script.c"
+EXEMPT="bin.h bio.h config_win.h pstdint.h uce-dirent.h ttcp.c $LEXERS"
 
 FOUND=
 for file in $FILES ; do
