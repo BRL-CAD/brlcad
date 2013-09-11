dc54ffb8b6ed3067691515eccb398ac4f229ec38
Thomas Browder <tom.browder@gmail.com>
2013-09-11 00:43:22 +0000

    add more tmp files (NOINSTALL) for testing TCLAP implementation
    svn:revision:57537
    svn:branch:trunk
    svn:account:tbrowder2

---
+++ src/util/bu_opt_parse.cpp
@@ -0,0 +1,23 @@
+/*                 B U _ O P T _ P A R S E . C P P
+ * BRL-CAD
+ *
+ * Copyright (c) 2013 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+
+#include "bu_opt_parse.h"
+
+#define BU_OPT_PARSE_DUMMY_Cpp
