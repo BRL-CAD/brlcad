0c905b9667e2b1a7c438fc678b09c228b629012d
Carl Moore <cmoore@brlcad.org>
2013-09-27 20:47:17 +0000

    disable 'Nothing created' and implement the elsewhere-used 'Program continues running:'; simplify some brace usage
    svn:revision:57926
    svn:branch:trunk
    svn:account:carlmoore

--- src/libged/coil.c
+++ src/libged/coil.c
@@ -39,6 +39,7 @@
 
 #define D2R(x) (x * DEG2RAD)
 #define DEFAULT_COIL_OBJECT "coil"
+#define DEFAULT_COIL_FILENAME "coil.g"
 
 int usedefaults;
 
@@ -392,6 +393,10 @@ ReadArgs(struct ged *gedp, int argc, const char *argv[], struct bu_vls *name, st
                 return GED_ERROR;
 	}
     }
+    if (argc == 1) {
+	usage(gedp);
+	bu_vls_printf(gedp->ged_result_str,"\n       Program continues running:\n\n");
+    }
     if ((argc - bu_optind) == 1) {
 	have_name = 1;
 	bu_vls_sprintf(name, "%s", argv[bu_optind]);
@@ -440,7 +445,7 @@ ged_coil(struct ged *gedp, int argc, const char *argv[])
 
     /* Process arguments */
     if (ReadArgs(gedp, argc, argv, &name, &sections, &mean_outer_diameter, &wire_diameter, &helix_angle, &pitch, &nt, &start_cap_type, &end_cap_type, &overall_length, &lhf) != GED_OK) {
-	bu_vls_printf(gedp->ged_result_str, "Nothing created");
+/*	bu_vls_printf(gedp->ged_result_str, "Nothing created"); */
 	return GED_ERROR;
     }
 
@@ -449,7 +454,7 @@ ged_coil(struct ged *gedp, int argc, const char *argv[])
     if (BU_LIST_IS_EMPTY(&sections)) {
 
 	if (usedefaults)
-	    bu_vls_printf(gedp->ged_result_str, "Creating a coil with default parameters.\n");
+	    bu_vls_printf(gedp->ged_result_str, "Creating %s with default parameters.\n",DEFAULT_COIL_FILENAME);
 
 	if (mean_outer_diameter < 0 || wire_diameter < 0 || helix_angle < 0 || pitch < 0 || nt < 0 || start_cap_type < 0 || end_cap_type < 0) {
 	    bu_vls_printf(gedp->ged_result_str, "negative value in one or more arguments supplied to coil");
@@ -461,26 +466,22 @@ ged_coil(struct ged *gedp, int argc, const char *argv[])
 	    wire_diameter = 100;
 	}
 
-	if (ZERO(wire_diameter) && mean_outer_diameter > 0) {
+	if (ZERO(wire_diameter) && mean_outer_diameter > 0)
 	    wire_diameter = mean_outer_diameter/10;
-	}
 
-	if (ZERO(mean_outer_diameter) && wire_diameter > 0) {
+	if (ZERO(mean_outer_diameter) && wire_diameter > 0)
 	    mean_outer_diameter = wire_diameter * 10;
-	}
 
-	if (ZERO(pitch)) {
+	if (ZERO(pitch))
 	    pitch = wire_diameter;
-	}
 
 	if (pitch < wire_diameter) {
 	    bu_vls_printf(gedp->ged_result_str, "WARNING:  Pitch less than wire diameter.  Setting pitch to wire diameter: %f mm\n", wire_diameter);
 	    pitch = wire_diameter;
 	}
 
-	if (nt == 0) {
+	if (nt == 0)
 	    nt = 30;
-	}
 
 	BU_ALLOC(coil_data, struct coil_data_t);
 	coil_data->nt = nt;
