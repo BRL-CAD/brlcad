4363ac71950f96146fd631a829eab2f624078bb9
Christopher Sean Morrison <brlcad@gmail.com>
2013-09-30 13:47:12 +0000

    start bringing this all together.  stub in a new (empty) constraint command.
    svn:revision:57977
    svn:branch:trunk
    svn:account:brlcad

---
+++ src/libged/constraint/constraint.c
@@ -0,0 +1,52 @@
+/*                    C O N S T R A I N T . C
+ * BRL-CAD
+ *
+ * Copyright (c) 2013 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+
+#include "common.h"
+
+#include "ged.h"
+
+
+int
+ged_constraint(struct ged *gedp, int argc, const char *argv[])
+{
+    GED_CHECK_ARGC_GT_0(gedp, argc, GED_ERROR);
+
+    /* intialize result */
+    bu_vls_trunc(gedp->ged_result_str, 0);
+
+    if (argc < 2) {
+	/* must be wanting help */
+	bu_vls_printf(gedp->ged_result_str, "Usage: %s {list|add|del}\n", argv[0]);
+	return GED_HELP;
+    }
+
+    return 0;
+}
+
+
+/*
+ * Local Variables:
+ * tab-width: 8
+ * mode: C
+ * indent-tabs-mode: t
+ * c-file-style: "stroustrup"
+ * End:
+ * ex: shiftwidth=4 tabstop=8
+ */
