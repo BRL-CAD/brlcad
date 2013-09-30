ee3e2368cfce44160f4afbe12490b58072a22061
Christopher Sean Morrison <brlcad@gmail.com>
2013-09-30 17:31:25 +0000

    start hashing out the primary commands and the user interface. this is roughly the approach of the attr command.
    svn:revision:57993
    svn:branch:trunk
    svn:account:brlcad

--- src/libged/constraint/constraint.c
+++ src/libged/constraint/constraint.c
@@ -23,19 +23,51 @@
 #include "ged.h"
 
 
+typedef enum {
+    PC_EVAL,
+    PC_GET,
+    PC_RM,
+    PC_SET,
+    PC_SHOW
+} constraint_cmd_t;
+
+
+constraint_cmd_t
+constraint_cmd(const char *arg)
+{
+    if (arg)
+	return PC_EVAL;
+    return PC_EVAL;
+}
+
+
 int
 ged_constraint(struct ged *gedp, int argc, const char *argv[])
 {
-    GED_CHECK_ARGC_GT_0(gedp, argc, GED_ERROR);
+    static const char *usage = "{set|get|show|rm|eval} [constraint_name] [key [value] ...]";
+
+    constraint_cmd_t cmd;
 
     /* intialize result */
     bu_vls_trunc(gedp->ged_result_str, 0);
 
+    GED_CHECK_ARGC_GT_0(gedp, argc, GED_ERROR);
+
     if (argc < 2) {
 	/* must be wanting help */
-	bu_vls_printf(gedp->ged_result_str, "Usage: %s {list|add|del}\n", argv[0]);
+	bu_vls_printf(gedp->ged_result_str, "Usage: %s %s\n", argv[0], usage);
 	return GED_HELP;
     }
+
+    GED_CHECK_DATABASE_OPEN(gedp, GED_ERROR);
+
+    /* this is only valid for v5 databases */
+    if (db_version(gedp->ged_wdbp->dbip) < 5) {
+	bu_vls_printf(gedp->ged_result_str, "Attributes are not available for this database format.\nPlease upgrade your database format using \"dbupgrade\" to enable attributes.");
+	return GED_ERROR;
+    }
+
+    cmd = constraint_cmd(argv[1]);
 
     return 0;
 }
