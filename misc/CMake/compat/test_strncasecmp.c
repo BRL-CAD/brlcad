a6f6790aa5d948ab600c8129c71f805fda3c20c5
Thomas Browder <tom.browder@gmail.com>
2013-10-19 21:57:12 +0000

    use correct args for the function
    svn:revision:58237
    svn:branch:trunk
    svn:account:tbrowder2

--- misc/CMake/compat/test_strncasecmp.c
+++ misc/CMake/compat/test_strncasecmp.c
@@ -5,8 +5,9 @@ main(void)
 {
   const char s1[] = "one";
   const char s2[] = "two";
+  const size_t n  = 2;
 
-  int res __attribute__((unused)) = strncasecmp(s1, s2);
+  int res __attribute__((unused)) = strncasecmp(s1, s2, n);
 
   return 0;
 }
