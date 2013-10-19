fd7ebfea067893de6ef565127c086aaa6f9c3ad8
Thomas Browder <tom.browder@gmail.com>
2013-10-19 12:16:59 +0000

    two more compat headers
    svn:revision:58224
    svn:branch:trunk
    svn:account:tbrowder2

---
+++ misc/CMake/compat/sigaction_compat.h
@@ -0,0 +1,8 @@
+#ifndef BRLCAD_SIGACTION_COMPAT_H
+#define BRLCAD_SIGACTION_COMPAT_H
+
+/* see file 'src/compat/README.compat' for more details */
+
+#include <sigaction.h>
+
+#endif /* BRLCAD_SIGACTION_COMPAT_H */
---
+++ misc/CMake/compat/strtok_r_compat.h
@@ -0,0 +1,9 @@
+#ifndef BRLCAD_STRTOK_R_COMPAT_H
+#define BRLCAD_STRTOK_R_COMPAT_H
+
+/* see file 'src/compat/README.compat' for more details */
+
+/* from <string.h> */
+char *strtok_r(char *str, const char *delim, char **saveptr);
+
+#endif /* BRLCAD_STRTOK_R_COMPAT_H */
