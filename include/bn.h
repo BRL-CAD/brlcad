6f015f7167a14f7001f1f678c5054ae844641f50
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2013-11-01 18:35:13 +0000

    Start working on getting the monotone chain convex hull algorithm ported to libbn.
    svn:revision:58393
    svn:branch:trunk
    svn:account:starseeker

--- include/bn.h
+++ include/bn.h
@@ -509,12 +509,32 @@ BN_EXPORT extern void bn_tcl_mat_print();
  *
  * See also <a href="http://geomalgorithms.com/a12-_hull-3.html">http://geomalgorithms.com/a12-_hull-3.html</a>
  *
- * @param[out]	hull output convex hull array of vertices in ccw orientation (max is n)
+ * @param[out]	hull convex hull array vertices in ccw orientation (max is n)
  * @param	polyline The points defining the input polyline, stored with ccw orientation
  * @param	n the number of points in polyline
  * @return the number of points in the output hull array
  */
 BN_EXPORT int bn_polyline_2D_hull(point_t** hull, const point_t* polyline, int n);
+
+/**
+ * @brief
+ * Monotone Chain 2D convex hull algorithm for unordered co-planar point sets
+ *
+ * Another efficient algorithm for convex hulls in two dimensions.
+ * Andrew, A. M. Information Processing Letters 9.5 (1979): 216-219.
+ *
+ * See also <a href="http://geomalgorithms.com/a10-_hull-1.html">http://geomalgorithms.com/a10-_hull-1.html</a>
+ *
+ * The monotone chain algorithm is used here primarily due to its simpler, coordinate based
+ * comparison function.
+ *
+ * @param[out]	hull convex hull array vertices in ccw orientation (max is n)
+ * @param	pnts The input points for which a convex hull will be built
+ * @param	n the number of points in the input set 
+ * @return the number of points in the output hull array
+ */
+BN_EXPORT int bn_2D_hull(point_t** hull, const point_t* polyline, int n);
+
 
 /*----------------------------------------------------------------------*/
 
--- src/libbn/chull.c
+++ src/libbn/chull.c
@@ -16,13 +16,7 @@
  *
  * This file implements various algorithms for finding convex hull
  * of point sets in 2D and 3D.
- *
- * The implementation of Melkman's algorithm for convex hulls of simple
- * polylines is a translation of softSurfer's implementation:
- * http://geomalgorithms.com/a12-_hull-3.html
- *
  */
-
 
 #include "common.h"
 #include <stdlib.h>
@@ -37,6 +31,10 @@
  */
 #define isLeft(L0, L1, p) ((L1[X] - L0[X])*(p[Y] - L0[Y]) - (p[X] - L0[X])*(L1[Y] - L0[Y]))
 
+/* The implementation of Melkman's algorithm for convex hulls of simple
+ * polylines is a translation of softSurfer's implementation:
+ * http://geomalgorithms.com/a12-_hull-3.html
+ */
 int
 bn_polyline_2D_hull(point_t** hull, const point_t* polyline, int n)
 {
@@ -96,12 +94,91 @@ bn_polyline_2D_hull(point_t** hull, const point_t* polyline, int n)
     return h-1;
 }
 
+/* The implementation of the Monotone Chain algorithm for
+ * convex hulls of 2D point sets is a translation of
+ * softSurfer's C++ implementation:
+ * http://geomalgorithms.com/a10-_hull-1.html
+ */
+int
+bn_2D_hull(point_t **UNUSED(hull), const point_t *UNUSED(pnts), int UNUSED(n))
+{
+#if 0
+    /* TODO - first thing, need to copy pnts array to something
+       that can be sorted and sort it */
 
-/* TODO - the melkman algorithm isn't going to be enough if we accept generic coplanar point sets
- * as valid input - need to do the full convex hull algorithm, maybe exposing this option as a
- * special purpose faster function when the input properties can be guaranteed */
+    /* the output array H[] will be used as the stack */
+    int    bot=0, top=(-1);   /* indices for bottom and top of the stack */
+    int    i;                 /* array scan index */
 
+    /* Get the indices of points with min x-coord and min|max y-coord */
+    int minmin = 0, minmax;
+    float xmin = P[0].x;
+    for (i=1; i<n; i++)
+        if (P[i].x != xmin) break;
+    minmax = i-1;
+    if (minmax == n-1) {       /* degenerate case: all x-coords == xmin */
+        H[++top] = P[minmin];
+        if (P[minmax].y != P[minmin].y) /* a  nontrivial segment */
+            H[++top] =  P[minmax];
+        H[++top] = P[minmin];            /* add polygon endpoint */
+        return top+1;
+    }
 
+    /* Get the indices of points with max x-coord and min|max y-coord */
+    int maxmin, maxmax = n-1;
+    float xmax = P[n-1].x;
+    for (i=n-2; i>=0; i--)
+        if (P[i].x != xmax) break;
+    maxmin = i+1;
+
+    /* Compute the lower hull on the stack H */
+    H[++top] = P[minmin];      /* push  minmin point onto stack */
+    i = minmax;
+    while (++i <= maxmin)
+    {
+        /* the lower line joins P[minmin]  with P[maxmin] */
+        if (isLeft( P[minmin], P[maxmin], P[i])  >= 0 && i < maxmin)
+            continue;           /* ignore P[i] above or on the lower line */
+
+        while (top > 0)         /* there are at least 2 points on the stack */
+        {
+            /* test if  P[i] is left of the line at the stack top */
+            if (isLeft(  H[top-1], H[top], P[i]) > 0)
+                 break;         /* P[i] is a new hull  vertex */
+            else
+                 top--;         /* pop top point off  stack */
+        }
+        H[++top] = P[i];        /* push P[i] onto stack */
+    }
+
+    /* Next, compute the upper hull on the stack H above  the bottom hull */
+    if (maxmax != maxmin)      /* if  distinct xmax points */
+         H[++top] = P[maxmax];  /* push maxmax point onto stack */
+    bot = top;                  /* the bottom point of the upper hull stack */
+    i = maxmin;
+    while (--i >= minmax)
+    {
+        /* the upper line joins P[maxmax]  with P[minmax] */
+        if (isLeft( P[maxmax], P[minmax], P[i])  >= 0 && i > minmax)
+            continue;           /* ignore P[i] below or on the upper line */
+
+        while (top > bot)     /* at least 2 points on the upper stack */
+        {
+            /* test if  P[i] is left of the line at the stack top */
+            if (isLeft(  H[top-1], H[top], P[i]) > 0)
+                 break;         /* P[i] is a new hull  vertex */
+            else
+                 top--;         /* pop top point off  stack */
+        }
+        H[++top] = P[i];        /* push P[i] onto stack */
+    }
+    if (minmax != minmin)
+        H[++top] = P[minmin];  /* push  joining endpoint onto stack */
+
+    return top+1;
+#endif
+return 0;
+}
 
 /*
  * Local Variables:
