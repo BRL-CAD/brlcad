08a186f4fc9905cecd84b310edecb46a5d3e6649
Thomas Browder <tom.browder@gmail.com>
2013-11-11 22:56:12 +0000

    add info
    svn:revision:58522
    svn:branch:trunk
    svn:account:tbrowder2

--- src/other/boost/update-brlcad-boost-to-1.55.0.sh
+++ src/other/boost/update-brlcad-boost-to-1.55.0.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # use after executing:
+#   update-boost-tree.pl boost_1_55_0
 
 makedir -p boost/smart_ptr/detail
 cp boost_1_55_0/boost/smart_ptr/detail/sp_nullptr_t.hpp boost/smart_ptr/detail
