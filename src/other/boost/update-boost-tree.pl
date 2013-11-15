e3bc6542b5d53309bdbbb7d5135bfe3f128aff49
Thomas Browder <tom.browder@gmail.com>
2013-11-15 12:58:15 +0000

    fix typo
    svn:revision:58543
    svn:branch:trunk
    svn:account:tbrowder2

--- src/other/boost/update-boost-tree.pl
+++ src/other/boost/update-boost-tree.pl
@@ -209,7 +209,7 @@ for (;;) {
 	qx(mkdir -p $dir) if (! -d $dir);
 	my $nf = sprintf "$Bdir/%s", $f;
 	copy($nf, $dir);
-	print "WARNING:  Nnexpected dup new file '$f' (must have been missing).\n"
+	print "WARNING:  Unexpected dup new file '$f' (must have been missing).\n"
 	  if (exists $new_files{$f});
 	$new_files{$f} = 1;
 	next RUN;
