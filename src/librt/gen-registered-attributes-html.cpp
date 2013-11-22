b245e6f09ab12160c5883f52f3f4427b001b8cd3
Thomas Browder <tom.browder@gmail.com>
2013-11-22 15:21:02 +0000

    start a function to generate xml tables for inclusion in 'attributes.xml'
    svn:revision:58617
    svn:branch:trunk
    svn:account:tbrowder2

--- src/librt/gen-registered-attributes-html.cpp
+++ src/librt/gen-registered-attributes-html.cpp
@@ -21,6 +21,7 @@ using namespace std;
 using namespace db5_attrs_private;
 
 // local funcs
+static void gen_attr_xml_tables(const std::string& fname, const std::string& fname2);
 static void gen_attr_html_page(const std::string& fname);
 static void open_file_write(ofstream& fs, const string& f);
 
@@ -30,11 +31,22 @@ static bool debug(true);
 int
 main()
 {
-    string fname("db5_types.c");
+    load_maps();
 
     // write the html file
     string ofil("t.html");
     gen_attr_html_page(ofil);
+
+    // write the xml table for file 'attributes.xml'
+    string adir("../../doc/docbook/system/man5/en");
+
+    // these two files are included in the manually generated
+    // 'attributes.xml' file:
+    string stable(adir + "attributes-standard-table.xml");
+    string utable(adir + "attributes-user-table.xml");
+
+    gen_attr_xml_tables(stable, utable);
+
     return 0;
 }
 
@@ -48,6 +60,145 @@ open_file_write(ofstream& fs, const string& f)
     exit(1);
   }
 } // open_file_write
+
+void
+gen_attr_xml_tables(const std::string& fname, const std::string& fname2)
+{
+    ofstream fo, fo2;
+    open_file_write(fo, fname);
+    open_file_write(fo2, fname2);
+
+    fo <<
+        "<!doctype html>\n"
+        "<html lang=\"en\">\n"
+        "<head>\n"
+        "  <title>brlcad-attributes.html</title>\n"
+        "  <meta charset = \"UTF-8\" />\n"
+        "  <style type = \"text/css\">\n"
+        "  table, td, th {\n"
+        "    border: 1px solid black;\n"
+        "  }\n"
+        "  </style>\n"
+        "</head>\n"
+        "<body>\n"
+        "  <h1>BRL-CAD Standard and User-Registered Attributes</h2>\n"
+        "  <p>Following are lists of the BRL-CAD standard and user-registered attribute names\n"
+        "  along with their value definitions and aliases (if any).  Users should\n"
+        "  not assign values to them in other than their defined format.\n"
+        "  (Note that attribute names are not case-sensitive although their canonical form is\n"
+        "  lower-case.)</p>\n"
+
+        "  <p>Some attributes have ASCII names but binary values (e.g., 'mtime').  Their values cannot\n"
+        "  be modified by a user with the 'attr' command.  In some cases, but not all, their\n"
+        "  values may be shown in a human readable form with the 'attr' command.)</p>\n"
+
+        "  <p>If a user wishes to register an attribute to protect its use for models\n"
+        "  transferred to other BRL-CAD users, submit the attribute along with a description\n"
+        "  of its intended use to the\n"
+        "  <a href=\"mailto:brlcad-devel@lists.sourceforge.net\">BRL-CAD developers</a>.\n"
+        "  Its approval will be formal when it appears in the separate, registered attribute\n"
+        "  table following the standard attribute table.</p>\n"
+        ;
+
+    // need a table here (5 columns at the moment)
+    fo <<
+        "  <h3>BRL-CAD Standard Attributes</h3>\n"
+        "  <table>\n"
+        "    <tr>\n"
+        "      <th>Attribute</th>\n"
+        "      <th>Binary?</th>\n"
+        "      <th>Definition</th>\n"
+        "      <th>Example</th>\n"
+        "      <th>Aliases</th>\n"
+        "    </tr>\n"
+        ;
+
+    // track ATTR_REGISTERED type for separate listing
+    map<string,db5_attr_t> rattrs;
+    for (map<string,db5_attr_t>::iterator i = name2attr.begin(); i != name2attr.end(); ++i) {
+        const string& name(i->first);
+        db5_attr_t& a(i->second);
+        if (a.attr_subtype == ATTR_REGISTERED) {
+            rattrs.insert(make_pair(name,a));
+            continue;
+        }
+        fo <<
+            "    <tr>\n"
+            "      <td>" << name                       << "</td>\n"
+            "      <td>" << (a.is_binary ? "yes" : "") << "</td>\n"
+            "      <td>" << a.description              << "</td>\n"
+            "      <td>" << a.examples                 << "</td>\n"
+            "      <td>"
+            ;
+        if (!a.aliases.empty()) {
+            for (set<string>::iterator j = a.aliases.begin();
+                 j != a.aliases.end(); ++j) {
+                if (j != a.aliases.begin())
+                    fo << ", ";
+                fo << *j;
+            }
+        }
+        fo <<
+            "</td>\n"
+            "    </tr>\n"
+            ;
+    }
+    fo << "  </table>\n";
+
+    // now show ATTR_REGISTERED types, if any
+    fo << "  <h3>User-Registered Attributes</h3>\n";
+    if (rattrs.empty()) {
+        fo << "    <p>None at this time.</p>\n";
+    } else {
+        // need a table here
+        fo <<
+            "  <table>\n"
+            "    <tr>\n"
+            "      <th>Attribute</th>\n"
+            "      <th>Binary?</th>\n"
+            "      <th>Definition</th>\n"
+            "      <th>Example</th>\n"
+            "      <th>Aliases</th>\n"
+            "    </tr>\n"
+            ;
+        for (map<string,db5_attr_t>::iterator i = rattrs.begin(); i != rattrs.end(); ++i) {
+            const string& name(i->first);
+            db5_attr_t& a(i->second);
+            fo <<
+                "    <tr>\n"
+                "      <td>" << name                       << "</td>\n"
+                "      <td>" << (a.is_binary ? "yes" : "") << "</td>\n"
+                "      <td>" << a.description              << "</td>\n"
+                "      <td>" << a.examples                 << "</td>\n"
+                "      <td>"
+                ;
+            if (!a.aliases.empty()) {
+                for (set<string>::iterator j = a.aliases.begin();
+                     j != a.aliases.end(); ++i) {
+                    if (j != a.aliases.begin())
+                        fo << ", ";
+                    fo << *j;
+                }
+            }
+            fo <<
+                "</td>\n"
+                "    </tr>\n"
+                ;
+        }
+        fo << "  </table>\n";
+    }
+
+    fo <<
+        "</body>\n"
+        "</html>\n"
+        ;
+
+    fo.close();
+
+    if (debug)
+        printf("DEBUG:  see output file '%s'\n", fname.c_str());
+
+} // gen_attr_xml_table
 
 void
 gen_attr_html_page(const std::string& fname)
@@ -100,7 +251,7 @@ gen_attr_html_page(const std::string& fname)
         "    </tr>\n"
         ;
 
-    // track ATTR_REGISTERED type
+    // track ATTR_REGISTERED type for separate listing
     map<string,db5_attr_t> rattrs;
     for (map<string,db5_attr_t>::iterator i = name2attr.begin(); i != name2attr.end(); ++i) {
         const string& name(i->first);
@@ -132,6 +283,7 @@ gen_attr_html_page(const std::string& fname)
     }
     fo << "  </table>\n";
 
+    // now show ATTR_REGISTERED types, if any
     fo << "  <h3>User-Registered Attributes</h3>\n";
     if (rattrs.empty()) {
         fo << "    <p>None at this time.</p>\n";
