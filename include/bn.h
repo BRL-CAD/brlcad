6aa8b7f8b2cf66088cef00dfd1331a15a01e70b2
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2013-11-25 18:55:34 +0000

    The coordinate manipulation probably shouldn't be part of the public API.
    svn:revision:58657
    svn:branch:trunk
    svn:account:starseeker

--- include/bn.h
+++ include/bn.h
@@ -545,67 +545,6 @@ BN_EXPORT int bn_2d_chull(point2d_t** hull, const point2d_t* points_2d, int n);
 
 /**
  * @brief
- * Find a 2D coordinate system for a set of co-planar 3D points
- *
- * Based on the planar normal and the vector from the center point to the
- * point furthest from that center, find vectors describing a 2D coordinate system.
- *
- * @param[out]	origin_pnt Origin of 2D coordinate system in 3 space
- * @param[out]	u_axis 3D vector describing the U axis of the 2D coordinate system in 3 space
- * @param[out]	v_axis 3D vector describing the V axis of the 2D coordinate system in 3 space
- * @param	points_3d Array of 3D points
- * @param	n the number of points in the input set
- * @return 0 if successful
- */
-BN_EXPORT int bn_coplanar_2d_coord_sys(
-	point_t *origin_pnt,
-	vect_t *u_axis,
-	vect_t *v_axis,
-	const point_t *points_3d,
-       	int n);
-
-/**
- * @brief
- * Find 2D coordinates for a set of co-planar 3D points
- *
- * @param[out]	points_2d Array of parameterized 2D points
- * @param	origin_pnt Origin of 2D coordinate system in 3 space
- * @param	u_axis 3D vector describing the U axis of the 2D coordinate system in 3 space
- * @param	v_axis 3D vector describing the V axis of the 2D coordinate system in 3 space
- * @param	points_3d 3D input points
- * @param	n the number of points in the input set
- * @return 0 if successful
- */
-BN_EXPORT int bn_coplanar_3d_to_2d(
-	point2d_t **points_2d,
-	const point_t *origin_pnt,
-	const vect_t *u_axis,
-	const vect_t *v_axis,
-	const point_t *points_3d,
-       	int n);
-
-/**
- * @brief
- * Find 3D coordinates for a set of 2D points given a coordinate system
- *
- * @param[out]	points_3d Array of 3D points
- * @param	origin_pnt Origin of 2D coordinate system in 3 space
- * @param	u_axis 3D vector describing the U axis of the 2D coordinate system in 3 space
- * @param	v_axis 3D vector describing the V axis of the 2D coordinate system in 3 space
- * @param	points_2d 2D input points
- * @param	n the number of points in the input set
- * @return 0 if successful
- */
-BN_EXPORT int bn_coplanar_2d_to_3d(
-	point_t **points_3d,
-	const point_t *origin_pnt,
-	const vect_t *u_axis,
-	const vect_t *v_axis,
-	const point2d_t* points_2d,
-       	int n);
-
-/**
- * @brief
  * Find 3D coplanar point convex hull for unordered co-planar point sets
  *
  * This function assumes an input an array of 3D points which are coplanar
--- src/libbn/chull.c
+++ src/libbn/chull.c
@@ -23,6 +23,20 @@
 
 #include "bn.h"
 
+/**
+ * @brief
+ * Find a 2D coordinate system for a set of co-planar 3D points
+ *
+ * Based on the planar normal and the vector from the center point to the
+ * point furthest from that center, find vectors describing a 2D coordinate system.
+ *
+ * @param[out]  origin_pnt Origin of 2D coordinate system in 3 space
+ * @param[out]  u_axis 3D vector describing the U axis of the 2D coordinate system in 3 space
+ * @param[out]  v_axis 3D vector describing the V axis of the 2D coordinate system in 3 space
+ * @param       points_3d Array of 3D points
+ * @param       n the number of points in the input set
+ * @return 0 if successful
+ */
 int
 bn_coplanar_2d_coord_sys(point_t *origin_pnt, vect_t *u_axis, vect_t *v_axis, const point_t *points_3d, int n)
 {
@@ -72,6 +86,19 @@ bn_coplanar_2d_coord_sys(point_t *origin_pnt, vect_t *u_axis, vect_t *v_axis, co
     return 0;
 }
 
+/**
+ * @brief
+ * Find 2D coordinates for a set of co-planar 3D points
+ *
+ * @param[out]  points_2d Array of parameterized 2D points
+ * @param       origin_pnt Origin of 2D coordinate system in 3 space
+ * @param       u_axis 3D vector describing the U axis of the 2D coordinate system in 3 space
+ * @param       v_axis 3D vector describing the V axis of the 2D coordinate system in 3 space
+ * @param       points_3d 3D input points
+ * @param       n the number of points in the input set
+ * @return 0 if successful
+ */
+
 #if 0
 int
 bn_coplanar_3d_to_2d(point2d_t **points_2d, const point_t *origin_pnt,
@@ -85,6 +112,18 @@ bn_coplanar_3d_to_2d(point2d_t **points_2d, const point_t *origin_pnt,
 }
 
 
+/**
+ * @brief
+ * Find 3D coordinates for a set of 2D points given a coordinate system
+ *
+ * @param[out]  points_3d Array of 3D points
+ * @param       origin_pnt Origin of 2D coordinate system in 3 space
+ * @param       u_axis 3D vector describing the U axis of the 2D coordinate system in 3 space
+ * @param       v_axis 3D vector describing the V axis of the 2D coordinate system in 3 space
+ * @param       points_2d 2D input points
+ * @param       n the number of points in the input set
+ * @return 0 if successful
+ */
 int
 bn_coplanar_2d_to_3d(point_t **points_3d, const point_t *origin_pnt,
 	             const vect_t *u_axis, const vect_t *v_axis,
