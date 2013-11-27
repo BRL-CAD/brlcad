80f1300b746dd33fff37a75c3887de03483883eb
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2013-11-27 21:34:16 +0000

    Clearly a bug got introduced - printing out more info.
    svn:revision:58702
    svn:branch:trunk
    svn:account:starseeker

--- src/libbn/tests/bn_obr.c
+++ src/libbn/tests/bn_obr.c
@@ -36,6 +36,7 @@ main()
     point2d_t center;
     vect2d_t u, v;
     point2d_t pnts[4+1] = {{0}};
+    point2d_t output_pnts[4+1] = {{0}};
     int n = 4;
 
     V2SET(pnts[0], 1.5, 1.5);
@@ -52,6 +53,17 @@ main()
     bu_log("result: center (%f, %f)\n", V2ARGS(center));
     bu_log("        u      (%f, %f)\n", V2ARGS(u));
     bu_log("        v      (%f, %f)\n", V2ARGS(v));
+    V2ADD3(output_pnts[2], center, u, v);
+    VSCALE(u, u, -1);
+    V2ADD3(output_pnts[3], center, u, v);
+    VSCALE(v, v, -1);
+    V2ADD3(output_pnts[0], center, u, v);
+    VSCALE(u, u, -1);
+    V2ADD3(output_pnts[1], center, u, v);
+    bu_log("        op1    (%f, %f)\n", V2ARGS(output_pnts[0]));
+    bu_log("        op2    (%f, %f)\n", V2ARGS(output_pnts[1]));
+    bu_log("        op3    (%f, %f)\n", V2ARGS(output_pnts[2]));
+    bu_log("        op4    (%f, %f)\n", V2ARGS(output_pnts[3]));
     return 0;
 }
 
