83b0286cf8dbca8350729cbbbbdbbb3635d724b2
Thomas Browder <tom.browder@gmail.com>
2013-12-03 18:39:27 +0000

    add _H to header guard macro
    svn:revision:58771
    svn:branch:trunk
    svn:account:tbrowder2

--- include/dm-ps.h
+++ include/dm-ps.h
@@ -17,14 +17,15 @@
  * License along with this file; see the file named COPYING for more
  * information.
  */
+
 /** @addtogroup libdm */
 /** @{ */
 /** @file dm-ps.h
  *
  */
 
-#ifndef __DM_PS__
-#define __DM_PS__
+#ifndef __DM_PS_H__
+#define __DM_PS_H__
 
 /*
  * Display coordinate conversion:
@@ -51,7 +52,7 @@ struct ps_vars {
 
 extern struct ps_vars head_ps_vars;
 
-#endif /* __DM_PS__ */
+#endif /* __DM_PS_H__ */
 
 /** @} */
 /*
