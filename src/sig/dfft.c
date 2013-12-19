a597cab456223f379859eb37cda96067a10f02e3
Christopher Sean Morrison <brlcad@gmail.com>
2013-12-19 04:38:13 +0000

    reorder functions to avoid forward decls, reduce scope for several globals, note which functions are remote
    svn:revision:59016
    svn:branch:trunk
    svn:account:brlcad

--- src/sig/dfft.c
+++ src/sig/dfft.c
@@ -38,92 +38,57 @@
 
 #define MAXFFT 4096
 
-double data[MAXFFT];		/* Data buffer: 2*Points in spectrum */
 
+/* functions specified elsewhere (these probably belong in a header) */
+void cbweights(double *filter, int window, int points); /* in butter.c */
+void rfft(double *dat, int N); /* in dfft.c */
+void LintoLog(double *in, double *out, int num); /* in interp.c */
+
+
+/* FIXME: these need to get shoved into a struct and passed */
 double mindB = -120.0;
 int lflag = 0;
 int cflag = 0;
-int phase = 0;
 int linear_output = 0;
 int ascii_output = 0;
 int normalize_output = 0;
-
 double cbfilter[27];
-void cbweights(double *filter, int window, int points);
-double cbsum;
-void fftdisp(double *dat, int N);
-void fftmag2(double *mags, double *dat, int N);
-void fftphase(double *dat, int N);
-void rfft(double *dat, int N);
-void LintoLog(double *in, double *out, int num);
 
-static const char usage[] =
-  "Usage: dfft [options] [width (1024)] < doubles > 512logmags\n"
-  "\n"
-  "Options:\n"
-  "  -d dB  minimum dB (default 120)\n"
-  "  -l     log frequency scale\n"
-  "  -c     critical band filter (3rd octave)\n"
-  "  -p     phase\n"
-  "  -N     normalized PSD to max magnitude\n"
-  "  -L     linear output (no dB mag)\n"
-  "  -A     ascii output\n"
-  ;
 
-static const char optstring[] = "d:clpLANh?";
-
-int main(int argc, char **argv)
+static void
+fftmag2(double *mags, double *dat, int N)
 {
-    int i, n, c;
-    int L = 1024;
+    int i;
+    double value, dB;
 
-    while ((c = bu_getopt(argc, argv, optstring)) != -1) {
-	switch (c) {
-	    case 'd': mindB = -atof(bu_optarg); break;
-	    case 'c': cflag++; break;
-	    case 'l': lflag++; break;
-	    case 'p': phase++; break;
-	    case 'L': linear_output++; break;
-	    case 'A': ascii_output++; break;
-	    case 'N': normalize_output++; break;
-	    default:  bu_exit(1, "%s", usage);
+    /* DC */
+    mags[0] = dat[0]*dat[0];
+
+    /* Normal */
+    for (i = 1; i < N/2; i++) {
+	mags[i] = dat[i]*dat[i] + dat[N-i]*dat[N-i];
+    }
+
+    /* Nyquist */
+    mags[N/2] = dat[N/2]*dat[N/2];
+
+
+    if (!linear_output) {
+	/* Log output */
+	for (i = 0; i <= N/2; i++) {
+	    value = mags[i];
+	    if (value > 1.0e-18)
+		dB = 10*log10(value);
+	    else
+		dB = -180.0;
+	    mags[i] = dB;
 	}
     }
-
-    if (isatty(STDIN_FILENO) || isatty(STDOUT_FILENO))
-	bu_exit(1, "%s", usage);
-
-    /* Calculate Critical Band filter weights */
-    if (cflag) {
-	cbweights(&cbfilter[0], L, 19);
-	cbsum = 0.0;
-	for (i = 0; i < 19; i++)
-	    cbsum += cbfilter[i];
-    }
-
-    while ((n = fread(data, sizeof(*data), L, stdin)) > 0) {
-	if (n != L) {
-	    fprintf(stderr, "dfft: warning - partial record, adding %d zeros\n", L-n);
-	    memset((char *)&data[n], 0, L-n);
-	}
-
-	/* Do a spectrum */
-	/* if (L == 256) rfft256(data); etc. XXX */
-	rfft(data, L);
-
-	/* Put it on screen */
-	if (phase)
-	    fftphase(data, L);
-	else
-	    fftdisp(data, L);
-    }
-
-    return 0;
 }
 
 
-void
-fftdisp(double *dat, int N)
+static void
+fftdisp(double *dat, int N, double cbsum)
 {
     int i, j;
     double mags[MAXFFT];
@@ -194,39 +159,7 @@ fftdisp(double *dat, int N)
 }
 
 
-void
-fftmag2(double *mags, double *dat, int N)
-{
-    int i;
-    double value, dB;
-
-    /* DC */
-    mags[0] = dat[0]*dat[0];
-
-    /* Normal */
-    for (i = 1; i < N/2; i++) {
-	mags[i] = dat[i]*dat[i] + dat[N-i]*dat[N-i];
-    }
-
-    /* Nyquist */
-    mags[N/2] = dat[N/2]*dat[N/2];
-
-
-    if (!linear_output) {
-	/* Log output */
-	for (i = 0; i <= N/2; i++) {
-	    value = mags[i];
-	    if (value > 1.0e-18)
-		dB = 10*log10(value);
-	    else
-		dB = -180.0;
-	    mags[i] = dB;
-	}
-    }
-}
-
-
-void
+static void
 fftphase(double *dat, int N)
 {
     int i;
@@ -246,6 +179,75 @@ fftphase(double *dat, int N)
     ret = fwrite(out, sizeof(*out), N/2, stdout);
     if (ret != (size_t)(N/2))
 	perror("fwrite");
+}
+
+
+int
+main(int argc, char **argv)
+{
+    static const char usage[] =
+	"Usage: dfft [options] [width (1024)] < doubles > 512logmags\n"
+	"\n"
+	"Options:\n"
+	"  -d dB  minimum dB (default 120)\n"
+	"  -l     log frequency scale\n"
+	"  -c     critical band filter (3rd octave)\n"
+	"  -p     phase\n"
+	"  -N     normalized PSD to max magnitude\n"
+	"  -L     linear output (no dB mag)\n"
+	"  -A     ascii output\n"
+	;
+
+    static const char optstring[] = "d:clpLANh?";
+
+    int i, n, c;
+    int L = 1024;
+    double cbsum;
+    int phase = 0;
+    double data[MAXFFT];		/* Data buffer: 2*Points in spectrum */
+
+    while ((c = bu_getopt(argc, argv, optstring)) != -1) {
+	switch (c) {
+	    case 'd': mindB = -atof(bu_optarg); break;
+	    case 'c': cflag++; break;
+	    case 'l': lflag++; break;
+	    case 'p': phase++; break;
+	    case 'L': linear_output++; break;
+	    case 'A': ascii_output++; break;
+	    case 'N': normalize_output++; break;
+	    default:  bu_exit(1, "%s", usage);
+	}
+    }
+
+    if (isatty(STDIN_FILENO) || isatty(STDOUT_FILENO))
+	bu_exit(1, "%s", usage);
+
+    /* Calculate Critical Band filter weights */
+    if (cflag) {
+	cbweights(&cbfilter[0], L, 19);
+	cbsum = 0.0;
+	for (i = 0; i < 19; i++)
+	    cbsum += cbfilter[i];
+    }
+
+    while ((n = fread(data, sizeof(*data), L, stdin)) > 0) {
+	if (n != L) {
+	    fprintf(stderr, "dfft: warning - partial record, adding %d zeros\n", L-n);
+	    memset((char *)&data[n], 0, L-n);
+	}
+
+	/* Do a spectrum */
+	/* if (L == 256) rfft256(data); etc. XXX */
+	rfft(data, L);
+
+	/* Put it on screen */
+	if (phase)
+	    fftphase(data, L);
+	else
+	    fftdisp(data, L, cbsum);
+    }
+
+    return 0;
 }
 
 
