71b2ab3045ed8a15c00bc1247ecca1dac01ee938
Thomas Browder <tom.browder@gmail.com>
2013-12-23 20:28:55 +0000

    updates to get BRL-CAD C source files compiled warning- and error-free   with a C++ compiler:
    add macros in raytrace.h for proper casting of rt_functab functions to
    proper form
    
    use macros in table.c on all rt_functab functions
    
    successful debug build on Debian 7, 64-bit:
      no warnings or errors (except for the usual from libgiam, tcl, and step)
    
    successful 'make regress', 'make test', and 'make benchmark'
    
    svn:revision:59130
    svn:branch:trunk
    svn:account:tbrowder2

--- include/raytrace.h
+++ include/raytrace.h
@@ -2158,111 +2158,177 @@ struct rt_functab {
     int (*ft_prep)(struct soltab *stp,
 		   struct rt_db_internal *ip,
 		   struct rt_i *rtip);
+#define RTFUNCTAB_FUNC_PREP_CAST(_func) ((int (*)(struct soltab *, struct rt_db_internal *, struct rt_i *))_func)
+
     int (*ft_shot)(struct soltab *stp,
 		   struct xray *rp,
 		   struct application *ap, /* has resource */
 		   struct seg *seghead);
+#define RTFUNCTAB_FUNC_SHOT_CAST(_func) ((int (*)(struct soltab *, struct xray *, struct application *, struct seg *))_func)
+
     void (*ft_print)(const struct soltab *stp);
+#define RTFUNCTAB_FUNC_PRINT_CAST(_func) ((void (*)(const struct soltab *))_func)
+
     void (*ft_norm)(struct hit *hitp,
 		    struct soltab *stp,
 		    struct xray *rp);
+#define RTFUNCTAB_FUNC_NORM_CAST(_func) ((void (*)(struct hit *, struct soltab *, struct xray *))_func)
+
     int (*ft_piece_shot)(struct rt_piecestate *psp,
 			 struct rt_piecelist *plp,
 			 double dist, /* correction to apply to hit distances */
 			 struct xray *ray, /* ray transformed to be near cut cell */
 			 struct application *ap, /* has resource */
 			 struct seg *seghead);	/* used only for PLATE mode hits */
+#define RTFUNCTAB_FUNC_PIECE_SHOT_CAST(_func) ((int (*)(struct rt_piecestate *, struct rt_piecelist *, double dist, struct xray *, struct application *, struct seg *))_func)
+
     void (*ft_piece_hitsegs)(struct rt_piecestate *psp,
 			     struct seg *seghead,
 			     struct application *ap); /* has resource */
+#define RTFUNCTAB_FUNC_PIECE_HITSEGS_CAST(_func) ((void (*)(struct rt_piecestate *, struct seg *, struct application *))_func)
+
     void (*ft_uv)(struct application *ap, /* has resource */
 		  struct soltab *stp,
 		  struct hit *hitp,
 		  struct uvcoord *uvp);
+#define RTFUNCTAB_FUNC_UV_CAST(_func) ((void (*)(struct application *, struct soltab *, struct hit *, struct uvcoord *))_func)
+
     void (*ft_curve)(struct curvature *cvp,
 		     struct hit *hitp,
 		     struct soltab *stp);
+#define RTFUNCTAB_FUNC_CURVE_CAST(_func) ((void (*)(struct curvature *, struct hit *, struct soltab *))_func)
+
     int (*ft_classify)(const struct soltab * /*stp*/, const vect_t /*min*/, const vect_t /*max*/, const struct bn_tol * /*tol*/);
+#define RTFUNCTAB_FUNC_CLASS_CAST(_func) ((int (*)(const struct soltab *, const vect_t, const vect_t, const struct bn_tol *))_func)
+
     void (*ft_free)(struct soltab * /*stp*/);
+#define RTFUNCTAB_FUNC_FREE_CAST(_func) ((void (*)(struct soltab *))_func)
 
     int (*ft_plot)(struct bu_list * /*vhead*/,
 		   struct rt_db_internal * /*ip*/,
 		   const struct rt_tess_tol * /*ttol*/,
 		   const struct bn_tol * /*tol*/,
 		   const struct rt_view_info * /*view info*/);
+#define RTFUNCTAB_FUNC_PLOT_CAST(_func) ((int (*)(struct bu_list *, struct rt_db_internal *, const struct rt_tess_tol *, const struct bn_tol *, const struct rt_view_info *))_func)
+
     int (*ft_adaptive_plot)(struct rt_db_internal * /*ip*/,
 		   const struct rt_view_info * /*view info*/);
+#define RTFUNCTAB_FUNC_ADAPTIVE_PLOT_CAST(_func) ((int (*)(struct rt_db_internal *, const struct rt_view_info *))_func)
+
     void (*ft_vshot)(struct soltab * /*stp*/[],
 		     struct xray *[] /*rp*/,
 		     struct seg * /*segp*/,
 		     int /*n*/,
 		     struct application * /*ap*/);
+#define RTFUNCTAB_FUNC_VSHOT_CAST(_func) ((void (*)(struct soltab *[], struct xray *[], struct seg *, int, struct application *))_func)
+
     int (*ft_tessellate)(struct nmgregion ** /*r*/,
 			 struct model * /*m*/,
 			 struct rt_db_internal * /*ip*/,
 			 const struct rt_tess_tol * /*ttol*/,
 			 const struct bn_tol * /*tol*/);
+#define RTFUNCTAB_FUNC_TESS_CAST(_func) ((int (*)(struct nmgregion **, struct model *, struct rt_db_internal *, const struct rt_tess_tol *, const struct bn_tol *))_func)
     int (*ft_tnurb)(struct nmgregion ** /*r*/,
 		    struct model * /*m*/,
 		    struct rt_db_internal * /*ip*/,
 		    const struct bn_tol * /*tol*/);
+#define RTFUNCTAB_FUNC_TNURB_CAST(_func) ((int (*)(struct nmgregion **, struct model *, struct rt_db_internal *, const struct bn_tol *))_func)
+
     void (*ft_brep)(ON_Brep ** /*b*/,
 		    struct rt_db_internal * /*ip*/,
 		    const struct bn_tol * /*tol*/);
+#define RTFUNCTAB_FUNC_BREP_CAST(_func) ((void (*)(ON_Brep **, struct rt_db_internal *, const struct bn_tol *))_func)
+
     int (*ft_import5)(struct rt_db_internal * /*ip*/,
 		      const struct bu_external * /*ep*/,
 		      const mat_t /*mat*/,
 		      const struct db_i * /*dbip*/,
 		      struct resource * /*resp*/);
+#define RTFUNCTAB_FUNC_IMPORT5_CAST(_func) ((int (*)(struct rt_db_internal *, const struct bu_external *, const mat_t, const struct db_i *, struct resource *))_func)
+
     int (*ft_export5)(struct bu_external * /*ep*/,
 		      const struct rt_db_internal * /*ip*/,
 		      double /*local2mm*/,
 		      const struct db_i * /*dbip*/,
 		      struct resource * /*resp*/);
+#define RTFUNCTAB_FUNC_EXPORT5_CAST(_func) ((int (*)(struct bu_external *, const struct rt_db_internal *, double, const struct db_i *, struct resource *))_func)
+
     int (*ft_import4)(struct rt_db_internal * /*ip*/,
 		      const struct bu_external * /*ep*/,
 		      const mat_t /*mat*/,
 		      const struct db_i * /*dbip*/,
 		      struct resource * /*resp*/);
+#define RTFUNCTAB_FUNC_IMPORT4_CAST(_func) ((int (*)(struct rt_db_internal *, const struct bu_external *, const mat_t, const struct db_i *, struct resource *))_func)
+
     int	(*ft_export4)(struct bu_external * /*ep*/,
 		      const struct rt_db_internal * /*ip*/,
 		      double /*local2mm*/,
 		      const struct db_i * /*dbip*/,
 		      struct resource * /*resp*/);
+#define RTFUNCTAB_FUNC_EXPORT4_CAST(_func) ((int (*)(struct bu_external *, const struct rt_db_internal *, double, const struct db_i *, struct resource *))_func)
+
     void (*ft_ifree)(struct rt_db_internal * /*ip*/);
+#define RTFUNCTAB_FUNC_IFREE_CAST(_func) ((void (*)(struct rt_db_internal *))_func)
+
     int	(*ft_describe)(struct bu_vls * /*str*/,
 		       const struct rt_db_internal * /*ip*/,
 		       int /*verbose*/,
 		       double /*mm2local*/,
 		       struct resource * /*resp*/,
 		       struct db_i *);
+#define RTFUNCTAB_FUNC_DESCRIBE_CAST(_func) ((int (*)(struct bu_vls *, const struct rt_db_internal *, int, double, struct resource *, struct db_i *))_func)
+
     int	(*ft_xform)(struct rt_db_internal * /*op*/,
 		    const mat_t /*mat*/, struct rt_db_internal * /*ip*/,
 		    int /*free*/, struct db_i * /*dbip*/,
 		    struct resource * /*resp*/);
+#define RTFUNCTAB_FUNC_XFORM_CAST(_func) ((int (*)(struct rt_db_internal *, const mat_t, struct rt_db_internal *, int, struct db_i *, struct resource *))_func)
+
     const struct bu_structparse *ft_parsetab;	/**< @brief  rt_xxx_parse */
     size_t ft_internal_size;	/**< @brief  sizeof(struct rt_xxx_internal) */
     uint32_t ft_internal_magic;	/**< @brief  RT_XXX_INTERNAL_MAGIC */
+
     int	(*ft_get)(struct bu_vls *, const struct rt_db_internal *, const char *item);
+#define RTFUNCTAB_FUNC_GET_CAST(_func) ((int (*)(struct bu_vls *, const struct rt_db_internal *, const char *))_func)
+
     int	(*ft_adjust)(struct bu_vls *, struct rt_db_internal *, int /*argc*/, const char ** /*argv*/);
+#define RTFUNCTAB_FUNC_ADJUST_CAST(_func) ((int (*)(struct bu_vls *, struct rt_db_internal *, int, const char **))_func)
+
     int	(*ft_form)(struct bu_vls *, const struct rt_functab *);
+#define RTFUNCTAB_FUNC_FORM_CAST(_func) ((int (*)(struct bu_vls *, const struct rt_functab *))_func)
 
     void (*ft_make)(const struct rt_functab *, struct rt_db_internal * /*ip*/);
+#define RTFUNCTAB_FUNC_MAKE_CAST(_func) ((void (*)(const struct rt_functab *, struct rt_db_internal *))_func)
+
     int (*ft_params)(struct pc_pc_set *, const struct rt_db_internal * /*ip*/);
+#define RTFUNCTAB_FUNC_PARAMS_CAST(_func) ((int (*)(struct pc_pc_set *, const struct rt_db_internal *))_func)
+
     /* Axis aligned bounding box */
     int (*ft_bbox)(struct rt_db_internal * /*ip*/,
 		   point_t * /*min X, Y, Z of bounding RPP*/,
 		   point_t * /*max X, Y, Z of bounding RPP*/,
 		   const struct bn_tol *);
+#define RTFUNCTAB_FUNC_BBOX_CAST(_func) ((int (*)(struct rt_db_internal *, point_t *, point_t *, const struct bn_tol *))_func)
+
     void (*ft_volume)(fastf_t * /*vol*/, const struct rt_db_internal * /*ip*/);
+#define RTFUNCTAB_FUNC_VOLUME_CAST(_func) ((void (*)(fastf_t *, const struct rt_db_internal *))_func)
+
     void (*ft_surf_area)(fastf_t * /*area*/, const struct rt_db_internal * /*ip*/);
+#define RTFUNCTAB_FUNC_SURF_AREA_CAST(_func) ((void (*)(fastf_t *, const struct rt_db_internal *))_func)
+
     void (*ft_centroid)(point_t * /*cent*/, const struct rt_db_internal * /*ip*/);
+#define RTFUNCTAB_FUNC_CENTROID_CAST(_func) ((void (*)(point_t *, const struct rt_db_internal *))_func)
+
     int (*ft_oriented_bbox)(struct rt_arb_internal * /* bounding arb8 */,
 		   struct rt_db_internal * /*ip*/,
 		   const fastf_t);
+#define RTFUNCTAB_FUNC_ORIENTED_BBOX_CAST(_func) ((int (*)(struct rt_arb_internal *, struct rt_db_internal *, const fastf_t))_func)
+
     /** get a list of the selections matching a query */
     struct rt_selection_list *(*ft_find_selections)(const struct rt_db_internal *,
 						   const struct rt_selection_query *);
+#define RTFUNCTAB_FUNC_FIND_SELECTIONS_CAST(_func) ((struct rt_selection_list *(*)(const struct rt_db_internal *, const struct rt_selection_query *))_func)
+
     /** evaluate a logical selection expression (e.g. a INTERSECT b,
      *  NOT a) to create a new selection
      */
@@ -2270,10 +2336,13 @@ struct rt_functab {
 						 int op,
 						 const struct rt_selection *,
 						 const struct rt_selection *);
+#define RTFUNCTAB_FUNC_EVALUATE_SELECTION_CAST(_func) ((struct rt_selection *(*)(const struct rt_db_internal *, int op, const struct rt_selection *, const struct rt_selection *))_func)
+
     /** apply an operation to a selected subset of a primitive */
     int (*ft_process_selection)(struct rt_db_internal *,
 				const struct rt_selection *,
 				const struct rt_selection_operation *);
+#define RTFUNCTAB_FUNC_PROCESS_SELECTION_CAST(_func) ((int (*)(struct rt_db_internal *, const struct rt_selection *, const struct rt_selection_operation *))_func)
 };
 
 
