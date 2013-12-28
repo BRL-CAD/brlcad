16c60640e573eb659a8d8f53c34e275432aa275d
Christopher Sean Morrison <brlcad@gmail.com>
2013-12-28 17:36:45 +0000

    stub in an initial interface file for libcursor, which defines several functions wrapping termcap
    svn:revision:59215
    svn:branch:trunk
    svn:account:brlcad

---
+++ include/cursor.h
@@ -0,0 +1,39 @@
+/*                        C U R S O R . H
+ * BRL-CAD
+ *
+ * Copyright (c) 2013 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file cursor.h
+ *
+ * These are declarations of functions provided by libcursor, a simple
+ * wrapper library to terminal capabilities (termcap).
+ *
+ */
+
+
+extern int InitTermCap(FILE *fp);
+
+
+/*
+ * Local Variables:
+ * tab-width: 8
+ * mode: C
+ * indent-tabs-mode: t
+ * c-file-style: "stroustrup"
+ * End:
+ * ex: shiftwidth=4 tabstop=8
+ */
