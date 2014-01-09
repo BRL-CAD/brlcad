6f67e0c97c241db41d50c572a3f95721481d4fe7
Nicholas Reed <nreed1@umbc.edu>
2014-01-09 21:27:20 +0000

    actually allocate and initialize entries that are newly created
    svn:revision:59339
    svn:branch:trunk
    svn:account:n_reed

--- src/libged/select.c
+++ src/libged/select.c
@@ -513,6 +513,13 @@ ged_get_object_selections(struct ged *gedp, const char *object_name)
     entry = bu_hash_tbl_add(gedp->ged_selections, (unsigned char *)object_name,
 	    strlen(object_name), &int_new);
 
+    if (int_new) {
+	struct rt_object_selections *obj_selections;
+	BU_ALLOC(obj_selections, struct rt_object_selections);
+	obj_selections->sets = bu_hash_tbl_create(0);
+	bu_set_hash_value(entry, (unsigned char *)obj_selections);
+    }
+
     return (struct rt_object_selections *)bu_get_hash_value(entry);
 }
 
@@ -526,6 +533,13 @@ ged_get_selection_set(struct ged *gedp, const char *object_name, const char *sel
     obj_selections = ged_get_object_selections(gedp, object_name);
     entry = bu_hash_tbl_add(obj_selections->sets,
 		(const unsigned char *)selection_name, strlen(selection_name), &int_new);
+
+    if (int_new) {
+	struct rt_selection_set *set;
+	BU_ALLOC(set, struct rt_selection_set);
+	BU_PTBL_INIT(&set->selections);
+	bu_set_hash_value(entry, (unsigned char *)set);
+    }
 
     return (struct rt_selection_set *)bu_get_hash_value(entry);
 }
