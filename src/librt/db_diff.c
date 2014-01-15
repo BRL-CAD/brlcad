c2d4d0d541e79fc52b77a5a55e86ef3f665cf878
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2014-01-15 22:00:13 +0000

    collapse if conditionals per Sean's suggestion
    svn:revision:59416
    svn:branch:trunk
    svn:account:starseeker

--- src/librt/db_diff.c
+++ src/librt/db_diff.c
@@ -80,8 +80,7 @@ db_diff(const struct db_i *dbip1,
 	/* check if this object exists in the other database */
 	if ((dp2 = db_lookup(dbip2, dp1->d_namep, 0)) == RT_DIR_NULL) {
 	    this_diff++;
-	    if (del_func)
-		if (del_func(dbip1, dbip2, dp1, client_data)) error--;
+	    if (del_func && del_func(dbip1, dbip2, dp1, client_data)) error--;
 	    continue;
 	}
 
@@ -94,11 +93,9 @@ db_diff(const struct db_i *dbip1,
 
 	if (db_diff_external(&ext1, &ext2)) {
 	    this_diff++;
-	    if (chgd_func)
-		if (chgd_func(dbip1, dbip2, dp1, dp2, client_data)) error--;
+	    if (chgd_func && chgd_func(dbip1, dbip2, dp1, dp2, client_data)) error--;
 	} else {
-	    if (unch_func)
-		if (unch_func(dbip1, dbip2, dp1, client_data)) error--;
+	    if (unch_func && unch_func(dbip1, dbip2, dp1, client_data)) error--;
 	}
 
 	ret += this_diff;
@@ -117,8 +114,7 @@ db_diff(const struct db_i *dbip1,
 	/* check if this object exists in the other database */
 	if ((dp1 = db_lookup(dbip1, dp2->d_namep, 0)) == RT_DIR_NULL) {
 	    this_diff++;
-	    if (add_func)
-		if (add_func(dbip1, dbip2, dp2, client_data)) error--;
+	    if (add_func && add_func(dbip1, dbip2, dp2, client_data)) error--;
 	}
 
 	ret += this_diff;
