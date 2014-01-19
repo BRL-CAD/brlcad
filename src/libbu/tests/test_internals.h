8e15721dcba136103f95c3f6d817d445b411dfc0
Thomas Browder <tom.browder@gmail.com>
2014-01-19 11:57:48 +0000

    add a new header to help standardize correct use of zero or one for CTest's pass/fail convention
    svn:revision:59462
    svn:branch:trunk
    svn:account:tbrowder2

---
+++ src/libbu/tests/test_internals.h
@@ -0,0 +1,41 @@
+/*                  T E S T _ I N T E R N A L S . H
+ * BRL-CAD
+ *
+ * Copyright (c) 2014 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+
+
+#ifndef LIBBU_TESTS_TEST_INTERNALS_H
+#define LIBBU_TESTS_TEST_INTERNALS_H
+
+/* define pass/fail per CMake/CTest testing convention */
+const int PASS = 0;
+const int FAIL = 1;
+
+#endif /* LIBBU_TESTS_TEST_INTERNALS_H */
+
+
+/*
+ * Local Variables:
+ * mode: C
+ * tab-width: 8
+ * indent-tabs-mode: t
+ * c-file-style: "stroustrup"
+ * End:
+ * ex: shiftwidth=4 tabstop=8
+ */
+
