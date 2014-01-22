5d43addcb9839e117b14ac1ffa2ed2e774441782
Nicholas Reed <nreed1@umbc.edu>
2014-01-22 17:40:23 +0000

    move Subcurve member definitions to their own file
    svn:revision:59484
    svn:branch:trunk
    svn:account:n_reed

--- include/brep.h
+++ include/brep.h
@@ -1759,92 +1759,15 @@ public:
     Subcurve *m_children[2];
     ON_BOOL32 m_islinear;
 
-    Subcurve() : m_curve(NULL), m_islinear(false)
-    {
-	m_children[0] = m_children[1] = NULL;
-    }
-    Subcurve(ON_Curve *curve)
-    {
-	m_curve = curve;
-	if (curve) {
-	    m_node = curve->BoundingBox();
-	    m_t = curve->Domain();
-	    m_islinear = curve->IsLinear();
-	} else {
-	    m_islinear = false;
-	}
-	m_children[0] = m_children[1] = NULL;
-    }
-    Subcurve(const Subcurve &_scurve)
-    {
-	m_islinear = _scurve.m_islinear;
-	m_curve = _scurve.m_curve->Duplicate();
-	m_t = _scurve.m_t;
-	m_children[0] = m_children[1] = NULL;
-	SetBBox(_scurve.m_node);
-    }
-    ~Subcurve()
-    {
-	for (int i = 0; i < 2; i++) {
-	    if (m_children[i]) {
-		delete m_children[i];
-	    }
-	}
-	delete m_curve;
-    }
-    int Split()
-    {
-	if (m_children[0] && m_children[1]) {
-	    return 0;
-	}
-
-	for (int i = 0; i < 2; i++) {
-	    m_children[i] = new Subcurve();
-	}
-	ON_SimpleArray<double> pline_t;
-	double split_t = m_t.Mid();
-	if (m_curve->IsPolyline(NULL, &pline_t) && pline_t.Count() > 2) {
-	    split_t = pline_t[pline_t.Count() / 2];
-	}
-	if (m_curve->Split(split_t, m_children[0]->m_curve, m_children[1]->m_curve) == false) {
-	    return -1;
-	}
-
-	m_children[0]->m_t = m_children[0]->m_curve->Domain();
-	m_children[0]->SetBBox(m_children[0]->m_curve->BoundingBox());
-	m_children[0]->m_islinear = m_children[0]->m_curve->IsLinear();
-	m_children[1]->m_t = m_children[1]->m_curve->Domain();
-	m_children[1]->SetBBox(m_children[1]->m_curve->BoundingBox());
-	m_children[1]->m_islinear = m_children[1]->m_curve->IsLinear();
-
-	return 0;
-    }
-    void GetBBox(ON_3dPoint &min, ON_3dPoint &max)
-    {
-	min = m_node.m_min;
-	max = m_node.m_max;
-    }
-    void SetBBox(const ON_BoundingBox &bbox)
-    {
-	m_node = bbox;
-    }
-    bool IsPointIn(const ON_3dPoint &pt, double tolerance = 0.0)
-    {
-	ON_3dVector vtol(tolerance, tolerance, tolerance);
-	ON_BoundingBox new_bbox(m_node.m_min - vtol, m_node.m_max + vtol);
-	return new_bbox.IsPointIn(pt);
-    }
-    bool Intersect(const Subcurve &other, double tolerance = 0.0, ON_BoundingBox *intersection = NULL) const
-    {
-	ON_3dVector vtol(tolerance, tolerance, tolerance);
-	ON_BoundingBox new_bbox(m_node.m_min - vtol, m_node.m_max + vtol);
-	ON_BoundingBox box;
-	bool ret = box.Intersection(new_bbox, other.m_node);
-	if (intersection != NULL) {
-	    *intersection = box;
-	}
-	return ret;
-    }
+    Subcurve();
+    Subcurve(ON_Curve *curve);
+    Subcurve(const Subcurve &_scurve);
+    ~Subcurve();
+    int Split();
+    void GetBBox(ON_3dPoint &min, ON_3dPoint &max);
+    void SetBBox(const ON_BoundingBox &bbox);
+    bool IsPointIn(const ON_3dPoint &pt, double tolerance = 0.0);
+    bool Intersect(const Subcurve &other, double tolerance = 0.0, ON_BoundingBox *intersection = NULL) const;
 };
 
 /* Sub-division support for a surface.
