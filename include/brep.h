cf7d2931758fb602315e20cbeadbfc7e8446877b
Nicholas Reed <nreed1@umbc.edu>
2014-01-22 02:29:36 +0000

    moving ON_Ray member definitions outside the class definition, but keeping them in the header
    svn:revision:59481
    svn:branch:trunk
    svn:account:n_reed

--- include/brep.h
+++ include/brep.h
@@ -133,39 +133,13 @@ public:
     ON_3dPoint m_origin;
     ON_3dVector m_dir;
 
-    ON_Ray(ON_3dPoint &origin, ON_3dVector &dir) : m_origin(origin), m_dir(dir) {}
+    ON_Ray(ON_3dPoint &origin, ON_3dVector &dir);
+    ON_Ray(ON_2dPoint &origin, ON_2dVector &dir);
+    ON_Ray(const ON_Ray &r);
 
-    ON_Ray(ON_2dPoint &origin, ON_2dVector &dir) : m_origin(origin), m_dir(dir) {}
-
-    ON_Ray(const ON_Ray &r) : m_origin(r.m_origin), m_dir(r.m_dir) {}
-
-    ON_Ray &operator=(const ON_Ray &r)
-    {
-	m_origin = r.m_origin;
-	m_dir = r.m_dir;
-	return *this;
-    }
-
-    ON_3dPoint PointAt(double t) const
-    {
-	return m_origin + m_dir * t;
-    }
-
-    double DistanceTo(const ON_3dPoint &pt, double *out_t = NULL) const
-    {
-	ON_3dVector w = pt - m_origin;
-	double c1 = w * m_dir;
-	if (c1 <= 0) {
-	    return pt.DistanceTo(m_origin);
-	}
-	double c2 = m_dir * m_dir;
-	double b = c1 / c2;
-	ON_3dPoint p = m_dir * b + m_origin;
-	if (out_t != NULL) {
-	    *out_t = b;
-	}
-	return p.DistanceTo(pt);
-    }
+    ON_Ray &operator=(const ON_Ray &r);
+    ON_3dPoint PointAt(double t) const;
+    double DistanceTo(const ON_3dPoint &pt, double *out_t = NULL) const;
 
     /**
      * Intersect two 2d Rays
@@ -173,19 +147,75 @@ public:
      * @param isect [out] point of intersection
      * @return true if single point of intersection is found
      */
-    bool IntersectRay(const ON_Ray &v, ON_2dPoint &isect) const
-    {
-	double uxv, q_pxv;
-	/* consider parallel and collinear cases */
-	if (ZERO(uxv = V2CROSS(m_dir, v.m_dir)) ||
-	    (ZERO(q_pxv = V2CROSS(v.m_origin - m_origin, v.m_dir))))
-	{
-	    return false;
-	}
-	isect = PointAt(q_pxv / uxv);
-	return true;
-    }
+    bool IntersectRay(const ON_Ray &v, ON_2dPoint &isect) const;
 };
+
+inline
+ON_Ray::ON_Ray(ON_3dPoint &origin, ON_3dVector &dir)
+    : m_origin(origin), m_dir(dir)
+{
+}
+
+inline
+ON_Ray::ON_Ray(ON_2dPoint &origin, ON_2dVector &dir)
+    : m_origin(origin), m_dir(dir)
+{
+}
+
+inline
+ON_Ray::ON_Ray(const ON_Ray &r)
+    : m_origin(r.m_origin), m_dir(r.m_dir)
+{
+}
+
+inline
+ON_Ray &
+ON_Ray::operator=(const ON_Ray &r)
+{
+    m_origin = r.m_origin;
+    m_dir = r.m_dir;
+    return *this;
+}
+
+inline
+ON_3dPoint
+ON_Ray::PointAt(double t) const
+{
+    return m_origin + m_dir * t;
+}
+
+inline
+double
+ON_Ray::DistanceTo(const ON_3dPoint &pt, double *out_t /* = NULL */) const
+{
+    ON_3dVector w = pt - m_origin;
+    double c1 = w * m_dir;
+    if (c1 <= 0) {
+	return pt.DistanceTo(m_origin);
+    }
+    double c2 = m_dir * m_dir;
+    double b = c1 / c2;
+    ON_3dPoint p = m_dir * b + m_origin;
+    if (out_t != NULL) {
+	*out_t = b;
+    }
+    return p.DistanceTo(pt);
+}
+
+inline
+bool
+ON_Ray::IntersectRay(const ON_Ray &v, ON_2dPoint &isect) const
+{
+    double uxv, q_pxv;
+    /* consider parallel and collinear cases */
+    if (ZERO(uxv = V2CROSS(m_dir, v.m_dir)) ||
+	(ZERO(q_pxv = V2CROSS(v.m_origin - m_origin, v.m_dir))))
+    {
+	return false;
+    }
+    isect = PointAt(q_pxv / uxv);
+    return true;
+}
 
 BREP_EXPORT void brep_get_plane_ray(ON_Ray &r, plane_ray &pr);
 BREP_EXPORT void brep_r(const ON_Surface *surf, plane_ray &pr, pt2d_t uv, ON_3dPoint &pt, ON_3dVector &su, ON_3dVector &sv, pt2d_t R);
