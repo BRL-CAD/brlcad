cc29af91cb2b8f13ce4e96b4b1b554fa742f9475
Nicholas Reed <nreed1@umbc.edu>
2014-01-22 17:49:55 +0000

    move Subsurface member definitions to their own file
    svn:revision:59485
    svn:branch:trunk
    svn:account:n_reed

--- include/brep.h
+++ include/brep.h
@@ -1784,138 +1784,17 @@ public:
     Subsurface *m_children[4];
     ON_BOOL32 m_isplanar;
 
-    Subsurface() : m_surf(NULL), m_isplanar(false)
-    {
-	m_children[0] = m_children[1] = m_children[2] = m_children[3] = NULL;
-    }
-    Subsurface(ON_Surface *surf)
-    {
-	m_surf = surf;
-	if (surf) {
-	    SetBBox(surf->BoundingBox());
-	    m_u = surf->Domain(0);
-	    m_v = surf->Domain(1);
-	    m_isplanar = surf->IsPlanar();
-	} else {
-	    m_isplanar = false;
-	}
-	m_children[0] = m_children[1] = m_children[2] = m_children[3] = NULL;
-    }
-    Subsurface(const Subsurface &_ssurf)
-    {
-	m_surf = _ssurf.m_surf->Duplicate();
-	m_u = _ssurf.m_u;
-	m_v = _ssurf.m_v;
-	m_isplanar = _ssurf.m_isplanar;
-	m_children[0] = m_children[1] = m_children[2] = m_children[3] = NULL;
-	SetBBox(_ssurf.m_node);
-    }
-    ~Subsurface()
-    {
-	for (int i = 0; i < 4; i++) {
-	    if (m_children[i]) {
-		delete m_children[i];
-	    }
-	}
-	delete m_surf;
-    }
-    int Split()
-    {
-	if (m_children[0] && m_children[1] && m_children[2] && m_children[3]) {
-	    return 0;
-	}
+    Subsurface();
+    Subsurface(ON_Surface *surf);
+    Subsurface(const Subsurface &_ssurf);
+    ~Subsurface();
 
-	for (int i = 0; i < 4; i++) {
-	    m_children[i] = new Subsurface();
-	}
-	ON_Surface *temp_surf1 = NULL, *temp_surf2 = NULL;
-	ON_BOOL32 ret = true;
-	ret = m_surf->Split(0, m_surf->Domain(0).Mid(), temp_surf1, temp_surf2);
-	if (!ret) {
-	    delete temp_surf1;
-	    delete temp_surf2;
-	    return -1;
-	}
-
-	ret = temp_surf1->Split(1, m_surf->Domain(1).Mid(), m_children[0]->m_surf, m_children[1]->m_surf);
-	delete temp_surf1;
-	if (!ret) {
-	    delete temp_surf2;
-	    return -1;
-	}
-	m_children[0]->m_u = ON_Interval(m_u.Min(), m_u.Mid());
-	m_children[0]->m_v = ON_Interval(m_v.Min(), m_v.Mid());
-	m_children[0]->SetBBox(m_children[0]->m_surf->BoundingBox());
-	m_children[0]->m_isplanar = m_children[0]->m_surf->IsPlanar();
-	m_children[1]->m_u = ON_Interval(m_u.Min(), m_u.Mid());
-	m_children[1]->m_v = ON_Interval(m_v.Mid(), m_v.Max());
-	m_children[1]->SetBBox(m_children[1]->m_surf->BoundingBox());
-	m_children[1]->m_isplanar = m_children[1]->m_surf->IsPlanar();
-
-	ret = temp_surf2->Split(1, m_v.Mid(), m_children[2]->m_surf, m_children[3]->m_surf);
-	delete temp_surf2;
-	if (!ret) {
-	    return -1;
-	}
-	m_children[2]->m_u = ON_Interval(m_u.Mid(), m_u.Max());
-	m_children[2]->m_v = ON_Interval(m_v.Min(), m_v.Mid());
-	m_children[2]->SetBBox(m_children[2]->m_surf->BoundingBox());
-	m_children[2]->m_isplanar = m_children[2]->m_surf->IsPlanar();
-	m_children[3]->m_u = ON_Interval(m_u.Mid(), m_u.Max());
-	m_children[3]->m_v = ON_Interval(m_v.Mid(), m_v.Max());
-	m_children[3]->SetBBox(m_children[3]->m_surf->BoundingBox());
-	m_children[3]->m_isplanar = m_children[3]->m_surf->IsPlanar();
-
-	return 0;
-    }
-    void GetBBox(ON_3dPoint &min, ON_3dPoint &max)
-    {
-	min = m_node.m_min;
-	max = m_node.m_max;
-    }
-    void SetBBox(const ON_BoundingBox &bbox)
-    {
-	m_node = bbox;
-	/* Make sure that each dimension of the bounding box is greater than
-	 * ON_ZERO_TOLERANCE.
-	 * It does the same work as building the surface tree when ray tracing
-	 */
-	for (int i = 0; i < 3; i++) {
-	    double d = m_node.m_max[i] - m_node.m_min[i];
-	    if (ON_NearZero(d, ON_ZERO_TOLERANCE)) {
-		m_node.m_min[i] -= 0.001;
-		m_node.m_max[i] += 0.001;
-	    }
-	}
-    }
-    bool IsPointIn(const ON_3dPoint &pt, double tolerance = 0.0)
-    {
-	ON_3dVector vtol(tolerance, tolerance, tolerance);
-	ON_BoundingBox new_bbox(m_node.m_min - vtol, m_node.m_max + vtol);
-	return new_bbox.IsPointIn(pt);
-    }
-    bool Intersect(const Subcurve &curve, double tolerance = 0.0, ON_BoundingBox *intersection = NULL) const
-    {
-	ON_3dVector vtol(tolerance, tolerance, tolerance);
-	ON_BoundingBox new_bbox(m_node.m_min - vtol, m_node.m_max + vtol);
-	ON_BoundingBox box;
-	bool ret = box.Intersection(new_bbox, curve.m_node);
-	if (intersection != NULL) {
-	    *intersection = box;
-	}
-	return ret;
-    }
-    bool Intersect(const Subsurface &surf, double tolerance = 0.0, ON_BoundingBox *intersection = NULL) const
-    {
-	ON_3dVector vtol(tolerance, tolerance, tolerance);
-	ON_BoundingBox new_bbox(m_node.m_min - vtol, m_node.m_max + vtol);
-	ON_BoundingBox box;
-	bool ret = box.Intersection(new_bbox, surf.m_node);
-	if (intersection != NULL) {
-	    *intersection = ON_BoundingBox(box.m_min - vtol, box.m_max + vtol);
-	}
-	return ret;
-    }
+    int Split();
+    void GetBBox(ON_3dPoint &min, ON_3dPoint &max);
+    void SetBBox(const ON_BoundingBox &bbox);
+    bool IsPointIn(const ON_3dPoint &pt, double tolerance = 0.0);
+    bool Intersect(const Subcurve &curve, double tolerance = 0.0, ON_BoundingBox *intersection = NULL) const;
+    bool Intersect(const Subsurface &surf, double tolerance = 0.0, ON_BoundingBox *intersection = NULL) const;
 };
 
 /** The ON_PX_EVENT class is used to report point-point, point-curve
