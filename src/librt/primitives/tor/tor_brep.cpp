6f52ed78f7a1181512a7b0bc61db0ffeb6b13922
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2014-01-30 20:18:25 +0000

    The openNURBS torus brep conversion routine isn't able to handle tori with inner radius == 0 - it fails the validity check.  This routine goes ahead and creates the Brep anyway - we can apparently raytrace the result, but it causes some problems with our wireframe and shading codes...
    svn:revision:59597
    svn:branch:trunk
    svn:account:starseeker

--- src/librt/primitives/tor/tor_brep.cpp
+++ src/librt/primitives/tor/tor_brep.cpp
@@ -16,6 +16,23 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with this file; see the file named COPYING for more
  * information.
+ *
+ * Incorporates modified versions of openNURBS torus functions:
+ *
+ * Copyright (c) 1993-2012 Robert McNeel & Associates. All rights reserved.
+ * OpenNURBS, Rhinoceros, and Rhino3D are registered trademarks of Robert
+ * McNeel & Associates.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software.
+ *
+ * THIS SOFTWARE IS PROVIDED "AS IS" WITHOUT EXPRESS OR IMPLIED WARRANTY.
+ * ALL IMPLIED WARRANTIES OF FITNESS FOR ANY PARTICULAR PURPOSE AND OF
+ * MERCHANTABILITY ARE HEREBY DISCLAIMED.
+ *
  */
 /** @file tor_brep.cpp
  *
@@ -28,6 +45,68 @@
 #include "raytrace.h"
 #include "rtgeom.h"
 #include "opennurbs_torus.h"
+
+
+HIDDEN ON_RevSurface*
+Torus_RevSurfaceForm(const ON_Torus& torus )
+{
+    ON_Circle circle = torus.MinorCircleRadians(0.0);
+    ON_ArcCurve* circle_crv = new ON_ArcCurve(circle);
+    ON_RevSurface* pRevSurface = new ON_RevSurface();
+    pRevSurface->m_angle.Set(0.0,2.0*ON_PI);
+    pRevSurface->m_t = pRevSurface->m_angle;
+    pRevSurface->m_curve = circle_crv;
+    pRevSurface->m_axis.from = torus.plane.origin;
+    pRevSurface->m_axis.to = torus.plane.origin + torus.plane.zaxis;
+    pRevSurface->m_bTransposed = false;
+    double r[2], h[2];
+    h[0] = fabs(torus.minor_radius);
+    h[1] = -h[0];
+    r[0] = fabs(torus.major_radius) + h[0];
+    r[1] = -r[0];
+    int i, j, k, n=0;
+    ON_3dPoint pt[8];
+    for (i=0;i<2;i++)
+    {
+	for (j=0;j<2;j++)
+	{
+	    for (k=0;k<2;k++)
+	    {
+		pt[n++] = torus.plane.PointAt( r[i], r[j], h[k] );
+	    }
+	}
+    }
+    pRevSurface->m_bbox.Set( 3, 0, 8, 3, &pt[0].x );
+    return pRevSurface;
+}
+
+ON_Brep*
+Torus_Brep( const ON_Torus& torus)
+{
+    ON_BOOL32 bArcLengthParameterization = true;
+    ON_Brep* brep = NULL;
+    ON_RevSurface* pRevSurface = Torus_RevSurfaceForm(torus);
+    if ( pRevSurface )
+    {
+	if ( bArcLengthParameterization )
+	{
+	    double r = fabs(torus.major_radius);
+	    if ( r <= ON_SQRT_EPSILON )
+		r = 1.0;
+	    r *= ON_PI;
+	    pRevSurface->SetDomain(0,0.0,2.0*r);
+	    r = fabs(torus.minor_radius);
+	    if ( r <= ON_SQRT_EPSILON )
+		r = 1.0;
+	    r *= ON_PI;
+	    pRevSurface->SetDomain(1,0.0,2.0*r);
+	}
+	brep = ON_BrepRevSurface( pRevSurface, false, false, NULL );
+	if ( !brep )
+	    delete pRevSurface;
+    }
+    return brep;
+}
 
 
 extern "C" void
@@ -43,7 +122,7 @@ rt_tor_brep(ON_Brep **b, const struct rt_db_internal *ip, const struct bn_tol *U
     ON_3dVector normal(tip->h);
     ON_Plane p(origin, normal);
     ON_Torus tor(p, tip->r_a, tip->r_h);
-    *b = ON_BrepTorus(tor);
+    *b = Torus_Brep(tor);
 }
 
 
