6f86120baad7f21a1d01b724a7470dc916d01020
Nicholas Reed <nreed1@umbc.edu>
2014-01-30 21:31:52 +0000

    unclosed comment
    svn:revision:59602
    svn:branch:trunk
    svn:account:n_reed

--- src/util/mac-pix.c
+++ src/util/mac-pix.c
@@ -66,7 +66,7 @@ int file_xoff;
 int file_yoff;
 int scr_width = 1024;	/* If this and scr_height are later found to be zero,
 			 * they assume the values of file_width and file_height .
-			 * /
+			 */
 int scr_height = 1024;
 int scr_xoff;
 int scr_yoff;
