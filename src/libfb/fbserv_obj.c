eae6b72edcbc93e1b130cc1d5de3af2b911bca0f
Richard Weiss <r_weiss@brlcad.org>
2014-02-03 17:13:38 +0000

    Quiet a windows build warning.
    svn:revision:59650
    svn:branch:trunk
    svn:account:r_weiss

--- src/libfb/fbserv_obj.c
+++ src/libfb/fbserv_obj.c
@@ -131,7 +131,7 @@ existing_client_handler(ClientData clientData, int UNUSED(mask))
 	    bu_log("pkg_process error encountered (2)\n");
     }
 
-    if (fbsp->fbs_callback != FBS_CALLBACK_NULL) {
+    if (fbsp->fbs_callback != (void (*)(genptr_t))FBS_CALLBACK_NULL) {
 	/* need to cast func pointer explicitly to get the function call */
 	void (*cfp)(void *);
 	cfp = (void (*)(void *))fbsp->fbs_callback;
