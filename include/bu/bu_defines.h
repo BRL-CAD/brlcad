276173c5867653283cfe4362dddd6d648ebefb54
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2014-02-12 22:18:47 +0000

    Update cmake and header
    svn:revision:59859
    svn:branch:trunk
    svn:account:starseeker

--- include/bu/bu_defines.h
+++ include/bu/bu_defines.h
@@ -1,4 +1,4 @@
-/*                      D E F I N E S . H
+/*                      B U _ D E F I N E S . H
  * BRL-CAD
  *
  * Copyright (c) 2004-2014 United States Government as represented by
@@ -49,7 +49,7 @@
 /** @defgroup binding Language Bindings */
 /**   @defgroup tcl Tcl Interfacing */
 
-/** @file defines.h
+/** @file bu_defines.h
  *
  * Commonly used definitions for the BRL-CAD Utility Library, LIBBU.
  *
