5e797d0ce89c562b30a3c3d03489a7bb5ecf644b
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2014-02-13 18:53:30 +0000

    Move doxygen files to subdir
    svn:revision:59868
    svn:branch:trunk
    svn:account:starseeker

---
+++ misc/doxygen/doxygen_structure_libbu.h
@@ -0,0 +1,90 @@
+
+/* Top level library group */
+/** @defgroup libbu libbu (utility functions) */
+
+/* Second tier groups */
+/** @ingroup libbu */
+/**  @defgroup container Data Containers */
+/** @ingroup libbu */
+/**  @defgroup memory Memory Management */
+/** @ingroup libbu */
+/**  @defgroup io Input/Output */
+/** @ingroup libbu */
+/**  @defgroup data Data Management */
+/** @ingroup libbu */
+/**  @defgroup parallel  Parallel Processing */
+/** @ingroup libbu */
+/**  @defgroup binding Language Bindings */
+
+/* Data Containers */
+/** @ingroup container */
+/**   @defgroup avs Attribute/Value Sets */
+/** @ingroup container */
+/**   @defgroup bitv Bit Vectors */
+/** @ingroup container */
+/**   @defgroup color Color */
+/** @ingroup container */
+/**   @defgroup hash Hash Tables */
+/** @ingroup container */
+/**   @defgroup list Linked Lists */
+/** @ingroup container */
+/**   @defgroup parse Structure Parsing */
+/** @ingroup container */
+/**   @defgroup ptbl Pointer Tables */
+/** @ingroup container */
+/**   @defgroup rb Red-Black Trees */
+/** @ingroup container */
+/**   @defgroup vlb Variable-length Byte Buffers */
+/** @ingroup container */
+/**   @defgroup vls Variable-length Strings */
+
+/* Memory Management */
+/** @ingroup memory */
+/**   @defgroup magic Magic Numbers */
+/** @ingroup memory */
+/**   @defgroup malloc Allocation & Deallocation */
+/** @ingroup memory */
+/**   @defgroup mf Memory-mapped Files */
+
+/* I/O */
+/** @ingroup io */
+/**   @defgroup log Logging */
+/** @ingroup io */
+/**   @defgroup debug Debugging */
+/** @ingroup io */
+/**   @defgroup file File Processing */
+/** @ingroup io */
+/**   @defgroup vfont Vector Fonts */
+
+/* Data Management */
+/** @ingroup data */
+/**   @defgroup cmd Command History */
+/** @ingroup data */
+/**   @defgroup conv Data Conversion */
+/** @ingroup data */
+/**   @defgroup getopt Command-line Option Parsing*/
+/** @ingroup data */
+/**   @defgroup hton Network Byte-order Conversion */
+/** @ingroup data */
+/**   @defgroup hist Histogram Handling */
+/** @ingroup data */
+/**   @defgroup endian Endian Support */
+
+
+/* Parallel Processing */
+/** @ingroup parallel */
+/**   @defgroup thread Multithreading */
+
+/* Language Bindings */
+/** @ingroup binding */
+/**   @defgroup tcl Tcl Interfacing */
+
+/*
+ * Local Variables:
+ * mode: C
+ * tab-width: 8
+ * indent-tabs-mode: t
+ * c-file-style: "stroustrup"
+ * End:
+ * ex: shiftwidth=4 tabstop=8
+ */
