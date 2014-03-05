cd244f8b7e40c43269c13c8330e85b1497bc0560
Christopher Sean Morrison <brlcad@gmail.com>
2014-03-05 07:45:09 +0000

    save all files, removed _WIN32 wrappage
    svn:revision:60065
    svn:branch:trunk
    svn:account:brlcad

--- src/conv/dxf/g-dxf.c
+++ src/conv/dxf/g-dxf.c
@@ -473,9 +473,7 @@ main(int argc, char *argv[])
 
     if (!output_file) {
 	fp = stdout;
-#if defined(_WIN32) && !defined(__CYGWIN__)
 	setmode(fileno(fp), O_BINARY);
-#endif
     } else {
 	/* Open output file */
 	if ((fp=fopen(output_file, "w+b")) == NULL) {
