74a912d5601e00cd22905eb675fdad14e28c3b78
Keith Boman <indianlarry@verizon.net>
2014-03-21 14:53:38 +0000

    New classes added to load assemblies from 'Context_Dependent_Shape_Representation' using the 'Next_Assembly_Usage_Occurrence'.
    svn:revision:60175
    svn:branch:trunk
    svn:account:indianlarry

---
+++ src/conv/step/step-g/AssemblyComponentUsage.h
@@ -0,0 +1,63 @@
+/*                 AssemblyComponentUsage.h
+ * BRL-CAD
+ *
+ * Copyright (c) 1994-2014 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file step/AssemblyComponentUsage.h
+ *
+ * Class definition used to convert STEP "AssemblyComponentUsage" to BRL-CAD BREP
+ * structures.
+ *
+ */
+
+#ifndef CONV_STEP_STEP_G_ASSEMBLYCOMPONENTUSAGE_H
+#define CONV_STEP_STEP_G_ASSEMBLYCOMPONENTUSAGE_H
+
+#include "ProductDefinitionUsage.h"
+
+class AssemblyComponentUsage: public ProductDefinitionUsage
+{
+private:
+    static string entityname;
+    static EntityInstanceFunc GetInstance;
+
+protected:
+    string reference_designator;
+
+public:
+    AssemblyComponentUsage();
+    virtual ~AssemblyComponentUsage();
+    AssemblyComponentUsage(STEPWrapper *sw, int step_id);
+    bool Load(STEPWrapper *sw, SDAI_Application_instance *sse);
+    virtual bool LoadONBrep(ON_Brep *brep);
+    virtual void Print(int level);
+
+    //static methods
+    static STEPEntity *Create(STEPWrapper *sw, SDAI_Application_instance *sse);
+};
+
+#endif /* CONV_STEP_STEP_G_ASSEMBLYCOMPONENTUSAGE_H */
+
+/*
+ * Local Variables:
+ * tab-width: 8
+ * mode: C
+ * indent-tabs-mode: t
+ * c-file-style: "stroustrup"
+ * End:
+ * ex: shiftwidth=4 tabstop=8
+ */
