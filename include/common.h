42bb56f5c4c52944343ade800d08b8a5e342d709
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2014-04-01 20:51:33 +0000

    Until we get a final resolution on the lrint issue, add an additional compile test so we don't keep trying to use the current approach on systems were it fails to build.
    svn:revision:60334
    svn:branch:trunk
    svn:account:starseeker

--- include/common.h
+++ include/common.h
@@ -58,7 +58,7 @@
 #    define srand48(seed) (srand(seed))
 #  endif
 
-#  if !defined(__cplusplus) && !defined(HAVE_LRINT)
+#  if !defined(__cplusplus) && !defined(HAVE_LRINT) && defined(HAVE_WORKING_LRINT_MACRO)
 #    define lrint(_x) ((long int)(((_x)<0)?(_x)-0.5:(_x)+0.5))
 #    define HAVE_LRINT 1
 #  endif
