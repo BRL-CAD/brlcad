a015421a1da3ce105054b33161aea5628af89be0
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2014-04-02 13:04:17 +0000

    Add Hyp revert/checkpoint support
    svn:revision:60350
    svn:branch:trunk
    svn:account:starseeker

--- src/tclscripts/archer/HypEditFrame.tcl
+++ src/tclscripts/archer/HypEditFrame.tcl
@@ -36,6 +36,8 @@
 	# Override what's in GeometryEditFrame
 	method initGeometry {gdata}
 	method updateGeometry {}
+	method checkpointGeometry {}
+	method revertGeometry {}
 	method createGeometry {obj}
 	method p {obj args}
     }
@@ -59,6 +61,20 @@
 	variable mAz ""
 	variable mB ""
 	variable mC ""
+
+	# Checkpoint values
+	variable checkpointed_name ""
+	variable cmVx ""
+	variable cmVy ""
+	variable cmVz ""
+	variable cmHx ""
+	variable cmHy ""
+	variable cmHz ""
+	variable cmAx ""
+	variable cmAy ""
+	variable cmAz ""
+	variable cmB ""
+	variable cmC ""
 
 	# Methods used by the constructor.
 	# Override methods in GeometryEditFrame.
@@ -113,6 +129,8 @@
     set mC [bu_get_value_by_keyword bnr $gdata]
 
     GeometryEditFrame::initGeometry $gdata
+    set curr_name $itk_option(-geometryObject)
+    if {$cmVx == "" || "$checkpointed_name" != "$curr_name"} {checkpointGeometry}
 }
 
 ::itcl::body HypEditFrame::updateGeometry {} {
@@ -129,6 +147,37 @@
 	bnr $mC
 
     GeometryEditFrame::updateGeometry
+}
+
+::itcl::body HypEditFrame::checkpointGeometry {} {
+    set checkpointed_name $itk_option(-geometryObject)
+    set cmVx $mVx
+    set cmVy $mVy
+    set cmVz $mVz
+    set cmHx $mHx
+    set cmHy $mHy
+    set cmHz $mHz
+    set cmAx $mAx
+    set cmAy $mAy
+    set cmAz $mAz
+    set cmB  $mB
+    set cmC  $mC
+}
+
+::itcl::body HypEditFrame::revertGeometry {} {
+    set mVx $cmVx
+    set mVy $cmVy
+    set mVz $cmVz
+    set mHx $cmHx
+    set mHy $cmHy
+    set mHz $cmHz
+    set mAx $cmAx
+    set mAy $cmAy
+    set mAz $cmAz
+    set mB  $cmB
+    set mC  $cmC
+
+    updateGeometry
 }
 
 ::itcl::body HypEditFrame::createGeometry {obj} {
@@ -338,6 +387,18 @@
 	    -anchor e
     } {}
 
+    itk_component add checkpointButton {
+	::ttk::button $parent.checkpointButton \
+	-text {CheckPoint} \
+	-command "[::itcl::code $this checkpointGeometry]"
+    } {}
+
+    itk_component add revertButton {
+	::ttk::button $parent.revertButton \
+	-text {Revert} \
+	-command "[::itcl::code $this revertGeometry]"
+    } {}
+
     set row 0
     grid $itk_component(hypType) \
 	-row $row \
@@ -392,6 +453,22 @@
 	-row $row \
 	-column 4 \
 	-sticky nsew
+
+    incr row
+    set col 0
+    grid $itk_component(checkpointButton) \
+	-row $row \
+	-column $col \
+	-columnspan 2 \
+	-sticky nsew
+    incr col
+    incr col
+    grid $itk_component(revertButton) \
+	-row $row \
+	-column $col \
+	-columnspan 2 \
+	-sticky nsew
+
     grid columnconfigure $parent 1 -weight 1
     grid columnconfigure $parent 2 -weight 1
     grid columnconfigure $parent 3 -weight 1
