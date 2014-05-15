57bec90626299a44a70d0583c6b1a3134197b3e6
Thomas Browder <tom.browder@gmail.com>
2014-05-15 20:14:47 +0000

    implement '-report' option
    svn:revision:60646
    svn:branch:d-binding
    svn:account:tbrowder2

--- include/convert-h2d.pl
+++ include/convert-h2d.pl
@@ -17,7 +17,7 @@ die "ERROR:  Reference compiler 'dmd' not found.\n"
 my $p = basename($0);
 if (!@ARGV) {
   print <<"HERE";
-Usage: $p -go [-force][-debug]
+Usage: $p -report [-force][-debug]
 
 Converts C .h files in this directory to rudimentary .d files
   for linking C to the D language.
@@ -28,6 +28,10 @@ HERE
 
 my $force = 0;
 my $debug = 0;
+
+# modes
+my $report = 0;
+
 foreach my $arg (@ARGV) {
   my $val = undef;
   if ($arg =~ m{\A -f}xms) {
@@ -35,6 +39,14 @@ foreach my $arg (@ARGV) {
   }
   elsif ($arg =~ m{\A -d}xms) {
     $debug = 1;
+  }
+  # modes
+  elsif ($arg =~ m{\A -r}xms) {
+    $report = 1;
+  }
+  # error
+  else {
+    die "ERROR:  Unknown arg '$arg'.\n";
   }
 }
 
@@ -45,43 +57,64 @@ my $DIFF =  1;
 
 # collect all .h files and get the prefixes
 my @h = glob("*.h");
-my %h;
-@h{@h} = ();
 my $nh = @h;
-
+# ditto for .d files
+my @d = glob("*.d");
+my $nd = @d;
+my @fils = (@h, @d);
+my $nf = @fils;
+my %f;
+@f{@fils} = ();
+my %stats = ();
+get_status(\%stats, \%f);
 
 # get status of .h files
 print "There are $nh C header files in this directory:\n";
-my $nnew = 0;
-my $nsam = 0;
-my $ndif = 0;
-foreach my $f (@h) {
-  my $s = file_status($f);
-  $h{$f}{status} = $s;
-  if ($s == $NEW) {
-    ++$nnew;
-  }
-  elsif ($s == $SAME) {
-    ++$nsam;
-  }
-  elsif ($s == $DIFF) {
-    ++$ndif;
-  }
-  else {
-    die "ERROR:  Unknown status for '$f'!" if ! defined $s;
-  }
-}
-print "  new:       $nnew\n";
-print "  unchanged: $nsam\n";
-print "  changed:   $ndif\n";
-
-# report status
-foreach my $f (@h) {
-  my $s = $h{$f}{status};
-  $h{$f}{status} = file_status($f);
-}
+print "  new:       $stats{h}{new}\n";
+print "  unchanged: $stats{h}{sam}\n";
+print "  changed:   $stats{h}{dif}\n";
+print "There are $nd D header files in this directory:\n";
+print "  new:       $stats{d}{new}\n";
+print "  unchanged: $stats{d}{sam}\n";
+print "  changed:   $stats{d}{dif}\n";
 
 #### subroutines ####
+sub get_status {
+  my $sref = shift @_; # \%stats
+  my $fref = shift @_; # \%f
+
+  $sref->{h}{new} = 0;
+  $sref->{h}{sam} = 0;
+  $sref->{h}{dif} = 0;
+
+  $sref->{d}{new} = 0;
+  $sref->{d}{sam} = 0;
+  $sref->{d}{dif} = 0;
+
+  foreach my $f (keys %{$fref}) {
+    my $s = file_status($f);
+    my $typ = $f =~ m{\.h \z}xms ? 'h' : 'd';
+    die "ERROR:  Uknown file type for file '$f'!"
+      if ($typ eq 'd' && $f !~ m{\d \z}xms);
+
+    $fref->{$f}{status} = $s;
+    $fref->{$f}{typ}    = $typ;
+
+    if ($s == $NEW) {
+      ++$sref->{$typ}{new};
+    }
+    elsif ($s == $SAME) {
+      ++$sref->{$typ}{sam};
+    }
+    elsif ($s == $DIFF) {
+      ++$sref->{$typ}{dif};
+    }
+    else {
+      die "ERROR:  Unknown status for '$f'!" if ! defined $s;
+    }
+  }
+} # get_status
+
 sub file_status {
   # uses an md5 hash to determine if a file has changed (or is new)
   my $f = shift @_;
