4e60421e67a774306cc02e3be7675eaed40257a5
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2014-05-16 21:01:53 +0000

    Start working on using db_compare3 in the diff3_add function.
    svn:revision:60670
    svn:branch:trunk
    svn:account:starseeker

--- src/gtools/gdiff2.cpp
+++ src/gtools/gdiff2.cpp
@@ -183,7 +183,6 @@ struct diff3_result_container {
     struct bu_attribute_value_set attribute_merged;
 };
 
-#if 0
 static void
 diff3_result_init(struct diff3_result_container *result)
 {
@@ -230,7 +229,6 @@ diff3_result_init(struct diff3_result_container *result)
     BU_AVS_INIT(&result->attribute_merged);
 
 }
-#endif
 
 static void
 diff3_result_free(void *curr_result)
@@ -343,6 +341,7 @@ struct diff3_results {
     struct bu_ptbl *removed_both; /* directory pointers (ancestor dbip) */
     struct bu_ptbl *added_left_only; /* directory pointers (left dbip) */
     struct bu_ptbl *added_right_only; /* directory pointers (right dbip) */
+    struct bu_ptbl *added_both; /* directory pointers (left dbip) */
     struct bu_ptbl *changed; /* containers */
     struct bu_ptbl *conflict; /* containers */
 };
@@ -357,6 +356,7 @@ void diff3_results_init(struct diff3_results *results){
     BU_GET(results->removed_both, struct bu_ptbl);
     BU_GET(results->added_left_only, struct bu_ptbl);
     BU_GET(results->added_right_only, struct bu_ptbl);
+    BU_GET(results->added_both, struct bu_ptbl);
     BU_GET(results->changed, struct bu_ptbl);
     BU_GET(results->conflict, struct bu_ptbl);
 
@@ -366,6 +366,7 @@ void diff3_results_init(struct diff3_results *results){
     BU_PTBL_INIT(results->removed_both);
     BU_PTBL_INIT(results->added_left_only);
     BU_PTBL_INIT(results->added_right_only); 
+    BU_PTBL_INIT(results->added_both); 
     BU_PTBL_INIT(results->changed);
     BU_PTBL_INIT(results->conflict);
 
@@ -381,6 +382,7 @@ void diff3_results_free(struct diff3_results *results)
     bu_ptbl_free(results->removed_both);
     bu_ptbl_free(results->added_left_only);
     bu_ptbl_free(results->added_right_only); 
+    bu_ptbl_free(results->added_both); 
     for (i = 0; i < (int)BU_PTBL_LEN(results->changed); i++) {
 	struct diff3_result_container *result = (struct diff3_result_container *)BU_PTBL_GET(results->changed, i);
 	diff3_result_free((void *)result);
@@ -398,6 +400,7 @@ void diff3_results_free(struct diff3_results *results)
     BU_PUT(results->removed_both, struct bu_ptbl);
     BU_PUT(results->added_left_only, struct bu_ptbl);
     BU_PUT(results->added_right_only, struct bu_ptbl);
+    BU_PUT(results->added_both, struct bu_ptbl);
     BU_PUT(results->changed, struct bu_ptbl);
     BU_PUT(results->conflict, struct bu_ptbl);
 
@@ -529,8 +532,87 @@ diff_changed(const struct db_i *left, const struct db_i *right, const struct dir
 /* Callback functions for db_compare3 */
 /*******************************************************************/
 int
-diff3_added(const struct db_i *UNUSED(left), const struct db_i *UNUSED(ancestor), const struct db_i *UNUSED(right), const struct directory *UNUSED(dp_left), const struct directory *UNUSED(dp_ancestor), const struct directory *UNUSED(dp_right), void *UNUSED(data))
+diff3_added(const struct db_i *left, const struct db_i *UNUSED(ancestor), const struct db_i *right, const struct directory *dp_left, const struct directory *dp_ancestor, const struct directory *dp_right, void *data)
 {
+    /* The cases are:
+     *
+     * added in left only
+     * added in right only
+     * added in left and right, identically
+     * added in left and right, mergably
+     * added in left and right, conflict
+     */
+    struct diff3_results *results;
+    if (!data || !dp_ancestor)
+	return -1;
+
+    results = (struct diff3_results *)data;
+    if (!BU_PTBL_IS_INITIALIZED(results->added_left_only)) BU_PTBL_INIT(results->added_left_only);
+    if (!BU_PTBL_IS_INITIALIZED(results->added_right_only)) BU_PTBL_INIT(results->added_right_only);
+    if (!BU_PTBL_IS_INITIALIZED(results->added_both)) BU_PTBL_INIT(results->added_both);
+    if (!BU_PTBL_IS_INITIALIZED(results->changed)) BU_PTBL_INIT(results->changed);
+    if (!BU_PTBL_IS_INITIALIZED(results->conflict)) BU_PTBL_INIT(results->conflict);
+
+    if (dp_right == RT_DIR_NULL && dp_left == RT_DIR_NULL) return -1;
+    if (dp_left == RT_DIR_NULL) {
+	bu_ptbl_ins(results->added_right_only, (long *)dp_right);
+    }
+    if (dp_right == RT_DIR_NULL) {
+	bu_ptbl_ins(results->added_left_only, (long *)dp_left);
+    }
+
+    if ((dp_right != RT_DIR_NULL) && (dp_left != RT_DIR_NULL)) {
+	struct rt_db_internal intern_left;
+	struct rt_db_internal intern_right;
+	struct bn_tol diff_tol = BN_TOL_INIT_ZERO;
+	diff_tol.dist = results->diff_tolerance;
+
+	int diff3_status = 0;
+	struct diff3_result_container *result3 = (struct diff3_result_container *)bu_calloc(1, sizeof(struct diff3_result_container), "diff3 result struct");
+	diff3_result_init(result3);
+	RT_DB_INTERNAL_INIT(&intern_left);
+	RT_DB_INTERNAL_INIT(&intern_right);
+	/* Get the internal objects */
+	if (rt_db_get_internal(&intern_left, dp_left, left, (fastf_t *)NULL, &rt_uniresource) < 0) {
+	    return -1;
+	}
+	if (rt_db_get_internal(&intern_right, dp_right, right, (fastf_t *)NULL, &rt_uniresource) < 0) {
+	    rt_db_free_internal(&intern_left);
+	    return -1;
+	}
+
+	int diff_param_result = db_compare3(&intern_left, NULL, &intern_right, DB_COMPARE_PARAM, &(result3->param_unchanged),
+		&(result3->param_removed_left_only), &(result3->param_removed_right_only), &(result3->param_removed_both),
+		&(result3->param_added_left_only), &(result3->param_added_right_only), &(result3->param_added_both),
+		&(result3->param_added_conflict_left), &(result3->param_added_conflict_right),
+		&(result3->param_changed_left_only), &(result3->param_changed_right_only),
+		&(result3->param_changed_both), &(result3->param_changed_conflict_ancestor),
+		&(result3->param_changed_conflict_left),&(result3->param_changed_conflict_right),
+		&(result3->param_merged), &diff_tol);
+
+	int diff_attrs_result = db_compare3(&intern_left, NULL, &intern_right, DB_COMPARE_ATTRS, &(result3->attribute_unchanged),
+		&(result3->attribute_removed_left_only), &(result3->attribute_removed_right_only), &(result3->attribute_removed_both),
+		&(result3->attribute_added_left_only), &(result3->attribute_added_right_only), &(result3->attribute_added_both),
+		&(result3->attribute_added_conflict_left), &(result3->attribute_added_conflict_right),
+		&(result3->attribute_changed_left_only), &(result3->attribute_changed_right_only),
+		&(result3->attribute_changed_both), &(result3->attribute_changed_conflict_ancestor),
+		&(result3->attribute_changed_conflict_left),&(result3->attribute_changed_conflict_right),
+		&(result3->attribute_merged), &diff_tol);
+
+	diff3_status = (diff_param_result > diff_attrs_result) ? diff_param_result : diff_attrs_result;
+
+	switch (diff3_status) {
+	    case 0:
+		/* dp_left == dp_right */
+		bu_ptbl_ins(results->added_both, (long *)dp_left);
+		diff3_result_free(result3);
+		break;
+	    default:
+		return -1;
+		break;
+	}
+    }
+
     return 0;
 }
 
@@ -538,6 +620,14 @@ diff3_added(const struct db_i *UNUSED(left), const struct db_i *UNUSED(ancestor)
 int
 diff3_removed(const struct db_i *UNUSED(left), const struct db_i *UNUSED(ancestor), const struct db_i *UNUSED(right), const struct directory *UNUSED(dp_left), const struct directory *UNUSED(dp_ancestor), const struct directory *UNUSED(dp_right), void *UNUSED(data))
 {
+    /* The cases are:
+     *
+     * removed in left only
+     * removed in right only
+     * removed in both
+     *
+     * Cases where a change conflicts with a removal are handled by diff3_changed
+     */
     return 0;
 }
 
@@ -561,6 +651,24 @@ diff3_unchanged(const struct db_i *UNUSED(left), const struct db_i *UNUSED(ances
 int
 diff3_changed(const struct db_i *UNUSED(left), const struct db_i *UNUSED(ancestor), const struct db_i *UNUSED(right), const struct directory *UNUSED(dp_left), const struct directory *UNUSED(dp_ancestor), const struct directory *UNUSED(dp_right), void *UNUSED(data))
 {
+    /* The cases are:
+     *
+     * OK:
+     * changed in left, right unchanged
+     * changed in right, left unchanged
+     * changed in left and right, identically
+     * changed in left and right, mergably
+     *
+     *
+     * Conflicts:
+     * removed in left, changed in right 
+     * removed in right, changed in left
+     * changed in left, changed in right, conflict
+     *
+     *
+     * Cases where additions conflict with each other are handled by diff3_added
+     */
+ 
     return 0;
 }
 
