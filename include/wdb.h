e8a8c6ce3b21fec3e0236734b23e555281cd9b42
Christopher Sean Morrison <brlcad@gmail.com>
2014-05-16 18:01:20 +0000

    clean up the ascii art a little bit, even though if it doesn't entirely seem to match the description of what happens (the parameters and behavior are quite ambiguous).
    svn:revision:60663
    svn:branch:trunk
    svn:account:brlcad

--- include/wdb.h
+++ include/wdb.h
@@ -955,11 +955,9 @@ WDB_EXPORT void mk_freemembers( struct bu_list *headp );
  * modified as follows:
  *
  *        before                          after
- *      |                       -
- *      |                          / \
- *      |                         /   \
- *  original combination tree                /     \
- *                    original combination tree   make_hole_X
+ *          |                              /\
+ *          u                             u  -
+ *   orig_comb_tree          orig_comb_tree  make_hole_X
  *
  * The modified combination is written to the struct rt_wdb. Note that
  * to do dynamic geometry a "wdb_dbopen" would normally be called on
@@ -981,7 +979,6 @@ WDB_EXPORT extern int make_hole(struct rt_wdb *wdbp,
 
 
 /**
- *
  * This routine provides a quick approach to simply adding a hole to
  * existing prepped geometry.  The geometry must already be prepped
  * prior to calling this routine. After calling this routine, the
