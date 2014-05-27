ffbd9912148f8f407e036bfd4309c19fb647c01e
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2014-05-27 21:44:29 +0000

    Add back summary printout.
    svn:revision:60906
    svn:branch:trunk
    svn:account:starseeker

--- src/gtools/gdiff2/summary.c
+++ src/gtools/gdiff2/summary.c
@@ -54,8 +54,9 @@ diff_log_unchgd(const char *attr_name, const char *attr_val, void *data)
 }
 
 void
-diff_result_print(struct diff_result *dr, struct diff_state *state, struct bu_vls *diff_log) {
-    struct bu_vls tmp_str = BU_VLS_INIT_ZERO;
+diff_attrs_print(struct diff_result *dr, struct diff_state *state, struct bu_vls *diff_log) {
+    struct bu_vls tmp_param_str = BU_VLS_INIT_ZERO;
+    struct bu_vls tmp_attr_str = BU_VLS_INIT_ZERO;
     if (state->use_params == 1) {
 	int (*add_func)(const char *, const char *, void *) = NULL;
 	int (*del_func)(const char *, const char *, void *) = NULL;
@@ -65,7 +66,12 @@ diff_result_print(struct diff_result *dr, struct diff_state *state, struct bu_vl
 	if (state->return_removed && dr->param_state & DIFF_REMOVED) del_func = diff_log_del;
 	if (state->return_changed && dr->param_state & DIFF_CHANGED) chgd_func = diff_log_chgd;
 	if (state->return_unchanged && dr->param_state & DIFF_UNCHANGED) unchgd_func = diff_log_unchgd;
-	(void)db_avs_diff(dr->left_param_avs, dr->right_param_avs, state->diff_tol, add_func, del_func, chgd_func, unchgd_func, (void *)&tmp_str);
+	(void)db_avs_diff(dr->left_param_avs, dr->right_param_avs, state->diff_tol, add_func, del_func, chgd_func, unchgd_func, (void *)&tmp_param_str);
+	if (strlen(bu_vls_addr(&tmp_param_str))) {
+	    bu_vls_printf(diff_log, "Object %s parameters:\n", dr->obj_name);
+	    bu_vls_printf(diff_log, "%s\n", bu_vls_addr(&tmp_param_str));
+	}
+
     }
     if (state->use_attrs == 1) {
 	int (*add_func)(const char *, const char *, void *) = NULL;
@@ -76,42 +82,147 @@ diff_result_print(struct diff_result *dr, struct diff_state *state, struct bu_vl
 	if (state->return_removed && dr->attr_state & DIFF_REMOVED) del_func = diff_log_del;
 	if (state->return_changed && dr->attr_state & DIFF_CHANGED) chgd_func = diff_log_chgd;
 	if (state->return_unchanged && dr->attr_state & DIFF_UNCHANGED) unchgd_func = diff_log_unchgd;
-	(void)db_avs_diff(dr->left_attr_avs, dr->right_attr_avs, state->diff_tol, add_func, del_func, chgd_func, unchgd_func, (void *)&tmp_str);
+	(void)db_avs_diff(dr->left_attr_avs, dr->right_attr_avs, state->diff_tol, add_func, del_func, chgd_func, unchgd_func, (void *)&tmp_attr_str);
+	if (strlen(bu_vls_addr(&tmp_attr_str))) {
+	    bu_vls_printf(diff_log, "Object %s attributes:\n", dr->obj_name);
+	    bu_vls_printf(diff_log, "%s\n", bu_vls_addr(&tmp_attr_str));
+	}
     }
-    if (strlen(bu_vls_addr(&tmp_str)) > 0) {
-	bu_vls_printf(diff_log, "Object %s:\n%s", dr->obj_name, bu_vls_addr(&tmp_str));
-    }
-    bu_vls_free(&tmp_str);
+
+    bu_vls_free(&tmp_param_str);
+    bu_vls_free(&tmp_attr_str);
 }
 
 void
 diff_summarize(struct bu_vls *diff_log, const struct bu_ptbl *results, struct diff_state *state)
 {
     int i = 0;
-    int reported = 0;
+    struct bu_vls added_list = BU_VLS_INIT_ZERO;
+    struct bu_vls removed_list = BU_VLS_INIT_ZERO;
+    struct bu_vls changed_list = BU_VLS_INIT_ZERO;
+    struct bu_vls unchanged_list = BU_VLS_INIT_ZERO;
+    struct bu_vls added_tmp = BU_VLS_INIT_ZERO;
+    struct bu_vls removed_tmp = BU_VLS_INIT_ZERO;
+    struct bu_vls changed_tmp = BU_VLS_INIT_ZERO;
+    struct bu_vls unchanged_tmp = BU_VLS_INIT_ZERO;
+
 
     if (state->verbosity < 1 || !results)
 	return;
 
+    if (state->verbosity == 1) {
+	if (state->return_added == 1) bu_vls_sprintf(&added_list, "Added:\n");
+	if (state->return_removed == 1) bu_vls_sprintf(&removed_list, "Removed:\n");
+	if (state->return_changed == 1) bu_vls_sprintf(&changed_list, "Changed:\n");
+	if (state->return_unchanged == 1) bu_vls_sprintf(&unchanged_list, "Unchanged:\n");
+    }
+
     for (i = 0; i < (int)BU_PTBL_LEN(results); i++) {
 	struct diff_result *dr = (struct diff_result *)BU_PTBL_GET(results, i);
-	reported = 0;
-	if (dr->param_state == DIFF_ADDED) {
-	    if (state->return_added == 1) bu_log("+ %s\n", dr->obj_name);
-	    reported = 1;
-	}
-	if (!reported && dr->param_state == DIFF_REMOVED) {
-	    if (state->return_removed == 1) bu_log("- %s\n", dr->obj_name);
-	    reported = 1;
-	}
-	if (!reported && dr->param_state == DIFF_UNCHANGED && dr->attr_state == DIFF_UNCHANGED) {
-	    if (state->return_unchanged == 1) bu_log("  %s(%d,%d)\n", dr->obj_name, dr->param_state, dr->attr_state);
-	    reported = 1;
-	}
+	    if (dr->param_state == DIFF_ADDED) {
+		switch (state->verbosity) {
+		    case 1:
+			if (state->return_added == 1) {
+			    if (strlen(bu_vls_addr(&added_tmp)) + strlen(dr->obj_name) + 2 > 80) {
+				bu_vls_printf(&added_list, "%s\n", bu_vls_addr(&added_tmp));
+				bu_vls_sprintf(&added_tmp, "%s", dr->obj_name);
+			    } else {
+				if (strlen(bu_vls_addr(&added_tmp)) > 0) bu_vls_printf(&added_tmp, ", ");
+				bu_vls_printf(&added_tmp, "%s", dr->obj_name);
+			    }
+			}
+			break;
+		    case 2:
+			if (state->return_added == 1) bu_vls_printf(diff_log, "A %s\n", dr->obj_name);
+			break;
+		    case 3:
+			diff_attrs_print(dr, state, diff_log);
+			break;
+		    default:
+			break;
+		}
+	    }
+	    if (dr->param_state == DIFF_REMOVED) {
+		switch (state->verbosity) {
+		    case 1:
+			if (state->return_removed == 1) {
+			    if (strlen(bu_vls_addr(&removed_tmp)) + strlen(dr->obj_name) + 2 > 80) {
+				bu_vls_printf(&removed_list, "%s\n", bu_vls_addr(&removed_tmp));
+				bu_vls_sprintf(&removed_tmp, "%s", dr->obj_name);
+			    } else {
+				if (strlen(bu_vls_addr(&removed_tmp)) > 0) bu_vls_printf(&removed_tmp, ", ");
+				bu_vls_printf(&removed_tmp, "%s", dr->obj_name);
+			    }
+			}
+			break;
+		    case 2:
+			if (state->return_removed == 1) bu_vls_printf(diff_log, "D %s\n", dr->obj_name);
+			break;
+		    case 3:
+			diff_attrs_print(dr, state, diff_log);
+			break;
+		    default:
+			break;
+		}
+	    }
+	    if (dr->param_state == DIFF_UNCHANGED && dr->attr_state == DIFF_UNCHANGED) {
+		switch (state->verbosity) {
+		    case 1:
+			if (state->return_unchanged == 1) {
+			    if (strlen(bu_vls_addr(&unchanged_tmp)) + strlen(dr->obj_name) + 2 > 80) {
+				bu_vls_printf(&unchanged_list, "%s\n", bu_vls_addr(&unchanged_tmp));
+				bu_vls_sprintf(&unchanged_tmp, "%s", dr->obj_name);
+			    } else {
+				if (strlen(bu_vls_addr(&unchanged_tmp)) > 0) bu_vls_printf(&unchanged_tmp, ", ");
+				bu_vls_printf(&unchanged_tmp, "%s", dr->obj_name);
+			    }
+			}
+			break;
+		    case 2:
+			if (state->return_unchanged == 1) bu_vls_printf(diff_log, "  %s\n", dr->obj_name);
+			break;
+		    case 3:
+			diff_attrs_print(dr, state, diff_log);
+			break;
+		    default:
+			break;
+		}
+	    }
 
-	if (!reported && state->return_changed == 1) {
-	    diff_result_print(dr, state, diff_log);
-	}
+	    if ((dr->param_state != DIFF_UNCHANGED || dr->attr_state != DIFF_UNCHANGED) && dr->param_state != DIFF_ADDED && dr->param_state != DIFF_REMOVED) {
+		switch (state->verbosity) {
+		    case 1:
+			if (state->return_changed == 1) {
+			    if (strlen(bu_vls_addr(&changed_tmp)) + strlen(dr->obj_name) + 2 > 80) {
+				bu_vls_printf(&changed_list, "%s\n", bu_vls_addr(&changed_tmp));
+				bu_vls_sprintf(&changed_tmp, "%s", dr->obj_name);
+			    } else {
+				if (strlen(bu_vls_addr(&changed_tmp)) > 0) bu_vls_printf(&changed_tmp, ", ");
+				bu_vls_printf(&changed_tmp, "%s", dr->obj_name);
+			    }
+			}
+			break;
+		    case 2:
+			if (state->return_changed == 1) bu_vls_printf(diff_log, "M %s\n", dr->obj_name);
+			break;
+		    case 3:
+			diff_attrs_print(dr, state, diff_log);
+			break;
+		    default:
+			break;
+		}
+	    }
+    }
+
+    if (state->verbosity == 1) {
+	bu_vls_printf(&added_list, "%s\n", bu_vls_addr(&added_tmp));
+	bu_vls_printf(&removed_list, "%s\n", bu_vls_addr(&removed_tmp));
+	bu_vls_printf(&changed_list, "%s\n", bu_vls_addr(&changed_tmp));
+	bu_vls_printf(&unchanged_list, "%s\n", bu_vls_addr(&unchanged_tmp));
+	bu_vls_printf(diff_log, "%s\n", bu_vls_addr(&added_list));
+	bu_vls_printf(diff_log, "%s\n", bu_vls_addr(&removed_list));
+	bu_vls_printf(diff_log, "%s\n", bu_vls_addr(&changed_list));
+	bu_vls_printf(diff_log, "%s\n", bu_vls_addr(&unchanged_list));
     }
     bu_vls_printf(diff_log, "\n");
 }
