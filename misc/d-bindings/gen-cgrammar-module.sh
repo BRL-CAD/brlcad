c7c27bf69b8412167a2b930353dcf14d62e23031
Thomas Browder <tom.browder@gmail.com>
2014-05-29 20:15:18 +0000

    add the second grammar version to the auto-gen script
    svn:revision:60966
    svn:branch:d-binding
    svn:account:tbrowder2

--- misc/d-bindings/gen-cgrammar-module.sh
+++ misc/d-bindings/gen-cgrammar-module.sh
@@ -1,16 +1,21 @@
 #/bin/bash
 
 PM=CGrammar
+PM2=CGrammar2
 
 if [ -z "$1" ] ; then
   echo "Usage: $0 go"
   echo
-  echo "Generates the Perl module for the C grammar: '$PM.pm'."
+  echo "Generates the Perl modules for the C grammars:"
+  echo "  $PM.pm"
+  echo "  $PM2.pm"
   exit
 fi
 
 cmd="perl -MParse::RecDescent - c-grammar.txt $PM"
+cmd2="perl -MParse::RecDescent - c-grammar2.txt $PM2"
 
 `$cmd`
+`$cmd2`
 
-echo "See generated file '$PM.pm'."
+echo "See generated files '$PM.pm' and '$PM2.pm'."
