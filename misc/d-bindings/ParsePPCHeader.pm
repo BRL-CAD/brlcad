a14aff8c09557035be3e09818c4ad92da6d08b91
Thomas Browder <tom.browder@gmail.com>
2014-05-29 20:17:08 +0000

    stuff old code away; rename functions for clarity
    svn:revision:60967
    svn:branch:d-binding
    svn:account:tbrowder2

--- misc/d-bindings/ParsePPCHeader.pm
+++ misc/d-bindings/ParsePPCHeader.pm
@@ -7,6 +7,9 @@ use warnings;
 
 use CGrammar; # <== an auto-generated file
 
+# for debugging:
+my $COMP = 0;
+
 sub parse_cfile {
 
   my $ifil = shift @_;
@@ -48,22 +51,75 @@ sub parse_cfile {
   }
 
   use Data::Dumper;
-  $Data::Dumper::Terse  = 1;         # don't output names where feasible (doesn't work for my tree)
-  $Data::Dumper::Indent = 1;         # mild pretty print
-  $Data::Dumper::Purity = 1;
+  $Data::Dumper::Terse   = 1;         # don't output names where feasible (doesn't work for my tree)
+  $Data::Dumper::Indent  = 1;         # mild pretty print
+  $Data::Dumper::Purity  = 1;
+  $Data::Dumper::Deparse = 1;
 
   if (1) {
     print Dumper $ptree;
   }
   elsif (1) {
-    inspect_syntax_tree($ptree);
+    inspect_PRD_syntax_pure_autotree($ptree);
   }
 
   printf "DEBUG exit, file '%s', line %d\n", __FILE__, __LINE__; exit;
 
 } # parse_cfile
 
-sub inspect_syntax_tree {
+sub inspect_PRD_syntax_pure_autotree {
+  my $treeref = shift @_;
+  print "DEBUG:  syntax tree:\n";
+
+=pod
+
+  my $level = 0;
+  my $s = get_spaces($level);
+  my $r = ref $uref;
+  if (!$r) {
+    print_scalar($uref, $level);
+  }
+  elsif ($r eq 'ARRAY') {
+    print_array($uref, ++$level);
+  }
+  elsif ($r eq 'HASH') {
+    print_hash($uref, ++$level);
+  }
+  elsif ($COMP && $uref =~ /=/) {
+    print_scalar($uref, $level);
+  }
+  else {
+    warn_ref($r, $uref, $s, __FILE__, __LINE__);
+  }
+
+=cut
+
+} # inspect_PRD_syntax_pure_autotree
+
+sub get_spaces {
+  my $level = shift @_;
+  my $ns = $level * 3;
+  my $s = sprintf "%-*.*s", $ns, $ns, ' ';
+  return $s;
+} # get_spaces
+
+# mandatory true return for a Perl module
+1;
+
+
+
+
+
+
+
+
+########## EOF ###########
+__END__
+# currently unneeded routines
+
+=pod
+
+sub inspect_syntax_tree2 {
   my $uref = shift @_;
 
   print "DEBUG:  syntax tree:\n";
@@ -80,17 +136,63 @@ sub inspect_syntax_tree {
   elsif ($r eq 'HASH') {
     print_hash($uref, ++$level);
   }
-  else {
-    print "    WARNING:   unhandled ref type '$r'\n";
-    print "    scalar value: '$uref'\n";
+  elsif ($COMP && $uref =~ /=/) {
+    print_scalar($uref, $level);
   }
-} # handle_ref
+  else {
+    warn_ref($r, $uref, $s, __FILE__, __LINE__);
+  }
+} # inspect_syntax_tree2
+
+sub warn_ref {
+  my ($r, $uref, $s, $fil, $linenum) = @_;
+  print "${s}WARNING:   unhandled ref type '$r'\n";
+  print "${s}scalar value: '$uref'\n";
+  print "${s}file '$fil', line $linenum\n";
+} # warn_ref
 
 sub print_scalar {
-  my $val   = shift @_;
+  my $val = shift @_;
   my $level = shift @_; # use for number of leading spaces
+
+=pod
+
+  # note that a scalar may be a disguised ref, e.g.,
+  #   translation_unit=HASH(0x1347858)
+  my $val2 = undef;
+  my $idx = index $val, '=';
+  if ($idx >= 0) {
+    $val2 = substr $val, $idx+1;
+    $val = substr $val, 0, $idx+1; # save the '=' for output
+  }
+
+=cut
+
   my $s = get_spaces($level);
   print "${s}scalar value: $val\n";
+
+=pod
+
+  if (defined $val2) {
+    my $r = ref $val2;
+    if (!$r) {
+      print_scalar($val2, $level);
+    }
+    elsif ($r eq 'ARRAY') {
+      print_array($val2, ++$level);
+    }
+    elsif ($r eq 'HASH') {
+      print_hash($val2, ++$level);
+    }
+    elsif ($COMP && $val2 =~ /=/) {
+      print_scalar($val2, $level);
+    }
+    else {
+      warn_ref($r, $val2, $s, __FILE__, __LINE__);
+    }
+  }
+
+=cut
 
 } # print_scalar
 
@@ -109,9 +211,11 @@ sub print_array {
     elsif ($r eq 'HASH') {
       print_hash($val, ++$level);
     }
+    elsif ($COMP && $val =~ /=/) {
+      print_scalar($val, $level);
+    }
     else {
-      print "${s}WARNING:   unhandled ref type '$r'\n";
-      print "${s}scalar value: '$val'\n";
+      warn_ref($r, $val, $s, __FILE__, __LINE__);
     }
   }
 } # print_array
@@ -132,19 +236,13 @@ sub print_hash {
     elsif ($r eq 'HASH') {
       print_hash($val, ++$level);
     }
+    elsif ($COMP && $val =~ /=/) {
+      print_scalar($val, $level);
+    }
     else {
-      print "${s}WARNING:   unhandled ref type '$r'\n";
-      print "${s}scalar value: '$val'\n";
+      warn_ref($r, $val, $s, __FILE__, __LINE__);
     }
   }
 } # print_array
 
-sub get_spaces {
-  my $level = shift @_;
-  my $ns = $level * 3;
-  my $s = sprintf "%-*.*s", $ns, $ns, ' ';
-  return $s;
-} # get_spaces
-
-# mandatory true return for a Perl module
-1;
+=cut
