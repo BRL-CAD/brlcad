df9dd978940d07d5c431fed09629f8d151e738cc
Thomas Browder <tom.browder@gmail.com>
2014-05-29 20:12:21 +0000

    remove old, unused code
    svn:revision:60964
    svn:branch:d-binding
    svn:account:tbrowder2

--- misc/d-bindings/convert-h2d.pl
+++ misc/d-bindings/convert-h2d.pl
@@ -144,7 +144,6 @@ die "ERROR:  No mode selected.\n"
 
 if ($D::devel) {
   @ifils = ("${BP::IDIR}/bu.h");
-  #@ifils = ("./test-hdr.h");
 }
 
 # collect all .h and .d files; note that some .h files are obsolete
