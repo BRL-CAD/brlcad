fa3866c33ba0b77f55c4861bb0d1547e74bf99ee
Thomas Browder <tom.browder@gmail.com>
2014-05-31 19:23:06 +0000

    add more options for development
    svn:revision:61040
    svn:branch:d-binding
    svn:account:tbrowder2

--- misc/d-bindings/G.pm
+++ misc/d-bindings/G.pm
@@ -12,15 +12,16 @@ Readonly our $Hsuf => '.h';
 Readonly our $Dsuf => '.d';
 
 #==========================================================================
-# option vars for export
+# global option vars
 our $force      = 0;
 our $verbose    = 0;
 our $debug      = 0;
 our $clean      = 0;
 our $devel      = 0;
 our $chunkparse = 0; # 1 - parse input in chunks
+our $quitundef  = 0; # 1 - stop at first undef chunk
+our $maxchunks  = 0; # >0 - stop after N successful chunks
 
-# global vars
 Readonly our $NEW  => -1;
 Readonly our $SAME =>  0;
 Readonly our $DIFF =>  1;
