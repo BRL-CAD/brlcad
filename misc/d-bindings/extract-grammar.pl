ff381330c38d0a51e43e38224fea77144695af1c
Thomas Browder <tom.browder@gmail.com>
2014-06-02 12:21:46 +0000

    reading grammar mostly ok but one issue remains (plus minor formatting differences): WHILE is being missed
    svn:revision:61060
    svn:branch:d-binding
    svn:account:tbrowder2

--- misc/d-bindings/extract-grammar.pl
+++ misc/d-bindings/extract-grammar.pl
@@ -56,125 +56,163 @@ die "FATAL:  Unable to find grammar file '$ifil'.\n"
 open my $fp, '<', $ifil
   or die "$ifil: $!";
 
-my %prods = (); # hash of production names and their children
+my %prod  = (); # hash of production names and their children
+my @prods = (); # retain order as read
 
-# process each production as we encounter it
-my $curr_prod     = ''; # name
-my @curr_children = (); # all children
+extract_grammar(\%prod, \@prods);
 
-my $linenum = 0;
-while (defined(my $line = <$fp>)) {
-  ++$linenum;
-
-  $line = strip_comment($line);
-  if ($line !~ /\S+/) {
-    print "DEBUG:  skipping empty line '$line'.\n"
-      if (0 && $debug);
-    next;
-  }
-  next if $line =~ /<autotree>/;
-  chomp $line;
-
-  if (0 && $debug) {
-    print "DEBUG: non-empty line '$line'\n";
-  }
-
-  # trim
-  $line =~ s{\A \s+}{}x;
-  $line =~ s{\s+ \z}{}x;
-
-  # add some spaces on the line for more reliable tokenizing
-  # careful, ignore chars between '/' and '/', etc.
-  my $t1 = $line;
-  if ($line ne '/(typedef|extern|static|auto|register)(?![a-zA-Z0-9_])/'
-      && $line ne '/(void|char|short|int|long|float|double|signed|unsigned)(?![a-zA-Z0-9_])/'
-      && $line ne '/(struct|union)(?![a-zA-Z0-9_])/'
-      && $line ne '/(const|volatile)(?![a-zA-Z0-9_])/'
-      && $line ne '/(auto|break|case|char|const|continue|default|do|double|enum|extern|float|for|goto|if|int|long|register|return|short|signed|sizeof|static|struct|switch|typedef|union|unsigned|void|volatile|while)(?![a-zA-Z0-9_])/'
-      && $line ne 'CONSTANT:       /[+-]?(?=\d|\.\d)\d*(\.\d*)?([Ee]([+-]?\d+))?/'
-     ) {
-    $line =~ s{([a-zA-Z]) (\|)}{$1 $2}xg;
-    $line =~ s{(\|) (a-zA-Z])}{$1 $2}xg;
-    my $t2 = $line;
-    if (0 && $debug && $t1 ne $t2) {
-      print "DEBUG: original line with pipes:\n";
-      print "  '$t1'\n";
-      print "DEBUG: line with spaced pipes:\n";
-      print "  '$t2'\n";
+if ($debug) {
+  print "# C grammar:\n";
+  foreach my $p (@prods) {
+    my $caps = ($p =~ m{\A [A-Z_]+ \z}x) ? 1 : 0;
+    my @c = @{$prod{$p}};
+    my $nc = @c;
+    print "$p:";
+    print "\n" if !$caps;
+    for (my $i = 0; $i < $nc; ++$i) {
+      my $c = $c[$i];
+      print "\t";
+      print "| " if $i;
+      print "$c";
+      print "\n";
     }
+    print "\n" if !$caps;
   }
+  #print Dumper(\%prod);
+}
 
-  # check for a production line
-  my $have_colon = ($line =~ m{\A [a-zA-Z_]+\s*\:}x) ? 1 : 0;
+#### subroutines ####
+sub extract_grammar {
+  my $prod_href  = shift @_; # \%prod
+  my $prods_aref = shift @_; # \@prods
 
-  # check for malformed lines (or my misunderstanding)
-  if ($line =~ m{\A [a-zA-Z_]+\s*\:\S}x) {
-    die "bad colon line '$line'";
-  }
+  # certain lines can't be processed
+  my @pipes_ok
+    = (
+       '/(typedef|extern|static|auto|register)(?![a-zA-Z0-9_])/',
+       '/(void|char|short|int|long|float|double|signed|unsigned)(?![a-zA-Z0-9_])/',
+       '/(struct|union)(?![a-zA-Z0-9_])/',
+       '/(const|volatile)(?![a-zA-Z0-9_])/',
+       '/(auto|break|case|char|const|continue|default|do|double|enum|extern|float|for|goto|if|int|long|register|return|short|signed|sizeof|static|struct|switch|typedef|union|unsigned|void|volatile|while)(?![a-zA-Z0-9_])/',
+       'CONSTANT:       /[+-]?(?=\d|\.\d)\d*(\.\d*)?([Ee]([+-]?\d+))?/',
+       '( type_specifier | type_qualifier )(s)',
+       '( storage_class_specifier | type_specifier | type_qualifier )(s)',
+      );
+  my %pipes_ok;
+  @pipes_ok{@pipes_ok} = ();
 
-  if ($have_colon) {
+  # process each production as we encounter it
+  my $curr_prod     = ''; # name
+  my @curr_children = (); # all children
+
+  my $linenum = 0;
+  while (defined(my $line = <$fp>)) {
+    ++$linenum;
+
+    $line = strip_comment($line);
+    if ($line !~ /\S+/) {
+      print "DEBUG:  skipping empty line '$line'.\n"
+	if (0 && $debug);
+      next;
+    }
+    next if $line =~ /<autotree>/;
+    chomp $line;
+
     if (0 && $debug) {
-      my $s = $line;
-      print "DEBUG: original line with colon:\n";
-      print "  '$s'\n";
+      print "DEBUG: non-empty line '$line'\n";
     }
-    # affect only colons at end (or beginning?) of identifiers
-    $line =~ s{\A ([a-zA-Z_]+)\:}{$1 \:}x;
-    if (0 && $debug) {
-      my $s = $line;
-      print "       line with colon separated:\n";
-      print "  '$s'\n";
-    }
-  }
 
-  my @d = split(' ', $line);
+    # trim
+    $line =~ s{\A \s+}{}x;
+    $line =~ s{\s+ \z}{}x;
 
-  # is this a production definition line"
-  my $key = shift @d;
-  my $newprod = ($key =~ s{\: \z}{}x) ? 1 : 0;
-  my $key2 = $newprod ? '' : shift @d;
-  if (defined $key2) {
-    if ($key2 eq ':') {
-      $newprod = 1;
-      $key2 = '';
-    }
-    else {
-      unshift @d, $key2;
-    }
-  }
+    # add some spaces on the line for more reliable tokenizing
+    # careful, ignore chars between '/' and '/', etc.
+    # add space around some pipes
+    my $t1 = $line;
+    if (!exists $pipes_ok{$line}) {
+      $line =~ s{([a-zA-Z]) (\|)}{$1 $2}xg;
+      $line =~ s{(\|) (a-zA-Z])}{ $1 $2}xg;
+      # some lines  have been trimmed, remove leading pipes
+      $line =~ s{\A (\|)}{}x;
 
-  if ($newprod) {
-    # beginning of a production rule with non-empty @d as subrules
-    if ($curr_prod) {
-      # we may need to save the current one
-      die "FATAL: dup production rule '$curr_prod'"
-	if exists $prods{$curr_prod};
-      if (@curr_children) {
-	$prods{$curr_prod} = [@curr_children];
-	@curr_children = ();
+      my $t2 = $line;
+      if (0 && $debug && $t1 ne $t2) {
+	print "DEBUG: original line with pipes:\n";
+	print "  '$t1'\n";
+	print "DEBUG: line with spaced pipes:\n";
+	print "  '$t2'\n";
+      }
+    }
+
+    # check for a production line
+    my $have_colon = ($line =~ m{\A [a-zA-Z_]+\s*\:}x) ? 1 : 0;
+
+    # check for malformed lines (or my misunderstanding)
+    if ($line =~ m{\A [a-zA-Z_]+\s*\:\S}x) {
+      die "bad colon line '$line'";
+    }
+
+    if ($have_colon) {
+      if (0 && $debug) {
+	my $s = $line;
+	print "DEBUG: original line with colon:\n";
+	print "  '$s'\n";
+      }
+      # affect only colons at end (or beginning?) of identifiers
+      $line =~ s{\A ([a-zA-Z_]+)\:}{$1 \:}x;
+      if (0 && $debug) {
+	my $s = $line;
+	print "       line with colon separated:\n";
+	print "  '$s'\n";
+      }
+    }
+
+    my @d = split(' ', $line);
+
+    # is this a production definition line"
+    my $key = shift @d;
+    my $newprod = ($key =~ s{\: \z}{}x) ? 1 : 0;
+    my $key2 = $newprod ? '' : shift @d;
+    if (defined $key2) {
+      if ($key2 eq ':') {
+	$newprod = 1;
+	$key2 = '';
       }
       else {
-	$prods{$curr_prod} = [];
-	print "WARNING:  production rule '$curr_prod' has no children.\n";
+	unshift @d, $key2;
       }
     }
-    $curr_prod = $key;
-    # rejoin the line and reprocess it
-    my $pline = join(' ', @d);
-    my @t = split('\|', $pline);
-    foreach my $t (@t) {
-      # trim
-      $t =~ s{\A \s*}{}x;
-      $t =~ s{\s* \z}{}x;
-      push @curr_children, $t
-	if $t;
-    }
-  }
-  else {
-    if ($curr_prod) {
+
+    if ($newprod) {
+      # beginning of a production rule with non-empty @d as subrules
+      if ($curr_prod) {
+	# we may need to save the current one
+	die "FATAL: dup production rule '$curr_prod'"
+	  if exists $prod{$curr_prod};
+	if (@curr_children) {
+	  # special case for lines beginning with '(' (a hack, could be improved for generality)
+	  my $c = substr $curr_children[0], 0, 1;
+	  if ($c eq '(') {
+	    $prod_href->{$curr_prod} = [join(' ', @curr_children)];
+	  }
+	  else {
+	    $prod_href->{$curr_prod} = [@curr_children];
+	  }
+	  @curr_children = ();
+	}
+	else {
+	  $prod_href->{$curr_prod} = [];
+	  print "WARNING:  production rule '$curr_prod' has no children.\n";
+	}
+	# save the prod order
+	push @{$prods_aref}, $curr_prod;
+      }
+      $curr_prod = $key;
       # rejoin the line and reprocess it
-      my $pline = join(' ', ($key, @d));
-      my @t = split('\|', $pline);
+      my $pline = join(' ', @d);
+      # split on space-separated pipes
+      my @t = split(' \| ', $pline);
       foreach my $t (@t) {
 	# trim
 	$t =~ s{\A \s*}{}x;
@@ -184,32 +222,48 @@ while (defined(my $line = <$fp>)) {
       }
     }
     else {
-      print STDERR "ERROR: No \$curr_prod for line $linenum:\n";
-      print STDERR "  line: '$line'\n";
-      die "FATAL:  No \$curr_prod.";
+      if ($curr_prod) {
+	# rejoin the line and reprocess it
+	my $pline = join(' ', ($key, @d));
+	# split on space-separated pipes
+	if (!exists $pipes_ok{$pline}) {
+	  my @t = split(' \| ', $pline);
+	  foreach my $t (@t) {
+	    # trim
+	    $t =~ s{\A \s*}{}x;
+	    $t =~ s{\s* \z}{}x;
+	    push @curr_children, $t
+	      if $t;
+	  }
+	}
+	else {
+	  push @curr_children, $pline
+	      if $pline;
+	}
+      }
+      else {
+	print STDERR "ERROR: No \$curr_prod for line $linenum:\n";
+	print STDERR "  line: '$line'\n";
+	die "FATAL:  No \$curr_prod.";
+      }
     }
   }
-}
 
-# any remaining?
-# we may need to save the current one
-die "FATAL: dup production rule '$curr_prod'"
-  if exists $prods{$curr_prod};
-if (@curr_children) {
-  $prods{$curr_prod} = [@curr_children];
-  @curr_children = ();
-}
-else {
-  $prods{$curr_prod} = [];
-  print "WARNING:  production rule '$curr_prod' has no children.\n";
-}
+  # any remaining?
+  # we may need to save the current one
+  die "FATAL: dup production rule '$curr_prod'"
+    if exists $prod{$curr_prod};
+  if (@curr_children) {
+    $prod_href->{$curr_prod} = [@curr_children];
+    @curr_children = ();
+  }
+  else {
+    $prod_href->{$curr_prod} = [];
+    print "WARNING:  production rule '$curr_prod' has no children.\n";
+  }
 
-if ($debug) {
-  print "DEBUG: dumping results:\n";
-  print Dumper(\%prods);
-}
+} # extract_grammar
 
-#### subroutines ####
 sub longhelp {
   print <<"HERE";
 $usage
