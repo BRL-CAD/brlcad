2c8303138eafe516ac904e98e8b698c1dd62dcc7
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2014-06-03 20:11:05 +0000

    time is a problem as a variable name - change it.
    svn:revision:61089
    svn:branch:trunk
    svn:account:starseeker

--- src/libbu/tests/bu_date-time.c
+++ src/libbu/tests/bu_date-time.c
@@ -25,69 +25,69 @@ int
 main(int argc, char **argv)
 {
     struct bu_vls result = BU_VLS_INIT_ZERO;
-    int64_t time;
+    int64_t curr_time;
     int function_num;
     if (argc != 2)
 	bu_exit(1, "ERROR: wrong number of parameters");
     sscanf(argv[1], "%d", &function_num);
     switch (function_num) {
 	case 1:
-	    time = 1087449261LL;
-	    bu_utctime(&result, time);
+	    curr_time = 1087449261LL;
+	    bu_utctime(&result, curr_time);
 	    if (!BU_STR_EQUAL(result.vls_str, "2004-06-17T05:14:21Z"))
 		return 1;
 	    break;
 	case 2:
-	    time = 631152000LL;
-	    bu_utctime(&result, time);
+	    curr_time = 631152000LL;
+	    bu_utctime(&result, curr_time);
 	    if (!BU_STR_EQUAL(result.vls_str, "1990-01-01T00:00:00Z"))
 		return 1;
 	    break;
 	case 3:
-	    time = 936860949LL;
-	    bu_utctime(&result, time);
+	    curr_time = 936860949LL;
+	    bu_utctime(&result, curr_time);
 	    if (!BU_STR_EQUAL(result.vls_str, "1999-09-09T07:09:09Z"))
 		return 1;
 	    break;
 	case 4:
-	    time = 1388696601LL;
-	    bu_utctime(&result, time);
+	    curr_time = 1388696601LL;
+	    bu_utctime(&result, curr_time);
 	    if (!BU_STR_EQUAL(result.vls_str, "2014-01-02T21:03:21Z"))
 		return 1;
 	    break;
 	case 5:
-	    time = 0LL;
-	    bu_utctime(&result, time);
+	    curr_time = 0LL;
+	    bu_utctime(&result, curr_time);
 	    if (!BU_STR_EQUAL(result.vls_str, "1970-01-01T00:00:00Z"))
 		return 1;
 	    break;
 	case 6:
-	    time = 1LL;
-	    bu_utctime(&result, time);
+	    curr_time = 1LL;
+	    bu_utctime(&result, curr_time);
 	    if (!BU_STR_EQUAL(result.vls_str, "1970-01-01T00:00:01Z"))
 		return 1;
 	    break;
 	case 7:
-	    time = 1431482805LL;
-	    bu_utctime(&result, time);
+	    curr_time = 1431482805LL;
+	    bu_utctime(&result, curr_time);
 	    if (!BU_STR_EQUAL(result.vls_str, "2015-05-13T02:06:45Z"))
 		return 1;
 	    break;
 	case 8:
-	    time = 2147483647LL;
-	    bu_utctime(&result, time);
+	    curr_time = 2147483647LL;
+	    bu_utctime(&result, curr_time);
 	    if (!BU_STR_EQUAL(result.vls_str, "2038-01-19T03:14:07Z"))
 		return 1;
 	    break;
 	case 9:
-	    time = 2147483649LL;
-	    bu_utctime(&result, time);
+	    curr_time = 2147483649LL;
+	    bu_utctime(&result, curr_time);
 	    if (!BU_STR_EQUAL(result.vls_str, "2038-01-19T03:14:09Z"))
 		return 1;
 	    break;
 	case 10:
-	    time = 3147483649LL;
-	    bu_utctime(&result, time);
+	    curr_time = 3147483649LL;
+	    bu_utctime(&result, curr_time);
 	    if (!BU_STR_EQUAL(result.vls_str, "2069-09-27T05:00:49Z"))
 		return 1;
 	    break;
