69478cf3fbf7ba773cd5a75892d2615cf90dbc9d
Zhao Anqing <zaqcloud@hotmail.com>
2014-06-06 01:58:34 +0000

    change routines about shells to fit new nmg structure.
    svn:revision:61153
    svn:branch:nmgreorg
    svn:account:zhaoanqing

--- src/libged/shells.c
+++ src/libged/shells.c
@@ -77,56 +77,44 @@ ged_shells(struct ged *gedp, int argc, const char *argv[])
 	return GED_ERROR;
     }
 
-    m = (struct model *)old_intern.idb_ptr;
-    NMG_CK_MODEL(m);
+    s = (struct shell *)old_intern.idb_ptr;
+    NMG_CK_SHELL(s);
 
-    for (BU_LIST_FOR(r, nmgregion, &m->r_hd)) {
-	for (BU_LIST_FOR(s, shell, &r->s_hd)) {
-	    s_tmp = nmg_dup_shell(s, &trans_tbl, &gedp->ged_wdbp->wdb_tol);
-	    bu_free((genptr_t)trans_tbl, "trans_tbl");
+    s_tmp = nmg_dup_shell(s, &trans_tbl, &gedp->ged_wdbp->wdb_tol);
+    bu_free((genptr_t)trans_tbl, "trans_tbl");
 
-	    m_tmp = nmg_mmr();
-	    r_tmp = BU_LIST_FIRST(nmgregion, &m_tmp->r_hd);
-
-	    BU_LIST_DEQUEUE(&s_tmp->l);
-	    BU_LIST_APPEND(&r_tmp->s_hd, &s_tmp->l);
-	    s_tmp->r_p = r_tmp;
-	    nmg_m_reindex(m_tmp, 0);
-	    nmg_m_reindex(m, 0);
-
-	    bu_vls_printf(&shell_name, "shell.%d", shell_count);
-	    while (db_lookup(gedp->ged_wdbp->dbip, bu_vls_addr(&shell_name), 0) != RT_DIR_NULL) {
-		bu_vls_trunc(&shell_name, 0);
-		shell_count++;
-		bu_vls_printf(&shell_name, "shell.%d", shell_count);
-	    }
-
-	    /* Export NMG as a new solid */
-	    RT_DB_INTERNAL_INIT(&new_intern);
-	    new_intern.idb_major_type = DB5_MAJORTYPE_BRLCAD;
-	    new_intern.idb_type = ID_NMG;
-	    new_intern.idb_meth = &OBJ[ID_NMG];
-	    new_intern.idb_ptr = (genptr_t)m_tmp;
-
-	    new_dp=db_diradd(gedp->ged_wdbp->dbip, bu_vls_addr(&shell_name), RT_DIR_PHONY_ADDR, 0, RT_DIR_SOLID, (genptr_t)&new_intern.idb_type);
-	    if (new_dp == RT_DIR_NULL) {
-		bu_vls_printf(gedp->ged_result_str, "An error has occurred while adding a new object to the database.\n");
-		return GED_ERROR;
-	    }
-
-	    /* make sure the geometry/bounding boxes are up to date */
-	    nmg_rebound(m_tmp, &gedp->ged_wdbp->wdb_tol);
-
-	    if (rt_db_put_internal(new_dp, gedp->ged_wdbp->dbip, &new_intern, &rt_uniresource) < 0) {
-		/* Free memory */
-		nmg_km(m_tmp);
-		bu_vls_printf(gedp->ged_result_str, "rt_db_put_internal() failure\n");
-		return GED_ERROR;
-	    }
-	    /* Internal representation has been freed by rt_db_put_internal */
-	    new_intern.idb_ptr = (genptr_t)NULL;
-	}
+    bu_vls_printf(&shell_name, "shell.%d", shell_count);
+    while (db_lookup(gedp->ged_wdbp->dbip, bu_vls_addr(&shell_name), 0) != RT_DIR_NULL) {
+	bu_vls_trunc(&shell_name, 0);
+	shell_count++;
+	bu_vls_printf(&shell_name, "shell.%d", shell_count);
     }
+
+    /* Export NMG as a new solid */
+    RT_DB_INTERNAL_INIT(&new_intern);
+    new_intern.idb_major_type = DB5_MAJORTYPE_BRLCAD;
+    new_intern.idb_type = ID_NMG;
+    new_intern.idb_meth = &OBJ[ID_NMG];
+    new_intern.idb_ptr = (genptr_t)s_tmp;
+
+    new_dp=db_diradd(gedp->ged_wdbp->dbip, bu_vls_addr(&shell_name), RT_DIR_PHONY_ADDR, 0, RT_DIR_SOLID, (genptr_t)&new_intern.idb_type);
+    if (new_dp == RT_DIR_NULL) {
+	bu_vls_printf(gedp->ged_result_str, "An error has occurred while adding a new object to the database.\n");
+	return GED_ERROR;
+    }
+
+    /* make sure the geometry/bounding boxes are up to date */
+    nmg_rebound(s_tmp, &gedp->ged_wdbp->wdb_tol);
+
+    if (rt_db_put_internal(new_dp, gedp->ged_wdbp->dbip, &new_intern, &rt_uniresource) < 0) {
+	/* Free memory */
+	nmg_ks(s_tmp);
+	bu_vls_printf(gedp->ged_result_str, "rt_db_put_internal() failure\n");
+	return GED_ERROR;
+    }
+
+    /* Internal representation has been freed by rt_db_put_internal */
+    new_intern.idb_ptr = (genptr_t)NULL;
     bu_vls_free(&shell_name);
 
     return GED_OK;
