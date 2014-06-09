843342faee2548a79eba582b02e387d66a519f84
Thomas Browder <tom.browder@gmail.com>
2014-06-09 20:47:01 +0000

    add pretty printing and extraction
    svn:revision:61186
    svn:branch:d-binding
    svn:account:tbrowder2

--- misc/d-bindings/HObj.pm
+++ misc/d-bindings/HObj.pm
@@ -1,10 +1,16 @@
-package Hobj;
+package HObj;
+
+use v5.14;
 
 use strict;
 # get compile-time warnings with Class::Struct when overriding an accessor
 #use warnings;
 
+#use Text::Balanced 'extract_bracketed'; # extract balanced text (Conway)
+use Data::Dumper; $Data::Dumper::Indent = 1;
 use Class::Struct;
+
+use F;
 
 our %tag
   = (
@@ -17,6 +23,7 @@ our %tag
      # arrays need special accessors
      'tokens'     => '@', # original line tokenized
      'orig_array' => '@', # as extracted from the original header
+     'pretty'     => '@', # orig line split by 'exract_bracketed'
     );
 
 struct(HObj => \%tag);
@@ -35,6 +42,7 @@ sub get_new_hobj {
 		# arrays initially empty
 		'tokens'     => [], # original line tokenized
 		'orig_array' => [], # as extracted from the original header
+		'pretty'     => [], # orig line split by 'exract_bracketed'
 	       );
 
   return $b;
@@ -63,47 +71,160 @@ sub orig_array {
   }
 } # special accessor
 
+sub pretty {
+  my $self = shift @_;
+  my $aref = shift @_;
+  if (defined $aref) {
+    $self->{pretty} = [@{$aref}];
+  }
+  else {
+    return $self->{pretty};
+  }
+} # special accessor
+
+# open brackets
+my %ob
+  = (
+     '(' => 0,
+     #'[' => 1,
+     '{' => 2,
+     '<' => 3,
+    );
+# close brackets
+my %cb
+  = (
+     ')' => 0,
+     #']' => 1,
+     '}' => 2,
+     '>' => 3,
+     #';' => 3,
+    );
+
+sub lastchar {
+  # returns last character of a string
+  my $s = shift @_;
+  my $len = length $s;
+  return '' if !$len;
+
+  my $c = substr $s, $len-1, 1;
+  return $c;
+
+} # lastchar
+
+sub gen_pretty {
+  # put extracted object into pretty format for printing
+  my $self = shift @_;
+
+  my $text = $self->orig_line(); # the flattened line
+
+  # use a tmp array and a line
+  my @arr = ();
+  my $len = length $text;
+  my $line = '';
+  my $level = 0;
+  for (my $i = 0; $i < $len; ++$i) {
+    my $c = substr $text, $i, 1;
+    if (exists $ob{$c}) {
+      # an open bracket ends the line (and increases the level)
+      ++$level;
+      $line .= ' ' if ($line && lastchar($line) ne ' ');
+      $line .= $c;
+      push @arr, $line;
+
+      # next line should start with an indent
+      $line = F::get_spaces($level);
+    }
+    elsif (exists $cb{$c}) {
+      # a close bracket ends the current line and starts a new line
+      # (and decreases the level)
+      --$level;
+
+      push @arr, $line if $line;
+
+      $line = $level ? F::get_spaces($level) : '';
+      $line .= $c;
+    }
+    else {
+      if ($c eq ' ') {
+	$line .= $c if ($line && lastchar($line) ne ' ');
+      }
+      else {
+	$line .= $c;
+      }
+    }
+
+    # check for typedef
+    if ($line =~ m{\A typedef \s* \z}x) {
+      push @arr, 'typedef';
+      $line = '';
+    }
+  }
+  # remainder of any line
+  push @arr, $line
+    if $line;
+
+=pod
+
+  say "DEBUG: dumping \@arr array BEFORE cleanup:";
+  print Dumper(\@arr);
+
+=cut
+
+  # now tidy up a bit
+  foreach my $line (@arr) {
+    # remove spaces around square brackets
+    $line =~ s{\s* \[}{\[}xg;
+    $line =~ s{\[ \s*}{\[}xg;
+    $line =~ s{\s* \]}{\]}xg;
+    $line =~ s{\] \s*}{\]}xg;
+
+    # remove spaces before semicolons
+    $line =~ s{\s* ;}{;}xg;
+  }
+
+  # and save the mess
+  $self->pretty(\@arr);
+
+=pod
+
+  say "DEBUG: dumping \@arr array AFTER cleanup:";
+  print Dumper(\@arr);
+
+=cut
+
+} # gen_pretty
+
+sub print_pretty {
+  my $self = shift @_;
+} # print_pretty
+
 sub dump {
   my $self = shift @_;
-  print  "Dumping an Hobj object:\n";
+  say    "Dumping an Hobj object:";
   printf "  num: %d\n",        $self->num();
-  printf "  file: %s\n",       $self->file();
-  printf "  title: %s\n",      $self->title();
-  printf "  subtitle: %s\n",   $self->subtitle();
-  printf "  author: %s\n",     $self->author();
-  printf "  editor: %s\n",     $self->editor();
-  printf "  pub: %s\n",        $self->pub();
-  printf "  date: %s\n",       $self->date();
-  printf "  location: %s\n",   $self->location();
-  printf "  medium: %s\n",     $self->medium();
-  printf "  isbn: %s\n",       $self->isbn();
-  printf "  dtic_adnum: %s\n", $self->dtic_adnum();
-  printf "  keywords: %s\n",   $self->keywords();
+  printf "  type: %s\n",       $self->type();
+  printf "  orig_line: \"%s\"\n",  $self->orig_line();
+  printf "  first_line: %d\n", $self->first_line();
+  printf "  last_line: %d\n",  $self->last_line();
 
   my ($sref);
 
-  $sref = $self->dtic_abstract();
-  print "  dtic_abstract:\n";
+  $sref = $self->tokens();
+  say "  tokens:";
   if (defined $sref) {
-    print "    $_\n" for (@$sref);
+    say "    $_" for @$sref;
   }
 
-  $sref = $self->notes();
-  print "  notes:\n";
+  $sref = $self->orig_array();
+  say "  orig_array:";
   if (defined $sref) {
-    print "    $_\n" for (@$sref);
+    say "    $_" for @$sref;
   }
 
-  $sref = $self->pubsummary();
-  print "  pubsummary:\n";
+  $sref = $self->pretty();
+  say "  pretty:";
   if (defined $sref) {
-    print "    $_\n" for (@$sref);
-  }
-
-  $sref = $self->summary();
-  print "  summary:\n";
-  if (defined $sref) {
-    print "    $_\n" for (@$sref);
+    say "    $_" for @$sref;
   }
 
 } # dump
