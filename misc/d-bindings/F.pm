90f30fc903b0ff78e4bcb3b3639176e2d81632e7
Thomas Browder <tom.browder@gmail.com>
2014-06-11 18:59:51 +0000

    add new shared function
    svn:revision:61248
    svn:branch:d-binding
    svn:account:tbrowder2

--- misc/d-bindings/F.pm
+++ misc/d-bindings/F.pm
@@ -2,12 +2,34 @@ package F;
 
 # shared functions
 
+use v5.14;
+
 sub get_spaces {
   my $level = shift @_;
   my $ns = $level * 3;
   my $s = sprintf "%-*.*s", $ns, $ns, ' ';
   return $s;
 } # get_spaces
+
+sub debug_regex {
+  my $msg  = shift @_;
+  my @arr  = @_;
+
+  say "DEBUG: $msg";
+
+  my $nl = @arr;
+  for (my $i = 0; $i < $nl; ++$i) {
+    my $c = $arr[$i];
+    my $n = $i + 1;
+    if (defined $c) {
+      say "  \$$n = '$c'";
+    }
+    else {
+      say "  \$$n = 'undef'";
+    }
+  }
+
+} # debug_regex
 
 # mandatory true return for a Perl module
 1;
