a8b1b7a12eb71bb09aa9f4f4dd922dbb68354465
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2014-06-11 22:31:13 +0000

    Rework test a bit.
    svn:revision:61252
    svn:branch:openscenegraph
    svn:account:starseeker

--- src/libdm/osg-test.cpp
+++ src/libdm/osg-test.cpp
@@ -71,194 +71,24 @@ extern "C" {
 #include <osgText/Text>
 
 
-osg::Camera* createHUD()
+void
+create_solid_nodes(std::map<struct directory *, osg::ref_ptr<osg::Group> > *osg_nodes,
+      	struct db_i *dbip,
+	struct directory *dp)
 {
-    // create a camera to set up the projection and model view matrices, and the subgraph to draw in the HUD
-    osg::Camera* camera = new osg::Camera;
-
-    // set the projection matrix
-    camera->setProjectionMatrix(osg::Matrix::ortho2D(0,1280,0,1024));
-
-    // set the view matrix
-    camera->setReferenceFrame(osg::Transform::ABSOLUTE_RF);
-    camera->setViewMatrix(osg::Matrix::identity());
-
-    // only clear the depth buffer
-    camera->setClearMask(GL_DEPTH_BUFFER_BIT);
-
-    // draw subgraph after main camera view.
-    camera->setRenderOrder(osg::Camera::POST_RENDER);
-
-    // we don't want the camera to grab event focus from the viewers main camera(s).
-    camera->setAllowEventFocus(false);
-
-
-
-    // add to this camera a subgraph to render
-    {
-
-	osg::Geode* geode = new osg::Geode();
-
-	std::string timesFont("fonts/arial.ttf");
-
-	// turn lighting off for the text and disable depth test to ensure it's always ontop.
-	osg::StateSet* stateset = geode->getOrCreateStateSet();
-	stateset->setMode(GL_LIGHTING,osg::StateAttribute::OFF);
-
-	osg::Vec3 position(150.0f,800.0f,0.0f);
-	osg::Vec3 delta(0.0f,-120.0f,0.0f);
-
-	{
-	    osgText::Text* text = new  osgText::Text;
-	    geode->addDrawable( text );
-
-	    text->setFont(timesFont);
-	    text->setPosition(position);
-	    text->setText("Head Up Displays are simple :-)");
-
-	    position += delta;
-	}
-
-
-	{
-	    osgText::Text* text = new  osgText::Text;
-	    geode->addDrawable( text );
-
-	    text->setFont(timesFont);
-	    text->setPosition(position);
-	    text->setText("All you need to do is create your text in a subgraph.");
-
-	    position += delta;
-	}
-
-
-	{
-	    osgText::Text* text = new  osgText::Text;
-	    geode->addDrawable( text );
-
-	    text->setFont(timesFont);
-	    text->setPosition(position);
-	    text->setText("Then place an osg::Camera above the subgraph\n"
-		    "to create an orthographic projection.\n");
-
-	    position += delta;
-	}
-
-	{
-	    osgText::Text* text = new  osgText::Text;
-	    geode->addDrawable( text );
-
-	    text->setFont(timesFont);
-	    text->setPosition(position);
-	    text->setText("Set the Camera's ReferenceFrame to ABSOLUTE_RF to ensure\n"
-		    "it remains independent from any external model view matrices.");
-
-	    position += delta;
-	}
-
-	{
-	    osgText::Text* text = new  osgText::Text;
-	    geode->addDrawable( text );
-
-	    text->setFont(timesFont);
-	    text->setPosition(position);
-	    text->setText("And set the Camera's clear mask to just clear the depth buffer.");
-
-	    position += delta;
-	}
-
-	{
-	    osgText::Text* text = new  osgText::Text;
-	    geode->addDrawable( text );
-
-	    text->setFont(timesFont);
-	    text->setPosition(position);
-	    text->setText("And finally set the Camera's RenderOrder to POST_RENDER\n"
-		    "to make sure it's drawn last.");
-
-	    position += delta;
-	}
-
-
-	{
-	    osg::BoundingBox bb;
-	    for(unsigned int i=0;i<geode->getNumDrawables();++i)
-	    {
-		bb.expandBy(geode->getDrawable(i)->getBound());
-	    }
-
-	    osg::Geometry* geom = new osg::Geometry;
-
-	    osg::Vec3Array* vertices = new osg::Vec3Array;
-	    float depth = bb.zMin()-0.1;
-	    vertices->push_back(osg::Vec3(bb.xMin(),bb.yMax(),depth));
-	    vertices->push_back(osg::Vec3(bb.xMin(),bb.yMin(),depth));
-	    vertices->push_back(osg::Vec3(bb.xMax(),bb.yMin(),depth));
-	    vertices->push_back(osg::Vec3(bb.xMax(),bb.yMax(),depth));
-	    geom->setVertexArray(vertices);
-
-	    osg::Vec3Array* normals = new osg::Vec3Array;
-	    normals->push_back(osg::Vec3(0.0f,0.0f,1.0f));
-	    geom->setNormalArray(normals, osg::Array::BIND_OVERALL);
-
-	    osg::Vec4Array* colors = new osg::Vec4Array;
-	    colors->push_back(osg::Vec4(1.0f,1.0,0.8f,0.2f));
-	    geom->setColorArray(colors, osg::Array::BIND_OVERALL);
-
-	    geom->addPrimitiveSet(new osg::DrawArrays(GL_QUADS,0,4));
-
-	    osg::StateSet* stateset = geom->getOrCreateStateSet();
-	    stateset->setMode(GL_BLEND,osg::StateAttribute::ON);
-	    //stateset->setAttribute(new osg::PolygonOffset(1.0f,1.0f),osg::StateAttribute::ON);
-	    stateset->setRenderingHint(osg::StateSet::TRANSPARENT_BIN);
-
-	    geode->addDrawable(geom);
-	}
-
-	camera->addChild(geode);
-    }
-
-    return camera;
-}
-
-int main( int argc, char **argv )
-{
-    std::map<struct directory *, struct bu_list *> vlists;
-    std::map<struct directory *, osg::ref_ptr<osg::Group> > osg_nodes;
-    struct db_i *dbip = DBI_NULL;
-    struct directory *dp = RT_DIR_NULL;
     const struct bn_tol tol = {BN_TOL_MAGIC, 0.0005, 0.0005 * 0.0005, 1e-6, 1 - 1e-6};
     const struct rt_tess_tol rttol = {RT_TESS_TOL_MAGIC, 0.0, 0.01, 0};
-    if (argc != 3 || !argv) {
-	bu_exit(1, "Error - please specify a .g file and an object\n");
-    }
-    if (!bu_file_exists(argv[1], NULL)) {
-	bu_exit(1, "Cannot stat file %s\n", argv[1]);
-    }
 
-    if ((dbip = db_open(argv[1], DB_OPEN_READONLY)) == DBI_NULL) {
-	bu_exit(1, "Cannot open geometry database file %s\n", argv[1]);
-    }
-    RT_CK_DBI(dbip);
-    if (db_dirbuild(dbip) < 0) {
-	db_close(dbip);
-	bu_exit(1, "db_dirbuild failed on geometry database file %s\n", argv[1]);
-    }
+    std::map<struct directory *, struct bu_list *> vlists;
 
-    dp = db_lookup(dbip, argv[2], LOOKUP_QUIET);
-    if (dp == RT_DIR_NULL) {
-	bu_exit(1, "ERROR: Unable to fine object %s in %s\n", argv[2], argv[1]);
-    }
+    const char *solid_search = "! -type comb";
+    struct bu_ptbl solids = BU_PTBL_INIT_ZERO;
+    (void)db_search(&solids, DB_SEARCH_RETURN_UNIQ_DP, solid_search, 1, &dp, dbip);
 
     /* Need to initialize this for rt_obj_plot, which may call RT_ADD_VLIST */
     BU_LIST_INIT(&RTG.rtg_vlfree);
 
-    /* Get the plot vlists associated with the solid objects below the supplied object */
-    const char *solid_search = "! -type comb";
-    const char *comb_search = "-type comb";
-    struct bu_ptbl solids = BU_PTBL_INIT_ZERO;
-    struct bu_ptbl combs = BU_PTBL_INIT_ZERO;
-    (void)db_search(&solids, DB_SEARCH_RETURN_UNIQ_DP, solid_search, 1, &dp, dbip);
+
     for (int i = (int)BU_PTBL_LEN(&solids) - 1; i >= 0; i--) {
 	/* Get the vlist associated with this particular object */
 	struct directory *curr_dp = (struct directory *)BU_PTBL_GET(&solids, i);
@@ -343,23 +173,34 @@ int main( int argc, char **argv )
 		state->setMode(GL_LIGHTING, osg::StateAttribute::OFF);
 	    }
 	    wrapper->addChild(geode);
-	    osg_nodes[curr_dp] = wrapper.get();
+	    (*osg_nodes)[curr_dp] = wrapper.get();
+	    RT_FREE_VLIST(plot_segments);
+	    BU_PUT(plot_segments, struct bu_list);
 	}
     }
     db_search_free(&solids);
+
+}
+
+void
+create_comb_nodes(std::map<struct directory *, osg::ref_ptr<osg::Group> > *osg_nodes,
+      	struct db_i *dbip,
+	struct directory *dp)
+{
+    const char *comb_search = "-type comb";
+    struct bu_ptbl combs = BU_PTBL_INIT_ZERO;
     (void)db_search(&combs, DB_SEARCH_RETURN_UNIQ_DP, comb_search, 1, &dp, dbip);
     for (int i = (int)BU_PTBL_LEN(&combs) - 1; i >= 0; i--) {
-	unsigned char rgb[3] = { 255, 0, 0 };
-	struct rt_db_internal intern;
 	struct directory *curr_dp = (struct directory *)BU_PTBL_GET(&combs, i);
 	osg::ref_ptr<osg::Group> comb = new osg::Group;
 	comb->setName(curr_dp->d_namep);
-	osg_nodes[curr_dp] = comb.get();
+	(*osg_nodes)[curr_dp] = comb.get();
+
+	struct rt_db_internal intern;
 	(void)rt_db_get_internal(&intern, curr_dp, dbip, (fastf_t *)NULL, &rt_uniresource);
 	struct rt_comb_internal *comb_internal= (struct rt_comb_internal *)intern.idb_ptr;
-
-	float color[3] = {-1.0, -1.0, -1.0};
 	/* Set color for this comb */
+	float color[3] = {-1.0, -1.0, -1.0};
 	if (comb_internal->rgb_valid) {
 	    VSCALE(color, comb_internal->rgb, (1.0/255.0) );
 	} else {
@@ -378,12 +219,13 @@ int main( int argc, char **argv )
 	    mtl->setDiffuse(osg::Material::FRONT_AND_BACK, osg::Vec4(color[0], color[1], color[2], 1.0f));
 	    osg::StateSet* state = comb->getOrCreateStateSet();
 	    if (comb_internal->inherit || comb_internal->region_flag) {
-		state->setAttributeAndModes(mtl, osg::StateAttribute::OVERRIDE|osg::StateAttribute::ON);
+		state->setAttributeAndModes(mtl.get(), osg::StateAttribute::OVERRIDE|osg::StateAttribute::ON);
 	    } else {
-		state->setAttributeAndModes(mtl, osg::StateAttribute::ON);
+		state->setAttributeAndModes(mtl.get(), osg::StateAttribute::ON);
 	    }
 	}
 	rt_db_free_internal(&intern);
+
     }
     for (int i = (int)BU_PTBL_LEN(&combs) - 1; i >= 0; i--) {
 	struct directory *curr_dp = (struct directory *)BU_PTBL_GET(&combs, i);
@@ -397,8 +239,8 @@ int main( int argc, char **argv )
 		MAT_TRANSPOSE(m, curr_path->fp_mat[curr_path->fp_len - 1]);
 		osg::Matrixd osgMat(m);
 		osg::ref_ptr<osg::MatrixTransform> mt = new osg::MatrixTransform(osgMat);
-		mt->addChild(osg_nodes[DB_FULL_PATH_CUR_DIR(curr_path)]);
-		osg_nodes[curr_dp]->addChild(mt);
+		mt->addChild((*osg_nodes)[DB_FULL_PATH_CUR_DIR(curr_path)]);
+		(*osg_nodes)[curr_dp]->addChild(mt);
 
 		/* If this child is subtracted in this comb, stipple the line drawings */
 		if (curr_path->fp_bool[curr_path->fp_len - 1] == 4) {
@@ -417,39 +259,52 @@ int main( int argc, char **argv )
 		    ls->setPattern(0xCF33);
 		    ls->setFactor(1);
 		    state->setAttributeAndModes(ls.get());
-		    property_wrapper->addChild(osg_nodes[DB_FULL_PATH_CUR_DIR(curr_path)]);
-		    osg_nodes[curr_dp]->addChild(property_wrapper);
+		    property_wrapper->addChild((*osg_nodes)[DB_FULL_PATH_CUR_DIR(curr_path)]);
+		    (*osg_nodes)[curr_dp]->addChild(property_wrapper);
 		} else {
-		    osg_nodes[curr_dp]->addChild(osg_nodes[DB_FULL_PATH_CUR_DIR(curr_path)]);
+		    (*osg_nodes)[curr_dp]->addChild((*osg_nodes)[DB_FULL_PATH_CUR_DIR(curr_path)]);
 		}
 	    }
 	}
 	db_search_free(&comb_children);
     }
+    db_search_free(&combs);
+}
+
+int main( int argc, char **argv )
+{
+    std::map<struct directory *, osg::ref_ptr<osg::Group> > osg_nodes;
+    struct db_i *dbip = DBI_NULL;
+    struct directory *dp = RT_DIR_NULL;
+
+    if (argc != 3 || !argv) {
+	bu_exit(1, "Error - please specify a .g file and an object\n");
+    }
+    if (!bu_file_exists(argv[1], NULL)) {
+	bu_exit(1, "Cannot stat file %s\n", argv[1]);
+    }
+
+    if ((dbip = db_open(argv[1], DB_OPEN_READONLY)) == DBI_NULL) {
+	bu_exit(1, "Cannot open geometry database file %s\n", argv[1]);
+    }
+    RT_CK_DBI(dbip);
+    if (db_dirbuild(dbip) < 0) {
+	db_close(dbip);
+	bu_exit(1, "db_dirbuild failed on geometry database file %s\n", argv[1]);
+    }
+
+    dp = db_lookup(dbip, argv[2], LOOKUP_QUIET);
+    if (dp == RT_DIR_NULL) {
+	bu_exit(1, "ERROR: Unable to fine object %s in %s\n", argv[2], argv[1]);
+    }
+
+    create_solid_nodes(&(osg_nodes), dbip, dp);
+    create_comb_nodes(&(osg_nodes), dbip, dp);
 
     // construct the viewer.
     osgViewer::Viewer viewer;
-
-    viewer.setUpViewInWindow(0, 0, 640, 480);
-
-    // create a HUD as slave camera attached to the master view.
-#if 0
-    viewer.setUpViewAcrossAllScreens();
-
-    osgViewer::Viewer::Windows windows;
-    viewer.getWindows(windows);
-
-    if (windows.empty()) return 1;
-
-    osg::Camera* hudCamera = createHUD();
-
-    // set up cameras to render on the first window available.
-    hudCamera->setGraphicsContext(windows[0]);
-    hudCamera->setViewport(0,0,windows[0]->getTraits()->width, windows[0]->getTraits()->height);
-
-    viewer.addSlave(hudCamera, false);
-#endif
     viewer.setSceneData(osg_nodes[dp]);
+    viewer.setUpViewInWindow(0, 0, 640, 480);
 
     viewer.addEventHandler(new osgViewer::StatsHandler);
 
