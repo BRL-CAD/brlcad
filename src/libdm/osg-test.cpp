97f63088021d2947fe8577c99ac54f287ca6052b
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2014-06-12 01:16:53 +0000

    Might as well note that these don't improve the FPS...
    svn:revision:61256
    svn:branch:openscenegraph
    svn:account:starseeker

--- src/libdm/osg-test.cpp
+++ src/libdm/osg-test.cpp
@@ -308,6 +308,11 @@ int main( int argc, char **argv )
 
     viewer.addEventHandler(new osgViewer::StatsHandler);
 
+    // The settings below do not seem to improve FPS, and the latter results in no geometry being
+    // visible without improving the FPS.
+    viewer.getCamera()->setCullingMode(osg::CullSettings::NO_CULLING);
+    //viewer.getCamera()->setComputeNearFarMode(osg::CullSettings::DO_NOT_COMPUTE_NEAR_FAR);
+
     return viewer.run();
 }
 // Local Variables:
