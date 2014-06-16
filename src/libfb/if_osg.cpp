a6764c30a419a1758e8a6ad7de3ab70780c587de
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2014-06-16 23:26:33 +0000

    Merge the trimmed down FrameBufferManipulator into if_osg.cpp
    svn:revision:61333
    svn:branch:openscenegraph
    svn:account:starseeker

--- src/libfb/if_osg.cpp
+++ src/libfb/if_osg.cpp
@@ -42,7 +42,6 @@ extern "C" {
 #include <osgViewer/ViewerEventHandlers>
 
 #include <osgGA/StateSetManipulator>
-#include "osgGA_FrameBufferManipulator.h"
 
 #include <osg/ImageUtils>
 #include <osg/TextureRectangle>
@@ -52,6 +51,199 @@ extern "C" {
 #include <osg/Timer>
 
 #include <iostream>
+
+
+/* The following is a customization of OrbitManipulator for Framebuffer applications */
+/* -*-c++-*- OpenSceneGraph - Copyright (C) 1998-2010 Robert Osfield
+ *
+ * This library is open source and may be redistributed and/or modified under
+ * the terms of the OpenSceneGraph Public License (OSGPL) version 0.0 or
+ * (at your option) any later version.  The full license is in LICENSE file
+ * included with this distribution, and on the openscenegraph.org website.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * OpenSceneGraph Public License for more details.
+ *
+ * OrbitManipulator code Copyright (C) 2010 PCJohn (Jan Peciva)
+ * while some pieces of code were taken from OSG.
+ * Thanks to company Cadwork (www.cadwork.ch) and
+ * Brno University of Technology (www.fit.vutbr.cz) for open-sourcing this work.
+*/
+
+#include <osgGA/StandardManipulator>
+
+#include <osg/BoundsChecking>
+#include <cassert>
+
+using namespace osg;
+using namespace osgGA;
+
+namespace osgGA {
+
+
+/** FrameBufferManipulator is base class for interacting with a framebuffer .*/
+class OSGGA_EXPORT FrameBufferManipulator : public StandardManipulator
+{
+        typedef StandardManipulator inherited;
+
+    public:
+
+        FrameBufferManipulator( int flags = DEFAULT_SETTINGS );
+        FrameBufferManipulator( const FrameBufferManipulator& om,
+                          const osg::CopyOp& copyOp = osg::CopyOp::SHALLOW_COPY );
+
+        META_Object( osgGA, FrameBufferManipulator );
+
+        virtual void setByMatrix( const osg::Matrixd& matrix );
+        virtual void setByInverseMatrix( const osg::Matrixd& matrix );
+        virtual osg::Matrixd getMatrix() const;
+        virtual osg::Matrixd getInverseMatrix() const;
+
+        virtual void setTransformation( const osg::Vec3d& eye, const osg::Quat& rotation );
+        virtual void setTransformation( const osg::Vec3d& eye, const osg::Vec3d& center, const osg::Vec3d& up );
+        virtual void getTransformation( osg::Vec3d& eye, osg::Quat& rotation ) const;
+        virtual void getTransformation( osg::Vec3d& eye, osg::Vec3d& center, osg::Vec3d& up ) const;
+
+        virtual osgUtil::SceneView::FusionDistanceMode getFusionDistanceMode() const;
+        virtual float getFusionDistanceValue() const;
+
+    protected:
+
+        osg::Vec3d _center;
+        osg::Quat  _rotation;
+        double     _distance;
+
+        double _minimumDistance;
+        static int _minimumDistanceFlagIndex;
+};
+
+
+int FrameBufferManipulator::_minimumDistanceFlagIndex = allocateRelativeFlag();
+
+
+/// Constructor.
+FrameBufferManipulator::FrameBufferManipulator( int flags )
+   : inherited( flags ), _distance( 1. )
+{
+}
+
+
+/// Constructor.
+FrameBufferManipulator::FrameBufferManipulator( const FrameBufferManipulator& om, const CopyOp& copyOp )
+   : osg::Object(om, copyOp), inherited( om, copyOp ), _center( om._center ),
+     _rotation( om._rotation ), _distance( om._distance ), _minimumDistance( om._minimumDistance )
+{
+}
+
+
+/** Set the position of the manipulator using a 4x4 matrix.*/
+void FrameBufferManipulator::setByMatrix( const osg::Matrixd& matrix )
+{
+    _center = osg::Vec3d( 0., 0., -_distance ) * matrix;
+    _rotation = matrix.getRotate();
+
+    // fix current rotation
+    if( getVerticalAxisFixed() )
+        fixVerticalAxis( _center, _rotation, true );
+}
+
+
+/** Set the position of the manipulator using a 4x4 matrix.*/
+void FrameBufferManipulator::setByInverseMatrix( const osg::Matrixd& matrix )
+{
+    setByMatrix( osg::Matrixd::inverse( matrix ) );
+}
+
+
+/** Get the position of the manipulator as 4x4 matrix.*/
+osg::Matrixd FrameBufferManipulator::getMatrix() const
+{
+    return osg::Matrixd::translate( 0., 0., _distance ) *
+           osg::Matrixd::rotate( _rotation ) *
+           osg::Matrixd::translate( _center );
+}
+
+
+/** Get the position of the manipulator as a inverse matrix of the manipulator,
+    typically used as a model view matrix.*/
+osg::Matrixd FrameBufferManipulator::getInverseMatrix() const
+{
+    return osg::Matrixd::translate( -_center ) *
+           osg::Matrixd::rotate( _rotation.inverse() ) *
+           osg::Matrixd::translate( 0.0, 0.0, -_distance );
+}
+
+
+// doc in parent
+void FrameBufferManipulator::setTransformation( const osg::Vec3d& eye, const osg::Quat& rotation )
+{
+    _center = eye + rotation * osg::Vec3d( 0., 0., -_distance );
+    _rotation = rotation;
+
+    // fix current rotation
+    if( getVerticalAxisFixed() )
+        fixVerticalAxis( _center, _rotation, true );
+}
+
+
+// doc in parent
+void FrameBufferManipulator::getTransformation( osg::Vec3d& eye, osg::Quat& rotation ) const
+{
+    eye = _center - _rotation * osg::Vec3d( 0., 0., -_distance );
+    rotation = _rotation;
+}
+
+
+// doc in parent
+void FrameBufferManipulator::setTransformation( const osg::Vec3d& eye, const osg::Vec3d& center, const osg::Vec3d& up )
+{
+    Vec3d lv( center - eye );
+
+    Vec3d f( lv );
+    f.normalize();
+    Vec3d s( f^up );
+    s.normalize();
+    Vec3d u( s^f );
+    u.normalize();
+
+    osg::Matrixd rotation_matrix( s[0], u[0], -f[0], 0.0f,
+                            s[1], u[1], -f[1], 0.0f,
+                            s[2], u[2], -f[2], 0.0f,
+                            0.0f, 0.0f,  0.0f, 1.0f );
+
+    _center = center;
+    _distance = lv.length();
+    _rotation = rotation_matrix.getRotate().inverse();
+
+    // fix current rotation
+    if( getVerticalAxisFixed() )
+        fixVerticalAxis( _center, _rotation, true );
+}
+
+
+// doc in parent
+void FrameBufferManipulator::getTransformation( osg::Vec3d& eye, osg::Vec3d& center, osg::Vec3d& up ) const
+{
+    center = _center;
+    eye = _center + _rotation * osg::Vec3d( 0., 0., _distance );
+    up = _rotation * osg::Vec3d( 0., 1., 0. );
+}
+
+/** Get the FusionDistanceMode. Used by SceneView for setting up stereo convergence.*/
+osgUtil::SceneView::FusionDistanceMode FrameBufferManipulator::getFusionDistanceMode() const
+{
+    return osgUtil::SceneView::USE_FUSION_DISTANCE_VALUE;
+}
+
+/** Get the FusionDistanceValue. Used by SceneView for setting up stereo convergence.*/
+float FrameBufferManipulator::getFusionDistanceValue() const
+{
+    return _distance;
+}
+
+} // end osgGA namespace
 
 struct osginfo {
     osgViewer::Viewer *viewer;
