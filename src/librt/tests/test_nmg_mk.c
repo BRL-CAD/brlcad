f437df6a91e8e440459be7a41a8699a2f295b206
Zhao Anqing <zaqcloud@hotmail.com>
2014-06-17 01:29:06 +0000

    change unit test for nmg_mk to fit new nmg. Eliminate unless model and nmgregion as well as test for mmr and mrsv.
    svn:revision:61335
    svn:branch:nmgreorg
    svn:account:zhaoanqing

--- src/librt/tests/test_nmg_mk.c
+++ src/librt/tests/test_nmg_mk.c
@@ -25,114 +25,36 @@
 #include "raytrace.h"
 
 int
-test_nmg_mm(void)
+test_nmg_ms(void)
 {
     int result;
-    struct model *m;
+    struct shell *s;
 
     result = 0;
-    m = nmg_mm();
+    s = nmg_ms();
 
-    /* test m */
-    if (m->index != 0) {
-	bu_log("Error index of model in nmg_mm %ld\n", m->index);
+    /* test s */
+    if (s->index != 0) {
+	bu_log("Error index of shell in nmg_ms %ld\n", s->index);
 	result = -1;
     }
 
-    if (m->maxindex != 1) {
-	bu_log("Error maxindex of model in nmg_mm %ld\n", m->maxindex);
+    if (s->maxindex != 1) {
+	bu_log("Error maxindex of shell in nmg_ms %ld\n", s->maxindex);
 	result = -1;
     }
 
-    if (m->magic != NMG_MODEL_MAGIC) {
-	bu_log("Error magic of model in nmg_mm, it should be NMG_MODEL_MAGIC\n");
+    if (s->magic != NMG_SHELL_MAGIC) {
+	bu_log("Error magic of shell in nmg_ms, it should be NMG_SHELL_MAGIC\n");
 	result = -1;
     }
 
-    if (m->manifolds != NULL) {
-	bu_log("Error manifolds of model in nmg_mm, it should be NULL\n");
+    if (s->manifolds != NULL) {
+	bu_log("Error manifolds of shell in nmg_ms, it should be NULL\n");
 	result = -1;
     }
 
-    nmg_km(m);
-    return result;
-}
-
-int
-test_nmg_mmr(void)
-{
-    int result;
-    struct model *m;
-    struct nmgregion *r;
-
-    result = 0;
-    m = nmg_mmr();
-    r = BU_LIST_FIRST(nmgregion, &m->r_hd);
-
-    /* test m */
-    if (m->maxindex != 2) {
-	bu_log("Error maxindex of model in nmg_mmr %ld\n", m->maxindex);
-	result = -1;
-    }
-
-    /* test r */
-    if (r == NULL) {
-	bu_log("Error r_hd of model in nmg_mmr");
-	nmg_km(m);
-	result = -1;
-	return result;
-    }
-
-    if (r->index != 1) {
-	bu_log("Error index of region in nmg_mmr %ld\n", r->index);
-	result = -1;
-    }
-
-    if (r->l.magic != NMG_REGION_MAGIC) {
-	bu_log("Error magic of region in nmg_mmr, it should be NMG_REGION_MAGIC\n");
-	result = -1;
-    }
-
-    nmg_km(m);
-    return result;
-}
-
-int
-test_nmg_mrsv(void)
-{
-    int result;
-    struct model *m;
-    struct nmgregion *r;
-
-    result = 0;
-    m = nmg_mmr();
-    r = nmg_mrsv(m);
-
-    /* test m */
-    if (m->maxindex != 6) {
-	bu_log("Error maxindex of model in nmg_mrsv %ld\n", m->maxindex);
-	result = -1;
-    }
-
-    /* test r */
-    if (r == NULL) {
-	bu_log("Error r_hd of model in nmg_mrsv");
-	nmg_km(m);
-	result = -1;
-	return result;
-    }
-
-    if (r->index != 2) {
-	bu_log("Error index of region in nmg_mrsv %ld\n", r->index);
-	result = -1;
-    }
-
-    if (r->l.magic != NMG_REGION_MAGIC) {
-	bu_log("Error magic of region in nmg_mrsv, it should be NMG_REGION_MAGIC\n");
-	result = -1;
-    }
-
-    nmg_km(m);
+    nmg_ks(s);
     return result;
 }
 
@@ -140,40 +62,36 @@ int
 test_nmg_msv(void)
 {
     int result;
-    struct model *m;
-    struct nmgregion *r;
     struct shell *s;
 
     result = 0;
-    m = nmg_mmr();
-    r = BU_LIST_FIRST(nmgregion, &m->r_hd);
-    s = nmg_msv(r);
+    s = nmg_msv();
 
-    /* test m */
-    if (m->maxindex != 5) {
-	bu_log("Error maxindex of model in nmg_msv %ld\n", m->maxindex);
+    /* test s */
+    if (s->maxindex != 3) {
+	bu_log("Error maxindex of shell in nmg_msv %ld\n", s->maxindex);
 	result = -1;
     }
 
     /* test s */
     if (s == NULL) {
-	bu_log("Error r_hd of model in nmg_msv");
-	nmg_km(m);
+	bu_log("Error r_hd of shell in nmg_msv");
+	nmg_ks(s);
 	result = -1;
 	return result;
     }
 
-    if (s->index != 2) {
+    if (s->index != 0) {
 	bu_log("Error index of shell in nmg_msv %ld\n", s->index);
 	result = -1;
     }
 
-    if (s->l.magic != NMG_SHELL_MAGIC) {
+    if (s->magic != NMG_SHELL_MAGIC) {
 	bu_log("Error magic of shell in nmg_msv, it should be NMG_SHELL_MAGIC\n");
 	result = -1;
     }
 
-    nmg_km(m);
+    nmg_ks(s);
     return result;
 }
 
@@ -184,16 +102,8 @@ main(int argc, char **argv)
 	bu_exit(1, "Usage: %s\n", argv[0]);
     }
 
-    if (test_nmg_mm() < 0) {
-	bu_exit(1, "Test for nmg_mm failed!\n");
-    }
-
-    if (test_nmg_mmr() < 0) {
-	bu_exit(1, "Test for nmg_mmr failed!\n");
-    }
-
-    if (test_nmg_mrsv() < 0) {
-	bu_exit(1, "Test for nmg_mrsv failed!\n");
+    if (test_nmg_ms() < 0) {
+	bu_exit(1, "Test for nmg_ms failed!\n");
     }
 
     if (test_nmg_msv() < 0) {
