fdfa9017e6ec0c7885a49f640ee26e5fd33d331e
Daniel Rossberg <33295041+drossberg@users.noreply.github.com>
2014-06-30 08:02:47 +0000

    for Microsoft Visual Studio version 9 (2008) and lower(?): if _UINTPTR_T_DEFINED/_INTPTR_T_DEFINED the types are defined but not the other macros (UINTPTR_MAX etc.) prevent redefinition of uintptr_t/intptr_t for these cases it should be a valid assumption for every system that if _UINTPTR_T_DEFINED/_INTPTR_T_DEFINED is defined the type uintptr_t/intptr_t exists
    svn:revision:61478
    svn:branch:trunk
    svn:account:d_rossberg

--- include/pstdint.h
+++ include/pstdint.h
@@ -708,8 +708,12 @@ typedef uint_least32_t uint_fast32_t;
 #  ifndef UINTPTR_C
 #    define UINTPTR_C(x)                stdint_intptr_glue3(UINT,stdint_intptr_bits,_C)(x)
 #  endif
-  typedef stdint_intptr_glue3(uint,stdint_intptr_bits,_t) uintptr_t;
-  typedef stdint_intptr_glue3( int,stdint_intptr_bits,_t)  intptr_t;
+#  ifndef _UINTPTR_T_DEFINED
+     typedef stdint_intptr_glue3(uint,stdint_intptr_bits,_t) uintptr_t;
+#  endif
+#  ifndef _INTPTR_T_DEFINED
+     typedef stdint_intptr_glue3( int,stdint_intptr_bits,_t)  intptr_t;
+#  endif
 # else
 /* TODO -- This following is likely wrong for some platforms, and does
    nothing for the definition of uintptr_t. */
