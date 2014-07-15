cfe5c331ca6f7573953bd4f14f0ad9d62c84f4ff
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2014-07-15 15:34:19 +0000

    Add the constraints in main, instead of the class definition. This is closer to what a dynamic creation of a space from geometry parameters will look like.
    svn:revision:61737
    svn:branch:gecode
    svn:account:starseeker

--- src/libpc/gecode_solver_test.cpp
+++ src/libpc/gecode_solver_test.cpp
@@ -57,37 +57,35 @@
 
 using namespace Gecode;
 
-class SendMoreMoney : public Space {
-protected:
-  IntVarArray l;
+class EqSolve : public Space {
 public:
-  SendMoreMoney(void) : l(*this, 8, 0, 10) {
-    IntVar s(l[0]), e(l[1]), n(l[2]), d(l[3]),
-           m(l[4]), o(l[5]), r(l[6]), y(l[7]);
-    rel(*this, s != 0);
-    rel(*this, m != 0);
-    distinct(*this, l);
-    rel(*this,             1000*s + 100*e + 10*n + d
-                         + 1000*m + 100*o + 10*r + e
-              == 10000*m + 1000*o + 100*n + 10*e + y);
-    branch(*this, l, INT_VAR_SIZE_MIN(), INT_VAL_MIN());
+  IntVarArray l;
+  EqSolve(void) : l(*this, 8, 0, 10) {
+     branch(*this, l, INT_VAR_SIZE_MIN(), INT_VAL_MIN());
   }
-  SendMoreMoney(bool share, SendMoreMoney& s) : Space(share, s) {
+  EqSolve(bool share, EqSolve& s) : Space(share, s) {
     l.update(*this, share, s.l);
   }
   virtual Space* copy(bool share) {
-    return new SendMoreMoney(share,*this);
+    return new EqSolve(share,*this);
   }
   void print(void) const {
     std::cout << l << std::endl;
   }
 };
 
-int main(int argc, char* argv[]) {
-  SendMoreMoney* m = new SendMoreMoney;
-  DFS<SendMoreMoney> e(m);
+int main() {
+  EqSolve* m = new EqSolve;
+  rel((*m), m->l[0] != 0);
+  rel(*m, m->l[4] != 0);
+  distinct(*m, m->l);
+  rel(*m,             1000*m->l[0] + 100*m->l[1] + 10*m->l[2] + m->l[3]
+                        + 1000*m->l[4] + 100*m->l[5] + 10*m->l[6] + m->l[1]
+             == 10000*m->l[4] + 1000*m->l[5] + 100*m->l[2] + 10*m->l[1] + m->l[7]);
+
+  DFS<EqSolve> e(m);
   delete m;
-  while (SendMoreMoney* s = e.next()) {
+  while (EqSolve* s = e.next()) {
     s->print(); delete s;
   }
   return 0;
