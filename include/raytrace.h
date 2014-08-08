9f1de265e0e497fa9e1df63a8cef9386d596e861
Zhao Anqing <zaqcloud@hotmail.com>
2014-08-08 05:00:27 +0000

    remove some tree/rt_comb functions which are not used.
    svn:revision:62042
    svn:branch:nmgreorg
    svn:account:zhaoanqing

--- include/raytrace.h
+++ include/raytrace.h
@@ -7776,43 +7776,6 @@ RT_EXPORT extern void nmg_comb_merge(struct rt_comb_internal *comb1, struct rt_c
  */
 RT_EXPORT extern union tree *nmg_tree_union(union tree *tr1, union tree *tr2);
 
-/**
- * Initialize combination for nmg.
- */
-RT_EXPORT extern void nmg_comb_init(struct rt_comb_internal *comb);
-
-/**
- * Initialize combination for nmg.
- */
-RT_EXPORT extern void nmg_comb_free(struct rt_comb_internal *comb);
-
-/**
- * Initialize tree for nmg.
- */
-RT_EXPORT extern void nmg_tree_init(union tree *tp);
-
-/**
- * Free tree for nmg.
- */
-RT_EXPORT extern void nmg_tree_free(union tree *tp);
-
-/**
- * Count the node in nmg tree.
- */
-RT_EXPORT extern int nmg_tree_node_count(union tree *tp);
-
-/**
- * Count the leaf in nmg tree.
- */
-RT_EXPORT extern int nmg_tree_leaf_count(union tree *tp);
-
-RT_EXPORT extern int nmg_tree_maxindex_count(union tree *tp);
-
-RT_EXPORT extern struct rt_tree_array *nmg_tree_leaf_flatten(union tree *tp, const int leaf_count);
-
-RT_EXPORT extern void nmg_tree_leaf_flatten_helper(struct rt_tree_array *tree_array, union tree* tp, int *array_index);
-
-
 RT_EXPORT extern void rt_constraint_ifree(struct rt_db_internal *ip);
 
 RT_EXPORT extern int rt_constraint_export5(struct bu_external *ep,
