6f5f4dd47986c3d61e537671fc07e04fb99221b9
Christopher Sean Morrison <brlcad@gmail.com>
2014-09-15 17:48:26 +0000

    rename the function from rt_ to db_ namespace since it's more related to lower-level database handling than it is ray tracing (arguably should go even lower level but we don't currently have anything in-between and libbu feels too low.  resort the unicode handling in order of likely occurence.
    svn:revision:62732
    svn:branch:trunk
    svn:account:brlcad

--- src/librt/op.c
+++ src/librt/op.c
@@ -17,19 +17,18 @@
  * License along with this file; see the file named COPYING for more
  * information.
  */
-/** @file op.c
- *
- * These routines cover detecting and managing boolean operators.
- *
- */
 
 #include "common.h"
 
+/* interface header */
+#include "rt/op.h"
+
+/* implementation headers */
 #include <ctype.h>
 
 
-char
-rt_str2op(const char *str)
+db_op_t
+db_str2op(const char *str)
 {
     char ret = '\0';
 
@@ -39,6 +38,9 @@ rt_str2op(const char *str)
     if (isprint(str[0])) {
 	/* single byte/char */
 	switch (str[0]) {
+	    case 'u':
+		ret = 'u';
+		break;
 	    case '-':
 	    case '\\':
 		ret = '-';
@@ -48,18 +50,20 @@ rt_str2op(const char *str)
 	    case 'x':
 		ret = '+';
 		break;
-	    case 'u':
-		ret = 'u';
-		break;
 	}
     } else {
 	/* check if multibyte */
 
-	/* match unicode hyphenations/dashes, starting with hyphen,
-	 * non-breaking hypen, figure dash, en dash, em dash, and
-	 * horizontal bar.
-	 */
-	if (((unsigned char)str[0] == 0xE2 && (unsigned char)str[1] == 0x80 && (unsigned char)str[2] > 0x89 && (unsigned char)str[2] < 0x96)
+	if (((unsigned char)str[0] == 0xE2 && (unsigned char)str[1] == 0x88 && (unsigned char)str[2] == 0xAA) /* union symbol */
+		   || ((unsigned char)str[0] == 0xE2 && (unsigned char)str[1] == 0x8B && (unsigned char)str[2] == 0x83) /* n-ary union */
+	    )
+	{
+	    ret = 'u';
+	} else if (((unsigned char)str[0] == 0xE2 && (unsigned char)str[1] == 0x80 && (unsigned char)str[2] > 0x89 && (unsigned char)str[2] < 0x96)
+	    /* first check matches unicode symbol variants starting
+	     * with hyphen, non-breaking hypen, figure dash, en dash,
+	     * em dash, and horizontal bar.
+	     */
 	    || ((unsigned char)str[0] == 0xE2 && (unsigned char)str[1] == 0x88 && (unsigned char)str[2] == 0x92) /* minus sign */
 	    || ((unsigned char)str[0] == 0xEF && (unsigned char)str[1] == 0xB9 && (unsigned char)str[2] == 0x98) /* small em dash */
 	    || ((unsigned char)str[0] == 0xEF && (unsigned char)str[1] == 0xB9 && (unsigned char)str[2] == 0xA3) /* small hypen minus */
@@ -69,18 +73,13 @@ rt_str2op(const char *str)
 	    )
 	{
 	    ret = '-';
-	} else if (((unsigned char)str[0] == 0xE2 && (unsigned char)str[1] == 0x88 && (unsigned char)str[2] == 0xA9) /* difference symbol */
+	} else if (((unsigned char)str[0] == 0xE2 && (unsigned char)str[1] == 0x88 && (unsigned char)str[2] == 0xA9) /* intersection symbol */
 		   || ((unsigned char)str[0] == 0xE2 && (unsigned char)str[1] == 0x8B && (unsigned char)str[2] == 0x82) /* n-ary intersection */
 		   || ((unsigned char)str[0] == 0xCB && (unsigned char)str[1] == 0x96) /* utf-16, modifier plus sign */
 		   || ((unsigned char)str[0] == 0xC3 && (unsigned char)str[1] == 0x97) /* utf-16, multiplication sign */
 	    )
 	{
 	    ret = '+';
-	} else if (((unsigned char)str[0] == 0xE2 && (unsigned char)str[1] == 0x88 && (unsigned char)str[2] == 0xAA) /* union symbol */
-		   || ((unsigned char)str[0] == 0xE2 && (unsigned char)str[1] == 0x8B && (unsigned char)str[2] == 0x83) /* n-ary union */
-	    )
-	{
-	    ret = 'u';
 	}
     }
     return ret;
