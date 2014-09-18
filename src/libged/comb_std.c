2bfa4c96c45101b2e69d589ffd1664bcc5e3f2ae
Christopher Sean Morrison <brlcad@gmail.com>
2014-09-18 17:56:36 +0000

    expand comb_std with support for unicode operators and more consistent op handling.
    svn:revision:62832
    svn:branch:trunk
    svn:account:brlcad

--- src/libged/comb_std.c
+++ src/libged/comb_std.c
@@ -141,13 +141,13 @@ append_rparen(struct bu_list *hp)
 
 
 HIDDEN int
-add_operator(struct ged *gedp, struct bu_list *hp, char ch, short int *last_tok)
+add_operator(struct ged *gedp, struct bu_list *hp, char *ptr, short int *last_tok)
 {
-    char illegal[2];
+    db_op_t op = db_str2op(ptr);
 
     BU_CK_LIST_HEAD(hp);
 
-    switch (ch) {
+    switch (op) {
 	case DB_OP_UNION:
 	    append_union(hp);
 	    *last_tok = TOK_UNION;
@@ -161,9 +161,7 @@ add_operator(struct ged *gedp, struct bu_list *hp, char ch, short int *last_tok)
 	    *last_tok = TOK_SUBTR;
 	    break;
 	default:
-	    illegal[0] = ch;
-	    illegal[1] = '\0';
-	    bu_vls_printf(gedp->ged_result_str, "Illegal operator: %s, aborting\n", illegal);
+	    bu_vls_printf(gedp->ged_result_str, "Illegal operator: %c (0x%x), aborting\n", ptr[0], ptr[0]);
 	    free_tokens(hp);
 	    return GED_ERROR;
     }
@@ -566,7 +564,7 @@ ged_comb_std(struct ged *gedp, int argc, const char *argv[])
 
 	    if (last_tok == TOK_RPAREN) {
 		/* next token MUST be an operator */
-		if (add_operator(gedp, &tok_hd.l, *ptr, &last_tok) == GED_ERROR) {
+		if (add_operator(gedp, &tok_hd.l, ptr, &last_tok) == GED_ERROR) {
 		    free_tokens(&tok_hd.l);
 		    return GED_ERROR;
 		}
@@ -584,7 +582,7 @@ ged_comb_std(struct ged *gedp, int argc, const char *argv[])
 		ptr += name_len;
 	    } else if (last_tok == TOK_TREE) {
 		/* must be an operator */
-		if (add_operator(gedp, &tok_hd.l, *ptr, &last_tok) == GED_ERROR) {
+		if (add_operator(gedp, &tok_hd.l, ptr, &last_tok) == GED_ERROR) {
 		    free_tokens(&tok_hd.l);
 		    return GED_ERROR;
 		}
