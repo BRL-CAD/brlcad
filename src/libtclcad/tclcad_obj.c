9c3f2ebd20ffdce790ea65d5537f8f51446683bb
Jonathan Engbert <5529404+ejno@users.noreply.github.com>
2014-09-19 04:57:06 +0000

    move the previous option to preserve the TCL result into a separate function; use is clearer/more readable
    svn:revision:62857
    svn:branch:trunk
    svn:account:ejno

--- src/libtclcad/tclcad_obj.c
+++ src/libtclcad/tclcad_obj.c
@@ -1405,7 +1405,7 @@ Go_Init(Tcl_Interp *interp)
 
     {
 	const char *version_str = brlcad_version();
-	tclcad_eval(interp, 1, "set brlcad_version", 1, &version_str);
+	tclcad_eval_noresult(interp, "set brlcad_version", 1, &version_str);
     }
 
     /*XXX Use of brlcad_interp is temporary */
@@ -6328,7 +6328,7 @@ to_idle_mode(struct ged *gedp,
 	ged_grid(gedp, 2, (const char **)av);
 
 	if (0 < bu_vls_strlen(&gdvp->gdv_callback)) {
-	    tclcad_eval(current_top->to_interp, 1, bu_vls_addr(&gdvp->gdv_callback), 0, NULL);
+	    tclcad_eval_noresult(current_top->to_interp, bu_vls_addr(&gdvp->gdv_callback), 0, NULL);
 	}
 
 	need_refresh = 1;
@@ -6367,7 +6367,7 @@ to_is_viewable(struct ged_dm_view *gdvp)
     saved_result = Tcl_GetObjResult(current_top->to_interp);
     Tcl_IncrRefCount(saved_result);
 
-    if (tclcad_eval(current_top->to_interp, 0, "winfo viewable", 1, &pathname) != TCL_OK) {
+    if (tclcad_eval(current_top->to_interp, "winfo viewable", 1, &pathname) != TCL_OK) {
 	return 0;
     }
 
@@ -7000,7 +7000,7 @@ to_mouse_brep_selection_translate(struct ged *gedp,
     }
 
     /* need to tell front-end that we've modified the db */
-    tclcad_eval(current_top->to_interp, 1, "$::ArcherCore::application setSave", 0, NULL);
+    tclcad_eval_noresult(current_top->to_interp, "$::ArcherCore::application setSave", 0, NULL);
 
     gdvp->gdv_view->gv_prevMouseX = screen_end[X];
     gdvp->gdv_view->gv_prevMouseY = screen_end[Y];
@@ -7116,7 +7116,7 @@ to_mouse_constrain_rot(struct ged *gedp,
 
     if (ret == GED_OK) {
 	if (0 < bu_vls_strlen(&gdvp->gdv_callback)) {
-	    tclcad_eval(current_top->to_interp, 1, bu_vls_addr(&gdvp->gdv_callback), 0, NULL);
+	    tclcad_eval_noresult(current_top->to_interp, bu_vls_addr(&gdvp->gdv_callback), 0, NULL);
 	}
 
 	to_refresh_view(gdvp);
@@ -7231,7 +7231,7 @@ to_mouse_constrain_trans(struct ged *gedp,
 
     if (ret == GED_OK) {
 	if (0 < bu_vls_strlen(&gdvp->gdv_callback)) {
-	    tclcad_eval(current_top->to_interp, 1, bu_vls_addr(&gdvp->gdv_callback), 0, NULL);
+	    tclcad_eval_noresult(current_top->to_interp, bu_vls_addr(&gdvp->gdv_callback), 0, NULL);
 	}
 
 	to_refresh_view(gdvp);
@@ -8515,7 +8515,7 @@ to_mouse_orotate(struct ged *gedp,
 	args[1] = bu_vls_addr(&rot_x_vls);
 	args[2] = bu_vls_addr(&rot_y_vls);
 	args[3] = bu_vls_addr(&rot_z_vls);
-	tclcad_eval(current_top->to_interp, 0, command, sizeof(args) / sizeof(args[0]), args);
+	tclcad_eval(current_top->to_interp, command, sizeof(args) / sizeof(args[0]), args);
     } else {
 	char *av[6];
 
@@ -13966,7 +13966,7 @@ to_output_handler(struct ged *gedp, char *line)
     else
 	script = "puts";
 
-    tclcad_eval(current_top->to_interp, 1, script, 1, (const char **)&line);
+    tclcad_eval_noresult(current_top->to_interp, script, 1, (const char **)&line);
 }
 
 HIDDEN int
