43197522117e76ed6654b6922922080df3027298
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2014-09-22 14:19:22 +0000

    Remove the old mged function for invent_solid so we don't get conflict (thanks Keith) and fix bug introduced into draw.c during the rework.
    svn:revision:62898
    svn:branch:trunk
    svn:account:starseeker

--- src/libged/draw.c
+++ src/libged/draw.c
@@ -495,7 +495,8 @@ _ged_cvt_vlblock_to_solids(struct ged *gedp, struct bn_vlblock *vbp, const char 
 
     for (i = 0; i < vbp->nused; i++) {
 	if (BU_LIST_IS_EMPTY(&(vbp->head[i])))
-		snprintf(namebuf, 64, "%s%lx", shortname, vbp->rgb[i]);
+	    continue;
+	snprintf(namebuf, 64, "%s%lx", shortname, vbp->rgb[i]);
 	invent_solid(gedp->ged_gdp->gd_headDisplay, gedp->ged_wdbp->dbip, gedp->ged_create_vlist_callback, gedp->ged_free_vlist_callback, namebuf, &vbp->head[i], vbp->rgb[i], copy, 0.0, 0, gedp->freesolid);
     }
 }
