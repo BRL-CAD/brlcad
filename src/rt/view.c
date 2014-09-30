a43dba83facf7beb8d4dcd34c13dcddbc8f00be2
Carl Moore <cmoore@brlcad.org>
2014-09-30 19:16:01 +0000

    OK, now we expand the Usage for rt
    svn:revision:63066
    svn:branch:trunk
    svn:account:carlmoore

--- src/rt/view.c
+++ src/rt/view.c
@@ -86,6 +86,44 @@ usage(const char *argv0)
     bu_log(" -x #		librt debug flags\n");
     bu_log(" -N #		NMG debug flags\n");
     bu_log(" -X #		rt debug flags\n");
+    bu_log("-. #		Select factor in NUgrid algorithm (default is 1.5)\n");
+    bu_log("-, #		Selection of which space partitioning algorithm to use\n");
+    bu_log("-@ #		Set limit to each dimension of the nugrid\n");
+    bu_log("-b '# #'       Specify X and Y pixel coordinates (need quotes) for single ray to be fired, for debugging\n");
+    bu_log("-c script_command\n");
+    bu_log("		Supply, on command line, command which can appear in -M command script\n");
+    bu_log("-d #		Set flag for reporting of pixel distances\n");
+    bu_log("-f #		Set expected playback rate in frames-per-second (default is 30)\n");
+    bu_log("-g #		Set grid cell width, in millimeters\n");
+    bu_log("-h density,r,g,b\n");
+    bu_log("		Provide parameters for an exponential shading (default r,g,b is 0.8,0.9,0.99)\n");
+    bu_log("-i		Enable incremental mode processing\n");
+    bu_log("-j xmin,xmax,ymin,ymax\n");
+    bu_log("		Enable processing of sub-rectangle\n");
+    bu_log("-k xdir,ydir,zdir,dist\n");
+    bu_log("		Enable use of a cutting plane\n");
+    bu_log("-l #		Select lighting model (default is 0)\n");
+    bu_log("-t		Reverse the order of grid traversal (default is not to do that)\n");
+    bu_log("-u units	Specify the units (or use 'model' for the local model's units)\n");
+    bu_log("-v #		Set the verbosity bit vector flags\n");
+    bu_log("-A #		Set the ambient light intensity\n");
+    bu_log("-B		Turn on the 'benchmark' flag (default is off)\n");
+    bu_log("-C #/#/#	Set the background color to the RGB value #/#/#\n");
+    bu_log("-D #		Specify the starting frame number (ending frame number is specified via -K #)\n");
+    bu_log("-E #           Set the distance from eye point to center of the model RPP (default is sqrt(2))\n");
+    bu_log("-F framebuffer	Cause output to be sent to the indicated framebuffer\n");
+    bu_log("-G #		Set grid cell height, in millimeters\n");
+    bu_log("-H #		Set number of extra rays to fire\n");
+    bu_log("-I		Turn on interactive mode\n");
+    bu_log("-J #		Set a bit vector for 'jitter'\n");
+    bu_log("-K #		Specify the ending frame number (starting frame number is specified via -D #)\n");
+    bu_log("-O model.pix	Output .pix format file, double precision format\n");
+    bu_log("-Q x,y		Select pixel ray for query with debugging; compute other pixels without debugging\n");
+    bu_log("-S		Enable stereo viewing (off by default)\n");
+    bu_log("-W		Set background image color to white (default is black)\n");
+    bu_log("-! #		Turn on the libbu(3) library debugging flags\n");
+    bu_log("-+ t		Specify that output is NOT binary (default is that it is); -+ is otherwise not\n");
+    bu_log("		implemented\n");
 }
 
 
