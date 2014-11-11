68a6dc87046de0577f053bb8873a287a026342f6
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2014-11-11 23:05:03 +0000

    Add headers
    svn:revision:63399
    svn:branch:qtged
    svn:account:starseeker

--- src/qbrlcad/QAccordianWidget.h
+++ src/qbrlcad/QAccordianWidget.h
@@ -1,3 +1,29 @@
+/*              Q A C C O R D I A N W I D G E T . H
+ * BRL-CAD
+ *
+ * Copyright (c) 2014 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file QAccordianWidget.h
+ *
+ * Provides a structured container into which tool panels or objects can be
+ * added and then shown or hidden with a controlling button.  Can optionally
+ * enforce a rule that at most one object is open at a time.
+ *
+ */
 
 #include <QWidget>
 #include <QVBoxLayout>
