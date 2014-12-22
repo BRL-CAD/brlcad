a5e761bbe388170a1b0e93b36ce315eb9c66bcc8
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2014-12-22 18:14:02 +0000

    Make a stab at spotting cylinders.
    svn:revision:63808
    svn:branch:trunk
    svn:account:starseeker

--- src/librt/test_shape_recognition.cpp
+++ src/librt/test_shape_recognition.cpp
@@ -135,26 +135,158 @@ int
 is_cylinder(const object_data *data)
 {
     int ret = 1;
+    std::set<int>::iterator f_it;
     std::set<int> planar_surfaces;
     std::set<int> cylindrical_surfaces;
+    std::set<int> active_edges;
     // First, check surfaces.  If a surface is anything other than a plane or cylindrical,
-    // the verdict is no.
+    // the verdict is no.  If we don't have at least two planar surfaces and one
+    // cylindrical, the verict is no.
+    for (f_it = data->faces.begin(); f_it != data->faces.end(); f_it++) {
+	ON_BrepFace *used_face = &(data->brep->m_F[(*f_it)]);
+	ON_Surface *temp_surface = (ON_Surface *)used_face->SurfaceOf();
+	int surface_type = (int)GetSurfaceType(temp_surface);
+	switch (surface_type) {
+	    case SURFACE_PLANE:
+		planar_surfaces.insert(*f_it);
+		break;
+	    case SURFACE_CYLINDER:
+		cylindrical_surfaces.insert(*f_it);
+		break;
+	    default:
+		return 0;
+		break;
+	}
+    }
+    if (planar_surfaces.size() < 2) return 0;
+    if (cylindrical_surfaces.size() < 1) return 0;
 
-    // Second, check if all cylindrical surfaces share the same center line and all planar
-    // faces fit into either of two planes.
+    // Second, check if all cylindrical surfaces share the same axis.
+    ON_Cylinder cylinder;
+    data->brep->m_F[*cylindrical_surfaces.begin()].SurfaceOf()->IsCylinder(&cylinder);
+    //std::cout << "cylinder: " << cylinder.Axis().x << "," << cylinder.Axis().y << "," << cylinder.Axis().z << "\n";
+    for (f_it = cylindrical_surfaces.begin(); f_it != cylindrical_surfaces.end(); f_it++) {
+	ON_Cylinder f_cylinder;
+	data->brep->m_F[(*f_it)].SurfaceOf()->IsCylinder(&f_cylinder);
+	//std::cout << "f_cylinder: " << f_cylinder.Axis().x << "," << f_cylinder.Axis().y << "," << f_cylinder.Axis().z << "\n";
+	if (f_cylinder.Axis() != cylinder.Axis()) return 0;
+    }
 
-    // Third, check that the two planes are parallel to each other.
-    //
-    // Fourth, sort the edges into coplanar arcs/circles and lines.  In principle more general curves
-    // might be present in the cylindrical face of the cylinder, but for the moment assume
-    // the definitions consist of only circular arcs and lines for simplicity.
-    //
-    // Fifth, check if the coplanar arcs all lie on the same circle.
-    //
-    // Six, assemble the arcs (if possible) into complete circles.  If one or both of the
-    // edge loops in a planar face contains non-degenerate linear components, we do not have
-    // a simple cylinder.  A degenerate liner component is defined as two linear segments having
-    // the same two vertices - they are assumed to be mating edges in a larger shape.
+    // Third, see if all planes are coplanar with two and only two planes.
+    ON_Plane p1, p2;
+    int p2_set = 0;
+    data->brep->m_F[*planar_surfaces.begin()].SurfaceOf()->IsPlanar(&p1);
+    for (f_it = planar_surfaces.begin(); f_it != planar_surfaces.end(); f_it++) {
+	ON_Plane f_p;
+	data->brep->m_F[(*f_it)].SurfaceOf()->IsPlanar(&f_p);
+	if (!p2_set && f_p != p1) {
+	    p2 = f_p;
+	    continue;
+	};
+	if (f_p != p1 && f_p != p2) return 0;
+    }
+
+    // Fourth, check that the two planes are parallel to each other.
+    if (p1.Normal().IsParallelTo(p2.Normal()) == 0) return 0;
+
+    // Fifth, remove degenerate edge sets. A degenerate edge set is defined as two
+    // linear segments having the same two vertices.  (To be sure, we should probably
+    // check curve directions in loops in some fashion...)
+    std::set<int>::iterator e_it;
+    std::set<int> degenerate;
+    for (e_it = data->edges.begin(); e_it != data->edges.end(); e_it++) {
+	if (degenerate.find(*e_it) == degenerate.end()) {
+	    ON_BrepEdge& edge = data->brep->m_E[*e_it];
+	    if (edge.EdgeCurveOf()->IsLinear()) {
+		for (f_it = data->edges.begin(); f_it != data->edges.end(); f_it++) {
+		    ON_BrepEdge& edge2 = data->brep->m_E[*f_it];
+		    if (edge2.EdgeCurveOf()->IsLinear()) {
+			if ((edge.Vertex(0)->Point() == edge2.Vertex(0)->Point() && edge.Vertex(1)->Point() == edge2.Vertex(1)->Point()) ||
+				(edge.Vertex(1)->Point() == edge2.Vertex(0)->Point() && edge.Vertex(0)->Point() == edge2.Vertex(1)->Point()))
+			{
+			    degenerate.insert(*e_it);
+			    degenerate.insert(*f_it);
+			    break;
+			}
+		    }
+		}
+	    }
+	}
+    }
+    active_edges = data->edges;
+    for (e_it = degenerate.begin(); e_it != degenerate.end(); e_it++) {
+	std::cout << "erasing " << *e_it << "\n";
+	active_edges.erase(*e_it);
+    }
+    std::cout << "Active Edge set: ";
+    for (e_it = active_edges.begin(); e_it != active_edges.end(); e_it++) {
+	std::cout << (int)(*e_it);
+	f_it = e_it;
+	f_it++;
+	if (f_it != active_edges.end()) std::cout << ",";
+    }
+    std::cout << "\n";
+
+    // Sixth, check for any remaining linear segments.  For rpc primitives
+    // those are expected, but for a true cylinder the linear segments should
+    // all wash out in the degenerate pass.
+    for (e_it = active_edges.begin(); e_it != active_edges.end(); e_it++) {
+	ON_BrepEdge& edge = data->brep->m_E[*e_it];
+	if (edge.EdgeCurveOf()->IsLinear()) return 0;
+    }
+
+    // Seventh, sort the curved edges into one of two circles.  Again, in more
+    // general cases we might have other curves but a true cylinder should have
+    // all of its arcs on these two circles.  We don't need to check for closed
+    // loops because we are assuming that in the input Brep; any curve except
+    // arc curves that survived the degeneracy test has already resulted in an
+    // earlier rejection.
+    std::set<int> arc_set_1, arc_set_2;
+    ON_Circle set1_c, set2_c;
+    int arc1_circle_set= 0;
+    int arc2_circle_set = 0;
+    for (e_it = active_edges.begin(); e_it != active_edges.end(); e_it++) {
+	ON_BrepEdge& edge = data->brep->m_E[*e_it];
+	ON_Arc arc;
+	if (edge.EdgeCurveOf()->IsArc(NULL, &arc, 0.01)) {
+	    int assigned = 0;
+	    ON_Circle circ(arc.StartPoint(), arc.MidPoint(), arc.EndPoint());
+	    //std::cout << "circ " << circ.Center().x << " " << circ.Center().y << " " << circ.Center().z << "\n";
+	    if (!arc1_circle_set) {
+		arc1_circle_set = 1;
+		set1_c = circ;
+		//std::cout << "center 1 " << set1_c.Center().x << " " << set1_c.Center().y << " " << set1_c.Center().z << "\n";
+	    } else {
+		if (!arc2_circle_set) {
+		    if (!(NEAR_ZERO(circ.Center().DistanceTo(set1_c.Center()), 0.01))){
+			arc2_circle_set = 1;
+			set2_c = circ;
+			//std::cout << "center 2 " << set2_c.Center().x << " " << set2_c.Center().y << " " << set2_c.Center().z << "\n";
+		    }
+		}
+	    }
+	    if (NEAR_ZERO(circ.Center().DistanceTo(set1_c.Center()), 0.01)){
+		arc_set_1.insert(*e_it);
+		assigned = 1;
+	    }
+	    if (arc2_circle_set) {
+		if (NEAR_ZERO(circ.Center().DistanceTo(set2_c.Center()), 0.01)){
+		    arc_set_2.insert(*e_it);
+		    assigned = 1;
+		}
+	    }
+	    if (!assigned) {
+		std::cout << "found extra circle - no go\n";
+		return 0;
+	    }
+	}
+    }
+    ON_3dVector hvect(set2_c.Center() - set1_c.Center());
+
+    std::cout << "rcc: in " << data->key.c_str() << " rcc " << set1_c.Center().x << " " << set1_c.Center().y << " " << set1_c.Center().z << " " << hvect.x << " " << hvect.y << " " << hvect.z << " " << set1_c.Radius() << "\n";
+
+
+    // TODO - check for different radius values between the two circles - for a pure cylinder test should reject, but we can easily handle it with the tgc...
 
     return ret;
 }
@@ -202,6 +334,7 @@ print_objects(std::set<object_data> *object_set)
 	}
 	std::cout << "\n";
 	if (is_arbn(&(*o_it))) std::cout << "Object is arbn\n";
+	if (is_cylinder(&(*o_it))) std::cout << "Object is rcc\n";
 	cnt++;
 	std::cout << "\n";
     }
