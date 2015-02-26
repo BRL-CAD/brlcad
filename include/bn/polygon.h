1a5f1fe7dd7a0f3c7737a3fbd64c77f837892209
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2015-02-26 23:26:11 +0000

    Note CCW requirement in the header docs
    svn:revision:64306
    svn:branch:trunk
    svn:account:starseeker

--- include/bn/polygon.h
+++ include/bn/polygon.h
@@ -125,10 +125,16 @@ BN_EXPORT extern int bn_pt_in_polygon(size_t npts, const point2d_t *pts, const p
  * @brief
  * Triangulate a 2D polygon.
  *
- * This routine generates a triangulation of the input polygon.  There are assumed to be no holes
- * in the triangulation area.  No points are added as part of this triangulation process - the
- * result uses only those points in the original polygon, and hence only the face information is
- * created as output.
+ * This routine generates a triangulation of the input polygon using the method
+ * documented in David Eberly's Triangulation by Ear Clipping, section 2:
+ * http://www.geometrictools.com/Documentation/TriangulationByEarClipping.pdf
+ *
+ * The input polygon cannot have holes and must be provided as an array of
+ * counter-clockwise 2D points.
+ *
+ * No points are added as part of this triangulation process - the result uses
+ * only those points in the original polygon, and hence only the face
+ * information is created as output.
  *
  * @param[out] faces Set of faces in the triangulation, stored as integer indices to the pts.  The first three indices are the vertices of the first face, the second three define the second face, and so forth.
  * @param[out] num_faces Number of faces created
