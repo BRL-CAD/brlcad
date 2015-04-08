afd910d6627ea6d00ded360b00e926f45048dcfa
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2015-04-08 01:58:17 +0000

    More setup for screened poisson testing.  This might actually have some potential when it comes to level-of-detail rendering - a low-point-count fast raytrace that is wrapped in a mesh, stepping up the number of points for more detail...
    svn:revision:64617
    svn:branch:trunk
    svn:account:starseeker

--- src/libgcv/screened_poisson.cpp
+++ src/libgcv/screened_poisson.cpp
@@ -28,6 +28,135 @@ extern "C" {
 #include "raytrace.h"
 #include "gcv_private.h"
 }
+#if 0
+#include "MyTime.h"
+#include "MarchingCubes.h"
+#include "Octree.h"
+#include "SparseMatrix.h"
+#include "PPolynomial.h"
+#include "MemoryUsage.h"
+#include "MultiGridOctreeData.h"
+#endif
+
+#define DEFAULT_FULL_DEPTH 5
+struct gcvpnt {
+    point_t p;
+    vect_t n;
+    int is_set;
+};
+
+struct npoints {
+    struct gcvpnt in;
+    struct gcvpnt out;
+};
+
+struct gcv_point_container {
+    struct npoints *pts;
+    int pnt_cnt;
+    int capacity;
+};
+
+#if 0
+class GCVPointStream : public PoinStream< double >
+{
+    struct gcvpnt *gcvpnts;
+    int curr_ind;
+    public:
+    GCVPointStream( struct gcvpnt *pnts );
+    ~GCVPointStream( void );
+    void reset( void );
+    bool nextPoint( Point3D< Real >& p , Point3D< Real >& n );
+
+}
+
+GCVPointStream< double >::GCVPointStream( struct gcvpnt *pnts )
+{
+    gcvpnts = pnts;
+    curr_int = 0;
+}
+
+GCVPointStream< double >::~GCVPointStream( void )
+{
+}
+
+void GCVPointStream< double >::reset( void ) { curr_ind = 0; }
+bool GCVPointStream< double >::nextPoint( Point3D< double >& p , Point3D< double >& n )
+{
+    while (gcvpnts[curr_ind] && !gcvpnts[curr_ind].is_set) curr_ind++;
+    if (!gcvpnts[curr_ind]) return false;
+    p[0] = gcvpnts[curr_ind].p[0] , p[1] = gcvpnts[curr_ind].p[1] , p[2] = gcvpnts[curr_ind].p[2];
+    n[0] = gcvpnts[curr_ind].n[0] , n[1] = gcvpnts[curr_ind].n[1] , n[2] = gcvpnts[curr_ind].n[2];
+    curr_ind++;
+    return true;
+}
+
+
+class PossRecVertex
+{
+    public:
+	const static int Components=3;
+	static PossRecProperty Properties[];
+
+	Point3D< double > point;
+
+	PossRecVertex( void ) { ; }
+	PossRecVertex( Point3D< double > p ) { point=p; }
+	PossRecVertex operator + ( PossRecVertex p ) const { return PossRecVertex( point+p.point ); }
+	PossRecVertex operator - ( PossRecVertex p ) const { return PossRecVertex( point-p.point ); }
+	template< class _double > PossRecVertex operator * ( _double s ) const { return PossRecVertex( point*s ); }
+	template< class _double > PossRecVertex operator / ( _double s ) const { return PossRecVertex( point/s ); }
+	PossRecVertex& operator += ( PossRecVertex p ) { point += p.point ; return *this; }
+	PossRecVertex& operator -= ( PossRecVertex p ) { point -= p.point ; return *this; }
+	template< class _double > PossRecVertex& operator *= ( _double s ) { point *= s ; return *this; }
+	template< class _double > PossRecVertex& operator /= ( _double s ) { point /= s ; return *this; }
+};
+PossRecVertex< double > operator * ( XForm4x4< _double > xForm , PossRecVertex< double > v ) { return PossRecVertex< double >( xForm * v.point ); }
+PossRecProperty PossRecVertex< double >::Properties[]=
+{
+    { _strdup( "x" ) , PossRecType< double >() , PossRecType< double >() , int( offsetof( PossRecVertex , point.coords[0] ) ) , 0 , 0 , 0 , 0 },
+    { _strdup( "y" ) , PossRecType< double >() , PossRecType< double >() , int( offsetof( PossRecVertex , point.coords[1] ) ) , 0 , 0 , 0 , 0 },
+    { _strdup( "z" ) , PossRecType< double >() , PossRecType< double >() , int( offsetof( PossRecVertex , point.coords[2] ) ) , 0 , 0 , 0 , 0 }
+};
+
+
+int
+PoissonBuild(struct gcv)
+{
+    XForm4x4< double > xForm;
+    xForm = XForm4x4< double >::Identity();
+    Octree< double > tree;
+    tree.threads = bu_avail_cpus();
+    OctNode< TreeNodeData >::SetAllocator( MEMORY_ALLOCATOR_BLOCK_SIZE );
+    double maxMemoryUsage;
+    typename Octree< double >::PointInfo* pointInfo = new typename Octree< double >::PointInfo();
+    typename Octree< double >::NormalInfo* normalInfo = new typename Octree< double >::NormalInfo();
+    std::vector< double >* kernelDensityWeights = new std::vector< double >();
+    std::vector< double >* centerWeights = new std::vector< double >();
+    PointStream< float >* pointStream = new GCVPointStream< float >();
+    int pointCount = tree.template SetTree< float >( pointStream , 0 , 8 , DEFAULT_FULL_DEPTH , 6 , 1.0 , 1.1, 0 , 0 , 4.0 , 1 , *pointInfo , *normalInfo , *kernelDensityWeights , *centerWeights , 1 , xForm , 0 );
+    delete kernelDensityWeights;
+    kernelDensityWeights = NULL;
+    Pointer( double ) constraints = tree.SetLaplacianConstraints( *normalInfo );
+    delete normalInfo;
+    Pointer( double ) solution = tree.SolveSystem( *pointInfo , constraints , 0 , 8 , 8 , 0 , float(1e-3) );
+    delete pointInfo;
+    FreePointer(constraints);
+    CoredFileMeshData< PossRecVertex > mesh;
+    double isoValue = tree.GetIsoValue( solution , *centerWeights );
+    tree.GetMCIsoSurface( NullPointer< double >() , solution , isoValue , mesh , true , 1 , 0 );
+/* mesh to BRL-CAD triangles */
+}
+#endif
+
+struct rt_parallel_container {
+    struct rt_i *rtip;
+    struct resource *resp;
+    struct bu_vls *logs;
+    struct gcv_point_container *npts;
+    int ray_dir;
+    int ncpus;
+    fastf_t delta;
+};
 
 /* add all hit point info to info list */
 HIDDEN int
@@ -36,14 +165,24 @@ add_hit_pnts(struct application *app, struct partition *partH, struct seg *UNUSE
 
     struct partition *pp;
     struct soltab *stp;
-    point_t hit_pnt;
-    vect_t hit_normal;
+    /*point_t hit_pnt;
+    vect_t hit_normal;*/
+    struct gcv_point_container *c = (struct gcv_point_container *)(app->a_uptr);
+    struct npoints *npt;
+
+    if (c->pnt_cnt > c->capacity-1) {
+	c->capacity *= 4;
+	c->pts = (struct npoints *)bu_realloc((char *)c->pts, c->capacity * sizeof(struct npoints), "enlarge results array");
+    }
 
     RT_CK_APPLICATION(app);
-    struct bu_vls *fp = (struct bu_vls *)(app->a_uptr);
+    /*struct bu_vls *fp = (struct bu_vls *)(app->a_uptr);*/
 
     /* add all hit points */
     for (pp = partH->pt_forw; pp != partH; pp = pp->pt_forw) {
+
+	npt = &(c->pts[c->pnt_cnt]);
+
 	/* add "in" hit point info */
 	stp = pp->pt_inseg->seg_stp;
 
@@ -59,17 +198,20 @@ add_hit_pnts(struct application *app, struct partition *partH, struct seg *UNUSE
 	    }
 	}
 
-
-	VJOIN1(hit_pnt, app->a_ray.r_pt, pp->pt_inhit->hit_dist, app->a_ray.r_dir);
-	RT_HIT_NORMAL(hit_normal, pp->pt_inhit, stp, &(app->a_ray), pp->pt_inflip);
-	bu_vls_printf(fp, "%f %f %f %f %f %f\n", hit_pnt[0], hit_pnt[1], hit_pnt[2], hit_normal[0], hit_normal[1], hit_normal[2]);
+        
+	VJOIN1(npt->in.p, app->a_ray.r_pt, pp->pt_inhit->hit_dist, app->a_ray.r_dir);
+	RT_HIT_NORMAL(npt->in.n, pp->pt_inhit, stp, &(app->a_ray), pp->pt_inflip);
+	npt->in.is_set = 1;
+	//bu_vls_printf(fp, "%f %f %f %f %f %f\n", hit_pnt[0], hit_pnt[1], hit_pnt[2], hit_normal[0], hit_normal[1], hit_normal[2]);
 	/* add "out" hit point info (unless half-space) */
 	stp = pp->pt_inseg->seg_stp;
 	if (bu_strncmp("half", stp->st_meth->ft_label, 4) != 0) {
-	    VJOIN1(hit_pnt, app->a_ray.r_pt, pp->pt_outhit->hit_dist, app->a_ray.r_dir);
-	    RT_HIT_NORMAL(hit_normal, pp->pt_outhit, stp, &(app->a_ray), pp->pt_outflip);
-	    bu_vls_printf(fp, "%f %f %f %f %f %f\n", hit_pnt[0], hit_pnt[1], hit_pnt[2], hit_normal[0], hit_normal[1], hit_normal[2]);
+	    VJOIN1(npt->out.p, app->a_ray.r_pt, pp->pt_outhit->hit_dist, app->a_ray.r_dir);
+	    RT_HIT_NORMAL(npt->out.n, pp->pt_outhit, stp, &(app->a_ray), pp->pt_outflip);
+	    npt->out.is_set = 1;
+	    //bu_vls_printf(fp, "%f %f %f %f %f %f\n", hit_pnt[0], hit_pnt[1], hit_pnt[2], hit_normal[0], hit_normal[1], hit_normal[2]);
 	}
+	c->pnt_cnt++;
     }
     return 1;
 }
@@ -82,15 +224,6 @@ ignore_miss(struct application *app)
     //bu_log("miss!\n");
     return 0;
 }
-
-struct rt_parallel_container {
-    struct rt_i *rtip;
-    struct resource *resp;
-    struct bu_vls *logs;
-    int ray_dir;
-    int ncpus;
-    fastf_t delta;
-};
 
 void
 _rt_gen_worker(int cpu, void *ptr)
@@ -110,7 +243,7 @@ _rt_gen_worker(int cpu, void *ptr)
     ap.a_onehit = 0;
     ap.a_logoverlap = rt_silent_logoverlap;
     ap.a_resource = &state->resp[cpu];
-    ap.a_uptr = (void *)(&state->logs[cpu]);
+    ap.a_uptr = (void *)(&state->npts[cpu]);
 
     /* get min and max points of bounding box */
     VMOVE(min, state->rtip->mdl_min);
@@ -158,7 +291,8 @@ _rt_gen_worker(int cpu, void *ptr)
 HIDDEN int
 _rt_generate_points(struct bu_ptbl *hit_pnts, struct db_i *dbip, const char *obj, const char *file, fastf_t delta)
 {
-    int i, dir1;
+    int i, dir1, j;
+    point_t min, max;
     int ncpus = bu_avail_cpus();
     struct rt_parallel_container *state;
     struct bu_vls vlsstr;
@@ -169,18 +303,31 @@ _rt_generate_points(struct bu_ptbl *hit_pnts, struct db_i *dbip, const char *obj
     BU_GET(state, struct rt_parallel_container);
 
     state->rtip = rt_new_rti(dbip);
+    state->delta = delta;
 
     if (rt_gettree(state->rtip, obj) < 0) return -1;
     rt_prep_parallel(state->rtip, 1);
 
-    state->resp = (struct resource *)bu_calloc(2*ncpus, sizeof(struct resource), "resources");
-    for (i = 0; i < 2*ncpus; i++) {
+    state->resp = (struct resource *)bu_calloc(ncpus+1, sizeof(struct resource), "resources");
+    for (i = 0; i < ncpus+1; i++) {
 	rt_init_resource(&(state->resp[i]), i, state->rtip);
     }
 
-    state->logs = (struct bu_vls *)bu_calloc(2*ncpus, sizeof(struct bu_vls), "resources");
-    for (i = 0; i < 2*ncpus; i++) {
-	bu_vls_init(&(state->logs[i]));
+    state->npts = (struct gcv_point_container *)bu_calloc(ncpus+1, sizeof(struct gcv_point_container), "point container arrays");
+    int n[3];
+    VMOVE(min, state->rtip->mdl_min);
+    VMOVE(max, state->rtip->mdl_max);
+    for (i = 0; i < 3; i++) {
+	n[i] = (int)((max[i] - min[i])/state->delta) + 2;
+	if(n[i] < 12) n[i] = 12;
+    }
+    int total = 0;
+    for (i = 0; i < 3; i++) total += n[i]*n[(i+1)%3];
+    if (total > 1e6) total = 1e6;
+    for (i = 0; i < ncpus+1; i++) {
+	state->npts[i].pts = (struct npoints *)bu_calloc(total, sizeof(struct npoints), "npoints arrays");
+	state->npts[i].pnt_cnt = 0;
+	state->npts[i].capacity = total;
     }
 
     for (dir1 = 0; dir1 < 3; dir1++) {
@@ -189,12 +336,19 @@ _rt_generate_points(struct bu_ptbl *hit_pnts, struct db_i *dbip, const char *obj
 	state->delta = delta;
 	bu_parallel(_rt_gen_worker, ncpus, (void *)state);
     }
-
-    FILE *fp = fopen(file, "w");
-    for (i = 0; i < 2*ncpus; i++) {
-	if (bu_vls_strlen(&(state->logs[i])) > 0)
-	    fprintf(fp, "%s", bu_vls_addr(&(state->logs[i])));
+    struct bu_vls log = BU_VLS_INIT_ZERO;
+    for (i = 0; i < ncpus+1; i++) {
+	bu_log("%d, pnt_cnt: %d\n", i, state->npts[i].pnt_cnt);
+	for (j = 0; j < state->npts[i].pnt_cnt; j++) {
+	    struct npoints *npt = &(state->npts[i].pts[j]);
+	    if (npt->in.is_set)
+		bu_vls_printf(&log, "%f %f %f %f %f %f\n", npt->in.p[0], npt->in.p[1], npt->in.p[2], npt->in.n[0], npt->in.n[1], npt->in.n[2]);
+	    if (npt->out.is_set)
+		bu_vls_printf(&log, "%f %f %f %f %f %f\n", npt->out.p[0], npt->out.p[1], npt->out.p[2], npt->out.n[0], npt->out.n[1], npt->out.n[2]);
+	}
     }
+    FILE *fp = fopen(file, "w");
+    fprintf(fp, "%s", bu_vls_addr(&log));
     fclose(fp);
     return 0;
 }
@@ -203,12 +357,14 @@ extern "C" void
 gcv_generate_mesh(int **faces, int *num_faces, point_t **points, int *num_pnts,
 	struct db_i *dbip, const char *obj, const char *file, fastf_t delta)
 {
+    fastf_t d = delta;
     struct bu_ptbl *hit_pnts;
     if (!faces || !num_faces || !points || !num_pnts) return;
     if (!dbip || !obj) return;
     BU_GET(hit_pnts, struct bu_ptbl);
     bu_ptbl_init(hit_pnts, 64, "hit pnts");
-    if (_rt_generate_points(hit_pnts, dbip, obj, file, delta)) {
+    if (NEAR_ZERO(d, SMALL_FASTF)) d = 1;
+    if (_rt_generate_points(hit_pnts, dbip, obj, file, d)) {
 	(*num_faces) = 0;
 	(*num_pnts) = 0;
 	return;
