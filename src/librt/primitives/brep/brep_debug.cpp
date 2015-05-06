f70becd0bfcd776aa38db1795fd103a824003dc2
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2015-05-06 20:44:38 +0000

    switch g_ group to librt for doxygen
    svn:revision:64872
    svn:branch:trunk
    svn:account:starseeker

--- src/librt/primitives/brep/brep_debug.cpp
+++ src/librt/primitives/brep/brep_debug.cpp
@@ -17,7 +17,7 @@
  * License along with this file; see the file named COPYING for more
  * information.
  */
-/** @addtogroup g_ */
+/** @addtogroup librt */
 /** @{ */
 /** @file brep_debug.cpp
  *
