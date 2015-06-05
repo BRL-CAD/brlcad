2c965beef3c788589e5e28238acca525cecef71f
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2015-06-05 23:33:35 +0000

    bu_opt_parse_str isn't really viable with this new setup - need to get rid of it.
    svn:revision:65207
    svn:branch:trunk
    svn:account:starseeker

--- include/bu/opt.h
+++ include/bu/opt.h
@@ -93,7 +93,14 @@ struct bu_opt_desc {
  *
  */
 BU_EXPORT extern int bu_opt_parse(const char ***unused, size_t sizeof_unused, struct bu_vls *msgs, int ac, const char **argv, struct bu_opt_desc *ds);
+
 /**
+ *
+ * TODO - get rid of this - using argv pointers to return unused argv
+ * entries means we can't make an argv array that's invisible to the
+ * calling program - the callee will have to make an argv and use
+ * bu_opt_parse.
+ *
  * Option parse an argv array defined as a space separated string.  This
  * is a convenience function that calls bu_opt_parse and also handles
  * breaking str down into a proper argv array. */
