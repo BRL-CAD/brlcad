ce505f76427c0c62aa9517cd557bfbf42f6ffe06
Nicholas Reed <nreed1@umbc.edu>
2015-06-15 22:56:56 +0000

    fix typo
    svn:revision:65311
    svn:branch:trunk
    svn:account:n_reed

--- include/bu/opt.h
+++ include/bu/opt.h
@@ -116,7 +116,7 @@ typedef int (*bu_opt_arg_process_t)(struct bu_vls *, int argc, const char **argv
 
 
 #define BU_OPT_CHECK_ARGV0(_log_vls, _argc, _argv, _opt_name) \
-if (_argc < 1 || !_argv || !_argv[0] || _argv[0][0] != '\0') { \
+if (_argc < 1 || !_argv || !_argv[0] || _argv[0][0] == '\0') { \
     if (_log_vls) { \
 	bu_vls_printf(_log_vls, "Error: missing required argument: " _opt_name "\n"); \
     } \
