a671774d6f09de2551a24883048b38ed3e2f65d0
Jonathan Engbert <5529404+ejno@users.noreply.github.com>
2015-06-25 16:05:27 +0000

    check range of reduction_level
    svn:revision:65479
    svn:branch:trunk
    svn:account:ejno

--- src/librt/reduce.c
+++ src/librt/reduce.c
@@ -119,6 +119,12 @@ rt_obj_reduce(struct rt_db_internal *dest,
     RT_CK_DB_INTERNAL(dest);
     RT_CK_DB_INTERNAL(internal);
 
+    if (reduction_level > 1.0 && !EQUAL(reduction_level, 1.0))
+	if (reduction_level < 0.0 && !EQUAL(reduction_level, 0.0))
+	    bu_bomb("rt_obj_reduce(): invalid reduction_level");
+
+    reduction_level = FMAX(0.0, FMIN(1.0, reduction_level));
+
     if (internal->idb_major_type != DB5_MAJORTYPE_BRLCAD)
 	switch (internal->idb_minor_type) {
 	    case ID_BOT: {
