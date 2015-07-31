c806fa5a6def9f3895ace347334812624f7699d3
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2015-07-31 22:01:17 +0000

    Tweaks
    svn:revision:65793
    svn:branch:trunk
    svn:account:starseeker

--- src/libanalyze/analyze_private.h
+++ src/libanalyze/analyze_private.h
@@ -1,4 +1,4 @@
-/*               A N A L Y Z E _ P R I V A T E . C
+/*               A N A L Y Z E _ P R I V A T E . H
  * BRL-CAD
  *
  * Copyright (c) 2015 United States Government as represented by
@@ -20,14 +20,16 @@
 /** @file libanalyze/analyze_private.h
  *
  */
+#ifndef ANALYZE_PRIVATE_H
+#define ANALYZE_PRIVATE_H
+
 #include "common.h"
 #include "bu/ptbl.h"
 
 #include "raytrace.h"
 #include "analyze.h"
 
-#ifndef ANALYZE_PRIVATE_H
-#define ANALYZE_PRIVATE_H
+__BEGIN_DECLS
 
 struct minimal_partitions {
     fastf_t *hits;
@@ -40,19 +42,21 @@ struct minimal_partitions {
 };
 
 
-void analyze_gen_worker(int cpu, void *ptr);
+extern void analyze_gen_worker(int cpu, void *ptr);
 
 /* Returns count of rays in rays array */
-int analyze_get_bbox_rays(fastf_t **rays, point_t min, point_t max, struct bn_tol *tol);
+extern int analyze_get_bbox_rays(fastf_t **rays, point_t min, point_t max, struct bn_tol *tol);
 
-int
+extern int
 analyze_get_solid_partitions(struct bu_ptbl *results, struct rt_gen_worker_vars *pstate, const fastf_t *rays, int ray_cnt,
-	        struct db_i *dbip, const char *obj, struct bn_tol *tol, int ncpus, int filter);
-
+	struct db_i *dbip, const char *obj, struct bn_tol *tol, int ncpus, int filter);
 
 typedef struct xray * (*getray_t)(void *ptr);
 typedef int *         (*getflag_t)(void *ptr);
-void analyze_seg_filter(struct bu_ptbl *segs, getray_t gray, getflag_t gflag, struct rt_i *rtip, struct resource *resp, fastf_t tol, int ncpus);
+
+extern void analyze_seg_filter(struct bu_ptbl *segs, getray_t gray, getflag_t gflag, struct rt_i *rtip, struct resource *resp, fastf_t tol, int ncpus);
+
+__END_DECLS
 
 #endif /* ANALYZE_PRIVATE_H */
 
