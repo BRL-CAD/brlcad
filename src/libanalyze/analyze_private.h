4a2a5026bbce472e9ffdd06b344f48a1bbe03913
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2015-08-19 15:47:05 +0000

    Tweak the ray generation - not sure the solid ray check will hold up with this approach, but need a better way to minimize the number of rays we're producing for testing.  Still need to do entry/exit only checking of partitions - that'll help a lot in the thin axis aligned testing cases.
    svn:revision:66039
    svn:branch:trunk
    svn:account:starseeker

--- src/libanalyze/analyze_private.h
+++ src/libanalyze/analyze_private.h
@@ -46,6 +46,7 @@ extern void analyze_gen_worker(int cpu, void *ptr);
 
 /* Returns count of rays in rays array */
 ANALYZE_EXPORT extern int analyze_get_bbox_rays(fastf_t **rays, point_t min, point_t max, struct bn_tol *tol);
+ANALYZE_EXPORT extern int analyze_get_scaled_bbox_rays(fastf_t **rays, point_t min, point_t max, fastf_t ratio);
 
 ANALYZE_EXPORT extern int analyze_get_solid_partitions(struct bu_ptbl *results, struct rt_gen_worker_vars *pstate, const fastf_t *rays, int ray_cnt,
 	struct db_i *dbip, const char *obj, struct bn_tol *tol, int ncpus, int filter);
--- src/libanalyze/util.cpp
+++ src/libanalyze/util.cpp
@@ -195,6 +195,121 @@ memfree:
     return ret;
 }
 
+/* TODO - consolidate with above */
+extern "C" int
+analyze_get_scaled_bbox_rays(fastf_t **rays, point_t min, point_t max, fastf_t ratio)
+{
+    int ret, count;
+    point_t mid;
+    struct rt_pattern_data *xdata = NULL;
+    struct rt_pattern_data *ydata = NULL;
+    struct rt_pattern_data *zdata = NULL;
+
+    if (!rays) return 0;
+
+    if (NEAR_ZERO(DIST_PT_PT_SQ(min, max), VUNITIZE_TOL)) return 0;
+
+    /* We've got the bounding box - set up the grids */
+    VSET(mid, (max[0] + min[0])/2, (max[1] + min[1])/2, (max[2] + min[2])/2);
+
+    BU_GET(xdata, struct rt_pattern_data);
+    VSET(xdata->center_pt, min[0] - 0.1 * fabs(min[0]), mid[1], mid[2]);
+    VSET(xdata->center_dir, 1, 0, 0);
+    xdata->vn = 2;
+    xdata->pn = 2;
+    xdata->n_vec = (vect_t *)bu_calloc(xdata->vn + 1, sizeof(vect_t), "vects array");
+    xdata->n_p = (fastf_t *)bu_calloc(xdata->pn + 1, sizeof(fastf_t), "params array");
+    xdata->n_p[0] = (max[1] - min[1])*ratio;
+    xdata->n_p[1] = (max[2] - min[2])*ratio;
+    VSET(xdata->n_vec[0], 0, max[1] - mid[1], 0);
+    VSET(xdata->n_vec[1], 0, 0, max[2] - mid[2]);
+    ret = rt_pattern(xdata, RT_PATTERN_RECT_ORTHOGRID);
+    bu_free(xdata->n_vec, "x vec inputs");
+    bu_free(xdata->n_p, "x p inputs");
+    if (ret < 0) {
+	ret = 0;
+	goto memfree;
+    }
+
+    BU_GET(ydata, struct rt_pattern_data);
+    VSET(ydata->center_pt, mid[0], min[1] - 0.1 * fabs(min[1]), mid[2]);
+    VSET(ydata->center_dir, 0, 1, 0);
+    ydata->vn = 2;
+    ydata->pn = 2;
+    ydata->n_vec = (vect_t *)bu_calloc(ydata->vn + 1, sizeof(vect_t), "vects array");
+    ydata->n_p = (fastf_t *)bu_calloc(ydata->pn + 1, sizeof(fastf_t), "params array");
+    ydata->n_p[0] = (max[0] - min[0])*ratio;
+    ydata->n_p[1] = (max[2] - min[2])*ratio;
+    VSET(ydata->n_vec[0], max[0] - mid[0], 0, 0);
+    VSET(ydata->n_vec[1], 0, 0, max[2] - mid[2]);
+    ret = rt_pattern(ydata, RT_PATTERN_RECT_ORTHOGRID);
+    bu_free(ydata->n_vec, "y vec inputs");
+    bu_free(ydata->n_p, "y p inputs");
+    if (ret < 0) {
+	ret = 0;
+	goto memfree;
+    }
+
+    BU_GET(zdata, struct rt_pattern_data);
+    VSET(zdata->center_pt, mid[0], mid[1], min[2] - 0.1 * fabs(min[2]));
+    VSET(zdata->center_dir, 0, 0, 1);
+    zdata->vn = 2;
+    zdata->pn = 2;
+    zdata->n_vec = (vect_t *)bu_calloc(zdata->vn + 1, sizeof(vect_t), "vects array");
+    zdata->n_p = (fastf_t *)bu_calloc(zdata->pn + 1, sizeof(fastf_t), "params array");
+    zdata->n_p[0] = (max[0] - min[0])*ratio;
+    zdata->n_p[1] = (max[1] - min[1])*ratio;
+    VSET(zdata->n_vec[0], max[0] - mid[0], 0, 0);
+    VSET(zdata->n_vec[1], 0, max[1] - mid[1], 0);
+    ret = rt_pattern(zdata, RT_PATTERN_RECT_ORTHOGRID);
+    bu_free(zdata->n_vec, "x vec inputs");
+    bu_free(zdata->n_p, "x p inputs");
+    if (ret < 0) {
+	ret = 0;
+	goto memfree;
+    }
+
+    /* Consolidate the grids into a single ray array */
+    {
+	size_t si, sj;
+	(*rays) = (fastf_t *)bu_calloc((xdata->ray_cnt + ydata->ray_cnt + zdata->ray_cnt + 1) * 6, sizeof(fastf_t), "rays");
+	count = 0;
+	for (si = 0; si < xdata->ray_cnt; si++) {
+	    for (sj = 0; sj < 6; sj++) {
+		(*rays)[6*count+sj] = xdata->rays[6*si + sj];
+	    }
+	    count++;
+	}
+	for (si = 0; si < ydata->ray_cnt; si++) {
+	    for (sj = 0; sj < 6; sj++) {
+		(*rays)[6*count+sj] = ydata->rays[6*si + sj];
+	    }
+	    count++;
+	}
+	for (si = 0; si < zdata->ray_cnt; si++) {
+	    for (sj = 0; sj < 6; sj++) {
+		(*rays)[6*count+sj] = zdata->rays[6*si+sj];
+	    }
+	    count++;
+	}
+
+    }
+/*
+    bu_log("ray cnt: %d\n", count);
+*/
+    return count;
+
+memfree:
+    /* Free memory not stored in tables */
+    if (xdata && xdata->rays) bu_free(xdata->rays, "x rays");
+    if (ydata && ydata->rays) bu_free(ydata->rays, "y rays");
+    if (zdata && zdata->rays) bu_free(zdata->rays, "z rays");
+    if (xdata) BU_PUT(xdata, struct rt_pattern_data);
+    if (ydata) BU_PUT(ydata, struct rt_pattern_data);
+    if (zdata) BU_PUT(zdata, struct rt_pattern_data);
+    return ret;
+}
+
 
 struct segfilter_container {
     int ray_dir;
