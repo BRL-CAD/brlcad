2365e09288b6901fe95865203b40ce60fee56861
Jonathan Engbert <5529404+ejno@users.noreply.github.com>
2015-09-07 21:29:23 +0000

    document return value of conversion_fn()
    svn:revision:66162
    svn:branch:trunk
    svn:account:ejno

--- src/libgcv/plugin.h
+++ src/libgcv/plugin.h
@@ -45,7 +45,7 @@ struct gcv_converter {
      * Allocates and initializes data for bu_opt argument parsing.
      * create_opts_fn and free_opts_fn must either both be NULL or both be set.
      *
-     * Must set *options_desc to point to a block of bu_opt_desc's suitable for freeing with bu_free().
+     * Must set *options_desc to point to a block of struct bu_opt_desc's suitable for freeing with bu_free().
      * Must set *options_data to point to converter-specific options suitable for freeing with free_opts_fn().
      */
     void (* const create_opts_fn)(struct bu_opt_desc **options_desc,
@@ -57,6 +57,7 @@ struct gcv_converter {
     /**
      * Perform the conversion operation.
      * options_data must be set if create_opts_fn is set, or NULL otherwise.
+     * Returns 0 on failure, 1 on success.
      */
     int (* const conversion_fn)(const char *path, struct db_i *dbip,
 				const struct gcv_opts *gcv_options, const void *options_data);
