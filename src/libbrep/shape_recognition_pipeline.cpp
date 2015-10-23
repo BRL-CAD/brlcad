323e51bfdd78afe987047e85e0d1244e9aaf1cb7
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2015-10-23 00:36:53 +0000

    Checkpoint latest iteration of tikz debug plotting for cylinders.
    svn:revision:66499
    svn:branch:trunk
    svn:account:starseeker

--- src/libbrep/shape_recognition_pipeline.cpp
+++ src/libbrep/shape_recognition_pipeline.cpp
@@ -1,28 +1,3 @@
-/*  S H A P E _ R E C O G N I T I O N _ P I P E L I N E . C P P
- * BRL-CAD
- *
- * Copyright (c) 2015 United States Government as represented by
- * the U.S. Army Research Laboratory.
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public License
- * version 2.1 as published by the Free Software Foundation.
- *
- * This library is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this file; see the file named COPYING for more
- * information.
- */
-/** @file shape_recognition_pipeline.cpp
- *
- * Brief description
- *
- */
-
 #include "common.h"
 
 #include <set>
