31cd46c64f1a78bf7723bc4c7b02fda0ac0552c0
Thomas Browder <tom.browder@gmail.com>
2015-11-19 22:10:54 +0000

    add a stub of a test file for binary attributes
    svn:revision:66619
    svn:branch:attr-extension-mods
    svn:account:tbrowder2

---
+++ src/librt/tests/test_binary_attribute.c
@@ -0,0 +1,44 @@
+/*                   T E S T _ B U N D L E . C
+ * BRL-CAD
+ *
+ * Copyright (c) 2013-2014 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+
+#include "common.h"
+
+
+#include "bn.h"
+#include "vmath.h"
+#include "raytrace.h"
+
+
+int
+main(int argc, char **argv)
+{
+
+    return 0;
+}
+
+/*
+ * Local Variables:
+ * tab-width: 8
+ * mode: C
+ * indent-tabs-mode: t
+ * c-file-style: "stroustrup"
+ * End:
+ * ex: shiftwidth=4 tabstop=8
+ */
