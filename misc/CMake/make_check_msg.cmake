859d0e6d11d995340a0616f66abbdc2f830f33e6
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2015-11-25 21:04:05 +0000

    take a stab at setting up unit and benchmark test sets, and have make check run the unit, benchmark, and regression tests that are expected to succeed.
    svn:revision:66705
    svn:branch:trunk
    svn:account:starseeker

--- misc/CMake/make_check_msg.cmake
+++ misc/CMake/make_check_msg.cmake
@@ -1,9 +1,10 @@
 message("
 **********************************************************************
-NOTE: The \"check\" a.k.a. \"BRL-CAD Unit Testing\" target runs the
-subset of BRL-CAD's unit tests that are expected to work (i.e. those
-tests which are not currently in development.  All dependencies
-required by the tests are compiled automatically.
+NOTE: The \"check\" a.k.a. \"BRL-CAD Validation Testing\" target runs
+BRL-CAD's unit, system, integration, and regression tests but not the
+BRL-CAD Benchmark.  For a build to be considered viable for production
+use, these tests must pass along with the benchmark target.  All
+dependencies required by the tests are compiled automatically.
 **********************************************************************
 ")
 
