e5edc6d10d0dcf5ee21bb7ce5abcb38b58bc9980
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2015-12-02 22:56:31 +0000

    using this in libged, at least for the moment.
    svn:revision:66718
    svn:branch:trunk
    svn:account:starseeker

--- include/brep.h
+++ include/brep.h
@@ -1385,6 +1385,8 @@ sub_curve(const ON_Curve *in, double a, double b);
 extern BREP_EXPORT ON_Surface *
 sub_surface(const ON_Surface *in, int dir, double a, double b);
 
+extern BREP_EXPORT void
+ON_MinMaxInit(ON_3dPoint *min, ON_3dPoint *max);
 
 extern BREP_EXPORT int
 ON_Curve_PolyLine_Approx(ON_Polyline *polyline, const ON_Curve *curve, double tol);
@@ -1395,7 +1397,7 @@ ON_Curve_PolyLine_Approx(ON_Polyline *polyline, const ON_Curve *curve, double to
  * expose as a feature long term - probably should be a more
  * generic API that supports multiple formats... */
 extern BREP_EXPORT int
-ON_BrepTikz(ON_String &s, const ON_Brep *brep, const char *color, const char *prefix);
+ON_BrepTikz(ON_String &s, const ON_Brep *brep, const char *color, const char *prefix, double tol);
 
 /* Shape recognition functions - HIGHLY EXPERIMENTAL,
  * DO NOT RELY ON - the odds are quite good that this whole
