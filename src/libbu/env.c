893bc909ff4f44112872795e4df265836ad666bb
Christopher Sean Morrison <brlcad@gmail.com>
2015-12-02 23:21:24 +0000

    if we have setenv, just use it
    svn:revision:66721
    svn:branch:trunk
    svn:account:brlcad

--- src/libbu/env.c
+++ src/libbu/env.c
@@ -47,21 +47,25 @@ bu_setenv(const char *name, const char *value, int overwrite)
 {
     int errcode = 0;
 
+#ifdef HAVE_SETENV
+    return setenv(name, value, overwrite);
+#else
+
     if (!overwrite) {
 	size_t envsize = 0;
 
-#ifdef HAVE_GETENV_S
+#  ifdef HAVE_GETENV_S
 	errcode = getenv_s(&envsize, NULL, 0, name);
-#else
+#  else
 	if (getenv(name) == NULL)
 	    errcode = EINVAL;
-#endif
+#  endif
 	if (errcode || envsize)
 	    return errcode;
     }
-#ifdef HAVE__PUTENV_S
+#  ifdef HAVE__PUTENV_S
     return _putenv_s(name, value);
-#else
+#  else
     {
 	size_t maxlen = strlen(name)+strlen(value)+2;
 	char *keyval = (char *)malloc(maxlen);
@@ -70,6 +74,8 @@ bu_setenv(const char *name, const char *value, int overwrite)
 	snprintf(keyval, maxlen, "%s=%s", name, value);
 	return putenv(keyval);
     }
+#  endif
+
 #endif
 }
 
