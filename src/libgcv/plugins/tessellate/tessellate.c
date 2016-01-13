b817b7e196b97d6d819dd8ddc8effc49df908025
Jonathan Engbert <5529404+ejno@users.noreply.github.com>
2016-01-13 21:43:16 +0000

    fix comment
    svn:revision:66861
    svn:branch:trunk
    svn:account:ejno

--- src/libgcv/plugins/tessellate/tessellate.c
+++ src/libgcv/plugins/tessellate/tessellate.c
@@ -17,7 +17,7 @@
  * License along with this file; see the file named COPYING for more
  * information.
  */
-/** @file facetize.c
+/** @file tessellate.c
  *
  * Tessellate the specified objects at the region level.
  *
