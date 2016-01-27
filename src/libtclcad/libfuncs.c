35038cc5beb25a53f0e78d7ef2d20fd65476417b
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2016-01-27 15:43:37 +0000

    Move the Pkg_Init function to tclcad.
    svn:revision:66952
    svn:branch:trunk
    svn:account:starseeker

--- src/libtclcad/libfuncs.c
+++ src/libtclcad/libfuncs.c
@@ -1364,6 +1364,14 @@ Sysv_Init(Tcl_Interp *UNUSED(interp))
     return TCL_OK;
 }
 
+int
+Pkg_Init(Tcl_Interp *interp)
+{
+    if (!interp)
+	return TCL_ERROR;
+    return TCL_OK;
+}
+
 #define RT_FUNC_TCL_CAST(_func) ((int (*)(ClientData clientData, Tcl_Interp *interp, int argc, const char *const *argv))_func)
 
 static int tclcad_rt_shootray(ClientData clientData, Tcl_Interp *interp, int argc, const char *const *argv);
