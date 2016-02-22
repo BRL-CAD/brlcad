ad88e4bf2ac8013d8faf5b07241a5460a39608b7
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2016-02-22 23:50:19 +0000

    Start some experiments with hash.  If I'm not mistaken, bu_hash is not designed to work with anything other than string keys?
    svn:revision:67102
    svn:branch:trunk
    svn:account:starseeker

--- src/libbu/tests/hash.c
+++ src/libbu/tests/hash.c
@@ -27,6 +27,59 @@
 #include <tcl.h>
 #endif
 
+const char *array1[] = {
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7"
+};
+
+const char *array2[] = {
+    "r1",
+    "r2",
+    "r3",
+    "r4",
+    "r5",
+    "r6",
+    "r7"
+};
+
+int
+hash_basic_test() {
+    int i = 0;
+    struct bu_hash_tbl *tbl = bu_hash_tbl_create(1);
+    struct bu_hash_tbl *ntbl = bu_hash_tbl_create(1);
+
+    for (i = 0; i < 7; i++) {
+	int n = 0;
+	struct bu_hash_entry *b, *ne;
+	b = bu_hash_tbl_add(tbl, (const unsigned char *)array1[i], strlen(array1[i]), &n);
+	if (n) {
+	    bu_set_hash_value(b, (unsigned char *)array2[i]);
+	}
+	n = 0;
+	ne = bu_hash_tbl_add(ntbl, (unsigned char *)&i, 1, &n);
+	if (n) {
+	    bu_set_hash_value(ne, (unsigned char *)array2[i]);
+	}
+
+    }
+
+    for (i = 0; i < 7; i++) {
+	struct bu_hash_entry *t, *p;
+	unsigned long idx;
+	t = bu_hash_tbl_find(tbl, (const unsigned char *)array1[i], strlen(array1[i]), &p, &idx);
+	bu_log("Hash lookup %d: %s -> %s,%s\n", i, array1[i], t->key, t->value);
+	t = bu_hash_tbl_find(ntbl, (unsigned char *)&i, 1, &p, &idx);
+	bu_log("Hash lookup %d: %s -> %s,%s\n", i, array1[i], t->key, t->value);
+    }
+
+    return 1;
+}
+
 int
 main(int argc, const char **argv)
 {
@@ -45,7 +98,7 @@ main(int argc, const char **argv)
 
     switch (test_num) {
 	case 0:
-	    /*ret = hash_noop_test();*/
+	    ret = hash_basic_test();
 	case 1:
 	    /*ret = ;*/
 	    break;
