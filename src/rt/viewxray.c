484111aa17ca5b8473c36c62d55e94d35976971b
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2016-04-05 12:23:34 +0000

    Another instance of BU_MIME_IMAGE use with bu_path_component
    svn:revision:67558
    svn:branch:trunk
    svn:account:starseeker

--- src/rt/viewxray.c
+++ src/rt/viewxray.c
@@ -120,8 +120,8 @@ view_init(struct application *UNUSED(ap), char *UNUSED(file), char *UNUSED(obj),
 	if (outputfile) {
 	    struct bu_vls c = BU_VLS_INIT_ZERO;
 	    char buf[BUFSIZ];
-	    if (bu_path_component(&c, outputfile, (bu_path_component_t)BU_MIME_IMAGE)) {
-		if (bu_file_mime_int(bu_vls_addr(&c)) != BU_MIME_IMAGE_UNKNOWN) {
+	    if (bu_path_component(&c, outputfile, BU_PATH_EXT)) {
+		if (bu_file_mime(bu_vls_addr(&c), BU_MIME_IMAGE) != BU_MIME_IMAGE_UNKNOWN) {
 		    bu_strlcpy(buf, outputfile, BUFSIZ);
 		    bu_strlcat(buf, floatfileext, BUFSIZ);
 		    outputfile = floatfilename = bu_strdup(buf);
