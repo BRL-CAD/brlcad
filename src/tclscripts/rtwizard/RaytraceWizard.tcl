ea2150bbb051eb87674a8b7e0374fe0dcb84a050
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2016-04-09 17:43:28 +0000

    Use the -L option in rtwizard to help interactivity.  Expose the threshold as a user configurable option.
    svn:revision:67591
    svn:branch:trunk
    svn:account:starseeker

--- src/tclscripts/rtwizard/RaytraceWizard.tcl
+++ src/tclscripts/rtwizard/RaytraceWizard.tcl
@@ -93,6 +93,9 @@ namespace eval RaytraceWizard {
 	# As of now we haven't drawn anything
 	set ::first_draw 1
 
+	# Initial BoT drawing limit set to 5000
+	set ::bot_threshold 5000
+
 	#
 	# Create the Feedback
 	#
