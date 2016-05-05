0171da00f7293d68270d2af44be7ae3ba2c63592
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2016-05-05 13:36:19 +0000

    whoops - double-asc is in util, don't forget it
    svn:revision:67834
    svn:branch:trunk
    svn:account:starseeker

--- src/util/double-asc.c
+++ src/util/double-asc.c
@@ -157,6 +157,9 @@ main (int argc, char **argv)
     int i;
     int row, col;	/* coords within input stream */
 
+    bu_log("DEPRECATION WARNING:  This command is scheduled for removal.  Please contact the developers if you use this command.\n\n");
+    sleep(1);
+
     if (!get_args(argc, argv)) {
 	print_usage();
     }
