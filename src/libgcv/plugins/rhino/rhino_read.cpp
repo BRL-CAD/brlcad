2b05f56bd6c380b19025d2c37cb82f3b9b00e4f8
Jonathan Engbert <5529404+ejno@users.noreply.github.com>
2016-06-15 21:24:58 +0000

    rename solids after parent combs (in progress)
    svn:revision:68109
    svn:branch:trunk
    svn:account:ejno

--- src/libgcv/plugins/rhino/rhino_read.cpp
+++ src/libgcv/plugins/rhino/rhino_read.cpp
@@ -684,54 +684,6 @@ polish_output(const gcv_opts &gcv_options, db_i &db)
     bu_ptbl found = BU_PTBL_INIT_ZERO;
     AutoPtr<bu_ptbl, db_search_free> autofree_found(&found);
 
-    db_update_nref(&db, &rt_uniresource);
-
-    // rename shapes after their parent combs
-    if (0 > db_search(&found, DB_SEARCH_TREE, "-type shape", 0, NULL, &db))
-	throw std::runtime_error("db_search() failed");
-
-    if (BU_PTBL_LEN(&found)) {
-	db_full_path **entry;
-	const std::string unnamed_pattern = gcv_options.default_name + std::string("*");
-
-	for (BU_PTBL_FOR(entry, (db_full_path **), &found)) {
-	    if (DB_FULL_PATH_CUR_DIR(*entry)->d_nref != 1)
-		continue;
-
-	    for (ssize_t i = (*entry)->fp_len - 2; i >= 0; --i) {
-		bu_attribute_value_set avs;
-		AutoPtr<bu_attribute_value_set, bu_avs_free> autofree_avs(&avs);
-
-		if (db5_get_attributes(&db, &avs, (*entry)->fp_names[i]))
-		    throw std::runtime_error("db5_get_attributes() failed");
-
-		if (!bu_strcmp(bu_avs_get(&avs, "rhino::type"), "ON_Layer")) {
-		    const std::string prefix = (*entry)->fp_names[i]->d_namep;
-		    std::string suffix = ".s";
-		    std::size_t num = 1;
-
-		    while (db_lookup(&db, (prefix + suffix).c_str(), false))
-			suffix = "_" + lexical_cast<std::string>(++num) + ".s";
-
-		    bu_ptbl stack = BU_PTBL_INIT_ZERO;
-		    AutoPtr<bu_ptbl, bu_ptbl_free> autofree_stack(&stack);
-
-		    if (!db_comb_mvall((*entry)->fp_names[(*entry)->fp_len - 2], &db,
-				       DB_FULL_PATH_CUR_DIR(*entry)->d_namep, (prefix + suffix).c_str(), &stack))
-			throw std::runtime_error("db_comb_mvall() failed");
-
-		    if (db_rename(&db, DB_FULL_PATH_CUR_DIR(*entry), (prefix + suffix).c_str()))
-			throw std::runtime_error("db_rename() failed");
-
-		    break;
-		}
-	    }
-	}
-    }
-
-    db_search_free(&found);
-    BU_PTBL_INIT(&found);
-
     if (0 > db_search(&found, DB_SEARCH_RETURN_UNIQ_DP,
 		      ("-attr rhino::type=ON_Layer -or ( ( -attr rhino::type=ON_Layer -or -attr rhino::type=ON_InstanceDefinition -or -attr rhino::type=ON_InstanceRef ) -not -name IDef* -not -name "
 		       + std::string(gcv_options.default_name) + "* )").c_str(), 0, NULL, &db))
@@ -755,6 +707,56 @@ polish_output(const gcv_opts &gcv_options, db_i &db)
 	for (BU_PTBL_FOR(entry, (directory **), &found))
 	    if (db5_update_attribute((*entry)->d_namep, "region", "R", &db))
 		throw std::runtime_error("db5_update_attribute() failed");
+    }
+
+    // rename shapes after their parent layers
+    db_search_free(&found);
+    BU_PTBL_INIT(&found);
+    std::map<const directory *, std::string> renamed;
+
+    if (0 > db_search(&found, DB_SEARCH_TREE, "-type shape", 0, NULL, &db))
+	throw std::runtime_error("db_search() failed");
+
+    if (BU_PTBL_LEN(&found)) {
+	db_full_path **entry;
+
+	for (BU_PTBL_FOR(entry, (db_full_path **), &found)) {
+	    if (!renamed.count(DB_FULL_PATH_CUR_DIR(*entry)))
+		for (ssize_t i = (*entry)->fp_len - 2; i >= 0; --i) {
+		    bu_attribute_value_set avs;
+		    AutoPtr<bu_attribute_value_set, bu_avs_free> autofree_avs(&avs);
+
+		    if (db5_get_attributes(&db, &avs, (*entry)->fp_names[i]))
+			throw std::runtime_error("db5_get_attributes() failed");
+
+		    if (!bu_strcmp(bu_avs_get(&avs, "rhino::type"), "ON_Layer")) {
+			const std::string prefix = (*entry)->fp_names[i]->d_namep;
+			std::string suffix = ".s";
+			std::size_t num = 1;
+
+			while (db_lookup(&db, (prefix + suffix).c_str(), false))
+			    suffix = "_" + lexical_cast<std::string>(++num) + ".s";
+
+			renamed.insert(std::make_pair(DB_FULL_PATH_CUR_DIR(*entry),
+						      DB_FULL_PATH_CUR_DIR(*entry)->d_namep));
+
+			if (db_rename(&db, DB_FULL_PATH_CUR_DIR(*entry), (prefix + suffix).c_str()))
+			    throw std::runtime_error("db_rename() failed");
+
+			break;
+		    }
+		}
+
+	    if ((*entry)->fp_len > 1) {
+		bu_ptbl stack = BU_PTBL_INIT_ZERO;
+		AutoPtr<bu_ptbl, bu_ptbl_free> autofree_stack(&stack);
+
+		if (!db_comb_mvall((*entry)->fp_names[(*entry)->fp_len - 2], &db,
+				   renamed.at(DB_FULL_PATH_CUR_DIR(*entry)).c_str(),
+				   DB_FULL_PATH_CUR_DIR(*entry)->d_namep, &stack))
+		    throw std::runtime_error("db_comb_mvall() failed");
+	    }
+	}
     }
 }
 
