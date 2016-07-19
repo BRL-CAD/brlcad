867d2b87d3d7e0fca42b1de988fcbf5d2d9f32c0
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2016-07-19 02:39:44 +0000

    Strip down re2c directory.
    svn:revision:68367
    svn:branch:trunk
    svn:account:starseeker

--- misc/tools/re2c/test/error1.c
+++
@@ -1,1 +0,0 @@
-re2c: error: line 18, column 5: illegal closure form, use '{n}', '{n,}', '{n,m}' where n and m are numbers
--- misc/tools/re2c/test/error11.c
+++
@@ -1,1 +0,0 @@
-re2c: error: line 2, column 9: Illegal unicode character, non hexadecimal digit found
