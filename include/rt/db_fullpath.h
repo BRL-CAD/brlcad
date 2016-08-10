770e753287c6820ec703fa32fceca817b33a4d98
Jonathan Engbert <5529404+ejno@users.noreply.github.com>
2016-08-10 20:07:57 +0000

    add forward declaration
    svn:revision:68630
    svn:branch:trunk
    svn:account:ejno

--- include/rt/db_fullpath.h
+++ include/rt/db_fullpath.h
@@ -26,6 +26,7 @@
 
 __BEGIN_DECLS
 
+struct bu_vls;    /* forward declaration */
 struct resource;  /* forward declaration */
 struct db_i;      /* forward declaration */
 struct directory; /* forward declaration */
