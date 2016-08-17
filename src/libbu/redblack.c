34db9398bf1e62b28e7c8aeb4b6fc11deb1fbd9b
Christopher Sean Morrison <brlcad@gmail.com>
2016-08-17 02:42:18 +0000

    not strictly valid, but good enough for printing a debug line and should quell the conversion warning.  go to an integer type before going to void.  note that compilers have extreme flexibility as to how they encode function pointers, so the value printed is essentially meaningless (it could be a string)
    svn:revision:68704
    svn:branch:trunk
    svn:account:brlcad

--- src/libbu/redblack.c
+++ src/libbu/redblack.c
@@ -889,7 +889,7 @@ bu_rb_summarize_tree(struct bu_rb_tree *tree)
 	for (i = 0; i < tree->rbt_nm_orders; ++i) {
 	    bu_log("| %3d   |   <%p>   |    %-3.3s    | <%p> | <%p> | <%p> |\n",
 		   i,
-		   (void *)RB_COMPARE_FUNC(tree, i),
+		   (void *)(uintptr_t)RB_COMPARE_FUNC(tree, i),
 		   RB_GET_UNIQUENESS(tree, i) ? "Yes" : "No",
 		   (void *)RB_ROOT(tree, i),
 		   (RB_ROOT(tree, i) == BU_RB_NODE_NULL) ? NULL : (void *)(RB_ROOT(tree, i)->rbn_package)[i],
