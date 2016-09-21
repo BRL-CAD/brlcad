fe3ceb723ffa528fb62f4f76b159fccd44b9f817
Robert Parker <unacknowledged@users.noreply.github.com>
2016-09-21 16:24:26 +0000

    Change default nbflag settings for mouse_ray and pane_mouse_ray to 0.
    svn:revision:68879
    svn:branch:trunk
    svn:account:bob1961

--- src/tclscripts/lib/Ged.tcl
+++ src/tclscripts/lib/Ged.tcl
@@ -764,11 +764,11 @@ package provide cadwidgets::Ged 1.0
 	method init_view_scale {{_button 1}}
 	method init_view_translate {{_button 1}}
 	method center_ray {{_pflag 0}}
-	method mouse_ray {_x _y {_pflag 0} {_prflag 1} {_nbflag 1} {_ohflag 0} {_bdflag 1}}
+	method mouse_ray {_x _y {_pflag 0} {_prflag 1} {_nbflag 0} {_ohflag 0} {_bdflag 1}}
 	method pane_mouse_3dpoint {_pane _x _y {_vflag 1}}
 	method pane_mouse_data_label {_pane _x _y}
 	method pane_mouse_data_pick {_pane _x _y}
-	method pane_mouse_ray {_pane _x _y {_pflag 0} {_prflag 1} {_nbflag 1} {_ohflag 0} {_bdflag 1}}
+	method pane_mouse_ray {_pane _x _y {_pflag 0} {_prflag 1} {_nbflag 0} {_ohflag 0} {_bdflag 1}}
 	method pane {args}
 	method init_shoot_ray {_rayname _prep _no_bool _onehit _bot_dflag _objects}
 	method shoot_ray_who {_start _op _target _prep _no_bool _onehit _bot_dflag}
@@ -5167,7 +5167,7 @@ package provide cadwidgets::Ged 1.0
 }
 
 
-::itcl::body cadwidgets::Ged::mouse_ray {_x _y {_pflag 0} {_prflag 1} {_nbflag 1} {_ohflag 0} {_bdflag 1}} {
+::itcl::body cadwidgets::Ged::mouse_ray {_x _y {_pflag 0} {_prflag 1} {_nbflag 0} {_ohflag 0} {_bdflag 1}} {
     pane_mouse_ray $itk_option(-pane) $_x $_y $_pflag $_prflag $_nbflag $_ohflag $_bdflag
 }
 
@@ -5266,7 +5266,7 @@ package provide cadwidgets::Ged 1.0
     }
 }
 
-::itcl::body cadwidgets::Ged::pane_mouse_ray {_pane _x _y {_pflag 0} {_prflag 1} {_nbflag 1} {_ohflag 0} {_bdflag 1}} {
+::itcl::body cadwidgets::Ged::pane_mouse_ray {_pane _x _y {_pflag 0} {_prflag 1} {_nbflag 0} {_ohflag 0} {_bdflag 1}} {
     set mLastMouseRayPos "$_x $_y"
 
     set view [$mGed screen2view $itk_component($_pane) $_x $_y]
