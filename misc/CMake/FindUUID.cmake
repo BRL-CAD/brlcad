e654b1930b605fe20db210f5f387b6cb04b40ee7
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2016-10-03 15:01:02 +0000

    ws
    svn:revision:68972
    svn:branch:trunk
    svn:account:starseeker

--- misc/CMake/FindUUID.cmake
+++ misc/CMake/FindUUID.cmake
@@ -58,9 +58,9 @@ set(CMAKE_FIND_FRAMEWORK_SAVE ${CMAKE_FIND_FRAMEWORK})
 set(CMAKE_FIND_FRAMEWORK NEVER)
 
 find_path(UUID_INCLUDE_DIR uuid/uuid.h
-/usr/local/include
-/usr/include
-)
+  /usr/local/include
+  /usr/include
+  )
 
 if (UUID_LIBRARY AND UUID_INCLUDE_DIR)
   set(UUID_LIBRARIES ${UUID_LIBRARY})
@@ -71,15 +71,15 @@ endif ()
 
 
 if (UUID_FOUND)
-   if (NOT UUID_FIND_QUIETLY)
-      message(STATUS "Found UUID: ${UUID_LIBRARIES}")
-   endif ()
+  if (NOT UUID_FIND_QUIETLY)
+    message(STATUS "Found UUID: ${UUID_LIBRARIES}")
+  endif ()
 else ()
-   if (UUID_FIND_REQUIRED)
-      message( "library: ${UUID_LIBRARY}" )
-      message( "include: ${UUID_INCLUDE_DIR}" )
-      message(FATAL_ERROR "Could not find UUID library")
-   endif ()
+  if (UUID_FIND_REQUIRED)
+    message( "library: ${UUID_LIBRARY}" )
+    message( "include: ${UUID_INCLUDE_DIR}" )
+    message(FATAL_ERROR "Could not find UUID library")
+  endif ()
 endif ()
 
 # Deprecated declarations.
@@ -94,3 +94,11 @@ set(CMAKE_FIND_FRAMEWORK ${CMAKE_FIND_FRAMEWORK_SAVE})
 
 # We've done the work - be quiet if called again
 set(UUID_FIND_QUIETLY ON CACHE INTERNAL "Quitet FindUUID messages")
+
+# Local Variables:
+# tab-width: 8
+# mode: cmake
+# indent-tabs-mode: t
+# End:
+# ex: shiftwidth=2 tabstop=8
+
