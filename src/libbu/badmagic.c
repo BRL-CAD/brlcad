f3ef0a96c911aa3257b7eb3904bd75330a9fa0fd
Christopher Sean Morrison <brlcad@gmail.com>
2016-10-11 03:39:03 +0000

    this is better since uintptr_t will hold the whole address and size_t will not necessarily, but still doubting the utility/correctness of the alignment check
    svn:revision:69014
    svn:branch:trunk
    svn:account:brlcad

--- src/libbu/badmagic.c
+++ src/libbu/badmagic.c
@@ -35,7 +35,7 @@ bu_badmagic(const uint32_t *ptr, uint32_t magic, const char *str, const char *fi
     if (UNLIKELY(!(ptr))) {
 	snprintf(buf, MAGICBUFSIZ, "ERROR: NULL %s pointer, file %s, line %d\n",
 		 str, file, line);
-    } else if (UNLIKELY(((size_t)(ptr)) & (sizeof(size_t)-1))) {
+    } else if (UNLIKELY((ptr) & (sizeof(uintptr_t)-1))) {
 	snprintf(buf, MAGICBUFSIZ, "ERROR: %p mis-aligned %s pointer, file %s, line %d\n",
 		 (void *)ptr, str, file, line);
     } else if (UNLIKELY(*(ptr) != (uint32_t)(magic))) {
