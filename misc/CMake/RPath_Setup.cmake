4e12f5adbf7e02db1424f4ac113ddcb659b5c4b7
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2016-10-24 01:30:45 +0000

    OSX assigns rpaths individually, not as a common colon separated string.
    svn:revision:69164
    svn:branch:trunk
    svn:account:starseeker

--- misc/CMake/RPath_Setup.cmake
+++ misc/CMake/RPath_Setup.cmake
@@ -47,7 +47,7 @@ set(CMAKE_BUILD_WITH_INSTALL_RPATH FALSE)
 if(NOT APPLE)
   set(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/${LIB_DIR}:\$ORIGIN/../${LIB_DIR}")
 else(NOT APPLE)
-  set(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/${LIB_DIR}:@loader_path/../${LIB_DIR}")
+  set(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/${LIB_DIR};@loader_path/../${LIB_DIR}")
 endif(NOT APPLE)
 # On OSX, we need to set INSTALL_NAME_DIR instead of RPATH for CMake < 3.0
 # http://www.cmake.org/cmake/help/cmake-2-8-docs.html#variable:CMAKE_INSTALL_NAME_DIR
