e2f87421e01ccc939adfb0f3415c854c0d689d5a
Jonathan Engbert <5529404+ejno@users.noreply.github.com>
2017-01-25 22:28:49 +0000

    fix
    svn:revision:69357
    svn:branch:trunk
    svn:account:ejno

--- src/libged/simulate/simulation.cpp
+++ src/libged/simulate/simulation.cpp
@@ -304,8 +304,9 @@ Simulation::Region::get_regions(db_i &db, const std::string &root_path,
 				     attribute_prefix + "type=region");
 
     if (0 > db_search(&found, DB_SEARCH_TREE,
-		      (std::string() + "-depth 1 -attr " + attribute_prefix + "* -not ( -attr " +
-		       attribute_prefix + "type=region -or -type shape )").c_str(), full_path.fp_len,
+		      (std::string() + "-depth >0 -attr " + attribute_prefix + "* -not ( -attr " +
+		       attribute_prefix + "type=region -or ( -type shape -not -below -attr " +
+		       attribute_prefix + "type=region ) )").c_str(), full_path.fp_len,
 		      full_path.fp_names, &db))
 	bu_bomb("db_search() failed");
 
