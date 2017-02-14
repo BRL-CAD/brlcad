243ea7eecebb46f059b5026304233e2ad111a7bd
Christopher Sean Morrison <brlcad@gmail.com>
2017-02-14 08:58:11 +0000

    implement a simple filesystem traversal to get all installed docs.  implemented non-recursively for safety using a simple stack.
    svn:revision:69422
    svn:branch:trunk
    svn:account:brlcad

--- src/libged/help/help.c
+++ src/libged/help/help.c
@@ -26,11 +26,68 @@
 int
 ged_help(struct ged *gedp, int argc, const char *argv[])
 {
+    char *dir;
+    size_t i, count, files_count, dir_count;
+    char **entries = NULL;
+    char **files;
+    char **dirs;
+    size_t max_count = 2048;
+
     /* initialize result */
     bu_vls_trunc(gedp->ged_result_str, 0);
 
     if (argc < 1 || !argv)
 	return -1;
+
+    count = 0;
+    entries = bu_calloc(sizeof(char *), max_count, "allocate entries");
+    dirs = bu_calloc(sizeof(char *), max_count, "allocate dirs");
+
+    dir = bu_strdup(bu_brlcad_dir("doc", 0));
+
+    dirs[0] = dir;
+    dir_count = 1;
+    files_count = 0;
+
+    while (dir_count-- > 0) {
+	char *curdir = dirs[dir_count];
+
+	bu_log("Processing %s\n", curdir);
+	files_count = bu_file_list(curdir, NULL, &files);
+
+	for (i = 0; i < files_count; i++) {
+	    struct bu_vls filepath = BU_VLS_INIT_ZERO;
+
+	    /* skip the canonical dirs */
+	    if (BU_STR_EQUIV(files[i], ".") || BU_STR_EQUIV(files[i], ".."))
+	    {
+		continue;
+	    }
+
+	    bu_vls_sprintf(&filepath, "%s%c%s", curdir, BU_DIR_SEPARATOR, files[i]);
+
+	    if (!bu_file_directory(bu_vls_cstr(&filepath))) {
+		bu_log("FILE: %s\n", bu_vls_cstr(&filepath));
+		if (count > max_count-1) {
+		    max_count *= 2;
+		    entries = bu_realloc(entries, sizeof(char *) * max_count, "increase entries");
+		}
+		entries[count++] = bu_vls_strdup(&filepath);
+	    } else {
+		bu_log(" DIR: %s\n", bu_vls_cstr(&filepath));
+		dirs[dir_count] = bu_strdup(bu_vls_cstr(&filepath));
+		dir_count++;
+	    }
+
+	    bu_vls_free(&filepath);
+	}
+
+	bu_free(curdir, "free curdir");
+    }
+
+    bu_argv_free(files_count, files);
+    bu_argv_free(count, entries);
+    bu_free(dir, "free doc dir");
 
     return 0;
 }
@@ -40,13 +97,16 @@ HIDDEN int
 help_load(struct ged *gedp)
 {
     int ret = 0;
-    extern const struct ged_cmd *help_cmd(void);
-    const struct ged_cmd *cmds = help_cmd();
+    HIDDEN struct bu_list *help_cmd(void);
+    struct bu_list *hp = help_cmd();
     struct ged_cmd *cmd;
 
-    while (BU_LIST_WHILE(cmd, ged_cmd, &(cmds->l))) {
+    for (BU_LIST_FOR(cmd, ged_cmd, hp)) {
 	ret += gedp->add(gedp, cmd);
     }
+
+    BU_PUT(hp, struct bu_list);
+
     return ret;
 }
 
@@ -62,9 +122,11 @@ help_unload(struct ged *gedp)
 }
 
 
-const struct ged_cmd *
+HIDDEN struct bu_list *
 help_cmd(void)
 {
+    struct bu_list *hp;
+
     static struct ged_cmd cmd[6] = {
 	{
 	    BU_LIST_INIT_ZERO, "help",
@@ -96,18 +158,25 @@ help_cmd(void)
 	}
     };
 
-    BU_LIST_INIT_MAGIC(&(cmd[0].l), GED_CMD_MAGIC);
+    BU_GET(hp, struct bu_list);
+    BU_LIST_INIT(hp);
+
+#if 0
+    BU_LIST_MAGIC_SET(&(cmd[0].l), GED_CMD_MAGIC);
     BU_LIST_MAGIC_SET(&(cmd[1].l), GED_CMD_MAGIC);
     BU_LIST_MAGIC_SET(&(cmd[2].l), GED_CMD_MAGIC);
     BU_LIST_MAGIC_SET(&(cmd[3].l), GED_CMD_MAGIC);
     BU_LIST_MAGIC_SET(&(cmd[4].l), GED_CMD_MAGIC);
+    BU_LIST_MAGIC_SET(&(cmd[5].l), GED_CMD_MAGIC);
+#endif
 
-    BU_LIST_PUSH(&(cmd[0].l), &(cmd[1].l));
-    BU_LIST_PUSH(&(cmd[0].l), &(cmd[2].l));
-    BU_LIST_PUSH(&(cmd[0].l), &(cmd[3].l));
-    BU_LIST_PUSH(&(cmd[0].l), &(cmd[4].l));
+    BU_LIST_PUSH(hp, &(cmd[0].l));
+    BU_LIST_PUSH(hp, &(cmd[1].l));
+    BU_LIST_PUSH(hp, &(cmd[2].l));
+    BU_LIST_PUSH(hp, &(cmd[3].l));
+    BU_LIST_PUSH(hp, &(cmd[4].l));
 
-    return &cmd[0];
+    return hp;
 }
 
 
