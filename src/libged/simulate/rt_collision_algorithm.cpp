ba29c2e5b63b905c353ceccb53f4e85d8bac73df
Jonathan Engbert <5529404+ejno@users.noreply.github.com>
2017-03-24 13:08:38 +0000

    fix issue introduced in r69556; the xrays head node should be on the heap because other nodes in the bu_list refer to it
    svn:revision:69567
    svn:branch:trunk
    svn:account:ejno

--- src/libged/simulate/rt_collision_algorithm.cpp
+++ src/libged/simulate/rt_collision_algorithm.cpp
@@ -105,20 +105,23 @@ free_xrays(xrays * const rays)
     RT_CK_RAY(&rays->ray);
 
     bu_list_free(&rays->l);
+    bu_free(rays, "rays");
 }
 
 
-HIDDEN xrays
+HIDDEN xrays *
 generate_ray_grid(const btVector3 &center, const btScalar radius,
 		  const btVector3 &normal, const unsigned grid_radius)
 {
     if (radius < 0.0 || !NEAR_EQUAL(normal.length(), 1.0, RT_DOT_TOL))
 	bu_bomb("invalid argument");
 
-    xrays result;
-    BU_LIST_INIT(&result.l);
+    // the xrays `result` must be on the heap because other nodes in the
+    // `bu_list` point to it
+    xrays * const result = static_cast<xrays *>(bu_malloc(sizeof(xrays), "result"));
+    BU_LIST_INIT(&result->l);
 
-    xray &center_ray = result.ray;
+    xray &center_ray = result->ray;
     center_ray.magic = RT_RAY_MAGIC;
     center_ray.index = 0;
     VMOVE(center_ray.r_pt, center);
@@ -139,12 +142,12 @@ generate_ray_grid(const btVector3 &center, const btScalar radius,
     // NOTE: Bullet's collision tolerance is 4 units (4mm)
     const btScalar grid_size = radius / grid_radius;
 
-    rt_gen_circular_grid(&result, &center_ray, radius, up_vect, grid_size);
+    rt_gen_circular_grid(result, &center_ray, radius, up_vect, grid_size);
     return result;
 }
 
 
-HIDDEN xrays
+HIDDEN xrays *
 get_rays(const btRigidBody &body_a, const btRigidBody &body_b,
 	 const unsigned grid_radius)
 {
@@ -184,11 +187,11 @@ calculate_contact_points(btManifoldResult &result,
     const db_full_path &body_b_path = static_cast<const simulate::RtMotionState *>
 				      (body_b.getMotionState())->get_path();
 
-    xrays rays = get_rays(body_a, body_b, grid_radius);
-    const simulate::AutoPtr<xrays, free_xrays> autofree_rays(&rays);
+    const simulate::AutoPtr<xrays, free_xrays> rays(get_rays(body_a, body_b,
+	    grid_radius));
     const std::vector<std::pair<btVector3, btVector3> > overlaps =
-	rt_instance.get_overlaps(body_a_path, body_b_path, rays);
-    const btVector3 normal_world_on_b(V3ARGS(rays.ray.r_dir));
+	rt_instance.get_overlaps(body_a_path, body_b_path, *rays.ptr);
+    const btVector3 normal_world_on_b(V3ARGS(rays.ptr->ray.r_dir));
 
     for (std::vector<std::pair<btVector3, btVector3> >::const_iterator it =
 	     overlaps.begin(); it != overlaps.end(); ++it) {
@@ -199,7 +202,7 @@ calculate_contact_points(btManifoldResult &result,
     if (debug_draw.getDebugMode() & btIDebugDraw::DBG_DrawFrames) {
 	xrays *entry = NULL;
 
-	for (BU_LIST_FOR(entry, xrays, &rays.l)) {
+	for (BU_LIST_FOR(entry, xrays, &rays.ptr->l)) {
 	    const btVector3 point(V3ARGS(entry->ray.r_pt));
 	    const btVector3 direction(V3ARGS(entry->ray.r_dir));
 
