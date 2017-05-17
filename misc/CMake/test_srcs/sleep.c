e82b7d900b5a7901dec30f00a7ef4024c428e75c
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2017-05-17 21:26:03 +0000

    It's Sleep on Windows...
    svn:revision:69826
    svn:branch:trunk
    svn:account:starseeker

--- misc/CMake/test_srcs/sleep.c
+++ misc/CMake/test_srcs/sleep.c
@@ -1,7 +1,14 @@
 #include "common.h"
 #include "bio.h"
 
+#ifdef _WIN32
+# define sleep Sleep
+# define WAITLEN 1000
+#else
+# define WAITLEN 1
+#endif
+
 int main(void) {
-  sleep(1);
+  sleep(WAITLEN);
   return 0;
 }
