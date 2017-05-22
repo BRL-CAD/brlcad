2a1f17ba573200dea7281f0d094f529b5f7d4097
Christopher Sean Morrison <brlcad@gmail.com>
2017-05-22 14:56:51 +0000

    might as well reduce to the config_win.h define
    svn:revision:69848
    svn:branch:trunk
    svn:account:brlcad

--- misc/CMake/test_srcs/sleep.c
+++ misc/CMake/test_srcs/sleep.c
@@ -1,13 +1,10 @@
 #include "bio.h"
 
 #ifdef _WIN32
-# define sleep Sleep
-# define WAITLEN 1000
-#else
-# define WAITLEN 1
+#  define sleep(_SECONDS) (Sleep(1000 * (_SECONDS)))
 #endif
 
 int main(void) {
-  sleep(WAITLEN);
+  sleep(1);
   return 0;
 }
