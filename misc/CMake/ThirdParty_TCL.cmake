4d2386e998afe0d5941be32c99e94afe384245cc
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2017-07-29 20:01:37 +0000

    Start preparing the ground work for a distcheck introspection that is based entirely on files - no specification of directories.  This will be both to simplify the logic and to increase robustness for the non-svn distcheck case - if we have all files listed explicitly, svn is not strictly needed to do a full source repo verification.  For the moment this will break distcheck, but need to checkpoint before the next stage.
    svn:revision:70026
    svn:branch:trunk
    svn:account:starseeker

--- misc/CMake/ThirdParty_TCL.cmake
+++ misc/CMake/ThirdParty_TCL.cmake
@@ -123,6 +123,15 @@ macro(THIRD_PARTY_TCL_PACKAGE pkgname dir wishcmd depends required_vars NEEDS_LI
   set(${PKGNAME_UPPER}_DISABLE_TEST 0)
   set(${PKGNAME_UPPER}_MET_CONDITION 0)
 
+  # 0. Whether or not we're building the sources, we are tracking the files
+  # that are supposed to be in the directory
+  get_filename_component(DIR_NAME "${dir}" NAME)
+  if(NOT EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/${DIR_NAME}.dist")
+    message(FATAL_ERROR "Third party component ${dir} does not have a dist file at \"${CMAKE_CURRENT_SOURCE_DIR}/${DIR_NAME}.dist\"")
+  endif(NOT EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/${DIR_NAME}.dist")
+  include("${CMAKE_CURRENT_SOURCE_DIR}/${DIR_NAME}.dist")
+  CMAKEFILES_IN_DIR(${DIR_NAME}_ignore_files ${dir})
+
   # 1. First up - If any of the required flags are off, not only is this extension
   # build a no-go but so is the test - BRL-CAD doesn't require it.
   set(DISABLE_STR "")
@@ -321,8 +330,6 @@ macro(THIRD_PARTY_TCL_PACKAGE pkgname dir wishcmd depends required_vars NEEDS_LI
   string(TOLOWER ${pkgname} PKGNAME_LOWER)
 
   # If testing was disabled, we only care what the _BUILD variable says.
-  # Even if DISABLED, we still need the distcheck ignore, so do the following
-  # for ALL situations.
   if(${PKGNAME_UPPER}_DISABLE_TEST)
     if(${CMAKE_PROJECT_NAME}_${PKGNAME_UPPER}_BUILD)
       add_subdirectory(${dir})
@@ -332,10 +339,6 @@ macro(THIRD_PARTY_TCL_PACKAGE pkgname dir wishcmd depends required_vars NEEDS_LI
 	  add_dependencies(${pkgname} ${dep})
 	endif(BRLCAD_BUILD_${DEP_UPPER})
       endforeach(dep ${depends})
-      include("${CMAKE_CURRENT_SOURCE_DIR}/${PKGNAME_LOWER}.dist")
-      CMAKEFILES_IN_DIR(${PKGNAME_LOWER}_ignore_files ${dir})
-    else(${CMAKE_PROJECT_NAME}_${PKGNAME_UPPER}_BUILD)
-      CMAKEFILES(${dir})
     endif(${CMAKE_PROJECT_NAME}_${PKGNAME_UPPER}_BUILD)
   endif(${PKGNAME_UPPER}_DISABLE_TEST)
 
@@ -383,10 +386,6 @@ macro(THIRD_PARTY_TCL_PACKAGE pkgname dir wishcmd depends required_vars NEEDS_LI
 	  add_dependencies(${pkgname} ${dep})
 	endif(BRLCAD_BUILD_${DEP_UPPER})
       endforeach(dep ${depends})
-      include("${CMAKE_CURRENT_SOURCE_DIR}/${PKGNAME_LOWER}.dist")
-      CMAKEFILES_IN_DIR(${PKGNAME_LOWER}_ignore_files ${dir})
-    else(${CMAKE_PROJECT_NAME}_${PKGNAME_UPPER}_BUILD)
-      CMAKEFILES(${dir})
     endif(${CMAKE_PROJECT_NAME}_${PKGNAME_UPPER}_BUILD)
   endif(NOT ${PKGNAME_UPPER}_MET_CONDITION)
 
