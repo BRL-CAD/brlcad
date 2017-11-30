ac1302ed2d3c57b20fc26e4dac564c48885f3033
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2017-11-30 21:51:15 +0000

    We shouldn't need this any more...
    svn:revision:70459
    svn:branch:trunk
    svn:account:starseeker

--- src/libgcv/plugins/obj/wfobj/obj_parser.cpp
+++ src/libgcv/plugins/obj/wfobj/obj_parser.cpp
@@ -204,11 +204,7 @@ extern "C" WFOBJ_EXPORT int obj_parse(const char *filename, obj_parser_t parser,
     int err = 0;
 
     try {
-#ifdef HAVE_UNIQUE_PTR
 	std::unique_ptr<objFileContents> sentry(new objFileContents);
-#else
-	std::auto_ptr<objFileContents> sentry(new objFileContents);
-#endif
 
 	objCombinedState state(p, sentry.get());
 
