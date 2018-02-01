d540842d0372f1bf699ce4a1400456adacd87d69
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2018-02-01 18:33:27 +0000

    Get libbu building with MSVC
    svn:revision:70658
    svn:branch:trunk
    svn:account:starseeker

--- src/libbu/gethostname.c
+++ src/libbu/gethostname.c
@@ -28,7 +28,7 @@
 #include "bu/endian.h"
 
 /* strict c89 doesn't declare gethostname() */
-#if defined(HAVE_GETHOSTNAME) && !defined(HAVE_DECL_GETHOSTNAME) && !defined(__cplusplus)
+#if defined(HAVE_GETHOSTNAME) && !defined(HAVE_DECL_GETHOSTNAME) && !defined(__cplusplus) && !defined(_WINSOCKAPI_)
 extern int gethostname(char *name, size_t len);
 #endif
 
