9b857f1321089b05b9d40d3c7bf8458fab463f84
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2018-02-08 12:55:26 +0000

    we want quick/hard failure if any of the make commands fail.
    svn:revision:70686
    svn:branch:trunk
    svn:account:starseeker

--- misc/clang-static-analyzer-run.sh
+++ misc/clang-static-analyzer-run.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
+
+# We want the script to fail hard and immediately if anything goes wrong, in
+# order to avoid masking failures (e.g. build failures, which will leave
+# empty scan reports but certainly don't constitute successes. See (for example)
+# https://stackoverflow.com/q/821396
+set -e
+set -o pipefail
+
 export CCC_CC=clang
 export CCC_CXX=clang++
 
-# TODO - test if this is a valid way to enable the new Z3 static analyzer support...
+# This appears to be a workable way to enable the new Z3 static analyzer
+# support, but at least as of 2017-12 it greatly slows the testing (by orders
+# of magnitude).  Might be viable (or at least more useful even if it can't
+# quickly complete) if we completely pre-build src/other
 #export CCC_ANALYZER_CONSTRAINTS_MODEL=z3
 
 expected_success=()
@@ -85,7 +96,7 @@ cleantest libdm
 cleantest libfft
 failingtest libtclcad
 cleantest libtermio
-failingtest libremrt
+cleantest libremrt
 cleantest librender
 
 # Executables
