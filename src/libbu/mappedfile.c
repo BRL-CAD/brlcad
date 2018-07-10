5a2cae4107e9f0a2715b5edfa54a27651cae0d63
Christopher Sean Morrison <brlcad@gmail.com>
2018-07-10 23:04:05 +0000

    put the semaphore next to the item demanding protection.
    svn:revision:71143
    svn:branch:trunk
    svn:account:brlcad

--- src/libbu/mappedfile.c
+++ src/libbu/mappedfile.c
@@ -140,18 +140,15 @@ bu_open_mapped_file(const char *name, const char *appl)
 
 	    /* It is safe to reuse mp */
 	    mp->uses++;
-
 	    bu_semaphore_release(BU_SEM_MAPPEDFILE);
-	    if (UNLIKELY(bu_debug&BU_DEBUG_MAPPED_FILE))
-		bu_pr_mapped_file("open_reused", mp);
 
 	    return mp;
 	}
 
 	/* It is safe to reuse mp */
 	mp->uses++;
-
 	bu_semaphore_release(BU_SEM_MAPPEDFILE);
+
 	return mp;
 
     }
