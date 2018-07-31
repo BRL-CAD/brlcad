49bd21c8a2c98f71729bade3d6d0b5b6dd0a2274
Christopher Sean Morrison <brlcad@gmail.com>
2018-07-31 23:03:26 +0000

    document bu_getopt's new :: behavior, clarify the old behavior
    svn:revision:71329
    svn:branch:trunk
    svn:account:brlcad

--- include/bu/getopt.h
+++ include/bu/getopt.h
@@ -64,13 +64,24 @@ BU_EXPORT extern char *bu_optarg;
 /**
  * Get option letter from argument vector.
  *
- * returns the next known option character in ostr.  If bu_getopt()
- * encounters a character not found in ostr or if it detects a missing
+ * @param nargc number of arguments
+ * @param nargv array of argument strings
+ * @param ostr option string
+ *
+ * The 'ostr' option string may contain individual characters (e.g.,
+ * "abc"), characters followed by a colon to indicate a required
+ * argument (e.g., "a:bc", 'a' requires an argument, 'b' and 'c' do
+ * not), and characters followed by two colons to indicate an optional
+ * argument (e.g., "ab::c", 'b' may be followed by an argument, but
+ * none require an argument).
+ *
+ * Returns -1 when the argument list is exhausted, otherwise it
+ * returns the next known option character.  If bu_getopt() encounters
+ * a character not found in ostr or if it detects a required missing
  * option argument, it returns `?' (question mark).  If ostr has a
  * leading `:' then a missing option argument causes `:' to be
  * returned instead of `?'.  In either case, the variable bu_optopt is
- * set to the character that caused the error.  The bu_getopt()
- * function returns -1 when the argument list is exhausted.
+ * set to the character that caused the error.
  */
 BU_EXPORT extern int bu_getopt(int nargc, char * const nargv[], const char *ostr);
 
