53fa1cdeb5c64294945bdd2458e1fb9ed57fdbbd
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2018-08-30 22:15:14 +0000

    Add some headers - based on libbu/thread.cpp will probably need more here for the thread_local bit...
    svn:revision:71635
    svn:branch:trunk
    svn:account:starseeker

--- src/libnmg/alloc.cpp
+++ src/libnmg/alloc.cpp
@@ -1,10 +1,30 @@
+#include "common.h"
 
+#ifdef HAVE_PTHREAD_H
+#  include <pthread.h>
+#endif
+
+#include <cassert>
+
+#include "bu/malloc.h"
+#include "bu/parallel.h"
+
+#if defined(HAVE_THREAD_LOCAL)
 static thread_local struct bu_pool *data = {NULL};
 
+#elif defined(HAVE___THREAD)
 
-/* #define NMG_GETSTRUCT(p, str) p = nmg_alloc(sizeof(p)); */
+static __thread struct bu_pool *data = {NULL};
 
-void *
+#elif defined(HAVE___DECLSPEC_THREAD)
+
+static __declspec(thread) struct bu_pool *data = {NULL};
+
+#endif
+
+/* #define NMG_GETSTRUCT(p, _type) p = (struct _type *)nmg_alloc(sizeof(struct _type)) */
+
+extern "C" void *
 nmg_alloc(size_t size)
 {
   assert(size < 2048);
@@ -19,7 +39,7 @@ nmg_free(void *)
   /* nada */
 }
 
-void
+extern "C" void
 nmg_destroy()
 {
   bu_pool_delete(data);
