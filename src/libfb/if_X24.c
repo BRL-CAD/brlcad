b4b7d250b9109a1542ab390ee42598879836186e
Christopher Sean Morrison <brlcad@gmail.com>
2018-09-19 02:46:00 +0000

    increase the refresh rate to 50ms instead of 60s so we can use it in event polling
    svn:revision:71735
    svn:branch:trunk
    svn:account:brlcad

--- src/libfb/if_X24.c
+++ src/libfb/if_X24.c
@@ -3533,7 +3533,7 @@ fb X24_interface =  {
     0L,			/* page_curpos */
     0L,			/* page_pixels */
     0,			/* debug */
-    60000000,		/* refresh rate - from fbserv, which had 60 seconds as a default (not sure why) */
+    50000,		/* refresh rate */
     {0}, /* u1 */
     {0}, /* u2 */
     {0}, /* u3 */
