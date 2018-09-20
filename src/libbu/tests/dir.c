540d1e6e3c6142ed55251fdaeda4ac66d5019750
Christopher Sean Morrison <brlcad@gmail.com>
2018-09-20 18:48:00 +0000

    stub a unit test for bu_dir()
    svn:revision:71771
    svn:branch:trunk
    svn:account:brlcad

---
+++ src/libbu/tests/dir.c
@@ -0,0 +1,50 @@
+/*                          D I R . C
+ * BRL-CAD
+ *
+ * Copyright (c) 2018 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+
+#include "common.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "bu.h"
+
+
+int
+main(int argc, char *argv[])
+{
+    if (argc != 1) {
+	fprintf(stderr, "Usage: %s\n", argv[0]);
+	return 1;
+    }
+
+    return 0;
+}
+
+
+/*
+ * Local Variables:
+ * mode: C
+ * tab-width: 8
+ * indent-tabs-mode: t
+ * c-file-style: "stroustrup"
+ * End:
+ * ex: shiftwidth=4 tabstop=8
+ */
