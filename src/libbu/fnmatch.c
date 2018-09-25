72856305366f425a210684fa291d24f2eac88740
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2018-09-25 23:44:10 +0000

    This code is basically just the fnmatch code from BSD.
    svn:revision:71836
    svn:branch:trunk
    svn:account:starseeker

--- src/libbu/fnmatch.c
+++ src/libbu/fnmatch.c
@@ -1,23 +1,5 @@
 /*                       F N M A T C H . C
- * BRL-CAD
  *
- * Copyright (c) 1993-2018 United States Government as represented by
- * the U.S. Army Research Laboratory.
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public License
- * version 2.1 as published by the Free Software Foundation.
- *
- * This library is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this file; see the file named COPYING for more
- * information.
- */
-/*
  * Based off of OpenBSD's fnmatch.c v 1.13 2006/03/31
  *
  * Copyright (c) 1989, 1993, 1994
