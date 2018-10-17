9e8f58a7e6f1c7d1af3061ef41b94139b23e6a0c
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2018-10-17 13:15:22 +0000

    Fix header and footer for chull3d.cxx
    svn:revision:71982
    svn:branch:trunk
    svn:account:starseeker

--- src/libbg/chull3d.cxx
+++ src/libbg/chull3d.cxx
@@ -1,3 +1,28 @@
+/*                     C H U L L 3 D . C X X
+ * BRL-CAD
+ *
+ * Copyright (c) 2018 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file chull3d.cxx
+ *
+ * Interface to the Quickhull algorithm code for creating a convex
+ * hull from a set of faces and vertices.
+ */
+
 #include "common.h"
 
 #include <set>
@@ -72,13 +97,12 @@ bg_3d_chull(int **faces, int *num_faces, point_t **vertices, int *num_vertices,
     return 3;
 }
 
-/*
- * Local Variables:
- * mode: C
- * tab-width: 8
- * indent-tabs-mode: t
- * c-file-style: "stroustrup"
- * End:
- * ex: shiftwidth=4 tabstop=8
- */
+// Local Variables:
+// tab-width: 8
+// mode: C++
+// c-basic-offset: 4
+// indent-tabs-mode: t
+// c-file-style: "stroustrup"
+// End:
+// ex: shiftwidth=4 tabstop=8
 
