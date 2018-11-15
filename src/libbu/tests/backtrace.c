ec49765d459f8cd20a30707a86b89739f384151b
Christopher Sean Morrison <brlcad@gmail.com>
2018-11-15 06:52:08 +0000

    add a preliminary unit test for bu_backtrace().  commonly missing from crash reports, needs to be more reliably included.
    svn:revision:72015
    svn:branch:trunk
    svn:account:brlcad

---
+++ src/libbu/tests/backtrace.c
@@ -0,0 +1,88 @@
+/*                   B O O L E A N I Z E . C
+ * BRL-CAD
+ *
+ * Copyright (c) 2018 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+
+#include "common.h"
+
+#include "bu.h"
+
+
+static int
+go_deeper(int depth, FILE *fp)
+{
+    if (depth > 1)
+	return go_deeper(--depth, fp);
+
+    return bu_backtrace(fp);
+}
+
+
+static int
+go_deep(int depth, FILE *fp)
+{
+    return go_deeper(depth, fp);
+}
+
+
+int
+main(int argc, char *argv[])
+{
+    const char *output = NULL;
+    FILE *fp = NULL;
+    int result;
+
+    if (argc != 2) {
+	fprintf(stderr, "Usage: %s {file}\n", argv[0]);
+	return 1;
+    }
+
+    if (argc > 1)
+	output = argv[1];
+    bu_file_delete(output);
+    if (bu_file_exists(output, NULL))
+	bu_exit(1, "ERROR: backtrace output [%s] already exists and couldn't be deleted\n", output);
+    fp = fopen(output, "w");
+    if (!fp)
+	bu_exit(2, "ERROR: Unable to open backtrace output [%s]\n", output);
+
+    bu_debug |= BU_DEBUG_BACKTRACE;
+
+    result = go_deep(3, fp);
+    fclose(fp);
+
+    if (result != 0 || !bu_file_exists(output, NULL)) {
+	printf("bu_backtrace: [FAILED]\n");
+	return 1;
+    }
+
+    printf("bu_backtrace: [PASSED]\n");
+    bu_file_delete(output);
+    return 0;
+}
+
+
+/*
+ * Local Variables:
+ * mode: C
+ * tab-width: 8
+ * indent-tabs-mode: t
+ * c-file-style: "stroustrup"
+ * End:
+ * ex: shiftwidth=4 tabstop=8
+ */
