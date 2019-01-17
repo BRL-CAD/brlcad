b85bf3114d7f79c445abb2e4dbf6135c5063ae2b
Christopher Sean Morrison <brlcad@gmail.com>
2019-01-17 23:33:39 +0000

    adjust missing from the list
    svn:revision:72227
    svn:branch:trunk
    svn:account:brlcad

--- src/mged/wdb_obj.c
+++ src/mged/wdb_obj.c
@@ -10150,6 +10150,7 @@ wdb_importFg4Section_tcl(void *clientData,
 
 
 static struct bu_cmdtab wdb_newcmds[] = {
+    {"adjust",		(int (*)(void *, int, const char **))ged_adjust},
     {"arced",		(int (*)(void *, int, const char **))ged_arced},
     {"cc",		(int (*)(void *, int, const char **))ged_cc},
     {"color",		(int (*)(void *, int, const char **))ged_color},
