9c03e74f6c60f97f43ea85f09301c1f09f7c41ec
Christopher Sean Morrison <brlcad@gmail.com>
2019-03-21 05:20:01 +0000

    reworked to go through the same mapping harness for debugging and maintenance simplicity.  report failure modes distinct from parsed value issues.
    svn:revision:72686
    svn:branch:trunk
    svn:account:brlcad

--- src/libbu/tests/mappedfile.c
+++ src/libbu/tests/mappedfile.c
@@ -26,98 +26,97 @@
 #include "bu.h"
 
 
-struct bu_mapped_worker_info {
-    int file_cnt;
-    int status;
+const char *FILE_PREFIX = "bu_mapped_file_";
+
+struct mapped_file_worker_info {
+    long int file_cnt;
+    long int value;
 };
+
+
+static long int
+mapped_file_read_number(long int i)
+{
+    char filename[MAXPATHLEN] = {0};
+    struct bu_mapped_file *mfp;
+    double num = -1.0;
+    char *endptr;
+
+    snprintf(filename, MAXPATHLEN, "%s%ld", FILE_PREFIX, i);
+
+    mfp = bu_open_mapped_file(filename, NULL);
+    if (!mfp) {
+	bu_log("%s -> [FAIL]  (unable to open mapped file)\n", filename);
+	return -1;
+    }
+
+    num = strtod(mfp->buf, &endptr);
+    bu_close_mapped_file(mfp);
+
+    if (num == 0.0 && endptr == mfp->buf) {
+	bu_log("%s -> [FAIL]  (unable to read number from file)\n", filename);
+	return -2;
+    }
+
+    return (long int)num;
+}
 
 
 static int
 test_mapped_file_serial(long int file_cnt)
 {
-    int status = 1;
-    struct bu_vls fname = BU_VLS_INIT_ZERO;
-    struct bu_mapped_file *mfp;
+    long int num = 0;
+    long int i;
 
-    for (long int i = 0; i < file_cnt; i++) {
-	long int fint = -1;
+    for (i = 0; i < file_cnt; i++) {
 
-	bu_vls_sprintf(&fname, "bu_mapped_file_input_%ld", i);
-	mfp = bu_open_mapped_file(bu_vls_cstr(&fname), NULL);
-	if (!mfp) {
-	    bu_log("ERROR: failed to map file %s\n", bu_vls_cstr(&fname));
+	num = mapped_file_read_number(i);
+	if (num != i) {
+	    bu_log("%s%ld -> %ld [FAIL]  (should be: %ld)\n", FILE_PREFIX, i, num, i);
 	    break;
 	}
-
-	if (mfp) {
-	    fint = strtod(mfp->buf, NULL);
-	}
-	if (fint != i) {
-	    bu_log("%s -> %ld [FAIL]  (should be: %ld)\n", bu_vls_cstr(&fname), fint, i);
-	    status = 1;
-	}
-	bu_close_mapped_file(mfp);
-
-	if (status)
-	    break;
     }
-    if (!status) {
-	bu_log("Serial mapped file test: [PASS]\n");
+    if (num == file_cnt) {
+	bu_log("Mapped file serial test: [PASS]\n");
+	return 0;
     }
 
-    bu_vls_free(&fname);
-
-    return status;
-}
-
-
-static int
-mapped_worker_read(int cpu, long int file_id)
-{
-    int status = 0;
-    long int fint = -1;
-    struct bu_vls fname = BU_VLS_INIT_ZERO;
-    struct bu_mapped_file *mfp;
-
-    bu_vls_sprintf(&fname, "bu_mapped_file_input_%ld", file_id);
-    mfp = bu_open_mapped_file(bu_vls_cstr(&fname), NULL);
-
-    if (mfp) {
-	fint = strtod(mfp->buf, NULL);
-    }
-    if (fint != file_id) {
-	bu_log("%s(%d) -> %ld [FAIL]  (should be: %ld)\n", bu_vls_cstr(&fname), cpu, fint, file_id);
-	status = 1;
-    }
-    bu_close_mapped_file(mfp);
-
-    return status;
+    return 1;
 }
 
 
 static void
-mapped_worker(int cpu, void *data)
+mapped_file_worker(int cpu, void *data)
 {
-    struct bu_mapped_worker_info *info = &(((struct bu_mapped_worker_info *)data)[cpu]);
     int order = cpu % 2;
+    long int i;
+    struct mapped_file_worker_info *info = &(((struct mapped_file_worker_info *)data)[cpu]);
 
-    info->status = 0;
+    info->value = 0;
 
     if (order) {
-	for (int i = 0; i < info->file_cnt; i++) {
-	    info->status = mapped_worker_read(cpu, i);
-	    if (info->status) {
-		return;
+	for (i = 0; i < info->file_cnt; i++) {
+	    info->value = mapped_file_read_number(i);
+	    if (info->value != i) {
+		bu_log("%s%ld -> %ld [FAIL]  (should be: %ld)\n", FILE_PREFIX, i, info->value, i);
+		break;
 	    }
 	}
     } else {
-	for (int i = info->file_cnt - 1; i >= 0; i--) {
-	    info->status = mapped_worker_read(cpu, i);
-	    if (info->status) {
-		return;
+	for (i = info->file_cnt - 1; i >= 0; i--) {
+	    info->value = mapped_file_read_number(i);
+	    if (info->value != i) {
+		bu_log("%s%ld -> %ld [FAIL]  (should be: %ld)\n", FILE_PREFIX, i, info->value, i);
+		break;
 	    }
 
 	}
+    }
+
+    if ((order && info->value == info->file_cnt) || (!order && info->value < 0)) {
+	info->value = 0;
+    } else {
+	info->value = 1;
     }
 }
 
@@ -127,23 +126,27 @@ test_mapped_file_parallel(size_t file_cnt)
 {
     int ret = 0;
     int ncpus = bu_avail_cpus();
-    struct bu_mapped_worker_info *infos = (struct bu_mapped_worker_info *)bu_calloc(ncpus+1, sizeof(struct bu_mapped_worker_info), "parallel data");
+    struct mapped_file_worker_info *infos;
 
-    /* parallel mapped file IO */
+    infos = (struct mapped_file_worker_info *)bu_calloc(ncpus+1, sizeof(struct mapped_file_worker_info), "parallel data");
 
     for (int i = 0; i < ncpus; i++) {
 	infos[i].file_cnt = file_cnt;
-	infos[i].status = 0;
+	infos[i].value = 0;
     }
-    bu_parallel(mapped_worker, ncpus, (void *)infos);
+
+    bu_parallel(mapped_file_worker, ncpus, (void *)infos);
+
     for (int i = 0; i < ncpus; i++) {
-	if (infos[i].status) {
+	if (infos[i].value) {
 	    ret = 1;
+	    break;
 	}
     }
+
     bu_free(infos, "free parallel data");
-    if (!ret) {
-	bu_log("Parallel mapped file test: [PASS]\n");
+    if (ret == 0) {
+	bu_log("Mapped file parallel test: [PASS]\n");
     }
 
     return ret;
@@ -157,7 +160,6 @@ main(int ac, char *av[])
     int ret;
     long int file_cnt = 100;
     long int test_num = 0;
-    const char *file_prefix = "bu_mapped_file_";
     struct bu_vls fname = BU_VLS_INIT_ZERO;
 
     if (ac < 2 || ac > 3) {
@@ -171,7 +173,7 @@ main(int ac, char *av[])
 
     /* Create our set of test input files */
     for (long int i = 0; i < file_cnt; i++) {
-	bu_vls_sprintf(&fname, "%s%ld", file_prefix, i);
+	bu_vls_sprintf(&fname, "%s%ld", FILE_PREFIX, i);
 	fp = fopen(bu_vls_cstr(&fname), "wb");
 	if (!fp)
 	    bu_exit(1, "Unable to create test input file %s\n", bu_vls_cstr(&fname));
@@ -194,7 +196,7 @@ main(int ac, char *av[])
 
     /* Delete our set of test input files */
     for (long int i = 0; i < file_cnt; i++) {
-	bu_vls_sprintf(&fname, "%s%ld", file_prefix, i);
+	bu_vls_sprintf(&fname, "%s%ld", FILE_PREFIX, i);
 	bu_file_delete(bu_vls_cstr(&fname));
     }
 
