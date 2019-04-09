f14d16d2bd9c9c4839530204a4fdac46428b1793
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2019-04-09 14:49:33 +0000

    Consolidate duplicate logic
    svn:revision:72883
    svn:branch:trunk
    svn:account:starseeker

--- src/librt/primitives/brep/brep_cdt.cpp
+++ src/librt/primitives/brep/brep_cdt.cpp
@@ -225,75 +225,58 @@ getEdgePoints(ON_BrepTrim &trim,
     evals += (surface_EvNormal(s, start_2d.x, start_2d.y, start_3d, start_norm)) ? 1 : 0;
     evals += (surface_EvNormal(s, end_2d.x, end_2d.y, end_3d, end_norm)) ? 1 : 0;
 
+    if (evals != 4) {
+	start_2d = trim.PointAt(range.m_t[0]);
+	end_2d = trim.PointAt(range.m_t[1]);
+	start_3d = s->PointAt(start_2d.x,start_2d.y);
+	end_3d = s->PointAt(end_2d.x,end_2d.y);
+	evals = 4;
+    }
+
+    BrepTrimPoint *sbtp = new BrepTrimPoint;
+    sbtp->p3d = new ON_3dPoint(start_3d);
+    sbtp->p2d = start_2d;
+    sbtp->tangent = start_tang;
+    sbtp->normal = start_norm;
+    sbtp->t = range.m_t[0];
+    (*param_points)[sbtp->t] = sbtp;
+
+    BrepTrimPoint *ebtp = new BrepTrimPoint;
+    ebtp->p3d = new ON_3dPoint(end_3d);
+    ebtp->p2d = end_2d;
+    ebtp->tangent = end_tang;
+    ebtp->normal = end_norm;
+    ebtp->t = range.m_t[1];
+    (*param_points)[ebtp->t] = ebtp;
+
+
     if (trim.IsClosed()) {
+
 	double mid_range = (range.m_t[0] + range.m_t[1]) / 2.0;
 	ON_3dPoint mid_2d(0.0, 0.0, 0.0);
 	ON_3dPoint mid_3d(0.0, 0.0, 0.0);
 	ON_3dVector mid_tang(0.0, 0.0, 0.0);
 	ON_3dVector mid_norm(0.0, 0.0, 0.0);
+
 	evals += (trim.EvTangent(mid_range, mid_2d, mid_tang)) ? 1 : 0;
 	evals += (surface_EvNormal(s, mid_2d.x, mid_2d.y, mid_3d, mid_norm)) ? 1 : 0;
-
 	if (evals != 6) {
-	    start_2d = trim.PointAt(range.m_t[0]);
-	    end_2d = trim.PointAt(range.m_t[1]);
-	    start_3d = s->PointAt(start_2d.x,start_2d.y);
-	    end_3d = s->PointAt(end_2d.x,end_2d.y);
 	    mid_2d = trim.PointAt(mid_range);
-	    mid_3d =  s->PointAt(mid_2d.x, mid_2d.y);
+	    mid_3d = s->PointAt(mid_2d.x, mid_2d.y);
 	}
 
-	BrepTrimPoint *sbtp = new BrepTrimPoint;
-	sbtp->p3d = new ON_3dPoint(s->PointAt(trim.PointAt(range.m_t[0]).x, trim.PointAt(range.m_t[0]).y));
-	sbtp->p2d = start_2d;
-	sbtp->tangent = start_tang;
-	sbtp->normal = start_norm;
-	sbtp->t = range.m_t[0];
-	(*param_points)[sbtp->t] = sbtp;
-
 	BrepTrimPoint *mbtp = new BrepTrimPoint;
-	mbtp->p3d = new ON_3dPoint(s->PointAt(trim.PointAt(mid_range).x, trim.PointAt(mid_range).y));
+	mbtp->p3d = new ON_3dPoint(mid_3d);
 	mbtp->p2d = mid_2d;
 	mbtp->tangent = mid_tang;
 	mbtp->normal = mid_norm;
 	mbtp->t = mid_range;
 	(*param_points)[mbtp->t] = mbtp;
 
-	BrepTrimPoint *ebtp = new BrepTrimPoint;
-	ebtp->p3d = new ON_3dPoint(s->PointAt(trim.PointAt(range.m_t[1]).x, trim.PointAt(range.m_t[1]).y));
-	ebtp->p2d = end_2d;
-	ebtp->tangent = end_tang;
-	ebtp->normal = end_norm;
-	ebtp->t = range.m_t[1];
-	(*param_points)[ebtp->t] = ebtp;
-
 	getEdgePoints(trim, sbtp, mbtp, &cdt_tol, *param_points);
 	getEdgePoints(trim, mbtp, ebtp, &cdt_tol, *param_points);
 
     } else {
-
-	if (evals != 4) {
-	    start_2d = trim.PointAt(range.m_t[0]);
-	    end_2d = trim.PointAt(range.m_t[1]);
-	    start_3d = s->PointAt(start_2d.x,start_2d.y);
-	    end_3d = s->PointAt(end_2d.x,end_2d.y);
-	}
-
-	BrepTrimPoint *sbtp = new BrepTrimPoint;
-	sbtp->p3d = new ON_3dPoint(start_3d);
-	sbtp->p2d = start_2d;
-	sbtp->tangent = start_tang;
-	sbtp->normal = start_norm;
-	sbtp->t = range.m_t[0];
-	(*param_points)[sbtp->t] = sbtp;
-
-	BrepTrimPoint *ebtp = new BrepTrimPoint;
-	ebtp->p3d = new ON_3dPoint(end_3d);
-	ebtp->p2d = end_2d;
-	ebtp->tangent = end_tang;
-	ebtp->normal = end_norm;
-	ebtp->t = range.m_t[1];
-	(*param_points)[ebtp->t] = ebtp;
 
 	getEdgePoints(trim, sbtp, ebtp, &cdt_tol, *param_points);
 
