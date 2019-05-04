dbc237c8586c91bee1bff0dc9525f1003ee18bdc
Christopher Sean Morrison <brlcad@gmail.com>
2019-05-04 04:03:36 +0000

    stub an init function for libbu with cautionary instructions.  this is really intended to be only used where the alternative is manually adding an init function to all applications or every API call.  initial use case for this is in order to obtain the initial working directory of applications.
    svn:revision:72994
    svn:branch:trunk
    svn:account:brlcad

---
+++ src/libbu/init.c
@@ -0,0 +1,51 @@
+/*                          I N I T . C
+ * BRL-CAD
+ *
+ * Copyright (c) 2019 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file init.c
+ *
+ * Sole purpose of this file is to capture status and aspects of an
+ * application and the application's environment when first run such
+ * as initial working directory and available memory.  That is, it's
+ * intended to be reflective, not generative or conditional logic.
+ *
+ * Please do NOT add application-specific logic here.
+ *
+ * NOTE: as this init is global to ALL applications before main(),
+ * care must be taken to not write to STDOUT or STDERR or app output
+ * may be corrupted, signals can be raised, or worse.
+ *
+ */
+
+#include "common.h"
+
+
+INITIALIZE(libbu)
+{
+}
+
+
+/*
+ * Local Variables:
+ * tab-width: 8
+ * mode: C
+ * indent-tabs-mode: t
+ * c-file-style: "stroustrup"
+ * End:
+ * ex: shiftwidth=4 tabstop=8
+ */
