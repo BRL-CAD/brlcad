98942d5f4bdd4b62a2a1a4959efd331c862dd0b3
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2019-05-26 16:23:22 +0000

    whoops - rev, not revision_number
    svn:revision:73186
    svn:branch:trunk
    svn:account:starseeker

--- misc/repoconv/svnfexport/common_structs.h
+++ misc/repoconv/svnfexport/common_structs.h
@@ -116,7 +116,7 @@ int verify_repos(long int rev, std::string branch_git)
     if (branch_mappings.find(branch_git) != branch_mappings.end()) {
 	// TODO - some of these mappings are rev number specific...
 	branch_svn = branch_mappings[branch_git];
-	if (rev.revision_number < 36472 && branch_svn == std::string("dmtogl")) {
+	if (rev < 36472 && branch_svn == std::string("dmtogl")) {
 	    branch_svn = std::string("dmtogl-branch");
 	}
     } else {
