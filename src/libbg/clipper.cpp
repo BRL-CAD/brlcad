89ee52a077097056c157dec5160dfd9265473c2e
Christopher Sean Morrison <brlcad@gmail.com>
2019-06-12 22:46:09 +0000

    operator= is used on an Int128, but it was never defined.  provide it.
    svn:revision:73287
    svn:branch:trunk
    svn:account:brlcad

--- src/libbg/clipper.cpp
+++ src/libbg/clipper.cpp
@@ -91,6 +91,13 @@ public:
 
     Int128(const Int128 &val): hi(val.hi), lo(val.lo){}
 
+    Int128& operator = (const Int128 &val)
+    {
+	hi = val.hi;
+	lo = val.lo;
+	return *this;
+    }
+
     long64 operator = (const long64 &val)
     {
 	hi = 0;
