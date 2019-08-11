bad7b97dc83c5dcbd72ccace10e69956a821cf0b
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2019-08-11 23:57:43 +0000

    We want to be aggressive near the singularity, but less so further away.
    svn:revision:73613
    svn:branch:trunk
    svn:account:starseeker

--- src/libbrep/csweep.cpp
+++ src/libbrep/csweep.cpp
@@ -873,6 +873,10 @@ csweep_t::grow_loop(double deg, bool stop_on_contained)
 	    // That's all the triangles from this ring - if we haven't
 	    // terminated yet, pull the next triangle set.
 
+	    if (!stop_on_contained) {
+		angle = 0.25 * angle;
+	    }
+
 	    // We queue these up in a specific order - we want any triangles
 	    // actually using flipped or uncontained vertices to be at the top
 	    // of the stack (i.e. the first ones tried.  Loop through twice -
