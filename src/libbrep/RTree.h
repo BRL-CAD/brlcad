4ea2ad5242e57ed15ee8f8946c0102816e245321
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2019-09-18 17:44:41 +0000

    Existing behavior seems to work, and substituting m_level - 1 does not work.
    svn:revision:73957
    svn:branch:trunk
    svn:account:starseeker

--- src/libbrep/RTree.h
+++ src/libbrep/RTree.h
@@ -1485,7 +1485,6 @@ bool RTREE_QUAL::RemoveRect(Rect* a_rect, const DataType& a_id, Node** a_root)
 
             for (int index = 0; index < tempNode->m_count; ++index)
             {
-                // TODO go over this code. should I use (tempNode->m_level - 1)?
                 InsertRect(tempNode->m_branch[index],
                     a_root,
                     tempNode->m_level);
