8a2d73ff93a9a73b6b07ff725c0084c7756922e6
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2019-11-29 03:54:24 +0000

    Support for the debug command reading and writing hex values directly with the -V option.
    svn:revision:74382
    svn:branch:trunk
    svn:account:starseeker

--- src/libged/debug.c
+++ src/libged/debug.c
@@ -102,7 +102,7 @@ ged_debug(struct ged *gedp, int argc, const char **argv)
     /* initialize result */
     bu_vls_trunc(gedp->ged_result_str, 0);
 
-    if (argc > 3) {
+    if (argc > 4) {
 	return GED_ERROR;
     }
 
@@ -120,7 +120,7 @@ ged_debug(struct ged *gedp, int argc, const char **argv)
     if (argc > 1) {
 
 	if (BU_STR_EQUAL(argv[1], "-h")) {
-	    bu_vls_printf(gedp->ged_result_str, "debug [-h] [-l [lib]] [lib] [flag]\n\n");
+	    bu_vls_printf(gedp->ged_result_str, "debug [-h] [-l [lib]] [-V [lib] [val]] [[lib] [flag]]\n\n");
 	    bu_vls_printf(gedp->ged_result_str, "Available libs:\n");
 	    lcnt = 0;
 	    while (dbg_lib_entries[lcnt]) {
@@ -156,17 +156,86 @@ ged_debug(struct ged *gedp, int argc, const char **argv)
 	    return GED_OK;
 	}
 
+
+	if (BU_STR_EQUAL(argv[1], "-V")) {
+	    lcnt = 0;
+	    if (argc == 2) {
+		/* Bare -v option - print all the hex values */
+		while (dbg_lib_entries[lcnt]) {
+		    unsigned int *cvect = dbg_vars[lcnt];
+		    bu_vls_printf(gedp->ged_result_str, "%*s: 0x%08x\n", max_strlen, dbg_libs[lcnt], *cvect);
+		    lcnt++;
+		}
+		return GED_OK;
+	    }
+	    if (argc == 3) {
+		/* -v option with one arg - either printing a value for one library, or
+		 * setting a hex value for all libraries.  This is usually useful as
+		 * a way to clear all debugging settings by passing 0x0 */
+		if (argv[2][0] == '0' && argv[2][1] == 'x') {
+		    long fvall = strtol(argv[2], NULL, 0);
+		    if (fvall < 0) {
+			bu_vls_printf(gedp->ged_result_str, "unusable hex value %ld\n", fvall);
+			return GED_ERROR;
+		    }
+		    while (dbg_lib_entries[lcnt]) {
+			unsigned int *cvect = dbg_vars[lcnt];
+			*cvect = (unsigned int) fvall;
+			lcnt++;
+		    }
+		    print_all_set_lib_flags(gedp, max_strlen);
+		    return GED_OK;
+		} else {
+		    while (dbg_lib_entries[lcnt]) {
+			if (BU_STR_EQUIV(argv[2], dbg_libs[lcnt])) {
+			    unsigned int *cvect = dbg_vars[lcnt];
+			    bu_vls_printf(gedp->ged_result_str, "0x%08x\n", *cvect);
+			    return GED_OK;
+			}
+			lcnt++;
+		    }
+		    bu_vls_printf(gedp->ged_result_str, "invalid input: %s\n", argv[2]);
+		    return GED_ERROR;
+		}
+	    }
+	    if (argc > 3) {
+		/* Specific value for specific library */
+		while (dbg_lib_entries[lcnt]) {
+		    if (BU_STR_EQUIV(argv[2], dbg_libs[lcnt])) {
+			unsigned int *cvect = dbg_vars[lcnt];
+			/* If we have a hex number, set it */
+			if (argv[3][0] == '0' && argv[3][1] == 'x') {
+			    long fvall = strtol(argv[3], NULL, 0);
+			    if (fvall < 0) {
+				bu_vls_printf(gedp->ged_result_str, "unusable hex value %ld\n", fvall);
+				return GED_ERROR;
+			    }
+			    *cvect = (unsigned int)fvall;
+			} else {
+			    bu_vls_printf(gedp->ged_result_str, "unusable value %s\n", argv[3]);
+			    return GED_ERROR;
+			}
+			bu_vls_printf(gedp->ged_result_str, "0x%08x\n", *cvect);
+			return GED_OK;
+		    }
+		}
+		lcnt++;
+		bu_vls_printf(gedp->ged_result_str, "invalid input: %s\n", argv[2]);
+		return GED_ERROR;
+	    }
+	}
+
 	lcnt = 0;
 	while (dbg_lib_entries[lcnt]) {
 	    if (BU_STR_EQUIV(argv[1], dbg_libs[lcnt])) {
-		/* If we have a specified flag, toggle it.  Else, just print
-		 * what is active */
 		if (argc > 2) {
+		    /* If we have a specified flag, toggle it.  Else, just print
+		     * what is active */
+		    unsigned int *cvect = dbg_vars[lcnt];
 		    int found = 0;
 		    int ecnt = 0;
 		    while (dbg_lib_entries[lcnt][ecnt].val) {
 			if (BU_STR_EQUIV(argv[2], dbg_lib_entries[lcnt][ecnt].key)) {
-			    unsigned int *cvect = dbg_vars[lcnt];
 			    if (*cvect & dbg_lib_entries[lcnt][ecnt].val) {
 				*cvect = *cvect & ~(dbg_lib_entries[lcnt][ecnt].val);
 			    } else {
