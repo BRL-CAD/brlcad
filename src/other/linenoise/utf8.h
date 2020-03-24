f2824e941cbab881890a4114c7cf1e7390410490
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2020-03-24 19:15:38 +0000

    Whoops, was compairing against stale tree.
    svn:revision:75093
    svn:branch:tcltk86
    svn:account:starseeker

--- src/other/linenoise/utf8.h
+++ src/other/linenoise/utf8.h
@@ -8,9 +8,9 @@ extern "C" {
 /**
  * UTF-8 utility functions
  *
- * (c) 2010-2016 Steve Bennett <steveb@workware.net.au>
+ * (c) 2010-2019 Steve Bennett <steveb@workware.net.au>
  *
- * See LICENCE for licence details.
+ * See utf8.c for licence details.
  */
 
 #ifndef USE_UTF8
