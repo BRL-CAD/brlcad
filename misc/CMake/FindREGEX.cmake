f494e2c6f73df5e5ce2b780de8c3126f2146ec5d
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2020-03-31 17:26:45 +0000

    Use plural form for regex lib and include
    svn:revision:75195
    svn:branch:trunk
    svn:account:starseeker

--- misc/CMake/FindREGEX.cmake
+++ misc/CMake/FindREGEX.cmake
@@ -36,8 +36,8 @@
 # - Find regex
 # Find the native REGEX includes and library
 #
-#  REGEX_INCLUDE_DIR   - where to find regex.h, etc.
-#  REGEX_LIBRARY      - List of libraries when using regex.
+#  REGEX_INCLUDE_DIRS   - where to find regex.h, etc.
+#  REGEX_LIBRARIES      - List of libraries when using regex.
 #  REGEX_FOUND          - True if regex found.
 #
 #=============================================================================
@@ -51,7 +51,7 @@ find_path(REGEX_INCLUDE_DIR regex.h)
 set(REGEX_NAMES c regex compat)
 foreach(rname ${REGEX_NAMES})
   if(NOT REGEX_LIBRARY)
-    CHECK_LIBRARY_EXISTS(${rname} regcomp "" HAVE_REGEX_LIB)
+    check_library_exists(${rname} regcomp "" HAVE_REGEX_LIB)
     if(HAVE_REGEX_LIB)
       find_library(REGEX_LIBRARY NAMES ${rname})
     endif(HAVE_REGEX_LIB)
@@ -63,8 +63,10 @@ mark_as_advanced(REGEX_LIBRARY REGEX_INCLUDE_DIR)
 # handle the QUIETLY and REQUIRED arguments and set REGEX_FOUND to TRUE if
 # all listed variables are TRUE
 include(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(REGEX DEFAULT_MSG REGEX_INCLUDE_DIR REGEX_LIBRARY)
+find_package_handle_standard_args(REGEX DEFAULT_MSG REGEX_INCLUDE_DIR REGEX_LIBRARY)
 
+set(REGEX_INCLUDE_DIRS ${REGEX_INCLUDE_DIR})
+set(REGEX_LIBRARIES ${REGEX_LIBRARY})
 
 # Local Variables:
 # tab-width: 8
