26b58bb1e7e9837ce22a39e6290f7206eecc8ad0
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2020-04-10 23:35:48 +0000

    checkpoint
    svn:revision:75352
    svn:branch:trunk
    svn:account:starseeker

--- src/conv/step/step-g/ShellBasedSurfaceModel.h
+++ src/conv/step/step-g/ShellBasedSurfaceModel.h
@@ -1,3 +1,28 @@
+/*        S H E L L B A S E D S U R F A C E M O D E L . H
+ * BRL-CAD
+ *
+ * Copyright (c) 2020 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file ShellBasedSurfaceModel.h
+ *
+ * Brief description
+ *
+ */
+
 #ifndef CONV_STEP_STEP_G_SHELLBASEDSURFACEMODEL_H
 #define CONV_STEP_STEP_G_SHELLBASEDSURFACEMODEL_H
 
