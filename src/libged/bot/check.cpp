ccb33f84d229554ec56f9b479bbf7a337f82b9c2
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2020-05-02 00:06:37 +0000

    The license regression caught me - forgot to put the header back on the check.cpp breakout.
    svn:revision:75664
    svn:branch:trunk
    svn:account:starseeker

--- src/libged/bot/check.cpp
+++ src/libged/bot/check.cpp
@@ -1,3 +1,29 @@
+/*                       C H E C K . C P P
+ * BRL-CAD
+ *
+ * Copyright (c) 2020 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file check.cpp
+ *
+ * Implementation of routines for checking the topological integrity
+ * of a mesh defined with a BoT primitive.
+ *
+ */
+
 #include "common.h"
 
 #include <stdlib.h>
