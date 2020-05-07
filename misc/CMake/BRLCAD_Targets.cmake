85f84c3303fb9f16eb22186f844d3d2be7a68ec3
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2020-05-07 20:51:27 +0000

    Wrap the test in a define check so it's off by default
    svn:revision:75742
    svn:branch:bioh
    svn:account:starseeker

--- misc/CMake/BRLCAD_Targets.cmake
+++ misc/CMake/BRLCAD_Targets.cmake
@@ -537,37 +537,42 @@ endfunction(BRLCAD_ADDLIB libname srcslist libslist)
 
 #-----------------------------------------------------------------------------
 # Compile test for isolated header files, to check that they are self contained
-add_custom_target(check-headers ALL)
-set_target_properties(check-headers PROPERTIES FOLDER "BRL-CAD Header Checks")
+if (DEFINED BRLCAD_HDR_CHECK)
+  add_custom_target(check-headers ALL)
+  set_target_properties(check-headers PROPERTIES FOLDER "BRL-CAD Header Checks")
+endif (DEFINED BRLCAD_HDR_CHECK)
+
 function(BRLCAD_CHECK_HDRS hdrfiles)
 
-  if (MSVC)
-    # Don't know how to do this on Windows
-    return()
-  endif()
+  if (DEFINED BRLCAD_HDR_CHECK)
+    if (MSVC)
+      # Don't know how to do this on Windows
+      return()
+    endif()
 
-  get_filename_component(SRCDIR "${CMAKE_CURRENT_SOURCE_DIR}" NAME)
+    get_filename_component(SRCDIR "${CMAKE_CURRENT_SOURCE_DIR}" NAME)
 
-  foreach(hdrfile ${${hdrfiles}})
+    foreach(hdrfile ${${hdrfiles}})
 
-    string(REPLACE "-" "_" STMPFILE "${SRCDIR}_${hdrfile}")
-    string(REPLACE "." "_" STMPFILE "${STMPFILE}")
-    string(REPLACE "/" "_" STMPFILE "${STMPFILE}")
+      string(REPLACE "-" "_" STMPFILE "${SRCDIR}_${hdrfile}")
+      string(REPLACE "." "_" STMPFILE "${STMPFILE}")
+      string(REPLACE "/" "_" STMPFILE "${STMPFILE}")
 
-    add_custom_command(
-      OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${STMPFILE}.stamp
-      COMMAND ${CMAKE_CXX_COMPILER} -I${CMAKE_SOURCE_DIR}/include -I${CMAKE_SOURCE_DIR}/src/other/openNURBS
-      -I${CMAKE_SOURCE_DIR}/src/other/tcl/generic -fsyntax-only -Wall -Wextra -Wno-deprecated
-      ${CMAKE_CURRENT_SOURCE_DIR}/${hdrfile}
-      COMMAND ${CMAKE_COMMAND} -E touch ${CMAKE_CURRENT_BINARY_DIR}/${hdrfile}.stamp
-      DEPENDS ${hdrfile}
-      )
-    add_custom_target(${STMPFILE}-check DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/${STMPFILE}.stamp)
-    set_target_properties(${STMPFILE}-check PROPERTIES FOLDER "BRL-CAD Header Checks")
+      add_custom_command(
+	OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${STMPFILE}.stamp
+	COMMAND ${CMAKE_CXX_COMPILER} -I${CMAKE_SOURCE_DIR}/include -I${CMAKE_SOURCE_DIR}/src/other/openNURBS
+	-I${CMAKE_SOURCE_DIR}/src/other/tcl/generic -fsyntax-only -Wall -Wextra -Wno-deprecated
+	${CMAKE_CURRENT_SOURCE_DIR}/${hdrfile}
+	COMMAND ${CMAKE_COMMAND} -E touch ${CMAKE_CURRENT_BINARY_DIR}/${hdrfile}.stamp
+	DEPENDS ${hdrfile}
+	)
+      add_custom_target(${STMPFILE}-check DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/${STMPFILE}.stamp)
+      set_target_properties(${STMPFILE}-check PROPERTIES FOLDER "BRL-CAD Header Checks")
 
-    add_dependencies(check-headers ${STMPFILE}-check)
+      add_dependencies(check-headers ${STMPFILE}-check)
 
-  endforeach(hdrfile ${${hdrfiles}})
+    endforeach(hdrfile ${${hdrfiles}})
+  endif (DEFINED BRLCAD_HDR_CHECK)
 
 endfunction()
 
