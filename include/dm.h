d1de6dcb1de9684737f706dc94a9914e926e3c55
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2020-06-04 15:25:25 +0000

    screengrab and overlay have the new icv logic - remove the icv2fb and fb2icv commands.  Restore png2fb for the moment to keep full compatibility for older codes, but we should be well positioned to begin phasing out fb2pix, pix2fb and png2fb in favor of more generic options.  In the meantime screengrab and overlay have new functionality.
    svn:revision:76021
    svn:branch:dm-fb-merge
    svn:account:starseeker

--- include/dm.h
+++ include/dm.h
@@ -411,6 +411,7 @@ DM_EXPORT extern int fb_write_fp(struct fb *ifp, FILE *fp, int req_width, int re
 DM_EXPORT extern int fb_read_fd(struct fb *ifp, int fd,  int file_width, int file_height, int file_xoff, int file_yoff, int scr_width, int scr_height, int scr_xoff, int scr_yoff, int fileinput, char *file_name, int one_line_only, int multiple_lines, int autosize, int inverse, int clear, int zoom, struct bu_vls *result);
 DM_EXPORT extern int fb_read_icv(struct fb *ifp, icv_image_t *img, int file_xoff, int file_yoff, int file_maxwidth, int file_maxheight, int scr_xoff, int scr_yoff, int clear, int zoom, int inverse, int one_line_only, int multiple_lines, struct bu_vls *result);
 DM_EXPORT extern icv_image_t *fb_write_icv(struct fb *ifp, int scr_xoff, int scr_yoff, int width, int height);
+DM_EXPORT extern int fb_read_png(struct fb *ifp, FILE *fp_in, int file_xoff, int file_yoff, int scr_xoff, int scr_yoff, int clear, int zoom, int inverse, int one_line_only, int multiple_lines, int verbose, int header_only, double def_screen_gamma, struct bu_vls *result);
 
 DM_EXPORT extern void fb_set_interface(struct fb *ifp, const char *interface_type);
 DM_EXPORT extern void fb_set_name(struct fb *ifp, const char *name);
