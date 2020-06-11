57654c11b39ba759e654ca2f2f3acb65438ca66b
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2020-06-11 17:33:13 +0000

    consolidate
    svn:revision:76113
    svn:branch:bioh
    svn:account:starseeker

--- src/burst2/paint.cpp
+++ src/burst2/paint.cpp
@@ -25,9 +25,14 @@
 #include "common.h"
 
 #include <assert.h>
+#include <memory.h>
+#include <signal.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "vmath.h"
+#include "bu/log.h"
+#include "bu/str.h"
 #include "raytrace.h"
 #include "fb.h"
 
@@ -167,6 +172,75 @@ paintSpallFb(struct application *ap)
 	bu_log("Write failed to pixel <%d, %d> from cell <%d, %d>.\n", x, y, ap->a_x, ap->a_y);
     return;
 }
+
+/*
+  Must be called after gridInit() so that gridsz is setup.
+*/
+int
+openFbDevice(struct burst_state *s, struct bu_vls *fbdev)
+{
+    bu_log("Opening frame buffer");
+    if (s->zoom < 1) {
+	bu_log("Device is too small to display image.");
+	return 1;
+    }
+    if (((s->fbiop != FB_NULL && fb_getwidth(s->fbiop) != s->devwid) || s->pixgrid == NULL)
+	&& (s->pixgrid = (unsigned char *) calloc(s->devwid*3, sizeof(unsigned char))) == (unsigned char *) NULL) {
+	bu_exit(EXIT_FAILURE, "Memory allocation of %ld bytes failed.", sizeof(unsigned char)*s->devwid);
+    }
+    (void) memset((char *) s->pixgrid, 0, sizeof(unsigned char)*s->devwid*3);
+    if (s->fbiop != FB_NULL && fb_close(s->fbiop) == -1) {
+	bu_exit(EXIT_FAILURE, "failed to close fb");
+    }
+    s->fbiop = fb_open(bu_vls_cstr(fbdev), s->devwid, s->devhgt);
+    if (s->fbiop == NULL) {
+	return 0;
+    } else if (fb_clear(s->fbiop, s->pixblack) == -1
+	       || (fb_zoom(s->fbiop, 1, 1) == -1)
+	       || (fb_window(s->fbiop, s->devwid/2, s->devhgt/2) == -1)
+	) {
+	return 1;
+    }
+    return 0;
+}
+
+int
+imageInit(struct burst_state *s)
+{
+    int needopen = 0;
+    static char lastfbfile[LNBUFSZ]={0}; /* last fbfile */
+    s->devwid = 512;
+    s->devhgt = 512;
+    if (!bu_vls_strlen(&s->fbfile))
+	return 1;
+    s->zoom = s->devwid / s->gridsz - 1;
+    while (s->zoom < 3  && s->devwid < MAXDEVWID) {
+	s->devwid *= 2;
+	s->devhgt *= 2;
+	s->zoom = s->devwid / s->gridsz;
+    }
+    if (s->zoom * s->gridsz == s->devwid)
+	s->zoom--;
+    V_MAX(s->zoom, 1);
+
+    /* Determine whether it is necessary to open fbfile. */
+    if (s->fbiop == FB_NULL || fb_getwidth(s->fbiop) != s->devwid)
+	needopen = 1; /* not currently open or size changed */
+    else
+	if (lastfbfile[0] != '\0' && !BU_STR_EQUAL(bu_vls_cstr(&s->fbfile), lastfbfile))
+	    needopen = 1; /* name changed */
+    bu_strlcpy(lastfbfile, bu_vls_cstr(&s->fbfile), LNBUFSZ);
+
+    if (needopen) {
+	if (!openFbDevice(s, &s->fbfile))
+	    return 0;
+	paintGridFb(s);
+    } else
+	if (!(s->firemode & FM_SHOT) || (s->firemode & FM_FILE))
+	    paintGridFb(s);
+    return 1;
+}
+
 
 
 /*
