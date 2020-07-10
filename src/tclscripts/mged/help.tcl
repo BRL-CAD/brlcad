db15aef1e13bddbe3f0b5f027f2c27c484d51bff
Christopher Sean Morrison <brlcad@gmail.com>
2020-07-10 07:10:00 +0000

    adjust the wording and note reid's optional increment argument
    svn:revision:76303
    svn:branch:trunk
    svn:account:brlcad

--- src/tclscripts/mged/help.tcl
+++ src/tclscripts/mged/help.tcl
@@ -155,7 +155,7 @@ Example: e_id 1000 2000 3000-4000}}
 set mged_help_data(eac)		{{air_code(s)}	{display all regions with given air code(s) (i.e., numbers >=0)}}
 set mged_help_data(echo)	{{[text]}	{echo arguments back}}
 set mged_help_data(edcodes)	{{[-n] object(s)}	{edit region ident codes.   only reports matches without renaming with the -n option.}}
-set mged_help_data(edcomb)	{{combname regionflag regionID airID los% [materialID]}	{edit combination record info}}
+set mged_help_data(edcomb)	{{combname [regionflag regionID airID los% materialID]}	{edit combination record info}}
 set mged_help_data(edgedir)	{{[delta_x delta_y delta_z]|[rot fb]}	{define direction of ARB edge being moved}}
 set mged_help_data(edmater)	{{comb(s)}	{edit combination materials}}
 set mged_help_data(erase)	{{<objects>}	{remove objects from the screen}}
@@ -187,7 +187,7 @@ set mged_help_data(igraph)      {{}    {interactive graph for the objects of the
 set mged_help_data(ill)		{{name}	{illuminate object}}
 set mged_help_data(in)		{{[-f] [-s] parameters...}	{keyboard entry of solids.  -f for no drawing, -s to enter solid edit}}
 set mged_help_data(inside)	{{[outside_solid new_inside_solid thicknesses]}	{finds inside solid per specified thicknesses. Note that in an edit mode the edited solid is used for the outside_solid and should not appear on the command line }}
-set mged_help_data(item)	{{region regionID [airID [materialID [los%]]]}	{set region ident codes}}
+set mged_help_data(item)	{{region_name regionID [airID [materialID [los%]]]}	{set region ident codes}}
 set mged_help_data(joint)	{{command [options]}	{articulation/animation commands}}
 set mged_help_data(journal)	{{[-d] fileName}	{record all commands and timings to journal}}
 set mged_help_data(keep)	$helplib_data(wdb_keep)
@@ -260,7 +260,7 @@ set mged_help_data(refresh)	{{}	{send new control list}}
 set mged_help_data(regdebug)	{{[number]}	{toggle display manager debugging or set debug level}}
 set mged_help_data(regdef)	{{regionID [airID [los% [materialID]]]}	{change next region default codes}}
 set mged_help_data(regions)	{{file object(s)}	{make ascii summary of regions}}
-set mged_help_data(reid)	{{comb regionID}	{incrementally assign region IDs to all regions under a given combination starting with the given region ID number}}
+set mged_help_data(reid)	{{[-n num] comb regionID}	{sequentially assign IDs to all regions under a specified combination starting from regionID in increments of 'num'}}
 set mged_help_data(release)	{{[name]}	{release display processor}}
 set mged_help_data(relos)	{{comb los%}	{assign same line-of-sight thickness percentage to all regions under a given combination}}
 set mged_help_data(remat)	{{comb materialID}	{assign the same material ID number to all regions under a given combination}}
