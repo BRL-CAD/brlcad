a45eddf9f9782b9babeacb7da9165edb0975a02e
Christopher Sean Morrison <brlcad@gmail.com>
2020-07-25 04:50:43 +0000

    bw-fb needs to specify binary for windows
    svn:revision:76479
    svn:branch:trunk
    svn:account:brlcad

--- src/fb/bw-fb.c
+++ src/fb/bw-fb.c
@@ -159,11 +159,12 @@ get_args(int argc, char **argv)
 	    return 0;
 	file_name = "-";
 	infd = 0;
+	setmode(fileno(stdin), O_RDONLY|O_BINARY);
     } else {
 	char *ifname;
 	file_name = argv[bu_optind];
 	ifname = bu_file_realpath(file_name, NULL);
-	if ((infd = open(ifname, 0)) < 0) {
+	if ((infd = open(ifname, O_RDONLY|O_BINARY)) < 0) {
 	    fprintf(stderr,
 		    "bw-fb: cannot open \"%s (canonical %s)\" for reading\n",
 		    file_name, ifname);
