61ed137aa12df94c48b04fa2cedba1370d81fc2e
Christopher Sean Morrison <brlcad@gmail.com>
2020-07-28 08:10:45 +0000

    port dmod and imod
    svn:revision:76552
    svn:branch:trunk
    svn:account:brlcad

--- src/sig/imod.c
+++ src/sig/imod.c
@@ -115,7 +115,7 @@ get_args(int argc, char *argv[])
 	char *file_name = NULL;
 	file_name = argv[bu_optind];
 	ifname = bu_file_realpath(file_name, NULL);
-	if (freopen(ifname, "r", stdin) == NULL) {
+	if (freopen(ifname, "rb", stdin) == NULL) {
 	    fprintf(stderr,
 		    "%s: cannot open \"%s(canonical %s)\" for reading\n",
 		    progname, file_name, ifname);
@@ -181,6 +181,9 @@ main(int argc, char *argv[])
     if (!get_args(argc, argv) || isatty(fileno(stdin)) || isatty(fileno(stdout))) {
 	bu_exit(1, "%s", usage);
     }
+
+    setmode(fileno(stdin), O_BINARY);
+    setmode(fileno(stdout), O_BINARY);
 
     mk_trans_tbl();
 
