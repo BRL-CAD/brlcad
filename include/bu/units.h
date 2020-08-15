19ed8fadd8234486e9e7c7e6cba18077faaf5114
Christopher Sean Morrison <brlcad@gmail.com>
2020-08-15 12:27:33 +0000

    bu_units_string is itself undocumented and appears to be used via the undocumented 'units -t' switch for getting a listing of length units.  needs cleanup
    svn:revision:76776
    svn:branch:trunk
    svn:account:brlcad

--- include/bu/units.h
+++ include/bu/units.h
@@ -43,13 +43,14 @@ __BEGIN_DECLS
  * (e.g., "cm^3", "cu yards"), or mass (e.g., "kg", "grain", or "oz")
  * return the multiplier (aka conversion factor) that converts the
  * unit into the default (millimeters for length, mm^3 for volume, and
- * grams for mass.)  Values may be optionally specified with the unit
+ * grams for mass.) Values may be optionally specified with the unit
  * (e.g., "5ft") to get the conversion factor for a particular
  * quantity.
  *
  * Returns 0.0 on error and >0.0 on success
  */
 BU_EXPORT extern double bu_units_conversion(const char *str);
+
 
 /**
  * Given a conversion factor to mm, search the table to find what unit
@@ -62,7 +63,11 @@ BU_EXPORT extern double bu_units_conversion(const char *str);
  * NULL	No known unit matches this conversion factor.
  */
 BU_EXPORT extern const char *bu_units_string(const double mm);
+
+
+/** undocumented */
 BU_EXPORT extern struct bu_vls *bu_units_strings_vls(void);
+
 
 /**
  * Given a conversion factor to mm, search the table to find the
@@ -74,6 +79,7 @@ BU_EXPORT extern struct bu_vls *bu_units_strings_vls(void);
  */
 BU_EXPORT extern const char *bu_nearest_units_string(const double mm);
 
+
 /**
  * Given a string of the form "25cm" or "5.2ft" returns the
  * corresponding distance in mm.
@@ -83,6 +89,7 @@ BU_EXPORT extern const char *bu_nearest_units_string(const double mm);
  * >0 on success
  */
 BU_EXPORT extern double bu_mm_value(const char *s);
+
 
 /**
  * Used primarily as a hooked function for bu_structparse tables to
@@ -106,6 +113,7 @@ BU_EXPORT extern void bu_mm_cvt(const struct bu_structparse *sdp,
 #define	BU_HN_GETSCALE		0x10
 #define	BU_HN_AUTOSCALE		0x20
 
+
 /**
  * Convert digital sizes to human readable form.  Based off the
  * BSD function humanize_number(3).
@@ -116,6 +124,7 @@ BU_EXPORT extern void bu_mm_cvt(const struct bu_structparse *sdp,
  * number will be returned instead.
  */
 BU_EXPORT extern int bu_humanize_number(char *buf, size_t len, int64_t quotient, const char *suffix, size_t scale, int flags);
+
 
 /*
  * Converts the number given in 'str', which may be given in a humanized
