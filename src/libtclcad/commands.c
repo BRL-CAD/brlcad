5fcc0b7eeff861d063920ee2b0c608d8249a575c
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2020-10-15 14:40:26 +0000

    Break just the I/O portion out into a C++ file, fix a few things in the Windows portion.
    svn:revision:77453
    svn:branch:trunk
    svn:account:starseeker

--- src/libtclcad/commands.c
+++ src/libtclcad/commands.c
@@ -1,4 +1,4 @@
-/*                     C O M M A N D S . C P P
+/*                     C O M M A N D S . C
  * BRL-CAD
  *
  * Copyright (c) 2000-2020 United States Government as represented by
@@ -19,7 +19,7 @@
  */
 /** @addtogroup libtclcad */
 /** @{ */
-/** @file libtclcad/commands.cpp
+/** @file libtclcad/commands.c
  *
  * A quasi-object-oriented database interface.
  *
@@ -31,9 +31,6 @@
 
 #include "common.h"
 
-#include <map>
-
-extern "C" {
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
@@ -79,38 +76,14 @@ extern "C" {
 #  include <GL/gl.h>
 #endif
 
-    /* For the moment call internal libged functions - a cleaner
-     * solution will be needed eventually */
+/* For the moment call internal libged functions - a cleaner
+ * solution will be needed eventually */
 #include "../libged/ged_private.h"
 
-    /* Private headers */
+/* Private headers */
 #include "brlcad_version.h"
 #include "./tclcad_private.h"
 #include "./view/view.h"
-}
-
-struct tclcad_process_channels {
-    Tcl_Channel cstdin;
-    Tcl_Channel cstdout;
-    Tcl_Channel cstderr;
-};
-
-struct tclcad_io_data *
-tclcad_create_io_data()
-{
-    struct tclcad_io_data *d;
-    BU_GET(d, struct tclcad_io_data);
-    d->state = new std::map<struct ged_subprocess *, struct tclcad_process_channels *>;
-    return d;
-}
-
-TCLCAD_EXPORT void
-tclcad_destroy_io_data(struct tclcad_io_data *d)
-{
-    std::map<struct ged_subprocess *, struct tclcad_process_channels *> *s = (std::map<struct ged_subprocess *, struct tclcad_process_channels *> *)d->state;
-    delete s;
-    BU_PUT(d, struct tclcad_io_data);
-}
 
 HIDDEN int to_base2local(struct ged *gedp,
 	int argc,
@@ -1210,124 +1183,6 @@ to_create_cmd(Tcl_Interp *interp,
 
     return TCL_OK;
 }
-
-
-/* Wrappers for setting up/tearing down IO handler */
-#ifndef _WIN32
-void
-tclcad_create_io_handler(struct ged_subprocess *p, bu_process_io_t d, ged_io_func_t callback, void *data)
-{
-    if (!p || !p->p || !p->gedp || !p->gedp->ged_io_data)
-	return;
-    int *fdp = (int *)bu_process_fd(p->p, d);
-    if (fdp) {
-	struct tclcad_io_data *t_iod = (struct tclcad_io_data *)p->gedp->ged_io_data;
-	Tcl_CreateFileHandler(*fdp, t_iod->io_mode, callback, (ClientData)data);
-    }
-}
-
-
-void
-tclcad_delete_io_handler(struct ged_subprocess *p, bu_process_io_t d)
-{
-    if (!p) return;
-    int *fdp = (int *)bu_process_fd(p->p, d);
-    if (fdp) {
-	Tcl_DeleteFileHandler(*fdp);
-	close(*fdp);
-    }
-}
-
-
-#else
-void
-tclcad_create_io_handler(struct ged_subprocess *p, bu_process_io_t d, ged_io_func_t callback, void *data)
-{
-    if (!p || !p->p || !p->gedp || !p->gedp->ged_io_data)
-	return;
-    struct tclcad_io_data *t_iod = (struct tclcad_io_data *)p->gedp->ged_io_data;
-    std::map<struct ged_subprocess *, struct tclcad_process_channels *> *pmap = (std::map<struct ged_subprocess *, struct tclcad_process_channels *> *)t_iod->state;
-    struct tclcad_process_channels *pchan = NULL;
-    if (pmap->find(p) == pmap->end()) {
-	BU_GET(pchan, struct tclcad_process_channels);
-	pchan->cstdin = NULL;
-	pchan->cstdout = NULL;
-	pchan->cstderr = NULL;
-	pmap[p] = pchan;
-    } else {
-	pchan = pmap[p];
-    }
-
-    HANDLE *fdp = (HANDLE *)bu_process_fd(p->p, d);
-    if (fdp) {
-	switch (d) {
-	    case BU_PROCESS_STDIN:
-		if (!pchan->cstdin) {
-		    pchan->cstdin = Tcl_MakeFileChannel(*fdp, t_iod->io_mode);
-		    Tcl_CreateChannelHandler(pchan->cstdin, t_iod->io_mode, callback, (ClientData)data);
-		}
-		break;
-	    case BU_PROCESS_STDOUT:
-		if (!pchan->cstdout) {
-		    pchan->cstdout = Tcl_MakeFileChannel(*fdp, t_iod->io_mode);
-		    Tcl_CreateChannelHandler(pchan->cstdout, t_iod->io_mode, callback, (ClientData)data);
-		}
-		break;
-	    case BU_PROCESS_STDERR:
-		if (!pchan->cstderr) {
-		    pchan->cstderr = Tcl_MakeFileChannel(*fdp, t_iod->io_mode);
-		    Tcl_CreateChannelHandler(pchan->cstderr, t_iod->io_mode, callback, (ClientData)data);
-		}
-		break;
-	}
-    }
-}
-
-
-void
-tclcad_delete_io_handler(struct ged_subprocess *p, bu_process_io_t d)
-{
-    if (!p || !p->p || !p->gedp || !p->gedp->ged_io_data)
-	return;
-    struct tclcad_io_data *t_iod = (struct tclcad_io_data *)p->gedp->ged_io_data;
-    std::map<struct ged_subprocess *, struct tclcad_process_channels *> *pmap = (std::map<struct ged_subprocess *, struct tclcad_process_channels *> *)t_iod->state;
-    struct tclcad_process_channels *pchan = NULL;
-    if (!pmap || pmap->find(p) == pmap->end()) {
-	return;
-    }
-    pchan = pmap[p];
-    if (!pchan->cstdin && !pchan->cstdout && !pchan->cstdout) {
-	// All subprocess channels destroyed; we're done with the I/O from this subprocess, clean up
-	BU_PUT(pchan, struct tclcad_process_channels);
-	pmap.erase(p);
-    }
-
-    switch (d) {
-	case BU_PROCESS_STDIN:
-	    if (pchan->cstdin) {
-		Tcl_DeleteChannelHandler(pchan->cstdin, NULL, (ClientData)NULL);
-		Tcl_Close(t_iod->interp, pchan->cstdin);
-		pchan->cstdin = NULL;
-	    }
-	    break;
-	case BU_PROCESS_STDOUT:
-	    if (pchan->cstdout) {
-		Tcl_DeleteChannelHandler(pchan->cstdout, NULL, (ClientData)NULL);
-		Tcl_Close(t_iod->interp, pchan->cstdout);
-		pchan->cstdout = NULL;
-	    }
-	    break;
-	case BU_PROCESS_STDERR:
-	    if (pchan->cstderr) {
-		Tcl_DeleteChannelHandler(pchan->cstderr, NULL, (ClientData)NULL);
-		Tcl_Close(t_iod->interp, pchan->cstderr);
-		pchan->cstderr = NULL;
-	    }
-	    break;
-    }
-}
-#endif
-
 
 /**
  * @brief
@@ -6761,12 +6616,14 @@ to_rt_gettrees_application(struct ged *gedp,
     return ap;
 }
 
-// Local Variables:
-// tab-width: 8
-// mode: C++
-// c-basic-offset: 4
-// indent-tabs-mode: t
-// c-file-style: "stroustrup"
-// End:
-// ex: shiftwidth=4 tabstop=8
 
+
+/*
+ * Local Variables:
+ * tab-width: 8
+ * mode: C
+ * indent-tabs-mode: t
+ * c-file-style: "stroustrup"
+ * End:
+ * ex: shiftwidth=4 tabstop=8
+ */
