da7109b7f31520c7716412dcfa5c73ade60da708
Christopher Sean Morrison <brlcad@gmail.com>
2020-11-04 03:06:11 +0000

    match decl, use our typedefs
    svn:revision:77675
    svn:branch:trunk
    svn:account:brlcad

--- src/libbn/axis.c
+++ src/libbn/axis.c
@@ -39,8 +39,8 @@
 void
 tp_3axis(FILE *fp,		/**< output file */
 	 char *string,		/**< label for axis */
-	 fastf_t *origin,	/**< simple 3d point */
-	 fastf_t *rot,		/**< rotation angle */
+	 point_t origin,	/**< simple 3d point */
+	 mat_t rot,		/**< rotation angle */
 	 double length,		/**< length of axis */
 	 int ccw,    		/**< 0=clockwise, !0=counter clockwise (ccw) */
 	 int ndigits,		/**< # digits wide */
