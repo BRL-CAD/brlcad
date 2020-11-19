f9f89b89e5722daaa97dbcff3e86c46e79ca48dc
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2020-11-19 17:08:03 +0000

    Experiment to see if we can just force the rpath issue directly in configure.
    svn:revision:77751
    svn:branch:extbuild
    svn:account:starseeker

--- src/other/ext/tcl.cmake
+++ src/other/ext/tcl.cmake
@@ -75,7 +75,8 @@ if (BRLCAD_TCL_BUILD)
     ExternalProject_Add(TCL_BLD
       URL "${CMAKE_CURRENT_SOURCE_DIR}/tcl"
       BUILD_ALWAYS ${EXTERNAL_BUILD_UPDATE} ${LOG_OPTS}
-      PATCH_COMMAND rpath_replace ${TCL_PATCH_FILES}
+      PATCH_COMMAND patch "${TCL_SRC_DIR}/unix/configure" "${CMAKE_CURRENT_SOURCE_DIR}/CMake/tcl_configure.patch"
+      COMMAND rpath_replace ${TCL_PATCH_FILES}
       COMMAND tcl_replace ${TCL_REWORK_FILES}
       CONFIGURE_COMMAND CPPFLAGS=-I${CMAKE_BINARY_ROOT}/${INCLUDE_DIR} LDFLAGS=-L${CMAKE_BINARY_ROOT}/${LIB_DIR} ${TCL_SRC_DIR}/unix/configure --prefix=${TCL_INSTDIR}
       BUILD_COMMAND make -j${pcnt}
