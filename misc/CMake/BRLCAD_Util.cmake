2a820615d5a7b540bd77aacbb1ecef5d32955f38
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2020-11-30 16:34:14 +0000

    Not sure yet if this is practical, but see if we can abstract the platform naming of the libraries into a function based on the pattern.  If the naming is too erratic this won't work in general, but initial regex linux test works so checkpointing.
    svn:revision:77831
    svn:branch:extbuild
    svn:account:starseeker

--- misc/CMake/BRLCAD_Util.cmake
+++ misc/CMake/BRLCAD_Util.cmake
@@ -575,6 +575,55 @@ int main(int argc, const char **argv) {
 
 endfunction(generate_dreport)
 
+# Wrap the platform specific naming conventions we must manage to copy
+# build output from 3rd party build systems.
+function(set_lib_vars RVAR root vmaj vmin vpatch)
+
+  # OpenBSD has its own naming conventions.  Set a platform variable based on
+  # the OS name so we can test for it succinctly.
+  if ("${CMAKE_SYSTEM}" MATCHES ".*OpenBSD.*")
+    set(OPENBSD ON)
+  endif ("${CMAKE_SYSTEM}" MATCHES ".*OpenBSD.*")
+
+  unset(${RVAR}_BASENAME)
+  unset(${RVAR}_STATICNAME)
+  unset(${RVAR}_SUFFIX)
+  unset(${RVAR}_SYMLINK1)
+  unset(${RVAR}_SYMLINK2)
+
+  if (MSVC)
+    set(${RVAR}_BASENAME ${root})
+    set(${RVAR}_STATICNAME ${root}-static)
+    set(${RVAR}_SUFFIX ${CMAKE_SHARED_LIBRARY_SUFFIX})
+    set(${RVAR}_SYMLINK_1 ${${RVAR}_BASENAME}${CMAKE_SHARED_LIBRARY_SUFFIX})
+    set(${RVAR}_SYMLINK_2 ${${RVAR}_BASENAME}${CMAKE_SHARED_LIBRARY_SUFFIX}.${vmaj})
+  elseif (APPLE)
+    set(${RVAR}_BASENAME lib${root})
+    set(${RVAR}_STATICNAME lib${root})
+    set(${RVAR}_SUFFIX .${vmaj}.${vmin}.${vpatch}${CMAKE_SHARED_LIBRARY_SUFFIX})
+    set(${RVAR}_SYMLINK_1 ${${RVAR}_BASENAME}${CMAKE_SHARED_LIBRARY_SUFFIX})
+    set(${RVAR}_SYMLINK_2 ${${RVAR}_BASENAME}.${vmaj}${CMAKE_SHARED_LIBRARY_SUFFIX})
+  elseif (OPENBSD)
+    set(${RVAR}_BASENAME lib${root})
+    set(${RVAR}_STATICNAME lib${root})
+    set(${RVAR}_SUFFIX ${CMAKE_SHARED_LIBRARY_SUFFIX}.${vmaj}.${vmin})
+  else (MSVC)
+    set(${RVAR}_BASENAME lib${root})
+    set(${RVAR}_STATICNAME lib${root})
+    set(${RVAR}_SUFFIX ${CMAKE_SHARED_LIBRARY_SUFFIX}.${vmaj}.${vmin}.${vpatch})
+    set(${RVAR}_SYMLINK_1 ${${RVAR}_BASENAME}${CMAKE_SHARED_LIBRARY_SUFFIX})
+    set(${RVAR}_SYMLINK_2 ${${RVAR}_BASENAME}${CMAKE_SHARED_LIBRARY_SUFFIX}.${vmaj})
+  endif (MSVC)
+
+  # Communicate the answers to the parent scope - these are the return values.
+  set(${RVAR}_BASENAME   "${${RVAR}_BASENAME}"   PARENT_SCOPE)
+  set(${RVAR}_STATICNAME "${${RVAR}_STATICNAME}" PARENT_SCOPE)
+  set(${RVAR}_SUFFIX     "${${RVAR}_SUFFIX}"     PARENT_SCOPE)
+  set(${RVAR}_SYMLINK1   "${${RVAR}_SYMLINK1}"   PARENT_SCOPE)
+  set(${RVAR}_SYMLINK2   "${${RVAR}_SYMLINK2}"   PARENT_SCOPE)
+
+endfunction(set_lib_vars RVAR root)
+
 # Local Variables:
 # tab-width: 8
 # mode: cmake
