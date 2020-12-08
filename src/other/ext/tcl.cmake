4038fa31eea01b9f14068c42e545f68582d40d1e
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2020-12-08 15:13:05 +0000

    Make the dependency of the tclsh exe on the tcl staging explicit.  We need all the pieces in place before tclsh will run, and we do need it to run during the build.
    svn:revision:77949
    svn:branch:extbuild
    svn:account:starseeker

--- src/other/ext/tcl.cmake
+++ src/other/ext/tcl.cmake
@@ -373,6 +373,9 @@ if (BRLCAD_TCL_BUILD)
   # Anything building against the stub will want the headers, etc. in place
   add_dependencies(tclstub tcl_stage)
 
+  # Anything using the executable needs everything in place
+  add_dependencies(tclsh_exe tcl_stage)
+
   set(TCL_LIBRARY tcl CACHE STRING "Building bundled tcl" FORCE)
   set(TCL_LIBRARIES tcl CACHE STRING "Building bundled tcl" FORCE)
   set(TCL_STUB_LIBRARY tclstub CACHE STRING "Building bundled tcl" FORCE)
