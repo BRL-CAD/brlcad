2a86a5fc229130752584f1e49e8eb7ea5110fb3a
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2021-04-03 11:37:28 -0400

    Properly expose redraw as a ged_exec command.

--- src/libged/draw/draw.c
+++ src/libged/draw/draw.c
@@ -1230,7 +1230,7 @@ ged_ev_core(struct ged *gedp, int argc, const char *argv[])
 }
 
 int
-ged_redraw(struct ged *gedp, int argc, const char *argv[])
+ged_redraw_core(struct ged *gedp, int argc, const char *argv[])
 {
     int ret;
     struct display_list *gdlp;
@@ -1318,6 +1318,9 @@ const struct ged_cmd e_cmd = { &e_cmd_impl };
 struct ged_cmd_impl ev_cmd_impl = {"ev", ged_ev_core, GED_CMD_DEFAULT};
 const struct ged_cmd ev_cmd = { &ev_cmd_impl };
 
+struct ged_cmd_impl redraw_cmd_impl = {"redraw", ged_redraw_core, GED_CMD_DEFAULT};
+const struct ged_cmd redraw_cmd = { &redraw_cmd_impl };
+
 extern int ged_loadview_core(struct ged *gedp, int argc, const char *argv[]);
 struct ged_cmd_impl loadview_cmd_impl = {"loadview", ged_loadview_core, GED_CMD_DEFAULT};
 const struct ged_cmd loadview_cmd = { &loadview_cmd_impl };
@@ -1326,9 +1329,9 @@ extern int ged_preview_core(struct ged *gedp, int argc, const char *argv[]);
 struct ged_cmd_impl preview_cmd_impl = {"preview", ged_preview_core, GED_CMD_DEFAULT};
 const struct ged_cmd preview_cmd = { &preview_cmd_impl };
 
-const struct ged_cmd *draw_cmds[] = { &draw_cmd, &e_cmd, &ev_cmd, &loadview_cmd, &preview_cmd, NULL };
+const struct ged_cmd *draw_cmds[] = { &draw_cmd, &e_cmd, &ev_cmd, &redraw_cmd, &loadview_cmd, &preview_cmd, NULL };
 
-static const struct ged_plugin pinfo = { GED_API,  draw_cmds, 5 };
+static const struct ged_plugin pinfo = { GED_API,  draw_cmds, 6 };
 
 COMPILER_DLLEXPORT const struct ged_plugin *ged_plugin_info()
 {
