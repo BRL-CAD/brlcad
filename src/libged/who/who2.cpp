93765e8de8426008c17f9f919e394850b6986767
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2021-04-30 11:42:28 -0400

    More work on who2 - get expected result from havoc now.

--- src/libged/who/who2.cpp
+++ src/libged/who/who2.cpp
@@ -66,6 +66,7 @@ struct dfp_cmp {
 extern "C" int
 ged_who2_core(struct ged *gedp, int argc, const char *argv[])
 {
+    struct bu_vls pvls = BU_VLS_INIT_ZERO;
     GED_CHECK_DATABASE_OPEN(gedp, GED_ERROR);
     GED_CHECK_DRAWABLE(gedp, GED_ERROR);
     GED_CHECK_ARGC_GT_0(gedp, argc, GED_ERROR);
@@ -83,161 +84,199 @@ ged_who2_core(struct ged *gedp, int argc, const char *argv[])
     s = &s1;
     snext = &s2;
 
+    // Seed the sets with what we know to be drawn.  At this stage,
+    // we don't yet know of anything that is not drawn.  We put the
+    // known drawn objects into the drawn set, and queue their parents
+    // for checking.
+    std::set<std::string> not_drawn;
+    std::set<std::string> drawn;
+    std::set<std::string> finalized;
     for (size_t i = 0; i < BU_PTBL_LEN(gedp->ged_gvp->gv_scene_objs); i++) {
 	struct bview_scene_obj *sobj = (struct bview_scene_obj *)BU_PTBL_GET(gedp->ged_gvp->gv_scene_objs, i);
 	if (sobj->s_type_flags & BVIEW_DBOBJ_BASED) {
 	    struct draw_update_data_t *ud = (struct draw_update_data_t *)sobj->s_i_data;
-	    struct db_full_path *fp;
-	    if (s->find(&ud->fp) == s->end()) {
+
+	    // This path is drawn
+	    bu_vls_trunc(&pvls, 0);
+	    db_path_to_vls(&pvls, &ud->fp);
+	    drawn.insert(std::string(bu_vls_cstr(&pvls)));
+
+	    // The parent needs to be characterized, if there is one.
+	    if (ud->fp.fp_len == 1) {
+		// If there isn't anything above the current dp (such as a
+		// solid with no hierarchy is drawn) it's a finalized object
+		finalized.insert(std::string(bu_vls_cstr(&pvls)));
+	    } else {
+		// Have path depth - get the parent and queue it up
+		struct db_full_path *fp;
 		BU_GET(fp, struct db_full_path);
 		db_full_path_init(fp);
 		db_dup_full_path(fp, &ud->fp);
-		s->insert(fp);
+		DB_FULL_PATH_POP(fp);
+		if (s->find(fp) == s->end()) {
+		    s->insert(fp);
+		} else {
+		    db_free_full_path(fp);
+		    BU_PUT(fp, struct db_full_path);
+		}
 	    }
 	}
     }
 
-    std::set<std::string> drawn;
+    // Debugging - print out the set to confirm the ordering
     std::set<struct db_full_path *, dfp_cmp>::iterator s_it;
     for (s_it = s->begin(); s_it != s->end(); s_it++) {
 	struct db_full_path *fp = *s_it;
-	char *sfp = db_path_to_string(fp);
-	bu_log("%zd %s\n", (*s_it)->fp_len, sfp);
-	if (!(DB_FULL_PATH_CUR_DIR(fp)->d_flags & RT_DIR_COMB)) {
-	    // By definition, solids are fully drawn
-	    drawn.insert(std::string(sfp));
-	}
-	bu_free(sfp, "path string");
+	bu_vls_trunc(&pvls, 0);
+	db_path_to_vls(&pvls, fp);
+	bu_log("%zd %s\n", fp->fp_len, bu_vls_cstr(&pvls));
     }
 
-    std::set<std::string> finalized;
+    // Main characterization loop
+    size_t longest = (s->size()) ? (*s->begin())->fp_len : 0;
     while (s->size()) {
-	struct db_full_path *fp = *s->begin();
+	struct db_full_path *pp = *s->begin();
+	s->erase(s->begin());
 
-	// We'll be checking the parent of fp, but we want
-	// fp to be part of the comparison set.  Make a copy
-	// to serve as the parent path.
-	struct db_full_path *pp;
-	BU_GET(pp, struct db_full_path);
-	db_full_path_init(pp);
-	db_dup_full_path(pp, fp);
-	DB_FULL_PATH_POP(pp);
-
-	char *ppstr = db_path_to_string(pp);
-	bu_log("Checking children of %s\n", ppstr);
-	bu_free(ppstr, "ppstr");
-
-	// if pp_len == 0, fp is a top level object and its finalized -
-	// add it to the final set.
-	if (!pp->fp_len) {
-	    s->erase(fp);
-	    char *str = db_path_to_string(fp);
-	    finalized.insert(std::string(str));
-	    bu_free(str, "str");
+	// If we already know it's not drawn, don't process again
+	bu_vls_trunc(&pvls, 0);
+	db_path_to_vls(&pvls, pp);
+	if (not_drawn.find(std::string(bu_vls_cstr(&pvls))) != not_drawn.end()) {
 	    db_free_full_path(pp);
 	    BU_PUT(pp, struct db_full_path);
-	    db_free_full_path(fp);
-	    BU_PUT(fp, struct db_full_path);
-	} else {
-	    // If we have a deeper path get the list of immediate children from pp:
-	    struct directory **children = NULL;
-	    struct rt_db_internal in;
-	    struct rt_comb_internal *comb;
-	    if (rt_db_get_internal(&in, DB_FULL_PATH_CUR_DIR(pp), gedp->ged_wdbp->dbip, NULL, &rt_uniresource) < 0) {
-		char *str = db_path_to_string(fp);
-		finalized.insert(std::string(str));
-		bu_free(str, "str");
-		db_free_full_path(pp);
-		BU_PUT(pp, struct db_full_path);
+	    if (!s->size()) {
+		std::set<struct db_full_path *, dfp_cmp> *tmp = s;
+		s = snext;
+		snext = tmp;
+		longest = (s->size()) ? (*s->begin())->fp_len : 0;
 	    }
-	    comb = (struct rt_comb_internal *)in.idb_ptr;
-	    db_comb_children(gedp->ged_wdbp->dbip, comb, &children, NULL, NULL);
+	    continue;
+	}
 
-	    // For all children, see if the path is in s, snext or finalized. If it is
-	    // present in any of those it is drawn.
-	    int i = 0;
-	    int not_found = 0;
-	    struct directory *dp =  children[0];
+	// If pp is shorter than the longest paths of this cycle, defer it
+	// until the next round.  Longer paths need to be fully processed
+	// before we can correctly characterize shorter ones, and it may happen
+	// that a shorter path queues a parent that's also a parent in a longer
+	// path path before we're ready for it.
+	if (pp->fp_len < longest) {
+	    snext->insert(pp);
+	    if (!s->size()) {
+		std::set<struct db_full_path *, dfp_cmp> *tmp = s;
+		s = snext;
+		snext = tmp;
+		longest = (s->size()) ? (*s->begin())->fp_len : 0;
+	    }
+	    continue;
+	}
+
+	// Get the list of immediate children from pp:
+	struct directory **children = NULL;
+	struct rt_db_internal in;
+	struct rt_comb_internal *comb;
+	if (rt_db_get_internal(&in, DB_FULL_PATH_CUR_DIR(pp), gedp->ged_wdbp->dbip, NULL, &rt_uniresource) < 0) {
+	    not_drawn.insert(std::string(bu_vls_cstr(&pvls)));
+	    db_free_full_path(pp);
+	    BU_PUT(pp, struct db_full_path);
+	    if (!s->size()) {
+		std::set<struct db_full_path *, dfp_cmp> *tmp = s;
+		s = snext;
+		snext = tmp;
+		longest = (s->size()) ? (*s->begin())->fp_len : 0;
+	    }
+	    continue;
+	}
+	comb = (struct rt_comb_internal *)in.idb_ptr;
+	db_comb_children(gedp->ged_wdbp->dbip, comb, &children, NULL, NULL);
+
+	// If we have a comb with no children, we know the answer - it cannot
+	// be drawn because there is nothing to draw.
+	if (children[0] == RT_DIR_NULL) {
+	    not_drawn.insert(std::string(bu_vls_cstr(&pvls)));
+	    bu_free(children, "free children struct directory ptr array");
+	    db_free_full_path(pp);
+	    BU_PUT(pp, struct db_full_path);
+	    if (!s->size()) {
+		std::set<struct db_full_path *, dfp_cmp> *tmp = s;
+		s = snext;
+		snext = tmp;
+		longest = (s->size()) ? (*s->begin())->fp_len : 0;
+	    }
+	    continue;
+	}
+
+	int i = 0;
+	int not_found = 0;
+	struct directory *dp =  children[0];
+	while (dp != RT_DIR_NULL) {
+	    db_add_node_to_full_path(pp, dp);
+	    bu_vls_trunc(&pvls, 0);
+	    db_path_to_vls(&pvls, pp);
+	    if (drawn.find(std::string(bu_vls_cstr(&pvls))) == drawn.end()) {
+		// This path is not drawn.  That means the parent comb is also
+		// not (fully) drawn.
+		not_found = 1;
+	    }
+	    i++;
+	    dp = children[i];
+	    DB_FULL_PATH_POP(pp);
+	}
+
+	// If everything is good, this is a drawn path.  Flag it and insert its
+	// parent into the next queue.
+	if (!not_found) {
+	    bu_vls_trunc(&pvls, 0);
+	    db_path_to_vls(&pvls, pp);
+	    drawn.insert(std::string(bu_vls_cstr(&pvls)));
+	    if (pp->fp_len == 1) {
+		// Fully drawn, no parent - finalize
+		bu_vls_trunc(&pvls, 0);
+		db_path_to_vls(&pvls, pp);
+		finalized.insert(std::string(bu_vls_cstr(&pvls)));
+	    } else {
+		DB_FULL_PATH_POP(pp);
+		bu_vls_trunc(&pvls, 0);
+		db_path_to_vls(&pvls, pp);
+		if (pp->fp_len && snext->find(pp) == snext->end()) {
+		    struct db_full_path *npp;
+		    BU_GET(npp, struct db_full_path);
+		    db_full_path_init(npp);
+		    db_dup_full_path(npp, pp);
+		    snext->insert(npp);
+		}
+	    }
+	    db_free_full_path(pp);
+	    BU_PUT(pp, struct db_full_path);
+	} else {
+	    // If we had a not_found path, we need to finalize any child paths
+	    // that are drawn - that is the terminating condition for walking
+	    // back up paths.
+	    i = 0;
+	    dp = children[0];
+	    bu_vls_trunc(&pvls, 0);
+	    db_path_to_vls(&pvls, pp);
+
 	    while (dp != RT_DIR_NULL) {
 		db_add_node_to_full_path(pp, dp);
-		char *str = db_path_to_string(pp);
-		if (drawn.find(std::string(str)) == drawn.end()) {
-		    not_found = 1;
-		    bu_log("%s not found\n", str);
-		    bu_free(str, "str");
-		    DB_FULL_PATH_POP(pp);
-		    break;
+		bu_vls_trunc(&pvls, 0);
+		db_path_to_vls(&pvls, pp);
+		if (drawn.find(std::string(bu_vls_cstr(&pvls))) != drawn.end()) {
+		    // This path is drawn - finalize
+		    finalized.insert(std::string(bu_vls_cstr(&pvls)));
 		}
-		bu_free(str, "str");
 		DB_FULL_PATH_POP(pp);
 		i++;
 		dp = children[i];
 	    }
-	    i = 0;
-	    dp =  children[0];
-
-	    // If all child paths are present in s or snext, the parent is
-	    // fully realized in the scene.  If all child paths are NOT present
-	    // in s, those that are present are all finalized since their
-	    // immediate parent is not fully realized in the scene and they
-	    // themselves are the drawn object instances.
-	    if (not_found) {
-		// Not fully realized
-		while (dp != RT_DIR_NULL) {
-		    db_add_node_to_full_path(pp, dp);
-		    char *str = db_path_to_string(pp);
-		    if (drawn.find(std::string(str)) != drawn.end()) {
-			// We have a path that is drawn, in a comb not fully
-			// drawn.  Finalize it and remove it from processing
-			bu_log("finalizing partial path: %s\n", str);
-			finalized.insert(std::string(str));
-			s->erase(pp);
-			snext->erase(pp);
-		    }
-		    bu_free(str, "str");
-		    DB_FULL_PATH_POP(pp);
-		    i++;
-		    dp = children[i];
-		}
-		// Children handled - we're done processing this branch of the tree
-		s->erase(pp);
-		snext->erase(pp);
-	    } else {
-		// Fully realized
-		while (dp != RT_DIR_NULL) {
-		    db_add_node_to_full_path(pp, dp);
-		    s_it = s->find(pp);
-		    // Children are not required to be unique in a comb, so we
-		    // need to make sure we've not already deleted this
-		    // particular child path from the set.
-		    if (s_it != s->end()) {
-			struct db_full_path *op = *s_it;
-			s->erase(s_it);
-			db_free_full_path(op);
-			BU_PUT(op, struct db_full_path);
-		    }
-		    DB_FULL_PATH_POP(pp);
-		    i++;
-		    dp = children[i];
-		}
-		if (BU_STR_EQUAL(db_path_to_string(pp), "/component/bed/r1064")) {
-		    bu_log("about to get weird\n");
-		}
-		char *fstr = db_path_to_string(pp);
-		drawn.insert(std::string(fstr));
-		bu_free(fstr, "fstr");
-		bu_log("inserting fully realized comb: %s\n", db_path_to_string(pp));
-		struct db_full_path *npp;
-		BU_GET(npp, struct db_full_path);
-		db_full_path_init(npp);
-		db_dup_full_path(npp, pp);
-		snext->insert(npp);
-	    }
-	    bu_free(children, "free children struct directory ptr array");
-
+	    // Handled the children - the parent classifies as not_drawn
+	    bu_vls_trunc(&pvls, 0);
+	    db_path_to_vls(&pvls, pp);
+	    not_drawn.insert(std::string(bu_vls_cstr(&pvls)));
 	    db_free_full_path(pp);
 	    BU_PUT(pp, struct db_full_path);
 	}
+
+	bu_free(children, "free children struct directory ptr array");
+
 
 	// If s is not empty after this process, we have more paths from other
 	// sources to process, and we continue in a similar fashion.  Otherwise,
@@ -246,6 +285,7 @@ ged_who2_core(struct ged *gedp, int argc, const char *argv[])
 	    std::set<struct db_full_path *, dfp_cmp> *tmp = s;
 	    s = snext;
 	    snext = tmp;
+	    longest = (s->size()) ? (*s->begin())->fp_len : 0;
 	}
     }
 
