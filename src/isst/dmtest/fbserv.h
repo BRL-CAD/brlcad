58d8d55c2bd525c33817b744352f0cd33a583c7d
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2021-05-19 21:41:58 -0400

    Add header

--- src/isst/dmtest/fbserv.h
+++ src/isst/dmtest/fbserv.h
@@ -1,7 +1,44 @@
+/*                        F B S E R V . H
+ * BRL-CAD
+ *
+ * Copyright (c) 2021 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file fbserv.h
+ *
+ * Brief description
+ *
+ */
+
 #ifndef QDM_FBSERV_H
 #define QDM_FBSERV_H
 
 #include "common.h"
+
+#include <QObject>
+#include <QThread>
+#include <iostream>
+
+#ifdef Q_OS_WIN
+#include <QWinEventNotifier>
+#include <windows.h>
+#else
+#include <QSocketNotifier>
+#endif
+
 #include "dm/fbserv.h"
 
 __BEGIN_DECLS
