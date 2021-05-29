b4ab3eb6d4e8936c642812479e993b710d97e8ff
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2021-05-28 22:23:33 -0400

    Start breaking out the view altering part of draw2.

--- src/libged/draw/draw2.cpp
+++ src/libged/draw/draw2.cpp
@@ -711,127 +711,15 @@ alphanum_cmp(const void *a, const void *b, void *UNUSED(data)) {
     return alphanum_impl(bu_vls_cstr(&ga->g->s_name), bu_vls_cstr(&gb->g->s_name), NULL);
 }
 
-extern "C" int
-ged_draw2_core(struct ged *gedp, int argc, const char *argv[])
+// TODO - based on adaptive or non-adaptive plotting, we're either doing the general view objects or
+// a view specific object.  Need to adjust the logic accordingly - right now it's assuming ged_gvp
+// as the only view of interest.
+static int
+ged_draw_view(struct ged *gedp, struct bview *v, struct bv_settings *vs, int argc, const char *argv[], int bot_threshold, int no_autoview)
 {
-    int print_help = 0;
-    int bot_threshold = -1;
-    int no_autoview = 0;
-    static const char *usage = "[options] path1 [path2 ...]";
-    GED_CHECK_DATABASE_OPEN(gedp, GED_ERROR);
-    GED_CHECK_DRAWABLE(gedp, GED_ERROR);
-    GED_CHECK_VIEW(gedp, GED_ERROR);
-
-    /* skip command name argv[0] */
-    argc-=(argc>0); argv+=(argc>0);
-
-    /* initialize result */
-    bu_vls_trunc(gedp->ged_result_str, 0);
-
-
-    /* User settings may override various options - set up to collect them */
-    struct bv_settings vs = BV_SETTINGS_INIT;
-
-    int drawing_modes[6] = {-1, 0, 0, 0, 0, 0};
-    struct bu_opt_desc d[16];
-    BU_OPT(d[0],  "h", "help",          "",                 NULL, &print_help,         "Print help and exit");
-    BU_OPT(d[1],  "?", "",              "",                 NULL, &print_help,         "");
-    BU_OPT(d[2],  "m", "mode",         "#",          &bu_opt_int, &drawing_modes[0],  "0=wireframe;1=shaded bots;2=shaded;3=evaluated");
-    BU_OPT(d[3],   "", "wireframe",     "",                 NULL, &drawing_modes[1],  "Draw using only wireframes (mode = 0)");
-    BU_OPT(d[4],   "", "shaded",        "",                 NULL, &drawing_modes[2],  "Shade bots and polysolids (mode = 1)");
-    BU_OPT(d[5],   "", "shaded-all",    "",                 NULL, &drawing_modes[3],  "Shade all solids, not evaluated (mode = 2)");
-    BU_OPT(d[6],  "E", "evaluate",      "",                 NULL, &drawing_modes[4],  "Wireframe with evaluate booleans (mode = 3)");
-    BU_OPT(d[7],   "", "hidden-line",   "",                 NULL, &drawing_modes[5],  "Hidden line wireframes");
-    BU_OPT(d[8],  "t", "transparency", "#",      &bu_opt_fastf_t, &vs.transparency,   "Set transparency level in drawing: range 0 (clear) to 1 (opaque)");
-    BU_OPT(d[9],  "x", "",             "#",      &bu_opt_fastf_t, &vs.transparency,   "");
-    BU_OPT(d[10], "L", "",             "#",          &bu_opt_int, &bot_threshold,     "Set face count level for drawing bounding boxes instead of BoT triangles (NOTE: passing this updates the global view setting - bot_threshold is a view property).");
-    BU_OPT(d[11], "S", "no-subtract",   "",                 NULL, &vs.draw_non_subtract_only,  "Do not draw subtraction solids");
-    BU_OPT(d[12],  "", "no-dash",       "",                 NULL, &vs.draw_solid_lines_only,  "Use solid lines rather than dashed for subtraction solids");
-    BU_OPT(d[13], "C", "color",         "r/g/b", &draw_opt_color, &vs,                "Override object colors");
-    BU_OPT(d[14],  "", "line-width",   "#",          &bu_opt_int, &vs.s_line_width,   "Override default line width");
-    BU_OPT(d[14], "R", "no-autoview",   "",                 NULL, &no_autoview,       "Do not calculate automatic view, even if initial scene is empty.");
-    BU_OPT_NULL(d[15]);
-
-    /* If no args, must be wanting help */
-    if (!argc) {
-	_ged_cmd_help(gedp, usage, d);
-	return GED_OK;
-    }
-
-    /* If adaptive plotting is enabled, we need to generate wireframes
-     * specific to that view */
-    int have_adaptive = 0;
-    int have_non_adaptive = 0;
-    for (size_t i = 0; i < BU_PTBL_LEN(&gedp->ged_views); i++) {
-	struct bview *v = (struct bview *)BU_PTBL_GET(&gedp->ged_views, i);
-	if (v->adaptive_plot) {
-	    have_adaptive = 1;
-	} else {
-	    have_non_adaptive = 1;
-	}
-    }
-
-    if (have_adaptive) {
-	bu_log("Adaptive plotting enabled\n");
-    }
-
-    if (have_adaptive && have_non_adaptive) {
-	bu_log("Both adaptive and non-adaptive views present\n");
-    }
-
-    /* TODO - below logic assumes ged_gvp.  Need to repackage it
-     * into a function that takes a bview pointer as an arg, so
-     * we can repeat drawing for multiple views if necessary.
-     * Until we do that, ged_gvp must be set... */
-    if (!gedp->ged_gvp) {
-	bu_vls_printf(gedp->ged_result_str, "No current GED view defined");
-	return GED_ERROR;
-    }
-
-
-
-    /* Option defaults come from the current view, but may be overridden for
-     * the purposes of the current draw command by command line options. */
-    bv_settings_sync(&vs, &gedp->ged_gvp->gvs);
-
-    /* Process command line args into vs with bu_opt */
-    int opt_ret = bu_opt_parse(NULL, argc, argv, d);
-
-    if (print_help) {
-	_ged_cmd_help(gedp, usage, d);
-	return GED_OK;
-    }
-
-    // Whatever is left after argument processing are the potential draw paths
-    argc = opt_ret;
-
-    // Drawing modes may be set either by -m or by the more verbose options,
-    // with the latter taking precedence if both are set.
-    int have_override = 0;
-    for (int i = 1; i < 6; i++) {
-	if (drawing_modes[i]) {
-	    have_override++;
-	}
-    }
-    if (have_override > 1 || (have_override &&  drawing_modes[0] > -1)) {
-	bu_vls_printf(gedp->ged_result_str, "Multiple view modes specified\n");
-	return GED_ERROR;
-    }
-    if (have_override) {
-	for (int i = 1; i < 6; i++) {
-	    if (drawing_modes[i]) {
-		drawing_modes[0] = i - 1;
-		break;
-	    }
-	}
-    }
-    if (drawing_modes[0] > -1) {
-	vs.s_dmode = drawing_modes[0];
-    }
-
     // Abbreviations for convenience
     struct db_i *dbip = gedp->ged_wdbp->dbip;
-    struct bu_ptbl *sg = gedp->ged_gvp->gv_db_grps;
+    struct bu_ptbl *sg = v->gv_db_grps;
     struct bv_scene_obj *free_scene_obj = gedp->free_scene_obj;
 
 
@@ -845,13 +733,13 @@ ged_draw2_core(struct ged *gedp, int argc, const char *argv[])
     bounds_data.res = &rt_uniresource;
     bounds_data.have_bbox = 0;
     bounds_data.dbip = dbip;
-    bounds_data.skip_subtractions = vs.draw_non_subtract_only;
+    bounds_data.skip_subtractions = vs->draw_non_subtract_only;
     VSET(bounds_data.min, INFINITY, INFINITY, INFINITY);
     VSET(bounds_data.max, -INFINITY, -INFINITY, -INFINITY);
     bounds_data.s_size = &s_size;
     // Before we start doing anything with sg, record if things are starting
     // out empty.
-    if (!BU_PTBL_LEN(sg) && !BU_PTBL_LEN(gedp->ged_gvp->gv_view_objs)) {
+    if (!BU_PTBL_LEN(sg) && !BU_PTBL_LEN(v->gv_view_objs)) {
 	blank_slate = 1;
     }
 
@@ -883,7 +771,7 @@ ged_draw2_core(struct ged *gedp, int argc, const char *argv[])
 
     /* Bot threshold a per-view setting. */
     if (bot_threshold >= 0) {
-	gedp->ged_gvp->bot_threshold = bot_threshold;
+	v->bot_threshold = bot_threshold;
     }
 
 
@@ -978,8 +866,8 @@ ged_draw2_core(struct ged *gedp, int argc, const char *argv[])
 	    bv_scene_obj_init(g->g, free_scene_obj);
 	    db_path_to_vls(&g->g->s_name, fp);
 	    db_path_to_vls(&g->g->s_uuid, fp);
-	    bv_settings_sync(&g->g->s_os, &vs);
-	    bu_ptbl_ins(gedp->ged_gvp->gv_db_grps, (long *)g);
+	    bv_settings_sync(&g->g->s_os, vs);
+	    bu_ptbl_ins(v->gv_db_grps, (long *)g);
 
 	    // If we're a blank slate, we're adaptive, and autoview isn't off
 	    // we need to be building up some sense of the view and object
@@ -987,7 +875,7 @@ ged_draw2_core(struct ged *gedp, int argc, const char *argv[])
 	    // for each group we're going to be using mat and walking the tree
 	    // of fp (or getting its box directly if fp is a solid with no
 	    // parents) to build up bounding info.
-	    if (blank_slate && gedp->ged_gvp->adaptive_plot && !no_autoview) {
+	    if (blank_slate && v->adaptive_plot && !no_autoview) {
 		mat_t cmat;
 		MAT_COPY(cmat, mat);
 		_bound_fp(fp, &cmat, (void *)&bounds_data);
@@ -999,17 +887,16 @@ ged_draw2_core(struct ged *gedp, int argc, const char *argv[])
 	    // Clear anything superseded by the new path
 	    for (g_it = clear.begin(); g_it != clear.end(); g_it++) {
 		struct bv_scene_group *cg = *g_it;
-		bu_ptbl_rm(gedp->ged_gvp->gv_db_grps, (long *)cg);
+		bu_ptbl_rm(v->gv_db_grps, (long *)cg);
 		bv_scene_obj_free(cg->g, free_scene_obj);
 		BU_PUT(cg, struct bv_scene_group);
 	    }
 	}
     }
 
-    if (blank_slate && gedp->ged_gvp->adaptive_plot && !no_autoview) {
+    if (blank_slate && v->adaptive_plot && !no_autoview) {
 	// We've checked the paths for bounding info - now set up the
 	// view
-	struct bview *v = gedp->ged_gvp;
 	point_t center = VINIT_ZERO;
 	point_t radial;
 	if (bounds_data.have_bbox) {
@@ -1056,7 +943,7 @@ ged_draw2_core(struct ged *gedp, int argc, const char *argv[])
 
 	// Get an initial color from the path, if we're not overridden
 	struct bu_color c;
-	if (!vs.color_override) {
+	if (!vs->color_override) {
 	    unsigned char dc[3];
 	    dc[0] = 255;
 	    dc[1] = 0;
@@ -1068,7 +955,7 @@ ged_draw2_core(struct ged *gedp, int argc, const char *argv[])
 	// Prepare tree walking data container
 	struct draw_data_t dd;
 	dd.dbip = gedp->ged_wdbp->dbip;
-	dd.v = gedp->ged_gvp;
+	dd.v = v;
 	dd.tol = &gedp->ged_wdbp->wdb_tol;
 	dd.ttol = &gedp->ged_wdbp->wdb_ttol;
 	dd.free_scene_obj = gedp->free_scene_obj;
@@ -1076,20 +963,20 @@ ged_draw2_core(struct ged *gedp, int argc, const char *argv[])
 	dd.bound_only = 0;
 	dd.res = &rt_uniresource;
 	dd.bool_op = 2; // Default to union
-	if (vs.color_override) {
-	    bu_color_from_rgb_chars(&dd.c, vs.color);
+	if (vs->color_override) {
+	    bu_color_from_rgb_chars(&dd.c, vs->color);
 	} else {
 	    HSET(dd.c.buc_rgb, c.buc_rgb[0], c.buc_rgb[1], c.buc_rgb[2], c.buc_rgb[3]);
 	}
 	dd.s_size = &s_size;
-	dd.vs = &vs;
+	dd.vs = vs;
 	dd.g = g;
 
 	// In drawing mode 3 (bigE) we are producing an evaluated shape,
 	// rather than iterating to get the solids
-	if (vs.s_dmode == 3) {
-	    if (vs.color_override) {
-		VMOVE(g->g->s_color, vs.color);
+	if (vs->s_dmode == 3) {
+	    if (vs->color_override) {
+		VMOVE(g->g->s_color, vs->color);
 	    } else {
 		bu_color_to_rgb_chars(&c, g->g->s_color);
 	    }
@@ -1126,7 +1013,7 @@ ged_draw2_core(struct ged *gedp, int argc, const char *argv[])
     }
 
     // Sort
-    bu_sort(BU_PTBL_BASEADDR(gedp->ged_gvp->gv_db_grps), BU_PTBL_LEN(gedp->ged_gvp->gv_db_grps), sizeof(struct bv_scene_group *), alphanum_cmp, NULL);
+    bu_sort(BU_PTBL_BASEADDR(v->gv_db_grps), BU_PTBL_LEN(v->gv_db_grps), sizeof(struct bv_scene_group *), alphanum_cmp, NULL);
 
 
     // If we're starting from scratch and we're not being told to leave the
@@ -1144,6 +1031,124 @@ ged_draw2_core(struct ged *gedp, int argc, const char *argv[])
     // triangles, etc. for that object based on current settings.  It is then
     // the job of the dm to display the scene objects supplied by the view.
     return GED_OK;
+}
+
+
+extern "C" int
+ged_draw2_core(struct ged *gedp, int argc, const char *argv[])
+{
+    int print_help = 0;
+    int bot_threshold = -1;
+    int no_autoview = 0;
+    static const char *usage = "[options] path1 [path2 ...]";
+    GED_CHECK_DATABASE_OPEN(gedp, GED_ERROR);
+    GED_CHECK_DRAWABLE(gedp, GED_ERROR);
+    GED_CHECK_VIEW(gedp, GED_ERROR);
+
+    /* skip command name argv[0] */
+    argc-=(argc>0); argv+=(argc>0);
+
+    /* initialize result */
+    bu_vls_trunc(gedp->ged_result_str, 0);
+
+    /* User settings may override various options - set up to collect them */
+    struct bv_settings vs = BV_SETTINGS_INIT;
+
+    /* Option defaults come from the current view, but may be overridden for
+     * the purposes of the current draw command by command line options. */
+    if (gedp->ged_gvp)
+	bv_settings_sync(&vs, &gedp->ged_gvp->gvs);
+
+    int drawing_modes[6] = {-1, 0, 0, 0, 0, 0};
+    struct bu_opt_desc d[16];
+    BU_OPT(d[0],  "h", "help",          "",                 NULL, &print_help,         "Print help and exit");
+    BU_OPT(d[1],  "?", "",              "",                 NULL, &print_help,         "");
+    BU_OPT(d[2],  "m", "mode",         "#",          &bu_opt_int, &drawing_modes[0],  "0=wireframe;1=shaded bots;2=shaded;3=evaluated");
+    BU_OPT(d[3],   "", "wireframe",     "",                 NULL, &drawing_modes[1],  "Draw using only wireframes (mode = 0)");
+    BU_OPT(d[4],   "", "shaded",        "",                 NULL, &drawing_modes[2],  "Shade bots and polysolids (mode = 1)");
+    BU_OPT(d[5],   "", "shaded-all",    "",                 NULL, &drawing_modes[3],  "Shade all solids, not evaluated (mode = 2)");
+    BU_OPT(d[6],  "E", "evaluate",      "",                 NULL, &drawing_modes[4],  "Wireframe with evaluate booleans (mode = 3)");
+    BU_OPT(d[7],   "", "hidden-line",   "",                 NULL, &drawing_modes[5],  "Hidden line wireframes");
+    BU_OPT(d[8],  "t", "transparency", "#",      &bu_opt_fastf_t, &vs.transparency,   "Set transparency level in drawing: range 0 (clear) to 1 (opaque)");
+    BU_OPT(d[9],  "x", "",             "#",      &bu_opt_fastf_t, &vs.transparency,   "");
+    BU_OPT(d[10], "L", "",             "#",          &bu_opt_int, &bot_threshold,     "Set face count level for drawing bounding boxes instead of BoT triangles (NOTE: passing this updates the global view setting - bot_threshold is a view property).");
+    BU_OPT(d[11], "S", "no-subtract",   "",                 NULL, &vs.draw_non_subtract_only,  "Do not draw subtraction solids");
+    BU_OPT(d[12],  "", "no-dash",       "",                 NULL, &vs.draw_solid_lines_only,  "Use solid lines rather than dashed for subtraction solids");
+    BU_OPT(d[13], "C", "color",         "r/g/b", &draw_opt_color, &vs,                "Override object colors");
+    BU_OPT(d[14],  "", "line-width",   "#",          &bu_opt_int, &vs.s_line_width,   "Override default line width");
+    BU_OPT(d[14], "R", "no-autoview",   "",                 NULL, &no_autoview,       "Do not calculate automatic view, even if initial scene is empty.");
+    BU_OPT_NULL(d[15]);
+
+    /* If no args, must be wanting help */
+    if (!argc) {
+	_ged_cmd_help(gedp, usage, d);
+	return GED_OK;
+    }
+
+    /* Process command line args into vs with bu_opt */
+    int opt_ret = bu_opt_parse(NULL, argc, argv, d);
+
+    if (print_help) {
+	_ged_cmd_help(gedp, usage, d);
+	return GED_OK;
+    }
+
+    // Whatever is left after argument processing are the potential draw paths
+    argc = opt_ret;
+
+    // Drawing modes may be set either by -m or by the more verbose options,
+    // with the latter taking precedence if both are set.
+    int have_override = 0;
+    for (int i = 1; i < 6; i++) {
+	if (drawing_modes[i]) {
+	    have_override++;
+	}
+    }
+    if (have_override > 1 || (have_override &&  drawing_modes[0] > -1)) {
+	bu_vls_printf(gedp->ged_result_str, "Multiple view modes specified\n");
+	return GED_ERROR;
+    }
+    if (have_override) {
+	for (int i = 1; i < 6; i++) {
+	    if (drawing_modes[i]) {
+		drawing_modes[0] = i - 1;
+		break;
+	    }
+	}
+    }
+    if (drawing_modes[0] > -1) {
+	vs.s_dmode = drawing_modes[0];
+    }
+
+    /* If adaptive plotting is enabled, we need to generate wireframes
+     * specific to that view */
+    int have_adaptive = 0;
+    int have_non_adaptive = 0;
+    for (size_t i = 0; i < BU_PTBL_LEN(&gedp->ged_views); i++) {
+	struct bview *v = (struct bview *)BU_PTBL_GET(&gedp->ged_views, i);
+	if (v->adaptive_plot) {
+	    have_adaptive = 1;
+	} else {
+	    have_non_adaptive = 1;
+	}
+    }
+
+    if (have_adaptive) {
+	bu_log("Adaptive plotting enabled\n");
+    }
+
+    if (have_adaptive && have_non_adaptive) {
+	bu_log("Both adaptive and non-adaptive views present\n");
+    }
+
+    // For now, we need ged_gvp...
+    if (!gedp->ged_gvp) {
+	bu_vls_printf(gedp->ged_result_str, "No current GED view defined");
+	return GED_ERROR;
+    }
+
+    return ged_draw_view(gedp, gedp->ged_gvp, &vs, argc, argv, bot_threshold, no_autoview);
+
 }
 
 /*
