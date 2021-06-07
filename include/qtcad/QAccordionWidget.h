506049de2f6d21fa66e696addf45a0e89d3d2361
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2021-06-06 21:45:35 -0400

    Need another design run at QAccordionWidget
    What's there isn't behaving when we do things like make the window
    large, and some of the internal widget resizing is bizarre. Need to take
    a step back on this widget and reconsider the behavior we need.  Might
    be better to do something like make a QWidget containing a QGridLayout
    and roll our own behavior rather than trying to shoehorn QSplitter into
    doing this.  QSplitter's widget resizing behavior isn't exactly what
    we need for this case (close, but we want to relocate widgets down the
    stack rather than resize the one immediately below the spacer being
    moved...)

--- include/qtcad/QAccordionWidget.h
+++ include/qtcad/QAccordionWidget.h
@@ -23,6 +23,27 @@
  * added and then shown or hidden with a controlling button.  Can optionally
  * enforce a rule that at most one object is open at a time.
  *
+ * TODO - need to switch the custom resizing code that tries to "fill" the
+ * column to including the stack of widgets in a scroll area, instead.
+ * Need to spell out in detail what expected behavior is, but what we're
+ * currently trying to do here isn't really workable.  What we need to do:
+ *
+ * 1.  Default added widget to its preferred size.  That may mean for the
+ * default all-open stack we're taller than the window, which is why we
+ * need the scroll area.  Hide overall column scrollbars until we need them.
+ *
+ * 2.  For each open widget, store its last size (if the user resizes the
+ * splitter position) so we can remember what size to re-open it to if
+ * we close it.  Unlike current behavior, this should only change if the
+ * palette tool options minimum size requires growth or the user resizes
+ * the window.
+ *
+ * 3.  If we shrink or expand a widget via the splitter bar, move widgets
+ * below in the stack down or up rather than resizing the widget immediately
+ * below the splitter being moved.  (i.e. we want widget resizing to
+ * be local - the current non-local impact is just too messy.  However,
+ * we DO want to allow that interactive resizing, and so far I'm not
+ * seeing a pre-existing widget that will do so...)
  */
 
 #ifndef QACCORDIANWIDGET_H
