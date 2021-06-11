c3aece8da6457b44e88fc4a75a7887249c35adf2
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2021-06-11 14:56:37 -0400

    Stub in view settings tool - view_info is read-only, need to explore a two-way communication scenario.

---
+++ src/qged/plugins/view_settings/view_settings.cpp
@@ -0,0 +1,66 @@
+/*                 V I E W _ I N F O . C P P
+ * BRL-CAD
+ *
+ * Copyright (c) 2014-2021 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file view_info.cpp
+ *
+ */
+
+#include "qtcad/QToolPalette.h"
+#include "../plugin.h"
+
+void *
+view_settings_tool_create()
+{
+    QIcon *obj_icon = new QIcon(QPixmap(":settings.svg"));
+
+    QString viewstr("view controls ");
+    QPushButton *view_control = new QPushButton(viewstr);
+    view_control->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum);
+
+    QToolPaletteElement *el = new QToolPaletteElement(obj_icon, view_control);
+
+    return el;
+}
+
+extern "C" {
+    struct qged_tool_impl view_settings_tool_impl = {
+	view_settings_tool_create
+    };
+
+    const struct qged_tool view_settings_tool = { &view_settings_tool_impl, 1 };
+    const struct qged_tool *view_settings_tools[] = { &view_settings_tool, NULL };
+
+    static const struct qged_plugin pinfo = { QGED_VC_TOOL_PLUGIN, view_settings_tools, 1 };
+
+    COMPILER_DLLEXPORT const struct qged_plugin *qged_plugin_info()
+    {
+	return &pinfo;
+    }
+}
+
+
+
+// Local Variables:
+// tab-width: 8
+// mode: C++
+// c-basic-offset: 4
+// indent-tabs-mode: t
+// c-file-style: "stroustrup"
+// End:
+// ex: shiftwidth=4 tabstop=8
