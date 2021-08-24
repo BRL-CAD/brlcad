3edd98cffae79a97f162c23a803cea492a7a4ae5
Sean Morrison <brlcad@gmail.com>
2021-08-24 11:59:13 -0400

    limit large memory allocation to prevent maliciousness
    CID 1489217 found tainted allocations.  since pkg sizes are
    potentially very large (e.g., sending a large file as a single
    package), we do want to allow large sizes, but still need to impose
    some upper limit.  using the max size of ssize_t (i.e., max int) minus
    the two bytes being added during malloc() since that's nominally what
    a valid caller might maximally send.
    
    (cherry picked from commit 40c394bc54ab78282bc2ab8e8604601483609057)

--- src/libpkg/pkg.c
+++ src/libpkg/pkg.c
@@ -1347,6 +1347,10 @@ _pkg_gethdr(struct pkg_conn *pc, char *buf)
     if (pc->pkc_left == 0)
 	return 1;		/* msg here, no data */
 
+    /* ensure we don't allocate maliciously */
+    if (pc->pkc_len > SSIZE_MAX-2)
+	return -1;
+
     if (buf) {
 	pc->pkc_buf = buf;
     } else {
@@ -1387,6 +1391,11 @@ pkg_waitfor (int type, char *buf, size_t len, struct pkg_conn *pc)
     }
     if (_pkg_gethdr(pc, buf) < 0)
 	return -1;
+
+    /* ensure we don't allocate maliciously */
+    if (pc->pkc_len > SSIZE_MAX-2)
+	return -1;
+
     if (pc->pkc_type != type) {
 	/* A message of some other type has unexpectedly arrived. */
 	if (pc->pkc_len > 0) {
