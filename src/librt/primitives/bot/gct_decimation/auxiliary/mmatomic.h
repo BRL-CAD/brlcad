fcb52b06a57f018c9edd3d7a93a3c538e5d61e0d
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2021-10-21 15:21:52 -0400

    Remove a couple functions in favor of just putting the asm lines in directly.

--- src/librt/primitives/bot/gct_decimation/auxiliary/mmatomic.h
+++ src/librt/primitives/bot/gct_decimation/auxiliary/mmatomic.h
@@ -45,36 +45,15 @@
 #define MM_ATOMIC_ACCESS_L(v) ((v)->value)
 #define mmAtomicCmpReplace32(v,old,vnew) (mmAtomicCmpXchg32(v,old,vnew)==(old))
 
+
 #ifdef __x86_64__
-
-#define mmAtomicL mmAtomic64
-#define mmAtomicWriteL(v,i) mmAtomicWrite64(v,i)
-#define mmAtomicReadL(v) mmAtomicRead64(v)
-#define mmAtomicAndL(v,i) mmAtomicAnd64(v,i)
-#define mmAtomicOrL(v,i) mmAtomicOr64(v,i)
-#define mmAtomicCmpReplaceL(v,i,j) mmAtomicCmpReplace64(v,i,j)
-
-#define mmAtomicP mmAtomic64
-#define mmAtomicReadP(v) (void *)(uintptr_t)mmAtomicRead64(v)
-#define mmAtomicWriteP(v,i) mmAtomicWrite64(v,(int64_t)i)
-
+#  define mmAtomicP mmAtomic64
+#  define mmAtomicWriteP(v,i) mmAtomicWrite64(v,(int64_t)i)
 #elif defined(__i386__)
-
-#define mmAtomicL mmAtomic32
-#define mmAtomicWriteL(v,i) mmAtomicWrite32(v,i)
-#define mmAtomicReadL(v) mmAtomicRead32(v)
-#define mmAtomicAndL(v,i) mmAtomicAnd32(v,i)
-#define mmAtomicOrL(v,i) mmAtomicOr32(v,i)
-#define mmAtomicCmpReplaceL(v,i,j) mmAtomicCmpReplace32(v,i,j)
-
-#define mmAtomicP mmAtomic32
-#define mmAtomicReadP(v) (void *)(uintptr_t)mmAtomicRead32(v)
-#define mmAtomicWriteP(v,i) mmAtomicWrite32(v,(int32_t)i)
-
+#  define mmAtomicP mmAtomic32
+#  define mmAtomicWriteP(v,i) mmAtomicWrite32(v,(int32_t)i)
 #else
-
-#error should not reach here
-
+#  error should not reach here
 #endif
 
 
@@ -83,32 +62,16 @@ typedef struct {
 } mmAtomic32;
 
 
-/* Do nothing, prevent compiler reordering */
-static inline void mmBarrier()
-{
-    __asm__ __volatile__("":::"memory");
-}
-
-
-static inline void mmAtomicPause()
-{
-    __asm__ __volatile__(
-	"rep; nop"
-	:
-	::"memory");
-}
-
-
 static inline int32_t mmAtomicRead32(mmAtomic32 *v)
 {
-    mmBarrier();
+    __asm__ __volatile__("":::"memory");
     return v->value;
 }
 
 
 static inline void mmAtomicWrite32(mmAtomic32 *v, int32_t i)
 {
-    mmBarrier();
+    __asm__ __volatile__("":::"memory");
     v->value = i;
 }
 
@@ -116,27 +79,27 @@ static inline void mmAtomicWrite32(mmAtomic32 *v, int32_t i)
 static inline void mmAtomicAnd32(mmAtomic32 *v, int32_t i)
 {
     __asm__ __volatile__(
-	"lock; andl %1,%0"
-	:"=m"(v->value)
-	:"ir"(i), "m"(v->value) :"memory");
+	    "lock; andl %1,%0"
+	    :"=m"(v->value)
+	    :"ir"(i), "m"(v->value) :"memory");
 }
 
 
 static inline void mmAtomicOr32(mmAtomic32 *v, int32_t i)
 {
     __asm__ __volatile__(
-	"lock; orl %1,%0"
-	:"=m"(v->value)
-	:"ir"(i), "m"(v->value) :"memory");
+	    "lock; orl %1,%0"
+	    :"=m"(v->value)
+	    :"ir"(i), "m"(v->value) :"memory");
 }
 
 
 static inline void mmAtomicAdd32(mmAtomic32 *v, int32_t i)
 {
     __asm__ __volatile__(
-	"lock; addl %1,%0"
-	:"=m"(v->value)
-	:"ir"(i), "m"(v->value) :"memory");
+	    "lock; addl %1,%0"
+	    :"=m"(v->value)
+	    :"ir"(i), "m"(v->value) :"memory");
 }
 
 
@@ -144,9 +107,9 @@ static inline int32_t mmAtomicCmpXchg32(mmAtomic32 *v, int32_t old, int32_t vnew
 {
     int32_t prev;
     __asm__ __volatile__(
-	"lock; cmpxchgl %1,%2"
-	:"=a"(prev)
-	:"r"(vnew), "m"(v->value), "a"(old) :"memory");
+	    "lock; cmpxchgl %1,%2"
+	    :"=a"(prev)
+	    :"r"(vnew), "m"(v->value), "a"(old) :"memory");
     return prev;
 }
 
@@ -167,7 +130,7 @@ static inline void mmAtomicSpin32(mmAtomic32 *v, int32_t old, int32_t vnew)
 {
     for (; mmAtomicCmpXchg32(v, old, vnew) != old;) {
 	for (; mmAtomicRead32(v) != old;)
-	    mmAtomicPause();
+	    __asm__ __volatile__("rep; nop":::"memory");
     }
 }
 
@@ -175,17 +138,17 @@ static inline void mmAtomicSpin32(mmAtomic32 *v, int32_t old, int32_t vnew)
 static inline void mmAtomicSpinWaitEq32(mmAtomic32 *v, int32_t i)
 {
     __asm__ __volatile__(
-	"jmp 1f\n"
-	".p2align 6\n"
-	"2:\n"
-	"rep; nop\n"
-	"1:\n"
-	"cmpl %1,%0\n"
-	"jnz 2b\n"
-	".p2align 4\n"
-	"3:\n"
-	:
-	:"m"(v->value), "r"(i) :"memory");
+	    "jmp 1f\n"
+	    ".p2align 6\n"
+	    "2:\n"
+	    "rep; nop\n"
+	    "1:\n"
+	    "cmpl %1,%0\n"
+	    "jnz 2b\n"
+	    ".p2align 4\n"
+	    "3:\n"
+	    :
+	    :"m"(v->value), "r"(i) :"memory");
 }
 
 
@@ -204,9 +167,9 @@ static inline int mmAtomicAddTestNegative32(mmAtomic32 *v, int32_t i)
 {
     unsigned char c;
     __asm__ __volatile__(
-	"lock; addl %2,%0; sets %1"
-	:"=m"(v->value), "=qm"(c)
-	:"ir"(i), "m"(v->value) :"memory");
+	    "lock; addl %2,%0; sets %1"
+	    :"=m"(v->value), "=qm"(c)
+	    :"ir"(i), "m"(v->value) :"memory");
     return c;
 }
 
@@ -215,7 +178,7 @@ static inline int mmAtomicLockTryRead32(mmAtomicLock32 *v, int spincount)
 {
     do {
 	if (mmAtomicRead32(&v->v) < 0)
-	    mmAtomicPause();
+	    __asm__ __volatile__("rep; nop":::"memory");
 	else {
 	    if (!(mmAtomicAddTestNegative32(&v->v, 1)))
 		return 1;
@@ -232,7 +195,7 @@ static inline int mmAtomicLockTryWrite32(mmAtomicLock32 *v, int spincount)
 {
     do {
 	if (mmAtomicRead32(&v->v))
-	    mmAtomicPause();
+	    __asm__ __volatile__("rep; nop":::"memory");
 	else {
 	    if (!(mmAtomicCmpXchg32(&v->v, 0, MM_ATOMIC_LOCK32_WRITE)))
 		return 1;
@@ -258,9 +221,6 @@ static inline void mmAtomicLockDoneWrite32(mmAtomicLock32 *v)
 #ifdef __x86_64__
 
 
-#define mmAtomicCmpReplace64(v,old,vnew) (mmAtomicCmpXchg64(v,old,vnew)==(old))
-
-
 typedef struct {
     volatile int64_t value;
 } mmAtomic64;
@@ -268,68 +228,17 @@ typedef struct {
 
 static inline int64_t mmAtomicRead64(mmAtomic64 *v)
 {
-    mmBarrier();
+    __asm__ __volatile__("":::"memory");
     return v->value;
 }
 
 
 static inline void mmAtomicWrite64(mmAtomic64 *v, int64_t i)
 {
-    mmBarrier();
+    __asm__ __volatile__("":::"memory");
     v->value = i;
 }
 
-
-static inline void mmAtomicAnd64(mmAtomic64 *v, int64_t i)
-{
-    __asm__ __volatile__(
-	"lock; andq %1,%0"
-	:"=m"(v->value)
-	:"ir"(i), "m"(v->value) :"memory");
-}
-
-
-static inline void mmAtomicOr64(mmAtomic64 *v, int64_t i)
-{
-    __asm__ __volatile__(
-	"lock; orq %1,%0"
-	:"=m"(v->value)
-	:"ir"(i), "m"(v->value) :"memory");
-}
-
-
-static inline int64_t mmAtomicCmpXchg64(mmAtomic64 *v, int64_t vold, int64_t vnew)
-{
-    int64_t prev;
-    __asm__ __volatile__(
-	"lock; cmpxchgq %1,%2"
-	:"=a"(prev)
-	:"r"(vnew), "m"(v->value), "a"(vold) :"memory");
-    return prev;
-}
-
-
-static void mmQuellPedantic(int var)
-{
-if (!var) {
-	(void)mmAtomicAnd32(NULL, 0);
-	(void)mmAtomicOr32(NULL, 0);
-	(void)mmAtomicAddRead32(NULL, 0);
-	(void)mmAtomicSpinWaitEq32(NULL, 0);
-	(void)mmAtomicLockInit32(NULL);
-	(void)mmAtomicLockTryRead32(NULL, 0);
-	(void)mmAtomicLockTryWrite32(NULL, 0);
-	(void)mmAtomicLockDoneRead32(NULL);
-	(void)mmAtomicLockDoneWrite32(NULL);
-	(void)mmAtomicRead64(NULL);
-	(void)mmAtomicWrite64(NULL, 0);
-	(void)mmAtomicAnd64(NULL, 0);
-	(void)mmAtomicOr64(NULL, 0);
-	(void)mmAtomicCmpXchg64(NULL, 0, 0);
-	(void)mmQuellPedantic(1);
-}
-}
-
 #endif
 
 
@@ -337,3 +246,13 @@ if (!var) {
 
 
 #endif
+
+/*
+ * Local Variables:
+ * tab-width: 8
+ * mode: C
+ * indent-tabs-mode: t
+ * c-file-style: "stroustrup"
+ * End:
+ * ex: shiftwidth=4 tabstop=8
+ */
