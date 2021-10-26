c65267215efba5c8062a68834bc30c3187606105
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2021-10-26 16:07:35 -0400

    Don't attempt to redraw if there are no views.

--- src/libged/draw/draw2.cpp
+++ src/libged/draw/draw2.cpp
@@ -953,6 +953,10 @@ ged_redraw2_core(struct ged *gedp, int argc, const char *argv[])
     if (cv) {
 	return _ged_redraw_view(gedp, cv, argc, argv);
     } else {
+	if (!BU_PTBL_LEN(&gedp->ged_views)) {
+	    bu_vls_printf(gedp->ged_result_str, "No views defined\n");
+	    return GED_OK;
+	}
 	for (size_t i = 0; i < BU_PTBL_LEN(&gedp->ged_views); i++) {
 	    struct bview *v = (struct bview *)BU_PTBL_GET(&gedp->ged_views, i);
 	    int nret = _ged_redraw_view(gedp, v, argc, argv);
