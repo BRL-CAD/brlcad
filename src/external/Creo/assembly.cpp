047fa08d8604944e1ba7d13e6ab6361d13e1bafe
Chambers <1594260127121005@mil>
2021-11-05 09:27:32 -0400

    Fixed copyright comment, missing "/"
    Signed-off-by: Chambers <1594260127121005@mil>

--- src/external/Creo/assembly.cpp
+++ src/external/Creo/assembly.cpp
@@ -17,7 +17,7 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with this file; see the file named COPYING for more
  * information.
- *
+ */
 /**
  * @file assembly.cpp
  */
