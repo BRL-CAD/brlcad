12fa27e47b32cfe6f2712c0ce72dffc282d5abee
f4alt <christopherjmacgregor@gmail.com>
2021-11-15 23:23:07 -0600

    comment cleanup

--- src/art/brlcadplugin.cpp
+++ src/art/brlcadplugin.cpp
@@ -191,12 +191,9 @@ BrlcadObject::BrlcadObject(
 {
     configure_raytrace_application(get_database().c_str(), get_object_count(), get_objects());
     VSET(min, m_params.get_required<double>("minX"), m_params.get_required<double>("minY"), m_params.get_required<double>("minZ"));
+    VSET(max, m_params.get_required<double>("maxX"), m_params.get_required<double>("maxY"), m_params.get_required<double>("maxZ"));
     // VMOVE(min, ap->a_uvec);
     // VMOVE(max, ap->a_vvec);
-    VSET(max, m_params.get_required<double>("maxX"), m_params.get_required<double>("maxY"), m_params.get_required<double>("maxZ"));
-
-    // fprintf(output, "constructor built here \n");
-    // fflush(output);
 }
 
 
@@ -212,9 +209,6 @@ BrlcadObject:: BrlcadObject(
     this->resources = p_resources;
     VMOVE(this->min, ap->a_uvec);
     VMOVE(this->max, ap->a_vvec);
-
-    // fprintf(output, "other constructor built here \n");
-    // fflush(output);
 }
 
 
@@ -222,8 +216,6 @@ BrlcadObject:: BrlcadObject(
 void
 BrlcadObject::release()
 {
-    // bu_free(resources, "appleseed");
-    //bu_free(ap, "appleseed");
     delete this;
 }
 
@@ -347,7 +339,7 @@ BrlcadObject::intersect(const asr::ShadingRay& ray) const
 
 // Compute a front point, a back point and the geometric normal in object
 // instance space for a given ray with origin being a point on the surface
-void
+void 
 BrlcadObject::refine_and_offset(
     const asf::Ray3d& obj_inst_ray,
     asf::Vector3d& obj_inst_front_point,
