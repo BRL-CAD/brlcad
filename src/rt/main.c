cda9fc01a1cf3824708ae7b336cbb4b4629224ba
treydinges <treydinges@gmail.com>
2021-12-06 23:35:33 -0600

    fixes for gqa and rtweight to be read only

--- src/rt/main.c
+++ src/rt/main.c
@@ -474,25 +474,6 @@ int main(int argc, char *argv[])
 	outputfile = (char *)0;
 
     /*
-     *  Initialize application.
-     *  Note that width & height may not have been set yet,
-     *  since they may change from frame to frame.
-     */
-    need_fb = view_init(&APP, (char *)title_file, (char *)title_obj, outputfile != (char *)0, framebuffer != (char *)0);
-    if ((outputfile == (char *)0) && !need_fb) {
-	/* If not going to framebuffer, or to a file, then use stdout */
-	if (outfp == NULL) outfp = stdout;
-	/* output_is_binary is changed by view_init, as appropriate */
-	if (output_is_binary && isatty(fileno(outfp))) {
-	    fprintf(stderr, "rt:  attempting to send binary output to terminal, aborting\n");
-#ifdef MPI_ENABLED
-	    MPI_Finalize();
-#endif
-	    return 14;
-	}
-    }
-
-    /*
      *  Initialize all the per-CPU memory resources.
      *  The number of processors can change at runtime, init them all.
      */
@@ -539,6 +520,26 @@ int main(int argc, char *argv[])
 #endif
 
 	def_tree(APP.a_rt_i);		/* Load the default trees */
+
+	/*
+     *  Initialize application.
+     *  Note that width & height may not have been set yet,
+     *  since they may change from frame to frame.
+     */
+    need_fb = view_init(&APP, (char *)title_file, (char *)title_obj, outputfile != (char *)0, framebuffer != (char *)0);
+    if ((outputfile == (char *)0) && !need_fb) {
+	/* If not going to framebuffer, or to a file, then use stdout */
+	if (outfp == NULL) outfp = stdout;
+	/* output_is_binary is changed by view_init, as appropriate */
+	if (output_is_binary && isatty(fileno(outfp))) {
+	    fprintf(stderr, "rt:  attempting to send binary output to terminal, aborting\n");
+#ifdef MPI_ENABLED
+	    MPI_Finalize();
+#endif
+	    return 14;
+	}
+    }
+
 	/* orientation command has not been used */
 	if (!orientflag)
 	    do_ae(azimuth, elevation);
