562e9bd950917756a81712700f10e65ef66f1e37
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2021-12-13 16:38:03 -0500

    Report count is down to 19

--- misc/clang-static-analyzer-run.sh
+++ misc/clang-static-analyzer-run.sh
@@ -74,7 +74,7 @@ runtest all
 echo "Summary: $failure failures found."
 
 # If we have more than the expected failure count, error out
-if [ "$failure" -gt "20" ]; then
+if [ "$failure" -gt "19" ]; then
 	exit 1
 fi
 
