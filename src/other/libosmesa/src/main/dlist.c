bd00540baff6627e664e60425270804e98131dfa
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2022-01-05 17:00:23 -0500

    Free allocated memory in error case (CID 503737)

--- src/other/libosmesa/src/main/dlist.c
+++ src/other/libosmesa/src/main/dlist.c
@@ -3658,7 +3658,7 @@ save_TexSubImage1D(GLenum target, GLint level, GLint xoffset,
     Node *n;
     GLvoid *image = unpack_image(1, width, 1, 1, format, type,
 				 pixels, &ctx->Unpack);
-    ASSERT_OUTSIDE_SAVE_BEGIN_END_AND_FLUSH(ctx);
+    ASSERT_OUTSIDE_SAVE_BEGIN_END_FREE_AND_FLUSH(ctx, image);
     n = ALLOC_INSTRUCTION(ctx, OPCODE_TEX_SUB_IMAGE1D, 7);
     if (n) {
 	n[1].e = target;
