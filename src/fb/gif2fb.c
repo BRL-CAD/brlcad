df0630d7e61b637dea26ba8f17f21411994f496e
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2022-01-08 18:01:27 -0500

    Fix bounds check (CID 491475)

--- src/fb/gif2fb.c
+++ src/fb/gif2fb.c
@@ -286,7 +286,7 @@ main(int argc, char **argv)
 	int ih_height = WORD(Im.IH_Height);
 	int ih_width = WORD(Im.IH_Width);
 
-	if (ih_height < 0 || ih_height > 0xffff || ih_width < 0 || ih_height > 0xffff)
+	if (ih_height < 0 || ih_height > 0xffff || ih_width < 0 || ih_width > 0xffff)
 	    bu_exit(1, "Invalid height in GIF Header\n");
 
 	/*
