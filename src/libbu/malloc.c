e93359d607e62d5dbaa3e97247874662ef60b09a
Christopher Sean Morrison <brlcad@gmail.com>
2022-01-25 22:21:55 +0000

    remove temp debugging logic
    introduced a new environment variable and encountered a maintenance cost.  also,
    shouldn't be needed as a breakpoint can be set on the error statement instead of
    adding a manual pause.  alternatively, we could dump a stacktrace (with or without
    bombing) to catch them or do what libc does and simply ignore them if they are no
    longer useful to us.
    
    (cherry picked from commit 092a8e66e270d0cfd82b17c89e35ca635caabfe6)

--- src/libbu/malloc.c
+++ src/libbu/malloc.c
@@ -183,24 +183,7 @@ bu_free(void *ptr, const char *str)
 	str = nul;
 
     if (UNLIKELY(ptr == (char *)0 || ptr == (char *)(-1L))) {
-	const char *msg_sleep_time_str = NULL;
 	fprintf(stderr, "%p free ERROR %s\n", ptr, str);
-	/* There is a class of parallel failures that produce attempts to free
-	 * NULL values, but are difficult to reproduce.  To get a debugger on
-	 * those cases to see how we got there, we need time.  If we hit this
-	 * case, spend a little extra time to see if an environment variable is
-	 * set to a time value in seconds.  If it is set, sleep to give the
-	 * user time to attach the debugger. */
-	msg_sleep_time_str = getenv("BU_NIL_FREE_SLEEP_SEC");
-	if (msg_sleep_time_str) {
-	    unsigned long stime = 0;
-	    char *end;
-	    errno = 0;
-	    stime = strtol(msg_sleep_time_str, &end, 10);
-	    if (errno != ERANGE && errno != EINVAL && stime > 0 && stime < UINT_MAX) {
-		sleep((unsigned int)stime);
-	    }
-	}
 	return;
     }
 
