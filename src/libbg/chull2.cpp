30cb043c47909ffc30876b1a738a886a1ce47dbf
Sean Morrison <brlcad@gmail.com>
2022-01-27 13:00:55 -0500

    ahh, need cast because these are an array of points
    (cherry picked from commit 12be71b56b5a5bd74b1eb469f8b09790c96e12bf)

--- src/libbg/chull2.cpp
+++ src/libbg/chull2.cpp
@@ -185,7 +185,7 @@ bg_2d_chull2(int **hull, const point2d_t *points_2d, int n)
 	(*hull)[i] = p2int[pointers[pchull[i]]];
     }
 
-    bu_argv_free(n, pointers);
+    bu_argv_free(n, (char **)pointers);
     bu_free(psorted, "free sorted points");
     bu_free(pchull, "free unmapped hull");
 
