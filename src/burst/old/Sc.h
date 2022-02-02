2fad64474f58328acaf1c990bb825a0a126b8fa1
Sean Morrison <brlcad@gmail.com>
2022-02-02 14:20:19 -0500

    Revert "Remove old burst code"
    This reverts commit d0e5cc6b2855e397e133ec299642b0e3fc72e01a.
    
    since burst's outputs are inputs to another tool outside our control,
    removing burst is not minimally impacting unless/until we demonstrate
    it produces the same output.  there's no unit tests demonstrating this
    yet.  there is a regression test, but it doesn't do a comparison.
    
    if output and behavior has been manually confirmed, then we should
    document what did change (via manpage updates and NEWS any changes)
    and name the new binary burst instead of nburst to avoid affecting
    downstream more than necessary.

---
+++ src/burst/old/Sc.h
@@ -0,0 +1,167 @@
+/*                            S C . H
+ * BRL-CAD
+ *
+ * Copyright (c) 2004-2021 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ *
+ */
+/** @file burst/Sc.h
+ *
+ */
+
+/**
+   <Sc.h> -- MUVES "Sc" (Screen manager) package definitions
+**/
+
+#ifndef BURST_SC_H
+#define BURST_SC_H
+#include "./burst.h"
+
+/**
+   int ScInit(FILE *fp)
+
+   ScInit() must be invoked before any other function in the Sc
+   package.  Stream fp must be open for writing and all terminal
+   control sequences will be sent to fp, giving the application
+   the option of using STDOUT for other things.  Besides setting
+   the output stream, ScInit() does the following:
+
+   Initializes the terminal.
+
+   Fills terminal name and capabilities into these externals:
+
+   char ScTermname[ScTERMSIZ]	(terminal name from $TERM)
+   char ScTermcap[ScTCAPSIZ]	(terminal capabilities entry)
+
+   Gets terminal control strings into external variables (here
+   are some that don't have individual functions to output them,
+   the ones that DO are below):
+
+   char *ScBC (backspace character)
+   char *ScPC (padding character)
+   char *ScUP (move the cursor up one line)
+   char *ScTI (initialize the terminal)
+
+   Gets individual terminal parameters into these externals:
+
+   int ScLI (number of lines on screen)
+   int ScCO (number of columns on screen)
+
+   Returns "1" for success, "0" for failure and prints
+   appropriate diagnostics on STDERR if $TERM is not set or
+   there is a problem in retrieving the corresponding termcap
+   entry.
+**/
+    extern int ScInit(FILE *fp);
+
+/**
+
+Below are functions paired with terminal control strings that
+they output to the stream specified with ScInit().  It is not
+recommended that the control strings be used directly, but it
+may be useful in certain applications to check their value;
+if ScInit() has not been invoked or the corresponding terminal
+capability does not exist, its control string will be NULL,
+AND the function will return "false".  Otherwise, they will
+return "true" (assuming that it worked).  There is no way to
+be sure of this.
+
+char *ScCE (clear from under the cursor to end of line)
+int ScClrEOL(void)
+
+char *ScCS (change scrolling region)
+int ScClrScrlReg(void)
+
+char *ScSE (end standout mode)
+int ScClrStandout(void)
+
+char *ScCL (clear screen, and home cursor)
+int ScClrText(void)
+
+char *ScAL (insert a line under the cursor)
+int ScInsertLn(void)
+
+char *ScDL (delete the line under the cursor)
+int ScDeleteLn(void)
+
+char *ScSR (scroll text backwards 1 line)
+int ScDnScroll(void)
+
+char *ScHO (move cursor to top-left corner of screen)
+int ScHmCursor(void)
+
+char *ScCM (move cursor to column and row <x, y>)
+int ScMvCursor(x, y)
+
+char *ScCS (set scrolling region from top to btm incl.)
+int ScSetScrlReg(top, btm)
+
+char *ScSO (begin standout mode)
+int ScSetStandout(void)
+
+char *ScSF (scroll text forwards 1 line)
+int ScUpScroll(void)
+
+**/
+extern char *ScBC;
+extern char *ScPC;
+extern char *ScUP;
+extern char *ScCS;
+extern char *ScSO;
+extern char *ScSE;
+extern char *ScCE;
+extern char *ScCL;
+extern char *ScHO;
+extern char *ScCM;
+extern char *ScTI;
+extern char *ScDL;
+extern char *ScSR;
+extern char *ScSF;
+
+extern int ScLI;
+extern int ScCO;
+
+extern int ScClrEOL(void);
+extern int ScClrScrlReg(void);
+extern int ScClrStandout(void);
+extern int ScClrText(void);
+extern int ScDeleteLn(void);
+extern int ScDnScroll(void);
+extern int ScHmCursor(void);
+extern int ScInsertLn(void);
+extern int ScMvCursor(int x, int y);
+extern int ScSetScrlReg(int top, int btm);
+extern int ScSetStandout(void);
+extern int ScUpScroll(void);
+
+#define ScTCAPSIZ 1024
+#define ScTERMSIZ 80
+
+extern char ScTermcap[];
+extern char ScTermname[];
+
+#endif		/* BURST_SC_H */
+
+
+/*
+ * Local Variables:
+ * mode: C
+ * tab-width: 8
+ * indent-tabs-mode: t
+ * c-file-style: "stroustrup"
+ * End:
+ * ex: shiftwidth=4 tabstop=8
+ */
---
+++ src/burst/old/trie.c
@@ -0,0 +1,262 @@
+/*                          T R I E . C
+ * BRL-CAD
+ *
+ * Copyright (c) 2004-2021 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ *
+ */
+/** @file burst/trie.c
+ *
+ */
+
+#include "common.h"
+
+#include <assert.h>
+#include <stdio.h>
+#include <signal.h>
+
+#include "bu/log.h"
+
+#include "./burst.h"
+#include "./extern.h"
+#include "./ascii.h"
+
+
+static Func *matchTrie();
+
+/*
+  Trie *addTrie(char *name, Trie **triepp)
+
+  Insert the name in the trie specified by triepp, if it
+  doesn't already exist there.
+  Return pointer to leaf node associated with name.
+*/
+Trie *
+addTrie(char *name, Trie **triepp)
+{
+    Trie *curp;
+    if (*name == NUL) {
+	/* End of name, see if name already exists. */
+	if (*triepp == TRIE_NULL) {
+	    /* Name does not exist, make leaf node. */
+	    NewTrie(*triepp);
+	    (*triepp)->l.t_altr = (*triepp)->l.t_next
+		= TRIE_NULL;
+	    (*triepp)->l.t_func = NULL_FUNC;
+	} else
+	    if ((*triepp)->n.t_next != TRIE_NULL)
+		/* Name is subset of another name. */
+		return addTrie(name, &(*triepp)->n.t_altr);
+	/* else -- Name already inserted, so do nothing. */
+	return *triepp;
+    }
+    /* Find matching letter, this level.  */
+    for (curp = *triepp;
+	 curp != TRIE_NULL && *name != curp->n.t_char;
+	 curp = curp->n.t_altr
+	)
+	;
+    if (curp == TRIE_NULL) {
+	/* No Match, this level, so create new alternate. */
+	curp = *triepp;
+	NewTrie(*triepp);
+	(*triepp)->n.t_altr = curp;
+	(*triepp)->n.t_char = *name;
+	(*triepp)->n.t_next = TRIE_NULL;
+	return addTrie(++name, &(*triepp)->n.t_next);
+    } else
+	/* Found matching character. */
+	return addTrie(++name, &curp->n.t_next);
+}
+
+
+Func *
+getTrie(char *name, Trie *triep)
+{
+    Trie *curp = NULL;
+    assert(triep != TRIE_NULL);
+
+    /* Traverse next links to end of region name. */
+    for (; triep != TRIE_NULL; triep = triep->n.t_next) {
+	curp = triep;
+	if (*name == NUL) {
+	    /* End of user-typed name. */
+	    if (triep->n.t_altr != TRIE_NULL)
+		/* Ambiguous at this point. */
+		return NULL_FUNC;
+	    else {
+		/* Complete next character. */
+		*name++ = triep->n.t_char;
+		*name = NUL;
+	    }
+	} else
+	    if (*name == '*')
+		return matchTrie(triep);
+	    else	/* Not at end of user-typed name yet, traverse
+			   alternate list to find current letter.
+			*/ {
+		for (;
+		     triep != TRIE_NULL
+			 &&	*name != triep->n.t_char;
+		     triep = triep->n.t_altr
+		    )
+		    ;
+		if (triep == TRIE_NULL) {
+		    /* Non-existent name, truncate bad part. */
+		    *name = NUL;
+		    return NULL_FUNC;
+		} else
+		    name++;
+	    }
+    }
+    /* Clobber key-stroke, and return it. */
+    --name;
+    *name = NUL;
+    if (curp == TRIE_NULL) {
+	bu_exit(BRLCAD_ERROR, "curp == TRIE_NULL, trie.c line %d\n", __LINE__);
+    }
+    return curp->l.t_func;
+}
+
+
+#define MAX_TRIE_LEVEL (32*16)
+
+static Func *
+matchTrie(Trie *triep)
+{
+    Func *func;
+    if (triep == TRIE_NULL)
+	func = NULL_FUNC;
+    else
+	if (triep->n.t_altr != TRIE_NULL)
+	    func = NULL_FUNC;	/* Ambiguous root, no match.  */
+	else
+	    if (triep->n.t_next == TRIE_NULL)
+		func = triep->l.t_func;	/* At leaf node, return datum.  */
+	    else				/* Keep going to leaf.  */
+		func = matchTrie(triep->n.t_next);
+    return func;
+}
+
+
+void
+prntTrie(Trie *triep, int level)
+{
+    Trie *tp = triep;
+    static char name_buf[MAX_TRIE_LEVEL+1];
+    static char *namep = NULL;
+
+    if (tp == TRIE_NULL)
+	return;
+    if (tp->n.t_altr != TRIE_NULL)
+	prntTrie(tp->n.t_altr, level);
+    if (level == 0)
+	namep = name_buf;
+    if (namep)
+	*namep = tp->n.t_char;
+    if (tp->n.t_next == TRIE_NULL) {
+	/* At end of name, so print it out. */
+	if (namep)
+	    *namep = NUL;
+	brst_log("%s\n", name_buf);
+    } else {
+	if (namep)
+	    namep++;
+	prntTrie(tp->n.t_next, level+1);
+	if (namep)
+	    namep--;
+    }
+    return;
+}
+
+
+int
+writeTrie(Trie *triep, int level, FILE *fp)
+{
+    Trie *tp = triep;
+    static char name_buf[MAX_TRIE_LEVEL+1], *namep = NULL;
+    if (tp == TRIE_NULL)
+	return 1;
+    if (tp->n.t_altr != TRIE_NULL)
+	(void) writeTrie(tp->n.t_altr, level, fp);
+    if (level == 0)
+	namep = name_buf;
+    if (!namep)
+	return 1;
+    *namep = tp->n.t_char;
+    if (tp->n.t_next == TRIE_NULL) {
+	/* At end of name, so print it out. */
+	*namep = NUL;
+	(void) fprintf(fp, "%s\n", name_buf);
+    } else {
+	namep++;
+	(void) writeTrie(tp->n.t_next, level+1, fp);
+	namep--;
+    }
+    return 1;
+}
+
+
+int
+readTrie(FILE *fp, Trie **triepp)
+{
+    static char name_buf[MAX_TRIE_LEVEL+1];
+    while (bu_fgets(name_buf, MAX_TRIE_LEVEL, fp) != NULL) {
+	name_buf[strlen(name_buf)-1] = '\0'; /* Clobber new-line. */
+	(void) addTrie(name_buf, triepp);
+    }
+    return 1;
+}
+
+
+void
+ring_Bell()
+{
+    (void) putchar(BEL);
+    return;
+}
+
+
+char *
+char_To_String(int i)
+{
+    static char buf[4];
+    if (i >= SP && i < DEL) {
+	buf[0] = i;
+	buf[1] = NUL;
+    } else
+	if (i >= NUL && i < SP) {
+	    buf[0] = '^';
+	    buf[1] = i + 64;
+	    buf[2] = NUL;
+	} else
+	    if (i == DEL)
+		return "DL";
+	    else
+		return "EOF";
+    return buf;
+}
+
+
+/*
+ * Local Variables:
+ * mode: C
+ * tab-width: 8
+ * indent-tabs-mode: t
+ * c-file-style: "stroustrup"
+ * End:
+ * ex: shiftwidth=4 tabstop=8
+ */
