0f10e5b1c2e2884dc474ca933d2a1b96a25d18c9
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2022-02-24 20:32:39 -0500

    Try to adjust the xxhash logic for Coverity issues
    New Coverity scanner doesn't like some of the individual XXH64_update
    calls, reporting out of bounds access in the internals.  At a glance
    it looks like small objects may be part of the problem?  See if we
    can simply feed the structs in whole to the update routine, rater than
    calling out the individual slots.  If the updating works correctly this
    would be much better anyway, since we could adjust the structs more
    easily...
    
    If this either doesn't work for view updating or doesn't clear the
    issue, I'll have to dissect more carefully what the internals of the
    xxhash routine are wanting as input...

--- src/libbv/hash.c
+++ src/libbv/hash.c
@@ -43,28 +43,7 @@ _bv_adc_state_hash(XXH64_state_t *state, struct bv_adc_state *v)
     if (!v || !state)
 	return;
 
-    XXH64_update(state, &v->draw, sizeof(int));
-    XXH64_update(state, &v->dv_x, sizeof(int));
-    XXH64_update(state, &v->dv_y, sizeof(int));
-    XXH64_update(state, &v->dv_a1, sizeof(int));
-    XXH64_update(state, &v->dv_a2, sizeof(int));
-    XXH64_update(state, &v->dv_dist, sizeof(int));
-    XXH64_update(state, &v->pos_model, sizeof(fastf_t[3]));
-    XXH64_update(state, &v->pos_view, sizeof(fastf_t[3]));
-    XXH64_update(state, &v->pos_grid, sizeof(fastf_t[3]));
-    XXH64_update(state, &v->a1, sizeof(fastf_t));
-    XXH64_update(state, &v->a2, sizeof(fastf_t));
-    XXH64_update(state, &v->dst, sizeof(fastf_t));
-    XXH64_update(state, &v->anchor_pos, sizeof(int));
-    XXH64_update(state, &v->anchor_a1, sizeof(int));
-    XXH64_update(state, &v->anchor_a2, sizeof(int));
-    XXH64_update(state, &v->anchor_dst, sizeof(int));
-    XXH64_update(state, &v->anchor_pt_a1, sizeof(fastf_t[3]));
-    XXH64_update(state, &v->anchor_pt_a2, sizeof(fastf_t[3]));
-    XXH64_update(state, &v->anchor_pt_dst, sizeof(fastf_t[3]));
-    XXH64_update(state, &v->line_color, sizeof(int[3]));
-    XXH64_update(state, &v->tick_color, sizeof(int[3]));
-    XXH64_update(state, &v->line_width, sizeof(int));
+    XXH64_update(state, v, sizeof(struct bv_adc_state));
 }
 
 static void
@@ -74,22 +53,7 @@ _bv_axes_hash(XXH64_state_t *state, struct bv_axes *v)
     if (!v || !state)
 	return;
 
-    XXH64_update(state, &v->draw, sizeof(int));
-    XXH64_update(state, &v->axes_pos, sizeof(point_t));
-    XXH64_update(state, &v->axes_size, sizeof(fastf_t));
-    XXH64_update(state, &v->line_width, sizeof(int));
-    XXH64_update(state, &v->pos_only, sizeof(int));
-    XXH64_update(state, &v->axes_color, sizeof(int[3]));
-    XXH64_update(state, &v->label_color, sizeof(int[3]));
-    XXH64_update(state, &v->triple_color, sizeof(int));
-    XXH64_update(state, &v->tick_enabled, sizeof(int));
-    XXH64_update(state, &v->tick_length, sizeof(int));
-    XXH64_update(state, &v->tick_major_length, sizeof(int));
-    XXH64_update(state, &v->tick_interval, sizeof(fastf_t));
-    XXH64_update(state, &v->ticks_per_major, sizeof(int));
-    XXH64_update(state, &v->tick_threshold, sizeof(int));
-    XXH64_update(state, &v->tick_color, sizeof(int[3]));
-    XXH64_update(state, &v->tick_major_color, sizeof(int[3]));
+    XXH64_update(state, v, sizeof(struct bv_axes));
 }
 
 static void
@@ -99,15 +63,7 @@ _bv_data_arrow_state_hash(XXH64_state_t *state, struct bv_data_arrow_state *v)
     if (!v || !state)
 	return;
 
-    XXH64_update(state, &v->gdas_draw, sizeof(int));
-    XXH64_update(state, &v->gdas_color, sizeof(int[3]));
-    XXH64_update(state, &v->gdas_line_width, sizeof(int));
-    XXH64_update(state, &v->gdas_tip_length, sizeof(int));
-    XXH64_update(state, &v->gdas_tip_width, sizeof(int));
-    XXH64_update(state, &v->gdas_num_points, sizeof(int));
-    for (int i = 0; i < v->gdas_num_points; i++) {
-	XXH64_update(state, &v->gdas_points[i], sizeof(point_t));
-    }
+    XXH64_update(state, v, sizeof(struct bv_data_arrow_state));
 }
 
 static void
@@ -117,14 +73,9 @@ _bv_data_axes_state_hash(XXH64_state_t *state, struct bv_data_axes_state *v)
     if (!v || !state)
 	return;
 
-    XXH64_update(state, &v->draw, sizeof(int));
-    XXH64_update(state, &v->color, sizeof(int[3]));
-    XXH64_update(state, &v->line_width, sizeof(int));
-    XXH64_update(state, &v->size, sizeof(fastf_t));
-    XXH64_update(state, &v->num_points, sizeof(int));
-    for (int i = 0; i < v->num_points; i++) {
-	XXH64_update(state, &v->points[i], sizeof(point_t));
-    }
+    XXH64_update(state, v, sizeof(struct bv_data_axes_state));
+    if (v->num_points)
+	XXH64_update(state, v->points, v->num_points * sizeof(point_t));
 }
 
 static void
@@ -134,16 +85,12 @@ _bv_data_label_state_hash(XXH64_state_t *state, struct bv_data_label_state *v)
     if (!v || !state)
 	return;
 
-    XXH64_update(state, &v->gdls_draw, sizeof(int));
-    XXH64_update(state, &v->gdls_color, sizeof(int[3]));
-    XXH64_update(state, &v->gdls_num_labels, sizeof(int));
-    XXH64_update(state, &v->gdls_size, sizeof(int));
+    XXH64_update(state, v, sizeof(struct bv_data_label_state));
+    if (v->gdls_size)
+	XXH64_update(state, v->gdls_points, v->gdls_size * sizeof(point_t));
     for (int i = 0; i < v->gdls_num_labels; i++) {
 	if (strlen(v->gdls_labels[i]))
 	    XXH64_update(state, v->gdls_labels[i], strlen(v->gdls_labels[i]));
-    }
-    for (int i = 0; i < v->gdls_size; i++) {
-	XXH64_update(state, &v->gdls_points[i], sizeof(point_t));
     }
 }
 
@@ -155,13 +102,9 @@ _bv_data_line_state_hash(XXH64_state_t *state, struct bv_data_line_state *v)
     if (!v || !state)
 	return;
 
-    XXH64_update(state, &v->gdls_draw, sizeof(int));
-    XXH64_update(state, &v->gdls_color, sizeof(int[3]));
-    XXH64_update(state, &v->gdls_line_width, sizeof(int));
-    XXH64_update(state, &v->gdls_num_points, sizeof(int));
-    for (int i = 0; i < v->gdls_num_points; i++) {
-	XXH64_update(state, &v->gdls_points[i], sizeof(point_t));
-    }
+    XXH64_update(state, v, sizeof(struct bv_data_line_state));
+    if (v->gdls_num_points)
+	XXH64_update(state, v->gdls_points, v->gdls_num_points * sizeof(point_t));
 }
 
 static void
@@ -171,10 +114,9 @@ _bg_poly_contour_hash(XXH64_state_t *state, struct bg_poly_contour *v)
     if (!v || !state)
 	return;
 
-    XXH64_update(state, &v->num_points, sizeof(size_t));
-    for (size_t i = 0; i < v->num_points; i++) {
-	XXH64_update(state, &v->point[i], sizeof(point_t));
-    }
+    XXH64_update(state, v, sizeof(struct bg_poly_contour));
+    if (v->num_points)
+	XXH64_update(state, v->point, v->num_points * sizeof(point_t));
 }
 
 static void
@@ -184,18 +126,13 @@ _bg_polygon_hash(XXH64_state_t *state, struct bg_polygon *v)
     if (!v || !state)
 	return;
 
-    XXH64_update(state, &v->num_contours, sizeof(size_t));
-    XXH64_update(state, &v->gp_color, sizeof(int[3]));
-    XXH64_update(state, &v->gp_line_width, sizeof(int));
-    XXH64_update(state, &v->gp_line_style, sizeof(int));
+    XXH64_update(state, v, sizeof(struct bg_polygon));
 
     for (size_t i = 0; i < v->num_contours; i++) {
 	_bg_poly_contour_hash(state, &v->contour[i]);
     }
-    if (v->hole) {
-	for (size_t i = 0; i < v->num_contours; i++) {
-	    XXH64_update(state, &v->hole[i], sizeof(int));
-	}
+    if (v->hole && v->num_contours) {
+	XXH64_update(state, v->hole, v->num_contours * sizeof(int));
     }
 }
 
@@ -206,7 +143,7 @@ _bg_polygons_hash(XXH64_state_t *state, struct bg_polygons *v)
     if (!v || !state)
 	return;
 
-    XXH64_update(state, &v->num_polygons, sizeof(size_t));
+    XXH64_update(state, v, sizeof(struct bg_polygons));
     for (size_t i = 0; i < v->num_polygons; i++) {
 	_bg_polygon_hash(state, &v->polygon[i]);
     }
@@ -219,24 +156,8 @@ _bv_data_polygon_state_hash(XXH64_state_t *state, bv_data_polygon_state *v)
     if (!v || !state)
 	return;
 
-    XXH64_update(state, &v->gdps_draw, sizeof(int));
-    XXH64_update(state, &v->gdps_moveAll, sizeof(int));
-    XXH64_update(state, &v->gdps_color, sizeof(int[3]));
-    XXH64_update(state, &v->gdps_line_width, sizeof(int));
-    XXH64_update(state, &v->gdps_line_style, sizeof(int));
-    XXH64_update(state, &v->gdps_cflag, sizeof(int));
-    XXH64_update(state, &v->gdps_target_polygon_i, sizeof(size_t));
-    XXH64_update(state, &v->gdps_curr_polygon_i, sizeof(size_t));
-    XXH64_update(state, &v->gdps_curr_point_i, sizeof(size_t));
-    XXH64_update(state, &v->gdps_prev_point, sizeof(point_t));
-    XXH64_update(state, &v->gdps_clip_type, sizeof(bg_clip_t));
-    XXH64_update(state, &v->gdps_scale, sizeof(fastf_t));
-    XXH64_update(state, &v->gdps_origin, sizeof(point_t));
-    XXH64_update(state, &v->gdps_rotation, sizeof(mat_t));
-    XXH64_update(state, &v->gdps_view2model, sizeof(mat_t));
-    XXH64_update(state, &v->gdps_model2view, sizeof(mat_t));
+    XXH64_update(state, v, sizeof(bv_data_polygon_state));
     _bg_polygons_hash(state, &v->gdps_polygons);
-    XXH64_update(state, &v->gdps_data_vZ, sizeof(fastf_t));
 }
 
 static void
@@ -246,15 +167,7 @@ _bv_grid_state_hash(XXH64_state_t *state, struct bv_grid_state *v)
     if (!v || !state)
 	return;
 
-    XXH64_update(state, &v->rc, sizeof(int));
-    XXH64_update(state, &v->draw, sizeof(int));
-    XXH64_update(state, &v->snap, sizeof(int));
-    XXH64_update(state, &v->anchor, sizeof(fastf_t[3]));
-    XXH64_update(state, &v->res_h, sizeof(fastf_t));
-    XXH64_update(state, &v->res_v, sizeof(fastf_t));
-    XXH64_update(state, &v->res_major_h, sizeof(int));
-    XXH64_update(state, &v->res_major_v, sizeof(int));
-    XXH64_update(state, &v->color, sizeof(int[3]));
+    XXH64_update(state, v, sizeof(struct bv_grid_state));
 }
 
 static void
@@ -264,9 +177,7 @@ _bv_other_state_hash(XXH64_state_t *state, struct bv_other_state *v)
     if (!v || !state)
 	return;
 
-    XXH64_update(state, &v->gos_draw, sizeof(int));
-    XXH64_update(state, &v->gos_line_color, sizeof(int[3]));
-    XXH64_update(state, &v->gos_text_color, sizeof(int[3]));
+    XXH64_update(state, v, sizeof(struct bv_other_state));
 }
 
 
@@ -277,20 +188,7 @@ _bv_interactive_rect_state_hash(XXH64_state_t *state, struct bv_interactive_rect
     if (!v || !state)
 	return;
 
-    XXH64_update(state, &v->active, sizeof(int));
-    XXH64_update(state, &v->draw, sizeof(int));
-    XXH64_update(state, &v->line_width, sizeof(int));
-    XXH64_update(state, &v->line_style, sizeof(int));
-    XXH64_update(state, &v->pos, sizeof(int[2]));
-    XXH64_update(state, &v->dim, sizeof(int[2]));
-    XXH64_update(state, &v->x, sizeof(fastf_t));
-    XXH64_update(state, &v->y, sizeof(fastf_t));
-    XXH64_update(state, &v->width, sizeof(fastf_t));
-    XXH64_update(state, &v->height, sizeof(fastf_t));
-    XXH64_update(state, &v->bg, sizeof(int[3]));
-    XXH64_update(state, &v->color, sizeof(int[3]));
-    XXH64_update(state, &v->cdim, sizeof(int[2]));
-    XXH64_update(state, &v->aspect, sizeof(fastf_t));
+    XXH64_update(state, v, sizeof(struct bv_interactive_rect_state));
 }
 
 static void
@@ -300,50 +198,21 @@ _bv_obj_settings_hash(XXH64_state_t *state, struct bv_obj_settings *v)
     if (!v || !state)
 	return;
 
-    XXH64_update(state, &v->draw_non_subtract_only, sizeof(int));
-    XXH64_update(state, &v->draw_solid_lines_only, sizeof(int));
-    XXH64_update(state, &v->s_arrow_tip_length, sizeof(fastf_t));
-    XXH64_update(state, &v->s_arrow_tip_width, sizeof(fastf_t));
-    XXH64_update(state, &v->s_dmode, sizeof(int));
-    XXH64_update(state, &v->s_line_width, sizeof(int));
-    XXH64_update(state, &v->transparency, sizeof(fastf_t));
+    XXH64_update(state, v, sizeof(struct bv_obj_settings));
 }
 
 void
 bv_scene_obj_hash(XXH64_state_t *state, struct bv_scene_obj *s)
 {
-    XXH64_update(state, &s->s_size, sizeof(fastf_t));
-    XXH64_update(state, &s->s_csize, sizeof(fastf_t));
-    XXH64_update(state, &s->s_center, sizeof(vect_t));
-    XXH64_update(state, &s->s_displayobj, sizeof(int));
+    /* First, do sanity checks */
+    if (!s || !state)
+	return;
+
+    XXH64_update(state, s, sizeof(struct bv_scene_obj));
     struct bv_vlist *tvp;
     for (BU_LIST_FOR(tvp, bv_vlist, &((struct bv_vlist *)&s->s_vlist)->l)) {
-	XXH64_update(state, &tvp->nused, sizeof(size_t));
-	XXH64_update(state, &tvp->cmd, sizeof(int[BV_VLIST_CHUNK]));
-	XXH64_update(state, &tvp->pt, sizeof(point_t[BV_VLIST_CHUNK]));
+	XXH64_update(state, tvp, sizeof(struct bv_vlist));
     }
-    XXH64_update(state, &s->s_vlen, sizeof(int));
-    XXH64_update(state, &s->s_flag, sizeof(char));
-    XXH64_update(state, &s->s_iflag, sizeof(char));
-    XXH64_update(state, &s->s_arrow, sizeof(int));
-    XXH64_update(state, &s->s_soldash, sizeof(char));
-    XXH64_update(state, &s->s_old.s_Eflag, sizeof(char));
-    XXH64_update(state, &s->s_old.s_uflag, sizeof(char));
-    XXH64_update(state, &s->s_old.s_dflag, sizeof(char));
-    XXH64_update(state, &s->s_old.s_cflag, sizeof(char));
-    XXH64_update(state, &s->s_old.s_wflag, sizeof(char));
-    XXH64_update(state, &s->s_changed, sizeof(int));
-    XXH64_update(state, &s->s_old.s_basecolor, sizeof(unsigned char[3]));
-    XXH64_update(state, &s->s_color, sizeof(unsigned char[3]));
-    XXH64_update(state, &s->s_old.s_regionid, sizeof(short));
-    XXH64_update(state, &s->s_dlist, sizeof(unsigned int));
-    XXH64_update(state, &s->s_mat, sizeof(mat_t));
-    XXH64_update(state, &s->adaptive_wireframe, sizeof(int));
-    XXH64_update(state, &s->view_scale, sizeof(fastf_t));
-    XXH64_update(state, &s->bot_threshold, sizeof(size_t));
-    XXH64_update(state, &s->curve_scale, sizeof(fastf_t));
-    XXH64_update(state, &s->point_scale, sizeof(fastf_t));
-
     _bv_obj_settings_hash(state, &s->s_os);
 }
 
@@ -368,8 +237,8 @@ bv_dl_hash(struct display_list *dl)
     while (BU_LIST_NOT_HEAD(gdlp, dl)) {
 	next_gdlp = BU_LIST_PNEXT(display_list, gdlp);
 
+	XXH64_update(state, gdlp, sizeof(struct display_list));
 	XXH64_update(state, bu_vls_cstr(&gdlp->dl_path), bu_vls_strlen(&gdlp->dl_path));
-	XXH64_update(state, &gdlp->dl_wflag, sizeof(int));
 
 	for (BU_LIST_FOR(sp, bv_scene_obj, &gdlp->dl_head_scene_obj)) {
 	    bv_scene_obj_hash(state, sp);
@@ -387,29 +256,17 @@ bv_dl_hash(struct display_list *dl)
 void
 bv_settings_hash(XXH64_state_t *state, struct bview_settings *s)
 {
-    XXH64_update(state, &s->adaptive_plot, sizeof(int));
-    XXH64_update(state, &s->bot_threshold, sizeof(size_t));
-    XXH64_update(state, &s->curve_scale, sizeof(fastf_t));
-    XXH64_update(state, &s->point_scale, sizeof(fastf_t));
-    XXH64_update(state, &s->redraw_on_zoom, sizeof(int));
+    XXH64_update(state, s, sizeof(struct bview_settings));
 
     _bv_obj_settings_hash(state, &s->obj_s);
-    XXH64_update(state, &s->gv_zclip, sizeof(int));
-    XXH64_update(state, &s->gv_cleared, sizeof(int));
     _bv_adc_state_hash(state, &s->gv_adc);
     _bv_axes_hash(state, &s->gv_model_axes);
     _bv_axes_hash(state, &s->gv_view_axes);
-
-    XXH64_update(state, &s->gv_snap_lines, sizeof(int));
-    XXH64_update(state, &s->gv_snap_tol_factor, sizeof(double));
     _bv_grid_state_hash(state, &s->gv_grid);
     _bv_other_state_hash(state, &s->gv_center_dot);
-
     _bv_other_state_hash(state, &s->gv_view_params);
     _bv_other_state_hash(state, &s->gv_view_scale);
     _bv_interactive_rect_state_hash(state, &s->gv_rect);
-    XXH64_update(state, &s->gv_fps, sizeof(int));
-    XXH64_update(state, &s->gv_fb_mode, sizeof(int));
 
 #if 0
     for (size_t i = 0; i < BU_PTBL_LEN(v->gv_selected); i++) {
@@ -434,35 +291,10 @@ bv_hash(struct bview *v)
     XXH64_reset(state, 0);
 
     // Deliberately not checking name - a rename doesn't change the view
-    XXH64_update(state, &v->gv_i_scale, sizeof(fastf_t));
-    XXH64_update(state, &v->gv_a_scale, sizeof(fastf_t));
-    XXH64_update(state, &v->gv_scale, sizeof(fastf_t));
-    XXH64_update(state, &v->gv_size, sizeof(fastf_t));
-    XXH64_update(state, &v->gv_isize, sizeof(fastf_t));
-    XXH64_update(state, &v->gv_perspective, sizeof(fastf_t));
-    XXH64_update(state, &v->gv_aet, sizeof(vect_t));
-    XXH64_update(state, &v->gv_eye_pos, sizeof(vect_t));
-    XXH64_update(state, &v->gv_keypoint, sizeof(vect_t));
-    XXH64_update(state, &v->gv_coord, sizeof(char));
-    XXH64_update(state, &v->gv_rotate_about, sizeof(char));
-    XXH64_update(state, &v->gv_rotation, sizeof(mat_t));
-    XXH64_update(state, &v->gv_center, sizeof(mat_t));
-    XXH64_update(state, &v->gv_model2view, sizeof(mat_t));
-    XXH64_update(state, &v->gv_pmodel2view, sizeof(mat_t));
-    XXH64_update(state, &v->gv_view2model, sizeof(mat_t));
-    XXH64_update(state, &v->gv_pmat, sizeof(mat_t));
-    XXH64_update(state, &v->gv_width, sizeof(int));
-    XXH64_update(state, &v->gv_height, sizeof(int));
-    XXH64_update(state, &v->gv_prevMouseX, sizeof(fastf_t));
-    XXH64_update(state, &v->gv_prevMouseY, sizeof(fastf_t));
-    XXH64_update(state, &v->gv_minMouseDelta, sizeof(fastf_t));
-    XXH64_update(state, &v->gv_maxMouseDelta, sizeof(fastf_t));
-    XXH64_update(state, &v->gv_rscale, sizeof(fastf_t));
-    XXH64_update(state, &v->gv_sscale, sizeof(fastf_t));
-    XXH64_update(state, &v->gv_data_vZ, sizeof(fastf_t));
+    XXH64_update(state, v, sizeof(struct bview));
 
     if (v->gv_s)
-	bv_settings_hash(state, &v->gv_ls);
+	bv_settings_hash(state, v->gv_s);
     bv_settings_hash(state, &v->gv_ls);
 
     _bv_data_arrow_state_hash(state, &v->gv_tcl.gv_data_arrows);
