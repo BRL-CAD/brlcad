3e12e70257d5c0e78a1cd0d11a22e0d33ed756b5
David Reeves <ddreeves70@gmail.com>
2022-03-28 13:15:17 -0500

    wip

---
+++ include/qtcad/Application.h
@@ -0,0 +1,33 @@
+#ifndef QGED_APPLICATION_H
+#define QGED_APPLICATION_H
+
+#include "MainWindow.h"
+#include <QApplication>
+#include <QMap>
+
+/* Command type for application level commands */
+typedef int (*appCommandFunc)(void *, int, const char **);
+
+namespace qtcad {
+
+class Application : public QApplication
+{
+    Q_OBJECT
+  public:
+    Application(int argc, char **argv, int pConsoleMode, int pSwrastMode, int pQuadMode);
+
+  public slots:
+    void mainWindowClosing();
+
+  signals:
+    void closing();
+
+  private:
+    void initialize();
+
+    MainWindow *mainWindow;
+};
+
+} // namespace qged
+
+#endif // QGED_APPLICATION_H
