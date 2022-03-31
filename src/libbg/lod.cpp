2cf265d0fcc6651a9d319bd709aec2064bfcc879
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2022-03-31 12:11:06 -0400

    The bounds check is for debugging only.

--- src/libbg/lod.cpp
+++ src/libbg/lod.cpp
@@ -1001,14 +1001,14 @@ POPState::level_pnt(point_t *o, const point_t *p, int level)
     fastf_t nx = snap((*p)[X], minx, maxx, level);
     fastf_t ny = snap((*p)[Y], miny, maxy, level);
     fastf_t nz = snap((*p)[Z], minz, maxz, level);
-
     VSET(*o, nx, ny, nz);
-
+#if 0
     double poffset = DIST_PNT_PNT(*o, *p);
     if (poffset > (maxx - minx) && poffset > (maxy - miny) && poffset > (maxz - minz)) {
 	bu_log("Error: %f %f %f -> %f %f %f\n", V3ARGS(*p), V3ARGS(*o));
 	bu_log("bound: %f %f %f -> %f %f %f\n", minx, miny, minz, maxx, maxy, maxz);
     }
+#endif
 }
 
 // Compares two coordinates for equality (on a given precision level)
