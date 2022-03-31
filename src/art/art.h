5e14c6a5d10a20c7d3222e4b112c042a059caa10
Christopher Sean Morrison <brlcad@gmail.com>
2022-03-30 23:08:41 -0400

    more header/footer/style cleanup

--- src/art/art.h
+++ src/art/art.h
@@ -17,50 +17,33 @@
  * License along with this file; see the file named COPYING for more
  * information.
  */
- /** @file art/artplugin.cpp
-  *
-  * Once you have appleseed installed, run BRL-CAD's CMake with APPLESEED_ROOT
-  * set to enable this program:
-  *
-  * cmake .. -DAPPLESEED_ROOT=/path/to/appleseed -DBRLCAD_PNG=SYSTEM -DBRLCAD_ZLIB=SYSTEM
-  *
-  * (the appleseed root path should contain bin, lib and include directories)
-  *
-  * On Linux, if using the prebuilt binary you'll need to set LD_LIBRARY_PATH:
-  * export LD_LIBRARY_PATH=/path/to/appleseed/lib
-  *
-  *
-  * The example scene object used by helloworld is found at:
-  * https://raw.githubusercontent.com/appleseedhq/appleseed/master/sandbox/examples/cpp/helloworld/data/scene.obj
-  *
-  * basic example helloworld code from
-  * https://github.com/appleseedhq/appleseed/blob/master/sandbox/examples/cpp/helloworld/helloworld.cpp
-  * has the following license:
-  *
-  * This software is released under the MIT license.
-  *
-  * Copyright (c) 2010-2013 Francois Beaune, Jupiter Jazz Limited
-  * Copyright (c) 2014-2018 Francois Beaune, The appleseedhq Organization
-  *
-  * Permission is hereby granted, free of charge, to any person obtaining a copy
-  * of this software and associated documentation files (the "Software"), to deal
-  * in the Software without restriction, including without limitation the rights
-  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-  * copies of the Software, and to permit persons to whom the Software is
-  * furnished to do so, subject to the following conditions:
-  *
-  * The above copyright notice and this permission notice shall be included in
-  * all copies or substantial portions of the Software.
-  *
-  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-  * THE SOFTWARE.
-  *
-  */
+/*********************************************************************
+ *
+ * This software is released under the MIT license.
+ *
+ * Copyright (c) 2010-2013 Francois Beaune, Jupiter Jazz Limited
+ * Copyright (c) 2014-2018 Francois Beaune, The appleseedhq Organization
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+
 
 #include "common.h"
 
@@ -117,18 +100,23 @@ int APPLESEED_DLL_EXPORT brlcad_miss(struct application* UNUSED(ap));
 
 class APPLESEED_DLL_EXPORT BrlcadObject : public asr::ProceduralObject
 {
+
 public:
     BrlcadObject(const char* name, const asr::ParamArray& params);
     BrlcadObject(const char* name, const asr::ParamArray& params, struct application* ap, struct resource* resources);
+
     std::string *name = nullptr;
+
     void release() override;
     const char* get_model() const override;
     bool on_frame_begin(const asr::Project& project, const asr::BaseGroup* parent, asr::OnFrameBeginRecorder& recorder, asf::IAbortSwitch* abort_switch) override;
     asr::GAABB3 compute_local_bbox() const override;
     size_t get_material_slot_count() const override;
     const char* get_material_slot(const size_t index) const override;
+
     void intersect(const asr::ShadingRay& ray, IntersectionResult& result) const override;
     bool intersect(const asr::ShadingRay& ray) const override;
+
     void refine_and_offset(
 	const asf::Ray3d& obj_inst_ray,
 	asf::Vector3d& obj_inst_front_point,
@@ -151,6 +139,7 @@ private:
 
     void configure_raytrace_application(const char* path, int objc, std::vector<std::string> objects);
 };
+
 
 /*
  * Local Variables:
