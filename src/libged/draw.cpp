76b6814cdef1a793504551fec01666d1a47bc4df
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2022-04-04 19:35:33 -0400

    Correct shaded mesh drawing

--- src/libged/draw.cpp
+++ src/libged/draw.cpp
@@ -188,13 +188,14 @@ draw_scene(struct bv_scene_obj *s)
 		    struct rt_bot_internal *bot = (struct rt_bot_internal *)ip->idb_ptr;
 		    RT_BOT_CK_MAGIC(bot);
 
-		    // Basic setup (TODO - make sure we don't rebuild cache every time - just if the key
-		    // lookup fails...)
+		    // Basic setup - build cache if we don't have it and return key
 		    unsigned long long key = bg_mesh_lod_cache((const point_t *)bot->vertices, bot->num_vertices, bot->faces, bot->num_faces);
 		    s->draw_data = (void *)bg_mesh_lod_init(key);
 		    // Initialize the LoD data to the current view
-		    int level = bg_mesh_lod_view((struct bg_mesh_lod *)s->draw_data, s->s_v, 0);
-		    if (bg_mesh_lod_level((struct bg_mesh_lod *)s->draw_data, level) != level) {
+		    struct bv_mesh_lod_info *linfo = (struct bv_mesh_lod_info *)s->draw_data;
+		    struct bg_mesh_lod *l = (struct bg_mesh_lod *)linfo->lod;
+		    int level = bg_mesh_lod_view(l, s->s_v, 0);
+		    if (bg_mesh_lod_level(l, level) != level) {
 			bu_log("Error loading info for level %d\n", level);
 		    }
 
