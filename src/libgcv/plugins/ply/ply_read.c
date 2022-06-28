fd2e50b9108c78c22baf7d4f645ee2372990c779
f4alt <christopherjmacgregor@gmail.com>
2022-06-28 10:07:15 -0400

    shift ply conversion from src/conv to gcv - preliminary move

---
+++ src/libgcv/plugins/ply/ply_read.c
@@ -0,0 +1,35 @@
+/*                       P L Y _ R E A D . C
+ * BRL-CAD
+ *
+ * Copyright (c) 2004-2022 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ *
+ */
+/** @file conv/ply/ply-g.c
+ *
+ * Program to convert the PLY format to the BRL-CAD format.
+ *
+ */
+
+/*
+ * Local Variables:
+ * mode: C
+ * tab-width: 8
+ * indent-tabs-mode: t
+ * c-file-style: "stroustrup"
+ * End:
+ * ex: shiftwidth=4 tabstop=8
+ */
---
+++ src/libgcv/plugins/ply/ply_write.c
@@ -0,0 +1,36 @@
+/*                  P L Y _ W R I T E . C
+ * BRL-CAD
+ *
+ * Copyright (c) 2003-2022 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ *
+ */
+/** @file conv/ply/g-ply.c
+ *
+ * Program to convert a BRL-CAD model (in a .g file) to the Stanford
+ * PLY format by calling on the NMG booleans. Based on g-stl.c.
+ *
+ */
+
+/*
+ * Local Variables:
+ * mode: C
+ * tab-width: 8
+ * indent-tabs-mode: t
+ * c-file-style: "stroustrup"
+ * End:
+ * ex: shiftwidth=4 tabstop=8
+ */
