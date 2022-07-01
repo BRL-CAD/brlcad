4583910b183d8ecbbb512265410725230d35a4e2
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2022-07-01 19:56:55 -0400

    Can't rely on the lod key C++ string internal being stable

--- src/libbg/lod.cpp
+++ src/libbg/lod.cpp
@@ -1365,15 +1365,16 @@ POPState::cache_write(const char *component, std::stringstream &s)
 
     // Write out key/value to LMDB database, where the key is the hash
     // and the value is the serialized LoD data
-    //
+    char *keycstr = bu_strdup(keystr.c_str());
     mdb_txn_begin(c->i->lod_env, NULL, 0, &c->i->lod_txn);
     mdb_dbi_open(c->i->lod_txn, NULL, 0, &c->i->lod_dbi);
     mdb_key.mv_size = keystr.length()*sizeof(char);
-    mdb_key.mv_data = (void *)keystr.c_str();
+    mdb_key.mv_data = (void *)keycstr;
     mdb_data.mv_size = buffer.length();
     mdb_data.mv_data = (void *)buffer.c_str();
     int rc = mdb_put(c->i->lod_txn, c->i->lod_dbi, &mdb_key, &mdb_data, 0);
     mdb_txn_commit(c->i->lod_txn);
+    bu_free(keycstr, "keycstr");
 
     return (!rc) ? true : false;
 }
@@ -1392,16 +1393,18 @@ POPState::cache_get(void **data, const char *component)
     // the default size limit (511)
     //if (keystr.length()*sizeof(char) > mdb_env_get_maxkeysize(c->i->lod_env))
     //	return 0;
-
+    char *keycstr = bu_strdup(keystr.c_str());
     mdb_txn_begin(c->i->lod_env, NULL, 0, &c->i->lod_txn);
     mdb_dbi_open(c->i->lod_txn, NULL, 0, &c->i->lod_dbi);
     mdb_key.mv_size = keystr.length()*sizeof(char);
-    mdb_key.mv_data = (void *)keystr.c_str();
+    mdb_key.mv_data = (void *)keycstr;
     int rc = mdb_get(c->i->lod_txn, c->i->lod_dbi, &mdb_key, &mdb_data);
     if (rc) {
+	bu_free(keycstr, "keycstr");
 	(*data) = NULL;
 	return 0;
     }
+    bu_free(keycstr, "keycstr");
     (*data) = mdb_data.mv_data;
 
     return mdb_data.mv_size;
