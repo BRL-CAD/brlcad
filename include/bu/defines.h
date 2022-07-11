c714aebb0aeea45cd29628eaf510989a3f6343a7
Christopher Sean Morrison <brlcad@gmail.com>
2022-07-11 00:41:47 -0400

    replace _BU_ATTR_ANALYZER_NORETURN with FAUX_NORETURN.
    however, it is redundant and misleading when combined with NORETURN, so remove from current instances.  need to audit if there are other commands that do return but could be annotated as simulating no-return.

--- include/bu/defines.h
+++ include/bu/defines.h
@@ -115,19 +115,6 @@
 
 
 /**
-* The 'analyzer_noreturn' attribute is (currently) clang-specific and
-* is almost identical to 'noreturn' except that it does not affect
-* code generation.  It's useful for annotating assertion handlers
-* that actually can/do return (exceptions and longjmp do not count),
-* but that any code analysis should pretend they do not.
-*/
-#ifdef HAVE_ANALYZER_NORETURN_ATTRIBUTE
-#  define _BU_ATTR_ANALYZER_NORETURN __attribute__((analyzer_noreturn))
-#else
-#  define _BU_ATTR_ANALYZER_NORETURN
-#endif
-
-/**
  * shorthand declaration of a function that should always be inline
  */
 
--- include/bu/magic.h
+++ include/bu/magic.h
@@ -260,7 +260,7 @@ __BEGIN_DECLS
  * pointer.  It's primarily a support routine for BU_CKMAG macro, but
  * may be used elsewhere.  This function never returns.
  */
-BU_EXPORT _BU_ATTR_ANALYZER_NORETURN NORETURN extern void bu_badmagic(const uint32_t *ptr, uint32_t magic, const char *str, const char *file, int line);
+BU_EXPORT NORETURN extern void bu_badmagic(const uint32_t *ptr, uint32_t magic, const char *str, const char *file, int line);
 
 
 /** @brief Routines involved with handling "magic numbers" used to identify various in-memory data structures. */
