533b8c6ca03f288caaf17fd267dee687fb1689e2
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2022-08-23 14:39:36 -0400

    Reorganize the plotting code to make indiviual elements reusable

--- src/libbg/trimesh_plot3.cpp
+++ src/libbg/trimesh_plot3.cpp
@@ -69,68 +69,93 @@ plot_point2d_t(FILE *plot_file, const point2d_t *p, double r)
     pdv_3cont(plot_file, origin);
 }
 
-
 int
-bg_trimesh_2d_plot3(const char *fname, const int *faces, size_t num_faces, const point2d_t *p, size_t num_pnts)
+pntset_2d_plot3(const char *fname, std::unordered_set<int> &pinds, const point2d_t *p, struct bu_color *c)
 {
-    unsigned char rgb[3] = {255, 0, 0};
-    struct bu_color c = BU_COLOR_INIT_ZERO;
-
     /* If inputs are insufficient, we can't plot */
-    if (!fname || !faces || num_faces == 0 || !p || num_pnts == 0)
+    if (!fname || !p | !c)
 	return BRLCAD_ERROR;
 
-    if (bu_file_exists(fname, NULL))
-       return BRLCAD_ERROR;
-
-    FILE *plot_file = fopen (fname, "w");
+    /* Note - deliberately assuming we may be adding to an existing plot file */
+    FILE *plot_file = fopen (fname, "a");
     if (!plot_file)
 	return BRLCAD_ERROR;
 
-    // Get the bounding box, both so we can draw cross-hairs for any
-    // stray points and to write out a bounding box
+    // Write color
+    if (c)
+	pl_color_buc(plot_file, c);
+
+    // Get the bounding box
     point2d_t bmin = {INFINITY, INFINITY};
     point2d_t bmax = {-INFINITY, -INFINITY};
-    for (size_t i = 0; i < num_pnts; i++) {
-	V2MINMAX(bmin, bmax, p[i]);
+    std::unordered_set<int>::iterator p_it;
+    for (p_it = pinds.begin(); p_it != pinds.end(); p_it++) {
+	V2MINMAX(bmin, bmax, p[*p_it]);
     }
     fastf_t bdist = DIST_PNT2_PNT2(bmin, bmax);
-    bu_color_from_rgb_chars(&c, rgb);
-    pl_color_buc(plot_file, &c);
-    BB_PLOT_2D(bmin, bmax);
 
     // Assemble the set of all points, and then remove any that are
     // actively used in the faces.  What's left are unused points
-    std::unordered_set<int> unused_pnts;
-    for (size_t i = 0; i < num_pnts; i++)
-	unused_pnts.insert(i);
-    for (size_t i = 0; i < num_faces; i++) {
-	unused_pnts.erase(faces[i*3+0]);
-	unused_pnts.erase(faces[i*3+1]);
-	unused_pnts.erase(faces[i*3+2]);
-    }
-
-    if (unused_pnts.size()) {
-	rgb[0] = 255;
-	rgb[1] = 0;
-	rgb[2] = 0;
-	bu_color_from_rgb_chars(&c, rgb);
-	pl_color_buc(plot_file, &c);
-	std::unordered_set<int>::iterator u_it;
-	for (u_it = unused_pnts.begin(); u_it != unused_pnts.end(); u_it++) {
-	    plot_point2d_t(plot_file, &p[*u_it], bdist*0.02);
-
+    if (pinds.size()) {
+	for (p_it = pinds.begin(); p_it != pinds.end(); p_it++) {
+	    plot_point2d_t(plot_file, &p[*p_it], bdist*0.02);
 	}
     }
 
-    // Now, on to the triangles themselves.  If we plot just the edges we can't see
-    // single triangle holes, so we also need to plot interiors
+    fclose(plot_file);
+
+    /* Success */
+    return BRLCAD_OK;
+}
+
+int
+pntset_2d_bbox_plot3(const char *fname, std::unordered_set<int> &pinds, const point2d_t *p, struct bu_color *c)
+{
+    /* If inputs are insufficient, we can't plot */
+    if (!fname || !p)
+	return BRLCAD_ERROR;
+
+    /* Note - deliberately assuming we may be adding to an existing plot file */
+    FILE *plot_file = fopen (fname, "a");
+    if (!plot_file)
+	return BRLCAD_ERROR;
+
+    // Write color
+    if (c)
+	pl_color_buc(plot_file, c);
+
+    // Get the bounding box
+    point2d_t bmin = {INFINITY, INFINITY};
+    point2d_t bmax = {-INFINITY, -INFINITY};
+    std::unordered_set<int>::iterator p_it;
+    for (p_it = pinds.begin(); p_it != pinds.end(); p_it++) {
+	V2MINMAX(bmin, bmax, p[*p_it]);
+    }
+    BB_PLOT_2D(bmin, bmax);
+
+    fclose(plot_file);
+
+    /* Success */
+    return BRLCAD_OK;
+}
+
+int
+tri_edges_2d_plot3(const char *fname, const int *faces, size_t num_faces, const point2d_t *p, struct bu_color *c)
+{
+    /* If inputs are insufficient, we can't plot */
+    if (!fname || !faces || num_faces == 0 || !p)
+	return BRLCAD_ERROR;
+
+    FILE *plot_file = fopen (fname, "a");
+    if (!plot_file)
+	return BRLCAD_ERROR;
+
+    // Write color
+    if (c)
+	pl_color_buc(plot_file, c);
+
+
     for (size_t i = 0; i < num_faces; i++) {
-	rgb[0] = 255;
-	rgb[1] = 255;
-	rgb[2] = 0;
-	bu_color_from_rgb_chars(&c, rgb);
-	pl_color_buc(plot_file, &c);
 	point_t tp[3];
 	point_t tporig;
 	point_t tc = VINIT_ZERO;
@@ -151,24 +176,83 @@ bg_trimesh_2d_plot3(const char *fname, const int *faces, size_t num_faces, const
 	    pdv_3cont(plot_file, tp[j]);
 	}
 	pdv_3cont(plot_file, tporig);
+    }
+    fclose(plot_file);
+
+    /* Success */
+    return BRLCAD_OK;
+}
+
+int
+tri_interior_2d_plot3(const char *fname, const int *faces, size_t num_faces, const point2d_t *p, struct bu_color *c)
+{
+    /* If inputs are insufficient, we can't plot */
+    if (!fname || !faces || num_faces == 0 || !p)
+	return BRLCAD_ERROR;
+
+    FILE *plot_file = fopen (fname, "a");
+    if (!plot_file)
+	return BRLCAD_ERROR;
+
+    // Write color
+    if (c)
+	pl_color_buc(plot_file, c);
+
+
+    for (size_t i = 0; i < num_faces; i++) {
+	point_t tp[3];
+	point_t tc = VINIT_ZERO;
+	for (int j = 0; j < 3; j++) {
+	    int pind = faces[i*3+j];
+	    VSET(tp[j], p[pind][X], p[pind][Y], 0);
+	    tc[X] += tp[j][X];
+	    tc[Y] += tp[j][Y];
+	}
+	tc[X] = tc[X]/3.0;
+	tc[Y] = tc[Y]/3.0;
 
 	// Interior
-	rgb[0] = 0;
-	rgb[1] = 255;
-	rgb[2] = 255;
-	bu_color_from_rgb_chars(&c, rgb);
-	pl_color_buc(plot_file, &c);
 	for (size_t j = 0; j < 3; j++) {
 	    pdv_3move(plot_file, tp[j]);
 	    pdv_3cont(plot_file, tc);
 	}
 
+    }
+
+    fclose(plot_file);
+
+    /* Success */
+    return BRLCAD_OK;
+}
+
+int
+tri_normals_2d_plot3(const char *fname, const int *faces, size_t num_faces, const point2d_t *p, struct bu_color *c)
+{
+    /* If inputs are insufficient, we can't plot */
+    if (!fname || !faces || num_faces == 0 || !p)
+	return BRLCAD_ERROR;
+
+    FILE *plot_file = fopen (fname, "a");
+    if (!plot_file)
+	return BRLCAD_ERROR;
+
+    // Write color
+    if (c)
+	pl_color_buc(plot_file, c);
+
+    for (size_t i = 0; i < num_faces; i++) {
+	point_t tp[3];
+	point_t tc = VINIT_ZERO;
+	for (int j = 0; j < 3; j++) {
+	    int pind = faces[i*3+j];
+	    VSET(tp[j], p[pind][X], p[pind][Y], 0);
+	    tc[X] += tp[j][X];
+	    tc[Y] += tp[j][Y];
+	}
+	tc[X] = tc[X]/3.0;
+	tc[Y] = tc[Y]/3.0;
+
 	// The 3D normal indicates CW vs CCW
-	rgb[0] = 0;
-	rgb[1] = 255;
-	rgb[2] = 0;
-	bu_color_from_rgb_chars(&c, rgb);
-	pl_color_buc(plot_file, &c);
 	point_t p1, p2, p3;
 	VSET(p1, tp[0][X], tp[0][Y], 0);
 	VSET(p2, tp[1][X], tp[1][Y], 0);
@@ -182,11 +266,83 @@ bg_trimesh_2d_plot3(const char *fname, const int *faces, size_t num_faces, const
 	pdv_3move(plot_file, tc);
 	pdv_3cont(plot_file, vtip);
     }
+
     fclose(plot_file);
 
     /* Success */
     return BRLCAD_OK;
 }
+
+int
+bg_trimesh_2d_plot3(const char *fname, const int *faces, size_t num_faces, const point2d_t *p, size_t num_pnts)
+{
+    unsigned char rgb[3] = {255, 0, 0};
+    struct bu_color c = BU_COLOR_INIT_ZERO;
+
+    /* If inputs are insufficient, we can't plot */
+    if (!fname || !faces || num_faces == 0 || !p || num_pnts == 0)
+	return BRLCAD_ERROR;
+
+    if (bu_file_exists(fname, NULL))
+       return BRLCAD_ERROR;
+
+    FILE *plot_file = fopen (fname, "w");
+    if (!plot_file)
+	return BRLCAD_ERROR;
+
+    // Assemble the set of all points
+    std::unordered_set<int> pnts;
+    for (size_t i = 0; i < num_pnts; i++)
+	pnts.insert(i);
+
+    // Plot the bounding box of the points
+    rgb[0] = 255;
+    rgb[1] = 0;
+    rgb[2] = 0;
+    bu_color_from_rgb_chars(&c, rgb);
+    pntset_2d_bbox_plot3(fname, pnts, p, &c);
+
+    // Remove any that are actively used in the faces.  What's left are unused
+    // points - plot them
+    for (size_t i = 0; i < num_faces; i++) {
+	pnts.erase(faces[i*3+0]);
+	pnts.erase(faces[i*3+1]);
+	pnts.erase(faces[i*3+2]);
+    }
+    if (pnts.size()) {
+	rgb[0] = 255;
+	rgb[1] = 0;
+	rgb[2] = 0;
+	bu_color_from_rgb_chars(&c, rgb);
+	pntset_2d_plot3(fname, pnts, p, &c);
+    }
+
+    // Plot triangle interiors
+    rgb[0] = 0;
+    rgb[1] = 255;
+    rgb[2] = 255;
+    bu_color_from_rgb_chars(&c, rgb);
+    tri_interior_2d_plot3(fname, faces, num_faces, p, &c);
+
+    // Plot triangle edges
+    rgb[0] = 255;
+    rgb[1] = 255;
+    rgb[2] = 0;
+    bu_color_from_rgb_chars(&c, rgb);
+    tri_edges_2d_plot3(fname, faces, num_faces, p, &c);
+
+    // Plot triangle normals
+    rgb[0] = 0;
+    rgb[1] = 255;
+    rgb[2] = 0;
+    bu_color_from_rgb_chars(&c, rgb);
+    tri_normals_2d_plot3(fname, faces, num_faces, p, &c);
+
+    /* Success */
+    return BRLCAD_OK;
+}
+
+
 
 // Local Variables:
 // tab-width: 8
