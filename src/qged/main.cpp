95f628278e69cc29e5e64bdf3666fb7de9c31778
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2022-08-26 12:18:40 -0400

    Hmm.  Qt6 and breeze theme aren't playing nice.  Also need to figure out why tab completion isn't happy with Qt6...

--- src/qged/main.cpp
+++ src/qged/main.cpp
@@ -231,10 +231,15 @@ int main(int argc, char *argv[])
     app.installEventFilter(efilter);
 
     // Use the dark theme from https://github.com/Alexhuszagh/BreezeStyleSheets
+    //
+    // TODO - need to fix a bug with the theme - observing it in qged.  See
+    // https://github.com/Alexhuszagh/BreezeStyleSheets/issues/25
+#if 0
     QFile file(":/dark.qss");
     file.open(QFile::ReadOnly | QFile::Text);
     QTextStream stream(&file);
     app.setStyleSheet(stream.readAll());
+#endif
 
     // The main window defines the primary BRL-CAD interface.
     app.w = new BRLCAD_MainWindow(swrast_mode, quad_mode);
