4b58eb6a3e623da138d55a040d180427408cabd0
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2022-09-14 21:55:55 -0400

    OpenMesh/OpenVDB build logic reorg
    OpenMesh and OpenVDB logic both pertain to the bot subcommand - move
    their build logic to that level.  Get OpenVDB code updated as far as
    trying to compile the remesh.cpp file if the flag is enabled - doesn't
    build with newer OpenVDB so some updating will be in order when that
    code is revisited, but should at least be able to flip the switch and
    try the compile now.  Focus is OpenMesh, but the pattern for OpenVDB is
    similar so handling both to keep things cleaner.

--- misc/CMake/FindBlosc.cmake
+++ misc/CMake/FindBlosc.cmake
@@ -1,28 +1,5 @@
-# Copyright (c) DreamWorks Animation LLC
-#
-# All rights reserved. This software is distributed under the
-# Mozilla Public License 2.0 ( http://www.mozilla.org/MPL/2.0/ )
-#
-# Redistributions of source code must retain the above copyright
-# and license notice and the following restrictions and disclaimer.
-#
-# *     Neither the name of DreamWorks Animation nor the names of
-# its contributors may be used to endorse or promote products derived
-# from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY INDIRECT, INCIDENTAL,
-# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-# IN NO EVENT SHALL THE COPYRIGHT HOLDERS' AND CONTRIBUTORS' AGGREGATE
-# LIABILITY FOR ALL CLAIMS REGARDLESS OF THEIR BASIS EXCEED US$250.00.
+# Copyright Contributors to the OpenVDB Project
+# SPDX-License-Identifier: MPL-2.0
 #
 #[=======================================================================[.rst:
 
@@ -55,6 +32,17 @@ This will define the following variables:
   The version of the Blosc library which was found.
 ``Blosc_INCLUDE_DIRS``
   Include directories needed to use Blosc.
+``Blosc_RELEASE_LIBRARIES``
+  Libraries needed to link to the release version of Blosc.
+``Blosc_RELEASE_LIBRARY_DIRS``
+  Blosc release library directories.
+``Blosc_DEBUG_LIBRARIES``
+  Libraries needed to link to the debug version of Blosc.
+``Blosc_DEBUG_LIBRARY_DIRS``
+  Blosc debug library directories.
+
+Deprecated - use [RELEASE|DEBUG] variants:
+
 ``Blosc_LIBRARIES``
   Libraries needed to link to Blosc.
 ``Blosc_LIBRARY_DIRS``
@@ -68,7 +56,11 @@ The following cache variables may also be set:
 ``Blosc_INCLUDE_DIR``
   The directory containing ``blosc.h``.
 ``Blosc_LIBRARY``
-  The path to the Blosc library.
+  The path to the Blosc library. may include target_link_libraries() debug/optimized keywords
+``Blosc_LIBRARY_RELEASE``
+  The path to the Blosc release library.
+``Blosc_LIBRARY_DEBUG``
+  The path to the Blosc debug library.
 
 Hints
 ^^^^^
@@ -76,38 +68,62 @@ Hints
 Instead of explicitly setting the cache variables, the following variables
 may be provided to tell this module where to look.
 
-``BLOSC_ROOT``
+``Blosc_ROOT``
   Preferred installation prefix.
 ``BLOSC_INCLUDEDIR``
   Preferred include directory e.g. <prefix>/include
 ``BLOSC_LIBRARYDIR``
   Preferred library directory e.g. <prefix>/lib
+``BLOSC_DEBUG_SUFFIX``
+  Suffix of the debug version of blosc. Defaults to "_d", OR the empty string for VCPKG_TOOLCHAIN
 ``SYSTEM_LIBRARY_PATHS``
-  Paths appended to all include and lib searches.
+  Global list of library paths intended to be searched by and find_xxx call
+``BLOSC_USE_STATIC_LIBS``
+  Only search for static blosc libraries
+``BLOSC_USE_EXTERNAL_SOURCES``
+  Set to ON if Blosc has been built using external sources for LZ4, snappy,
+  zlib and zstd. Default is OFF.
+``DISABLE_CMAKE_SEARCH_PATHS``
+  Disable CMakes default search paths for find_xxx calls in this module
 
 #]=======================================================================]
+
+cmake_minimum_required(VERSION 3.18)
+include(GNUInstallDirs)
 
 mark_as_advanced(
   Blosc_INCLUDE_DIR
   Blosc_LIBRARY
 )
 
-# Append BLOSC_ROOT or $ENV{BLOSC_ROOT} if set (prioritize the direct cmake var)
-set(_BLOSC_ROOT_SEARCH_DIR "")
+set(_FIND_BLOSC_ADDITIONAL_OPTIONS "")
+if(DISABLE_CMAKE_SEARCH_PATHS)
+  set(_FIND_BLOSC_ADDITIONAL_OPTIONS NO_DEFAULT_PATH)
+endif()
 
-if(BLOSC_ROOT)
-  list(APPEND _BLOSC_ROOT_SEARCH_DIR ${BLOSC_ROOT})
-else()
-  set(_ENV_BLOSC_ROOT $ENV{BLOSC_ROOT})
-  if(_ENV_BLOSC_ROOT)
-    list(APPEND _BLOSC_ROOT_SEARCH_DIR ${_ENV_BLOSC_ROOT})
-  endif()
+# Set _BLOSC_ROOT based on a user provided root var. Xxx_ROOT and ENV{Xxx_ROOT}
+# are prioritised over the legacy capitalized XXX_ROOT variables for matching
+# CMake 3.12 behaviour
+# @todo  deprecate -D and ENV BLOSC_ROOT from CMake 3.12
+if(Blosc_ROOT)
+  set(_BLOSC_ROOT ${Blosc_ROOT})
+elseif(DEFINED ENV{Blosc_ROOT})
+  set(_BLOSC_ROOT $ENV{Blosc_ROOT})
+elseif(BLOSC_ROOT)
+  set(_BLOSC_ROOT ${BLOSC_ROOT})
+elseif(DEFINED ENV{BLOSC_ROOT})
+  set(_BLOSC_ROOT $ENV{BLOSC_ROOT})
 endif()
 
 # Additionally try and use pkconfig to find blosc
-
-find_package(PkgConfig)
-pkg_check_modules(PC_Blosc QUIET blosc)
+if(USE_PKGCONFIG)
+  if(NOT DEFINED PKG_CONFIG_FOUND)
+    find_package(PkgConfig)
+  endif()
+  if(PKG_CONFIG_FOUND)
+    pkg_check_modules(PC_Blosc QUIET blosc)
+  endif()
+endif()
 
 # ------------------------------------------------------------------------
 #  Search for blosc include DIR
@@ -116,16 +132,16 @@ pkg_check_modules(PC_Blosc QUIET blosc)
 set(_BLOSC_INCLUDE_SEARCH_DIRS "")
 list(APPEND _BLOSC_INCLUDE_SEARCH_DIRS
   ${BLOSC_INCLUDEDIR}
-  ${_BLOSC_ROOT_SEARCH_DIR}
+  ${_BLOSC_ROOT}
   ${PC_Blosc_INCLUDE_DIRS}
   ${SYSTEM_LIBRARY_PATHS}
 )
 
 # Look for a standard blosc header file.
 find_path(Blosc_INCLUDE_DIR blosc.h
-  NO_DEFAULT_PATH
+  ${_FIND_BLOSC_ADDITIONAL_OPTIONS}
   PATHS ${_BLOSC_INCLUDE_SEARCH_DIRS}
-  PATH_SUFFIXES include
+  PATH_SUFFIXES ${CMAKE_INSTALL_INCLUDEDIR} include
 )
 
 if(EXISTS "${Blosc_INCLUDE_DIR}/blosc.h")
@@ -145,10 +161,19 @@ if(EXISTS "${Blosc_INCLUDE_DIR}/blosc.h")
   )
   string(STRIP "${_blosc_version_minor_string}" Blosc_VERSION_MINOR)
 
+  file(STRINGS "${Blosc_INCLUDE_DIR}/blosc.h"
+     _blosc_version_release_string REGEX "#define BLOSC_VERSION_RELEASE +[0-9]+ "
+  )
+  string(REGEX REPLACE "#define BLOSC_VERSION_RELEASE +([0-9]+).*$" "\\1"
+    _blosc_version_release_string "${_blosc_version_release_string}"
+  )
+  string(STRIP "${_blosc_version_release_string}" Blosc_VERSION_RELEASE)
+
   unset(_blosc_version_major_string)
   unset(_blosc_version_minor_string)
+  unset(_blosc_version_release_string)
 
-  set(Blosc_VERSION ${Blosc_VERSION_MAJOR}.${Blosc_VERSION_MINOR})
+  set(Blosc_VERSION ${Blosc_VERSION_MAJOR}.${Blosc_VERSION_MINOR}.${Blosc_VERSION_RELEASE})
 endif()
 
 # ------------------------------------------------------------------------
@@ -158,31 +183,111 @@ endif()
 set(_BLOSC_LIBRARYDIR_SEARCH_DIRS "")
 list(APPEND _BLOSC_LIBRARYDIR_SEARCH_DIRS
   ${BLOSC_LIBRARYDIR}
-  ${_BLOSC_ROOT_SEARCH_DIR}
+  ${_BLOSC_ROOT}
   ${PC_Blosc_LIBRARY_DIRS}
   ${SYSTEM_LIBRARY_PATHS}
 )
 
-# Static library setup
-if(UNIX AND BLOSC_USE_STATIC_LIBS)
-  set(_BLOSC_ORIG_CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_FIND_LIBRARY_SUFFIXES})
-  set(CMAKE_FIND_LIBRARY_SUFFIXES ".a")
+# Library suffix handling
+
+set(_BLOSC_ORIG_CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_FIND_LIBRARY_SUFFIXES})
+
+if(MSVC)
+  if(BLOSC_USE_STATIC_LIBS)
+    set(CMAKE_FIND_LIBRARY_SUFFIXES ".lib")
+  endif()
+else()
+  if(BLOSC_USE_STATIC_LIBS)
+    set(CMAKE_FIND_LIBRARY_SUFFIXES ".a")
+  endif()
 endif()
 
-set(BLOSC_PATH_SUFFIXES
-  lib64
-  lib
-)
+set(Blosc_LIB_COMPONENTS "")
+# NOTE: Search for debug version first (see vcpkg hack)
+list(APPEND BLOSC_BUILD_TYPES DEBUG RELEASE)
 
-find_library(Blosc_LIBRARY blosc
-  NO_DEFAULT_PATH
-  PATHS ${_BLOSC_LIBRARYDIR_SEARCH_DIRS}
-  PATH_SUFFIXES ${BLOSC_PATH_SUFFIXES}
-)
+foreach(BUILD_TYPE ${BLOSC_BUILD_TYPES})
+  set(_BLOSC_LIB_NAME blosc)
 
-if(UNIX AND BLOSC_USE_STATIC_LIBS)
-  set(CMAKE_FIND_LIBRARY_SUFFIXES ${_BLOSC_ORIG_CMAKE_FIND_LIBRARY_SUFFIXES})
-  unset(_BLOSC_ORIG_CMAKE_FIND_LIBRARY_SUFFIXES)
+  set(_BLOSC_CMAKE_IGNORE_PATH ${CMAKE_IGNORE_PATH})
+  if(VCPKG_TOOLCHAIN)
+    # Blosc is installed very strangely in VCPKG (debug/release libs have the
+    # same name, static build uses external deps, dll doesn't) and blosc itself
+    # comes with almost zero downstream CMake support for us to detect settings.
+    # We should not support external package managers in our own modules like
+    # this, but there doesn't seem to be a work around
+    if(NOT DEFINED BLOSC_DEBUG_SUFFIX)
+      set(BLOSC_DEBUG_SUFFIX "")
+    endif()
+    if(BUILD_TYPE STREQUAL RELEASE)
+      if(EXISTS ${Blosc_LIBRARY_DEBUG})
+        get_filename_component(_BLOSC_DEBUG_DIR ${Blosc_LIBRARY_DEBUG} DIRECTORY)
+        list(APPEND CMAKE_IGNORE_PATH ${_BLOSC_DEBUG_DIR})
+      endif()
+    endif()
+  endif()
+
+  if(BUILD_TYPE STREQUAL DEBUG)
+    if(NOT DEFINED BLOSC_DEBUG_SUFFIX)
+      set(BLOSC_DEBUG_SUFFIX _d)
+    endif()
+    set(_BLOSC_LIB_NAME "${_BLOSC_LIB_NAME}${BLOSC_DEBUG_SUFFIX}")
+  endif()
+
+  find_library(Blosc_LIBRARY_${BUILD_TYPE} ${_BLOSC_LIB_NAME}
+    ${_FIND_BLOSC_ADDITIONAL_OPTIONS}
+    PATHS ${_BLOSC_LIBRARYDIR_SEARCH_DIRS}
+    PATH_SUFFIXES ${CMAKE_INSTALL_LIBDIR} lib64 lib
+  )
+
+  list(APPEND Blosc_LIB_COMPONENTS ${Blosc_LIBRARY_${BUILD_TYPE}})
+  set(CMAKE_IGNORE_PATH ${_BLOSC_CMAKE_IGNORE_PATH})
+endforeach()
+
+# Reset library suffix
+
+set(CMAKE_FIND_LIBRARY_SUFFIXES ${_BLOSC_ORIG_CMAKE_FIND_LIBRARY_SUFFIXES})
+unset(_BLOSC_ORIG_CMAKE_FIND_LIBRARY_SUFFIXES)
+
+if(Blosc_LIBRARY_DEBUG AND Blosc_LIBRARY_RELEASE)
+  # if the generator is multi-config or if CMAKE_BUILD_TYPE is set for
+  # single-config generators, set optimized and debug libraries
+  get_property(_isMultiConfig GLOBAL PROPERTY GENERATOR_IS_MULTI_CONFIG)
+  if(_isMultiConfig OR CMAKE_BUILD_TYPE)
+    set(Blosc_LIBRARY optimized ${Blosc_LIBRARY_RELEASE} debug ${Blosc_LIBRARY_DEBUG})
+  else()
+    # For single-config generators where CMAKE_BUILD_TYPE has no value,
+    # just use the release libraries
+    set(Blosc_LIBRARY ${Blosc_LIBRARY_RELEASE})
+  endif()
+  # FIXME: This probably should be set for both cases
+  set(Blosc_LIBRARIES optimized ${Blosc_LIBRARY_RELEASE} debug ${Blosc_LIBRARY_DEBUG})
+endif()
+
+# if only the release version was found, set the debug variable also to the release version
+if(Blosc_LIBRARY_RELEASE AND NOT Blosc_LIBRARY_DEBUG)
+  set(Blosc_LIBRARY_DEBUG ${Blosc_LIBRARY_RELEASE})
+  set(Blosc_LIBRARY       ${Blosc_LIBRARY_RELEASE})
+  set(Blosc_LIBRARIES     ${Blosc_LIBRARY_RELEASE})
+endif()
+
+# if only the debug version was found, set the release variable also to the debug version
+if(Blosc_LIBRARY_DEBUG AND NOT Blosc_LIBRARY_RELEASE)
+  set(Blosc_LIBRARY_RELEASE ${Blosc_LIBRARY_DEBUG})
+  set(Blosc_LIBRARY         ${Blosc_LIBRARY_DEBUG})
+  set(Blosc_LIBRARIES       ${Blosc_LIBRARY_DEBUG})
+endif()
+
+# If the debug & release library ends up being the same, omit the keywords
+if("${Blosc_LIBRARY_RELEASE}" STREQUAL "${Blosc_LIBRARY_DEBUG}")
+  set(Blosc_LIBRARY   ${Blosc_LIBRARY_RELEASE} )
+  set(Blosc_LIBRARIES ${Blosc_LIBRARY_RELEASE} )
+endif()
+
+if(Blosc_LIBRARY)
+  set(Blosc_FOUND TRUE)
+else()
+  set(Blosc_FOUND FALSE)
 endif()
 
 # ------------------------------------------------------------------------
@@ -198,21 +303,82 @@ find_package_handle_standard_args(Blosc
   VERSION_VAR Blosc_VERSION
 )
 
-if(Blosc_FOUND)
-  set(Blosc_LIBRARIES ${Blosc_LIBRARY})
-  set(Blosc_INCLUDE_DIRS ${Blosc_INCLUDE_DIR})
-  set(Blosc_DEFINITIONS ${PC_Blosc_CFLAGS_OTHER})
-
-  get_filename_component(Blosc_LIBRARY_DIRS ${Blosc_LIBRARY} DIRECTORY)
-
-  if(NOT TARGET Blosc::blosc)
-    add_library(Blosc::blosc UNKNOWN IMPORTED)
-    set_target_properties(Blosc::blosc PROPERTIES
-      IMPORTED_LOCATION "${Blosc_LIBRARIES}"
-      INTERFACE_COMPILE_DEFINITIONS "${Blosc_DEFINITIONS}"
-      INTERFACE_INCLUDE_DIRECTORIES "${Blosc_INCLUDE_DIRS}"
-    )
+if(NOT Blosc_FOUND)
+  if(Blosc_FIND_REQUIRED)
+    message(FATAL_ERROR "Unable to find Blosc")
   endif()
-elseif(Blosc_FIND_REQUIRED)
-  message(FATAL_ERROR "Unable to find Blosc")
+  return()
 endif()
+
+# Partition release/debug lib vars
+
+set(Blosc_RELEASE_LIBRARIES ${Blosc_LIBRARY_RELEASE})
+get_filename_component(Blosc_RELEASE_LIBRARY_DIRS ${Blosc_RELEASE_LIBRARIES} DIRECTORY)
+set(Blosc_DEBUG_LIBRARIES ${Blosc_LIBRARY_DEBUG})
+get_filename_component(Blosc_DEBUG_LIBRARY_DIRS ${Blosc_DEBUG_LIBRARIES} DIRECTORY)
+set(Blosc_LIBRARIES ${Blosc_RELEASE_LIBRARIES})
+set(Blosc_LIBRARY_DIRS ${Blosc_RELEASE_LIBRARY_DIRS})
+set(Blosc_INCLUDE_DIRS ${Blosc_INCLUDE_DIR})
+set(Blosc_INCLUDE_DIRS ${Blosc_INCLUDE_DIR})
+
+# Configure lib type. If XXX_USE_STATIC_LIBS, we always assume a static
+# lib is in use. If win32, we can't mark the import .libs as shared, so
+# these are always marked as UNKNOWN. Otherwise, infer from extension.
+set(BLOSC_LIB_TYPE UNKNOWN)
+if(BLOSC_USE_STATIC_LIBS)
+  set(BLOSC_LIB_TYPE STATIC)
+elseif(UNIX)
+  get_filename_component(_BLOSC_EXT ${Blosc_LIBRARY_RELEASE} EXT)
+  if(_BLOSC_EXT STREQUAL ".a")
+    set(BLOSC_LIB_TYPE STATIC)
+  elseif(_BLOSC_EXT STREQUAL ".so" OR
+         _BLOSC_EXT STREQUAL ".dylib")
+    set(BLOSC_LIB_TYPE SHARED)
+  endif()
+endif()
+
+get_filename_component(Blosc_LIBRARY_DIRS ${Blosc_LIBRARY_RELEASE} DIRECTORY)
+
+if(NOT TARGET Blosc::blosc)
+  add_library(Blosc::blosc ${BLOSC_LIB_TYPE} IMPORTED)
+  set_target_properties(Blosc::blosc PROPERTIES
+    INTERFACE_COMPILE_OPTIONS "${PC_Blosc_CFLAGS_OTHER}"
+    INTERFACE_INCLUDE_DIRECTORIES "${Blosc_INCLUDE_DIRS}")
+
+  # Standard location
+  set_target_properties(Blosc::blosc PROPERTIES
+    IMPORTED_LINK_INTERFACE_LANGUAGES "CXX"
+    IMPORTED_LOCATION "${Blosc_LIBRARY}")
+
+  # Release location
+  if(EXISTS "${Blosc_LIBRARY_RELEASE}")
+    set_property(TARGET Blosc::blosc APPEND PROPERTY
+      IMPORTED_CONFIGURATIONS RELEASE)
+    set_target_properties(Blosc::blosc PROPERTIES
+      IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+      IMPORTED_LOCATION_RELEASE "${Blosc_LIBRARY_RELEASE}")
+  endif()
+
+  # Debug location
+  if(EXISTS "${Blosc_LIBRARY_DEBUG}")
+    set_property(TARGET Blosc::blosc APPEND PROPERTY
+      IMPORTED_CONFIGURATIONS DEBUG)
+    set_target_properties(Blosc::blosc PROPERTIES
+      IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+      IMPORTED_LOCATION_DEBUG "${Blosc_LIBRARY_DEBUG}")
+  endif()
+
+  # Blosc may optionally be compiled with external sources for
+  # lz4, snappy and zlib . Add them as interface libs if requested
+  # (there doesn't seem to be a way to figure this out automatically).
+  # We assume they live along side blosc
+  if(BLOSC_USE_EXTERNAL_SOURCES)
+    set_target_properties(Blosc::blosc PROPERTIES
+      INTERFACE_LINK_DIRECTORIES
+         "\$<\$<CONFIG:Release>:${Blosc_RELEASE_LIBRARY_DIRS}>;\$<\$<CONFIG:Debug>:${Blosc_DEBUG_LIBRARY_DIRS}>")
+    target_link_libraries(Blosc::blosc INTERFACE
+      $<$<CONFIG:Release>:lz4;snappy;zlib;zstd>
+      $<$<CONFIG:Debug>:lz4d;snappyd;zlibd;zstd>)
+  endif()
+endif()
+
