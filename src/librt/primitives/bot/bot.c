59aa9e3802da02c347128b174f6e09e4142a0e21
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2022-09-20 22:48:05 -0400

    bu_strdup warns on NULL - check first

--- src/librt/primitives/bot/bot.c
+++ src/librt/primitives/bot/bot.c
@@ -459,7 +459,8 @@ rt_bot_prep(struct soltab *stp, struct rt_db_internal *ip, struct rt_i *rtip)
     RT_BOT_CK_MAGIC(bot_ip);
 
     rt_bot_mintie = RT_DEFAULT_MINTIE;
-    char *bmintie = bu_strdup(getenv("LIBRT_BOT_MINTIE"));
+    char *ebmintie = getenv("LIBRT_BOT_MINTIE");
+    char *bmintie = (ebmintie) ? bu_strdup(ebmintie) : NULL;
     if (bmintie) {
 	rt_bot_mintie = atoi(bmintie);
 	bu_free(bmintie, "LIBRT_BOT_MINTIE copy");
