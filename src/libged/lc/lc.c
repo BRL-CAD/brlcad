79a59c79abc9f78eb4a9eaa5e1a40cf8aaa4f0bc
Christopher Sean Morrison <brlcad@gmail.com>
2022-09-28 00:16:54 -0400

    missing the -m mismatch option added in 6015dc11f0d

--- src/libged/lc/lc.c
+++ src/libged/lc/lc.c
@@ -150,7 +150,7 @@ ged_lc_core(struct ged *gedp, int argc, const char *argv[])
     int orig_argc;
     const char **orig_argv;
 
-    static const char *usage = "[-d|-s] [-r] [-z] [-0|-1|-2|-3|-4|-5] [-f {FileName}] {GroupName}";
+    static const char *usage = "[-d|-m|-s] [-r] [-z] [-0|-1|-2|-3|-4|-5] [-f {fileName}] {groupName}";
 
     int c;
     int error_cnt = 0;
