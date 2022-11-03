c3cb1bf32c28c761a7b136ce1cd4aaa1ac81511e
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2022-11-03 12:07:16 -0400

    Remove some VMS ifdefs

--- src/other/libosmesa/src/main/imports.c
+++ src/other/libosmesa/src/main/imports.c
@@ -55,9 +55,6 @@
 #define vsnprintf _vsnprintf
 #elif defined(__IBMC__) || defined(__IBMCPP__) || ( defined(__VMS) && __CRTL_VER < 70312000 )
 extern int vsnprintf(char *str, size_t count, const char *fmt, va_list arg);
-#ifdef __VMS
-#include "vsnprintf.c"
-#endif
 #endif
 
 /**********************************************************************/
