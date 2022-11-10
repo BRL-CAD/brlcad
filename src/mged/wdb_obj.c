6b72555c7077f21960440746ce856e7d5e0bdbca
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2022-11-10 10:41:01 -0500

    MGED's wdb_reopen operates directly on the wdbp dbip pointer - adjust its logic.

--- src/mged/wdb_obj.c
+++ src/mged/wdb_obj.c
@@ -917,10 +917,16 @@ wdb_reopen_cmd(struct rt_wdb *wdbp,
 	    return TCL_ERROR;
 	}
 
+	// Stash wdb type and interp
+	int wdb_type = wdbp->wdb_type;
+	void *interp = wdbp->wdb_interp;
+
 	/* close current database */
 	db_close(wdbp->dbip);
 
-	wdbp->dbip = dbip;
+	/* get the appropriate type for the new dbip */
+	wdbp = wdb_dbopen(dbip, wdb_type);
+	wdbp->wdb_interp = interp;
 
 	Tcl_AppendResult((Tcl_Interp *)wdbp->wdb_interp, wdbp->dbip->dbi_filename, (char *)NULL);
 	return TCL_OK;
