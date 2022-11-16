98f4cc872f6eb99a6d9085575986cd6df90b76cd
f4alt <christopherjmacgregor@gmail.com>
2022-11-16 12:18:33 -0500

    add warning to bot_dump
    bot_dump does not try to infer the format with the file extension,
    so if no -t is given it defaults to STL (unbeknownst to the user). The
    outputted file was valid - just not an OBJ. Add a warning for users who
    run the command without specifying.

--- src/libged/bot/bot_dump.c
+++ src/libged/bot/bot_dump.c
@@ -803,6 +803,7 @@ static int
 bot_dump_get_args(struct ged *gedp, int argc, const char *argv[])
 {
     int c;
+    int specified_t = 0;
 
     output_type = OTYPE_STL;
     binary = 0;
@@ -843,6 +844,7 @@ bot_dump_get_args(struct ged *gedp, int argc, const char *argv[])
 		    bu_vls_printf(gedp->ged_result_str, "Usage: %s %s\n", argv[0], usage);
 		    return BRLCAD_ERROR;
 		}
+		specified_t = 1;
 		break;
 	    case 'u':
 		cfactor = bu_units_conversion(bu_optarg);
@@ -856,6 +858,14 @@ bot_dump_get_args(struct ged *gedp, int argc, const char *argv[])
 		bu_vls_printf(gedp->ged_result_str, "Usage: %s %s\n", argv[0], usage);
 		return BRLCAD_ERROR;
 	}
+    }
+
+    /* let the user know we do not try to be smart with the specified file extension and will 
+     * default to stl if no type is specified
+     */
+    if (!specified_t) {
+	char* warning = "WARNING: no format type '-t' specified, defaulting to stl\n";
+	bu_vls_printf(gedp->ged_result_str, "%s", warning);
     }
 
     return BRLCAD_OK;
