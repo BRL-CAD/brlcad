dc09740218fefb229bd287eec3db5dc5c4ee8014
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2022-12-05 17:39:31 -0500

    Since we're always using PROJ, we're always going to have SQLITE.  At some point we'll need to check the behavior with system PROJ and built GDAL to see how the PROJ_LIBRARY_RELEASE and PROJ_LIBRARY_DEBUG variables mix with different Windows build configs, but that's most likely a very rare build configuration.

--- src/other/ext/CMake/FindPROJ.cmake
+++ src/other/ext/CMake/FindPROJ.cmake
@@ -27,13 +27,13 @@ endif()
 # Normal search.
 set(_PROJ_x86 "(x86)")
 set(_PROJ_SEARCH_NORMAL
-    PATHS
-	 "$ENV{OSGEO4W_HOME}"
-	 "C:/OSGeo4W"
-  	 "$ENV{ProgramFiles}/proj"
-	 "$ENV{ProgramFiles${_PROJ_x86}}/proj"
-	 )
- unset(_PROJ_x86)
+  PATHS
+  "$ENV{OSGEO4W_HOME}"
+  "C:/OSGeo4W"
+  "$ENV{ProgramFiles}/proj"
+  "$ENV{ProgramFiles${_PROJ_x86}}/proj"
+  )
+unset(_PROJ_x86)
 list(APPEND _PROJ_SEARCHES _PROJ_SEARCH_NORMAL)
 
 # Try each search configuration.
@@ -42,21 +42,21 @@ foreach(search ${_PROJ_SEARCHES})
 endforeach()
 
 if (PROJ_INCLUDE_DIR)
-	# Extract version from proj.h (ex: 480)
-	file(STRINGS ${PROJ_INCLUDE_DIR}/proj.h PJ_MAJORSTR REGEX "#define[ ]+PROJ_VERSION_MAJOR[ ]+[0-9]+")
-	string(REGEX MATCH "[0-9]+" PJ_MAJOR ${PJ_MAJORSTR})
-	file(STRINGS ${PROJ_INCLUDE_DIR}/proj.h PJ_MINORSTR REGEX "#define[ ]+PROJ_VERSION_MINOR[ ]+[0-9]+")
-	string(REGEX MATCH "[0-9]+" PJ_MINOR ${PJ_MINORSTR})
-	file(STRINGS ${PROJ_INCLUDE_DIR}/proj.h PJ_PATCHSTR REGEX "#define[ ]+PROJ_VERSION_PATCH[ ]+[0-9]+")
-	string(REGEX MATCH "[0-9]+" PJ_PATCH ${PJ_PATCHSTR})
-	set(PROJ_VERSION ${PJ_MAJOR}.${PJ_MINOR}.${PJ_PATCH})
+  # Extract version from proj.h (ex: 480)
+  file(STRINGS ${PROJ_INCLUDE_DIR}/proj.h PJ_MAJORSTR REGEX "#define[ ]+PROJ_VERSION_MAJOR[ ]+[0-9]+")
+  string(REGEX MATCH "[0-9]+" PJ_MAJOR ${PJ_MAJORSTR})
+  file(STRINGS ${PROJ_INCLUDE_DIR}/proj.h PJ_MINORSTR REGEX "#define[ ]+PROJ_VERSION_MINOR[ ]+[0-9]+")
+  string(REGEX MATCH "[0-9]+" PJ_MINOR ${PJ_MINORSTR})
+  file(STRINGS ${PROJ_INCLUDE_DIR}/proj.h PJ_PATCHSTR REGEX "#define[ ]+PROJ_VERSION_PATCH[ ]+[0-9]+")
+  string(REGEX MATCH "[0-9]+" PJ_PATCH ${PJ_PATCHSTR})
+  set(PROJ_VERSION ${PJ_MAJOR}.${PJ_MINOR}.${PJ_PATCH})
 endif()
 
 set(PROJ_NAMES ${PROJ_NAMES} proj proj_i)
 if(NOT PROJ_LIBRARY)
-	foreach(search ${_PROJ_SEARCHES})
-		find_library(PROJ_LIBRARY NAMES ${PROJ_NAMES} NAMES_PER_DIR ${${search}} PATH_SUFFIXES lib)
-	endforeach()
+  foreach(search ${_PROJ_SEARCHES})
+    find_library(PROJ_LIBRARY NAMES ${PROJ_NAMES} NAMES_PER_DIR ${${search}} PATH_SUFFIXES lib)
+  endforeach()
 endif()
 unset(PROJ_NAMES)
 
@@ -68,8 +68,8 @@ endif()
 # if all listed variables are TRUE
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(PROJ DEFAULT_MSG
-	PROJ_LIBRARY
-	PROJ_INCLUDE_DIR)
+  PROJ_LIBRARY
+  PROJ_INCLUDE_DIR)
 
 # Local Variables:
 # tab-width: 8
