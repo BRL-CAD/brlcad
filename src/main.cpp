17adab04fc408e40c918254fada719fadf566837
Danny Chen <zhuodannychen@gmail.com>
2023-03-05 21:56:17 -0600

    added argument support

--- src/main.cpp
+++ src/main.cpp
@@ -9,12 +9,14 @@
 
 int main(int argc, char **argv) {
     if (argc == 2) {
-        const char* filename = argv[1];
+        std::string filename = argv[1];
         std::cout << "Processing file: " << filename << std::endl;
-        const char* mgedTops = "../../../../../build/bin/mged ../db/moss.g tops";
-        auto result1 = system(mgedTops);
-        const char* rtAll = "../../../../../build/bin/rt -C 255/255/255 -s 1024 -c \"set ambSamples=64\" ../db/moss.g all.g";
-        auto result2 = system(rtAll);
+
+        std::string genTops = "../../../../../build/bin/mged " + filename + " tops";
+        auto result1 = system(genTops.c_str());
+
+        std::string renderAll = "../../../../../build/bin/rt -C 255/255/255 -s 1024 -c \"set ambSamples=64\" " + filename + " all.g";
+        auto result2 = system(renderAll.c_str());
         std::cout << "Success\n";
     } else {
         return 1;
