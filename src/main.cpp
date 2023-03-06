4166a485bd4fab61065832ce04a25ea6ca2a4f07
Michael Tao <mtao369@gmail.com>
2023-03-06 14:11:38 -0600

    trying out shapes

--- src/main.cpp
+++ src/main.cpp
@@ -58,6 +58,15 @@ int main(int argc, char **argv) {
             // second argument: image to be shown(Mat object)
             imshow("Report", img);
 
+            /*  Mat whiteMatrix(200, 200, CV_8UC3, Scalar(255, 255, 255));//Declaring a white matrix
+                Point center(100, 100);//Declaring the center point
+                int radius = 50; //Declaring the radius
+                Scalar line_Color(0, 0, 0);//Color of the circle
+                int thickness = 2;//thickens of the line
+                namedWindow("whiteMatrix");//Declaring a window to show the circle
+                circle(whiteMatrix, center,radius, line_Color, thickness);//Using circle()function to draw the line//
+                imshow("WhiteMatrix", whiteMatrix);//Showing the circle//*/
+
             waitKey(0); //wait infinite time for a keypress
 
             // destroy the window with the name, "MyWindow"
