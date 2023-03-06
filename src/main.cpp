1cd2b11901835f1afbf10d12bb34354f88f22d13
“allyhoskinson” <allyhoskinson@tamu.edu>
2023-03-06 00:08:03 -0600

    Added image frame and text output functionalities

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,16 @@
 #include<cstdlib>
 #include<string>
 
+#include<opencv2/opencv.hpp>
+#include<opencv2/core/core.hpp>
+#include "opencv2/highgui/highgui.hpp"
+
 // /brlcad/build/share/db/moss.g
 // /brlcad/src/gtools/rgen/visualization
 // ../../../../../build/bin/mged ../db/moss.g tops
 // ../../../../../build/bin/rt ../db/moss.g all.g
+
+using namespace cv;
 
 int main(int argc, char **argv) {
     if (argc == 2) {
@@ -17,6 +23,45 @@ int main(int argc, char **argv) {
         auto result2 = system(rtAll);
         std::cout << "Success\n";
     } else {
+            // To create an image
+            // CV_8UC3 depicts : (3 channels,8 bit image depth)
+            // Height  = 750 pixels, Width = 1500 pixels
+            // Background is set to white
+            Mat img(750, 1500, CV_8UC3, Scalar(255,255,255));
+
+            putText(img, "Report:", Point(0, 50), FONT_HERSHEY_DUPLEX, 1.0, CV_RGB(0, 0, 0), 1);
+
+            // check whether the image is loaded or not
+            if (img.empty())
+            {
+                std::cout << "\n Image not created. You"
+                    " have done something wrong. \n";
+                return -1;    // Unsuccessful.
+            }
+
+            // first argument: name of the window
+            // second argument: flag- types: 
+            // WINDOW_NORMAL If this is set, the user can 
+            //               resize the window.
+            // WINDOW_AUTOSIZE If this is set, the window size
+            //                is automatically adjusted to fit 
+            //                the displayed image, and you cannot
+            //                change the window size manually.
+            // WINDOW_OPENGL  If this is set, the window will be
+            //                created with OpenGL support.
+            namedWindow("Report", WINDOW_AUTOSIZE);
+
+            //Saves to png
+            imwrite("report.png", img);
+
+            // first argument: name of the window
+            // second argument: image to be shown(Mat object)
+            imshow("Report", img);
+
+            waitKey(0); //wait infinite time for a keypress
+
+            // destroy the window with the name, "MyWindow"
+            destroyWindow("Report");
         return 1;
     }
 }
