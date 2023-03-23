f9bce4bf0c781717fbb05da06f9334b4134e5651
Michael Tao <mtao369@gmail.com>
2023-03-22 22:34:27 -0500

    first iteration of the ifPaint

--- IFPainter.cpp
+++ IFPainter.cpp
@@ -22,24 +22,68 @@ void IFPainter::drawImage(int x, int y, int width, int height, std::string imgPa
 	// Perhaps there's a better way to store images; reading in images multiple times is inefficient, though if we only need
 	// to read in the image once (which I believe is the case), then reading images from a file should also be fine.
 	// TODO: this
+
+	//storing the desired image and resizing it to the desired width and height
+	Mat lilImage = imread(imgPath , cv::IMREAD_UNCHANGED);
+	Mat resized_image;
+	resize(lilImage, resized_image, Size(width, height), INTER_LINEAR);
+
+	//Trying to copyto the image on to the private image frame, img
+	Mat destRoi;
+	try {
+		destRoi = img(cv::Rect(x, y, resized_image.cols, resized_image.rows));
+	}  catch (...) {
+		std::cerr << "Trying to create roi out of image boundaries" << std::endl;
+		return -1;
+	}
+	resized_image.copyTo(destRoi);
+
 }
 
-void IFPainter::drawText(int x, int y, int fontSize, int width, std::string text)
+void IFPainter::drawText(int x, int y, double fontSize, int width, std::string text, bool italics)
 {
 	// TODO: this
 	// keep in mind that we'll need options for bold, italics, etc.
 	// Try to make these methods as intuitive as possible in the idea of "we want text here, so we're confident that text will go here"
 	// Keep in mind text wrapping.
+
+	Point text_position(x, y);
+	Scalar font_color(0, 0, 0);
+	//boldness can be adjusted through font_weight
+	double font_weight = 1;
+	//The italics doesnt look really look like the traditional italics
+	if (italics == true) {
+		putText(img, text, text_position, FONT_ITALIC, fontSize, font_color, font_weight);
+	}
+	putText(img, text, text_position, FONT_HERSHEY_PLAIN, fontSize, font_color, font_weight);
 }
 
 void IFPainter::drawLine(int x1, int y1, int x2, int y2, int width, cv::Scalar color)
 {
 	// TODO: this
+	int lineType = LINE_8;
+	Point start(x1, y1);
+	Point end(x2, y2);
+	line(img,
+		start,
+		end,
+		color,
+		width,
+		lineType);
 }
 
-void IFPainter::drawRect(int x1, int y1, int x2, int y2, cv::Scalar color)
+void IFPainter::drawRect(int x1, int y1, int x2, int y2, int width, cv::Scalar color)
 {
 	// TODO: this
+	//Should I try to fill the rectangle?
+	Point topLeft(x1, y1);
+	Point bottomRight(x2, y2);
+	rectangle(img,
+			  topLeft,
+			  bottomRight,
+              color,
+              width,
+			  LINE_8);
 }
 
 
