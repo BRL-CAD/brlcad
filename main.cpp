75d204986f25e138271fdaaeaf758b753778a1d6
Danny Chen <zhuodannychen@gmail.com>
2023-03-26 18:45:53 -0500

    made render brighter

--- main.cpp
+++ main.cpp
@@ -115,8 +115,8 @@ void generateReport(Options opt)
     }
 
     // paint renderings
-    //makeRenderSection(img, info, 0, 0, 2400, 2400, opt);
-    makeRenderSection(img, info, 0, 100, 1200, 900);
+    makeRenderSection(img, info, 0, 0, 2400, 2400, opt);
+    // makeRenderSection(img, info, 0, 100, 1200, 900);
 
     // paint text sections (no method headers yet)
     // paintTitle
