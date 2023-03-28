ca5ed6a3f7cf124ec1b4d1cda389f7229f233393
Danny Chen <zhuodannychen@gmail.com>
2023-03-27 22:55:13 -0500

    quick bug fixes

--- FactsHandler.cpp
+++ FactsHandler.cpp
@@ -6,7 +6,7 @@ void makeTopSection(IFPainter& img, InformationGatherer& info, int offsetX, int 
 
 	//Draw text on top
 	std::string text = "Owner: " + info.getInfo("owner") + " Version: " + info.getInfo("version") + " Last Updated: " + info.getInfo("lastUpdate") + " Classification: " + info.getInfo("classification");
-	img.drawText(offsetX + width / 100, offsetY + height / 1.5, width / 1500, width / 2000, text, false, true);
+	img.drawText(offsetX + width / 100, offsetY + height / 1.5, width / 1500, width / 2000, text, false, true, false, 0);
 }
 
 void makeBottomSection(IFPainter& img, InformationGatherer& info, int offsetX, int offsetY, int width, int height) { 
@@ -15,7 +15,7 @@ void makeBottomSection(IFPainter& img, InformationGatherer& info, int offsetX, i
 
 	//Draw text on bottom
 	std::string text = "Preparer: " + info.getInfo("preparer") + " Source File: " + info.getInfo("file") + " Date Generated: " + info.getInfo("dateGenerated") + " Model Checksum: " + info.getInfo("checksum");
-	img.drawText(offsetX + width / 100, offsetY + height / 1.5, width / 1500, width / 2000, text, false, true);
+	img.drawText(offsetX + width / 100, offsetY + height / 1.5, width / 1500, width / 2000, text, false, true, false, 0);
 }
 
 void makeFileInfoSection(IFPainter& img, InformationGatherer& info, int offsetX, int offsetY, int width, int height) {
