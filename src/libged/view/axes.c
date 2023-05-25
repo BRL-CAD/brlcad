a7bea55f315d01f43dbde47e331d2b6a91e5f716
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2023-05-25 14:06:43 -0400

    Don't add an axes: prefix to the name automatically.

--- src/libged/view/axes.c
+++ src/libged/view/axes.c
@@ -94,7 +94,7 @@ _axes_cmd_create(void *bs, int argc, const char **argv)
     s->s_type_flags |= BV_AXES;
 
     bu_vls_init(&s->s_name);
-    bu_vls_printf(&s->s_name, "axes:%s", gd->vobj);
+    bu_vls_printf(&s->s_name, "%s", gd->vobj);
 
     return BRLCAD_OK;
 }
