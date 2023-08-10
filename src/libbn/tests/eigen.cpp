2e66983f4ef5a95ab94ec1ef2d4201ae4619b9b5
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2023-08-10 11:12:40 -0400

    Add an actual verification of the identity status post-Eigen

--- src/libbn/tests/eigen.cpp
+++ src/libbn/tests/eigen.cpp
@@ -30,21 +30,27 @@
 #include <math.h>
 #include <Eigen/Geometry>
 #include "bu/app.h"
-#include "bn.h"
+#include "bu/exit.h"
+#include "bn/mat.h"
 
 int main(int UNUSED(ac), const char **av)
 {
     bu_setprogname(av[0]);
 
+    // Set up a vmath zero matrix without reference to Eigen
     mat_t bn_mat;
     MAT_ZERO(bn_mat);
-
     bn_mat_print("Initial Zero Matrix", bn_mat);
 
+    // Establish an Eigen map to the vmath mat_t storage
     Eigen::Map<Eigen::Matrix<fastf_t, 4, 4>> emat(bn_mat);
 
+    // Set the matrix to identity in Eigen
     emat = Eigen::Matrix<fastf_t, 4, 4>::Identity();
 
+    // Verify (independent of Eigen) that the libbn data received the update
+    if (!bn_mat_is_identity(bn_mat))
+	bu_exit(BRLCAD_ERROR, "Identity matrix assignment failed\n");
     bn_mat_print("Identity Matrix", bn_mat);
 
     return 0;
