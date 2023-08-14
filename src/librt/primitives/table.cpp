daa6e7beb1717c1d105cb017a82720a852023e62
f4alt <christopherjmacgregor@gmail.com>
2023-08-14 15:25:29 -0400

    minimal creation func for eto

--- src/librt/primitives/table.cpp
+++ src/librt/primitives/table.cpp
@@ -1183,7 +1183,7 @@ const struct rt_functab OBJ[] = {
 	RTFUNCTAB_FUNC_GET_CAST(rt_generic_get),
 	RTFUNCTAB_FUNC_ADJUST_CAST(rt_generic_adjust),
 	RTFUNCTAB_FUNC_FORM_CAST(rt_generic_form),
-	NULL, /* make */
+	RTFUNCTAB_FUNC_MAKE_CAST(rt_eto_make),
 	RTFUNCTAB_FUNC_PARAMS_CAST(rt_eto_params),
 	RTFUNCTAB_FUNC_BBOX_CAST(rt_eto_bbox),
 	RTFUNCTAB_FUNC_VOLUME_CAST(rt_eto_volume),
