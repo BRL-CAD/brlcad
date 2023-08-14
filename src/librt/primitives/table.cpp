e66e39d76ab9d93cdc39b8f654f310747e3a1474
f4alt <christopherjmacgregor@gmail.com>
2023-08-14 16:47:26 -0400

    minimal creation func for extrude

--- src/librt/primitives/table.cpp
+++ src/librt/primitives/table.cpp
@@ -1509,7 +1509,7 @@ const struct rt_functab OBJ[] = {
 	RTFUNCTAB_FUNC_GET_CAST(rt_extrude_get),
 	RTFUNCTAB_FUNC_ADJUST_CAST(rt_extrude_adjust),
 	RTFUNCTAB_FUNC_FORM_CAST(rt_extrude_form),
-	NULL, /* make */
+	RTFUNCTAB_FUNC_MAKE_CAST(rt_extrude_make),
 	RTFUNCTAB_FUNC_PARAMS_CAST(rt_extrude_params),
 	RTFUNCTAB_FUNC_BBOX_CAST(rt_extrude_bbox),
 	RTFUNCTAB_FUNC_VOLUME_CAST(rt_extrude_volume),
