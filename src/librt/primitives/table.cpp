875e5c3d0ff3a88f86cbbe55659e7f6fdbdf2ee6
f4alt <christopherjmacgregor@gmail.com>
2023-08-14 15:01:49 -0400

    minimal creation func for rpc

--- src/librt/primitives/table.cpp
+++ src/librt/primitives/table.cpp
@@ -995,7 +995,7 @@ const struct rt_functab OBJ[] = {
 	RTFUNCTAB_FUNC_GET_CAST(rt_generic_get),
 	RTFUNCTAB_FUNC_ADJUST_CAST(rt_generic_adjust),
 	RTFUNCTAB_FUNC_FORM_CAST(rt_generic_form),
-	NULL, /* make */
+	RTFUNCTAB_FUNC_MAKE_CAST(rt_rpc_make),
 	RTFUNCTAB_FUNC_PARAMS_CAST(rt_rpc_params),
 	RTFUNCTAB_FUNC_BBOX_CAST(rt_rpc_bbox),
 	RTFUNCTAB_FUNC_VOLUME_CAST(rt_rpc_volume),
