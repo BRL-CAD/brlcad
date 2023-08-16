ee0562a971a198a891765c49c16658266f77d78e
f4alt <christopherjmacgregor@gmail.com>
2023-08-16 12:48:28 -0400

    minimal creation func for submodel
    also correct some edge case allocation assumptions

--- src/librt/primitives/table.cpp
+++ src/librt/primitives/table.cpp
@@ -1556,7 +1556,7 @@ const struct rt_functab OBJ[] = {
 	RTFUNCTAB_FUNC_GET_CAST(rt_generic_get),
 	RTFUNCTAB_FUNC_ADJUST_CAST(rt_generic_adjust),
 	RTFUNCTAB_FUNC_FORM_CAST(rt_generic_form),
-	NULL, /* make */
+	RTFUNCTAB_FUNC_MAKE_CAST(rt_submodel_make),
 	RTFUNCTAB_FUNC_PARAMS_CAST(rt_submodel_params),
 	NULL, /* bbox */
 	NULL, /* volume */
