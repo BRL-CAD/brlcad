6a2318adf76d17c3a1638a95671bd7525f4d0f66
f4alt <christopherjmacgregor@gmail.com>
2023-08-17 13:33:54 -0400

    minimal creation func for brep
    change adjust function return expectations to allow for empty 'put'
    default creation

--- src/librt/primitives/table.cpp
+++ src/librt/primitives/table.cpp
@@ -1983,7 +1983,7 @@ const struct rt_functab OBJ[] = {
 	RTFUNCTAB_FUNC_GET_CAST(rt_brep_get),
 	RTFUNCTAB_FUNC_ADJUST_CAST(rt_brep_adjust),
 	RTFUNCTAB_FUNC_FORM_CAST(rt_generic_form),
-	NULL, /* make */
+	RTFUNCTAB_FUNC_MAKE_CAST(rt_brep_make),
 	RTFUNCTAB_FUNC_PARAMS_CAST(rt_brep_params),
 	RTFUNCTAB_FUNC_BBOX_CAST(rt_brep_bbox),
 	NULL, /* volume */
