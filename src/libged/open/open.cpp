e0384c3abc29a6b470869b607ef92aaa6fce6b11
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2023-08-30 12:02:38 -0400

    Add flip and create options to GED open

--- src/libged/open/open.cpp
+++ src/libged/open/open.cpp
@@ -29,14 +29,19 @@
 
 #include "bu/cmd.h"
 #include "bg/lod.h"
+#include "bu/opt.h"
 
 #include "../ged_private.h"
 
 
 extern "C" int
-ged_reopen_core(struct ged *gedp, int argc, const char *argv[])
+ged_opendb_core(struct ged *gedp, int argc, const char *argv[])
 {
-    static const char *usage = "[filename]";
+    int flip_endian = 0;
+    int force_create = 0;
+    int print_help = 0;
+    const char *cmdname = argv[0];
+    static const char *usage = "[options] [filename]\n";
 
     GED_CHECK_ARGC_GT_0(gedp, argc, BRLCAD_ERROR);
 
@@ -49,8 +54,27 @@ ged_reopen_core(struct ged *gedp, int argc, const char *argv[])
 	return BRLCAD_OK;
     }
 
-    if (argc != 2) {
-	bu_vls_printf(gedp->ged_result_str, "Usage: %s %s", argv[0], usage);
+    if (!BU_STR_EQUAL(cmdname, "reopen")) {
+	struct bu_opt_desc d[4];
+	BU_OPT(d[0], "c", "create",  "",  NULL,  &force_create,  "Creates a new database if not already extant.");
+	BU_OPT(d[1], "f", "flip-endian",  "",  NULL,  &flip_endian,  "Opens file as a binary-incompatible v4 geometry database.");
+	BU_OPT(d[2], "h", "help",  "",  NULL,  &print_help,  "Print help.");
+	BU_OPT_NULL(d[3]);
+
+	// We know we're the opendb command - start processing args
+	argc--; argv++;
+
+	int ac = bu_opt_parse(NULL, argc, argv, d);
+	argc = ac;
+
+	if (argc != 1) {
+	    bu_vls_printf(gedp->ged_result_str, "%s ", cmdname);
+	    _ged_cmd_help(gedp, usage, d);
+	    return BRLCAD_ERROR;
+	}
+    } else {
+	// If we're doing reopen, the options aren't active
+	bu_vls_printf(gedp->ged_result_str, "%s filename", cmdname);
 	return BRLCAD_ERROR;
     }
 
@@ -58,12 +82,32 @@ ged_reopen_core(struct ged *gedp, int argc, const char *argv[])
      * we can actually open what the caller provided */
     struct db_i *new_dbip = NULL;
     struct mater *old_materp = rt_material_head();
-    if ((new_dbip = _ged_open_dbip(argv[1], 0)) == DBI_NULL) {
+    int existing_only = !force_create;
+    if ((new_dbip = _ged_open_dbip(argv[0], existing_only)) == DBI_NULL) {
+
 	/* Restore RT's material head */
 	rt_new_material_head(old_materp);
 
-	bu_vls_printf(gedp->ged_result_str, "ged_reopen_core: failed to open %s\n", argv[1]);
+	bu_vls_printf(gedp->ged_result_str, "ged_opendb_core: failed to open %s\n", argv[0]);
 	return BRLCAD_ERROR;
+    }
+
+    /* Handle forced endian flipping, if needed and requested */
+    if (flip_endian) {
+	if (db_version(new_dbip) != 4) {
+	    bu_vls_printf(gedp->ged_result_str, "WARNING: [%s] is not a v4 database.  The -f option will be ignored.\n", argv[0]);
+	} else {
+	    if (new_dbip->dbi_version < 0) {
+		bu_vls_printf(gedp->ged_result_str, "Database [%s] was already (perhaps automatically) flipped, -f is redundant.\n", argv[0]);
+	    } else {
+		bu_vls_printf(gedp->ged_result_str, "Treating [%s] as a binary-incompatible v4 geometry database.\n", argv[0]);
+		bu_vls_printf(gedp->ged_result_str, "Endianness flipped.  Converting to READ ONLY.\n");
+		/* flip the version number to indicate a flipped database. */
+		new_dbip->dbi_version *= -1;
+		/* do NOT write to a flipped database */
+		new_dbip->dbi_read_only = 1;
+	    }
+	}
     }
 
     /* Close current database, if we have one */
@@ -110,10 +154,10 @@ ged_reopen_core(struct ged *gedp, int argc, const char *argv[])
 extern "C" {
 #ifdef GED_PLUGIN
 #include "../include/plugin.h"
-struct ged_cmd_impl reopen_cmd_impl = {"reopen", ged_reopen_core, GED_CMD_DEFAULT};
+struct ged_cmd_impl reopen_cmd_impl = {"reopen", ged_opendb_core, GED_CMD_DEFAULT};
 const struct ged_cmd reopen_cmd = { &reopen_cmd_impl };
 
-struct ged_cmd_impl open_cmd_impl = {"opendb", ged_reopen_core, GED_CMD_DEFAULT};
+struct ged_cmd_impl open_cmd_impl = {"opendb", ged_opendb_core, GED_CMD_DEFAULT};
 const struct ged_cmd open_cmd = { &open_cmd_impl };
 
 
