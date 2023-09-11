455d5525d2b1161d0bd4d940b35d3311998c6fd5
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2023-09-11 16:24:22 -0400

    Not using it yet, but start setting up to build linenoise as a library

--- src/other/linenoise/linenoise.h
+++ src/other/linenoise/linenoise.h
@@ -41,6 +41,28 @@
 extern "C" {
 #endif
 
+#ifndef COMPILER_DLLEXPORT
+# if defined(_WIN32)
+#  define COMPILER_DLLEXPORT __declspec(dllexport)
+#  define COMPILER_DLLIMPORT __declspec(dllimport)
+# else
+#  define COMPILER_DLLEXPORT __attribute__ ((visibility ("default")))
+#  define COMPILER_DLLIMPORT __attribute__ ((visibility ("default")))
+# endif
+#endif
+
+#ifndef LINENOISE_EXPORT
+#  if defined(LINENOISE_DLL_EXPORTS) && defined(LINENOISE_DLL_IMPORTS)
+#    error "Only LINENOISE_DLL_EXPORTS or LINENOISE_DLL_IMPORTS can be defined, not both."
+#  elif defined(LINENOISE_DLL_EXPORTS)
+#    define LINENOISE_EXPORT COMPILER_DLLEXPORT
+#  elif defined(LINENOISE_DLL_IMPORTS)
+#    define LINENOISE_EXPORT COMPILER_DLLIMPORT
+#  else
+#    define LINENOISE_EXPORT
+#  endif
+#endif
+
 #ifndef NO_COMPLETION
 typedef struct linenoiseCompletions {
   size_t len;
@@ -56,18 +78,18 @@ typedef void(linenoiseCompletionCallback)(const char *prefix, linenoiseCompletio
  * Sets the current tab completion handler and returns the previous one, or NULL
  * if no prior one has been set.
  */
-linenoiseCompletionCallback * linenoiseSetCompletionCallback(linenoiseCompletionCallback *comp, void *userdata);
+LINENOISE_EXPORT linenoiseCompletionCallback * linenoiseSetCompletionCallback(linenoiseCompletionCallback *comp, void *userdata);
 
 /*
  * Adds a copy of the given string to the given completion list. The copy is owned
  * by the linenoiseCompletions object.
  */
-void linenoiseAddCompletion(linenoiseCompletions *comp, const char *str);
+LINENOISE_EXPORT void linenoiseAddCompletion(linenoiseCompletions *comp, const char *str);
 
 typedef char*(linenoiseHintsCallback)(const char *, int *color, int *bold, void *userdata);
 typedef void(linenoiseFreeHintsCallback)(void *hint, void *userdata);
-void linenoiseSetHintsCallback(linenoiseHintsCallback *callback, void *userdata);
-void linenoiseSetFreeHintsCallback(linenoiseFreeHintsCallback *callback);
+LINENOISE_EXPORT void linenoiseSetHintsCallback(linenoiseHintsCallback *callback, void *userdata);
+LINENOISE_EXPORT void linenoiseSetFreeHintsCallback(linenoiseFreeHintsCallback *callback);
 
 #endif
 
@@ -78,22 +100,22 @@ void linenoiseSetFreeHintsCallback(linenoiseFreeHintsCallback *callback);
  * a copy of the entered string (for ENTER) or NULL (on EOF).  The
  * caller owns the returned string and must eventually free() it.
  */
-char *linenoise(const char *prompt);
+LINENOISE_EXPORT char *linenoise(const char *prompt);
 
 /**
  * Like linenoise() but starts with an initial buffer.
  */
-char *linenoiseWithInitial(const char *prompt, const char *initial);
+LINENOISE_EXPORT char *linenoiseWithInitial(const char *prompt, const char *initial);
 
 /**
  * Clear the screen.
  */
-void linenoiseClearScreen(void);
+LINENOISE_EXPORT void linenoiseClearScreen(void);
 
 /*
  * Adds a copy of the given line of the command history.
  */
-int linenoiseHistoryAdd(const char *line);
+LINENOISE_EXPORT int linenoiseHistoryAdd(const char *line);
 
 /*
  * Sets the maximum length of the command history, in lines.
@@ -101,46 +123,46 @@ int linenoiseHistoryAdd(const char *line);
  * retaining only the most recent entries. If len is 0 or less
  * then this function does nothing.
  */
-int linenoiseHistorySetMaxLen(int len);
+LINENOISE_EXPORT int linenoiseHistorySetMaxLen(int len);
 
 /*
  * Returns the current maximum length of the history, in lines.
  */
-int linenoiseHistoryGetMaxLen(void);
+LINENOISE_EXPORT int linenoiseHistoryGetMaxLen(void);
 
 /*
  * Saves the current contents of the history to the given file.
  * Returns 0 on success.
  */
-int linenoiseHistorySave(const char *filename);
+LINENOISE_EXPORT int linenoiseHistorySave(const char *filename);
 
 /*
  * Replaces the current history with the contents
  * of the given file.  Returns 0 on success.
  */
-int linenoiseHistoryLoad(const char *filename);
+LINENOISE_EXPORT int linenoiseHistoryLoad(const char *filename);
 
 /*
  * Frees all history entries, clearing the history.
  */
-void linenoiseHistoryFree(void);
+LINENOISE_EXPORT void linenoiseHistoryFree(void);
 
 /*
  * Returns a pointer to the list of history entries, writing its
  * length to *len if len is not NULL. The memory is owned by linenoise
  * and must not be freed.
  */
-char **linenoiseHistory(int *len);
+LINENOISE_EXPORT char **linenoiseHistory(int *len);
 
 /*
  * Returns the number of display columns in the current terminal.
  */
-int linenoiseColumns(void);
+LINENOISE_EXPORT int linenoiseColumns(void);
 
 /**
  * Enable or disable multiline mode (disabled by default)
  */
-void linenoiseSetMultiLine(int enableml);
+LINENOISE_EXPORT void linenoiseSetMultiLine(int enableml);
 
 #ifdef __cplusplus
 }
