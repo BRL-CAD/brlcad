717d78983185307e803b097e54743495f6709628
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2023-09-26 14:33:26 -0400

    If we have no contours, don't try to fill.

--- src/libbg/polygon_bview.c
+++ src/libbg/polygon_bview.c
@@ -78,14 +78,13 @@ bv_fill_polygon(struct bv_scene_obj *s)
 
     struct bv_polygon *p = (struct bv_polygon *)s->s_i_data;
 
-    if (p->polygon.contour[0].open) {
+    if (!p || !p->polygon.num_contours)
 	return;
-    }
-    if (p->fill_delta < BN_TOL_DIST) {
-	return;
-    }
 
-    if (!p->polygon.num_contours)
+    if (!p->polygon.contour || p->polygon.contour[0].open)
+	return;
+
+    if (p->fill_delta < BN_TOL_DIST)
 	return;
 
     struct bg_polygon *fill = bg_polygon_fill_segments(&p->polygon, p->fill_dir, p->fill_delta);
