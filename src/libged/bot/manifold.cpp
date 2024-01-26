75e579d02aa764d8f00c2accce0f241072fe2c59
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2024-01-26 12:08:32 -0500

    Not correct yet, but start figuring out how to use geogram to process BoT data.

--- src/libged/bot/manifold.cpp
+++ src/libged/bot/manifold.cpp
@@ -50,7 +50,12 @@
 #endif
 
 #include "geogram/basic/process.h"
+#include <geogram/basic/command_line.h>
+#include <geogram/basic/command_line_args.h>
 #include "geogram/mesh/mesh.h"
+#include "geogram/mesh/mesh_geometry.h"
+#include "geogram/mesh/mesh_repair.h"
+#include "geogram/mesh/mesh_fill_holes.h"
 
 #include "bu/cmd.h"
 #include "bu/color.h"
@@ -63,16 +68,165 @@
 #include "./ged_bot.h"
 #include "../ged_private.h"
 
+static struct rt_bot_internal *
+manifold_process(struct rt_bot_internal *bot, int repair)
+{
+    if (!bot)
+	return NULL;
+
+    manifold::Mesh bot_mesh;
+    for (size_t j = 0; j < bot->num_vertices ; j++)
+	bot_mesh.vertPos.push_back(glm::vec3(bot->vertices[3*j], bot->vertices[3*j+1], bot->vertices[3*j+2]));
+    if (bot->orientation == RT_BOT_CW) {
+	for (size_t j = 0; j < bot->num_faces; j++)
+	    bot_mesh.triVerts.push_back(glm::vec3(bot->faces[3*j], bot->faces[3*j+2], bot->faces[3*j+1]));
+    } else {
+	for (size_t j = 0; j < bot->num_faces; j++)
+	    bot_mesh.triVerts.push_back(glm::vec3(bot->faces[3*j], bot->faces[3*j+1], bot->faces[3*j+2]));
+    }
+
+    manifold::MeshGL bot_gl(bot_mesh);
+    if (!bot_gl.NumTri()) {
+	// GetMeshGL failed, cannot process.
+	return NULL;
+    }
+
+    if (!bot_gl.Merge()) {
+	// BoT is already manifold
+	return bot;
+    }
+
+    // If we're not going to try and repair it, it's just a
+    // non-manifold mesh report at this point.
+    if (!repair)
+	return NULL;
+
+    manifold::Manifold omanifold(bot_gl);
+    if (omanifold.Status() != manifold::Manifold::Error::NoError) {
+	// MeshGL.Merge() failed to produce a manifold mesh"
+	return NULL;
+    }
+
+    // Original wasn't manifold, but the Manifold Merge() function was
+    // able to produce a variation that is.  Make a new BoT
+    manifold::Mesh omesh = omanifold.GetMesh();
+    struct rt_bot_internal *nbot;
+    BU_GET(nbot, struct rt_bot_internal);
+    nbot->magic = RT_BOT_INTERNAL_MAGIC;
+    nbot->mode = RT_BOT_SOLID;
+    nbot->orientation = RT_BOT_CCW;
+    nbot->thickness = NULL;
+    nbot->face_mode = (struct bu_bitv *)NULL;
+    nbot->bot_flags = 0;
+    nbot->num_vertices = (int)omesh.vertPos.size();
+    nbot->num_faces = (int)omesh.triVerts.size();
+    nbot->vertices = (double *)calloc(omesh.vertPos.size()*3, sizeof(double));;
+    nbot->faces = (int *)calloc(omesh.triVerts.size()*3, sizeof(int));
+    for (size_t j = 0; j < omesh.vertPos.size(); j++) {
+	nbot->vertices[3*j] = omesh.vertPos[j].x;
+	nbot->vertices[3*j+1] = omesh.vertPos[j].y;
+	nbot->vertices[3*j+2] = omesh.vertPos[j].z;
+    }
+    for (size_t j = 0; j < omesh.triVerts.size(); j++) {
+	nbot->faces[3*j] = omesh.triVerts[j].x;
+	nbot->faces[3*j+1] = omesh.triVerts[j].y;
+	nbot->faces[3*j+2] = omesh.triVerts[j].z;
+    }
+
+    return nbot;
+}
+
+static struct rt_bot_internal *
+geogram_mesh_repair(struct rt_bot_internal *bot)
+{
+    if (!bot)
+	return NULL;
+
+    GEO::CmdLine::set_arg("algo:hole_filling", "loop_split");
+
+    GEO::Mesh gm;
+    gm.vertices.assign_points((double *)bot->vertices, 3, bot->num_vertices);
+
+    GEO::vector<GEO::index_t> tris;
+    for (size_t i = 0; i < bot->num_faces * 3; i++) {
+	tris.push_back(bot->faces[i]);
+    }
+    gm.facets.assign_triangle_mesh(tris, false);
+
+    GEO::fill_holes(gm, 1e30);
+
+    GEO::mesh_repair(gm, GEO::MeshRepairMode(GEO::MESH_REPAIR_DEFAULT));
+
+    // Post repair, make sure mesh is still a triangle mesh
+    gm.facets.triangulate();
+
+    // Once Geogram is done with it, ask Manifold what it thinks
+    // of the result - if Manifold doesn't think we're there, then
+    // the results won't fly for boolean evaluation and we go ahead
+    // and reject.
+    manifold::Mesh gmm;
+    for(GEO::index_t v = 0; v < gm.vertices.nb(); v++) {
+	double gm_v[3];
+	const double *p = gm.vertices.point_ptr(v);
+	for (int i = 0; i < 3; i++) {
+	    gm_v[i] = p[i];
+	}
+	gmm.vertPos.push_back(glm::vec3(gm_v[0], gm_v[1], gm_v[2]));
+    }
+    for (GEO::index_t f = 0; f < gm.facets.nb(); f++) {
+	double tri_verts[3];
+	for (int i = 0; i < 3; i++) {
+	    tri_verts[i] = gm.facets.vertex(f, i);
+	}
+	// TODO - CW vs CCW orientation handling?
+	gmm.triVerts.push_back(glm::vec3(tri_verts[0], tri_verts[1], tri_verts[2]));
+    }
+
+    manifold::Manifold gmanifold(gmm);
+    if (gmanifold.Status() != manifold::Manifold::Error::NoError) {
+	// Repair failed
+	return NULL;
+    }
+
+    // If output is manifold, make a new bot
+    manifold::Mesh omesh = gmanifold.GetMesh();
+    struct rt_bot_internal *nbot;
+    BU_GET(nbot, struct rt_bot_internal);
+    nbot->magic = RT_BOT_INTERNAL_MAGIC;
+    nbot->mode = RT_BOT_SOLID;
+    nbot->orientation = RT_BOT_CCW;
+    nbot->thickness = NULL;
+    nbot->face_mode = (struct bu_bitv *)NULL;
+    nbot->bot_flags = 0;
+    nbot->num_vertices = (int)omesh.vertPos.size();
+    nbot->num_faces = (int)omesh.triVerts.size();
+    nbot->vertices = (double *)calloc(omesh.vertPos.size()*3, sizeof(double));;
+    nbot->faces = (int *)calloc(omesh.triVerts.size()*3, sizeof(int));
+    for (size_t j = 0; j < omesh.vertPos.size(); j++) {
+	nbot->vertices[3*j] = omesh.vertPos[j].x;
+	nbot->vertices[3*j+1] = omesh.vertPos[j].y;
+	nbot->vertices[3*j+2] = omesh.vertPos[j].z;
+    }
+    for (size_t j = 0; j < omesh.triVerts.size(); j++) {
+	nbot->faces[3*j] = omesh.triVerts[j].x;
+	nbot->faces[3*j+1] = omesh.triVerts[j].y;
+	nbot->faces[3*j+2] = omesh.triVerts[j].z;
+    }
+    return nbot;
+}
+
 extern "C" int
 _bot_cmd_manifold(void *bs, int argc, const char **argv)
 {
-    const char *usage_string = "bot manifold <objname> [outobj]";
-    const char *purpose_string = "Check if Manifold thinks the BoT is a manifold mesh.  If not, if an outobj name has been supplied apply MeshGL Merge to see if can be automatically repaired.  If it can be, the result will be written to outobj.";
+    const char *usage_string = "bot manifold <objname> [repaired_obj_name]";
+    const char *purpose_string = "Check if Manifold thinks the BoT is a manifold mesh.  If not, and a repaired_obj_name has been supplied, attempt to produce a manifold output using objname's geometry as an input.  If successful, the resulting manifold geometry will be written to repaired_obj_name.";
     if (_bot_cmd_msgs(bs, argc, argv, usage_string, purpose_string)) {
 	return BRLCAD_OK;
     }
 
-    // GEO::initialize needs to be called once.
+    // GEO::initialize needs to be called once.  TODO - this probably
+    // should happen once for all of libged, if we use geogram in
+    // multiple commands...
     static int geo_inited = 0;
     if (!geo_inited) {
 	GEO::initialize();
@@ -98,55 +252,55 @@ _bot_cmd_manifold(void *bs, int argc, const char **argv)
 	return BRLCAD_ERROR;
     }
 
-    manifold::Mesh bot_mesh;
-    for (size_t j = 0; j < bot->num_vertices ; j++)
-	bot_mesh.vertPos.push_back(glm::vec3(bot->vertices[3*j], bot->vertices[3*j+1], bot->vertices[3*j+2]));
-    if (bot->orientation == RT_BOT_CW) {
-	for (size_t j = 0; j < bot->num_faces; j++)
-	    bot_mesh.triVerts.push_back(glm::vec3(bot->faces[3*j], bot->faces[3*j+2], bot->faces[3*j+1]));
-    } else {
-	for (size_t j = 0; j < bot->num_faces; j++)
-	    bot_mesh.triVerts.push_back(glm::vec3(bot->faces[3*j], bot->faces[3*j+1], bot->faces[3*j+2]));
-    }
+    int repair_flag = (argc == 2) ?  1 : 0;
 
-    manifold::MeshGL bot_gl(bot_mesh);
-    if (!bot_gl.NumTri()) {
-	bu_vls_printf(gb->gedp->ged_result_str, "%s: GetMeshGL failed, cannot process.", gb->dp->d_namep);
-	return BRLCAD_ERROR;
-    }
-    if (!bot_gl.Merge()) {
+    struct rt_bot_internal *mbot = manifold_process(bot, repair_flag);
+
+    // If we're already manifold, there's nothing to do
+    if (mbot && mbot == bot) {
 	bu_vls_printf(gb->gedp->ged_result_str, "BoT %s is manifold", gb->dp->d_namep);
-	return BRLCAD_ERROR;
+	return BRLCAD_OK;
     }
-    manifold::Manifold omanifold(bot_gl);
-    if (omanifold.Status() != manifold::Manifold::Error::NoError) {
-	// Tried, but output isn't manifold - no dice.
-	bu_vls_printf(gb->gedp->ged_result_str, "%s: MeshGL.Merge() failed to produce a manifold mesh - repair failed", gb->dp->d_namep);
+
+    // If we don't have a repair target, just report non-manifold
+    if (!mbot && !repair_flag) {
+    	bu_vls_printf(gb->gedp->ged_result_str, "BoT %s is NOT manifold", gb->dp->d_namep);
 	return BRLCAD_ERROR;
     }
 
-    manifold::Mesh omesh = omanifold.GetMesh();
-    struct rt_bot_internal *nbot;
-    BU_GET(nbot, struct rt_bot_internal);
-    nbot->magic = RT_BOT_INTERNAL_MAGIC;
-    nbot->mode = RT_BOT_SOLID;
-    nbot->orientation = RT_BOT_CCW;
-    nbot->thickness = NULL;
-    nbot->face_mode = (struct bu_bitv *)NULL;
-    nbot->bot_flags = 0;
-    nbot->num_vertices = (int)omesh.vertPos.size();
-    nbot->num_faces = (int)omesh.triVerts.size();
-    nbot->vertices = (double *)calloc(omesh.vertPos.size()*3, sizeof(double));;
-    nbot->faces = (int *)calloc(omesh.triVerts.size()*3, sizeof(int));
-    for (size_t j = 0; j < omesh.vertPos.size(); j++) {
-	nbot->vertices[3*j] = omesh.vertPos[j].x;
-	nbot->vertices[3*j+1] = omesh.vertPos[j].y;
-	nbot->vertices[3*j+2] = omesh.vertPos[j].z;
+    // If we're repairing and Manifold Merge was able to fix it, go with that
+    if (mbot && repair_flag) {
+	struct rt_db_internal intern;
+	RT_DB_INTERNAL_INIT(&intern);
+	intern.idb_major_type = DB5_MAJORTYPE_BRLCAD;
+	intern.idb_type = ID_BOT;
+	intern.idb_meth = &OBJ[ID_BOT];
+	intern.idb_ptr = (void *)mbot;
+
+	const char *rname = argv[1];
+
+	struct directory *dp = db_diradd(gb->gedp->dbip, rname, RT_DIR_PHONY_ADDR, 0, RT_DIR_SOLID, (void *)&intern.idb_type);
+	if (dp == RT_DIR_NULL) {
+	    bu_vls_printf(gb->gedp->ged_result_str, "Failed to write out new BoT %s", rname);
+	    return BRLCAD_ERROR;
+	}
+
+	if (rt_db_put_internal(dp, gb->gedp->dbip, &intern, &rt_uniresource) < 0) {
+	    bu_vls_printf(gb->gedp->ged_result_str, "Failed to write out new BoT %s", rname);
+	    return BRLCAD_ERROR;
+	}
+
+	bu_vls_printf(gb->gedp->ged_result_str, "Repair completed successfully (Manifold Merge op, minimal changes) and written to %s", rname);
+	return BRLCAD_OK;
     }
-    for (size_t j = 0; j < omesh.triVerts.size(); j++) {
-	nbot->faces[3*j] = omesh.triVerts[j].x;
-	nbot->faces[3*j+1] = omesh.triVerts[j].y;
-	nbot->faces[3*j+2] = omesh.triVerts[j].z;
+
+    // Not manifold, not fixed by the first pass, and we want to repair it.
+    // Time to attempt mesh repair
+    mbot = geogram_mesh_repair(bot);
+
+    if (!mbot) {
+	bu_vls_printf(gb->gedp->ged_result_str, "Unable to repair BoT %s", gb->dp->d_namep);
+	return BRLCAD_ERROR;
     }
 
     struct rt_db_internal intern;
@@ -154,14 +308,22 @@ _bot_cmd_manifold(void *bs, int argc, const char **argv)
     intern.idb_major_type = DB5_MAJORTYPE_BRLCAD;
     intern.idb_type = ID_BOT;
     intern.idb_meth = &OBJ[ID_BOT];
-    intern.idb_ptr = (void *)nbot;
+    intern.idb_ptr = (void *)mbot;
 
-    if (rt_db_put_internal(gb->dp, gb->gedp->dbip, &intern, &rt_uniresource) < 0) {
-	bu_vls_printf(gb->gedp->ged_result_str, "Failed to write out new BoT %s", gb->dp->d_namep);
+    const char *rname = argv[1];
+
+    struct directory *dp = db_diradd(gb->gedp->dbip, rname, RT_DIR_PHONY_ADDR, 0, RT_DIR_SOLID, (void *)&intern.idb_type);
+    if (dp == RT_DIR_NULL) {
+	bu_vls_printf(gb->gedp->ged_result_str, "Failed to write out new BoT %s", rname);
 	return BRLCAD_ERROR;
     }
 
-    bu_vls_printf(gb->gedp->ged_result_str, "Repair complete, written to %s", gb->dp->d_namep);
+    if (rt_db_put_internal(dp, gb->gedp->dbip, &intern, &rt_uniresource) < 0) {
+	bu_vls_printf(gb->gedp->ged_result_str, "Failed to write out new BoT %s", rname);
+	return BRLCAD_ERROR;
+    }
+
+    bu_vls_printf(gb->gedp->ged_result_str, "Repair completed successfully (Geogram hole fill) and written to %s", rname);
     return BRLCAD_OK;
 }
 
