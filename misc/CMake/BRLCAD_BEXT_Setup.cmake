3a19adc6ad121c2ecb4f79fee7568aabc19f300d
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2024-02-16 13:02:31 -0500

    Allow for a pre-supplied bext that isn't a git repo

--- misc/CMake/BRLCAD_BEXT_Setup.cmake
+++ misc/CMake/BRLCAD_BEXT_Setup.cmake
@@ -35,16 +35,26 @@ include(CMakeParseArguments)
 
 function(bext_setup)
 
-  set(BEXT_SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/bext)
   set(BEXT_BUILD_DIR ${CMAKE_CURRENT_BINARY_DIR}/bext_build)
   set(BEXT_INSTALL_DIR ${CMAKE_CURRENT_BINARY_DIR})
 
-  find_program(GIT_EXEC git REQUIRED)
+  # If we don't have
+  if (NOT DEFINED BEXT_SOURCE_DIR)
+    set(BEXT_SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/bext)
+    if (NOT EXISTS ${BEXT_SOURCE_DIR})
+      find_program(GIT_EXEC git REQUIRED)
+      execute_process(COMMAND ${GIT_EXEC} clone https://github.com/BRL-CAD/bext.git)
+    endif (NOT EXISTS ${BEXT_SOURCE_DIR})
+  endif (NOT DEFINED BEXT_SOURCE_DIR)
   if (NOT EXISTS ${BEXT_SOURCE_DIR})
-    execute_process(COMMAND ${GIT_EXEC} clone https://github.com/BRL-CAD/bext.git)
-  else (NOT EXISTS ${BEXT_SOURCE_DIR})
-    execute_process(COMMAND ${GIT_EXEC} pull WORKING_DIRECTORY ${BEXT_SOURCE_DIR})
+    message(FATAL_ERROR "bext directory ${BEXT_SOURCE_DIR} is not present")
   endif (NOT EXISTS ${BEXT_SOURCE_DIR})
+
+  # If we're using a bext git repository, try to make sure it is current
+  if (EXISTS ${BEXT_SOURCE_DIR}/.git)
+    find_program(GIT_EXEC git REQUIRED)
+    execute_process(COMMAND ${GIT_EXEC} pull WORKING_DIRECTORY ${BEXT_SOURCE_DIR})
+  endif (EXISTS ${BEXT_SOURCE_DIR}/.git)
 
   if (NOT EXISTS ${BEXT_BUILD_DIR})
     file(MAKE_DIRECTORY ${BEXT_BUILD_DIR})
