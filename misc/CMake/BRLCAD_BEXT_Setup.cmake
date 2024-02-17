e5b694bd50d60943644dcb330abd8175ab587b78
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2024-02-16 20:44:14 -0500

    Leave it up to the user to determine when to update bext

--- misc/CMake/BRLCAD_BEXT_Setup.cmake
+++ misc/CMake/BRLCAD_BEXT_Setup.cmake
@@ -50,12 +50,6 @@ function(bext_setup)
     message(FATAL_ERROR "bext directory ${BEXT_SOURCE_DIR} is not present")
   endif (NOT EXISTS ${BEXT_SOURCE_DIR})
 
-  # If we're using a bext git repository, try to make sure it is current
-  if (EXISTS ${BEXT_SOURCE_DIR}/.git)
-    find_program(GIT_EXEC git REQUIRED)
-    execute_process(COMMAND ${GIT_EXEC} pull WORKING_DIRECTORY ${BEXT_SOURCE_DIR})
-  endif (EXISTS ${BEXT_SOURCE_DIR}/.git)
-
   if (NOT EXISTS ${BEXT_BUILD_DIR})
     file(MAKE_DIRECTORY ${BEXT_BUILD_DIR})
   endif (NOT EXISTS ${BEXT_BUILD_DIR})
