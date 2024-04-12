48c9463a7c6cae9daf26acbb713ec1148e50ab3b
TheZouave007 <shadow.sprint256@gmail.com>
2024-04-11 23:17:18 -0500

    Changed RT_EXPORT to extern - this code is meant to be used interally by librt

--- src/librt/cut_hlbvh.h
+++ src/librt/cut_hlbvh.h
@@ -18,21 +18,21 @@ struct bvh_flat_node {
 
 #ifndef HLBVH_IMPLEMENTATION
 
-RT_EXPORT struct bu_pool *
+extern struct bu_pool *
 hlbvh_init_pool(size_t n_primatives);
 
-RT_EXPORT struct bvh_build_node *
+extern struct bvh_build_node *
 hlbvh_create(long max_prims_in_node, struct bu_pool *pool, const fastf_t *centroids_prims,
 	     const fastf_t *bounds_prims, long *total_nodes,
 	     const long n_primitives, long **ordered_prims);
 
-RT_EXPORT struct bvh_flat_node *
+extern struct bvh_flat_node *
 hlbvh_flatten(const struct bvh_build_node *root, long nodes_created);
 
-RT_EXPORT void
+extern void
 hlbvh_shot_raw(struct bvh_build_node* root, struct xray* rp, long** check_tris, size_t* num_check_tris);
 
-RT_EXPORT void
+extern void
 hlbvh_shot_flat(struct bvh_flat_node* root, struct xray* rp, long** check_tris, size_t* num_check_tris);
 
 #endif // HLBVH_IMPLEMENTATION
