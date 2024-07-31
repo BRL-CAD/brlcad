7c387658f5636dd1f4694ba5a0c60c47dc20059d
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2024-07-31 10:46:27 -0400

    Do some setup in summary command to get ready to accept object arguments

--- src/libged/summary/summary.c
+++ src/libged/summary/summary.c
@@ -91,9 +91,14 @@ summary_dir(struct ged *gedp,
 int
 ged_summary_core(struct ged *gedp, int argc, const char *argv[])
 {
-    char *cp;
-    int flags = 0;
-    static const char *usage = "[p r g]";
+    int print_help = 0;
+    struct bu_opt_desc d[3];
+    struct bu_vls usage = BU_VLS_INIT_ZERO;
+    struct bu_vls obj_name = BU_VLS_INIT_ZERO;
+    BU_OPT(d[0], "h", "help",      "",         NULL,  &print_help,  "Print help and exit");
+    BU_OPT(d[1], "?",     "",      "",         NULL,  &print_help,  "");
+    BU_OPT(d[1], "o",  "obj",  "name",  &bu_opt_vls,    &obj_name,  "Specify database object to summarize");
+    BU_OPT_NULL(d[2]);
 
     GED_CHECK_DATABASE_OPEN(gedp, BRLCAD_ERROR);
     GED_CHECK_ARGC_GT_0(gedp, argc, BRLCAD_ERROR);
@@ -101,35 +106,86 @@ ged_summary_core(struct ged *gedp, int argc, const char *argv[])
     /* initialize result */
     bu_vls_trunc(gedp->ged_result_str, 0);
 
-    if (argc == 1) {
+    /* parse standard options */
+    int opt_ret = bu_opt_parse(NULL, argc, argv, d);
+
+    /* adjust argc to match the leftovers of the options parsing */
+    argc = opt_ret;
+
+    /* done with command name argv[0] */
+    argc-=(argc>0); argv+=(argc>0);
+
+    if (print_help) {
+	_ged_cmd_help(gedp, bu_vls_cstr(&usage), d);
+	bu_vls_free(&usage);
+	bu_vls_free(&obj_name);
+	return BRLCAD_OK;
+    }
+    bu_vls_free(&usage);
+
+
+    if (!argc && !bu_vls_strlen(&obj_name)) {
 	summary_dir(gedp, 0);
+	bu_vls_free(&obj_name);
 	return BRLCAD_OK;
     }
 
-    if (2 < argc) {
-	bu_vls_printf(gedp->ged_result_str, "Usage: %s %s", argv[0], usage);
-	return BRLCAD_ERROR;
-    }
+    if (argc == 1 && strlen(argv[0]) == 1) {
+	// NOTE:  special casing of p, r and g is deprecated, but for now
+	// handle these options as we originally would have.
+	const char *cp = (const char *)argv[0];
+	int flags = 0;
+	while (*cp) {
+	    switch (*cp++) {
+		case 'p':
+		    flags |= RT_DIR_SOLID;
+		    break;
+		case 'r':
+		    flags |= RT_DIR_REGION;
+		    break;
+		case 'g':
+		    flags |= RT_DIR_COMB;
+		    break;
+		default:
+		    flags = 0;
+	    }
+	}
 
-    cp = (char *)argv[1];
-    while (*cp) {
-	switch (*cp++) {
-	    case 'p':
-		flags |= RT_DIR_SOLID;
-		break;
-	    case 'r':
-		flags |= RT_DIR_REGION;
-		break;
-	    case 'g':
-		flags |= RT_DIR_COMB;
-		break;
-	    default:
-		bu_vls_printf(gedp->ged_result_str, "%s:  p, r or g are the only valid parameters\n", argv[0]);
-		return BRLCAD_ERROR;
+	if (flags) {
+	    summary_dir(gedp, flags);
+	    bu_vls_free(&obj_name);
+	    return BRLCAD_OK;
 	}
     }
 
-    summary_dir(gedp, flags);
+    if (!bu_vls_strlen(&obj_name)) {
+	if (argc != 1) {
+	    bu_vls_printf(gedp->ged_result_str, "expecting a single object name.\n");
+	    bu_vls_free(&obj_name);
+	    return BRLCAD_ERROR;
+	}
+	bu_vls_sprintf(&obj_name, "%s", argv[0]);
+    } else {
+	if (argc) {
+	    bu_vls_printf(gedp->ged_result_str, "object specified both via --obj option and as a command argument.\n");
+	    bu_vls_free(&obj_name);
+	    return BRLCAD_ERROR;
+	}
+    }
+
+    // Summarize the object
+    struct directory *dp = db_lookup(gedp->dbip, bu_vls_cstr(&obj_name), LOOKUP_QUIET);
+    if (dp == RT_DIR_NULL) {
+	bu_vls_printf(gedp->ged_result_str, "object %s not found.\n", bu_vls_cstr(&obj_name));
+	bu_vls_free(&obj_name);
+	return BRLCAD_ERROR;
+    }
+
+    // TODO - primitive specific logic (walk combs, count BoT data, etc...)
+
+    bu_vls_printf(gedp->ged_result_str, "TODO - object summary.\n");
+
+    bu_vls_free(&obj_name);
     return BRLCAD_OK;
 }
 
