c5416cbab77a3feaaeec84caf04221e764f6498c
f4alt <christopherjmacgregor@gmail.com>
2024-08-07 09:25:09 -0400

    have summary be useful for objects

--- src/libged/summary/summary.c
+++ src/libged/summary/summary.c
@@ -32,39 +32,116 @@
 #include "../ged_private.h"
 
 
+struct summary_specifics {
+    const char* name;	// name, if requested summary of object
+
+    /* combinations */
+    int combs;		// num combinations (non-region)
+    int regs;		// num regions
+
+    /* solids */
+    int bots;		// num bot solids
+    int brep;		// num brep solids
+    int othr;		// num primitives (not bot/brep)
+
+    /* solid specifics */
+    unsigned long bot_triangles;
+};
+
+static void comb_counter(struct db_i* dbip, struct directory* dp, void* cdata)
+{
+
+    struct summary_specifics* ssp = (struct summary_specifics*)cdata;
+
+    if (dp->d_flags & RT_DIR_REGION) {
+	ssp->regs++;
+    } else {
+	ssp->combs++;
+    }
+}
+
+static void solid_counter(struct db_i* dbip, struct directory* dp, void* cdata)
+{
+    struct summary_specifics* ssp = (struct summary_specifics*)cdata;
+
+    int id = -1;
+    struct rt_db_internal intern;
+    if (rt_db_get_internal(&intern, dp, dbip, (fastf_t*)NULL, NULL) < 0)
+	return;
+
+    // filter items we care about summarizing
+    int mtype = intern.idb_minor_type;
+    if (mtype == ID_BOT) {
+	struct rt_bot_internal* botip = (struct rt_bot_internal*)intern.idb_ptr;
+	ssp->bots++;
+	ssp->bot_triangles += botip->num_faces;
+    } else if (mtype == ID_BREP) {
+	ssp->brep++;
+    } else {
+	ssp->othr++;
+    }
+}
+
+static void
+print_counts(struct bu_vls* output, struct summary_specifics* ssp)
+{
+    if (ssp->name)
+	bu_vls_printf(output, "Summary (%s):\n", ssp->name);
+    else
+	bu_vls_printf(output, "Summary:\n");
+
+    int sol_total = ssp->bots + ssp->brep + ssp->othr;
+    bu_vls_printf(output, "  %5d region; %d non-region combinations\n", ssp->regs, ssp->combs);
+    bu_vls_printf(output, "  %5d solids\n", sol_total);
+
+    if (ssp->bot_triangles)
+	bu_vls_printf(output, "%10d bots (%lu faces)\n", ssp->bots, ssp->bot_triangles);
+    else
+	bu_vls_printf(output, "%10d bots\n", ssp->bots);
+    bu_vls_printf(output, "%10d breps\n", ssp->brep);
+    bu_vls_printf(output, "%10d BRL-CAD primitives\n", ssp->othr);
+}
+
 /*
  * Summarize the contents of the directory by categories
  * (solid, comb, region).  If flag is != 0, it is interpreted
  * as a request to print all the names in that category (e.g., RT_DIR_SOLID).
  */
 static void
-summary_dir(struct ged *gedp,
-		int flag)
+summary_dir(struct ged *gedp, int flag, struct bu_vls* specific)
 {
-    struct directory *dp;
-    int i;
-    static int sol, comb, reg;
-    struct directory **dirp;
-    struct directory **dirp0 = (struct directory **)NULL;
+    struct directory* dp = NULL;
+    struct directory* specific_dp = NULL;
+    struct summary_specifics ss = {0};
 
-    sol = comb = reg = 0;
-    for (i = 0; i < RT_DBNHASH; i++) {
-	for (dp = gedp->dbip->dbi_Head[i]; dp != RT_DIR_NULL; dp = dp->d_forw) {
-	    if (dp->d_flags & RT_DIR_SOLID)
-		sol++;
-	    if (dp->d_flags & RT_DIR_COMB) {
-		if (dp->d_flags & RT_DIR_REGION)
-		    reg++;
-		else
-		    comb++;
-	    }
+    if (specific) {
+	const char* specific_cstr = bu_vls_cstr(specific);
+	specific_dp = db_lookup(gedp->dbip, specific_cstr, 1);
+	if (!specific_dp) {
+	    // print usage?
+	    return;
 	}
+
+	ss.name = specific_cstr;
+	db_functree(gedp->dbip, specific_dp, comb_counter, solid_counter, NULL, (void*)&ss);
+
+	print_counts(gedp->ged_result_str, &ss);
+	return;
     }
 
-    bu_vls_printf(gedp->ged_result_str, "Summary:\n");
-    bu_vls_printf(gedp->ged_result_str, "  %5d primitives\n", sol);
-    bu_vls_printf(gedp->ged_result_str, "  %5d region; %d non-region combinations\n", reg, comb);
-    bu_vls_printf(gedp->ged_result_str, "  %5d total objects\n\n", sol+reg+comb);
+    // get generic counts for database
+    FOR_ALL_DIRECTORY_START(dp, gedp->dbip) {
+	// do stuff with count
+	if (dp->d_flags & RT_DIR_SOLID) {
+	    ss.othr++;
+	} else if (dp->d_flags & RT_DIR_REGION) {
+	    ss.regs++;
+	} else if (dp->d_flags & RT_DIR_COMB) {
+	    ss.combs++;
+	} // else - we don't care
+    } FOR_ALL_DIRECTORY_END
+
+    print_counts(gedp->ged_result_str, &ss);
 
     if (flag == 0)
 	return;
@@ -72,16 +149,16 @@ summary_dir(struct ged *gedp,
     /* Print all names matching the flags parameter */
     /* THIS MIGHT WANT TO BE SEPARATED OUT BY CATEGORY */
 
-    dirp = _ged_dir_getspace(gedp->dbip, 0);
-    dirp0 = dirp;
+    struct directory** dirp = _ged_dir_getspace(gedp->dbip, 0);
+    struct directory** dirp0 = dirp;
     /*
      * Walk the directory list adding pointers (to the directory entries
      * of interest) to the array
      */
-    for (i = 0; i < RT_DBNHASH; i++)
-	for (dp = gedp->dbip->dbi_Head[i]; dp != RT_DIR_NULL; dp = dp->d_forw)
-	    if (dp->d_flags & flag)
-		*dirp++ = dp;
+    FOR_ALL_DIRECTORY_START(dp, gedp->dbip) {
+	if (dp->d_flags & flag)
+	    *dirp++ = dp;
+    } FOR_ALL_DIRECTORY_END
 
     _ged_vls_col_pr4v(gedp->ged_result_str, dirp0, (int)(dirp - dirp0), 0, 0);
     bu_free((void *)dirp0, "dir_getspace");
@@ -92,13 +169,13 @@ int
 ged_summary_core(struct ged *gedp, int argc, const char *argv[])
 {
     int print_help = 0;
-    struct bu_opt_desc d[3];
+    struct bu_opt_desc d[4];
     struct bu_vls usage = BU_VLS_INIT_ZERO;
     struct bu_vls obj_name = BU_VLS_INIT_ZERO;
     BU_OPT(d[0], "h", "help",      "",         NULL,  &print_help,  "Print help and exit");
     BU_OPT(d[1], "?",     "",      "",         NULL,  &print_help,  "");
-    BU_OPT(d[1], "o",  "obj",  "name",  &bu_opt_vls,    &obj_name,  "Specify database object to summarize");
-    BU_OPT_NULL(d[2]);
+    BU_OPT(d[2], "o",  "obj",  "name",  &bu_opt_vls,    &obj_name,  "Specify database object to summarize");
+    BU_OPT_NULL(d[3]);
 
     GED_CHECK_DATABASE_OPEN(gedp, BRLCAD_ERROR);
     GED_CHECK_ARGC_GT_0(gedp, argc, BRLCAD_ERROR);
@@ -123,13 +200,14 @@ ged_summary_core(struct ged *gedp, int argc, const char *argv[])
     }
     bu_vls_free(&usage);
 
-
+    /* must be wanting database summary */
     if (!argc && !bu_vls_strlen(&obj_name)) {
-	summary_dir(gedp, 0);
+	summary_dir(gedp, 0, NULL);
 	bu_vls_free(&obj_name);
 	return BRLCAD_OK;
     }
 
+    /* TODO: deprecate me */
     if (argc == 1 && strlen(argv[0]) == 1) {
 	// NOTE:  special casing of p, r and g is deprecated, but for now
 	// handle these options as we originally would have.
@@ -152,12 +230,13 @@ ged_summary_core(struct ged *gedp, int argc, const char *argv[])
 	}
 
 	if (flags) {
-	    summary_dir(gedp, flags);
+	    summary_dir(gedp, flags, NULL);
 	    bu_vls_free(&obj_name);
 	    return BRLCAD_OK;
 	}
     }
 
+    /* ensure we have one object name */
     if (!bu_vls_strlen(&obj_name)) {
 	if (argc != 1) {
 	    bu_vls_printf(gedp->ged_result_str, "expecting a single object name.\n");
@@ -173,18 +252,10 @@ ged_summary_core(struct ged *gedp, int argc, const char *argv[])
 	}
     }
 
-    // Summarize the object
-    struct directory *dp = db_lookup(gedp->dbip, bu_vls_cstr(&obj_name), LOOKUP_QUIET);
-    if (dp == RT_DIR_NULL) {
-	bu_vls_printf(gedp->ged_result_str, "object %s not found.\n", bu_vls_cstr(&obj_name));
-	bu_vls_free(&obj_name);
-	return BRLCAD_ERROR;
-    }
+    /* Summarize the object */
+    summary_dir(gedp, 0, &obj_name);
 
-    // TODO - primitive specific logic (walk combs, count BoT data, etc...)
-
-    bu_vls_printf(gedp->ged_result_str, "TODO - object summary.\n");
-
+    /* cleanup */
     bu_vls_free(&obj_name);
     return BRLCAD_OK;
 }
