b904337def310c004557be05396d3bcb8e8f2ca3
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2024-09-19 10:50:47 -0400

    Fail earlier and with error messages in GetFileNameFromHandle
    The reason we were getting a nonsense string rather than an empty string
    from GetFileNameFromHandle appears to be the string copy still taking
    place even if the CreateFileMapping operation failed.
    
    Rework logic to bail immediately if one of the steps fails.

--- src/libbu/fchmod.c
+++ src/libbu/fchmod.c
@@ -58,11 +58,6 @@ extern int fchmod(int, mode_t);
 int
 GetFileNameFromHandle(HANDLE hFile, char filepath[])
 {
-    int bSuccess = 0;
-    TCHAR pszFilename[MAXPATHLEN+1];
-    char filename[MAXPATHLEN+1];
-    HANDLE hFileMap;
-
     /* Get the file size. */
     DWORD dwFileSizeHi = 0;
     DWORD dwFileSizeLo = GetFileSize(hFile, &dwFileSizeHi);
@@ -73,65 +68,83 @@ GetFileNameFromHandle(HANDLE hFile, char filepath[])
     }
 
     /* Create a file mapping object. */
-    hFileMap = CreateFileMapping(hFile, NULL, PAGE_READONLY, 0, 1, NULL);
+    HANDLE hFileMap = CreateFileMapping(hFile, NULL, PAGE_READONLY, 0, 1, NULL);
 
-    if (hFileMap) {
-	/* Create a file mapping to get the file name. */
-	void* pMem = MapViewOfFile(hFileMap, FILE_MAP_READ, 0, 0, 1);
+    if (!hFileMap) {
+	_tprintf(TEXT("CreateFileMapping failed, unable to retrieve filename\n"));
+	return FALSE;
+    }
 
-	if (pMem) {
-	    if (GetMappedFileName (GetCurrentProcess(), pMem, pszFilename, MAXPATHLEN)) {
+    /* Create a file mapping to get the file name. */
+    void* pMem = MapViewOfFile(hFileMap, FILE_MAP_READ, 0, 0, 1);
 
-		/* Translate path with device name to drive letters. */
-		TCHAR szTemp[MAXPATHLEN+1];
-		szTemp[0] = '\0';
+    if (!pMem) {
+	_tprintf(TEXT("MapViewOfFile failed, unable to retrieve filename\n"));
+	CloseHandle(hFileMap);
+	return FALSE;
+    }
 
-		if (GetLogicalDriveStrings(MAXPATHLEN, szTemp)) {
-		    TCHAR szName[MAXPATHLEN];
-		    TCHAR szDrive[3] = TEXT(" :");
-		    int bFound = 0;
-		    TCHAR* p = szTemp;
+    TCHAR pszFilename[MAXPATHLEN+1];
+    if (!GetMappedFileName (GetCurrentProcess(), pMem, pszFilename, MAXPATHLEN)) {
+	_tprintf(TEXT("GetMappedFileName failed, unable to retrieve filename\n"));
+	UnmapViewOfFile(pMem);
+	CloseHandle(hFileMap);
+	return FALSE;
+    }
 
-		    do {
-			/* Copy the drive letter to the template string */
-			*szDrive = *p;
+    /* Translate path with device name to drive letters. */
+    TCHAR szTemp[MAXPATHLEN+1];
+    szTemp[0] = '\0';
 
-			/* Look up each device name */
-			if (QueryDosDevice(szDrive, szName, MAXPATHLEN)) {
-			    size_t uNameLen = _tcslen(szName);
+    if (!GetLogicalDriveStrings(MAXPATHLEN, szTemp)) {
+	_tprintf(TEXT("GetLogicalDriveStrings failed, unable to retrieve filename\n"));
+	UnmapViewOfFile(pMem);
+	CloseHandle(hFileMap);
+	return FALSE;
+    }
 
-			    if (uNameLen < MAXPATHLEN) {
-				bFound = _tcsnicmp(pszFilename, szName, uNameLen) == 0;
+    TCHAR szName[MAXPATHLEN];
+    TCHAR szDrive[3] = TEXT(" :");
+    int bFound = 0;
+    const TCHAR* p = szTemp;
 
-				if (bFound && *(pszFilename + uNameLen) == _T('\\')) {
-				    /* Reconstruct pszFilename using szTempFile */
-				    /* Replace device path with DOS path */
-				    TCHAR szTempFile[MAXPATHLEN];
-				    StringCchPrintf(szTempFile, MAXPATHLEN, TEXT("%s%s"), szDrive, pszFilename+uNameLen);
-				    StringCchCopyN(pszFilename, MAXPATHLEN+1, szTempFile, _tcslen(szTempFile));
-				}
-			    }
-			}
+    do {
+	/* Copy the drive letter to the template string */
+	*szDrive = *p;
 
-			/* Go to the next NULL character. */
-			while (*p++);
-		    } while (!bFound && *p)
-			; /* end of string */
+	/* Look up each device name */
+	if (QueryDosDevice(szDrive, szName, MAXPATHLEN)) {
+	    size_t uNameLen = _tcslen(szName);
+
+	    if (uNameLen < MAXPATHLEN) {
+		bFound = _tcsnicmp(pszFilename, szName, uNameLen) == 0;
+
+		if (bFound && *(pszFilename + uNameLen) == _T('\\')) {
+		    /* Reconstruct pszFilename using szTempFile */
+		    /* Replace device path with DOS path */
+		    TCHAR szTempFile[MAXPATHLEN];
+		    StringCchPrintf(szTempFile, MAXPATHLEN, TEXT("%s%s"), szDrive, pszFilename+uNameLen);
+		    StringCchCopyN(pszFilename, MAXPATHLEN+1, szTempFile, _tcslen(szTempFile));
 		}
 	    }
-	    bSuccess = TRUE;
-	    UnmapViewOfFile(pMem);
 	}
 
-	CloseHandle(hFileMap);
-    }
+	/* Go to the next NULL character. */
+	while (*p++);
+    } while (!bFound && *p)
+    ; /* end of string */
+
+    UnmapViewOfFile(pMem);
+    CloseHandle(hFileMap);
+
     if (sizeof(TCHAR) == sizeof(wchar_t)) {
+	char filename[MAXPATHLEN+1];
 	wcstombs(filename, (const wchar_t *)pszFilename, MAXPATHLEN);
 	bu_strlcpy(filepath, filename, MAXPATHLEN);
     } else {
 	bu_strlcpy(filepath, pszFilename, MAXPATHLEN);
     }
-    return (bSuccess);
+    return TRUE;
 }
 #endif
 
