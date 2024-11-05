0b489b40e8680f07c96754df815ea1873feb0ee6
Christopher Sean Morrison <brlcad@gmail.com>
2024-11-05 15:17:46 -0500

    convert argument parsing to bu_opt (only using it for help).  document levels.

--- src/libged/solid_report/solid_report.c
+++ src/libged/solid_report/solid_report.c
@@ -24,13 +24,23 @@
  */
 
 #include "common.h"
+
 #include <stdlib.h>
 
+#include "bu/opt.h"
 #include "ged.h"
 #include "../ged_private.h"
 
+
 /*
- * Returns the solid table & vector list as a string
+ * Returns the list of displayed solids and/or vector list information
+ * based on the provided level:
+ *
+ *   <= -2 print primitive names (path leaves)
+ *   == -1 print paths
+ *   == 0 print paths + ILLUM on illuminated
+ *   == 1 print paths + ILLUM on illuminated + center/region/color info
+ *   >= 2 print paths + ILLUM on illuminated + center/region/color info + vector lists
  *
  * Usage:
  * solid_report [lvl]
@@ -39,8 +49,14 @@
 int
 ged_solid_report_core(struct ged *gedp, int argc, const char *argv[])
 {
+    int print_help = 0;
     int lvl = 0;
-    static const char *usage = "lvl";
+    static const char *usage = "[-2|-1|0|1|2|3]";
+    const char *argv0 = argv[0];
+    struct bu_opt_desc d[3];
+    BU_OPT(d[0], "h", "help",      "",         NULL,  &print_help,  "Print help and exit");
+    BU_OPT(d[1], "?",     "",      "",         NULL,  &print_help,  "");
+    BU_OPT_NULL(d[2]);
 
     GED_CHECK_DATABASE_OPEN(gedp, BRLCAD_ERROR);
     GED_CHECK_DRAWABLE(gedp, BRLCAD_ERROR);
@@ -49,19 +65,20 @@ ged_solid_report_core(struct ged *gedp, int argc, const char *argv[])
     /* initialize result */
     bu_vls_trunc(gedp->ged_result_str, 0);
 
-    if (argc > 2) {
-	/* assume user needs help */
-	bu_vls_printf(gedp->ged_result_str, "Usage: %s %s", argv[0], usage);
+    int opt_ret = bu_opt_parse(NULL, argc, argv, d);
+
+    if (opt_ret < 1 || opt_ret > 2 || print_help) {
+	bu_vls_printf(gedp->ged_result_str, "Usage: %s %s", argv0, usage);
 	return BRLCAD_ERROR;
     }
 
-    if (argc > 1)   /* don't use default level */
+    if (opt_ret == 2)
 	lvl = atoi(argv[1]);
 
-    if (lvl <= 3)
-	dl_print_schain(gedp->ged_gdp->gd_headDisplay, gedp->dbip, lvl, 0, gedp->ged_result_str);
-    else
-	dl_print_schain(gedp->ged_gdp->gd_headDisplay, gedp->dbip, 0, 1, gedp->ged_result_str);
+    if (lvl > 3)
+	lvl = 3;
+
+    dl_print_schain(gedp->ged_gdp->gd_headDisplay, gedp->dbip, lvl, 0, gedp->ged_result_str);
 
     return BRLCAD_OK;
 }
