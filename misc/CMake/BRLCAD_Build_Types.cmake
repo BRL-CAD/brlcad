11102587f9ea0830e5b1e0e7deb64376956453d5
Christopher Sean Morrison <brlcad@gmail.com>
2025-01-15 19:56:56 -0500

    shorten the preamble to just the current impact.
    this significantly reduces the commentary to what the code is currently doing, with less elaboration on why.  bext needing to be built in advance is the driving constraint here, so that much is acknowledged.

--- misc/CMake/BRLCAD_Build_Types.cmake
+++ misc/CMake/BRLCAD_Build_Types.cmake
@@ -31,78 +31,25 @@
 # WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
 # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-# Multiconfig on Windows with bext is a major problem.  Using Debug
-# and Release DLLs together is hard:
-# https://stackoverflow.com/questions/74857726/using-a-release-configured-dll-in-another-project-that-is-configured-for-debuggi
-# and early experiments indicate BRL-CAD (and/or its dependencies) are
-# not able to work mixing a Release bext with a Debug BRL-CAD build or
-# vice versa.
 #
-# The options are basically:
+###
 #
-# 1.  Have the CMake logic manage both debug and release
-# configurations of bext, and try to handle only including and copying
-# the configuration we need at runtime somehow.  Slow, complex, error
-# prone, crazy high space requirements, hard to maintain...
-#
-# 2.  Disable multiconfig altogether - reduce the available build
-# configurations displayed in Visual Studio to one, based on a CMake
-# setting.  We already reduce the types to Release and Debug to make
-# Visual Studio match our other build configs, so the mechanism to do
-# this does already exist.  The below logic can then build the one
-# correct bext output type, and bundling becomes simple.  When an
-# external bext_output is used, it will be the user's responsibility
-# to make sure the supplied bext is of a type compatible with their
-# selected build configuration.
-#
-# At the moment we're opting for #2 - force CMAKE_CONFIGURATION_TYPES
-# to be set to a single value and matching a value supplied to
-# CMAKE_BUILD_TYPE (or defaulting to Debug if no such setting is
-# supplied.)  Essentially, this forces multiconfig build tools to work
-# more like a basic make/ninja build, requiring a full configure pass
-# to change build configs.  Users would be responsible for pointing
-# the build to a bext_output with the correct build config for their
-# proposed build type, if they're not going to have configure set
-# things up for them.
-#
-# The constraints we're faced with when it comes to bext are:
-#
-# 1.  We need the build outputs of bext in place during the configure
-#     process for find_package to detect - that would mean always
-#     building *both* configurations if we have a multiconfig build
-#     tool.
-#
-# 2.  The bext contents must be copied to the correct runtime
-#     locations during the configure process to stage them for
-#     potential use when executables are run during the build process
-#     itself - adding configuration awareness to that process would
-#     significantly increase the complexity.
-#
-# 3.  It's not clear how to get CMake's package detection to support
-#     finding multiple configuration copies of library outputs - some
-#     packages have at least some support for detecting multiple
-#     configs, but many do not.
-#
-# 4.  We would need configuration aware install rules for all the bext
-#     output.
-#
-# 5.  For the case where we don't have a pre-existing bext_output
-#     available and BRL-CAD's configure process needs to do the full
-#     setup, we don't want to have to compile all possible Windows
-#     build configurations of bext during configure time.  In terms of
-#     time, space, and management complexity that's asking for
-#     trouble.  A Debug build of BRL-CAD would not want to waste time
-#     building both Debug and Release versions of bext to have
-#     available, and we can't build bext at run time because the
-#     BRL-CAD configure needs the build outputs of the bext compile to
-#     work.
-#
-# Bottom line - because a huge amount of significant work now has to
-# be finished *before* the BRL-CAD configure is complete, we can't
+# CMAKE_CONFIGURATION_TYPES is forced to a single value that matches
+# the supplied CMAKE_BUILD_TYPE (or defaulting to Release if no such
+# setting is supplied).  This is done because bext compilation must be
+# finished *before* the BRL-CAD configure is complete, thus we can't
 # afford to defer the build type decision to build time anymore.
-
-# BRL-CAD Build Types Configuration
+#
+# We disable multiconfig altogether (elsewhere) which reduces the
+# available build configurations displayed in Visual Studio, XCode,
+# and similar to just the CMAKE_BUILD_TYPE setting.  Supported build
+# types are Release or Debug (not currently imposed here).
+#
+# NOTE: When an external bext_output is used, it is the user's
+# responsibility to make sure the supplied bext is compatible with
+# their selected build configuration.
+#
+###
 
 #---------------------------------------------------------------------
 # Function to initialize Release as the default build type.
