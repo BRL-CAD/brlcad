1028503c388556c7eb6ce5f955c6b96049ad94cb
Christopher Sean Morrison <brlcad@gmail.com>
2025-01-26 02:13:49 -0500

    use av also

--- src/libbu/tests/version.c
+++ src/libbu/tests/version.c
@@ -45,7 +45,7 @@ int
 main(int ac, char *av[])
 {
     printf("%d.%d.%d\n", BRLCAD_VERSION_MAJOR, BRLCAD_VERSION_MINOR, BRLCAD_VERSION_PATCH);
-    if (ac > 1)
+    if (ac > 1 && av)
 	return BRLCAD_VERSION_MAJOR+BRLCAD_VERSION_MINOR+BRLCAD_VERSION_PATCH;
     else
 	return 0;
