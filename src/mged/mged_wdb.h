2eb54707b1b954fcbe76d92e5bcbd8a583071811
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2025-06-18 15:12:18 -0400

    Update copyright

--- src/mged/mged_wdb.h
+++ src/mged/mged_wdb.h
@@ -1,7 +1,7 @@
 /*                     M G E D _ W D B . H
  * BRL-CAD
  *
- * Copyright (c) 2024 United States Government as represented by
+ * Copyright (c) 2024-2025 United States Government as represented by
  * the U.S. Army Research Laboratory.
  *
  * This program is free software; you can redistribute it and/or
