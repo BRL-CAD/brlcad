5ec87a7f726ef57facd595459b979e8287072bb3
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2025-06-25 20:43:19 -0400

    Explore what making a scene obj looks like with new setup.

--- src/libged/lint/lint.cpp
+++ src/libged/lint/lint.cpp
@@ -347,9 +347,13 @@ ged_lint_core(struct ged *gedp, int argc, const char *argv[])
     }
 
     if (visualize) {
-	struct bview *view = gedp->ged_gvp;
 	if (gedp->new_cmd_forms) {
-	    bv_vlblock_obj(ldata.vbp, view, "lint_visual");
+	    BViewState *bvs = gedp->dbi_state->GetBViewState();
+	    bvs->RemoveObj("lint_visual");
+	    struct bv_scene_obj *s = bv_obj_get(gedp->free_scene_objs);
+	    bu_vls_sprintf(&s->s_name, "lint_visual");
+	    bv_vlblock_obj(s, ldata.vbp);
+	    bvs->AddObj(s);
 	} else {
 	    _ged_cvt_vlblock_to_solids(gedp, ldata.vbp, "lint_visual", 0);
 	}
