4890caf981a9d397a012347ceb366bdd7b034593
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2025-07-16 10:38:29 -0400

    A 0 threshold is pointless, use 1 as a minimum

--- src/gtools/gdiff/main.cpp
+++ src/gtools/gdiff/main.cpp
@@ -284,8 +284,8 @@ gdiff_grp_th(struct bu_vls *msg, size_t argc, const char **argv, void *set_var)
     // Sanity
     if ((*th) > 100)
 	(*th) = 100;
-    if ((*th) < 100)
-	(*th) = 0;
+    if ((*th) < 1)
+	(*th) = 1;
     return ret;
 }
 
@@ -322,7 +322,7 @@ main(int argc, const char **argv)
     BU_OPT(d[8],  "t", "tolerance",  "#",       &bu_opt_fastf_t,   &state->diff_tol->dist,   "numerical distance tolerance for same/different decisions (RT_LEN_TOL is default)");
     BU_OPT(d[9],  "v", "verbosity",  "",        &bu_opt_incr_long, &state->verbosity,        "increase output verbosity (multiple specifications of -v increase verbosity more)");
     BU_OPT(d[10], "q", "quiet",      "",        &bu_opt_incr_long, &state->quiet,            "decrease output verbosity (multiple specifications of -q decrease verbosity more)");
-    BU_OPT(d[11], "G", "groups",     "<#>",     &gdiff_grp_th,     &group_threshold,         "group files into similar bins per a similarity hashing threshold (0-100, default 70)");
+    BU_OPT(d[11], "G", "groups",     "<#>",     &gdiff_grp_th,     &group_threshold,         "group files into similar bins per a similarity threshold (1-100, default 70).  (Lower number means allow less similar files to group.)");
     BU_OPT_NULL(d[12]);
 
     int ret_ac = bu_opt_parse(&msg, argc, argv, d);
