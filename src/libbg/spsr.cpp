691c24a7eab11336dd8f01437e61c097d93fdd12
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2025-07-18 14:50:27 -0400

    AI for the win again - update to newest PoissonRecon API.

--- src/libbg/spsr.cpp
+++ src/libbg/spsr.cpp
@@ -1,7 +1,7 @@
 /*                         S P S R . C P P
  * BRL-CAD
  *
- * Copyright (c) 2015-2024 United States Government as represented by
+ * Copyright (c) 2015-2025 United States Government as represented by
  * the U.S. Army Research Laboratory.
  *
  * This library is free software; you can redistribute it and/or
@@ -19,13 +19,10 @@
  */
 /** @file spsr.cpp
  *
- * Interface to Adaptive Multigrid Solvers Version 16.10.
- * (AMS originally was the Screened Poisson Surface Reconstruction code.)
+ * Interface to Adaptive Multigrid Solvers for Screened Poisson Surface Reconstruction
+ * Compatible with mkazhdan/PoissonRecon API (v18.x+).
  *
- * TODO - update to 18.70 API - apparently there have been significant changes
- * upstream.
- *
- * TODO - it may be worth porting the Open3D ball pivot code to vmath.h
+ *  TODO - it may be worth porting the Open3D ball pivot code to vmath.h
  * types and using straight nanoflann to provide another alternative to
  * the SPSR methodology:
  *
@@ -48,78 +45,76 @@
 #include "bg/spsr.h"
 #include "SPSR/Reconstructors.h"
 
-template< typename Real , unsigned int Dim >
-struct PtStream : public Reconstructor::InputSampleStream<Real, Dim>
+using namespace PoissonRecon;
+
+// Input stream for oriented samples: implements `read` for coordinates and normals
+template<typename Real, unsigned int Dim>
+struct PtStream : public Reconstructor::InputOrientedSampleStream<Real, Dim>
 {
-    // Constructs a stream that contains the specified number of samples
-    PtStream( const point_t *ipnts, const vect_t *inorms, int cnt) : _size(cnt) , _current(0), _input_pnts(ipnts), _input_nrmls(inorms) {}
-
-    // Overrides the pure abstract method from InputSampleStream< Real , Dim >
-    void reset( void ){ _current = 0; }
-
-    // Overrides the pure abstract method from InputSampleStream< Real , Dim >
-    bool base_read( Point< Real, Dim > &p , Point< Real, Dim > &n )
+    PtStream(const point_t *ipnts, const vect_t *inorms, int cnt)
+	: _size(cnt), _current(0), _input_pnts(ipnts), _input_nrmls(inorms) {}
+    void reset(void) { _current = 0; }
+    bool read(Point<Real, Dim> &p, Point<Real, Dim> &n)
     {
-	if( _current < _size )
-	{
-	    for( size_t d = 0 ; d < Dim; d++ )
-	       	p[d] = _input_pnts[_current][d];
-	    for( size_t d = 0 ; d < Dim; d++ )
-	       	n[d] = _input_nrmls[_current][d];
+	if (_current < _size) {
+	    for (size_t d = 0; d < Dim; d++)
+		p[d] = _input_pnts[_current][d];
+	    for (size_t d = 0; d < Dim; d++)
+		n[d] = _input_nrmls[_current][d];
 	    _current++;
 	    return true;
 	}
-	else return false;
+	return false;
     }
-
     protected:
-    unsigned int _size , _current;
+    unsigned int _size, _current;
     const point_t *_input_pnts;
     const vect_t *_input_nrmls;
 };
 
-// A stream into which we can write polygons of the form std::vector< node_index_type >
-template< typename Index >
-struct PolygonStream : public Reconstructor::OutputFaceStream< 2 >
+// Output face stream: must implement write() and size()
+template <typename Index>
+struct PolygonStream : public Reconstructor::OutputFaceStream<2>
 {
-    // Construct a stream that adds polygons to the vector of polygons
-    PolygonStream( std::vector< std::vector< Index > > &polygonStream ) : _polygons( polygonStream ) {}
-
-    // Override the pure abstract method from OutputPolygonStream
-    void base_write( const std::vector< node_index_type > &polygon )
+    PolygonStream(std::vector<std::vector<Index>> &polygonStream) : _polygons(polygonStream) {}
+    size_t size(void) const { return _polygons.size(); }
+    size_t write(const std::vector<node_index_type> &polygon)
     {
-	std::vector< Index > poly( polygon.size() );
-	for( unsigned int i=0 ; i<polygon.size() ; i++ ) poly[i] = (Index)polygon[i];
-	_polygons.push_back( poly );
+	std::vector<Index> poly(polygon.size());
+	for (size_t i = 0; i < polygon.size(); i++) poly[i] = (Index)polygon[i];
+	_polygons.push_back(poly);
+	return _polygons.size() - 1;
     }
     protected:
-    std::vector< std::vector< Index > > &_polygons;
+    std::vector<std::vector<Index>> &_polygons;
 };
 
-// A stream into which we can write the output vertices of the extracted mesh
-template< typename Real , unsigned int Dim >
-struct VertexStream : public Reconstructor::OutputVertexStream< Real , Dim >
+// Output vertex stream: must implement write() and size()
+template <typename Real, unsigned int Dim>
+struct VertexStream : public Reconstructor::OutputLevelSetVertexStream<Real, Dim>
 {
-    // Construct a stream that adds vertices into the coordinates
-    VertexStream( std::vector< Real > &vCoordinates ) : _vCoordinates( vCoordinates ) {}
-
-    // Override the pure abstract method from Reconstructor::OutputVertexStream< Real , Dim >
-    void base_write( Point< Real , Dim > p , Point< Real , Dim > , Real ){ for( unsigned int d=0 ; d<Dim ; d++ ) _vCoordinates.push_back( p[d] ); }
+    VertexStream(std::vector<Real> &vCoordinates) : _vCoordinates(vCoordinates) {}
+    size_t size(void) const { return _vCoordinates.size() / Dim; }
+    size_t write(const Point<Real, Dim> &p, const Point<Real, Dim> &, const Real &)
+    {
+	for (unsigned int d = 0; d < Dim; d++) _vCoordinates.push_back(p[d]);
+	return _vCoordinates.size() / Dim - 1;
+    }
     protected:
-    std::vector< Real > &_vCoordinates;
+    std::vector<Real> &_vCoordinates;
 };
-
 
 extern "C" int
 bg_3d_spsr(int **faces, int *num_faces, point_t **points, int *num_pnts,
-	   const point_t *input_points_3d, const vect_t *input_normals_3d,
-	   int num_input_pnts, struct bg_3d_spsr_opts *spsr_opts)
+	const point_t *input_points_3d, const vect_t *input_normals_3d,
+	int num_input_pnts, struct bg_3d_spsr_opts *spsr_opts)
 {
-    static const unsigned int FEMSig = FEMDegreeAndBType< Reconstructor::Poisson::DefaultFEMDegree , Reconstructor::Poisson::DefaultFEMBoundary >::Signature;
+    using Real = fastf_t;
+    static const unsigned int FEMSig = FEMDegreeAndBType<Reconstructor::Poisson::DefaultFEMDegree, Reconstructor::Poisson::DefaultFEMBoundary>::Signature;
+    using FEMSigs = IsotropicUIntPack<3, FEMSig>;
 
-    ThreadPool::Init(ThreadPool::THREAD_POOL , std::thread::hardware_concurrency());
-
-    Reconstructor::Poisson::SolutionParameters< fastf_t > solverParams;
+    // Solver and extraction parameters
+    Reconstructor::Poisson::SolutionParameters<Real> solverParams;
     solverParams.verbose = false;
     solverParams.depth = (spsr_opts) ? spsr_opts->depth : 8;
     solverParams.fullDepth = (spsr_opts) ? spsr_opts->full_depth : 11;
@@ -128,53 +123,59 @@ bg_3d_spsr(int **faces, int *num_faces, point_t **points, int *num_pnts,
 
     Reconstructor::LevelSetExtractionParameters extractionParams;
     extractionParams.forceManifold = true;
-    extractionParams.linearFit = false; // Should be true if using SSD
+    extractionParams.linearFit = false;
     extractionParams.polygonMesh = false;
     extractionParams.verbose = false;
 
-    PtStream<fastf_t, 3> vstream(input_points_3d, input_normals_3d, num_input_pnts);
+    PtStream<Real, 3> vstream(input_points_3d, input_normals_3d, num_input_pnts);
 
-    Reconstructor::Poisson::Implicit<fastf_t, 3, FEMSig> implicit(vstream, solverParams);
+    using Implicit = Reconstructor::Implicit<Real, 3, FEMSigs>;
+    using Solver = Reconstructor::Poisson::Solver<Real, 3, FEMSigs>;
+    Implicit *implicit = Solver::Solve(vstream, solverParams);
 
-    // vectors for storing the polygons (specifically, triangles) and the coordinates of the vertices
-    std::vector< std::vector< int > > polygons;
-    std::vector< fastf_t > vCoordinates;
+    if (!implicit) {
+	bu_log("PoissonRecon: Solver::Solve failed\n");
+	return -1;
+    }
 
-    // Streams backed by these vectors
-    PolygonStream< int > pStream( polygons );
-    VertexStream< fastf_t , 3 > vStream( vCoordinates );
+    std::vector<std::vector<int>> polygons;
+    std::vector<Real> vCoordinates;
+    PolygonStream<int> pStream(polygons);
+    VertexStream<Real, 3> vStream(vCoordinates);
 
-    // Extract the iso-surface
-    implicit.extractLevelSet( vStream , pStream , extractionParams );
+    implicit->extractLevelSet(vStream, pStream, extractionParams);
 
-    // Translate to libbg output types
     *num_faces = (int)(polygons.size());
-    *num_pnts = (int)(vCoordinates.size()/3);
+    *num_pnts = (int)(vCoordinates.size() / 3);
     bu_log("Point cnt: %d\n", *num_pnts);
     bu_log("Face cnt: %d\n", *num_faces);
-    (*faces) = (int *)bu_calloc(*num_faces*3, sizeof(int), "faces array");
+
+    // Allocate output arrays
+    (*faces) = (int *)bu_calloc(*num_faces * 3, sizeof(int), "faces array");
     (*points) = (point_t *)bu_calloc(*num_pnts, sizeof(point_t), "points array");
+
+    // Copy faces (triangulated output expected)
     for (int i = 0; i < *num_faces; i++) {
 	if (polygons[i].size() != 3) {
 	    bu_log("Warning: polygon %d is not a triangle (has %zu vertices)\n", i, polygons[i].size());
-	    // TODO - triangulate
+	    // TODO - triangulate if we don't have a triangle.
 	    continue;
 	}
 	(*faces)[3*i+0] = polygons[i][0];
 	(*faces)[3*i+1] = polygons[i][1];
 	(*faces)[3*i+2] = polygons[i][2];
     }
+    // Copy points
     for (int i = 0; i < *num_pnts; i++) {
 	(*points)[i][X] = vCoordinates[3*i+0];
 	(*points)[i][Y] = vCoordinates[3*i+1];
 	(*points)[i][Z] = vCoordinates[3*i+2];
     }
 
-    // All done - clean up the thread pool
-    ThreadPool::Terminate();
-
+    delete implicit;
     return 0;
 }
+
 
 // Local Variables:
 // tab-width: 8
@@ -183,4 +184,4 @@ bg_3d_spsr(int **faces, int *num_faces, point_t **points, int *num_pnts,
 // indent-tabs-mode: t
 // c-file-style: "stroustrup"
 // End:
-// ex: shiftwidth=4 tabstop=8
+// ex: shiftwidth=4 tabstop=8 cino=N-s
