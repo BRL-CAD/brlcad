6f6c14d79560941874d9f6eb5a3a73de0560e5b1
Christopher Sean Morrison <brlcad@gmail.com>
2025-08-08 17:09:46 -0400

    restore documented CLINE behavior on windows.  we just forcibly set the offset before using the structparse table

--- src/rt/do.c
+++ src/rt/do.c
@@ -501,22 +501,18 @@ parse_deprecated(const struct bu_structparse *UNUSED(sp), const char *name, void
 extern struct bu_structparse view_parse[];
 static int rt_bot_minpieces_deprecated = 0;
 
+/* librt global */
+extern fastf_t rt_cline_radius; 
+
+
 struct bu_structparse set_parse[] = {
     {"%d",	1, "width",			bu_byteoffset(width),			BU_STRUCTPARSE_FUNC_NULL, NULL, NULL },
     {"%d",	1, "height",			bu_byteoffset(height),			BU_STRUCTPARSE_FUNC_NULL, NULL, NULL },
     {"%d",	1, "save_overlaps",		bu_byteoffset(save_overlaps),		BU_STRUCTPARSE_FUNC_NULL, NULL, NULL },
     {"%f",	1, "perspective",		bu_byteoffset(rt_perspective),		BU_STRUCTPARSE_FUNC_NULL, NULL, NULL },
     {"%f",	1, "angle",			bu_byteoffset(rt_perspective),		BU_STRUCTPARSE_FUNC_NULL, NULL, NULL },
-    {"%d",	1, "rt_bot_minpieces",		bu_byteoffset(rt_bot_minpieces_deprecated),	parse_deprecated, NULL, NULL },
-#if !defined(_WIN32) || defined(__CYGWIN__)
-    /* FIXME: these cannot be listed in here because they are LIBRT
-     * globals.  due to the way symbols are not imported until a DLL
-     * is loaded on Windows, the byteoffset address of the global is
-     * not known at compile-time.  they would needed to be added to
-     * set_parse() during runtime initialization.
-     */
-    {"%f",	1, "rt_cline_radius",		bu_byteoffset(rt_cline_radius),		BU_STRUCTPARSE_FUNC_NULL, NULL, NULL },
-#endif
+    {"%d",	1, "rt_bot_minpieces", bu_byteoffset(rt_bot_minpieces_deprecated),	parse_deprecated, NULL, NULL },
+    {"%f",	1, "rt_cline_radius", 0 /* must be set manually since from lib */, 	BU_STRUCTPARSE_FUNC_NULL, NULL, NULL },
     /* daisy-chain to additional app-specific parameters */
     {"%p",	1, "Application-Specific Parameters", bu_byteoffset(view_parse[0]),	BU_STRUCTPARSE_FUNC_NULL, NULL, NULL },
     {"",	0, (char *)0,		0,						BU_STRUCTPARSE_FUNC_NULL, NULL, NULL }
@@ -529,6 +525,15 @@ struct bu_structparse set_parse[] = {
 int cm_set(const int argc, const char **argv)
 {
     struct bu_vls str = BU_VLS_INIT_ZERO;
+
+    /* FIXME: this init should happen elsewhere but neither set_parse
+     * is not exposed externally and app init is only external.
+     * rt_cline_radius must be handled separately because it's
+     * imported from librt with an address not known until runtime.
+     */
+    if (!set_parse[6].sp_offset && BU_STR_EQUAL(set_parse[6].sp_name, "rt_cline_radius")) {
+	set_parse[6].sp_offset = bu_byteoffset(rt_cline_radius);
+    }
 
     if (argc <= 1) {
 	bu_struct_print("Generic and Application-Specific Parameter Values",
