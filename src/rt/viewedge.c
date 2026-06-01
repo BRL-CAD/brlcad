63d869f8e526dc44b6531a4e0ba59879b06d7a8a
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2026-06-01 15:13:14 -0400

    No need to stage this resource lookup through BU_PTBL_GET and rti_resources

--- src/rt/viewedge.c
+++ src/rt/viewedge.c
@@ -497,7 +497,7 @@ view_init(struct application *ap, char *file, char *UNUSED(obj), int minus_o, in
 	    RT_APPLICATION_INIT(occlusion_apps[i]);
 
 	    occlusion_apps[i]->a_rt_i = occlusion_rtip;
-	    occlusion_apps[i]->a_resource = (struct resource *)BU_PTBL_GET(&occlusion_rtip->rti_resources, i);
+	    occlusion_apps[i]->a_resource = &occlusion_resources[i];
 	    occlusion_apps[i]->a_onehit = 1;
 	    occlusion_apps[i]->a_hit = occlusion_hit;
 	    occlusion_apps[i]->a_miss = occlusion_miss;
