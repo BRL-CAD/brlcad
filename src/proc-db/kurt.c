05eb37f71ebfebb70a082b9839d4afbe772bacb4
Christopher Sean Morrison <brlcad@gmail.com>
2026-06-09 21:18:05 -0400

    looks like kurt is still incomplete, but now at least renders the surface of evaluated points.  TODO: use the normals that were computed.

--- src/proc-db/kurt.c
+++ src/proc-db/kurt.c
@@ -30,8 +30,10 @@
 
 #include "vmath.h"
 #include "bu/app.h"
+#include "bu/malloc.h"
 #include "bn.h"
 #include "raytrace.h"
+#include "rt/geom.h"
 #include "wdb.h"
 
 
@@ -43,6 +45,7 @@ struct val {
 } val[20][20];
 
 void do_cell(struct val *vp, double xc, double yc);
+void make_surface(int quant);
 void pnorms(fastf_t (*norms)[3], fastf_t (*verts)[3], fastf_t *centroid, int npts);
 void do_light(char *name, fastf_t *pos, fastf_t *dir_at, int da_flag, double r, unsigned char *rgb);
 
@@ -80,6 +83,11 @@ main(int argc, char **argv)
 	    do_cell(&val[ix][iy], x, y);
 	}
     }
+
+    /* Turn the sampled grid of evaluated points into a renderable
+     * surface, then assemble a top-level region named "all".
+     */
+    make_surface(quant);
 
     db_close(outfp->dbip);
     bu_log(" done.\n");
@@ -126,6 +134,87 @@ do_cell(struct val *vp, double xc, double yc)
 	    }
 	}
     }
+}
+
+
+/*
+ * Build a renderable surface from the grid of evaluated points.
+ *
+ * The multi-valued cubic always has at least one real root for every
+ * sampled (x, y), so the lowest sheet (v_z[0]) provides a complete,
+ * hole-free height field across the whole quant x quant grid.  We
+ * triangulate that height field into a single BoT surface and wrap it
+ * in a top-level region named "all" with a pleasant material.
+ */
+void
+make_surface(int quant)
+{
+    size_t nverts;
+    size_t nfaces;
+    fastf_t *verts;
+    int *faces;
+    int ix, iy;
+    size_t v;
+    size_t f;
+    struct wmember head;
+    unsigned char rgb[3];
+
+    if (quant < 2)
+	return;
+
+    nverts = (size_t)quant * (size_t)quant;
+    nfaces = (size_t)(quant-1) * (size_t)(quant-1) * 2;
+
+    verts = (fastf_t *)bu_malloc(nverts * 3 * sizeof(fastf_t), "kurt verts");
+    faces = (int *)bu_malloc(nfaces * 3 * sizeof(int), "kurt faces");
+
+    /* Lay out one vertex per grid cell using the lowest sheet. */
+    v = 0;
+    for (ix=0; ix < quant; ix++) {
+	for (iy=0; iy < quant; iy++) {
+	    struct val *vp = &val[ix][iy];
+	    verts[v*3 + 0] = vp->v_x;
+	    verts[v*3 + 1] = vp->v_y;
+	    verts[v*3 + 2] = (vp->v_n > 0) ? vp->v_z[0] : 0.0;
+	    v++;
+	}
+    }
+
+    /* Two triangles per grid quad.  Vertex index is ix*quant + iy. */
+    f = 0;
+    for (ix=0; ix < quant-1; ix++) {
+	for (iy=0; iy < quant-1; iy++) {
+	    int v00 = ix*quant + iy;
+	    int v01 = ix*quant + (iy+1);
+	    int v10 = (ix+1)*quant + iy;
+	    int v11 = (ix+1)*quant + (iy+1);
+
+	    faces[f*3 + 0] = v00;
+	    faces[f*3 + 1] = v10;
+	    faces[f*3 + 2] = v11;
+	    f++;
+
+	    faces[f*3 + 0] = v00;
+	    faces[f*3 + 1] = v11;
+	    faces[f*3 + 2] = v01;
+	    f++;
+	}
+    }
+
+    mk_bot(outfp, "kurt.bot", RT_BOT_SURFACE, RT_BOT_UNORIENTED, 0,
+	   nverts, nfaces, verts, faces,
+	   (fastf_t *)NULL, (struct bu_bitv *)NULL);
+
+    bu_free((void *)verts, "kurt verts");
+    bu_free((void *)faces, "kurt faces");
+
+    /* Top-level region "all" with a pleasant blue-green plastic. */
+    BU_LIST_INIT(&head.l);
+    (void)mk_addmember("kurt.bot", &head.l, NULL, WMOP_UNION);
+    rgb[0] = 80;
+    rgb[1] = 160;
+    rgb[2] = 200;
+    mk_lcomb(outfp, "all", &head, 1, "plastic", "sh=10 sp=0.6 di=0.4", rgb, 0);
 }
 
 
