f81a81e72c88a75b39aacb5b4451943f757a34d0
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2026-07-14 22:52:05 -0400

    Fix header

--- regress/mged/search_exec_procs.tcl
+++ regress/mged/search_exec_procs.tcl
@@ -1,3 +1,23 @@
+#           S E A R C H _ E X E C _ P R O C S . T C L
+# BRL-CAD
+#
+# Copyright (c) 2026 United States Government as represented by
+# the U.S. Army Research Laboratory.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public License
+# version 2.1 as published by the Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this file; see the file named COPYING for more
+# information.
+#
+###
 # Procedures and aliases in this file must be available to search -exec after
 # it is sourced into MGED's main Tcl interpreter.
 
@@ -35,3 +55,12 @@ proc search_exec_verify {} {
     }
     puts "PASS search -exec sourced Tcl state"
 }
+
+# Local Variables:
+# tab-width: 8
+# mode: Tcl
+# c-basic-offset: 4
+# tcl-indent-level: 4
+# indent-tabs-mode: t
+# End:
+# ex: shiftwidth=4 tabstop=8 cino=N-s
