7f38eee8544b8195c9a72c8977b5b8f02c85f69f
Christopher Sean Morrison <brlcad@gmail.com>
2026-07-22 15:50:43 -0400

    update to the latest calling pattern 'comb obj rm'

--- src/libged/rm/rm.c
+++ src/libged/rm/rm.c
@@ -32,11 +32,11 @@
  * expanded against the database via db_path_glob.
  *
  * Path operands of the form "comb/child" remove only that parent-child
- * instance from the combination tree (analogous to "comb rm --comb").
- * Use "comb/child@N" to remove the Nth same-name child instance.
+ * instance from the combination tree (analogous to "comb obj.c rm").
+ * Use "obj.c/child@N" to remove the Nth same-name child instance.
  *
  * Batch removal of direct combination members is available through
- * "comb rm --comb combination child...".
+ * "comb obj.c rm child...".
  */
 
 #include "common.h"
@@ -328,13 +328,16 @@ _rm_check_legacy_form(struct ged *gedp, int argc, const char *argv[])
 
     _rm_vls_append_argv(&legacy_args, argc, argv);
     bu_vls_printf(gedp->ged_result_str,
-	    "rm: ambiguous request: '%s' is a combination followed by bare object names.\n"
-	    "rm: no objects were deleted.\n"
-	    "rm: to remove members from the combination, use: comb rm --comb %s\n"
-	    "rm: or use explicit paths such as: rm %s/%s\n"
-	    "rm: to force object deletion for this command, use: rm -f %s\n",
-	    argv[0], bu_vls_cstr(&legacy_args), argv[0], argv[1],
-	    bu_vls_cstr(&legacy_args));
+		  "rm: ambiguous request: '%s' is a combination followed by bare object names.\n"
+		  "rm: no objects were deleted.\n"
+		  "rm: to remove members from the combination, use: comb %s rm",
+		  argv[0], argv[0]);
+    for (i = 1; i < argc; i++)
+	bu_vls_printf(gedp->ged_result_str, " %s", argv[i]);
+    bu_vls_printf(gedp->ged_result_str,
+		  "\nrm: or use explicit paths such as: rm %s/%s\n"
+		  "rm: to force object deletion for this command, use: rm -f %s\n",
+		  argv[0], argv[1], bu_vls_cstr(&legacy_args));
     bu_vls_free(&legacy_args);
 
     return 1;
