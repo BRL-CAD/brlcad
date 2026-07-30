ab19d73403873f6061201efca21d08406ea38b4b
Christopher Sean Morrison <brlcad@gmail.com>
2026-07-30 01:12:10 -0400

    test the new cpu timers

--- src/libbu/tests/test_datetime.c
+++ src/libbu/tests/test_datetime.c
@@ -36,6 +36,196 @@
 #include "bu.h"
 
 
+enum {
+    CPU_TIMER_ITERATIONS = 1000000,
+    CPU_TIMER_SAMPLE_INTERVAL = 1024,
+    CPU_TIMER_PARALLEL_MAX_ITERATIONS = 100000000,
+    CPU_TIMER_PARALLEL_THREADS = 2
+};
+
+static const double CPU_TIMER_BUSY_SCALE = 0.000001;
+static const int64_t CPU_TIMER_PARALLEL_TARGET_NSEC = 100000000;
+static const int64_t CPU_TIMER_PARALLEL_TOLERANCE_NSEC = 50000000;
+static const int64_t CPU_TIMER_BARRIER_SLEEP_USEC = 1000;
+static const int64_t CPU_TIMER_BARRIER_TIMEOUT_USEC = 5000000;
+
+struct cpu_timer_parallel_data {
+    size_t ncpu;
+    size_t ready;
+    size_t slots_used;
+    int invalid_slot;
+    int timer_unavailable;
+    int timer_backwards;
+    int work_failed;
+    int barrier_timeout;
+    int64_t thread_delta[CPU_TIMER_PARALLEL_THREADS];
+};
+
+
+static int
+test_cpu_timer(int64_t (*timer)(void), const char *timer_name)
+{
+    int64_t time0 = timer();
+    int64_t time1 = time0;
+    int i = 0;
+    volatile double busy = 0.0;
+
+    if (time0 < 0)
+	return 0;
+
+    for (i = 0; i < CPU_TIMER_ITERATIONS; i++) {
+	busy += (double)i * CPU_TIMER_BUSY_SCALE;
+	if ((i % CPU_TIMER_SAMPLE_INTERVAL) == 0) {
+	    int64_t now = timer();
+
+	    if (now < 0)
+		bu_exit(1, "ERROR: %s became unavailable!\n", timer_name);
+	    if (now < time1)
+		bu_exit(1, "ERROR: %s went backwards!\n", timer_name);
+	    time1 = now;
+	}
+    }
+
+    if (busy < 0.0)
+	return 1;
+    if (time1 < time0)
+	bu_exit(1, "ERROR: %s went backwards!\n", timer_name);
+
+    return 0;
+}
+
+
+static void
+cpu_timer_parallel_worker(int UNUSED(cpu), void *ptr)
+{
+    struct cpu_timer_parallel_data *data = (struct cpu_timer_parallel_data *)ptr;
+    size_t slot = 0;
+    int ready = 0;
+    int valid_slot = 0;
+    int64_t wait_start = bu_gettime();
+    int64_t start = 0;
+    int64_t end = 0;
+    int i = 0;
+    volatile double busy = 0.0;
+
+    bu_semaphore_acquire(BU_SEM_GENERAL);
+    if (data->slots_used < CPU_TIMER_PARALLEL_THREADS) {
+	slot = data->slots_used;
+	data->slots_used++;
+	valid_slot = 1;
+    } else {
+	data->invalid_slot = 1;
+    }
+    data->ready++;
+    bu_semaphore_release(BU_SEM_GENERAL);
+
+    while (!ready) {
+	bu_semaphore_acquire(BU_SEM_GENERAL);
+	ready = (data->ready >= data->ncpu);
+	bu_semaphore_release(BU_SEM_GENERAL);
+	if (!ready) {
+	    int64_t now = bu_gettime();
+
+	    if (wait_start >= 0 && now >= 0 && now - wait_start > CPU_TIMER_BARRIER_TIMEOUT_USEC) {
+		bu_semaphore_acquire(BU_SEM_GENERAL);
+		data->barrier_timeout = 1;
+		bu_semaphore_release(BU_SEM_GENERAL);
+		return;
+	    }
+	    (void)bu_snooze(CPU_TIMER_BARRIER_SLEEP_USEC);
+	}
+    }
+
+    start = bu_thread_getctime();
+    if (start < 0) {
+	bu_semaphore_acquire(BU_SEM_GENERAL);
+	data->timer_unavailable = 1;
+	bu_semaphore_release(BU_SEM_GENERAL);
+	return;
+    }
+
+    end = start;
+    for (i = 0; i < CPU_TIMER_PARALLEL_MAX_ITERATIONS && end - start < CPU_TIMER_PARALLEL_TARGET_NSEC; i++) {
+	busy += (double)i * CPU_TIMER_BUSY_SCALE;
+	if ((i % CPU_TIMER_SAMPLE_INTERVAL) == 0) {
+	    end = bu_thread_getctime();
+	    if (end < start) {
+		bu_semaphore_acquire(BU_SEM_GENERAL);
+		data->timer_backwards = 1;
+		bu_semaphore_release(BU_SEM_GENERAL);
+		return;
+	    }
+	}
+    }
+
+    end = bu_thread_getctime();
+    bu_semaphore_acquire(BU_SEM_GENERAL);
+    if (end < 0) {
+	data->timer_unavailable = 1;
+    } else if (end < start) {
+	data->timer_backwards = 1;
+    } else if (busy < 0.0) {
+	data->work_failed = 1;
+    } else if (valid_slot) {
+	data->thread_delta[slot] = end - start;
+    }
+    bu_semaphore_release(BU_SEM_GENERAL);
+}
+
+
+static int
+test_cpu_timer_scope(void)
+{
+    struct cpu_timer_parallel_data data = {0, 0, 0, 0, 0, 0, 0, 0, {0, 0}};
+    int64_t process_start = bu_getctime();
+    int64_t process_end = 0;
+    int64_t process_delta = 0;
+    int64_t thread_delta = 0;
+    int64_t tolerance = CPU_TIMER_PARALLEL_TOLERANCE_NSEC;
+    size_t i = 0;
+
+    if (process_start < 0 || bu_thread_getctime() < 0)
+	return 0;
+    if (bu_avail_cpus() < CPU_TIMER_PARALLEL_THREADS)
+	return 0;
+
+    data.ncpu = CPU_TIMER_PARALLEL_THREADS;
+    bu_parallel(cpu_timer_parallel_worker, data.ncpu, &data);
+    process_end = bu_getctime();
+
+    if (process_end < process_start)
+	bu_exit(1, "ERROR: Process CPU time went backwards!\n");
+    if (data.invalid_slot)
+	bu_exit(1, "ERROR: CPU timer test recorded too many worker slots!\n");
+    if (data.timer_backwards)
+	bu_exit(1, "ERROR: Thread CPU time went backwards in parallel test!\n");
+    if (data.barrier_timeout)
+	bu_exit(1, "ERROR: CPU timer parallel workers did not start together!\n");
+    if (data.work_failed)
+	return 1;
+    if (data.timer_unavailable)
+	return 0;
+
+    process_delta = process_end - process_start;
+    for (i = 0; i < CPU_TIMER_PARALLEL_THREADS; i++)
+	thread_delta += data.thread_delta[i];
+
+    if (process_delta <= 0 || thread_delta <= 0)
+	bu_exit(1, "ERROR: CPU timer parallel test did not record elapsed CPU time!\n");
+
+    if (process_delta / 3 > tolerance)
+	tolerance = process_delta / 3;
+
+    if (process_delta + tolerance < thread_delta)
+	bu_exit(1, "ERROR: Thread CPU deltas exceed process CPU delta (%lld > %lld)!\n", (long long)thread_delta, (long long)process_delta);
+
+    if (process_delta * 4 < thread_delta * 3)
+	bu_exit(1, "ERROR: Process CPU delta does not include parallel worker time (%lld < %lld)!\n", (long long)process_delta, (long long)thread_delta);
+
+    return 0;
+}
+
+
 int
 main(int argc, char *argv[])
 {
@@ -149,6 +339,12 @@ main(int argc, char *argv[])
 	    if (!BU_STR_EQUAL(result.vls_str, "2069-09-27T05:00:49Z"))
 		return 1;
 	    break;
+	case 11:
+	    return test_cpu_timer(bu_timer_cpu, "Process CPU time");
+	case 12:
+	    return test_cpu_timer(bu_timer_cpu_thread, "Thread CPU time");
+	case 13:
+	    return test_cpu_timer_scope();
 #if 0
 	case 11:
 	    {
