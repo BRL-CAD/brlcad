BRL-CAD/brlcad

commit 4c7519fd51e343341ecbfa20217a9ed19a3933c4

we only care about the pixel count line
svn:revision:77447
svn:branch:trunk
svn:account:brlcad

bench/run.sh
@@ -1140,7 +1140,7 @@ EOF
 	ls -la *.pix*
     fi
     $VERBOSE_ECHO "DEBUG: $CMP $PIX/${bench_testname}.pix ${bench_testname}.pix"
-    cmp_result="`eval \\\"${CMP}\\\" \\\"${PIX}/${bench_testname}.pix\\\" ${bench_testname}.pix 2>&1`"
+    cmp_result="`eval \\\"${CMP}\\\" \\\"${PIX}/${bench_testname}.pix\\\" ${bench_testname}.pix 2>&1 | grep pixels`"
     ret=$?
 
     $ECHO "$cmp_result"

commit 47d0665e5a7e7efb291d77d27ab2bc0304d5d120

fix bug I introduced in 4c7519fd51e where we're testing grep result instead of pixcmp result. effectively broken the validation since benchmark would only fail validation if the output failed to include the word 'pixels'.  this restores behavior so we may see more benchmark validation diversity.

bench/run.sh
@@ -1148,9 +1148,10 @@ EOF
 	ls -la *.pix*
     fi
     $VERBOSE_ECHO "DEBUG: $CMP $PIX/${bench_testname}.pix ${bench_testname}.pix"
-    cmp_result="`eval \\\"${CMP}\\\" \\\"${PIX}/${bench_testname}.pix\\\" ${bench_testname}.pix 2>&1 | grep pixels`"
+    cmp_result="`eval \\\"${CMP}\\\" \\\"${PIX}/${bench_testname}.pix\\\" ${bench_testname}.pix 2>&1`"
     ret=$?
 
+    cmp_result="`echo \\\"$cmp_result\\\" | grep pixels`"
     $ECHO "$cmp_result"
 
     if test $ret = 0 ; then