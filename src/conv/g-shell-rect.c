f6fbd14204f707f804932720cfa67853bdfe2a19
Christopher Sean Morrison <brlcad@gmail.com>
2026-09-07 13:54:50 -0400

    ahh, update the external callers to libnmg

--- src/conv/g-shell-rect.c
+++ src/conv/g-shell-rect.c
@@ -443,12 +443,12 @@ Get_extremes(struct shell *s, struct application *ap, struct hitmiss **hitmiss,
 	a_hit = BU_LIST_LAST(hitmiss, &rd.rd_hit);
 	VMOVE(hit2, a_hit->hit.hit_point);
 
-	NMG_FREE_HITLIST(&rd.rd_hit);
+	NMG_FREE_HITLIST_RD(&rd, &rd.rd_hit);
 
 	ret = 1;
     }
 
-    NMG_FREE_HITLIST(&rd.rd_miss);
+    NMG_FREE_HITLIST_RD(&rd, &rd.rd_miss);
 
     return ret;
 }
