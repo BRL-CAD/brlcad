f5e606a2d765cbb4fe471c64d10ea7797334550a
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2026-09-15 19:23:52 +0000

    Use the X11 enablement line to report if Xmin is being used

--- misc/CMake/BRLCAD_Summary.cmake
+++ misc/CMake/BRLCAD_Summary.cmake
@@ -358,8 +358,7 @@ function(BRLCAD_Summary)
   ###################################################
 
   # Build options
-  set(BRLCAD_ENABLE_X11_LABEL "X11 support (optional) ")
-  set(BRLCAD_X11_PROVIDER_RESOLVED_LABEL "X11 provider ")
+  set(BRLCAD_X11_SUPPORT_LABEL "X11 support (optional) ")
   set(BRLCAD_ENABLE_OPENGL_LABEL "OpenGL support (optional) ")
   set(BRLCAD_ENABLE_QT_LABEL "Qt support (optional) ")
   set(BRLCAD_ENABLE_RUNTIME_DEBUG_LABEL "Run-time debuggability (optional) ")
@@ -386,7 +385,15 @@ function(BRLCAD_Summary)
   # Make sets to use for iteration over all report items
   set(BUILD_REPORT_ITEMS)
 
-  set(FEATURE_REPORT_ITEMS BRLCAD_ENABLE_OPENGL BRLCAD_ENABLE_X11 BRLCAD_ENABLE_QT BRLCAD_ENABLE_RUNTIME_DEBUG)
+  if(NOT BRLCAD_ENABLE_X11)
+    set(BRLCAD_X11_SUPPORT "OFF")
+  elseif(BRLCAD_X11_PROVIDER_RESOLVED STREQUAL "XMIN")
+    set(BRLCAD_X11_SUPPORT "Xmin")
+  else()
+    set(BRLCAD_X11_SUPPORT "System")
+  endif()
+
+  set(FEATURE_REPORT_ITEMS BRLCAD_ENABLE_OPENGL BRLCAD_X11_SUPPORT BRLCAD_ENABLE_QT BRLCAD_ENABLE_RUNTIME_DEBUG)
 
   set(
     OTHER_REPORT_ITEMS
@@ -399,10 +406,6 @@ function(BRLCAD_Summary)
     BRLCAD_INSTALL_EXAMPLE_GEOMETRY
     BRLCAD_EXTRADOCS_BUILD
   )
-
-  if(BRLCAD_ENABLE_X11)
-    list(APPEND OTHER_REPORT_ITEMS BRLCAD_X11_PROVIDER_RESOLVED)
-  endif()
 
   if(BRLCAD_ENABLE_ADDRESS_SANITIZER OR
      BRLCAD_ENABLE_UNDEFINED_SANITIZER OR
