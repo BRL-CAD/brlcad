97920fbc90eee4e66aeaf71e060bd8a899822062
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2026-09-21 22:07:58 -0400

    license info

--- src/libbu/tests/test_scan.c
+++ src/libbu/tests/test_scan.c
@@ -1,3 +1,26 @@
+/*                     T E S T _ S C A N . C
+ * BRL-CAD
+ *
+ * Copyright (c) 2026 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file test_scan.c
+ *
+ */
+
 #include "common.h"
 
 #include <string.h>
@@ -138,3 +161,13 @@ main(int UNUSED(argc), char *argv[])
 
     return test_scan();
 }
+
+/*
+ * Local Variables:
+ * tab-width: 8
+ * mode: C
+ * indent-tabs-mode: t
+ * c-file-style: "stroustrup"
+ * End:
+ * ex: shiftwidth=4 tabstop=8 cino=N-s
+ */
